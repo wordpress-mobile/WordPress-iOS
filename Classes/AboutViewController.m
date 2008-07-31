6ef9c0da89de5e43931794d5e0eed033bb674147
iammattthomas <mt@automattic.com>
2008-07-31 18:10:24 +0000

    Change wording in default preview and change version to 1.1

--- Classes/AboutViewController.m
+++ Classes/AboutViewController.m
@@ -31,7 +31,7 @@
 
 	//If you need to do additional setup after loading the view, override viewDidLoad.
 - (void)viewDidLoad {
-	[webView loadHTMLString:@"<font face=\"Helvetica\"> <p style=\"color:rgb(51,51,51);\"><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><b style=\"font-size:18px;\"><br>WordPress for iPhone</b><br>1.1 Beta<br><br>"
+	[webView loadHTMLString:@"<font face=\"Helvetica\"> <p style=\"color:rgb(51,51,51);\"><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><b style=\"font-size:18px;\"><br>WordPress for iPhone</b><br>Version 1.1<br><br>"
 	"An Open Source iPhone app for WordPress blogs.<br><br>"
 	"Designed by Automattic in Alabama. Developed by Effigent and the WordPress community.<br><br>"
 	 "For more information or to contribute to the project,<br />visit our web site at <a style=\"color:rgb(37,131,173);text-decoration:none\" href=\"http://iphone.wordpress.net/\">iphone.wordpress.org</a>.</p></font>" baseURL:nil];
