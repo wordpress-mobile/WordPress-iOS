abd70a2dec2222a5a2958a393e962d8272e46f3b
janakiram <>
2008-09-16 18:11:10 +0000

    Added new View Controller to show the Categories in Tree based hierarchy.

---
+++ Classes/WPSegmentedSelectionTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  WPSegmentedSelectionTableViewController.h
+//  WordPress
+//
+//  Created by Janakiram on 16/09/08.
+//  Copyright 2008 Effigent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "WPSelectionTableViewController.h"
+
+@interface WPSegmentedSelectionTableViewController : WPSelectionTableViewController {
+
+}
+
+@end
