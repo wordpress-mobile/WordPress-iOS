8e8816ef017bdc4b73a3679bec048cb820ee1710
janakiram <>
2008-09-24 09:30:18 +0000

    Updated the Custom Navigation Item to look properly in Landscape mode as well.

--- Classes/WPNavigationLeftButtonView.h
+++ Classes/WPNavigationLeftButtonView.h
@@ -11,7 +11,6 @@
 @interface WPNavigationLeftButtonView : UIView {
 
     UIButton *addButton;
-    UILabel *addLabel;
 }
 
 @property (nonatomic, assign) NSString *title;
--- Classes/WPNavigationLeftButtonView.m
+++ Classes/WPNavigationLeftButtonView.m
@@ -6,26 +6,18 @@
 @dynamic title;
 
 + (WPNavigationLeftButtonView *) createView  {
-	
-	UIApplication *app = [UIApplication sharedApplication];
-	UIInterfaceOrientation statusOrientation = [app statusBarOrientation];
-	if(statusOrientation == UIInterfaceOrientationLandscapeLeft || statusOrientation == UIInterfaceOrientationLandscapeRight)
-	{
-		
-		WPLog(@" UIInterfaceOrientationIsLandscape ");
-		return [[WPNavigationLeftButtonView alloc] initWithFrame:CGRectMake(0, 0, 60, 25)];
-	}  
-    WPLog(@" UIInterfaceOrientationPotrait ");
-	return [[WPNavigationLeftButtonView alloc] initWithFrame:CGRectMake(0, 0, 65, 30)];
+	return [[WPNavigationLeftButtonView alloc] initWithFrame:CGRectMake(0, 0, 65, 31)];
 }
 
 
 - (NSString *)title {
-    return addLabel.text;
+	return addButton.currentTitle;
 }
 
 - (void)setTitle:(NSString*)aTitle {
-    addLabel.text = aTitle;
+	[addButton setTitle:aTitle forState:UIControlStateNormal];
+	[addButton setTitle:aTitle forState:UIControlStateHighlighted];
+	[addButton setTitle:aTitle forState:UIControlStateSelected];
 }
 
 
@@ -33,40 +25,23 @@
     [addButton addTarget:aTarget action:action forControlEvents:UIControlEventTouchUpInside];
 }
 
-
-
 - (id)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
         // Initialization code
         CGRect rect = frame;
         addButton = [[UIButton alloc] initWithFrame:rect];
         UIImage *img = [UIImage imageNamed:@"backNav.png"];
-        [addButton setImage:img forState:UIControlStateNormal];
-        [addButton setImage:img forState:UIControlStateHighlighted];
-        [addButton setImage:img forState:UIControlStateSelected];
-        
-        rect.origin.y +=3;
-        rect.size.height -=10;
-        
-        addLabel = [[UILabel alloc] initWithFrame:rect];
-        addLabel.backgroundColor = [UIColor clearColor];
-        addLabel.lineBreakMode = UILineBreakModeTailTruncation;
-        addLabel.textColor = [UIColor whiteColor];
-        addLabel.font = [UIFont boldSystemFontOfSize:13];
-        [addLabel setTextAlignment:UITextAlignmentCenter];
-		
+        [addButton setBackgroundImage:img forState:UIControlStateNormal];
+        [addButton setBackgroundImage:img forState:UIControlStateHighlighted];
+        [addButton setBackgroundImage:img forState:UIControlStateSelected];
+		[addButton setFont:[UIFont boldSystemFontOfSize:13]];
         [self addSubview:addButton];
-        [self addSubview:addLabel];
-        
     }
     return self;
 }
 
 - (void)dealloc {
-    
-	
     [addButton release];
-    [addLabel release];
     [super dealloc];
 }
 
