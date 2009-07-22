cdf702486d22516dd7b129b1680b4a5d7116f065
quamen <>
2009-07-22 23:34:15 +0000

    Fix crash when adding a new blog.

--- Classes/BlogsViewController.m
+++ Classes/BlogsViewController.m
@@ -49,7 +49,7 @@
     self.tableView.allowsSelectionDuringEditing = YES;
 
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(blogsRefreshNotificationReceived:) name:@"BlogsRefreshNotification" object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(showBlogWithoutAnimation:) name:@"NewBlogAdded" object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(showBlogWithoutAnimation) name:@"NewBlogAdded" object:nil];
     
 }
 
