1853b86ba906d14afa79cd3f395e46854c40b404
jb6262 <>
2009-10-31 21:10:15 +0000

    These are for the testing of the new GUI idea Matt and I are discussing...

---
+++ Classes/EditPostModalViewController.h
@@ -0,0 +1,16 @@
+//
+//  EditPostModalViewController.h
+//  WordPress
+//
+//  Created by John Bickerstaff on 10/25/09.
+//  Copyright 2009 Smilodon Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface EditPostModalViewController : UIViewController {
+
+}
+
+@end
---
+++ Classes/EditPostModalViewController.m
@@ -0,0 +1,58 @@
+//
+//  EditPostModalViewController.m
+//  WordPress
+//
+//  Created by John Bickerstaff on 10/25/09.
+//  Copyright 2009 Smilodon Software. All rights reserved.
+//
+
+#import "EditPostModalViewController.h"
+
+
+@implementation EditPostModalViewController
+
+/*
+ // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+        // Custom initialization
+    }
+    return self;
+}
+*/
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+*/
+
+
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    //return (interfaceOrientation == UIInterfaceOrientationPortrait);
+	return YES;
+}
+
+
+- (void)didReceiveMemoryWarning {
+	// Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+	
+	// Release any cached data, images, etc that aren't in use.
+}
+
+- (void)viewDidUnload {
+	// Release any retained subviews of the main view.
+	// e.g. self.myOutlet = nil;
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
