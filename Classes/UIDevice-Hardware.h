7c0de7cd37462c5239041738673d6a403927e96a
frsh <>
2010-06-09 04:58:47 +0000

    Updated device names for more accurate descriptions.

--- Classes/UIDevice-Hardware.h
+++ Classes/UIDevice-Hardware.h
@@ -2,10 +2,10 @@
 
 #define IPHONE_SIMULATOR_NAMESTRING @"iPhone Simulator"
 #define IPAD_SIMULATOR_NAMESTRING @"iPad Simulator"
-#define IPHONE_1G_NAMESTRING @"iPhone 1G"
+#define IPHONE_1G_NAMESTRING @"iPhone"
 #define IPHONE_3G_NAMESTRING @"iPhone 3G"
 #define IPHONE_3GS_NAMESTRING @"iPhone 3GS"
-#define IPHONE_4G_NAMESTRING @"iPhone 4G"
+#define IPHONE_4G_NAMESTRING @"iPhone 4"
 #define IPOD_1G_NAMESTRING @"iPod Touch 1G"
 #define IPOD_2G_NAMESTRING @"iPod Touch 2G"
 #define IPAD_1G_NAMESTRING @"iPad"
