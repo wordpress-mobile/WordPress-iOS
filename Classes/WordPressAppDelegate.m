dae980dffa74e99278d0f44274ac9f627efd8abd
frsh <>
2010-09-13 17:45:06 +0000

    Adding user feedback for situations where Core Data version migration fails.

--- Classes/WordPressAppDelegate.m
+++ Classes/WordPressAppDelegate.m
@@ -330,31 +330,22 @@ static WordPressAppDelegate *wordPressApp = NULL;
 	NSError *error = nil;
     persistentStoreCoordinator_ = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
     if (![persistentStoreCoordinator_ addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:options error:&error]) {
-        /*
-         Replace this implementation with code to handle the error appropriately.
-         
-         abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. If it is not possible to recover from the error, display an alert panel that instructs the user to quit the application by pressing the Home button.
-         
-         Typical reasons for an error here include:
-         * The persistent store is not accessible;
-         * The schema for the persistent store is incompatible with current managed object model.
-         Check the error message to determine what the actual problem was.
-         
-         
-         If the persistent store is not accessible, there is typically something wrong with the file path. Often, a file URL is pointing into the application's resources directory instead of a writeable directory.
-         
-         If you encounter schema incompatibility errors during development, you can reduce their frequency by:
-         * Simply deleting the existing store:
-         [[NSFileManager defaultManager] removeItemAtURL:storeURL error:nil]
-         
-         * Performing automatic lightweight migration by passing the following dictionary as the options parameter: 
-         [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:YES],NSMigratePersistentStoresAutomaticallyOption, [NSNumber numberWithBool:YES], NSInferMappingModelAutomaticallyOption, nil];
-         
-         Lightweight migration will only work for a limited set of schema changes; consult "Core Data Model Versioning and Data Migration Programming Guide" for details.
-         
-         */
-        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-        abort();
+		NSLog(@"Error opening the database. Deleting the file and trying again.");
+		
+		//delete the sqlite file and try again
+		[[NSFileManager defaultManager] removeItemAtPath:storeURL.path error:nil];
+		if (![persistentStoreCoordinator_ addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
+			NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+			abort();
+		}
+		
+		//if the app did not quit, show the alert to inform the users that the data have been deleted
+		UIAlertView *alert = [[[UIAlertView alloc] initWithTitle:@"Error establishing database connection." 
+														 message:@"Please delete the app and reinstall." 
+														delegate:nil 
+											   cancelButtonTitle:@"OK" 
+											   otherButtonTitles:nil] autorelease];
+		[alert show];
     }    
     
     return persistentStoreCoordinator_;
