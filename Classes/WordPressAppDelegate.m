5420a44fb72f021584da05c4af3de74f88e32de2
frsh <>
2010-12-16 23:40:08 +0000

    2.6.3 App Store submission.

--- Classes/WordPressAppDelegate.m
+++ Classes/WordPressAppDelegate.m
@@ -102,9 +102,7 @@ static WordPressAppDelegate *wordPressApp = NULL;
 
 - (void)applicationDidFinishLaunching:(UIApplication *)application {
 	[self checkIfStatsShouldRun];
-#ifndef DEBUG
-#warning Need Flurry api key for distribution
-#endif
+	
     [FlurryAPI startSession:@"NPFZWR9J1MI9QU1ICU9H"]; // FIXME: set up real api key for distribution
 	[FlurryAPI setSessionReportsOnPauseEnabled:YES];
 
