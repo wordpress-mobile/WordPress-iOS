2964bdeadf514f9685db21d87e2bdad0c2768328
daniloercoli <ercoli@automattic.com>
2011-02-22 10:15:46 +0000

    fixed 704 by assigning the status of first responder to the statusCell

--- Classes/PostSettingsViewController.m
+++ Classes/PostSettingsViewController.m
@@ -137,6 +137,7 @@
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
     [self reloadData];
+	[statusTableViewCell becomeFirstResponder];
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
