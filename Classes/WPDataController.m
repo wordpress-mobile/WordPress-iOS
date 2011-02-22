be63625252147c7d9783dfd7a9d762d8548ababe
daniloercoli <ercoli@automattic.com>
2011-02-22 11:17:43 +0000

    fixed #712 by using the correct comment_parent parameter key

--- Classes/WPDataController.m
+++ Classes/WPDataController.m
@@ -556,7 +556,7 @@
     NSMutableDictionary *commentParams = [NSMutableDictionary dictionary];
     
     [commentParams setObject:comment.content forKey:@"content"];
-    [commentParams setObject:comment.parentID forKey:@"parent"];
+    [commentParams setObject:comment.parentID forKey:@"comment_parent"]; //keep attention. getComment, getComments are returning a different key "parent" that is a string.
     [commentParams setObject:comment.postID forKey:@"post_id"];
     [commentParams setObject:comment.status forKey:@"status"];
     
