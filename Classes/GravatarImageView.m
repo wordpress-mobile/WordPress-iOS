c174c7e0c2a4be4e7af285b44f450422dee966c6
daniloercoli <ercoli@automattic.com>
2011-04-04 10:55:44 +0000

    fixes #455 by adding the patch from @WillerZ

--- Classes/GravatarImageView.m
+++ Classes/GravatarImageView.m
@@ -110,7 +110,7 @@ static void addRoundedRectToPath(CGContextRef context, CGRect rect, float radius
 }
 
 - (NSURL *)gravatarURLForEmail:(NSString *)emailString {
-    NSString *emailHash = [md5(emailString) lowercaseString];
+     NSString *emailHash = md5([emailString lowercaseString]);
     NSString *url = [NSString stringWithFormat:GRAVATAR_URL, emailHash];
     return [NSURL URLWithString:url];
 }
@@ -122,7 +122,7 @@ NSString *md5(NSString *str) {
     CC_MD5(cStr, strlen(cStr), result);
     
     return [NSString stringWithFormat:
-            @"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
+			  @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
             result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7],
             result[8], result[9], result[10], result[11], result[12], result[13], result[14], result[15]
             ];
