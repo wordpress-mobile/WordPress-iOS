8ec9e36ca463c19d43ad77926dbb6e5c12f429d5
koke <jorge@automattic.com>
2012-04-17 10:02:32 +0000

    Use production URL for push notification XML-RPC. refs #631

--- Classes/Constants.h
+++ Classes/Constants.h
@@ -17,7 +17,7 @@
 #define kTextFieldFont          @"Arial"
 #define kTextViewPlaceholder	@"Tap here to begin writing."
 #define kAppStoreURL			@"http://itunes.apple.com/us/app/wordpress/id335703880?mt=8"
-#define kNotificationAuthURL	@"https://kokesbox.wordpress.com/xmlrpc.php"
+#define kNotificationAuthURL	@"https://wordpress.com/xmlrpc.php"
 
 #define kMobileReaderFakeLoaderURL		@"https://en.wordpress.com/reader/mobile/v2/loader"
 #define kMobileReaderURL		@"https://en.wordpress.com/reader/mobile/v2/?chrome=no"
