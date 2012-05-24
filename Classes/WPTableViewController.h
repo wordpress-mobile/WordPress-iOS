42a01fca982c8ceb7d3cca0c6c051da27d9adfe4
irbrad <brad@automattic.com>
2012-05-24 09:13:45 +0000

    adding new controller for combining UITableViewController based views (comments, pages, posts)

---
+++ Classes/WPTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  WPTableViewController.h
+//  WordPress
+//
+//  Created by Brad Angelcyk on 5/22/12.
+//  Copyright (c) 2012 WordPress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WPTableViewController : UITableViewController
+
+@end
