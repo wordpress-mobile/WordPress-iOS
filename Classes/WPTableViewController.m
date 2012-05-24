36d3e5efb27d1b498c8553102735c3e354e4ac5e
irbrad <brad@automattic.com>
2012-05-24 09:14:42 +0000

    removing default #warning's for a clean build.

--- Classes/WPTableViewController.m
+++ Classes/WPTableViewController.m
@@ -50,14 +50,12 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
     return 0;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
     return 0;
 }
