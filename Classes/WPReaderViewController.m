7037dad3cf6f3c07584afbaf8ed684577e8ca06a
daniloercoli <ercoli@automattic.com>
2012-06-12 15:25:00 +0000

    Remove the list item highlight in the list for tapped items in the Reader list view when on iPhone

--- Classes/WPReaderViewController.m
+++ Classes/WPReaderViewController.m
@@ -249,10 +249,15 @@
 {
     NSDictionary *article = (NSDictionary *)item;
     NSLog(@"Show detail view: %@", article);
+    
+    //Remove the list item highlight in the list for tapped items
+    if ( IS_IPHONE ) {     
+        //[self.webView stringByEvaluatingJavaScriptFromString:@"var selected = jq('#subscriptions .selected-item');selected.removeClass('selected-item');"];
+        [self.webView stringByEvaluatingJavaScriptFromString:@"Reader2.removeHighlight()"];
+    }
     [self.panelNavigationController popToRootViewControllerAnimated:NO];
     self.detailViewController.currentItem = [article JSONString];
     [self.panelNavigationController pushViewController:self.detailViewController animated:YES];
-
 }
 
 #pragma mark - webView related methods
