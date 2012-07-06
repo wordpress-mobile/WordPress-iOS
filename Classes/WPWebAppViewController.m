2ef52abecb08b60d7e92945977204bbc4c4e2eac
beaucollins <robert@automattic.com>
2012-07-06 18:12:24 +0000

    Allow rotation

--- Classes/WPWebAppViewController.m
+++ Classes/WPWebAppViewController.m
@@ -53,7 +53,7 @@
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
     // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES;
 }
 
 
