361e7b557fe1842de8339e6ace5eaae39d35932a
aerych <eric@automattic.com>
2012-09-18 21:07:26 +0000

    Login screen was not showing the correct background on the iPhone 5.

--- Classes/WPcomLoginViewController.m
+++ Classes/WPcomLoginViewController.m
@@ -50,6 +50,7 @@
     [super viewDidLoad];
 
     self.tableView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"welcome_bg_pattern"]];
+    self.tableView.backgroundView = nil;
     
     self.wpComApi = [WordPressComApi sharedApi];
 	self.footerText = @" ";
