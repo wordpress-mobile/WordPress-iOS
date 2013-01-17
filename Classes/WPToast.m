feb44c5440a0697ecd965245955be0def298e6cd
beaucollins <robert@automattic.com>
2013-01-17 21:44:43 +0000

    Refactored WPToast
    * Don't use a singleton
    * Use IBOulet to access the nib views, instead of relying on their position in the file
    * Define constants for durations
    * Remove dependency on PanelNavigationController and the app delegate

--- Classes/WPToast.m
+++ Classes/WPToast.m
@@ -7,105 +7,91 @@
 //
 
 #import <QuartzCore/QuartzCore.h>
-#import "WordPressAppDelegate.h"
-#import "PanelNavigationController.h"
 #import "WPToast.h"
 
-@implementation WPToast
+const CGFloat WPToastAnimationZoomDuration = 0.15f;
+const CGFloat WPToastAnimationFadeDuration = 0.25f;
+const CGFloat WPToastAnimationVisibleDuration = 0.35f;
 
-+ (WPToast *)sharedInstance
-{
-    static WPToast *sharedInstance = nil;
-    static dispatch_once_t onceToken = 0;
-    dispatch_once(&onceToken, ^{
-        sharedInstance = [[WPToast alloc] init];
-    });
-    return sharedInstance;
-}
+@interface WPToast ()
+@property (nonatomic, strong) IBOutlet UIView *toastView;
+@property (nonatomic, strong) IBOutlet UILabel *toastLabel;
+@property (nonatomic, strong) IBOutlet UIImageView *toastIcon;
+@end
+@implementation WPToast
 
 + (void)showToastWithMessage:(NSString *)message andImage:(UIImage *)image
 {
-    [[self sharedInstance] showToastWithMessage:message andImage:image];
+    [[[self alloc] init] showToastWithMessage:message andImage:image];
 }
 
 - (void)showToastWithMessage:(NSString *)message andImage:(UIImage *)image
 {
-    PanelNavigationController *panelNavigationController = [self panelNavigationController];
+    UIViewController *parentViewController = [[[UIApplication sharedApplication] keyWindow] rootViewController];
+    UIView *parentView = parentViewController.view;
+
+    [[NSBundle mainBundle] loadNibNamed:@"ToastView" owner:self options:nil];
+    [self.toastView setFrame:parentView.bounds];
+    [self.toastView setAlpha:0.1f];
+    [self.toastView setCenter:CGPointMake(CGRectGetMidX(parentView.bounds), CGRectGetMidY(parentView.bounds))];
+    [self.toastView.layer setCornerRadius:20.0f];
+    [parentViewController.view addSubview:self.toastView];
     
-    UIView *toastView = [[[NSBundle mainBundle] loadNibNamed:@"ToastView" owner:panelNavigationController options:nil] objectAtIndex:0];
-    [toastView setFrame:CGRectMake(0.0f, 0.0f, panelNavigationController.view.frame.size.height, panelNavigationController.view.frame.size.height)];
-    [toastView setAlpha:0.1f];
-    [toastView setCenter:CGPointMake(panelNavigationController.view.bounds.size.width / 2, panelNavigationController.view.bounds.size.height / 2)];
-    [toastView.layer setCornerRadius:20.0f];
-    [panelNavigationController.view addSubview:toastView];
+    self.toastLabel.text = message;
+    self.toastLabel.alpha = 0.0f;
     
-    UILabel *toastLabel = [[toastView subviews] objectAtIndex:0];
-    toastLabel.text = message;
-    toastLabel.alpha = 0.0f;
-    
-    UIImageView *toastIcon = [[toastView subviews] objectAtIndex:1];
-    toastIcon.image = image;
-    toastIcon.alpha = 0.0f;
-    
-    [UIView beginAnimations:@"toast_zoom_in" context:(__bridge void *)(toastView)];
-    [UIView setAnimationDuration:0.15f];
+    self.toastIcon.image = image;
+    self.toastIcon.alpha = 0.0f;
+
+    [UIView beginAnimations:@"toast_zoom_in" context:(__bridge void *)(self.toastView)];
+    [UIView setAnimationDuration:WPToastAnimationZoomDuration];
     [UIView setAnimationDelegate:self];
     [UIView setAnimationDidStopSelector:@selector(animationDidStop:finished:context:)];
     [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
-    toastView.alpha= 1.0f;
+    self.toastView.alpha= 1.0f;
     CGFloat toastOffset = 95.0f;
-    if (IS_IPHONE && UIInterfaceOrientationIsPortrait(panelNavigationController.interfaceOrientation))
+    if (IS_IPHONE && UIInterfaceOrientationIsPortrait(parentViewController.interfaceOrientation))
         toastOffset = 125.0f;
-    toastView.frame = CGRectMake((panelNavigationController.view.bounds.size.width / 2) - 95.0f, (panelNavigationController.view.bounds.size.height / 2) - toastOffset, 190.0f, 190.0f);
+    self.toastView.frame = CGRectMake((parentViewController.view.bounds.size.width / 2) - 95.0f, (parentViewController.view.bounds.size.height / 2) - toastOffset, 190.0f, 190.0f);
     [UIView commitAnimations];    
 }
 
 - (void)animationDidStop:(NSString*)animationID finished:(BOOL)finished context:(void *)context {
-    PanelNavigationController *panelNavigationController = [self panelNavigationController];
-    
+    UIViewController *parentViewController = [[[UIApplication sharedApplication] keyWindow] rootViewController];
+
     UIView *toastView = (__bridge UIView *)context;
     if([animationID isEqualToString:@"toast_zoom_in"]) {
-        UILabel *toastLabel = [[toastView subviews] objectAtIndex:0];
-        UIImageView *toastIcon = [[toastView subviews] objectAtIndex:1];
         [UIView beginAnimations:@"content_fade_in" context:(__bridge void *)(toastView)];
-        [UIView setAnimationDuration:0.35f];
+        [UIView setAnimationDuration:WPToastAnimationFadeDuration];
         [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
         [UIView setAnimationDelegate:self];
         [UIView setAnimationDidStopSelector:@selector(animationDidStop:finished:context:)];
-        toastLabel.alpha = 1.0f;
-        toastIcon.alpha = 1.0f;
+        self.toastLabel.alpha = 1.0f;
+        self.toastIcon.alpha = 1.0f;
         [UIView commitAnimations];
     } else if ([animationID isEqualToString:@"content_fade_in"]) {
-        UILabel *toastLabel = [[toastView subviews] objectAtIndex:0];
-        UIImageView *toastIcon = [[toastView subviews] objectAtIndex:1];
         [UIView beginAnimations:@"content_fade_out" context:(__bridge void *)(toastView)];
-        [UIView setAnimationDelay:0.35f];
-        [UIView setAnimationDuration:0.25f];
+        [UIView setAnimationDelay:WPToastAnimationVisibleDuration];
+        [UIView setAnimationDuration:WPToastAnimationFadeDuration];
         [UIView setAnimationCurve:UIViewAnimationCurveEaseOut];
         [UIView setAnimationDelegate:self];
         [UIView setAnimationDidStopSelector:@selector(animationDidStop:finished:context:)];
-        toastLabel.alpha = 0.0f;
-        toastIcon.alpha = 0.0f;
+        self.toastLabel.alpha = 0.0f;
+        self.toastIcon.alpha = 0.0f;
         [UIView commitAnimations];
     }else if ([animationID isEqualToString:@"content_fade_out"]) {
         [UIView beginAnimations:@"toast_zoom_out" context:(__bridge void *)(toastView)];
-        [UIView setAnimationDuration:0.15f];
+        [UIView setAnimationDuration:WPToastAnimationZoomDuration];
         [UIView setAnimationCurve:UIViewAnimationCurveEaseOut];
         CGFloat toastOffset = 95.0f;
-        if (IS_IPHONE && UIInterfaceOrientationIsPortrait(panelNavigationController.interfaceOrientation))
+        if (IS_IPHONE && UIInterfaceOrientationIsPortrait(parentViewController.interfaceOrientation))
             toastOffset = 125.0f;
-        toastView.frame = CGRectMake(panelNavigationController.view.bounds.size.width / 2, panelNavigationController.view.bounds.size.height / 2, 0.0f, 0.0f);
+        toastView.frame = CGRectMake(parentViewController.view.bounds.size.width / 2, parentViewController.view.bounds.size.height / 2, 0.0f, 0.0f);
         toastView.alpha = 0.0f;
         [UIView commitAnimations];
     } else if ([animationID isEqualToString:@"toast_zoom_out"]) {
         [toastView removeFromSuperview];
     }
-}
-
-- (PanelNavigationController *)panelNavigationController
-{
-    WordPressAppDelegate *appDelegate = [WordPressAppDelegate sharedWordPressApplicationDelegate];
-    return appDelegate.panelNavigationController;
 }
 
 @end
