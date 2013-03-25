9f0770b69dc9a32137d3f8e9782e814e49cc4edc
aerych <eric@automattic.com>
2013-03-25 10:01:56 -0500

    Stubs out Reader ViewControllers and a new datamodel version. Adds reader API calls to the WordPressComApi. Adds basic unit tests for the api -- only testing the presence of the end points at this time but something to build on.

---
+++ WordPress/Classes/ReaderPostDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReaderPostDetailViewController.h
+//  WordPress
+//
+//  Created by Eric J on 3/21/13.
+//  Copyright (c) 2013 WordPress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReaderPostDetailViewController : UIViewController
+
+@end
---
+++ WordPress/Classes/ReaderPostsViewController.h
@@ -0,0 +1,14 @@
+//
+//  ReaderPostsViewController.h
+//  WordPress
+//
+//  Created by Eric J on 3/21/13.
+//  Copyright (c) 2013 WordPress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "WPTableViewController.h"
+
+@interface ReaderPostsViewController : WPTableViewController <DetailViewDelegate>
+
+@end
