d7aa50e783bdef6d2f8f7c3fd8d101b97cd1835d
aerych <eric@automattic.com>
2013-03-26 19:02:54 -0500

    Committing progress on data model changes. Removes the 3.6 model version. Replaces it with version 11. Adds classes for the reader post and comment models. Abstracts the abstractpost and comment models so the different post and comment models share inheritance.
    Migration is up next.
    This is a work in progress, do not build from this commit.

---
+++ WordPress/Classes/ReaderComment.m
@@ -0,0 +1,15 @@
+//
+//  ReaderComment.m
+//  WordPress
+//
+//  Created by Eric J on 3/25/13.
+//  Copyright (c) 2013 WordPress. All rights reserved.
+//
+
+#import "ReaderComment.h"
+
+
+@implementation ReaderComment
+
+
+@end
---
+++ WordPress/Classes/ReaderPost.h
@@ -0,0 +1,16 @@
+//
+//  ReaderPost.h
+//  WordPress
+//
+//  Created by Eric J on 3/25/13.
+//  Copyright (c) 2013 WordPress. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+#import "AbstractPost.h"
+
+@interface ReaderPost : AbstractPost
+
+
+@end
