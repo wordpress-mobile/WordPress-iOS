65f1818c4973fad666c5d25dc9e788cb083884ce
Jorge Bernal <jorge@automattic.com>
2013-04-24 20:20:31 +0200

    Updated emoji replacements. Better performance

--- WordPress/Classes/NSString+Helpers.m
+++ WordPress/Classes/NSString+Helpers.m
@@ -59,36 +59,38 @@
 
 - (NSString *)stringByReplacingHTMLEmoticonsWithEmoji {
     NSMutableString *result = [NSMutableString stringWithString:self];
+
+    NSDictionary *replacements = @{
+                                   @"arrow": @"➡",
+                                   @"biggrin": @"😃",
+                                   @"confused": @"😕",
+                                   @"cool": @"😎",
+                                   @"cry": @"😢",
+                                   @"eek": @"😮",
+                                   @"evil": @"😈",
+                                   @"exclaim": @"❗",
+                                   @"idea": @"💡",
+                                   @"lol": @"😄",
+                                   @"mad": @"😠",
+                                   @"mrgreen": @"🐸",
+                                   @"neutral": @"😐",
+                                   @"question": @"❓",
+                                   @"razz": @"😛",
+                                   @"redface": @"😊",
+                                   @"rolleyes": @"😒",
+                                   @"sad": @"😞",
+                                   @"smile": @"😊",
+                                   @"surprised": @"😮",
+                                   @"twisted": @"👿",
+                                   @"wink": @"😉"
+                                   };
+
     NSError *error = NULL;
     NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"<img src='.*?wp-includes/images/smilies/icon_(.+?).gif'.*?class='wp-smiley' ?/?>" options:NSRegularExpressionCaseInsensitive error:&error];
     NSArray *matches = [regex matchesInString:result options:0 range:NSMakeRange(0, [result length])];
     for (NSTextCheckingResult *match in [matches reverseObjectEnumerator]) {
         NSRange range = [match rangeAtIndex:1];
         NSString *icon = [result substringWithRange:range];
-        NSDictionary *replacements = @{
-                                       @"arrow": @"➡",
-                                       @"biggrin": @"😁",
-                                       @"confused": @"😕",
-                                       @"cool": @"😎",
-                                       @"cry": @"😢",
-                                       @"eek": @"😬",
-                                       @"evil": @"😈",
-                                       @"exclaim": @"❗",
-                                       @"idea": @"💡",
-                                       @"lol": @"😁",
-                                       @"mad": @"😠",
-                                       @"mrgreen": @"🐸",
-                                       @"neutral": @"😐",
-                                       @"question": @"❓",
-                                       @"razz": @"😛",
-                                       @"redface": @"😳",
-                                       @"rolleyes": @"😯",
-                                       @"sad": @"😞",
-                                       @"smile": @"😀",
-                                       @"surprised": @"😮",
-                                       @"twisted": @"👿",
-                                       @"wink": @"😜"
-                                       };
         NSString *replacement = [replacements objectForKey:icon];
         if (replacement) {
             [result replaceCharactersInRange:[match range] withString:replacement];
