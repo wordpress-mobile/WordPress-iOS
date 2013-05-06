13a52a4fbaf63b9c7b1cef091de42feac634cf81
aerych <eric@automattic.com>
2013-05-06 17:32:11 -0500

    Updates to comment model. Refactors ReaderPostDetailViewController to inherit from WPRefreshViewController. Partially refactored to show previous content as a tableview header. Toolbar and toolbar buttons need to be implemented.

--- WordPress/Classes/ReaderComment.h
+++ WordPress/Classes/ReaderComment.h
@@ -16,6 +16,38 @@
 @property (nonatomic, strong) NSString *authorAvatarURL;
 @property (nonatomic, strong) ReaderPost *post;
 
+
+/*
+ Fetches comments for the specified post.
+ 
+ @param post The post that owns the comments.
+ @param context The managed object context to query.
+ 
+ @return Returns an array of comments.
+ */
 + (NSArray *)fetchCommentsForPost:(ReaderPost *)post withContext:(NSManagedObjectContext *)context;
+
+
+/*
+ Save or update comments for the specified post.
+ 
+ @param comments An array of comment dictionaries to save or update.
+ @param post The post that owns the comments.
+ @param context The managed object context to query.
+ 
+ @return Returns an array of posts.
+ */
++ (void)syncComments:(NSArray *)comments forPost:(ReaderPost *)post withContext:(NSManagedObjectContext *)context;
+
+
+/**
+ Create or update an existing ReaderComment with the specified dictionary.
+ 
+ @param dict A dictionary representing the ReaderComment
+ @param post The post that owns the comment.
+ @param context The Managed Object Context to fetch from.
+ */
++ (void)createOrUpdateWithDictionary:(NSDictionary *)dict forPost:(ReaderPost *)post withContext:(NSManagedObjectContext *)context;
+
 
 @end
