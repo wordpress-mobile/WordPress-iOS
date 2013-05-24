b368e43625847708764dc7920f4acedd3ba984f8
aerych <eric@automattic.com>
2013-05-24 16:42:08 -0500

    Factor out the reader's post detail view into its own class for readability.

--- WordPress/Classes/ReaderPostDetailViewController.m
+++ WordPress/Classes/ReaderPostDetailViewController.m
@@ -7,41 +7,29 @@
 //
 
 #import "ReaderPostDetailViewController.h"
-#import <DTCoreText/DTCoreText.h>
 #import <QuartzCore/QuartzCore.h>
-#import <MediaPlayer/MediaPlayer.h>
 #import <MessageUI/MFMailComposeViewController.h>
 #import "UIImageView+Gravatar.h"
 #import "WPActivities.h"
 #import "WPWebViewController.h"
-#import "ReaderMediaView.h"
-#import "ReaderImageView.h"
-#import "ReaderVideoView.h"
 #import "PanelNavigationConstants.h"
 #import "WordPressAppDelegate.h"
 #import "WordPressComApi.h"
 #import "ReaderComment.h"
 #import "ReaderCommentTableViewCell.h"
-#import "WPImageViewController.h"
-#import "WPWebVideoViewController.h"
 #import "WPToast.h"
+#import "ReaderPostDetailView.h"
 
-@interface ReaderPostDetailViewController ()<DTAttributedTextContentViewDelegate, UIActionSheetDelegate, MFMailComposeViewControllerDelegate, UITextViewDelegate>
+@interface ReaderPostDetailViewController ()<ReaderPostDetailViewDelegate, UIActionSheetDelegate, MFMailComposeViewControllerDelegate, UITextViewDelegate>
 
 @property (nonatomic, strong) ReaderPost *post;
-@property (nonatomic, strong) UIView *headerView;
-@property (nonatomic, strong) UIView *authorView;
-@property (nonatomic, strong) UIImageView *avatarImageView;
-@property (nonatomic, strong) UILabel *authorLabel;
-@property (nonatomic, strong) UILabel *dateLabel;
-@property (nonatomic, strong) UILabel *blogLabel;
+@property (nonatomic, strong) ReaderPostDetailView *headerView;
 @property (nonatomic, strong) UIBarButtonItem *commentButton;
 @property (nonatomic, strong) UIBarButtonItem *likeButton;
 @property (nonatomic, strong) UIBarButtonItem *followButton;
 @property (nonatomic, strong) UIBarButtonItem *reblogButton;
 @property (nonatomic, strong) UIBarButtonItem *shareButton;
 @property (nonatomic, strong) UIBarButtonItem *sendCommentButton;
-@property (nonatomic, strong) DTAttributedTextContentView *textContentView;
 @property (nonatomic, strong) UIView *footerView;
 @property (nonatomic, strong) UIView *commentFormPointer;
 @property (nonatomic, strong) UILabel *commentFormLabel;
@@ -49,9 +37,7 @@
 @property (nonatomic, strong) UITextView *commentTextView;
 @property (nonatomic, strong) UIButton *commentSubmitButton;
 @property (nonatomic, strong) UIActivityIndicatorView *activityView;
-@property (nonatomic, strong) NSMutableSet *mediaPlayers;
 @property (nonatomic, strong) UIActionSheet *linkOptionsActionSheet;
-@property (nonatomic, strong) NSMutableArray *mediaArray;
 @property (nonatomic, strong) NSMutableArray *comments;
 @property (nonatomic, strong) NSArray *rowHeights;
 @property (nonatomic, strong) NSFetchedResultsController *resultsController;
@@ -60,25 +46,16 @@
 
 - (void)prepareComments;
 - (void)updateRowHeightsForWidth:(CGFloat)width;
-- (void)updateLayout;
-- (void)updateMediaLayout:(ReaderMediaView *)mediaView;
 - (void)updateToolbar;
 
-- (void)handleAuthorViewTapped:(id)sender;
 - (void)handleCommentButtonTapped:(id)sender;
 - (void)handleFollowButtonTapped:(id)sender;
-- (void)handleImageLinkTapped:(id)sender;
 - (void)handleLikeButtonTapped:(id)sender;
-- (void)handleLinkTapped:(id)sender;
 - (void)handleReblogButtonTapped:(id)sender;
 - (void)handleShareButtonTapped:(id)sender;
 - (void)handleSendCommentButtonTapped:(id)sender;
-- (void)handleTitleButtonTapped:(id)sender;
-- (void)handleVideoTapped:(id)sender;
 - (void)handleCloseKeyboard:(id)sender;
-- (void)handleCloseModal:(id)sender;
 - (void)handleFooterViewTapped:(id)sender;
-- (void)handleMediaViewLoaded:(ReaderMediaView *)mediaView;
 - (BOOL)setMFMailFieldAsFirstResponder:(UIView*)view mfMailField:(NSString*)field;
 
 @end
@@ -99,7 +76,6 @@
 	self = [super initWithStyle:UITableViewStylePlain];
 	if(self) {
 		self.post = apost;
-		self.mediaArray = [NSMutableArray array];
 		self.comments = [NSMutableArray array];
 		self.rowHeights = [NSArray array];
 	}
@@ -189,79 +165,13 @@
 	self.reblogButton = [[UIBarButtonItem alloc] initWithCustomView:reblogBtn];
 	[self updateToolbar];
 	
-	CGFloat width = self.tableView.frame.size.width;
-	
-	self.headerView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, width, 190.0f)];
+	self.headerView = [[ReaderPostDetailView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, self.tableView.frame.size.width, 190.0f) post:self.post delegate:self];
 	_headerView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
 	_headerView.backgroundColor = [UIColor whiteColor];
-	
-	self.authorView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, width, 80.0f)];
-	_authorView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-	[_headerView addSubview:_authorView];
-	
-	
-	UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-	button.frame = _authorView.frame;
-	[button addTarget:self action:@selector(handleAuthorViewTapped:) forControlEvents:UIControlEventTouchUpInside];
-	[_authorView addSubview:button];
-	
-	self.avatarImageView = [[UIImageView alloc] initWithFrame:CGRectMake(10.0f, 10.0f, 60.0f, 60.0f)];
-	_avatarImageView.autoresizingMask = UIViewAutoresizingFlexibleRightMargin;
-	[_avatarImageView setImageWithURL:[NSURL URLWithString:self.post.authorAvatarURL] placeholderImage:[UIImage imageNamed:@""]];
-	[_authorView addSubview:_avatarImageView];
-	
-	self.authorLabel = [[UILabel alloc] initWithFrame:CGRectMake(75.0f, 10.0f, width - 90.0f, 20.0f)];
-	_authorLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-	_authorLabel.backgroundColor = [UIColor clearColor];
-	_authorLabel.font = [UIFont systemFontOfSize:14.0f];
-	_authorLabel.text = (self.post.author != nil) ? self.post.author : self.post.authorDisplayName;
-	_authorLabel.textColor = [UIColor grayColor];
-	[_authorView addSubview:_authorLabel];
-	
-	self.dateLabel = [[UILabel alloc] initWithFrame:CGRectMake(75.0f, 30.0f, width - 90.0f, 20.0f)];
-	_dateLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-	_dateLabel.backgroundColor = [UIColor clearColor];
-	_dateLabel.font = [UIFont systemFontOfSize:14.0f];
-	_dateLabel.text = [NSString stringWithFormat:@"%@ on", [self.post prettyDateString]];
-	_dateLabel.textColor = [UIColor grayColor];
-	[_authorView addSubview:_dateLabel];
-	
-	self.blogLabel = [[UILabel alloc] initWithFrame:CGRectMake(75.0f, 50.0f, width - 90.0f, 20.0f)];
-	_blogLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-	_blogLabel.backgroundColor = [UIColor clearColor];
-	_blogLabel.font = [UIFont systemFontOfSize:14.0f];
-	_blogLabel.text = self.post.blogName;
-	_blogLabel.textColor = [UIColor grayColor];
-	[_authorView addSubview:_blogLabel];
-	
-	self.textContentView = [[DTAttributedTextContentView alloc] initWithFrame:CGRectMake(0.0f, 80.0f, width, 100.0f)]; // Starting height is arbitrary
-	_textContentView.delegate = self;
-	_textContentView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-	_textContentView.backgroundColor = [UIColor clearColor];
-	_textContentView.edgeInsets = UIEdgeInsetsMake(0.0f, 10.0f, 0.0f, 10.0f);
-	_textContentView.shouldDrawImages = NO;
-	_textContentView.shouldDrawLinks = NO;
-	[_headerView addSubview:_textContentView];
-		
-	NSString *str = @"";
-	NSString *content = self.post.content;
-	
-	if([self.post.postTitle length] > 0) {
-		str = [NSString stringWithFormat:@"<h2 style=\"color:#000000;font-size:18px;font-weight:100;margin-bottom:8px;\">%@</h2>%@", self.post.postTitle, content];
-	} else {
-		str = content;
-	}
-	NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithDictionary:@{
-														  DTDefaultFontFamily:@"Helvetica",
-										   NSTextSizeMultiplierDocumentOption:[NSNumber numberWithFloat:1.3f]
-								 }];
-	_textContentView.attributedString = [[NSAttributedString alloc] initWithHTMLData:[str dataUsingEncoding:NSUTF8StringEncoding]
-																			 options:dict
-																  documentAttributes:NULL];
+	[self.tableView setTableHeaderView:_headerView];
 	
 	[self prepareComments];
 	[self updateRowHeightsForWidth:self.tableView.frame.size.width];
-	[self updateLayout];
 }
 
 
@@ -311,20 +221,7 @@
 
 - (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation {
 	[super didRotateFromInterfaceOrientation:fromInterfaceOrientation];
-
-	// Figure out image sizes after orientation change.
-	for (ReaderMediaView *mediaView in _mediaArray) {
-		[self updateMediaLayout:mediaView];
-	}
-	
-	// Then update the layout
-	// need to reset the layouter because otherwise we get the old framesetter or cached layout frames
-	_textContentView.layouter = nil;
-	
-	// layout might have changed due to image sizes
-	[_textContentView relayoutText];
-	
-	[self updateLayout];
+	[_headerView updateLayout];
 }
 
 
@@ -366,52 +263,6 @@
 															  tableStyle:UITableViewStylePlain
 															   cellStyle:UITableViewCellStyleDefault
 														 reuseIdentifier:@"ReaderCommentCell"];
-}
-
-
-- (void)updateLayout {
-	// Size the textContentView
-	CGRect frame = _textContentView.frame;
-	CGFloat height = [_textContentView suggestedFrameSizeToFitEntireStringConstraintedToWidth:frame.size.width].height;
-	frame.size.height = height;
-	_textContentView.frame = frame;
-	
-	frame = _headerView.frame;
-	frame.size.height = height + _textContentView.frame.origin.y + 10.0f; // + bottom padding
-	_headerView.frame = frame;
-	
-	self.tableView.tableHeaderView = _headerView;
-}
-
-
-- (void)updateMediaLayout:(ReaderMediaView *)imageView {
-
-	NSURL *url = imageView.contentURL;
-
-	CGFloat width = _textContentView.frame.size.width;
-	CGSize viewSize = imageView.image.size;
-	viewSize.width = width - (_textContentView.edgeInsets.left + _textContentView.edgeInsets.right);
-	if (viewSize.height > 0 && (viewSize.width > _textContentView.frame.size.width)) {
-
-		// The ReaderImageView view will conform to the width constraints of the _textContentView. We want the image itself to run out to the edges,
-		// so position it offset by the inverse of _textContentView's edgeInsets.
-		UIEdgeInsets edgeInsets = _textContentView.edgeInsets;
-		edgeInsets.left = 0.0f - edgeInsets.left;
-		edgeInsets.top = 0.0f;
-		edgeInsets.right = 0.0f - edgeInsets.right;
-		edgeInsets.bottom = 0.0f;
-		imageView.edgeInsets = edgeInsets;
-		
-		viewSize.height = imageView.image.size.height * (width / imageView.image.size.width);
-	}
-	
-	NSPredicate *pred = [NSPredicate predicateWithFormat:@"contentURL == %@", url];
-	
-	// update all attachments that matchin this URL (possibly multiple images with same size)
-	for (DTTextAttachment *attachment in [self.textContentView.layoutFrame textAttachmentsWithPredicate:pred]) {
-		attachment.originalSize = imageView.image.size;
-		attachment.displaySize = viewSize;
-	}
 }
 
 
@@ -464,13 +315,6 @@
 }
 
 
-- (void)handleAuthorViewTapped:(id)sender {
-	WPWebViewController *controller = [[WPWebViewController alloc] init];
-	[controller setUrl:[NSURL URLWithString:self.post.permaLink]];
-	[self.panelNavigationController pushViewController:controller animated:YES];
-}
-
-
 - (void)handleCommentButtonTapped:(id)sender {
 	if([[self.tableView visibleCells] count] == 0) {
 		if ([self.comments count] > 0) {
@@ -492,34 +336,6 @@
 }
 
 
-- (void)handleImageLinkTapped:(id)sender {
-	ReaderImageView *imageView = (ReaderImageView *)sender;
-	
-	if(imageView.linkURL) {
-		NSString *url = [imageView.linkURL absoluteString];
-		
-		BOOL matched = NO;
-		NSArray *types = @[@".png", @".jpg", @".gif", @".jpeg"];
-		for (NSString *type in types) {
-			if (NSNotFound != [url rangeOfString:type].location) {
-				matched = YES;
-				break;
-			}
-		}
-		
-		if (matched) {
-			[WPImageViewController presentAsModalWithURL:((ReaderImageView *)sender).linkURL];
-		} else {
-			WPWebViewController *controller = [[WPWebViewController alloc] init];
-			[controller setUrl:((ReaderImageView *)sender).linkURL];
-			[self.panelNavigationController pushViewController:controller animated:YES];		
-		}
-	} else {
-		[WPImageViewController presentAsModalWithImage:imageView.image];
-	}
-}
-
-
 - (void)handleLikeButtonTapped:(id)sender {
 	NSLog(@"Like Tapped");
 	[self.post toggleLikedWithSuccess:^{
@@ -528,13 +344,6 @@
 		[self updateToolbar];
 	}];
 	[self updateToolbar];
-}
-
-
-- (void)handleLinkTapped:(id)sender {
-	WPWebViewController *controller = [[WPWebViewController alloc] init];
-	[controller setUrl:((DTLinkButton *)sender).URL];
-	[self.panelNavigationController pushViewController:controller animated:YES];
 }
 
 
@@ -652,51 +461,8 @@
 }
 
 
-- (void)handleTitleButtonTapped:(id)sender {
-	WPWebViewController *controller = [[WPWebViewController alloc] init];
-	[controller setUrl:[NSURL URLWithString:self.post.permaLink]];
-	[self.panelNavigationController pushViewController:controller animated:YES];
-}
-
-
-- (void)handleVideoTapped:(id)sender {
-	ReaderVideoView *videoView = (ReaderVideoView *)sender;
-	if(videoView.contentType == ReaderVideoContentTypeVideo) {
-		
-		MPMoviePlayerViewController *controller = [[MPMoviePlayerViewController alloc] initWithContentURL:videoView.contentURL];
-		controller.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
-		controller.modalPresentationStyle = UIModalPresentationFormSheet;
-		[self.panelNavigationController presentModalViewController:controller animated:YES];
-		
-	} else {
-		// Should either be an iframe, or an object embed. In either case a src attribute should have been parsed for the contentURL.
-		// Assume this is content we can show and try to load it.
-		WPWebVideoViewController *controller = [WPWebVideoViewController presentAsModalWithURL:videoView.contentURL];
-		controller.title = (videoView.title != nil) ? videoView.title : @"Video";
-	}
-}
-
-
-- (void)handleMediaViewLoaded:(ReaderMediaView *)mediaView {
-	
-	[self updateMediaLayout:mediaView];
-	
-	// need to reset the layouter because otherwise we get the old framesetter or cached layout frames
-	self.textContentView.layouter = nil;
-	
-	// layout might have changed due to image sizes
-	[self.textContentView relayoutText];
-	
-	[self updateLayout];
-}
-
-
 - (void)handleCloseKeyboard:(id)sender {
 	[self.view endEditing:YES];
-}
-
-- (void)handleCloseModal:(id)sender {
-	[self dismissModalViewControllerAnimated:YES];
 }
 
 
@@ -886,6 +652,13 @@
 }
 
 
+#pragma mark - ReaderPostDetailView Delegate Methods
+
+- (void)readerPostDetailViewLayoutChanged {
+	self.tableView.tableHeaderView = _headerView;
+}
+
+
 #pragma mark - UITextView Delegate Methods
 
 - (void)textViewDidBeginEditing:(UITextView *)textView {
@@ -989,6 +762,7 @@
     }
 }
 
+
 #pragma mark - MFMailComposeViewControllerDelegate
 
 - (void)mailComposeController:(MFMailComposeViewController*)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError*)error {
@@ -1021,87 +795,5 @@
     return NO;
 }
 
-
-#pragma mark - DTCoreAttributedTextContentView Delegate Methods
-
-- (UIView *)attributedTextContentView:(DTAttributedTextContentView *)attributedTextContentView viewForAttributedString:(NSAttributedString *)string frame:(CGRect)frame {
-	NSDictionary *attributes = [string attributesAtIndex:0 effectiveRange:NULL];
-	
-	NSURL *URL = [attributes objectForKey:DTLinkAttribute];
-	NSString *identifier = [attributes objectForKey:DTGUIDAttribute];
-	
-	DTLinkButton *button = [[DTLinkButton alloc] initWithFrame:frame];
-	button.URL = URL;
-	button.minimumHitSize = CGSizeMake(25, 25); // adjusts it's bounds so that button is always large enough
-	button.GUID = identifier;
-	
-	// get image with normal link text
-	UIImage *normalImage = [attributedTextContentView contentImageWithBounds:frame options:DTCoreTextLayoutFrameDrawingDefault];
-	[button setImage:normalImage forState:UIControlStateNormal];
-	
-	// get image for highlighted link text
-	UIImage *highlightImage = [attributedTextContentView contentImageWithBounds:frame options:DTCoreTextLayoutFrameDrawingDrawLinksHighlighted];
-	[button setImage:highlightImage forState:UIControlStateHighlighted];
-	
-	// use normal push action for opening URL
-	[button addTarget:self action:@selector(handleLinkTapped:) forControlEvents:UIControlEventTouchUpInside];
-
-	return button;
-}
-
-
-- (UIView *)attributedTextContentView:(DTAttributedTextContentView *)attributedTextContentView viewForAttachment:(DTTextAttachment *)attachment frame:(CGRect)frame {
-	
-	if (attachment.contentType == DTTextAttachmentTypeImage) {
-		
-		ReaderImageView *imageView = [[ReaderImageView alloc] initWithFrame:frame];
-		[_mediaArray addObject:imageView];
-		imageView.linkURL = attachment.hyperLinkURL;
-		[imageView addTarget:self action:@selector(handleImageLinkTapped:) forControlEvents:UIControlEventTouchUpInside];
-		
-		if (attachment.contents) {
-			[imageView setImage:(UIImage *)attachment.contents];
-		} else {
-			[imageView setImageWithURL:attachment.contentURL
-					  placeholderImage:[UIImage imageNamed:@""]
-							   success:^(id readerImageView) {
-								   [self handleMediaViewLoaded:readerImageView];
-							   } failure:^(id readerImageView, NSError *error) {
-								   [self handleMediaViewLoaded:readerImageView];
-							   }];
-		}
-		return imageView;
-		
-	} else {
-		
-		ReaderVideoContentType videoType;
-		
-		if (attachment.contentType == DTTextAttachmentTypeVideoURL) {
-			videoType = ReaderVideoContentTypeVideo;
-		} else if (attachment.contentType == DTTextAttachmentTypeIframe) {
-			videoType = ReaderVideoContentTypeIFrame;
-		} else if (attachment.contentType == DTTextAttachmentTypeObject) {
-			videoType = ReaderVideoContentTypeEmbed;
-		} else {
-			return nil; // Can't handle whatever this is :P
-		}
-		
-		ReaderVideoView *videoView = [[ReaderVideoView alloc] initWithFrame:frame];
-		[_mediaArray addObject:videoView];
-		[videoView setContentURL:attachment.contentURL ofType:videoType success:^(id readerVideoView) {
-			[self handleMediaViewLoaded:readerVideoView];
-			
-		} failure:^(id readerVideoView, NSError *error) {
-			[self handleMediaViewLoaded:readerVideoView];
-			
-		}];
-		
-		[videoView addTarget:self action:@selector(handleVideoTapped:) forControlEvents:UIControlEventTouchUpInside];
-
-		[self handleMediaViewLoaded:videoView];
-		return videoView;
-	}
-
-}
 
 @end
