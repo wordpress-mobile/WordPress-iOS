521b9f5791796d3b30a78148689783a069ee8e15
Aaron Douglas <astralbodies@gmail.com>
2013-06-07 14:52:37 -0500

    Clearing archived log files now works; had to skip current file due to async nature of DDLog.  Added more helper text to log list.  Decreased size of back button when in log viewer for iPhone prettification.

--- WordPress/Classes/ActivityLogViewController.m
+++ WordPress/Classes/ActivityLogViewController.m
@@ -14,6 +14,7 @@
 {
     NSArray *logFiles;
     NSDateFormatter *dateFormatter;
+    DDFileLogger *fileLogger;
 }
 
 @end
@@ -24,12 +25,22 @@
 {
     self = [super initWithStyle:UITableViewStyleGrouped];
     if (self) {
+        // TODO - Replace this call with an injected value, depending on design conventions already in place
+        WordPressAppDelegate *delegate = (WordPressAppDelegate *)[[UIApplication sharedApplication] delegate];
+        fileLogger = delegate.fileLogger;
+
         dateFormatter = [[NSDateFormatter alloc] init];
         dateFormatter.dateStyle = NSDateFormatterMediumStyle;
         dateFormatter.doesRelativeDateFormatting = YES;
         dateFormatter.timeStyle = NSDateFormatterShortStyle;
 
         self.title = NSLocalizedString(@"Activity Logs", @"");
+
+        UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Logs", @"")
+                                                                       style:UIBarButtonItemStyleBordered
+                                                                      target:nil
+                                                                      action:nil];
+        [[self navigationItem] setBackBarButtonItem:backButton];
     }
 
     return self;
@@ -42,17 +53,18 @@
     self.tableView.backgroundView = nil;
     self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"settings_bg"]];
 
-    // TODO - Replace this call with an injected value, depending on design conventions already in place
-    WordPressAppDelegate *delegate = (WordPressAppDelegate *)[[UIApplication sharedApplication] delegate];
-    DDFileLogger *fileLogger = delegate.fileLogger;
-
-    logFiles = fileLogger.logFileManager.sortedLogFileInfos;
+    [self loadLogFiles];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)loadLogFiles
+{
+    logFiles = fileLogger.logFileManager.sortedLogFileInfos;
 }
 
 #pragma mark - Table view data source
@@ -82,12 +94,12 @@
     if (indexPath.section == 0) {
         DDLogFileInfo *logFileInfo = (DDLogFileInfo *)logFiles[indexPath.row];
         cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-        cell.textLabel.text = [dateFormatter stringFromDate:logFileInfo.creationDate];
+        cell.textLabel.text = indexPath.row == 0 ? NSLocalizedString(@"Current", @"") : [dateFormatter stringFromDate:logFileInfo.creationDate];
         cell.textLabel.textAlignment = NSTextAlignmentLeft;
     } else {
         cell.accessoryType = UITableViewCellAccessoryNone;
         cell.textLabel.textAlignment = NSTextAlignmentCenter;
-        cell.textLabel.text = NSLocalizedString(@"Clear Activity Logs", @"");
+        cell.textLabel.text = NSLocalizedString(@"Clear Old Activity Logs", @"");
     }
 
 
@@ -103,6 +115,14 @@
     return nil;
 }
 
+- (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section
+{
+    if (section == 0) {
+        return NSLocalizedString(@"Log files are rolled automatically each day and at most 7 files are kept.", @"");
+    }
+
+    return nil;
+}
 
 #pragma mark - Table view delegate
 
@@ -119,7 +139,15 @@
                                                                                                        forDateString:[dateFormatter stringFromDate:logFileInfo.creationDate]];
         [self.navigationController pushViewController:detailViewController animated:YES];
     } else {
+        for (DDLogFileInfo *logFileInfo in logFiles) {
+            if (logFileInfo.isArchived)
+                [[NSFileManager defaultManager] removeItemAtPath:logFileInfo.filePath error:nil];
+        }
 
+        DDLogWarn(@"All archived log files erased.");
+
+        [self loadLogFiles];
+        [self.tableView reloadData];
     }
 }
 
