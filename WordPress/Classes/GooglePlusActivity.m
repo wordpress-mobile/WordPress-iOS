931c6df8e7b5232d30153dd2562a5aa9cd14e66a
Jorge Bernal <jorge@automattic.com>
2013-06-19 10:09:22 +0200

    Added sharing to Google+

---
+++ WordPress/Classes/GooglePlusActivity.m
@@ -0,0 +1,64 @@
+//
+//  GooglePlusActivity.m
+//  WordPress
+//
+//  Created by Jorge Bernal on 6/19/13.
+//  Copyright (c) 2013 WordPress. All rights reserved.
+//
+
+#import <SVProgressHUD/SVProgressHUD.h>
+#import <google-plus-ios-sdk/GPPShare.h>
+#import <google-plus-ios-sdk/GPPSignIn.h>
+
+#import "GooglePlusActivity.h"
+
+@interface GooglePlusActivity () <GPPShareDelegate>
+
+@end
+
+@implementation GooglePlusActivity {
+    NSURL *_URL;
+}
+
+- (UIImage *)activityImage {
+	return [UIImage imageNamed:@"NNPocketActivity"];
+}
+
+- (NSString *)activityTitle {
+	return @"Google+";
+}
+
+- (NSString *)activityType {
+	return NSStringFromClass([self class]);
+}
+
+- (BOOL)canPerformWithActivityItems:(NSArray *)activityItems {
+	for (id activityItem in activityItems) {
+		if ([activityItem isKindOfClass:[NSURL class]] && [[UIApplication sharedApplication] canOpenURL:activityItem]) {
+			return YES;
+		}
+	}
+
+	return NO;
+}
+
+- (void)prepareWithActivityItems:(NSArray *)activityItems {
+	for (id activityItem in activityItems) {
+		if ([activityItem isKindOfClass:[NSURL class]]) {
+			_URL = activityItem;
+		}
+	}
+}
+
+- (void)performActivity {
+    [[GPPShare sharedInstance] setDelegate:self];
+    id<GPPShareBuilder> shareBuilder = [[GPPShare sharedInstance] shareDialog];
+    [shareBuilder setURLToShare:_URL];
+    [shareBuilder open];
+}
+
+- (void)finishedSharing:(BOOL)shared {
+    [self activityDidFinish:shared];
+}
+
+@end
