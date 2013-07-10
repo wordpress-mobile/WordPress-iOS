d06d19b3f0d41c7f9fb64b832cd9aacd684e94fc
Maxime Biais <maxime.biais@gmail.com>
2013-07-10 12:15:52 +0200

    new subview: RevisionView

---
+++ WordPress/Classes/RevisionView.h
@@ -0,0 +1,13 @@
+//
+//  RevisionView.h
+//  WordPress
+//
+//  Created by Maxime Biais on 10/07/13.
+//  Copyright (c) 2013 WordPress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RevisionView : UIView
+
+@end
---
+++ WordPress/Classes/RevisionView.m
@@ -0,0 +1,31 @@
+//
+//  RevisionView.m
+//  WordPress
+//
+//  Created by Maxime Biais on 10/07/13.
+//  Copyright (c) 2013 WordPress. All rights reserved.
+//
+
+#import "RevisionView.h"
+
+@implementation RevisionView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
