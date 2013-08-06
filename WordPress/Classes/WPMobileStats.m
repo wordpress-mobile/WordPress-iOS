b0ca50d8ec80717aa8729753838b1a4faee63c1c
Sendhil Panchadsaram <sendhilp@gmail.com>
2013-08-06 16:55:42 -0700

    Added WordPress.com stats endpoints to reader.

--- WordPress/Classes/WPMobileStats.m
+++ WordPress/Classes/WPMobileStats.m
@@ -302,6 +302,19 @@ NSString *const StatsEventAddBlogsClickedAddSelected = @"Add Blogs - Clicked Add
     [[self sharedInstance] trackEventForWPComWithSavedProperties:event];
 }
 
++ (void)pingWPComStatsEndpoint:(NSString *)statName
+{
+    int x = arc4random();
+    NSString *statsURL = [NSString stringWithFormat:@"%@%@%@%@%d" , kMobileReaderURL, @"&template=stats&stats_name=", statName, @"&rnd=", x];
+    NSMutableURLRequest* request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:statsURL  ]];
+    WordPressAppDelegate *appDelegate = (WordPressAppDelegate *)[[UIApplication sharedApplication] delegate];
+    [request setValue:[appDelegate applicationUserAgent] forHTTPHeaderField:@"User-Agent"];
+    @autoreleasepool {
+        NSURLConnection *conn = [[NSURLConnection alloc] initWithRequest:request delegate:nil];
+        [conn start];
+    }
+}
+
 + (void)clearPropertiesForAllEvents
 {
     [[self sharedInstance] clearPropertiesForAllEvents];
