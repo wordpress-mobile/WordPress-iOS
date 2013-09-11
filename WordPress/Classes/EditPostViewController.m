4e3384c5280d3ec1851be4df59d540a45ec0f63a
Sendhil Panchadsaram <sendhilp@gmail.com>
2013-09-11 15:49:08 -0700

    Fixed bug where post editor wouldn't correctly indicate disabled state
    Fixes #49.

--- WordPress/Classes/EditPostViewController.m
+++ WordPress/Classes/EditPostViewController.m
@@ -566,6 +566,13 @@ CGFloat const EditPostViewControllerTextViewOffset = 10.0;
 
     BOOL updateEnabled = self.hasChanges || self.apost.remoteStatus == AbstractPostRemoteStatusFailed;
     [self.navigationItem.rightBarButtonItem setEnabled:updateEnabled];
+
+    // Seems to be a bug with UIBarButtonItem respecting the UIControlStateDisabled text color
+    if (updateEnabled) {
+        [self.navigationItem.rightBarButtonItem setTitleTextAttributes:@{UITextAttributeFont: [WPStyleGuide regularTextFont], UITextAttributeTextColor : [UIColor whiteColor]} forState:UIControlStateNormal];
+    } else {
+        [self.navigationItem.rightBarButtonItem setTitleTextAttributes:@{UITextAttributeFont: [WPStyleGuide regularTextFont], UITextAttributeTextColor : [UIColor lightGrayColor]}  forState:UIControlStateNormal];
+    }
 }
 
 - (void)refreshUIForCurrentPost {
