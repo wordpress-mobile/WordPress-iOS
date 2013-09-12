99bd0f1fc15369e0500050c314e4699d01f824fa
Sendhil Panchadsaram <sendhilp@gmail.com>
2013-09-12 15:21:23 -0700

    Adjusting quick photo camera modal presentation style.

--- WordPress/Classes/QuickPhotoViewController.m
+++ WordPress/Classes/QuickPhotoViewController.m
@@ -175,6 +175,7 @@
     picker.mediaTypes = [NSArray arrayWithObject:(NSString *)kUTTypeImage];
     picker.allowsEditing = NO;
     picker.delegate = self;
+    picker.modalPresentationStyle = UIModalPresentationPageSheet;
     
     if (IS_IPAD && self.sourceType != UIImagePickerControllerSourceTypeCamera) {
         self.popController = [[UIPopoverController alloc] initWithContentViewController:picker];
