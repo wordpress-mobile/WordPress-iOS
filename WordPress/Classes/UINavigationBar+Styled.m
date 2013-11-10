5bda540b5b4cceb8fd9ac196d2bae46a8fbebafd
Tom <me@tomwitkin.com>
2013-11-09 16:25:17 -0800

    #386 - Removed shadows from navigation- and toolbars

--- WordPress/Classes/UINavigationBar+Styled.m
+++ WordPress/Classes/UINavigationBar+Styled.m
@@ -8,33 +8,6 @@
 
 #import "UINavigationBar+Styled.h"
 
-#import <objc/runtime.h>
-
-@implementation UINavigationBar (Styled) 
-
-- (void)layoutSubviewsWithShadows {
-    // Since we exchanged implementations, this actually calls UIKit's layoutSubviews
-    [self layoutSubviewsWithShadows];
-
-    // Super sneaky/hacky way of getting dropshadows on all our styled navbars.
-    if ([[self class] respondsToSelector:@selector(appearance)]) {
-        NSInteger shadowTag = 1;
-        UIView *shadowView = [self viewWithTag:shadowTag];
-
-        if (shadowView == nil) {
-            UIImageView *shadowImg = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"navbar_shadow"]];
-            shadowImg.frame = CGRectMake(0.0f, self.frame.size.height, self.frame.size.width, 4.0f);
-            shadowImg.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleWidth;
-            shadowImg.tag = shadowTag;
-            [self addSubview:shadowImg];
-        }
-    }
-}
-
-+ (void)load {
-    Method origMethod = class_getInstanceMethod(self, @selector(layoutSubviews));
-    Method newMethod = class_getInstanceMethod(self, @selector(layoutSubviewsWithShadows));
-    method_exchangeImplementations(origMethod, newMethod);
-}
+@implementation UINavigationBar (Styled)
 
 @end
--- WordPress/Classes/UIToolbar+Styled.m
+++ WordPress/Classes/UIToolbar+Styled.m
@@ -8,35 +8,6 @@
 
 #import "UIToolbar+Styled.h"
 
-#import <objc/runtime.h>
-
 @implementation UIToolbar (Styled)
-
-- (void)layoutSubviewsWithShadows {
-    // Since we exchanged implementations, this actually calls UIKit's layoutSubviews
-    [self layoutSubviewsWithShadows];
-
-    // Super sneaky/hacky way of getting dropshadows on all our styled toolbars.
-    if ([[self class] respondsToSelector:@selector(appearance)]) {
-        NSInteger shadowTag = 1;        
-        UIView *shadowView = [self viewWithTag:shadowTag];
-
-        if (shadowView == nil) {
-            UIImageView *shadowImg = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"toolbar_shadow"]];
-            shadowImg.frame = CGRectMake(0.0f, -4.0f, self.frame.size.width, 4.0f);
-            // Even though it feels like we want flexible top margin, we're using negative positioning
-            // so flexible bottom margin seems to do the trick for this
-            shadowImg.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleWidth;
-            shadowImg.tag = shadowTag;
-            [self addSubview:shadowImg];
-        }
-    }
-}
-
-+ (void)load {
-    Method origMethod = class_getInstanceMethod(self, @selector(layoutSubviews));
-    Method newMethod = class_getInstanceMethod(self, @selector(layoutSubviewsWithShadows));
-    method_exchangeImplementations(origMethod, newMethod);
-}
 
 @end
