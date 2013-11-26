ad87de1db52fb57d182d75250bf90c66344722d5
Mike Johnston <kineticpoet@gmail.com>
2013-11-26 10:55:52 -0800

    Change iPad tables to have a fixed and wider width

--- WordPress/Classes/WPTableViewCell.m
+++ WordPress/Classes/WPTableViewCell.m
@@ -8,16 +8,15 @@
 
 #import "WPTableViewCell.h"
 
-CGFloat const WPTableViewCellMarginPercentage = 0.2;
+CGFloat const WPTableViewFixedWidth = 500;
 
 @implementation WPTableViewCell
 
 - (void)setFrame:(CGRect)frame {
     // On iPad, add a margin around tables
     if (IS_IPAD) {
-        CGFloat inset = ceilf(self.superview.frame.size.width * WPTableViewCellMarginPercentage);
-        frame.origin.x = inset;
-        frame.size.width = self.superview.frame.size.width - 2 * inset;
+        frame.origin.x = (self.superview.frame.size.width - WPTableViewFixedWidth) / 2;
+        frame.size.width = WPTableViewFixedWidth;
     }
     [super setFrame:frame];
 }
