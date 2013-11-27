1aabc8adb8ba658ad799924e64d242e6badaa88e
Jorge Bernal <jorge@automattic.com>
2013-11-27 19:52:48 +0100

    Blog list tweaks
    - Removed edit button
    - Don't allow hiding wp.com blogs from the list
    - Separate wp.com and self hosted in sections

--- WordPress/Classes/BlogListViewController.m
+++ WordPress/Classes/BlogListViewController.m
@@ -42,7 +42,6 @@ CGFloat const blavatarImageSize = 50.f;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.navigationItem.leftBarButtonItem = self.editButtonItem;
     self.settingsButton = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Settings", nil)
                                                            style:UIBarButtonItemStylePlain
                                                           target:self
@@ -79,8 +78,6 @@ CGFloat const blavatarImageSize = 50.f;
     self.resultsController.delegate = self;
     [self.resultsController performFetch:nil];
     [self.tableView reloadData];
-
-    self.editButtonItem.enabled = ([self numSites] > 0);
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
@@ -92,21 +89,8 @@ CGFloat const blavatarImageSize = 50.f;
     return [[self.resultsController fetchedObjects] count];
 }
 
-- (void)showManageBlogsHintIfNeeded {
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-
-    if ([defaults objectForKey:HasSeenManageBlogsHintDefaultsKey]) {
-        return;
-    }
-
-    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Hidden Blogs", nil)
-                                                        message:NSLocalizedString(@"If you want to see this or other hidden blogs in the app, you can do it from Settings > Manage Blogs", nil)
-                                                       delegate:nil
-                                              cancelButtonTitle:NSLocalizedString(@"OK", nil)
-                                              otherButtonTitles:nil];
-    [alertView show];
-    [defaults setBool:YES forKey:HasSeenManageBlogsHintDefaultsKey];
-    [defaults synchronize];
+- (BOOL)hasDotComAndSelfHosted {
+    return ([[self.resultsController sections] count] > 1);
 }
 
 #pragma mark - Actions
@@ -129,17 +113,15 @@ CGFloat const blavatarImageSize = 50.f;
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
+    return [self.resultsController.sections count] + 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    NSInteger numRows = [self numSites];
-    
-    // Plus an extra row for 'Add a Site' (when not editing)
-    if (![tableView isEditing])
-        numRows += 1;
-    
-    return numRows;
+    if (section == [self sectionForAddSite]) {
+        return 1;
+    }
+    id<NSFetchedResultsSectionInfo> sectionInfo = [[self.resultsController sections] objectAtIndex:section];
+    return sectionInfo.numberOfObjects;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -156,46 +138,45 @@ CGFloat const blavatarImageSize = 50.f;
     return cell;
 }
 
-- (NSString *)tableView:(UITableView *)tableView titleForDeleteConfirmationButtonForRowAtIndexPath:(NSIndexPath *)indexPath {
-    Blog *blog = [self.resultsController objectAtIndexPath:[NSIndexPath indexPathForRow:indexPath.row inSection:0]];
-    if (blog.isWPcom) {
-        return NSLocalizedString(@"Hide", nil);
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    if (![self hasDotComAndSelfHosted]) {
+        return nil;
     }
+
+    if (section < [self sectionForAddSite]) {
+        return [[self.resultsController sectionIndexTitles] objectAtIndex:section];
+    }
+
+    return nil;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForDeleteConfirmationButtonForRowAtIndexPath:(NSIndexPath *)indexPath {
     return NSLocalizedString(@"Remove", @"Button label when removing a blog");
 }
 
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    return indexPath.row != [self rowForAddSite];
-}
-
-- (void)setEditing:(BOOL)editing animated:(BOOL)animated {
-    [super setEditing:editing animated:animated];
-    
-    // And the "Add a Site" row too
-    NSArray *rowsToChange = [NSArray arrayWithObject:[NSIndexPath indexPathForRow:[self rowForAddSite] inSection:0]];
-    
-    UITableViewRowAnimation rowAnimation = animated ? UITableViewRowAnimationFade : UITableViewRowAnimationNone;
-    [self.tableView beginUpdates];
-    if (editing) {
-        [self.tableView deleteRowsAtIndexPaths:rowsToChange withRowAnimation:rowAnimation];
-        [self.navigationItem setRightBarButtonItem:nil animated:animated];
-    } else {
-        [self.tableView insertRowsAtIndexPaths:rowsToChange withRowAnimation:rowAnimation];
-        [self.navigationItem setRightBarButtonItem:self.settingsButton animated:animated];
+    if (indexPath.section == [self sectionForAddSite]) {
+        return NO;
     }
-    [self.tableView endUpdates];
-}
 
+
+    id<NSFetchedResultsSectionInfo> sectionInfo = [[self.resultsController sections] objectAtIndex:indexPath.section];
+    if ([[sectionInfo name] isEqualToString:@"0"]) {
+        return YES;
+    }
+
+    return NO;
+}
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
         [WPMobileStats trackEventForWPCom:StatsEventSettingsRemovedBlog];
         
-        Blog *blog = [self.resultsController objectAtIndexPath:[NSIndexPath indexPathForRow:indexPath.row inSection:0]];
+        Blog *blog = [self.resultsController objectAtIndexPath:indexPath];
         if (blog.isWPcom) {
+            DDLogWarn(@"Tried to remove a WordPress.com blog. This shouldn't happen but just in case, let's hide it");
             blog.visible = NO;
             [blog dataSave];
-            [self showManageBlogsHintIfNeeded];
         } else {
             [blog remove];
         }
@@ -217,8 +198,8 @@ CGFloat const blavatarImageSize = 50.f;
     }
 }
 
-- (NSInteger)rowForAddSite {
-    return [self numSites];
+- (NSInteger)sectionForAddSite {
+    return [self.resultsController.sections count];
 }
 
 - (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath {
@@ -226,8 +207,19 @@ CGFloat const blavatarImageSize = 50.f;
     cell.accessoryType = UITableViewCellAccessoryNone;
     cell.accessoryView = nil;
     
-    if (indexPath.row < [self rowForAddSite]) {
-        Blog *blog = [self.resultsController objectAtIndexPath:[NSIndexPath indexPathForRow:indexPath.row inSection:0]];
+    if (indexPath.section == [self sectionForAddSite]) {
+        cell.textLabel.text = NSLocalizedString(@"Add a Site", @"");
+        cell.selectionStyle = UITableViewCellSelectionStyleBlue;
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+
+        // To align the label, create and add a blank image
+        UIGraphicsBeginImageContextWithOptions(CGSizeMake(blavatarImageSize, blavatarImageSize), NO, 0.0);
+        UIImage *blank = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        [cell.imageView setImage:blank];
+    } else {
+
+        Blog *blog = [self.resultsController objectAtIndexPath:indexPath];
         if ([blog.blogName length] != 0) {
             cell.textLabel.text = blog.blogName;
         } else {
@@ -236,39 +228,25 @@ CGFloat const blavatarImageSize = 50.f;
         
         [cell.imageView setImageWithBlavatarUrl:blog.blavatarUrl isWPcom:blog.isWPcom];
         cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-        
-//        if (indexPath.row == 0) {
-//            [self maskImageView:cell.imageView corner:UIRectCornerTopLeft];
-//        } else if (indexPath.row == ([self.tableView numberOfRowsInSection:indexPath.section] -1)) {
-//            [self maskImageView:cell.imageView corner:UIRectCornerBottomLeft];
-//        } else {
-//            cell.imageView.layer.mask = NULL;
-//        }
-    } else {
-        cell.textLabel.text = NSLocalizedString(@"Add a Site", @"");
-        cell.selectionStyle = UITableViewCellSelectionStyleBlue;
-        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-        
-        // To align the label, create and add a blank image
-        UIGraphicsBeginImageContextWithOptions(CGSizeMake(blavatarImageSize, blavatarImageSize), NO, 0.0);
-        UIImage *blank = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        [cell.imageView setImage:blank];
     }
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
     // No top margin on iPhone
-    if (IS_IPHONE)
-        return 1;
-    
-    return 40;
+    if (section == [self sectionForAddSite]) {
+        return 20.0;
+    }
+    if (IS_IPHONE && section == 0) {
+        return 36.0;
+    }
+
+    return 40.0;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     
-    if (indexPath.row < [self rowForAddSite]) {
+    if (indexPath.section < [self sectionForAddSite]) {
         [WPMobileStats trackEventForWPCom:StatsEventSettingsClickedEditBlog];
         
         Blog *blog = [self.resultsController objectAtIndexPath:indexPath];
@@ -316,7 +294,7 @@ CGFloat const blavatarImageSize = 50.f;
     _resultsController = [[NSFetchedResultsController alloc]
                           initWithFetchRequest:fetchRequest
                           managedObjectContext:moc
-                          sectionNameKeyPath:nil
+                          sectionNameKeyPath:@"isWPcom"
                           cacheName:nil];
     _resultsController.delegate = self;
     
@@ -368,6 +346,28 @@ CGFloat const blavatarImageSize = 50.f;
                                                     arrayWithObject:newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
             break;
     }
+}
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeSection:(id<NSFetchedResultsSectionInfo>)sectionInfo atIndex:(NSUInteger)sectionIndex forChangeType:(NSFetchedResultsChangeType)type {
+    switch (type) {
+        case NSFetchedResultsChangeInsert:
+            [self.tableView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+
+        case NSFetchedResultsChangeDelete:
+            [self.tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+
+        default:
+            break;
+    }
+}
+
+- (NSString *)controller:(NSFetchedResultsController *)controller sectionIndexTitleForSectionName:(NSString *)sectionName {
+    if ([sectionName isEqualToString:@"1"]) {
+        return [NSString stringWithFormat:NSLocalizedString(@"%@'s blogs", @"Section header for WordPress.com blogs"), [[WPAccount defaultWordPressComAccount] username]];
+    }
+    return NSLocalizedString(@"Self Hosted", @"Section header for self hosted blogs");
 }
 
 #pragma mark - UIViewControllerTransitioningDelegate
