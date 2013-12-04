e42826a01664ac69f0c9780bda5df404cd3e2f04
Tom <me@tomwitkin.com>
2013-12-04 12:43:17 -0800

    Fixed incorrectly named instance variables

--- WordPress/Classes/WPAnimatedBox.m
+++ WordPress/Classes/WPAnimatedBox.m
@@ -10,11 +10,11 @@
 
 @interface WPAnimatedBox () {
     
-    UIImageView *container;
-    UIImageView *page1;
-    UIImageView *page2;
-    UIImageView *page3;
-    BOOL isPreparedToAnimate;
+    UIImageView *_container;
+    UIImageView *_page1;
+    UIImageView *_page2;
+    UIImageView *_page3;
+    BOOL _isPreparedToAnimate;
 }
 
 @end
@@ -32,75 +32,75 @@ static CGFloat const WPAnimatedBoxAnimationTolerance = 5.0;
 
 - (void)setupView {
     
-    container = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"animatedBox"]];
-    [container sizeToFit];
+    _container = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"animatedBox"]];
+    [_container sizeToFit];
     
-    page1 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"animatedBoxPage1"]];
-    [page1 sizeToFit];
+    _page1 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"animatedBoxPage1"]];
+    [_page1 sizeToFit];
     
-    page2 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"animatedBoxPage2"]];
-    [page2 sizeToFit];
+    _page2 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"animatedBox_page2"]];
+    [_page2 sizeToFit];
     
-    page3 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"animatedBoxPage3"]];
-    [page3 sizeToFit];
+    _page3 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"animatedBox_page3"]];
+    [_page3 sizeToFit];
     
     // view's are laid out by pixels for accuracy
     self.frame = CGRectMake(0, 0, WPAnimatedBoxSideLength, WPAnimatedBoxSideLength);
-    container.frame = CGRectMake(0, CGRectGetHeight(self.frame) - CGRectGetHeight(container.frame), CGRectGetWidth(container.frame), CGRectGetHeight(container.frame));
-    page1.frame = CGRectMake(28, WPAnimatedBoxAnimationTolerance + 11, CGRectGetWidth(page1.frame), CGRectGetHeight(page1.frame));
-    page2.frame = CGRectMake(17, WPAnimatedBoxAnimationTolerance + 0, CGRectGetWidth(page2.frame), CGRectGetHeight(page2.frame));
-    page3.frame = CGRectMake(2, WPAnimatedBoxAnimationTolerance + 15, CGRectGetWidth(page3.frame), CGRectGetHeight(page3.frame));
+    _container.frame = CGRectMake(0, CGRectGetHeight(self.frame) - CGRectGetHeight(_container.frame), CGRectGetWidth(_container.frame), CGRectGetHeight(_container.frame));
+    _page1.frame = CGRectMake(28, WPAnimatedBoxAnimationTolerance + 11, CGRectGetWidth(_page1.frame), CGRectGetHeight(_page1.frame));
+    _page2.frame = CGRectMake(17, WPAnimatedBoxAnimationTolerance + 0, CGRectGetWidth(_page2.frame), CGRectGetHeight(_page2.frame));
+    _page3.frame = CGRectMake(2, WPAnimatedBoxAnimationTolerance + 15, CGRectGetWidth(_page3.frame), CGRectGetHeight(_page3.frame));
     
-    [self addSubview:container];
-    [self insertSubview:page1 belowSubview:container];
-    [self insertSubview:page2 belowSubview:page1];
-    [self insertSubview:page3 belowSubview:page2];
+    [self addSubview:_container];
+    [self insertSubview:_page1 belowSubview:_container];
+    [self insertSubview:_page2 belowSubview:_page1];
+    [self insertSubview:_page3 belowSubview:_page2];
     
     // add motion effects
     UIInterpolatingMotionEffect *page1MotionEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"frame.origin.y" type:UIInterpolatingMotionEffectTypeTiltAlongVerticalAxis];
     page1MotionEffect.minimumRelativeValue = [NSNumber numberWithInt:4];
     page1MotionEffect.maximumRelativeValue = [NSNumber numberWithInt:-4];
-    [page1 addMotionEffect:page1MotionEffect];
+    [_page1 addMotionEffect:page1MotionEffect];
     
-    UIInterpolatingMotionEffect *page3MotionEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"frame.origin.y" type:UIInterpolatingMotionEffectTypeTiltAlongVerticalAxis];
-    page3MotionEffect.minimumRelativeValue = [NSNumber numberWithInt:7];
-    page3MotionEffect.maximumRelativeValue = [NSNumber numberWithInt:-7];
-    [page3 addMotionEffect:page3MotionEffect];
+    UIInterpolatingMotionEffect *_page3MotionEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"frame.origin.y" type:UIInterpolatingMotionEffectTypeTiltAlongVerticalAxis];
+    _page3MotionEffect.minimumRelativeValue = [NSNumber numberWithInt:7];
+    _page3MotionEffect.maximumRelativeValue = [NSNumber numberWithInt:-7];
+    [_page3 addMotionEffect:_page3MotionEffect];
     
     self.clipsToBounds = YES;
 }
 
 - (void)prepareAnimation:(BOOL)animated {
     
-    if (isPreparedToAnimate) {
+    if (_isPreparedToAnimate) {
         return;
     }
     
     [UIView animateWithDuration:animated ? 0.2 : 0.0
                      animations:^{
                          // Transform pages all the way down
-                         NSArray *pages = @[page1, page2, page3];
+                         NSArray *pages = @[_page1, _page2, _page3];
                          for (UIView *view in pages) {
                              CGFloat YOrigin = CGRectGetMinY(view.frame);
                              view.transform = CGAffineTransformMakeTranslation(0, CGRectGetHeight(self.frame) - YOrigin);
                          }
                      }];
     
-    isPreparedToAnimate = YES;
+    _isPreparedToAnimate = YES;
 }
 
 - (void)animate {
     
-    isPreparedToAnimate = NO;
+    _isPreparedToAnimate = NO;
     
     [UIView animateWithDuration:1.4 delay:0.1 usingSpringWithDamping:0.5 initialSpringVelocity:0.1 options:UIViewAnimationOptionCurveEaseOut animations:^{
-        page1.transform = CGAffineTransformIdentity;
+        _page1.transform = CGAffineTransformIdentity;
     } completion:nil];
     [UIView animateWithDuration:1 delay:0.0 usingSpringWithDamping:0.65 initialSpringVelocity:0.01 options:UIViewAnimationOptionCurveEaseOut animations:^{
-        page2.transform = CGAffineTransformIdentity;
+        _page2.transform = CGAffineTransformIdentity;
     } completion:nil];
     [UIView animateWithDuration:1.2 delay:0.2 usingSpringWithDamping:0.5 initialSpringVelocity:0.1 options:UIViewAnimationOptionCurveEaseOut animations:^{
-        page3.transform = CGAffineTransformIdentity;
+        _page3.transform = CGAffineTransformIdentity;
     } completion:nil];
 }
 
