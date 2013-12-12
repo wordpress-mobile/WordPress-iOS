486533cfcb9fdd733fa0d4baafc4032d180aaf07
Aaron Douglas <astralbodies@gmail.com>
2013-12-12 07:38:14 -0600

    Remove Select button and dismiss on tap of blog every time

--- WordPress/Classes/BlogSelectorViewController.m
+++ WordPress/Classes/BlogSelectorViewController.m
@@ -26,7 +26,7 @@ static CGFloat const blavatarImageSize = 50.f;
 @property (nonatomic) BOOL sectionDeletedByController;
 
 @property (nonatomic, strong) NSManagedObjectID *selectedObjectID;
-@property (nonatomic, copy) void (^selectedCompletionHandler)(NSManagedObjectID *selectedObjectID, BOOL finished);
+@property (nonatomic, copy) void (^selectedCompletionHandler)(NSManagedObjectID *selectedObjectID);
 @property (nonatomic, copy) void (^cancelCompletionHandler)(void);
 
 @end
@@ -34,7 +34,7 @@ static CGFloat const blavatarImageSize = 50.f;
 @implementation BlogSelectorViewController
 
 - (id)initWithSelectedBlogObjectID:(NSManagedObjectID *)objectID
-                selectedCompletion:(void (^)(NSManagedObjectID *, BOOL))selected
+                selectedCompletion:(void (^)(NSManagedObjectID *))selected
                   cancelCompletion:(void (^)())cancel {
     self = [super initWithStyle:UITableViewStylePlain];
     
@@ -63,20 +63,6 @@ static CGFloat const blavatarImageSize = 50.f;
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(wordPressComApiDidLogin:) name:WordPressComApiDidLoginNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(wordPressComApiDidLogout:) name:WordPressComApiDidLogoutNotification object:nil];
 
-    // Remove one-pixel gap resulting from a top-aligned grouped table view
-    if (IS_IPHONE) {
-        UIEdgeInsets tableInset = [self.tableView contentInset];
-        tableInset.top = -1;
-        self.tableView.contentInset = tableInset;
-
-        UIBarButtonItem *selectButtonItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Select", @"")
-                                                                             style:UIBarButtonItemStylePlain
-                                                                            target:self
-                                                                            action:@selector(selectButtonTapped:)];
-        
-        self.navigationItem.rightBarButtonItem = selectButtonItem;
-    }
-    
     [WPStyleGuide configureColorsForView:self.view andTableView:self.tableView];
  
     [self.tableView registerClass:[WPTableViewCell class] forCellReuseIdentifier:BlogCellIdentifier];
@@ -121,12 +107,6 @@ static CGFloat const blavatarImageSize = 50.f;
 - (IBAction)cancelButtonTapped:(id)sender {
     if (self.cancelCompletionHandler) {
         self.cancelCompletionHandler();
-    }
-}
-
-- (IBAction)selectButtonTapped:(id)sender {
-    if (self.selectedCompletionHandler) {
-        self.selectedCompletionHandler(self.selectedObjectID, YES);
     }
 }
 
@@ -244,8 +224,13 @@ static CGFloat const blavatarImageSize = 50.f;
         [tableView reloadRowsAtIndexPaths:@[previousIndexPath] withRowAnimation:UITableViewRowAnimationNone];
     }
 
+    // Fire off the selection after a short delay to let animations complete for selection/deselection
     if (self.selectedCompletionHandler) {
-        self.selectedCompletionHandler(self.selectedObjectID, IS_IPAD ? YES : NO);
+        double delayInSeconds = 0.2;
+        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+            self.selectedCompletionHandler(self.selectedObjectID);
+        });
     }
 }
 
