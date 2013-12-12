385ab0b2e1d60ca9c4ac0ebbeec142048be19843
Beau Collins <beaucollins@gmail.com>
2013-12-12 14:45:39 -0800

    Custom views for displaying an inline composer
    Uses a 'hidden' text view that uses an accessory view with a text view in it that is then displayed with the keyboard.
    A delgate interface on top of InlineComposeView will provide the interaction that UIViewControllers will use to get the content from the text field

---
+++ WordPress/Classes/InlineComposeToolbarView.m
@@ -0,0 +1,31 @@
+//
+//  InlineComposeToolbarView.m
+//  WordPress
+//
+//  Created by Beau Collins on 12/12/13.
+//  Copyright (c) 2013 WordPress. All rights reserved.
+//
+
+#import "InlineComposeToolbarView.h"
+
+@implementation InlineComposeToolbarView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
