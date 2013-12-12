faf8f68f5b20ae683b3d0f7e4ca06f5a8bc7ff98
Tom <me@tomwitkin.com>
2013-12-12 14:40:57 -0800

    Created new cell class WPContentCell

---
+++ WordPress/Classes/WPContentCell.h
@@ -0,0 +1,21 @@
+//
+//  WPContentCell.h
+//  
+//
+//  Created by Tom Witkin on 12/12/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WPTableViewCell.h"
+
+@class AbstractPost;
+@interface WPContentCell : WPTableViewCell
+
+@property (readwrite, weak) AbstractPost *post;
+
+- (void)runSpinner:(BOOL)value;
++ (CGFloat)rowHeightForPost:(AbstractPost *)post andWidth:(CGFloat)width;
+
+@end
