4602310db04e9e1ddaa1143e5bd6409851ce92a0
Mike Johnston <kineticpoet@gmail.com>
2013-12-14 15:26:53 -0800

    Display comments using Reader renderer (native)

--- WordPress/Classes/CommentView.m
+++ WordPress/Classes/CommentView.m
@@ -9,6 +9,7 @@
 #import "CommentView.h"
 #import "WPContentViewSubclass.h"
 #import "UIImageView+Gravatar.h"
+#import "NSAttributedString+HTML.h"
 
 @implementation CommentView
 
@@ -16,6 +17,8 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
+        UIView *contentView = [self viewForFullContent];
+        [self addSubview:contentView];
     }
     return self;
 }
@@ -24,6 +27,26 @@
     [super configureContentView:contentProvider];
     
     [self.avatarImageView setImageWithGravatarEmail:[contentProvider gravatarEmailForDisplay] fallbackImage:[UIImage imageNamed:@"comment-default-gravatar-image"]];
+    
+    NSString *partialHtml = [contentProvider contentForDisplay];
+    NSString *fullHtml;
+    
+    // The comment content is...partly HTML. But not completely. For unknown reasons.
+    if (partialHtml == nil) {
+        fullHtml = [NSString stringWithFormat:@"<html><head></head><body><p>%@</p></body></html>", @"<br />"];
+    } else {
+        fullHtml = [NSString stringWithFormat:@"<html><head></head><body><p>%@</p></body></html>", [[partialHtml trim] stringByReplacingOccurrencesOfString:@"\n" withString:@"<br />"]];
+    }
+
+    NSData *data = [fullHtml dataUsingEncoding:NSUTF8StringEncoding];
+    self.textContentView.attributedString = [[NSAttributedString alloc] initWithHTMLData:data
+                                                                                 options:[WPStyleGuide defaultDTCoreTextOptions]
+                                                                      documentAttributes:nil];
+    [self.textContentView relayoutText];
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];    
 }
 
 @end
