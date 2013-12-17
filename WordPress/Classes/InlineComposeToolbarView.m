e76554e3b596035e0817e6742a938578e9a86d1d
Beau Collins <beaucollins@gmail.com>
2013-12-17 09:18:03 -0800

    Remove unused stub method

--- WordPress/Classes/InlineComposeToolbarView.m
+++ WordPress/Classes/InlineComposeToolbarView.m
@@ -89,13 +89,4 @@ CGFloat InlineComposeToolbarViewMinToolbarWidth = 320.f;
 
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
-
 @end
