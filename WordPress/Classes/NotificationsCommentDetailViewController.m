3b5baa405be2173dd103c70d55b7816c243db66b
Tom <me@tomwitkin.com>
2013-12-20 09:47:41 -0800

    Fixed comments view issue on iPhone

--- WordPress/Classes/NotificationsCommentDetailViewController.m
+++ WordPress/Classes/NotificationsCommentDetailViewController.m
@@ -85,9 +85,9 @@ const CGFloat NotificationsCommentDetailViewControllerReplyTextViewDefaultHeight
     self.commentView.delegate = self;
     
     WPFixedWidthScrollView *scrollView = [[WPFixedWidthScrollView alloc] initWithRootView:self.commentView];
-    scrollView.contentInset = UIEdgeInsetsMake(WPTableViewTopMargin, 0, WPTableViewTopMargin, 0);
     scrollView.alwaysBounceVertical = YES;
     if (IS_IPAD) {
+        scrollView.contentInset = UIEdgeInsetsMake(WPTableViewTopMargin, 0, WPTableViewTopMargin, 0);
         scrollView.contentWidth = WPTableViewFixedWidth;
     };
     self.view = scrollView;
