9bf2e294a2b58e62711c3975dfd764fcd5ee0d01
Aaron Douglas <astralbodies@gmail.com>
2013-12-26 14:55:10 -0600

    Get rid of the NSCache for cells - UITableView knows how to cache cells
    Blanks rows were appearing erratically while scrolling around a notification comment thread.

--- WordPress/Classes/NotificationsCommentDetailViewController.m
+++ WordPress/Classes/NotificationsCommentDetailViewController.m
@@ -581,7 +581,7 @@ NS_ENUM(NSUInteger, NotifcationCommentCellType){
         case NotificationCommentCellTypeHeader:
         {
             if (comment.isLoaded || mainComment) {
-                NoteCommentCell *headerCell = [tableView dequeueReusableCellWithIdentifier:NoteCommentHeaderCellIdentifiter];
+                NoteCommentCell *headerCell = [tableView dequeueReusableCellWithIdentifier:NoteCommentHeaderCellIdentifiter forIndexPath:indexPath];
                 if ([comment isParentComment]) {
                     [headerCell displayAsParentComment];
                 }
@@ -590,20 +590,14 @@ NS_ENUM(NSUInteger, NotifcationCommentCellType){
                 cell = headerCell;
 
             } else {
-                NoteCommentLoadingCell *loadingCell = [tableView dequeueReusableCellWithIdentifier:NoteCommentLoadingCellIdentifiter];
+                NoteCommentLoadingCell *loadingCell = [tableView dequeueReusableCellWithIdentifier:NoteCommentLoadingCellIdentifiter forIndexPath:indexPath];
                 cell = loadingCell;
             }
             break;
         }
         case NotificationCommentCellTypeContent:
         {
-            NoteCommentContentCell *contentCell = [tableView dequeueReusableCellWithIdentifier:NoteCommentContentCellIdentifiter];
-            if ([self.contentCache objectForKey:comment]) {
-                contentCell = [self.contentCache objectForKey:comment];
-            } else {
-                [self.contentCache setObject:contentCell forKey:comment];
-            }
-            
+            NoteCommentContentCell *contentCell = [tableView dequeueReusableCellWithIdentifier:NoteCommentContentCellIdentifiter forIndexPath:indexPath];
             contentCell.delegate = self;
             NSString *html = [comment.commentData valueForKey:@"content"];
             if (!html) {
