7bd81a7f9b14d1477b040bb746128a44899c3803
Rebecca <h4xnoodle@gmail.com>
2014-01-10 03:32:21 -0500

    Darker graph colour, format x axis labels

--- WordPress/Classes/StatsViewsVisitors.m
+++ WordPress/Classes/StatsViewsVisitors.m
@@ -17,6 +17,7 @@ NSString *const StatsPointCountKey = @"count";
 @interface StatsViewsVisitors ()
 
 @property (nonatomic, strong) NSMutableDictionary *viewsVisitorsData;
+@property (nonatomic, strong) NSDateFormatter *dateFormatter;
 
 @end
 
@@ -30,14 +31,26 @@ NSString *const StatsPointCountKey = @"count";
     return self;
 }
 
+- (NSDateFormatter *)dateFormatter {
+    if (_dateFormatter) {
+        return _dateFormatter;
+    }
+    
+    _dateFormatter = [[NSDateFormatter alloc] init];
+    _dateFormatter.locale = [NSLocale currentLocale];
+    return _dateFormatter;
+}
+
 - (void)addViewsVisitorsWithData:(NSDictionary *)data unit:(StatsViewsVisitorsUnit)unit {
     NSMutableArray *periodToViews = [NSMutableArray array];
     NSMutableArray *periodToVisitors = [NSMutableArray array];
     NSArray *periodData = data[@"data"];
     [periodData enumerateObjectsUsingBlock:^(NSArray *d, NSUInteger idx, BOOL *stop) {
-        [periodToViews addObject:@{StatsPointNameKey: d[0], StatsPointCountKey: d[1]}];
-        [periodToVisitors addObject:@{StatsPointNameKey: d[0], StatsPointCountKey: d[2]}];
+        [periodToViews addObject:@{StatsPointNameKey: [self nicePointName:d[0] forUnit:unit], StatsPointCountKey: d[1]}];
+        [periodToVisitors addObject:@{StatsPointNameKey: [self nicePointName:d[0] forUnit:unit], StatsPointCountKey: d[2]}];
     }];
+    
+    self.dateFormatter = nil;
     
     _viewsVisitorsData[@(unit)] = @{StatsViewsCategory: periodToViews,
                                     StatsVisitorsCategory: periodToVisitors};
@@ -45,6 +58,31 @@ NSString *const StatsPointCountKey = @"count";
 
 - (NSDictionary *)viewsVisitorsForUnit:(StatsViewsVisitorsUnit)unit {
     return _viewsVisitorsData[@(unit)];
+}
+
+- (NSString *)nicePointName:(NSString *)name forUnit:(StatsViewsVisitorsUnit)unit {
+    switch (unit) {
+        case StatsViewsVisitorsUnitDay:
+        {
+            self.dateFormatter.dateFormat = @"yyyy-MM-dd";
+            NSDate *d = [self.dateFormatter dateFromString:name];
+            self.dateFormatter.dateFormat = @"LLL dd";
+            return [self.dateFormatter stringFromDate:d];
+        }
+        case StatsViewsVisitorsUnitWeek:
+            // Assumes format: yyyyWxx where xx is the week number with leading zero
+            return [NSLocalizedString(@"Week", @"Stats 'nice' name prefix for week unit")
+                    stringByAppendingFormat:@" %@",[name substringWithRange:NSMakeRange(name.length-2, 2)]];
+        case StatsViewsVisitorsUnitMonth:
+        {
+            self.dateFormatter.dateFormat = @"yyyy-MM-dd";
+            NSDate *d = [self.dateFormatter dateFromString:name];
+            self.dateFormatter.dateFormat = @"LLL yy"; // L is stand-alone month
+            return [self.dateFormatter stringFromDate:d];
+        }
+        default:
+            return @"";
+    }
 }
 
 @end
