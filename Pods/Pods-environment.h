b6ec9da27f2ecfda9c063fe77c83321e2b779acf
Aaron Douglas <astralbodies@gmail.com>
2014-01-17 11:58:26 +0900

    Updated Pods so dependencies are properly managed for wpxmlrpc 0.3

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -46,19 +46,19 @@
 #define COCOAPODS_POD_AVAILABLE_DTFoundation_Core
 #define COCOAPODS_VERSION_MAJOR_DTFoundation_Core 1
 #define COCOAPODS_VERSION_MINOR_DTFoundation_Core 6
-#define COCOAPODS_VERSION_PATCH_DTFoundation_Core 0
+#define COCOAPODS_VERSION_PATCH_DTFoundation_Core 1
 
 // DTFoundation/DTHTMLParser
 #define COCOAPODS_POD_AVAILABLE_DTFoundation_DTHTMLParser
 #define COCOAPODS_VERSION_MAJOR_DTFoundation_DTHTMLParser 1
 #define COCOAPODS_VERSION_MINOR_DTFoundation_DTHTMLParser 6
-#define COCOAPODS_VERSION_PATCH_DTFoundation_DTHTMLParser 0
+#define COCOAPODS_VERSION_PATCH_DTFoundation_DTHTMLParser 1
 
 // DTFoundation/UIKit
 #define COCOAPODS_POD_AVAILABLE_DTFoundation_UIKit
 #define COCOAPODS_VERSION_MAJOR_DTFoundation_UIKit 1
 #define COCOAPODS_VERSION_MINOR_DTFoundation_UIKit 6
-#define COCOAPODS_VERSION_PATCH_DTFoundation_UIKit 0
+#define COCOAPODS_VERSION_PATCH_DTFoundation_UIKit 1
 
 // EmailChecker
 #define COCOAPODS_POD_AVAILABLE_EmailChecker
@@ -153,6 +153,6 @@
 // wpxmlrpc
 #define COCOAPODS_POD_AVAILABLE_wpxmlrpc
 #define COCOAPODS_VERSION_MAJOR_wpxmlrpc 0
-#define COCOAPODS_VERSION_MINOR_wpxmlrpc 2
+#define COCOAPODS_VERSION_MINOR_wpxmlrpc 3
 #define COCOAPODS_VERSION_PATCH_wpxmlrpc 0
 
