f9021395d48b66f7abb996141bbba5d3883009da
aerych <eric@automattic.com>
2014-01-23 02:14:06 -0600

    Removes some unused properties. Extends UITableViewController.

--- WordPress/Classes/PostSettingsViewController.h
+++ WordPress/Classes/PostSettingsViewController.h
@@ -10,7 +10,7 @@
 #import <UIKit/UIKit.h>
 #import "AbstractPost.h"
 
-@interface PostSettingsViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+@interface PostSettingsViewController : UITableViewController
 
 @property (nonatomic, strong) NSString *statsPrefix;
 
