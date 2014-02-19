13da6a4af431f3199cb56fd2d3816da8c2e46cac
Jorge Leandro Perez <jorge@lantean.co>
2014-02-19 12:47:54 -0300

    Updates 3rd party frameworks

---
+++ Pods/Simperium/Simperium/SPMemberJSON.h
@@ -0,0 +1,13 @@
+//
+//  SPMemberJSON.h
+//  Simperium
+//
+//  Created by Jorge Leandro Perez on 02-18-2014.
+//  Copyright (c) 2014 Simperium. All rights reserved.
+//
+
+#import "SPMember.h"
+
+@interface SPMemberJSON : SPMember
+
+@end
