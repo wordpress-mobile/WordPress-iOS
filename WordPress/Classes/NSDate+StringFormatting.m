fb82899e3d04fb866925d1ab3f46bc530e95a986
Aaron Douglas <astralbodies@gmail.com>
2014-03-19 14:53:03 -0500

    Switched to NSDateComponents to create the date "short string"
    This change also allows for localization of the units of time single characters.  NSDateComponents seems to perform well enough during scrolling - CALayer stuff swamped the CPU comparatively during instrumentation.

--- WordPress/Classes/NSDate+StringFormatting.m
+++ WordPress/Classes/NSDate+StringFormatting.m
@@ -12,19 +12,23 @@
 
 - (NSString *)shortString {
     NSString *shortString;
-	NSTimeInterval diff = [[NSDate date] timeIntervalSince1970] - [self timeIntervalSince1970];
-    
-	if(diff < 60) {
-		shortString = [NSString stringWithFormat:@"%is", (int)diff];
-	} else if(diff < 3600) {
-		shortString = [NSString stringWithFormat:@"%im", (int)floor(diff / 60)];
-	} else if (diff < 86400) {
-		shortString = [NSString stringWithFormat:@"%ih", (int)floor(diff / 3600)];
-	} else {
-		shortString = [NSString stringWithFormat:@"%id", (int)floor(diff / 86400)];
-	}
-    
-	return shortString;
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *dateComponents = [calendar components:NSDayCalendarUnit | NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit
+                                                   fromDate:self
+                                                     toDate:[NSDate date]
+                                                    options:0];
+
+    if (dateComponents.day > 0) {
+        shortString = [NSString stringWithFormat:@"%i%@", dateComponents.day, NSLocalizedString(@"d", @"Days single character abbreviation")];
+    } else if (dateComponents.hour > 0) {
+        shortString = [NSString stringWithFormat:@"%i%@", dateComponents.hour, NSLocalizedString(@"h", @"Hours single character abbreviation")];
+    } else if (dateComponents.minute > 0) {
+        shortString = [NSString stringWithFormat:@"%i%@", dateComponents.minute, NSLocalizedString(@"m", @"Minutes single character abbreviation")];
+    } else {
+        shortString = [NSString stringWithFormat:@"%i%@", dateComponents.second, NSLocalizedString(@"s", @"Seconds single character abbreviation")];
+    }
+
+    return shortString;
 }
 
 @end
