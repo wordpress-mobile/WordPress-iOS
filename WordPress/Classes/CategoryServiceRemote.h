4a0339a6219d2236e3e638b123146ed0bc7f473f
Aaron Douglas <astralbodies@gmail.com>
2014-03-20 14:40:16 -0500

    Updated copyright statement on new Service class files

--- WordPress/Classes/CategoryServiceRemote.h
+++ WordPress/Classes/CategoryServiceRemote.h
@@ -1,10 +1,11 @@
-//
-//  CategoryServiceRemote.h
-//  WordPress
-//
-//  Created by Aaron Douglas on 3/18/14.
-//  Copyright (c) 2014 WordPress. All rights reserved.
-//
+/*
+ * CategoryServiceRemote.h
+ *
+ * Copyright (c) 2014 WordPress. All rights reserved.
+ *
+ * Licensed under GNU General Public License 2.0.
+ * Some rights reserved. See license.txt
+ */
 
 #import <Foundation/Foundation.h>
 
