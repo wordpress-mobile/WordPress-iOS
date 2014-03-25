faff80b5f4bba83d84f2ac4d1bfb7a063d2c7fc6
Aaron Douglas <astralbodies@gmail.com>
2014-03-25 08:47:00 -0500

    Update Reachability pod to latest version 3.1.1

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -111,8 +111,8 @@
 // Reachability
 #define COCOAPODS_POD_AVAILABLE_Reachability
 #define COCOAPODS_VERSION_MAJOR_Reachability 3
-#define COCOAPODS_VERSION_MINOR_Reachability 0
-#define COCOAPODS_VERSION_PATCH_Reachability 0
+#define COCOAPODS_VERSION_MINOR_Reachability 1
+#define COCOAPODS_VERSION_PATCH_Reachability 1
 
 // SVProgressHUD
 #define COCOAPODS_POD_AVAILABLE_SVProgressHUD
