abb87230aa0ee347d62533e98b70811d131c120d
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-04-01 16:35:51 -0700

    Removed copyright/author/license info from core data models.

--- WordPress/Classes/StatsViewByCountry.h
+++ WordPress/Classes/StatsViewByCountry.h
@@ -1,12 +1,3 @@
-/*
- * StatsViewByCountry.h
- *
- * Copyright (c) 2014 WordPress. All rights reserved.
- *
- * Licensed under GNU General Public License 2.0.
- * Some rights reserved. See license.txt
- */
-
 #import "StatsTitleCountItem.h"
 
 @interface StatsViewByCountry : StatsTitleCountItem
