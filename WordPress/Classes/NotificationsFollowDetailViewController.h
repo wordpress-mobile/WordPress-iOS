a2528c4cfdb28668aa354b594a92f26e04276c75
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-04-01 16:02:16 -0700

    Removed copyright/author/license info from Notification related files.

--- WordPress/Classes/NotificationsFollowDetailViewController.h
+++ WordPress/Classes/NotificationsFollowDetailViewController.h
@@ -1,11 +1,3 @@
-//
-//  NotificationsLikesDetailViewController.h
-//  WordPress
-//
-//  Created by Dan Roundhill on 11/29/12.
-//  Copyright (c) 2012 WordPress. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @class Note;
