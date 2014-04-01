b5068c4b1ea0ac84f957ba980eb08ab0357737a8
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-04-01 16:00:59 -0700

    Removed copyright/author/license info from NUX related files.

--- WordPress/Classes/WPNUXMainButton.m
+++ WordPress/Classes/WPNUXMainButton.m
@@ -1,11 +1,3 @@
-//
-//  WPNUXMainButton.m
-//  WordPress
-//
-//  Created by Sendhil Panchadsaram on 5/14/13.
-//  Copyright (c) 2013 WordPress. All rights reserved.
-//
-
 #import "WPNUXMainButton.h"
 
 @implementation WPNUXMainButton {
