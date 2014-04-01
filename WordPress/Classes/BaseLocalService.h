a2f7d15f3ab376f28853e1b84b89422c0fd2dbaa
Aaron Douglas <astralbodies@gmail.com>
2014-04-01 10:39:27 -0500

    Updated copyright information in new classes

--- WordPress/Classes/BaseLocalService.h
+++ WordPress/Classes/BaseLocalService.h
@@ -1,10 +1,11 @@
-//
-//  BaseService.h
-//  WordPress
-//
-//  Created by Aaron Douglas on 4/1/14.
-//  Copyright (c) 2014 WordPress. All rights reserved.
-//
+/*
+ * BaseLocalService.h
+ *
+ * Copyright (c) 2014 WordPress. All rights reserved.
+ *
+ * Licensed under GNU General Public License 2.0.
+ * Some rights reserved. See license.txt
+ */
 
 #import <Foundation/Foundation.h>
 
