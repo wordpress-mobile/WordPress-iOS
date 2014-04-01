53dc5a743c3276ba390a790d375443523c19dee2
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-04-01 16:11:39 -0700

    Removed copyright/author/license info from post vc related files.

--- WordPress/Classes/FeaturedImageViewController.h
+++ WordPress/Classes/FeaturedImageViewController.h
@@ -1,11 +1,3 @@
-//
-//  FeaturedImageViewController.h
-//  WordPress
-//
-//  Created by Eric Johnson on 1/28/14.
-//  Copyright (c) 2014 WordPress. All rights reserved.
-//
-
 #import "WPImageViewController.h"
 #import "Post.h"
 
