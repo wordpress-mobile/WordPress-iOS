8a7a7e4e6e87bc6d415c4c5db460cdca0b719791
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-04-01 16:06:15 -0700

    Removed copyright/author/license info from media related controllers.

--- WordPress/Classes/MediaSearchFilterHeaderView.h
+++ WordPress/Classes/MediaSearchFilterHeaderView.h
@@ -1,12 +1,3 @@
-/*
- * MediaSearchFilterHeaderView.h
- *
- * Copyright (c) 2013 WordPress. All rights reserved.
- *
- * Licensed under GNU General Public License 2.0.
- * Some rights reserved. See license.txt
- */
-
 #import <UIKit/UIKit.h>
 
 @class MediaBrowserViewController;
