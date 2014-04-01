43694ef382fe0385755881437ac157c4a1403c2a
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-04-01 16:08:35 -0700

    Removed copyright/author/license info from post geolocation files.

--- WordPress/Classes/PostGeolocationView.h
+++ WordPress/Classes/PostGeolocationView.h
@@ -1,11 +1,3 @@
-//
-//  PostGeolocationView.h
-//  WordPress
-//
-//  Created by Eric Johnson on 1/23/14.
-//  Copyright (c) 2014 WordPress. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
 #import "Coordinate.h"
