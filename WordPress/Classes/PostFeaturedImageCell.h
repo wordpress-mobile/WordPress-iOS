ae490f49b430979972828809c9bf6c6811242feb
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-04-01 16:29:00 -0700

    Removed copyright/author/license info from various table view cells.

--- WordPress/Classes/PostFeaturedImageCell.h
+++ WordPress/Classes/PostFeaturedImageCell.h
@@ -1,11 +1,3 @@
-//
-//  PostFeaturedImageCell.h
-//  WordPress
-//
-//  Created by Eric Johnson on 1/9/14.
-//  Copyright (c) 2014 WordPress. All rights reserved.
-//
-
 #import "WPTableViewCell.h"
 
 @interface PostFeaturedImageCell : WPTableViewCell
--- WordPress/Classes/PostGeolocationCell.h
+++ WordPress/Classes/PostGeolocationCell.h
@@ -1,11 +1,3 @@
-//
-//  PostGeolocationCell.h
-//  WordPress
-//
-//  Created by Eric Johnson on 1/9/14.
-//  Copyright (c) 2014 WordPress. All rights reserved.
-//
-
 #import "WPTableViewCell.h"
 #import "Coordinate.h"
 
