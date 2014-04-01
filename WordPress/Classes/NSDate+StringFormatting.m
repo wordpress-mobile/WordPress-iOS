ba2663ec8a64e619e91ab8a5bbda682956d18749
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-04-01 15:58:16 -0700

    Removed copyright/author/license info from categories.

--- WordPress/Classes/NSDate+StringFormatting.m
+++ WordPress/Classes/NSDate+StringFormatting.m
@@ -1,11 +1,3 @@
-//
-//  NSDate+StringFormatting.m
-//  WordPress
-//
-//  Created by Michael Johnston on 11/17/13.
-//  Copyright (c) 2013 WordPress. All rights reserved.
-//
-
 #import "NSDate+StringFormatting.h"
 
 @implementation NSDate (StringFormatting)
