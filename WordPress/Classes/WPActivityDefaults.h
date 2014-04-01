784fa4b067d34a8912cd8721cf319df719c90eb1
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-04-01 16:22:08 -0700

    Removed copyright/author/license info from sharing related utils.

--- WordPress/Classes/WPActivityDefaults.h
+++ WordPress/Classes/WPActivityDefaults.h
@@ -1,11 +1,3 @@
-//
-//  WPActivityDefaults.h
-//  WordPress
-//
-//  Created by Jorge Bernal on 7/26/13.
-//  Copyright (c) 2013 WordPress. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface WPActivityDefaults : NSObject
