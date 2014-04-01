7a9e81e241d4a2469c97e53335c5c4f00b698e57
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-04-01 16:26:30 -0700

    Removed copyright/author/license info from utility related files.

--- WordPress/Classes/ReachabilityUtils.h
+++ WordPress/Classes/ReachabilityUtils.h
@@ -1,11 +1,3 @@
-//
-//  ReachabilityUtils.h
-//  WordPress
-//
-//  Created by Eric on 8/29/12.
-//  Copyright (c) 2012 WordPress. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface ReachabilityUtils : NSObject
