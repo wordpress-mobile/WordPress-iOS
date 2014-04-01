765ff4a1156fa90e6c52b16b729f7aa16395bb84
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-04-01 16:19:39 -0700

    Removed copyright/author/license info from settings related files.

--- WordPress/Classes/SettingsViewController.m
+++ WordPress/Classes/SettingsViewController.m
@@ -1,13 +1,4 @@
 /*
- * SettingsViewController.m
- *
- * Copyright (c) 2013 WordPress. All rights reserved.
- *
- * Licensed under GNU General Public License 2.0.
- * Some rights reserved. See license.txt
- */
-
-/*
  
  Settings contents:
  
