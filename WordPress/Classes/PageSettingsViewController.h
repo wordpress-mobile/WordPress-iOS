87c14b778b03393ab013c83833315a7df72fafdf
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-04-01 16:07:47 -0700

    Removed copyright/author/license info from page related controllers.

--- WordPress/Classes/PageSettingsViewController.h
+++ WordPress/Classes/PageSettingsViewController.h
@@ -1,11 +1,3 @@
-//
-//  PageSettingsViewController.h
-//  WordPress
-//
-//  Created by Eric Johnson on 2/11/14.
-//  Copyright (c) 2014 WordPress. All rights reserved.
-//
-
 #import "PostSettingsViewController.h"
 
 @interface PageSettingsViewController : PostSettingsViewController
