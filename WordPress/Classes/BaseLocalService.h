6ad8fe30a76a8e2efb7691f3d723f8832733ec41
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-04-01 16:39:27 -0700

    Removed copyright/author/license info from service and remote files.

--- WordPress/Classes/BaseLocalService.h
+++ WordPress/Classes/BaseLocalService.h
@@ -1,12 +1,3 @@
-/*
- * BaseLocalService.h
- *
- * Copyright (c) 2014 WordPress. All rights reserved.
- *
- * Licensed under GNU General Public License 2.0.
- * Some rights reserved. See license.txt
- */
-
 #import <Foundation/Foundation.h>
 
 @protocol BaseLocalService <NSObject>
--- WordPress/Classes/CategoryService.h
+++ WordPress/Classes/CategoryService.h
@@ -1,12 +1,3 @@
-/*
- * CategoryService.h
- *
- * Copyright (c) 2014 WordPress. All rights reserved.
- *
- * Licensed under GNU General Public License 2.0.
- * Some rights reserved. See license.txt
- */
-
 #import <Foundation/Foundation.h>
 #import "BaseLocalService.h"
 
