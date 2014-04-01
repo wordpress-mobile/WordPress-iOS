32438e7eec1d7299795c72b8c2cc632ebde97ee3
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-04-01 16:40:12 -0700

    Removed copyright/author/license info from app stats files.

--- WordPress/Classes/WPMobileStats.h
+++ WordPress/Classes/WPMobileStats.h
@@ -1,11 +1,3 @@
-//
-//  WPMobileStats.h
-//  WordPress
-//
-//  Created by Sendhil Panchadsaram on 5/14/13.
-//  Copyright (c) 2013 WordPress. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 // General
