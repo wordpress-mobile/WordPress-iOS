1f04d2ccbf7ac0fe3747ecb0a9e02b87210aec10
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-04-01 16:20:44 -0700

    Removed copyright/author/license info from theme related files.

--- WordPress/Classes/ThemeDetailsViewController.m
+++ WordPress/Classes/ThemeDetailsViewController.m
@@ -1,12 +1,3 @@
-/*
- * ThemeDetailsViewController.m
- *
- * Copyright (c) 2013 WordPress. All rights reserved.
- *
- * Licensed under GNU General Public License 2.0.
- * Some rights reserved. See license.txt
- */
-
 #import "ThemeDetailsViewController.h"
 #import "Theme.h"
 #import "WPImageSource.h"
