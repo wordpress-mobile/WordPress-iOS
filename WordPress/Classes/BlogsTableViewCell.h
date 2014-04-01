bbb9871bf3767d1c21fa57a3a649649cca880227
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-04-01 16:29:00 -0700

    Removed copyright/author/license info from various table view cells.

--- WordPress/Classes/BlogsTableViewCell.h
+++ WordPress/Classes/BlogsTableViewCell.h
@@ -1,11 +1,3 @@
-//
-//  BlogsTableViewCell.h
-//  WordPress
-//
-//  Created by Dan Roundhill on 3/24/11.
-//  Copyright 2011 WordPress. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface BlogsTableViewCell : UITableViewCell {
