04c1163311f327e30a6dffa601a9a88291d199da
Dan Roundhill <dan@automattic.com>
2014-04-02 14:02:45 -0700

    Removed authorship info from NotifiactionsBigBadgeViewController.

--- WordPress/Classes/NotificationsBigBadgeViewController.h
+++ WordPress/Classes/NotificationsBigBadgeViewController.h
@@ -1,11 +1,3 @@
-//
-//  NotificationsBigBadgeViewController.h
-//  WordPress
-//
-//  Created by Dan Roundhill on 3/28/14.
-//  Copyright (c) 2014 WordPress. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import "Note.h"
 
