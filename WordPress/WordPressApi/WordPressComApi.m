4ffb2817b9ca36f50da6ad2132aa1ef907f4ffcc
Jorge Leandro Perez <jorge@lantean.co>
2014-04-02 14:02:52 -0300

    Note: Requesting Meta Attribute
    Added metaPostID and metaSiteID helpers to Note model class.

--- WordPress/WordPressApi/WordPressComApi.m
+++ WordPress/WordPressApi/WordPressComApi.m
@@ -17,7 +17,7 @@
 
 static NSString *const WordPressComApiClientEndpointURL = @"https://public-api.wordpress.com/rest/v1/";
 static NSString *const WordPressComApiOauthBaseUrl = @"https://public-api.wordpress.com/oauth2";
-NSString *const WordPressComApiNotificationFields = @"id,type,unread,body,subject,timestamp";
+NSString *const WordPressComApiNotificationFields = @"id,type,unread,body,subject,timestamp,meta";
 static NSString *const WordPressComApiLoginUrl = @"https://wordpress.com/wp-login.php";
 NSString *const WordPressComApiErrorDomain = @"com.wordpress.api";
 NSString *const WordPressComApiErrorCodeKey = @"WordPressComApiErrorCodeKey";
