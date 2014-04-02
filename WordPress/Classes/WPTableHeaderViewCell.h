91cef3b6d19aa341efaaa32b653bae50ee097575
Jorge Leandro Perez <jorge@lantean.co>
2014-04-02 14:23:10 -0300

    Nukes Author / Licensing info from newly added files

--- WordPress/Classes/WPTableHeaderViewCell.h
+++ WordPress/Classes/WPTableHeaderViewCell.h
@@ -1,11 +1,3 @@
-//
-//  WPTableHeaderViewCell.h
-//  WordPress
-//
-//  Created by Jorge Leandro Perez on 3/31/14.
-//  Copyright (c) 2014 WordPress. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import "WPTableViewCell.h"
 
