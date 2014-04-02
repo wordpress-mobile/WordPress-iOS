939c3437ee022eb2186b840359ba12bd7a8c0af8
Aaron Douglas <astralbodies@gmail.com>
2014-04-02 14:57:19 -0500

    Refactor JetpackSettingsVC to use AccountService instead of WPAccount

--- WordPress/Classes/JetpackSettingsViewController.m
+++ WordPress/Classes/JetpackSettingsViewController.m
@@ -17,6 +17,8 @@
 #import "WPNUXSecondaryButton.h"
 #import "UILabel+SuggestSize.h"
 #import "WordPressComOAuthClient.h"
+#import "AccountService.h"
+#import "ContextManager.h"
 
 CGFloat const JetpackiOS7StatusBarOffset = 20.0;
 CGFloat const JetpackStandardOffset = 16;
@@ -316,10 +318,17 @@ CGFloat const JetpackSignInButtonHeight = 41.0;
     [_blog validateJetpackUsername:_usernameField.text
                           password:_passwordField.text
                            success:^{
-                               if (![[[WPAccount defaultWordPressComAccount] restApi] hasCredentials]) {
+                               NSManagedObjectContext *context = [[ContextManager sharedInstance] mainContext];
+                               AccountService *accountService = [[AccountService alloc] initWithManagedObjectContext:context];
+                               WPAccount *defaultAccount = [accountService defaultWordPressComAccount];
+
+                               if (![[defaultAccount restApi] hasCredentials]) {
                                    [[WordPressComOAuthClient client] authenticateWithUsername:_usernameField.text password:_passwordField.text success:^(NSString *authToken) {
-                                       WPAccount *account = [WPAccount createOrUpdateWordPressComAccountWithUsername:_usernameField.text password:_passwordField.text authToken:authToken];
-                                       [WPAccount setDefaultWordPressComAccount:account];
+                                       NSManagedObjectContext *context = [[ContextManager sharedInstance] mainContext];
+                                       AccountService *accountService = [[AccountService alloc] initWithManagedObjectContext:context];
+
+                                       WPAccount *account = [accountService createOrUpdateWordPressComAccountWithUsername:_usernameField.text password:_passwordField.text authToken:authToken];
+                                       [accountService setDefaultWordPressComAccount:account];
                                        finishedBlock(YES);
                                    } failure:failureBlock];
                                } else {
@@ -488,8 +497,12 @@ CGFloat const JetpackSignInButtonHeight = 41.0;
 - (void)checkForJetpack {
     if ([_blog hasJetpack]) {
         if (!_blog.jetpackUsername || !_blog.jetpackPassword) {
-            _usernameField.text = [[WPAccount defaultWordPressComAccount] username];
-            _passwordField.text = [[WPAccount defaultWordPressComAccount] password];
+            NSManagedObjectContext *context = [[ContextManager sharedInstance] mainContext];
+            AccountService *accountService = [[AccountService alloc] initWithManagedObjectContext:context];
+            WPAccount *defaultAccount = [accountService defaultWordPressComAccount];
+
+            _usernameField.text = [defaultAccount username];
+            _passwordField.text = [defaultAccount password];
             [self updateSaveButton];
         }
         return;
