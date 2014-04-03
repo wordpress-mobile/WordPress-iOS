c429b65a45a8fa247b5c5b3616e7c8e9bdcd270d
Aaron Douglas <astralbodies@gmail.com>
2014-04-03 15:10:20 -0500

    Removed copyright headers

--- WordPress/Classes/NoteService.m
+++ WordPress/Classes/NoteService.m
@@ -1,12 +1,3 @@
-/*
- * NoteService.m
- *
- * Copyright (c) 2014 WordPress. All rights reserved.
- *
- * Licensed under GNU General Public License 2.0.
- * Some rights reserved. See license.txt
- */
-
 #import "NoteService.h"
 #import "ContextManager.h"
 #import "Note.h"
