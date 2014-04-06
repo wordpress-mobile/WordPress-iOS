27f2cc2152f7d9829159928e2ed36bdffa19fbf4
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-04-06 13:00:23 -0700

    Adding skeleton for WPStats.

---
+++ WordPress/Classes/WPStats.h
@@ -0,0 +1,32 @@
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSUInteger, WPStat) {
+    WPStatApplicationOpened,
+    WPStatApplicationClosed,
+    WPStatThemesAccessThemeBrowser,
+    WPStatThemesChangeATheme,
+    WPStatReaderAccessedReader,
+    WPStatReaderOpenArticle,
+    WPStatReaderLikeArticle,
+    WPStatReaderReblogArticle,
+    WPStatReaderLoadMoreArticles,
+    WPStatReaderFollowReaderTag,
+    WPStatReaderUnfollowReaderTag,
+    WPStatReaderFilterByReaderTag,
+    WPStatReaderLoadFreshlyPressed,
+    WPStatReaderCommentOnArticle,
+    WPStatStatsAccessStats,
+    WPStatEditorCreatePost,
+    WPStatEditorAddPhotoViaLocalLibrary,
+    WPStatEditorAddPhotoViaWPMediaLibrary,
+    WPStatEditorUpdatePost,
+    WPStatEditorPublishPost,
+    WPStatNotificationsAccessNotifications,
+    WPStatNotificationsOpenNotificationDetails,
+};
+
+@interface WPStats : NSObject
+
++ (void)track:(WPStat)stat;
+
+@end
