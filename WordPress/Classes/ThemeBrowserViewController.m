4f2116516db1c5f40ea05e317685946f37f74b15
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-04-07 13:06:25 -0700

    Adding stat tracking for themes.

--- WordPress/Classes/ThemeBrowserViewController.m
+++ WordPress/Classes/ThemeBrowserViewController.m
@@ -62,6 +62,8 @@ static NSString *const SearchHeaderIdentifier = @"search_header";
     self.navigationItem.rightBarButtonItem = sortButton;
     
     [self syncThemesAndCurrentTheme];
+    
+    [WPStats track:WPStatThemesAccessThemeBrowser];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
