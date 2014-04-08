e90e1885a86134e34c267d6879fe59f74f66eb7a
Aaron Douglas <astralbodies@gmail.com>
2014-04-08 16:22:46 -0500

    Change StatsButtonCell to a segmented control rather than UIButtons
    Allows for a touch down inside change and it just plain looks tappable now.  Still need to finalize some of the changes and tweak the fonts used in the control.

--- WordPress/Classes/StatsButtonCell.h
+++ WordPress/Classes/StatsButtonCell.h
@@ -1,13 +1,24 @@
 #import "StatsViewController.h"
 #import "WPTableViewCell.h"
 
+@protocol StatsButtonCellDelegate;
+
 @interface StatsButtonCell : WPTableViewCell
 
-@property (nonatomic, strong) NSMutableArray *buttons;
-@property (nonatomic, assign) NSUInteger currentActiveButton;
+@property (nonatomic, strong) UISegmentedControl *segmentedControl;
+@property (nonatomic, weak) id<StatsButtonCellDelegate> delegate;
 
 + (CGFloat)heightForRow;
 
-- (void)addButtonWithTitle:(NSString *)title target:(id)target action:(SEL)action section:(StatsSection)section;
+//- (void)addButtonWithTitle:(NSString *)title target:(id)target action:(SEL)action section:(StatsSection)section;
+
+- (void)addSegmentWithTitle:(NSString *)title;
+- (void)segmentChanged:(UISegmentedControl *)sender;
+
+@end
+
+@protocol StatsButtonCellDelegate  <NSObject>
+
+- (void)statsButtonCell:(StatsButtonCell *)statsButtonCell didSelectIndex:(NSUInteger)index;
 
 @end
