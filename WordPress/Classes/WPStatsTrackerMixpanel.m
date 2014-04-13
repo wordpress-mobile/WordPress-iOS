cc9b664b946acf9cac1d310fc87c2c5416e55675
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-04-12 17:32:26 -0700

    Revert "Fixing redundancy in WPStatOpenedViewAdmin tracking for Mixpanel."
    This reverts commit 13efa1c5823a8e8c48f4f5f7399ad609252515da.

--- WordPress/Classes/WPStatsTrackerMixpanel.m
+++ WordPress/Classes/WPStatsTrackerMixpanel.m
@@ -240,7 +240,8 @@
             instructions = [WPStatsTrackerMixpanelInstructionsForStat mixpanelInstructionsWithPropertyIncrementor:@"number_of_times_opened_view_site" forStat:WPStatApplicationClosed];
             break;
         case WPStatOpenedViewAdmin:
-            instructions = [WPStatsTrackerMixpanelInstructionsForStat mixpanelInstructionsWithSuperPropertyAndPeoplePropertyIncrementor:@"number_of_times_opened_view_admin"];
+            instructions = [WPStatsTrackerMixpanelInstructionsForStat mixpanelInstructionsWithPropertyIncrementor:@"number_of_times_opened_view_admin" forStat:WPStatApplicationClosed];
+            [instructions setSuperPropertyAndPeoplePropertyToIncrement:@"number_of_times_opened_view_admin"];
             break;
         case WPStatOpenedMediaLibrary:
             instructions = [WPStatsTrackerMixpanelInstructionsForStat mixpanelInstructionsWithPropertyIncrementor:@"number_of_times_opened_media_library" forStat:WPStatApplicationClosed];
