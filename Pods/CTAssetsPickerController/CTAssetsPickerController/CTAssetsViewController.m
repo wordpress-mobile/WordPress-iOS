c057604ae9620c257b285dfd3e41008d2c13bde8
Mike Johnston <kineticpoet@gmail.com>
2014-04-14 10:44:52 -0700

    Add CTAssetsPickerController to pods and update pods

---
+++ Pods/CTAssetsPickerController/CTAssetsPickerController/CTAssetsViewController.m
@@ -0,0 +1,412 @@
+/*
+ CTAssetsViewController.m
+ 
+ The MIT License (MIT)
+ 
+ Copyright (c) 2013 Clement CN Tsang
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ 
+ */
+
+#import "CTAssetsPickerConstants.h"
+#import "CTAssetsPickerController.h"
+#import "CTAssetsViewController.h"
+#import "CTAssetsViewCell.h"
+#import "CTAssetsSupplementaryView.h"
+
+
+
+NSString * const CTAssetsViewCellIdentifier = @"CTAssetsViewCellIdentifier";
+NSString * const CTAssetsSupplementaryViewIdentifier = @"CTAssetsSupplementaryViewIdentifier";
+
+
+
+@interface CTAssetsPickerController ()
+
+- (void)finishPickingAssets:(id)sender;
+
+- (NSString *)toolbarTitle;
+- (UIView *)noAssetsView;
+
+@end
+
+
+
+@interface CTAssetsViewController ()
+
+@property (nonatomic, weak) CTAssetsPickerController *picker;
+@property (nonatomic, strong) NSMutableArray *assets;
+
+@end
+
+
+
+
+
+@implementation CTAssetsViewController
+
+
+- (id)init
+{
+    UICollectionViewFlowLayout *layout = [self collectionViewFlowLayoutOfOrientation:self.interfaceOrientation];
+    
+    if (self = [super initWithCollectionViewLayout:layout])
+    {
+        self.collectionView.allowsMultipleSelection = YES;
+        
+        [self.collectionView registerClass:CTAssetsViewCell.class
+                forCellWithReuseIdentifier:CTAssetsViewCellIdentifier];
+        
+        [self.collectionView registerClass:CTAssetsSupplementaryView.class
+                forSupplementaryViewOfKind:UICollectionElementKindSectionFooter
+                       withReuseIdentifier:CTAssetsSupplementaryViewIdentifier];
+        
+        self.preferredContentSize = kPopoverContentSize;
+    }
+    
+    [self addNotificationObserver];
+    
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self setupViews];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self setupButtons];
+    [self setupToolbar];
+    [self setupAssets];
+}
+
+- (void)dealloc
+{
+    [self removeNotificationObserver];
+}
+
+
+#pragma mark - Accessors
+
+- (CTAssetsPickerController *)picker
+{
+    return (CTAssetsPickerController *)self.navigationController;
+}
+
+
+#pragma mark - Rotation
+
+- (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+{
+    UICollectionViewFlowLayout *layout = [self collectionViewFlowLayoutOfOrientation:toInterfaceOrientation];
+    [self.collectionView setCollectionViewLayout:layout animated:YES];
+}
+
+
+#pragma mark - Setup
+
+- (void)setupViews
+{
+    self.collectionView.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)setupButtons
+{
+    self.navigationItem.rightBarButtonItem =
+    [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Done", nil)
+                                     style:UIBarButtonItemStyleDone
+                                    target:self.picker
+                                    action:@selector(finishPickingAssets:)];
+    
+    self.navigationItem.rightBarButtonItem.enabled = (self.picker.selectedAssets.count > 0);
+}
+
+- (void)setupToolbar
+{
+    self.toolbarItems = self.picker.toolbarItems;
+}
+
+- (void)setupAssets
+{
+    self.title = [self.assetsGroup valueForProperty:ALAssetsGroupPropertyName];
+    
+    if (!self.assets)
+        self.assets = [[NSMutableArray alloc] init];
+    else
+        [self.assets removeAllObjects];
+    
+    ALAssetsGroupEnumerationResultsBlock resultsBlock = ^(ALAsset *asset, NSUInteger index, BOOL *stop)
+    {
+        if (asset)
+            [self.assets addObject:asset];
+        else
+            [self reloadData];
+    };
+    
+    [self.assetsGroup enumerateAssetsUsingBlock:resultsBlock];
+}
+
+
+#pragma mark - Collection View Layout
+
+- (UICollectionViewFlowLayout *)collectionViewFlowLayoutOfOrientation:(UIInterfaceOrientation)orientation
+{
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+    layout.itemSize             = kThumbnailSize;
+    layout.footerReferenceSize  = CGSizeMake(0, 44.0);
+    
+    if (UIInterfaceOrientationIsLandscape(orientation) && (UI_USER_INTERFACE_IDIOM() != UIUserInterfaceIdiomPad))
+    {
+        layout.sectionInset            = UIEdgeInsetsMake(9.0, 2.0, 0, 2.0);
+        layout.minimumInteritemSpacing = 3.0;
+        layout.minimumLineSpacing      = 3.0;
+    }
+    else
+    {
+        layout.sectionInset            = UIEdgeInsetsMake(9.0, 0, 0, 0);
+        layout.minimumInteritemSpacing = 2.0;
+        layout.minimumLineSpacing      = 2.0;
+    }
+    
+    return layout;
+}
+
+
+#pragma mark - Notifications
+
+- (void)addNotificationObserver
+{
+    NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
+    
+    [center addObserver:self
+               selector:@selector(assetsLibraryChanged:)
+                   name:ALAssetsLibraryChangedNotification
+                 object:nil];
+    
+    [center addObserver:self
+               selector:@selector(selectedAssetsChanged:)
+                   name:CTAssetsPickerSelectedAssetsChangedNotification
+                 object:nil];
+}
+
+- (void)removeNotificationObserver
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+
+#pragma mark - Assets Library Changed
+
+- (void)assetsLibraryChanged:(NSNotification *)notification
+{
+    // Reload all assets
+    if (notification.userInfo == nil)
+        [self performSelectorOnMainThread:@selector(setupAssets) withObject:nil waitUntilDone:NO];
+    
+    // Reload effected assets groups
+    if (notification.userInfo.count > 0)
+        [self reloadAssetsGroupForUserInfo:notification.userInfo];
+}
+
+
+#pragma mark - Reload Assets Group
+
+- (void)reloadAssetsGroupForUserInfo:(NSDictionary *)userInfo
+{
+    NSSet *URLs = [userInfo objectForKey:ALAssetLibraryUpdatedAssetGroupsKey];
+    NSURL *URL  = [self.assetsGroup valueForProperty:ALAssetsGroupPropertyURL];
+    
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF == %@", URL];
+    NSArray *matchedGroups = [URLs.allObjects filteredArrayUsingPredicate:predicate];
+    
+    // Reload assets if current assets group is updated
+    if (matchedGroups.count > 0)
+        [self performSelectorOnMainThread:@selector(setupAssets) withObject:nil waitUntilDone:NO];
+}
+
+
+
+#pragma mark - Selected Assets Changed
+
+- (void)selectedAssetsChanged:(NSNotification *)notification
+{
+    NSArray *selectedAssets = (NSArray *)notification.object;
+    
+    [[self.toolbarItems objectAtIndex:1] setTitle:[self.picker toolbarTitle]];
+    
+    [self.picker setToolbarHidden:(selectedAssets.count == 0) animated:YES];
+}
+
+
+#pragma mark - Reload Data
+
+- (void)reloadData
+{
+    if (self.assets.count > 0)
+    {
+        [self.collectionView reloadData];
+        
+        if (CGPointEqualToPoint(self.collectionView.contentOffset, CGPointZero))
+            [self.collectionView setContentOffset:CGPointMake(0, self.collectionViewLayout.collectionViewContentSize.height)];
+    }
+    else
+    {
+        [self showNoAssets];
+    }
+}
+
+
+#pragma mark - No assets
+
+- (void)showNoAssets
+{
+    self.collectionView.backgroundView = [self.picker noAssetsView];
+}
+
+
+#pragma mark - Collection View Data Source
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return self.assets.count;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    CTAssetsViewCell *cell =
+    [collectionView dequeueReusableCellWithReuseIdentifier:CTAssetsViewCellIdentifier
+                                              forIndexPath:indexPath];
+    
+    ALAsset *asset = [self.assets objectAtIndex:indexPath.row];
+    
+    if ([self.picker.delegate respondsToSelector:@selector(assetsPickerController:shouldEnableAsset:)])
+        cell.enabled = [self.picker.delegate assetsPickerController:self.picker shouldEnableAsset:asset];
+    else
+        cell.enabled = YES;
+    
+    // XXX
+    // Setting `selected` property blocks further deselection.
+    // Have to call selectItemAtIndexPath too. ( ref: http://stackoverflow.com/a/17812116/1648333 )
+    if ([self.picker.selectedAssets containsObject:asset])
+    {
+        cell.selected = YES;
+        [collectionView selectItemAtIndexPath:indexPath animated:NO scrollPosition:UICollectionViewScrollPositionNone];
+    }
+    
+    [cell bind:asset];
+    
+    return cell;
+}
+
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
+{
+    CTAssetsSupplementaryView *view =
+    [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionFooter
+                                       withReuseIdentifier:CTAssetsSupplementaryViewIdentifier
+                                              forIndexPath:indexPath];
+    
+    [view bind:self.assets];
+    
+    return view;
+}
+
+
+#pragma mark - Collection View Delegate
+
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    ALAsset *asset = [self.assets objectAtIndex:indexPath.row];
+    
+    CTAssetsViewCell *cell = (CTAssetsViewCell *)[collectionView cellForItemAtIndexPath:indexPath];
+    
+    if (!cell.isEnabled)
+        return NO;
+    else if ([self.picker.delegate respondsToSelector:@selector(assetsPickerController:shouldSelectAsset:)])
+        return [self.picker.delegate assetsPickerController:self.picker shouldSelectAsset:asset];
+    else
+        return YES;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    ALAsset *asset = [self.assets objectAtIndex:indexPath.row];
+    
+    [self.picker selectAsset:asset];
+    
+    if ([self.picker.delegate respondsToSelector:@selector(assetsPickerController:didSelectAsset:)])
+        [self.picker.delegate assetsPickerController:self.picker didSelectAsset:asset];
+}
+
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldDeselectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    ALAsset *asset = [self.assets objectAtIndex:indexPath.row];
+    
+    if ([self.picker.delegate respondsToSelector:@selector(assetsPickerController:shouldDeselectAsset:)])
+        return [self.picker.delegate assetsPickerController:self.picker shouldDeselectAsset:asset];
+    else
+        return YES;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didDeselectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    ALAsset *asset = [self.assets objectAtIndex:indexPath.row];
+    
+    [self.picker deselectAsset:asset];
+    
+    if ([self.picker.delegate respondsToSelector:@selector(assetsPickerController:didDeselectAsset:)])
+        [self.picker.delegate assetsPickerController:self.picker didDeselectAsset:asset];
+}
+
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    ALAsset *asset = [self.assets objectAtIndex:indexPath.row];
+    
+    if ([self.picker.delegate respondsToSelector:@selector(assetsPickerController:shouldHighlightAsset:)])
+        return [self.picker.delegate assetsPickerController:self.picker shouldHighlightAsset:asset];
+    else
+        return YES;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didHighlightItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    ALAsset *asset = [self.assets objectAtIndex:indexPath.row];
+    
+    if ([self.picker.delegate respondsToSelector:@selector(assetsPickerController:didHighlightAsset:)])
+        [self.picker.delegate assetsPickerController:self.picker didHighlightAsset:asset];
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didUnhighlightItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    ALAsset *asset = [self.assets objectAtIndex:indexPath.row];
+    
+    if ([self.picker.delegate respondsToSelector:@selector(assetsPickerController:didUnhighlightAsset:)])
+        [self.picker.delegate assetsPickerController:self.picker didUnhighlightAsset:asset];
+}
+
+
+@end
