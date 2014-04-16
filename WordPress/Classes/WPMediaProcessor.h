26705fc7780283b94571b1a183735d67a6e59265
Mike Johnston <kineticpoet@gmail.com>
2014-04-15 23:18:27 -0700

    Process local images after returning from picker

--- WordPress/Classes/WPMediaProcessor.h
+++ WordPress/Classes/WPMediaProcessor.h
@@ -1,5 +1,9 @@
 #import <Foundation/Foundation.h>
 
+@class Media;
+
 @interface WPMediaProcessor : NSObject
+
+- (void)processImage:(UIImage *)theImage media:(Media *)imageMedia metadata:(NSDictionary *)metadata;
 
 @end
