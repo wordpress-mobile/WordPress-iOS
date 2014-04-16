89154a54bb81bd8e6b0e8edcdb446b54911ea232
Mike Johnston <kineticpoet@gmail.com>
2014-04-15 23:41:13 -0700

    Refactor resize methods from MediaBrowserViewController

--- WordPress/Classes/WPMediaProcessor.h
+++ WordPress/Classes/WPMediaProcessor.h
@@ -1,9 +1,12 @@
 #import <Foundation/Foundation.h>
-
-@class Media;
+#import "Media.h"
 
 @interface WPMediaProcessor : NSObject
 
 - (void)processImage:(UIImage *)theImage media:(Media *)imageMedia metadata:(NSDictionary *)metadata;
+- (CGSize)sizeForImage:(UIImage *)image
+           mediaResize:(MediaResize)resize
+  blogResizeDimensions:(NSDictionary *)dimensions;
+- (UIImage *)resizeImage:(UIImage *)original toSize:(CGSize)newSize;
 
 @end
