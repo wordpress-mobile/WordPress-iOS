1787bfae65c7e70815566e0d1f50c5f9667d076c
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-05-01 22:08:51 -0700

    Moved WPMediaPersister functionality into Media. h/t @koke
    @koke rightly pointed out WPMediaPersister functionality better
    belongs in Media than in it's own class.

--- WordPress/Classes/Media.m
+++ WordPress/Classes/Media.m
@@ -3,6 +3,7 @@
 #import "NSString+Helpers.h"
 #import "AFHTTPRequestOperation.h"
 #import "ContextManager.h"
+#import <ImageIO/ImageIO.h>
 
 @interface Media (PrivateMethods)
 
@@ -38,6 +39,9 @@
 
 @synthesize unattached;
 
+NSUInteger const MediaDefaultThumbnailSize = 75;
+CGFloat const MediaDefaultJPEGCompressionQuality = 0.9;
+
 + (Media *)newMediaForPost:(AbstractPost *)post {
     Media *media = [NSEntityDescription insertNewObjectForEntityForName:@"Media" inManagedObjectContext:post.managedObjectContext];
     media.blog = post.blog;
@@ -52,6 +56,84 @@
     media.mediaID = @0;
     return media;
 }
+
++ (Media *)newMediaForPost:(AbstractPost *)post withImage:(UIImage *)image andMetadata:(NSDictionary *)metadata
+{
+    NSData *imageData = UIImageJPEGRepresentation(image, MediaDefaultJPEGCompressionQuality);
+    UIImage *imageThumbnail = [self generateThumbnailFromImage:image andSize:CGSizeMake(MediaDefaultThumbnailSize, MediaDefaultThumbnailSize)];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.locale = [NSLocale currentLocale];
+    [dateFormatter setDateFormat:@"yyyyMMdd-HHmmss-A"];
+
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *filename = [NSString stringWithFormat:@"%@.jpg", [dateFormatter stringFromDate:[NSDate date]]];
+    NSString *filepath = [documentsDirectory stringByAppendingPathComponent:filename];
+
+    if (metadata != nil) {
+        // Write the EXIF data with the image data to disk
+        CGImageSourceRef source = NULL;
+        CGImageDestinationRef destination = NULL;
+        BOOL success = NO;
+        // This will be the data CGImageDestinationRef will write into
+        NSMutableData *destinationData = [NSMutableData data];
+
+        source = CGImageSourceCreateWithData((__bridge CFDataRef)imageData, NULL);
+        if (source) {
+            CFStringRef UTI = CGImageSourceGetType(source); // this is the type of image (e.g., public.jpeg)
+            destination = CGImageDestinationCreateWithData((__bridge CFMutableDataRef)destinationData,UTI,1,NULL);
+
+            if(destination) {
+                // Add the image contained in the image source to the destination, copying the old metadata
+                CGImageDestinationAddImageFromSource(destination, source,0, (__bridge CFDictionaryRef) metadata);
+
+                // Tell the destination to write the image data and metadata into our data object
+                // It will return false if something goes wrong
+                success = CGImageDestinationFinalize(destination);
+                CFRelease(destination);
+            } else {
+                DDLogWarn(@"Media processor could not create image destination");
+            }
+
+            CFRelease(source);
+        } else {
+            DDLogWarn(@"Media processor could not create image source");
+        }
+
+        if (!success) {
+            DDLogWarn(@"Media processor could not create data from image destination");
+            // Write the data without EXIF to disk
+            NSFileManager *fileManager = [NSFileManager defaultManager];
+            [fileManager createFileAtPath:filepath contents:imageData attributes:nil];
+        } else {
+            [destinationData writeToFile:filepath atomically:YES];
+        }
+    } else {
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        [fileManager createFileAtPath:filepath contents:imageData attributes:nil];
+    }
+
+    UIDeviceOrientation currentOrientation = [UIDevice currentDevice].orientation;
+    Media *media = [self newMediaForPost:post];
+    media.orientation = UIDeviceOrientationIsLandscape(currentOrientation) ? @"landscape" : @"portrait";
+    media.creationDate = [NSDate date];
+    media.filename = filename;
+    media.localURL = filepath;
+    media.filesize = @(imageData.length/1024);
+    media.mediaType = MediaTypeImage;
+    media.thumbnail = UIImageJPEGRepresentation(imageThumbnail, MediaDefaultJPEGCompressionQuality);
+    media.width = @(image.size.width);
+    media.height = @(image.size.height);
+    [media save];
+    
+    return media;
+}
+
++ (UIImage *)generateThumbnailFromImage:(UIImage *)theImage andSize:(CGSize)targetSize
+{
+    return [theImage thumbnailImage:MediaDefaultThumbnailSize transparentBorder:0 cornerRadius:0 interpolationQuality:kCGInterpolationHigh];
+}
+
 
 + (Media *)createOrReplaceMediaFromJSON:(NSDictionary *)json forBlog:(Blog *)blog {
     NSSet *existing = [blog.media filteredSetUsingPredicate:[NSPredicate predicateWithFormat:@"mediaID == %@", [json[@"attachment_id"] numericValue]]];
--- WordPress/Classes/WPMediaPersister.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@class Media;
-@interface WPMediaPersister : NSObject
-
-+ (void)saveMedia:(Media *)imageMedia withImage:(UIImage *)image metadata:(NSDictionary *)metadata featured:(BOOL)isFeatured;
-
-@end
