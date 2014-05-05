9e5f1a29791795ad0467d5449e94668a17b6754c
aerych <eric@automattic.com>
2014-05-05 13:11:04 -0500

    Tests for reader services.
    This is a first pass at tests for the reader service.
    It is not exhaustive.  Some things that could have been tested have been omitted
    due to planned changes in the reader code, other things will be added over time.

--- WordPress/Classes/RemoteReaderTopic.h
+++ WordPress/Classes/RemoteReaderTopic.h
@@ -5,7 +5,6 @@
 @property (nonatomic) BOOL isRecommended;
 @property (nonatomic) BOOL isSubscribed;
 @property (nonatomic, strong) NSString *path;
-@property (nonatomic, strong) NSArray *posts;
 @property (nonatomic, strong) NSString *title;
 @property (nonatomic, strong) NSNumber *topicID;
 @property (nonatomic, strong) NSString *type;
