3417e018954d39565ffb39ee3ad923288ddbc388
Aaron Douglas <astralbodies@gmail.com>
2014-05-07 18:50:59 -0500

    Calling all endpoints on batch, need to map and return data still

--- WordPress/Classes/WPStatsService.h
+++ WordPress/Classes/WPStatsService.h
@@ -1,5 +1,13 @@
 #import <Foundation/Foundation.h>
+#import "StatsSummary.h"
+#import "StatsViewsVisitors.h"
 
+typedef void (^StatsCompletion)(StatsSummary *summary, NSDictionary *topPosts, NSDictionary *clicks, NSDictionary *countryViews, NSDictionary *referrers, NSDictionary *searchTerms, StatsViewsVisitors *viewsVisitors);
 
 @interface WPStatsService : NSObject
+
+- (instancetype)initWithSiteId:(NSNumber *)siteId;
+
+- (void)retrieveStatsWithCompletionHandler:(StatsCompletion)completion;
+
 @end
--- WordPress/Classes/WPStatsService.m
+++ WordPress/Classes/WPStatsService.m
@@ -1,7 +1,59 @@
 #import "WPStatsService.h"
+#import "WPStatsServiceRemote.h"
+#import "AccountService.h"
+#import "BlogService.h"
+#import "ContextManager.h"
+#import "WPAccount.h"
+#import "Blog.h"
 
+@interface WPStatsService ()
 
-@implementation WPStatsService {
+@property (nonatomic, strong) NSNumber *siteId;
+
+@end
+
+@implementation WPStatsService
+{
 
 }
+
+- (instancetype)initWithSiteId:(NSNumber *)siteId {
+    self = [super init];
+    if (self) {
+        _siteId = siteId;
+    }
+
+    return self;
+}
+
+- (void)retrieveStatsWithCompletionHandler:(StatsCompletion)completion
+{
+    void (^failure)(NSError *error) = ^void (NSError *error) {
+        DDLogError(@"Error while retrieving stats: %@", error);
+
+        if (completion) {
+            completion(nil, nil, nil, nil, nil, nil, nil);
+        }
+    };
+
+    NSManagedObjectContext *context = [[ContextManager sharedInstance] mainContext];
+    BlogService *blogService = [[BlogService alloc] initWithManagedObjectContext:context];
+    WPAccount *account = nil;
+
+    // Find blog by ID if it's set up on this device
+    Blog *blog = [blogService blogByBlogId:self.siteId];
+    if (blog) {
+        account = blog.account;
+    } else {
+        // Otherwise use default WP.com account for authentication of remote stats data
+        AccountService *accountService = [[AccountService alloc] initWithManagedObjectContext:context];
+        account = [accountService defaultWordPressComAccount];
+    }
+
+    WPStatsServiceRemote *remote = [[WPStatsServiceRemote alloc] initWithRemoteApi:account.restApi andSiteId:self.siteId];
+    [remote fetchStatsForSiteId:self.siteId
+          withCompletionHandler:completion
+                 failureHandler:failure];
+}
+
 @end
