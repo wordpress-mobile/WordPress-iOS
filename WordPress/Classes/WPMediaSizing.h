5d2b5edcbaeb2330e397da2e6888e8c846105b93
Jorge Bernal <jorge@automattic.com>
2014-05-08 10:54:51 +0200

    Optimize images instead of multiple resizing options
    Unless the user selected 'Original' for image processing, scale images down to
    2048x2048. I chose 2048 since it'll allow for full width retina images
    on most themes. Those with the odd giant image themes can always switch
    to full size images.
    
    Simplified media processing:
    
    - Avoid unnecessary CGImage <-> UIImage conversions
    - WPMediaMetadataExtractor can be replaced by a much simpler
      `representation.metadata`
    - Removed WPMediaSizing
    - Deprecated media.orientation
    - Moved media creation to a new MediaService
    
    Note that iOS seems to be ignoring CompressionQuality, although I don't
    know why yet
    
    See #1685

--- WordPress/Classes/WPMediaSizing.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "Media.h"
-
-@interface WPMediaSizing : NSObject
-
-+ (MediaResize)mediaResizePreference;
-+ (UIImage *)correctlySizedImage:(UIImage *)fullResolutionImage forBlogDimensions:(NSDictionary *)dimensions;
-
-@end
