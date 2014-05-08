49a0b6269574c9eb0476f79eba1b193e0c63248d
Aaron Douglas <astralbodies@gmail.com>
2014-05-08 15:12:25 -0500

    Added first unit test for WPStatsService

---
+++ WordPress/WordPressTest/WPStatsServiceTest.m
@@ -0,0 +1,52 @@
+#import <XCTest/XCTest.h>
+#import "WPStatsService.h"
+#import "WPAccount.h"
+#import "WPStatsServiceRemote.h"
+#import <OCMock/OCMock.h>
+
+@interface WPStatsServiceTest : XCTestCase
+
+@property (strong) id remoteMock;
+@property (strong) WPStatsService *statsService;
+
+@end
+
+@implementation WPStatsServiceTest
+
+- (void)setUp
+{
+    [super setUp];
+
+
+    self.remoteMock = [OCMockObject mockForClass:[WPStatsServiceRemote class]];
+    id account = [OCMockObject mockForClass:[WPAccount class]];
+
+    self.statsService = [[WPStatsService alloc] initWithSiteId:@2 andAccount:account];
+    self.statsService.remote = self.remoteMock;
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+
+    self.statsService = nil;
+    self.remoteMock = nil;
+}
+
+- (void)testExample
+{
+    void (^failure)(NSError *error) = ^void(NSError *error) {
+    };
+
+    StatsCompletion completion = ^(StatsSummary *summary, NSDictionary *topPosts, NSDictionary *clicks, NSDictionary *countryViews, NSDictionary *referrers, NSDictionary *searchTerms, StatsViewsVisitors *viewsVisitors) {
+    };
+
+    [[self.remoteMock expect] fetchStatsForSiteId:@2 withCompletionHandler:completion failureHandler:[OCMArg isNotNil]];
+
+    [self.statsService retrieveStatsWithCompletionHandler:completion failureHandler:failure];
+    
+    [self.remoteMock verify];
+
+}
+
+@end
