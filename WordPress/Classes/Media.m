85e66e9b0e72a543dc06fd83ab11572c9c5b453c
Jorge Leandro Perez <jorge@lantean.co>
2014-05-08 11:19:58 -0300

    Fixes static analyzer warnings. Part 1

--- WordPress/Classes/Media.m
+++ WordPress/Classes/Media.m
@@ -1,6 +1,7 @@
 #import "Media.h"
 #import "UIImage+Resize.h"
 #import "NSString+Helpers.h"
+#import "NSString+Util.h"
 #import "AFHTTPRequestOperation.h"
 #import "ContextManager.h"
 #import <ImageIO/ImageIO.h>
@@ -212,6 +213,11 @@ CGFloat const MediaDefaultJPEGCompressionQuality = 0.9;
         self.mediaTypeString = @"powerpoint";
     } else {
         self.mediaTypeString = @"document";
+    }
+    
+    if (fileUTI) {
+        CFRelease(fileUTI);
+        fileUTI = nil;
     }
 }
 
