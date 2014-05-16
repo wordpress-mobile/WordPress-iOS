020b6a1431750036dcca6c17221f794f76bd81ef
Jorge Bernal <jorge@automattic.com>
2014-05-16 07:11:28 +0200

    Refactor media uploads into MediaServiceRemote

---
+++ WordPress/Classes/MediaServiceRemoteXMLRPC.m
@@ -0,0 +1,62 @@
+#import "MediaServiceRemoteXMLRPC.h"
+
+#import "Blog.h"
+#import <WordPressApi/WPXMLRPCClient.h>
+
+@interface MediaServiceRemoteXMLRPC ()
+@property (nonatomic) WPXMLRPCClient *api;
+@end
+
+@implementation MediaServiceRemoteXMLRPC
+
+- (id)initWithApi:(WPXMLRPCClient *)api
+{
+    self = [super init];
+    if (self) {
+        _api = api;
+    }
+
+    return self;
+}
+
+- (AFHTTPRequestOperation *)uploadFile:(NSString *)path
+                                ofType:(NSString *)type
+                          withFilename:(NSString *)filename
+                                toBlog:(Blog *)blog
+                               success:(void (^)(NSNumber *mediaID, NSString *url))success
+                               failure:(void (^)(NSError *))failure {
+    NSDictionary *data = @{
+                           @"name": filename,
+                           @"type": type,
+                           @"bits": [NSInputStream inputStreamWithFileAtPath:path],
+                           };
+    NSArray *parameters = [blog getXMLRPCArgsWithExtra:data];
+    NSURLRequest *request = [self.api requestWithMethod:@"wp.uploadFile" parameters:parameters];
+    AFHTTPRequestOperation *operation = [self.api HTTPRequestOperationWithRequest:request
+                                                                          success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                                                                              NSDictionary *response = (NSDictionary *)responseObject;
+
+                                                                              if (![response isKindOfClass:[NSDictionary class]]) {
+                                                                                  NSError *error = [NSError errorWithDomain:NSURLErrorDomain code:NSURLErrorBadServerResponse userInfo:@{NSLocalizedDescriptionKey: NSLocalizedString(@"The server returned an empty response. This usually means you need to increase the memory limit for your site.", @"")}];
+                                                                                  if (failure) {
+                                                                                      failure(error);
+                                                                                  }
+                                                                                  return;
+                                                                              }
+
+                                                                              NSNumber *ID = [response numberForKey:@"id"];
+                                                                              NSString *url = [response stringForKey:@"url"];
+
+                                                                              if (success) {
+                                                                                  success(ID, url);
+                                                                              }
+                                                                          } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                                                                              if (failure) {
+                                                                                  failure(error);
+                                                                              }
+                                                                          }];
+    [self.api enqueueHTTPRequestOperation:operation];
+    return operation;
+}
+
+@end
