4e0b8648b64e4d63dda31b0cfccdcbadbad1f0e4
Jorge Bernal <jorge@automattic.com>
2014-05-30 17:58:49 +0200

    Sync options using REST when available

--- WordPress/Classes/Networking/BlogServiceRemoteREST.m
+++ WordPress/Classes/Networking/BlogServiceRemoteREST.m
@@ -1,5 +1,6 @@
 #import "BlogServiceRemoteREST.h"
 #import <WordPressComApi.h>
+#import "Blog.h"
 
 @interface BlogServiceRemoteREST ()
 
@@ -15,6 +16,55 @@
         _api = api;
     }
     return self;
+}
+
+- (void)syncOptionsForBlog:(Blog *)blog success:(OptionsHandler)success failure:(void (^)(NSError *))failure
+{
+    NSString *path = [NSString stringWithFormat:@"sites/%@", blog.dotComID];
+    [self.api GET:path
+       parameters:nil
+          success:^(AFHTTPRequestOperation *operation, id responseObject) {
+              NSDictionary *response = (NSDictionary *)responseObject;
+              NSDictionary *options = [self mapOptionsFromResponse:response];
+              if (success) {
+                  success(options);
+              }
+          } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+              if (failure) {
+                  failure(error);
+              }
+          }];
+}
+
+#pragma mark - Mapping methods
+
+- (NSDictionary *)mapOptionsFromResponse:(NSDictionary *)response {
+    NSMutableDictionary *options = [NSMutableDictionary dictionary];
+    options[@"home_url"] = response[@"URL"];
+    options[@"software_version"] = [response valueForKeyPath:@"options.software_version"];
+    options[@"post_thumbnail"] = [response valueForKeyPath:@"options.featured_images_enabled"];
+    // We'd be better off saving this as a BOOL property on Blog, but let's do what XML-RPC does for now
+    options[@"blog_public"] = [[response numberForKey:@"is_private"] boolValue] ? @"-1" : @"0";
+    if ([[options numberForKey:@"jetpack"] boolValue]) {
+        options[@"jetpack_client_id"] = [options numberForKey:@"ID"];
+    }
+
+    NSArray *optionsDirectMapKeys = @[
+                                @"admin_url",
+                                @"login_url",
+                                @"image_default_link_type",
+                                ];
+    for (NSString *key in optionsDirectMapKeys) {
+        NSString *sourceKeyPath = [NSString stringWithFormat:@"options.%@", key];
+        options[key] = [response valueForKeyPath:sourceKeyPath];
+    }
+
+    NSMutableDictionary *valueOptions = [NSMutableDictionary dictionaryWithCapacity:options.count];
+    [options enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        valueOptions[key] = @{@"value": obj};
+    }];
+
+    return [NSDictionary dictionaryWithDictionary:valueOptions ];
 }
 
 @end
