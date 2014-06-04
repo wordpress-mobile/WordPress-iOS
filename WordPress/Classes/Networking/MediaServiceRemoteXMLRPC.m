c0be6291406a4791c4081482aa0bf3e45dda4eb5
Sérgio Estêvão <sergio.estevao@smilemachine.com>
2014-06-05 00:42:07 +0100

    Relayout of method for improved readability.

--- WordPress/Classes/Networking/MediaServiceRemoteXMLRPC.m
+++ WordPress/Classes/Networking/MediaServiceRemoteXMLRPC.m
@@ -33,29 +33,30 @@
     NSArray *parameters = [blog getXMLRPCArgsWithExtra:data];
     NSURLRequest *request = [self.api streamingRequestWithMethod:@"wp.uploadFile" parameters:parameters];
     AFHTTPRequestOperation *operation = [self.api HTTPRequestOperationWithRequest:request
-                                                                          success:^(AFHTTPRequestOperation *operation, id responseObject) {
-                                                                              NSDictionary *response = (NSDictionary *)responseObject;
+                                                                          success:^(AFHTTPRequestOperation *operation, id responseObject)
+    {
+      NSDictionary *response = (NSDictionary *)responseObject;
 
-                                                                              if (![response isKindOfClass:[NSDictionary class]]) {
-                                                                                  NSError *error = [NSError errorWithDomain:NSURLErrorDomain code:NSURLErrorBadServerResponse userInfo:@{NSLocalizedDescriptionKey: NSLocalizedString(@"The server returned an empty response. This usually means you need to increase the memory limit for your site.", @"")}];
-                                                                                  if (failure) {
-                                                                                      failure(error);
-                                                                                  }
-                                                                                  return;
-                                                                              }
+      if (![response isKindOfClass:[NSDictionary class]]) {
+          NSError *error = [NSError errorWithDomain:NSURLErrorDomain code:NSURLErrorBadServerResponse userInfo:@{NSLocalizedDescriptionKey: NSLocalizedString(@"The server returned an empty response. This usually means you need to increase the memory limit for your site.", @"")}];
+          if (failure) {
+              failure(error);
+          }
+          return;
+      }
 
-                                                                              NSNumber *ID = [response numberForKey:@"id"];
-                                                                              NSString *url = [response stringForKey:@"url"];
-                                                                              NSString * shortCode = [response stringForKey:@"videopress_shortcode"];
-                                                                              if (success) {
-                                                                                  success(ID, url, shortCode);
-                                                                              }
-                                                                          } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-                                                                              if (failure) {
-                                                                                  failure(error);
-                                                                              }
-                                                                          }];
-    return operation;
+      NSNumber *ID = [response numberForKey:@"id"];
+      NSString *url = [response stringForKey:@"url"];
+      NSString * shortCode = [response stringForKey:@"videopress_shortcode"];
+      if (success) {
+          success(ID, url, shortCode);
+      }
+  } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+      if (failure) {
+        failure(error);
+      }
+  }];
+  return operation;
 }
 
 @end
