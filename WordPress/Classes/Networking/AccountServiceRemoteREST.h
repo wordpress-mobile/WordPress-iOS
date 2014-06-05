d68a46d9c28b3c9ce412e2ebc9c507960130bdfe
Jorge Bernal <jorge@automattic.com>
2014-06-05 16:29:23 +0200

    Restore AccountServiceRemote protocol
    Deleted by mistake on previous commit

--- WordPress/Classes/Networking/AccountServiceRemoteREST.h
+++ WordPress/Classes/Networking/AccountServiceRemoteREST.h
@@ -2,5 +2,5 @@
 #import "AccountServiceRemote.h"
 #import "ServiceRemoteREST.h"
 
-@interface AccountServiceRemoteREST : NSObject <ServiceRemoteREST>
+@interface AccountServiceRemoteREST : NSObject <AccountServiceRemote, ServiceRemoteREST>
 @end
