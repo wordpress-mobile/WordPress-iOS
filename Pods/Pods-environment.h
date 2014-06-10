306ec60f34e764d5c268910490cd616f4c3dc62f
Matt Bumgardner <matt@bumgardner.org>
2014-06-10 10:51:28 -0500

    Updated pod for WPEditor.

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -174,6 +174,12 @@
 #define COCOAPODS_VERSION_MINOR_Taplytics 2
 #define COCOAPODS_VERSION_PATCH_Taplytics 50
 
+// UIAlertView+Blocks
+#define COCOAPODS_POD_AVAILABLE_UIAlertView_Blocks
+#define COCOAPODS_VERSION_MAJOR_UIAlertView_Blocks 0
+#define COCOAPODS_VERSION_MINOR_UIAlertView_Blocks 8
+#define COCOAPODS_VERSION_PATCH_UIAlertView_Blocks 1
+
 // UIDeviceIdentifier
 #define COCOAPODS_POD_AVAILABLE_UIDeviceIdentifier
 #define COCOAPODS_VERSION_MAJOR_UIDeviceIdentifier 0
