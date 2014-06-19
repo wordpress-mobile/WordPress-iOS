6408a1a7f73972c7d4e4c6fc6db5d81b11fd74db
Jorge Bernal <jorge@automattic.com>
2014-06-19 13:28:25 +0200

    Remove comment sync code from BlogService/Comment

--- WordPress/Classes/Models/Comment.m
+++ WordPress/Classes/Models/Comment.m
@@ -11,74 +11,10 @@ NSString * const CommentStatusSpam = @"spam";
 // draft is used for comments that have been composed but not succesfully uploaded yet
 NSString * const CommentStatusDraft = @"draft";
 
-@interface Comment (WordPressApi)
-- (void)updateFromDictionary:(NSDictionary *)commentInfo;
-@end
-
-
 @implementation Comment
 @dynamic blog;
 @dynamic post;
 @synthesize isNew;
-
-#pragma mark - Creating and finding comment objects
-
-+ (Comment *)findWithBlog:(Blog *)blog andCommentID:(NSNumber *)commentID {
-    NSSet *results = [blog.comments filteredSetUsingPredicate:[NSPredicate predicateWithFormat:@"commentID == %@",commentID]];
-    
-    if (results && (results.count > 0)) {
-        return [[results allObjects] objectAtIndex:0];
-    }
-    return nil;    
-}
-
-+ (Comment *)createOrReplaceFromDictionary:(NSDictionary *)commentInfo forBlog:(Blog *)blog {
-    if ([commentInfo objectForKey:@"comment_id"] == nil) {
-        return nil;
-    }
-    
-    Comment *comment = [self findWithBlog:blog andCommentID:[[commentInfo objectForKey:@"comment_id"] numericValue]];
-    
-    if (comment == nil) {
-        comment = [Comment newCommentForBlog:blog];
-        comment.isNew = YES;
-    }
-    
-    [comment updateFromDictionary:commentInfo];
-    [comment findPostWithContext:blog.managedObjectContext];
-    
-    return comment;
-}
-
-+ (void)mergeNewComments:(NSArray *)newComments forBlog:(Blog *)blog {
-    NSManagedObjectContext *backgroundMOC = [[ContextManager sharedInstance] newDerivedContext];
-    [backgroundMOC performBlock:^{
-        NSMutableArray *commentsToKeep = [NSMutableArray array];
-        Blog *contextBlog = (Blog *)[backgroundMOC existingObjectWithID:blog.objectID error:nil];
-        
-        for (NSDictionary *commentInfo in newComments) {
-            Comment *newComment = [Comment createOrReplaceFromDictionary:commentInfo forBlog:contextBlog];
-            if (newComment != nil) {
-                [commentsToKeep addObject:newComment];
-            } else {
-                DDLogInfo(@"-[Comment createOrReplaceFromDictionary:forBlog:] returned a nil comment: %@", commentInfo);
-            }
-        }
-        
-        NSSet *existingComments = contextBlog.comments;
-        if (existingComments && (existingComments.count > 0)) {
-            for (Comment *comment in existingComments) {
-                // Don't delete unpublished comments
-                if(![commentsToKeep containsObject:comment] && comment.commentID != nil) {
-                    DDLogInfo(@"Deleting Comment: %@", comment);
-                    [backgroundMOC deleteObject:comment];
-                }
-            }
-        }
-        
-        [[ContextManager sharedInstance] saveDerivedContext:backgroundMOC];
-    }];
-}
 
 #pragma mark - Helper methods
 
@@ -134,56 +70,6 @@ NSString * const CommentStatusDraft = @"draft";
     return date;
 }
 
-#pragma mark - Private Methods
-
-+ (Comment *)newCommentForBlog:(Blog *)blog {
-    Comment *comment = [NSEntityDescription insertNewObjectForEntityForName:NSStringFromClass([Comment class]) inManagedObjectContext:blog.managedObjectContext];
-    comment.blog = blog;
-    return comment;
-}
-
-- (void)findPostWithContext:(NSManagedObjectContext *)context {
-    Post *contextPost;
-    if (self.post) {
-        contextPost = (Post*)[context objectWithID:self.post.objectID];
-    }
-    Blog *contextBlog = (Blog*)[context objectWithID:self.blog.objectID];
-    
-    if (contextPost && self.postID && [contextPost.postID isEqual:self.postID]) {
-        return;
-    }
-	
-	if(self.postID) {
-        NSSet *posts = [contextBlog.posts filteredSetUsingPredicate:[NSPredicate predicateWithFormat:@"postID == %@", self.postID]];
-        self.post = [posts anyObject];
-	}
-}
-
-- (void)save {
-    [self.managedObjectContext performBlock:^{
-        [self.managedObjectContext save:nil];
-    }];
-}
-
-@end
-
-@implementation Comment (WordPressApi)
-
-- (void)updateFromDictionary:(NSDictionary *)commentInfo {
-    self.author          = [commentInfo objectForKey:@"author"];
-    self.author_email    = [commentInfo objectForKey:@"author_email"];
-    self.author_url      = [commentInfo objectForKey:@"author_url"];
-    self.commentID       = [[commentInfo objectForKey:@"comment_id"] numericValue];
-    self.content         = [commentInfo objectForKey:@"content"];
-    self.dateCreated     = [commentInfo objectForKey:@"date_created_gmt"];
-    self.link            = [commentInfo objectForKey:@"link"];
-    self.parentID        = [[commentInfo objectForKey:@"parent"] numericValue];
-    self.postID          = [[commentInfo objectForKey:@"post_id"] numericValue];
-    self.postTitle       = [commentInfo objectForKey:@"post_title"];
-    self.status          = [commentInfo objectForKey:@"status"];
-    self.type            = [commentInfo objectForKey:@"type"];    
-}
-
 #pragma mark - WPContentViewProvider protocol
 
 - (NSString *)blogNameForDisplay {
@@ -197,6 +83,5 @@ NSString * const CommentStatusDraft = @"draft";
     }
     return status;
 }
-
 
 @end
