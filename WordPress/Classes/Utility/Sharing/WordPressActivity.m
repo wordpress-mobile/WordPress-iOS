2b83b274827c5de48c49e1bf2c9cfa0e327b15e0
Chris Truman <cleetruman@gmail.com>
2014-06-20 10:36:50 -0700

    Back to spaces

--- WordPress/Classes/Utility/Sharing/WordPressActivity.m
+++ WordPress/Classes/Utility/Sharing/WordPressActivity.m
@@ -13,57 +13,57 @@
 @implementation WordPressActivity
 
 - (UIImage *)activityImage {
-	return [UIImage imageNamed:@"sidebar-logo"];
+    return [UIImage imageNamed:@"sidebar-logo"];
 }
 
 - (NSString *)activityTitle {
-	return @"WordPress";
+    return @"WordPress";
 }
 
 - (NSString *)activityType {
-	return NSStringFromClass([self class]);
+    return NSStringFromClass([self class]);
 }
 
 - (BOOL)canPerformWithActivityItems:(NSArray *)activityItems {
-	for (id activityItem in activityItems) {
-		if ([activityItem isKindOfClass:[NSURL class]] && [[UIApplication sharedApplication] canOpenURL:activityItem]) {
-			return YES;
-		}
-	}
+    for (id activityItem in activityItems) {
+        if ([activityItem isKindOfClass:[NSURL class]] && [[UIApplication sharedApplication] canOpenURL:activityItem]) {
+            return YES;
+        }
+    }
 	
-	return NO;
+    return NO;
 }
 
 - (void)prepareWithActivityItems:(NSArray *)activityItems {
-	for (id activityItem in activityItems) {
-		if ([activityItem isKindOfClass:[NSURL class]]) {
-			self.URL = activityItem;
+    for (id activityItem in activityItems) {
+        if ([activityItem isKindOfClass:[NSURL class]]) {
+            self.URL = activityItem;
 		}
-		if ([activityItem isKindOfClass:[NSDictionary class]]) {
-			self.title = activityItem[@"title"];
-			self.summary = activityItem[@"summary"];
-			self.tags = activityItem[@"tags"];
-		}
-	}
+        if ([activityItem isKindOfClass:[NSDictionary class]]) {
+            self.title = activityItem[@"title"];
+            self.summary = activityItem[@"summary"];
+            self.tags = activityItem[@"tags"];
+        }
+    }
 }
 
 -(UIViewController *)activityViewController{
-	NSString * content = [self.summary stringByAppendingString:[NSString stringWithFormat:@"\n\n <a href=\"%@\">%@</a>", self.URL, self.URL]];
+    NSString * content = [self.summary stringByAppendingString:[NSString stringWithFormat:@"\n\n <a href=\"%@\">%@</a>", self.URL, self.URL]];
 
-	EditPostViewController * editPostViewController = [[EditPostViewController alloc] initWithTitle:self.title andContent:content andTags:self.tags andImage:nil];
-	editPostViewController.activityDelegate = self;
+    EditPostViewController * editPostViewController = [[EditPostViewController alloc] initWithTitle:self.title andContent:content andTags:self.tags andImage:nil];
+    editPostViewController.activityDelegate = self;
 
-	UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:editPostViewController];
-	navController.modalPresentationStyle = UIModalPresentationCurrentContext;
-	navController.navigationBar.translucent = NO;
-	navController.restorationIdentifier = WPEditorNavigationRestorationID;
-	navController.restorationClass = [EditPostViewController class];
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:editPostViewController];
+    navController.modalPresentationStyle = UIModalPresentationCurrentContext;
+    navController.navigationBar.translucent = NO;
+    navController.restorationIdentifier = WPEditorNavigationRestorationID;
+    navController.restorationClass = [EditPostViewController class];
 
-	return navController;
+    return navController;
 }
 
 - (void)editPostViewDismissed {
-	[self activityDidFinish:YES];
+    [self activityDidFinish:YES];
 }
 
 @end
