e4dd2c3be9f48e6668b2eff36bcea31f3467bc4f
aerych <eric@automattic.com>
2014-06-23 16:54:49 -0500

    Better constraint wrangling for action buttons.
    Misc code style and tweaks.

--- WordPress/Classes/ViewRelated/Views/WPContentActionView.m
+++ WordPress/Classes/ViewRelated/Views/WPContentActionView.m
@@ -1,12 +1,18 @@
 #import "WPContentActionView.h"
 #import "NSDate+StringFormatting.h"
 
+const CGFloat WPContentActionViewButtonHeight = 48.0;
+const CGFloat WPContentActionViewBorderHeight = 1.0;
+const CGFloat WPContentActionViewButtonSpacing = 12.0;
+
 @interface WPContentActionView()
 
 @property (nonatomic, strong) NSMutableArray *actionButtons;
 @property (nonatomic, strong) UIView *borderView;
 @property (nonatomic, strong) UIButton *timeButton;
 @property (nonatomic, strong) NSTimer *dateRefreshTimer;
+@property (nonatomic, assign) BOOL needsUpdateButtonConstraints;
+@property (nonatomic, strong) NSMutableArray *buttonConstraints;
 
 @end
 
@@ -26,75 +32,103 @@
     if (self) {
         self.translatesAutoresizingMaskIntoConstraints = NO;
         self.actionButtons = [NSMutableArray array];
+        self.buttonConstraints = [NSMutableArray array];
 
         self.borderView = [self viewForBorder];
         [self addSubview:self.borderView];
 
         self.timeButton = [self buttonForTimeButton];
         [self addSubview:self.timeButton];
+
+        [self configureConstraints];
     }
     return self;
 }
 
 - (void)updateConstraints
 {
-    [self configureConstraints];
+    [self configureButtonConstraints];
     [super updateConstraints];
-}
-
-- (void)configureConstraints
-{
-    [self removeConstraints:self.constraints];
-
-    NSDictionary *views = NSDictionaryOfVariableBindings(_timeButton, _borderView);
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|[_timeButton]"
-                                                                 options:NSLayoutFormatAlignAllLeft
-                                                                 metrics:nil
-                                                                   views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_timeButton(48)]|"
-                                                                 options:NSLayoutFormatAlignAllCenterY
-                                                                 metrics:nil
-                                                                   views:views]];
-
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|[_borderView]|"
-                                                                 options:NSLayoutFormatAlignAllTop
-                                                                 metrics:nil
-                                                                   views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_borderView(1)]"
-                                                                 options:NSLayoutFormatAlignAllTop
-                                                                 metrics:nil
-                                                                   views:views]];
-
-
-    UIButton *previousButton;
-    NSArray* reversedActionButtons = [[self.actionButtons reverseObjectEnumerator] allObjects];
-    for (UIButton *button in reversedActionButtons) {
-        if (previousButton) {
-            views = NSDictionaryOfVariableBindings(button, previousButton);
-            [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"[button(48)]-12-[previousButton]"
-                                                                         options:NSLayoutFormatAlignAllBaseline
-                                                                         metrics:nil
-                                                                           views:views]];
-        } else {
-            views = NSDictionaryOfVariableBindings(button);
-            [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"[button]|"
-                                                                         options:NSLayoutFormatAlignAllBaseline
-                                                                         metrics:nil
-                                                                           views:views]];
-        }
-        UIView *me = self;
-        views = NSDictionaryOfVariableBindings(button, me);
-        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"[me]-(<=1)-[button(48)]"
-                                                                     options:NSLayoutFormatAlignAllCenterY
-                                                                     metrics:nil
-                                                                       views:views]];
-        previousButton = button;
-    }
 }
 
 - (CGSize)intrinsicContentSize
 {
-    return CGSizeMake(280.0, 48.0);
+    return CGSizeMake(200.0, WPContentActionViewButtonHeight);
+}
+
+- (void)configureConstraints
+{
+    NSDictionary *views = NSDictionaryOfVariableBindings(_timeButton, _borderView);
+    NSDictionary *metrics = @{@"buttonHeight":@(WPContentActionViewButtonHeight),
+                              @"borderHeight":@(WPContentActionViewBorderHeight)};
+
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|[_timeButton]"
+                                                                 options:NSLayoutFormatAlignAllLeft
+                                                                 metrics:metrics
+                                                                   views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_timeButton(buttonHeight)]|"
+                                                                 options:NSLayoutFormatAlignAllCenterY
+                                                                 metrics:metrics
+                                                                   views:views]];
+
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|[_borderView]|"
+                                                                 options:NSLayoutFormatAlignAllTop
+                                                                 metrics:metrics
+                                                                   views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_borderView(borderHeight)]"
+                                                                 options:NSLayoutFormatAlignAllTop
+                                                                 metrics:metrics
+                                                                   views:views]];
+    [self setNeedsUpdateConstraints];
+}
+
+// Existing button constraints must be deleted prior to this
+- (void)configureButtonConstraints
+{
+    if (!self.needsUpdateButtonConstraints) {
+        return;
+    }
+    self.needsUpdateButtonConstraints = NO;
+
+    NSMutableArray *constraints = [NSMutableArray array];
+    UIButton *previousButton;
+    NSArray* reversedActionButtons = [[self.actionButtons reverseObjectEnumerator] allObjects];
+    for (UIButton *button in reversedActionButtons) {
+        NSDictionary *views;
+        NSDictionary *metrics = @{@"buttonHeight":@(WPContentActionViewButtonHeight),
+                                  @"buttonSpacing":@(WPContentActionViewButtonSpacing)};
+        if (previousButton) {
+            views = NSDictionaryOfVariableBindings(button, previousButton);
+
+            [constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"[button(buttonHeight)]-(buttonSpacing)-[previousButton]"
+                                                                                     options:NSLayoutFormatAlignAllBaseline
+                                                                                     metrics:metrics
+                                                                                       views:views]];
+        } else {
+            views = NSDictionaryOfVariableBindings(button);
+            [constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"[button]|"
+                                                                                     options:NSLayoutFormatAlignAllBaseline
+                                                                                     metrics:nil
+                                                                                       views:views]];
+        }
+        UIView *me = self;
+        views = NSDictionaryOfVariableBindings(button, me);
+        [constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"[me]-(<=1)-[button(buttonHeight)]"
+                                                                                 options:NSLayoutFormatAlignAllCenterY
+                                                                                 metrics:metrics
+                                                                                   views:views]];
+        previousButton = button;
+    }
+    self.buttonConstraints = constraints;
+    [self addConstraints:constraints];
+}
+
+- (void)resetButtonConstraints
+{
+    [self removeConstraints:self.buttonConstraints];
+    [self.buttonConstraints removeAllObjects];
+    self.needsUpdateButtonConstraints = YES;
+    [self setNeedsUpdateConstraints];
 }
 
 
@@ -105,7 +139,7 @@
     actionButton.translatesAutoresizingMaskIntoConstraints = NO;
     [self.actionButtons addObject:actionButton];
     [self addSubview:actionButton];
-    [self setNeedsUpdateConstraints];
+    [self resetButtonConstraints];
 }
 
 - (void)removeAllActionButtons
@@ -114,7 +148,7 @@
         [button removeFromSuperview];
     }
     [self.actionButtons removeAllObjects];
-    [self setNeedsUpdateConstraints];
+    [self resetButtonConstraints];
 }
 
 - (void)setContentProvider:(id<WPContentViewProvider>)contentProvider
@@ -163,7 +197,7 @@
 
     // Disable it for now (could be used for permalinks in the future)
     [button setImage:[UIImage imageNamed:@"reader-postaction-time"] forState:UIControlStateDisabled];
-    [button setTitleColor:[UIColor colorWithHexString:@"aaa"] forState:UIControlStateDisabled];
+    [button setTitleColor:[UIColor colorWithRed:170.0/255.0 green:170.0/255.0 blue:170.0/255.0 alpha:1.0] forState:UIControlStateDisabled];
     [button setEnabled:NO];
 
     return button;
