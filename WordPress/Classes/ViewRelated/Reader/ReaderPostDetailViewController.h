93e9d71362804157acea812cf6033839545d1d0e
aerych <eric@automattic.com>
2014-06-26 19:49:20 -0500

    First pass at detail view controller.
    Code style and clean up
    Removes obsolete code
    Wires up to use new views and autolayout
    The code that gets the post cell's size is bugged in this commit. The call to layoutSubviews interferes with drawing text. Text doesn't appear until scrolled or media is loaded.

--- WordPress/Classes/ViewRelated/Reader/ReaderPostDetailViewController.h
+++ WordPress/Classes/ViewRelated/Reader/ReaderPostDetailViewController.h
@@ -5,7 +5,6 @@
 @interface ReaderPostDetailViewController : UITableViewController
 
 @property (nonatomic, strong) ReaderPost *post;
-@property (nonatomic, assign) BOOL showInlineActionBar;
 
 - (id)initWithPost:(ReaderPost *)post featuredImage:(UIImage *)image avatarImage:(UIImage *)avatarImage;
 - (id)initWithPost:(ReaderPost *)post avatarImageURL:(NSURL *)avatarImageURL;
