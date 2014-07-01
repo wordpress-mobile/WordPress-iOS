a54567d7e753bcebfdc06351d52c254f496c726b
Jorge Leandro Perez <jorge@lantean.co>
2014-07-01 13:46:51 -0300

    CommentViewController: Code Cleanup

--- WordPress/Classes/ViewRelated/Views/CommentViewController.h
+++ WordPress/Classes/ViewRelated/Views/CommentViewController.h
@@ -1,12 +1,13 @@
 #import <UIKit/UIKit.h>
 
+
 @class Comment;
 @class CommentsViewController;
+
 @interface CommentViewController : UIViewController
 
-@property (nonatomic, strong) Comment *comment;
-@property (nonatomic, weak) CommentsViewController *commentsViewController;
-@property BOOL wasLastCommentPending;
+@property (nonatomic, strong)   Comment                 *comment;
+@property (nonatomic, weak)     CommentsViewController  *commentsViewController;
 
 - (void)cancelView:(id)sender;
 - (void)showComment:(Comment *)comment;
