1aaa81f15490636fc5995c2a008ee56fd1cb3282
Jorge Leandro Perez <jorge@lantean.co>
2014-07-11 09:58:17 -0300

    Updates SocketRocket Pod

--- Pods/SocketRocket/SocketRocket/SRWebSocket.h
+++ Pods/SocketRocket/SocketRocket/SRWebSocket.h
@@ -22,49 +22,76 @@ typedef enum {
     SR_OPEN         = 1,
     SR_CLOSING      = 2,
     SR_CLOSED       = 3,
-    
 } SRReadyState;
+
+typedef enum SRStatusCode : NSInteger {
+    SRStatusCodeNormal = 1000,
+    SRStatusCodeGoingAway = 1001,
+    SRStatusCodeProtocolError = 1002,
+    SRStatusCodeUnhandledType = 1003,
+    // 1004 reserved.
+    SRStatusNoStatusReceived = 1005,
+    // 1004-1006 reserved.
+    SRStatusCodeInvalidUTF8 = 1007,
+    SRStatusCodePolicyViolated = 1008,
+    SRStatusCodeMessageTooBig = 1009,
+} SRStatusCode;
 
 @class SRWebSocket;
 
 extern NSString *const SRWebSocketErrorDomain;
 
+#pragma mark - SRWebSocketDelegate
+
 @protocol SRWebSocketDelegate;
+
+#pragma mark - SRWebSocket
 
 @interface SRWebSocket : NSObject <NSStreamDelegate>
 
-@property (nonatomic, assign) id <SRWebSocketDelegate> delegate;
+@property (nonatomic, weak) id <SRWebSocketDelegate> delegate;
 
 @property (nonatomic, readonly) SRReadyState readyState;
 @property (nonatomic, readonly, retain) NSURL *url;
 
 // This returns the negotiated protocol.
-// It will be niluntil after the handshake completes.
+// It will be nil until after the handshake completes.
 @property (nonatomic, readonly, copy) NSString *protocol;
 
-// Protocols should be an array of strings that turn into Sec-WebSocket-Protocol
+// Protocols should be an array of strings that turn into Sec-WebSocket-Protocol.
 - (id)initWithURLRequest:(NSURLRequest *)request protocols:(NSArray *)protocols;
 - (id)initWithURLRequest:(NSURLRequest *)request;
 
-// Some helper constructors
+// Some helper constructors.
 - (id)initWithURL:(NSURL *)url protocols:(NSArray *)protocols;
 - (id)initWithURL:(NSURL *)url;
 
-// SRWebSockets are intended one-time-use only.  Open should be called once and only once
+// Delegate queue will be dispatch_main_queue by default.
+// You cannot set both OperationQueue and dispatch_queue.
+- (void)setDelegateOperationQueue:(NSOperationQueue*) queue;
+- (void)setDelegateDispatchQueue:(dispatch_queue_t) queue;
+
+// By default, it will schedule itself on +[NSRunLoop SR_networkRunLoop] using defaultModes.
+- (void)scheduleInRunLoop:(NSRunLoop *)aRunLoop forMode:(NSString *)mode;
+- (void)unscheduleFromRunLoop:(NSRunLoop *)aRunLoop forMode:(NSString *)mode;
+
+// SRWebSockets are intended for one-time-use only.  Open should be called once and only once.
 - (void)open;
 
 - (void)close;
 - (void)closeWithCode:(NSInteger)code reason:(NSString *)reason;
 
-// Send a UTF8 String or Data
+// Send a UTF8 String or Data.
 - (void)send:(id)data;
 
 @end
 
+#pragma mark - SRWebSocketDelegate
+
 @protocol SRWebSocketDelegate <NSObject>
 
-// message will either be an NSString if the server is using text 
-// or NSData if the server is using binary
+// message will either be an NSString if the server is using text
+// or NSData if the server is using binary.
 - (void)webSocket:(SRWebSocket *)webSocket didReceiveMessage:(id)message;
 
 @optional
@@ -75,6 +102,7 @@ extern NSString *const SRWebSocketErrorDomain;
 
 @end
 
+#pragma mark - NSURLRequest (CertificateAdditions)
 
 @interface NSURLRequest (CertificateAdditions)
 
@@ -82,9 +110,18 @@ extern NSString *const SRWebSocketErrorDomain;
 
 @end
 
+#pragma mark - NSMutableURLRequest (CertificateAdditions)
 
 @interface NSMutableURLRequest (CertificateAdditions)
 
 @property (nonatomic, retain) NSArray *SR_SSLPinnedCertificates;
+
+@end
+
+#pragma mark - NSRunLoop (SRWebSocket)
+
+@interface NSRunLoop (SRWebSocket)
+
++ (NSRunLoop *)SR_networkRunLoop;
 
 @end
