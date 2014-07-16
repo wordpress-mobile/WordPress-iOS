7a50f8d0d58d8adc330d527da7fadb22636fd74d
Brad Angelcyk <brad@angelcyk.com>
2014-07-16 15:54:36 -0400

    adds teardown method to log user out if need be adds a bit of organization to the IntegrationsTests group

--- WordPress/IntegrationTests/LoginTest.m
+++ WordPress/IntegrationTests/LoginTest.m
@@ -1,45 +1,68 @@
 #import <Subliminal/Subliminal.h>
 #import "IntegrationTestsInfo.h"
+#import "ContextManager.h"
+#import "AccountService.h"
+#import "WordPressAppDelegate.h"
 
 @interface LoginTest : SLTest
-
 @end
 
-@implementation LoginTest
-
-- (void)setUpTest {
-	// Navigate to the part of the app being exercised by the test cases,
-	// initialize SLElements common to the test cases, etc.
+@implementation LoginTest {
+    SLTextField *_usernameField;
+    SLTextField *_passwordField;
+    SLButton *_signInButton;
+    
+    AccountService *_accountService;
 }
 
-- (void)tearDownTest {
-	// Navigate back to "home", if applicable.
+- (void)setUpTest {
+    _usernameField = [SLTextField elementWithAccessibilityIdentifier:@"Username"];
+    _passwordField = [SLTextField elementWithAccessibilityIdentifier:@"Password"];
+    _signInButton = [SLButton elementWithAccessibilityIdentifier:@"Sign In"];
+
+    NSManagedObjectContext *context = [[ContextManager sharedInstance] mainContext];
+    _accountService = [[AccountService alloc] initWithManagedObjectContext:context];
+}
+
+- (void)tearDownTestCaseWithSelector:(SEL)testCaseSelector {
+    if ([_accountService defaultWordPressComAccount]) {
+        [[SLElement elementWithAccessibilityLabel:@"Me"] tap];
+        [[SLElement elementWithAccessibilityLabel:@"Settings"] tap];
+        [[SLElement elementWithAccessibilityIdentifier:@"wpcom-sign-out"] tap];
+        [[SLElement elementWithAccessibilityLabel:@"Sign Out"] tap];
+        
+        // wait for reader to finish up some stuff in the background
+        [self wait:10.0];
+    }
 }
 
 - (void)testLoginSucceedsWithUsernameAndPassword {
-    SLTextField *usernameField = [SLTextField elementWithAccessibilityIdentifier:@"Username"];
-    SLTextField *passwordField = [SLTextField elementWithAccessibilityIdentifier:@"Password"];
-    SLElement *submitButton = [SLElement elementWithAccessibilityIdentifier:@"Sign In"];
-    
-    [usernameField setText:[IntegrationTestsInfo WPComUsername]];
-    [passwordField setText:[IntegrationTestsInfo WPComPassword]];
-    
-    [submitButton tap];
+    [_usernameField setText:[IntegrationTestsInfo WPComUsername]];
+    [_passwordField setText:[IntegrationTestsInfo WPComPassword]];
+    [_signInButton tap];
     
     SLAssertTrueWithTimeout([[SLElement elementWithAccessibilityIdentifier:@"tabBar"] isValid],
                             5.0,
                             @"Sign in was not successful.");
+    
+    [self wait:5.0];
 }
 
-- (void)testLogoutSucceeds {
-    [[SLElement elementWithAccessibilityLabel:@"Me"] tap];
-    [[SLElement elementWithAccessibilityLabel:@"Settings"] tap];
-    [[SLElement elementWithAccessibilityIdentifier:@"wpcom-sign-out"] tap];
-    [[SLElement elementWithAccessibilityLabel:@"Sign Out"] tap];
+- (void)testLoginFailsWithBadUsernameAndPassword {
+    [_usernameField setText:@"baduser"];
+    [_passwordField setText:@"badpass"];
+    [_signInButton tap];
     
-    SLAssertTrueWithTimeout([[SLTextField elementWithAccessibilityIdentifier:@"Username"] isValid],
+    SLElement *errorMessage = [SLElement elementWithAccessibilityLabel:@"Sorry, we can't log you in."];
+
+    SLAssertTrueWithTimeout([errorMessage isValid],
                             5.0,
-                            @"Sign out failed.");
+                            @"Sign in worked with an invalid user/pass.");
+    
+    [[SLElement elementWithAccessibilityLabel:@"OK"] tap];
+}
+
+- (void)tearDownTest {
 }
 
 @end
