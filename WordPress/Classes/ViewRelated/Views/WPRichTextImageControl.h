ff403b4b63aac7cc7091520e243050e0642c6eb6
aerych <eric@automattic.com>
2014-07-18 17:51:22 -0500

    Don't change counter or queue in success block.

---
+++ WordPress/Classes/ViewRelated/Views/WPRichTextImageControl.h
@@ -0,0 +1,13 @@
+//
+//  WPRichTextImageControl.h
+//  WordPress
+//
+//  Created by aerych on 7/18/14.
+//  Copyright (c) 2014 WordPress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WPRichTextImageControl : UIControl
+
+@end
