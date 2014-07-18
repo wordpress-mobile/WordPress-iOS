e434a1d1231ffc780fbabff6375a136f3a9aee26
Jorge Leandro Perez <jorge@lantean.co>
2014-07-18 12:55:45 -0300

    Updates Simperium to Mk 0.6.6

--- Pods/Simperium/Simperium/SPAuthenticationValidator.m
+++ Pods/Simperium/Simperium/SPAuthenticationValidator.m
@@ -25,7 +25,7 @@ static int kDefaultMinimumPasswordLength = 4;
     // From http://stackoverflow.com/a/3638271/1379066
     BOOL stricterFilter = NO; // Discussion http://blog.logichigh.com/2010/09/02/validating-an-e-mail-address/
     NSString *stricterFilterString = @"[A-Z0-9a-z\\._%+-]+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2,4}";
-    NSString *laxString = @".+@([A-Za-z0-9]+\\.)+[A-Za-z]{2}[A-Za-z]*";
+    NSString *laxString = @".+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2}[A-Za-z]*";
     NSString *emailRegex = stricterFilter ? stricterFilterString : laxString;
     NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
     
