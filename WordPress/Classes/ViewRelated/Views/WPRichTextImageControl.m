f111edc38078199703ad5a46d10911e26389932a
aerych <eric@automattic.com>
2014-07-18 17:51:22 -0500

    Don't change counter or queue in success block.

---
+++ WordPress/Classes/ViewRelated/Views/WPRichTextImageControl.m
@@ -0,0 +1,31 @@
+//
+//  WPRichTextImageControl.m
+//  WordPress
+//
+//  Created by aerych on 7/18/14.
+//  Copyright (c) 2014 WordPress. All rights reserved.
+//
+
+#import "WPRichTextImageControl.h"
+
+@implementation WPRichTextImageControl
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
