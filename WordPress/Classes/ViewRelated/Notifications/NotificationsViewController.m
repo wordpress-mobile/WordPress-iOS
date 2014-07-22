1bde3a79cdcf55c264294cb18bf4188a4d49d11c
Jorge Leandro Perez <jorge@lantean.co>
2014-07-22 14:40:58 -0300

    Fixes conflict

--- WordPress/Classes/ViewRelated/Notifications/NotificationsViewController.m
+++ WordPress/Classes/ViewRelated/Notifications/NotificationsViewController.m
@@ -14,15 +14,13 @@
 #import "Meta.h"
 
 #import "NotificationSettingsViewController.h"
-<<<<<<< HEAD
+
 #import "NoteTableViewCell.h"
 #import "NotificationsManager.h"
 #import "NotificationDetailsViewController.h"
 
 #import "WPAccount.h"
-=======
-#import "NotificationsBigBadgeDetailViewController.h"
->>>>>>> origin/develop
+
 #import "AccountService.h"
 
 #import "ReaderPost.h"
