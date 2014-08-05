ab63fe518f350e44274b97da3a96e6dfc3502232
Jorge Leandro Perez <jorge@lantean.co>
2014-08-05 10:22:49 -0300

    Adds warnings

--- WordPress/Classes/ViewRelated/Notifications/NotificationsViewController.h
+++ WordPress/Classes/ViewRelated/Notifications/NotificationsViewController.h
@@ -5,6 +5,7 @@
 
 @interface NotificationsViewController : WPTableViewController
 
+#warning TODO: Verify if this in New Model
 - (void)showDetailsForNoteWithID:(NSString *)notificationID animated:(BOOL)animated;
 
 @end
