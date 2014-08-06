6f49b47bb704836d2ec5e149af0a99186fe39bbd
aerych <eric@automattic.com>
2014-08-05 20:25:33 -0500

    Omit protocol when searching for image URL.

--- WordPress/Classes/ViewRelated/Reader/ReaderPostDetailViewController.m
+++ WordPress/Classes/ViewRelated/Reader/ReaderPostDetailViewController.m
@@ -342,8 +342,10 @@ static CGFloat const SectionHeaderHeight = 25.0f;
     NSURL *featuredImageURL = [self.post featuredImageURLForDisplay];
     if (featuredImageURL) {
         // If ReaderPostView has a featured image, show it unless you're showing full detail & featured image is in the post already
+        // One URL might be http and the other https, so don't include the protocol in the check.
+        NSString *featuredImagePath = [[[featuredImageURL absoluteString] componentsSeparatedByString:@"://"] lastObject];
         NSString *content = [self.post contentForDisplay];
-        if ([content rangeOfString:[featuredImageURL absoluteString]].length > 0) {
+        if ([content rangeOfString:featuredImagePath].length > 0) {
             self.postView.alwaysHidesFeaturedImage = YES;
         } else {
             [self fetchFeaturedImage];
