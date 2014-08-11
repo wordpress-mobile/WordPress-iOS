0863de040f2f16aa51e6d2af38dd666138b6962d
Brad Angelcyk <brad@automattic.com>
2014-08-11 10:02:57 -0400

    removes initWithFrame method

--- WordPress/Classes/ViewRelated/NUX/WPNUXHelpBadgeLabel.m
+++ WordPress/Classes/ViewRelated/NUX/WPNUXHelpBadgeLabel.m
@@ -2,12 +2,6 @@
 
 @implementation WPNUXHelpBadgeLabel
 
-- (id)initWithFrame:(CGRect)frame
-{
-    self = [super initWithFrame:frame];
-    return self;
-}
-
 - (void)drawTextInRect:(CGRect)rect
 {
     UIEdgeInsets insets = {0, 0, 1, 0};
