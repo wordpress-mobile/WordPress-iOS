2d7bf1f0934b5d7d75116ac27928a9c8026098b0
Jorge Leandro Perez <jorge@lantean.co>
2014-08-15 01:32:00 -0300

    Implements UIDevice+Helpers

--- WordPress/Classes/ViewRelated/Notifications/Style/UIDevice+Helpers.swift
+++ WordPress/Classes/ViewRelated/Notifications/Style/UIDevice+Helpers.swift
@@ -1,11 +1,3 @@
-//
-//  UIDevice+Helpers.swift
-//  WordPress
-//
-//  Created by Jorge Leandro Perez on 8/14/14.
-//  Copyright (c) 2014 WordPress. All rights reserved.
-//
-
 import Foundation
 
 
