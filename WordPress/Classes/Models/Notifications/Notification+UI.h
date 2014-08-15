586535ca2a9b43b1be9a92546d1e4b844c5755c3
Jorge Leandro Perez <jorge@lantean.co>
2014-08-15 01:33:09 -0300

    Notification+UI is now implemented in Swift

--- WordPress/Classes/Models/Notifications/Notification+UI.h
+++
@@ -1,9 +0,0 @@
-#import "Notification.h"
-
-
-
-@interface NotificationBlock (UI)
-@property (nonatomic, strong, readonly) NSAttributedString *attributedSubject;
-@property (nonatomic, strong, readonly) NSAttributedString *attributedTextRegular;
-@property (nonatomic, strong, readonly) NSAttributedString *attributedTextQuoted;
-@end
