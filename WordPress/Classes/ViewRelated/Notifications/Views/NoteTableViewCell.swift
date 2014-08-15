d0cf813a330bc84b8d4978c3db1329ac1bd47e53
Jorge Leandro Perez <jorge@lantean.co>
2014-08-15 01:35:09 -0300

    Nukes redundant code

--- WordPress/Classes/ViewRelated/Notifications/Views/NoteTableViewCell.swift
+++ WordPress/Classes/ViewRelated/Notifications/Views/NoteTableViewCell.swift
@@ -11,7 +11,6 @@ import Foundation
     
 // FIXME: Cleanup
     private let SubjectPaddingRight:        CGFloat     = 2
-    private let PaddingY:                   CGFloat     = 1
     private let NumberOfLines:              NSInteger   = 0
     private let NoticonRadius:              CGFloat     = 10
     private let PlaceholderImageName                    = "gravatar"
@@ -30,7 +29,7 @@ import Foundation
     }
     public var iconURL:             NSURL! {
         didSet {
-            if (!iconURL) {
+            if !iconURL {
                 return;
             }
             
@@ -81,8 +80,8 @@ import Foundation
     
     public override func layoutSubviews() {
         super.layoutSubviews()
-        self.contentView.layoutIfNeeded()
-        self.refreshLabelPreferredMaxLayoutWidth()
+        contentView.layoutIfNeeded()
+        refreshLabelPreferredMaxLayoutWidth()
     }
 
     private func refreshLabelPreferredMaxLayoutWidth() {
@@ -111,34 +110,4 @@ import Foundation
             backgroundColor             = NotificationColors.backgroundUnread
         }
     }
-    
-
-// FIXME: Category please
-    public func layoutHeightWithWidth(width: CGFloat) -> CGFloat {
-        // Layout: Setup the cell with the given width
-        bounds = CGRect(x: 0, y: 0, width: width, height: CGRectGetHeight(self.bounds))
-        layoutIfNeeded()
-        
-        // iPad: Limit the width
-        let cappedWidth = min(WPTableViewFixedWidth, width)
-        let maximumSize = CGSize(width: cappedWidth, height: 0)
-        let layoutSize  = contentView.systemLayoutSizeFittingSize(maximumSize)
-        
-        return ceil(layoutSize.height) + PaddingY;
-    }
-    
-    public class func reuseIdentifier() -> String {
-        let name = NSStringFromClass(self)
-        
-        if let nameWithoutNamespaces = name.componentsSeparatedByString(".").last {
-            return nameWithoutNamespaces
-        } else {
-            return name
-        }
-    }
-    
-    public class func layoutIdentifier() -> String {
-        return reuseIdentifier() //.join("-layout")
-    }
-    
 }
