5854f9d980cb98f780170075c2b665c59910eda7
Jorge Leandro Perez <jorge@lantean.co>
2014-08-15 17:21:45 -0300

    CommentService: Implements REST Helpers

--- WordPress/Classes/Networking/CommentServiceRemoteREST.h
+++ WordPress/Classes/Networking/CommentServiceRemoteREST.h
@@ -3,4 +3,39 @@
 #import "ServiceRemoteREST.h"
 
 @interface CommentServiceRemoteREST : NSObject <CommentServiceRemote, ServiceRemoteREST>
+
+/**
+ Moderate a comment with a given ID
+ */
+- (void)moderateCommentWithID:(NSNumber *)commentID
+                       blogID:(NSNumber *)blogID
+                       status:(NSString *)status
+                      success:(void (^)())success
+                      failure:(void (^)(NSError *error))failure;
+
+/**
+ Trashes a comment with a given ID
+ */
+- (void)trashCommentWithID:(NSNumber *)commentID
+                    blogID:(NSNumber *)blogID
+                   success:(void (^)())success
+                   failure:(void (^)(NSError *error))failure;
+
+/**
+ Like a comment with a given ID
+ */
+- (void)likeCommentWithID:(NSNumber *)commentID
+                   blogID:(NSNumber *)blogID
+                  success:(void (^)())success
+                  failure:(void (^)(NSError *error))failure;
+
+
+/**
+ Unlike a comment with a given ID
+ */
+- (void)unlikeCommentWithID:(NSNumber *)commentID
+                     blogID:(NSNumber *)blogID
+                    success:(void (^)())success
+                    failure:(void (^)(NSError *error))failure;
+
 @end
