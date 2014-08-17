a1ff9f94c02ba18d1e7b856824928a4a75c9903a
Jorge Leandro Perez <jorge@lantean.co>
2014-08-16 23:50:36 -0300

    NoteBlockText: Fixes numberOfLines override

--- WordPress/Classes/ViewRelated/Notifications/Views/NoteBlockQuoteTableViewCell.swift
+++ WordPress/Classes/ViewRelated/Notifications/Views/NoteBlockQuoteTableViewCell.swift
@@ -3,12 +3,11 @@ import Foundation
 
 @objc public class NoteBlockQuoteTableViewCell : NoteBlockTextTableViewCell
 {
-    public override func numberOfLines() -> Int {
+    public override var numberOfLines: Int {
         let lines = 4
         return lines
     }
-    
-    public override func labelInsets() -> UIEdgeInsets {
+    public override var labelInsets: UIEdgeInsets {
         let insets = UIEdgeInsets(top:0, left:46, bottom:0, right:20)
         return insets
     }
