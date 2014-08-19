2e2d295ec4aa8707f7efde2732d5e3ec5a57515f
Jorge Leandro Perez <jorge@lantean.co>
2014-08-19 14:44:15 -0300

    Decoupling EditCommentViewController

--- WordPress/Classes/ViewRelated/Comments/CommentViewController.h
+++ WordPress/Classes/ViewRelated/Comments/CommentViewController.h
@@ -6,9 +6,5 @@
 
 @property (nonatomic, strong) Comment *comment;
 @property (nonatomic, weak) CommentsViewController *commentsViewController;
-@property BOOL wasLastCommentPending;
-
-- (void)cancelView:(id)sender;
-- (void)showComment:(Comment *)comment;
 
 @end
