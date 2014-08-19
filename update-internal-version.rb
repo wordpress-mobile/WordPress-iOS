cf50d16887eeee3ddb744cc9e16e6903fd6933aa
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-08-18 19:17:06 -0700

    Removing prompt for version number.

--- update-internal-version.rb
+++ update-internal-version.rb
@@ -7,12 +7,14 @@ rescue LoadError
   exit
 end
 
-print "Enter the version number to use as the base (i.e. 4.2.0) "
-base_version_number = gets.chomp
+internal_plist = Plist::parse_xml('./WordPress/WordPress-Internal-Info.plist')
 
+base_version_number = internal_plist["CFBundleVersion"]
+
+# Check if we need to add a .0 to the end(i.e. 4.2 -> 4.2.0)
+base_version_number = "#{base_version_number}.0" if base_version_number =~ /^\d+\.\d+$/
 version_number_with_date = "#{base_version_number}.#{Time.now.strftime("%Y%m%d")}"
 
-internal_plist = Plist::parse_xml('./WordPress/WordPress-Internal-Info.plist')
 internal_plist["CFBundleVersion"] = version_number_with_date
 internal_plist["CFBundleShortVersionString"] = version_number_with_date
 internal_plist.save_plist("./WordPress/WordPress-Internal-Info.plist")
