14b8b501f910deb9ad6745df8c4dac1bed430c44
aerych <eric@automattic.com>
2014-08-19 09:52:59 -0500

    Updates remote comment and remote rest service.
    Adds support for author's avatar url and syncing a hierarchical list of comments.

--- WordPress/Classes/Networking/CommentServiceRemoteREST.h
+++ WordPress/Classes/Networking/CommentServiceRemoteREST.h
@@ -3,4 +3,22 @@
 #import "ServiceRemoteREST.h"
 
 @interface CommentServiceRemoteREST : NSObject <CommentServiceRemote, ServiceRemoteREST>
+
+/**
+ Fetch a hierarchical list of comments for the specified post on the specified site.
+ The comments are returned in the order of nesting, not date.
+ The request fetches the default number of *parent* comments (20) but may return more
+ depending on the number of child comments.
+
+ @param postID The ID of the post.
+ @param siteID The ID of the origin site.
+ @param page The page number to fetch.
+ @param success block called on a successful fetch.
+ @param failure block called if there is any error. `error` can be any underlying network error.
+ */
+- (void)syncHierarchicalCommentsForPost:(NSNumber *)postID
+                               fromSite:(NSNumber *)siteID
+                                   page:(NSUInteger)page
+                                success:(void (^)(NSArray *comments))success
+                                failure:(void (^)(NSError *error))failure;
 @end
