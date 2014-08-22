2775c891bc6afe025918e575bfeae119d92f4b35
Aaron Douglas <astralbodies@gmail.com>
2014-08-22 09:02:18 -0500

    Added a today widget for stats - basic functionality at this point

---
+++ Pods/Pods-WordPressTodayWidget-AFNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WordPressTodayWidget_AFNetworking : NSObject
+@end
+@implementation PodsDummy_Pods_WordPressTodayWidget_AFNetworking
+@end
---
+++ Pods/Pods-WordPressTodayWidget-DTFoundation-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WordPressTodayWidget_DTFoundation : NSObject
+@end
+@implementation PodsDummy_Pods_WordPressTodayWidget_DTFoundation
+@end
