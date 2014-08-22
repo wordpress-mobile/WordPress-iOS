b1203c86bd095cc5d1aaca2e560980fbc1045a67
Aaron Douglas <astralbodies@gmail.com>
2014-08-22 12:40:10 -0500

    Persisting today data upon disappearance of view

--- WordPress/WordPressTodayWidget/TodayViewController.swift
+++ WordPress/WordPressTodayWidget/TodayViewController.swift
@@ -7,8 +7,15 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     @IBOutlet var viewsCountLabel: UILabel?
     @IBOutlet var topPostLabel: UILabel?
     
+    var siteName: String = ""
+    var visitorCount: String = ""
+    var viewCount: String = ""
+    var topPostTitle: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        NSLog("*********** TodayViewController viewDidLoad()")
+        
         // Do any additional setup after loading the view from its nib.
         
         let sharedDefaults = NSUserDefaults(suiteName: "group.org.wordpress")
@@ -22,12 +29,42 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         }
     }
     
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        var userDefaults = NSUserDefaults.standardUserDefaults()
+        userDefaults.setObject(self.siteName, forKey: "TodaySiteName")
+        userDefaults.setObject(self.visitorCount, forKey: "TodayVisitorCount")
+        userDefaults.setObject(self.viewCount, forKey: "TodayViewCount")
+        userDefaults.setObject(self.topPostTitle, forKey: "TodayTopPostTitle")
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+
+        let sharedDefaults = NSUserDefaults(suiteName: "group.org.wordpress")
+        self.siteName = sharedDefaults.stringForKey("WordPressTodayWidgetSiteName") ?? ""
+
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        self.visitorCount = userDefaults.stringForKey("TodayVisitorCount") ?? ""
+        self.viewCount = userDefaults.stringForKey("TodayViewCount") ?? ""
+        self.topPostTitle = userDefaults.stringForKey("TodayTopPostTitle") ?? ""
+
+        NSLog("*********** TodayViewController viewWillAppear: siteName:\(self.siteName), visitorCount:\(self.visitorCount)")
+        
+        self.siteNameLabel?.text = self.siteName
+        self.visitorsCountLabel?.text = self.visitorCount
+        self.viewsCountLabel?.text = self.viewCount
+        self.topPostLabel?.text = self.topPostTitle
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
+        NSLog("*********** TodayViewController widgetPerformUpdateWithCompletionHandler()")
         // Perform any setup necessary in order to update the view.
         
         // If an error is encoutered, use NCUpdateResult.Failed
@@ -36,7 +73,7 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         
         let sharedDefaults = NSUserDefaults(suiteName: "group.org.wordpress")
         let siteId = sharedDefaults.objectForKey("WordPressTodayWidgetSiteId") as NSNumber?
-        let siteName = sharedDefaults.stringForKey("WordPressTodayWidgetSiteName")
+        self.siteName = sharedDefaults.stringForKey("WordPressTodayWidgetSiteName") ?? ""
         let timeZoneName = sharedDefaults.stringForKey("WordPressTodayWidgetTimeZone")
         let oauth2Token = sharedDefaults.stringForKey("WordPressTodayWidgetOAuth2Token")
         
@@ -57,19 +94,20 @@ class TodayViewController: UIViewController, NCWidgetProviding {
             
             var topPostsArray = topPosts["today"] as NSArray
             var topPost = topPostsArray[0] as WPStatsTopPost
+            self.topPostTitle = topPost.title
             
             var numberFormatter = NSNumberFormatter()
             numberFormatter.locale = NSLocale.currentLocale()
             numberFormatter.numberStyle = NSNumberFormatterStyle.DecimalStyle
             numberFormatter.maximumFractionDigits = 0
             
-            let visitorCountString = numberFormatter.stringFromNumber(wpStatsSummary.visitorCountToday)
-            let viewCountString = numberFormatter.stringFromNumber(wpStatsSummary.viewCountToday)
+            self.visitorCount = numberFormatter.stringFromNumber(wpStatsSummary.visitorCountToday)
+            self.viewCount = numberFormatter.stringFromNumber(wpStatsSummary.viewCountToday)
             
-            self.siteNameLabel?.text = siteName
-            self.visitorsCountLabel?.text = visitorCountString
-            self.viewsCountLabel?.text = viewCountString
-            self.topPostLabel?.text = topPost.title
+            self.siteNameLabel?.text = self.siteName
+            self.visitorsCountLabel?.text = self.visitorCount
+            self.viewsCountLabel?.text = self.viewCount
+            self.topPostLabel?.text = self.topPostTitle
             
             completionHandler(NCUpdateResult.NewData)
             }, failureHandler: { (error) -> Void in
