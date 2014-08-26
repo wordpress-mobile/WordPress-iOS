d63ccb087a3bf1ee84e25f15c4da7cd998a368fa
Jorge Leandro Perez <jorge@lantean.co>
2014-08-26 14:05:47 -0300

    Implements ReplyTextView

--- WordPress/Classes/ViewRelated/Notifications/Views/ReplyTextView.swift
+++ WordPress/Classes/ViewRelated/Notifications/Views/ReplyTextView.swift
@@ -1,28 +1,132 @@
 import Foundation
 
 
-@objc public class ReplyTextView : UIView
+@objc public class ReplyTextView : UIView, UITextViewDelegate
 {
-    public func dismiss() {
-        endEditing(true)
-        textField.resignFirstResponder()
+    // MARK: - Initializers
+    public convenience init(width: Int) {
+        self.init(frame: CGRect(x: 0, y: 0, width: width, height: 0))
+        
+        textView.font                   = WPStyleGuide.Notifications.Fonts.blockRegular
+        placeholderLabel.font           = WPStyleGuide.Notifications.Fonts.blockRegular
+        backgroundColor                 = WPStyleGuide.Notifications.Colors.replyBackground
     }
     
-    public var isVisible: Bool = true {
+    public required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.frame.size.height          = textViewMinHeight
+        
+        // Load the nib + add its container view
+        bundle = NSBundle.mainBundle().loadNibNamed("ReplyTextView", owner: self, options: nil)
+        addSubview(containerView)
+        
+        // We want this view to stick at the bottom
+        contentMode                     = .BottomLeft
+        autoresizingMask                = .FlexibleWidth | .FlexibleTopMargin
+        containerView.autoresizingMask  = .FlexibleWidth | .FlexibleHeight
+        
+        // Setup the TextView
+        textView.delegate               = self
+        textView.scrollsToTop           = false
+        textView.contentInset           = UIEdgeInsetsZero
+    }
+    
+    
+    // MARK: - Public Properties
+    public var font: UIFont! {
         didSet {
-            let height: CGFloat = isVisible ? textViewHeight : CGFloat.min
-            
-            for constraint in constraints() as [NSLayoutConstraint] {
-                if constraint.firstAttribute == NSLayoutAttribute.Height {
-                    constraint.constant = height
-                }
-            }
+            textView.font               = font
+            placeholderLabel.font       = font
         }
     }
     
+    public var placeholder: String! {
+        didSet {
+            placeholderLabel.text       = placeholder
+        }
+    }
+
+    public var replyText: String! {
+        didSet {
+            replyButton.setTitle(replyText, forState: .Normal)
+        }
+    }
+    
+    
+    // MARK: - Public Helpers
+    public func alignAtBottomOfSuperview() {
+        if let theSuperview = superview {
+            frame.origin.y = CGRectGetMaxY(theSuperview.bounds) - bounds.height
+        }
+    }
+    
+    
+    // MARK: - UITextViewDelegate
+    public func textViewDidChange(textView: UITextView!) {
+        placeholderLabel.hidden = !textView.text.isEmpty
+        updateTextViewSize()
+        scrollToCaretInTextView()
+    }
+    
+    
+    // MARK: - View Methods
+    public override func becomeFirstResponder() -> Bool {
+        return textView.becomeFirstResponder()
+    }
+    
+    public override func resignFirstResponder() -> Bool {
+        endEditing(true)
+        return textView.resignFirstResponder()
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        containerView.frame.size.width = self.bounds.width
+    }
+    
+    
+    // MARK: - Private Helpers
+    private func updateTextViewSize() {
+        let textSize        = textView.contentSize
+        
+        let oldHeight       = frame.height
+        let textHeight      = ceil(textSize.height) + textViewPadding.bottom + textViewPadding.top
+        let newHeight       = min(max(textHeight, textViewMinHeight), textViewMaxHeight)
+        
+        if oldHeight == newHeight {
+            return
+        }
+
+        frame.size.height   = newHeight
+        frame.origin.y      += oldHeight - newHeight
+    }
+    
+    private func scrollToCaretInTextView() {
+        textView.layoutIfNeeded()
+        
+        var caretRect           = textView.caretRectForPosition(textView.selectedTextRange.start)
+        caretRect.size.height   += textView.textContainerInset.bottom + textViewPadding.bottom
+        
+        caretRect               = CGRectIntegral(caretRect)
+        
+        textView.scrollRectToVisible(caretRect, animated: false)
+    }
+    
+    
     // MARK: - Constants
-    private let textViewHeight: CGFloat = 44
+    private let textViewPadding:            UIEdgeInsets = UIEdgeInsets(top: 3, left: 0, bottom: 3, right: 0)
+    private let textViewMaxHeight:          CGFloat     = 84
+    private let textViewMinHeight:          CGFloat     = 44
+    private let bundle:                     NSArray?
     
     // MARK: - IBOutlets
-    @IBOutlet private var textField: UITextField!
+    @IBOutlet private var textView:         UITextView!
+    @IBOutlet private var placeholderLabel: UILabel!
+    @IBOutlet private var replyButton:      UIButton!
+    @IBOutlet private var containerView:    UIView!
 }
