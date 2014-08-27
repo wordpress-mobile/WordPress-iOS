fc0e5b6f61cd8c8d07a96734e37308f9e8a0c078
Jorge Leandro Perez <jorge@lantean.co>
2014-08-26 22:51:35 -0300

    Notifications: Fixes Autolayout calculation

--- WordPress/Classes/ViewRelated/Notifications/Style/UITableViewCell+Helpers.swift
+++ WordPress/Classes/ViewRelated/Notifications/Style/UITableViewCell+Helpers.swift
@@ -5,13 +5,13 @@ extension UITableViewCell
 {
     public func layoutHeightWithWidth(width: CGFloat) -> CGFloat {
         // Layout: Setup the cell with the given width
-        bounds = CGRect(x: 0, y: 0, width: width, height: self.bounds.height)
+        let cappedWidth = min(WPTableViewFixedWidth, width)
+        bounds          = CGRect(x: 0, y: 0, width: cappedWidth, height: self.bounds.height)
+        setNeedsLayout()
         layoutIfNeeded()
         
         // iPad: Limit the width
-        let cappedWidth = min(WPTableViewFixedWidth, width)
-        let maximumSize = CGSize(width: cappedWidth, height: 0)
-        let layoutSize  = contentView.systemLayoutSizeFittingSize(maximumSize)
+        let layoutSize  = contentView.systemLayoutSizeFittingSize(UILayoutFittingCompressedSize)
         
         // Workaround: Layout calculations fail under certain scenarios by 1px, cutting labels
         let PaddingY: CGFloat = 1
