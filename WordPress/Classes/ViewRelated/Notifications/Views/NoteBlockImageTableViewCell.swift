7124637cb906e82bc799e52fe4949b1707321058
Jorge Leandro Perez <jorge@lantean.co>
2014-08-27 19:32:36 -0300

    Updates UIImageView Helper Extensions

--- WordPress/Classes/ViewRelated/Notifications/Views/NoteBlockImageTableViewCell.swift
+++ WordPress/Classes/ViewRelated/Notifications/Views/NoteBlockImageTableViewCell.swift
@@ -9,56 +9,26 @@ import Foundation
             return
         }
 
-        if let unwrappedURL = url {
-            blockImageView.downloadImage(unwrappedURL, placeholderImage: nil, success: displayImageWithAnimation, failure: nil)
+        let success = { (image: UIImage) in
+            self.blockImageView.displayImageWithSpringAnimation(image)
         }
+        
+        blockImageView.downloadImage(url, placeholderName: nil, success: success, failure: nil)
         
         imageURL = url
     }
     
     // MARK: - View Methods
     public override func awakeFromNib() {
+        super.awakeFromNib()
         selectionStyle  = .None
         backgroundColor = WPStyleGuide.Notifications.Colors.blockBackground
     }
     
     // MARK: - Private Methods
-    private func displayImageWithAnimation(image: UIImage) {
-        blockImageView.image        = image
-        blockImageView.hidden       = false
-        blockImageView.transform    = Animation.transformInitial
-        
-        var animations = {
-            self.blockImageView.transform = Animation.transformFinal
-        }
-        
-        UIView.animateWithDuration(Animation.duration,
-            delay:                  Animation.delay,
-            usingSpringWithDamping: Animation.damping,
-            initialSpringVelocity:  Animation.velocity,
-            options:                nil,
-            animations:             animations,
-            completion:             nil
-        )
-    }
+
     
     // MARK: - Private
-    private struct Animation {
-        static let duration     = 0.5
-        static let delay        = 0.2
-        static let damping      = CGFloat(0.7)
-        static let velocity     = CGFloat(0.5)
-        static let scaleInitial = CGFloat(0.0)
-        static let scaleFinal   = CGFloat(1.0)
-        
-        static var transformInitial: CGAffineTransform {
-        return CGAffineTransformMakeScale(scaleInitial, scaleInitial)
-        }
-        
-        static var transformFinal: CGAffineTransform {
-        return CGAffineTransformMakeScale(scaleFinal, scaleFinal)
-        }
-    }
     private var imageURL:               NSURL?
     
     // MARK: - IBOutlets
