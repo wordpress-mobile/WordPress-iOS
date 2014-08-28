b91f190123e0ed2f07d50196a3cea448eb079d4d
Jorge Leandro Perez <jorge@lantean.co>
2014-08-28 16:21:34 -0300

    Folder Cleanup

---
+++ WordPress/Classes/ViewRelated/Notifications/Style/WPStyleGuide+Comments.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+
+extension WPStyleGuide
+{
+    public struct Comments
+    {
+        public struct Fonts
+        {
+            private static let replyFontSize    = CGFloat(UIDevice.isPad() ? 16 : 14)
+            public static let replyButton       = WPFontManager.openSansBoldFontOfSize(13)
+            public static let replyText         = WPFontManager.openSansRegularFontOfSize(replyFontSize)
+        }
+
+        public struct Colors
+        {
+            public static let replyEnabled      = UIColor(red: 0xA1/255.0, green: 0xB9/255.0, blue: 0xCA/255.0, alpha: 0xFF/255.0)
+            public static let replyDisabled     = WPStyleGuide.newKidOnTheBlockBlue()
+            public static let replyPlaceholder  = UIColor(red: 0xC6/255.0, green: 0xD5/255.0, blue: 0xDF/255.0, alpha: 0xFF/255.0)
+            public static let replyText         = WPStyleGuide.littleEddieGrey()
+            public static let replySeparator    = UIColor(red: 0xA3/255.0, green: 0xB9/255.0, blue: 0xCA/255.0, alpha: 0xFF/255.0)
+            public static let replyBackground   = UIColor(red: 0xF1/255.0, green:0xF6/255.0, blue:0xF9/255.0, alpha:0xFF/255.0)
+        }
+    }
+}
