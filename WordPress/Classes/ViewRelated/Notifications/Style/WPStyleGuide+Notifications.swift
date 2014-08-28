6b19c8e11fd01e39ccf837a9467b1132b4851ab2
Jorge Leandro Perez <jorge@lantean.co>
2014-08-28 16:21:34 -0300

    Folder Cleanup

--- WordPress/Classes/ViewRelated/Notifications/Style/WPStyleGuide+Notifications.swift
+++ WordPress/Classes/ViewRelated/Notifications/Style/WPStyleGuide+Notifications.swift
@@ -10,7 +10,8 @@ extension WPStyleGuide
             public static let noticon           = UIFont(name: "Noticons", size: 16)
             public static let timestamp         = WPFontManager.openSansRegularFontOfSize(14)
             public static let header            = WPStyleGuide.tableviewSectionHeaderFont()
-            
+            public static let reply             = WPFontManager.openSansBoldFontOfSize(13)
+
             private static let subjectSize      = CGFloat(14)
             public static let subjectRegular    = WPFontManager.openSansRegularFontOfSize(subjectSize)
             public static let subjectBold       = WPFontManager.openSansBoldFontOfSize(subjectSize)
@@ -34,7 +35,12 @@ extension WPStyleGuide
             public static let backgroundUnread  = UIColor(red: 0xF1/255.0, green: 0xF6/255.0, blue: 0xF9/255.0, alpha: 0xFF/255.0)
             
             public static let timestamp         = UIColor(red: 0xB7/255.0, green: 0xC9/255.0, blue: 0xD5/255.0, alpha: 0xFF/255.0)
-            
+
+            public static let replyEnabled      = UIColor(red: 0xA1/255.0, green: 0xB9/255.0, blue: 0xCA/255.0, alpha: 0xFF/255.0)
+            public static let replyDisabled     = WPStyleGuide.newKidOnTheBlockBlue()
+            public static let replyPlaceholder  = UIColor(red: 0xC6/255.0, green: 0xD5/255.0, blue: 0xDF/255.0, alpha: 0xFF/255.0)
+            public static let replySeparator    = UIColor(red: 0xA3/255.0, green: 0xB9/255.0, blue: 0xCA/255.0, alpha: 0xFF/255.0)
+
             public static let blockBackground   = UIColor.whiteColor()
             public static let blockText         = WPStyleGuide.littleEddieGrey()
             public static let blockLink         = WPStyleGuide.baseLighterBlue()
