5195b3cab1b3f0a2284776ad6cd8bc5dc5181626
Jorge Leandro Perez <jorge@lantean.co>
2014-08-29 00:17:44 -0300

    ReplyTextView: Implements Reply Callback

--- WordPress/Classes/ViewRelated/Notifications/Style/WPStyleGuide+Comments.swift
+++ WordPress/Classes/ViewRelated/Notifications/Style/WPStyleGuide+Comments.swift
@@ -14,8 +14,8 @@ extension WPStyleGuide
 
         public struct Colors
         {
-            public static let replyEnabled      = UIColor(red: 0xA1/255.0, green: 0xB9/255.0, blue: 0xCA/255.0, alpha: 0xFF/255.0)
-            public static let replyDisabled     = WPStyleGuide.newKidOnTheBlockBlue()
+            public static let replyEnabled      = WPStyleGuide.newKidOnTheBlockBlue()
+            public static let replyDisabled     = UIColor(red: 0xA1/255.0, green: 0xB9/255.0, blue: 0xCA/255.0, alpha: 0xFF/255.0)
             public static let replyPlaceholder  = UIColor(red: 0xC6/255.0, green: 0xD5/255.0, blue: 0xDF/255.0, alpha: 0xFF/255.0)
             public static let replyText         = WPStyleGuide.littleEddieGrey()
             public static let replySeparator    = UIColor(red: 0xA3/255.0, green: 0xB9/255.0, blue: 0xCA/255.0, alpha: 0xFF/255.0)
