27b49aa1028b37f4aa652356baff92d30041bc50
Jorge Leandro Perez <jorge@lantean.co>
2014-08-30 23:52:23 -0300

    Fixes ReplyTextView Sizing Glitch

--- WordPress/Classes/ViewRelated/Notifications/Extensions/UIView+Helpers.swift
+++ WordPress/Classes/ViewRelated/Notifications/Extensions/UIView+Helpers.swift
@@ -3,6 +3,17 @@ import Foundation
 
 extension UIView
 {
+    public func constraintForAttribute(attribute: NSLayoutAttribute) -> CGFloat? {
+        for constraint in constraints() as [NSLayoutConstraint] {
+            if constraint.firstItem as NSObject == self {
+                if constraint.firstAttribute == attribute || constraint.secondAttribute == attribute {
+                    return constraint.constant
+                }
+            }
+        }
+        return nil
+    }
+
     public func updateConstraint(attribute: NSLayoutAttribute, constant: CGFloat) {
         updateConstraintForView(self, attribute: attribute, constant: constant)
     }
