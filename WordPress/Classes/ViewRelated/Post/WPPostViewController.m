77f7b80eeeb76ba08f02aaa8e420851c3cd6d4d6
Diego Rey Mendez <diegoreymendez@gmail.com>
2014-09-02 12:39:29 -0300

    Tapping the keep editing button works again.

--- WordPress/Classes/ViewRelated/Post/WPPostViewController.m
+++ WordPress/Classes/ViewRelated/Post/WPPostViewController.m
@@ -1089,6 +1089,9 @@ name:MediaShouldInsertBelowNotification object:nil];
 			if ([actionSheet numberOfButtons] == 2) {
                 
 				[actionSheet dismissWithClickedButtonIndex:0 animated:YES];
+				
+				[self startEditing];
+				
                 // Save draft
 			} else {
                 // If you tapped on a button labeled "Save Draft", you probably expect the post to be saved as a draft
