7d4b9d26d7ca0f2fd0caa87d668b359811fb63de
Aaron Douglas <astralbodies@gmail.com>
2014-09-03 07:19:28 -0500

    Added constants for all parameters for Today Widget

--- WordPress/Classes/System/Constants.m
+++ WordPress/Classes/System/Constants.m
@@ -9,3 +9,14 @@ NSString *const WPComDefaultAccountUrlKey                = @"AccountDefaultDotco
 NSString *const WPComDefaultAccountUsernameKey            = @"wpcom_username_preference";
 
 NSString *const WPNotificationsJetpackInformationURL    = @"http://jetpack.me/about/";
+
+NSString *const WPAppGroupName = @"group.org.wordpress";
+
+NSString *const WPStatsTodayWidgetOAuth2TokenKeychainUsername = @"OAuth2Token";
+NSString *const WPStatsTodayWidgetOAuth2TokenKeychainServiceName = @"TodayWidget";
+NSString *const WPStatsTodayWidgetOAuth2TokenKeychainAccessGroup = @"3TMU3BH3NK.org.wordpress";
+NSString *const WPStatsTodayWidgetUserDefaultsSiteIdKey = @"WordPressTodayWidgetSiteId";
+NSString *const WPStatsTodayWidgetUserDefaultsSiteNameKey = @"WordPressTodayWidgetSiteName";
+NSString *const WPStatsTodayWidgetUserDefaultsSiteTimeZoneKey = @"WordPressTodayWidgetTimeZone";
+NSString *const WPStatsTodayWidgetUserDefaultsViewCountKey = @"TodayViewCount";
+NSString *const WPStatsTodayWidgetUserDefaultsVisitorCountKey = @"TodayVisitorCount";
