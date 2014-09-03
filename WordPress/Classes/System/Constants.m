1504a798b569a65fbe0105fd7f4bdca8a4fb3692
Aaron Douglas <astralbodies@gmail.com>
2014-09-03 12:31:46 -0500

    Support Internal build for Today Extension

--- WordPress/Classes/System/Constants.m
+++ WordPress/Classes/System/Constants.m
@@ -10,11 +10,16 @@ NSString *const WPComDefaultAccountUsernameKey            = @"wpcom_username_pre
 
 NSString *const WPNotificationsJetpackInformationURL    = @"http://jetpack.me/about/";
 
+#ifdef INTERNAL_BUILD
+NSString *const WPAppGroupName = @"group.org.wordpress.internal";
+NSString *const WPStatsTodayWidgetOAuth2TokenKeychainAccessGroup = @"99KV9Z6BKV.org.wordpress.internal";
+#else
 NSString *const WPAppGroupName = @"group.org.wordpress";
+NSString *const WPStatsTodayWidgetOAuth2TokenKeychainAccessGroup = @"3TMU3BH3NK.org.wordpress";
+#endif
 
 NSString *const WPStatsTodayWidgetOAuth2TokenKeychainUsername = @"OAuth2Token";
 NSString *const WPStatsTodayWidgetOAuth2TokenKeychainServiceName = @"TodayWidget";
-NSString *const WPStatsTodayWidgetOAuth2TokenKeychainAccessGroup = @"3TMU3BH3NK.org.wordpress";
 NSString *const WPStatsTodayWidgetUserDefaultsSiteIdKey = @"WordPressTodayWidgetSiteId";
 NSString *const WPStatsTodayWidgetUserDefaultsSiteNameKey = @"WordPressTodayWidgetSiteName";
 NSString *const WPStatsTodayWidgetUserDefaultsSiteTimeZoneKey = @"WordPressTodayWidgetTimeZone";
