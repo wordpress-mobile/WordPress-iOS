8735674b2070723d86aff1b39f48e22c3643963d
Oguz Kocer <oguz.kocer@automattic.com>
2014-09-04 20:42:02 +0300

    Only show suggestions list if it's a WPcom blog for notifications

--- WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationDetailsViewController.m
+++ WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationDetailsViewController.m
@@ -948,7 +948,14 @@ static CGFloat NotificationSectionSeparator     = 10;
 - (void)didStartAtMention:(UIView *)view
 {
     NSNumber *siteID = self.note.metaSiteID;
-    if ([[SuggestionService shared] shouldShowSuggestionsPageForSiteID:siteID]) {
+    if (!siteID) {
+        return;
+    }
+
+    NSManagedObjectContext *context = [[ContextManager sharedInstance] mainContext];
+    BlogService *service = [[BlogService alloc] initWithManagedObjectContext:context];
+    Blog *blog = [service blogByBlogId:siteID];
+    if (blog && blog.isWPcom && [[SuggestionService shared] shouldShowSuggestionsPageForSiteID:siteID]) {
         SuggestionsTableViewController *suggestionsController = [[SuggestionsTableViewController alloc] initWithSiteID:siteID];
         suggestionsController.delegate = self;
         [self.navigationController pushViewController:suggestionsController animated:YES];
