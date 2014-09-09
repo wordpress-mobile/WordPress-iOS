6f340f4a21084aec0a17bfa9c47ef3dd88393187
Diego Rey Mendez <diegoreymendez@gmail.com>
2014-09-09 11:21:49 -0300

    Integrated the latest editor pod. Cocoapods is also forcefully updating NSLogger for some reason.

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -153,7 +153,7 @@
 // NSLogger
 #define COCOAPODS_POD_AVAILABLE_NSLogger
 #define COCOAPODS_VERSION_MAJOR_NSLogger 1
-#define COCOAPODS_VERSION_MINOR_NSLogger 2
+#define COCOAPODS_VERSION_MINOR_NSLogger 5
 #define COCOAPODS_VERSION_PATCH_NSLogger 0
 
 // NSLogger-CocoaLumberjack-connector
