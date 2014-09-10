985630a7425c83f318fee56fb60529fb007e195b
Jorge Leandro Perez <jorge@lantean.co>
2014-09-10 18:17:05 -0300

    Implements WPDynamicHeightTextView

---
+++ WordPress/Classes/Utility/WPDynamicHeightTextView.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+
+public class WPDynamicHeightTextView : UITextView
+{
+    public var preferredMaxLayoutWidth: CGFloat = 0 {
+        didSet {
+            invalidateIntrinsicContentSize()
+        }
+    }
+    
+    public override func intrinsicContentSize() -> CGSize {
+        let maxWidth = (preferredMaxLayoutWidth != 0) ? preferredMaxLayoutWidth : frame.width
+        let maxSize = CGSize(width: maxWidth, height: CGFloat.max)
+        let requiredSize = sizeThatFits(maxSize)
+        return requiredSize
+    }
+}
