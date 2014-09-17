79b0c0071638302f178dbcd405121203e1ecd5bd
Gregory Cornelius <gcorne@gmail.com>
2014-09-17 16:51:45 -0600

    Reader: Add/Remove edit button when adding/removing first/last tag

--- WordPress/Classes/ViewRelated/Reader/SubscribedTopicsViewController.h
+++ WordPress/Classes/ViewRelated/Reader/SubscribedTopicsViewController.h
@@ -1,6 +1,9 @@
 #import <UIKit/UIKit.h>
 #import "ReaderEditableSubscriptionPage.h"
 
+typedef void (^TopicChanged)();
+
 @interface SubscribedTopicsViewController : UIViewController<ReaderEditableSubscriptionPage>
 - (BOOL) isEditable;
+@property (nonatomic, copy) TopicChanged topicChangedBlock;
 @end
