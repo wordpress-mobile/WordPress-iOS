7d267df06a5238534f73e21310c9c1f16342defd
Matt Husby <matthusby@gmail.com>
2014-09-18 11:19:49 -0600

    Set the background color for WPWebView to itsEverywhereGrey and add category for UIColor to get hex value

---
+++ WordPress/Classes/Categories/UIColor+HTML.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+HTML.h
+//  WordPress
+//
+//  Created by Matt Husby on 9/18/14.
+//  Copyright (c) 2014 WordPress. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIColor (HTML)
+
+- (NSString *)hexStringForWebViews;
+
+@end
