d53a3500000454dda1070957b7156a613b3a46f0
Aaron Douglas <astralbodies@gmail.com>
2014-09-19 21:33:53 -0600

    Whacked Pods directory

--- Pods/Pods-WordPressTodayWidget-CocoaLumberjack-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WordPressTodayWidget_CocoaLumberjack : NSObject
-@end
-@implementation PodsDummy_Pods_WordPressTodayWidget_CocoaLumberjack
-@end
