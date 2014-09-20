70007b6ccc5c78f354c646e3e2a522f50f608707
Jorge Leandro Perez <jorge@lantean.co>
2014-09-19 18:25:42 -0600

    EditCommentViewController: Switching to Blocks

--- WordPress/Classes/ViewRelated/Comments/EditReplyViewController.m
+++ WordPress/Classes/ViewRelated/Comments/EditReplyViewController.m
@@ -1,5 +1,4 @@
 #import "EditReplyViewController.h"
-#import "EditCommentViewController+Internals.h"
 
 
 
@@ -12,23 +11,6 @@
     self.title = NSLocalizedString(@"Edit Reply", @"");
     self.navigationItem.rightBarButtonItem.title = NSLocalizedString(@"Send", @"");
     self.content = [NSString string];
-}
-
-
-#pragma mark - Helper Methods
-
-- (void)finishWithUpdates
-{
-    if ([self.replyDelegate respondsToSelector:@selector(editReplyViewController:didFinishWithContent:)]) {
-        [self.replyDelegate editReplyViewController:self didFinishWithContent:self.textView.text];
-    }
-}
-
-- (void)finishWithoutUpdates
-{
-    if ([self.replyDelegate respondsToSelector:@selector(editReplyViewControllerFinished:)]) {
-        [self.replyDelegate editReplyViewControllerFinished:self];
-    }
 }
 
 @end
