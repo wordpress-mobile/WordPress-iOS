64fc688574da9ceed970c9d6d8f53bbb488c290f
Sérgio Estâvão <sergio.estevao@automattic.com>
2014-10-03 17:04:23 +0100

    Clean up header comments

--- WordPress/UITests/LoginTests.m
+++ WordPress/UITests/LoginTests.m
@@ -1,11 +1,3 @@
-//
-//  LoginTests.m
-//  LoginTests
-//
-//  Created by Sergio Estevao on 01/10/2014.
-//  Copyright (c) 2014 WordPress. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 #import <KIF/KIF.h>
