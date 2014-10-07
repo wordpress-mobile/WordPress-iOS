8145dafefde87352052cfde03ab2edd1d62927f7
Allen Snook <allen@allendav.com>
2014-10-07 11:43:15 -0700

    Adding SuggestionsTableView and Cell

---
+++ WordPress/Classes/ViewRelated/Suggestions/SuggestionsTableView.m
@@ -0,0 +1,194 @@
+#import "SuggestionsTableView.h"
+#import "SuggestionsTableViewCell.h"
+#import "Suggestion.h"
+#import "UIImageView+AFNetworking.h"
+#import "SuggestionService.h"
+
+NSString * const CellIdentifier = @"SuggestionsTableViewCell";
+
+@interface SuggestionsTableView ()
+
+@property (nonatomic, strong) NSNumber *siteID;
+@property (nonatomic, strong) NSArray *suggestions;
+@property (nonatomic, strong) NSString *searchText;
+@property (nonatomic, strong) NSArray *searchResults;
+
+@end
+
+@implementation SuggestionsTableView
+
+- (id)initWithFrame:(CGRect)frame andSiteID:(NSNumber *)siteID
+{    
+    self = [super initWithFrame:frame];
+    if (self) {
+        UINib *nib = [UINib nibWithNibName:@"SuggestionsTableViewCell" bundle:nil];
+        [self registerNib:nib forCellReuseIdentifier:CellIdentifier];
+
+        _siteID = siteID;
+        _searchText = @"";
+        _suggestions = [[NSArray alloc] init];
+        _searchResults = [[NSArray alloc] init];
+        
+        [self setDataSource:self];
+        
+        [self updateSearchResultsForText:@""];
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    self.delegate = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{   
+    self.rowHeight = 50.0;
+    
+    // suppress display of blank rows
+    self.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(suggestionListUpdated:)
+                                                 name:SuggestionListUpdatedNotification
+                                               object:nil];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+-(void)filterSuggestionsForKeyPress:(NSString *)keypress inWord:(NSString *)word
+{    
+    if ([keypress isEqualToString:@"@"]) {
+        if ([keypress isEqualToString:word]) {
+            [self updateSearchResultsForText:@""];
+            [self reloadData];
+            [self showSuggestions:YES];
+        }
+    } else {
+        if ([word hasPrefix:@"@"]) {
+            [self updateSearchResultsForText:[word substringFromIndex:1]];
+            [self reloadData];
+            [self showSuggestions:YES];
+        } else {
+            [self updateSearchResultsForText:@""];
+            [self reloadData];
+            [self showSuggestions:NO];
+        }
+    }
+}
+
+- (void)updateSearchResultsForText:(NSString *)text
+{
+    // Save how we got here
+    _searchText = text;
+    
+    if (_searchText.length > 0) {
+        NSPredicate *resultPredicate = [NSPredicate predicateWithFormat:@"(displayName contains[c] %@) OR (userLogin contains[c] %@)",
+                                        _searchText, _searchText];
+        self.searchResults = [self.suggestions filteredArrayUsingPredicate:resultPredicate];
+    }
+    else {
+        self.searchResults = self.suggestions;
+    }
+}
+
+- (void)showSuggestions:(BOOL)show
+{
+    if (show) {
+        self.hidden = NO;
+        [self.superview bringSubviewToFront:self];
+    } else {
+        [self updateSearchResultsForText:@""];
+        self.hidden = YES;
+        [self.superview sendSubviewToBack:self];
+    }
+}
+
+#pragma mark - UITableViewDataSource methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (!self.suggestions) {
+        return 1;
+    }
+    
+    return self.searchResults.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    SuggestionsTableViewCell *cell = [self dequeueReusableCellWithIdentifier:CellIdentifier
+                                                                forIndexPath:indexPath];
+    
+    if (!self.suggestions) {
+        cell.usernameLabel.text = NSLocalizedString(@"Loading...", @"Suggestions loading message");
+        cell.displayNameLabel.text = nil;
+        [cell.avatarImageView setImage:nil];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        return cell;
+    }
+    
+    Suggestion *suggestion = [self.searchResults objectAtIndex:indexPath.row];
+    
+    cell.usernameLabel.text = [NSString stringWithFormat:@"@%@", suggestion.userLogin];
+    cell.displayNameLabel.text = suggestion.displayName;
+    cell.selectionStyle = UITableViewCellSelectionStyleDefault;
+    
+    [self setAvatarForSuggestion:suggestion forCell:cell indexPath:indexPath];
+    
+    return cell;
+}
+
+#pragma mark - Suggestion list management
+
+- (void)suggestionListUpdated:(NSNotification *)notification
+{
+    // only reload if the suggestion list is updated for the current site
+    if ([notification.object isEqualToNumber:_siteID]) {
+        self.suggestions = [[SuggestionService shared] suggestionsForSiteID:_siteID];
+        
+        [self updateSearchResultsForText:_searchText];
+        
+        [self reloadData];
+    }
+}
+
+- (NSArray *)suggestions
+{
+    if (!_suggestions) {
+        _suggestions = [[SuggestionService shared] suggestionsForSiteID:_siteID];
+    }
+    return _suggestions;
+}
+
+#pragma mark - Avatar helper
+
+- (void)setAvatarForSuggestion:(Suggestion *)post forCell:(SuggestionsTableViewCell *)cell indexPath:(NSIndexPath *)indexPath
+{
+    CGSize imageSize = CGSizeMake(SuggestionsTableViewCellAvatarSize, SuggestionsTableViewCellAvatarSize);
+    UIImage *image = [post cachedAvatarWithSize:imageSize];
+    if (image) {
+        [cell.avatarImageView setImage:image];
+    } else {
+        [cell.avatarImageView setImage:[UIImage imageNamed:@"gravatar"]];
+        [post fetchAvatarWithSize:imageSize success:^(UIImage *image) {
+            if (!image) {
+                return;
+            }
+            if (cell == [self cellForRowAtIndexPath:indexPath]) {
+                [cell.avatarImageView setImage:image];
+            }
+        }];
+    }
+}
+
+@end
---
+++ WordPress/Classes/ViewRelated/Suggestions/SuggestionsTableViewCell.m
@@ -0,0 +1,81 @@
+#import "SuggestionsTableViewCell.h"
+
+NSInteger const SuggestionsTableViewCellAvatarSize = 32;
+
+@implementation SuggestionsTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        reuseID = reuseIdentifier;
+        
+        _usernameLabel = [[UILabel alloc] init];
+        [_usernameLabel setTextColor:[WPStyleGuide wordPressBlue]];
+        [_usernameLabel setFont:[WPStyleGuide regularTextFont]];
+        [_usernameLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
+        [self.contentView addSubview:_usernameLabel];
+        
+        _displayNameLabel = [[UILabel alloc] init];
+        [_displayNameLabel setTextColor:[WPStyleGuide readGrey]];
+        [_displayNameLabel setFont:[WPStyleGuide regularTextFont]];
+        [_displayNameLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
+        _displayNameLabel.textAlignment = NSTextAlignmentRight;
+        [self.contentView addSubview:_displayNameLabel];
+        
+        _avatarImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, SuggestionsTableViewCellAvatarSize, SuggestionsTableViewCellAvatarSize)];
+        _avatarImageView.contentMode = UIViewContentModeScaleAspectFit;
+        _avatarImageView.clipsToBounds = YES;
+        _avatarImageView.image = [UIImage imageNamed:@"gravatar.png"];
+        [_avatarImageView setTranslatesAutoresizingMaskIntoConstraints:NO];
+        [self.contentView addSubview:_avatarImageView];
+        
+        NSDictionary *views = @{@"contentview": self.contentView,
+                                @"username": _usernameLabel,
+                                @"displayname": _displayNameLabel,
+                                @"avatar": _avatarImageView };
+        
+        // Horizontal spacing
+        NSArray *horizConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[avatar(32)]-16-[username]-[displayname]-|"
+                                                                            options:0
+                                                                            metrics:nil
+                                                                              views:views];
+        [self.contentView addConstraints:horizConstraints];
+        
+        // Vertical height constraint
+        [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[contentview(==48)]"
+                                                                                 options:0
+                                                                                 metrics:nil
+                                                                                   views:views]];
+        
+        // Vertically constrain centers of each element
+        [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:_usernameLabel
+                                                                     attribute:NSLayoutAttributeCenterY
+                                                                     relatedBy:NSLayoutRelationEqual
+                                                                        toItem:self.contentView
+                                                                     attribute:NSLayoutAttributeCenterY
+                                                                    multiplier:1.0
+                                                                      constant:0]];
+        
+        [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:_displayNameLabel
+                                                                     attribute:NSLayoutAttributeCenterY
+                                                                     relatedBy:NSLayoutRelationEqual
+                                                                        toItem:self.contentView
+                                                                     attribute:NSLayoutAttributeCenterY
+                                                                    multiplier:1.0
+                                                                      constant:0]];
+        
+        [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:_avatarImageView
+                                                                     attribute:NSLayoutAttributeCenterY
+                                                                     relatedBy:NSLayoutRelationEqual
+                                                                        toItem:self.contentView
+                                                                     attribute:NSLayoutAttributeCenterY
+                                                                    multiplier:1.0
+                                                                      constant:0]];
+        
+        
+    }
+    return self;
+}
+
+@end
