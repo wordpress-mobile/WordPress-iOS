84f1a7bcba0ab6db71d0cf9d1f049d3224960ddd
Allen Snook <allen@allendav.com>
2014-10-08 11:43:34 -0700

    Add protocols between VC and ReplyTextView
    SuggestionsDelegate protocol and SuggestableView protocol (note: this
    disables showing suggestions for the moment - need to add impl details
    to ReplyTextView.swift)

--- WordPress/Classes/ViewRelated/Suggestions/SuggestionsTableView.h
+++ WordPress/Classes/ViewRelated/Suggestions/SuggestionsTableView.h
@@ -1,8 +1,40 @@
 #import <UIKit/UIKit.h>
 
+@protocol SuggestionsDelegate
+
+@optional
+
+/*
+ * Child views of a UIViewController can call this method to have the
+ * UIViewController update the SuggestionsTableView with appropriate suggestions (if any)
+*/
+- (void)didTypeInWord:(NSString *)word;
+
+/*
+ * If the user picks a suggestion from the SuggestionsTableView, the SuggestionsTableView
+ * will call this method to have the UIViewController prompt the appropriate child
+ * to replace the search term with the suggestion (e.g. at the caret)
+*/
+- (void)didSelectSuggestion:(NSString *)suggestion forSearchText:(NSString *)text;
+
+@end
+
+@protocol SuggestableView
+
+@optional
+
+/*
+ * Child views of a UIViewController should implement this method so the
+ * view controller can prompt them to replace recently typed text with the
+ * suggested text the user picked
+ */
+- (void)replaceRecentlyTypedWord:(NSString *)word withSuggestion:(NSString *)suggestion;
+
+@end
+
 @interface SuggestionsTableView : UITableView <UITableViewDataSource>
 
 - (id)initWithWidth:(CGFloat)width andSiteID:(NSNumber *)siteID;
-- (void)filterSuggestionsForText:(NSString *)text;
+- (void)showSuggestionsForWord:(NSString *)word;
 
 @end
