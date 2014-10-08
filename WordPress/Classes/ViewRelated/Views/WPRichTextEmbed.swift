e00f61413c57defec7edbb9afa45b7dbf5eddc0b
aerych <eric@automattic.com>
2014-10-08 14:57:23 -0500

    Tweaks initial default viewport setting.

--- WordPress/Classes/ViewRelated/Views/WPRichTextEmbed.swift
+++ WordPress/Classes/ViewRelated/Views/WPRichTextEmbed.swift
@@ -102,7 +102,7 @@ class WPRichTextEmbed : UIView, UIWebViewDelegate, WPRichTextMediaAttachment
     }
 
     func loadHTMLString(html: NSString) {
-        var htmlString = NSString(format: "<html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0\" /></head><body>%@</body></html>", html)
+        var htmlString = NSString(format: "<html><head><meta name=\"viewport\" content=\"width=available-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no\" /></head><body>%@</body></html>", html)
         webView.loadHTMLString(htmlString, baseURL: nil)
     }
 
