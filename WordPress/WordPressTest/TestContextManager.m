44f63d0173f5674b41da085db3c5f9b799135923
Aaron Douglas <astralbodies@gmail.com>
2014-10-09 12:50:25 -0500

    Beginning a new TestContextManager to assist better unit testing
    No incremental upgrade changes yet

---
+++ WordPress/WordPressTest/TestContextManager.m
@@ -0,0 +1,17 @@
+//
+//  TestContextManager.m
+//  WordPress
+//
+//  Created by Aaron Douglas on 10/9/14.
+//  Copyright (c) 2014 WordPress. All rights reserved.
+//
+
+#import "TestContextManager.h"
+
+@implementation TestContextManager
+
+@synthesize managedObjectModel = _managedObjectModel;
+@synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
+@synthesize mainContext = _mainContext;
+
+@end
