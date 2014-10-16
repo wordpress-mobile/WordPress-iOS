5b381b85264d0e2fe14d0438da3ead29093b46f1
Jorge Bernal <jorge@automattic.com>
2014-10-16 16:20:06 +0200

    Implemented batched metadata sync in REST
    Metadata now only includes options and post formats, media is synced
    separately

--- WordPress/Classes/Networking/BlogServiceRemoteREST.m
+++ WordPress/Classes/Networking/BlogServiceRemoteREST.m
@@ -23,7 +23,7 @@
 {
     NSParameterAssert(blog != nil);
     NSParameterAssert(blog.dotComID != nil);
-    NSString *path = [NSString stringWithFormat:@"sites/%@", blog.dotComID];
+    NSString *path = [self pathForOptionsWithBlog:blog];
     [self.api GET:path
        parameters:nil
           success:^(AFHTTPRequestOperation *operation, id responseObject) {
@@ -43,11 +43,11 @@
 {
     NSParameterAssert(blog != nil);
     NSParameterAssert(blog.dotComID != nil);
-    NSString *path = [NSString stringWithFormat:@"sites/%@/post-formats", blog.dotComID];
+    NSString *path = [self pathForPostFormatsWithBlog:blog];
     [self.api GET:path
        parameters:nil
           success:^(AFHTTPRequestOperation *operation, id responseObject) {
-              NSDictionary *formats = responseObject[@"formats"];
+              NSDictionary *formats = [self mapPostFormatsFromResponse:responseObject[@"formats"]];
               if (success) {
                   success(formats);
               }
@@ -56,6 +56,56 @@
                   failure(error);
               }
           }];
+}
+
+- (void)syncBlogMetadata:(Blog *)blog
+          optionsSuccess:(OptionsHandler)optionsSuccess
+      postFormatsSuccess:(PostFormatsHandler)postFormatsSuccess
+          overallSuccess:(void (^)(void))overallSuccess
+                 failure:(void (^)(NSError *))failure
+{
+    NSParameterAssert(blog != nil);
+    NSParameterAssert(blog.dotComID != nil);
+    NSString *optionsPath = [NSString stringWithFormat:@"/%@", [self pathForOptionsWithBlog:blog]];
+    NSString *postFormatsPath = [NSString stringWithFormat:@"/%@", [self pathForPostFormatsWithBlog:blog]];
+    NSDictionary *parameters = @{
+                                 @"urls": @[
+                                          optionsPath,
+                                          postFormatsPath
+                                          ]
+                                  };
+    [self.api GET:@"batch"
+       parameters:parameters
+          success:^(AFHTTPRequestOperation *operation, id responseObject) {
+              NSDictionary *response = (NSDictionary *)responseObject;
+              NSDictionary *options = [self mapOptionsFromResponse:response[optionsPath]];
+              NSDictionary *postFormats = [self mapPostFormatsFromResponse:response[postFormatsPath][@"formats"]];
+              if (optionsSuccess) {
+                  optionsSuccess(options);
+              }
+              if (postFormatsSuccess) {
+                  postFormatsSuccess(postFormats);
+              }
+              if (overallSuccess) {
+                  overallSuccess();
+              }
+          } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+              if (failure) {
+                  failure(error);
+              }
+          }];
+}
+
+#pragma mark - API paths
+
+- (NSString *)pathForOptionsWithBlog:(Blog *)blog
+{
+    return [NSString stringWithFormat:@"sites/%@", blog.dotComID];
+}
+
+- (NSString *)pathForPostFormatsWithBlog:(Blog *)blog
+{
+    return [NSString stringWithFormat:@"sites/%@/post-formats", blog.dotComID];
 }
 
 #pragma mark - Mapping methods
@@ -75,7 +125,6 @@
                                 @"admin_url",
                                 @"login_url",
                                 @"image_default_link_type",
-                                @"post_formats",
                                 @"software_version",
                                 @"videopress_enabled",
                                 ];
@@ -90,6 +139,15 @@
     }];
 
     return [NSDictionary dictionaryWithDictionary:valueOptions ];
+}
+
+- (NSDictionary *)mapPostFormatsFromResponse:(id)response
+{
+    if ([response isKindOfClass:[NSDictionary class]]) {
+        return response;
+    } else {
+        return @{};
+    }
 }
 
 @end
