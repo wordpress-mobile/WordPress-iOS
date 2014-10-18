b834773c4b7ef121080d7fa9844cafcd4c03e8f5
aerych <eric@automattic.com>
2014-10-17 19:35:32 -0500

    Makes freshly pressed the default topic.

--- WordPress/Classes/Services/ReaderTopicService.m
+++ WordPress/Classes/Services/ReaderTopicService.m
@@ -87,7 +87,9 @@ static NSString * const ReaderTopicCurrentTopicURIKey = @"ReaderTopicCurrentTopi
     }
 
     if (topic == nil) {
+        // clear any saved topic that is no longer valid
         [self setCurrentTopic:nil];
+
         // Return a default topic
         NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"ReaderTopic"];
         request.predicate = [NSPredicate predicateWithFormat:@"type == %@", ReaderTopicTypeList];
@@ -98,10 +100,19 @@ static NSString * const ReaderTopicCurrentTopicURIKey = @"ReaderTopicCurrentTopi
             DDLogError(@"%@ error fetching topic: %@", NSStringFromSelector(_cmd), error);
             return nil;
         }
-        if ([topics count] > 0) {
-            topic = [topics objectAtIndex:0];
-            [self setCurrentTopic:topic];
+
+        if ([topics count] == 0) {
+            return nil;
         }
+
+        NSArray *matches = [topics filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"path CONTAINS[cd] %@", @"freshly-pressed"]];
+        if ([matches count]) {
+            topic = matches[0];
+        } else {
+            topic = topics[0];
+        }
+
+        [self setCurrentTopic:topic];
     }
 
     return topic;
