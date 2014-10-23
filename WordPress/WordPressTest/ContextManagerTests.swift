5dbdf337b01a384b86140f9f48918fde9910c63d
Aaron Douglas <astralbodies@gmail.com>
2014-10-23 07:18:40 -0500

    Made migration policy use generic NSManagedObjects
    Also removed all service calls as this migration may occur after those calls have been removed or changed entirely in the future.

--- WordPress/WordPressTest/ContextManagerTests.swift
+++ WordPress/WordPressTest/ContextManagerTests.swift
@@ -59,7 +59,64 @@ class ContextManagerTests: XCTestCase {
         
         XCTAssertNotNil(object, "Object should exist in new PSC")
     }
+    
+    func testMigrate21to22() {
+        let model21Url = self.urlForModelName("WordPress 21")
+        let model = NSManagedObjectModel(contentsOfURL: model21Url!)
+        var psc = NSPersistentStoreCoordinator(managedObjectModel: model!)
+        
+        let fileManager = NSFileManager.defaultManager()
+        let storeUrl = contextManager.storeURL()
+        let directoryUrl = storeUrl.URLByDeletingLastPathComponent
+        
+        let files = fileManager.contentsOfDirectoryAtURL(directoryUrl!, includingPropertiesForKeys: nil, options: NSDirectoryEnumerationOptions.SkipsSubdirectoryDescendants, error: nil) as Array<NSURL>
+        for file in files {
+            let range = file.lastPathComponent.rangeOfString(storeUrl.lastPathComponent, options: nil, range: nil, locale: nil)
+            if range?.startIndex != range?.endIndex {
+                fileManager.removeItemAtURL(file, error: nil)
+            }
+        }
+        
+        
+        let persistentStore = psc.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: storeUrl, options: nil, error: nil)
+        
+        XCTAssertNotNil(persistentStore, "Store should exist")
+        
+        let mocOriginal = NSManagedObjectContext(concurrencyType: NSManagedObjectContextConcurrencyType.MainQueueConcurrencyType)
+        mocOriginal.persistentStoreCoordinator = psc
+        let account = NSEntityDescription.insertNewObjectForEntityForName("Account", inManagedObjectContext: mocOriginal) as WPAccount
+        account.username = "username"
+        account.isWpcom = true
+        account.authToken = "authtoken"
+        account.xmlrpc = "http://example.com/xmlrpc.php"
+        
+        mocOriginal.obtainPermanentIDsForObjects([account], error: nil)
 
+        var error: NSError?
+        mocOriginal.save(&error)
+        
+        NSUserDefaults.standardUserDefaults().setURL(account.objectID.URIRepresentation(), forKey: "AccountDefaultDotcom")
+        NSUserDefaults.standardUserDefaults().synchronize()
+        
+        psc.removePersistentStore(persistentStore!, error: nil);
+        
+        let standardPSC = contextManager.standardPSC
+        
+        XCTAssertNotNil(standardPSC, "New store should exist")
+        XCTAssertTrue(standardPSC.persistentStores.count == 1, "Should be one persistent store.")
+        
+        let mocSecond = NSManagedObjectContext(concurrencyType: NSManagedObjectContextConcurrencyType.MainQueueConcurrencyType)
+        mocSecond.persistentStoreCoordinator = standardPSC
+        
+        let fetchRequest = NSFetchRequest(entityName: "Account")
+        let results = mocSecond.executeFetchRequest(fetchRequest, error: nil)
+        
+        XCTAssertTrue(results!.count == 1, "Should have one account")
+        
+        let newAccount = results![0] as WPAccount
+        XCTAssertNotNil(newAccount.uuid, "UUID should be assigned")
+        XCTAssertTrue(newAccount.username == "username", "Usernames should match")
+    }
     
     func urlForModelName(name: NSString!) -> NSURL? {
         var bundle = NSBundle.mainBundle()
