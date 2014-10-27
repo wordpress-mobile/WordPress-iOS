708d9bdda1fee4c450901dfcd3340e4969180757
Aaron Douglas <astralbodies@gmail.com>
2014-10-27 07:16:23 -0500

    Wrap DDLog in methods to make it possible to Cocoalumberjack in Swift

--- WordPress/Classes/System/WordPress-Bridging-Header.h
+++ WordPress/Classes/System/WordPress-Bridging-Header.h
@@ -1,11 +1,8 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
 #import <AFNetworking/UIKit+AFNetworking.h>
 
 #import "Notification.h"
 
+#import "DDLogSwift.h"
 #import "NSObject+Helpers.h"
 #import "NSAttributedString+Util.h"
 #import "NSDate+StringFormatting.h"
