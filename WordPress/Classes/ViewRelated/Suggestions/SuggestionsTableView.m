efe84fea6c8ca7421af7572e69a0bd322b4f1f02
Allen Snook <allen@allendav.com>
2014-10-28 12:33:10 -0700

    Broke out a few helper methods from initWithSiteID for readability / maintainability - props @diegoreymendez

--- WordPress/Classes/ViewRelated/Suggestions/SuggestionsTableView.m
+++ WordPress/Classes/ViewRelated/Suggestions/SuggestionsTableView.m
@@ -29,74 +29,90 @@ CGFloat const RowHeight = 44.0f;
         _suggestions = [[SuggestionService sharedInstance] suggestionsForSiteID:_siteID];
         _searchText = @"";
         _searchResults = [[NSMutableArray alloc] init];
-
-        _headerView = [[UIView alloc] init];
-        _headerView.backgroundColor = [UIColor colorWithRed:0.f green:0.f blue:0.f alpha:0.3f];
-        [_headerView setTranslatesAutoresizingMaskIntoConstraints:NO];
-        [self addSubview:_headerView];
-                
-        _tableView = [[UITableView alloc] init];
-        [_tableView registerClass:[SuggestionsTableViewCell class] forCellReuseIdentifier:CellIdentifier];
-        [_tableView setDataSource:self];
-        [_tableView setDelegate:self];
-        [_tableView setTranslatesAutoresizingMaskIntoConstraints:NO];
-        [_tableView setRowHeight:RowHeight];
-        // Table separator insets defined to match left edge of username in cell.
-        [_tableView setSeparatorInset:UIEdgeInsetsMake(0.f, 47.f, 0.f, 0.f)];
-        // iOS8 added and requires the following in order for that separator inset to be used
-        if ([self.tableView respondsToSelector:@selector(setLayoutMargins:)]) {
-            [_tableView setLayoutMargins:UIEdgeInsetsZero];
-        }
-        [self addSubview:_tableView];
         
-        // Pin the table view to the view's edges
-        NSDictionary *views = @{@"headerview": self.headerView,
-                                @"tableview": self.tableView };
-        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[headerview]|"
-                                                                     options:0
-                                                                     metrics:nil
-                                                                       views:views]];
-        
-        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[tableview]|"
-                                                                          options:0
-                                                                          metrics:nil
-                                                                            views:views]];
-        
-        // Vertically arrange the header and table
-        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[headerview][tableview]|"
-                                                                          options:0
-                                                                          metrics:nil
-                                                                            views:views]];
-
-        // Add a height constraint to the table view
-        self.heightConstraint = [NSLayoutConstraint constraintWithItem:self.tableView
-                                                             attribute:NSLayoutAttributeHeight
-                                                             relatedBy:NSLayoutRelationEqual
-                                                                toItem:nil
-                                                             attribute:nil
-                                                            multiplier:1
-                                                              constant:0.f];
-        self.heightConstraint.priority = 300;
-        
-        [self addConstraint:self.heightConstraint];
-                        
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(suggestionListUpdated:)
-                                                     name:SuggestionListUpdatedNotification
-                                                   object:nil];
-        
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(keyboardDidChangeFrame:)
-                                                     name:UIKeyboardDidChangeFrameNotification
-                                                   object:nil];
-        
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(deviceOrientationDidChange:)
-                                                     name:UIDeviceOrientationDidChangeNotification
-                                                   object:nil];
+        [self setupHeaderView];
+        [self setupTableView];
+        [self setupConstraints];
+        [self startObservingNotifications];
     }
-    
     return self;
+}
+
+- (void)setupHeaderView
+{
+    _headerView = [[UIView alloc] init];
+    _headerView.backgroundColor = [UIColor colorWithRed:0.f green:0.f blue:0.f alpha:0.3f];
+    [_headerView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self addSubview:_headerView];
+}
+
+- (void)setupTableView
+{
+    _tableView = [[UITableView alloc] init];
+    [_tableView registerClass:[SuggestionsTableViewCell class] forCellReuseIdentifier:CellIdentifier];
+    [_tableView setDataSource:self];
+    [_tableView setDelegate:self];
+    [_tableView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [_tableView setRowHeight:RowHeight];
+    // Table separator insets defined to match left edge of username in cell.
+    [_tableView setSeparatorInset:UIEdgeInsetsMake(0.f, 47.f, 0.f, 0.f)];
+    // iOS8 added and requires the following in order for that separator inset to be used
+    if ([self.tableView respondsToSelector:@selector(setLayoutMargins:)]) {
+        [_tableView setLayoutMargins:UIEdgeInsetsZero];
+    }
+    [self addSubview:_tableView];
+}
+
+- (void)setupConstraints
+{
+    // Pin the table view to the view's edges
+    NSDictionary *views = @{@"headerview": self.headerView,
+                             @"tableview": self.tableView };
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[headerview]|"
+                                                                 options:0
+                                                                 metrics:nil
+                                                                   views:views]];
+        
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[tableview]|"
+                                                                 options:0
+                                                                 metrics:nil
+                                                                   views:views]];
+        
+    // Vertically arrange the header and table
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[headerview][tableview]|"
+                                                                 options:0
+                                                                 metrics:nil
+                                                                   views:views]];
+
+    // Add a height constraint to the table view
+    self.heightConstraint = [NSLayoutConstraint constraintWithItem:self.tableView
+                                                         attribute:NSLayoutAttributeHeight
+                                                         relatedBy:NSLayoutRelationEqual
+                                                            toItem:nil
+                                                         attribute:nil
+                                                        multiplier:1
+                                                          constant:0.f];
+    self.heightConstraint.priority = 300;
+        
+    [self addConstraint:self.heightConstraint];
+}
+
+- (void)startObservingNotifications
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(suggestionListUpdated:)
+                                                 name:SuggestionListUpdatedNotification
+                                               object:nil];
+        
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardDidChangeFrame:)
+                                                 name:UIKeyboardDidChangeFrameNotification
+                                               object:nil];
+        
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(deviceOrientationDidChange:)
+                                                 name:UIDeviceOrientationDidChangeNotification
+                                               object:nil];
 }
 
 - (void)dealloc
