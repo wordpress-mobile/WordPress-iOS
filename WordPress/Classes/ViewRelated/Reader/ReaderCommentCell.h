0a208f9527c66cd0d238de25925a06db5d7d9c1e
aerych <eric@automattic.com>
2014-10-31 15:54:59 -0500

    Remove cells delegate protocol in favor of CommentContentViewDelegate
    Do not perform unnecessary content manipulation when preparing for reuse.

--- WordPress/Classes/ViewRelated/Reader/ReaderCommentCell.h
+++ WordPress/Classes/ViewRelated/Reader/ReaderCommentCell.h
@@ -1,18 +1,11 @@
 #import "WPTableViewCell.h"
 
+@protocol CommentContentViewDelegate;
 @class Comment;
-
-@protocol ReaderCommentCellDelegate <NSObject>
-@optional
-- (void)commentCell:(UITableViewCell *)cell replyToComment:(Comment *)comment;
-- (void)commentCell:(UITableViewCell *)cell linkTapped:(NSURL *)url;
-- (void)commentCell:(UITableViewCell *)cell toggleLikeStatusForComment:(Comment *)comment;
-@end
-
 
 @interface ReaderCommentCell : WPTableViewCell
 
-@property (nonatomic, weak) id<ReaderCommentCellDelegate> delegate;
+@property (nonatomic, weak) id<CommentContentViewDelegate> delegate;
 @property (nonatomic) BOOL needsExtraPadding;
 @property (nonatomic) BOOL isFirstNestedComment;
 @property (nonatomic) BOOL hidesBorder;
