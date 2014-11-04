23444f057d1798382fc497c796f41324c4b54c24
Jorge Leandro Perez <jorge@lantean.co>
2014-11-04 14:39:53 -0300

    Workaround to fix DTCoreText mess

--- WordPress/Classes/ViewRelated/Notifications/RichTextView/NSAttributedString+RichTextView.swift
+++ WordPress/Classes/ViewRelated/Notifications/RichTextView/NSAttributedString+RichTextView.swift
@@ -1,12 +1,20 @@
 import Foundation
+import UIKit
 
+
+// NOTE: 
+// This is not cool. The reason why we need this constant, is because DTCoreText overrides NSAttachmentAttributeName.
+// Even using Swift's namespaces ("UIKit.NSAttachmentAttributeName") doesn't return the right value.
+// Please, nuke DTCoreText, and remove this constant.
+//
+public let UIKitAttachmentAttributeName = "NSAttachment"
 
 extension NSAttributedString
 {
     public func enumerateAttachments(block: (attachment: NSTextAttachment, range: NSRange) -> ()) {
         let range = NSMakeRange(0, length)
-        
-        enumerateAttribute(NSAttachmentAttributeName, inRange: range, options: .LongestEffectiveRangeNotRequired) {
+
+        enumerateAttribute(UIKitAttachmentAttributeName, inRange: range, options: .LongestEffectiveRangeNotRequired) {
             (value: AnyObject!, range: NSRange, stop: UnsafeMutablePointer<ObjCBool>) -> Void in
             
             if let attachment = value as? NSTextAttachment {
