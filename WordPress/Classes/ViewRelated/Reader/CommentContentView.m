e28d8b143f5dbe4037e4e7bb4154d67a8af06b4f
Dan Roundhill <dan@automattic.com>
2014-11-11 14:53:25 -0800

    Corrected WPContentAttribution view to use CircularImageView.
    Code style cleanup, hat tip @aerych.

--- WordPress/Classes/ViewRelated/Reader/CommentContentView.m
+++ WordPress/Classes/ViewRelated/Reader/CommentContentView.m
@@ -172,13 +172,8 @@ static const UIEdgeInsets ReplyAndLikeButtonEdgeInsets = {0.0f, 4.0f, 0.0f, -4.0
 
 - (CircularImageView *)imageViewForAvatar
 {
-    CircularImageView *imageView = [[CircularImageView alloc] initWithFrame:
-                                    CGRectMake(0.0f,
-                                               0.0f,
-                                               CommentContentViewAvatarSize,
-                                               CommentContentViewAvatarSize
-                                               )
-                                    ];
+    CGRect avatarFrame = CGRectMake(0.0f, 0.0f, CommentContentViewAvatarSize, CommentContentViewAvatarSize);
+    CircularImageView *imageView = [[CircularImageView alloc] initWithFrame:avatarFrame];
     imageView.translatesAutoresizingMaskIntoConstraints = NO;
     return imageView;
 }
