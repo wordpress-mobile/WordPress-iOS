baa2d6d26d7816f98aaec87db5621b41445cf96f
Diego Rey Mendez <diegoreymendez@gmail.com>
2014-12-02 15:45:45 -0300

    Shows an alert if the user opens in preview mode a post with unsaved changes.

--- WordPress/Classes/Models/AbstractPost.h
+++ WordPress/Classes/Models/AbstractPost.h
@@ -28,4 +28,18 @@
 - (BOOL)hasCategories;
 - (BOOL)hasTags;
 
+/**
+ *  @brief      Call this method to know whether this post has a revision or not.
+ *
+ *  @returns    YES if this post has a revision, NO otherwise.
+ */
+- (BOOL)hasRevision;
+
+/**
+ *  @brief      Call this method to know whether this post has unsaved changes or not.
+ *
+ *  @returns    YES if the post has unsaved changes.  NO otherwise.
+ */
+- (BOOL)hasUnsavedChanges;
+
 @end
