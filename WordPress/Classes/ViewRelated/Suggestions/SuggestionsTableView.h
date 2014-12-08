176c34bec640fe4055df746db5bc730442ace42c
Allen Snook <allen@allendav.com>
2014-12-08 12:09:25 -0800

    Add @mention suggestions to the "long form" iPad commenting flow (notifications and me>sites>comments)

--- WordPress/Classes/ViewRelated/Suggestions/SuggestionsTableView.h
+++ WordPress/Classes/ViewRelated/Suggestions/SuggestionsTableView.h
@@ -5,8 +5,14 @@
 @interface SuggestionsTableView : UIView <UITableViewDataSource, UITableViewDelegate>
 
 @property (nonatomic, weak) id <SuggestionsTableViewDelegate> suggestionsDelegate;
+@property (nonatomic) BOOL useTransparentHeader;
 
 - (instancetype)initWithSiteID:(NSNumber *)siteID;
+
+/*
+ * Whether the SuggestionsTableView header subview should have a clear background
+ */
+- (void)setUseTransparentHeader:(BOOL)useTransparentHeader;
 
 /*
  * Show suggestions for the given word - returns YES if at least one suggestion is being shown
@@ -31,6 +37,12 @@
  * to replace the search term with the suggestion (e.g. at the caret)
 */
 - (void)suggestionsTableView:(SuggestionsTableView *)suggestionsTableView didSelectSuggestion:(NSString *)suggestion forSearchText:(NSString *)text;
+
+/*
+ * When the suggestionsTableView has completed subview layout, the SuggestionsTableView
+ * will call this method to let the UIViewController know
+ */
+- (void)suggestionsTableView:(SuggestionsTableView *)suggestionsTableView didChangeTableBounds:(CGRect)bounds;
 
 @end
 
