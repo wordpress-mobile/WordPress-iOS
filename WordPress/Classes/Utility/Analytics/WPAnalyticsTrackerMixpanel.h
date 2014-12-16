f476020213cec6f3e2859f34ae3c88c3fdeeaa3c
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-12-15 17:27:12 -0800

    Adding in ability to time events for Mixpanel

--- WordPress/Classes/Utility/Analytics/WPAnalyticsTrackerMixpanel.h
+++ WordPress/Classes/Utility/Analytics/WPAnalyticsTrackerMixpanel.h
@@ -1,9 +1,7 @@
 #import <Foundation/Foundation.h>
 #import "WPAnalytics.h"
 
-@interface WPAnalyticsTrackerMixpanel : NSObject <WPAnalyticsTracker> {
-    NSMutableDictionary *_aggregatedStatProperties;
-}
+@interface WPAnalyticsTrackerMixpanel : NSObject <WPAnalyticsTracker>
 
 + (void)resetEmailRetrievalCheck;
 
