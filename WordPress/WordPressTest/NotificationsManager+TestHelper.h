c52dd590b776ede5636d473494f44c4528bf2996
Jorge Leandro Perez <jorge@lantean.co>
2015-01-08 10:34:44 -0300

    Fixes SettingsViewControllerTest

---
+++ WordPress/WordPressTest/NotificationsManager+TestHelper.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+#import "NotificationsManager.h"
+
+
+@interface NotificationsManager (TestHelper)
+
++ (void)setDummyDeviceToken;
++ (void)removeDummyDeviceToken;
+
+@end
