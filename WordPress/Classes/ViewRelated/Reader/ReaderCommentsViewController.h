98c0303f3f522c0c32f7dc8026407852c43b109b
Jorge Leandro Perez <jorge@lantean.co>
2015-01-09 15:59:28 -0300

    ReaderCommentsViewController: Adds lazy helpers

--- WordPress/Classes/ViewRelated/Reader/ReaderCommentsViewController.h
+++ WordPress/Classes/ViewRelated/Reader/ReaderCommentsViewController.h
@@ -6,6 +6,9 @@
 
 @property (nonatomic, strong, readonly) ReaderPost *post;
 
+- (void)setupWithPostID:(NSNumber *)postID siteID:(NSNumber *)siteID;
+
 + (instancetype)controllerWithPost:(ReaderPost *)post;
++ (instancetype)controllerWithPostID:(NSNumber *)postID siteID:(NSNumber *)siteID;
 
 @end
