89eb2b52aa8d5b8880b8c3cf6d654f153cef772e
Sérgio Estêvão <sergio.estevao@automattic.com>
2015-01-12 12:20:22 +0000

    Check for representation existence before we try to export.
    If no representation for the asset we return an error.

--- WordPress/Classes/Utility/WPAssetExporter.m
+++ WordPress/Classes/Utility/WPAssetExporter.m
@@ -1,7 +1,8 @@
-
 #import "WPAssetExporter.h"
-
 #import "WPImageOptimizer.h"
+
+NSString * const WPAssetExportErrorDomain = @"org.wordpress.assetexporter";
+const NSInteger WPAssetExportMissingAsset = 1;
 
 @interface WPAssetExporter ()
 
@@ -40,7 +41,16 @@
    stripGeoLocation:(BOOL)stripGeoLocation
   completionHandler:(void (^)(BOOL success, CGSize resultingSize, NSData *thumbnailData, NSError *error)) handler
 {
-
+    if (!asset.defaultRepresentation) {
+        if (handler) {
+            NSDictionary * userInfo = @{NSLocalizedDescriptionKey:NSLocalizedString(@"The media you are trying to use is not available locally at the moment. If it belongs to a photo stream please try again later.", @"Message that explains to a user that the current asset they selected is not available on the device. This normally happens when user selects a media that belogns to a photostream that needs to be downloaded locally first.")};
+            NSError * error = [NSError errorWithDomain:WPAssetExportErrorDomain
+                                                  code:WPAssetExportMissingAsset
+                                              userInfo:userInfo];
+            handler(NO, CGSizeZero, nil, error);
+        }
+        return;
+    }
     [self.operationQueue addOperationWithBlock:^{
         UIImage *thumbnail = [UIImage imageWithCGImage:asset.thumbnail];
         NSData *thumbnailJPEGData = UIImageJPEGRepresentation(thumbnail, 1.0);
