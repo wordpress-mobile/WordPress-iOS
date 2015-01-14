900830fe17f1462189912715bca7179c14a560ce
Jorge Leandro Perez <jorge@lantean.co>
2015-01-14 18:06:41 -0300

    ReaderPostHeaderView: Adds callback helper

--- WordPress/Classes/ViewRelated/Reader/ReaderPostHeaderView.h
+++ WordPress/Classes/ViewRelated/Reader/ReaderPostHeaderView.h
@@ -5,6 +5,12 @@ extern const CGFloat PostHeaderViewAvatarSize;
 @interface ReaderPostHeaderView : UIView
 
 /**
+ A ReaderPostHeaderCallback block to be executed whenever the user pressed this view.
+ */
+typedef void (^ReaderPostHeaderCallback)(void);
+@property (nonatomic, copy) ReaderPostHeaderCallback onClick;
+
+/**
  Set's the image to display as the avatar.
 
  @param image A UIImage with a width and height equal to `WPContentAttributionViewAvatarSize`
