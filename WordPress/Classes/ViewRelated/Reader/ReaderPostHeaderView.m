c4f27087a2e07db2d768332ae0adab5f10c5e2ad
Jorge Leandro Perez <jorge@lantean.co>
2015-01-15 14:14:56 -0300

    ReaderPostHeader: Adds disclosure image

--- WordPress/Classes/ViewRelated/Reader/ReaderPostHeaderView.m
+++ WordPress/Classes/ViewRelated/Reader/ReaderPostHeaderView.m
@@ -3,6 +3,8 @@
 
 const CGFloat PostHeaderViewAvatarSize = 32.0;
 const CGFloat PostHeaderViewLabelHeight = 18.0;
+const CGFloat PostHeaderDisclosureButtonWidth = 8.0f;
+const CGFloat PostHeaderDisclosureButtonHeight = 13.0f;
 
 @interface ReaderPostHeaderView()
 
@@ -10,6 +12,7 @@ const CGFloat PostHeaderViewLabelHeight = 18.0;
 @property (nonatomic, strong) CircularImageView *avatarImageView;
 @property (nonatomic, strong) UILabel *titleLabel;
 @property (nonatomic, strong) UILabel *subtitleLabel;
+@property (nonatomic, strong) UIButton *disclosureButton;
 
 @end
 
@@ -31,10 +34,14 @@ const CGFloat PostHeaderViewLabelHeight = 18.0;
         _tapsRegoznier = [self newTapGestureRecognizer];
         [self addGestureRecognizer:_tapsRegoznier];
         
+        _disclosureButton = [self newDisclosureButton];
+        [self addSubview:_disclosureButton];
+        
         [self configureConstraints];
     }
     return self;
 }
+
 
 #pragma mark - Public Methods
 
@@ -43,9 +50,25 @@ const CGFloat PostHeaderViewLabelHeight = 18.0;
     return CGSizeMake(PostHeaderViewAvatarSize, PostHeaderViewAvatarSize);
 }
 
+- (void)setShowsDisclosureIndicator:(BOOL)showsDisclosure
+{
+    _showsDisclosureIndicator = showsDisclosure;
+    [self refreshDisclosureButton];
+}
+
+- (UIImage *)avatarImage
+{
+    return self.avatarImageView.image;
+}
+
 - (void)setAvatarImage:(UIImage *)image
 {
     self.avatarImageView.image = image;
+}
+
+- (NSString *)title
+{
+    return self.titleLabel.text;
 }
 
 - (void)setTitle:(NSString *)title
@@ -53,18 +76,37 @@ const CGFloat PostHeaderViewLabelHeight = 18.0;
     self.titleLabel.text = title;
 }
 
+- (NSString *)subtitle
+{
+    return self.subtitleLabel.text;
+}
+
 - (void)setSubtitle:(NSString *)title
 {
     self.subtitleLabel.text = title;
 }
 
+
 #pragma mark - Private Methods
+
+- (void)refreshDisclosureButton
+{
+    // Note: iOS 7 doesn't allow us to simply disable a constraint. Let's improve this once the deploymentTarget is updated!
+    CGFloat targetWidth = self.showsDisclosureIndicator ? PostHeaderDisclosureButtonWidth : 0.0f;
+    [self.disclosureButton updateConstraint:NSLayoutAttributeWidth constant:targetWidth];
+    self.disclosureButton.hidden = !self.showsDisclosureIndicator;
+}
 
 - (void)configureConstraints
 {
-    NSDictionary *views = NSDictionaryOfVariableBindings(_avatarImageView, _titleLabel, _subtitleLabel);
-    NSDictionary *metrics = @{@"avatarSize": @(PostHeaderViewAvatarSize),
-                              @"labelHeight":@(PostHeaderViewLabelHeight)};
+    NSDictionary *views   = NSDictionaryOfVariableBindings(_avatarImageView, _titleLabel, _subtitleLabel, _disclosureButton);
+    NSDictionary *metrics = @{
+        @"avatarSize"       : @(PostHeaderViewAvatarSize),
+        @"labelHeight"      : @(PostHeaderViewLabelHeight),
+        @"disclosureWidth"  : @(PostHeaderDisclosureButtonWidth),
+        @"disclosureHeight" : @(PostHeaderDisclosureButtonHeight)
+    };
+    
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|[_avatarImageView(avatarSize)]"
                                                                  options:0
                                                                  metrics:metrics
@@ -73,11 +115,19 @@ const CGFloat PostHeaderViewLabelHeight = 18.0;
                                                                  options:0
                                                                  metrics:metrics
                                                                    views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"[_avatarImageView]-[_titleLabel]|"
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"[_disclosureButton(disclosureWidth)]|"
                                                                  options:0
                                                                  metrics:metrics
                                                                    views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"[_avatarImageView]-[_subtitleLabel]|"
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[_disclosureButton(disclosureHeight)]"
+                                                                 options:0
+                                                                 metrics:metrics
+                                                                   views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"[_avatarImageView]-[_titleLabel]-[_disclosureButton]"
+                                                                 options:0
+                                                                 metrics:metrics
+                                                                   views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"[_avatarImageView]-[_subtitleLabel]-[_disclosureButton]"
                                                                  options:0
                                                                  metrics:metrics
                                                                    views:views]];
@@ -85,6 +135,13 @@ const CGFloat PostHeaderViewLabelHeight = 18.0;
                                                                  options:NSLayoutFormatAlignAllLeft
                                                                  metrics:metrics
                                                                    views:views]];
+    [self addConstraint:[NSLayoutConstraint constraintWithItem:_disclosureButton
+                                                     attribute:NSLayoutAttributeCenterY
+                                                     relatedBy:NSLayoutRelationEqual
+                                                        toItem:_avatarImageView
+                                                     attribute:NSLayoutAttributeCenterY
+                                                    multiplier:1.0f
+                                                      constant:0.0f]];
     [super setNeedsUpdateConstraints];
 }
 
@@ -133,6 +190,15 @@ const CGFloat PostHeaderViewLabelHeight = 18.0;
     tapGesture.numberOfTouchesRequired = 1;
     tapGesture.numberOfTapsRequired = 1;
     return tapGesture;
+}
+
+- (UIButton *)newDisclosureButton
+{
+    UIImage *chevronImage = [UIImage imageNamed:@"disclosure-chevron"];
+    UIButton *disclosureButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [disclosureButton setBackgroundImage:chevronImage forState:UIControlStateNormal];
+    disclosureButton.translatesAutoresizingMaskIntoConstraints = NO;
+    return disclosureButton;
 }
 
 
