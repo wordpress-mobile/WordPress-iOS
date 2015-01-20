db6443b89be8cb917332bfcfe6a54dd36e8dfdd8
Sérgio Estêvão <sergio.estevao@automattic.com>
2015-01-20 18:13:39 -0500

    Disable some redundant tests and extra logging on oclint.

--- Scripts/run-oclint.sh
+++ Scripts/run-oclint.sh
@@ -2,14 +2,14 @@
 source ~/.bash_profile
 
 check_file="$1"
-oclint_args="-disable-rule=ShortVariableName -disable-rule=LongLine -rc LONG_LINE=300 -rc SHORT_VARIABLE_NAME=1 -rc LONG_METHOD=75 -rc LONG_VARIABLE_NAME=40"
+oclint_args="-disable-rule=ShortVariableName -disable-rule=LongLine -disable-rule=UnusedMethodParameter -rc LONG_METHOD=75 -rc LONG_VARIABLE_NAME=40"
 temp_dir="/tmp"
 build_dir="${temp_dir}/WPiOS_linting"
 compile_commands_path=${temp_dir}/compile_commands.json
 xcodebuild_log_path=${temp_dir}/xcodebuild.log
 
 if [ "${TRAVIS}" = "true" ]; then
-  echo "[*] installing travis"
+  echo "[*] installing oclint 0.8.1"
   pushd .
   cd ${temp_dir}
   curl http://archives.oclint.org/releases/0.8/oclint-0.8.1-x86_64-darwin-14.0.0.tar.gz > oclint.tar.gz
@@ -24,10 +24,6 @@ if [ $? -eq 1 ]; then
     echo >&2 "oclint not found, analyzing stopped"
     exit 1
 fi
-
-oclint --version
-xctool --version
-xcodebuild -version
 
 echo "[*] cleaning up generated files"
 [[ -f $compile_commands_path ]] && rm ${compile_commands_path}
@@ -78,5 +74,5 @@ else
     exclude_files="-e Pods/ -e Vendor/ -e WordPressTodayWidget/ -e SFHFKeychainUtils.m -e Constants.m"
 fi
 
-eval "oclint-json-compilation-database -v $exclude_files oclint_args \"$oclint_args\" $include_files $pipe_command"
+eval "oclint-json-compilation-database $exclude_files oclint_args \"$oclint_args\" $include_files $pipe_command"
 exit 0
