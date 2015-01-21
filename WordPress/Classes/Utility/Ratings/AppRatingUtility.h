ab629f34f74a6954cd6ed0bc0cf9a63c7288caf3
Sendhil Panchadsaram <sendhilp@gmail.com>
2015-01-20 22:33:48 -0500

    Adjusting some spacing in the documentation.

--- WordPress/Classes/Utility/Ratings/AppRatingUtility.h
+++ WordPress/Classes/Utility/Ratings/AppRatingUtility.h
@@ -1,19 +1,23 @@
 #import <Foundation/Foundation.h>
 
 /**
- This class will help track whether or not a user should be prompted for an app review. This class is loosely based on Appirater (https://github.com/arashpayan/appirater)
+ This class will help track whether or not a user should be prompted for an app 
+ review. This class is loosely based on Appirater 
+ (https://github.com/arashpayan/appirater)
  */
 @interface AppRatingUtility : NSObject
 
 /**
- *  This should be called with the current App Version so as to setup internal tracking.
+ *  This should be called with the current App Version so as to setup internal 
+    tracking.
  *
  *  @param version version number of the app, e.g. CFBundleShortVersionString
  */
 + (void)initializeForVersion:(NSString *)version;
 
 /**
- *  This checks if we've disabled app review prompts for a feature or at a global level
+ *  This checks if we've disabled app review prompts for a feature or at a 
+    global level
  *
  *  @param success called on successfully retrieving the details
  *  @param failure called when we were unable to retrieve the details
@@ -41,14 +45,18 @@
 + (void)incrementSignificantEventForSection:(NSString *)section;
 
 /**
- *  Sets the number of system wide significant events are required when calling `shouldPromptForAppReview`. Ideally this number should be a number less than the total of all the signficant event counts for each section so as to trigger the review prompt for a fairly active user who uses the app in a broad fashion.
+ *  Sets the number of system wide significant events are required when calling `shouldPromptForAppReview`. Ideally this number should be a number less 
+    than the total of all the signficant event counts for each section so as to
+    trigger the review prompt for a fairly active user who uses the app in a 
+    broad fashion.
  *
  *  @param numberOfEvents number of events required to trigger the generic check for an app review.
  */
 + (void)setSystemWideSignificantEventsCount:(NSUInteger)numberOfEvents;
 
 /**
- *  Indicates that the user didn't want to review the app or leave feedback for this version.
+ *  Indicates that the user didn't want to review the app or leave feedback for 
+    this version.
  */
 + (void)declinedToRateCurrentVersion;
 
@@ -73,14 +81,21 @@
 + (void)likedCurrentVersion;
 
 /**
- *  Checks if the user should be prompted for an app review based on `systemWideSignificantEventsCount` and also if the user hasn't been configured to skip being prompted for this release.  Note that this method will check to see if app review prompts on a global basis have been shut off.
+ *  Checks if the user should be prompted for an app review based on `systemWideSignificantEventsCount` and also if the user hasn't been
+    configured to skip being prompted for this release.  Note that this method 
+    will check to see if app review prompts on a global basis have been shut 
+    off.
  *
  *  @return returns true when the user has performed enough significant events and isn't configured to skip being prompted for this release.
  */
 + (BOOL)shouldPromptForAppReview;
 
 /**
- *  Checks if the user should be prompted for an app review based on the number of significant events configured for this particular section and if the user hasn't been configured to skip being prompted for this release. Note that this method will check to see if prompts for this section have been shut off entirely.
+ *  Checks if the user should be prompted for an app review based on the number 
+    of significant events configured for this particular section and if the user 
+    hasn't been configured to skip being prompted for this release. Note that 
+    this method will check to see if prompts for this section have been shut 
+    off entirely.
  *
  *  @param section section name, e.g. "Notifications"
  *
