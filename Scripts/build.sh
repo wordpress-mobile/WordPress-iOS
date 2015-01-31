0e03b4454dcac78ec9144aa3bfa664db27dd21de
Sérgio Estêvão <sergio.estevao@automattic.com>
2015-01-31 00:09:20 +0000

    Build even if xcpretty is not available.

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,20 +1,28 @@
-#checking if xcpretty is available to use
-command -v xcpretty >/dev/null
-if [ $? -eq 1 ]; then
-echo >&2 "xcpretty not found don't use it."
-exit 0
-fi
-
 if [ ! $TRAVIS ]; then
 	TRAVIS_XCODE_WORKSPACE=WordPress.xcworkspace
 	TRAVIS_XCODE_SCHEME=WordPress
     TRAVIS_XCODE_SDK=iphonesimulator8.1
 fi
 
+#checking if xcpretty is available to use
+command -v xcpretty >/dev/null
+if [ $? -eq 1 ]; then
+echo >&2 "xcpretty not found don't use it."
+xcodebuild build test \
+	-destination "platform=iOS Simulator,name=iPhone 4s,OS=8.1" \
+	-workspace "$TRAVIS_XCODE_WORKSPACE" \
+	-scheme "$TRAVIS_XCODE_SCHEME" \
+	-configuration Debug
+else
 xcodebuild build test \
 	-destination "platform=iOS Simulator,name=iPhone 4s,OS=8.1" \
 	-workspace "$TRAVIS_XCODE_WORKSPACE" \
 	-scheme "$TRAVIS_XCODE_SCHEME" \
 	-configuration Debug \
 	-sdk "$TRAVIS_XCODE_SDK" | xcpretty -c && exit ${PIPESTATUS[0]}
+fi
+
+
+
+
 	
