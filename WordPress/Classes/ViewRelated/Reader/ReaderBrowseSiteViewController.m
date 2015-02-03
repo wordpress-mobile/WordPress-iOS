93a1f4e8a472fc4d1322f350f7f5b2bb95cea673
aerych <eric@automattic.com>
2015-02-03 17:26:00 -0600

    Configure ReaderBrowseSiteViewController

--- WordPress/Classes/ViewRelated/Reader/ReaderBrowseSiteViewController.m
+++ WordPress/Classes/ViewRelated/Reader/ReaderBrowseSiteViewController.m
@@ -19,23 +19,54 @@ static const NSInteger ReaderSiteHeaderHeight = 44.0;
 
 @interface ReaderBrowseSiteViewController ()
 @property (nonatomic, strong) ReaderPostsViewController *postsViewController;
-@property (nonatomic, strong) UIView *headerView;
+@property (nonatomic, strong) ReaderSiteHeaderView *headerView;
+@property (nonatomic, strong) ReaderPost *post;
+@property (nonatomic, strong) ReaderTopic *siteTopic;
 @end
 
 @implementation ReaderBrowseSiteViewController
 
+#pragma mark - Lifecycle Methods
+
+- (instancetype)initWithPost:(ReaderPost *)post
+{
+    self = [super init];
+    if (self) {
+        _post = post;
+    }
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    self.title = NSLocalizedString(@"Preview Site", @"");
+
+    if (!self.post) {
+        return;
+    }
+
+    [self createSiteTopic];
     [self configureHeaderView];
     [self configurePostsViewController];
     [self configureViewConstraints];
 }
 
+- (void)createSiteTopic
+{
+    ReaderTopicService *topicService = [[ReaderTopicService alloc] initWithManagedObjectContext:[[ContextManager sharedInstance] mainContext]];
+    self.siteTopic = [topicService siteTopicForPost:self.post];
+}
+
+#pragma mark - Configuration
+
 - (void)configureHeaderView
 {
     self.headerView = [[ReaderSiteHeaderView alloc] initWithFrame:CGRectMake(0.0, 0.0, CGRectGetWidth(self.view.bounds), ReaderSiteHeaderHeight)];
     self.headerView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.headerView.title = self.siteTopic.title;
+    self.headerView.subtitle = self.siteTopic.topicDescription;
+    // TODO: Load blavatar
     [self.view addSubview:self.headerView];
 }
 
@@ -48,6 +79,8 @@ static const NSInteger ReaderSiteHeaderHeight = 44.0;
     childView.frame = self.view.bounds;
     [self.view addSubview:childView];
     [self.postsViewController didMoveToParentViewController:self];
+
+    self.postsViewController.readerTopic = self.siteTopic;
 }
 
 - (void)configureViewConstraints
@@ -71,6 +104,5 @@ static const NSInteger ReaderSiteHeaderHeight = 44.0;
 
     [self.view setNeedsUpdateConstraints];
 }
-
 
 @end
