fb653e7582ffeed52b9dfc4ddfaaf44f99d47cf2
aerych <eric@automattic.com>
2015-02-04 15:28:20 -0600

    Adds flag to skip the normal iPad top padding.

--- WordPress/Classes/ViewRelated/Reader/ReaderPostsViewController.h
+++ WordPress/Classes/ViewRelated/Reader/ReaderPostsViewController.h
@@ -3,4 +3,5 @@
 @class ReaderTopic;
 @interface ReaderPostsViewController : UITableViewController
 @property (nonatomic, strong) ReaderTopic *readerTopic;
+@property (nonatomic, assign) BOOL skipIpadTopPadding;
 @end
