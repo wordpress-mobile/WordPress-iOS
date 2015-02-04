9d09fb574e9b17981fdecf6652f81c15998f83ce
aerych <eric@automattic.com>
2015-02-04 15:29:28 -0600

    Removes blue background styling.

--- WordPress/Classes/ViewRelated/Reader/ReaderSiteHeaderView.m
+++ WordPress/Classes/ViewRelated/Reader/ReaderSiteHeaderView.m
@@ -2,19 +2,10 @@
 
 @implementation ReaderSiteHeaderView
 
-- (instancetype)initWithFrame:(CGRect)frame
-{
-    self = [super initWithFrame:frame];
-    if (self) {
-        self.backgroundColor = [WPStyleGuide baseDarkerBlue];
-    }
-    return self;
-}
-
 - (UILabel *)newLabelForTitle
 {
     UILabel *label = [super newLabelForTitle];
-    label.backgroundColor = [WPStyleGuide baseDarkerBlue];
+    label.backgroundColor = [UIColor clearColor];
     label.textColor = [WPStyleGuide littleEddieGrey];
     return label;
 }
@@ -22,7 +13,7 @@
 - (UILabel *)newLabelForSubtitle
 {
     UILabel *label = [super newLabelForSubtitle];
-    label.backgroundColor = [WPStyleGuide baseDarkerBlue];
+    label.backgroundColor = [UIColor clearColor];
     label.textColor = [WPStyleGuide allTAllShadeGrey];
     return label;
 }
