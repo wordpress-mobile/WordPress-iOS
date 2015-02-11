7c845bb313300990ab54c5f11a798a799edb6a0c
aerych <eric@automattic.com>
2015-02-11 12:39:09 -0600

    Do not use URL for site description fallback.

--- WordPress/Classes/Services/ReaderTopicService.m
+++ WordPress/Classes/Services/ReaderTopicService.m
@@ -367,7 +367,7 @@ static NSString * const ReaderTopicCurrentTopicPathKey = @"ReaderTopicCurrentTop
     topic.topicID = post.siteID;
     topic.type = ReaderTopicTypeSite;
     topic.title = post.blogName;
-    topic.topicDescription = ([post.blogDescription length] > 0) ? post.blogDescription : post.blogURL;
+    topic.topicDescription = post.blogDescription;
     topic.path = path;
 
     return topic;
