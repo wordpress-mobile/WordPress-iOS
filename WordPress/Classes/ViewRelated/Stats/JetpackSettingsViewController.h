7115bb8eceba850c49b7ebc2161318122e315471
Jorge Leandro Perez <jorge@lantean.co>
2015-02-12 17:54:41 -0300

    JetpackSettingsViewController: Header Cleanup

--- WordPress/Classes/ViewRelated/Stats/JetpackSettingsViewController.h
+++ WordPress/Classes/ViewRelated/Stats/JetpackSettingsViewController.h
@@ -5,11 +5,12 @@
 typedef void(^JetpackSettingsCompletionBlock)(BOOL didAuthenticate);
 
 @interface JetpackSettingsViewController : UIViewController
-@property (nonatomic, assign) BOOL canBeSkipped;
-// Navigation bar is hidden and all buttons are added into the
-// view on initial sign in
-@property (nonatomic, assign) BOOL showFullScreen;
-@property (nonatomic, copy) JetpackSettingsCompletionBlock completionBlock;
-- (void)setCompletionBlock:(JetpackSettingsCompletionBlock)completionBlock; // Just so Xcode autocompletes the block
-- (id)initWithBlog:(Blog *)blog;
+
+// Navigation bar is hidden and all buttons are added into the view on initial sign in
+@property (nonatomic, assign) BOOL                              showFullScreen;
+@property (nonatomic, assign) BOOL                              canBeSkipped;
+@property (nonatomic,   copy) JetpackSettingsCompletionBlock    completionBlock;
+
+- (instancetype)initWithBlog:(Blog *)blog;
+
 @end
