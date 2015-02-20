031ef5111f9cb90387776a97f6fe601388604ef1
Jorge Leandro Perez <jorge@lantean.co>
2015-02-20 13:16:15 -0300

    Adds WordPress UserAgent Helper

--- WordPress/Classes/Categories/UIDevice+Helpers.m
+++ WordPress/Classes/Categories/UIDevice+Helpers.m
@@ -4,6 +4,18 @@ static NSString * const WordPressIdentifierDefaultsKey = @"WordPressIdentifier";
 
 @implementation UIDevice (Helpers)
 
+- (NSString *)wordPressUserAgent
+{
+    NSString *appVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
+    NSString *userAgent = [NSString stringWithFormat:@"wp-iphone/%@ (%@ %@, %@) Mobile",
+                           appVersion,
+                           self.systemName,
+                           self.systemVersion,
+                           self.model];
+    
+    return userAgent;
+}
+
 - (NSString *)wordPressIdentifier
 {
     NSString *uuid;
