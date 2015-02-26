2a2e59e61ee7b05411113346b2fca182d775bd31
Jorge Leandro Perez <jorge@lantean.co>
2015-02-26 10:00:17 -0300

    NotificationsVIewController: Disables row animations when not visible

--- WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationsViewController.m
+++ WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationsViewController.m
@@ -155,6 +155,9 @@ static NSTimeInterval NotificationsSyncTimeout          = 10;
     // Manually deselect the selected row. This is required due to a bug in iOS7 / iOS8
     [self.tableView deselectSelectedRowWithAnimation:YES];
     
+    // While we're onscreen, please, update rows with animations
+    self.tableViewHandler.updateRowAnimation = UITableViewRowAnimationFade;
+    
     // Refresh the UI
     [self hookApplicationStateNotes];
     [self trackAppearedIfNeeded];
@@ -175,6 +178,9 @@ static NSTimeInterval NotificationsSyncTimeout          = 10;
 {
     [super viewWillDisappear:animated];
     [self unhookApplicationStateNotes];
+    
+    // Bufix: If we're not onscreen, don't use row animations. Otherwise the fade animation might get animated incrementally
+    self.tableViewHandler.updateRowAnimation = UITableViewRowAnimationNone;
 }
 
 - (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
