598fc872062b5cb52f46a5e98edef097c1078a7b
Sendhil Panchadsaram <sendhilp@gmail.com>
2015-02-25 18:10:22 -0800

    Track helpshift being opened from the login screen

--- WordPress/Classes/ViewRelated/NUX/LoginViewController.m
+++ WordPress/Classes/ViewRelated/NUX/LoginViewController.m
@@ -786,6 +786,7 @@ static CGFloat const OnePasswordPaddingX                        = 9.0;
                                @"SiteURL": _siteUrlText.text};
 
     [[Helpshift sharedInstance] showConversation:self withOptions:@{HSCustomMetadataKey: metaData}];
+    [WPAnalytics track:WPAnalyticsStatSupportOpenedHelpshiftScreen];
 }
 
 - (BOOL)isUrlWPCom:(NSString *)url
