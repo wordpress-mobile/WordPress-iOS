6f799f1f9fe185a6d0516eb3b56311448668ff03
Jorge Leandro Perez <jorge@lantean.co>
2015-03-02 16:31:13 -0300

    Notification: Exposing Range Constants

--- WordPress/Classes/Models/Notifications/Notification.h
+++ WordPress/Classes/Models/Notifications/Notification.h
@@ -19,6 +19,14 @@ extern NSString * NoteActionReplyKey;
 extern NSString * NoteActionApproveKey;
 extern NSString * NoteActionEditKey;
 
+extern NSString * NoteRangeTypeUser;
+extern NSString * NoteRangeTypePost;
+extern NSString * NoteRangeTypeComment;
+extern NSString * NoteRangeTypeStats;
+extern NSString * NoteRangeTypeBlockquote;
+extern NSString * NoteRangeTypeNoticon;
+extern NSString * NoteRangeTypeSite;
+
 extern NSString * NoteMediaTypeImage;
 
 typedef NS_ENUM(NSInteger, NoteBlockType)
