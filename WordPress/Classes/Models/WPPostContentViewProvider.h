0e11e95350cdcdc7d3ac282f9e8e6f1d13af3a49
aerych <eric@automattic.com>
2015-03-04 15:59:40 -0600

    Adds WPPostContentViewProvider protocol

---
+++ WordPress/Classes/Models/WPPostContentViewProvider.h
@@ -0,0 +1,13 @@
+//
+//  WPPostContentViewProvider.h
+//  WordPress
+//
+//  Created by aerych on 3/4/15.
+//  Copyright (c) 2015 WordPress. All rights reserved.
+//
+
+#ifndef WordPress_WPPostContentViewProvider_h
+#define WordPress_WPPostContentViewProvider_h
+
+
+#endif
