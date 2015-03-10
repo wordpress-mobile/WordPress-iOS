97d1f2211864202301327fb2ca32799ebb44e8ab
Jorge Leandro Perez <jorge@lantean.co>
2015-03-10 12:29:56 -0300

    Adds Notification Helper

--- WordPress/Classes/Models/Notifications/Notification.m
+++ WordPress/Classes/Models/Notifications/Notification.m
@@ -32,6 +32,7 @@ NSString const *NoteTypeUser            = @"user";
 NSString const *NoteTypeComment         = @"comment";
 NSString const *NoteTypeMatcher         = @"automattcher";
 NSString const *NoteTypePost            = @"post";
+NSString const *NoteTypeFollow          = @"follow";
 
 NSString const *NoteMetaKey             = @"meta";
 NSString const *NoteMediaKey            = @"media";
@@ -603,6 +604,11 @@ NSString const *NotePostIdKey           = @"post_id";
 - (BOOL)isPost
 {
     return [self.type isEqual:NoteTypePost];
+}
+
+- (BOOL)isFollow
+{
+    return [self.type isEqual:NoteTypeFollow];
 }
 
 - (BOOL)isBadge
