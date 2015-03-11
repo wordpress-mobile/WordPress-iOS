fd93a95f9897ee2ee79c26b350ee8d65e6e81b8a
Will Kwon <will@willkwon.com>
2015-03-10 18:57:32 -0700

    BlogService now exposes the number of visible WPCom accounts

--- WordPress/Classes/Services/BlogService.m
+++ WordPress/Classes/Services/BlogService.m
@@ -332,6 +332,11 @@ NSString *const LastUsedBlogURLDefaultsKey = @"LastUsedBlogURLDefaultsKey";
     [blog.api enqueueXMLRPCRequestOperation:operation];
 }
 
+- (BOOL)hasVisibleWPComAccounts
+{
+    return [self blogCountVisibleForWPComAccounts] > 0;
+}
+
 - (NSInteger)blogCountForAllAccounts
 {
     return [self blogCountWithPredicate:nil];
@@ -340,6 +345,12 @@ NSString *const LastUsedBlogURLDefaultsKey = @"LastUsedBlogURLDefaultsKey";
 - (NSInteger)blogCountSelfHosted
 {
     NSPredicate *predicate = [NSPredicate predicateWithFormat:@"account.isWpcom = %@" argumentArray:@[@(NO)]];
+    return [self blogCountWithPredicate:predicate];
+}
+
+- (NSInteger)blogCountVisibleForWPComAccounts
+{
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"account.isWpcom = %@ AND visible = %@" argumentArray:@[@(YES), @(YES)]];
     return [self blogCountWithPredicate:predicate];
 }
 
