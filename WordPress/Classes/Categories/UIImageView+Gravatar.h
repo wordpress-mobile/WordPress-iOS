4871dacee997475ea93ce584b9d9416f3fb4e022
aerych <eric@automattic.com>
2015-03-11 17:33:24 -0500

    Makes blavatar method public

--- WordPress/Classes/Categories/UIImageView+Gravatar.h
+++ WordPress/Classes/Categories/UIImageView+Gravatar.h
@@ -1,6 +1,7 @@
 @interface UIImageView (Gravatar)
 
 - (NSURL *)blavatarURLForHost:(NSString *)host;
+- (NSURL *)blavatarURLForHost:(NSString *)host withSize:(NSInteger)size;
 - (void)setImageWithGravatarEmail:(NSString *)emailAddress;
 - (void)setImageWithGravatarEmail:(NSString *)emailAddress fallbackImage:(UIImage *)fallbackImage;
 - (void)setImageWithBlavatarUrl:(NSString *)blavatarUrl;
