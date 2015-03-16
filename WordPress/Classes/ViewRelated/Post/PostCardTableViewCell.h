a9a2eeefa2b70431ebc65206f7375daa24685daa
aerych <eric@automattic.com>
2015-03-16 11:10:05 -0500

    Restructures cell view hierarchy.
    Tweaks spacing a bit.
    Groups author info into header view.
    Adds label for author blog.
    Adds inner content view anticipating fixed width for iPad

--- WordPress/Classes/ViewRelated/Post/PostCardTableViewCell.h
+++ WordPress/Classes/ViewRelated/Post/PostCardTableViewCell.h
@@ -3,10 +3,13 @@
 
 @interface PostCardTableViewCell : UITableViewCell
 
+@property (nonatomic, strong) IBOutlet UIView *innerContentView;
 @property (nonatomic, strong) IBOutlet UIView *shadowView;
 @property (nonatomic, strong) IBOutlet UIView *postContentView;
+@property (nonatomic, strong) IBOutlet UIView *headerView;
 @property (nonatomic, strong) IBOutlet UIImageView *avatarImageView;
-@property (nonatomic, strong) IBOutlet UILabel *authorLabel;
+@property (nonatomic, strong) IBOutlet UILabel *authorBlogLabel;
+@property (nonatomic, strong) IBOutlet UILabel *authorNameLabel;
 @property (nonatomic, strong) IBOutlet UIImageView *featuredImageView;
 @property (nonatomic, strong) IBOutlet UILabel *titleLabel;
 @property (nonatomic, strong) IBOutlet UILabel *snippetLabel;
@@ -21,13 +24,14 @@
 @property (nonatomic, strong) IBOutlet UIButton *metaButtonCenter;
 @property (nonatomic, strong) IBOutlet UIButton *metaButtonLeft;
 @property (nonatomic, strong) IBOutlet UIView *actionBar;
-@property (nonatomic, strong) IBOutlet NSLayoutConstraint *avatarImageViewLowerConstraint;
+@property (nonatomic, strong) IBOutlet NSLayoutConstraint *headerViewHeightConstraint;
+@property (nonatomic, strong) IBOutlet NSLayoutConstraint *headerViewLowerConstraint;
 @property (nonatomic, strong) IBOutlet NSLayoutConstraint *titleLowerConstraint;
 @property (nonatomic, strong) IBOutlet NSLayoutConstraint *snippetLowerConstraint;
 @property (nonatomic, strong) IBOutlet NSLayoutConstraint *dateViewLowerConstraint;
 @property (nonatomic, strong) IBOutlet NSLayoutConstraint *statusHeightConstraint;
 @property (nonatomic, strong) IBOutlet NSLayoutConstraint *statusViewLowerConstraint;
-@property (nonatomic, strong) IBOutlet NSLayoutConstraint *wrapperViewLowerConstraint;
+@property (nonatomic, strong) IBOutlet NSLayoutConstraint *postContentBottomConstraint;
 
 - (void)configureCell:(id<WPPostContentViewProvider>)contentProvider;
 
