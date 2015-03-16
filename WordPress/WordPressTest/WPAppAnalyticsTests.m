a18424743598e31ecb8cf9b7c5447d0c1f05b5d0
Diego Rey Mendez <diegoreymendez@gmail.com>
2015-03-16 13:46:15 -0300

    Working on unit tests for WPAppANalytics.

---
+++ WordPress/WordPressTest/WPAppAnalyticsTests.m
@@ -0,0 +1,22 @@
+//
+//  WPAppAnalyticsTests.m
+//  WordPress
+//
+//  Created by Diego E. Rey Mendez on 3/16/15.
+//  Copyright (c) 2015 WordPress. All rights reserved.
+//
+
+#import <OCMock/OCMock.h>
+#import "WPAppAnalytics.h"
+
+@interface WPAppAnalyticsTests : XCTestCase
+@end
+
+@implementation WPAppAnalyticsTests
+
+- (void)testInitialization
+{
+    //WPAppAnalytics* analytics = [[WPAppAnalytics alloc] init];
+}
+
+@end
