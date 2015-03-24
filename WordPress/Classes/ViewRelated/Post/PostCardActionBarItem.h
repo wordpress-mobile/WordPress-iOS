dcfc0480de2783c4dce4dbdb6f58b2a059ca01fd
aerych <eric@automattic.com>
2015-03-24 17:10:08 -0500

    Convenience constructor.

--- WordPress/Classes/ViewRelated/Post/PostCardActionBarItem.h
+++ WordPress/Classes/ViewRelated/Post/PostCardActionBarItem.h
@@ -1,7 +1,11 @@
 #import <Foundation/Foundation.h>
 
 @interface PostCardActionBarItem : NSObject
+
 @property (nonatomic, strong) NSString *title;
 @property (nonatomic, strong) UIImage *image;
 @property (nonatomic, strong) UIImage *highlightedImage;
+
++ (instancetype)itemWithTitle:(NSString *)title image:(UIImage *)image highlightedImage:(UIImage *)image;
+
 @end
