a798f02a7a86aefd065f52981bb8bcfcdd6d10b4
Diego Rey Mendez <diegoreymendez@gmail.com>
2015-03-26 15:20:07 -0300

    Documents some methods.

--- WordPress/Classes/Utility/Logging/WPLogger.h
+++ WordPress/Classes/Utility/Logging/WPLogger.h
@@ -8,10 +8,21 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ *  @class      WPlogger
+ *  @brief      This module takes care of the logging setup for WPiOS.
+ */
 @interface WPLogger : NSObject
 
 #pragma mark - Reading from the log
 
+/**
+ *  @brief      Retrieves log data from the log files.
+ * 
+ *  @param      maxSize     The maximum number of bytes to retrieve from the log files.
+ *
+ *  @returns    The requested log data.
+ */
 - (NSString *)getLogFilesContentWithMaxSize:(NSInteger)maxSize;
 
 @end
