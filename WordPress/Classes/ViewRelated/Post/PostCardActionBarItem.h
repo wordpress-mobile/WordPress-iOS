daca4b9aff403a13b1f98bea8cd5ace56f10b6cf
aerych <eric@automattic.com>
2015-03-26 17:14:29 -0500

    Adds callback block for items.

--- WordPress/Classes/ViewRelated/Post/PostCardActionBarItem.h
+++ WordPress/Classes/ViewRelated/Post/PostCardActionBarItem.h
@@ -1,10 +1,13 @@
 #import <Foundation/Foundation.h>
 
+typedef void(^PostCardActionBarItemCallback)();
+
 @interface PostCardActionBarItem : NSObject
 
 @property (nonatomic, strong) NSString *title;
 @property (nonatomic, strong) UIImage *image;
 @property (nonatomic, strong) UIImage *highlightedImage;
+@property (nonatomic, copy) PostCardActionBarItemCallback callback;
 
 + (instancetype)itemWithTitle:(NSString *)title image:(UIImage *)image highlightedImage:(UIImage *)image;
 
