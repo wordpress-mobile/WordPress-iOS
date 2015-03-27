fa991940c5c30fda39405902b3e5bbabc52300ba
Diego Rey Mendez <diegoreymendez@gmail.com>
2015-03-27 12:01:23 -0300

    Removed unnecessary headers.

--- WordPress/Classes/Utility/WPCrashlytics.h
+++ WordPress/Classes/Utility/WPCrashlytics.h
@@ -1,11 +1,3 @@
-//
-//  WPCrashlytics.h
-//  WordPress
-//
-//  Created by Diego E. Rey Mendez on 3/26/15.
-//  Copyright (c) 2015 WordPress. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 /**
