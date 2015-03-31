fbc07be24ef28473c4d327abc8046091bc84ccb6
Jorge Leandro Perez <jorge@lantean.co>
2015-03-31 12:03:31 -0300

    Notifications: Implements Likes Unit Test

--- WordPress/WordPressTest/NotificationTests.m
+++ WordPress/WordPressTest/NotificationTests.m
@@ -59,6 +59,43 @@
     XCTAssertNotNil(media.mediaURL, @"Missing mediaURL");
 }
 
+- (void)testLikeNotificationContainsOneSubjectBlock
+{
+    Notification *note = [self loadLikeNotification];
+    XCTAssertNotNil(note.subjectBlock, @"Missing subjectBlock");
+    XCTAssertNotNil(note.subjectBlock.text, @"Subject Block has no text!");
+}
+
+- (void)testLikeNotificationContainsHeader
+{
+    Notification *note = [self loadLikeNotification];
+    NotificationBlockGroup *header = note.headerBlockGroup;
+    XCTAssertNotNil(header, @"Missing headerBlockGroup");
+
+    NotificationBlock *gravatarBlock = [header blockOfType:NoteBlockTypeImage];
+    XCTAssertNotNil(gravatarBlock.text, @"Missing Gravatar Text");
+    
+    NotificationMedia *media = gravatarBlock.media.firstObject;
+    XCTAssertNotNil(media.mediaURL, @"Missing Gravatar URL");
+    
+    NotificationBlock *snippetBlock = [header blockOfType:NoteBlockTypeText];
+    XCTAssertNotNil(snippetBlock.text, @"Missing Snippet Block");
+}
+
+- (void)testLikeNotificationContainsUserBlocksInTheBody
+{
+    Notification *note = [self loadLikeNotification];
+    for (NotificationBlockGroup *group in note.bodyBlockGroups) {
+        XCTAssertTrue(group.type == NoteBlockGroupTypeUser);
+    }
+}
+
+- (void)testLikeNotificationContainsPostAndSiteID
+{
+    Notification *note = [self loadLikeNotification];
+    XCTAssertNotNil(note.metaSiteID, @"Missing SiteID");
+    XCTAssertNotNil(note.metaPostID, @"Missing PostID");
+}
 
 
 #pragma mark - Helpers
@@ -72,6 +109,12 @@
 {
     return (Notification *)[self.contextManager loadEntityNamed:self.entityName
                                              withContentsOfFile:@"notifications-badge.json"];
+}
+
+- (Notification *)loadLikeNotification
+{
+    return (Notification *)[self.contextManager loadEntityNamed:self.entityName
+                                             withContentsOfFile:@"notifications-like.json"];
 }
 
 @end
