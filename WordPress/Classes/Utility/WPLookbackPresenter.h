f96a52553e92943ea7a922184c66d68a478fd8c8
Diego Rey Mendez <diegoreymendez@gmail.com>
2015-04-01 21:08:42 -0300

    Removes some unnecessary constraints.

--- WordPress/Classes/Utility/WPLookbackPresenter.h
+++ WordPress/Classes/Utility/WPLookbackPresenter.h
@@ -18,8 +18,8 @@ extern NSString* const WPLookbackPresenterShakeToPullUpFeedbackKey;
  *
  *  @param      token       The token to use to initialize lookback.  Cannot be an empty string and
  *                          cannot be nil.
- *  @param      userId      The identifier that lookback will use for the user.  Cannot be an empty
- *                          string and cannot be nil.
+ *  @param      userId      The identifier that lookback will use for the user.  Can be nil or empty
+ *                          since the user may not be logged in yet.
  *  @param      window      The window to add the shake gesture recognizer to.  Cannot be nil.
  *
  *  @returns    The initialized object.
