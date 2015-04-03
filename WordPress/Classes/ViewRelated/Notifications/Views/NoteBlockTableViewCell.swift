97c1e19be7c08186549fc7a876a5e806a729369c
Jorge Leandro Perez <jorge@lantean.co>
2015-04-03 12:39:15 -0300

    NoteBlockTableViewCell now has a custom separator

--- WordPress/Classes/ViewRelated/Notifications/Views/NoteBlockTableViewCell.swift
+++ WordPress/Classes/ViewRelated/Notifications/Views/NoteBlockTableViewCell.swift
@@ -3,5 +3,10 @@ import Foundation
 
 @objc public class NoteBlockTableViewCell : WPTableViewCell
 {
-
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundView = separatorsView
+    }
+    
+    public var separatorsView = NoteSeparatorsView()
 }
