470d9d4b5e5c5afdfa8c699e03a79dbec51b0354
Sendhil Panchadsaram <sendhilp@gmail.com>
2015-04-13 15:49:24 -0700

    Adding in documentation for `LoginFacade`, `OnePasswordFacade` and `LoginFields`

--- WordPress/Classes/Services/OnePasswordFacade.h
+++ WordPress/Classes/Services/OnePasswordFacade.h
@@ -2,9 +2,25 @@
 
 typedef void (^OnePasswordFacadeCallback)(NSString *username, NSString *password, NSError *);
 
+/**
+ *  This protocol is a Facade that hides some of the implementation details for interacting with 1Password.
+ */
 @protocol OnePasswordFacade
 
+/**
+ *  This method will pull up the 1Password extension and display any logins for the passed in `loginUrl`
+ *
+ *  @param loginUrl       the URL of the site in question.
+ *  @param viewController the view controller of the class that needs the 1Password extension to appear. Note this can't be nil.
+ *  @param completion     block that is called when 1Password is done retrieving the password.
+ */
 - (void)findLoginForURLString:(NSString *)loginUrl viewController:(UIViewController *)viewController completion:(OnePasswordFacadeCallback)completion;
+
+/**
+ *  A method to check if the 1Password extension is enabled
+ *
+ *  @return whether the 1Password extension is enabled.
+ */
 - (BOOL)isOnePasswordEnabled;
 
 @end
--- WordPress/Classes/ViewRelated/NUX/LoginFields.m
+++ WordPress/Classes/ViewRelated/NUX/LoginFields.m
@@ -2,7 +2,12 @@
 
 @implementation LoginFields
 
-+ (instancetype)loginFieldsWithUsername:(NSString *)username password:(NSString *)password siteUrl:(NSString *)siteUrl multifactorCode:(NSString *)multifactorCode userIsDotCom:(BOOL)userIsDotCom shouldDisplayMultiFactor:(BOOL)shouldDisplayMultifactor
++ (instancetype)loginFieldsWithUsername:(NSString *)username
+                               password:(NSString *)password
+                                siteUrl:(NSString *)siteUrl
+                        multifactorCode:(NSString *)multifactorCode
+                           userIsDotCom:(BOOL)userIsDotCom
+               shouldDisplayMultiFactor:(BOOL)shouldDisplayMultifactor
 {
     LoginFields *loginFields = [LoginFields new];
     loginFields.username = username;
