74864ca5b729d11facd4b92b2c2ec7f70b713832
aerych <eric@automattic.com>
2015-04-24 14:19:55 -0500

    Add reader style cell highlight effect to post cards.

--- WordPress/Classes/ViewRelated/Post/PostCardTableViewCell.m
+++ WordPress/Classes/ViewRelated/Post/PostCardTableViewCell.m
@@ -173,6 +173,55 @@
     [self setNeedsUpdateConstraints];
 }
 
+- (void)setHighlighted:(BOOL)highlighted animated:(BOOL)animated
+{
+    BOOL previouslyHighlighted = self.highlighted;
+    [super setHighlighted:highlighted animated:animated];
+
+    if (previouslyHighlighted == highlighted) {
+        return;
+    }
+
+    if (highlighted) {
+        [self setHighlightedEffect:highlighted animated:animated];
+    } else {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (!self.selected) {
+                [self setHighlightedEffect:highlighted animated:animated];
+            }
+        });
+    }
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+    [self setHighlightedEffect:selected animated:animated];
+}
+
+- (void)setHighlightedEffect:(BOOL)highlighted animated:(BOOL)animated
+{
+    [UIView animateWithDuration:animated ? .1f : 0.f
+                          delay:0
+                        options:UIViewAnimationCurveEaseInOut
+                     animations:^{
+                         self.shadowView.hidden = highlighted;
+                         self.alpha = highlighted ? .7f : 1.f;
+                         if (highlighted) {
+                             CGFloat perspective = IS_IPAD ? -0.00005 : -0.0001;
+                             CATransform3D transform = CATransform3DIdentity;
+                             transform.m24 = perspective;
+                             transform = CATransform3DScale(transform, .98f, .98f, 1);
+                             self.contentView.layer.transform = transform;
+                             self.contentView.layer.shouldRasterize = YES;
+                             self.contentView.layer.rasterizationScale = [[UIScreen mainScreen] scale];
+                         } else {
+                             self.contentView.layer.shouldRasterize = NO;
+                             self.contentView.layer.transform = CATransform3DIdentity;
+                         }
+                     } completion:nil];
+}
+
 
 #pragma mark - Helpers
 
