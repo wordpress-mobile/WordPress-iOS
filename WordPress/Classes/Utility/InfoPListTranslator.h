90b1f9a28f3b397b97c086b94180580c8e6918be
Sérgio Estêvão <sergio.estevao@automattic.com>
2015-04-27 23:37:13 +1000

    Update translation strings.

--- WordPress/Classes/Utility/InfoPListTranslator.h
+++ WordPress/Classes/Utility/InfoPListTranslator.h
@@ -1,7 +1,7 @@
 #import <Foundation/Foundation.h>
 
 /**
- This class purpose is to feed strings existent on the Info.plist file to the translation scripts.
+ This class purpose is to feed strings existing on the Info.plist file to the translation scripts.
  When the tranlators provide the translation the tokens will show up in the Localizable.strings files.
  Then the strings need to be copied over to the correct InfoPList.strings file for each language
  */
