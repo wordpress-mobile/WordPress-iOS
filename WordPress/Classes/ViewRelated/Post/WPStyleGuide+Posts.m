28ed2e745f17bea3d7e66b2d333c966222ba7b64
aerych <eric@automattic.com>
2015-04-28 14:51:23 -0500

    Fixes truncated descenders on ipad.

--- WordPress/Classes/ViewRelated/Post/WPStyleGuide+Posts.m
+++ WordPress/Classes/ViewRelated/Post/WPStyleGuide+Posts.m
@@ -39,14 +39,14 @@
 
 + (void)applyPostAuthorSiteStyle:(UILabel *)label
 {
-    CGFloat fontSize = [UIDevice isPad] ? 16.0 : 14.0;
+    CGFloat fontSize = 14.0;
     label.font = [WPFontManager openSansRegularFontOfSize:fontSize];
     label.textColor = [self greyDarken20];
 }
 
 + (void)applyPostAuthorNameStyle:(UILabel *)label
 {
-    CGFloat fontSize = [UIDevice isPad] ? 14.0 : 12.0;
+    CGFloat fontSize = 12.0;
     label.font = [WPFontManager openSansRegularFontOfSize:fontSize];
     label.textColor = [self grey];
 }
