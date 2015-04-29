0ba2568c951eddc10dd315e8807c0d7404bd8503
aerych <eric@automattic.com>
2015-04-29 09:38:50 -0500

    Cleans up popover controller if its VC is canceled.
    Fixes an issue where the filters popover could not be opened if the selection VC's cancel button was tapped.

--- WordPress/Classes/ViewRelated/Post/PostSettingsSelectionViewController.h
+++ WordPress/Classes/ViewRelated/Post/PostSettingsSelectionViewController.h
@@ -3,6 +3,7 @@
 @interface PostSettingsSelectionViewController : UITableViewController
 
 @property (nonatomic, copy) void(^onItemSelected)(NSObject *);
+@property (nonatomic, copy) void(^onCancel)();
 
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary;
 - (instancetype)initWithStyle:(UITableViewStyle)style andDictionary:(NSDictionary *)dictionary;
