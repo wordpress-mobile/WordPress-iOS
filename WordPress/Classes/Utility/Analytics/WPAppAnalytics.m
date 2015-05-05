dbc0f98a5ef651ddf166c01182fea3ca8f5e5d19
Aaron Douglas <astralbodies@gmail.com>
2015-05-05 09:29:59 -0500

    Remove TRACKS_ENABLED preprocessor macro in favor of Mixpanel Tweak

--- WordPress/Classes/Utility/Analytics/WPAppAnalytics.m
+++ WordPress/Classes/Utility/Analytics/WPAppAnalytics.m
@@ -71,10 +71,7 @@ static NSString* const WPAppAnalyticsKeyTimeInApp = @"time_in_app";
     }
 
     [WPAnalytics registerTracker:[[WPAnalyticsTrackerWPCom alloc] init]];
-    
-#ifdef TRACKS_ENABLED
     [WPAnalytics registerTracker:[WPAnalyticsTrackerAutomatticTracks new]];
-#endif
 
     if ([self isTrackingUsage]) {
         [self beginSession];
