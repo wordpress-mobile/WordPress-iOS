887ad3adea7ecaa92fad1241d403701c19adbd5e
aerych <aerychj@gmail.com>
2015-05-05 13:54:06 -0500

    Moves method to helper class.

--- WordPress/Classes/Categories/NSString+Helpers.m
+++ WordPress/Classes/Categories/NSString+Helpers.m
@@ -14,6 +14,25 @@ static NSString *const Ellipsis =  @"\u2026";
     return [[[string stringByStrippingHTML] stringByDecodingXMLCharacters] stringByTrimmingCharactersInSet:charSet];
 }
 
++ (NSString *)stripShortcodesFromString:(NSString *)string
+{
+    static NSRegularExpression *regex;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSError *error;
+        NSString *pattern = @"\\[[^\\]]+\\]";
+        regex = [[NSRegularExpression alloc] initWithPattern:pattern options:NSRegularExpressionCaseInsensitive error:&error];
+        if (error) {
+            DDLogError(@"Error parsing regex: %@", error);
+        }
+    });
+    NSRange range = NSMakeRange(0, [string length]);
+    return [regex stringByReplacingMatchesInString:string
+                                           options:NSMatchingReportCompletion
+                                             range:range
+                                      withTemplate:@""];
+}
+
 // Taken from AFNetworking's AFPercentEscapedQueryStringPairMemberFromStringWithEncoding
 - (NSString *)stringByUrlEncoding
 {
