1b46fe43951089b20e89bf8d55cf1da6dba187d9
aerych <aerychj@gmail.com>
2015-05-05 14:40:06 -0500

    Fix comment.

--- WordPress/Classes/Models/BasePost.m
+++ WordPress/Classes/Models/BasePost.m
@@ -154,7 +154,7 @@ NSString * const PostStatusDeleted = @"deleted"; // Returned by wpcom REST API w
 
     /*
      If the date is nil it means publish immediately so set the status to publish.
-     If the date is in the future set the status to publish.
+     If the date is in the future set the status to scheduled.
      If the date is now or in the past, and the status is scheduled, set the status
      to published.
      */
