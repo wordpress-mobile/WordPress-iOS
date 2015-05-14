a78850aa4dd7eca811c6c19908d12083476bab76
aerych <eric@automattic.com>
2015-05-14 10:49:24 -0500

    Map strings instead of conditionals.

--- WordPress/Classes/Categories/NSDate+StringFormatting.m
+++ WordPress/Classes/Categories/NSDate+StringFormatting.m
@@ -105,304 +105,122 @@
         index = maxYears;
     }
 
-    switch (index) {
-        // YEARS
-        case 1004:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In many years", @"In many years");
-            } else {
-                return  NSLocalizedString(@"Many years ago", @"Many years ago");
-            }
-        case 1003:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 3 years", @"In 3 years");
-            } else {
-                return  NSLocalizedString(@"3 years ago", @"3 years ago");
-            }
-        case 1002:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 2 years", @"In 2 years");
-            } else {
-                return  NSLocalizedString(@"2 years ago", @"2 years ago");
-            }
-        case 1001:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 1 year", @"In 1 year");
-            } else {
-                return  NSLocalizedString(@"A year ago", @"A year ago");
-            }
-
-        // MONTHS
-        case 112:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 12 months", @"In 12 months");
-            } else {
-                return  NSLocalizedString(@"12 months ago", @"12 months ago");
-            }
-        case 111:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 11 months", @"In 11 months");
-            } else {
-                return  NSLocalizedString(@"11 months ago", @"11 months ago");
-            }
-        case 110:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 10 months", @"In 10 months");
-            } else {
-                return  NSLocalizedString(@"10 months ago", @"10 months ago");
-            }
-        case 109:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 9 months", @"In 9 months");
-            } else {
-                return  NSLocalizedString(@"9 months ago", @"9 months ago");
-            }
-        case 108:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 8 months", @"In 8 months");
-            } else {
-                return  NSLocalizedString(@"8 months ago", @"8 months ago");
-            }
-        case 107:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 7 months", @"In 7 months");
-            } else {
-                return  NSLocalizedString(@"7 months ago", @"7 months ago");
-            }
-        case 106:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 6 months", @"In 6 months");
-            } else {
-                return  NSLocalizedString(@"6 months ago", @"6 months ago");
-            }
-        case 105:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 5 months", @"In 5 months");
-            } else {
-                return  NSLocalizedString(@"5 months ago", @"5 months ago");
-            }
-        case 104:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 4 months", @"In 4 months");
-            } else {
-                return  NSLocalizedString(@"4 months ago", @"4 months ago");
-            }
-        case 103:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 3 months", @"In 3 months");
-            } else {
-                return  NSLocalizedString(@"3 months ago", @"3 months ago");
-            }
-        case 102:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 2 months", @"In 2 months");
-            } else {
-                return  NSLocalizedString(@"2 months ago", @"2 months ago");
-            }
-        case 101:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 1 month", @"In 1 month");
-            } else {
-                return  NSLocalizedString(@"A month ago", @"A months ago");
-            }
-
-        // DAYS
-        case 31:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 31 days", @"In 31 days");
-            } else {
-                return  NSLocalizedString(@"31 days ago", @"31 days ago");
-            }
-        case 30:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 30 days", @"In 30 days");
-            } else {
-                return  NSLocalizedString(@"30 days ago", @"30 days ago");
-            }
-        case 29:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 29 days", @"In 29 days");
-            } else {
-                return  NSLocalizedString(@"29 days ago", @"29 days ago");
-            }
-        case 28:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 28 days", @"In 28 days");
-            } else {
-                return  NSLocalizedString(@"28 days ago", @"28 days ago");
-            }
-        case 27:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 27 days", @"In 27 days");
-            } else {
-                return  NSLocalizedString(@"27 days ago", @"27 days ago");
-            }
-        case 26:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 26 days", @"In 26 days");
-            } else {
-                return  NSLocalizedString(@"26 days ago", @"26 days ago");
-            }
-        case 25:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 25 days", @"In 25 days");
-            } else {
-                return  NSLocalizedString(@"25 days ago", @"25 days ago");
-            }
-        case 24:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 24 days", @"In 24 days");
-            } else {
-                return  NSLocalizedString(@"24 days ago", @"24 days ago");
-            }
-        case 23:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 23 days", @"In 23 days");
-            } else {
-                return  NSLocalizedString(@"23 days ago", @"23 days ago");
-            }
-        case 22:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 22 days", @"In 22 days");
-            } else {
-                return  NSLocalizedString(@"22 days ago", @"22 days ago");
-            }
-        case 21:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 21 days", @"In 21 days");
-            } else {
-                return  NSLocalizedString(@"21 days ago", @"21 days ago");
-            }
-        case 20:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 20 days", @"In 20 days");
-            } else {
-                return  NSLocalizedString(@"20 days ago", @"20 days ago");
-            }
-        case 19:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 19 days", @"In 19 days");
-            } else {
-                return  NSLocalizedString(@"19 days ago", @"19 days ago");
-            }
-        case 18:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 18 days", @"In 18 days");
-            } else {
-                return  NSLocalizedString(@"18 days ago", @"18 days ago");
-            }
-        case 17:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 17 days", @"In 17 days");
-            } else {
-                return  NSLocalizedString(@"17 days ago", @"17 days ago");
-            }
-        case 16:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 16 days", @"In 16 days");
-            } else {
-                return  NSLocalizedString(@"16 days ago", @"16 days ago");
-            }
-        case 15:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 15 days", @"In 15 days");
-            } else {
-                return  NSLocalizedString(@"15 days ago", @"15 days ago");
-            }
-        case 14:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 14 days", @"In 14 days");
-            } else {
-                return  NSLocalizedString(@"14 days ago", @"14 days ago");
-            }
-        case 13:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 13 days", @"In 13 days");
-            } else {
-                return  NSLocalizedString(@"13 days ago", @"13 days ago");
-            }
-        case 12:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 12 days", @"In 12 days");
-            } else {
-                return  NSLocalizedString(@"12 days ago", @"12 days ago");
-            }
-        case 11:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 11 days", @"In 11 days");
-            } else {
-                return  NSLocalizedString(@"11 days ago", @"11 days ago");
-            }
-        case 10:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 10 days", @"In 10 days");
-            } else {
-                return  NSLocalizedString(@"10 days ago", @"10 days ago");
-            }
-        case 9:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 9 days", @"In 9 days");
-            } else {
-                return  NSLocalizedString(@"9 days ago", @"9 days ago");
-            }
-        case 8:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 8 days", @"In 8 days");
-            } else {
-                return  NSLocalizedString(@"8 days ago", @"8 days ago");
-            }
-        case 7:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 7 days", @"In 7 days");
-            } else {
-                return  NSLocalizedString(@"7 days ago", @"7 days ago");
-            }
-        case 6:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 6 days", @"In 6 days");
-            } else {
-                return  NSLocalizedString(@"6 days ago", @"6 days ago");
-            }
-        case 5:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 5 days", @"In 5 days");
-            } else {
-                return  NSLocalizedString(@"5 days ago", @"5 days ago");
-            }
-        case 4:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 4 days", @"In 4 days");
-            } else {
-                return  NSLocalizedString(@"4 days ago", @"4 days ago");
-            }
-        case 3:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 3 days", @"In 3 days");
-            } else {
-                return  NSLocalizedString(@"3 days ago", @"3 days ago");
-            }
-        case 2:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"In 2 days", @"In 2 days");
-            } else {
-                return  NSLocalizedString(@"2 days ago", @"2 days ago");
-            }
-        case 1:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"Tomorrow", @"Tomorrow");
-            } else {
-                return  NSLocalizedString(@"Yesterday", @"Yesterday");
-            }
-        case 0:
-            if (isFutureDate) {
-                return  NSLocalizedString(@"Later today", @"Later today");
-            } else {
-                return  NSLocalizedString(@"Today", @"Today");
-            }
+    NSDictionary *map = isFutureDate ? [self futureConciseStringMap] : [self conciseStringMap];
+    NSString *response = [map objectForKey:@(index)];
+    if (response) {
+        return response;
     }
 
     // Safety Net
     return responseForNoMatchFound;
+}
+
++ (NSDictionary *)futureConciseStringMap
+{
+    return @{
+             @1004 : NSLocalizedString(@"In many years", @"In many years"),
+             @1003 : NSLocalizedString(@"In 3 years", @"In 3 years"),
+             @1002 : NSLocalizedString(@"In 2 years", @"In 2 years"),
+             @1001 : NSLocalizedString(@"Next year", @"Next year"),
+             @112 : NSLocalizedString(@"In 12 months", @"In 12 months"),
+             @111 : NSLocalizedString(@"In 11 months", @"In 11 months"),
+             @110 : NSLocalizedString(@"In 10 months", @"In 10 months"),
+             @109 : NSLocalizedString(@"In 9 months", @"In 9 months"),
+             @108 : NSLocalizedString(@"In 8 months", @"In 8 months"),
+             @107 : NSLocalizedString(@"In 7 months", @"In 7 months"),
+             @106 : NSLocalizedString(@"In 6 months", @"In 6 months"),
+             @105 : NSLocalizedString(@"In 5 months", @"In 5 months"),
+             @104 : NSLocalizedString(@"In 4 months", @"In 4 months"),
+             @103 : NSLocalizedString(@"In 3 months", @"In 3 months"),
+             @102 : NSLocalizedString(@"In 2 months", @"In 2 months"),
+             @101 : NSLocalizedString(@"Next month", @"Next month"),
+             @31 : NSLocalizedString(@"In 31 days", @"In 31 days"),
+             @30 : NSLocalizedString(@"In 30 days", @"In 30 days"),
+             @29 : NSLocalizedString(@"In 29 days", @"In 29 days"),
+             @28 : NSLocalizedString(@"In 28 days", @"In 28 days"),
+             @27 : NSLocalizedString(@"In 27 days", @"In 27 days"),
+             @26 : NSLocalizedString(@"In 26 days", @"In 26 days"),
+             @25 : NSLocalizedString(@"In 25 days", @"In 25 days"),
+             @24 : NSLocalizedString(@"In 24 days", @"In 24 days"),
+             @23 : NSLocalizedString(@"In 23 days", @"In 23 days"),
+             @22 : NSLocalizedString(@"In 22 days", @"In 22 days"),
+             @21 : NSLocalizedString(@"In 21 days", @"In 21 days"),
+             @20 : NSLocalizedString(@"In 20 days", @"In 20 days"),
+             @19 : NSLocalizedString(@"In 19 days", @"In 19 days"),
+             @18 : NSLocalizedString(@"In 18 days", @"In 18 days"),
+             @17 : NSLocalizedString(@"In 17 days", @"In 17 days"),
+             @16 : NSLocalizedString(@"In 16 days", @"In 16 days"),
+             @15 : NSLocalizedString(@"In 15 days", @"In 15 days"),
+             @14 : NSLocalizedString(@"In 14 days", @"In 14 days"),
+             @13 : NSLocalizedString(@"In 13 days", @"In 13 days"),
+             @12 : NSLocalizedString(@"In 12 days", @"In 12 days"),
+             @11 : NSLocalizedString(@"In 11 days", @"In 11 days"),
+             @10 : NSLocalizedString(@"In 10 days", @"In 10 days"),
+             @9 : NSLocalizedString(@"In 9 days", @"In 9 days"),
+             @8 : NSLocalizedString(@"In 8 days", @"In 8 days"),
+             @7 : NSLocalizedString(@"In 7 days", @"In 7 days"),
+             @6 : NSLocalizedString(@"In 6 days", @"In 6 days"),
+             @5 : NSLocalizedString(@"In 5 days", @"In 5 days"),
+             @4 : NSLocalizedString(@"In 4 days", @"In 4 days"),
+             @3 : NSLocalizedString(@"In 3 days", @"In 3 days"),
+             @2 : NSLocalizedString(@"In 2 days", @"In 2 days"),
+             @1 : NSLocalizedString(@"Tomorrow", @"Tomorrow"),
+             @0 : NSLocalizedString(@"Later Today", @"Later Today"),
+             };
+}
+
++ (NSDictionary *)conciseStringMap
+{
+    return @{
+             @1004 : NSLocalizedString(@"4 years ago", @"4 years ago"),
+             @1003 : NSLocalizedString(@"3 years ago", @"3 years ago"),
+             @1002 : NSLocalizedString(@"2 years ago", @"2 years ago"),
+             @1001 : NSLocalizedString(@"Last year", @"Last year"),
+             @112 : NSLocalizedString(@"12 months ago", @"12 months ago"),
+             @111 : NSLocalizedString(@"11 months ago", @"11 months ago"),
+             @110 : NSLocalizedString(@"10 months ago", @"10 months ago"),
+             @109 : NSLocalizedString(@"9 months ago", @"9 months ago"),
+             @108 : NSLocalizedString(@"8 months ago", @"8 months ago"),
+             @107 : NSLocalizedString(@"7 months ago", @"7 months ago"),
+             @106 : NSLocalizedString(@"6 months ago", @"6 months ago"),
+             @105 : NSLocalizedString(@"5 months ago", @"5 months ago"),
+             @104 : NSLocalizedString(@"4 months ago", @"4 months ago"),
+             @103 : NSLocalizedString(@"3 months ago", @"3 months ago"),
+             @102 : NSLocalizedString(@"2 months ago", @"2 months ago"),
+             @102 : NSLocalizedString(@"Last month", @"Last month"),
+             @31 : NSLocalizedString(@"31 days ago", @"31 days ago"),
+             @30 : NSLocalizedString(@"30 days ago", @"30 days ago"),
+             @29 : NSLocalizedString(@"29 days ago", @"29 days ago"),
+             @28 : NSLocalizedString(@"28 days ago", @"28 days ago"),
+             @27 : NSLocalizedString(@"27 days ago", @"27 days ago"),
+             @26 : NSLocalizedString(@"26 days ago", @"26 days ago"),
+             @25 : NSLocalizedString(@"25 days ago", @"25 days ago"),
+             @24 : NSLocalizedString(@"24 days ago", @"24 days ago"),
+             @23 : NSLocalizedString(@"23 days ago", @"23 days ago"),
+             @22 : NSLocalizedString(@"22 days ago", @"22 days ago"),
+             @21 : NSLocalizedString(@"21 days ago", @"21 days ago"),
+             @20 : NSLocalizedString(@"20 days ago", @"20 days ago"),
+             @19 : NSLocalizedString(@"19 days ago", @"19 days ago"),
+             @18 : NSLocalizedString(@"18 days ago", @"18 days ago"),
+             @17 : NSLocalizedString(@"17 days ago", @"17 days ago"),
+             @16 : NSLocalizedString(@"16 days ago", @"16 days ago"),
+             @15 : NSLocalizedString(@"15 days ago", @"15 days ago"),
+             @14 : NSLocalizedString(@"14 days ago", @"14 days ago"),
+             @13 : NSLocalizedString(@"13 days ago", @"13 days ago"),
+             @12 : NSLocalizedString(@"12 days ago", @"12 days ago"),
+             @11 : NSLocalizedString(@"11 days ago", @"11 days ago"),
+             @10 : NSLocalizedString(@"10 days ago", @"10 days ago"),
+             @9 : NSLocalizedString(@"9 days ago", @"9 days ago"),
+             @8 : NSLocalizedString(@"8 days ago", @"8 days ago"),
+             @7 : NSLocalizedString(@"7 days ago", @"7 days ago"),
+             @6 : NSLocalizedString(@"6 days ago", @"6 days ago"),
+             @5 : NSLocalizedString(@"5 days ago", @"5 days ago"),
+             @4 : NSLocalizedString(@"4 days ago", @"4 days ago"),
+             @3 : NSLocalizedString(@"3 days ago", @"3 days ago"),
+             @2 : NSLocalizedString(@"2 days ago", @"2 days ago"),
+             @1 : NSLocalizedString(@"Yesterday", @"Yesterday"),
+             @0 : NSLocalizedString(@"Today", @"Today"),
+             };
 }
 
 @end
