570141b2d46aa8c41a22e0054ccb64ffbdcc50c2
Jorge Leandro Perez <jorge@lantean.co>
2015-05-19 14:57:26 -0300

    LoginViewController: Increases SMS Label Maximum Number of Lines

--- WordPress/Classes/ViewRelated/NUX/LoginViewController.m
+++ WordPress/Classes/ViewRelated/NUX/LoginViewController.m
@@ -88,7 +88,7 @@ static CGFloat const GeneralWalkthroughAlphaHidden              = 0.0f;
 static CGFloat const GeneralWalkthroughAlphaEnabled             = 1.0f;
 
 static UIOffset const LoginOnePasswordPadding                   = {9.0, 0.0f};
-static NSInteger const LoginVerificationCodeNumberOfLines       = 2;
+static NSInteger const LoginVerificationCodeNumberOfLines       = 3;
 
 - (void)dealloc
 {
