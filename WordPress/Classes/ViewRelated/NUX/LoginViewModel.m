d6f580ebe0b577c187efb7c1bafd01d2b5c5123e
Jorge Leandro Perez <jorge@lantean.co>
2015-05-20 12:02:56 -0300

    Updates SMS Sent String

--- WordPress/Classes/ViewRelated/NUX/LoginViewModel.m
+++ WordPress/Classes/ViewRelated/NUX/LoginViewModel.m
@@ -187,7 +187,7 @@ static NSString *const ForgotPasswordRelativeUrl = @"/wp-login.php?action=lostpa
 
 - (void)requestOneTimeCode
 {
-    NSString *message = NSLocalizedString(@"SMS Sent!", @"One Time Code has been sent via SMS");
+    NSString *message = NSLocalizedString(@"SMS Sent", @"One Time Code has been sent via SMS");
     [self.presenter showAlertWithMessage:message];
     [self.loginFacade requestOneTimeCodeWithLoginFields:[self loginFields]];
 }
