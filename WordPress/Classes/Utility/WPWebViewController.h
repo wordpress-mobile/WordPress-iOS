ca8aac7593a0b6dbdd5d3f4a055b439447fefd2e
Jorge Leandro Perez <jorge@lantean.co>
2015-05-26 17:17:01 -0300

    WPWebViewController: Nukes iPad NavBar Hack

--- WordPress/Classes/Utility/WPWebViewController.h
+++ WordPress/Classes/Utility/WPWebViewController.h
@@ -8,7 +8,6 @@
 @property (nonatomic, strong) IBOutlet UIView *loadingView;
 @property (nonatomic, strong) IBOutlet UIActivityIndicatorView *activityIndicator;
 @property (nonatomic, strong) IBOutlet UILabel *loadingLabel;
-@property (nonatomic, strong) IBOutlet UINavigationBar *iPadNavBar;
 @property (nonatomic, strong) IBOutlet UIBarButtonItem *backButton;
 @property (nonatomic, strong) IBOutlet UIBarButtonItem *forwardButton;
 @property (nonatomic, strong) IBOutlet UIBarButtonItem *refreshButton;
