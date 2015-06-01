f611c5fa075ebabd286b45dae301d462ea0d7975
Jorge Leandro Perez <jorge@lantean.co>
2015-06-01 17:38:44 -0300

    CreateAccountViewController: TOS WebView is now Modal

--- WordPress/Classes/ViewRelated/NUX/CreateAccountAndBlogViewController.m
+++ WordPress/Classes/ViewRelated/NUX/CreateAccountAndBlogViewController.m
@@ -570,8 +570,9 @@ static UIOffset const CreateAccountAndBlogOnePasswordPadding = {9.0, 0.0};
 {
     NSURL *targetURL = [NSURL URLWithString:WPAutomatticTermsOfServiceURL];
     WPWebViewController *webViewController = [WPWebViewController webViewControllerWithURL:targetURL];
-    [self.navigationController pushViewController:webViewController animated:YES];
-    [self.navigationController setNavigationBarHidden:NO animated:YES];
+    
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:webViewController];
+    [self presentViewController:navController animated:YES completion:nil];
 }
 
 - (void)keyboardWillShow:(NSNotification *)notification
