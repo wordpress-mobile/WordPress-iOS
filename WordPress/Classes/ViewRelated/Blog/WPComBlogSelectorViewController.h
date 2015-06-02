99330609b0538d8f968d05a60bd8d76f203ed0d7
aerych <eric@automattic.com>
2015-06-02 16:01:09 -0500

    Only show blogs hosted at WordPress.com when reblogging.

--- WordPress/Classes/ViewRelated/Blog/WPComBlogSelectorViewController.h
+++ WordPress/Classes/ViewRelated/Blog/WPComBlogSelectorViewController.h
@@ -1,5 +1,8 @@
 #import "BlogSelectorViewController.h"
 
+/**
+ A version of the blog selector that shows only blogs hosted on WordPress.com.
+ */
 @interface WPComBlogSelectorViewController : BlogSelectorViewController
 
 @end
