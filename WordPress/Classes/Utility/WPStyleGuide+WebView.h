8f237d4d8ea66c926e48e83b3c8340bb1e8fa96e
Jorge Leandro Perez <jorge@lantean.co>
2015-06-02 17:29:42 -0300

    WPWebViewController: Applying custom style when displayed as Modal

---
+++ WordPress/Classes/Utility/WPStyleGuide+WebView.h
@@ -0,0 +1,13 @@
+#import <Foundation/Foundation.h>
+
+
+
+#pragma mark - WebViewController Styles
+
+@interface WPStyleGuide (WebView)
+
+// Color
++ (UIColor *)webViewModalNavigationBarBackground;
++ (UIColor *)webViewModalNavigationBarShadow;
+
+@end
