cb0540122549e95f5805099f1cdfd3aea96ce6af
Jorge Leandro Perez <jorge@lantean.co>
2015-06-02 13:02:12 -0300

    NotificationsViewController: Fixes indentation

--- WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationsViewController.m
+++ WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationsViewController.m
@@ -737,7 +737,7 @@ static NSString const *NotificationsNetworkStatusKey    = @"network_status";
 
 - (void)didTapNoResultsView:(WPNoResultsView *)noResultsView
 {
-	NSURL *targetURL                        = [NSURL URLWithString:WPJetpackInformationURL];
+    NSURL *targetURL                        = [NSURL URLWithString:WPJetpackInformationURL];
     WPWebViewController *webViewController  = [WPWebViewController webViewControllerWithURL:targetURL];
  
     UINavigationController *navController   = [[UINavigationController alloc] initWithRootViewController:webViewController];
