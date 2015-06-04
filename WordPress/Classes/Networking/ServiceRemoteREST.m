7ad99efe7485aa209302fd1a8d9dc9ac9a606b2c
Diego Rey Mendez <diegoreymendez@gmail.com>
2015-06-04 15:11:17 -0300

    Removes a header (WIP).

--- WordPress/Classes/Networking/ServiceRemoteREST.m
+++ WordPress/Classes/Networking/ServiceRemoteREST.m
@@ -1,11 +1,3 @@
-//
-//  ServiceRemoteREST.m
-//  WordPress
-//
-//  Created by Diego E. Rey Mendez on 6/4/15.
-//  Copyright (c) 2015 WordPress. All rights reserved.
-//
-
 #import "ServiceRemoteREST.h"
 #import "WordPressComApi.h"
 
