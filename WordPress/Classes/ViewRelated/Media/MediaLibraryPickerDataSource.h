e6303a3ce1d25e3f926d684c16e4151d120a1d00
Sérgio Estêvão <sergio.estevao@automattic.com>
2015-06-05 13:52:59 +0700

    Implement picker with empty media group.

--- WordPress/Classes/ViewRelated/Media/MediaLibraryPickerDataSource.h
+++ WordPress/Classes/ViewRelated/Media/MediaLibraryPickerDataSource.h
@@ -1,6 +1,10 @@
 #import <Foundation/Foundation.h>
 #import <WPMediaPicker/WPMediaPicker.h>
 
+@interface MediaLibraryGroup: NSObject <WPMediaGroup>
+
+@end
+
 @interface MediaLibraryPickerDataSource : NSObject <WPMediaCollectionDataSource>
 
 @end
