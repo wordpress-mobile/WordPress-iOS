ad31995878359ec9ac5d98cd6956ac8023f88b14
Sérgio Estêvão <sergio.estevao@automattic.com>
2015-06-05 11:56:52 +0700

    Add media library sync methods.

--- WordPress/Classes/Networking/MediaServiceRemote.h
+++ WordPress/Classes/Networking/MediaServiceRemote.h
@@ -5,15 +5,30 @@
 
 @protocol MediaServiceRemote <NSObject>
 
-- (void) getMediaWithID:(NSNumber *) mediaID
+
+- (void)getMediaWithID:(NSNumber *) mediaID
                  forBlog:(Blog *)blog
                  success:(void (^)(RemoteMedia *remoteMedia))success
                  failure:(void (^)(NSError *error))failure;
 
-- (void) createMedia:(RemoteMedia *)media
+- (void)createMedia:(RemoteMedia *)media
                 forBlog:(Blog *)blog
                progress:(NSProgress **)progress
                 success:(void (^)(RemoteMedia *remoteMedia))success
                 failure:(void (^)(NSError *error))failure;
+
+/**
+ *  Get Media items from blog using the options parameter.
+ *
+ *  @param blog    from to where fetch the media object.
+ *  @param options an dictionary with optional parameters for the request.
+ *  @param success a block to be executed when the request finishes with success.
+ *  @param failure a block to be execute when the request fails.
+ */
+- (void)getMediaLibraryForBlog:(Blog *)blog
+                       options:(NSDictionary *)options
+                       success:(void (^)(NSArray *))success
+                       failure:(void (^)(NSError *))failure;
+
 
 @end
