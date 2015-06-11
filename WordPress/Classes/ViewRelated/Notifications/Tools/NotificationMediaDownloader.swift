e11439bf9e19214039fe04ac218d5a7613888292
Jorge Leandro Perez <jorge@lantean.co>
2015-06-11 19:52:01 -0300

    Implements NotificationMediaDownloader Resizing API

--- WordPress/Classes/ViewRelated/Notifications/Tools/NotificationMediaDownloader.swift
+++ WordPress/Classes/ViewRelated/Notifications/Tools/NotificationMediaDownloader.swift
@@ -7,24 +7,33 @@ import Foundation
         downloadQueue.cancelAllOperations()
     }
     
-    public init(maximumImageWidth: CGFloat) {
-        maxImageWidth = maximumImageWidth
-        super.init()
-    }
-    
     // MARK: - Public Helpers
-    public typealias SuccessBlock = (Void -> Void)
-    
-    public func downloadMediaWithUrls(urls: NSSet, completion: SuccessBlock) {
-        let allUrls     = urls.allObjects as? [NSURL]
-        let missingUrls = allUrls?.filter { self.shouldDownloadImage(url: $0) }
+    public func downloadMedia(#urls: Set<NSURL>, maximumWidth: CGFloat, completion: SuccessBlock) {
+        let missingUrls = filter(urls) { self.shouldDownloadImage(url: $0) }
 
-        missingUrls?.map { (url) in
-            self.downloadImage(url) {
-                self.resizeImageIfNeeded($0, maxImageWidth: self.maxImageWidth) {
-                    self.mediaMap[url] = $0
+        for url in missingUrls {
+            downloadImage(url) {
+                self.originalImagesMap[url] = $0
+                
+                self.resizeImageIfNeeded($0, maximumWidth: maximumWidth) {
+                    self.resizedImagesMap[url] = $0
                     completion()
                 }
+            }
+        }
+    }
+    
+    public func resizeMediaWithIncorrectSize(maximumWidth: CGFloat, completion: SuccessBlock) {
+        for (url, originalImage) in originalImagesMap {
+            let targetSize = cappedImageSize(originalImage.size, maximumWidth: maximumWidth)
+
+            if resizedImagesMap[url]?.size == targetSize {
+                continue
+            }
+println("Resizing \(url) original \(originalImage.size) target \(targetSize)")
+            resizeImageIfNeeded(originalImage, maximumWidth: maximumWidth) {
+                self.resizedImagesMap[url] = $0
+                completion()
             }
         }
     }
@@ -32,7 +41,7 @@ import Foundation
     public func imagesForUrls(urls: [NSURL]) -> [NSURL: UIImage] {
         var filtered = [NSURL: UIImage]()
         
-        for (url, image) in mediaMap {
+        for (url, image) in resizedImagesMap {
             if contains(urls, url) {
                 filtered[url] = image
             }
@@ -51,7 +60,6 @@ import Foundation
         let operation                   = AFHTTPRequestOperation(request: request)
         operation.responseSerializer    = responseSerializer
         operation.setCompletionBlockWithSuccess({
-            [weak self]
             (AFHTTPRequestOperation operation, AnyObject responseObject) -> Void in
             
             if let unwrappedImage = responseObject as? UIImage {
@@ -61,52 +69,70 @@ import Foundation
         }, failure: nil)
         
         downloadQueue.addOperation(operation)
-        retryMap[url] = retryCount(url) + 1
+        increaseRetryCount(url)
     }
     
-    private func shouldDownloadImage(url url: NSURL) -> Bool {
-        if mediaMap[url] != nil || retryCount(url) > maximumRetryCount {
-            return false
-        }
-
+    private func shouldDownloadImage(#url: NSURL) -> Bool {
         let operations  = downloadQueue.operations as? [AFHTTPRequestOperation]
-        let filtered    = operations?.filter { $0.request.URL!.isEqual(url) }
+        let filtered    = operations?.filter { $0.request.URL!.isEqual(url) } ?? [AFHTTPRequestOperation]()
         
-        return filtered?.count == 0
-    }    
+        return originalImagesMap[url] == nil && !exceededRetryCount(url) && filtered.isEmpty
+    }
     
-    private func retryCount(url: NSURL) -> Int {
+    
+    // MARK: - Retry Helpers
+    private func increaseRetryCount(url: NSURL) {
+        retryMap[url] = currentRetryCount(url) + 1
+    }
+    
+    private func currentRetryCount(url: NSURL) -> Int {
         return retryMap[url] ?? 0
+    }
+    
+    private func exceededRetryCount(url: NSURL) -> Bool {
+        return currentRetryCount(url) >= maximumRetryCount
     }
     
     
     // MARK: - Async Image Resizing Helpers
-    private func resizeImageIfNeeded(image: UIImage, maxImageWidth: CGFloat, callback: ((UIImage) -> ())) {
-        if image.size.width <= maxImageWidth {
+    private func resizeImageIfNeeded(image: UIImage, maximumWidth: CGFloat, callback: ((UIImage) -> ())) {
+        let targetSize = cappedImageSize(image.size, maximumWidth: maximumWidth)
+        if image.size == targetSize {
             callback(image)
             return
         }
-
-        var targetSize      = image.size
-        targetSize.height   = round(maxImageWidth * targetSize.height / targetSize.width)
-        targetSize.width    = maxImageWidth
         
         dispatch_async(resizeQueue) {
-            let resizedImage = image.imageCroppedToFitSize(targetSize, ignoreAlpha: false)
+            let resizedImage = image.imageScaledToFitSize(targetSize, ignoreAlpha: false)
             dispatch_async(dispatch_get_main_queue()) {
                 callback(resizedImage)
             }
         }
     }
+
+    private func cappedImageSize(originalSize: CGSize, maximumWidth: CGFloat) -> CGSize {
+        var targetSize = originalSize
+
+        if targetSize.width > maximumWidth {
+            targetSize.height   = round(maximumWidth * targetSize.height / targetSize.width)
+            targetSize.width    = maximumWidth
+        }
+        
+        return targetSize
+    }
     
-    // MARK: - Constants
+    
+    // MARK: - Public Aliases
+    public typealias SuccessBlock   = (Void -> Void)
+    
+    // MARK: - Private Constants
     private let maximumRetryCount   = 3
     
     // MARK: - Private Properties
     private let responseSerializer  = AFImageResponseSerializer()
     private let downloadQueue       = NSOperationQueue()
     private let resizeQueue         = dispatch_queue_create("notifications.media.resize", DISPATCH_QUEUE_CONCURRENT)
-    private var mediaMap            = [NSURL: UIImage]()
+    private var originalImagesMap   = [NSURL: UIImage]()
+    private var resizedImagesMap    = [NSURL: UIImage]()
     private var retryMap            = [NSURL: Int]()
-    private let maxImageWidth:      CGFloat
 }
