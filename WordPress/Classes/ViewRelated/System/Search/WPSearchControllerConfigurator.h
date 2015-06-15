4c909266c37a3a3000a4cde7d6cf1f2506692b96
Will Kwon <will@willkwon.com>
2015-06-15 13:52:59 -0700

    Adding init being unavailable

--- WordPress/Classes/ViewRelated/System/Search/WPSearchControllerConfigurator.h
+++ WordPress/Classes/ViewRelated/System/Search/WPSearchControllerConfigurator.h
@@ -8,6 +8,7 @@ extern const CGFloat SearchWrapperViewLandscapeHeight;
 
 @interface WPSearchControllerConfigurator : NSObject
 
+- (instancetype)init __attribute__((unavailable("Must call initWithSearchController")));
 - (instancetype)initWithSearchController:(WPSearchController *)searchController
                    withSearchWrapperView:(UIView *)searchWrapperView
                             withDelegate:(id<WPSearchControllerDelegate, WPSearchResultsUpdating>)delegate;
