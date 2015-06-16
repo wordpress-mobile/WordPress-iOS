e916e8dc37f26dab0f7cc9bf06f0d637d65f7905
Will Kwon <will@willkwon.com>
2015-06-15 18:37:44 -0700

    Formatting

--- WordPress/Classes/ViewRelated/Blog/BlogListViewController.h
+++ WordPress/Classes/ViewRelated/Blog/BlogListViewController.h
@@ -1,7 +1,12 @@
 #import <UIKit/UIKit.h>
 #import "WPSearchController.h"
 
-@interface BlogListViewController : UIViewController <NSFetchedResultsControllerDelegate, UIDataSourceModelAssociation, WPSearchResultsUpdating, WPSearchControllerDelegate, UITableViewDelegate, UITableViewDataSource>
+@interface BlogListViewController : UIViewController <NSFetchedResultsControllerDelegate,
+                                                            UIDataSourceModelAssociation,
+                                                            WPSearchResultsUpdating,
+                                                            WPSearchControllerDelegate,
+                                                            UITableViewDelegate,
+                                                            UITableViewDataSource>
 
 - (void)bypassBlogListViewController;
 - (BOOL)shouldBypassBlogListViewControllerWhenSelectedFromTabBar;
