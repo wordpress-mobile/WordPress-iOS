c82e4896485ca3b4def3bac868bde55011228bbd
Will Kwon <will@willkwon.com>
2015-06-15 18:54:48 -0700

    Adding a new protocol instead of having two

--- WordPress/Classes/ViewRelated/System/Search/WPSearchControllerConfigurator.h
+++ WordPress/Classes/ViewRelated/System/Search/WPSearchControllerConfigurator.h
@@ -7,12 +7,22 @@ extern const CGFloat SearchWrapperViewPortraitHeight;
 extern const CGFloat SearchWrapperViewLandscapeHeight;
 extern const NSTimeInterval SearchBarAnimationDuration;
 
+@protocol WPSearchControllerWithResultsUpdatingDelegate <NSObject>
+
+@required
+- (void)presentSearchController:(WPSearchController *)searchController;
+- (void)willPresentSearchController:(WPSearchController *)searchController;
+- (void)willDismissSearchController:(WPSearchController *)searchController;
+- (void)updateSearchResultsForSearchController:(WPSearchController *)searchController;
+
+@end
+
 @interface WPSearchControllerConfigurator : NSObject
 
 - (instancetype)init __attribute__((unavailable("Must call initWithSearchController")));
 - (instancetype)initWithSearchController:(WPSearchController *)searchController
                    withSearchWrapperView:(UIView *)searchWrapperView
-                            withDelegate:(id<WPSearchControllerDelegate, WPSearchResultsUpdating>)delegate;
+                            withDelegate:(id<WPSearchControllerWithResultsUpdatingDelegate>)delegate;
 - (void)configureSearchControllerBarAndWrapperView;
 
 @end
