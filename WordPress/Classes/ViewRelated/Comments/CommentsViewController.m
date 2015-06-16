9ef6f2e6734cb9bc706f3707bd9ae6911a9dbea3
Jorge Leandro Perez <jorge@lantean.co>
2015-06-16 12:22:53 -0300

    CommentService: Updating lastCommentsSync Property

--- WordPress/Classes/ViewRelated/Comments/CommentsViewController.m
+++ WordPress/Classes/ViewRelated/Comments/CommentsViewController.m
@@ -278,10 +278,7 @@ NSTimeInterval const CommentsRefreshTimeoutInSeconds    = 60 * 5; // 5 minutes
         }
         
         [commentService syncCommentsForBlog:blogInContext
-                                    success:^{
-                                                blogInContext.lastCommentsSync = [NSDate date];
-                                                [[ContextManager sharedInstance] saveContext:context];
-                                        
+                                    success:^{                                        
                                                 if (success) {
                                                     dispatch_async(dispatch_get_main_queue(), ^{
                                                         success(true);
