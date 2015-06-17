a1ed625bafda7b4ab646e3fb9225518086128c7c
Jorge Leandro Perez <jorge@lantean.co>
2015-06-17 16:08:03 -0300

    NotificationsService: Implements getAllSettings

--- WordPress/Classes/Services/NotificationsService.swift
+++ WordPress/Classes/Services/NotificationsService.swift
@@ -1,15 +1,35 @@
-//
-//  NotificationsService.swift
-//  WordPress
-//
-//  Created by Jorge Leandro Perez on 6/17/15.
-//  Copyright (c) 2015 WordPress. All rights reserved.
-//
-
 import Foundation
 
 
-public class NotificationsService
+public class NotificationsService : NSObject, LocalCoreDataService
 {
+    /**
+    *  @details     Designated Initializer
+    *  @param       managedObjectContext    A Reference to the MOC that should be used to interact with
+    *                                       the Core Data Persistent Store.
+    */
+    public required init(managedObjectContext: NSManagedObjectContext) {
+        super.init()
+        self.managedObjectContext       = managedObjectContext
+        self.notificationsServiceRemote = NotificationsServiceRemote(api: apiForRequest())
+    }
     
+    /**
+    *  @details     Helper method to get the WordPress.com REST Api, if any
+    *  @returns     WordPressComApi instance, if applicable, or nil.
+    */
+    private func apiForRequest() -> WordPressComApi? {
+        let accountService = AccountService(managedObjectContext: managedObjectContext)
+        let unwrappedRestApi = accountService.defaultWordPressComAccount()?.restApi
+        
+        if unwrappedRestApi != nil && unwrappedRestApi?.hasCredentials() == true {
+            return unwrappedRestApi!
+        }
+
+        return nil
+    }
+    
+    // MARK: - Private Internal Properties
+    private var managedObjectContext :      NSManagedObjectContext!
+    private var notificationsServiceRemote: NotificationsServiceRemote?
 }
