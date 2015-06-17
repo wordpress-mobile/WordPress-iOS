4694a5e5a0d9b8ac1ba69cbbf110282012831f59
Sérgio Estêvão <sergio.estevao@automattic.com>
2015-06-17 10:52:03 +0700

    Sync media library from server

--- WordPress/Classes/Networking/MediaServiceRemote.h
+++ WordPress/Classes/Networking/MediaServiceRemote.h
@@ -16,4 +16,18 @@
                 success:(void (^)(RemoteMedia *remoteMedia))success
                 failure:(void (^)(NSError *error))failure;
 
+/**
+ *  Get Media items from blog using the options parameter.
+ *
+ *  @param blog    from to where fetch the media object.
+ *  @param options an dictionary with optional parameters for the request.
+ *  @param success a block to be executed when the request finishes with success.
+ *  @param failure a block to be execute when the request fails.
+ */
+- (void)getMediaLibraryForBlog:(Blog *)blog
+                       options:(NSDictionary *)options
+                       success:(void (^)(NSArray *))success
+                       failure:(void (^)(NSError *))failure;
+
+
 @end
