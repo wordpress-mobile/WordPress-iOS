ee7234489aec6c4f0466c8c1acdb12ba0b55e8df
Jorge Leandro Perez <jorge@lantean.co>
2015-06-17 11:34:37 -0300

    Adds stub NotificationsService and NotificationsServiceRemote

---
+++ WordPress/Classes/Services/NotificationsService.swift
@@ -0,0 +1,15 @@
+//
+//  NotificationsService.swift
+//  WordPress
+//
+//  Created by Jorge Leandro Perez on 6/17/15.
+//  Copyright (c) 2015 WordPress. All rights reserved.
+//
+
+import Foundation
+
+
+public class NotificationsService
+{
+    
+}
