1c63c2becbd4dc68ead276f03d1abbf4cd5be9ff
Will Kwon <will@willkwon.com>
2015-06-18 11:15:32 -0700

    Adding comments

--- WordPress/Classes/ViewRelated/System/Search/WPSearchControllerConfigurator.h
+++ WordPress/Classes/ViewRelated/System/Search/WPSearchControllerConfigurator.h
@@ -1,6 +1,13 @@
 #import <Foundation/Foundation.h>
 #import "WPSearchController.h"
 
+/**
+ WPSeachControllerConfigurator is a worker class that does the work of
+ configuring the SearchController and SearchBar properties that multiple
+ view controllers have. Once WPSearchController is deprecated (iOS 7 support)
+ is removed, searchController must be an instance of UISearchController
+ */
+
 extern const CGFloat SearchBarWidth;
 extern const CGFloat SearchBariPadWidth;
 extern const CGFloat SearchWrapperViewPortraitHeight;
@@ -12,6 +19,8 @@ extern const NSTimeInterval SearchBarAnimationDuration;
 - (instancetype)init __attribute__((unavailable("Must call initWithSearchController")));
 - (instancetype)initWithSearchController:(WPSearchController *)searchController
                    withSearchWrapperView:(UIView *)searchWrapperView;
+
+// Sets properties of the searchController, searchBar, and searchWrapperView
 - (void)configureSearchControllerBarAndWrapperView;
 
 @end
