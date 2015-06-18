4310343062dd4618ca67996ea4054f7d9bf90f53
Will Kwon <will@willkwon.com>
2015-06-18 10:42:03 -0700

    Returning instancetype

--- WordPress/Classes/ViewRelated/Blog/BlogListViewController.m
+++ WordPress/Classes/ViewRelated/Blog/BlogListViewController.m
@@ -48,7 +48,7 @@ static CGFloat const BLVCSectionHeaderHeightForIPad = 40.0;
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
-- (id)init
+- (instancetype)init
 {
     self = [super init];
     if (self) {
