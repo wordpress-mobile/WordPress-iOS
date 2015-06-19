d2d6591eadb2da6bac9e35dbfce3b44e35d939ed
Jorge Leandro Perez <jorge@lantean.co>
2015-06-19 15:31:43 -0300

    Documents RemoteNotificationSettings Parser

--- WordPress/Classes/Networking/RemoteNotificationsSettings.swift
+++ WordPress/Classes/Networking/RemoteNotificationsSettings.swift
@@ -1,25 +1,39 @@
 import Foundation
 
 
+/**
+*  @class           RemoteNotificationsSettings
+*  @brief           The goal of this class is to parse Notification Settings data from the backend, and structure
+*                   it into a flat hierarchy, for easy access / mapping.
+*/
+
 public class RemoteNotificationsSettings
 {
     let sites               : [Site]
     let other               : [Other]
     let wpcom               : WordPressCom?
     
-    init?(rawSettings : NSDictionary?) {
-        sites               = Site.parseSites(rawSettings?.arrayForKey("sites") as? [NSDictionary])
-        other               = Other.parseOther(rawSettings?.dictionaryForKey("other"))
-        wpcom               = WordPressCom(rawWordPressCom: rawSettings?.dictionaryForKey("wpcom"))
+    init?(dictionary : NSDictionary?) {
+        sites               = Site.parseSites(dictionary?.arrayForKey("sites") as? [NSDictionary])
+        other               = Other.parseOther(dictionary?.dictionaryForKey("other"))
+        wpcom               = WordPressCom(dictionary: dictionary?.dictionaryForKey("wpcom"))
         
-        if rawSettings == nil {
+        if dictionary == nil {
             return nil
         }
     }
     
 
-    // MARK: - Nested Enums
-    public enum Kind : String {
+    /**
+    *  @enum        StreamKind
+    *  @brief       Each WordPress.com site may contain a different set of notification preferences, depending on
+    *               the Stream Kind:
+    *               -   WordPress.com Timeline
+    *               -   Emails
+    *               -   Push Notifications
+    *
+    */
+    public enum StreamKind : String {
         case Timeline       = "timeline"
         case Email          = "email"
         case Device         = "device"
@@ -28,11 +42,14 @@ public class RemoteNotificationsSettings
     }
     
     
-    // MARK: - Nested Class'ess
+    /**
+    *  @class       Site
+    *  @brief       This nested class represents the Notification Settings, for a given Site, in a specific stream.
+    */
     public class Site
     {
         let siteId          : Int
-        let kind            : Kind
+        let streamKind      : StreamKind
         let newComment      : Bool
         let commentLike     : Bool
         let postLike        : Bool
@@ -40,9 +57,9 @@ public class RemoteNotificationsSettings
         let achievement     : Bool
         let mentions        : Bool
         
-        init?(siteId theSiteId: Int?, kind theKind: Kind, dict rawSite: NSDictionary?) {
+        init?(siteId theSiteId: Int?, streamKind theStreamKind: StreamKind, dict rawSite: NSDictionary?) {
             siteId          = theSiteId                                         ?? Int.max
-            kind            = theKind
+            streamKind      = theStreamKind
             newComment      = rawSite?.numberForKey("new-comment")?.boolValue   ?? false
             commentLike     = rawSite?.numberForKey("comment-like")?.boolValue  ?? false
             postLike        = rawSite?.numberForKey("post-like")?.boolValue     ?? false
@@ -64,8 +81,8 @@ public class RemoteNotificationsSettings
             for rawSite in rawSites! {
                 let siteId = rawSite.numberForKey("site_id") as? Int
                 
-                for kind in Kind.allValues {
-                    if let site = Site(siteId: siteId, kind: kind, dict: rawSite.dictionaryForKey(kind.rawValue)) {
+                for streamKind in StreamKind.allValues {
+                    if let site = Site(siteId: siteId, streamKind: streamKind, dict: rawSite.dictionaryForKey(streamKind.rawValue)) {
                         parsed.append(site)
                     }
                 }
@@ -75,14 +92,20 @@ public class RemoteNotificationsSettings
         }
     }
     
+    
+    /**
+    *  @class       Other
+    *  @brief       This nested class represents the Notification Settings for "Other Sites" (AKA 3rd party blogs), 
+    *               in a specific stream.
+    */
     public class Other
     {
-        let kind            : Kind
+        let streamKind      : StreamKind
         let commentLike     : Bool
         let commentReply    : Bool
         
-        init?(kind theKind: Kind, rawOther : NSDictionary?) {
-            kind            = theKind
+        init?(streamKind theStreamKind: StreamKind, rawOther : NSDictionary?) {
+            streamKind      = theStreamKind
             commentLike     = rawOther?.numberForKey("comment-like")?.boolValue  ?? false
             commentReply    = rawOther?.numberForKey("comment-reply")?.boolValue ?? false
             
@@ -97,8 +120,8 @@ public class RemoteNotificationsSettings
                 return parsed
             }
             
-            for kind in Kind.allValues {
-                if let other = Other(kind: kind, rawOther: rawOther?.dictionaryForKey(kind.rawValue)) {
+            for streamKind in StreamKind.allValues {
+                if let other = Other(streamKind: streamKind, rawOther: rawOther?.dictionaryForKey(streamKind.rawValue)) {
                     parsed.append(other)
                 }
             }
@@ -107,6 +130,12 @@ public class RemoteNotificationsSettings
         }
     }
     
+    
+    /**
+    *  @class       WordPressCom
+    *  @brief       This nested class represents the Notification Settings for WordPress.com. This is not 
+    *               associated to a specific site.
+    */
     public class WordPressCom
     {
         let news            : Bool
@@ -114,13 +143,13 @@ public class RemoteNotificationsSettings
         let promotion       : Bool
         let digest          : Bool
         
-        init?(rawWordPressCom : NSDictionary?) {
-            news            = rawWordPressCom?.numberForKey("news")?.boolValue              ?? false
-            recommendations = rawWordPressCom?.numberForKey("recommendation")?.boolValue    ?? false
-            promotion       = rawWordPressCom?.numberForKey("promotion")?.boolValue         ?? false
-            digest          = rawWordPressCom?.numberForKey("digest")?.boolValue            ?? false
+        init?(dictionary : NSDictionary?) {
+            news            = dictionary?.numberForKey("news")?.boolValue              ?? false
+            recommendations = dictionary?.numberForKey("recommendation")?.boolValue    ?? false
+            promotion       = dictionary?.numberForKey("promotion")?.boolValue         ?? false
+            digest          = dictionary?.numberForKey("digest")?.boolValue            ?? false
             
-            if rawWordPressCom == nil {
+            if dictionary == nil {
                 return nil
             }
         }
