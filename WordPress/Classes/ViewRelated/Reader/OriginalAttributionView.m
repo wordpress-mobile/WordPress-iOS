3c5eb130f246e18263b79288b819cdee08bf6c4d
aerych <aerychj@gmail.com>
2015-06-22 07:24:03 -0500

    Stubs new attribution view.

---
+++ WordPress/Classes/ViewRelated/Reader/OriginalAttributionView.m
@@ -0,0 +1,10 @@
+#import "OriginalAttributionView.h"
+
+@interface OriginalAttributionView()
+@property (nonatomic, weak, readwrite) IBOutlet UIImageView *imageView;
+@property (nonatomic, weak, readwrite) IBOutlet UILabel *label;
+@end
+
+@implementation OriginalAttributionView
+
+@end
