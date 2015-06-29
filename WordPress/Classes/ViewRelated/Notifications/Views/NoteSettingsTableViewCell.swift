87347e7a7e515bb379a6aa2748e4ed4e4b63bde7
Jorge Leandro Perez <jorge@lantean.co>
2015-06-29 16:28:30 -0300

    Implements NoteSettingsTableViewCell

--- WordPress/Classes/ViewRelated/Notifications/Views/NoteSettingsTableViewCell.swift
+++ WordPress/Classes/ViewRelated/Notifications/Views/NoteSettingsTableViewCell.swift
@@ -1,9 +1,7 @@
-//
-//  NoteSettingsTableVIewCell.swift
-//  WordPress
-//
-//  Created by Jorge Leandro Perez on 6/29/15.
-//  Copyright (c) 2015 WordPress. All rights reserved.
-//
-
 import Foundation
+
+
+public class NoteSettingsTableViewCell : UITableViewCell
+{
+    @IBOutlet private var flipSwitch : UISwitch!
+}
