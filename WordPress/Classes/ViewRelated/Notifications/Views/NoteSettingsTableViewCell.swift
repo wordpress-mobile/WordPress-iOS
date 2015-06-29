363bcbd57f82021d045092f8450a7cd94e2f0408
Jorge Leandro Perez <jorge@lantean.co>
2015-06-29 16:28:02 -0300

    Updates Notifications Storyboard

---
+++ WordPress/Classes/ViewRelated/Notifications/Views/NoteSettingsTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  NoteSettingsTableVIewCell.swift
+//  WordPress
+//
+//  Created by Jorge Leandro Perez on 6/29/15.
+//  Copyright (c) 2015 WordPress. All rights reserved.
+//
+
+import Foundation
