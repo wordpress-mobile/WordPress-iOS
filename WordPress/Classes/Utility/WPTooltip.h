bd34711d3a0aa50b0ce76f9300e5cc709295f547
Matt Bumgardner <matt@bumgardner.org>
2015-07-07 11:33:39 -0500

    Added new tooltip for formatbar onboarding

--- WordPress/Classes/Utility/WPTooltip.h
+++ WordPress/Classes/Utility/WPTooltip.h
@@ -1,5 +1,14 @@
 #import <Foundation/Foundation.h>
 
+
+typedef NS_ENUM(NSInteger, WPTooltipDirection) {
+    WPTooltipDirectionUp,
+    WPTooltipDirectionDown,
+    WPTooltipDirectionLeft,
+    WPTooltipDirectionRight,
+    WPTooltipDirectionNone
+};
+
 /**
  *  @class      WPTooltip
  *  @brief      This class allows the implementer to display a simple, animated popover.
@@ -12,10 +21,11 @@
 
 /**
  *  @brief      Shows the tooltip.
- *  @param      view    The view which contains the tooltip.
- *  @param      frame   The frame to animate out of.
- *  @param      text    The text to display.
+ *  @param      view        The view which contains the tooltip.
+ *  @param      frame       The frame to animate out of.
+ *  @param      text        The text to display.
+ *  @param      direction   The direction of the popover.
  */
-+ (void)displayToolTipInView:(UIView *)view fromFrame:(CGRect)frame withText:(NSString *)text;
++ (void)displayToolTipInView:(UIView *)view fromFrame:(CGRect)frame withText:(NSString *)text direction:(WPTooltipDirection)direction;
 
 @end
