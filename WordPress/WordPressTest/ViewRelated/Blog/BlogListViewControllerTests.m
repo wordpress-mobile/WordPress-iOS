a48a6ac97378105fd6de0c585ab3f87480f34478
Will Kwon <will@willkwon.com>
2015-07-06 23:45:38 -0700

    Tests for making sure the searchButton is disabled/enabled when toggling editMode

--- WordPress/WordPressTest/ViewRelated/Blog/BlogListViewControllerTests.m
+++ WordPress/WordPressTest/ViewRelated/Blog/BlogListViewControllerTests.m
@@ -1,40 +1,40 @@
-//
-//  BlogListViewControllerTests.m
-//  WordPress
-//
-//  Created by Will Kwon on 7/6/15.
-//  Copyright (c) 2015 WordPress. All rights reserved.
-//
+@protocol NSFetchedResultsControllerDelegate <NSObject>
+
+@end
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import "BlogListViewController.h"
 
 @interface BlogListViewControllerTests : XCTestCase
+
+@property (nonatomic, strong) BlogListViewController *blogListViewController;
 
 @end
 
 @implementation BlogListViewControllerTests
 
-- (void)setUp {
+- (void)setUp
+{
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.blogListViewController = [[BlogListViewController alloc] init];
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+- (void)tearDown
+{
+    self.blogListViewController = nil;
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
+- (void)testSetEditingTrueDisablesSearchButton
+{
+    [self.blogListViewController setEditing:YES];
+    XCTAssertFalse(self.blogListViewController.navigationItem.rightBarButtonItem.enabled);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testSetEditingFalseEnablesSearchButton
+{
+    [self.blogListViewController setEditing:NO];
+    XCTAssertTrue(self.blogListViewController.navigationItem.rightBarButtonItem.enabled);
 }
-
 @end
