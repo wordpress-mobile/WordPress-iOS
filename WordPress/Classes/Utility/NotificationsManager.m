ffc2c8a4e96a5950ad82710113449734815def58
Jorge Leandro Perez <jorge@lantean.co>
2015-07-08 17:14:23 -0300

    NotificationsManager: Nukes legacy NotificationSettings Helpers

--- WordPress/Classes/Utility/NotificationsManager.m
+++ WordPress/Classes/Utility/NotificationsManager.m
@@ -30,7 +30,6 @@ NSString *const NotificationsManagerDidRegisterDeviceToken          = @"Notifica
 NSString *const NotificationsManagerDidUnregisterDeviceToken        = @"NotificationsManagerDidUnregisterDeviceToken";
 
 static NSString *const NotificationsDeviceIdKey                     = @"notification_device_id";
-static NSString *const NotificationsPreferencesKey                  = @"notification_preferences";
 static NSString *const NotificationsDeviceToken                     = @"apnsDeviceToken";
 
 // These correspond to the 'category' data WP.com will send with a push notification
@@ -130,7 +129,6 @@ static NSString *const NotificationActionCommentApprove             = @"COMMENT_
         NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
         [defaults removeObjectForKey:NotificationsDeviceToken];
         [defaults removeObjectForKey:NotificationsDeviceIdKey];
-        [defaults removeObjectForKey:NotificationsPreferencesKey];
         [defaults synchronize];
         
         NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
@@ -279,108 +277,6 @@ static NSString *const NotificationActionCommentApprove             = @"COMMENT_
 
 #pragma mark - WordPress.com XML RPC API
 
-+ (NSDictionary *)notificationSettingsDictionary
-{
-    NSManagedObjectContext *context = [[ContextManager sharedInstance] mainContext];
-    AccountService *accountService  = [[AccountService alloc] initWithManagedObjectContext:context];
-    WPAccount *defaultAccount       = [accountService defaultWordPressComAccount];
-
-    if (![[defaultAccount restApi] hasCredentials]) {
-        return nil;
-    }
-
-    NSDictionary *notificationPreferences = [[NSUserDefaults standardUserDefaults] objectForKey:NotificationsPreferencesKey];
-    if (!notificationPreferences) {
-        return nil;
-    }
-
-    NSMutableArray *notificationPrefArray = [[notificationPreferences allKeys] mutableCopy];
-    if ([notificationPrefArray indexOfObject:@"muted_blogs"] != NSNotFound) {
-        [notificationPrefArray removeObjectAtIndex:[notificationPrefArray indexOfObject:@"muted_blogs"]];
-    }
-
-    // Build the dictionary to send in the API call
-    NSMutableDictionary *updatedSettings = [[NSMutableDictionary alloc] init];
-    for (int i = 0; i < [notificationPrefArray count]; i++) {
-        NSDictionary *updatedSetting = [notificationPreferences objectForKey:[notificationPrefArray objectAtIndex:i]];
-        [updatedSettings setValue:[updatedSetting objectForKey:@"value"] forKey:[notificationPrefArray objectAtIndex:i]];
-    }
-
-    //Check and send 'mute_until' value
-    NSMutableDictionary *muteDictionary = [notificationPreferences objectForKey:@"mute_until"];
-    if (muteDictionary != nil  && [muteDictionary objectForKey:@"value"] != nil) {
-        [updatedSettings setValue:[muteDictionary objectForKey:@"value"] forKey:@"mute_until"];
-    } else {
-        [updatedSettings setValue:@"0" forKey:@"mute_until"];
-    }
-
-    NSArray *blogsArray = [[notificationPreferences objectForKey:@"muted_blogs"] objectForKey:@"value"];
-    NSMutableArray *mutedBlogsArray = [[NSMutableArray alloc] init];
-    for (int i=0; i < [blogsArray count]; i++) {
-        NSDictionary *userBlog = [blogsArray objectAtIndex:i];
-        if ([[userBlog objectForKey:@"value"] intValue] == 1) {
-            [mutedBlogsArray addObject:userBlog];
-        }
-    }
-
-    if ([mutedBlogsArray count] > 0) {
-        [updatedSettings setValue:mutedBlogsArray forKey:@"muted_blogs"];
-    }
-
-    if ([updatedSettings count] == 0) {
-        return nil;
-    }
-
-    return updatedSettings;
-}
-
-+ (void)saveNotificationSettings
-{
-    NSDictionary *settings          = [NotificationsManager notificationSettingsDictionary];
-    if (!settings) {
-        DDLogError(@"%@ %@ returning early because of blank notifications setting dictionary", self, NSStringFromSelector(_cmd));
-        return;
-    }
-    
-    NSString *deviceId              = [[NSUserDefaults standardUserDefaults] stringForKey:NotificationsDeviceIdKey];
-    NSManagedObjectContext *context = [[ContextManager sharedInstance] mainContext];
-    AccountService *accountService  = [[AccountService alloc] initWithManagedObjectContext:context];
-    WPAccount *defaultAccount       = [accountService defaultWordPressComAccount];
-
-    [[defaultAccount restApi] saveNotificationSettings:settings
-                                              deviceId:deviceId
-                                               success:^{
-                                                   DDLogInfo(@"Notification settings successfully sent to WP.com\n Settings: %@", settings);
-                                               } failure:^(NSError *error){
-                                                   DDLogError(@"Failed to update notification settings on WP.com %@", error.localizedDescription);
-                                               }];
-}
-
-+ (void)fetchNotificationSettingsWithSuccess:(void (^)())success failure:(void (^)(NSError *))failure
-{
-    NSString *deviceId              = [[NSUserDefaults standardUserDefaults] stringForKey:NotificationsDeviceIdKey];
-    NSManagedObjectContext *context = [[ContextManager sharedInstance] mainContext];
-    AccountService *accountService  = [[AccountService alloc] initWithManagedObjectContext:context];
-    WPAccount *defaultAccount       = [accountService defaultWordPressComAccount];
-    
-    [[defaultAccount restApi] fetchNotificationSettingsWithDeviceId:deviceId
-                                                            success:^(NSDictionary *settings) {
-                                                                NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-                                                                [defaults setObject:settings forKey:NotificationsPreferencesKey];
-                                                                [defaults synchronize];
-                                                                
-                                                                DDLogInfo(@"Received notification settings %@", settings);
-                                                                if (success) {
-                                                                    success();
-                                                                }
-                                                            } failure:^(NSError *error) {
-                                                                DDLogError(@"Failed to fetch notification settings %@ with device ID %@", error, deviceId);
-                                                                if (failure) {
-                                                                    failure(error);
-                                                                }
-                                                            }];
-}
-
 + (void)syncPushNotificationInfo
 {
     NSString *token                 = [[NSUserDefaults standardUserDefaults] objectForKey:NotificationsDeviceToken];
@@ -394,7 +290,6 @@ static NSString *const NotificationActionCommentApprove             = @"COMMENT_
 
                                              NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
                                              [defaults setObject:deviceId forKey:NotificationsDeviceIdKey];
-                                             [defaults setObject:settings forKey:NotificationsPreferencesKey];
                                              [defaults synchronize];
                                          } failure:^(NSError *error) {
                                              DDLogError(@"Failed to receive supported notification list: %@", error);
