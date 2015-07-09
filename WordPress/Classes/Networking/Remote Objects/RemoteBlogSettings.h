e75c69868bcbcf301d26410ba6c831ca47999b55
Sérgio Estêvão <sergio.estevao@automattic.com>
2015-07-09 10:24:22 +0100

    Remove unused fields.

--- WordPress/Classes/Networking/Remote Objects/RemoteBlogSettings.h
+++ WordPress/Classes/Networking/Remote Objects/RemoteBlogSettings.h
@@ -3,13 +3,7 @@
 @interface RemoteBlogSettings : NSObject
 
 // General
-@property (copy) NSString *ID;
 @property (copy) NSString *name;
 @property (copy) NSString *desc;
-@property (copy) NSNumber *blogPublic;
-@property (copy) NSString *language;
-@property (assign) BOOL relatedPosts;
-@property (assign) BOOL relatedPostsShowHeadline;
-@property (assign) BOOL relatedPostsShowThumbnails;
 
 @end
