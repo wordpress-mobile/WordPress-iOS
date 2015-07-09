44a6ff0addc3bdeb538f57fb817e9feb8ee738d1
Jorge Leandro Perez <jorge@lantean.co>
2015-07-09 16:43:44 -0300

    Updates Swift Bridging Header

--- WordPress/Classes/System/WordPress-Bridging-Header.h
+++ WordPress/Classes/System/WordPress-Bridging-Header.h
@@ -6,13 +6,18 @@
 #import "Notification.h"
 #import "Notification+Internals.h"
 
+#import "Constants.h"
+
 #import "DDLogSwift.h"
 #import "NSObject+Helpers.h"
 #import "NSAttributedString+Util.h"
+#import "NSBundle+VersionNumberHelper.h"
 #import "NSDate+StringFormatting.h"
 #import "NSURL+Util.h"
 #import "UIAlertView+Blocks.h"
 #import "UIAlertViewProxy.h"
+#import "UIImageView+Gravatar.h"
+#import "UIImage+Tint.h"
 
 #import "ContextManager.h"
 
@@ -21,15 +26,17 @@
 
 #import "WordPressComApi.h"
 
+#import "SuggestionsTableView.h"
 #import "WPStyleGuide.h"
 #import "WPFontManager.h"
 #import "WPRichTextView.h"
 #import "WPTableViewCell.h"
 #import "WPTableViewSectionHeaderView.h"
-#import "UIImageView+Gravatar.h"
-
-#import "SuggestionsTableView.h"
+#import "WPWebViewController.h"
 #import "WPAnalyticsTrackerWPCom.h"
+
+#import "ABXAppStore.h"
+#import "AppRatingUtility.h"
 
 #import <DTCoreText/DTCoreText.h>
 #import <SFHFKeychainUtils.h>
