29989f5c9382f48b8e03c983f54b3ce23b9936b9
aerych <aerychj@gmail.com>
2015-07-09 09:53:15 -0500

    Updates to support different styles for site or post attribution.

--- WordPress/Classes/ViewRelated/Reader/OriginalAttributionView.h
+++ WordPress/Classes/ViewRelated/Reader/OriginalAttributionView.h
@@ -1,8 +1,36 @@
 #import <UIKit/UIKit.h>
 
+@protocol OriginalAttributionViewDelegate;
+
 @interface OriginalAttributionView : UIView
 
-@property (nonatomic, weak, readonly) IBOutlet UIImageView *imageView;
-@property (nonatomic, weak, readonly) IBOutlet UILabel *label;
+@property (nonatomic, weak) id<OriginalAttributionViewDelegate>delegate;
 
+/**
+ Resets the view. 
+ Clears the avatar image and the text shown.
+ */
+- (void)reset;
+
+/**
+ Sets the author and blog information for the Editor Pick attribution style
+ */
+- (void)setPostAttributionWithGravatar:(NSURL *)avatarURL
+                             forAuthor:(NSString *)authorName
+                             authorURL:(NSURL *)authorURL
+                                  blog:(NSString *)blogName
+                               blogURL:(NSURL *)blogURL;
+
+/**
+ Sets the blog information for the Site Pick attribution style
+ */
+- (void)setSiteAttributionWithBlavatar:(NSURL *)blavatarURL
+                               forBlog:(NSString *)blogName
+                               blogURL:(NSURL *)blogURL;
+@end
+
+@protocol OriginalAttributionViewDelegate <NSObject>
+@optional
+- (void)originalAttributionView:(OriginalAttributionView *)view
+              didTapLink:(NSURL *)link;
 @end
