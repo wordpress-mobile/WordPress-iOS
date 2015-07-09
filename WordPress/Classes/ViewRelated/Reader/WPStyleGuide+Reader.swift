0102610f4dc56b524a38932cb541e327c3cae8c3
aerych <aerychj@gmail.com>
2015-07-09 09:54:55 -0500

    Adds reader specific style sheet and styles for original attribution.

--- WordPress/Classes/ViewRelated/Reader/WPStyleGuide+Reader.swift
+++ WordPress/Classes/ViewRelated/Reader/WPStyleGuide+Reader.swift
@@ -1,9 +1,31 @@
-//
-//  WPStyleGuide+Reader.swift
-//  WordPress
-//
-//  Created by aerych on 7/8/15.
-//  Copyright (c) 2015 WordPress. All rights reserved.
-//
-
 import Foundation
+
+
+extension WPStyleGuide
+{
+
+    // MARK: Original Post/Site Attribution Styles. 
+
+    public class func originalAttributionParagraphAttributes() -> NSDictionary {
+        let fontSize:CGFloat = UIDevice.isPad() ? 16.0 : 14.0
+        let font = WPFontManager.openSansRegularFontOfSize(fontSize)
+
+        let lineHeight:CGFloat = UIDevice.isPad() ? 24.0 : 21.0
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.minimumLineHeight = lineHeight
+        paragraphStyle.maximumLineHeight = lineHeight
+        return [
+            NSParagraphStyleAttributeName : paragraphStyle,
+            NSFontAttributeName : font,
+            NSForegroundColorAttributeName: WPStyleGuide.grey(),
+        ] as NSDictionary
+    }
+
+    public class func originalAttributionLinkAttributes(linkURL:NSURL) -> NSDictionary {
+        return [
+            NSForegroundColorAttributeName : WPStyleGuide.mediumBlue(),
+            NSLinkAttributeName: linkURL,
+        ] as NSDictionary
+    }
+
+}
