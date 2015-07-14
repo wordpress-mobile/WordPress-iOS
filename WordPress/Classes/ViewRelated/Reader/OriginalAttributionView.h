b9045bb6b536cca9a1a1799b346ceb5db09df46e
aerych <aerychj@gmail.com>
2015-07-14 13:19:15 -0500

    OriginalAttributionView implements DiscoverPostAttributionView protocol.

--- WordPress/Classes/ViewRelated/Reader/OriginalAttributionView.h
+++ WordPress/Classes/ViewRelated/Reader/OriginalAttributionView.h
@@ -1,23 +1,5 @@
 #import <UIKit/UIKit.h>
+#import "DiscoverPostAttributionView.h"
 
-@interface OriginalAttributionView : UIView
-
-/**
- Resets the view. 
- Clears the avatar image and the text shown.
- */
-- (void)reset;
-
-/**
- Sets the author and blog information for the Editor Pick attribution style
- */
-- (void)setPostAttributionWithGravatar:(NSURL *)avatarURL
-                             forAuthor:(NSString *)authorName
-                                  blog:(NSString *)blogName;
-
-/**
- Sets the blog information for the Site Pick attribution style
- */
-- (void)setSiteAttributionWithBlavatar:(NSURL *)blavatarURL
-                               forBlog:(NSString *)blogName;
+@interface OriginalAttributionView : UIView <DiscoverPostAttributionView>
 @end
