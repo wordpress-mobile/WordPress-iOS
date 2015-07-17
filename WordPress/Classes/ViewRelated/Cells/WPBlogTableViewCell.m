d155a2ca6ca994f358268751b4e10d920ef7d692
Jorge Bernal <jorge@automattic.com>
2015-07-17 09:43:53 +0200

    Refactor Blog list cells
    Moved the creation of the visibility switch to the cell, and used
    `editingAccessoryView` instead of constantly changing the `accessoryView`.

--- WordPress/Classes/ViewRelated/Cells/WPBlogTableViewCell.m
+++ WordPress/Classes/ViewRelated/Cells/WPBlogTableViewCell.m
@@ -6,7 +6,20 @@
 {
     // Ignore the style argument, override with subtitle style.
     self = [super initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:reuseIdentifier];
+    if (self) {
+        [self setupCell];
+    }
     return self;
+}
+
+- (void)setupCell
+{
+    if (!self.visibilitySwitch) {
+        UISwitch *visibilitySwitch = [UISwitch new];
+
+        self.editingAccessoryView = visibilitySwitch;
+        self.visibilitySwitch = visibilitySwitch;
+    }
 }
 
 @end
