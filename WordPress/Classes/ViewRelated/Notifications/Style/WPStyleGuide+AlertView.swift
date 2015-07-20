a60507c00e0388873368dd0531415fdc58975827
Jorge Leandro Perez <jorge@lantean.co>
2015-07-20 17:56:03 -0300

    Implements AlertView Helper

---
+++ WordPress/Classes/ViewRelated/Notifications/Style/WPStyleGuide+AlertView.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+
+extension WPStyleGuide
+{
+    public struct AlertView
+    {
+        public static let titleFont     = WPFontManager.openSansLightFontOfSize(16)
+        public static let detailsFont   = WPFontManager.openSansLightFontOfSize(14)
+        
+        public static let titleColor    = UIColor(red: 0x87/255.0, green: 0xA6/255.0, blue: 0xBC/255.0, alpha: 0xFF/255.0)
+        public static let detailsColor  = UIColor(red: 0x46/255.0, green: 0x68/255.0, blue: 0x83/255.0, alpha: 0xFF/255.0)
+    }
+}
