1fc8b4aee4091573969641da559e6cdc4973ddcf
Jorge Leandro Perez <jorge@lantean.co>
2015-07-20 20:24:04 -0300

    Implements AlertInternalView

--- WordPress/Classes/ViewRelated/Notifications/Alerts/AlertView.swift
+++ WordPress/Classes/ViewRelated/Notifications/Alerts/AlertView.swift
@@ -5,6 +5,9 @@ public class AlertView : NSObject
 {
     public typealias Completion = (() -> ())
     
+    
+    
+    // MARK: - Initializers
     public init(title: String, message: String, button: String, completion: Completion?) {
         super.init()
         
@@ -12,43 +15,37 @@ public class AlertView : NSObject
         NSBundle.mainBundle().loadNibNamed(AlertView.classNameWithoutNamespaces(), owner: self, options: nil)
         
         // Check the Outlets
-        assert(backgroundView   != nil)
-        assert(alertView        != nil)
-        assert(titleLabel       != nil)
-        assert(descriptionLabel != nil)
+        assert(internalView != nil)
         
-        // Setup please!
-        alertView.layer.cornerRadius    = cornerRadius
-        titleLabel.font                 = Styles.titleFont
-        descriptionLabel.font           = Styles.detailsFont
-        
-        titleLabel.textColor            = Styles.titleColor
-        descriptionLabel.textColor      = Styles.detailsColor
-        
-        titleLabel.text                 = title
-        descriptionLabel.text           = message
+        internalView.titleLabel.text        = title
+        internalView.descriptionLabel.text  = message
 
-        dismissButton.titleLabel?.font  = Styles.buttonFont
-// TODO
-//  1. Completion Callback
-//  2. Document please
-//  3. Who retains / releases this view?
-//  4. Semi-bold the key words in the numbered steps
+// TODO: Semi-bold the key words in the numbered steps
     }
     
+    
+    
+    // MARK: - Public Methods
     public func show() {
         let targetView = keyView()
 
         // Attach the BackgroundView
         targetView.endEditing(true)
-        targetView.addSubview(backgroundView)
+        targetView.addSubview(internalView)
         
         // We should cover everything
-        backgroundView.setTranslatesAutoresizingMaskIntoConstraints(false)
-        targetView.pinSubviewToAllEdges(backgroundView)
+        internalView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        targetView.pinSubviewToAllEdges(internalView)
         
         // Animate!
-        backgroundView.fadeInWithAnimation()
+        internalView.fadeInWithAnimation()
+        
+        // Note:
+        // The internalView will retain the AlertView itself. After it's dismissed, everything will
+        // be automatically cleaned up!
+        internalView?.onClick = {
+            self.dismiss()
+        }
     }
     
     
@@ -57,33 +54,24 @@ public class AlertView : NSObject
         return UIApplication.sharedApplication().keyWindow?.subviews.first as! UIView
     }
     
-    @IBAction private func buttonWasPressed(sender: AnyObject!) {
+    private func dismiss() {
         let animationDuration = NSTimeInterval(0.3)
         let finalAlpha = CGFloat(0)
         
         UIView.animateWithDuration(animationDuration, animations: { () -> Void in
-                self.backgroundView.alpha = finalAlpha
+                self.internalView.alpha = finalAlpha
             },
             completion: { (success: Bool) -> Void in
-                self.backgroundView.removeFromSuperview()
+                self.internalView.removeFromSuperview()
                 self.onCompletion?()
             })
     }
     
-
-    // MARK: - Private Aliases
-    private typealias Styles = WPStyleGuide.AlertView
     
-    // MARK: - Private Constants
-    private let cornerRadius = CGFloat(7)
     
     // MARK: - Private Properties
     private var onCompletion : Completion?
     
     // MARK: - Private Outlets
-    @IBOutlet private var backgroundView    : UIView!
-    @IBOutlet private var alertView         : UIView!
-    @IBOutlet private var titleLabel        : UILabel!
-    @IBOutlet private var descriptionLabel  : UILabel!
-    @IBOutlet private var dismissButton     : UIButton!
+    @IBOutlet private var internalView : AlertInternalView!
 }
