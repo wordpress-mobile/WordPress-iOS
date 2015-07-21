c06aa66ef6991581fca34ff761074bf219d25b09
Jorge Leandro Perez <jorge@lantean.co>
2015-07-21 11:46:08 -0300

    NotificationSettingStreams: Push Notifications Alert Style

--- WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationSettingStreamsViewController.swift
+++ WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationSettingStreamsViewController.swift
@@ -123,10 +123,10 @@ public class NotificationSettingStreamsViewController : UITableViewController
         let title   = NSLocalizedString("Push Notifications have been turned off in iOS Settings",
                                         comment: "Displayed when Push Notifications are disabled (iOS 7)")
         let message = NSLocalizedString("To enable notifications:\n\n" +
-                                        "1. Open iOS Settings\n" +
-                                        "2. Tap Notifications\n" +
-                                        "3. Select WordPress\n" +
-                                        "4. Turn on Allow Notifications",
+                                        "1. Open **iOS Settings**\n" +
+                                        "2. Tap **Notifications**\n" +
+                                        "3. Select **WordPress**\n" +
+                                        "4. Turn on **Allow Notifications**",
                                         comment: "Displayed when Push Notifications are disabled (iOS 7)")
         let button = NSLocalizedString("Dismiss", comment: "Dismiss the AlertView")
         
