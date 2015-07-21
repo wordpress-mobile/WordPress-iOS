88563b805ece6d0f3b6b01d1e82c583391af1944
Jorge Leandro Perez <jorge@lantean.co>
2015-07-21 14:31:35 -0300

    NotificationSettings: Documenting Class'ess

--- WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationSettingStreamsViewController.swift
+++ WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationSettingStreamsViewController.swift
@@ -1,6 +1,14 @@
 import Foundation
 
 
+/**
+*  @class           NotificationSettingStreamsViewController
+*  @brief           This class will simply render the collection of Streams available for a given
+*                   NotificationSettings collection.
+*                   A Stream represents a possible way in which notifications are communicated.
+*                   For instance: Push Notifications / WordPress.com Timeline / Email
+*/
+
 public class NotificationSettingStreamsViewController : UITableViewController
 {
     // MARK: - View Lifecycle
