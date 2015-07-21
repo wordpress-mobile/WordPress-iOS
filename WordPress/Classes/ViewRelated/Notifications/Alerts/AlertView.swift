b27c8dcce16d3e18c8249d094e355d2178f607fe
Jorge Leandro Perez <jorge@lantean.co>
2015-07-21 11:45:39 -0300

    AlertView: Adds Bold Markers support

--- WordPress/Classes/ViewRelated/Notifications/Alerts/AlertView.swift
+++ WordPress/Classes/ViewRelated/Notifications/Alerts/AlertView.swift
@@ -4,9 +4,7 @@ import Foundation
 public class AlertView : NSObject
 {
     public typealias Completion = (() -> ())
-    
-    
-    
+
     // MARK: - Initializers
     public init(title: String, message: String, button: String, completion: Completion?) {
         super.init()
@@ -16,11 +14,10 @@ public class AlertView : NSObject
         
         // Check the Outlets
         assert(internalView != nil)
-        
-        internalView.titleLabel.text        = title
-        internalView.descriptionLabel.text  = message
 
-// TODO: Semi-bold the key words in the numbered steps
+        // Done!
+        internalView.titleLabel.text = title
+        internalView.descriptionLabel.attributedText = applyMessageStyles(message)
     }
     
     
@@ -49,17 +46,15 @@ public class AlertView : NSObject
     }
     
     
+    
     // MARK: - Private Helpers
     private func keyView() -> UIView {
         return UIApplication.sharedApplication().keyWindow?.subviews.first as! UIView
     }
     
-    private func dismiss() {
-        let animationDuration = NSTimeInterval(0.3)
-        let finalAlpha = CGFloat(0)
-        
-        UIView.animateWithDuration(animationDuration, animations: { () -> Void in
-                self.internalView.alpha = finalAlpha
+    private func dismiss() {        
+        UIView.animateWithDuration(WPAnimationDurationFast, animations: { () -> Void in
+                self.internalView.alpha = WPAlphaZero
             },
             completion: { (success: Bool) -> Void in
                 self.internalView.removeFromSuperview()
@@ -68,6 +63,29 @@ public class AlertView : NSObject
     }
     
     
+    
+    // MARK: - Style Helpers
+    private func applyMessageStyles(message: String) -> NSAttributedString {
+        let attributedMessage = NSMutableAttributedString(string: message, attributes: Style.detailsRegularAttributes)
+        
+        // Apply Bold Styles
+        let boldPattern = "(\\*{1,2}).+?\\1"
+        attributedMessage.applyStylesToMatchesWithPattern(boldPattern, styles: Style.detailsBoldAttributes)
+        
+        // Replace the Bold Markers
+        let range = NSRange(location: 0, length: attributedMessage.length)
+        attributedMessage.mutableString.replaceOccurrencesOfString("**",
+            withString  : "",
+            options     : .CaseInsensitiveSearch,
+            range       : range)
+        
+        return attributedMessage
+    }
+    
+    
+    
+    // MARK: - Private Aliases
+    private typealias Style = WPStyleGuide.AlertView
     
     // MARK: - Private Properties
     private var onCompletion : Completion?
