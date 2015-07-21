422f9af844f03a78e9d45c28cb1f48cac9c81a6f
Jorge Leandro Perez <jorge@lantean.co>
2015-07-21 14:42:53 -0300

    AlertView: Adds Documentation

--- WordPress/Classes/ViewRelated/Notifications/Alerts/AlertView.swift
+++ WordPress/Classes/ViewRelated/Notifications/Alerts/AlertView.swift
@@ -1,11 +1,24 @@
 import Foundation
 
 
+/**
+*  @class           AlertView
+*  @brief           The purpose of this class is to render a simple AlertView, with a custom style,
+*                   and the following sections: [ Title, Message, Button ]
+*/
+
 public class AlertView : NSObject
 {
+    // MARK: - Public Aliases
     public typealias Completion = (() -> ())
 
-    // MARK: - Initializers
+    
+    /**
+    *  @details     Designated Initializer
+    *  @param       title       The title of the AlertView.
+    *  @param       message     Message string to be displayed. Note: Bold is supported, **markdown flavor**.
+    *  @param       completion  A closure to be executed right after the button is pressed.
+    */
     public init(title: String, message: String, button: String, completion: Completion?) {
         super.init()
         
@@ -22,7 +35,9 @@ public class AlertView : NSObject
     
     
     
-    // MARK: - Public Methods
+    /**
+    *  @details     Displays the AlertView in the window.
+    */
     public func show() {
         let targetView = keyView()
 
@@ -47,11 +62,19 @@ public class AlertView : NSObject
     
     
     
-    // MARK: - Private Helpers
+    /**
+    *  @details     Finds the root view attached to the window
+    *  @returns     The Key View.
+    */
     private func keyView() -> UIView {
         return UIApplication.sharedApplication().keyWindow?.subviews.first as! UIView
     }
     
+    
+    
+    /**
+    *  @details     Fades out the AlertView, and, on completion, will cleanup and hit the completion closure.
+    */
     private func dismiss() {        
         UIView.animateWithDuration(WPAnimationDurationFast, animations: { () -> Void in
                 self.internalView.alpha = WPAlphaZero
@@ -64,7 +87,11 @@ public class AlertView : NSObject
     
     
     
-    // MARK: - Style Helpers
+    /**
+    *  @details     Apples Bold Style over all of the text surrounded by **double stars** (and removes the markers!).
+    *  @param       message     The Message that should be stylized.
+    *  @returns                 The Message with Bold Substrings styled.
+    */
     private func applyMessageStyles(message: String) -> NSAttributedString {
         let attributedMessage = NSMutableAttributedString(string: message, attributes: Style.detailsRegularAttributes)
         
