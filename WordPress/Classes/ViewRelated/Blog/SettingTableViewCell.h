e3f86e5f670bd1e5d99f5b2e37c05b013646057b
Sérgio Estêvão <sergio.estevao@automattic.com>
2015-07-24 11:40:41 +0100

    Refactor setting cell type to avoid code repetition.

--- WordPress/Classes/ViewRelated/Blog/SettingTableViewCell.h
+++ WordPress/Classes/ViewRelated/Blog/SettingTableViewCell.h
@@ -2,4 +2,10 @@
 
 @interface SettingTableViewCell : WPTableViewCell
 
+- (instancetype)initWithLabel:(NSString *)label editable:(BOOL)editable reuseIdentifier:(NSString *)reuseIdentifier  NS_DESIGNATED_INITIALIZER;
+
+- (void)setTextValue:(NSString *)value;
+
+- (NSString *)textValue;
+
 @end
