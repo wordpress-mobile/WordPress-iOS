f797231d93e54865b0a9d71935210b49ea05ddc4
Sérgio Estêvão <sergio.estevao@automattic.com>
2015-07-24 11:40:41 +0100

    Refactor setting cell type to avoid code repetition.

--- WordPress/Classes/ViewRelated/Blog/SettingTableViewCell.m
+++ WordPress/Classes/ViewRelated/Blog/SettingTableViewCell.m
@@ -4,11 +4,34 @@
 
 - (instancetype) initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
+    return [self initWithLabel:@"" editable:YES reuseIdentifier:reuseIdentifier];
+}
+
+- (instancetype)initWithLabel:(NSString *)label editable:(BOOL)editable reuseIdentifier:(NSString *)reuseIdentifier
+{
     self = [super initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:reuseIdentifier];
     if (self) {
-        
+        self.textLabel.text = label;
+        [WPStyleGuide configureTableViewCell:self];
+        if (editable) {
+            self.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            self.selectionStyle = UITableViewCellSelectionStyleDefault;
+        } else {
+            self.accessoryType = UITableViewCellAccessoryNone;
+            self.selectionStyle = UITableViewCellSelectionStyleNone;
+        }
     }
     return self;
+}
+
+- (void)setTextValue:(NSString *)value
+{
+    self.detailTextLabel.text = value;
+}
+
+- (NSString *)textValue
+{
+    return self.detailTextLabel.text;
 }
 
 @end
