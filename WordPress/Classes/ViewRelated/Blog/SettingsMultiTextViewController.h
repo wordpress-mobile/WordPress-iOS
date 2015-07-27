27c30f5cba404a89175664e767c1254936f7c09f
Sérgio Estêvão <sergio.estevao@automattic.com>
2015-07-27 15:03:21 +0100

    Improve code layout.

--- WordPress/Classes/ViewRelated/Blog/SettingsMultiTextViewController.h
+++ WordPress/Classes/ViewRelated/Blog/SettingsMultiTextViewController.h
@@ -5,6 +5,9 @@
 @property (nonatomic, copy) void(^onValueChanged)(NSString *);
 @property (nonatomic, copy) void(^onCancel)();
 
-- (instancetype)initWithText:(NSString *)text placeholder:(NSString *)placeholder hint:(NSString *)hint isPassword:(BOOL)isPassword;
+- (instancetype)initWithText:(NSString *)text
+                 placeholder:(NSString *)placeholder
+                        hint:(NSString *)hint
+                  isPassword:(BOOL)isPassword;
 
 @end
