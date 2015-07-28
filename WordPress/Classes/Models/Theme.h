ac7646b90b4f743c9c992b66b1bd31f5dd056a1d
Diego Rey Mendez <diegoreymendez@gmail.com>
2015-07-28 12:15:45 +0200

    WIP: implementing CoreData support for Themes.

--- WordPress/Classes/Models/Theme.h
+++ WordPress/Classes/Models/Theme.h
@@ -20,6 +20,48 @@
 - (BOOL)isCurrentTheme;
 - (BOOL)isPremium;
 
+#pragma mark - Creating themes
+
+/**
+ *  @brief      Creates and initializes a new theme with the specified theme Id in the specified
+ *              context.
+ *
+ *  @param      themeId     The ID of the new theme.  Cannot be nil.
+ *  @param      context     The managed object context to use for the insert operation.  Cannot be
+ *                          nil.
+ *
+ *  @returns    The newly created and initialized object.
+ */
++ (Theme *)newThemeWithId:(NSString *)themeId
+   inManagedObjectContext:(NSManagedObjectContext *)context;
+
+#pragma mark - Finding existing themes
+
+/**
+ *  @brief      Obtains the theme with the specified ID if it exists.
+ *
+ *  @param      themeId     The ID of the theme to retrieve.  Cannot be nil.
+ *  @param      context     The managed object context to use for the find operation.  Cannot be
+ *                          nil.
+ *
+ *  @returns    The stored theme matching the specified ID if found, or nil if it's not found.
+ */
++ (Theme *)findThemeWithId:(NSString *)themeId
+    inManagedObjectContext:(NSManagedObjectContext *)context;
+
+/**
+ *  @brief      Obtains the theme with the specified ID if it exists, otherwise a new theme is
+ *              created and returned.
+ *
+ *  @param      themeId     The ID of the theme to retrieve.  Cannot be nil.
+ *  @param      context     The managed object context to use for the find operation.  Cannot be
+ *                          nil.
+ *
+ *  @returns    The stored theme matching the specified ID if found, or nil if it's not found.
+ */
++ (Theme *)findOrCreateThemeWithId:(NSString *)themeId
+            inManagedObjectContext:(NSManagedObjectContext *)context;
+
 @end
 
 @interface Theme (WordPressComApi)
