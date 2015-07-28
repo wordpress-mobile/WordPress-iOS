a571a05bcdde8b94705581fede77c409e4a901a5
Jorge Leandro Perez <jorge@lantean.co>
2015-07-28 13:46:00 -0300

    NotificationDetailsViewController: Implements DeletionRequest Mechanism

--- WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationDetailsViewController.h
+++ WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationDetailsViewController.h
@@ -3,16 +3,29 @@
 
 
 @class Notification;
-typedef void (^NotificationDetailsCallbackBlock)();
 
+typedef void (^NotificationDetailsDeletionActionBlock)();
+typedef void (^NotificationDetailsDeletionRequestBlock)(NotificationDetailsDeletionActionBlock onRequestTimeout);
+
+
+
+/**
+ *  @class      NotificationDetailsViewController
+ *  @brief      This class will render a given Notification entity, onscreen.
+ *  @details    Whenever the Notification is remotely updated, this class will automatically take care of
+ *              refreshing the User Interface for you.
+ */
 
 @interface NotificationDetailsViewController : UIViewController <UIViewControllerRestoration>
 
 /**
- *	@brief		This block will be called whenever a destructive action is performed over a Notification object.
+ *	@brief		Whenever the user performs a destructive action, the Deletion Request Callback will be called,
+ *              and a closure that will effectively perform the deletion action will be sent over.
+ *  @details    This mechanism has been implemented so that the presenter ViewController can allow the user to
+ *              undo the destructive action, before it's effectively executed.
  */
 
-@property (nonatomic, copy) NotificationDetailsCallbackBlock onDestructionCallback;
+@property (nonatomic, copy) NotificationDetailsDeletionRequestBlock onDeletionRequestCallback;
 
 /**
  *	@brief		This method renders the details view, for any given notification/
