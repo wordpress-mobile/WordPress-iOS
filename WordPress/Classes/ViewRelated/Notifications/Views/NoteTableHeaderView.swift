4ffd1091565c1c841529dcc97fa0876f2c0d8440
Jorge Leandro Perez <jorge@lantean.co>
2015-07-31 12:53:18 -0300

    Documenting NoteTableHeaderView

--- WordPress/Classes/ViewRelated/Notifications/Views/NoteTableHeaderView.swift
+++ WordPress/Classes/ViewRelated/Notifications/Views/NoteTableHeaderView.swift
@@ -1,6 +1,13 @@
 import Foundation
 
 
+
+/**
+*  @class      NoteTableHeaderView
+*  @brief      This class renders a view with top and bottom separators, meant to be used as UITableView
+*              section header in NotificationsViewController.
+*/
+
 @objc public class NoteTableHeaderView : UIView
 {
     // MARK: - Public Properties
