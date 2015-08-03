7e95f2a5a5c5943bfff30396404a6b05ec57d15c
Jorge Leandro Perez <jorge@lantean.co>
2015-08-03 13:28:23 -0300

    WPGUIConstants: Adds default row height constant

--- WordPress/Classes/System/WPGUIConstants.m
+++ WordPress/Classes/System/WPGUIConstants.m
@@ -11,6 +11,7 @@ const CGFloat WPAnimationDurationFast       = 0.15;
 const CGFloat WPAnimationDurationFaster     = 0.07;
 
 const CGFloat WPTableViewTopMargin          = 40.0;
+const CGFloat WPTableViewDefaultRowHeight   = 44.0;
 const CGRect WPTableHeaderPadFrame          = {0.0, 0.0, 0.0, 40.0};
 const CGRect WPTableFooterPadFrame          = {0.0, 0.0, 0.0, 48.0};
 const UIEdgeInsets WPTableViewContentInsets = {40.0, 0.0, 0.0, 0.0};
