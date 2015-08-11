e65c4d682d2e0b586bbdd7b132aa5407e5b3d7a4
Jorge Leandro Perez <jorge@lantean.co>
2015-08-11 16:27:34 -0300

    Implements WPStyleGuide+Comments

---
+++ WordPress/Classes/ViewRelated/Comments/WPStyleGuide+Comments.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+
+/**
+*  @class           WPStyleGuide+Comments
+*  @brief           This class groups all of the styles used by all of the CommentsViewController.
+*/
+
+extension WPStyleGuide
+{
+    public struct Comments
+    {
+        // MARK: - Public Properties
+        //
+        public static let titleRegularStyle         = [ NSParagraphStyleAttributeName:  titleParagraph,
+                                                        NSFontAttributeName:            titleRegularFont,
+                                                        NSForegroundColorAttributeName: titleTextColor ]
+        
+        public static let timestampFont             = WPStyleGuide.subtitleFont()
+        public static let timestampColor            = WPStyleGuide.allTAllShadeGrey()
+        public static let gravatarPlaceholderImage  = UIImage(named: "gravatar")
+        
+        
+        // MARK: - Private Properties Properties
+        //
+        private static let titleRegularFont         = WPFontManager.openSansRegularFontOfSize(14)
+        private static let titleBoldFont            = WPFontManager.openSansBoldFontOfSize(14)
+        
+        private static let titleTextColor           = WPStyleGuide.littleEddieGrey()
+        private static let titleLineSize            = CGFloat(18)
+        private static let titleParagraph           = NSMutableParagraphStyle(
+            minLineHeight: titleLineSize, maxLineHeight: titleLineSize, lineBreakMode: .ByWordWrapping, alignment: .Left
+        )
+    }
+}
