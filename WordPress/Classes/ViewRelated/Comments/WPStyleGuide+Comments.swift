849aaceee2915b4e69dc9c03bc8082ee92483f3f
Jorge Leandro Perez <jorge@lantean.co>
2015-08-13 16:33:12 -0300

    Updates WPStyleGuide Comment Styles

--- WordPress/Classes/ViewRelated/Comments/WPStyleGuide+Comments.swift
+++ WordPress/Classes/ViewRelated/Comments/WPStyleGuide+Comments.swift
@@ -12,28 +12,80 @@ extension WPStyleGuide
     {
         // MARK: - Public Properties
         //
-        public static let titleRegularStyle         = [ NSParagraphStyleAttributeName:  titleParagraph,
-                                                        NSFontAttributeName:            titleRegularFont,
-                                                        NSForegroundColorAttributeName: titleTextColor ]
+        public static let gravatarApprovedImage     = UIImage(named: "gravatar")!
+        public static let gravatarUnapprovedImage   = UIImage(named: "gravatar-unapproved")!
+        
+        public static func gravatarPlaceholderImage(isApproved approved: Bool) -> UIImage {
+            return approved ? gravatarApprovedImage : gravatarUnapprovedImage
+        }
+        
+        public static func detailsRegularStyle(isApproved approved: Bool) -> [String : AnyObject] {
+            let color = approved ? WPStyleGuide.littleEddieGrey() : alertYellowDark
+            
+            return  [   NSParagraphStyleAttributeName   : titleParagraph,
+                        NSFontAttributeName             : titleRegularFont,
+                        NSForegroundColorAttributeName  : color ]
+        }
 
-        public static let titleBoldStyle            = [ NSParagraphStyleAttributeName:  titleParagraph,
-                                                        NSFontAttributeName:            titleBoldFont,
-                                                        NSForegroundColorAttributeName: titleTextColor ]
+        public static func detailsRegularRedStyle(isApproved approved: Bool) -> [String : AnyObject] {
+            let color = approved ? WPStyleGuide.littleEddieGrey() : alertRedDarker
+            
+            return  [   NSParagraphStyleAttributeName   : titleParagraph,
+                        NSFontAttributeName             : titleRegularFont,
+                        NSForegroundColorAttributeName  : color ]
+        }
+
+        public static func detailsItalicsStyle(isApproved approved: Bool) -> [String : AnyObject] {
+            let color = approved ? WPStyleGuide.littleEddieGrey() : alertRedDarker
+            
+            return  [   NSParagraphStyleAttributeName   : titleParagraph,
+                        NSFontAttributeName             : titleItalicsFont,
+                        NSForegroundColorAttributeName  : color ]
+        }
         
-        public static let timestampFont             = WPStyleGuide.subtitleFont()
-        public static let timestampColor            = WPStyleGuide.allTAllShadeGrey()
-        public static let gravatarPlaceholderImage  = UIImage(named: "gravatar")
+        public static func detailsBoldStyle(isApproved approved: Bool) -> [String : AnyObject] {
+            let color = approved ? WPStyleGuide.littleEddieGrey() : alertRedDarker
+            
+            return  [   NSParagraphStyleAttributeName   : titleParagraph,
+                        NSFontAttributeName             : titleBoldFont,
+                        NSForegroundColorAttributeName  : color ]
+        }
         
+        public static func timestampStyle(isApproved approved: Bool) -> [String: AnyObject] {
+            let color = approved ? WPStyleGuide.allTAllShadeGrey() : alertYellowDark
+            
+            return  [   NSFontAttributeName             : timestampFont,
+                        NSForegroundColorAttributeName  : color ]
+        }
+        
+        public static func backgroundColor(isApproved approved: Bool) -> UIColor {
+            return approved ? UIColor.whiteColor() : alertYellowLighter
+        }
+        
+        public static func timestampImage(isApproved approved: Bool) -> UIImage {
+            let timestampImage = UIImage(named: "reader-postaction-time")!
+            return approved ? timestampImage : timestampImage.imageTintedWithColor(alertYellowDark)
+        }
+        
+        
+        // MARK: - New Colors
+        //
+        public static let alertYellowDark       = UIColor(red: 0xF0/255.0, green: 0xB8/255.0, blue: 0x49/255.0, alpha: 0xFF/255.0)
+        public static let alertYellowLighter    = UIColor(red: 0xFE/255.0, green: 0xF8/255.0, blue: 0xEE/255.0, alpha: 0xFF/255.0)
+        public static let alertRedDarker        = UIColor(red: 0x6D/255.0, green: 0x18/255.0, blue: 0x18/255.0, alpha: 0xFF/255.0)
+
         
         // MARK: - Private Properties Properties
         //
-        private static let titleFontSize            = CGFloat(14)
-        private static let titleRegularFont         = WPFontManager.openSansRegularFontOfSize(titleFontSize)
-        private static let titleBoldFont            = WPFontManager.openSansSemiBoldFontOfSize(titleFontSize)
+        private static let timestampFont        = WPStyleGuide.subtitleFont()
         
-        private static let titleTextColor           = WPStyleGuide.littleEddieGrey()
-        private static let titleLineSize            = CGFloat(18)
-        private static let titleParagraph           = NSMutableParagraphStyle(
+        private static let titleFontSize        = CGFloat(14)
+        private static let titleRegularFont     = WPFontManager.openSansRegularFontOfSize(titleFontSize)
+        private static let titleBoldFont        = WPFontManager.openSansSemiBoldFontOfSize(titleFontSize)
+        private static let titleItalicsFont     = WPFontManager.openSansItalicFontOfSize(titleFontSize)
+        
+        private static let titleLineSize        = CGFloat(18)
+        private static let titleParagraph       = NSMutableParagraphStyle(
             minLineHeight: titleLineSize, maxLineHeight: titleLineSize, lineBreakMode: .ByWordWrapping, alignment: .Left
         )
     }
