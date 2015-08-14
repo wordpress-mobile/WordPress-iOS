f4bdb00006fa6e071e13cacf3c334d2abf031e2e
Jorge Leandro Perez <jorge@lantean.co>
2015-08-14 14:52:15 -0300

    NoteBlockCommentTableViewCell: Updates Unapproved Style

--- WordPress/Classes/ViewRelated/Notifications/Style/WPStyleGuide+Notifications.swift
+++ WordPress/Classes/ViewRelated/Notifications/Style/WPStyleGuide+Notifications.swift
@@ -20,7 +20,7 @@ extension WPStyleGuide
         public static let noticonTextColor          = UIColor.whiteColor()
         public static let noticonReadColor          = UIColor(red: 0xA4/255.0, green: 0xB9/255.0, blue: 0xC9/255.0, alpha: 0xFF/255.0)
         public static let noticonUnreadColor        = UIColor(red: 0x25/255.0, green: 0x9C/255.0, blue: 0xCF/255.0, alpha: 0xFF/255.0)
-        public static let noticonUnmoderatedColor   = UIColor(red: 0xFF/255.0, green: 0xBA/255.0, blue: 0x00/255.0, alpha: 0xFF/255.0)
+        public static let noticonUnmoderatedColor   = WPStyleGuide.alertYellowDark()
 
         public static let noteBackgroundReadColor   = UIColor.whiteColor()
         public static let noteBackgroundUnreadColor = UIColor(red: 0xF1/255.0, green: 0xF6/255.0, blue: 0xF9/255.0, alpha: 0xFF/255.0)
@@ -112,9 +112,9 @@ extension WPStyleGuide
 
         public static let blockApprovedBgColor      = UIColor.clearColor()
         
-        public static let blockUnapprovedSideColor  = UIColor(red: 0xFF/255.0, green: 0xBA/255.0, blue: 0x00/255.0, alpha: 0xFF/255.0)
-        public static let blockUnapprovedBgColor    = UIColor(red: 0xFF/255.0, green: 0xBA/255.0, blue: 0x00/255.0, alpha: 0x19/255.0)
-        public static let blockUnapprovedTextColor  = UIColor(red: 0xF0/255.0, green: 0x82/255.0, blue: 0x1E/255.0, alpha: 0xFF/255.0)
+        public static let blockUnapprovedSideColor  = WPStyleGuide.alertYellowDark()
+        public static let blockUnapprovedBgColor    = WPStyleGuide.alertYellowLighter()
+        public static let blockUnapprovedTextColor  = WPStyleGuide.alertRedDarker()
         public static let blockUnapprovedLinkColor  = WPStyleGuide.mediumBlue()
         
         public static let contentBlockRegularStyle  = [ NSParagraphStyleAttributeName:  contentBlockParagraph,
@@ -188,7 +188,7 @@ extension WPStyleGuide
         }
 
         public static func blockDetailsColorForComment(isApproved approved: Bool) -> UIColor {
-            return approved ? blockQuotedColor : blockUnapprovedTextColor
+            return approved ? blockQuotedColor : blockUnapprovedSideColor
         }
         
         public static func blockLinkColorForComment(isApproved approved: Bool) -> UIColor {
