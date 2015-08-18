2e18145bed935c5715ada91afa3dd22126e8fded
Jorge Leandro Perez <jorge@lantean.co>
2015-08-18 10:06:09 -0600

    Implements NSMutableAttributedString Extension Unit Test

--- WordPress/Classes/Extensions/NSMutableAttributedString+Helpers.swift
+++ WordPress/Classes/Extensions/NSMutableAttributedString+Helpers.swift
@@ -4,7 +4,7 @@ import Foundation
 extension NSMutableAttributedString
 {
     /**
-    *  @details     Applies a collection of Styles to all of the substrings that match with a given pattern
+    *  @details     Applies a collection of Styles to all of the substrings that match a given pattern
     *  @param       pattern     A Regex pattern that should be used to look up for matches
     *  @param       styles      Collection of styles to be applied on the matched strings
     */
