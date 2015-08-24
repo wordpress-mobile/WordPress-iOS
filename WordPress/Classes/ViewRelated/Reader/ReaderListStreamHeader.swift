1140639659e00058ed2219e83a01777793841c77
aerych <eric@automattic.com>
2015-08-24 16:44:59 -0500

    First pass at stream header style and config.

--- WordPress/Classes/ViewRelated/Reader/ReaderListStreamHeader.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderListStreamHeader.swift
@@ -2,11 +2,31 @@ import Foundation
 
 @objc public class ReaderListStreamHeader: UIView, ReaderStreamHeader
 {
-
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var detailLabel: UILabel!
     public var delegate: ReaderStreamHeaderDelegate?
 
-    public func configureHeader(topic: ReaderTopic) {
 
+    // MARK: - Lifecycle Methods
+
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+
+        applyStyles()
+    }
+
+    func applyStyles() {
+        WPStyleGuide.applyReaderStreamHeaderTitleStyle(titleLabel)
+        WPStyleGuide.applyReaderStreamHeaderDetailStyle(detailLabel)
+    }
+    
+
+    // MARK: - Configuration
+
+    public func configureHeader(topic: ReaderTopic) {
+        titleLabel.text = topic.title
+// TODO: Wire up when supported by the topic
+//        detailLabel.text = "sites . followers"
     }
 
 }
