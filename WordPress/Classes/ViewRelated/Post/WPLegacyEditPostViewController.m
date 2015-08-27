813a3fc37a4509516f9dd6c8270c4e2f05ea8c46
Sérgio Estêvão <sergio.estevao@automattic.com>
2015-08-27 11:06:15 +0100

    Refactor word count algorithm to string helper category.

--- WordPress/Classes/ViewRelated/Post/WPLegacyEditPostViewController.m
+++ WordPress/Classes/ViewRelated/Post/WPLegacyEditPostViewController.m
@@ -22,6 +22,7 @@
 #import <WPMediaPicker/WPMediaPicker.h>
 #import "WordPress-Swift.h"
 #import "WPAndDeviceMediaLibraryDataSource.h"
+#import "NSString+Helpers.h"
 
 NSString *const WPLegacyEditorNavigationRestorationID = @"WPLegacyEditorNavigationRestorationID";
 NSString *const WPLegacyAbstractPostRestorationKey = @"WPLegacyAbstractPostRestorationKey";
@@ -681,21 +682,9 @@ NS_ENUM(NSInteger, WPLegacyEditPostViewControllerActionSheet)
 - (void)logSavePostStats
 {
     NSString *buttonTitle = self.navigationItem.rightBarButtonItem.title;
-
-    // This word counting algorithm is from : http://stackoverflow.com/a/13367063
-    __block NSInteger originalWordCount = 0;
-    [self.post.original.content enumerateSubstringsInRange:NSMakeRange(0, [self.post.original.content length])
-                                                   options:NSStringEnumerationByWords | NSStringEnumerationLocalized
-                                                usingBlock:^(NSString *substring, NSRange substringRange, NSRange enclosingRange, BOOL *stop){
-                                                    originalWordCount++;
-                                                }];
-
-    __block NSInteger wordCount = 0;
-    [self.post.content enumerateSubstringsInRange:NSMakeRange(0, [self.post.content length])
-                                          options:NSStringEnumerationByWords | NSStringEnumerationLocalized
-                                       usingBlock:^(NSString *substring, NSRange substringRange, NSRange enclosingRange, BOOL *stop){
-                                           wordCount++;
-                                       }];
+    
+    NSInteger originalWordCount = [self.post.original.content wordCount];
+    NSInteger wordCount = [self.post.content wordCount];
 
     NSMutableDictionary *properties = [[NSMutableDictionary alloc] initWithCapacity:2];
     properties[@"word_count"] = @(wordCount);
