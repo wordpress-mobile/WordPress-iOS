22af5afc423ab894d4ed98d5359a7e13db539389
aerych <eric@automattic.com>
2015-08-27 12:35:30 -0500

    Animates the loading view.
    Adds a convenience method to WPAnimatedBox
    Some code style tweaks.

--- WordPress/Classes/ViewRelated/Views/WPAnimatedBox.m
+++ WordPress/Classes/ViewRelated/Views/WPAnimatedBox.m
@@ -92,4 +92,13 @@ static CGFloat const WPAnimatedBoxAnimationTolerance = 5.0;
     } completion:nil];
 }
 
+- (void)prepareAndAnimateAfterDelay:(CGFloat)delayInSeconds
+{
+    [self prepareAndAnimateAfterDelay:NO];
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [self animate];
+    });
+}
+
 @end
