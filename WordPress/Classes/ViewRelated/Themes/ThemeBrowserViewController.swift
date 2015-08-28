520c8155c1c7fecef647424e7907d8ad0c83e9eb
Diego Rey Mendez <diegoreymendez@gmail.com>
2015-08-28 14:19:22 -0300

    WIP: Added search bar to the theme browser VC.

--- WordPress/Classes/ViewRelated/Themes/ThemeBrowserViewController.swift
+++ WordPress/Classes/ViewRelated/Themes/ThemeBrowserViewController.swift
@@ -1,4 +1,40 @@
 import Foundation
 
-class ThemeBrowserViewController : UICollectionViewController {
+class ThemeBrowserViewController : UICollectionViewController, UISearchBarDelegate {
+    
+    // MARK: - UICollectionViewController protocol UICollectionViewDataSource
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1;
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell : UICollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("ThemeBrowserCell", forIndexPath: indexPath) as! UICollectionViewCell
+        
+        return cell
+    }
+    
+    override func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        
+        var reuseIdentifier : String? = nil
+        
+        if kind == UICollectionElementKindSectionHeader {
+            reuseIdentifier = "ThemeBrowserHeaderView"
+        } else {
+            reuseIdentifier = "ThemeBrowserFooterView"
+        }
+        
+        return collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: reuseIdentifier!, forIndexPath: indexPath) as! UICollectionReusableView
+    }
+    
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int
+    {
+        return 1;
+    }
+    
+    // MARK: - UISearchBarDelegate
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        // SEARCH AWAY!!!
+    }
 }
