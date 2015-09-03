332e05526a81dd29e14e92bffcc8266a02eaaa0f
aerych <eric@automattic.com>
2015-09-03 11:07:34 -0500

    Addresses layout issues with headers on the iPad.

--- WordPress/Classes/ViewRelated/Reader/ReaderSiteStreamHeader.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderSiteStreamHeader.swift
@@ -22,6 +22,7 @@ import Foundation
     }
 
     func applyStyles() {
+        backgroundColor = WPStyleGuide.greyLighten30()
         WPStyleGuide.applyReaderStreamHeaderTitleStyle(titleLabel)
         WPStyleGuide.applyReaderStreamHeaderDetailStyle(detailLabel)
         WPStyleGuide.applyReaderSiteStreamDescriptionStyle(descriptionLabel)
