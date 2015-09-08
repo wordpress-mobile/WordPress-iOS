00178b5e228cad7ec0de8b9cd21503f9053bb9df
Alex Curylo <alex@alexcurylo.com>
2015-09-08 13:24:45 -0700

    Publicizer API tests

--- WordPress/WordPressTest/BlogServiceRemoteRESTTests.m
+++ WordPress/WordPressTest/BlogServiceRemoteRESTTests.m
@@ -3,6 +3,11 @@
 #import "Blog.h"
 #import "BlogServiceRemoteREST.h"
 #import "WordPressComApi.h"
+#import "Publicizer.h"
+
+@implementation Publicizer(UnitTesting)
+- (Blog *)blog { return nil; }
+@end
 
 @interface BlogServiceRemoteRESTTests : XCTestCase
 @end
@@ -115,7 +120,6 @@
                                             failure:^(NSError *error) {}]);
 }
 
-
 #pragma mark - Synchronizing connections for a blog
 
 - (void)testThatSyncConnectionsForBlogWorks
@@ -149,6 +153,137 @@
     XCTAssertThrows([service syncConnectionsForBlog:nil
                                             success:^(NSArray *connections) {}
                                             failure:^(NSError *error) {}]);
+}
+
+#pragma mark - Publicizer management for a blog
+
+- (void)testThatCheckAuthorizationForPublicizerWorks
+{
+    Blog *blog = OCMStrictClassMock([Blog class]);
+    OCMStub([blog dotComID]).andReturn(@10);
+    Publicizer *publicizer = OCMStrictClassMock([Publicizer class]);
+    OCMStub([publicizer blog]).andReturn(blog);
+    
+    WordPressComApi *api = OCMStrictClassMock([WordPressComApi class]);
+    BlogServiceRemoteREST *service = nil;
+    
+    NSString *url = @"me/keyring-connections";
+    
+    OCMStub([api GET:[OCMArg isEqual:url]
+          parameters:[OCMArg isNil]
+             success:[OCMArg isNotNil]
+             failure:[OCMArg isNotNil]]);
+    
+    XCTAssertNoThrow(service = [[BlogServiceRemoteREST alloc] initWithApi:api]);
+    
+    [service checkAuthorizationForPublicizer:publicizer
+                                     success:^(NSDictionary *authorization) {}
+                                     failure:^(NSError *error) {}];
+}
+
+- (void)testThatCheckAuthorizationForPublicizerThrowsExceptionWithoutBlog
+{
+    Publicizer *publicizer = OCMStrictClassMock([Publicizer class]);
+    OCMStub([publicizer blog]).andReturn(nil);
+    
+    WordPressComApi *api = OCMStrictClassMock([WordPressComApi class]);
+    BlogServiceRemoteREST *service = nil;
+    
+    XCTAssertNoThrow(service = [[BlogServiceRemoteREST alloc] initWithApi:api]);
+    XCTAssertThrows([service checkAuthorizationForPublicizer:nil
+                                                     success:^(NSDictionary *authorization) {}
+                                                     failure:^(NSError *error) {}]);
+    XCTAssertThrows([service checkAuthorizationForPublicizer:publicizer
+                                                     success:^(NSDictionary *authorization) {}
+                                                     failure:^(NSError *error) {}]);
+}
+
+- (void)testThatConnectPublicizerWorks
+{
+    Blog *blog = OCMStrictClassMock([Blog class]);
+    OCMStub([blog dotComID]).andReturn(@10);
+    Publicizer *publicizer = OCMStrictClassMock([Publicizer class]);
+    OCMStub([publicizer blog]).andReturn(blog);
+
+    WordPressComApi *api = OCMStrictClassMock([WordPressComApi class]);
+    BlogServiceRemoteREST *service = nil;
+    
+    NSString *url = [NSString stringWithFormat:@"sites/%@/publicize-connections/new", blog.dotComID];
+
+    OCMStub([api POST:[OCMArg isEqual:url]
+           parameters:[OCMArg isNotNil]
+              success:[OCMArg isNotNil]
+              failure:[OCMArg isNotNil]]);
+    
+    XCTAssertNoThrow(service = [[BlogServiceRemoteREST alloc] initWithApi:api]);
+    
+    [service connectPublicizer:publicizer
+             withAuthorization:@{@"ID":@"keyring"}
+                       success:^(NSArray *connections) {}
+                       failure:^(NSError *error) {}];
+}
+
+- (void)testThatConnectPublicizerThrowsExceptionWithoutAuthorization
+{
+    Blog *blog = OCMStrictClassMock([Blog class]);
+    OCMStub([blog dotComID]).andReturn(@10);
+    Publicizer *publicizer = OCMStrictClassMock([Publicizer class]);
+    OCMStub([publicizer blog]).andReturn(blog);
+    
+    WordPressComApi *api = OCMStrictClassMock([WordPressComApi class]);
+    BlogServiceRemoteREST *service = nil;
+    
+    XCTAssertNoThrow(service = [[BlogServiceRemoteREST alloc] initWithApi:api]);
+    XCTAssertThrows([service connectPublicizer:nil
+                             withAuthorization:@{@"ID":@"keyring"}
+                                       success:^(NSArray *connections) {}
+                                       failure:^(NSError *error) {}]);
+    XCTAssertThrows([service connectPublicizer:publicizer
+                             withAuthorization:@{}
+                                       success:^(NSArray *connections) {}
+                                       failure:^(NSError *error) {}]);
+}
+
+- (void)testThatDisconnectPublicizerWorks
+{
+    Blog *blog = OCMStrictClassMock([Blog class]);
+    OCMStub([blog dotComID]).andReturn(@10);
+    Publicizer *publicizer = OCMStrictClassMock([Publicizer class]);
+    OCMStub([publicizer blog]).andReturn(blog);
+    OCMStub([publicizer connectionID]).andReturn(10);
+
+    WordPressComApi *api = OCMStrictClassMock([WordPressComApi class]);
+    BlogServiceRemoteREST *service = nil;
+    
+    NSString *url = [NSString stringWithFormat:@"sites/%@/connections/%d/delete", blog.dotComID, (int)publicizer.connectionID];
+    
+    OCMStub([api POST:[OCMArg isEqual:url]
+           parameters:[OCMArg isNil]
+              success:[OCMArg isNotNil]
+              failure:[OCMArg isNotNil]]);
+    
+    XCTAssertNoThrow(service = [[BlogServiceRemoteREST alloc] initWithApi:api]);
+    
+    [service disconnectPublicizer:publicizer
+                          success:^(NSArray *connections) {}
+                          failure:^(NSError *error) {}];
+}
+
+- (void)testThatDisconnectPublicizerThrowsExceptionWithoutBlog
+{
+    Publicizer *publicizer = OCMStrictClassMock([Publicizer class]);
+    OCMStub([publicizer blog]).andReturn(nil);
+
+    WordPressComApi *api = OCMStrictClassMock([WordPressComApi class]);
+    BlogServiceRemoteREST *service = nil;
+    
+    XCTAssertNoThrow(service = [[BlogServiceRemoteREST alloc] initWithApi:api]);
+    XCTAssertThrows([service disconnectPublicizer:nil
+                                          success:^(NSArray *connections) {}
+                                          failure:^(NSError *error) {}]);
+    XCTAssertThrows([service disconnectPublicizer:publicizer
+                                          success:^(NSArray *connections) {}
+                                          failure:^(NSError *error) {}]);
 }
 
 @end
