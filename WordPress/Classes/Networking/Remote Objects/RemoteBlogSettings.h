ed30d8ba6eea3a7c341bc493fb0c6c930a0b7e67
Sérgio Estêvão <sergio.estevao@automattic.com>
2015-09-09 15:35:05 +0100

    Read related posts attributes on settings REST calls.

--- WordPress/Classes/Networking/Remote Objects/RemoteBlogSettings.h
+++ WordPress/Classes/Networking/Remote Objects/RemoteBlogSettings.h
@@ -7,6 +7,12 @@
 @property (copy) NSString *desc;
 @property (copy) NSNumber *privacy;
 
+// Reading
+@property (copy) NSNumber *relatedPostsAllowed;
+@property (copy) NSNumber *relatedPostsEnabled;
+@property (copy) NSNumber *relatedPostsShowHeadline;
+@property (copy) NSNumber *relatedPostsShowThumbnails;
+
 // Writing
 @property (copy) NSNumber *defaultCategory;
 @property (copy) NSString *defaultPostFormat;
