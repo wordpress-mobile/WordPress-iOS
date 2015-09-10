637087fd998160d0463cb973d5004eb9c474cfe4
Sendhil Panchadsaram <sendhilp@gmail.com>
2015-09-10 12:43:46 -0700

    RichTextView.swift: Swift 2.0 Migration

--- WordPress/Classes/ViewRelated/Views/RichTextView/RichTextView.swift
+++ WordPress/Classes/ViewRelated/Views/RichTextView/RichTextView.swift
@@ -176,13 +176,13 @@ import Foundation
         addSubview(textView)
         
         // Setup Layout
-        textView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        textView.translatesAutoresizingMaskIntoConstraints = false
         pinSubviewToAllEdges(textView)
     }
 
     private func renderAttachments() {
         // Nuke old attachments
-        attachmentViews.map { $0.removeFromSuperview() }
+        _ = attachmentViews.map { $0.removeFromSuperview() }
         attachmentViews.removeAll(keepCapacity: false)
         
         // Proceed only if needed
@@ -200,7 +200,7 @@ import Foundation
             }
             
             let unwrappedView           = attachmentView!
-            unwrappedView.frame.origin  = self.textView.frameForTextInRange(range).integerRect.origin
+            unwrappedView.frame.origin  = self.textView.frameForTextInRange(range).integral.origin
             self.textView.addSubview(unwrappedView)
             self.attachmentViews.append(unwrappedView)
         }
