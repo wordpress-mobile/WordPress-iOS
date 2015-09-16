1f54e0feb630355776a478f53056aae1c28de974
Sérgio Estêvão <sergio.estevao@automattic.com>
2015-09-16 13:40:21 +0100

    Select simulator version to run.

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -5,6 +5,7 @@ if [ ! $TRAVIS ]; then
 fi
 
 xcodebuild build test \
+        -destination "platform=iOS Simulator,name=iPhone 6" \
 	-workspace "$TRAVIS_XCODE_WORKSPACE" \
 	-scheme "$TRAVIS_XCODE_SCHEME" \
 	-sdk "$TRAVIS_XCODE_SDK" \
