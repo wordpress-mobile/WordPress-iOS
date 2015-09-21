6c8ebb8fe17bbbe689f790a87ce54d751792d355
Jorge Bernal <jorge@automattic.com>
2015-09-21 18:55:24 +0200

    Added model and (cell) view model

--- WordPress/Classes/ViewRelated/People/PeopleViewController.swift
+++ WordPress/Classes/ViewRelated/People/PeopleViewController.swift
@@ -1,6 +1,42 @@
 import UIKit
 
 class PeopleViewController: UITableViewController {
+    let models: People = {
+        let person1 = Person(
+            ID: 0,
+            username: "alternatekev",
+            firstName: "Kevin",
+            lastName: "Conboy",
+            displayName: "Kevin Conboy",
+            role: .Editor,
+            pending: false,
+            siteID: 0,
+            avatarURL: nil
+        )
+        let person2 = Person(
+            ID: 0,
+            username: "melchoyce",
+            firstName: "Mel",
+            lastName: "Choyce",
+            displayName: "Mel Choyce",
+            role: .Author,
+            pending: true,
+            siteID: 0,
+            avatarURL: nil
+        )
+        let person3 = Person(
+            ID: 0,
+            username: "ryelle",
+            firstName: "Kelly",
+            lastName: "Dwan",
+            displayName: "Kelly Dwan",
+            role: .Contributor,
+            pending: false,
+            siteID: 0,
+            avatarURL: nil
+        )
+        return [person1, person2, person3]
+    }()
 }
 
 extension PeopleViewController {
@@ -9,36 +45,16 @@ extension PeopleViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3;
+        return models.count;
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("PeopleCell") as! PeopleCell
-        switch indexPath.row {
-        case 0:
-            cell.displayNameLabel.text = "Kevin Conboy"
-            cell.usernameLabel.text = "@alternatekev"
-            cell.roleBadge.borderColor = WPStyleGuide.People.editorColor
-            cell.roleBadge.backgroundColor = WPStyleGuide.People.editorColor
-            cell.roleBadge.textColor = WPStyleGuide.People.RoleBadge.textColor
-            cell.roleBadge.text = "Editor"
-        case 1:
-            cell.displayNameLabel.text = "Mel Choyce"
-            cell.usernameLabel.text = "@melchoyce"
-            cell.roleBadge.borderColor = WPStyleGuide.People.authorColor
-            cell.roleBadge.backgroundColor = WPStyleGuide.People.RoleBadge.textColor
-            cell.roleBadge.textColor = WPStyleGuide.People.authorColor
-            cell.roleBadge.text = "Author – Pending"
-        case 2:
-            cell.displayNameLabel.text = "Kelly Dwan"
-            cell.usernameLabel.text = "@ryelle"
-            cell.roleBadge.borderColor = WPStyleGuide.People.contributorColor
-            cell.roleBadge.backgroundColor = WPStyleGuide.People.contributorColor
-            cell.roleBadge.textColor = WPStyleGuide.People.RoleBadge.textColor
-            cell.roleBadge.text = "Contributor"
-        default:
-            break
-        }
+        let person = models[indexPath.row]
+        let viewModel = PeopleCellViewModel(person: person)
+
+        cell.bindViewModel(viewModel)
+        
         return cell
     }
 }
