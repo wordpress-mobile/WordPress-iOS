9fd015a4f3730d7eae9cbb23f291026fd61c8e62
aerych <aerychj@gmail.com>
2015-09-25 15:07:52 -0500

    Fixes layout issues with certain UITableViews on iOS 9

--- WordPress/Classes/ViewRelated/Reader/SubscribedTopicsViewController.m
+++ WordPress/Classes/ViewRelated/Reader/SubscribedTopicsViewController.m
@@ -42,6 +42,7 @@
     self.tableViewHandler = [[WPTableViewHandler alloc] initWithTableView:self.tableView];
     self.tableViewHandler.delegate = self;
 
+    [WPStyleGuide resetReadableMarginsForTableView:self.tableView];
     [WPStyleGuide configureColorsForView:self.view andTableView:self.tableView];
 }
 
