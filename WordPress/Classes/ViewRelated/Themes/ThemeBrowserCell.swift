c6d3e68c1cbf45d9489b84ce38a18ed957e89ae3
Alex Curylo <alex@alexcurylo.com>
2015-10-05 18:45:46 -0700

    Initial layout of theme browser cell

--- WordPress/Classes/ViewRelated/Themes/ThemeBrowserCell.swift
+++ WordPress/Classes/ViewRelated/Themes/ThemeBrowserCell.swift
@@ -6,36 +6,71 @@ public class ThemeBrowserCell : UICollectionViewCell {
     
     @IBOutlet weak var imageView : UIImageView!
     @IBOutlet weak var nameLabel : UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
     
     // MARK: - Properties
     
     private var theme : Theme? {
         didSet {
-            self.refreshGUI()
+            refreshGUI()
         }
     }
     
     // MARK: - Additional initialization
     
     public func configureWithTheme(theme: Theme?) {
-        self.theme = theme;
+        self.theme = theme
     }
     
     // MARK: - GUI
     
+    /**
+    @brief Calculates cell height fitting a given width
+    
+    @param width    Intended width
+    @return         Matching height
+    */
+    public class func heightForWidth(width: CGFloat) -> CGFloat {
+        let imageRatio = CGFloat(0.75)
+        let infoBarHeight = CGFloat(55)
+        
+        let imageHeight = width * imageRatio
+        let height = imageHeight + infoBarHeight
+        
+        return height
+    }
+    
+    override public func prepareForReuse() {
+        super.prepareForReuse()
+        theme = nil
+    }
+    
     private func refreshGUI() {
-        if let unwrappedTheme = self.theme {
-            if let unwrappedPreviewUrl = unwrappedTheme.previewUrl {
-                self.refreshPreviewImage(unwrappedPreviewUrl)
+        if let theme = theme {
+           if let imageUrl = theme.screenshotUrl where !imageUrl.isEmpty {
+                self.refreshScreenshotImage(imageUrl)
+            } else {
+                imageView.image = nil
             }
             
-            self.nameLabel.text = unwrappedTheme.name
+            nameLabel.text = theme.name
+            priceLabel.text = "" // FIXME: Add price to Core Data
+        } else {
+            imageView.image = nil
+            nameLabel.text = nil
+            priceLabel.text = nil
         }
     }
     
-    private func refreshPreviewImage(imageUrl: String) {    
+    private func refreshScreenshotImage(imageUrl: String) {
         let imageUrl = NSURL(string: imageUrl)
         
-        self.imageView.downloadImage(imageUrl, placeholderImage: nil)
+        imageView.downloadImage(imageUrl, placeholderImage: nil)
     }
+
+    // MARK: - Actions
+    
+    @IBAction private func didTapActionButton(sender: UIButton) {
+    }
+    
 }
