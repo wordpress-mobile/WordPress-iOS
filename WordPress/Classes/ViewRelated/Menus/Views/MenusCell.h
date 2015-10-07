e781180b968e96a87be0d8c0190b21c98c59e66a
Brent Coursey <brent@kurzee.com>
2015-10-07 14:00:17 -0500

    Removing source header comments on MenusCell.

--- WordPress/Classes/ViewRelated/Menus/Views/MenusCell.h
+++ WordPress/Classes/ViewRelated/Menus/Views/MenusCell.h
@@ -1,11 +1,3 @@
-//
-//  MenusCell.h
-//  WordPress
-//
-//  Created by Kurzee on 10/7/15.
-//  Copyright © 2015 WordPress. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface MenusCell : UITableViewCell
