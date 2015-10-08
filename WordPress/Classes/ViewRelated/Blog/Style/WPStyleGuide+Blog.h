0ec8def0d50d488a40147a69dba7bf7f928cb757
Will Kwon <will@willkwon.com>
2015-10-07 21:56:04 -0700

    Adding Blog category

---
+++ WordPress/Classes/ViewRelated/Blog/Style/WPStyleGuide+Blog.h
@@ -0,0 +1,13 @@
+//
+//  WPStyleGuide+Blog.h
+//  WordPress
+//
+//  Created by Will Kwon on 10/7/15.
+//  Copyright © 2015 WordPress. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WPStyleGuide_Blog : NSObject
+
+@end
---
+++ WordPress/Classes/ViewRelated/Blog/Style/WPStyleGuide+Blog.m
@@ -0,0 +1,13 @@
+//
+//  WPStyleGuide+Blog.m
+//  WordPress
+//
+//  Created by Will Kwon on 10/7/15.
+//  Copyright © 2015 WordPress. All rights reserved.
+//
+
+#import "WPStyleGuide+Blog.h"
+
+@implementation WPStyleGuide_Blog
+
+@end
