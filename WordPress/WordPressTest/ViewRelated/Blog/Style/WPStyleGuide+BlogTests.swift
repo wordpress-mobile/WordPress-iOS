d3e9d7027378e6cc7ecadf69da75973ce71aeea8
Will Kwon <will@willkwon.com>
2015-10-08 19:57:58 -0700

    Doing in swift

---
+++ WordPress/WordPressTest/ViewRelated/Blog/Style/WPStyleGuide+BlogTests.swift
@@ -0,0 +1,35 @@
+//
+//  WPStyleGuide+BlogTests.swift
+//  WordPress
+//
+//  Created by Will Kwon on 10/8/15.
+//  Copyright © 2015 WordPress. All rights reserved.
+//
+
+import XCTest
+
+class WPStyleGuide_BlogTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
