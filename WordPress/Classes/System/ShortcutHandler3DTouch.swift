48ad14e39c9354486db73b73762b56ed09812794
Will Kwon <will@willkwon.com>
2015-10-13 14:53:15 -0600

    Adding 3d touch handler class

---
+++ WordPress/Classes/System/ShortcutHandler3DTouch.swift
@@ -0,0 +1,13 @@
+//
+//  ShortcutHandler3DTouch.swift
+//  WordPress
+//
+//  Created by Will Kwon on 10/13/15.
+//  Copyright © 2015 WordPress. All rights reserved.
+//
+
+import UIKit
+
+class ShortcutHandler3DTouch: NSObject {
+
+}
