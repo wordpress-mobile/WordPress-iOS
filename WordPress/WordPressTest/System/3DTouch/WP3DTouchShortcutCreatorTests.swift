22a5e51686b803758643d59ee22edd61d5063ea3
Will Kwon <will@willkwon.com>
2015-10-21 14:17:05 -0700

    Adding test for loggedOutShortcuts

--- WordPress/WordPressTest/System/3DTouch/WP3DTouchShortcutCreatorTests.swift
+++ WordPress/WordPressTest/System/3DTouch/WP3DTouchShortcutCreatorTests.swift
@@ -1,35 +1,21 @@
-//
-//  WP3DTouchShortcutCreatorTests.swift
-//  WordPress
-//
-//  Created by Will Kwon on 10/21/15.
-//  Copyright © 2015 WordPress. All rights reserved.
-//
-
 import XCTest
+import WordPress
 
 class WP3DTouchShortcutCreatorTests: XCTestCase {
+    var testShortcutCreator: WP3DTouchShortcutCreator!
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        testShortcutCreator = WP3DTouchShortcutCreator()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        testShortcutCreator = nil
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testCreateShortcutLoggedOutCreatesLoggedOutShortcuts() {
+        testShortcutCreator.createShortcuts(false)
+        XCTAssertEqual(UIApplication.sharedApplication().shortcutItems!, testShortcutCreator.loggedOutShortcutArray())
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
