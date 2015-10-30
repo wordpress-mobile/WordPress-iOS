f29422bad86f3a74dd4945a25b6dff75659706a0
Jorge Leandro Perez <jorge@lantean.co>
2015-10-30 16:46:38 -0300

    Implements Stub DiscussionSettingsViewController

---
+++ WordPress/Classes/ViewRelated/Blog/DiscussionSettingsViewController.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+
+/**
+ *  @class          DiscussionSettingsViewController
+ *  @brief          The purpose of this class is to render the Discussion Settings associated to a site, and
+ *                  allow the user to tune those settings, as required.
+ */
+
+public class DiscussionSettingsViewController : UITableViewController
+{
+}
