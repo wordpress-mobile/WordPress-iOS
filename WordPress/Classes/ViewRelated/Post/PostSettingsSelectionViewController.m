0d7b4d9966d273c67c93558e31fbce5d9180e59f
Jorge Leandro Perez <jorge@lantean.co>
2015-11-03 17:48:28 -0300

    PostSettingsSelectionViewController: Enhancements

--- WordPress/Classes/ViewRelated/Post/PostSettingsSelectionViewController.m
+++ WordPress/Classes/ViewRelated/Post/PostSettingsSelectionViewController.m
@@ -1,5 +1,6 @@
 #import "PostSettingsSelectionViewController.h"
 #import "WPStyleGuide.h"
+#import "NSDictionary+SafeExpectations.h"
 #import "NSString+XMLExtensions.h"
 #import "WPTableViewCell.h"
 #import "WPTableViewSectionHeaderFooterView.h"
@@ -49,17 +50,14 @@ NSString * const SettingsSelectionCurrentValueKey = @"CurrentValue";
 {
     self = [self initWithStyle:style];
     if (self) {
-        self.title = [dictionary objectForKey:SettingsSelectionTitleKey];
-        _titles = [dictionary objectForKey:SettingsSelectionTitlesKey];
-        _values = [dictionary objectForKey:SettingsSelectionValuesKey];
-        _hints = [dictionary objectForKey:SettingsSelectionHintsKey];
-        _defaultValue = [dictionary objectForKey:SettingsSelectionDefaultValueKey];
-        _currentValue = [dictionary objectForKey:SettingsSelectionCurrentValueKey];
-
-        if (_currentValue == nil) {
-            _currentValue = _defaultValue;
-        }
+        self.title = [dictionary stringForKey:SettingsSelectionTitleKey];
+        _titles = [dictionary arrayForKey:SettingsSelectionTitlesKey];
+        _values = [dictionary arrayForKey:SettingsSelectionValuesKey];
+        _hints = [dictionary arrayForKey:SettingsSelectionHintsKey];
+        _defaultValue = dictionary[SettingsSelectionDefaultValueKey];
+        _currentValue = dictionary[SettingsSelectionCurrentValueKey] ?: _defaultValue;
     }
+    
     return self;
 }
 
@@ -101,15 +99,14 @@ NSString * const SettingsSelectionCurrentValueKey = @"CurrentValue";
     if (!self.hints) {
         return nil;
     }
+    
     if (!_hintView) {
         _hintView = [[WPTableViewSectionHeaderFooterView alloc] initWithReuseIdentifier:nil style:WPTableViewSectionStyleFooter];
     }
+    
     NSUInteger position = [self.values indexOfObject:self.currentValue];
-    NSString * hint = @"";
-    if (position != NSNotFound) {
-        hint = self.hints[position];
-    }
-    [_hintView setTitle:hint];
+    _hintView.title = (position != NSNotFound) ? self.hints[position] : [NSString string];
+
     return _hintView;
 }
 
@@ -135,14 +132,10 @@ NSString * const SettingsSelectionCurrentValueKey = @"CurrentValue";
     if (cell == nil) {
         cell = [[WPTableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:CellIdentifier];
     }
-    cell.accessoryType = UITableViewCellAccessoryNone;
-
-    cell.textLabel.text = [NSString decodeXMLCharactersIn:[self.titles objectAtIndex:indexPath.row]];
 
     NSString *val = [self.values objectAtIndex:indexPath.row];
-    if ([self.currentValue isEqual:val]) {
-        cell.accessoryType = UITableViewCellAccessoryCheckmark;
-    }
+    cell.accessoryType = [self.currentValue isEqual:val] ? UITableViewCellAccessoryCheckmark : UITableViewCellAccessoryNone;
+    cell.textLabel.text = [NSString decodeXMLCharactersIn:[self.titles objectAtIndex:indexPath.row]];
 
     [WPStyleGuide configureTableViewCell:cell];
 
@@ -153,8 +146,9 @@ NSString * const SettingsSelectionCurrentValueKey = @"CurrentValue";
 {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
 
-    NSObject *val = [self.values objectAtIndex:indexPath.row];
+    NSObject *val = self.values[indexPath.row];
     self.currentValue = val;
+    
     [self.tableView reloadData];
 
     if (self.onItemSelected != nil) {
