2709e5bc67512a077af8e51647da16d4a73a08c0
Jorge Leandro Perez <jorge@lantean.co>
2015-11-05 17:28:13 -0300

    Implements BlogSettings

--- WordPress/Classes/Models/BlogSettings.swift
+++ WordPress/Classes/Models/BlogSettings.swift
@@ -1,11 +1,30 @@
 import Foundation
 
 
-public class BlogSettings
+public class BlogSettings : NSManagedObject
 {
-    @NSManaged var blog                         : Blog
-    @NSManaged var relatedPostsAllowed          : Bool
-    @NSManaged var relatedPostsEnabled          : Bool
-    @NSManaged var relatedPostsShowHeadline     : Bool
-    @NSManaged var relatedPostsShowThumbnails   : Bool
+    @NSManaged var blog                                 : Blog
+    
+    @NSManaged var commentsAllowed                      : Bool
+    @NSManaged var commentsCloseAutomatically           : Bool
+    @NSManaged var commentsCloseAutomaticallyAfterDays  : Bool
+
+    @NSManaged var commentsPageSize                     : Int
+    @NSManaged var commentsPagingEnabled                : Bool
+    
+    @NSManaged var commentsRequireManualModeration      : Bool
+    @NSManaged var commentsRequireRegistration          : Bool
+    
+    @NSManaged var commentsSortOrder                    : Int
+    
+    @NSManaged var commentsThreadingEnabled             : Bool
+    @NSManaged var commentsThreadingDepth               : Int
+
+    @NSManaged var pingbacksOutboundEnabled             : Bool
+    @NSManaged var pingbacksInboundEnabled              : Bool
+    
+    @NSManaged var relatedPostsAllowed                  : Bool
+    @NSManaged var relatedPostsEnabled                  : Bool
+    @NSManaged var relatedPostsShowHeadline             : Bool
+    @NSManaged var relatedPostsShowThumbnails           : Bool
 }
