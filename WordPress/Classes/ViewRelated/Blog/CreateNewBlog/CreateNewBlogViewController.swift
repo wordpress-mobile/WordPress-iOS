b4d6c26cfd0e9ac70d715b5ba40b31468ac39169
Will Kwon <will@willkwon.com>
2015-11-05 15:37:47 -0800

    Create New Blog view controller

---
+++ WordPress/Classes/ViewRelated/Blog/CreateNewBlog/CreateNewBlogViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CreateNewBlogViewController.swift
+//  WordPress
+//
+//  Created by Will Kwon on 11/5/15.
+//  Copyright © 2015 WordPress. All rights reserved.
+//
+
+import UIKit
+
+class CreateNewBlogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
