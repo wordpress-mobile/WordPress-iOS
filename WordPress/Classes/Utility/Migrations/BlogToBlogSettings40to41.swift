53f5444395cf4dd0ee616b6a39e1484c59d3bbb9
Jorge Leandro Perez <jorge@lantean.co>
2015-11-09 14:50:16 -0300

    BlogToBlogSettings40to41: Destination Blog Mapping shouldn't rely on NSFetchRequest

--- WordPress/Classes/Utility/Migrations/BlogToBlogSettings40to41.swift
+++ WordPress/Classes/Utility/Migrations/BlogToBlogSettings40to41.swift
@@ -10,10 +10,10 @@ import Foundation
  */
 class BlogToBlogSettings40to41: NSEntityMigrationPolicy
 {
-    override func createRelationshipsForDestinationInstance(blogSettings: NSManagedObject, entityMapping mapping: NSEntityMapping, manager: NSMigrationManager) throws {
+    override func createRelationshipsForDestinationInstance(dBlogSettings: NSManagedObject, entityMapping mapping: NSEntityMapping, manager: NSMigrationManager) throws {
         
         // 1. Load the Source Blog
-        let sInstances = manager.sourceInstancesForEntityMappingNamed(mapping.name, destinationInstances: [blogSettings])
+        let sInstances = manager.sourceInstancesForEntityMappingNamed(mapping.name, destinationInstances: [dBlogSettings])
         assert(sInstances.count == 1)
         
         guard let sBlog = sInstances.first else {
@@ -21,35 +21,16 @@ class BlogToBlogSettings40to41: NSEntityMigrationPolicy
             return
         }
         
-        // 2. Load the Source Blog ID
-        guard let blogID = sBlog.valueForKey("blogID") as? NSNumber else {
-            print("Migration Error: Couldn't load blogID")
-            return
-        }
-        
-        // 3. Load the Destination Blog Instance
-        guard let dBlog = blogWithBlogID(blogID, context: manager.destinationContext) else {
+        // 2. Load the Destination Blog Instance
+        let dInstances = manager.destinationInstancesForEntityMappingNamed("BlogToBlog", sourceInstances: [sBlog])
+        assert(sInstances.count == 1)
+
+        guard let dBlog = dInstances.first else {
             print("Migration Error: Couldn't load Destination Blog Instance")
             return
         }
         
-        // 4. Set the BlogSetting <> Blog Relationship
-        blogSettings.setValue(dBlog, forKey: "blog")
-    }
-    
-
-    // MARK: - Private Helpers
-    private func blogWithBlogID(blogID : NSNumber, context: NSManagedObjectContext) -> NSManagedObject? {
-        let request                 = NSFetchRequest(entityName: Blog.classNameWithoutNamespaces())
-        request.predicate           = NSPredicate(format: "blogID == %@", blogID)
-        request.includesSubentities = false
-
-        do {
-            return try context.executeFetchRequest(request).first as? NSManagedObject
-        } catch {
-            print("Migration Error: \(error)")
-        }
-        
-        return nil
+        // 3. Set the BlogSetting <> Blog Relationship
+        dBlogSettings.setValue(dBlog, forKey: "blog")
     }
 }
