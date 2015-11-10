fe9f09408ef0e9ec56348671e59840857129b2ee
Sérgio Estêvão <sergio.estevao@automattic.com>
2015-11-10 00:30:42 +0000

    Remove ALAsset methods and related helpers.

--- WordPress/Classes/Services/MediaService.m
+++ WordPress/Classes/Services/MediaService.m
@@ -135,73 +135,6 @@ NSInteger const MediaMaxImageSizeDimension = 3000;
         }];
 }
 
-- (void)createMediaWithAsset:(ALAsset *)asset
-             forPostObjectID:(NSManagedObjectID *)postObjectID
-                  completion:(void (^)(Media *media, NSError *error))completion
-{
-    BOOL geoLocationEnabled = NO;
-    NSError *error = nil;
-    AbstractPost *post = (AbstractPost *)[self.managedObjectContext existingObjectWithID:postObjectID error:&error];
-    if (!post) {
-        if (completion) {
-            completion(nil, error);
-        }
-        return;
-    }
-    MediaType mediaType = MediaTypeDocument;
-    NSSet *allowedFileTypes = nil;
-    if ([[asset valueForProperty:ALAssetPropertyType] isEqualToString:ALAssetTypePhoto]) {
-        mediaType = MediaTypeImage;
-        allowedFileTypes = post.blog.allowedFileTypes;
-    } else if ([[asset valueForProperty:ALAssetPropertyType] isEqualToString:ALAssetTypeVideo]) {
-        // We ignore allowsFileTypes for videos because if videopress is not enabled we still can upload
-        // files even if the allowed file types says no.
-        allowedFileTypes = nil;
-        mediaType = MediaTypeVideo;
-    }
-
-    geoLocationEnabled = post.blog.geolocationEnabled;
-
-    CGSize maxImageSize = [MediaService maxImageSizeSetting];
-    
-    NSString *mediaPath = [self pathForAsset:asset supportedFileFormats:allowedFileTypes];
-
-    [[WPAssetExporter sharedInstance] exportAsset:asset
-                                           toFile:mediaPath
-                                         resizing:maxImageSize
-                                 stripGeoLocation:!geoLocationEnabled
-                                completionHandler:^(BOOL success, CGSize resultingSize, NSData *thumbnailData, NSError *error) {
-        if (!success) {
-            if (completion){
-                completion(nil, error);
-            }
-            return;
-        }
-        [self.managedObjectContext performBlock:^{
-            
-            AbstractPost *post = (AbstractPost *)[self.managedObjectContext objectWithID:postObjectID];
-            Media *media = [self newMediaForPost:post];
-            media.filename = [mediaPath lastPathComponent];
-            media.absoluteLocalURL = mediaPath;
-            media.absoluteThumbnailLocalURL = [self pathForThumbnailOfFile:mediaPath];
-            [thumbnailData writeToFile:media.absoluteThumbnailLocalURL atomically:NO];
-            NSDictionary *fileAttributes = [[NSFileManager defaultManager] attributesOfItemAtPath:mediaPath error:nil];
-            // This is kind of lame, but we've been storing file size as KB so far
-            // We should store size in bytes or rename the property to avoid confusion
-            media.filesize = @([fileAttributes fileSize] / 1024);
-            media.width = @(resultingSize.width);
-            media.height = @(resultingSize.height);
-            media.mediaType = mediaType;
-            //make sure that we only return when object is properly created and saved
-            [[ContextManager sharedInstance] saveContext:self.managedObjectContext withCompletionBlock:^{
-                if (completion) {
-                    completion(media, nil);
-                }
-            }];
-        }];
-                                }];
-}
-
 - (void)uploadMedia:(Media *)media
            progress:(NSProgress **)progress
            success:(void (^)())success
@@ -394,12 +327,12 @@ NSInteger const MediaMaxImageSizeDimension = 3000;
         WPImageSource *imageSource = [WPImageSource sharedSource];
         void (^successBlock)(UIImage *) = ^(UIImage *image) {
             [self.managedObjectContext performBlock:^{
-                NSString *filePath = [self pathForFilename:[self pathForThumbnailOfFile:media.filename] supportedFileFormats:nil];
-                media.absoluteThumbnailLocalURL = filePath;            
+                NSURL *fileURL = [self urlForMediaWithFilename:[self pathForThumbnailOfFile:media.filename]
+                                                  andExtension:[self extensionForUTI:(__bridge NSString*)kUTTypeJPEG]];
+                media.absoluteThumbnailLocalURL = [fileURL path];
                 [self.managedObjectContext save:nil];
                 [[[self class] queueForResizeMediaOperations] addOperationWithBlock:^{                    
-                    NSData *data = UIImagePNGRepresentation(image);
-                    [data writeToFile:filePath atomically:YES];
+                    [image writeToURL:fileURL type:(__bridge NSString*)kUTTypeJPEG compressionQuality:0.9 metadata:nil error:nil];
                     if (success) {
                         success(image);
                     }
@@ -508,45 +441,6 @@ static NSString * const MediaDirectory = @"Media";
         index++;
     }
     return resultURL;
-}
-
-- (NSString *)pathForAsset:(ALAsset *)asset supportedFileFormats:(NSSet *)supportedFileFormats
-{
-    NSString *filename = asset.defaultRepresentation.filename;
-    return [self pathForFilename:filename supportedFileFormats:supportedFileFormats];
-
-}
-
-- (NSString *)pathForFilename:(NSString *)filename supportedFileFormats:(NSSet *)supportedFileFormats
-{
-    NSFileManager *fileManager = [NSFileManager defaultManager];
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString *documentsDirectory = [paths firstObject];
-    NSString *mediaDirectory = [documentsDirectory stringByAppendingPathComponent:MediaDirectory];
-    BOOL isDirectory;
-    NSError *error;
-    if (![fileManager fileExistsAtPath:mediaDirectory isDirectory:&isDirectory] || !isDirectory){
-        if ([fileManager createDirectoryAtPath:mediaDirectory withIntermediateDirectories:YES attributes:nil error:&error]){
-            [[NSURL fileURLWithPath:mediaDirectory] setResourceValue:@(NO) forKey:NSURLIsExcludedFromBackupKey error:nil];
-        } else {
-            DDLogError(@"%@", [error localizedDescription]);
-        }
-    }
-    NSString *path = [mediaDirectory stringByAppendingPathComponent:filename];
-    NSString *basename = [filename stringByDeletingPathExtension];
-    NSString *extension = [[filename pathExtension] lowercaseString];
-    if (supportedFileFormats && ![supportedFileFormats containsObject:extension]){
-        extension = @"png";
-        filename = [NSString stringWithFormat:@"%@.%@", basename, extension];
-        path = [mediaDirectory stringByAppendingPathComponent:filename];
-    }
-    NSUInteger index = 0;
-    while ([fileManager fileExistsAtPath:path]) {
-        NSString *alternativeFilename = [NSString stringWithFormat:@"%@-%d.%@", basename, index, extension];
-        path = [mediaDirectory stringByAppendingPathComponent:alternativeFilename];
-        index++;
-    }
-    return path;
 }
 
 - (NSString *)pathForThumbnailOfFile:(NSString *)filename
