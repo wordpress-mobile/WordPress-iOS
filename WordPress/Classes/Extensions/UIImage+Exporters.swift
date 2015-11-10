a3ae4c5ea6dfc2ba0c122a678f28a830356e6c10
Sérgio Estêvão <sergio.estevao@automattic.com>
2015-11-10 00:15:02 +0000

    Add missing method argument documentation.

--- WordPress/Classes/Extensions/UIImage+Exporters.swift
+++ WordPress/Classes/Extensions/UIImage+Exporters.swift
@@ -21,6 +21,7 @@ extension UIImage {
      - url: file url to where the asset should be exported, this must be writable location
      - type: the UTI format to use when exporting the asset
      - compressionQuality: defines the compression quality of the export. This is only relevant for type formats that support a quality parameter. Ex: jpeg
+     - metadata: the image metadata to save to file.
      */
     func writeToURL(url: NSURL, type:String, compressionQuality :Float = 0.9,  metadata:[String:AnyObject]? = nil) throws -> ()
     {
