a09743c0c7a5fdb5700a3cd0e1c4d88b0204fec9
Will Kwon <will@willkwon.com>
2015-11-10 21:33:56 -0800

    Removing default header

--- WordPress/Classes/ViewRelated/Blog/CreateNewBlog/CreateNewBlogViewController.h
+++ WordPress/Classes/ViewRelated/Blog/CreateNewBlog/CreateNewBlogViewController.h
@@ -1,11 +1,3 @@
-//
-//  CreateNewBlogViewController.h
-//  WordPress
-//
-//  Created by Will Kwon on 11/10/15.
-//  Copyright © 2015 WordPress. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface CreateNewBlogViewController : UIViewController
