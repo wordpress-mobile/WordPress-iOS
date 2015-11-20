2271d464b0eab88fd1e3685aebd5a580c722a7c4
Jorge Bernal <jbernal@gmail.com>
2015-11-20 11:07:30 +0100

    Adds ImmuTable tests

---
+++ WordPress/WordPressTest/ImmuTableTest.swift
@@ -0,0 +1,60 @@
+//
+//  ImmuTableTest.swift
+//  WordPress
+//
+//  Created by Jorge Bernal Ordovas on 20/11/15.
+//  Copyright © 2015 WordPress. All rights reserved.
+//
+
+import XCTest
+@testable import WordPress
+
+class ImmuTableTest: XCTestCase {
+
+    func testRegisterRows() {
+        let mockTable = MockTableView()
+        let rowsToRegister: [ImmuTableRow.Type] = [
+            BasicImmuTableRow.self,
+            ImageImmuTableRow.self,
+            TestImmuTableRow.self
+        ]
+
+        ImmuTable.registerRows(rowsToRegister, tableView: mockTable)
+        XCTAssertEqual(2, mockTable.registeredClasses.count, "Each cell class shouldn't be registered more than once")
+    }
+
+}
+
+class TestTableViewCell: UITableViewCell {}
+
+struct BasicImmuTableRow: CustomImmuTableRow {
+    typealias CellType = UITableViewCell
+    let title: String
+    var action: ImmuTableActionType? = nil
+    func configureCell(cell: UITableViewCell) {
+    }
+}
+
+struct ImageImmuTableRow: CustomImmuTableRow {
+    typealias CellType = UITableViewCell
+    let title: String
+    let image: UIImage
+    var action: ImmuTableActionType? = nil
+    func configureCell(cell: UITableViewCell) {
+    }
+}
+
+struct TestImmuTableRow: CustomImmuTableRow {
+    typealias CellType = TestTableViewCell
+    let title: String
+    var action: ImmuTableActionType? = nil
+    func configureCell(cell: UITableViewCell) {
+    }
+}
+
+class MockTableView: CellRegistrable {
+    var registeredClasses = [(String, AnyClass)]()
+    func registerClass(cellClass: AnyClass?, forCellReuseIdentifier identifier: String) {
+        registeredClasses.append((identifier, cellClass!))
+    }
+}
