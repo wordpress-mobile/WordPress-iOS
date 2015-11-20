51b71d448e5b63065040e9cf12a4eb6d97f0da1f
Jorge Bernal <jbernal@gmail.com>
2015-11-20 18:11:56 +0100

    Added Account Settings model and remote

---
+++ WordPress/Classes/Models/AccountSettings.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+struct AccountSettings {
+    // MARK: - My Profile
+    let firstName: String   // first_name
+    let lastName: String    // last_name
+    let displayName: String // display_name
+    let aboutMe: String     // description
+
+    // MARK: - Account Settings
+    let username: String    // user_login
+    let email: String       // user_email
+    let primarySiteID: Int  // primary_site_ID
+    let webAddress: String  // user_URL
+    let language: String    // language
+}
