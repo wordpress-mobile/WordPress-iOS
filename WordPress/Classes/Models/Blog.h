1836dd792d4998a6130a2637620ea2f12f8efa7d
Jorge Leandro Perez <jorge@lantean.co>
2015-11-20 14:41:29 -0300

    Blog: Nuking privacy Wrapper

--- WordPress/Classes/Models/Blog.h
+++ WordPress/Classes/Models/Blog.h
@@ -83,7 +83,6 @@ typedef NS_ENUM(NSInteger, SiteVisibility) {
 @property (nonatomic, strong, readwrite) NSString *blogTagline;
 @property (nonatomic, assign, readwrite) NSNumber *defaultCategoryID;
 @property (nonatomic, strong, readwrite) NSString *defaultPostFormat;
-@property (nonatomic, assign, readwrite) NSNumber *privacy;
 
 /**
  *  @details    Maps to a BlogSettings instance, which contains a collection of the available preferences, 
