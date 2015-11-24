fcab146eb5998be9ac27538690e894e706c4f678
Jorge Bernal <jbernal@gmail.com>
2015-11-24 19:47:21 +0100

    Updated OHHTTPStubs helper to the new API

--- WordPress/WordPressTest/OHHTTPStubs+Helpers.swift
+++ WordPress/WordPressTest/OHHTTPStubs+Helpers.swift
@@ -6,15 +6,15 @@ extension OHHTTPStubs
     public static func stubRequestsWithPath(path: String, responseJsonEncodedFile: String) {
         let contentTypeJson = "application/json"
         
-        OHHTTPStubs.shouldStubRequestsPassingTest({ (request: NSURLRequest!) -> Bool in
+        OHHTTPStubs.stubRequestsPassingTest({ (request: NSURLRequest) -> Bool in
                 let range = request.URL?.absoluteString.rangeOfString(path)
                 return range != nil
             
             },
-            withStubResponse: { (request: NSURLRequest!) -> OHHTTPStubsResponse! in
-                return OHHTTPStubsResponse(file: responseJsonEncodedFile,
-                                    contentType: contentTypeJson,
-                                   responseTime: OHHTTPStubsDownloadSpeedWifi)
+            withStubResponse: { (request: NSURLRequest!) -> OHHTTPStubsResponse in
+                return OHHTTPStubsResponse(fileAtPath: responseJsonEncodedFile,
+                    statusCode: 200,
+                    headers: ["Content-Type": contentTypeJson])
         })
     }
 }
