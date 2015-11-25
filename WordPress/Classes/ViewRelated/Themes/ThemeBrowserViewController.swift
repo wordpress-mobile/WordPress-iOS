9cc72a1470c89c311ac7dcea8ed944f0d051c3d6
Alex Curylo <alex@alexcurylo.com>
2015-11-25 11:23:56 -0800

    Customized theme Demo URL renamed View

--- WordPress/Classes/ViewRelated/Themes/ThemeBrowserViewController.swift
+++ WordPress/Classes/ViewRelated/Themes/ThemeBrowserViewController.swift
@@ -35,9 +35,9 @@ public protocol ThemePresenter: class {
     var searchType: ThemeType { get set }
     
     func presentCustomizeForTheme(theme: Theme?)
-    func presentDemoForTheme(theme: Theme?)
     func presentDetailsForTheme(theme: Theme?)
     func presentSupportForTheme(theme: Theme?)
+    func presentViewForTheme(theme: Theme?)
 }
 
 @objc public class ThemeBrowserViewController : UICollectionViewController, UICollectionViewDelegateFlowLayout, NSFetchedResultsControllerDelegate, UISearchControllerDelegate, UISearchResultsUpdating, ThemePresenter, WPContentSyncHelperDelegate {
@@ -363,7 +363,7 @@ public protocol ThemePresenter: class {
             if theme.isCurrentTheme() {
                 presentCustomizeForTheme(theme)
             } else {
-                presentDemoForTheme(theme)
+                presentViewForTheme(theme)
             }
         }
     }
@@ -487,14 +487,6 @@ public protocol ThemePresenter: class {
         presentUrlForTheme(url)
     }
 
-    public func presentDemoForTheme(theme: Theme?) {
-        guard let theme = theme, url = NSURL(string: theme.demoUrl) else {
-            return
-        }
-        
-        presentUrlForTheme(url)
-    }
-
     public func presentDetailsForTheme(theme: Theme?) {
         guard let theme = theme, url = NSURL(string: theme.detailsUrl()) else {
             return
@@ -505,6 +497,14 @@ public protocol ThemePresenter: class {
     
     public func presentSupportForTheme(theme: Theme?) {
         guard let theme = theme, url = NSURL(string: theme.supportUrl()) else {
+            return
+        }
+        
+        presentUrlForTheme(url)
+    }
+    
+    public func presentViewForTheme(theme: Theme?) {
+        guard let theme = theme, url = NSURL(string: theme.viewUrl()) else {
             return
         }
         
