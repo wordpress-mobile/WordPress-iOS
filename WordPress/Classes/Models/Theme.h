e0bc4f4c4e372a9b8ed5bf229dd52bd3f7ffcd60
Alex Curylo <alex@alexcurylo.com>
2015-11-26 16:16:00 -0800

    Add 'author', 'authorUrl', 'purchased' attributes to Theme data model

--- WordPress/Classes/Models/Theme.h
+++ WordPress/Classes/Models/Theme.h
@@ -12,10 +12,13 @@
 @property (nonatomic, retain) NSString *screenshotUrl;
 @property (nonatomic, retain) NSNumber *trendingRank;
 @property (nonatomic, retain) NSString *version;
+@property (nonatomic, retain) NSString *author;
+@property (nonatomic, retain) NSString *authorUrl;
 @property (nonatomic, retain) NSArray *tags;
 @property (nonatomic, retain) NSString *name;
 @property (nonatomic, retain) NSString *previewUrl;
 @property (nonatomic, retain) NSString *price;
+@property (nonatomic, retain) NSNumber *purchased;
 @property (nonatomic, retain) NSString *demoUrl;
 @property (nonatomic, retain) NSString *stylesheet;
 @property (nonatomic, retain) NSNumber *order;
