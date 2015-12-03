8dedb53ad3824b55b38db853b3be3d144232cf41
Jorge Bernal <jbernal@gmail.com>
2015-12-03 15:54:23 +0100

    Updated tests to match the new rounding behaviour

--- WordPress/WordPressTest/ViewRelated/Cells/MediaSizeSliderCellTest.swift
+++ WordPress/WordPressTest/ViewRelated/Cells/MediaSizeSliderCellTest.swift
@@ -18,20 +18,18 @@ class MediaSizeSliderCellTest: XCTestCase {
 
         model.step  = 60
 
-        model.value = 100
+        model.value = 80
         XCTAssertEqual(model.value, 100)
-        model.value = 105
-        XCTAssertEqual(model.value, 100)
-        model.value = 295
-        // FIXME: (@koke 2015-12-02)
-        // IMO this should round to 300 as it's the nearest multiple, but for now
-        // I'm just moving code around. I'll fix the rounding later.
-        XCTAssertEqual(model.value, 240)
-//        XCTAssertEqual(model.value, 300)
-        model.value = 300
-        XCTAssertEqual(model.value, 300)
-        model.value = 150
+        model.value = 110
         XCTAssertEqual(model.value, 120)
+        model.value = 150
+        XCTAssertEqual(model.value, 180)
+        model.value = 295
+        XCTAssertEqual(model.value, 300)
+        model.value = 301
+        XCTAssertEqual(model.value, 300)
+        model.value = 360
+        XCTAssertEqual(model.value, 300)
     }
 
 }
