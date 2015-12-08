ed0b1f9cce6ab6a202f742685f7f034492fe6dfd
Jorge Bernal <jbernal@gmail.com>
2015-12-08 19:46:34 +0100

    Added more documentation for ImmuTable

--- WordPress/Classes/Utility/ImmuTable.swift
+++ WordPress/Classes/Utility/ImmuTable.swift
@@ -89,13 +89,35 @@ extension UITableView: CellRegistrator {
 
  Each row contains the model necessary to configure a specific type of UITableViewCell.
 
+ To use ImmuTable, first you need to create some custom rows. An example row for a cell
+ that acts as a button which performs a destructive action could look like this:
+
+     struct DestructiveButtonRow: ImmuTableRow {
+         static let cell = ImmuTableCell.Class(UITableViewCell.self)
+         let title: String
+         let action: ImmuTableActionType?
+
+         func configureCell(cell: UITableViewCell) {
+             cell.textLabel?.text = title
+             cell.textLabel?.textAlignment = .Center
+             cell.textLabel?.textColor = UIColor.redColor()
+         }
+     }
+
+ The easiest way to use ImmuTable is through ImmuTableViewHandler, which takes a
+ UITableViewController as an argument, and acts as the table view delegate and data
+ source. You would then assign an `ImmuTable` object to the handler's `viewModel`
+ property.
+
  - attention: before using any ImmuTableRow type, you need to call `registerRows(_:tableView:)`
  passing the row type. This is needed so ImmuTable can register the class or nib with the table view.
  If you fail to do this, UIKit will raise an exception when it tries to load the row.
  */
 public struct ImmuTable {
+    /// An array of the sections to be represented in the table view
     public let sections: [ImmuTableSection]
 
+    /// Initializes an ImmuTable object with the given sections
     public init(sections: [ImmuTableSection]) {
         self.sections = sections
     }
