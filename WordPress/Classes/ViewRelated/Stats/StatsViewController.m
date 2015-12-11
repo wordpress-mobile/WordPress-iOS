5389040ce3715bb2db9722403936c60a1e058209
Aaron Douglas <astralbodies@gmail.com>
2015-12-11 07:59:48 -0600

    Switch to WPWebViewController.

--- WordPress/Classes/ViewRelated/Stats/StatsViewController.m
+++ WordPress/Classes/ViewRelated/Stats/StatsViewController.m
@@ -2,8 +2,6 @@
 #import "Blog.h"
 #import "WordPressAppDelegate.h"
 #import "JetpackSettingsViewController.h"
-#import "StatsWebViewController.h"
-#import "WPChromelessWebViewController.h"
 #import "WPAccount.h"
 #import "ContextManager.h"
 #import "BlogService.h"
@@ -14,6 +12,7 @@
 #import <WordPressShared/WPNoResultsView.h>
 #import "WordPress-Swift.h"
 #import "WPAppAnalytics.h"
+#import "WPWebViewController.h"
 
 static NSString *const StatsBlogObjectURLRestorationKey = @"StatsBlogObjectURL";
 
@@ -162,9 +161,8 @@ static NSString *const StatsBlogObjectURLRestorationKey = @"StatsBlogObjectURL";
 
 - (void)statsViewController:(WPStatsViewController *)controller openURL:(NSURL *)url
 {
-    WPChromelessWebViewController *vc = [[WPChromelessWebViewController alloc] init];
-    [vc loadPath:url.absoluteString];
-    [self.navigationController pushViewController:vc animated:YES];
+    WPWebViewController *webVC = [WPWebViewController authenticatedWebViewController:url];
+    [self.navigationController pushViewController:webVC animated:YES];
 }
 
 
