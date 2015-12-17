b7dd886fcdd010e420dbafe892b48cf60b28c1cc
Jorge Bernal <jbernal@gmail.com>
2015-12-17 15:10:51 +0100

    Remove extra guard

--- WordPress/Classes/ViewRelated/Settings/AboutViewController.swift
+++ WordPress/Classes/ViewRelated/Settings/AboutViewController.swift
@@ -106,7 +106,6 @@ public class AboutViewController : UITableViewController
     }
 
     public override func tableView(tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
-        guard let view = view as? UITableViewHeaderFooterView else { return }
         WPStyleGuide.configureTableViewSectionFooter(view)
         view.textLabel?.textAlignment = .Center
     }
