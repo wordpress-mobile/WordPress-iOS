9b9d2920f2b6f8d95b0b8f3fa1cf1e52868392e3
Brent Coursey <brent@kurzee.com>
2015-12-17 21:19:51 -0600

    Setting up selection view.

--- WordPress/Classes/ViewRelated/Menus/Views/MenuItemTypeSelectionView.m
+++ WordPress/Classes/ViewRelated/Menus/Views/MenuItemTypeSelectionView.m
@@ -1,5 +1,11 @@
 #import "MenuItemTypeSelectionView.h"
 
+@interface MenuItemTypeSelectionView ()
+
+@property (nonatomic, strong) UIStackView *stackView;
+
+@end
+
 @implementation MenuItemTypeSelectionView
 
 - (void)awakeFromNib
@@ -7,6 +13,16 @@
     [super awakeFromNib];
     
     self.backgroundColor = [UIColor whiteColor];
+    self.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    {
+        UIStackView *stackView = [[UIStackView alloc] init];
+        
+        
+        
+        [self addSubview:stackView];
+        self.stackView = stackView;
+    }
 }
 
 @end
