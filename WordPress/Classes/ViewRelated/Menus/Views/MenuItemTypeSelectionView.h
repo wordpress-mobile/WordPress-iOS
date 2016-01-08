1dd164577d8e87966d9650f30e3ea1a9e5c7fa13
Brent Coursey <brent@kurzee.com>
2016-01-08 12:34:27 -0600

    Support for multi-line labels on types, selection animations, layout fixes, design tweaks.

--- WordPress/Classes/ViewRelated/Menus/Views/MenuItemTypeSelectionView.h
+++ WordPress/Classes/ViewRelated/Menus/Views/MenuItemTypeSelectionView.h
@@ -1,5 +1,17 @@
 #import <UIKit/UIKit.h>
+#import "MenuItem.h"
+
+@protocol MenuItemTypeSelectionViewDelegate;
 
 @interface MenuItemTypeSelectionView : UIView
+
+@property (nonatomic, weak) id <MenuItemTypeSelectionViewDelegate> delegate;
+
+@end
+
+@protocol MenuItemTypeSelectionViewDelegate <NSObject>
+
+- (void)itemTypeSelectionViewChanged:(MenuItemTypeSelectionView *)typeSelectionView type:(MenuItemType)itemType;
+- (BOOL)itemTypeSelectionViewRequiresCompactLayout:(MenuItemTypeSelectionView *)typeSelectionView;
 
 @end
