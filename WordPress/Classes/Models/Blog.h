319b93b0c8db126a709e99f011237d96c22d301d
James Frost <james.frost@automattic.com>
2016-01-13 14:06:32 +0000

    Replaced Jetpack check in BlogSiteVisibilityHelper with a new BlogFeaturePrivate.
    BlogFeaturePrivate simply returns true if the blog isHostedAtWPcom.

--- WordPress/Classes/Models/Blog.h
+++ WordPress/Classes/Models/Blog.h
@@ -31,6 +31,8 @@ typedef NS_ENUM(NSUInteger, BlogFeature) {
     BlogFeaturePushNotifications,
     /// Does the blog support theme browsing?
     BlogFeatureThemeBrowsing,
+    /// Does the blog support private visibility?
+    BlogFeaturePrivate
 };
 
 typedef NS_ENUM(NSInteger, SiteVisibility) {
