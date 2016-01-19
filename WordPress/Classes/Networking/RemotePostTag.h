fca33fddec765144b51b3fbd17e54668d7c70539
kurzee <brent@kurzee.com>
2016-01-19 16:21:16 -0600

    Remote methods for getting Tags.

---
+++ WordPress/Classes/Networking/RemotePostTag.h
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+
+@interface RemotePostTag : NSObject
+
+@property (nonatomic, strong) NSNumber *tagID;
+@property (nonatomic, strong) NSString *name;
+
+@end
--- WordPress/Classes/Networking/TaxonomyServiceRemote.h
+++ WordPress/Classes/Networking/TaxonomyServiceRemote.h
@@ -1,14 +1,18 @@
 #import <Foundation/Foundation.h>
 
 @class RemotePostCategory;
+@class RemotePostTag;
 
 @protocol TaxonomyServiceRemote <NSObject>
 
-- (void)getCategoriesWithSuccess:(void (^)(NSArray *categories))success
-                       failure:(void (^)(NSError *error))failure;
+- (void)getCategoriesWithSuccess:(void (^)(NSArray <RemotePostCategory *> *categories))success
+                         failure:(void (^)(NSError *error))failure;
 
 - (void)createCategory:(RemotePostCategory *)category
                success:(void (^)(RemotePostCategory *category))success
                failure:(void (^)(NSError *error))failure;
+
+- (void)getTagsWithSuccess:(void (^)(NSArray <RemotePostTag *> *tags))success
+                   failure:(void (^)(NSError *error))failure;
 
 @end
