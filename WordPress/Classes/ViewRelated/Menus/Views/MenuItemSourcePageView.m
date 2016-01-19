26ad51ebe2d0d7a87926de5d2ca2afc843587ba8
kurzee <brent@kurzee.com>
2016-01-19 13:59:47 -0600

    Cleaning out sample MenuItemSourceView data handling.

--- WordPress/Classes/ViewRelated/Menus/Views/MenuItemSourcePageView.m
+++ WordPress/Classes/ViewRelated/Menus/Views/MenuItemSourcePageView.m
@@ -8,33 +8,6 @@
     if(self) {
         
         [self insertSearchBarIfNeeded];
-        
-        // samples
-        {
-            MenuItemSource *source = [[MenuItemSource alloc] init];
-            source.title = @"Home";
-            source.badgeTitle = @"SITE";
-            source.selected = YES;
-            
-            [self insertSource:source];
-        }
-        {
-            MenuItemSource *source = [[MenuItemSource alloc] init];
-            source.title = @"About";
-            [self insertSource:source];
-        }
-        {
-            MenuItemSource *source = [[MenuItemSource alloc] init];
-            source.title = @"Contact";
-            [self insertSource:source];
-        }
-        {
-            MenuItemSource *source = [[MenuItemSource alloc] init];
-            source.title = @"Work";
-            [self insertSource:source];
-        }
-        
-        [self.tableView reloadData];
     }
     
     return self;
