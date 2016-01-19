6fd6f309a821100065c4a1702f6c12cf89c749e6
kurzee <brent@kurzee.com>
2016-01-19 15:57:54 -0600

    Protocol rename PostCategoryServiceRemote to TaxonomyServiceRemote

--- WordPress/Classes/Networking/TaxonomyServiceRemote.h
+++ WordPress/Classes/Networking/TaxonomyServiceRemote.h
@@ -2,7 +2,7 @@
 
 @class RemotePostCategory;
 
-@protocol PostCategoryServiceRemote <NSObject>
+@protocol TaxonomyServiceRemote <NSObject>
 
 - (void)getCategoriesWithSuccess:(void (^)(NSArray *categories))success
                        failure:(void (^)(NSError *error))failure;
