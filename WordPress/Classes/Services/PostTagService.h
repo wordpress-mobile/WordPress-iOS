caf7e642cccf3d5f81cb8e24f4e7734bdaeeac04
kurzee <brent@kurzee.com>
2016-01-20 13:38:46 -0600

    Adding comments for taxonomy additions.

--- WordPress/Classes/Services/PostTagService.h
+++ WordPress/Classes/Services/PostTagService.h
@@ -4,6 +4,8 @@
 
 @interface PostTagService : LocalCoreDataService
 
+/* Fetches the associated tags for blog and replaces all currently persisted PostTag entities for blog.tags
+ */
 - (void)syncTagsForBlog:(Blog *)blog
                       success:(void (^)())success
                       failure:(void (^)(NSError *error))failure;
