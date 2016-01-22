a3d3dafcb2fa12824b30960078d7eb8645503b9c
Alex Curylo <alex@alexcurylo.com>
2016-01-22 11:10:59 -0800

    Updates Swift documentation style.

--- WordPress/Classes/Services/DeleteSiteService.swift
+++ WordPress/Classes/Services/DeleteSiteService.swift
@@ -4,22 +4,25 @@ import WordPressComAnalytics
 public extension Blog
 {
     /// Only WordPress.com hosted sites we administer may be deleted
+    ///
+    /// - Returns: Whether this blog may be deleted
+    ///
     func supportsDeleteSiteServices() -> Bool {
         return isHostedAtWPcom && isAdmin
     }
 }
 
 /// DeleteSiteService handles deletion of a user's site.
-
+///
 public class DeleteSiteService : LocalCoreDataService
 {
-    /**
-     Deletes the WordPress.com site for the specified Blog.
-     
-     - parameter blog:    The Blog whose site to delete
-     - parameter success: Optional success block with no parameters
-     - parameter failure: Optional failure block with NSError parameter
-     */
+    /// Deletes the specified WordPress.com site.
+    ///
+    /// - Parameters:
+    ///     - blog:    The Blog whose site to delete
+    ///     - success: Optional success block with no parameters
+    ///     - failure: Optional failure block with NSError
+    ///
     public func deleteSiteForBlog(blog: Blog, success: (() -> Void)?, failure: (NSError -> Void)?) {
         let blogObjectID = blog.objectID
         let remote = deleteSiteServiceRemoteForBlog(blog)
@@ -32,12 +35,12 @@ public class DeleteSiteService : LocalCoreDataService
             })
     }
     
-    /**
-     Removes Blog with the specified ID from Core Data.
-     
-     - parameter objectID: Core Data ID of the Blog to remove
-     - parameter success:  Optional success block with no parameters
-     */
+    /// Removes Blog with the specified ID from Core Data.
+    ///
+    /// - Parameters:
+    ///     - objectID: Core Data ID of the Blog to remove
+    ///     - success:  Optional success block with no parameters
+    ///
     public func removeBlogWithObjectID(objectID: NSManagedObjectID, success: (() -> Void)?) {
         managedObjectContext.performBlock {
             guard let blog = (try? self.managedObjectContext.existingObjectWithID(objectID)) as? Blog else {
@@ -63,7 +66,15 @@ public class DeleteSiteService : LocalCoreDataService
         }
     }
     
-    /// Only WordPress.com API supports delete site
+    /// Creates a remote service for site deletion
+    ///
+    /// - Note: Only WordPress.com API supports delete site
+    ///
+    /// - Parameters:
+    ///     - blog: The Blog whose site to delete
+    ///
+    /// - Returns: Remote service for site deletion
+    ///
     func deleteSiteServiceRemoteForBlog(blog: Blog) -> DeleteSiteServiceRemote {
         return DeleteSiteServiceRemote(api: blog.restApi())
     }
