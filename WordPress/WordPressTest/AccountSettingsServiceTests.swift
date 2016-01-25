5eae3de4d19eca6277369336ccb01d7e09892bad
Jorge Bernal <jbernal@gmail.com>
2016-01-25 12:17:42 +0100

    Unit tests for settings reachability

--- WordPress/WordPressTest/AccountSettingsServiceTests.swift
+++ WordPress/WordPressTest/AccountSettingsServiceTests.swift
@@ -28,6 +28,8 @@ class AccountSettingsServiceTests: XCTestCase {
         super.tearDown()
     }
 
+    // MARK: - request
+
     func testRequestSuccessful() {
         let scheduler = TestScheduler(initialClock: 0, resolution: 0.01)
         let mockRemote = MockAccountSettingsRemote()
@@ -169,6 +171,184 @@ class AccountSettingsServiceTests: XCTestCase {
             next(600, .Stalled),
             next(700, .Idle),
             completed(700)
+            ])
+    }
+
+    // MARK: - refresh
+
+    func testRefreshRepeatsSuccessfulRequest() {
+        let scheduler = TestScheduler(initialClock: 0, resolution: 0.1)
+        let mockRemote = MockAccountSettingsRemote()
+        var requestCount = 0
+        mockRemote.settings = Observable<AccountSettings>.create { observer in
+            requestCount += 1
+            return scheduler.scheduleRelativeVirtual(requestCount, dueTime: 10, action: { _ in
+                observer.on(.Next(TestData.sampleSettings))
+                observer.on(.Completed)
+                return NopDisposable.instance
+            })
+        }
+
+        let service = AccountSettingsService(userID: 123, remote: mockRemote)
+        service.testScheduler = scheduler
+
+        let res = scheduler.start {
+            service.refresh
+        }
+
+        XCTAssertEqual(requestCount, 2)
+        XCTAssertEqual(res.events, [
+            next(200, .Refreshing),
+            next(210, .Idle),
+            next(800, .Refreshing),
+            next(810, .Idle)
+            ])
+    }
+
+    func testRefreshDoesntRepeatFailedRequest() {
+        let scheduler = TestScheduler(initialClock: 0, resolution: 0.1)
+        let mockRemote = MockAccountSettingsRemote()
+        var requestCount = 0
+        let unexpected = NSError(domain: "Unexpected", code: -999, userInfo: nil)
+        mockRemote.settings = Observable<AccountSettings>.create { observer in
+            requestCount += 1
+            return scheduler.scheduleRelativeVirtual(requestCount, dueTime: 10, action: { _ in
+                observer.on(.Error(unexpected))
+                return NopDisposable.instance
+            })
+        }
+
+        let service = AccountSettingsService(userID: 123, remote: mockRemote)
+        service.testScheduler = scheduler
+
+        let res = scheduler.start {
+            service.refresh
+        }
+
+        XCTAssertEqual(requestCount, 1)
+        XCTAssertEqual(res.events, [
+            next(200, .Refreshing),
+            error(210, unexpected)
+            ])
+    }
+
+    func testRefreshDoesntRequestIfUnreachable() {
+        let scheduler = TestScheduler(initialClock: 0, resolution: 0.1)
+        let mockRemote = MockAccountSettingsRemote()
+        var requestCount = 0
+        mockRemote.settings = Observable<AccountSettings>.create { observer in
+            requestCount += 1
+            return scheduler.scheduleRelativeVirtual(requestCount, dueTime: 10, action: { _ in
+                observer.on(.Next(TestData.sampleSettings))
+                observer.on(.Completed)
+                return NopDisposable.instance
+            })
+        }
+
+        let service = AccountSettingsService(userID: 123, remote: mockRemote)
+        service.testScheduler = scheduler
+        service.testReachability = Observable.create { observer in
+            return scheduler.scheduleAbsoluteVirtual((), time: 200, action: { _ in
+                observer.on(.Next(false))
+                return NopDisposable.instance
+            })
+        }
+
+        let res = scheduler.start {
+            service.refresh
+        }
+
+        XCTAssertEqual(requestCount, 0)
+        XCTAssertEqual(res.events, [
+            next(200, .Offline),
+            ])
+
+    }
+
+    func testRefreshRetriesWhenReachable() {
+        let scheduler = TestScheduler(initialClock: 0, resolution: 0.1)
+        let mockRemote = MockAccountSettingsRemote()
+        var requestCount = 0
+        mockRemote.settings = Observable<AccountSettings>.create { observer in
+            requestCount += 1
+            return scheduler.scheduleRelativeVirtual(requestCount, dueTime: 10, action: { _ in
+                observer.on(.Next(TestData.sampleSettings))
+                observer.on(.Completed)
+                return NopDisposable.instance
+            })
+        }
+
+        let service = AccountSettingsService(userID: 123, remote: mockRemote)
+        service.testScheduler = scheduler
+        service.testReachability = Observable.create { observer in
+            scheduler.scheduleAbsoluteVirtual((), time: 200, action: { _ in
+                observer.on(.Next(false))
+                return NopDisposable.instance
+            })
+            scheduler.scheduleAbsoluteVirtual((), time: 500, action: { _ in
+                observer.on(.Next(true))
+                return NopDisposable.instance
+            })
+            scheduler.scheduleAbsoluteVirtual((), time: 800, action: { _ in
+                observer.on(.Next(false))
+                return NopDisposable.instance
+            })
+            return NopDisposable.instance
+        }
+
+        let res = scheduler.start {
+            service.refresh
+        }
+
+        XCTAssertEqual(requestCount, 1)
+        XCTAssertEqual(res.events, [
+            next(200, .Offline),
+            next(500, .Refreshing),
+            next(510, .Idle),
+            next(800, .Offline)
+            ])
+    }
+
+    func testRefreshDoesntRepeatFailedRequestAfterReachable() {
+        let scheduler = TestScheduler(initialClock: 0, resolution: 0.1)
+        let mockRemote = MockAccountSettingsRemote()
+        var requestCount = 0
+        let unexpected = NSError(domain: "Unexpected", code: -999, userInfo: nil)
+        mockRemote.settings = Observable<AccountSettings>.create { observer in
+            requestCount += 1
+            return scheduler.scheduleRelativeVirtual(requestCount, dueTime: 10, action: { _ in
+                observer.on(.Error(unexpected))
+                return NopDisposable.instance
+            })
+        }
+
+        let service = AccountSettingsService(userID: 123, remote: mockRemote)
+        service.testScheduler = scheduler
+        service.testReachability = Observable.create { observer in
+            scheduler.scheduleAbsoluteVirtual((), time: 200, action: { _ in
+                observer.on(.Next(false))
+                return NopDisposable.instance
+            })
+            scheduler.scheduleAbsoluteVirtual((), time: 500, action: { _ in
+                observer.on(.Next(true))
+                return NopDisposable.instance
+            })
+            scheduler.scheduleAbsoluteVirtual((), time: 800, action: { _ in
+                observer.on(.Next(false))
+                return NopDisposable.instance
+            })
+            return NopDisposable.instance
+        }
+
+        let res = scheduler.start {
+            service.refresh
+        }
+
+        XCTAssertEqual(requestCount, 1)
+        XCTAssertEqual(res.events, [
+            next(200, .Offline),
+            next(500, .Refreshing),
+            error(510, unexpected)
             ])
     }
 
