03feabfc774d5dc7cba0cff1ba6bf6f6e2851d36
kurzee <brent@kurzee.com>
2016-01-27 14:04:59 -0600

    Reordering Taxonomy methods.

--- WordPress/Classes/Networking/TaxonomyServiceRemote.h
+++ WordPress/Classes/Networking/TaxonomyServiceRemote.h
@@ -8,16 +8,16 @@
  */
 @protocol TaxonomyServiceRemote <NSObject>
 
-/* Fetch a list of categories associated with the site.
- */
-- (void)getCategoriesWithSuccess:(void (^)(NSArray <RemotePostCategory *> *categories))success
-                         failure:(void (^)(NSError *error))failure;
-
 /* Create a new category with the site.
  */
 - (void)createCategory:(RemotePostCategory *)category
                success:(void (^)(RemotePostCategory *category))success
                failure:(void (^)(NSError *error))failure;
+
+/* Fetch a list of categories associated with the site.
+ */
+- (void)getCategoriesWithSuccess:(void (^)(NSArray <RemotePostCategory *> *categories))success
+                         failure:(void (^)(NSError *error))failure;
 
 /* Fetch a list of tags associated with the site.
  */
