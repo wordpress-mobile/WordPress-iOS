2000ddbe1648ebef999215b67034ea16f93e6bde
kurzee <brent@kurzee.com>
2016-01-28 14:35:20 -0600

    PostTag paging via PostTagService.

--- WordPress/Classes/Services/PostTagService.h
+++ WordPress/Classes/Services/PostTagService.h
@@ -1,13 +1,19 @@
 #import "LocalCoreDataService.h"
 
 @class Blog;
+@class PostTag;
 
 @interface PostTagService : LocalCoreDataService
 
-/* Fetches the associated tags for blog and merges all currently persisted PostTag entities for blog.tags
+/* Sync an initial batch of tags for blog via default remote parameters and responses.
  */
 - (void)syncTagsForBlog:(Blog *)blog
-                      success:(void (^)())success
-                      failure:(void (^)(NSError *error))failure;
+                success:(void (^)())success
+                failure:(void (^)(NSError *error))failure;
 
+/* Sync additional tags for blog via paging maintained within an instance of PostTagService.
+ */
+- (void)loadMoreTagsForBlog:(Blog *)blog
+                    success:(void (^)(NSArray <PostTag *> *tags))success
+                    failure:(void (^)(NSError *error))failure;
 @end
