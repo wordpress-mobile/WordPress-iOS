f1947ab1d96da88b6a658800040356a27f399254
aerych <eric@automattic.com>
2016-02-02 14:01:57 -0600

    Sharing: Fetch remote sharing buttons.

---
+++ WordPress/Classes/Networking/Remote Objects/RemoteSharingButton.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+public class RemoteSharingButton
+{
+    var buttonID = ""
+    var name = ""
+    var shortname = ""
+    var custom = false
+    var enabled = false
+    var visibility: String?
+    var order = 0
+}
