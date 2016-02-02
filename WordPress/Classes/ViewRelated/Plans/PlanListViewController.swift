a14b2b8ee66fe15352027274821f1b4044968267
Jorge Bernal <jbernal@gmail.com>
2016-02-02 11:57:43 +0100

    Initial style for plan list, and add prices

--- WordPress/Classes/ViewRelated/Plans/PlanListViewController.swift
+++ WordPress/Classes/ViewRelated/Plans/PlanListViewController.swift
@@ -31,18 +31,76 @@ class PlanListViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
+        WPStyleGuide.configureTableViewSmallSubtitleCell(cell)
         let plan = availablePlans[indexPath.row]
+        let active = plan == activePlan
 
-        if plan == activePlan {
+        if active {
             cell.imageView?.image = plan.activeImage
         } else {
             cell.imageView?.image = plan.image
         }
-        cell.textLabel?.text = plan.title
+        cell.textLabel?.attributedText = attributedTitleForPlan(plan, active: active)
         cell.detailTextLabel?.text = plan.description
 
         cell.selectionStyle = .None
 
         return cell
+    }
+
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 92
+    }
+
+    // TODO: Prices should always come from StoreKit
+    // @koke 2016-02-02
+    private func priceForPlan(plan: Plan) -> String? {
+        switch plan {
+        case .Free:
+            return nil
+        case .Premium:
+            return "$99.99"
+        case .Business:
+            return "$299.99"
+        }
+    }
+
+    private func attributedTitleForPlan(plan: Plan, active: Bool) -> NSAttributedString {
+        let titleAttributes = [
+            NSFontAttributeName: WPStyleGuide.tableviewTextFont(),
+            NSForegroundColorAttributeName: WPStyleGuide.tableViewActionColor()
+        ]
+        let priceAttributes = [
+            NSFontAttributeName: WPFontManager.openSansRegularFontOfSize(14.0),
+            NSForegroundColorAttributeName: WPStyleGuide.darkGrey()
+        ]
+        let pricePeriodAttributes = [
+            NSFontAttributeName: WPFontManager.openSansItalicFontOfSize(13.0),
+            NSForegroundColorAttributeName: WPStyleGuide.greyLighten20()
+        ]
+        let planTitle = NSAttributedString(string: plan.title, attributes: titleAttributes)
+
+        let attributedTitle = NSMutableAttributedString(attributedString: planTitle)
+
+        if active {
+            let currentPlanAttributes = [
+                NSFontAttributeName: WPFontManager.openSansSemiBoldFontOfSize(11.0),
+                NSForegroundColorAttributeName: WPStyleGuide.validGreen()
+            ]
+            let currentPlan = NSLocalizedString("Current Plan", comment: "").uppercaseStringWithLocale(NSLocale.currentLocale())
+            let attributedCurrentPlan = NSAttributedString(string: currentPlan, attributes: currentPlanAttributes)
+            attributedTitle.appendString(" ")
+            attributedTitle.appendAttributedString(attributedCurrentPlan)
+        } else if let price = priceForPlan(plan) {
+            attributedTitle.appendString(" ")
+            let attributedPrice = NSAttributedString(string: price, attributes: priceAttributes)
+            attributedTitle.appendAttributedString(attributedPrice)
+
+            attributedTitle.appendString(" ")
+            let pricePeriod = NSAttributedString(string: NSLocalizedString("per year", comment: ""), attributes: pricePeriodAttributes)
+            attributedTitle.appendAttributedString(pricePeriod)
+        }
+        return attributedTitle
+
     }
 }
