c5624d419f5594222ae62eac299d832be09374b6
James Frost <james.frost@automattic.com>
2016-02-02 16:16:34 +0000

    PlanDetailViewController: Added placeholder content rows with ImmuTable.

--- WordPress/Classes/ViewRelated/Plans/PlanDetailViewController.swift
+++ WordPress/Classes/ViewRelated/Plans/PlanDetailViewController.swift
@@ -3,7 +3,13 @@ import WordPressShared
 
 class PlanDetailViewController: UITableViewController {
     var plan: Plan
-        
+    
+    private let cellIdentifier = "PlanFeatureListItem"
+    
+    private lazy var handler: ImmuTableViewHandler = {
+        return ImmuTableViewHandler(takeOver: self)
+    }()
+    
     lazy private var cancelXButton: UIBarButtonItem = {
         let button = UIBarButtonItem(image: UIImage(named: "gridicons-cross"), style: .Plain, target: self, action: "closeTapped")
         button.accessibilityLabel = NSLocalizedString("Close", comment: "Dismiss the current view")
@@ -16,24 +22,77 @@ class PlanDetailViewController: UITableViewController {
         super.init(style: .Plain)
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        title = plan.title
-        navigationItem.leftBarButtonItem = cancelXButton
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = plan.title
+        navigationItem.leftBarButtonItem = cancelXButton
+        
+        setupImmuTable()
+    }
+    
+    private func setupImmuTable() {
+        ImmuTable.registerRows([FeatureListItemRow.self], tableView: tableView)
+        
+        handler.viewModel = ImmuTable(sections:
+            [ ImmuTableSection(rows: [
+                FeatureListItemRow(title: "WordPress.com Site"),
+                FeatureListItemRow(title: "Full Access to Web Version"),
+                FeatureListItemRow(title: "A Custom Site Address", webOnly: true),
+                FeatureListItemRow(title: "No Ads"),
+                FeatureListItemRow(title: "Custom Fonts & Colors"),
+                FeatureListItemRow(title: "CSS Editing"),
+                FeatureListItemRow(title: "Video Storage & Hosting"),
+                FeatureListItemRow(title: "eCommerce", disabled: true),
+                FeatureListItemRow(title: "Premium Themes", disabled: true),
+                FeatureListItemRow(title: "Google Analytics", disabled: true),
+                FeatureListItemRow(title: "Storage Space", detailText: "13GB"),
+                FeatureListItemRow(title: "Support", detailText: "In-App & Direct Email"),
+                ])
+            ]
+        )
     }
     
     //MARK: - IBActions
     
     @IBAction private func closeTapped() {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+}
+
+struct FeatureListItemRow : ImmuTableRow {
+    static let cell = ImmuTableCell.Class(WPTableViewCellValue1)
+    
+    let title: String
+    let webOnly: Bool
+    let disabled: Bool
+    let detailText: String?
+    let action: ImmuTableAction? = nil
+    
+    private var shouldShowCheckmark: Bool {
+        return !disabled && detailText == nil
+    }
+    
+    init(title: String, webOnly: Bool = false, disabled: Bool = false, detailText: String? = nil) {
+        self.title = title
+        self.webOnly = webOnly
+        self.disabled = disabled
+        self.detailText = detailText
+    }
+    
+    func configureCell(cell: UITableViewCell) {
+        cell.textLabel?.text = title
+        cell.detailTextLabel?.text = (webOnly) ? "WEB ONLY" : detailText
+        cell.accessoryType = (shouldShowCheckmark) ? .Checkmark : .None
+        
+        WPStyleGuide.configureTableViewCell(cell)
     }
 }
