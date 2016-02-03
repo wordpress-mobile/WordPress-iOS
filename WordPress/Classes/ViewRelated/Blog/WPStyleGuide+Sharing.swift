580accaa0359082da9f33d61a45c49a61be1eb08
aerych <eric@automattic.com>
2016-02-03 16:13:19 -0600

    Sharing: Updates image. Adds some padding for the text.

--- WordPress/Classes/ViewRelated/Blog/WPStyleGuide+Sharing.swift
+++ WordPress/Classes/ViewRelated/Blog/WPStyleGuide+Sharing.swift
@@ -9,13 +9,14 @@ extension WPStyleGuide
 
     public class func sharingCellWarningAccessoryImageView() -> UIImageView {
 
-        let imageSize = 22.0
-        let imageView = UIImageView(frame:CGRect(x: 0, y: 0, width: imageSize, height: imageSize))
+        let imageSize = 20.0
+        let horizontalPadding = 8.0;
+        let imageView = UIImageView(frame:CGRect(x: 0, y: 0, width: imageSize + horizontalPadding, height: imageSize))
 
         let noticeImage = UIImage(named: "gridicons-notice")
         imageView.image = noticeImage?.imageWithRenderingMode(.AlwaysTemplate)
         imageView.tintColor = jazzyOrange()
-        
+        imageView.contentMode = .Right
         return imageView
     }
 
