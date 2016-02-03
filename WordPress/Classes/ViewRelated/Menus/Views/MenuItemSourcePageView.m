802388e4bdfddb7c4121b3e3ad9957d6d3a76413
kurzee <brent@kurzee.com>
2016-02-03 14:38:45 -0600

    Pages searching while editing MenuItems as a page.

--- WordPress/Classes/ViewRelated/Menus/Views/MenuItemSourcePageView.m
+++ WordPress/Classes/ViewRelated/Menus/Views/MenuItemSourcePageView.m
@@ -2,6 +2,12 @@
 #import "PostService.h"
 #import "Page.h"
 
+@interface MenuItemSourcePageView ()
+
+@property (nonatomic, strong) PostService *pageSearchService;
+
+@end
+
 @implementation MenuItemSourcePageView
 
 - (id)init
@@ -26,18 +32,21 @@
     NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
     NSEntityDescription *entity = [NSEntityDescription entityForName:NSStringFromClass([Page class]) inManagedObjectContext:[self managedObjectContext]];
     [fetchRequest setEntity:entity];
-    NSPredicate *defaultPredicate = [self defaultFetchRequestPredicate];
-    NSPredicate *filteredPredicate = [NSPredicate predicateWithFormat:@"original == nil && status IN %@", @[PostStatusPublish, PostStatusPrivate]];
-    
-    NSPredicate *predicate = [NSCompoundPredicate andPredicateWithSubpredicates:@[defaultPredicate, filteredPredicate]];
-    fetchRequest.predicate = predicate;
-    
+    fetchRequest.predicate = [self defaultFetchRequestPredicate];
     NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"postTitle"
                                                                    ascending:YES
                                                                     selector:@selector(caseInsensitiveCompare:)];
     [fetchRequest setSortDescriptors:[NSArray arrayWithObjects:sortDescriptor, nil]];
     
     return fetchRequest;
+}
+
+- (NSPredicate *)defaultFetchRequestPredicate
+{
+    NSPredicate *basePredicate = [super defaultFetchRequestPredicate];
+    NSPredicate *filteredPredicate = [NSPredicate predicateWithFormat:@"original == nil && status IN %@", @[PostStatusPublish, PostStatusPrivate]];
+    NSPredicate *predicate = [NSCompoundPredicate andPredicateWithSubpredicates:@[basePredicate, filteredPredicate]];
+    return predicate;
 }
 
 - (void)syncPages
@@ -60,5 +69,43 @@
     [cell setTitle:page.titleForDisplay];
 }
 
+#pragma mark - searching
+
+- (void)searchBarInputChangeDetectedForLocalResultsUpdateWithText:(NSString *)searchText
+{
+    NSPredicate *defaultPredicate = [self defaultFetchRequestPredicate];
+    NSPredicate *searchPredicate = [NSPredicate predicateWithFormat:@"postTitle CONTAINS[cd] %@", searchText];
+    NSPredicate *predicate = nil;
+    
+    if (searchText.length) {
+        predicate = [NSCompoundPredicate andPredicateWithSubpredicates:@[defaultPredicate, searchPredicate]];
+    } else {
+        predicate = defaultPredicate;
+    }
+    
+    if ([self.resultsController.fetchRequest.predicate isEqual:predicate]) {
+        // same predicate, no update needed
+        return;
+    }
+    
+    DDLogDebug(@"MenuItemSourcePageView: Updating fetch request predicate");
+    self.resultsController.fetchRequest.predicate = predicate;
+    [self performResultsControllerFetchRequest];
+    [self.tableView reloadData];
+}
+
+- (void)searchBarInputChangeDetectedForRemoteResultsUpdateWithText:(NSString *)searchText
+{
+    if (!searchText.length) {
+        return;
+    }
+    
+    if (!self.pageSearchService) {
+        self.pageSearchService = [[PostService alloc] initWithManagedObjectContext:[self managedObjectContext]];
+    }
+    
+    DDLogDebug(@"MenuItemSourcePageView: Searching posts via PostService");
+    [self.pageSearchService searchPostsWithQuery:searchText ofType:PostServiceTypePage withStatuses:@[PostStatusPrivate, PostStatusPublish] forBlog:[self blog] success:nil failure:nil];
+}
 
 @end
