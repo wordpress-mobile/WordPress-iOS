f6f86739d696f48d0d87310fc0c726960ecaca52
Jorge Bernal <jbernal@gmail.com>
2016-02-04 10:58:41 +0100

    Rewrite plans list with ImmuTable

--- WordPress/Classes/ViewRelated/Plans/PlanListViewController.swift
+++ WordPress/Classes/ViewRelated/Plans/PlanListViewController.swift
@@ -1,13 +1,73 @@
 import UIKit
 import WordPressShared
 
-class PlanListViewController: UITableViewController {
-    let cellIdentifier = "PlanListItem"
-    let availablePlans = [
-        Plan.Free,
-        Plan.Premium,
-        Plan.Business
+struct PlanListRow: ImmuTableRow {
+    static let cell = ImmuTableCell.Class(WPTableViewCellSubtitle)
+    static let customHeight: Float? = 92
+
+    let title: String
+    let active: Bool
+    let price: String
+    let description: String
+    let icon: UIImage
+
+    let action: ImmuTableAction? = nil
+    
+    let titleAttributes = [
+        NSFontAttributeName: WPStyleGuide.tableviewTextFont(),
+        NSForegroundColorAttributeName: WPStyleGuide.tableViewActionColor()
     ]
+    let priceAttributes = [
+        NSFontAttributeName: WPFontManager.openSansRegularFontOfSize(14.0),
+        NSForegroundColorAttributeName: WPStyleGuide.darkGrey()
+    ]
+    let pricePeriodAttributes = [
+        NSFontAttributeName: WPFontManager.openSansItalicFontOfSize(13.0),
+        NSForegroundColorAttributeName: WPStyleGuide.greyLighten20()
+    ]
+    
+    func configureCell(cell: UITableViewCell) {
+        WPStyleGuide.configureTableViewSmallSubtitleCell(cell)
+        cell.imageView?.image = icon
+        cell.textLabel?.attributedText = attributedTitle
+        cell.textLabel?.adjustsFontSizeToFitWidth = true
+        cell.detailTextLabel?.text = description
+        cell.detailTextLabel?.textColor = WPStyleGuide.grey()
+        cell.selectionStyle = .None
+    }
+
+    var attributedTitle: NSAttributedString {
+        let planTitle = NSAttributedString(string: title, attributes: titleAttributes)
+
+        let attributedTitle = NSMutableAttributedString(attributedString: planTitle)
+
+        if active {
+            let currentPlanAttributes = [
+                NSFontAttributeName: WPFontManager.openSansSemiBoldFontOfSize(11.0),
+                NSForegroundColorAttributeName: WPStyleGuide.validGreen()
+            ]
+            let currentPlan = NSLocalizedString("Current Plan", comment: "").uppercaseStringWithLocale(NSLocale.currentLocale())
+            let attributedCurrentPlan = NSAttributedString(string: currentPlan, attributes: currentPlanAttributes)
+            attributedTitle.appendString(" ")
+            attributedTitle.appendAttributedString(attributedCurrentPlan)
+        } else if !price.isEmpty {
+            attributedTitle.appendString(" ")
+            let attributedPrice = NSAttributedString(string: price, attributes: priceAttributes)
+            attributedTitle.appendAttributedString(attributedPrice)
+
+            attributedTitle.appendString(" ")
+            let pricePeriod = NSAttributedString(string: NSLocalizedString("per year", comment: ""), attributes: pricePeriodAttributes)
+            attributedTitle.appendAttributedString(pricePeriod)
+        }
+        return attributedTitle
+    }
+}
+
+final class PlanListViewController: UITableViewController {
+    private lazy var handler: ImmuTableViewHandler = {
+        return ImmuTableViewHandler(takeOver: self)
+    }()
+
     let activePlan: Plan?
 
     convenience init(blog: Blog) {
@@ -26,100 +86,47 @@ class PlanListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.registerClass(WPTableViewCellSubtitle.self, forCellReuseIdentifier: cellIdentifier)
+        WPStyleGuide.resetReadableMarginsForTableView(tableView)
         WPStyleGuide.configureColorsForView(view, andTableView: tableView)
+        ImmuTable.registerRows([PlanListRow.self], tableView: tableView)
+        bindViewModel()
     }
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return availablePlans.count
+    func bindViewModel() {
+        handler.viewModel = ImmuTable(sections: [
+            ImmuTableSection(
+                headerText: NSLocalizedString("WordPress.com Plans", comment: "Title for the Plans list header"),
+                rows: [
+                    rowForPlan(.Free),
+                    rowForPlan(.Premium),
+                    rowForPlan(.Business)
+                ])
+            ])
     }
 
-    override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return WPTableViewSectionHeaderFooterView.heightForHeader(title, width: tableView.frame.width)
-    }
+    func rowForPlan(plan: Plan) -> PlanListRow {
+        let active = (activePlan == plan)
+        let icon = active ? plan.activeImage : plan.image
 
-    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let title = NSLocalizedString("WordPress.com Plans", comment: "Title for the Plans list header")
-        let view = WPTableViewSectionHeaderFooterView(reuseIdentifier: nil, style: .Header)
-        view.title = title
-        return view
-    }
-
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
-        WPStyleGuide.configureTableViewSmallSubtitleCell(cell)
-        let plan = availablePlans[indexPath.row]
-        let active = plan == activePlan
-
-        if active {
-            cell.imageView?.image = plan.activeImage
-        } else {
-            cell.imageView?.image = plan.image
-        }
-        cell.textLabel?.attributedText = attributedTitleForPlan(plan, active: active)
-        cell.textLabel?.adjustsFontSizeToFitWidth = true
-
-        cell.detailTextLabel?.text = plan.description
-        cell.detailTextLabel?.textColor = WPStyleGuide.grey()
-
-        cell.selectionStyle = .None
-
-        return cell
-    }
-
-    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 92
+        return PlanListRow(
+            title: plan.title,
+            active: active,
+            price: priceForPlan(plan),
+            description: plan.description,
+            icon: icon
+        )
     }
 
     // TODO: Prices should always come from StoreKit
     // @koke 2016-02-02
-    private func priceForPlan(plan: Plan) -> String? {
+    private func priceForPlan(plan: Plan) -> String {
         switch plan {
         case .Free:
-            return nil
+            return ""
         case .Premium:
             return "$99.99"
         case .Business:
             return "$299.99"
         }
-    }
-
-    private func attributedTitleForPlan(plan: Plan, active: Bool) -> NSAttributedString {
-        let titleAttributes = [
-            NSFontAttributeName: WPStyleGuide.tableviewTextFont(),
-            NSForegroundColorAttributeName: WPStyleGuide.tableViewActionColor()
-        ]
-        let priceAttributes = [
-            NSFontAttributeName: WPFontManager.openSansRegularFontOfSize(14.0),
-            NSForegroundColorAttributeName: WPStyleGuide.darkGrey()
-        ]
-        let pricePeriodAttributes = [
-            NSFontAttributeName: WPFontManager.openSansItalicFontOfSize(13.0),
-            NSForegroundColorAttributeName: WPStyleGuide.greyLighten20()
-        ]
-        let planTitle = NSAttributedString(string: plan.title, attributes: titleAttributes)
-
-        let attributedTitle = NSMutableAttributedString(attributedString: planTitle)
-
-        if active {
-            let currentPlanAttributes = [
-                NSFontAttributeName: WPFontManager.openSansSemiBoldFontOfSize(11.0),
-                NSForegroundColorAttributeName: WPStyleGuide.validGreen()
-            ]
-            let currentPlan = NSLocalizedString("Current Plan", comment: "").uppercaseStringWithLocale(NSLocale.currentLocale())
-            let attributedCurrentPlan = NSAttributedString(string: currentPlan, attributes: currentPlanAttributes)
-            attributedTitle.appendString(" ")
-            attributedTitle.appendAttributedString(attributedCurrentPlan)
-        } else if let price = priceForPlan(plan) {
-            attributedTitle.appendString(" ")
-            let attributedPrice = NSAttributedString(string: price, attributes: priceAttributes)
-            attributedTitle.appendAttributedString(attributedPrice)
-
-            attributedTitle.appendString(" ")
-            let pricePeriod = NSAttributedString(string: NSLocalizedString("per year", comment: ""), attributes: pricePeriodAttributes)
-            attributedTitle.appendAttributedString(pricePeriod)
-        }
-        return attributedTitle
-
     }
 }
