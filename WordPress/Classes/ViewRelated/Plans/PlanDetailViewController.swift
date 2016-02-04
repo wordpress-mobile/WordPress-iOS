688d4907b2bc7cc063da274c9b38abb625bba6ed
James Frost <james.frost@automattic.com>
2016-02-04 15:37:50 +0000

    Extracted PurchaseButton from PlanDetailViewController.

--- WordPress/Classes/ViewRelated/Plans/PlanDetailViewController.swift
+++ WordPress/Classes/ViewRelated/Plans/PlanDetailViewController.swift
@@ -103,7 +103,7 @@ class PlanDetailViewController: UITableViewController {
     }
 }
 
-// MARK: Table View Data Source
+// MARK: Table View Data Source / Delegate
 extension PlanDetailViewController {
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return viewModel.sections.count
@@ -187,51 +187,3 @@ class PlanFeatureListCell: UITableViewCell {
         titleLabel.text = nil
     }
 }
-
-@IBDesignable
-class BorderedButton: UIButton {
-    @IBInspectable var cornerRadius: CGFloat = 3.0 {
-        didSet {
-            updateAppearance()
-        }
-    }
-    
-    @IBInspectable var borderWidth: CGFloat = 1.0 {
-        didSet {
-            updateAppearance()
-        }
-    }
-    
-    override func tintColorDidChange() {
-        super.tintColorDidChange()
-        
-        updateAppearance()
-    }
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
-        setTitleColor(UIColor.whiteColor(), forState: [.Highlighted])
-    }
-    
-    override func willMoveToSuperview(newSuperview: UIView?) {
-        super.willMoveToSuperview(newSuperview)
-
-        updateAppearance()
-    }
-    
-    private func updateAppearance() {
-        contentEdgeInsets = UIEdgeInsets(top: 10.0, left: 19.0, bottom: 10.0, right: 19.0)
-        
-        layer.masksToBounds = true
-        layer.cornerRadius = cornerRadius
-        layer.borderWidth = borderWidth
-        layer.borderColor = tintColor.CGColor
-        layer.borderColor = tintColor.CGColor
-        
-        setTitleColor(tintColor, forState: .Normal)
-        
-        setBackgroundImage(UIImage(color: tintColor), forState: .Highlighted)
-    }
-}
-
