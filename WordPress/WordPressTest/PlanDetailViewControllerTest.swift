87ff3aaf4b4aeb2647056109e387a1fbbe37926b
James Frost <james.frost@automattic.com>
2016-02-08 15:23:21 +0000

    PlanDetailViewController: Replaced custom cell w/ WPTableViewCellValue1

---
+++ WordPress/WordPressTest/PlanDetailViewControllerTest.swift
@@ -0,0 +1,9 @@
+//
+//  PlanDetailViewControllerTest.swift
+//  WordPress
+//
+//  Created by James Frost on 08/02/2016.
+//  Copyright © 2016 WordPress. All rights reserved.
+//
+
+import Foundation
