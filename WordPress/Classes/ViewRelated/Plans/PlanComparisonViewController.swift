79893050338d5ad17c0e3ce7f8e23e425af9a686
James Frost <james.frost@automattic.com>
2016-02-12 11:36:06 +0000

    Convert PlanComparison to use a scrollview instead of pageviewcontroller

--- WordPress/Classes/ViewRelated/Plans/PlanComparisonViewController.swift
+++ WordPress/Classes/ViewRelated/Plans/PlanComparisonViewController.swift
@@ -4,22 +4,26 @@ import WordPressShared
 class PlanComparisonViewController: UIViewController {
     private let embedIdentifier = "PageViewControllerEmbedSegue"
     
-    var pageViewController: UIPageViewController!
-    
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var divider: UIView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var planStackView: UIStackView!
     
     var currentPlan: Plan = .Free {
         didSet {
-            title = currentPlan.title
-            
-            updatePageControl()
+            if currentPlan != oldValue {
+                updateForCurrentPlan()
+            }
         }
     }
-    
-    private var currentIndex: Int? {
-        return allPlans.indexOf(currentPlan)
+
+    private var currentIndex: Int {
+        return allPlans.indexOf(currentPlan) ?? 0
     }
+    
+    private lazy var viewControllers: [PlanDetailViewController] = {
+        return self.allPlans.map { PlanDetailViewController.controllerWithPlan($0) }
+    }()
     
     private let allPlans = [Plan.Free, Plan.Premium, Plan.Business]
     
@@ -49,17 +53,44 @@ class PlanComparisonViewController: UIViewController {
         
         navigationItem.leftBarButtonItem = cancelXButton
         
-        updatePageControl()
+        initializePlanDetailViewControllers()
+        updateForCurrentPlan()
     }
     
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        super.prepareForSegue(segue, sender: sender)
-        
-        if let pageViewController = segue.destinationViewController as? UIPageViewController where segue.identifier == embedIdentifier {
-            self.pageViewController = pageViewController
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
+
+        // If the view is changing size (e.g. on rotation, or multitasking), scroll to the correct page boundary based on the new size
+        coordinator.animateAlongsideTransition({ context in
+            self.scrollView.setContentOffset(CGPoint(x: CGFloat(self.currentIndex) * size.width, y: 0), animated: true)
+        }, completion: nil)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        scrollToPage(currentIndex, animated: false)
+    }
+
+    func initializePlanDetailViewControllers() {
+        for controller in viewControllers {
+            addChildViewController(controller)
             
-            configurePageViewController()
+            controller.view.translatesAutoresizingMaskIntoConstraints = false
+            planStackView.addArrangedSubview(controller.view)
+            
+            controller.view.widthAnchor.constraintEqualToAnchor(view.widthAnchor, multiplier: 1.0).active = true
+            controller.view.topAnchor.constraintEqualToAnchor(view.topAnchor).active = true
+            controller.view.bottomAnchor.constraintEqualToAnchor(divider.topAnchor).active = true
+            
+            controller.didMoveToParentViewController(self)
         }
+    }
+    
+    func updateForCurrentPlan() {
+        title = currentPlan.title
+        
+        updatePageControl()
     }
     
     // MARK: - IBActions
@@ -69,103 +100,60 @@ class PlanComparisonViewController: UIViewController {
     }
     
     @IBAction func pageControlChanged() {
-        guard let index = currentIndex else { return }
-        
-        if pageControl.currentPage > index {
-            scrollToNextPage() { success in
-                if success {
-                    self.currentPlan = self.allPlans[index + 1]
-                }
-            }
-        } else if pageControl.currentPage < index {
-            scrollToPreviousPage() { success in
-                if success {
-                    self.currentPlan = self.allPlans[index - 1]
-                }
-            }
-        }
-    }
-
-    // MARK: - PageViewController
-    private func configurePageViewController() {
-        pageViewController.dataSource = self
-        pageViewController.delegate = self
-        
-        if let index = currentIndex,
-            let initialViewController = viewControllerAtIndex(index) {
-                
-            pageViewController.setViewControllers([initialViewController], direction: .Forward, animated: false, completion: nil)
-        }
-    }
-    
-    private func viewControllerAtIndex(index: Int) -> UIViewController? {
-        guard index >= 0 && index < allPlans.count  else {
-            return nil
+        guard !scrollView.dragging else {
+            // If the user is currently dragging, reset the change and ignore it
+            pageControl.currentPage = currentIndex
+            return
         }
         
-        let plan = allPlans[index]
-        return PlanDetailViewController.controllerWithPlan(plan)
-    }
-    
-    private func indexOfViewController(viewController: PlanDetailViewController) -> Int {
-        let plan = viewController.plan
-        return allPlans.indexOf(plan)!
+        // Stop the user interacting whilst we animate a scroll
+        scrollView.userInteractionEnabled = false
+        
+        var targetPage = currentIndex
+        if pageControl.currentPage > currentIndex {
+            targetPage += 1
+        } else if pageControl.currentPage < currentIndex {
+            targetPage -= 1
+        }
+        
+        if allPlans.indices.contains(targetPage) {
+            scrollToPage(targetPage, animated: true)
+            currentPlan = allPlans[targetPage]
+        }
     }
     
     private func updatePageControl() {
-        if let index = currentIndex {
-            pageControl?.currentPage = index
-        }
+        pageControl?.currentPage = currentIndex
     }
 }
 
-extension PlanComparisonViewController: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
-    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
-        return previousViewController()
-    }
-    
-    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
-        return nextViewController()
-    }
-    
-    private func previousViewController() -> UIViewController? {
-        guard let index = currentIndex else { return nil }
-        
-        return viewControllerAtIndex(index - 1)
-    }
-    
-    private func nextViewController() -> UIViewController? {
-        guard let index = currentIndex else { return nil }
-        
-        return viewControllerAtIndex(index + 1)
-    }
-    
-    func pageViewController(pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
-        if let controller = pageViewController.viewControllers?.first as? PlanDetailViewController {
-            currentPlan = allPlans[indexOfViewController(controller)]
-            UIAccessibilityPostNotification(UIAccessibilityAnnouncementNotification, currentPlan.title)
+extension PlanComparisonViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        // Ignore programmatic scrolling
+        if scrollView.dragging {
+            currentPlan = allPlans[currentScrollViewPage()]
         }
     }
+    
+    func scrollViewDidEndScrollingAnimation(scrollView: UIScrollView) {
+        scrollView.userInteractionEnabled = true
+        
+        UIAccessibilityPostNotification(UIAccessibilityAnnouncementNotification, currentPlan.title)
+    }
+    
+    private func currentScrollViewPage() -> Int {
+        // Calculate which plan's VC is at the center of the view
+        let pageWidth = scrollView.bounds.width
+        let centerX = scrollView.contentOffset.x + (pageWidth / 2)
+        let currentPage = Int(floor(centerX / pageWidth))
 
-    /// `success` is if the scrolling action was successful (i.e. there was a page to scroll to)
-    private func scrollToNextPage(completion: ((success: Bool) -> ())? = nil) {
-        guard let nextViewController = nextViewController() else {
-            completion?(success: false)
-            return
-        }
-        
-        pageViewController.setViewControllers([nextViewController], direction: .Forward, animated: true, completion: nil)
-        completion?(success: true)
+        // Keep it within bounds
+        return max(min(currentPage, allPlans.count - 1), 0)
     }
     
-    /// `success` is true if the scrolling action was successful (i.e. there was a page to scroll to)
-    private func scrollToPreviousPage(completion: ((success: Bool) -> ())? = nil) {
-        guard let previousViewController = previousViewController() else {
-            completion?(success: false)
-            return
-        }
+    private func scrollToPage(page: Int, animated: Bool) {
+        let pageWidth = view.bounds.width
         
-        pageViewController.setViewControllers([previousViewController], direction: .Reverse, animated: true, completion: nil)
-        completion?(success: true)
+        scrollView.setContentOffset(CGPoint(x: CGFloat(page) * pageWidth, y: 0), animated: animated)
     }
 }
