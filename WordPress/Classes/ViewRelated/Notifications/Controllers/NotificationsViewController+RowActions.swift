1651e77ae99b831845efe5643d56ddaa967a8437
Jorge Leandro Perez <jorge@lantean.co>
2016-02-15 19:00:38 -0300

    NotificationsViewController+RowActions: Implements Comment Moderation

--- WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationsViewController+RowActions.swift
+++ WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationsViewController+RowActions.swift
@@ -5,7 +5,11 @@ import WordPressShared
 extension NotificationsViewController
 {
     public override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
+        guard let note = tableViewHandler.resultsController.objectAtIndexPath(indexPath) as? Notification else {
+            return false
+        }
+        
+        return note.isComment
     }
     
     public override func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
@@ -16,30 +20,51 @@ extension NotificationsViewController
         guard let note = tableViewHandler.resultsController.objectAtIndexPath(indexPath) as? Notification,
                     group = note.blockGroupOfType(NoteBlockGroupType.Comment),
                     block = group.blockOfType(.Comment) else {
-            return []
+            return nil
         }
-
-        var actions = [UITableViewRowAction]()
         
-        if block.isActionEnabled(NoteActionTrashKey) {
+        // Helpers
+        let isTrashEnabled      = block.isActionEnabled(NoteActionTrashKey)
+        let isApproveEnabled    = block.isActionEnabled(NoteActionApproveKey)
+        let isApproveOn         = block.isActionOn(NoteActionApproveKey)
+        var actions             = [UITableViewRowAction]()
+        
+        // Comments: Trash
+        if isTrashEnabled {
             let title = NSLocalizedString("Trash", comment: "Trashes a comment")
-            let handler = { (action: UITableViewRowAction, path: NSIndexPath) -> Void in
-// TODO: Implement Me
-            }
             
-            let trash = UITableViewRowAction(style: .Destructive, title: title, handler: handler)
+            let trash = UITableViewRowAction(style: .Destructive, title: title, handler: { [weak self] action, path in
+                self?.trashCommentWithBlock(block)
+                self?.tableView.setEditing(false, animated: true)
+            })
+            
             trash.backgroundColor = WPStyleGuide.errorRed()
             actions.append(trash)
         }
         
-        if block.isActionEnabled(NoteActionApproveKey) {
-            let isApproveOn         = block.isActionOn(NoteActionApproveKey)
-            let handler             = isApproveOn ? approveComment : unapproveComment
-            let title               = isApproveOn ? NSLocalizedString("Unapprove", comment: "Unapproves a Comment") :
-                                                    NSLocalizedString("Approve",   comment: "Approves a Comment")
+        // Comments: Unapprove
+        if isApproveEnabled && isApproveOn {
+            let title = NSLocalizedString("Unapprove", comment: "Unapproves a Comment")
             
-            let trash               = UITableViewRowAction(style: .Normal, title: title, handler: handler)
-            trash.backgroundColor   = isApproveOn ? WPStyleGuide.grey() : WPStyleGuide.wordPressBlue()
+            let trash = UITableViewRowAction(style: .Normal, title: title, handler: { [weak self] action, path in
+                self?.unapproveCommentWithBlock(block)
+                self?.tableView.setEditing(false, animated: true)
+            })
+            
+            trash.backgroundColor = WPStyleGuide.grey()
+            actions.append(trash)
+        }
+
+        // Comments: Approve
+        if isApproveEnabled && !isApproveOn {
+            let title = NSLocalizedString("Approve", comment: "Approves a Comment")
+            
+            let trash = UITableViewRowAction(style: .Normal, title: title, handler: { [weak self] action, path in
+                self?.approveCommentWithBlock(block)
+                self?.tableView.setEditing(false, animated: true)
+            })
+            
+            trash.backgroundColor = WPStyleGuide.wordPressBlue()
             actions.append(trash)
         }
         
@@ -47,11 +72,37 @@ extension NotificationsViewController
     }
     
     
-    private func approveComment(action: UITableViewRowAction, path: NSIndexPath) {
+    private func trashCommentWithBlock(block: NotificationBlock) {
 // TODO: Implement Me
     }
     
-    private func unapproveComment(action: UITableViewRowAction, path: NSIndexPath) {
-// TODO: Implement Me
+    private func approveCommentWithBlock(block: NotificationBlock) {
+        let context = ContextManager.sharedInstance().mainContext
+        let service = CommentService(managedObjectContext: context)
+        
+        service.approveCommentWithID(block.metaCommentID, siteID: block.metaSiteID, success: {
+                DDLogSwift.logInfo("Successfully approved comment \(block.metaSiteID).\(block.metaCommentID)")
+            },
+            failure: { error in
+                DDLogSwift.logInfo("Error while trying to moderate comment: \(error)")
+                block.removeActionOverrideForKey(NoteActionApproveKey)
+            })
+        
+        block.setActionOverrideValue(true, forKey: NoteActionApproveKey)
+    }
+    
+    private func unapproveCommentWithBlock(block: NotificationBlock) {
+        let context = ContextManager.sharedInstance().mainContext
+        let service = CommentService(managedObjectContext: context)
+        
+        service.unapproveCommentWithID(block.metaCommentID, siteID: block.metaSiteID, success: {
+                DDLogSwift.logInfo("Successfully unapproved comment \(block.metaSiteID).\(block.metaCommentID)")
+            },
+            failure: { error in
+                DDLogSwift.logInfo("Error while trying to moderate comment: \(error)")
+                block.removeActionOverrideForKey(NoteActionApproveKey)
+            })
+        
+        block.setActionOverrideValue(false, forKey: NoteActionApproveKey)
     }
 }
