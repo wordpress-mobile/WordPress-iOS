f9c93800ac56323d3035f948e378f4fdb563bd73
Jorge Leandro Perez <jorge@lantean.co>
2016-02-15 18:44:59 -0300

    Notification: Nukes didChangeOverrides public helper

--- WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationDetailsViewController.m
+++ WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationDetailsViewController.m
@@ -1117,7 +1117,6 @@ static NSString *NotificationsCommentIdKey              = @"NotificationsComment
 
 - (void)approveCommentWithBlock:(NotificationBlock *)block
 {
- 
     [WPAppAnalytics track:WPAnalyticsStatNotificationsCommentApproved withBlogID:block.metaSiteID];
     
     NSManagedObjectContext *context = [[ContextManager sharedInstance] mainContext];
@@ -1130,9 +1129,6 @@ static NSString *NotificationsCommentIdKey              = @"NotificationsComment
     }];
 
     [block setActionOverrideValue:@(true) forKey:NoteActionApproveKey];
- 
-    // Hack: force NSFetchedResultsController to reload this notification
-    [self.note didChangeOverrides];
     [self.tableView reloadData];
 }
 
@@ -1150,9 +1146,6 @@ static NSString *NotificationsCommentIdKey              = @"NotificationsComment
     }];
     
     [block setActionOverrideValue:@(false) forKey:NoteActionApproveKey];
-    
-    // Hack: force NSFetchedResultsController to reload this notification
-    [self.note didChangeOverrides];
     [self.tableView reloadData];
 }
 
