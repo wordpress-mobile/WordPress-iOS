40fae1acc20d03f9306a6219caf15988705ca883
kurzee <brent@kurzee.com>
2016-02-17 18:22:44 -0600

    Adding PostService sync option for purging previously synced posts.

--- WordPress/Classes/Services/PostServiceOptions.h
+++ WordPress/Classes/Services/PostServiceOptions.h
@@ -36,14 +36,22 @@ typedef NS_ENUM(NSUInteger, PostServiceResultsOrdering) {
 
 /**
  @class PostServiceSyncOptions
- @brief A object for passing parameters to the API when requesting specific filtering of posts.
- See each remote API for specifics regarding default values and limits.
+ @brief An object of options and paramters for specific filtering and syncing of posts.
+ See each remote API parameters for specifics regarding default values and limits.
  WP.com/REST Jetpack: https://developer.wordpress.com/docs/api/1.1/get/sites/%24site/posts/
  XML-RPC: https://codex.wordpress.org/XML-RPC_WordPress_API/Posts
  */
 @interface PostServiceSyncOptions : NSObject
 
-@property (nonatomic, strong) NSArray *statuses;
+/**
+ When set to true previously synced AbstractPosts matching statuses and authorID will be purged while updating.
+ */
+@property (nonatomic, assign) BOOL purgesLocalSync;
+
+/**
+ List of PostStatuses for which to query
+ */
+@property (nonatomic, strong) NSArray <NSString *> *statuses;
 
 /**
  The number of posts to return. Limit: 100.
