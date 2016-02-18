44293db8f4cc094ea113882266c85a6b8c605918
Jorge Bernal <jbernal@gmail.com>
2016-02-18 08:48:14 +0100

    Fix crash on Account Settings for self hosted
    I've removed most of the shared logic in SettingsCommon and only left the
    ImmuTable action helpers there. My Profile doesn't make sense without a
    service, while Account Settings still can display the app settings witout one.
    
    The table view model and error logic is still similar for both, but not the
    same, so each controller implements its own variant.
    
    I also now pass the service to the action helpers explicitly to avoid having to
    deal with optionals: the rows with actions that require a service won't be
    displayed if there's no service.

--- WordPress/Classes/ViewRelated/SettingsCommon.swift
+++ WordPress/Classes/ViewRelated/SettingsCommon.swift
@@ -1,45 +1,17 @@
 import RxSwift
 
-protocol SettingsController: ImmuTableController {
-    var service: AccountSettingsService { get }
-    func mapViewModel(settings: AccountSettings?, presenter: ImmuTablePresenter) -> ImmuTable
-}
-
-// MARK: - Shared implementation
-extension SettingsController {
-    var immutableRows: [ImmuTableRow.Type] {
-        return [
-            TextRow.self,
-            EditableTextRow.self,
-            MediaSizeRow.self,
-            SwitchRow.self]
-    }
-
-    func tableViewModelWithPresenter(presenter: ImmuTablePresenter) -> Observable<ImmuTable> {
-        return service.settings.map({ settings in
-            self.mapViewModel(settings, presenter: presenter)
-        })
-    }
-
-    var errorMessage: Observable<String?> {
-        return service.refresh
-            // replace errors with .Failed status
-            .catchErrorJustReturn(.Failed)
-            // convert status to string
-            .map({ $0.errorMessage })
-    }
-}
+protocol SettingsController: ImmuTableController {}
 
 // MARK: - Actions
 extension SettingsController {
-    func editText(changeType: (AccountSettingsChangeWithString), hint: String? = nil) -> ImmuTableRowControllerGenerator {
+    func editText(changeType: (AccountSettingsChangeWithString), hint: String? = nil, service: AccountSettingsService) -> ImmuTableRowControllerGenerator {
         return { row in
             let row = row as! EditableTextRow
-            return self.controllerForEditableText(row, changeType: changeType, hint: hint)
+            return self.controllerForEditableText(row, changeType: changeType, hint: hint, service: service)
         }
     }
 
-    func controllerForEditableText(row: EditableTextRow, changeType: (AccountSettingsChangeWithString), hint: String? = nil, isPassword: Bool = false) -> SettingsTextViewController {
+    func controllerForEditableText(row: EditableTextRow, changeType: (AccountSettingsChangeWithString), hint: String? = nil, isPassword: Bool = false, service: AccountSettingsService) -> SettingsTextViewController {
         let title = row.title
         let value = row.value
 
@@ -54,7 +26,7 @@ extension SettingsController {
             value in
 
             let change = changeType(value)
-            self.service.saveChange(change)
+            service.saveChange(change)
             DDLogSwift.logDebug("\(title) changed: \(value)")
         }
 
