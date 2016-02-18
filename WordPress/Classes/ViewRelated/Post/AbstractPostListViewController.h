06a8d4a5f8db25237430c0588c7d9b13b6defcc2
Diego Rey Mendez <diegoreymendez@gmail.com>
2016-02-18 13:37:19 -0300

    Merges the latest from develop and improves the code a bit.

--- WordPress/Classes/ViewRelated/Post/AbstractPostListViewController.h
+++ WordPress/Classes/ViewRelated/Post/AbstractPostListViewController.h
@@ -6,6 +6,11 @@
 
 @property (nonatomic, strong) Blog* __nullable blog;
 
+/**
+ *  Sets the filtering of this VC to show the posts with the specified status.
+ *
+ *  @param      status      The status of the type of post we want to show.
+ */
 - (void)setFilterWithPostStatus:(NSString* __nonnull)status;
 
 @end
