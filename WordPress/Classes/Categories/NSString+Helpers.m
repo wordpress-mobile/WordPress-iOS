2e25c8e7f1e1c53b05f10b4133393c034f5207aa
Jorge Leandro Perez <jorge@lantean.co>
2016-02-24 12:28:36 -0800

    NSString+Helpers: Importing Lumberjack

--- WordPress/Classes/Categories/NSString+Helpers.m
+++ WordPress/Classes/Categories/NSString+Helpers.m
@@ -1,5 +1,6 @@
 #import "NSString+Helpers.h"
 #import <CommonCrypto/CommonDigest.h>
+#import <CocoaLumberjack/CocoaLumberjack.h>
 #import <WordPressShared/NSString+XMLExtensions.h>
 
 static NSString *const Ellipsis =  @"\u2026";
