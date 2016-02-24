c5aa42a61b2a69f71870dfd51519bdc27d1d0768
Jorge Leandro Perez <jorge@lantean.co>
2016-02-24 14:08:17 -0800

    WPStyleGuide+Share: Cleanup

--- WordPress/WordPressShare/WPStyleGuide+Share.swift
+++ WordPress/WordPressShare/WPStyleGuide+Share.swift
@@ -4,13 +4,18 @@ import WordPressShared
 
 extension WPStyleGuide
 {
-    static func configureBlockTableViewCell(cell: UITableViewCell) {
-        WPStyleGuide.configureTableViewCell(cell)
-        cell.backgroundColor = UIColor.clearColor()
-        cell.imageView?.backgroundColor = WPStyleGuide.lightGrey()
-        cell.imageView?.layer.borderColor = UIColor.whiteColor().CGColor
-        cell.imageView?.layer.borderWidth = 1.5
-        cell.detailTextLabel?.font = WPStyleGuide.subtitleFont()
-        cell.detailTextLabel?.textColor = WPStyleGuide.greyDarken10()
+    class Share
+    {
+        static let blavatarPlaceholderImage = UIImage(named: "blavatar-default")
+        
+        static func configureBlogTableViewCell(cell: UITableViewCell) {
+            WPStyleGuide.configureTableViewCell(cell)
+            cell.backgroundColor = UIColor.clearColor()
+            cell.imageView?.backgroundColor = WPStyleGuide.lightGrey()
+            cell.imageView?.layer.borderColor = UIColor.whiteColor().CGColor
+            cell.imageView?.layer.borderWidth = 1.5
+            cell.detailTextLabel?.font = WPStyleGuide.subtitleFont()
+            cell.detailTextLabel?.textColor = WPStyleGuide.greyDarken10()
+        }
     }
 }
