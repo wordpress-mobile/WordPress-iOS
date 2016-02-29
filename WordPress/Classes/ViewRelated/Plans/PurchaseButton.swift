99112b75df2dc64a6858619a53096c6b7cfde461
James Frost <james.frost@automattic.com>
2016-02-29 12:53:20 +0000

    Simulated purchase process and animated purchase button.

--- WordPress/Classes/ViewRelated/Plans/PurchaseButton.swift
+++ WordPress/Classes/ViewRelated/Plans/PurchaseButton.swift
@@ -1,7 +1,8 @@
 import UIKit
+import MRProgress
 
 @IBDesignable
-class PurchaseButton: UIButton {
+class RoundedButton: UIButton {
     @IBInspectable var cornerRadius: CGFloat = 3.0 {
         didSet {
             updateAppearance()
@@ -59,6 +60,69 @@ class PurchaseButton: UIButton {
             setBackgroundImage(UIImage(color: tintColor), forState: .Highlighted)
         } else {
             setTitleColor(tintColor.colorWithAlphaComponent(0.3), forState: .Highlighted)
+        }
+    }
+}
+
+class PurchaseButton: RoundedButton {
+    var animatesWhenSelected: Bool = true
+
+    private var collapseConstraint: NSLayoutConstraint!
+    private lazy var activityIndicatorView: MRActivityIndicatorView = {
+        let activityView = MRActivityIndicatorView(frame: self.bounds)
+        activityView.tintColor = self.tintColor
+        activityView.lineWidth = 1.0
+        
+        self.addSubview(activityView)
+        activityView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.pinSubviewAtCenter(activityView)
+        activityView.centerXAnchor.constraintEqualToAnchor(self.centerXAnchor).active = true
+        activityView.widthAnchor.constraintEqualToAnchor(self.widthAnchor).active = true
+        activityView.widthAnchor.constraintEqualToAnchor(activityView.heightAnchor).active = true
+
+        return activityView
+    }()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        collapseConstraint = widthAnchor.constraintEqualToAnchor(heightAnchor)
+    }
+    
+    private var _cornerRadius: CGFloat = 0
+    override var selected: Bool {
+        didSet {
+            if oldValue == selected { return }
+            
+            if selected {
+                collapseConstraint.active = true
+
+                UIView.animateWithDuration(0.3, animations: {
+                    // Save the corner radius so we can restore it later
+                    self._cornerRadius = self.cornerRadius
+                    self.cornerRadius = self.bounds.height / 2
+                    self.titleLabel?.alpha = 0
+                    
+                    self.layoutIfNeeded()
+                    }, completion:  { finished in
+                        self.activityIndicatorView.startAnimating()
+                        self.borderWidth = 0
+                })
+            } else {
+                collapseConstraint.active = false
+                
+                self.activityIndicatorView.stopAnimating()
+                
+                UIView.animateWithDuration(0.3, animations: {
+                    self.cornerRadius = self._cornerRadius
+                    self.borderWidth = 1
+                    
+                    self.layoutIfNeeded()
+                    }, completion:  { finished in
+                        self.titleLabel?.alpha = 1
+                })
+            }
         }
     }
 }
