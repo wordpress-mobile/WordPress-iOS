e1886547d4af8bdba3b5adb2c59bf3a580c72c4e
Jorge Bernal <jbernal@gmail.com>
2016-03-02 15:57:43 +0100

    Fixed unit tests

--- WordPress/WordPressTest/StoreFacadeTests.swift
+++ WordPress/WordPressTest/StoreFacadeTests.swift
@@ -6,11 +6,14 @@ class StoreFacadeTests: XCTestCase {
     func testGetPricesForPlans() {
         let store = MockStoreFacade.succeeding(after: 0)
         store.getPricesForPlans([.Free, .Premium, .Business],
-            success: { prices in
-                expect(prices.count).to(equal(3))
-                expect(prices[0] as String).to(equal(""))
-                expect(prices[1] as String).to(equal("$99.88"))
-                expect(prices[2] as String).to(equal("$299.88"))
+            success: { pricedPlans in
+                expect(pricedPlans.count).to(equal(3))
+                expect(pricedPlans[0].price as String).to(equal(""))
+                expect(pricedPlans[0].plan).to(equal(Plan.Free))
+                expect(pricedPlans[1].price as String).to(equal("$99.88"))
+                expect(pricedPlans[1].plan).to(equal(Plan.Premium))
+                expect(pricedPlans[2].price as String).to(equal("$299.88"))
+                expect(pricedPlans[2].plan).to(equal(Plan.Business))
             }, failure: { _ in
                 XCTFail()
         })
@@ -22,11 +25,11 @@ class StoreFacadeTests: XCTestCase {
         store.products[1].priceLocale = NSLocale(localeIdentifier: "es-ES")
         store.getPricesForPlans(
             [.Free, .Premium, .Business],
-            success: { prices in
-                expect(prices.count).to(equal(3))
-                expect(prices[0] as String).to(equal(""))
-                expect(prices[1] as String).to(equal("99,88 €"))
-                expect(prices[2] as String).to(equal("299,88 €"))
+            success: { pricedPlans in
+                expect(pricedPlans.count).to(equal(3))
+                expect(pricedPlans[0].price as String).to(equal(""))
+                expect(pricedPlans[1].price as String).to(equal("99,88 €"))
+                expect(pricedPlans[2].price as String).to(equal("299,88 €"))
             },
             failure: { error in
                 XCTFail()
