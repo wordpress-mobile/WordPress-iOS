714d5f03b862f2e73e092296d98e22690e714580
kurzee <brent@kurzee.com>
2016-03-10 12:06:20 -0600

    Code cleanup for post type changes.

--- WordPress/Classes/Networking/Remote Objects/RemotePostType.m
+++ WordPress/Classes/Networking/Remote Objects/RemotePostType.m
@@ -6,14 +6,15 @@
 {
     NSDictionary *properties = @{
                                  @"name": self.name,
-                                 @"label": self.label
+                                 @"label": self.label,
+                                 @"apiQueryable": self.apiQueryable
                                  };
     return [NSString stringWithFormat:@"<%@: %p> (%@)", NSStringFromClass([self class]), self, properties];
 }
 
 - (NSString *)description
 {
-    return [NSString stringWithFormat:@"<%@: %p> %@[%@]", NSStringFromClass([self class]), self, self.name, self.label];
+    return [NSString stringWithFormat:@"<%@: %p> %@[%@], apiQueryable=%@", NSStringFromClass([self class]), self, self.name, self.label, self.apiQueryable];
 }
 
 @end
