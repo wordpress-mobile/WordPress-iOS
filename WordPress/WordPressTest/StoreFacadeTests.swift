47711238a48e2fca0c72d9a2a239b3d15ba72078
Jorge Bernal <jbernal@gmail.com>
2016-03-11 09:10:20 +0100

    Fixes unit tests

--- WordPress/WordPressTest/StoreFacadeTests.swift
+++ WordPress/WordPressTest/StoreFacadeTests.swift
@@ -5,15 +5,12 @@ import Nimble
 class StoreFacadeTests: XCTestCase {
     func testGetPricesForPlans() {
         let store = MockStoreFacade.succeeding(after: 0)
-        store.getPricesForPlans([.Free, .Premium, .Business],
+        store.getPricesForPlans(defaultPlans,
             success: { pricedPlans in
                 expect(pricedPlans.count).to(equal(3))
                 expect(pricedPlans[0].price as String).to(equal(""))
-                expect(pricedPlans[0].plan).to(equal(Plan.Free))
                 expect(pricedPlans[1].price as String).to(equal("$99.88"))
-                expect(pricedPlans[1].plan).to(equal(Plan.Premium))
                 expect(pricedPlans[2].price as String).to(equal("$299.88"))
-                expect(pricedPlans[2].plan).to(equal(Plan.Business))
             }, failure: { _ in
                 XCTFail()
         })
@@ -24,7 +21,7 @@ class StoreFacadeTests: XCTestCase {
         store.products[0].priceLocale = NSLocale(localeIdentifier: "es-ES")
         store.products[1].priceLocale = NSLocale(localeIdentifier: "es-ES")
         store.getPricesForPlans(
-            [.Free, .Premium, .Business],
+            defaultPlans,
             success: { pricedPlans in
                 expect(pricedPlans.count).to(equal(3))
                 expect(pricedPlans[0].price as String).to(equal(""))
