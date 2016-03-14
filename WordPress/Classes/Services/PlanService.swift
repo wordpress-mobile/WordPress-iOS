5221643e9c642c2c0f932a4894ded014158ac1e3
Jorge Bernal <jbernal@gmail.com>
2016-03-14 19:08:42 +0100

    Save active plan when we get it from the API

--- WordPress/Classes/Services/PlanService.swift
+++ WordPress/Classes/Services/PlanService.swift
@@ -13,12 +13,33 @@ struct PlanService<S: Store> {
         remote.getPlansForSite(siteID,
             success: {
                 activePlan, availablePlans in
+                PlanStorage.activatePlan(activePlan, forSite: siteID)
                 self.store.getPricesForPlans(availablePlans,
                     success: { pricedPlans in
                         let result = (activePlan: activePlan, availablePlans: pricedPlans)
                         success(result)
                     }, failure: failure)
             }, failure: failure)
+    }
+}
+
+struct PlanStorage {
+    static func activatePlan(plan: Plan, forSite siteID: Int) {
+        let manager = ContextManager.sharedInstance()
+        let context = manager.newDerivedContext()
+        let service = BlogService(managedObjectContext: context)
+        context.performBlockAndWait {
+            guard let blog = service.blogByBlogId(siteID) else {
+                let error = "Tried to activate a plan for a non-existing site (ID: \(siteID))"
+                assertionFailure(error)
+                DDLogSwift.logError(error)
+                return
+            }
+            if blog.planID != plan.id {
+                blog.planID = plan.id
+                manager.saveContextAndWait(context)
+            }
+        }
     }
 }
 
