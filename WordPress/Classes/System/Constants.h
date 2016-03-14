a2d6bf8e79249e859d7e1df37641cd392fbd961c
Jorge Leandro Perez <jorge@lantean.co>
2016-03-14 17:08:19 -0300

    Constants: Adds Comments

--- WordPress/Classes/System/Constants.h
+++ WordPress/Classes/System/Constants.h
@@ -1,11 +1,15 @@
 #import <Foundation/Foundation.h>
 
-extern NSString *const WPMobileReaderURL;
-extern NSString *const WPMobileReaderDetailURL;
 
+/// XMLRPC Constants
+///
 extern NSString *const WPComXMLRPCUrl;
 extern NSString *const WPComDefaultAccountUrlKey;
 
+/// WordPress URL's
+///
+extern NSString *const WPMobileReaderURL;
+extern NSString *const WPMobileReaderDetailURL;
 extern NSString *const WPJetpackInformationURL;
 extern NSString *const WPAutomatticMainURL;
 extern NSString *const WPAutomatticTermsOfServiceURL;
@@ -15,9 +19,13 @@ extern NSString *const WPGithubMainURL;
 extern NSString *const WPTwitterWordPressHandle;
 extern NSString *const WPTwitterWordPressMobileURL;
 
+/// Gravatar Constants
+///
 extern NSString *const WPBlavatarBaseURL;
 extern NSString *const WPGravatarBaseURL;
 
+/// Notifications Constants
+///
 extern NSString *const WPNotificationsBucketName;
 
 extern NSString *const WPAppOAuth2TokenKeychainAccessGroup;
@@ -34,11 +42,17 @@ extern NSString *const WPStatsTodayWidgetUserDefaultsSiteTimeZoneKey;
 extern NSString *const WPStatsTodayWidgetUserDefaultsViewCountKey;
 extern NSString *const WPStatsTodayWidgetUserDefaultsVisitorCountKey;
 
+/// iTunes Constants
+///
 extern NSString *const WPiTunesAppId;
 
+/// 1Password Constants
+///
 extern NSString *const WPOnePasswordWordPressTitle;
 extern NSString *const WPOnePasswordWordPressComURL;
 extern NSInteger const WPOnePasswordGeneratedMinLength;
 extern NSInteger const WPOnePasswordGeneratedMaxLength;
 
+/// Scheme Constants
+///
 extern NSString *const WPComScheme;
