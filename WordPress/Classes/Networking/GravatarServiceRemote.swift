5ebf16094840d82f9c95d018af8ea77518520b02
Jorge Leandro Perez <jorge@lantean.co>
2016-03-18 16:24:02 -0300

    GravatarServiceRemote: Documents methods

--- WordPress/Classes/Networking/GravatarServiceRemote.swift
+++ WordPress/Classes/Networking/GravatarServiceRemote.swift
@@ -1,22 +1,27 @@
 import Foundation
 
 
-///
+/// This ServiceRemote encapsulates all of the interaction with the Gravatar endpoint.
 ///
 public class GravatarServiceRemote : ServiceRemoteREST
 {
+    /// Designated Initializer
     ///
-    //
-    public override init?(api: WordPressComApi!) {
+    /// - Parameters:
+    ///     - api: A valid WordPressComApi instance, to be used by the service layer.
+    ///
+    public override init(api: WordPressComApi) {
         super.init(api: api)
-        if api == nil {
-            return nil
-        }
     }
     
+    
+    /// This method hits the Gravatar Endpoint, and uploads a new image, to be used as profile.
     ///
+    /// - Parameters:
+    ///     - image: The new Gravatar Image, to be uploaded
+    ///     - completion: An optional closure to be executed on completion.
     ///
-    public func uploadImage(image: UIImage) {
+    public func uploadImage(image: UIImage, completion: ((success: Bool, error: NSError?) -> ())?) {
         
     }
     
