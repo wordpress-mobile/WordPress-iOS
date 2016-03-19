b874bfe5cd1c92d0e4eca875ed09f4027858b4d8
Jorge Leandro Perez <jorge@lantean.co>
2016-03-19 13:21:32 -0300

    GravatarServiceRemote: Let there be Gravatar Image Upload

--- WordPress/Classes/Networking/GravatarServiceRemote.swift
+++ WordPress/Classes/Networking/GravatarServiceRemote.swift
@@ -3,15 +3,17 @@ import Foundation
 
 /// This ServiceRemote encapsulates all of the interaction with the Gravatar endpoint.
 ///
-public class GravatarServiceRemote : ServiceRemoteREST
+public class GravatarServiceRemote
 {
     /// Designated Initializer
     ///
     /// - Parameters:
-    ///     - api: A valid WordPressComApi instance, to be used by the service layer.
+    ///     - token: A valid WordPress.com User Token
+    ///     - accountEmail: Account Email
     ///
-    public override init(api: WordPressComApi) {
-        super.init(api: api)
+    public init(token: String, accountEmail: String) {
+        self.token          = token
+        self.accountEmail   = accountEmail
     }
     
     
@@ -22,20 +24,92 @@ public class GravatarServiceRemote : ServiceRemoteREST
     ///     - completion: An optional closure to be executed on completion.
     ///
     public func uploadImage(image: UIImage, completion: ((error: NSError?) -> ())?) {
-        let targetURL       = NSURL(string: gravatarEndpointURL)!
-        let request         = NSMutableURLRequest(URL: targetURL)
-        request.HTTPMethod  = HTTPPostMethod
+        guard let targetURL = NSURL(string: UploadParameters.endpointURL) else {
+            assertionFailure()
+            return
+        }
         
-        let payload         = UIImageJPEGRepresentation(image, defaultCompressionRatio)
-        let session         = NSURLSession.sharedSession()
-        session.uploadTaskWithRequest(request, fromData: payload) { (data, response, error) in
+        // Boundary
+        let boundary = boundaryForRequest()
+        
+        // Request
+        let request = NSMutableURLRequest(URL: targetURL)
+        request.HTTPMethod = UploadParameters.HTTPMethod
+        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        
+        // Body
+        let gravatarData = UIImageJPEGRepresentation(image, UploadParameters.compressionRatio)!
+        let requestBody = bodyWithGravatarData(gravatarData, account: accountEmail, boundary: boundary)
+        
+        // Task
+        let session = NSURLSession.sharedSession()
+        let task = session.uploadTaskWithRequest(request, fromData: requestBody) { (data, response, error) in
+            if let theData = data {
+                print("TEXT: \(NSString(data: theData, encoding: NSUTF8StringEncoding))")
+            }
             completion?(error: error)
         }
+        
+        task.resume()
     }
     
     
+    
+    // MARK: - Private Helpers
+    
+    /// Returns a new (randomized) Boundary String
+    ///
+    private func boundaryForRequest() -> String {
+        return "Boundary-" + NSUUID().UUIDString
+    }
+    
+    
+    /// Returns the Body for a Gravatar Upload OP.
+    ///
+    /// - Parameters:
+    ///     - gravatarData: The NSData-Encoded Image
+    ///     - account: The account that will get updated
+    ///     - boundary: The request's Boundary String
+    ///
+    /// - Returns: A NSData instance, containing the Request's Payload.
+    ///
+    private func bodyWithGravatarData(gravatarData: NSData, account: String, boundary: String) -> NSData {
+        let body = NSMutableData()
+        
+        // Image Payload
+        body.appendString("--\(boundary)\r\n")
+        body.appendString("Content-Disposition: form-data; name=\(UploadParameters.imageKey); ")
+        body.appendString("filename=\(UploadParameters.filename)\r\n")
+        body.appendString("Content-Type: \(UploadParameters.contentType);\r\n\r\n")
+        body.appendData(gravatarData)
+        body.appendString("\r\n")
+        
+        // Account Payload
+        body.appendString("--\(boundary)\r\n")
+        body.appendString("Content-Disposition: form-data; name=\"\(UploadParameters.accountKey)\"\r\n\r\n")
+        body.appendString("\(account)\r\n")
+        
+        // EOF!
+        body.appendString("--\(boundary)--\r\n")
+        
+        return body
+    }
+    
+    
+    
+    // MARK: - Private Properties
+    private let accountEmail    : String
+    private let token           : String
+    
     // MARK: - Private Constants
-    private let gravatarEndpointURL     = "https://api.gravatar.com/v1/upload-image"
-    private let HTTPPostMethod          = "POST"
-    private let defaultCompressionRatio = CGFloat(0.9)
+    private struct UploadParameters {
+        static let endpointURL          = "https://api.gravatar.com/v1/upload-image"
+        static let HTTPMethod           = "POST"
+        static let compressionRatio     = CGFloat(0.9)
+        static let contentType          = "application/octet-stream"
+        static let filename             = "test.png"
+        static let imageKey             = "filedata"
+        static let accountKey           = "account"
+    }
 }
