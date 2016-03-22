bb6e47695950cfd27630c94259d6187140f747d1
aerych <aerychj@gmail.com>
2016-03-22 11:56:58 -0500

    Sharing: Use correct context when saving.

--- WordPress/Classes/ViewRelated/Blog/SharingButtonsViewController.swift
+++ WordPress/Classes/ViewRelated/Blog/SharingButtonsViewController.swift
@@ -523,7 +523,8 @@ import WordPressShared
             tableView.reloadData()
         }
 
-        let service = BlogService(managedObjectContext: managedObjectContext)
+        let context = ContextManager.sharedInstance().mainContext
+        let service = BlogService(managedObjectContext: context)
         service.updateSettingsForBlog(self.blog, success: nil, failure: { [weak self] (error: NSError!) in
             DDLogSwift.logError(error.description)
             self?.showErrorSyncingMessage(error)
