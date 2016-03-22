6f8442fcc29676b52d82fa6a6769d018b77f1962
kurzee <brent@kurzee.com>
2016-03-22 17:58:40 -0500

    Improves layout handling for Menu header on all devices.

--- WordPress/Classes/ViewRelated/Menus/Views/MenusSelectionDetailView.m
+++ WordPress/Classes/ViewRelated/Menus/Views/MenusSelectionDetailView.m
@@ -6,7 +6,9 @@
 @interface MenusSelectionDetailView ()
 
 @property (nonatomic, weak) IBOutlet UIStackView *stackView;
-@property (nonatomic, strong) UILabel *textLabel;
+@property (nonatomic, strong) UIStackView *labelsStackView;
+@property (nonatomic, strong) UILabel *titleLabel;
+@property (nonatomic, strong) UILabel *subTitleLabel;
 @property (nonatomic, strong) UIImageView *iconView;
 @property (nonatomic, strong) UIImageView *accessoryView;
 
@@ -58,32 +60,62 @@
 - (void)setupArrangedViews
 {
     self.translatesAutoresizingMaskIntoConstraints = NO;
-    
-    UIEdgeInsets margins = UIEdgeInsetsZero;
-    margins.left = MenusDesignDefaultContentSpacing;
-    margins.right = MenusDesignDefaultContentSpacing;
-    self.stackView.layoutMargins = margins;
-    self.stackView.layoutMarginsRelativeArrangement = YES;
-    self.stackView.distribution = UIStackViewDistributionFillProportionally;
-    self.stackView.alignment = UIStackViewAlignmentCenter;
-    self.stackView.spacing = MenusDesignDefaultContentSpacing;
-    
+    {
+        UIStackView *stackView = self.stackView;
+        UIEdgeInsets margins = UIEdgeInsetsZero;
+        CGFloat spacing = MenusDesignDefaultContentSpacing;
+        if (IS_IPHONE) {
+            // If we know we're on an iPhone and the screen is 320pts, lessen the padding.
+            // Otherwise things get a bit crowded.
+            CGRect screenBounds = [UIScreen mainScreen].bounds;
+            if (screenBounds.size.width == 320 || screenBounds.size.height == 320) {
+                spacing = MenusDesignDefaultContentSpacing / 2.0;
+            }
+        }
+        margins.left = spacing;
+        margins.right = spacing;
+        stackView.layoutMargins = margins;
+        stackView.layoutMarginsRelativeArrangement = YES;
+        stackView.distribution = UIStackViewDistributionFill;
+        stackView.alignment = UIStackViewAlignmentCenter;
+        stackView.spacing = spacing;
+    }
     {
         UIImageView *iconView = [[UIImageView alloc] init];
         iconView.contentMode = UIViewContentModeScaleAspectFit;
         iconView.tintColor = [WPStyleGuide darkBlue];
         [iconView.widthAnchor constraintEqualToConstant:30].active = YES;
         [iconView.heightAnchor constraintEqualToConstant:30].active = YES;
-        
         [self.stackView addArrangedSubview:iconView];
         self.iconView = iconView;
     }
     {
+        UIStackView *stackView = [[UIStackView alloc] init];
+        stackView.alignment = UIStackViewAlignmentFill;
+        stackView.distribution = UIStackViewDistributionFill;
+        stackView.axis = UILayoutConstraintAxisVertical;
+        [stackView setContentHuggingPriority:UILayoutPriorityDefaultLow forAxis:UILayoutConstraintAxisHorizontal];
+        [self.stackView addArrangedSubview:stackView];
+        self.labelsStackView = stackView;
+    }
+    {
         UILabel *label = [[UILabel alloc] init];
         label.numberOfLines = 0;
-        self.textLabel = label;
-        [self.stackView addArrangedSubview:label];
-        [label.heightAnchor constraintEqualToAnchor:self.heightAnchor].active = YES;
+        label.font = [WPStyleGuide subtitleFont];
+        label.textColor = [WPStyleGuide grey];
+        self.subTitleLabel = label;
+        [self.labelsStackView addArrangedSubview:label];
+    }
+    {
+        UILabel *label = [[UILabel alloc] init];
+        label.numberOfLines = 1;
+        label.font = [WPStyleGuide regularTextFontSemiBold];
+        label.textColor = [WPStyleGuide darkGrey];
+        label.adjustsFontSizeToFitWidth = YES;
+        label.minimumScaleFactor = 0.50;
+        label.allowsDefaultTighteningForTruncation = YES;
+        self.titleLabel = label;
+        [self.labelsStackView addArrangedSubview:label];
     }
     {
         UIImageView *accessoryView = [[UIImageView alloc] init];
@@ -92,7 +124,6 @@
         accessoryView.tintColor = [WPStyleGuide mediumBlue];
         [accessoryView.widthAnchor constraintEqualToConstant:30].active = YES;
         [accessoryView.heightAnchor constraintEqualToConstant:30].active = YES;
-        
         [self.stackView addArrangedSubview:accessoryView];
         self.accessoryView = accessoryView;
     }
@@ -115,20 +146,11 @@
 
 - (void)setTitleText:(NSString *)title subTitleText:(NSString *)subtitle
 {
-    NSMutableAttributedString *mutableAttributedString = [[NSMutableAttributedString alloc] init];
-    {
-        NSDictionary *attributes =  @{NSFontAttributeName: [WPStyleGuide subtitleFont], NSForegroundColorAttributeName: [WPStyleGuide grey]};
-        NSAttributedString *attributedString = [[NSAttributedString alloc] initWithString:subtitle attributes:attributes];
-        [mutableAttributedString appendAttributedString:attributedString];
-    }
-    [mutableAttributedString.mutableString appendString:@"\n"];
-    {
-        NSDictionary *attributes =  @{NSFontAttributeName: [WPStyleGuide regularTextFontSemiBold], NSForegroundColorAttributeName: [WPStyleGuide darkGrey]};
-        NSAttributedString *attributedString = [[NSAttributedString alloc] initWithString:title attributes:attributes];
-        [mutableAttributedString appendAttributedString:attributedString];
-    }
+    self.subTitleLabel.text = subtitle;
+    self.titleLabel.text = title;
     
-    self.textLabel.attributedText = mutableAttributedString;
+    [self.labelsStackView setNeedsLayout];
+    [self.labelsStackView layoutIfNeeded];
 }
 
 #pragma mark - overrides
