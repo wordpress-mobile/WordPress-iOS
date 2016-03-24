19f12e90989cfa1bd610361938e454b7e42d8626
Jorge Leandro Perez <jorge@lantean.co>
2016-03-24 10:24:02 -0300

    MeHeaderView: Implements onPress

--- WordPress/Classes/ViewRelated/Me/MeHeaderView.h
+++ WordPress/Classes/ViewRelated/Me/MeHeaderView.h
@@ -1,11 +1,13 @@
 #import <UIKit/UIKit.h>
 
 extern const CGFloat MeHeaderViewHeight;
+typedef void (^MeHeaderViewCallback)(void);
 
 @interface MeHeaderView : UIView
 
 @property (nonatomic, copy) NSString *displayName;
 @property (nonatomic, copy) NSString *username;
 @property (nonatomic, copy) NSString *gravatarEmail;
+@property (nonatomic, copy) MeHeaderViewCallback onPress;
 
 @end
