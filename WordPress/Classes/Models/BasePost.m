7a893c0d53e75b9b8f69815d3802cd5d16e5d764
Diego Rey Mendez <diegoreymendez@gmail.com>
2016-03-30 11:46:20 -0300

    Renames WPContentViewProvider to PostContentProvider.

--- WordPress/Classes/Models/BasePost.m
+++ WordPress/Classes/Models/BasePost.m
@@ -187,7 +187,7 @@ NSString * const PostStatusDeleted = @"deleted"; // Returned by wpcom REST API w
 {
 }
 
-#pragma mark - WPContentViewProvider protocol
+#pragma mark - PostContentProvider protocol
 
 - (NSString *)titleForDisplay
 {
