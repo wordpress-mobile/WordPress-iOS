02831a8afa2d36c06cd85b8f43eee14f2ff59ae8
Sérgio Estêvão <sergio.estevao@automattic.com>
2016-03-30 15:50:43 +0100

    Refactor login and logout to extension.

--- WordPress/WordPressUITests/XCTest+Extensions.swift
+++ WordPress/WordPressUITests/XCTest+Extensions.swift
@@ -2,6 +2,10 @@ import XCTest
 
 extension XCTestCase {
 
+    var app: XCUIApplication {
+        get { return XCUIApplication()}
+    }
+
      public func waitForElementToAppear(element: XCUIElement,
                                         file: String = #file, line: UInt = #line) {
         let existsPredicate = NSPredicate(format: "exists == true")
@@ -15,5 +19,42 @@ extension XCTestCase {
                                                   inFile: file, atLine: line, expected: true)
             }
         }
+    }
+
+    public func logoutIfNeeded() {
+        if !app.textFields["Username / Email"].exists {
+            app.tabBars["Main Navigation"].buttons["Me"].tap()
+            app.tables.elementBoundByIndex(0).swipeUp()
+            app.tables.cells.elementBoundByIndex(5).tap()
+            app.alerts.buttons["Disconnect"].tap()
+        }
+    }
+
+    public func login() {
+        let usernameEmailTextField =  app.textFields["Username / Email"]
+        usernameEmailTextField.tap()
+        usernameEmailTextField.typeText(WordPressTestCredentials.oneStepUser)
+
+        let passwordSecureTextField = app.secureTextFields["Password"]
+        passwordSecureTextField.tap()
+        passwordSecureTextField.typeText(WordPressTestCredentials.oneStepPassword)
+
+        app.buttons["Sign In"].tap()
+
+        self.waitForElementToAppear(app.tabBars["Main Navigation"])
+    }
+
+    public func loginOther() {
+        let usernameEmailTextField =  app.textFields["Username / Email"]
+        usernameEmailTextField.tap()
+        usernameEmailTextField.typeText(WordPressTestCredentials.twoStepUser)
+
+        let passwordSecureTextField = app.secureTextFields["Password"]
+        passwordSecureTextField.tap()
+        passwordSecureTextField.typeText(WordPressTestCredentials.twoStepPassword)
+
+        app.buttons["Sign In"].tap()
+
+        self.waitForElementToAppear(app.tabBars["Main Navigation"])
     }
 }
