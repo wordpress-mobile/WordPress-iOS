63dbc0b7333d72fce58fc1802fa657f2e3eae25d
Sérgio Estêvão <sergio.estevao@automattic.com>
2016-03-30 17:25:07 +0100

    Add UI Tests for main navigation

---
+++ WordPress/WordPressUITests/MainNavigationTests.swift
@@ -0,0 +1,44 @@
+import XCTest
+
+class MainNavigationTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        // Logout first if needed
+        logoutIfNeeded()
+        login()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        logoutIfNeeded()
+        super.tearDown()
+    }
+
+    func testTabBarNavigation() {
+        app.tabBars["Main Navigation"].buttons["My Sites"].tap()
+        app.tabBars["Main Navigation"].buttons["My Sites"].tap()
+        self.waitForElementToAppear(app.tables["Blogs"])
+
+        app.tabBars["Main Navigation"].buttons["Reader"].tap()
+        self.waitForElementToAppear(app.tables["Reader"])
+
+        app.tabBars["Main Navigation"].buttons["Me"].tap()
+        self.waitForElementToAppear(app.staticTexts["Me"])
+
+        app.tabBars["Main Navigation"].buttons["Notifications"].tap()
+        self.waitForElementToAppear(app.staticTexts["Notifications"])
+    }
+
+}
+
+
