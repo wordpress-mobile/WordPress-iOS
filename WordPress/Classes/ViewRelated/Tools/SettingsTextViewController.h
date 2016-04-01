89defcf85f249301cabd0e5063fdd2173b3f6b1a
Jorge Leandro Perez <jorge@lantean.co>
2016-04-01 16:56:10 -0300

    SettingsTextViewController: Updates required initializer signature

--- WordPress/Classes/ViewRelated/Tools/SettingsTextViewController.h
+++ WordPress/Classes/ViewRelated/Tools/SettingsTextViewController.h
@@ -1,13 +1,27 @@
 #import <UIKit/UIKit.h>
 
+
+typedef void (^SettingsTextChanged)(NSString *);
+
+// Reusable component that renders a UITextField + Hint onscreen. Useful for Text / Password / Email data entry.
+//
 @interface SettingsTextViewController : UITableViewController
 
-@property (nonatomic, copy) void(^onValueChanged)(NSString *);
-@property (nonatomic, copy) void(^onCancel)();
+// Block to be executed on dismiss, if the value was effectively updated.
+//
+@property (nonatomic, copy) SettingsTextChanged onValueChanged;
 
-- (instancetype)initWithText:(NSString *)text
-                 placeholder:(NSString *)placeholder
-                        hint:(NSString *)hint
-                  isPassword:(BOOL)isPassword;
+// Indicates whether secure text entry is requried, or not.
+//
+@property (nonatomic, assign) BOOL isPassword;
+
+// Required initializer.
+//
+// Parameters:
+//  - text: The raw string (current value) to edit.
+//  - placeholder: Placeholder string to be displayed, in case the text is empty.
+//  - hint: String to be displayed at the bottom.
+//
+- (instancetype)initWithText:(NSString *)text placeholder:(NSString *)placeholder hint:(NSString *)hint;
 
 @end
