8db6faa2928fc1e807daa8598ad65dcf46aacb73
Jorge Leandro Perez <jorge@lantean.co>
2016-04-04 12:31:30 -0300

    UIViewController+Helpers: Adding isModal and isRootInNavigation

--- WordPress/Classes/Extensions/UIViewController+Helpers.swift
+++ WordPress/Classes/Extensions/UIViewController+Helpers.swift
@@ -3,11 +3,25 @@ import Foundation
 
 extension UIViewController
 {
+    // Determines if the current ViewController's View is visible and onscreen
+    //
     public func isViewOnScreen() -> Bool {
         let visibleAsRoot       = view.window?.rootViewController == self
         let visibleAsTopOnStack = navigationController?.topViewController == self && view.window != nil
         let visibleAsPresented  = view.window?.rootViewController?.presentedViewController == self
         
         return visibleAsRoot || visibleAsTopOnStack || visibleAsPresented
+    }
+    
+    // Determines if the current ViewController is being presented modally
+    //
+    public func isModal() -> Bool {
+        return self.presentingViewController != nil
+    }
+    
+    // Determines if the current ViewController is the only one in the Navigation stack
+    //
+    public func isRootInNavigation() -> Bool {
+        return navigationController?.viewControllers.count == 1
     }
 }
