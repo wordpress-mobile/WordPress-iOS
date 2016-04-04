2856119649dd589fa22ef99b09ca914093bf260f
aerych <eric@automattic.com>
2016-04-04 14:29:58 -0500

    Signin: First pass at adjusting views for keyboard.

---
+++ WordPress/Classes/ViewRelated/NUX/SigninKeyboardResponder.swift
@@ -0,0 +1,84 @@
+import UIKit
+
+///
+///
+protocol SigninKeyboardResponder: class
+{
+    var bottomContentConstraint: NSLayoutConstraint! {get}
+
+    func keyboardWillShow(notification: NSNotification)
+    func keyboardWillHide(notification: NSNotification)
+
+    func registerForKeyboardEvents(keyboardWillShowAction: Selector, keyboardWillHideAction: Selector)
+    func unregisterForKeyboardEvents()
+}
+
+extension SigninKeyboardResponder where Self: NUXAbstractViewController
+{
+
+    ///
+    ///
+    func registerForKeyboardEvents(keyboardWillShowAction: Selector, keyboardWillHideAction: Selector) {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: keyboardWillShowAction, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: keyboardWillHideAction, name: UIKeyboardWillHideNotification, object: nil)
+    }
+
+
+    ///
+    ///
+    func unregisterForKeyboardEvents() {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+    }
+
+
+    ///
+    ///
+    func keyboardWillShow(notification: NSNotification) {
+        guard let keyboardInfo = keyboardFrameAndDurationFromNotification(notification) else {
+            return
+        }
+
+        bottomContentConstraint.constant = keyboardInfo.keyboardFrame.height
+        UIView.animateWithDuration(keyboardInfo.animationDuration,
+                                   delay: 0,
+                                   options: .BeginFromCurrentState,
+                                   animations: { 
+                                        self.view.layoutIfNeeded()
+                                    },
+                                   completion: nil)
+    }
+
+
+    ///
+    ///
+    func keyboardWillHide(notification: NSNotification) {
+        guard let keyboardInfo = keyboardFrameAndDurationFromNotification(notification) else {
+                return
+        }
+
+        bottomContentConstraint.constant = 0
+        UIView.animateWithDuration(keyboardInfo.animationDuration,
+                                   delay: 0,
+                                   options: .BeginFromCurrentState,
+                                   animations: {
+                                        self.view.layoutIfNeeded()
+                                    },
+                                   completion: nil)
+    }
+
+
+    ///
+    ///
+    func keyboardFrameAndDurationFromNotification(notification: NSNotification) -> (keyboardFrame: CGRect, animationDuration: Double)? {
+
+        guard let userInfo = notification.userInfo,
+            let frame = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.CGRectValue(),
+            let duration = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue
+            else {
+                return nil
+        }
+        return (keyboardFrame: frame, animationDuration: duration)
+    }
+
+}
