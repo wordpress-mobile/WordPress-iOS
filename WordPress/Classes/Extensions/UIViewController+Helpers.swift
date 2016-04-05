c913747b677121fe1f62c98eccbe0e9c51aaf7f2
Jorge Leandro Perez <jorge@lantean.co>
2016-04-05 12:41:29 -0300

    UIViewController+Helpers: New isViewHorizontallyCompact Helper

--- WordPress/Classes/Extensions/UIViewController+Helpers.swift
+++ WordPress/Classes/Extensions/UIViewController+Helpers.swift
@@ -3,8 +3,8 @@ import Foundation
 
 extension UIViewController
 {
-    // Determines if the current ViewController's View is visible and onscreen
-    //
+    /// Determines if the current ViewController's View is visible and onscreen
+    ///
     public func isViewOnScreen() -> Bool {
         let visibleAsRoot       = view.window?.rootViewController == self
         let visibleAsTopOnStack = navigationController?.topViewController == self && view.window != nil
@@ -13,14 +13,20 @@ extension UIViewController
         return visibleAsRoot || visibleAsTopOnStack || visibleAsPresented
     }
     
-    // Determines if the current ViewController is being presented modally
-    //
+    /// Determines if the current ViewController's View is horizontally Compact
+    ///
+    public func isViewHorizontallyCompact() -> Bool {
+        return traitCollection.horizontalSizeClass == .Compact
+    }
+    
+    /// Determines if the current ViewController is being presented modally
+    ///
     public func isModal() -> Bool {
         return self.presentingViewController != nil
     }
     
-    // Determines if the current ViewController is the only one in the Navigation stack
-    //
+    /// Determines if the current ViewController is the only one in the Navigation stack
+    ///
     public func isRootInNavigation() -> Bool {
         return navigationController?.viewControllers.count == 1
     }
