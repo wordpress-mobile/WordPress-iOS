04e922a4af0934e570a00a18d6295e4f4ca2aab1
Jorge Leandro Perez <jorge@lantean.co>
2016-04-05 17:16:08 -0300

    SettingsTextViewController: Implements Modes

--- WordPress/Classes/ViewRelated/Tools/SettingsTextViewController.h
+++ WordPress/Classes/ViewRelated/Tools/SettingsTextViewController.h
@@ -1,31 +1,38 @@
 #import <UIKit/UIKit.h>
 
 
+// Typedef's
+typedef NS_ENUM(NSInteger, SettingsTextModes) {
+    SettingsTextModesText,
+    SettingsTextModesEmail,
+    SettingsTextModesPassword
+};
+
 typedef void (^SettingsTextChanged)(NSString *);
 
-// Reusable component that renders a UITextField + Hint onscreen. Useful for Text / Password / Email data entry.
-//
+/// Reusable component that renders a UITextField + Hint onscreen. Useful for Text / Password / Email data entry.
+///
 @interface SettingsTextViewController : UITableViewController
 
-// Block to be executed on dismiss, if the value was effectively updated.
-//
+/// Block to be executed on dismiss, if the value was effectively updated.
+///
 @property (nonatomic, copy) SettingsTextChanged onValueChanged;
 
-// Property to indicate whether email validation should be performed, or not.
-//
-@property (nonatomic, assign) BOOL isEmail;
+/// Sets the Text Input Mode:
+///
+/// - SettingsTextModesText: Default mode
+/// - SettingsTextModesEmail: Will perform Email validation before hitting the callback.
+/// - SettingsTextModesPassword: Secure Text Entry is enabled.
+///
+@property (nonatomic, assign) SettingsTextModes mode;
 
-// Indicates whether secure text entry is requried, or not.
-//
-@property (nonatomic, assign) BOOL isPassword;
-
-// Required initializer.
-//
-// Parameters:
-//  - text: The raw string (current value) to edit.
-//  - placeholder: Placeholder string to be displayed, in case the text is empty.
-//  - hint: String to be displayed at the bottom.
-//
+/// Required initializer.
+///
+/// Parameters:
+///  - text: The raw string (current value) to edit.
+///  - placeholder: Placeholder string to be displayed, in case the text is empty.
+///  - hint: String to be displayed at the bottom.
+///
 - (instancetype)initWithText:(NSString *)text placeholder:(NSString *)placeholder hint:(NSString *)hint;
 
 @end
