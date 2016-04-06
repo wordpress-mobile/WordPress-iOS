ff1d41abd0d93779739daf3016772f371a7f2e39
kurzee <brent@kurzee.com>
2016-04-06 15:09:03 -0500

    Adds Menus model additions.

--- WordPress/Classes/Models/Menu.h
+++ WordPress/Classes/Models/Menu.h
@@ -4,6 +4,8 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+extern NSString * const MenuDefaultID;
+
 /**
  *  @brief    An object encapsulating a Menu, the locations using it, and the items belonging to it.
  */
@@ -26,6 +28,12 @@ NS_ASSUME_NONNULL_BEGIN
 ///---------------------
 
 + (NSString *)entityName;
+
++ (Menu *)newMenu:(NSManagedObjectContext *)managedObjectContext;
++ (Menu *)defaultMenuForBlog:(Blog *)blog;
++ (Menu *)newDefaultMenu:(NSManagedObjectContext *)managedObjectContext;
++ (NSString *)defaultMenuName;
++ (NSString *)generateIncrementalNameFromMenus:(NSOrderedSet *)menus;
 
 @end
 
--- WordPress/Classes/Models/Menu.m
+++ WordPress/Classes/Models/Menu.m
@@ -1,6 +1,9 @@
 #import "Menu.h"
 #import "MenuItem.h"
 #import "MenuLocation.h"
+#import "Blog.h"
+
+NSString * const MenuDefaultID = @"0";
 
 @implementation Menu
 
@@ -15,5 +18,60 @@
 {
     return NSStringFromClass([self class]);
 }
+
++ (Menu *)newMenu:(NSManagedObjectContext *)managedObjectContext
+{
+    Menu *newMenu = [NSEntityDescription insertNewObjectForEntityForName:[Menu entityName] inManagedObjectContext:managedObjectContext];
+    return newMenu;
+}
+
++ (Menu *)defaultMenuForBlog:(Blog *)blog
+{
+    Menu *defaultMenu = nil;
+    for (Menu *menu in blog.menus) {
+        if ([menu.menuId isEqualToString:MenuDefaultID]) {
+            defaultMenu = menu;
+            break;
+        }
+    }
+    return defaultMenu;
+}
+
++ (Menu *)newDefaultMenu:(NSManagedObjectContext *)managedObjectContext
+{
+    // Create a new default menu.
+    Menu *defaultMenu = [self newMenu:managedObjectContext];
+    defaultMenu.menuId = MenuDefaultID;
+    defaultMenu.name = [self defaultMenuName];
+    return defaultMenu;
+}
+
++ (NSString *)defaultMenuName
+{
+    return NSLocalizedString(@"Default Menu", @"Menu name for the defaut menu that is automatically generated.");
+}
+
++ (NSString *)generateIncrementalNameFromMenus:(NSOrderedSet *)menus
+{
+    NSInteger highestInteger = 0;
+    for (Menu *menu in menus) {
+        if (!menu.name.length) {
+            continue;
+        }
+        NSString *nameNumberStr;
+        NSScanner *numberScanner = [NSScanner scannerWithString:menu.name];
+        NSCharacterSet *characterSet = [NSCharacterSet decimalDigitCharacterSet];
+        [numberScanner scanUpToCharactersFromSet:characterSet intoString:NULL];
+        [numberScanner scanCharactersFromSet:characterSet intoString:&nameNumberStr];
+        
+        if ([nameNumberStr integerValue] > highestInteger) {
+            highestInteger = [nameNumberStr integerValue];
+        }
+    }
+    highestInteger = highestInteger + 1;
+    NSString *menuStr = NSLocalizedString(@"Menu", @"The default text used for filling the name of a menu when creating it.");
+    return [NSString stringWithFormat:@"%@ %i", menuStr, highestInteger];
+}
+
 
 @end
