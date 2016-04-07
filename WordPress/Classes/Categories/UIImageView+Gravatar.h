f2e9a8fa2e2cae34ab5ae271d7d2852f70879331
Jorge Leandro Perez <jorge@lantean.co>
2016-04-07 14:52:05 -0300

    Reimplements UIImageView+Gravatar Helpers. In Swift. Prettier. Nicer.

--- WordPress/Classes/Categories/UIImageView+Gravatar.h
+++ WordPress/Classes/Categories/UIImageView+Gravatar.h
@@ -2,23 +2,8 @@
 #import <Foundation/Foundation.h>
 
 
-#pragma mark - Constants
-
-extern NSString *const GravatarRatingG;
-extern NSString *const GravatarRatingPG;
-extern NSString *const GravatarRatingR;
-extern NSString *const GravatarRatingX;
 
 @interface UIImageView (Gravatar)
-
-#pragma mark - Gravatar Helpers
-
-- (void)setImageWithGravatarEmail:(NSString *)emailAddress;
-- (void)setImageWithGravatarEmail:(NSString *)emailAddress gravatarRating:(NSString *)rating;
-- (void)setImageWithGravatarEmail:(NSString *)emailAddress fallbackImage:(UIImage *)fallbackImage;
-- (void)setImageWithGravatarEmail:(NSString *)emailAddress fallbackImage:(UIImage *)fallbackImage gravatarRating:(NSString *)rating;
-- (void)setImageWithGravatarEmail:(NSString *)emailAddress fallbackImage:(UIImage *)fallbackImage gravatarRating:(NSString *)rating policy:(NSURLRequestCachePolicy)policy;
-- (void)cacheGravatarImage:(UIImage *)gravatar gravatarRating:(NSString *)rating emailAddress:(NSString *)emailAddress;
 
 #pragma mark - Site Icon Helpers
 
