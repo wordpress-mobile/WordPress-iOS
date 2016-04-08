bbaf7eef1f4dde32085e44792075c8604a68e0cc
Diego Rey Mendez <diegoreymendez@gmail.com>
2016-04-08 14:35:49 -0300

    Improved AbstractPostListViewController a bit.

--- WordPress/Classes/ViewRelated/Post/AbstractPostListViewController.m
+++ WordPress/Classes/ViewRelated/Post/AbstractPostListViewController.m
@@ -20,7 +20,8 @@ const CGSize PreferredFiltersPopoverContentSize = {320.0, 220.0};
 
 const CGFloat DefaultHeightForFooterView = 44.0;
 
-@interface AbstractPostListViewController()
+@interface AbstractPostListViewController() <WPContentSyncHelperDelegate, WPNoResultsViewDelegate, WPSearchControllerDelegate, WPSearchResultsUpdating, WPTableViewHandlerDelegate>
+
 @property (nonatomic) BOOL needsRefreshCachedCellHeightsBeforeLayout;
 @end
 
