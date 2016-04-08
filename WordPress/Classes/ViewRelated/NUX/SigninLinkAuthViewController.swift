a641f94711d461c38831f60d7b6dd5a9db8f0841
aerych <eric@automattic.com>
2016-04-08 17:04:01 -0500

    Signin: Improve documentation. Adds assert.

--- WordPress/Classes/ViewRelated/NUX/SigninLinkAuthViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/SigninLinkAuthViewController.swift
@@ -3,7 +3,11 @@ import SVProgressHUD
 import WordPressComAnalytics
 import WordPressShared
 
-/// Handles the final step in the magic link auth proces.
+/// Handles the final step in the magic link auth process. At this point all the 
+/// necessary auth work should be done. We just need to create a WPAccount and to
+/// sync account info and blog details.
+/// The expectation is this controller will be momentarily visible when the app
+/// is resumed/launched via the appropriate custom scheme, and quickly dismiss.
 ///
 @objc class SigninLinkAuthViewController : NUXAbstractViewController, SigninWPComSyncHandler
 {
@@ -42,6 +46,9 @@ import WordPressShared
 
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+
+        // Gotta have email and token to use this vc
+        assert(!email.isEmpty && !token.isEmpty)
 
         if didSync {
             return
