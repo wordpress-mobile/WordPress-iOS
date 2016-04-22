34104d520617ed10a1a5f9b96a525a8bbda3dbd4
kurzee <brent@kurzee.com>
2016-04-22 15:11:13 -0500

    Nullability and method cleanup for PostTag service.

--- WordPress/Classes/Services/PostTagService.h
+++ WordPress/Classes/Services/PostTagService.h
@@ -1,5 +1,7 @@
 #import "LocalCoreDataService.h"
 
+NS_ASSUME_NONNULL_BEGIN
+
 @class Blog;
 @class PostTag;
 
@@ -9,24 +11,26 @@
  Sync an initial batch of tags for blog via default remote parameters and responses.
  */
 - (void)syncTagsForBlog:(Blog *)blog
-                success:(void (^)())success
-                failure:(void (^)(NSError *error))failure;
+                success:(nullable void (^)())success
+                failure:(nullable void (^)(NSError *error))failure;
 
 /**
  Sync an explicit number tags paginated by an offset for blog.
  */
 - (void)syncTagsForBlog:(Blog *)blog
-                 number:(NSNumber *)number
-                 offset:(NSNumber *)offset
-                success:(void (^)(NSArray <PostTag *> *tags))success
-                failure:(void (^)(NSError *error))failure;
+                 number:(nullable NSNumber *)number
+                 offset:(nullable NSNumber *)offset
+                success:(nullable void (^)(NSArray <PostTag *> *tags))success
+                failure:(nullable void (^)(NSError *error))failure;
 
 /**
  Search tags for blog matching a name or slug of the query. Case-insensitive search.
  */
 - (void)searchTagsWithName:(NSString *)nameQuery
                       blog:(Blog *)blog
-                   success:(void (^)(NSArray <PostTag *> *tags))success
-                   failure:(void (^)(NSError *error))failure;
+                   success:(nullable void (^)(NSArray <PostTag *> *tags))success
+                   failure:(nullable void (^)(NSError *error))failure;
 
 @end
+
+NS_ASSUME_NONNULL_END
