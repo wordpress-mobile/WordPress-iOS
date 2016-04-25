799ee16ee09693406de2c49b7ead442fa4f41e16
James Frost <james.frost@automattic.com>
2016-04-25 14:10:25 +0100

    WPBlogTableViewCell: Added nullability annotation to block parameter.

--- WordPress/Classes/ViewRelated/Cells/WPBlogTableViewCell.h
+++ WordPress/Classes/ViewRelated/Cells/WPBlogTableViewCell.h
@@ -3,6 +3,6 @@
 @interface WPBlogTableViewCell : WPTableViewCell
 
 @property (nonatomic, weak, nullable) UISwitch *visibilitySwitch;
-@property (nonatomic, copy, nullable) void (^visibilitySwitchToggled)(WPBlogTableViewCell *cell);
+@property (nonatomic, copy, nullable) void (^visibilitySwitchToggled)(WPBlogTableViewCell * _Nonnull cell);
 
 @end
