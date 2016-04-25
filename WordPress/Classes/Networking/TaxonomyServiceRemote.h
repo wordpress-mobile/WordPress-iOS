7010906053736cf641c38fb6eaf911baa24f822c
kurzee <brent@kurzee.com>
2016-04-25 17:11:55 -0500

    Adds Remote method for creating a new Tag for site.

--- WordPress/Classes/Networking/TaxonomyServiceRemote.h
+++ WordPress/Classes/Networking/TaxonomyServiceRemote.h
@@ -40,6 +40,13 @@ NS_ASSUME_NONNULL_BEGIN
                          failure:(nullable void (^)(NSError *error))failure;
 
 /**
+ Create a new tag with the site.
+ */
+- (void)createTag:(RemotePostTag *)tag
+               success:(nullable void (^)(RemotePostTag *tag))success
+               failure:(nullable void (^)(NSError *error))failure;
+
+/**
  Fetch a list of tags associated with the site.
  Note: Requests no paging parameters via the API defaulting the response.
  */
