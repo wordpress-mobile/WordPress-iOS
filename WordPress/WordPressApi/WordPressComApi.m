6fb3596251df1759b87c24bdaffeca2e5870e7b4
aerych <aerychj@gmail.com>
2016-04-25 17:17:19 -0500

    Reader: Builds endpoints preserving desired version.

--- WordPress/WordPressApi/WordPressComApi.m
+++ WordPress/WordPressApi/WordPressComApi.m
@@ -6,7 +6,7 @@
 #import "WPUserAgent.h"
 #import "NSDictionary+SafeExpectations.h"
 
-static NSString *const WordPressComApiClientEndpointURL = @"https://public-api.wordpress.com/rest/";
+NSString *const WordPressComApiClientEndpointURL = @"https://public-api.wordpress.com/rest/";
 static NSString *const WordPressComApiOauthBaseUrl = @"https://public-api.wordpress.com/oauth2";
 NSString *const WordPressComApiNotificationFields = @"id,type,unread,body,subject,timestamp,meta";
 static NSString *const WordPressComApiLoginUrl = @"https://wordpress.com/wp-login.php";
