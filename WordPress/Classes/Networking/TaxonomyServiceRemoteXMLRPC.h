e3a9109d6ad063935f248a9bc617b399cd07eb24
kurzee <brent@kurzee.com>
2016-04-25 17:12:40 -0500

    Moving taxonomy const strings to header for unit testing.

--- WordPress/Classes/Networking/TaxonomyServiceRemoteXMLRPC.h
+++ WordPress/Classes/Networking/TaxonomyServiceRemoteXMLRPC.h
@@ -2,6 +2,19 @@
 #import "TaxonomyServiceRemote.h"
 #import "ServiceRemoteXMLRPC.h"
 
+extern NSString * const TaxonomyXMLRPCCategoryIdentifier;
+extern NSString * const TaxonomyXMLRPCTagIdentifier;
+
+extern NSString * const TaxonomyXMLRPCIDParameter;
+extern NSString * const TaxonomyXMLRPCSlugParameter;
+extern NSString * const TaxonomyXMLRPCNameParameter;
+extern NSString * const TaxonomyXMLRPCParentParameter;
+extern NSString * const TaxonomyXMLRPCSearchParameter;
+extern NSString * const TaxonomyXMLRPCOrderParameter;
+extern NSString * const TaxonomyXMLRPCOrderByParameter;
+extern NSString * const TaxonomyXMLRPCNumberParameter;
+extern NSString * const TaxonomyXMLRPCOffsetParameter;
+
 @class RemoteCategory;
 
 @interface TaxonomyServiceRemoteXMLRPC : ServiceRemoteXMLRPC<TaxonomyServiceRemote>
