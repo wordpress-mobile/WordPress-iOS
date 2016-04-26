75141746ed155e2d129f73e302239afbbf343d93
aerych <aerychj@gmail.com>
2016-04-26 10:57:33 -0500

    Reader: Preview tags from correct api version.

--- WordPress/Classes/Networking/ReaderTopicServiceRemote.m
+++ WordPress/Classes/Networking/ReaderTopicServiceRemote.m
@@ -139,7 +139,7 @@ static NSString * const SiteDictionarySubscriptionsKey = @"subscribers_count";
 {
     NSString *path = [NSString stringWithFormat:@"read/tags/%@", slug];
     NSString *requestUrl = [self pathForEndpoint:path
-                                     withVersion:ServiceRemoteRESTApiVersion_1_1];
+                                     withVersion:ServiceRemoteRESTApiVersion_1_2];
 
     [self.api GET:requestUrl parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
         if (!success) {
