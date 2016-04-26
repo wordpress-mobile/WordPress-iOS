92c4eb76390f4904ac58d2eb2e6c9696e3c24242
Sérgio Estêvão <sergio.estevao@automattic.com>
2016-04-26 23:39:58 +0100

    Use date medium format when day difference is more than 7 days.

--- WordPress/Classes/Categories/NSDate+StringFormatting.m
+++ WordPress/Classes/Categories/NSDate+StringFormatting.m
@@ -4,10 +4,34 @@
 
 @implementation NSDate (StringFormatting)
 
++ (NSDateFormatter *)shortDateFormatter
+{
+    static NSDateFormatter *_shortDateFormatter = nil;
+    static dispatch_once_t _onceToken;
+    dispatch_once(&_onceToken, ^{
+        _shortDateFormatter = [[NSDateFormatter alloc] init];
+        _shortDateFormatter.dateStyle = NSDateFormatterMediumStyle;
+        _shortDateFormatter.timeStyle = NSDateFormatterNoStyle;
+    });
+
+    return _shortDateFormatter;
+}
+
 - (NSString *)shortString
-{    
-    TTTTimeIntervalFormatter *dateFormater = [[TTTTimeIntervalFormatter alloc] init];
-    NSString *shortString =  [dateFormater stringForTimeInterval:[self timeIntervalSinceNow]];
+{
+    NSString *shortString;
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *dateComponents = [calendar components:NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond
+                                                   fromDate:self
+                                                     toDate:[NSDate date]
+                                                    options:0];
+    NSInteger day = ABS(dateComponents.day);
+    if (day < 7) {
+        TTTTimeIntervalFormatter *dateFormater = [[TTTTimeIntervalFormatter alloc] init];
+        shortString =  [dateFormater stringForTimeInterval:[self timeIntervalSinceNow]];
+    } else {
+        shortString = [[[self class] shortDateFormatter] stringFromDate:self];
+    }
 
     return shortString;
 }
