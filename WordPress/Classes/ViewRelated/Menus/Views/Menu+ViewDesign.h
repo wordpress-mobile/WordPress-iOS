b836cd1caa8c8ff3d90e2b4233e44445116bacbf
kurzee <brent@kurzee.com>
2016-04-28 15:54:28 -0500

    Adding accessory views used in Menus when editing sources.

---
+++ WordPress/Classes/ViewRelated/Menus/Views/Menu+ViewDesign.h
@@ -0,0 +1,12 @@
+
+extern CGFloat const MenusDesignStrokeWidth;
+extern CGFloat const MenusDesignDefaultCornerRadius;
+extern CGFloat const MenusDesignDefaultContentSpacing;
+
+#import "Menu.h"
+
+@interface Menu (ViewDesign)
+
++ (UIEdgeInsets)viewDefaultDesignInsets;
+
+@end
