3b79377591507cf1bfce2e647fc94c09c0522dd1
kurzee <brent@kurzee.com>
2016-04-29 14:56:05 -0500

    Comment documentation on Menus categories.

--- WordPress/Classes/ViewRelated/Menus/Views/Menu+ViewDesign.h
+++ WordPress/Classes/ViewRelated/Menus/Views/Menu+ViewDesign.h
@@ -1,10 +1,12 @@
+#import "Menu.h"
 
 extern CGFloat const MenusDesignStrokeWidth;
 extern CGFloat const MenusDesignDefaultCornerRadius;
 extern CGFloat const MenusDesignDefaultContentSpacing;
 
-#import "Menu.h"
-
+/**
+ * Design category for providing common values used for drawing, layout, and views involving a Menu object.
+ */
 @interface Menu (ViewDesign)
 
 + (UIEdgeInsets)viewDefaultDesignInsets;
