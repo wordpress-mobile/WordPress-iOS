995ddc8e19101707e7b95f337cecd895bc0bba19
aerych <eric@automattic.com>
2016-05-02 14:09:10 -0500

    Signin: Extends 1Password facade for generating new 1Password entry.

--- WordPress/Classes/Services/OnePasswordFacade.h
+++ WordPress/Classes/Services/OnePasswordFacade.h
@@ -1,6 +1,14 @@
 #import <Foundation/Foundation.h>
 
-typedef void (^OnePasswordFacadeCallback)(NSString *username, NSString *password,  NSString *oneTimePassword, NSError *);
+NS_ASSUME_NONNULL_BEGIN
+typedef void (^OnePasswordFacadeCallback)(NSString * _Nullable username, NSString * _Nullable password,  NSString * _Nullable oneTimePassword, NSError * _Nullable);
+
+extern NSString * const WPOnePasswordTitleKey;
+extern NSString * const WPOnePasswordUsernameKey;
+extern NSString * const WPOnePasswordPasswordKey;
+extern NSString * const WPOnePasswordGeneratedPasswordMinLengthKey;
+extern NSString * const WPOnePasswordGeneratedPasswordMaxLengthKey;
+extern NSInteger WPOnePasswordErrorCodeCancelledByUser;
 
 /**
  *  This protocol is a Facade that hides some of the implementation details for interacting with 1Password.
@@ -24,7 +32,25 @@ typedef void (^OnePasswordFacadeCallback)(NSString *username, NSString *password
  */
 - (BOOL)isOnePasswordEnabled;
 
+/**
+ *  Stores a new entry in the 1Password extension.
+ *
+ *  @param URLString                    the URL of the site in question.
+ *  @param loginDetailsDictionary       a dictionary containing login credentials to store.
+ *  @param passwordGenerationOptions    a dictionary specifying password generation options.
+ *  @param viewController the view controller of the class that needs the 1Password extension to appear. Note this can't be nil.
+ *  @param sender         the control that triggered the action
+ *  @param completion     block that is called when 1Password is done setting the credentials.
+ */
+- (void)storeLoginForURLString:(NSString *)URLString
+                  loginDetails:(NSDictionary *)loginDetailsDictionary
+     passwordGenerationOptions:(NSDictionary *)passwordGenerationOptions
+             forViewController:(UIViewController *)viewController
+                        sender:(id)sender
+                    completion:(void (^)(NSDictionary *loginDictionary, NSError * _Nullable error))completion;
+
 @end
+NS_ASSUME_NONNULL_END
 
 @interface OnePasswordFacade : NSObject <OnePasswordFacade>
 
