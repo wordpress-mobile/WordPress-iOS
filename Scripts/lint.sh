4db98e50a0e380861d1431ca8dba145deb021006
Jorge Bernal <jbernal@gmail.com>
2016-05-04 11:23:23 +0200

    Integrate SwiftLint

---
+++ Scripts/lint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if ! which swiftlint >/dev/null; then
+  echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint or `brew install swiftlint`"
+fi
+
+scripts_dir=$( dirname $0 )
+project_dir=$( cd ${scripts_dir}/../; pwd )
+source_dir="${project_dir}/WordPress"
+
+cd "$project_dir"
+swiftlint
+cd -
