7eba3ae5ecbe6e9fedf380474f81de9b9ccd8b03
Jorge Leandro Perez <jorge@lantean.co>
2016-05-04 11:35:08 -0300

    PeopleRemote: Implements Update Role

--- WordPress/Classes/Networking/PeopleRemote.swift
+++ WordPress/Classes/Networking/PeopleRemote.swift
@@ -1,7 +1,14 @@
 import Foundation
 
 class PeopleRemote: ServiceRemoteREST {
-    func getTeamFor(siteID: Int, success: People -> (), failure: ErrorType -> ()) {
+    enum Error: ErrorType {
+        case DecodeError
+    }
+    
+    func getTeamFor(siteID  : Int,
+                    success : People -> (),
+                    failure : ErrorType -> ())
+    {
         let endpoint = "sites/\(siteID)/users"
         let path = pathForEndpoint(endpoint, withVersion: ServiceRemoteRESTApiVersion_1_1)
         let parameters = [
@@ -12,68 +19,100 @@ class PeopleRemote: ServiceRemoteREST {
         api.GET(path,
             parameters: parameters,
             success: {
-                (operation, responseObject) -> Void in
-
-                if let people = try? self.peopleFromResponse(responseObject, siteID: siteID) {
-                    success(people)
-                } else {
+                (operation, responseObject) in
+                guard let response = responseObject as? [String: AnyObject],
+                          people = try? self.peopleFromResponse(response, siteID: siteID) else
+                {
                     failure(Error.DecodeError)
+                    return
                 }
+
+                success(people)
             },
             failure: {
-                (operation, error) -> Void in
+                (operation, error) in
                 failure(error)
-        })
+            })
     }
+    
+    func updatePersonFor(siteID     : Int,
+                         personID   : Int,
+                         newRole    : Person.Role,
+                         success    : (Person -> ())? = nil,
+                         failure    : (ErrorType -> ())? = nil)
+    {
+        let endpoint = "sites/\(siteID)/users/\(personID)"
+        let path = pathForEndpoint(endpoint, withVersion: ServiceRemoteRESTApiVersion_1_1)
+        let parameters = ["roles" : [newRole.description]]
+        
+        api.POST(path,
+                parameters: parameters,
+                success: {
+                    (operation, responseObject) in
+                    guard let response = responseObject as? [String: AnyObject],
+                              person = try? self.personFromResponse(response, siteID: siteID) else
+                    {
+                        failure?(Error.DecodeError)
+                        return
+                    }
+                    
+                    success?(person)
+                },
+                failure: {
+                    (operation, error) in
+                    failure?(error)
+                })
+    }
+}
 
-    private func peopleFromResponse(responseObject: AnyObject, siteID: Int) throws -> People {
-        let response = responseObject as? [String: AnyObject]
-        let users = response.flatMap { return $0["users"] as? [[String: AnyObject]] }
-        guard let unwrappedUsers = users else {
+
+private extension PeopleRemote {
+    private func peopleFromResponse(response: [String: AnyObject], siteID: Int) throws -> People {
+        guard let users = response["users"] as? [[String: AnyObject]] else {
             throw Error.DecodeError
         }
 
-        let people = unwrappedUsers.map {
-            (user: [String: AnyObject]) -> Person? in
-            guard let ID = user["ID"] as? Int else {
-                return nil
-            }
-            guard let username = user["nice_name"] as? String else {
-                return nil
-            }
-            guard let displayName = user["name"] as? String else {
-                return nil
-            }
-
-            let firstName = user["first_name"] as? String
-            let lastName = user["last_name"] as? String
-            let avatarURL = (user["avatar_URL"] as? String)
-                .flatMap { NSURL(string: $0)}
-                .flatMap { Gravatar($0)?.canonicalURL }
-            let isSuperAdmin = user["is_super_admin"] as? Bool ?? false
-            let roles = user["roles"] as? [String]
-
-            let role = roles?.map({
-                (role: String) -> Person.Role in
-                return Person.Role(string: role)
-            }).sort().first ?? .Unsupported
-
-            return Person(ID: ID, username: username, firstName: firstName, lastName: lastName, displayName: displayName, role: role, siteID: siteID, avatarURL: avatarURL, isSuperAdmin: isSuperAdmin)
+        let people = try users.flatMap { (user) -> Person? in
+            return try personFromResponse(user, siteID: siteID)
         }
-
-        let errorCount = people.reduce(0) { (sum, person) -> Int in
-            if person == nil {
-                return sum + 1
-            }
-            return sum
-        }
-        if errorCount > 0 {
+        
+        return people
+    }
+    
+    private func personFromResponse(user: [String: AnyObject], siteID: Int) throws -> Person {
+        guard let ID = user["ID"] as? Int else {
             throw Error.DecodeError
         }
-        return people.flatMap { $0 }
-    }
-
-    enum Error: ErrorType {
-        case DecodeError
+        
+        guard let username = user["nice_name"] as? String else {
+            throw Error.DecodeError
+        }
+        
+        guard let displayName = user["name"] as? String else {
+            throw Error.DecodeError
+        }
+        
+        let firstName = user["first_name"] as? String
+        let lastName = user["last_name"] as? String
+        let avatarURL = (user["avatar_URL"] as? String)
+            .flatMap { NSURL(string: $0)}
+            .flatMap { Gravatar($0)?.canonicalURL }
+        
+        let isSuperAdmin = user["is_super_admin"] as? Bool ?? false
+        let roles = user["roles"] as? [String]
+        
+        let role = roles?.map({ (role) -> Person.Role in
+            return Person.Role(string: role)
+        }).sort().first ?? .Unsupported
+        
+        return Person(ID            : ID,
+                      username      : username,
+                      firstName     : firstName,
+                      lastName      : lastName,
+                      displayName   : displayName,
+                      role          : role,
+                      siteID        : siteID,
+                      avatarURL     : avatarURL,
+                      isSuperAdmin  : isSuperAdmin)
     }
 }
