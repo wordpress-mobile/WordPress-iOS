51c4441219852e2462861fe0fdd5a273713006b8
Jorge Bernal <jbernal@gmail.com>
2016-05-12 10:06:51 +0200

    Integrate EmailChecker in the app.
    Renamed as EmailTypoChecker.

--- WordPress/Classes/ViewRelated/NUX/SignupViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/SignupViewController.swift
@@ -483,7 +483,11 @@ extension SignupViewController: UITextFieldDelegate {
             return true
         }
 
-        let suggestedEmail = EmailCheckerHelper.suggestDomainCorrection(textField.text)
+        guard let text = textField.text else {
+            return true
+        }
+
+        let suggestedEmail = EmailTypoChecker.suggestDomainCorrection(text)
         if suggestedEmail != textField.text {
             textField.text = suggestedEmail
             didCorrectEmailOnce = true
