2390dca83bad99cbcc61459faec496a5b23b6902
Sérgio Estêvão <sergio.estevao@automattic.com>
2016-05-13 23:25:11 +0100

    Refactor WPUserAgent to use static methods that are thread safe and invoked only once.

--- WordPress/WordPressTest/WPUserAgentTests.m
+++ WordPress/WordPressTest/WPUserAgentTests.m
@@ -9,47 +9,6 @@ static NSString* const WPUserAgentKeyUserAgent = @"UserAgent";
 
 @implementation WPUserAgentTests
 
-/**
- *  @brief      Returns default UA for this device.
- *  @details    This method is duplicated on purpose since we want to make sure that any change to
- *              the WP UA in the app makes this test show an error unless updated.  This way we
- *              ensure the change is intentional.
- *              Also, the method temporarily unsets "UserAgent" from registered
- *              user defaults so that we always get the default value,
- *              independently from what's currently set as User-Agent.
- */
-- (NSString *)defaultUserAgent
-{
-    NSDictionary *originalRegisteredDefaults = [[NSUserDefaults standardUserDefaults] volatileDomainForName:NSRegistrationDomain];
-    
-    NSMutableDictionary *tempRegisteredDefaults = [NSMutableDictionary dictionaryWithDictionary:originalRegisteredDefaults];
-    [tempRegisteredDefaults removeObjectForKey:WPUserAgentKeyUserAgent];
-    [[NSUserDefaults standardUserDefaults] registerDefaults:tempRegisteredDefaults];
-    
-    NSString *userAgent = [self currentUserAgentFromUIWebView];
-    XCTAssertNotNil(userAgent, @"User agent shouldn't be nil");
-    XCTAssertTrue([userAgent length] > 0, @"User agent shouldn't be empty");
-    
-    [[NSUserDefaults standardUserDefaults] registerDefaults:originalRegisteredDefaults];
-    
-    return userAgent;
-}
-
-/**
- *  @brief      Calculates the wordpress UA for this device.
- *  @details    This method is duplicated on purpose since we want to make sure that any change to
- *              the WP UA in the app makes this test show an error unless updated.  This way we
- *              ensure the change is intentional.
- */
-- (NSString *)wordPressUserAgent
-{
-    NSString *defaultUA = [self defaultUserAgent];
-    NSString *appVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
-    NSString *userAgent = [NSString stringWithFormat:@"%@ wp-iphone/%@", defaultUA, appVersion];
-    
-    return userAgent;
-}
-
 - (NSString *)currentUserAgentFromUserDefaults
 {
     return [[NSUserDefaults standardUserDefaults] objectForKey:WPUserAgentKeyUserAgent];
@@ -62,28 +21,24 @@ static NSString* const WPUserAgentKeyUserAgent = @"UserAgent";
 
 - (void)testWordPressUserAgent
 {
-    NSString *wordPressUA = [self wordPressUserAgent];
-    WPUserAgent *userAgent = nil;
-    
-    XCTAssertNoThrow(userAgent = [[WPUserAgent alloc] init]);
-    XCTAssertTrue([userAgent isKindOfClass:[WPUserAgent class]]);
-    
-    XCTAssertTrue([[self wordPressUserAgent] isEqualToString:wordPressUA]);
+    NSString *appVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
+    NSString *customAgent = [NSString stringWithFormat:@"wp-iphone/%@", appVersion];
+
+    XCTAssertTrue([[WPUserAgent wordPressUserAgent] containsString:customAgent]);
 }
 
 - (void)testUseWordPressUserAgentInUIWebViews
 {
-    NSString *defaultUA = [self defaultUserAgent];
-    NSString *wordPressUA = [self wordPressUserAgent];
-    WPUserAgent *userAgent = nil;
-    
-    XCTAssertNoThrow(userAgent = [[WPUserAgent alloc] init]);
-    XCTAssertTrue([userAgent isKindOfClass:[WPUserAgent class]]);
-    
+    NSString *defaultUA = [WPUserAgent defaultUserAgent];
+    NSString *wordPressUA = [WPUserAgent wordPressUserAgent];
+
+    [[NSUserDefaults standardUserDefaults] removeObjectForKey:WPUserAgentKeyUserAgent];
+    [[NSUserDefaults standardUserDefaults] registerDefaults:@{WPUserAgentKeyUserAgent: defaultUA}];
+
     XCTAssertTrue([[self currentUserAgentFromUserDefaults] isEqualToString:defaultUA]);
     XCTAssertTrue([[self currentUserAgentFromUIWebView] isEqualToString:defaultUA]);
-    
-    [userAgent useWordPressUserAgentInUIWebViews];
+
+    [WPUserAgent useWordPressUserAgentInUIWebViews];
     
     XCTAssertTrue([[self currentUserAgentFromUserDefaults] isEqualToString:wordPressUA]);
     XCTAssertTrue([[self currentUserAgentFromUIWebView] isEqualToString:wordPressUA]);
