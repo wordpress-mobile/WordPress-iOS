9b5371552db4138799bebbaaf9e8642d4ab26872
Sérgio Estêvão <sergio.estevao@automattic.com>
2016-05-13 23:25:11 +0100

    Refactor WPUserAgent to use static methods that are thread safe and invoked only once.

--- WordPress/Classes/Utility/Analytics/WPAnalyticsTrackerWPCom.m
+++ WordPress/Classes/Utility/Analytics/WPAnalyticsTrackerWPCom.m
@@ -31,7 +31,7 @@
 {
     int x = arc4random();
     NSString *statsURL = [NSString stringWithFormat:@"%@%@%@%@%d" , WPMobileReaderURL, @"&template=stats&stats_name=", statName, @"&rnd=", x];
-    NSString *userAgent = [[WordPressAppDelegate sharedInstance].userAgent wordPressUserAgent];
+    NSString *userAgent = [WPUserAgent wordPressUserAgent];
     
     NSMutableURLRequest* request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:statsURL]];
     [request setValue:userAgent forHTTPHeaderField:@"User-Agent"];
