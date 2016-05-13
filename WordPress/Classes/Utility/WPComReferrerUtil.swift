272a82d4aa2c8a4a74ccae6f5c7643bd75f93384
aerych <eric@automattic.com>
2016-05-12 21:58:15 -0500

    Adds documentation. Avoid editing string with existing utm source.

--- WordPress/Classes/Utility/WPComReferrerUtil.swift
+++ WordPress/Classes/Utility/WPComReferrerUtil.swift
@@ -3,10 +3,19 @@ import Foundation
 @objc public class WPComReferrerUtil : NSObject
 {
 
+    /// Add the utm_source query string param to a URL string.
+    /// If the utm_source already exists the original pathis returned.
+    ///
+    /// - Parameters:
+    ///     - path: A URL string. Can be an absolute or relative URL string.
+    ///
+    /// - Returns: The path with the utm_source appended, or the original path if it already had a utm_source param.
+    ///
     class func addUtmSourceToURLPath(path: String) -> String {
-        if path.isEmpty {
+        if path.isEmpty || path.containsString(WPComReferrerGaUtmSourceKey) {
             return path
         }
+
         guard let urlencodedReferrer = WPComReferrerURL.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet()) else {
             return path
         }
