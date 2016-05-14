75751e015609529c8c1ee2aafb5c7ba6897b3318
Jorge Leandro Perez <jorge@lantean.co>
2016-05-14 10:37:19 -0300

    PeopleRemote: Implements Delete Person

--- WordPress/Classes/Networking/PeopleRemote.swift
+++ WordPress/Classes/Networking/PeopleRemote.swift
@@ -93,6 +93,38 @@ class PeopleRemote: ServiceRemoteREST {
     }
 
 
+    /// Deletes or removes a user from a site.
+    ///
+    /// - Parameters:
+    ///     - siteID: The ID of the site associated
+    ///     - personID: The ID of the person to be updated
+    ///     - reassignID: When present, all of the posts and pages that belong to `personID` will be reassigned
+    ///       to another person, with the specified ID.
+    ///     - success: Optional closure to be executed on success
+    ///     - failure: Optional closure to be executed on error.
+    ///
+    func deletePersonFrom(siteID     : Int,
+                          personID   : Int,
+                          reassignID : Int? = nil,
+                          success    : (Void -> Void)? = nil,
+                          failure    : (ErrorType -> Void)? = nil)
+    {
+        let endpoint = "sites/\(siteID)/users/\(personID)/delete"
+        let path = pathForEndpoint(endpoint, withVersion: ServiceRemoteRESTApiVersion_1_1)
+        var parameters = [String: AnyObject]()
+
+        if let reassignID = reassignID {
+            parameters["reassign"] = reassignID
+        }
+
+        api.POST(path, parameters: nil, success: { (operation, responseObject) in
+            success?()
+        }, failure: { (operation, error) in
+            failure?(error)
+        })
+    }
+
+
     /// Retrieves all of the Available Roles, for a given SiteID.
     ///
     /// - Parameters:
