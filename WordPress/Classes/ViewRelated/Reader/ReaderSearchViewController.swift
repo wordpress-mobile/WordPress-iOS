bfbaf76e5067994e6f1550bf63fe84582658be7b
aerych <eric@automattic.com>
2016-05-16 16:03:15 -0500

    Reader: Styles the search controller's search bar.

--- WordPress/Classes/ViewRelated/Reader/ReaderSearchViewController.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderSearchViewController.swift
@@ -1,4 +1,6 @@
 import Foundation
+import WordPressShared
+import Gridicons
 
 /// Displays a version of the post stream with a search bar positioned above the
 /// list of posts.  The user supplied search phrase is converted into a ReaderSearchTopic
@@ -8,6 +10,8 @@ import Foundation
 {
     @IBOutlet private weak var searchBar: UISearchBar!
     private var streamController: ReaderStreamViewController!
+
+    private let searchBarSearchIconSize = CGFloat(13.0)
 
     /// A convenience method for instantiating the controller from the storyboard.
     ///
@@ -23,8 +27,42 @@ import Foundation
 
     // MARK: Lifecycle methods
 
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupSearchBar()
+    }
+
+
     public override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         streamController = segue.destinationViewController as? ReaderStreamViewController
+    }
+
+
+    // MARK: - Configuration
+
+
+    func setupSearchBar() {
+        // Appearance must be set before the search bar is added to the view hierarchy.
+        let placeholderText = NSLocalizedString("Search on WordPress.com", comment: "Placeholder text for the Reader search feature.")
+        let attributes = WPStyleGuide.defaultSearchBarTextAttributes(WPStyleGuide.grey())
+        let attributedPlaceholder = NSAttributedString(string: placeholderText, attributes: attributes)
+        UITextField.appearanceWhenContainedInInstancesOfClasses([UISearchBar.self, ReaderSearchViewController.self]).attributedPlaceholder = attributedPlaceholder
+        let textAttributes = WPStyleGuide.defaultSearchBarTextAttributes(WPStyleGuide.greyDarken30())
+        UITextField.appearanceWhenContainedInInstancesOfClasses([UISearchBar.self, ReaderSearchViewController.self]).defaultTextAttributes = textAttributes
+
+        searchBar.translucent = false
+        searchBar.tintColor = WPStyleGuide.grey()
+        searchBar.barTintColor = WPStyleGuide.greyLighten30()
+        searchBar.backgroundImage = UIImage()
+        searchBar.setImage(UIImage(named: "icon-clear-textfield"), forSearchBarIcon: .Clear, state: .Normal)
+
+        let size = searchBarSearchIconSize * UIScreen.mainScreen().scale
+        let image = Gridicon.iconOfType(GridiconType.Search, withSize: CGSize(width: size, height: size))
+
+        searchBar.setImage(image, forSearchBarIcon: .Search, state: .Normal)
+        searchBar.accessibilityIdentifier = "Search"
     }
 
 
