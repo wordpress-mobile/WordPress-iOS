350012d0817a9bbf2313dc91b361622959930aae
James Frost <james.frost@automattic.com>
2016-05-17 17:37:54 +0100

    Added Domain model and DomainsRemote to fetch current blog domains.

--- WordPress/Classes/ViewRelated/Domains/DomainsListViewController.swift
+++ WordPress/Classes/ViewRelated/Domains/DomainsListViewController.swift
@@ -16,27 +16,39 @@ struct DomainListRow: ImmuTableRow {
 }
 
 class DomainsListViewController: UITableViewController, ImmuTablePresenter {
-    private lazy var handler: ImmuTableViewHandler = {
-        return ImmuTableViewHandler(takeOver: self)
-    }()
+    private var viewModel: ImmuTable!
 
-    private var viewModel: ImmuTable = {
-        let searchRow = DomainListRow(title: "Find a new domain", action: nil)
-        let connectRow = DomainListRow(title: "Or connect your own domain", action: nil)
-        let domainRow = DomainListRow(title: "hawthornecoffeeco.com", action: nil)
+    private var fetchRequest: NSFetchRequest {
+        let request = NSFetchRequest(entityName: Domain.entityName)
+        request.predicate = NSPredicate(format: "%K == %@", "blog", blog)
+        request.sortDescriptors = [NSSortDescriptor(key: "domain", ascending: true)]
 
-        return ImmuTable(sections: [
-            ImmuTableSection(headerText: "Add A New Domain",
-                rows: [ searchRow, connectRow ], footerText: nil),
-            ImmuTableSection(headerText: "Your Domains",
-                rows: [ domainRow ], footerText: nil) ])
-    }()
+        return request
+    }
 
-    var blog: Blog!
+    var blog: Blog! {
+        didSet {
+            if let context = blog.managedObjectContext {
+                fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                                                      managedObjectContext: context,
+                                                                      sectionNameKeyPath: nil,
+                                                                      cacheName: nil)
+                fetchedResultsController.delegate = self
+                let _ = try? fetchedResultsController.performFetch()
+            }
+
+            updateViewModel()
+        }
+    }
+    var service: DomainsService!
+    var fetchedResultsController: NSFetchedResultsController!
 
     class func controllerWithBlog(blog: Blog) -> DomainsListViewController {
         let storyboard = UIStoryboard(name: "Domains", bundle: NSBundle(forClass: self))
         let controller = storyboard.instantiateInitialViewController() as! DomainsListViewController
+
+        controller.blog = blog
+        controller.service = DomainsService(blog: blog)
 
         return controller
     }
@@ -49,6 +61,61 @@ class DomainsListViewController: UITableViewController, ImmuTablePresenter {
         WPStyleGuide.resetReadableMarginsForTableView(tableView)
         WPStyleGuide.configureColorsForView(view, andTableView: tableView)
         ImmuTable.registerRows([DomainListRow.self], tableView: tableView)
-        handler.viewModel = viewModel
+
+        service.refreshBlogDomains { success in
+
+        }
+    }
+
+    private func updateViewModel() {
+        let searchRow = DomainListRow(title: "Find a new domain", action: nil)
+        let connectRow = DomainListRow(title: "Or connect your own domain", action: nil)
+
+        var domainRows = [ImmuTableRow]()
+        if let domains = fetchedResultsController.fetchedObjects as? [ManagedDomain] {
+            domainRows = domains.map { DomainListRow(title: $0.domain, action: nil) }
+        }
+
+        viewModel = ImmuTable(sections: [
+            ImmuTableSection(headerText: "Add A New Domain", rows: [ searchRow, connectRow ], footerText: nil),
+            ImmuTableSection(headerText: "Your Domains", rows: domainRows, footerText: nil) ]
+        )
+
+        if isViewLoaded() {
+            tableView.reloadData()
+        }
+    }
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return viewModel.sections.count
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.sections[section].rows.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let row = viewModel.rowAtIndexPath(indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier(row.reusableIdentifier, forIndexPath: indexPath)
+
+        row.configureCell(cell)
+
+        return cell
+    }
+
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return viewModel.sections[section].headerText
+    }
+
+    override func tableView(tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return viewModel.sections[section].footerText
+    }
+}
+
+extension DomainsListViewController: NSFetchedResultsControllerDelegate {
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        updateViewModel()
+
+        tableView.reloadData()
     }
 }
