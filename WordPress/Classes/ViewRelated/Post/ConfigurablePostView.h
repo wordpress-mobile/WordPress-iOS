38abdef44cc0bf4792b75384c180f39506b8e8fe
Diego Rey Mendez <diegoreymendez@gmail.com>
2016-05-17 15:12:47 -0300

    Updated the documentation of ConfigurablePostView.

--- WordPress/Classes/ViewRelated/Post/ConfigurablePostView.h
+++ WordPress/Classes/ViewRelated/Post/ConfigurablePostView.h
@@ -3,22 +3,23 @@
 
 @class AbstractPost;
 
+/// Protocol that any view representing a post can implement.
+///
 @protocol ConfigurablePostView <NSObject>
 
-/// When called, the view should start representing the specified post object, and send any
-/// interaction events to the delegate.
+/// When called, the view should start representing the specified post object.
 ///
 /// - Parameters:
 ///     - post: the post to visually represent.
 ///
 - (void)configureWithPost:(nonnull AbstractPost*)post;
 
-/// Same as `configure(delegate:post:)` but only for the purpose of layout.
+/// Same as `configureWithPost:` but only for the purpose of layout.
 ///
 /// - Parameters:
 ///     - post: the post to visually represent.
 ///     - layoutOnly: `true` if the configure call is meant for layout purposes only.
-///             if set to `false`, this should behave exactly like `configure(delegate:post:)`.
+///             if set to `false`, this should behave exactly like `configureWithPost:`.
 ///
 - (void)configureWithPost:(nonnull AbstractPost*)post
             forLayoutOnly:(BOOL)layoutOnly;
