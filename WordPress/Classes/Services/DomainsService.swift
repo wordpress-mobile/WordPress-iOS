d039a5be7654bed73dd7b4e5362cdbbca3628cf8
James Frost <james.frost@automattic.com>
2016-05-23 10:32:10 +0100

    Split out Domains service from remote.

--- WordPress/Classes/Services/DomainsService.swift
+++ WordPress/Classes/Services/DomainsService.swift
@@ -1,71 +1,7 @@
 import Foundation
 
-class DomainsRemote: ServiceRemoteREST {
-    enum Error: ErrorType {
-        case DecodeError
-    }
-
-    func getDomainsForSite(siteID: Int, success: [Domain] -> Void, failure: ErrorType -> Void) {
-        let endpoint = "sites/\(siteID)/domains"
-        let path = pathForEndpoint(endpoint, withVersion: ServiceRemoteRESTApiVersion_1_1)
-
-        api.GET(path,
-                parameters: nil,
-                success: {
-                    _, response in
-                    do {
-                        try success(mapDomainsResponse(response))
-                    } catch {
-                        DDLogSwift.logError("Error parsing domains response (\(error)): \(response)")
-                        failure(error)
-                    }
-            }, failure: {
-                _, error in
-                failure(error)
-        })
-    }
-}
-
-private func mapDomainsResponse(response: AnyObject) throws -> [Domain] {
-    guard let json = response as? [String: AnyObject],
-        let domainsJson = json["domains"] as? [[String: AnyObject]] else {
-            throw DomainsRemote.Error.DecodeError
-    }
-
-    let domains = try domainsJson.map { domainJson -> Domain in
-
-        guard let domainName = domainJson["domain"] as? String,
-            let isPrimary = domainJson["primary_domain"] as? Bool else {
-                throw DomainsRemote.Error.DecodeError
-        }
-
-        return Domain(domainName: domainName, isPrimaryDomain: isPrimary, domainType: domainTypeFromDomainJSON(domainJson))
-    }
-
-    return domains
-}
-
-private func domainTypeFromDomainJSON(domainJson: [String: AnyObject]) -> DomainType {
-    if let type = domainJson["type"] as? String
-        where type == "redirect" {
-        return .SiteRedirect
-    }
-
-    if let wpComDomain = domainJson["wpcom_domain"] as? Bool
-        where wpComDomain == true {
-        return .WPCom
-    }
-
-    if let hasRegistration = domainJson["has_registration"] as? Bool
-        where hasRegistration == true {
-        return .Registered
-    }
-
-    return .Mapped
-}
-
 struct DomainsService {
-    let remote: DomainsRemote
+    let remote: DomainsServiceRemote
     let blog: Blog
     let siteID: Int
 
@@ -74,7 +10,7 @@ struct DomainsService {
     init(blog: Blog) {
         precondition(blog.dotComID != nil)
 
-        remote = DomainsRemote(api: blog.restApi())
+        remote = DomainsServiceRemote(api: blog.restApi())
         self.blog = blog
         siteID = Int(blog.dotComID!)
     }
