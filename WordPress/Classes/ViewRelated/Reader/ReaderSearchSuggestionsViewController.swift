d9fe0b5e7ff65c7aac9f21798cb124a0f42e9d1c
aerych <eric@automattic.com>
2016-05-26 16:36:43 -0500

    Reader: Adds ReaderSearchSuggestions managed object.

---
+++ WordPress/Classes/ViewRelated/Reader/ReaderSearchSuggestionsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ReaderSearchSuggestionsViewController.swift
+//  WordPress
+//
+//  Created by aerych on 5/25/16.
+//  Copyright © 2016 WordPress. All rights reserved.
+//
+
+import Foundation
