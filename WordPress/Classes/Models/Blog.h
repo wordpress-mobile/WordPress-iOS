6053ce5224d1a93ec28e14677c5f8fc35db33a29
Sérgio Estêvão <sergio.estevao@automattic.com>
2016-06-03 11:17:38 +0100

    Add new methods to access the xmlRpcApi on a blog.

--- WordPress/Classes/Models/Blog.h
+++ WordPress/Classes/Models/Blog.h
@@ -10,6 +10,7 @@ NS_ASSUME_NONNULL_BEGIN
 @class WordPressComApi;
 @class WordPressComRestApi;
 @class WPXMLRPCClient;
+@class WordPressOrgXMLRPCApi;
 
 extern NSString * const BlogEntityName;
 extern NSString * const PostFormatStandard;
@@ -126,6 +127,7 @@ typedef NS_ENUM(NSInteger, SiteVisibility) {
 @property (nonatomic,   weak,  readonly, nullable) NSArray *sortedPostFormatNames;
 @property (nonatomic,   weak,  readonly, nullable) NSArray *sortedPostFormats;
 @property (nonatomic, strong,  readonly, nullable) WPXMLRPCClient *api;
+@property (nonatomic, strong,  readonly, nullable) WordPressOrgXMLRPCApi *xmlRpcApi;
 @property (nonatomic,   weak,  readonly, nullable) NSString       *version;
 @property (nonatomic, strong,  readonly, nullable) NSString       *authToken;
 @property (nonatomic, strong,  readonly, nullable) NSSet *allowedFileTypes;
