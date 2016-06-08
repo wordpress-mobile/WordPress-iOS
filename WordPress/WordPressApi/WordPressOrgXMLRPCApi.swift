bbfb0fab27a17a361d2544bba09dfe7962bbe44a
Sérgio Estêvão <sergio.estevao@automattic.com>
2016-06-08 12:52:30 +0100

    Convert code to use NSURLSession directly.

--- WordPress/WordPressApi/WordPressOrgXMLRPCApi.swift
+++ WordPress/WordPressApi/WordPressOrgXMLRPCApi.swift
@@ -1,8 +1,7 @@
 import Foundation
-import AFNetworking
 import wpxmlrpc
 
-public class WordPressOrgXMLRPCApi: NSObject
+public class WordPressOrgXMLRPCApi: NSObject, NSURLSessionDelegate, NSURLSessionTaskDelegate
 {
     public typealias SuccessResponseBlock = (responseObject: AnyObject, httpResponse: NSHTTPURLResponse?) -> ()
     public typealias FailureReponseBlock = (error: NSError, httpResponse: NSHTTPURLResponse?) -> ()
@@ -10,41 +9,15 @@ public class WordPressOrgXMLRPCApi: NSObject
     private let endpoint: NSURL
     private let userAgent: String?
 
-    private lazy var sessionManager: AFHTTPSessionManager = {
+    private lazy var session: NSURLSession = {
         let sessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
         var additionalHeaders: [String : AnyObject] = ["Accept-Encoding":"gzip, deflate"]
         if let userAgent = self.userAgent {
             additionalHeaders["User-Agent"] = userAgent
         }
         sessionConfiguration.HTTPAdditionalHeaders = additionalHeaders
-        let sessionManager = AFHTTPSessionManager(baseURL:self.endpoint, sessionConfiguration:sessionConfiguration)
-        sessionManager.responseSerializer = WordPressOrgXMLRPCResponseSerializer()
-        sessionManager.requestSerializer = WordPressOrgXMLRPCRequestSerializer()
-        sessionManager.setTaskDidReceiveAuthenticationChallengeBlock({ (session, task, authenticationChallenge, urlCredential) -> NSURLSessionAuthChallengeDisposition in
-            switch authenticationChallenge.protectionSpace.authenticationMethod {
-            case NSURLAuthenticationMethodServerTrust:
-                var result = SecTrustResultType(kSecTrustResultInvalid)
-                if let serverTrust = authenticationChallenge.protectionSpace.serverTrust {
-                    let certificateStatus = SecTrustEvaluate(serverTrust, &result)
-                    if certificateStatus == 0 && result == SecTrustResultType(kSecTrustResultRecoverableTrustFailure) {
-                        //                    dispatch_async(dispatch_get_main_queue(), ^(void) {
-                        //                        [WPHTTPAuthenticationAlertController presentWithChallenge:challenge];
-                        //                        });
-                    } else {
-                        return .UseCredential
-                        //[challenge.sender continueWithoutCredentialForAuthenticationChallenge:challenge];
-                    }
-                }
-            case NSURLAuthenticationMethodClientCertificate:
-                return .UseCredential
-            case NSURLAuthenticationMethodHTTPBasic:
-                return .PerformDefaultHandling
-            default:
-                return .PerformDefaultHandling
-            }
-            return .PerformDefaultHandling
-        })
-        return sessionManager
+        let session = NSURLSession(configuration: sessionConfiguration)
+        return session
     }()
 
     public init(endpoint: NSURL, userAgent: String? = nil) {
@@ -54,22 +27,22 @@ public class WordPressOrgXMLRPCApi: NSObject
     }
 
     deinit {
-        sessionManager.invalidateSessionCancelingTasks(false)
+        session.finishTasksAndInvalidate()
     }
 
     /**
      Cancels all ongoing and makes the session so the object will not fullfil any more request
      */
     public func invalidateAndCancelTasks() {
-        sessionManager.invalidateSessionCancelingTasks(true)
+        session.invalidateAndCancel()
     }
 
     //MARK: - Network requests
 
     /**
-     Executes a GET request to the specified endpoint defined on URLString
+     Executes a XMLRPC call for the method specificied with the arguments provided.
 
-     - parameter URLString:  the url string to be added to the baseURL
+     - parameter method:  the xmlrpc method to be invoked
      - parameter parameters: the parameters to be encoded on the request
      - parameter success:    callback to be called on successful request
      - parameter failure:    callback to be called on failed request
@@ -79,119 +52,112 @@ public class WordPressOrgXMLRPCApi: NSObject
      will be invoked with the error specificing the serialization issues.
      */
     public func callMethod(method: String,
-                    parameters: [AnyObject]?,
-                    success: SuccessResponseBlock,
-                    failure: FailureReponseBlock) -> NSProgress?
+                           parameters: [AnyObject]?,
+                           success: SuccessResponseBlock,
+                           failure: FailureReponseBlock) -> NSProgress?
     {
-        let progress = NSProgress()
-        progress.totalUnitCount = 1
-        let xmlRPCParameters = XMLRPCCallParameters(method: method, parameters: parameters)
-        let task = sessionManager.POST(endpoint.absoluteString, parameters: xmlRPCParameters, success: { (dataTask, result) in
-            success(responseObject: result, httpResponse: dataTask.response as? NSHTTPURLResponse)
-            progress.completedUnitCount = 1
-            }, failure: { (dataTask, error) in
-                failure(error: error, httpResponse: dataTask?.response as? NSHTTPURLResponse)
-                progress.completedUnitCount = 1
-            }
-        )
-        if let task = task {
-            progress.cancellationHandler = {
-                task.cancel()
-            }
-            return progress
-        } else {
+        //Encode request
+        var request: NSURLRequest? = nil
+        do {
+            request = try requestWithMethod(method, parameters: parameters)
+        } catch let encodingError as NSError {
+            failure(error: encodingError, httpResponse: nil)
             return nil
         }
+
+        // Create task
+        let task = session.dataTaskWithRequest(request!) { (data, urlResponse, optionalError) in
+            guard let data = data,
+                let httpResponse = urlResponse as? NSHTTPURLResponse,
+                let contentType = httpResponse.allHeaderFields["Content-Type"] as? String
+                where optionalError == nil else {
+                    if let error = optionalError {
+                        failure(error: error, httpResponse: urlResponse as? NSHTTPURLResponse)
+                    } else {
+                        failure(error: WordPressOrgXMLRPCApiError.Unknown as NSError, httpResponse: urlResponse as? NSHTTPURLResponse)
+                    }
+                    return
+            }
+
+            if ["application/xml", "text/xml"].filter({ (type) -> Bool in return contentType.hasPrefix(type)}).count == 0 {
+                failure(error: WordPressOrgXMLRPCApiError.ResponseSerializationFailed as NSError, httpResponse: httpResponse)
+                return
+            }
+
+            let decoder = WPXMLRPCDecoder(data: data)
+
+            guard !decoder.isFault(),
+                let responseXML = decoder.object() else {
+                    let decoderError = decoder.error()
+                    failure(error: decoderError,  httpResponse: httpResponse)
+                    return
+            }
+
+            success(responseObject: responseXML, httpResponse: httpResponse)
+        }
+
+        // Progress report
+        let progress = NSProgress()
+        progress.totalUnitCount = 1
+        progress.cancellationHandler = {
+            task.cancel()
+        }
+        task.resume()
+        return progress
+    }
+
+    private func requestWithMethod(method: String, parameters: [AnyObject]?) throws -> NSURLRequest {
+        let mutableRequest = NSMutableURLRequest(URL: endpoint)
+        mutableRequest.HTTPMethod = "POST"
+        mutableRequest.setValue("text/xml", forHTTPHeaderField:"Content-Type")
+        let encoder = WPXMLRPCEncoder(method: method, andParameters: parameters)
+        mutableRequest.HTTPBody = try encoder.dataEncoded()
+
+        return mutableRequest
     }
 }
 
-private final class XMLRPCCallParameters {
-    let method: String
-    let parameters: [AnyObject]?
-    let streamingCacheFile: NSURL?
+extension NSURLSession: NSURLSessionDelegate {
 
-    init(method:String, parameters:[AnyObject]? = nil, streamingCacheFile: NSURL? = nil) {
-        self.method = method
-        self.parameters = parameters
-        self.streamingCacheFile = streamingCacheFile
+    public func URLSession(session: NSURLSession, didReceiveChallenge challenge: NSURLAuthenticationChallenge, completionHandler: (NSURLSessionAuthChallengeDisposition, NSURLCredential?) -> Void) {
+            switch challenge.protectionSpace.authenticationMethod {
+            case NSURLAuthenticationMethodServerTrust:
+                var result = SecTrustResultType(kSecTrustResultInvalid)
+                if let serverTrust = challenge.protectionSpace.serverTrust {
+                    let certificateStatus = SecTrustEvaluate(serverTrust, &result)
+                    if certificateStatus == 0 && result == SecTrustResultType(kSecTrustResultRecoverableTrustFailure) {
+                        //                    dispatch_async(dispatch_get_main_queue(), ^(void) {
+                        //                        [WPHTTPAuthenticationAlertController presentWithChallenge:challenge];
+                        //                        });
+                    } else {
+                        completionHandler(.PerformDefaultHandling, nil)
+                        //[challenge.sender continueWithoutCredentialForAuthenticationChallenge:challenge];
+                    }
+                }
+            case NSURLAuthenticationMethodClientCertificate:
+                completionHandler(.PerformDefaultHandling, nil)
+            case NSURLAuthenticationMethodHTTPBasic:
+                completionHandler(.PerformDefaultHandling, nil)
+            default:
+                completionHandler(.PerformDefaultHandling, nil)
+            }
+    }
+}
+
+extension NSURLSession: NSURLSessionTaskDelegate {
+    public func URLSession(session: NSURLSession, task: NSURLSessionTask, didReceiveChallenge challenge: NSURLAuthenticationChallenge, completionHandler: (NSURLSessionAuthChallengeDisposition, NSURLCredential?) -> Void) {
+        completionHandler(.PerformDefaultHandling, nil)
     }
 }
 
 /**
  Error constants for the WordPress XMLRPC API
-
- - CallNeedtoBePOST:                   The parameters sent to the server where invalid
  - RequestSerializationFailed:     The serialization of the request failed
+ - ResponseSerializationFailed:     The serialization of the response failed
  - Unknown:                        Unknow error happen
  */
 @objc public enum WordPressOrgXMLRPCApiError: Int, ErrorType {
-    case CallNeedsToBePOST
-    case InvalidArguments
     case RequestSerializationFailed
+    case ResponseSerializationFailed
     case Unknown
-}
-
-/// A custom serializer to handle XMLRPC responses
-final class WordPressOrgXMLRPCResponseSerializer: AFHTTPResponseSerializer
-{
-    override init() {
-        super.init()
-        self.acceptableContentTypes = ["application/xml", "text/xml"]
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-
-    override func responseObjectForResponse(response: NSURLResponse?, data: NSData?, error: NSErrorPointer) -> AnyObject? {
-        let responseObject = super.responseObjectForResponse(response, data: data, error: error)
-        guard error.memory == nil,
-            let unwrappedData = data else {
-            return responseObject
-        }
-        let decoder = WPXMLRPCDecoder(data:unwrappedData)
-
-        guard !decoder.isFault(),
-            let responseXML = decoder.object() else {
-            let decoderError = decoder.error()
-            error.memory = decoderError
-            return responseObject
-        }
-
-        return responseXML
-    }
-}
-
-/// A custom serializer to handle XMLRPC requests
-final class WordPressOrgXMLRPCRequestSerializer: AFHTTPRequestSerializer
-{
-    override func requestBySerializingRequest(request: NSURLRequest, withParameters parameters: AnyObject?, error: NSErrorPointer) -> NSURLRequest?
-    {
-        if request.HTTPMethod?.uppercaseString != "POST" {
-            // return error because XMLRPC only works with POST requests
-            let nserror = WordPressOrgXMLRPCApiError.CallNeedsToBePOST as NSError
-            error.memory = NSError(domain:nserror.domain,
-                                   code:nserror.code,
-                                   userInfo:[NSLocalizedDescriptionKey: NSLocalizedString("XMLRPC Calls need to be HTTP POST", comment:"")])
-            return nil
-        }
-        guard let xmlRPCCallParameters = parameters as? XMLRPCCallParameters else {
-            // return error because XMLRPC request must have a XMLRPCCallParameters object as parameter
-            let nserror = WordPressOrgXMLRPCApiError.InvalidArguments as NSError
-            error.memory = NSError(domain:nserror.domain,
-                                   code:nserror.code,
-                                   userInfo:[NSLocalizedDescriptionKey: NSLocalizedString("XMLRPC Calls need to use a XMLRPCCallParameters class",  comment:"")])
-            return nil
-        }
-        let mutableRequest: NSMutableURLRequest = request.mutableCopy() as! NSMutableURLRequest
-        mutableRequest.setValue("text/xml", forHTTPHeaderField:"Content-Type")
-        let encoder = WPXMLRPCEncoder(method:xmlRPCCallParameters.method, andParameters:xmlRPCCallParameters.parameters)
-        do {
-            mutableRequest.HTTPBody = try encoder.dataEncoded()
-        } catch let encodingError as NSError {
-            error.memory = encodingError
-        }
-
-        return mutableRequest
-    }
 }
