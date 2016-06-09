fc40ff4b009fd7e34ca0fc5461cbe6b218325eca
Sérgio Estêvão <sergio.estevao@automattic.com>
2016-06-09 01:16:25 +0100

    Add code to avoid display of multiple alert for the same server.

--- WordPress/WordPressApi/HTTPAuthenticationAlertController.swift
+++ WordPress/WordPressApi/HTTPAuthenticationAlertController.swift
@@ -3,7 +3,18 @@ import Foundation
 
 public class HTTPAuthenticationAlertController {
 
-    public func presentWithChallenge(challenge: NSURLAuthenticationChallenge) {
+    public typealias AuthenticationHandler = (NSURLSessionAuthChallengeDisposition, NSURLCredential?) -> Void
+
+    private static var onGoingChallenges = [NSURLProtectionSpace: [AuthenticationHandler]]()
+
+    static public func presentWithChallenge(challenge: NSURLAuthenticationChallenge, handler: AuthenticationHandler) {
+        if var handlers = onGoingChallenges[challenge.protectionSpace] {
+            handlers.append(handler)
+            onGoingChallenges[challenge.protectionSpace] = handlers
+            return
+        }
+        onGoingChallenges[challenge.protectionSpace] = [handler]
+
         let  controller: UIAlertController
         if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust {
             controller = controllerForServerTrustChallenge(challenge)
@@ -20,7 +31,17 @@ public class HTTPAuthenticationAlertController {
         presentingController.presentViewController(controller, animated:true, completion:nil)
     }
 
-    func controllerForServerTrustChallenge(challenge: NSURLAuthenticationChallenge) -> UIAlertController {
+    static func executeHandlerForChallenge(challenge: NSURLAuthenticationChallenge, disposition: NSURLSessionAuthChallengeDisposition, credential: NSURLCredential?) {
+        guard let handlers = onGoingChallenges[challenge.protectionSpace] else {
+            return
+        }
+        for handler in handlers {
+            handler(disposition, credential)
+        }
+        onGoingChallenges.removeValueForKey(challenge.protectionSpace)
+    }
+
+    static private func controllerForServerTrustChallenge(challenge: NSURLAuthenticationChallenge) -> UIAlertController {
         let title = NSLocalizedString("Certificate error", comment:"Popup title for wrong SSL certificate.")
         let message = String(format: NSLocalizedString("The certificate for this server is invalid. You might be connecting to a server that is pretending to be “%@” which could put your confidential information at risk.\n\nWould you like to trust the certificate anyway?", comment: ""), challenge.protectionSpace.host)
         let controller =  UIAlertController(title:title, message:message, preferredStyle:UIAlertControllerStyle.Alert)
@@ -28,7 +49,7 @@ public class HTTPAuthenticationAlertController {
         let cancelAction = UIAlertAction(title:NSLocalizedString("Cancel", comment:"Cancel button label"),
                                          style:UIAlertActionStyle.Default,
                                          handler:{ (action) in
-                                            challenge.sender?.cancelAuthenticationChallenge(challenge)
+                                            executeHandlerForChallenge(challenge, disposition: .CancelAuthenticationChallenge, credential: nil)
         })
         controller.addAction(cancelAction)
 
@@ -36,16 +57,15 @@ public class HTTPAuthenticationAlertController {
                                         style:UIAlertActionStyle.Default,
                                         handler:{ (action) in
                                             let credential = NSURLCredential(forTrust: challenge.protectionSpace.serverTrust!)
-
                                             NSURLCredentialStorage.sharedCredentialStorage().setDefaultCredential(credential, forProtectionSpace:challenge.protectionSpace)
-                                            challenge.sender?.useCredential(credential, forAuthenticationChallenge:challenge)
+                                            executeHandlerForChallenge(challenge, disposition: .UseCredential, credential: credential)
         })
         controller.addAction(trustAction)
         return controller
     }
 
-    func controllerForUserAuthenticationChallenge(challenge: NSURLAuthenticationChallenge) -> UIAlertController {
-        let title = NSLocalizedString("Authentication required", comment: "Popup title to ask for user credentials.")
+    static private func controllerForUserAuthenticationChallenge(challenge: NSURLAuthenticationChallenge) -> UIAlertController {
+        let title = String(format: NSLocalizedString("Authentication required for host: %@", comment: "Popup title to ask for user credentials."), challenge.protectionSpace.host)
         let message = NSLocalizedString("Please enter your credentials", comment: "Popup message to ask for user credentials (fields shown below).")
         let controller =  UIAlertController(title: title,
                                             message: message,
@@ -63,7 +83,7 @@ public class HTTPAuthenticationAlertController {
         let cancelAction = UIAlertAction(title:NSLocalizedString("Cancel", comment: "Cancel button label"),
                                          style: .Default,
                                          handler:{ (action) in
-                                            challenge.sender?.cancelAuthenticationChallenge(challenge)
+                                            executeHandlerForChallenge(challenge, disposition: .CancelAuthenticationChallenge, credential: nil)
         })
         controller.addAction(cancelAction)
 
@@ -72,11 +92,12 @@ public class HTTPAuthenticationAlertController {
                                         handler:{(action) in
                                             guard let username = controller.textFields?.first?.text,
                                                 let password = controller.textFields?.last?.text else {
+                                                    executeHandlerForChallenge(challenge, disposition: .CancelAuthenticationChallenge, credential: nil)
                                                     return
                                             }
                                             let credential = NSURLCredential(user: username, password: password, persistence:NSURLCredentialPersistence.Permanent)
                                             NSURLCredentialStorage.sharedCredentialStorage().setDefaultCredential(credential, forProtectionSpace: challenge.protectionSpace)
-                                            challenge.sender?.useCredential(credential, forAuthenticationChallenge: challenge)
+                                            executeHandlerForChallenge(challenge, disposition: .UseCredential, credential: credential)
         })
         controller.addAction(loginAction)
         return controller
