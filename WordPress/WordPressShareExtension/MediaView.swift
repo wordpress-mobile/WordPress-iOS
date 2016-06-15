655669f3f9df9af94a028e2e05550b62ba86a9d3
Jorge Leandro Perez <jorge@lantean.co>
2016-06-15 11:53:09 -0300

    ShareViewController: Wiring MediaView Helper

--- WordPress/WordPressShareExtension/MediaView.swift
+++ WordPress/WordPressShareExtension/MediaView.swift
@@ -2,11 +2,13 @@ import Foundation
 import UIKit
 
 
-///
+/// Displays an Image with a capped size, defined by the maximumSize property.
 ///
 class MediaView : UIView
 {
-    ///
+    // MARK: - Properties
+
+    /// Defines the maximum size that this view might occupy.
     ///
     var maximumSize = CGSizeMake(90, 90) {
         didSet {
@@ -14,7 +16,7 @@ class MediaView : UIView
         }
     }
 
-    ///
+    /// The image that should be displayed
     ///
     var image: UIImage? {
         get {
@@ -26,44 +28,50 @@ class MediaView : UIView
         }
     }
 
-    ///
+    /// Internal imageView Instance
     ///
     private let imageView = UIImageView()
 
-    ///
+    /// Internal activityIndicator Instance
     ///
     private let activityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: .Gray)
 
-    ///
-    ///
+
+
+    // MARK: - Initializers
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupSubviews()
     }
 
-    ///
-    ///
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setupSubviews()
     }
 
-    ///
+
+
+    // MARK: - Public Methods
+
+    /// Displays a spinner at the center of this view, and dims the imageView
     ///
     func startSpinner() {
         activityIndicatorView.startAnimating()
         imageView.alpha = Constants.alphaDimming
     }
 
-    ///
+    /// Stops the spinner, and restores the original alpha
     ///
     func stopSpinner() {
         activityIndicatorView.stopAnimating()
         imageView.alpha = Constants.alphaFull
     }
 
-    ///
-    ///
+
+
+    // MARK: - Private Helpers
+
     private func setupSubviews() {
         translatesAutoresizingMaskIntoConstraints = false
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -80,8 +88,6 @@ class MediaView : UIView
         activityIndicatorView.centerYAnchor.constraintEqualToAnchor(centerYAnchor).active = true
     }
 
-    ///
-    ///
     private func refreshContentSize() {
         let targetSize = (image != nil) ? maximumSize : CGSizeZero
         widthAnchor.constraintEqualToConstant(targetSize.width).active = true
