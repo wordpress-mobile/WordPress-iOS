df4ef7fd860e3214822031df3d9a9c59b346935e
Nate Heagy <nate@heagy.com>
2016-06-21 16:11:23 -0600

    Clarified a comment

--- WordPress/Classes/ViewRelated/Blog/BlogDetailsViewController.m
+++ WordPress/Classes/ViewRelated/Blog/BlogDetailsViewController.m
@@ -500,7 +500,7 @@ NSInteger const BlogDetailAccountHideViewAdminDay = 7;
     WordPressAppDelegate *appDelegate = [WordPressAppDelegate sharedInstance];
     BOOL isOnWifi = [appDelegate.internetReachability isReachableViaWiFi];
     
-    if (isOnWifi && oauthToken) // check for wifi
+    if (isOnWifi && oauthToken) // only preload on wifi
     {
         self.statsService = [[WPStatsService alloc] initWithSiteId:self.blog.siteID siteTimeZone:[self.blogService timeZoneForBlog:self.blog] oauth2Token:oauthToken andCacheExpirationInterval:5 * 60];
         [self.statsService retrieveInsightsStatsWithAllTimeStatsCompletionHandler:nil insightsCompletionHandler:nil todaySummaryCompletionHandler:nil latestPostSummaryCompletionHandler:nil commentsAuthorCompletionHandler:nil commentsPostsCompletionHandler:nil tagsCategoriesCompletionHandler:nil followersDotComCompletionHandler:nil followersEmailCompletionHandler:nil publicizeCompletionHandler:nil streakCompletionHandler:nil progressBlock:nil andOverallCompletionHandler:nil];
