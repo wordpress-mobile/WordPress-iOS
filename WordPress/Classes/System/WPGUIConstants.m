98d4c474877a732c884bb2d763db7a311e6613e2
kurzee <brent@kurzee.com>
2016-06-24 13:44:04 -0500

    Removes (nukes) GUI padding constants (values).

--- WordPress/Classes/System/WPGUIConstants.m
+++ WordPress/Classes/System/WPGUIConstants.m
@@ -12,6 +12,4 @@ const NSTimeInterval WPAnimationDurationFaster  = 0.07;
 
 const CGFloat WPTableViewTopMargin              = 40.0;
 const CGFloat WPTableViewDefaultRowHeight       = 44.0;
-const CGRect WPTableHeaderPadFrame              = {0.0, 0.0, 0.0, 40.0};
-const CGRect WPTableFooterPadFrame              = {0.0, 0.0, 0.0, 48.0};
 const UIEdgeInsets WPTableViewContentInsets     = {40.0, 0.0, 0.0, 0.0};
