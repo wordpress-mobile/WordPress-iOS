e97b62eb2c6ff4c7f89dcca6a13c8bbe9b6cb182
Jorge Leandro Perez <jorge@lantean.co>
2016-06-27 15:34:53 -0300

    NotificationsViewController+Extensions: Setup Constraints

--- WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationsViewController+Extensions.swift
+++ WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationsViewController+Extensions.swift
@@ -4,4 +4,17 @@ import WordPressShared
 
 extension NotificationsViewController
 {
+
+    // MARK: - Setup Helpers
+
+    func setupConstraints() {
+        precondition(ratingsTopConstraint != nil)
+        precondition(ratingsHeightConstraint != nil)
+
+        // Fix: contentInset breaks tableSectionViews. Let's just increase the headerView's height
+        ratingsTopConstraint.constant = UIDevice.isPad() ? CGRectGetHeight(WPTableHeaderPadFrame) : 0.0
+
+        // Ratings is initially hidden!
+        ratingsHeightConstraint.constant = 0
+    }
 }
