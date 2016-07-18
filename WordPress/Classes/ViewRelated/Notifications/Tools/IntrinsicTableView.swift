5af6dbe29fc00da76aea444f5cc9a4d16f655a96
Jorge Leandro Perez <jorge@lantean.co>
2016-07-18 18:13:27 -0300

    Implements IntrinsicTableView

---
+++ WordPress/Classes/ViewRelated/Notifications/Tools/IntrinsicTableView.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+
+
+/// This TableView subclass allows us to use UIStackView along with TableViews. Intrinsic Content Size
+/// will be automatically calculated, based on the Table's Content Size.
+///
+/// Ref.: 
+///     -   https://developer.apple.com/library/ios/technotes/tn2154/_index.html#//apple_ref/doc/uid/DTS40013309
+///     -   http://stackoverflow.com/questions/17334478/uitableview-within-uiscrollview-using-autolayout
+///
+class IntrinsicTableView: UITableView
+{
+    override func intrinsicContentSize() -> CGSize {
+        return CGSizeMake(UIViewNoIntrinsicMetric, contentSize.height)
+    }
+}
