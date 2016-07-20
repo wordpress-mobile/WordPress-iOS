22e007f087cbe4acea49d2b0306b7bbaf2047689
kurzee <brent@kurzee.com>
2016-07-20 14:59:45 -0500

    Blog Details: Header readability/stackview improvements.

--- WordPress/Classes/ViewRelated/Blog/BlogDetailHeaderView.m
+++ WordPress/Classes/ViewRelated/Blog/BlogDetailHeaderView.m
@@ -6,12 +6,13 @@
 
 
 const CGFloat BlogDetailHeaderViewBlavatarSize = 40.0;
-const CGFloat BlogDetailHeaderViewLabelHeight = 20.0;
 const CGFloat BlogDetailHeaderViewLabelHorizontalPadding = 10.0;
 
 @interface BlogDetailHeaderView ()
 
+@property (nonatomic, strong) UIStackView *stackView;
 @property (nonatomic, strong) UIImageView *blavatarImageView;
+@property (nonatomic, strong) UIStackView *labelsStackView;
 @property (nonatomic, strong) UILabel *titleLabel;
 @property (nonatomic, strong) UILabel *subtitleLabel;
 
@@ -23,16 +24,11 @@ const CGFloat BlogDetailHeaderViewLabelHorizontalPadding = 10.0;
 {
     self = [super initWithFrame:frame];
     if (self) {
-        _blavatarImageView = [self newImageViewForBlavatar];
-        [self addSubview:_blavatarImageView];
-
-        _titleLabel = [self newLabelForTitle];
-        [self addSubview:_titleLabel];
-
-        _subtitleLabel = [self newLabelForSubtitle];
-        [self addSubview:_subtitleLabel];
-
-        [self configureConstraints];
+        [self setupStackView];
+        [self setupBalavatarImageView];
+        [self setupLabelsStackView];
+        [self setupTitleLabel];
+        [self setupSubtitleLabel];
     }
     return self;
 }
@@ -47,39 +43,88 @@ const CGFloat BlogDetailHeaderViewLabelHorizontalPadding = 10.0;
     NSString *blogName = blog.settings.name;
     [self.titleLabel setText:((blogName && !blogName.isEmpty) ? blogName : blog.displayURL)];
     [self.subtitleLabel setText:blog.displayURL];
+    [self.labelsStackView setNeedsLayout];
 }
 
-#pragma mark - Private Methods
+#pragma mark - Subview setup
 
-- (void)configureConstraints
+- (void)setupStackView
 {
-    NSDictionary *views = NSDictionaryOfVariableBindings(_blavatarImageView, _titleLabel, _subtitleLabel);
-    NSDictionary *metrics = @{@"blavatarSize": @(BlogDetailHeaderViewBlavatarSize),
-                              @"labelHeight":@(BlogDetailHeaderViewLabelHeight),
-                              @"labelHorizontalPadding": @(BlogDetailHeaderViewLabelHorizontalPadding)};
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_blavatarImageView(blavatarSize)]|"
-                                                                 options:0
-                                                                 metrics:metrics
-                                                                   views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|[_blavatarImageView(blavatarSize)]-labelHorizontalPadding-[_titleLabel]|"
-                                                                 options:0
-                                                                 metrics:metrics
-                                                                   views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|[_blavatarImageView(blavatarSize)]-labelHorizontalPadding-[_subtitleLabel]|"
-                                                                 options:0
-                                                                 metrics:metrics
-                                                                   views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_titleLabel(labelHeight)][_subtitleLabel(labelHeight)]"
-                                                                 options:0
-                                                                 metrics:metrics
-                                                                   views:views]];
-    [super setNeedsUpdateConstraints];
+    UIStackView *stackView = [[UIStackView alloc] init];
+    stackView.translatesAutoresizingMaskIntoConstraints = NO;
+    stackView.axis = UILayoutConstraintAxisHorizontal;
+    stackView.distribution = UIStackViewDistributionFill;
+    stackView.alignment = UIStackViewAlignmentCenter;
+    stackView.spacing = BlogDetailHeaderViewLabelHorizontalPadding;
+    [self addSubview:stackView];
+
+    NSLayoutConstraint *leadingConstraint;
+    NSLayoutConstraint *trailingConstraint;
+
+    if ([WPDeviceIdentification isiPhone]) {
+        // On iPhone, the readable content guide seems to be accurately aligned with the cell's content margins.
+        UILayoutGuide *readableGuide = self.readableContentGuide;
+        leadingConstraint = [stackView.leadingAnchor constraintEqualToAnchor:readableGuide.leadingAnchor];
+        trailingConstraint = [stackView.trailingAnchor constraintEqualToAnchor:readableGuide.trailingAnchor];
+    } else {
+        // On iPad, the correct readable margins seem to already be inherited via the tableHeaderView
+        // so following this view's readable content guide is not necessary.
+        leadingConstraint = [stackView.leadingAnchor constraintEqualToAnchor:self.leadingAnchor];
+        trailingConstraint = [stackView.trailingAnchor constraintEqualToAnchor:self.trailingAnchor];
+    }
+
+    [NSLayoutConstraint activateConstraints:@[
+                                              leadingConstraint,
+                                              trailingConstraint,
+                                              [stackView.topAnchor constraintEqualToAnchor:self.topAnchor],
+                                              [stackView.bottomAnchor constraintEqualToAnchor:self.bottomAnchor],
+                                              [stackView.centerXAnchor constraintEqualToAnchor:self.centerXAnchor],
+                                              [stackView.centerYAnchor constraintEqualToAnchor:self.centerYAnchor]
+                                              ]];
+    _stackView = stackView;
 }
 
-#pragma mark - Subview factories
-
-- (UILabel *)newLabelForTitle
+- (void)setupBalavatarImageView
 {
+    NSAssert(_stackView != nil, @"stackView was nil");
+
+    CGRect blavatarFrame = CGRectMake(0.0f, 0.0f, BlogDetailHeaderViewBlavatarSize, BlogDetailHeaderViewBlavatarSize);
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:blavatarFrame];
+    imageView.backgroundColor = [UIColor whiteColor];
+    imageView.translatesAutoresizingMaskIntoConstraints = NO;
+    imageView.layer.borderColor = [[UIColor whiteColor] CGColor];
+    imageView.layer.borderWidth = 1.0;
+    [_stackView addArrangedSubview:imageView];
+
+    [NSLayoutConstraint activateConstraints:@[
+                                              [imageView.widthAnchor constraintEqualToConstant:BlogDetailHeaderViewBlavatarSize],
+                                              [imageView.heightAnchor constraintEqualToConstant:BlogDetailHeaderViewBlavatarSize]
+                                              ]];
+    _blavatarImageView = imageView;
+}
+
+- (void)setupLabelsStackView
+{
+    NSAssert(_stackView != nil, @"stackView was nil");
+
+    UIStackView *stackView = [[UIStackView alloc] init];
+    stackView.translatesAutoresizingMaskIntoConstraints = NO;
+    stackView.axis = UILayoutConstraintAxisVertical;
+    stackView.distribution = UIStackViewDistributionFill;
+    stackView.alignment = UIStackViewAlignmentFill;
+
+    [_stackView addArrangedSubview:stackView];
+
+    [stackView setContentHuggingPriority:UILayoutPriorityDefaultLow forAxis:UILayoutConstraintAxisHorizontal];
+    [stackView setContentCompressionResistancePriority:UILayoutPriorityDefaultLow forAxis:UILayoutConstraintAxisHorizontal];
+
+    _labelsStackView = stackView;
+}
+
+- (void)setupTitleLabel
+{
+    NSAssert(_labelsStackView != nil, @"labelsStackView was nil");
+
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectZero];
     label.translatesAutoresizingMaskIntoConstraints = NO;
     label.numberOfLines = 1;
@@ -89,11 +134,15 @@ const CGFloat BlogDetailHeaderViewLabelHorizontalPadding = 10.0;
     label.font = [WPFontManager systemRegularFontOfSize:16.0];
     label.adjustsFontSizeToFitWidth = NO;
 
-    return label;
+    [_labelsStackView addArrangedSubview:label];
+
+    _titleLabel = label;
 }
 
-- (UILabel *)newLabelForSubtitle
+- (void)setupSubtitleLabel
 {
+    NSAssert(_labelsStackView != nil, @"labelsStackView was nil");
+
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectZero];
     label.translatesAutoresizingMaskIntoConstraints = NO;
     label.numberOfLines = 1;
@@ -103,18 +152,9 @@ const CGFloat BlogDetailHeaderViewLabelHorizontalPadding = 10.0;
     label.font = [WPFontManager systemItalicFontOfSize:12.0];
     label.adjustsFontSizeToFitWidth = NO;
 
-    return label;
-}
+    [_labelsStackView addArrangedSubview:label];
 
-- (UIImageView *)newImageViewForBlavatar
-{
-    CGRect blavatarFrame = CGRectMake(0.0f, 0.0f, BlogDetailHeaderViewBlavatarSize, BlogDetailHeaderViewBlavatarSize);
-    UIImageView *imageView = [[UIImageView alloc] initWithFrame:blavatarFrame];
-    imageView.backgroundColor = [UIColor whiteColor];
-    imageView.translatesAutoresizingMaskIntoConstraints = NO;
-    imageView.layer.borderColor = [[UIColor whiteColor] CGColor];
-    imageView.layer.borderWidth = 1.0;
-    return imageView;
+    _subtitleLabel = label;
 }
 
 @end
