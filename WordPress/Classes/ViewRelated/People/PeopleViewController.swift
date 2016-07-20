d09621dafc998e01e82f31463abfc0fa2d272ae0
kurzee <brent@kurzee.com>
2016-07-20 17:12:39 -0500

    People: Enables forced margins, temporarily.

--- WordPress/Classes/ViewRelated/People/PeopleViewController.swift
+++ WordPress/Classes/ViewRelated/People/PeopleViewController.swift
@@ -154,6 +154,8 @@ public class PeopleViewController: UITableViewController, NSFetchedResultsContro
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add,
                                                             target: self,
                                                             action: #selector(invitePersonWasPressed))
+
+        tableView.cellLayoutMarginsFollowReadableWidth = false
         WPStyleGuide.configureColorsForView(view, andTableView: tableView)
 
         // By default, let's display the Blog's Users
