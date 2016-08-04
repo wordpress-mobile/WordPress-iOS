20bd9d3c755fcd5375e5e2244cc258869d8b8c63
Jorge Leandro Perez <jorge@lantean.co>
2016-08-04 11:35:40 -0300

    NotificationsViewController: Wiring new Methods

--- WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationsViewController.swift
+++ WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationsViewController.swift
@@ -240,7 +240,7 @@ extension NotificationsViewController
 
     override func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]? {
         guard let note = tableViewHandler?.resultsController.objectOfType(Notification.self, atIndexPath: indexPath),
-            let block = note.blockGroupOfType(.Comment)?.blockOfType(.Comment) else
+            let block = note.blockGroupOfType(.Comment)?.blockOfKind(.Comment) else
         {
             // Not every single row will have actions: Slight hack so that the UX isn't terrible:
             //  -   First: Return an Empty UITableViewRowAction
