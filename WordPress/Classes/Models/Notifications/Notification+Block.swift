cd05f22afd92ead31cbbd76b3ce7a60ca8971930
Jorge Leandro Perez <jorge@lantean.co>
2016-08-04 14:28:41 -0300

    Promotes Block and BlockGroups to top level

--- WordPress/Classes/Models/Notifications/Notification+Block.swift
+++ WordPress/Classes/Models/Notifications/Notification+Block.swift
@@ -2,291 +2,287 @@ import Foundation
 
 
 
+// MARK: - Notification.Block Implementation
 //
-//
-extension Notification
+class NotificationBlock
 {
-    class Block
-    {
-        ///
-        ///
-        typealias MetaKeys  = Notification.MetaKeys
-        typealias BlockKeys = Notification.BlockKeys
+    ///
+    ///
+    enum Kind {
+        case Text
+        case Image      // Includes Badges and Images
+        case User
+        case Comment
+    }
 
-        ///
-        ///
-        enum Kind {
-            case Text
-            case Image      // Includes Badges and Images
-            case User
-            case Comment
-        }
+    ///
+    ///
+    enum Action {
+        case Follow
+        case Like
+        case Spam
+        case Trash
+        case Reply
+        case Approve
+        case Edit
 
-        ///
-        ///
-        enum Action {
-            case Follow
-            case Like
-            case Spam
-            case Trash
-            case Reply
-            case Approve
-            case Edit
-
-            var toString: String {
-                switch self {
-                case Follow:    return "follow"
-                case Like:      return "like-comment"
-                case Spam:      return "spam-comment"
-                case Trash:     return "trash-comment"
-                case Reply:     return "replyto-comment"
-                case Approve:   return "approve-comment"
-                case Edit:      return "approve-comment"
-                }
+        var toString: String {
+            switch self {
+            case Follow:    return "follow"
+            case Like:      return "like-comment"
+            case Spam:      return "spam-comment"
+            case Trash:     return "trash-comment"
+            case Reply:     return "replyto-comment"
+            case Approve:   return "approve-comment"
+            case Edit:      return "approve-comment"
             }
         }
+    }
 
-        ///
-        ///
-        private(set) var kind: Kind
+    ///
+    ///
+    private(set) var kind: Kind
 
-        ///
-        ///
-        private(set) var text: String?
+    ///
+    ///
+    private(set) var actions: [String: AnyObject]?
 
-        ///
-        ///
-        var textOverride: String? {
-            didSet {
-                parent?.didChangeOverrides()
-            }
+    ///
+    ///
+    private var actionsOverride = [Action: Bool]()
+
+    ///
+    ///
+    private var dynamicAttributesCache = [String: AnyObject]()
+
+    ///
+    ///
+    private(set) var media: [NotificationMedia]
+
+    ///
+    ///
+    private(set) var meta: [String: AnyObject]?
+
+    ///
+    ///
+    private weak var parent: Notification?
+
+    ///
+    ///
+    private(set) var ranges: [NotificationRange]
+
+    ///
+    ///
+    private(set) var text: String?
+
+    ///
+    ///
+    var textOverride: String? {
+        didSet {
+            parent?.didChangeOverrides()
         }
+    }
 
-        ///
-        ///
-        private(set) var actions: [String: AnyObject]?
-
-        ///
-        ///
-        private var actionsOverride = [Action: Bool]()
-
-        ///
-        ///
-        private(set) var media: [NotificationMedia]
-
-        ///
-        ///
-        private weak var parent: Notification?
-
-        ///
-        ///
-        private(set) var meta: [String: AnyObject]?
-
-        ///
-        ///
-        private(set) var ranges: [NotificationRange]
+    ///
+    ///
+    typealias MetaKeys  = Notification.MetaKeys
+    typealias BlockKeys = Notification.BlockKeys
 
 
-        ///
-        ///
-        private var dynamicAttributesCache = [String: AnyObject]()
+    ///
+    ///
+    init(dictionary: [String: AnyObject], parent: Notification) {
+        let rawRanges = dictionary[BlockKeys.Ranges] as? [AnyObject]
+        let rawMedia = dictionary[BlockKeys.Media] as? [AnyObject]
 
+        text        = dictionary[BlockKeys.Text] as? String
+        ranges      = NotificationRange.rangesFromArray(rawRanges)
+        media       = NotificationMedia.mediaFromArray(rawMedia)
+        meta        = dictionary[BlockKeys.Meta] as? [String: AnyObject]
+        actions     = dictionary[BlockKeys.Actions] as? [String: AnyObject]
+        self.parent = parent
+        self.kind = .Comment
 
+        // Duck Typing code below: Infer block kind based on... stuff. (Sorry)
+        //
+        if (dictionary[BlockKeys.Kind] as? String)?.isEqual("user") ?? false {
+            kind = .User
 
-        ///
-        ///
-        init(dictionary: [String: AnyObject], parent: Notification) {
-            let rawRanges = dictionary[BlockKeys.Ranges] as? [AnyObject]
-            let rawMedia = dictionary[BlockKeys.Media] as? [AnyObject]
-
-            text        = dictionary[BlockKeys.Text] as? String
-            ranges      = NotificationRange.rangesFromArray(rawRanges)
-            media       = NotificationMedia.mediaFromArray(rawMedia)
-            meta        = dictionary[BlockKeys.Meta] as? [String: AnyObject]
-            actions     = dictionary[BlockKeys.Actions] as? [String: AnyObject]
-            self.parent = parent
-
+        } else if metaCommentID!.isEqual(parent.metaCommentID) && metaSiteID != nil {
             kind = .Comment
 
-            // Duck Typing code below: Infer block kind based on... stuff. (Sorry)
+        } else if (media.first?.isImage ?? false) || (media.first?.isBadge ?? false) {
+            kind = .Image
 
-//            let media = media.first
-//
-//            //  User
-//            if rawBlock[NoteTypeKey].isEqual(NoteTypeUser) {
-//                kind = .User
-//
-//                //  Comments
-//            } else if block.metaCommentID.isEqual(notification.metaCommentID) && block.metaSiteID != nil {
-//                kind = .Comment
-//
-//                //  Images
-//            } else if media?.isImage || media?.isBadge {
-//                kind = .Image
-//
-//                //  Text
-//            } else {
-//                kind = .Text
-//            }
+        } else {
+            kind = .Text
         }
+    }
 
-        /// Returns the Meta ID's collection, if any.
-        ///
-        private var metaIds: [String: AnyObject]? {
-            return meta?[MetaKeys.Ids] as? [String: AnyObject]
-        }
 
-        /// Returns the Meta ID's collection, if any.
-        ///
-        private var metaLinks: [String: AnyObject]? {
-            return meta?[MetaKeys.Links] as? [String: AnyObject]
-        }
-
-        ///
-        ///
-        private var metaTitles: [String: AnyObject]? {
-            return meta?[MetaKeys.Titles] as? [String: AnyObject]
-        }
-
-        ///
-        ///
-        var metaSiteID: NSNumber? {
-            return metaIds?[MetaKeys.Site] as? NSNumber
-        }
-
-        ///
-        ///
-        var metaCommentID: NSNumber? {
-            return metaIds?[MetaKeys.Comment] as? NSNumber
-        }
-
-        ///
-        ///
-        var metaLinksHome: NSURL? {
-            guard let rawLink = metaLinks?[MetaKeys.Home] as? String else {
+    /// Returns all of the Image URL's referenced by the NotificationMedia instances
+    ///
+    var imageUrls: [NSURL] {
+        return media.flatMap {
+            guard $0.isImage && $0.mediaURL != nil else {
                 return nil
             }
 
-            return NSURL(string: rawLink)
+            return $0.mediaURL
         }
+    }
 
-        ///
-        ///
-        var metaTitlesHome: String? {
-            return metaTitles?[MetaKeys.Home] as? String
-        }
+    /// Returns YES if the associated comment (if any) is approved. NO otherwise.
+    ///
+    var isCommentApproved: Bool {
+        return isActionOn(.Approve) || !isActionEnabled(.Approve)
+    }
 
-        /// Returns all of the Image URL's referenced by the NotificationMedia instances
-        ///
-        var imageUrls: [NSURL] {
-            return media.flatMap {
-                guard $0.isImage && $0.mediaURL != nil else {
-                    return nil
-                }
+    /// Returns the Meta ID's collection, if any.
+    ///
+    private var metaIds: [String: AnyObject]? {
+        return meta?[MetaKeys.Ids] as? [String: AnyObject]
+    }
 
-                return $0.mediaURL
-            }
-        }
+    /// Returns the Meta Links collection, if any.
+    ///
+    private var metaLinks: [String: AnyObject]? {
+        return meta?[MetaKeys.Links] as? [String: AnyObject]
+    }
 
-        /// Returns YES if the associated comment (if any) is approved. NO otherwise.
-        ///
-        var isCommentApproved: Bool {
-            return isActionOn(.Approve) || !isActionEnabled(.Approve)
-        }
+    ///
+    ///
+    private var metaTitles: [String: AnyObject]? {
+        return meta?[MetaKeys.Titles] as? [String: AnyObject]
+    }
 
-        /// Allows us to set a local override for a remote value. This is used to fake the UI, while
-        /// there's a BG call going on.
-        ///
-        func setOverrideValue(value: Bool, forAction action: Action) {
-            actionsOverride[action] = value
-            parent?.didChangeOverrides()
-        }
+    ///
+    ///
+    var metaCommentID: NSNumber? {
+        return metaIds?[MetaKeys.Comment] as? NSNumber
+    }
 
-        /// Removes any local (temporary) value that might have been set by means of *setActionOverrideValue*.
-        ///
-        func removeOverrideValueForAction(action: Action) {
-            actionsOverride.removeValueForKey(action)
-            parent?.didChangeOverrides()
-        }
-
-        /// Returns the Notification Block status for a given action. If there's any local override,
-        /// the (override) value will be returned.
-        ///
-        private func valueForAction(action: Action) -> Bool? {
-            guard let overrideValue = actionsOverride[action] else {
-                let value = actions?[action.toString] as? NSNumber
-                return value?.boolValue
-            }
-
-            return overrideValue
-        }
-
-        /// Returns *true* if a given action is available
-        ///
-        func isActionEnabled(action: Action) -> Bool {
-            return valueForAction(action) != nil
-        }
-
-        /// Returns *true* if a given action is toggled on. (I.e.: Approval = On, means that the comment
-        /// is currently approved).
-        ///
-        func isActionOn(action: Action) -> Bool {
-            return valueForAction(action) ?? false
-        }
-
-        // Dynamic Attribute Cache: used internally by the Interface Extension, as an optimization.
-        ///
-        func cacheValueForKey(key: String) -> AnyObject? {
-            return self.dynamicAttributesCache[key]
-        }
-
-        ///
-        ///
-        func setCacheValue(value: AnyObject?, forKey key: String) {
-            guard let value = value else {
-                dynamicAttributesCache.removeValueForKey(key)
-                return
-            }
-
-            dynamicAttributesCache[key] = value
-        }
-
-        /// Finds the first NotificationRange instance that maps to a given URL.
-        ///
-        func notificationRangeWithUrl(url: NSURL) -> NotificationRange? {
-            for range in ranges {
-                if let rangeURL = range.url where rangeURL.isEqual(url) {
-                    return range
-                }
-            }
-
+    ///
+    ///
+    var metaLinksHome: NSURL? {
+        guard let rawLink = metaLinks?[MetaKeys.Home] as? String else {
             return nil
         }
 
-        /// Finds the first NotificationRange instance that maps to a given CommentID.
-        ///
-        func notificationRangeWithCommentId(commentID: NSNumber) -> NotificationRange? {
-            for range in ranges {
-                if let rangeCommentID = range.commentID where rangeCommentID.isEqual(commentID) {
-                    return range
-                }
+        return NSURL(string: rawLink)
+    }
+
+    ///
+    ///
+    var metaSiteID: NSNumber? {
+        return metaIds?[MetaKeys.Site] as? NSNumber
+    }
+
+    ///
+    ///
+    var metaTitlesHome: String? {
+        return metaTitles?[MetaKeys.Home] as? String
+    }
+}
+
+
+
+// MARK: - NotificationBlock Methods
+//
+extension NotificationBlock
+{
+    /// Allows us to set a local override for a remote value. This is used to fake the UI, while
+    /// there's a BG call going on.
+    ///
+    func setOverrideValue(value: Bool, forAction action: Action) {
+        actionsOverride[action] = value
+        parent?.didChangeOverrides()
+    }
+
+    /// Removes any local (temporary) value that might have been set by means of *setActionOverrideValue*.
+    ///
+    func removeOverrideValueForAction(action: Action) {
+        actionsOverride.removeValueForKey(action)
+        parent?.didChangeOverrides()
+    }
+
+    /// Returns the Notification Block status for a given action. If there's any local override,
+    /// the (override) value will be returned.
+    ///
+    private func valueForAction(action: Action) -> Bool? {
+        guard let overrideValue = actionsOverride[action] else {
+            let value = actions?[action.toString] as? NSNumber
+            return value?.boolValue
+        }
+
+        return overrideValue
+    }
+
+    /// Returns *true* if a given action is available
+    ///
+    func isActionEnabled(action: Action) -> Bool {
+        return valueForAction(action) != nil
+    }
+
+    /// Returns *true* if a given action is toggled on. (I.e.: Approval = On, means that the comment
+    /// is currently approved).
+    ///
+    func isActionOn(action: Action) -> Bool {
+        return valueForAction(action) ?? false
+    }
+
+    // Dynamic Attribute Cache: used internally by the Interface Extension, as an optimization.
+    ///
+    func cacheValueForKey(key: String) -> AnyObject? {
+        return self.dynamicAttributesCache[key]
+    }
+
+    ///
+    ///
+    func setCacheValue(value: AnyObject?, forKey key: String) {
+        guard let value = value else {
+            dynamicAttributesCache.removeValueForKey(key)
+            return
+        }
+
+        dynamicAttributesCache[key] = value
+    }
+
+    /// Finds the first NotificationRange instance that maps to a given URL.
+    ///
+    func notificationRangeWithUrl(url: NSURL) -> NotificationRange? {
+        for range in ranges {
+            if let rangeURL = range.url where rangeURL.isEqual(url) {
+                return range
+            }
+        }
+
+        return nil
+    }
+
+    /// Finds the first NotificationRange instance that maps to a given CommentID.
+    ///
+    func notificationRangeWithCommentId(commentID: NSNumber) -> NotificationRange? {
+        for range in ranges {
+            if let rangeCommentID = range.commentID where rangeCommentID.isEqual(commentID) {
+                return range
+            }
+        }
+
+        return nil
+    }
+
+    ///
+    ///
+    class func blocksFromArray(rawBlocks: [AnyObject], parent: Notification) -> [NotificationBlock]? {
+        let parsed: [NotificationBlock] = rawBlocks.flatMap({
+            guard let rawBlock = $0 as? [String: AnyObject] else {
+                return nil
             }
 
-            return nil
-        }
+            return NotificationBlock(dictionary: rawBlock, parent: parent)
+        })
 
-        ///
-        ///
-        class func fromArray(rawBlocks: [AnyObject], notification: Notification) -> [Block]? {
-            let parsed: [Block] = rawBlocks.flatMap({
-                guard let rawBlock = $0 as? [String: AnyObject] else {
-                    return nil
-                }
-
-                return Block(dictionary: rawBlock, parent: notification)
-            })
-
-            return parsed.isEmpty ? nil : parsed
-        }
+        return parsed.isEmpty ? nil : parsed
     }
 }
--- WordPress/Classes/Models/Notifications/Notification+BlockGroup.swift
+++ WordPress/Classes/Models/Notifications/Notification+BlockGroup.swift
@@ -2,150 +2,156 @@ import Foundation
 
 
 
-// MARK: - Block Group
+// MARK: - Block Group: Multiple Blocks can be mapped to a single view
 //
-extension Notification
+class NotificationBlockGroup
 {
-    //// Adapter Class: Multiple Blocks can be mapped to a single view
-    class BlockGroup
-    {
-        ///
-        ///
-        private (set) var blocks: [Block]
+    ///
+    ///
+    enum Kind {
+        case Text
+        case Image
+        case User
+        case Comment    // Blocks: User  + Comment
+        case Actions    // Blocks: Comment
+        case Subject    // Blocks: Text  + Text
+        case Header     // Blocks: Image + Text
+        case Footer     // Blocks: Text
+    }
 
-        ///
-        ///
-        private (set) var kind: Kind
+    ///
+    ///
+    private (set) var blocks: [NotificationBlock]
 
-        ///
-        ///
-        enum Kind {
-            case Text
-            case Image
-            case User
-            case Comment    // Blocks: User  + Comment
-            case Actions    // Blocks: Comment
-            case Subject    // Blocks: Text  + Text
-            case Header     // Blocks: Image + Text
-            case Footer     // Blocks: Text
+    ///
+    ///
+    private (set) var kind: Kind
+
+    ///
+    ///
+    init(blocks: [NotificationBlock], kind: Kind) {
+        self.blocks = blocks
+        self.kind = kind
+    }
+
+
+    ///
+    ///
+    func blockOfKind(kind: NotificationBlock.Kind) -> NotificationBlock? {
+        for block in blocks where block.kind == kind {
+            return block
         }
 
+        return nil
+    }
 
-        ///
-        ///
-        init(blocks: [Block], kind: Kind) {
-            self.blocks = blocks
-            self.kind = kind
-        }
+    ///
+    ///
+    func imageUrlsFromBlocksInKindSet(kindSet: Set<NotificationBlock.Kind>) -> Set<NSURL> {
+        let filtered = blocks.filter { kindSet.contains($0.kind) }
+        let imageUrls = filtered.flatMap { $0.imageUrls }
+        return Set(imageUrls)
+    }
 
-
-        ///
-        ///
-        func blockOfKind(kind: Block.Kind) -> Block? {
-            for block in blocks where block.kind == kind {
-                return block
-            }
-
+    ///
+    ///
+    class func subjectGroupFromArray(rawBlocks: [AnyObject], parent: Notification) -> NotificationBlockGroup? {
+        // Subject: Contains a User + Text Block
+        guard let blocks = NotificationBlock.blocksFromArray(rawBlocks, parent: parent) else {
             return nil
         }
 
-        ///
-        ///
-        func imageUrlsFromBlocksInKindSet(kindSet: Set<Block.Kind>) -> Set<NSURL> {
-            let filtered = blocks.filter { kindSet.contains($0.kind) }
-            let imageUrls = filtered.flatMap { $0.imageUrls }
-            return Set(imageUrls)
+        return NotificationBlockGroup(blocks: blocks, kind: .Subject)
+    }
+
+    ///
+    ///
+    class func headerGroupFromArray(rawBlocks: [AnyObject], parent: Notification) -> NotificationBlockGroup? {
+        // Header: Contains a User + Text Block
+        guard let blocks = NotificationBlock.blocksFromArray(rawBlocks, parent: parent) else {
+            return nil
         }
 
-        ///
-        ///
-        class func subjectGroupFromArray(rawBlocks: [AnyObject], notification: Notification) -> BlockGroup? {
-            // Subject: Contains a User + Text Block
-            guard let blocks = Block.fromArray(rawBlocks, notification: notification) else {
-                return nil
-            }
+        return NotificationBlockGroup(blocks: blocks, kind: .Header)
+    }
 
-            return BlockGroup(blocks: blocks, kind: .Subject)
+    ///
+    ///
+    class func bodyGroupsFromArray(rawBlocks: [AnyObject], parent: Notification) -> [NotificationBlockGroup]? {
+        guard let blocks = NotificationBlock.blocksFromArray(rawBlocks, parent: parent) else {
+            return nil
         }
 
-        ///
-        ///
-        class func headerGroupFromArray(rawBlocks: [AnyObject], notification: Notification) -> BlockGroup? {
-            // Header: Contains a User + Text Block
-            guard let blocks = Block.fromArray(rawBlocks, notification: notification) else {
-                return nil
-            }
 
-            return BlockGroup(blocks: blocks, kind: .Header)
-        }
+        var groups = [NotificationBlockGroup]()
 
-        ///
-        ///
-        class func bodyGroupsFromArray(rawBlocks: [AnyObject], notification: Notification) -> [BlockGroup]? {
-            guard let blocks = Block.fromArray(rawBlocks, notification: notification) else {
-                return nil
-            }
-
-
-            var groups = [BlockGroup]()
-
-            // Comment: Contains a User + Comment Block
-            if notification.kind == .Comment {
-                //  Note:
-                //  I find myself, again, surrounded by the forces of Duck Typing. Comment Notifications are now
-                //  required to always render the Actions at the very bottom. This snippet is meant to adapt the backend
-                //  data structure, so that a single NotificationBlockGroup can be easily mapped against a single UI entity.
-                //
-                //  -   NoteBlockGroupTypeComment: NoteBlockTypeComment + NoteBlockTypeUser
-                //  -   Anything
-                //  -   NoteBlockGroupTypeActions: A copy of the NoteBlockTypeComment block
-                //
+        // Comment: Contains a User + Comment Block
+        if parent.kind == .Comment {
+            //  Note:
+            //  I find myself, again, surrounded by the forces of Duck Typing. Comment Notifications are now
+            //  required to always render the Actions at the very bottom. This snippet is meant to adapt the backend
+            //  data structure, so that a single NotificationBlockGroup can be easily mapped against a single UI entity.
+            //
+            //  -   NoteBlockGroupTypeComment: NoteBlockTypeComment + NoteBlockTypeUser
+            //  -   Anything
+            //  -   NoteBlockGroupTypeActions: A copy of the NoteBlockTypeComment block
+            //
 
 //                let commentBlock = [NotificationBlock firstBlockOfType:NoteBlockTypeComment fromBlocksArray:blocks];
-        //        NotificationBlock *userBlock    = [NotificationBlock firstBlockOfType:NoteBlockTypeUser fromBlocksArray:blocks];
-        //        NSArray *commentGroupBlocks     = @[commentBlock, userBlock];
-        //        NSArray *actionsGroupBlocks     = @[commentBlock];
-        //
-        //        NSMutableArray *middleBlocks    = [blocks mutableCopy];
-        //        [middleBlocks removeObjectsInArray:commentGroupBlocks];
-        //
-        //        // Finally, arrange the Block Groups
-        //        [groups addObject:[NotificationBlockGroup groupWithBlocks:commentGroupBlocks type:NoteBlockGroupTypeComment]];
-        //
-        //        for (NotificationBlock *block in middleBlocks) {
-        //
-        //            // Duck Typing Again:
-        //            // If the block contains a range that matches with the metaReplyID field, we'll need to render this
-        //            // with a custom style
-        //            //
-        //            BOOL isReply                = [block notificationRangeWithCommentId:notification.metaReplyID] != nil;
-        //            NoteBlockGroupType type     = isReply ? NoteBlockGroupTypeFooter : block.type;
-        //
-        //            [groups addObject:[NotificationBlockGroup groupWithBlocks:@[block] type:type]];
-        //        }
-        //
-        //        [groups addObject:[NotificationBlockGroup groupWithBlocks:actionsGroupBlocks type:NoteBlockGroupTypeActions]];
-        //
-        //
-            // Rest: 1-1 relationship
-            } else {
+    //        NotificationBlock *userBlock    = [NotificationBlock firstBlockOfType:NoteBlockTypeUser fromBlocksArray:blocks];
+    //        NSArray *commentGroupBlocks     = @[commentBlock, userBlock];
+    //        NSArray *actionsGroupBlocks     = @[commentBlock];
+    //
+    //        NSMutableArray *middleBlocks    = [blocks mutableCopy];
+    //        [middleBlocks removeObjectsInArray:commentGroupBlocks];
+    //
+    //        // Finally, arrange the Block Groups
+    //        [groups addObject:[NotificationBlockGroup groupWithBlocks:commentGroupBlocks type:NoteBlockGroupTypeComment]];
+    //
+    //        for (NotificationBlock *block in middleBlocks) {
+    //
+    //            // Duck Typing Again:
+    //            // If the block contains a range that matches with the metaReplyID field, we'll need to render this
+    //            // with a custom style
+    //            //
+    //            BOOL isReply                = [block notificationRangeWithCommentId:notification.metaReplyID] != nil;
+    //            NoteBlockGroupType type     = isReply ? NoteBlockGroupTypeFooter : block.type;
+    //
+    //            [groups addObject:[NotificationBlockGroup groupWithBlocks:@[block] type:type]];
+    //        }
+    //
+    //        [groups addObject:[NotificationBlockGroup groupWithBlocks:actionsGroupBlocks type:NoteBlockGroupTypeActions]];
+    //
+    //
+        // Rest: 1-1 relationship
+        } else {
 
-                //  More Duck Typing:
-                //
-                //  -   Notifications of the kind [Follow, Like, CommentLike] may contain a Footer block.
-                //  -   We can assume that whenever the last block is of the type NoteBlockTypeText, we're dealing with a footer.
-                //  -   Whenever we detect such a block, we'll map the NotificationBlock into a NoteBlockGroupTypeFooter group.
-                //
-                let canContainFooter = notification.kind == .Follow || notification.kind == .Like || notification.kind == .CommentLike
+            //  More Duck Typing:
+            //
+            //  -   Notifications of the kind [Follow, Like, CommentLike] may contain a Footer block.
+            //  -   We can assume that whenever the last block is of the type NoteBlockTypeText, we're dealing with a footer.
+            //  -   Whenever we detect such a block, we'll map the NotificationBlock into a NoteBlockGroupTypeFooter group.
+            //
+            let canContainFooter = parent.kind == .Follow || parent.kind == .Like || parent.kind == .CommentLike
 
-                for block in blocks {
-//                    let isFooter = canContainFooter && block.kind == .Text && blocks.last == block
-//                    let kind = isFooter ? .Footer : block.kind
-        //            [groups addObject:[NotificationBlockGroup groupWithBlocks:@[block] type:type]];
-                }
+            for (index, block) in blocks.enumerate() {
+                let isFooter = canContainFooter && block.kind == .Text && index == blocks.underestimateCount()
+                let kind = isFooter ? NotificationBlockGroup.Kind.Footer : blockGroupKindForBlock(block)
+                let group = NotificationBlockGroup(blocks: blocks, kind: kind)
+
+                groups.append(group)
             }
+        }
 
-            return groups
+        return groups
+    }
+
+    private class func blockGroupKindForBlock(block: NotificationBlock) -> NotificationBlockGroup.Kind {
+        switch block.kind {
+            case .Text:     return .Text
+            case .Image:    return .Image
+            case .User:     return .User
+            case .Comment:  return .Comment
         }
     }
 }
