a9fc447ea707a57fdf8379197f28a971721846ba
Jorge Leandro Perez <jorge@lantean.co>
2016-08-09 15:17:34 -0300

    Updates Bridging Header

--- WordPress/Classes/System/WordPress-Bridging-Header.h
+++ WordPress/Classes/System/WordPress-Bridging-Header.h
@@ -49,7 +49,6 @@
 #import "SuggestionService.h"
 #import "StatsViewController.h"
 #import "Meta.h"
-#import "Notification.h"
 #import "NSString+Helpers.h"
 #import "NSAttributedString+Util.h"
 #import "NSBundle+VersionNumberHelper.h"
