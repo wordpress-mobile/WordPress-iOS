c07be27c31bf7e4c7598c99cf2eb6d38426f1605
Jorge Leandro Perez <jorge@lantean.co>
2016-08-17 13:13:38 -0300

    NotificationSettingsServiceRemote: Updates error domain

--- WordPress/Classes/Networking/NotificationSettingsServiceRemote.swift
+++ WordPress/Classes/Networking/NotificationSettingsServiceRemote.swift
@@ -145,7 +145,7 @@ public class NotificationSettingsServiceRemote: ServiceRemoteWordPressComREST
         }
 
         var domain : String {
-            return "NotificationsServiceRemote"
+            return "NotificationSettingsServiceRemote"
         }
     }
 }
