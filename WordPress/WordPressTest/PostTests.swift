64f0d8ff9d57d8e05b7c4f6f118020720b4c470b
Nate Heagy <nate@heagy.com>
2016-08-24 22:50:07 -0600

    Fix tests

--- WordPress/WordPressTest/PostTests.swift
+++ WordPress/WordPressTest/PostTests.swift
@@ -17,7 +17,7 @@ class PostTests: XCTestCase {
     }
 
     private func newTestPost() -> Post {
-        return NSEntityDescription.insertNewObjectForEntityForName(Post.entityName, inManagedObjectContext: context) as! Post
+        return NSEntityDescription.insertNewObjectForEntityForName(Post.entityName(), inManagedObjectContext: context) as! Post
     }
 
     private func newTestPostCategory() -> PostCategory {
