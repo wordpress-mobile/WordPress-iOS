11dd044b2b35e08a3c5fe52455d8a0238b4d7e64
James Frost <james.frost@automattic.com>
2016-10-07 21:10:09 +0100

    Adds UIFeedbackGenerator wrapper classes for iOS 9

---
+++ WordPress/Classes/Utility/WPFeedbackGenerators.swift
@@ -0,0 +1,57 @@
+import UIKit
+
+@objc
+public enum WPNotificationFeedbackType : Int {
+    case Success
+    case Warning
+    case Error
+}
+
+/// iOS's taptic feedback classes are only available for iOS 10+.
+/// This is a small wrapper around UINotificationFeedbackGenerator, which simply 
+/// results in a no-op on iOS 9 – avoiding the need for conditional code 
+/// throughout the app.
+/// - seealso: UINotificationFeedbackGenerator
+@objc
+class WPNotificationFeedbackGenerator: NSObject {
+    class func notificationOccurred(notificationType: WPNotificationFeedbackType) {
+        guard #available(iOS 10, *) else {
+            return
+        }
+
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(UINotificationFeedbackType(rawValue: notificationType.rawValue)!)
+    }
+}
+
+@objc
+public enum WPImpactFeedbackStyle : Int {
+    case Light
+    case Medium
+    case Heavy
+}
+
+/// iOS's taptic feedback classes are only available for iOS 10+.
+/// This is a small wrapper around UIImpactFeedbackGenerator, which simply
+/// results in a no-op on iOS 9 – avoiding the need for conditional code
+/// throughout the app.
+/// - seealso: UIImpactFeedbackGenerator
+@objc
+class WPImpactFeedbackGenerator: NSObject {
+    let style: WPImpactFeedbackStyle
+
+    init(style: WPImpactFeedbackStyle) {
+        self.style = style
+        super.init()
+    }
+
+    /// call when your UI element impacts something else
+    func impactOccurred() {
+        guard #available(iOS 10, *) else {
+            return
+        }
+
+        let generator = UIImpactFeedbackGenerator(style: UIImpactFeedbackStyle(rawValue: style.rawValue)!)
+        generator.impactOccurred()
+    }
+}
