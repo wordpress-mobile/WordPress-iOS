21569c7568d011a58fd879504ab53d75600e05af
kurzee <brent@kurzee.com>
2016-10-13 16:53:42 -0500

    Reader Header: Revised readable margins for TagStreamHeader.

--- WordPress/Classes/ViewRelated/Reader/ReaderTagStreamHeader.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderTagStreamHeader.swift
@@ -3,11 +3,9 @@ import WordPressShared
 
 @objc public class ReaderTagStreamHeader: UIView, ReaderStreamHeader
 {
-    @IBOutlet private weak var innerContentView: UIView!
+    @IBOutlet private weak var borderedView: UIView!
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var followButton: PostMetaButton!
-    @IBOutlet private weak var contentIPadTopConstraint: NSLayoutConstraint?
-    @IBOutlet private weak var contentBottomConstraint: NSLayoutConstraint!
 
     public var delegate: ReaderStreamHeaderDelegate?
 
@@ -22,6 +20,8 @@ import WordPressShared
 
     func applyStyles() {
         backgroundColor = WPStyleGuide.greyLighten30()
+        borderedView.layer.borderColor = WPStyleGuide.readerCardCellBorderColor().CGColor
+        borderedView.layer.borderWidth = 1.0
         WPStyleGuide.applyReaderStreamHeaderTitleStyle(titleLabel)
     }
 
