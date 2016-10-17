96ad0856161f2c36461f28cda01b5e6a0381bb7f
Jorge Leandro Perez <jorge@lantean.co>
2016-10-17 12:10:32 -0300

    NotificationBlock+Interface: Trimming Subject / Snippet

--- WordPress/Classes/Extensions/Notifications/NotificationBlock+Interface.swift
+++ WordPress/Classes/Extensions/Notifications/NotificationBlock+Interface.swift
@@ -13,11 +13,13 @@ extension NotificationBlock
     /// Formats a NotificationBlock for usage in NoteTableViewCell, in the subject field
     ///
     var attributedSubjectText: NSAttributedString {
-        let attributedText = memoize { () -> NSAttributedString in
-            return self.textWithStyles(Styles.subjectRegularStyle,
+        let attributedText = memoize {
+            let subject = self.textWithStyles(Styles.subjectRegularStyle,
                 quoteStyles:    Styles.subjectItalicsStyle,
                 rangeStylesMap: Constants.subjectRangeStylesMap,
                 linksColor:     nil)
+
+            return subject.trimNewlines()
         }
 
         return attributedText(MemoizeKeys.subject)
@@ -26,11 +28,13 @@ extension NotificationBlock
     /// Formats a NotificationBlock for usage in NoteTableViewCell, in the snippet field
     ///
     var attributedSnippetText: NSAttributedString {
-        let attributedText = memoize { () -> NSAttributedString in
-            return self.textWithStyles(Styles.snippetRegularStyle,
+        let attributedText = memoize {
+            let snippet = self.textWithStyles(Styles.snippetRegularStyle,
                 quoteStyles:    nil,
                 rangeStylesMap: nil,
                 linksColor:     nil)
+
+            return snippet.trimNewlines()
         }
 
         return attributedText(MemoizeKeys.snippet)
