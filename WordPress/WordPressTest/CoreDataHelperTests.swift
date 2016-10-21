2ee1b153274f0cb27043f8188a3d887019905246
Jorge Leandro Perez <jorge@lantean.co>
2016-10-21 17:25:10 -0300

    Implements CoreDataHelperTests

---
+++ WordPress/WordPressTest/CoreDataHelperTests.swift
@@ -0,0 +1,30 @@
+import Foundation
+import XCTest
+import CoreData
+
+@testable import WordPress
+
+
+class CoreDataHelperTests: XCTestCase
+{
+    var contextManager: TestContextManager!
+
+    override func setUp() {
+        super.setUp()
+        contextManager = TestContextManager()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        contextManager.mainContext.reset()
+        ContextManager.overrideSharedInstance(nil)
+    }
+
+
+    
+    func testSomething() {
+
+        
+        NSLog("HERE")
+    }
+}
