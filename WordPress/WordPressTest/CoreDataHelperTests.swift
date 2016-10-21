b0835f473b5cc5300402283763acbc3811fef2b4
Jorge Leandro Perez <jorge@lantean.co>
2016-10-21 18:18:45 -0300

    CoreDataHelperTests: Implements Dynamic Dummy Stack

--- WordPress/WordPressTest/CoreDataHelperTests.swift
+++ WordPress/WordPressTest/CoreDataHelperTests.swift
@@ -5,24 +5,94 @@ import CoreData
 @testable import WordPress
 
 
+// MARK: - CoreData Helper Tests
+//
 class CoreDataHelperTests: XCTestCase
 {
-    var contextManager: TestContextManager!
+    var stack: DummyStack!
+    var helper: CoreDataHelper<DummyEntity>!
+
 
     override func setUp() {
         super.setUp()
-        contextManager = TestContextManager()
+        stack = DummyStack()
+        helper = CoreDataHelper<DummyEntity>(context: stack.context)
     }
 
-    override func tearDown() {
-        super.tearDown()
-        contextManager.mainContext.reset()
-        ContextManager.overrideSharedInstance(nil)
+    func testNewFetchRequestReturnsNewRequestWithGenericEntityName() {
+        let request = helper.newFetchRequest()
+        XCTAssert(request.entityName! == DummyEntity.entityName)
     }
 
-    func testSomething() {
+    func testInsertEntityReturnsNewManagedObjectOfTheGenericKind() {
+        let entity = helper.insertNewObject()
 
-
-        NSLog("HERE")
+        // Upcast to AnyObject to make really sure this works
+        let anyObject = entity as AnyObject
+        XCTAssert(anyObject is DummyEntity)
     }
+}
+
+
+
+// MARK: - Dummy Sample Entity
+//
+class DummyEntity: NSManagedObject, ManagedObject
+{
+    @NSManaged var key: String
+    @NSManaged var value: Int
+
+    static let entityName = "SomeRandomEntity"
+}
+
+
+// MARK: - InMemory Stack with Dynamic Model
+//
+class DummyStack
+{
+    lazy var model: NSManagedObjectModel = {
+        // Attributes
+        let keyAttribute = NSAttributeDescription()
+        keyAttribute.name = "key"
+        keyAttribute.attributeType = .StringAttributeType
+
+
+        let valueAttribute  = NSAttributeDescription()
+        valueAttribute.name = "value"
+        valueAttribute.attributeType = .Integer32AttributeType
+
+        // Entity
+        let entity = NSEntityDescription()
+        entity.name = DummyEntity.entityName
+        entity.managedObjectClassName = String(reflecting: DummyEntity.self)
+        entity.properties = [keyAttribute, valueAttribute]
+
+        // Tadaaaa
+        let model = NSManagedObjectModel()
+        model.entities = [entity]
+
+        return model
+    }()
+
+
+    lazy var context: NSManagedObjectContext = {
+        let context = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+        context.persistentStoreCoordinator = self.coordinator
+        return context
+    }()
+
+
+    lazy var coordinator: NSPersistentStoreCoordinator = {
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.model)
+        _ = try? coordinator.addPersistentStoreWithType(NSInMemoryStoreType, configuration: nil, URL: self.storeURL, options: nil)
+
+        return coordinator
+    }()
+
+    lazy var storeURL: NSURL = {
+        let documents = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true).last!
+        let foundationPath = documents as NSString
+        let dummyStore = foundationPath.stringByAppendingPathComponent("LordYosemite.sqlite")
+        return NSURL.fileURLWithPath(dummyStore)
+    }()
 }
