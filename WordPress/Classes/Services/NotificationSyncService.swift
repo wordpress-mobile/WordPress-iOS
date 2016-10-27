9666fee40b2de7681f422f8af62ff8cd389b22c6
Jorge Leandro Perez <jorge@lantean.co>
2016-10-27 12:58:41 -0300

    NotificationSyncService: Sync Updates Mark III

--- WordPress/Classes/Services/NotificationSyncService.swift
+++ WordPress/Classes/Services/NotificationSyncService.swift
@@ -25,26 +25,27 @@ class NotificationSyncService
     }
 
 
-    /// Syncs the latest *maximumNotes*: Deletes old entities, and upserts new ones!
+    /// Syncs the latest *maximumNotes*:
+    ///
+    /// - Latest 100 hashes are retrieved (++efficiency++)
+    /// - Only those Notifications that were remotely changed (Updated / Inserted) will be retrieved
+    /// - Local collection will be updated. Old notes will be purged!
     ///
     func sync() {
-        // Load latest 100 Notification Hashes. ++performance++
-        remote.loadLastestHashes(withPageSize: maximumNotes) { hashes in
+        remote.loadHashes(withPageSize: maximumNotes) { hashes in
             guard let hashes = hashes else {
                 return
             }
 
-            // Determine which notifications must be downloaded
-            self.process(remoteNotificationHashes: hashes) { noteIds in
+            self.determineUpdatedNotes(withRemoteHashes: hashes) { noteIds in
 
-                // Load Full Notification Documents
-                self.remote.loadNotes(noteIds: noteIds) { (notifications) in
-                    guard let notifications = notifications else {
+                self.remote.loadNotes(noteIds: noteIds) { remoteNotes in
+                    guard let remoteNotes = remoteNotes else {
                         return
                     }
 
-                    // Merge!
-                    self.merge(remoteNotifications: notifications)
+                    self.updateLocalNotes(withRemoteNotes: remoteNotes)
+                    self.deleteLocalMissingNotes(fromRemoteHashes: hashes)
                 }
             }
         }
@@ -96,7 +97,7 @@ private extension NotificationSyncService
 {
     ///
     ///
-    func process(remoteNotificationHashes hashes: [RemoteNotification], completion: ([String] -> Void)) {
+    func determineUpdatedNotes(withRemoteHashes hashes: [RemoteNotification], completion: ([String] -> Void)) {
         let noteIds = hashes.map { $0.notificationId }
         completion(noteIds)
     }
@@ -104,27 +105,17 @@ private extension NotificationSyncService
 
     ///
     ///
-    func merge(remoteNotifications notes: [RemoteNotification]) {
+    func updateLocalNotes(withRemoteNotes notes: [RemoteNotification]) {
         // Update Newest
-        // Delete Removed
+        // Insert Missing
     }
 
 
     ///
     ///
-    func update(notification local: Notification, with remote: RemoteNotification) {
-        local.notificationHash = remote.notificationHash
-        local.read = remote.read
-        local.icon = remote.icon
-        local.noticon = remote.noticon
-        local.timestamp = remote.timestamp
-        local.type = remote.type
-        local.url = remote.url
-        local.title = remote.title
-        local.subject = remote.subject
-        local.header = remote.header
-        local.body = remote.body
-        local.meta = remote.meta
+    func deleteLocalMissingNotes(fromRemoteHashes hashes: [RemoteNotification]) {
+        // Delete Removed
+        let remoteIds = hashes.map { $0.notificationId }
     }
 
 
