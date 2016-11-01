afe4ec7b01b358f9f4a2b9315aea16361e008427
aerych <aerychj@gmail.com>
2016-11-01 11:08:08 -0500

    Adds rich text formatting and attachment classes.

---
+++ WordPress/Classes/ViewRelated/Views/WPRichText/WPRichContentView.swift
@@ -0,0 +1,177 @@
+import Foundation
+import UIKit
+
+/// A subclass of UITextView for displaying HTML formatted strings.  Embedded content
+/// in tags like img, iframe, and video, are loaded manually and presented as subviews.
+///
+class WPRichContentView: UITextView
+{
+    /// Used to keep references to image attachments.
+    ///
+    var mediaArray = [RichMedia]()
+
+    /// Manages the layout and positioning of text attachments.
+    ///
+    lazy var attachmentManager: WPTextAttachmentManager = {
+        return WPTextAttachmentManager(textView: self, delegate: self)
+    }()
+
+    /// Used to load images for attachments.
+    ///
+    lazy var imageSource: WPTableImageSource = {
+        let source = WPTableImageSource()
+        source.delegate = self
+        source.forceLargerSizeWhenFetching = false
+        source.photonQuality = 65
+        return source
+    }()
+
+    /// The maximum size for images.
+    ///
+    lazy var maxDisplaySize: CGSize = {
+        let bounds = UIScreen.mainScreen().bounds
+        let side = max(bounds.size.width, bounds.size.height)
+        return CGSize(width: side, height: side)
+    }()
+
+    var content: String {
+        get {
+            return text ?? ""
+        }
+        set {
+            let str = newValue ?? ""
+            let style = "<style>body { font-family: Merriweather; font-size:16.0; line-height:1.6875; color: #2e4453; } a { color: #0087be; text-decoration: none; } a:active { color: #005082 } blockquote { text-style: italic; } </style>"
+            let content = style + str
+            let attrTxt = try! NSAttributedString.attributedStringFromHTMLString(content, defaultDocumentAttributes: nil)
+            attributedText = attrTxt
+        }
+    }
+
+
+    override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+
+        setupView()
+    }
+
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        setupView()
+    }
+
+
+    /// A convenience method for one-time, common setup that should be done in init.
+    ///
+    func setupView() {
+        // Because the attachment manager is a lazy property.
+        _ = attachmentManager
+    }
+
+}
+
+
+extension WPRichContentView: WPTextAttachmentManagerDelegate
+{
+    func attachmentManager(attachmentManager: WPTextAttachmentManager, viewForAttachment attachment: WPTextAttachment) -> UIView? {
+        if attachment.tagName == "img" {
+
+            return imageForAttachment(attachment)
+
+        } else {
+
+            return embedForAttachment(attachment)
+
+        }
+    }
+
+
+    /// Returns the view to use for an image attachment.
+    ///
+    /// - Parameters:
+    ///     - attachment: A WPTextAttachment for an image.
+    ///
+    /// - Returns: A WPRichTextImage instance configured for the attachment.
+    ///
+    func imageForAttachment(attachment: WPTextAttachment) -> WPRichTextImage {
+        let img = WPRichTextImage(frame: CGRect.zero)
+        let media = RichMedia(image: img, attachment: attachment)
+        let url = NSURL(string: attachment.src)
+        img.contentURL = url
+
+        let index = mediaArray.count
+        let indexPath = NSIndexPath(forRow: index, inSection: 1)
+        imageSource.fetchImageForURL(url, withSize: maxDisplaySize, indexPath: indexPath, isPrivate: false)
+
+        mediaArray.append(media)
+
+        return img
+    }
+
+
+    /// Returns the view to use for an embed attachment.
+    ///
+    /// - Parameters:
+    ///     - attachment: A WPTextAttachment for embedded content.
+    ///
+    /// - Returns: A WPRichTextEmbed instance configured for the attachment.
+    ///
+    func embedForAttachment(attachment: WPTextAttachment) -> WPRichTextEmbed {
+        let width = attachment.width ?? Int(frame.width)
+        let height = attachment.height ?? 50
+
+        let embed = WPRichTextEmbed(frame: CGRect(x: 0, y: 0, width: width, height: height))
+
+        if attachment.tagName == "iframe" {
+            embed.loadContentURL(NSURL(string: attachment.src)!)
+        } else {
+            embed.loadHTMLString(attachment.html!)
+        }
+
+        embed.success = { [weak self] _ in
+            self?.attachmentManager.layoutAttachmentViews()
+            self?.invalidateIntrinsicContentSize()
+        }
+
+        return embed
+    }
+
+}
+
+
+extension WPRichContentView: WPTableImageSourceDelegate
+{
+
+    func tableImageSource(tableImageSource: WPTableImageSource!, imageReady image: UIImage!, forIndexPath indexPath: NSIndexPath!) {
+        let richMedia = mediaArray[indexPath.row]
+
+        richMedia.image.imageView.image = image
+        richMedia.attachment.maxSize = image.size
+        richMedia.attachment.width = Int(image.size.width)
+        richMedia.attachment.height = Int(image.size.height)
+
+        let frame = CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height)
+        richMedia.image.frame = frame
+
+        attachmentManager.layoutAttachmentViews()
+        invalidateIntrinsicContentSize()
+
+    }
+
+
+    func tableImageSource(tableImageSource: WPTableImageSource!, imageFailedforIndexPath indexPath: NSIndexPath!, error: NSError!) {
+        let richMedia = mediaArray[indexPath.row]
+        DDLogSwift.logError("Error loading image: \(richMedia.attachment.src)")
+        DDLogSwift.logError("\(error)")
+    }
+}
+
+
+/// A simple struct used to keep references to a rich text image and its associated attachment.
+///
+struct RichMedia
+{
+    let image: WPRichTextImage
+    let attachment: WPTextAttachment
+}
---
+++ WordPress/Classes/ViewRelated/Views/WPRichText/WPTextAttachment.swift
@@ -0,0 +1,102 @@
+import Foundation
+import UIKit
+
+
+@objc public enum WPTextAttachmentAlignment: Int {
+    case None, Left, Right, Center
+}
+
+
+/// An NSTextAttachment for representing remote HTML content such as images, iframes and video.
+///
+public class WPTextAttachment: NSTextAttachment
+{
+    private(set) public var identifier: String
+    private(set) public var tagName: String
+    private(set) public var src: String
+    public var maxSize = CGSizeZero
+    public var align = WPTextAttachmentAlignment.None
+
+    internal(set) public var attributes: [String : String]?
+    internal(set) public var html: String?
+    internal(set) public var width = 0
+    internal(set) public var height = 0
+
+    // Keys used for NSCoding
+    private let identifierKey = "identifier"
+    private let tagNameKey = "tagName"
+    private let srcKey = "src"
+    private let maxSizeKey = "maxSize"
+    private let alignKey = "align"
+    private let attributesKey = "attributes"
+    private let htmlKey = "html"
+    private let widthKey = "width"
+    private let heightKey = "height"
+
+
+    /// Designated initializer.
+    ///
+    /// - Parameters:
+    ///     - tagName: The tag name of the HTML element represented by the attachment.
+    ///     - identifier: A string to use as the attachment's identifier. It should be unique in the context of its NSAttributedString.s
+    ///     - src: The URL pointing to the remote content represented by the attachment.
+    ///
+    public init(tagName: String, identifier: String, src: String) {
+        self.identifier = identifier
+        self.tagName = tagName
+        self.src = src
+
+        super.init(data: nil, ofType: nil)
+    }
+
+
+    /// For required NSCoding support.
+    ///
+    required public init?(coder aDecoder: NSCoder) {
+        self.identifier = aDecoder.decodeObjectForKey(identifierKey) as! String
+        self.tagName = aDecoder.decodeObjectForKey(tagNameKey) as! String
+        self.src = aDecoder.decodeObjectForKey(srcKey) as! String
+        self.maxSize = aDecoder.decodeCGSizeForKey(maxSizeKey)
+        self.align = WPTextAttachmentAlignment(rawValue: aDecoder.decodeIntegerForKey(alignKey))!
+
+        self.attributes = aDecoder.decodeObjectForKey(attributesKey) as? [String : String]
+        self.html = aDecoder.decodeObjectForKey(htmlKey) as? String
+        self.width = aDecoder.decodeIntegerForKey(widthKey)
+        self.height = aDecoder.decodeIntegerForKey(heightKey)
+
+        super.init(coder: aDecoder)
+    }
+
+
+    /// For NSCoding support.
+    ///
+    public override func encodeWithCoder(aCoder: NSCoder) {
+        super.encodeWithCoder(aCoder)
+
+        aCoder.encodeObject(identifier, forKey: identifierKey)
+        aCoder.encodeObject(tagName, forKey: tagNameKey)
+        aCoder.encodeObject(src, forKey: srcKey)
+        aCoder.encodeCGSize(maxSize, forKey: maxSizeKey)
+        aCoder.encodeInteger(align.rawValue, forKey: alignKey)
+
+        aCoder.encodeObject(attributes, forKey: attributesKey)
+        aCoder.encodeObject(html, forKey: htmlKey)
+        aCoder.encodeInteger(width, forKey: widthKey)
+        aCoder.encodeInteger(height, forKey: heightKey)
+    }
+
+
+    /// Adjusts the amount of space for the attachment glyph on a line fragment.
+    /// Used for clearing text trailing an attachment when align equals .None
+    ///
+    public override func attachmentBoundsForTextContainer(textContainer: NSTextContainer?, proposedLineFragment lineFrag: CGRect, glyphPosition position: CGPoint, characterIndex charIndex: Int) -> CGRect {
+        if align != .None || textContainer == nil {
+            return super.attachmentBoundsForTextContainer(textContainer, proposedLineFragment: lineFrag, glyphPosition: position, characterIndex: charIndex)
+        }
+
+        let width = (lineFrag.width - position.x) - (textContainer!.lineFragmentPadding * 2.0)
+        let rect = CGRectMake(0.0, 0.0, floor(width), 1.0)
+        return rect
+    }
+
+}
