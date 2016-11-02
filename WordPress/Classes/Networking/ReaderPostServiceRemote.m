9e50a58d9a74d18ddb24bf970357963aeec46604
aerych <eric@automattic.com>
2016-11-02 18:21:41 -0500

    Small tweak to regex pattern.

--- WordPress/Classes/Networking/ReaderPostServiceRemote.m
+++ WordPress/Classes/Networking/ReaderPostServiceRemote.m
@@ -906,8 +906,8 @@ static const NSUInteger ReaderPostTitleLength = 30;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         NSError *error;
-        regexStyle = [NSRegularExpression regularExpressionWithPattern:@"<style[^>]*>[\\s\\S]+?</style>" options:NSRegularExpressionCaseInsensitive error:&error];
-        regexScript = [NSRegularExpression regularExpressionWithPattern:@"<script[^>]*>[\\s\\S]+?</script>" options:NSRegularExpressionCaseInsensitive error:&error];
+        regexStyle = [NSRegularExpression regularExpressionWithPattern:@"<style[^>]*?>[\\s\\S]*?</style>" options:NSRegularExpressionCaseInsensitive error:&error];
+        regexScript = [NSRegularExpression regularExpressionWithPattern:@"<script[^>]*?>[\\s\\S]*?</script>" options:NSRegularExpressionCaseInsensitive error:&error];
     });
 
     string = [regexStyle stringByReplacingMatchesInString:string options:NSMatchingReportCompletion range:NSMakeRange(0, [string length]) withTemplate:@""];
