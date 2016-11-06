84a0381932a86677f0c84eece6abf575c4069dc5
Nate Heagy <nate@heagy.com>
2016-11-06 17:08:36 -0600

    Add simple into animation to post post VC

--- WordPress/Classes/ViewRelated/Post/PostPostViewController.swift
+++ WordPress/Classes/ViewRelated/Post/PostPostViewController.swift
@@ -17,6 +17,7 @@ class PostPostViewController: UIViewController {
     @IBOutlet var siteUrlLabel:UILabel!
     @IBOutlet var shareButton:UIButton!
     @IBOutlet var navBar:UINavigationBar!
+    @IBOutlet var actionsStackView:UIStackView!
     var post:Post?
 
     override func viewDidLoad() {
@@ -34,21 +35,6 @@ class PostPostViewController: UIViewController {
         super.viewWillAppear(animated)
         setupPost()
 
-/*[[UINavigationBar appearance] setBarTintColor:[WPStyleGuide wordPressBlue]];
- [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
-
- [[UINavigationBar appearanceWhenContainedInInstancesOfClasses:@[ [NUXNavigationController class]]] setShadowImage:[UIImage imageWithColor:[UIColor clearColor] havingSize:CGSizeMake(320.0, 4.0)]];
- [[UINavigationBar appearanceWhenContainedInInstancesOfClasses:@[ [NUXNavigationController class]]] setBackgroundImage:[UIImage imageWithColor:[UIColor clearColor] havingSize:CGSizeMake(320.0, 4.0)] forBarMetrics:UIBarMetricsDefault];
-
- [[UITabBar appearance] setShadowImage:[UIImage imageWithColor:[UIColor colorWithRed:210.0/255.0 green:222.0/255.0 blue:230.0/255.0 alpha:1.0]]];
- [[UITabBar appearance] setTintColor:[WPStyleGuide newKidOnTheBlockBlue]];
-
- [[UINavigationBar appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor whiteColor], NSFontAttributeName: [WPFontManager systemBoldFontOfSize:17.0]} ];
-
- [[UINavigationBar appearance] setBackgroundImage:[WPStyleGuide navigationBarBackgroundImage] forBarMetrics:UIBarMetricsDefault];
- [[UINavigationBar appearance] setShadowImage:[WPStyleGuide navigationBarShadowImage]];
- [[UINavigationBar appearance] setBarStyle:[WPStyleGuide navigationBarBarStyle]];
-*/
         navBar.barTintColor = WPStyleGuide.wordPressBlue()
         self.view.backgroundColor = WPStyleGuide.wordPressBlue()
         navBar.tintColor = UIColor.whiteColor()
@@ -56,16 +42,22 @@ class PostPostViewController: UIViewController {
         navBar.shadowImage = clearImage
         navBar.setBackgroundImage(clearImage, forBarMetrics: .Default)
 
-
         self.view.alpha = 0
+        self.actionsStackView.alpha = 0
     }
 
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
 
-        UIView.animateWithDuration(0.5) { 
+        self.actionsStackView.layer.transform = CATransform3DMakeTranslation(0, 100, 0)
+        self.actionsStackView.alpha = 1
+
+        UIView.animateWithDuration(0.66, delay:0, options:UIViewAnimationOptions.CurveEaseIn, animations: {
             self.view.alpha = 1
-        }
+            self.actionsStackView.layer.transform = CATransform3DMakeTranslation(0, 0, 0)
+            }, completion: { (success) in
+
+        })
     }
 
     override func prefersStatusBarHidden() -> Bool {
@@ -111,7 +103,7 @@ class PostPostViewController: UIViewController {
             return
         }
 
-        UIView.animateWithDuration(0.5, animations: { 
+        UIView.animateWithDuration(0.66, animations: {
                 self.view.alpha = 0.0
             }) { (success) in
                 if self.view.window == appDelegate.testExtraWindow {
