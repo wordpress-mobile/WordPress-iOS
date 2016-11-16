4077acc049768f20718de98e42adb5823e862ebe
Ernesto Carrion <ernesto@ride.com>
2016-11-16 15:14:42 -0500

    Adds tests for MediaServiceRemoteREST class

--- WordPress/Classes/Networking/MediaServiceRemoteREST.h
+++ WordPress/Classes/Networking/MediaServiceRemoteREST.h
@@ -2,5 +2,12 @@
 #import "MediaServiceRemote.h"
 #import "SiteServiceRemoteWordPressComREST.h"
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface MediaServiceRemoteREST : SiteServiceRemoteWordPressComREST <MediaServiceRemote>
+
+- (RemoteMedia *)remoteMediaFromJSONDictionary:(NSDictionary *)jsonMedia;
+
 @end
+
+NS_ASSUME_NONNULL_END
