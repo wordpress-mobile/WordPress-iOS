0f64e92674dd562c8385992fa120ccf142840790
James Frost <james.frost@automattic.com>
2016-12-13 16:46:23 +0000

    WPFullscreenNavigationTransition: Refactored to improve code readability

--- WordPress/Classes/Extensions/UINavigationController+SplitViewFullscreen.swift
+++ WordPress/Classes/Extensions/UINavigationController+SplitViewFullscreen.swift
@@ -84,15 +84,15 @@ class WPFullscreenNavigationTransition: NSObject, UIViewControllerAnimatedTransi
         }
 
         guard let fromView = transitionContext.viewForKey(UITransitionContextFromViewKey),
-            let toView = transitionContext.viewForKey(UITransitionContextToViewKey),
-            let fromVC = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey),
-            let toVC = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey) else {
+            let toView = transitionContext.viewForKey(UITransitionContextToViewKey) else {
                 transitionContext.completeTransition(false)
                 return
         }
 
-        let fromFrame = transitionContext.initialFrameForViewController(fromVC)
-        let targetFrame = transitionContext.finalFrameForViewController(toVC)
+        guard let model = WPFullscreenNavigationTransitionViewModel(transitionContext: transitionContext, operation: operation) else {
+            transitionContext.completeTransition(false)
+            return
+        }
 
         let containerView = transitionContext.containerView()
 
@@ -103,92 +103,57 @@ class WPFullscreenNavigationTransition: NSObject, UIViewControllerAnimatedTransi
         let navigationBarMask = UIView()
         navigationBarMask.backgroundColor = WPStyleGuide.wordPressBlue()
         containerView.addSubview(navigationBarMask)
-        navigationBarMask.frame = CGRect(x: 0, y: 0, width: UIScreen.mainScreen().bounds.width, height: fromFrame.origin.y)
+        navigationBarMask.frame = model.navigationBarMaskFrame
 
         // Ensure the from view is the correct size when we start
-        fromView.frame = fromFrame
-        toView.frame = targetFrame
+        fromView.frame = model.fromFrame
+        toView.frame = model.toFrame
 
-        // RTL support
-        let attribute = fromView.semanticContentAttribute
-        let layoutDirection = UIView.userInterfaceLayoutDirectionForSemanticContentAttribute(attribute)
-        let isRTLLayout = (layoutDirection == .RightToLeft)
+        toView.transform = model.toViewInitialTranform
 
         // Take a snapshot to hide any layout issues in the 'from' view as it
         // transitions to the new size.
         let snapshot = fromView.snapshotViewAfterScreenUpdates(false)!
-        let snapshotContainer = UIView(frame: fromFrame)
+        let snapshotContainer = UIView(frame: model.fromFrame)
         snapshotContainer.backgroundColor = fromView.backgroundColor
         snapshotContainer.addSubview(snapshot)
 
-        // Add a shadow layer to the left edge of the topmost view, matching
-        // the appearance of the standard UINavigationController transition.
-        let shadowWidth: CGFloat = 5.0
-        let shadowClearColor = UIColor(white: 0, alpha: 0).CGColor
-        let shadowDarkColor = UIColor(white: 0, alpha: 0.1).CGColor
-
+        // Add a thin shadow gradient down the leading edge of the topmost view,
+        // matching the appearance of the standard UINavigationController transition.
         let shadowLayer = CAGradientLayer()
         shadowLayer.locations = [0, 1]
-        shadowLayer.colors = [shadowClearColor, shadowDarkColor]
-
-        if isRTLLayout {
-            shadowLayer.colors = shadowLayer.colors!.reverse()
-        }
-
-        let shadowLayerX = (isRTLLayout) ? fromFrame.width : -shadowWidth
-        shadowLayer.frame = CGRect(x: shadowLayerX, y: 0, width: shadowWidth, height: fromFrame.height)
-
-        let centerLeft = CGPoint(x: 0, y: 0.5)
-        let centerRight = CGPoint(x: 1, y: 0.5)
-        shadowLayer.startPoint = centerLeft
-        shadowLayer.endPoint = centerRight
+        shadowLayer.colors = model.shadowColors
+        shadowLayer.frame = model.shadowFrame
+        shadowLayer.startPoint = model.shadowStartPoint
+        shadowLayer.endPoint = model.shadowEndPoint
 
         // Dim out the bottommost view, matching the appearance of the standard
         // UINavigationController transition.
-        let dimmingViewColor = UIColor(white: 0, alpha: 0.05)
-        let dimmingViewXOffset = (isRTLLayout) ? fromFrame.width : -fromFrame.width
-        let dimmingView = UIView(frame: CGRect(x: dimmingViewXOffset, y: 0, width: fromFrame.width, height: fromFrame.height))
-        dimmingView.backgroundColor = dimmingViewColor
+        let dimmingView = UIView(frame: model.dimmingViewFrame)
+        dimmingView.backgroundColor = model.dimmingViewColor
+        dimmingView.alpha = model.dimmingViewInitialAlpha
 
         containerView.addSubview(snapshotContainer)
 
         if pushing {
-            // Initially position the new VC offscreen
-            let initialXOffset = (isRTLLayout) ? -fromFrame.width : fromFrame.width
-            toView.transform = CGAffineTransformMakeTranslation(initialXOffset, 0)
             containerView.addSubview(toView)
 
             toView.layer.addSublayer(shadowLayer)
             toView.addSubview(dimmingView)
-            dimmingView.alpha = WPAlphaZero
         } else {
-            fromView.alpha = 0
+            fromView.alpha = WPAlphaZero
 
             containerView.insertSubview(toView, atIndex: 0)
 
-            if let splitViewPrimaryWidth = fromVC.splitViewController?.primaryColumnWidth where isRTLLayout {
-                    toView.transform = CGAffineTransformMakeTranslation(splitViewPrimaryWidth, 0)
-            }
-
             snapshotContainer.layer.addSublayer(shadowLayer)
             snapshotContainer.addSubview(dimmingView)
-            dimmingView.alpha = WPAlphaFull
         }
 
         let animations = {
-            toView.transform = CGAffineTransformIdentity
+            toView.transform = model.toViewFinalTranform
+            snapshotContainer.transform = model.snapshotContainerFinalTransform
 
-            if self.pushing {
-                if let splitViewPrimaryWidth = fromVC.splitViewController?.primaryColumnWidth {
-                    snapshotContainer.transform = CGAffineTransformMakeTranslation(splitViewPrimaryWidth, 0)
-                }
-                
-                dimmingView.alpha = WPAlphaFull
-            } else {
-                let targetXOffset = (isRTLLayout) ? -fromFrame.width : targetFrame.width
-                snapshotContainer.transform = CGAffineTransformMakeTranslation(targetXOffset, 0)
-                dimmingView.alpha = WPAlphaZero
-            }
+            dimmingView.alpha = model.dimmingViewFinalAlpha
         }
 
         let completion = { (finished: Bool) in
@@ -201,7 +166,7 @@ class WPFullscreenNavigationTransition: NSObject, UIViewControllerAnimatedTransi
             fromView.transform = CGAffineTransformIdentity
             toView.transform = CGAffineTransformIdentity
 
-            fromView.frame = fromFrame
+            fromView.frame = model.fromFrame
 
             transitionContext.completeTransition(finished)
         }
@@ -211,5 +176,98 @@ class WPFullscreenNavigationTransition: NSObject, UIViewControllerAnimatedTransi
                                    options: .CurveEaseInOut,
                                    animations: animations,
                                    completion: completion)
+    }
+}
+
+private struct WPFullscreenNavigationTransitionViewModel {
+    let transitionContext: UIViewControllerContextTransitioning
+
+    let fromFrame: CGRect
+    let toFrame: CGRect
+
+    let toViewInitialTranform: CGAffineTransform
+    let toViewFinalTranform = CGAffineTransformIdentity
+
+    let snapshotContainerFinalTransform: CGAffineTransform
+
+    let navigationBarMaskFrame: CGRect
+
+    let shadowWidth: CGFloat = 5.0
+    let shadowColors: [CGColor]
+    let shadowFrame: CGRect
+    let shadowStartPoint = CGPoint(x: 0, y: 0.5) // Center left
+    let shadowEndPoint =   CGPoint(x: 1, y: 0.5) // Center right
+    private let shadowClearColor = UIColor(white: 0, alpha: 0)
+    private let shadowDarkColor = UIColor(white: 0, alpha: 0.1)
+
+    let dimmingViewColor = UIColor(white: 0, alpha: 0.05)
+    let dimmingViewFrame: CGRect
+    let dimmingViewInitialAlpha: CGFloat
+    let dimmingViewFinalAlpha: CGFloat
+
+    init?(transitionContext: UIViewControllerContextTransitioning, operation: UINavigationControllerOperation) {
+        guard let fromVC = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey),
+            let toVC = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey) else {
+                return nil
+        }
+
+        self.transitionContext = transitionContext
+
+        // Frames
+
+        fromFrame = transitionContext.initialFrameForViewController(fromVC)
+        toFrame = transitionContext.finalFrameForViewController(toVC)
+
+        // RTL support
+
+        let attribute = transitionContext.containerView().semanticContentAttribute
+        let layoutDirection = UIView.userInterfaceLayoutDirectionForSemanticContentAttribute(attribute)
+        let isRTLLayout = (layoutDirection == .RightToLeft)
+
+        // Transforms
+
+        if operation == .Push {
+            let initialToViewXOrigin = (isRTLLayout) ? -fromFrame.width : fromFrame.width
+            toViewInitialTranform = CGAffineTransformMakeTranslation(initialToViewXOrigin, 0)
+        } else {
+            toViewInitialTranform = splitViewWidthTransformForViewController(fromVC, isRTLLayout: isRTLLayout)
+        }
+
+        if operation == .Push {
+            snapshotContainerFinalTransform = splitViewWidthTransformForViewController(fromVC, isRTLLayout: isRTLLayout)
+        } else {
+            let targetXOffset = (isRTLLayout) ? -fromFrame.width : toFrame.width
+            snapshotContainerFinalTransform = CGAffineTransformMakeTranslation(targetXOffset, 0)
+        }
+
+        // Navigation bar mask
+
+        navigationBarMaskFrame = CGRect(x: 0, y: 0, width: UIScreen.mainScreen().bounds.width, height: fromFrame.origin.y)
+
+        // Shadow layer
+
+        let shadowXOffset = (isRTLLayout) ? fromFrame.width : -shadowWidth
+        shadowFrame = CGRect(x: shadowXOffset, y: 0, width: shadowWidth, height: fromFrame.height)
+
+        if isRTLLayout {
+            shadowColors = [shadowDarkColor.CGColor, shadowClearColor.CGColor]
+        } else {
+            shadowColors = [shadowClearColor.CGColor, shadowDarkColor.CGColor]
+        }
+
+        // Dimming view
+
+        let dimmingViewXOffset = (isRTLLayout) ? fromFrame.width : -fromFrame.width
+        dimmingViewFrame = CGRect(x: dimmingViewXOffset, y: 0, width: fromFrame.width, height: fromFrame.height)
+        dimmingViewInitialAlpha = (operation == .Push) ? WPAlphaZero : WPAlphaFull
+        dimmingViewFinalAlpha =   (operation == .Push) ? WPAlphaFull : WPAlphaZero
+    }
+}
+
+private func splitViewWidthTransformForViewController(viewController: UIViewController, isRTLLayout: Bool) -> CGAffineTransform {
+    if let splitViewPrimaryWidth = viewController.splitViewController?.primaryColumnWidth where isRTLLayout {
+        return CGAffineTransformMakeTranslation(splitViewPrimaryWidth, 0)
+    } else {
+        return CGAffineTransformIdentity
     }
 }
