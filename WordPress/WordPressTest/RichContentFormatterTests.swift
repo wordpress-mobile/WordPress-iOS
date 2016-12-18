db3edfd8d9481c423d1d98809a52068586eccb42
aerych <aerychj@gmail.com>
2016-12-18 00:26:47 -0600

    Rich Text: Don't filter new lines from pre tags.

--- WordPress/WordPressTest/RichContentFormatterTests.swift
+++ WordPress/WordPressTest/RichContentFormatterTests.swift
@@ -18,16 +18,25 @@ class RichContentFormatterTests: XCTestCase
         let str = "<p>test</p><p>test</p>"
         let styleStr = "<script>alert();</script><style>body{color:#000;}</style><p>test</p><script>alert();</script><style>body{color:#000;}</style><p>test</p><script>alert();</script><style>body{color:#000;}</style>"
         let sanitizedStr = RichContentFormatter.removeForbiddenTags(styleStr)
-        XCTAssertTrue(str == sanitizedStr, "The inline styles were not removed.")
+        XCTAssertTrue(str == sanitizedStr, "The forbidden tags were not removed.")
     }
 
 
     func testNormalizeParagraphs() {
-        let str = "<p>test</p><p>test</p>"
-        let styleStr = "<div><p>test</p></div>\n<p><div>test</div></p>\n"
+        let str = "<p>test</p><pre>\n\ntest\n\n</pre><p>test</p>"
+        let styleStr = "<div><p>test</p></div><pre>\n\ntest\n\n</pre>\n<p><div>test</div></p>\n"
         let sanitizedStr = RichContentFormatter.normalizeParagraphs(styleStr)
-        XCTAssertTrue(str == sanitizedStr, "The inline styles were not removed.")
+        XCTAssertTrue(str == sanitizedStr, "Not all paragraphs were normalized.")
     }
+
+
+    func testFilterNewLines() {
+        let str = "<div><p>test</p></div><pre>\n\ntest\n\n</pre><p><div>test</div></p>"
+        let styleStr = "<div><p>test</p></div><pre>\n\ntest\n\n</pre>\n<p><div>test</div></p>\n"
+        let sanitizedStr = RichContentFormatter.filterNewLines(styleStr)
+        XCTAssertTrue(str == sanitizedStr, "Not all paragraphs were normalized.")
+    }
+
 
     func testResizeGalleryImageURLsForContentPublic() {
         guard
