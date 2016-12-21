9e013096f9a7ad0043fcd978ae79375051ec1044
aerych <eric@automattic.com>
2016-12-21 12:36:54 -0600

    Reader: Updates how post is marked in use.

--- WordPress/Classes/ViewRelated/Reader/ReaderDetailViewController.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderDetailViewController.swift
@@ -72,8 +72,13 @@ open class ReaderDetailViewController: UIViewController, UIViewControllerRestora
     open var post: ReaderPost? {
         didSet {
             oldValue?.removeObserver(self, forKeyPath: DetailConstants.LikeCountKeyPath)
+            oldValue?.inUse = false
 
-            post?.addObserver(self, forKeyPath: DetailConstants.LikeCountKeyPath, options: .new, context: nil)
+            if let newPost = post {
+                newPost.inUse = true
+                ContextManager.sharedInstance().save(newPost.managedObjectContext)
+                newPost.addObserver(self, forKeyPath: DetailConstants.LikeCountKeyPath, options: .new, context: nil)
+            }
             if isViewLoaded {
                 configureView()
             }
@@ -133,18 +138,12 @@ open class ReaderDetailViewController: UIViewController, UIViewControllerRestora
             return nil
         }
 
-        post.inUse = false
-        ContextManager.sharedInstance().save(context)
-
         return controllerWithPost(post)
     }
 
 
     open override func encodeRestorableState(with coder: NSCoder) {
         if let post = post {
-            let context = ContextManager.sharedInstance().mainContext
-            post.inUse = true
-            ContextManager.sharedInstance().save(context)
             coder.encode(post.objectID.uriRepresentation().absoluteString, forKey: type(of: self).restorablePostObjectURLhKey)
         }
 
@@ -156,7 +155,11 @@ open class ReaderDetailViewController: UIViewController, UIViewControllerRestora
 
 
     deinit {
-        post?.removeObserver(self, forKeyPath: DetailConstants.LikeCountKeyPath)
+        if let post = post {
+            post.inUse = false
+            ContextManager.sharedInstance().save(post.managedObjectContext)
+            post.removeObserver(self, forKeyPath: DetailConstants.LikeCountKeyPath)
+        }
         NotificationCenter.default.removeObserver(self)
     }
 
