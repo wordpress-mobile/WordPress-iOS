a4deac6e4bf6030c97416f9678101990c9232ff0
aerych <eric@automattic.com>
2016-12-21 12:43:14 -0600

    Reader: Updates how a topic is marked in use.

--- WordPress/Classes/ViewRelated/Reader/ReaderSearchViewController.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderSearchViewController.swift
@@ -50,9 +50,6 @@ import Gridicons
             return ReaderSearchViewController.controller()
         }
 
-        topic.inUse = false
-        ContextManager.sharedInstance().saveContextAndWait(context)
-
         let storyboard = UIStoryboard(name: "Reader", bundle: Bundle.main)
         let controller = storyboard.instantiateViewController(withIdentifier: "ReaderSearchViewController") as! ReaderSearchViewController
         controller.restoredSearchTopic = topic
@@ -62,8 +59,6 @@ import Gridicons
 
     open override func encodeRestorableState(with coder: NSCoder) {
         if let topic = streamController.readerTopic {
-            topic.inUse = true
-            ContextManager.sharedInstance().saveContextAndWait(topic.managedObjectContext)
             coder.encode(topic.path, forKey: type(of: self).restorableSearchTopicPathKey)
         }
         super.encodeRestorableState(with: coder)
