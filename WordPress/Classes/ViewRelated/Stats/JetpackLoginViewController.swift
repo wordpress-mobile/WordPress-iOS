236b0a7193aa2003b133e1485127b8ac6155a24a
Matt Bumgardner <matt@bumgardner.org>
2017-01-12 14:37:01 -0600

    Roughed in login form

--- WordPress/Classes/ViewRelated/Stats/JetpackLoginViewController.swift
+++ WordPress/Classes/ViewRelated/Stats/JetpackLoginViewController.swift
@@ -1,27 +1,25 @@
+import Foundation
 import UIKit
+import WordPressShared
+import WordPressComAnalytics
 
+/// A view controller that presents a Jetpack login form
+///
 class JetpackLoginViewController: UIViewController {
+    @IBOutlet weak var jetpackImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var usernameTextField: WPWalkthroughTextField!
+    @IBOutlet weak var passwordTextField: WPWalkthroughTextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
+
 
 }
