9155198ea468b06bb59b7e3df9b98114c4bf923c
Matt Bumgardner <matt@bumgardner.org>
2017-01-13 15:59:52 -0600

    Added some init code and updated the layout

--- WordPress/Classes/ViewRelated/Stats/JetpackLoginViewController.swift
+++ WordPress/Classes/ViewRelated/Stats/JetpackLoginViewController.swift
@@ -13,7 +13,8 @@ class JetpackLoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.view.backgroundColor = WPStyleGuide.itsEverywhereGrey()
+        setupControls()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +22,27 @@ class JetpackLoginViewController: UIViewController {
 
     }
 
+    func setupControls() {
+        //TODO: Complete the NSLocalized string comments
+        self.descriptionLabel.text = NSLocalizedString("Looks like you have Jetpack set up on your site. Congrats!\nSign in with your WordPress.com credentials below to enable Stats and Notifications.", comment: "")
+        self.passwordTextField.font = WPNUXUtility.descriptionTextFont()
+        self.descriptionLabel.textColor = WPStyleGuide.allTAllShadeGrey()
+        self.descriptionLabel.backgroundColor = UIColor.clear
 
+        self.usernameTextField.backgroundColor = UIColor.white
+        self.usernameTextField.placeholder = NSLocalizedString("WordPress.com username", comment: "")
+        self.usernameTextField.font = WPNUXUtility.textFieldFont()
+        self.usernameTextField.adjustsFontSizeToFitWidth = true
+        self.usernameTextField.autocorrectionType = .no
+        self.usernameTextField.autocapitalizationType = .none
+        self.usernameTextField.clearButtonMode = .whileEditing
+
+        self.passwordTextField.backgroundColor = UIColor.white
+        self.passwordTextField.placeholder = NSLocalizedString("WordPress.com password", comment: "")
+        self.passwordTextField.font = WPNUXUtility.textFieldFont()
+        self.passwordTextField.isSecureTextEntry = true
+        self.passwordTextField.showSecureTextEntryToggle = true
+        self.passwordTextField.clearsOnBeginEditing = true
+        self.passwordTextField.showTopLineSeparator = true
+    }
 }
