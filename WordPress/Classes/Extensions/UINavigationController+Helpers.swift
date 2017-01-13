b79e2acc81f75d139b41dd8d22dd15a20e523b08
James Frost <james.frost@automattic.com>
2017-01-13 19:16:29 +0000

    scrollContentToTopAnimated now works for embedded scrollviews in a vc.

--- WordPress/Classes/Extensions/UINavigationController+Helpers.swift
+++ WordPress/Classes/Extensions/UINavigationController+Helpers.swift
@@ -7,8 +7,16 @@ extension UINavigationController {
         if let topViewController = topViewController as? WPScrollableViewController {
             topViewController.scrollViewToTop()
         } else if let scrollView = topViewController?.view as? UIScrollView {
+            // If the view controller's view is a scrollview
             let offset = CGPoint(x: 0, y: -scrollView.contentInset.top)
             scrollView.setContentOffset(offset, animated: animated)
+        } else if let scrollViews = topViewController?.view.subviews.filter({ $0 is UIScrollView }) as? [UIScrollView] {
+            // If one of the top level views of the view controller's view
+            // is a scrollview
+            if let scrollView = scrollViews.first {
+                let offset = CGPoint(x: 0, y: -scrollView.contentInset.top)
+                scrollView.setContentOffset(offset, animated: animated)
+            }
         }
     }
 }
