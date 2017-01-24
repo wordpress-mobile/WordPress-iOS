4baa57bfa0e3b6af5ff8df95ec2348e6672f9305
Jorge Leandro Perez <jorge@lantean.co>
2017-01-24 19:09:06 +0100

    Implements AdaptiveNavigationController

---
+++ WordPress/Classes/ViewRelated/System/AdaptiveNavigationController.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+
+/// This UINavigationController subclass will take care, automatically, of hiding the UINavigationBar
+/// whenever the presentation style is not set to fullscreen (AKA "we're in Popover Mode!").
+///
+class AdaptiveNavigationController: UINavigationController {
+
+    /// Insets to be applied on the SourceView's Coordinates, in order to fine tune the Popover's arrow position.
+    ///
+    private let sourceInsets = CGVector(dx: -10, dy: 0)
+
+
+    /// This is the main method of this helper: we'll set both, the Presentation Style, Presentation Delegate,
+    /// and SourceView parameters, accordingly.
+    ///
+    ///  - Parameter presentingView: UIView instance from which the popover will be shown
+    ///
+    func configurePopoverPresentationStyle(from sourceView: UIView) {
+        modalPresentationStyle = .popover
+
+        guard let presentationController = popoverPresentationController else {
+            fatalError()
+        }
+
+        presentationController.sourceRect = sourceView.bounds.insetBy(dx: sourceInsets.dx, dy: sourceInsets.dy)
+        presentationController.sourceView = sourceView
+        presentationController.permittedArrowDirections = .any
+        presentationController.delegate = self
+    }
+}
+
+
+// MARK: - UIPopoverPresentationControllerDelegate Methods
+//
+extension AdaptiveNavigationController: UIPopoverPresentationControllerDelegate {
+    func presentationController(_ presentationController: UIPresentationController, willPresentWithAdaptiveStyle style: UIModalPresentationStyle, transitionCoordinator: UIViewControllerTransitionCoordinator?) {
+        guard let navigationController = presentationController.presentedViewController as? UINavigationController else {
+            return
+        }
+
+        let hidesNavigationBar = style != .fullScreen
+        navigationController.navigationBar.isHidden = hidesNavigationBar
+    }
+}
