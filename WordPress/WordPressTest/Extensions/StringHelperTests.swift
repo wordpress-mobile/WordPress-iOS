7c7722c0f3c7fbf9e1fb19bc3c932541fd60c5e3
Jorge Bernal <jbernal@gmail.com>
2017-01-25 09:16:43 +0100

    Add helpers to remove prefix/suffix from string

--- WordPress/WordPressTest/Extensions/StringHelperTests.swift
+++ WordPress/WordPressTest/Extensions/StringHelperTests.swift
@@ -19,4 +19,28 @@ class StringHelperTests: XCTestCase {
         let sourceString = "   \(trimmedString)   "
         XCTAssert(trimmedString == sourceString.trim())
     }
+
+    func testRemovePrefix() {
+        let string = "X-Post: This is a test"
+        XCTAssertEqual("This is a test", string.removingPrefix("X-Post: "))
+        XCTAssertEqual(string, string.removingPrefix("Something Else"))
+    }
+
+    func testRemoveSuffix() {
+        let string = "X-Post: This is a test"
+        XCTAssertEqual("X-Post: This is", string.removingSuffix(" a test"))
+        XCTAssertEqual(string, string.removingSuffix("Something Else"))
+    }
+
+    func testRemovePrefixPattern() {
+        let string = "X-Post: This is a test"
+        XCTAssertEqual("This is a test", try! string.removingPrefix(pattern: "X-.*?: +"))
+        XCTAssertEqual(string, try! string.removingPrefix(pattern: "Th.* "))
+    }
+
+    func testRemoveSuffixPattern() {
+        let string = "X-Post: This is a test"
+        XCTAssertEqual("X-Post: This is", try! string.removingSuffix(pattern: "( a)? +test"))
+        XCTAssertEqual(string, try! string.removingSuffix(pattern: "Th.* "))
+    }
 }
