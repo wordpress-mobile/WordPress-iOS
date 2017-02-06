e7052bd576e44b3163a41c753d2005141f07abb3
James Frost <james.frost@automattic.com>
2017-02-06 13:43:12 +0000

    Removed split view full screen mode.

--- WordPress/Classes/ViewRelated/Reader/ReaderStreamViewController.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderStreamViewController.swift
@@ -1723,7 +1723,7 @@ extension ReaderStreamViewController : WPTableViewHandlerDelegate {
 
         }
 
-        navigationController?.pushFullscreenViewController(controller, animated: true)
+        navigationController?.pushViewController(controller, animated: true)
     }
 
 
