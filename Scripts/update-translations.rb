10470ef4d8ff91edb517c486b7c4595d970355dd
Jorge Bernal <jbernal@gmail.com>
2017-02-15 18:00:50 +0100

    Add extension to extract-framework-translations script
    I prefer not to have it, but GitHub won't do syntax highlighting :/

--- Scripts/update-translations.rb
+++ Scripts/update-translations.rb
@@ -105,4 +105,4 @@ langs.each do |code,local|
   system "plutil -lint #{lang_dir}/Localizable.strings" and system "rm #{lang_dir}/Localizable.strings.bak"
   system "grep -a '\\x00\\x20\\x00\\x22\\x00\\x22\\x00\\x3b$' #{lang_dir}/Localizable.strings"
 end
-system "Scripts/extract-framework-translations"
+system "Scripts/extract-framework-translations.swift"
