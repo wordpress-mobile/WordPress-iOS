b7ab3765c0b63d8067855628f394e776977dc16d
Matt Bumgardner <matt@bumgardner.org>
2017-03-09 12:19:34 -0600

    Added new storyboard for delete site

--- WordPress/Classes/ViewRelated/Blog/SiteManagement/DeleteSiteViewController.swift
+++ WordPress/Classes/ViewRelated/Blog/SiteManagement/DeleteSiteViewController.swift
@@ -8,64 +8,74 @@ open class DeleteSiteViewController: UITableViewController {
 
     // MARK: - Properties
 
-    fileprivate let blog: Blog
+    var blog: Blog!
 
-    // MARK: - Properties: table content
+    @IBOutlet weak var warningImage: UIImageView!
+    @IBOutlet weak var siteTitleLabel: UILabel!
+    @IBOutlet weak var siteTitleSubText: UILabel!
 
-    let headerView: TableViewHeaderDetailView = {
-        let header = NSLocalizedString("Delete Site", comment: "Heading for instructions on Delete Site settings page")
-        let detail = NSLocalizedString("This action can not be undone. Deleting the site will remove all content, " +
-                                       "contributors, domains, and upgrades from the site.\n\n" +
-                                       "It you're unsure about what will be deleted or need any help, not to worry, " +
-                                       "our support team is here to answer any questions you may have.",
-                                       comment: "Detailed instructions on Delete Site settings page")
-
-       return TableViewHeaderDetailView(title: header, detail: detail)
-    }()
-
-    let contactCell: UITableViewCell = {
-        let contactTitle = NSLocalizedString("Contact Support", comment: "Button title for contacting support on Delete Site settings page")
-
-        let actionCell = WPTableViewCellDefault(style: .value1, reuseIdentifier: nil)
-        actionCell.textLabel?.text = contactTitle
-        WPStyleGuide.configureTableViewActionCell(actionCell)
-        actionCell.textLabel?.textAlignment = .center
-
-        return actionCell
-    }()
-
-    let deleteCell: UITableViewCell = {
-        let deleteTitle = NSLocalizedString("Delete Site", comment: "Button title to delete your site on the Delete Site settings page")
-
-        let actionCell = WPTableViewCellDefault(style: .value1, reuseIdentifier: nil)
-        actionCell.textLabel?.text = deleteTitle
-        WPStyleGuide.configureTableViewDestructiveActionCell(actionCell)
-
-        return actionCell
-    }()
+//    // MARK: - Properties: table content
+//
+//    let headerView: TableViewHeaderDetailView = {
+//        let header = NSLocalizedString("Delete Site", comment: "Heading for instructions on Delete Site settings page")
+//        let detail = NSLocalizedString("This action can not be undone. Deleting the site will remove all content, " +
+//                                       "contributors, domains, and upgrades from the site.\n\n" +
+//                                       "It you're unsure about what will be deleted or need any help, not to worry, " +
+//                                       "our support team is here to answer any questions you may have.",
+//                                       comment: "Detailed instructions on Delete Site settings page")
+//
+//       return TableViewHeaderDetailView(title: header, detail: detail)
+//    }()
+//
+//    let contactCell: UITableViewCell = {
+//        let contactTitle = NSLocalizedString("Contact Support", comment: "Button title for contacting support on Delete Site settings page")
+//
+//        let actionCell = WPTableViewCellDefault(style: .value1, reuseIdentifier: nil)
+//        actionCell.textLabel?.text = contactTitle
+//        WPStyleGuide.configureTableViewActionCell(actionCell)
+//        actionCell.textLabel?.textAlignment = .center
+//
+//        return actionCell
+//    }()
+//
+//    let deleteCell: UITableViewCell = {
+//        let deleteTitle = NSLocalizedString("Delete Site", comment: "Button title to delete your site on the Delete Site settings page")
+//
+//        let actionCell = WPTableViewCellDefault(style: .value1, reuseIdentifier: nil)
+//        actionCell.textLabel?.text = deleteTitle
+//        WPStyleGuide.configureTableViewDestructiveActionCell(actionCell)
+//
+//        return actionCell
+//    }()
 
     // MARK: - Initializer
 
-    init(blog: Blog) {
-        self.blog = blog
-        super.init(style: .grouped)
-    }
-
-    required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+//    init(blog: Blog) {
+//        self.blog = blog
+//        super.init(style: .grouped)
+//    }
+//
+//    required public init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
 
     // MARK: - View Lifecycle
 
+//    override open func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        title = NSLocalizedString("Delete Site", comment: "Title of settings page for deleting a site")
+//
+//        tableView.cellLayoutMarginsFollowReadableWidth = true
+//        tableView.estimatedSectionHeaderHeight = 100.0
+//        tableView.sectionHeaderHeight = UITableViewAutomaticDimension
+//
+//        WPStyleGuide.configureColors(for: view, andTableView: tableView)
+//    }
+
     override open func viewDidLoad() {
+        assert(blog != nil)
         super.viewDidLoad()
-
-        title = NSLocalizedString("Delete Site", comment: "Title of settings page for deleting a site")
-
-        tableView.cellLayoutMarginsFollowReadableWidth = true
-        tableView.estimatedSectionHeaderHeight = 100.0
-        tableView.sectionHeaderHeight = UITableViewAutomaticDimension
-
         WPStyleGuide.configureColors(for: view, andTableView: tableView)
     }
 
@@ -218,40 +228,40 @@ open class DeleteSiteViewController: UITableViewController {
     }
 }
 
-// MARK: - UITableViewDelegate
-
-extension DeleteSiteViewController {
-    override open func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        switch indexPath.row {
-        case 0:
-            return contactSupport()
-        default:
-            return deleteSite()
-        }
-    }
-
-    override open func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        return headerView
-    }
-}
-
-// MARK: - UITableViewDataSource
-
-extension DeleteSiteViewController {
-    override open func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override open func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
-    }
-
-    override open func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        switch indexPath.row {
-        case 0:
-            return contactCell
-        default:
-            return deleteCell
-        }
-    }
-}
+//// MARK: - UITableViewDelegate
+//
+//extension DeleteSiteViewController {
+//    override open func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        switch indexPath.row {
+//        case 0:
+//            return contactSupport()
+//        default:
+//            return deleteSite()
+//        }
+//    }
+//
+//    override open func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        return headerView
+//    }
+//}
+//
+//// MARK: - UITableViewDataSource
+//
+//extension DeleteSiteViewController {
+//    override open func numberOfSections(in tableView: UITableView) -> Int {
+//        return 1
+//    }
+//
+//    override open func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 2
+//    }
+//
+//    override open func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        switch indexPath.row {
+//        case 0:
+//            return contactCell
+//        default:
+//            return deleteCell
+//        }
+//    }
+//}
