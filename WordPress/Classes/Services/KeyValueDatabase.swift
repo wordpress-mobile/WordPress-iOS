38a42de4d03e37f7343efb27fffdefe9df89030e
Jorge Bernal <jbernal@gmail.com>
2017-03-09 11:45:40 +0100

    Introduce RecentSitesService

--- WordPress/Classes/Services/KeyValueDatabase.swift
+++ WordPress/Classes/Services/KeyValueDatabase.swift
@@ -5,7 +5,7 @@ import Foundation
 /// `NSString`, `NSNumber`, `NSDate`, `NSArray`, or `NSDictionary`.
 /// For `NSArray` and `NSDictionary` objects, their contents must be property
 /// list objects.
-protocol KeyValueDatabase {
+public protocol KeyValueDatabase {
     func object(forKey defaultName: String) -> Any?
     func set(_ value: Any?, forKey defaultName: String)
     func removeObject(forKey defaultName: String)
@@ -17,8 +17,11 @@ extension UserDefaults: KeyValueDatabase {}
 
 /// `EphemeralKeyValueDatabase` stores values in a dictionary in memory, and is
 /// never persisted between app launches.
-class EphemeralKeyValueDatabase: KeyValueDatabase {
+public class EphemeralKeyValueDatabase: KeyValueDatabase {
     fileprivate var memory = [String: Any]()
+
+    public init() {
+    }
 
     open func set(_ value: Any?, forKey defaultName: String) {
         memory[defaultName] = value
