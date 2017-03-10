5016830a009b86db0eed8f62d36b9f298a373abf
Jorge Bernal <jbernal@gmail.com>
2017-03-10 15:47:17 +0100

    Show recent sites at the top of the blog list

---
+++ WordPress/Classes/ViewRelated/Blog/BlogListDataSource.swift
@@ -0,0 +1,280 @@
+import CoreData
+import Foundation
+import UIKit
+import WordPressShared
+
+private typealias Table = [[Blog]]
+private protocol BlogListDataSourceMapper {
+    func map(_ data: [Blog]) -> Table
+}
+
+private struct BrowsingWithRecentDataSourceMapper: BlogListDataSourceMapper {
+    func map(_ data: [Blog]) -> Table {
+        let recentSiteUrls = RecentSitesService().recentSites
+        let visible = data.filter({ $0.visible })
+        let recent = recentSiteUrls.flatMap({ url in
+            return visible.first(where: { $0.url == url })
+        })
+        let other = visible.filter({ blog in
+            return !recent.contains(blog)
+        })
+        return [recent, other]
+    }
+}
+
+private struct BrowsingDataSourceMapper: BlogListDataSourceMapper {
+    func map(_ data: [Blog]) -> Table {
+        return [data.filter({ $0.visible })]
+    }
+}
+
+private struct EditingDataSourceMapper: BlogListDataSourceMapper {
+    func map(_ data: [Blog]) -> Table {
+        return [data.filter({ blog in
+            blog.supports(.visibility)
+        })]
+    }
+}
+
+private struct SearchingDataSourceMapper: BlogListDataSourceMapper {
+    let query: String
+
+    func map(_ data: [Blog]) -> Table {
+        guard let query = query.nonEmptyString() else {
+            return [data]
+        }
+        return [data.filter({ blog in
+            let nameContainsQuery = blog.settings?.name.map({ $0.localizedCaseInsensitiveContains(query) }) ?? false
+            let urlContainsQuery = blog.url.map({ $0.localizedCaseInsensitiveContains(query) }) ?? false
+            return nameContainsQuery || urlContainsQuery
+        })]
+    }
+}
+
+class BlogListDataSource: NSObject {
+
+    override init() {
+        super.init()
+        // We can't decide if we're using recent sites until the results controller
+        // is configured and we have a list of blogs, so we have to update this right
+        // after initializing the data source.
+        updateMode()
+        resultsController.delegate = self
+    }
+
+    // MARK: - Configuration
+
+    let recentSitesMinCount = 4
+
+    // MARK: - Inputs
+
+    var editing: Bool = false {
+        didSet {
+            updateMode()
+        }
+    }
+
+    var searching: Bool = false {
+        didSet {
+            updateMode()
+        }
+    }
+
+    var searchQuery: String = "" {
+        didSet {
+            updateMode()
+        }
+    }
+
+    // MARK: - Outputs
+
+    var dataChanged: (() -> Void)?
+
+    var visibilityChanged: ((Blog, Bool) -> Void)?
+
+    @objc(blogAtIndexPath:)
+    func blog(at indexPath: IndexPath) -> Blog {
+        return table[indexPath.section][indexPath.row]
+    }
+
+    @objc(indexPathForBlog:)
+    func indexPath(for blog: Blog) -> IndexPath? {
+        for (sectionIndex, section) in table.enumerated() {
+            for (rowIndex, row) in section.enumerated() {
+                if row == blog {
+                    return IndexPath(row: rowIndex, section: sectionIndex)
+                }
+            }
+        }
+        return nil
+    }
+
+    var allBlogsCount: Int {
+        return allBlogs.count
+    }
+
+    var displayedBlogsCount: Int {
+        return table.reduce(0, { result, section in
+            result + section.count
+        })
+    }
+
+    var visibleBlogsCount: Int {
+        return allBlogs.filter({ $0.visible }).count
+    }
+
+    // MARK: - Internal properties
+
+    fileprivate let resultsController: NSFetchedResultsController<Blog> = {
+        let context = ContextManager.sharedInstance().mainContext
+        let request = NSFetchRequest<Blog>(entityName: NSStringFromClass(Blog.self))
+        request.sortDescriptors = [
+            NSSortDescriptor(key: "accountForDefaultBlog.userID", ascending: false),
+            NSSortDescriptor(key: "settings.name", ascending: true, selector: #selector(NSString.localizedCaseInsensitiveCompare(_:)))
+        ]
+        let controller = NSFetchedResultsController(fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        do {
+            try controller.performFetch()
+        } catch {
+            fatalError("Error fetching blogs list: \(error)")
+        }
+        return controller
+    }()
+
+    fileprivate var mode: Mode = .browsing {
+        didSet {
+            if mode != oldValue {
+                dataChanged?()
+            }
+        }
+    }
+}
+
+// MARK: - Mode
+
+private extension BlogListDataSource {
+    enum Mode: Equatable {
+        case browsing
+        case browsingWithRecent
+        case editing
+        case searching(String)
+
+        var mapper: BlogListDataSourceMapper {
+            switch self {
+            case .browsing:
+                return BrowsingDataSourceMapper()
+            case .browsingWithRecent:
+                return BrowsingWithRecentDataSourceMapper()
+            case .editing:
+                return EditingDataSourceMapper()
+            case .searching(let query):
+                return SearchingDataSourceMapper(query: query)
+            }
+        }
+
+        static func == (lhs: Mode, rhs: Mode) -> Bool {
+            switch (lhs, rhs) {
+            case (.browsing, .browsing),
+                 (.browsingWithRecent, .browsingWithRecent),
+                 (.editing, .editing):
+                return true
+            case let (.searching(lquery), .searching(rquery)):
+                return lquery == rquery
+            default:
+                return false
+            }
+        }
+    }
+
+    func updateMode() {
+        // Extracted this into its own function so the compiler can enforce that
+        // a mode must be returned
+        mode = modeForCurrentState()
+    }
+
+    func modeForCurrentState() -> Mode {
+        if editing {
+            return .editing
+        }
+        if searching {
+            return .searching(searchQuery)
+        }
+        if visibleBlogsCount > recentSitesMinCount {
+            return .browsingWithRecent
+        }
+        return .browsing
+    }
+}
+
+// MARK: - Data
+
+private extension BlogListDataSource {
+    var table: Table {
+        return mode.mapper.map(allBlogs)
+    }
+
+    var allBlogs: [Blog] {
+        return resultsController.fetchedObjects ?? []
+    }
+}
+
+// MARK: - Results Controller Delegate
+
+extension BlogListDataSource: NSFetchedResultsControllerDelegate {
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        // TODO: only propagate if the filtered data changed
+        dataChanged?()
+    }
+}
+
+// MARK: - UITableView Data Source
+
+extension BlogListDataSource: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return table.count
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return table[section].count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let blog = self.blog(at: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "BlogCell") as? WPBlogTableViewCell else {
+            fatalError("Failed to get a blog cell")
+        }
+        let displayURL = blog.displayURL as? String ?? ""
+        if let name = blog.settings?.name?.nonEmptyString() {
+            cell.textLabel?.text = name
+            cell.detailTextLabel?.text = displayURL
+        } else {
+            cell.textLabel?.text = displayURL
+            cell.detailTextLabel?.text = nil
+        }
+        cell.accessoryType = .disclosureIndicator
+        cell.selectionStyle = tableView.isEditing ? .none : .blue
+        cell.imageView?.layer.borderColor = UIColor.white.cgColor
+        cell.imageView?.layer.borderWidth = 1.5
+        cell.imageView?.setImageWithSiteIcon(blog.icon)
+        cell.visibilitySwitch?.accessibilityIdentifier = String(format: "Switch-Visibility-%@", blog.settings?.name ?? "")
+        cell.visibilitySwitch?.isOn = blog.visible
+        cell.visibilitySwitchToggled = { [visibilityChanged] cell in
+            guard let isOn = cell.visibilitySwitch?.isOn else {
+                return
+            }
+            visibilityChanged?(blog, isOn)
+        }
+
+        if !blog.visible {
+            cell.textLabel?.textColor = WPStyleGuide.readGrey()
+        }
+
+        WPStyleGuide.configureTableViewBlogCell(cell)
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+}
