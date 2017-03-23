b394052f6ff512043bf90fe804f9c795aca32e48
kurzee <brent@kurzee.com>
2017-03-23 13:43:07 -0500

    Media: Additional tests for local MediaService functions.

--- WordPress/WordPressTest/MediaServiceTests.swift
+++ WordPress/WordPressTest/MediaServiceTests.swift
@@ -8,7 +8,33 @@ class MediaServiceTests: XCTestCase {
             let url = try MediaService.localMediaDirectory()
             XCTAssertTrue(url.lastPathComponent == "Media", "Error: local media directory is not named Media, as expected.")
         } catch {
-            XCTFail("Error accessing or creating local media directory")
+            XCTFail("Error accessing or creating local media directory: \(error)")
         }
+    }
+
+    func testThatLocalMediaURLWorks() {
+        do {
+            let basename = "media-service-test-sample"
+            let pathExtension = "jpg"
+            let expected = "\(basename).\(pathExtension)"
+            var url = try MediaService.localMediaURL(with: basename, fileExtension: pathExtension)
+            NSLog("url: \(url)")
+            XCTAssertTrue(url.lastPathComponent == expected, "Error: local media url have unexpected filename or extension: \(url)")
+            url = try MediaService.localMediaURL(with: expected, fileExtension: pathExtension)
+            XCTAssertTrue(url.lastPathComponent == expected, "Error: local media url have unexpected filename or extension: \(url)")
+            url = try MediaService.localMediaURL(with: basename + ".png", fileExtension: pathExtension)
+            XCTAssertTrue(url.lastPathComponent == expected, "Error: local media url have unexpected filename or extension: \(url)")
+        } catch {
+            XCTFail("Error creating local media URL: \(error)")
+        }
+    }
+
+    func testThatMediaTumbnailFilenameWorks() {
+        let basename = "media-service-test-sample"
+        let pathExtension = "jpg"
+        var thumbnail = MediaService.mediaFilenameAppendingThumbnail("\(basename).\(pathExtension)")
+        XCTAssertTrue(thumbnail == "\(basename)-thumbnail.\(pathExtension)", "Error: appending media thumbnail to filename returned unexpected result.")
+        thumbnail = MediaService.mediaFilenameAppendingThumbnail(basename)
+        XCTAssertTrue(thumbnail == "\(basename)-thumbnail", "Error: appending media thumbnail to filename returned unexpected result.")
     }
 }
