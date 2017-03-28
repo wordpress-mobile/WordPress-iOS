892147a37efa2256241f68c9510578e423d379f2
kurzee <brent@kurzee.com>
2017-03-28 17:45:59 -0500

    Media: better swift 3 naming for factory methods.
    Note: would prefer `makeMedia(with post:` and `makeMedia(with blog:`
    but ObjC doesn’t like the similar naming using “with”.

--- WordPress/Classes/Extensions/Media+Blog.swift
+++ WordPress/Classes/Extensions/Media+Blog.swift
@@ -4,7 +4,7 @@ extension Media {
 
     /// Inserts and returns a new managed Media object, in the context.
     ///
-    class func inserted(into context: NSManagedObjectContext) -> Media {
+    class func makeMedia(in context: NSManagedObjectContext) -> Media {
         let media = NSEntityDescription.insertNewObject(forEntityName: "Media", into: context) as! Media
         media.creationDate = Date()
         media.mediaID = 0
@@ -13,16 +13,16 @@ extension Media {
 
     /// Inserts and returns a new managed Media object, with a blog.
     ///
-    class func insertedWith(blog: Blog) -> Media {
-        let media = inserted(into: blog.managedObjectContext!)
+    class func makeMedia(blog: Blog) -> Media {
+        let media = makeMedia(in: blog.managedObjectContext!)
         media.blog = blog
         return media
     }
 
     /// Inserts and returns a new managed Media object, with a post.
     ///
-    class func insertedWith(post: AbstractPost) -> Media {
-        let media = insertedWith(blog: post.blog)
+    class func makeMedia(post: AbstractPost) -> Media {
+        let media = makeMedia(blog: post.blog)
         media.addPostsObject(post)
         return media
     }
