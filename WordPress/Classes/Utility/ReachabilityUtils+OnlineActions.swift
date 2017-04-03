885d42871f2a76f101546d63eaa9d9bada19dc25
elibud <elibud@gmail.com>
2017-04-03 12:16:02 -0300

    Handle OnlineOnlyActions on ReachabilityUtils

---
+++ WordPress/Classes/Utility/ReachabilityUtils+OnlineActions.swift
@@ -0,0 +1,27 @@
+//
+//  ReachabilityActionUtils.swift
+//  WordPress
+//
+//  Created by Elisa Budelli on 4/3/17.
+//  Copyright © 2017 WordPress. All rights reserved.
+//
+
+import Foundation
+
+extension ReachabilityUtils {
+
+    /// Performs the action when an internet connection is available
+    /// If no internet connection is available an error message is displayed
+    ///
+    class func onAvailableInternetConnectionDo(_ action: () -> Void) {
+        guard ReachabilityUtils.isInternetReachable() else {
+            let title = NSLocalizedString("No Connection",
+                                          comment: "Title of error prompt when no internet connection is available.")
+            let message = NSLocalizedString("The Internet connection appears to be offline",
+                                            comment: "Message of error prompt shown when a user tries to perform an action without an internet connection.")
+            WPError.showAlert(withTitle: title, message: message)
+            return
+        }
+        action()
+    }
+}
