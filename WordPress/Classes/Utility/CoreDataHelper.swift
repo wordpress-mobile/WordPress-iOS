43a5225181345433f1b272e46996dc64af7e825a
Jorge Leandro Perez <jorge@lantean.co>
2017-04-07 16:17:05 -0300

    CoreDataHelper: Reimplementing as a NSMOC Extension

--- WordPress/Classes/Utility/CoreDataHelper.swift
+++ WordPress/Classes/Utility/CoreDataHelper.swift
@@ -2,40 +2,40 @@ import Foundation
 
 
 
+extension NSManagedObject {
 
-// MARK: - Core Data Helpers
-//
-struct CoreDataHelper<T> where T: NSManagedObject, T: ManagedObject {
-    /// CoreData ManagedObjectContext
-    ///
-    let context: NSManagedObjectContext
+    class var entityName: String {
+        return entity().name ?? classNameWithoutNamespaces()
+    }
+}
 
+extension NSManagedObjectContext {
 
     /// Returns a new FetchRequest for the associated Entity
     ///
-    func newFetchRequest() -> NSFetchRequest<NSFetchRequestResult> {
+    func newFetchRequest<T: NSManagedObject>(for type: T.Type) -> NSFetchRequest<NSFetchRequestResult> {
         return NSFetchRequest(entityName: T.entityName)
     }
-
 
     /// Returns all of the entities that match with a given predicate.
     ///
     /// - Parameter predicate: Defines the conditions that any given object should meet. Optional.
     ///
-    func allObjects(matchingPredicate predicate: NSPredicate? = nil, sortedBy descriptors: [NSSortDescriptor]? = nil)  -> [T] {
-        let request = newFetchRequest()
+    func allObjects<T: NSManagedObject>(of type: T.Type, matching predicate: NSPredicate? = nil, sortedBy descriptors: [NSSortDescriptor]? = nil) -> [T] {
+        let request = newFetchRequest(for: type)
         request.predicate = predicate
         request.sortDescriptors = descriptors
 
-        return loadObjects(withFetchRequest: request)
+        return loadObjects(of: type, with: request)
     }
+
 
     /// Returns the number of entities found that match with a given predicate.
     ///
     /// - Parameter predicate: Defines the conditions that any given object should meet. Optional.
     ///
-    func countObjects(matchingPredicate predicate: NSPredicate? = nil) -> Int {
-        let request = newFetchRequest()
+    func countObjects<T: NSManagedObject>(of type: T.Type, matching predicate: NSPredicate? = nil) -> Int {
+        let request = newFetchRequest(for: type)
         request.predicate = predicate
         request.includesSubentities = false
         request.predicate = predicate
@@ -44,10 +44,10 @@ struct CoreDataHelper<T> where T: NSManagedObject, T: ManagedObject {
         var result = 0
 
         do {
-            result = try context.count(for: request)
+            result = try count(for: request)
         } catch {
             DDLogSwift.logError("Error counting objects [\(T.entityName)]: \(error)")
-            assert(false)
+            assertionFailure()
         }
 
         return result
@@ -55,20 +55,19 @@ struct CoreDataHelper<T> where T: NSManagedObject, T: ManagedObject {
 
     /// Deletes the specified Object Instance
     ///
-    func deleteObject(_ object: T) {
-        context.delete(object)
+    func deleteObject<T: NSManagedObject>(_ object: T) {
+        delete(object)
     }
 
     /// Deletes all of the NSMO instances associated to the current kind
     ///
-    func deleteAllObjects() {
-        let request = newFetchRequest()
+    func deleteAllObjects<T: NSManagedObject>(of type: T.Type) {
+        let request = newFetchRequest(for: type)
         request.includesPropertyValues = false
         request.includesSubentities = false
 
-        let objects = loadObjects(withFetchRequest: request)
-        for object in objects {
-            context.delete(object)
+        for object in loadObjects(of: type, with: request) {
+            delete(object)
         }
     }
 
@@ -76,55 +75,46 @@ struct CoreDataHelper<T> where T: NSManagedObject, T: ManagedObject {
     ///
     /// - Parameter predicate: Defines the conditions that any given object should meet.
     ///
-    func firstObject(matchingPredicate predicate: NSPredicate) -> T? {
-        let request = newFetchRequest()
+    func firstObject<T: NSManagedObject>(of type: T.Type, matching predicate: NSPredicate) -> T? {
+        let request = newFetchRequest(for: type)
         request.predicate = predicate
         request.fetchLimit = 1
 
-        let objects = loadObjects(withFetchRequest: request)
-        return objects.first
+        return loadObjects(of: type, with: request).first
     }
 
     /// Inserts a new Entity. For performance reasons, this helper *DOES NOT* persists the context.
     ///
-    func insertNewObject() -> T {
-        let name = T.entityName
-        let entity = NSEntityDescription.insertNewObject(forEntityName: name, into: context)
-
-        return entity as! T
+    func insertNewObject<T: NSManagedObject>(of type: T.Type) -> T {
+        return NSEntityDescription.insertNewObject(forEntityName: T.entityName, into: self) as! T
     }
 
     /// Loads a single NSManagedObject instance, given its ObjectID, if available.
     ///
     /// - Parameter objectID: Unique Identifier of the entity to retrieve, if available.
     ///
-    func loadObject(withObjectID objectID: NSManagedObjectID) -> T? {
+    func loadObject<T: NSManagedObject>(of type: T.Type, with objectID: NSManagedObjectID) -> T? {
         var result: T?
 
         do {
-            result = try context.existingObject(with: objectID) as? T
+            result = try existingObject(with: objectID) as? T
         } catch {
             DDLogSwift.logError("Error loading Object [\(T.entityName)]")
         }
 
         return result
     }
-}
 
-
-// MARK: - Private Helpers
-//
-private extension CoreDataHelper {
     /// Loads the collection of entities that match with a given Fetch Request
     ///
-    func loadObjects(withFetchRequest request: NSFetchRequest<NSFetchRequestResult>) -> [T] {
+    private func loadObjects<T: NSManagedObject>(of type: T.Type, with request: NSFetchRequest<NSFetchRequestResult>) -> [T] {
         var objects: [T]?
 
         do {
-            objects = try context.fetch(request) as? [T]
+            objects = try fetch(request) as? [T]
         } catch {
             DDLogSwift.logError("Error loading Objects [\(T.entityName)")
-            assert(false)
+            assertionFailure()
         }
 
         return objects ?? []
