902b8a438c40671f8d81f23a2663022b778f1e67
Matt Bumgardner <matt@bumgardner.org>
2017-04-10 13:53:03 -0500

    Refactored RemoteService tests; AccountServiceRemoteRESTTest added

---
+++ WordPress/WordPressTest/RemoteTestCase.swift
@@ -0,0 +1,40 @@
+import Foundation
+import XCTest
+import OHHTTPStubs
+@testable import WordPress
+
+class RemoteTestCase: XCTestCase {
+
+    // MARK: - Constants
+
+    let contentTypeJson     = "application/json"
+    let timeout             = TimeInterval(1000)
+
+    // MARK: - Properties
+
+    var restApi: WordPressComRestApi!
+
+    // MARK: - Overriden Methods
+
+    override func setUp() {
+        super.setUp()
+        restApi = WordPressComRestApi(oAuthToken: nil, userAgent: nil)
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        OHHTTPStubs.removeAllStubs()
+    }
+
+    // MARK: - Helpers
+
+    func stubRemoteResponse(_ endpoint: String, filename: String, contentType: String, status: Int32 = 200) {
+        stub(condition: { request in
+            return request.url?.absoluteString.range(of: endpoint) != nil
+        }) { _ in
+            let stubPath = OHPathForFile(filename, type(of: self))
+            return fixture(filePath: stubPath!, status: status, headers: ["Content-Type" as NSObject: contentType as AnyObject])
+        }
+    }
+
+}
