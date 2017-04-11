78470e4418f1fe4a398df8fa4cfbd225ac9b166e
Matt Bumgardner <matt@bumgardner.org>
2017-04-11 13:34:54 -0500

    Updates to AccountServiceRemote tests

--- WordPress/WordPressTest/AccountServiceRemoteRESTTests.swift
+++ WordPress/WordPressTest/AccountServiceRemoteRESTTests.swift
@@ -7,9 +7,10 @@ class AccountServiceRemoteRESTTests: RemoteTestCase {
     let meEndpoint       = "me"
     let meSitesEndpoint  = "me/sites"
 
-    let getBlogsSuccessMockFilename       = "me-sites-success.json"
-    let getBlogsEmptySuccessMockFilename  = "me-sites-empty-success.json"
-    let getBlogsFailureMockFilename       = "me-sites-failure.json"
+    let getBlogsSuccessMockFilename        = "me-sites-success.json"
+    let getBlogsEmptySuccessMockFilename   = "me-sites-empty-success.json"
+    let getBlogsAuthFailureMockFilename    = "me-sites-auth-failure.json"
+    let getBlogsBadJsonFailureMockFilename = "me-sites-bad-json-failure.json"
 
     // MARK: - Properties
 
@@ -30,7 +31,7 @@ class AccountServiceRemoteRESTTests: RemoteTestCase {
 
         stubRemoteResponse(meSitesEndpoint, filename: getBlogsSuccessMockFilename, contentType: contentTypeJson)
         remote.getBlogsWithSuccess({ blogs in
-            XCTAssertEqual(blogs?.count, 3, "There should be 3 blogs here.")
+            XCTAssertEqual(blogs?.count, 3, "There should be 3 blogs here")
             expect.fulfill()
         }, failure: { error in
             XCTFail("This callback shouldn't get called")
@@ -45,7 +46,7 @@ class AccountServiceRemoteRESTTests: RemoteTestCase {
 
         stubRemoteResponse(meSitesEndpoint, filename: getBlogsEmptySuccessMockFilename, contentType: contentTypeJson)
         remote.getBlogsWithSuccess({ blogs in
-            XCTAssertEqual(blogs?.count, 0, "There should be 0 blogs here.")
+            XCTAssertEqual(blogs?.count, 0, "There should be 0 blogs here")
             expect.fulfill()
         }, failure: { error in
             XCTFail("This callback shouldn't get called")
@@ -55,10 +56,52 @@ class AccountServiceRemoteRESTTests: RemoteTestCase {
         waitForExpectations(timeout: timeout, handler: nil)
     }
 
-    func testGetBlogsFails() {
-        let expect = expectation(description: "Get blogs failure")
+    func testGetBlogsWithServerErrorFails() {
+        let expect = expectation(description: "Get blogs server error failure")
+        stubRemoteResponse(meSitesEndpoint, data: Data(), contentType: nil, status: 500)
 
-        stubRemoteResponse(meSitesEndpoint, filename: getBlogsFailureMockFilename, contentType: "", status: 500)
+        remote.getBlogsWithSuccess({ blogs in
+            XCTFail("This callback shouldn't get called")
+            expect.fulfill()
+        }, failure: { error in
+            guard let error = error as NSError? else {
+                XCTFail("The returned error could not be cast as NSError")
+                expect.fulfill()
+                return
+            }
+            XCTAssertEqual(error.domain, String(reflecting: WordPressComRestApiError.self), "The error domain should be WordPressComRestApiError")
+            XCTAssertEqual(error.code, WordPressComRestApiError.unknown.rawValue, "The error code should be 7 - unknown")
+            expect.fulfill()
+        })
+
+        waitForExpectations(timeout: timeout, handler: nil)
+    }
+
+    func testGetBlogsWithBadAuthFails() {
+        let expect = expectation(description: "Get blogs auth failure")
+
+        stubRemoteResponse(meSitesEndpoint, filename: getBlogsAuthFailureMockFilename, contentType: contentTypeJson, status: 403)
+        remote.getBlogsWithSuccess({ blogs in
+            XCTFail("This callback shouldn't get called")
+            expect.fulfill()
+        }, failure: { error in
+            guard let error = error as NSError? else {
+                XCTFail("The returned error could not be cast as NSError")
+                expect.fulfill()
+                return
+            }
+            XCTAssertEqual(error.domain, String(reflecting: WordPressComRestApiError.self), "The error domain should be WordPressComRestApiError")
+            XCTAssertEqual(error.code, WordPressComRestApiError.authorizationRequired.rawValue, "The error code should be 2 - authorization_required")
+            expect.fulfill()
+        })
+
+        waitForExpectations(timeout: timeout, handler: nil)
+    }
+
+    func testGetBlogsWithBadJsonFails() {
+        let expect = expectation(description: "Get blogs with invalid json failure")
+
+        stubRemoteResponse(meSitesEndpoint, filename: getBlogsBadJsonFailureMockFilename, contentType: contentTypeJson, status: 200)
         remote.getBlogsWithSuccess({ blogs in
             XCTFail("This callback shouldn't get called")
             expect.fulfill()
