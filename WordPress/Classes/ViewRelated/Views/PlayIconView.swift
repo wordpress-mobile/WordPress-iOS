b519849b07f87f3cd6e7a8cb198d72df0389c367
James Frost <james.frost@automattic.com>
2017-04-15 09:15:20 +0100

    Add PlayIconView, a play button to match the system webview play button

---
+++ WordPress/Classes/ViewRelated/Views/PlayIconView.swift
@@ -0,0 +1,97 @@
+import UIKit
+import WordPressShared
+
+/// A circular 'play' icon for use on videos that should match the system
+/// play button appearance in web views.
+///
+class PlayIconView: UIView {
+    private static let defaultSize = CGSize(width: 110, height: 110)
+
+    private let playLayer = CAShapeLayer()
+    private let visualEffectsView = UIVisualEffectView(effect: UIBlurEffect(style: .light))
+
+    var isHighlighted: Bool {
+        didSet {
+            let iconColor = isHighlighted ? .black : WPStyleGuide.darkGrey()
+            playLayer.strokeColor = iconColor.cgColor
+            playLayer.fillColor = iconColor.cgColor
+        }
+    }
+
+    convenience init() {
+        self.init(frame: CGRect(x: 0,
+                                y: 0,
+                                width: type(of: self).defaultSize.width,
+                                height: type(of: self).defaultSize.height))
+    }
+
+    override init(frame: CGRect) {
+        isHighlighted = false
+
+        super.init(frame: frame)
+
+        addEffectsView()
+        layer.addSublayer(playLayer)
+        refreshRadius()
+        updatePlayLayer()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Not implemented")
+    }
+
+    override var frame: CGRect {
+        didSet {
+            refreshRadius()
+            updatePlayLayer()
+        }
+    }
+
+    private func addEffectsView() {
+        visualEffectsView.frame = bounds
+        addSubview(visualEffectsView)
+    }
+
+    fileprivate func refreshRadius() {
+        let radius: CGFloat = frame.width / 2
+
+        if visualEffectsView.layer.cornerRadius != radius {
+            visualEffectsView.layer.cornerRadius = radius
+        }
+
+        visualEffectsView.layer.masksToBounds = true
+        visualEffectsView.clipsToBounds = true
+    }
+
+    private func updatePlayLayer() {
+        let lineWidth: CGFloat = 5
+        let halfLineWidth: CGFloat = lineWidth / 2
+
+        let halfBoundsWidth: CGFloat = bounds.width / 2
+        let halfBoundsHeight: CGFloat = bounds.height / 2
+
+        let size = CGSize(width: halfBoundsWidth - lineWidth,
+                          height: halfBoundsHeight)
+        let halfSizeWidth: CGFloat = size.width / 2
+        let halfSizeHeight: CGFloat = size.height / 2
+
+        // Draw a triangle
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: size.width + halfLineWidth, y: halfSizeHeight + halfLineWidth))
+        path.addLine(to: CGPoint(x: halfLineWidth, y: size.height + halfLineWidth))
+        path.addLine(to: CGPoint(x: halfLineWidth, y: halfLineWidth))
+        path.addLine(to: CGPoint(x: size.width + halfLineWidth, y: halfSizeHeight + halfLineWidth))
+        path.close()
+
+        // Set a rounded line style
+        playLayer.path = path.cgPath
+        playLayer.lineWidth = lineWidth
+        playLayer.lineCap = kCALineCapRound
+        playLayer.lineJoin = kCALineJoinRound
+
+        let centerX = halfBoundsWidth - halfSizeWidth + lineWidth
+        let centerY = halfBoundsHeight - halfSizeWidth - lineWidth
+
+        playLayer.frame = CGRect(x: centerX, y: centerY, width: size.width, height: size.height)
+    }
+}
