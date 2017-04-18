50ae41ff0e26c39f25dd079a092ee0ae7c455628
Aaron Douglas <astralbodies@gmail.com>
2017-04-18 10:17:07 -0500

    Fixes tests with non-connected App Delegate

--- WordPress/WordPressTest/TestingAppDelegate.m
+++ WordPress/WordPressTest/TestingAppDelegate.m
@@ -2,4 +2,30 @@
 
 @implementation TestingAppDelegate
 
+- (void)applicationDidFinishLaunching:(UIApplication *)application {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = [UIViewController new];
+    [self.window makeKeyAndVisible];
+}
+
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
+{
+}
+
+
+
+
 @end
