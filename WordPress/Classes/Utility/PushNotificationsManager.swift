997d48a75442bb0d96408ee348c0c732b0382bc3
elibud <elibud@gmail.com>
2017-04-20 15:18:06 -0300

    Revert test change

--- WordPress/Classes/Utility/PushNotificationsManager.swift
+++ WordPress/Classes/Utility/PushNotificationsManager.swift
@@ -106,7 +106,7 @@ final public class PushNotificationsManager: NSObject {
     func registerDeviceToken(_ tokenData: Data) {
         // We want to register Helpshift regardless so that way if a user isn't logged in
         // they can still get push notifications that we replied to their support ticket.
-        // HelpshiftCore.registerDeviceToken(tokenData)
+        HelpshiftCore.registerDeviceToken(tokenData)
         Mixpanel.sharedInstance().people.addPushDeviceToken(tokenData)
 
         // Don't bother registering for WordPress anything if the user isn't logged in
