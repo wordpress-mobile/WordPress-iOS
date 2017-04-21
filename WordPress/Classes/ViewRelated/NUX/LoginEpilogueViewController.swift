f87a2425b3755859ef12c0cc80c106ddf7b06f50
Nate Heagy <nate@heagy.com>
2017-04-21 15:29:02 -0600

    Remove comment headers from new login files - adds missing IBAction to button in Login.storyboard - adds comments to EpilogueSegue.swift

--- WordPress/Classes/ViewRelated/NUX/LoginEpilogueViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/LoginEpilogueViewController.swift
@@ -1,12 +1,5 @@
-//
-//  PostLoginViewController.swift
-//  WordPress
-//
-//  Created by Nate Heagy on 2017-04-12.
-//  Copyright © 2017 WordPress. All rights reserved.
-//
-
 import UIKit
+
 class LoginEpilogueViewController: UIViewController {
     var originalPresentingVC: UIViewController?
     var dismissBlock: ((_ cancelled: Bool) -> Void)?
