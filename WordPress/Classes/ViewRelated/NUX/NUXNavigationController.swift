5adf2872dbbcc11027794a07b930d1ad72f55f13
Nate Heagy <nate@heagy.com>
2017-04-21 15:32:50 -0600

    Remove prepare(for…) call in NUXNavigationController.swift

--- WordPress/Classes/ViewRelated/NUX/NUXNavigationController.swift
+++ WordPress/Classes/ViewRelated/NUX/NUXNavigationController.swift
@@ -4,12 +4,5 @@ import UIKit
 /// appearance as part of the sign in flow.
 ///
 @objc class NUXNavigationController: RotationAwareNavigationViewController {
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let destination = segue.destination as? LoginEpilogueViewController,
-            let source = segue.source as? NUXNavigationController else {
-                return
-        }
-//        destination.dismissBlock = source.dismissBlock
-        destination.originalPresentingVC = self.presentingViewController
-    }
+
 }
