0bd1dcf657c111b1eb46fc725e4de31ecf282ffc
Aaron Douglas <astralbodies@gmail.com>
2017-04-25 12:06:36 -0500

    Updated pods; fixed optional with new Mixpanel API, removed space.

--- WordPress/Classes/ViewRelated/NUX/LoginSelfHostedViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/LoginSelfHostedViewController.swift
@@ -4,7 +4,7 @@ class LoginSelfHostedViewController: SigninSelfHostedViewController {
     override func needsMultifactorCode() {
         self.performSegue(withIdentifier: "show2FA", sender: self)
     }
-    
+
     override func dismiss() {
         self.performSegue(withIdentifier: "showEpilogue", sender: self)
     }
