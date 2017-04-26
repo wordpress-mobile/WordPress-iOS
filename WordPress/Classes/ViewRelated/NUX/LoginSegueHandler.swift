c56a53e9887aef9324ad8f326404dcb94a5f8c4e
Nate Heagy <nate@heagy.com>
2017-04-26 11:39:55 -0600

    Add link to Natasha's segue article - also make LoginSegueHandler extension more specific

--- WordPress/Classes/ViewRelated/NUX/LoginSegueHandler.swift
+++ WordPress/Classes/ViewRelated/NUX/LoginSegueHandler.swift
@@ -1,8 +1,11 @@
+// Based on this article by @NatashaTheRobot:
+// https://www.natashatherobot.com/protocol-oriented-segue-identifiers-swift/
+
 protocol LoginSegueHandler {
     associatedtype SegueIdentifier: RawRepresentable
 }
 
-extension LoginSegueHandler where Self: UIViewController, SegueIdentifier.RawValue == String {
+extension LoginSegueHandler where Self: NUXAbstractViewController, SegueIdentifier.RawValue == String {
     func performSegue(withIdentifier identifier: SegueIdentifier, sender: AnyObject?) {
         performSegue(withIdentifier: identifier.rawValue, sender: sender)
     }
