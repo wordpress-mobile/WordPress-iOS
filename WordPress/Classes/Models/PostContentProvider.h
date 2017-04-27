1d08bb885751be06b72abf891df74b73cbe4fc75
elibud <elibud@gmail.com>
2017-04-27 10:51:16 -0300

    Make - (NSString *)slugForDisplay optional on the protocol

--- WordPress/Classes/Models/PostContentProvider.h
+++ WordPress/Classes/Models/PostContentProvider.h
@@ -10,9 +10,9 @@
 - (NSURL *)avatarURLForDisplay; // Some providers use a hardcoded URL or blavatar URL
 - (NSString *)gravatarEmailForDisplay;
 - (NSDate *)dateForDisplay;
-- (NSString *)slugForDisplay;
 @optional
 - (BOOL)unreadStatusForDisplay;
 - (NSURL *)featuredImageURLForDisplay;
 - (NSURL *)authorURL;
+- (NSString *)slugForDisplay;
 @end
