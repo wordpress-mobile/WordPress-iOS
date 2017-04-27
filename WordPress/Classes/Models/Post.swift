a412c79415735264aa584f47ea58aa3318ec376b
Jorge Leandro Perez <jorge@lantean.co>
2017-04-27 17:21:25 -0300

    NSManagedObject: New entityName Mechanism

--- WordPress/Classes/Models/Post.swift
+++ WordPress/Classes/Models/Post.swift
@@ -27,7 +27,6 @@ fileprivate func > <T: Comparable>(lhs: T?, rhs: T?) -> Bool {
 
 @objc(Post)
 class Post: AbstractPost {
-
     static let typeDefaultIdentifier = "post"
 
     struct Constants {
@@ -43,6 +42,10 @@ class Post: AbstractPost {
 
     // MARK: - NSManagedObject
 
+    override class var entityName: String {
+        return "Post"
+    }
+    
     override func awakeFromFetch() {
         super.awakeFromFetch()
         buildContentPreview()
