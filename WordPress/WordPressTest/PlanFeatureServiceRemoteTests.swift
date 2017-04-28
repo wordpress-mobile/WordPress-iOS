c9828d5dd93f3c5e8f4f63d0c43b669b8d4da122
Matt Bumgardner <matt.bumgardner@gmail.com>
2017-04-28 12:50:59 -0500

    Remaining tests for PlanFeatureRemote

--- WordPress/WordPressTest/PlanFeatureServiceRemoteTests.swift
+++ WordPress/WordPressTest/PlanFeatureServiceRemoteTests.swift
@@ -7,7 +7,7 @@ class PlanFeatureServiceRemoteTests: RemoteTestCase {
     let planFeaturesEndpoint = "plans/features"
 
     let getPlanFeaturesSuccessMockFilename          = "plans-feature-success.json"
-    let getPlanFeaturesEmptyFailureMockFilename     = "plans-feature-empty-success.json"
+    let getPlanFeaturesEmptySuccessMockFilename     = "plans-feature-empty-success.json"
     let getPlanFeaturesBadJsonFailureMockFilename   = "plans-feature-bad-json-failure.json"
 
 
@@ -21,6 +21,7 @@ class PlanFeatureServiceRemoteTests: RemoteTestCase {
         super.setUp()
 
         remote = PlanFeatureServiceRemote(wordPressComRestApi: restApi)
+        clearDiskCache()
     }
 
     override func tearDown() {
@@ -29,7 +30,7 @@ class PlanFeatureServiceRemoteTests: RemoteTestCase {
 
     // MARK: - Get Plans Tests
 
-    func testGetPlansSucceeds() {
+    func testGetPlanFeaturesSucceeds() {
         let expect = expectation(description: "Get plan features success")
 
         stubRemoteResponse(planFeaturesEndpoint, filename: getPlanFeaturesSuccessMockFilename, contentType: .ApplicationJSON)
@@ -44,82 +45,49 @@ class PlanFeatureServiceRemoteTests: RemoteTestCase {
         waitForExpectations(timeout: timeout, handler: nil)
     }
 
-    func testGetPlansWithEmptyResponseArraySucceeds() {
-        let expect = expectation(description: "Get plans with empty response array success")
+    func testGetPlanFeaturesWithEmptyResponseArraySucceeds() {
+        let expect = expectation(description: "Get plan features with empty response array success")
 
-        stubRemoteResponse(planFeaturesEndpoint, filename: getPlanFeaturesEmptyFailureMockFilename, contentType: .ApplicationJSON)
+        stubRemoteResponse(planFeaturesEndpoint, filename: getPlanFeaturesEmptySuccessMockFilename, contentType: .ApplicationJSON)
         remote.getPlanFeatures({ planFeatures in
             expect.fulfill()
         }, failure: { error in
-            XCTFail("The getPlanFeatures should not fail when empty.")
+            XCTFail("getPlanFeatures should not fail when empty.")
             expect.fulfill()
         })
 
         waitForExpectations(timeout: timeout, handler: nil)
     }
 
-//    func testGetPlansWithBadSiteFails() {
-//        let expect = expectation(description: "Get plans with incorrect site failure")
-//
-//        stubRemoteResponse(sitePlansEndpoint, filename: getPlansBadSiteFailureMockFilename, contentType: .ApplicationJSON, status: 403)
-//        remote.getPlansForSite(siteID, success: { sitePlans in
-//            XCTFail("This callback shouldn't get called")
-//            expect.fulfill()
-//        }, failure: { error in
-//            let error = error as NSError
-//            XCTAssertEqual(error.domain, String(reflecting: WordPressComRestApiError.self), "The error domain should be WordPressComRestApiError")
-//            XCTAssertEqual(error.code, WordPressComRestApiError.authorizationRequired.rawValue, "The error code should be 2 - authorization_required")
-//            expect.fulfill()
-//        })
-//
-//        waitForExpectations(timeout: timeout, handler: nil)
-//    }
-//
-//    func testGetPlansWithServerErrorFails() {
-//        let expect = expectation(description: "Get plans server error failure")
-//
-//        stubRemoteResponse(sitePlansEndpoint, data: Data(), contentType: .NoContentType, status: 500)
-//        remote.getPlansForSite(siteID, success: { sitePlans in
-//            XCTFail("This callback shouldn't get called")
-//            expect.fulfill()
-//        }, failure: { error in
-//            let error = error as NSError
-//            XCTAssertEqual(error.domain, String(reflecting: WordPressComRestApiError.self), "The error domain should be WordPressComRestApiError")
-//            XCTAssertEqual(error.code, WordPressComRestApiError.unknown.rawValue, "The error code should be 7 - unknown")
-//            expect.fulfill()
-//        })
-//
-//        waitForExpectations(timeout: timeout, handler: nil)
-//    }
-//
-//    func testGetPlansWithBadAuthFails() {
-//        let expect = expectation(description: "Get plans with bad auth failure")
-//
-//        stubRemoteResponse(sitePlansEndpoint, filename: getPlansAuthFailureMockFilename, contentType: .ApplicationJSON, status: 403)
-//        remote.getPlansForSite(siteID, success: { sitePlans in
-//            XCTFail("This callback shouldn't get called")
-//            expect.fulfill()
-//        }, failure: { error in
-//            let error = error as NSError
-//            XCTAssertEqual(error.domain, String(reflecting: WordPressComRestApiError.self), "The error domain should be WordPressComRestApiError")
-//            XCTAssertEqual(error.code, WordPressComRestApiError.authorizationRequired.rawValue, "The error code should be 2 - authorization_required")
-//            expect.fulfill()
-//        })
-//
-//        waitForExpectations(timeout: timeout, handler: nil)
-//    }
-//
-//    func testGetPlansWithBadJsonFails() {
-//        let expect = expectation(description: "Get plans with invalid json response failure")
-//
-//        stubRemoteResponse(sitePlansEndpoint, filename: getPlansBadJsonFailureMockFilename, contentType: .ApplicationJSON, status: 200)
-//        remote.getPlansForSite(siteID, success: { sitePlans in
-//            XCTFail("This callback shouldn't get called")
-//            expect.fulfill()
-//        }, failure: { error in
-//            expect.fulfill()
-//        })
-//
-//        waitForExpectations(timeout: timeout, handler: nil)
-//    }
+    func testGetPlanFeaturesWithServerErrorFails() {
+        let expect = expectation(description: "Get plan features server error failure")
+
+        stubRemoteResponse(planFeaturesEndpoint, data: Data(), contentType: .NoContentType, status: 500)
+        remote.getPlanFeatures({ planFeatures in
+            XCTFail("This callback shouldn't get called")
+            expect.fulfill()
+        }, failure: { error in
+            let error = error as NSError
+            XCTAssertEqual(error.domain, String(reflecting: WordPressComRestApiError.self), "The error domain should be WordPressComRestApiError")
+            XCTAssertEqual(error.code, WordPressComRestApiError.unknown.rawValue, "The error code should be 7 - unknown")
+            expect.fulfill()
+        })
+
+        waitForExpectations(timeout: timeout, handler: nil)
+    }
+
+
+    func testGetPlanFeaturesWithBadJsonFails() {
+        let expect = expectation(description: "Get plan features with invalid json response failure")
+
+        stubRemoteResponse(planFeaturesEndpoint, filename: getPlanFeaturesBadJsonFailureMockFilename, contentType: .ApplicationJSON, status: 200)
+        remote.getPlanFeatures({ planFeatures in
+            XCTFail("This callback shouldn't get called")
+            expect.fulfill()
+        }, failure: { error in
+            expect.fulfill()
+        })
+
+        waitForExpectations(timeout: timeout, handler: nil)
+    }
 }
