e67f385139532fbf103a81a7e43be5b44a631acb
Aaron Douglas <astralbodies@gmail.com>
2017-05-05 14:17:11 -0500

    Adds a copy phase for stats framework and fixes bundle loading

--- WordPress/Classes/ViewRelated/Post/PostListViewController.swift
+++ WordPress/Classes/ViewRelated/Post/PostListViewController.swift
@@ -449,17 +449,7 @@ class PostListViewController: AbstractPostListViewController, UIViewControllerRe
         let identifier = NSStringFromClass(StatsPostDetailsTableViewController.self)
         let service = BlogService(managedObjectContext: ContextManager.sharedInstance().mainContext)
         let statsBundle = Bundle(for: WPStatsViewController.self)
-
-        guard let path = statsBundle.path(forResource: "WordPressCom-Stats-iOS", ofType: "bundle") else {
-            let message = "The stats bundle is missing"
-
-            assertionFailure(message)
-            DDLogSwift.logError("\(#file): \(#function) [\(#line)] - \(message)")
-            return
-        }
-
-        let bundle = Bundle(path: path)
-        let statsStoryboard = UIStoryboard(name: type(of: self).statsStoryboardName, bundle: bundle)
+        let statsStoryboard = UIStoryboard(name: type(of: self).statsStoryboardName, bundle: statsBundle)
         let viewControllerObject = statsStoryboard.instantiateViewController(withIdentifier: identifier)
 
         assert(viewControllerObject is StatsPostDetailsTableViewController)
