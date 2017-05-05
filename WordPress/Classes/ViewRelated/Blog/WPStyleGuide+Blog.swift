96b3f69db943bd047f35e959d24d7a41a2a7f816
Nate Heagy <nate@heagy.com>
2017-05-05 15:48:05 -0600

    Implement visual design for login epilogue screen

--- WordPress/Classes/ViewRelated/Blog/WPStyleGuide+Blog.swift
+++ WordPress/Classes/ViewRelated/Blog/WPStyleGuide+Blog.swift
@@ -3,14 +3,39 @@ import WordPressShared
 
 extension WPStyleGuide {
     public class func configureTableViewBlogCell(_ cell: UITableViewCell) {
-        configureTableViewCell(cell)
+        cell.textLabel?.font = tableviewTextFont()
+        cell.textLabel?.sizeToFit()
+        cell.textLabel?.textColor = darkGrey()
+
         cell.detailTextLabel?.font = self.subtitleFont()
+        cell.detailTextLabel?.sizeToFit()
         cell.detailTextLabel?.textColor = self.greyDarken10()
-        cell.backgroundColor = UIColor.white
 
         cell.imageView?.layer.borderColor = UIColor.white.cgColor
         cell.imageView?.layer.borderWidth = 1
+        cell.imageView?.tintColor = greyLighten10()
+
+        cell.backgroundColor = UIColor.white
     }
+
+    public class func configureCellForLogin(_ cell: WPBlogTableViewCell) {
+        // TODO: make this dynamic size once @elibud's dynamic type code is merged
+        cell.textLabel?.font = WPFontManager.systemSemiBoldFont(ofSize: 15.0)
+        cell.textLabel?.sizeToFit()
+        cell.textLabel?.textColor = darkGrey()
+
+        cell.detailTextLabel?.font = UIFont.preferredFont(forTextStyle: .footnote)
+        cell.detailTextLabel?.sizeToFit()
+        cell.detailTextLabel?.textColor = self.darkGrey() // TODO: waiting on @mattmiklic to double check this color
+
+        cell.imageView?.layer.borderColor = greyLighten20().cgColor
+        cell.imageView?.layer.borderWidth = 1
+        cell.imageView?.tintColor = greyLighten10()
+
+        cell.backgroundColor = lightGrey()
+    }
+
+    //public class func configure
 
     public class func cellGridiconAccessoryColor() -> UIColor {
         return UIColor(red: 200.0 / 255.0, green: 200.0 / 255.0, blue: 205.0 / 255.0, alpha: 1.0)
