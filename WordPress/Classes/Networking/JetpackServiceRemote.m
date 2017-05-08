8541f221bd13bc261eb74acfa29e239b955a3de5
aerych <eric@automattic.com>
2017-05-08 16:11:44 -0500

    Switch to new endpoint for better jetpack check.

--- WordPress/Classes/Networking/JetpackServiceRemote.m
+++ WordPress/Classes/Networking/JetpackServiceRemote.m
@@ -62,70 +62,26 @@ static NSString * const GetUsersBlogsApiPath = @"https://public-api.wordpress.co
 
 /**
     Check if the specified site is a Jetpack site.  The success block
-    receives two arguements, a bool indicating if the site is Jetpack, and
-    and optionally specifies a connection error for the site.
+    receives a bool indicating if the site hass Jetpack.
  */
-- (void)checkSiteIsJetpack:(NSURL *)siteURL
-                   success:(void (^)(BOOL isJetpack, NSError *error))success
-                   failure:(void (^)(NSError *error))failure
+- (void)checkSiteHasJetpack:(NSURL *)siteURL
+                    success:(void (^)(BOOL isJetpack))success
+                    failure:(void (^)(NSError *error))failure
 {
     NSString *siteStr = [[NSString stringWithFormat:@"%@%@", siteURL.host, siteURL.path]
                          stringByAddingPercentEncodingWithAllowedCharacters:NSCharacterSet.URLHostAllowedCharacterSet];
+    NSString *scheme = siteURL.scheme ?: @"http";
 
-    NSString *endpoint = [NSString stringWithFormat:@"sites/%@", siteStr];
+    NSString *endpoint = [NSString stringWithFormat:@"connect/site-info/%@/%@", scheme, siteStr];
     NSString *path = [self pathForEndpoint:endpoint withVersion:ServiceRemoteWordPressComRESTApiVersion_1_1];
 
     [self.wordPressComRestApi GET:path
                        parameters:nil
                           success:^(id responseObject, NSHTTPURLResponse *httpResponse) {
                               NSDictionary *dict = (NSDictionary *)responseObject;
-                              BOOL isJetpack = [[dict numberForKey:@"jetpack"] boolValue];
-                              success(isJetpack, nil);
-
+                              BOOL hasJetpack = [[dict numberForKey:@"hasJetpack"] boolValue];
+                              success(hasJetpack);
                           } failure:^(NSError *error, NSHTTPURLResponse *httpResponse) {
-
-                              if ([error.domain isEqualToString:WordPressComRestApiErrorDomain]) {
-                                  NSDictionary *userInfo = error.userInfo;
-                                  NSString *errorKey = [userInfo stringForKey:WordPressComRestApi.ErrorKeyErrorCode];
-                                  BOOL isJetpack = NO;
-
-                                  // Jetpack is installed and connected but there is some other error.
-                                  // It could be a php error.  It could be that xmlrpc requests have been blocked
-                                  // via .htaccess :
-                                  // <Files xmlrpc.php>
-                                  // order allow,deny
-                                  // deny from all
-                                  // </Files>
-                                  // Or it could be due to some other unknown error that caues the site
-                                  // to return ah HTTP 500 status code when queried by the REST API.
-                                  // NOTE: IF XML-RPC is disabled via `add_filter( 'xmlrpc_enabled', '__return_false' );`
-                                  // the REST API currently still treates it as working (system.listMethods still operates).
-                                  if ([errorKey isEqualToString:@"jetpack_error"] && error.code == WordPressComRestApiErrorUnknown) {
-                                      // Site is inaccessible. (500 error)
-                                      isJetpack = YES;
-                                      error = [NSError errorWithDomain:JetpackServiceRemoteErrorDomain
-                                                                  code:JetpackServiceRemoteErrorSiteInaccessable
-                                                              userInfo:error.userInfo];
-
-                                  }
-
-                                  // The unauthorized key is a special case here.
-                                  // The API call does not require credentials and does not return a 403 itself.
-                                  // This combination occurs when Jetpack is installed but not connected to wpcom.
-                                  if ([errorKey isEqualToString:@"unauthorized"] && error.code == WordPressComRestApiErrorAuthorizationRequired) {
-                                      // Jetpack is disabled/installed but not connected. (403 error)
-                                      isJetpack = YES;
-                                      error = [NSError errorWithDomain:JetpackServiceRemoteErrorDomain
-                                                                  code:JetpackServiceRemoteErrorJetpackDisabled
-                                                              userInfo:error.userInfo];
-
-
-                                  }
-
-                                  success(isJetpack, error);
-                                  return;
-                              }
-
                               failure(error);
                           }];
 }
