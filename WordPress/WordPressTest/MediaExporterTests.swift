4b5615553fb5e139023dc1a419a0c26a5a22afa1
kurzee <brent@kurzee.com>
2017-05-09 12:16:04 -0500

    Media: file extension helper as a String extension.

--- WordPress/WordPressTest/MediaExporterTests.swift
+++ WordPress/WordPressTest/MediaExporterTests.swift
@@ -35,9 +35,8 @@ class MediaExporterTests: XCTestCase {
     func testThatFileExtensionForTypeIsWorking() {
         // Testing JPEG as a simple test of the implementation.
         // Maybe expanding the test to all of our supported types would be helpful.
-        let exporter = MockMediaExporter()
         let expected = "jpeg"
-        XCTAssert(exporter.fileExtensionForUTType(kUTTypeJPEG as String) == expected, "Error: unexpected extension found when converting from UTType")
+        XCTAssert(String.fileExtensionForUTType(kUTTypeJPEG) == expected, "Error: unexpected extension found when converting from UTType")
     }
 
     func testThatURLFileSizeWorks() {
