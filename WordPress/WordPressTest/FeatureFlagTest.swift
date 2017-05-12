8d9a4e4143b595c065f074a44f6c7d9925edcb2b
Nate Heagy <nate@heagy.com>
2017-05-12 13:51:34 -0600

    Adding alpha-to-buddy line that wasn’t staged
    - also fixing tests

--- WordPress/WordPressTest/FeatureFlagTest.swift
+++ WordPress/WordPressTest/FeatureFlagTest.swift
@@ -7,15 +7,15 @@ class FeatureFlagTest: XCTestCase {
     func testBuild() {
         Build.withCurrent(.debug) {
             expect(build(.debug)).to(beTrue())
-            expect(build(.debug, .alpha)).to(beTrue())
-            expect(build(.alpha)).to(beFalse())
+            expect(build(.debug, .buddy)).to(beTrue())
+            expect(build(.buddy)).to(beFalse())
             expect(build(.internal)).to(beFalse())
             expect(build(.appStore)).to(beFalse())
         }
 
         Build.withCurrent(.appStore) {
             expect(build(.debug)).to(beFalse())
-            expect(build(.alpha)).to(beFalse())
+            expect(build(.buddy)).to(beFalse())
             expect(build(.internal)).to(beFalse())
             expect(build(.appStore)).to(beTrue())
             expect(build(.internal,.appStore)).to(beTrue())
