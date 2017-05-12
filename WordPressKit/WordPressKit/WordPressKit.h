90ba33e187095efa78a3a4149d8f684faea042bb
Aaron Douglas <astralbodies@gmail.com>
2017-05-12 08:25:37 -0500

    Initial remote framework project creation

---
+++ WordPressKit/WordPressKit/WordPressKit.h
@@ -0,0 +1,19 @@
+//
+//  WordPressKit.h
+//  WordPressKit
+//
+//  Created by Aaron Douglas on 5/12/17.
+//  Copyright © 2017 Automattic Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WordPressKit.
+FOUNDATION_EXPORT double WordPressKitVersionNumber;
+
+//! Project version string for WordPressKit.
+FOUNDATION_EXPORT const unsigned char WordPressKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WordPressKit/PublicHeader.h>
+
+
