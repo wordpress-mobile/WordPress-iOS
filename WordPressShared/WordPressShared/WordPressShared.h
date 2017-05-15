2e20752729f083ba95fbe1a75d1f21fce0f3d0bf
elibud <elibud@gmail.com>
2017-05-15 18:32:47 -0300

    Initial integration of WordPressShared as a framework

---
+++ WordPressShared/WordPressShared/WordPressShared.h
@@ -0,0 +1,23 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for WordPressShared.
+FOUNDATION_EXPORT double WordPressSharedVersionNumber;
+
+//! Project version string for WordPressShared.
+FOUNDATION_EXPORT const unsigned char WordPressSharedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WordPressShared/PublicHeader.h>
+
+#import <WordPressShared/NSString+Util.h>
+#import <WordPressShared/NSString+XMLExtensions.h>
+#import <WordPressShared/UIColor+Helpers.h>
+#import <WordPressShared/UIImage+Util.h>
+#import <WordPressShared/WPDeviceIdentification.h>
+#import <WordPressShared/WPFontManager.h>
+#import <WordPressShared/WPImageSource.h>
+#import <WordPressShared/WPNoResultsView.h>
+#import <WordPressShared/WPNUXUtility.h>
+#import <WordPressShared/WPSharedLogging.h>
+#import <WordPressShared/WPStyleGuide.h>
+#import <WordPressShared/WPTableViewCell.h>
+#import <WordPressShared/WPTextFieldTableViewCell.h>
