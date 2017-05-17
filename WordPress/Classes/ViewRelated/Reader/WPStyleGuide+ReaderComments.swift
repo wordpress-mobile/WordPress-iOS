c1115ce6b3cc47739ef22e31edb215ea85e84311
elibud <elibud@gmail.com>
2017-05-17 17:35:49 -0300

    Dynamic Type support on Reader

--- WordPress/Classes/ViewRelated/Reader/WPStyleGuide+ReaderComments.swift
+++ WordPress/Classes/ViewRelated/Reader/WPStyleGuide+ReaderComments.swift
@@ -3,20 +3,6 @@ import WordPressShared
 
 extension WPStyleGuide {
 
-    public class func navbarButtonTintColor() -> UIColor {
-        return UIColor(white: 1.0, alpha: 0.5)
-    }
-
-    // Styles used by Comments in the Reader
-
-    public class func commentTitleFont() -> UIFont {
-        return WPFontManager.systemBoldFont(ofSize: 14)
-    }
-
-    public class func commentBodyFont() -> UIFont {
-        return WPFontManager.systemRegularFont(ofSize: 14)
-    }
-
     public class func defaultSearchBarTextAttributes(_ color: UIColor) -> [String: AnyObject] {
         return [
             NSForegroundColorAttributeName: color,
