13f7eb70c32e7e89cc0caea4bdbb0e16df7f8748
aerych <eric@automattic.com>
2017-05-18 10:29:39 -0500

    Login: Adds container class for epilogue user info.

---
+++ WordPress/Classes/ViewRelated/NUX/LoginEpilogueUserInfo.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+/// A simple container for the user info shown on the login epilogue screen.
+///
+struct LoginEpilogueUserInfo {
+    var username = ""
+    var fullname = ""
+    var email = ""
+    var gravatarUrl: String?
+}
