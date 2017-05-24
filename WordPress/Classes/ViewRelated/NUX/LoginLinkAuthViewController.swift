649d3fa4a61b85a41cbf9c81600a6f6fa0214f42
Nate Heagy <nate@heagy.com>
2017-05-24 08:40:50 -0600

    Add top WP logo and fix login colors

--- WordPress/Classes/ViewRelated/NUX/LoginLinkAuthViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/LoginLinkAuthViewController.swift
@@ -1,8 +1,13 @@
 import UIKit
 
-class LoginLinkAuthViewController: SigninLinkAuthViewController {
+class LoginLinkAuthViewController: SigninLinkAuthViewController, LoginViewController {
     // let the storyboard's style stay
     override func setupStyles() {}
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavBarIcon()
+    }
 
     override func dismiss() {
         self.performSegue(withIdentifier: .showEpilogue, sender: self)
