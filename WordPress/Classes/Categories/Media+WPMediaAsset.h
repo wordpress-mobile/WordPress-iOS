c09bf3d49e121e6e1e504f342fcd409d2d0883af
kurzee <brent@kurzee.com>
2017-05-25 17:41:01 -0500

    Media: move Media category to its own file.
    This moves the existing Media category that conformed to WPMediaAsset
    to it’s own file, from MediaLibraryPickerDataSource.

---
+++ WordPress/Classes/Categories/Media+WPMediaAsset.h
@@ -0,0 +1,10 @@
+#import "Media.h"
+#import <WPMediaPicker/WPMediaPicker.h>
+
+/**
+ Provides an implementation of Media conforming to the WPMediaAsset protocol.
+ */
+@interface Media (WPMediaAsset) <WPMediaAsset>
+
+
+@end
