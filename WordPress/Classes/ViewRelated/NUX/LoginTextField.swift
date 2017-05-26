47c2a10df5952f8d1a4e06f6244d7f0877a0a301
Nate Heagy <nate@heagy.com>
2017-05-26 14:12:56 -0600

    Move login Next buttons to bottom right

--- WordPress/Classes/ViewRelated/NUX/LoginTextField.swift
+++ WordPress/Classes/ViewRelated/NUX/LoginTextField.swift
@@ -8,7 +8,7 @@ class LoginTextField: WPWalkthroughTextField {
             guard let context = UIGraphicsGetCurrentContext() else {
                 return
             }
-            
+
             drawTopLine(rect: rect, context: context)
             drawBottomLine(rect: rect, context: context)
         }
@@ -17,14 +17,14 @@ class LoginTextField: WPWalkthroughTextField {
     private func drawTopLine(rect: CGRect, context: CGContext) {
         drawBorderLine(from: CGPoint(x: rect.minX, y: rect.minY), to: CGPoint(x: rect.maxX, y: rect.minY), context: context)
     }
-    
+
     private func drawBottomLine(rect: CGRect, context: CGContext) {
         drawBorderLine(from: CGPoint(x: rect.minX, y: rect.maxY), to: CGPoint(x: rect.maxX, y: rect.maxY), context: context)
     }
-    
+
     private func drawBorderLine(from startPoint: CGPoint, to endPoint: CGPoint, context: CGContext) {
-        let path = UIBezierPath();
-        
+        let path = UIBezierPath()
+
         path.move(to: startPoint)
         path.addLine(to: endPoint)
         path.lineWidth = UIScreen.main.scale / 2.0
