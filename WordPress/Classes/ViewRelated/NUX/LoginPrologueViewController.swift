695a41b47612fb90f0e3a64f87268afb285dc8cc
Nate Heagy <nate@heagy.com>
2017-05-29 13:15:15 -0600

    Remove trailing whitespace

--- WordPress/Classes/ViewRelated/NUX/LoginPrologueViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/LoginPrologueViewController.swift
@@ -4,13 +4,13 @@ class LoginPrologueViewController: UIViewController {
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+
         self.navigationController?.setNavigationBarHidden(true, animated: false)
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
+
         self.navigationController?.setNavigationBarHidden(true, animated: animated)
     }
 
