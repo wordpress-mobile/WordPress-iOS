f677a989dfa2283cee71ae4bab9a6c4b1f7778b2
Nate Heagy <nate@heagy.com>
2017-05-31 10:10:21 -0600

    Add new animations for login prologue

--- WordPress/Classes/ViewRelated/NUX/LoginPrologueViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/LoginPrologueViewController.swift
@@ -1,6 +1,32 @@
 import UIKit
+import Lottie
 
 class LoginPrologueViewController: UIViewController {
+    @IBOutlet var animationHolder: UIView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let holder = animationHolder,
+        let animation = LOTAnimationView(name: "notifications")
+            else {
+            return
+        }
+//        let animation = UIView()
+        animation.translatesAutoresizingMaskIntoConstraints = false
+//        animation.backgroundColor = UIColor.red
+        animation.contentMode = .scaleAspectFit
+        holder.addSubview(animation)
+        
+        // setup autolayout
+        animation.leadingAnchor.constraint(equalTo: holder.leadingAnchor).isActive = true
+        animation.trailingAnchor.constraint(equalTo: holder.trailingAnchor).isActive = true
+        animation.topAnchor.constraint(equalTo: holder.topAnchor).isActive = true
+        //animation.centerXAnchor.constraint(equalTo: holder.centerXAnchor).isActive = true
+        animation.bottomAnchor.constraint(equalTo: holder.bottomAnchor).isActive = true
+        
+        animation.loopAnimation = true
+        animation.play()
+    }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -13,5 +39,4 @@ class LoginPrologueViewController: UIViewController {
 
         self.navigationController?.setNavigationBarHidden(false, animated: animated)
     }
-
 }
