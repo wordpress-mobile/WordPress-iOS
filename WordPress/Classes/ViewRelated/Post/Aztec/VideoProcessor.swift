370727fd50f099c654a8d76ee56463e6fbdbd259
Sergio Estevao <sergioestevao@gmail.com>
2017-06-01 17:03:46 +0100

    Add Video shortcodes processors and tests to project.

---
+++ WordPress/Classes/ViewRelated/Post/Aztec/VideoProcessor.swift
@@ -0,0 +1,73 @@
+import Foundation
+import Aztec
+
+public struct VideoProcessor {
+
+    static public var videoPressPreProcessor: Processor {
+        let videoPressProcessor = ShortcodeProcessor(tag:"wpvideo", replacer: { (shortcode) in
+        var html = "<video "
+        if let src = shortcode.attributes.unamed.first {
+            html += "src=\"videopress://\(src)\" "
+            html += "data-wpvideopress=\"\(src)\" "
+        }
+        if let width = shortcode.attributes.named["w"] {
+            html += "width=\(width) "
+        }
+        if let height = shortcode.attributes.named["h"] {
+            html += "height=\(height) "
+        }
+
+        html += "/>"
+        return html
+        })
+        return videoPressProcessor
+    }
+
+    static public var videoPressPostProcessor: Processor {
+        let postWordPressVideoProcessor = HTMLProcessor(tag:"video", replacer: { (shortcode) in
+            guard let videoPressID = shortcode.attributes.named["data-wpvideopress"] else {
+                return nil
+            }
+            var html = "[wpvideo \(videoPressID) "
+            if let width = shortcode.attributes.named["width"] {
+                html += "w=\(width) "
+            }
+            if let height = shortcode.attributes.named["height"] {
+                html += "h=\(height) "
+            }
+            html += "]"
+            return html
+        })
+        return postWordPressVideoProcessor
+    }
+
+    static public var wordPressVideoPreProcessor: Processor {
+        let wordPressVideoProcessor = ShortcodeProcessor(tag:"video", replacer: { (shortcode) in
+            var html = "<video "
+            if let src = shortcode.attributes.named["src"] {
+                html += "src=\"\(src)\" "
+            }
+            if let poster = shortcode.attributes.named["poster"] {
+                html += "poster=\"\(poster)\" "
+            }
+            html += "/>"
+            return html
+        })
+        return wordPressVideoProcessor
+    }
+
+    static public var wordPressVideoPostProcessor: Processor {
+        let postWordPressVideoProcessor = HTMLProcessor(tag:"video", replacer: { (shortcode) in
+            var html = "[video "
+            if let src = shortcode.attributes.named["src"] {
+                html += "src=\"\(src)\" "
+            }
+            if let poster = shortcode.attributes.named["poster"] {
+                html += "poster=\"\(poster)\" "
+            }
+            html += "]"
+            return html
+        })
+        return postWordPressVideoProcessor
+    }
+}
