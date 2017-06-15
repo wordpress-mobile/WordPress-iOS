1cb660180dcb953f6cc79fddb83ff75873c6bc89
aerych <eric@automattic.com>
2017-06-15 15:19:47 -0500

    Login: Let NSURLErrorNetworkConnectionLost errors pass through.

--- WordPressKit/WordPressKit/WordPressOrgXMLRPCValidator.swift
+++ WordPressKit/WordPressKit/WordPressOrgXMLRPCValidator.swift
@@ -69,8 +69,9 @@ open class WordPressOrgXMLRPCValidator: NSObject {
 
         validateXMLRPCURL(xmlrpcURL, success: success, failure: { (error) in
                 //DDLogSwift.logError(error.localizedDescription)
-                if error.domain == NSURLErrorDomain && error.code == NSURLErrorUserCancelledAuthentication ||
-                   error.domain == String(reflecting: WordPressOrgXMLRPCValidatorError.self) && error.code == WordPressOrgXMLRPCValidatorError.mobilePluginRedirectedError.rawValue {
+                if (error.domain == NSURLErrorDomain && error.code == NSURLErrorUserCancelledAuthentication) ||
+                   (error.domain == NSURLErrorDomain && error.code == NSURLErrorNetworkConnectionLost) ||
+                   (error.domain == String(reflecting: WordPressOrgXMLRPCValidatorError.self) && error.code == WordPressOrgXMLRPCValidatorError.mobilePluginRedirectedError.rawValue) {
                     failure(error)
                     return
                 }
