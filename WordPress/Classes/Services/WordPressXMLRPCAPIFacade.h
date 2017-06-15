457cc949e8906ada832496504e7df1e868a9e697
aerych <eric@automattic.com>
2017-06-15 10:47:40 -0500

    Preserve original errors userInfo dictionary.

--- WordPress/Classes/Services/WordPressXMLRPCAPIFacade.h
+++ WordPress/Classes/Services/WordPressXMLRPCAPIFacade.h
@@ -2,6 +2,8 @@
 
 @protocol WordPressXMLRPCAPIFacade
 
+extern NSString *const XMLRPCOriginalErrorUserInfoKey;
+
 - (void)guessXMLRPCURLForSite:(NSString *)url
                       success:(void (^)(NSURL *xmlrpcURL))success
                       failure:(void (^)(NSError *error))failure;
