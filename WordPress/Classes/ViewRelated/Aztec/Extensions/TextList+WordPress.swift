369c109b0c9511a98f3a148e33d24b3052bda2c3
Jorge Leandro Perez <jorge@lantean.co>
2017-06-16 12:07:33 -0300

    Implements TextList.Style

---
+++ WordPress/Classes/ViewRelated/Aztec/Extensions/TextList+WordPress.swift
@@ -0,0 +1,26 @@
+import Foundation
+import Aztec
+
+
+// MARK: - TextList.Style
+//
+private extension TextList.Style {
+
+    var formattingIdentifier: FormattingIdentifier {
+        switch self {
+        case .ordered:   return FormattingIdentifier.orderedlist
+        case .unordered: return FormattingIdentifier.unorderedlist
+        }
+    }
+
+    var description: String {
+        switch self {
+        case .ordered: return "Ordered List"
+        case .unordered: return "Unordered List"
+        }
+    }
+
+    var iconImage: UIImage? {
+        return formattingIdentifier.iconImage
+    }
+}
