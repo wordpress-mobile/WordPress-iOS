eedb65602af5122a26e33160a6718e3acd4c982f
kurzee <brent@kurzee.com>
2017-06-16 18:11:44 -0500

    Media: expanded and improved ThumbnailExporter implementation.
    Adds support for ensuring unique thumbnail URLs, querying available
    thumbnails by URL, better image support, tests.

--- WordPress/WordPressTest/MediaThumbnailExporterTests.swift
+++ WordPress/WordPressTest/MediaThumbnailExporterTests.swift
@@ -6,41 +6,6 @@ class MediaThumbnailExporterTests: XCTestCase {
     let testDeviceImageName = "test-image-device-photo-gps.jpg"
     let testDeviceVideoName = "test-video-device-gps.m4v"
 
-    func testThatThumbnailURLsWork() {
-        let mediaPath = MediaImageExporterTests.filePathForTestImageNamed(testDeviceImageName)
-        let url = URL(fileURLWithPath: mediaPath)
-
-        let exporter = MediaThumbnailExporter()
-        exporter.options.preferredSize = CGSize(width: 200, height: 200)
-        let preferredSizeAtScale = exporter.options.preferredSizeAtScale!
-
-        // Test that the generated thumbnail URLs match the expected format.
-        let expectedFilename = url.deletingPathExtension().lastPathComponent
-        let expectedExtension = url.resourceTypeIdentifierFileExtension!
-        let expectedThumbnailDesc = "thumbnail(\(Int(preferredSizeAtScale.width))x\(Int(preferredSizeAtScale.height))).\(expectedExtension)"
-
-        do {
-            let thumbnailURL = try exporter.expectedThumbnailURL(forFile: url)
-            let expectedLastPathComponent = "\(expectedFilename)-\(expectedThumbnailDesc)"
-            XCTAssert(thumbnailURL.lastPathComponent == expectedLastPathComponent, "Unexpected thumbnail URL generated from thumbnail exporter.")
-
-            // Test that the actual exported thumbnail URL matches the expected URL.
-            let expect = self.expectation(description: "thumbnail export by URL")
-            exporter.exportThumbnail(forFile: url,
-                                     onCompletion: { (export) in
-                                        XCTAssertTrue(export.url == thumbnailURL, "Unexpected thumbnail URL result from thumbnail export")
-                                        MediaExporterTests.cleanUpExportedMedia(atURL: export.url)
-                                        expect.fulfill()
-            }) { (error) in
-                XCTFail("Error: an error occurred testing a thumbnail export: \(error.toNSError())")
-                expect.fulfill()
-            }
-            waitForExpectations(timeout: 2.0, handler: nil)
-        } catch {
-            XCTFail("Error: an error occurred testing a thumbnail URL: \(error)")
-        }
-    }
-
     func testThatExportingImageThumbnailWorks() {
         let mediaPath = MediaImageExporterTests.filePathForTestImageNamed(testDeviceImageName)
         let expect = self.expectation(description: "thumbnail image export by URL")
@@ -49,7 +14,10 @@ class MediaThumbnailExporterTests: XCTestCase {
         let exporter = MediaThumbnailExporter()
         exporter.options.preferredSize = CGSize(width: 200, height: 200)
         exporter.exportThumbnail(forFile: url,
-                                 onCompletion: { (export) in
+                                 onCompletion: { (identifier, export) in
+                                    self.validateThumbnailExport(withExporter: exporter,
+                                                                 identifier: identifier,
+                                                                 export: export)
                                     MediaImageExporterTests.validateImageExport(export,
                                                                                 withExpectedSize: exporter.options.preferredMaximumSizeAtScale!)
                                     MediaExporterTests.cleanUpExportedMedia(atURL: export.url)
@@ -71,7 +39,10 @@ class MediaThumbnailExporterTests: XCTestCase {
         let exporter = MediaThumbnailExporter()
         exporter.options.preferredSize = CGSize(width: 200, height: 200)
         exporter.exportThumbnail(forFile: url,
-                                 onCompletion: { (export) in
+                                 onCompletion: { (identifier, export) in
+                                    self.validateThumbnailExport(withExporter: exporter,
+                                                                 identifier: identifier,
+                                                                 export: export)
                                     MediaImageExporterTests.validateImageExport(export,
                                                                                 withExpectedSize: exporter.options.preferredMaximumSizeAtScale!)
                                     MediaExporterTests.cleanUpExportedMedia(atURL: export.url)
@@ -81,5 +52,13 @@ class MediaThumbnailExporterTests: XCTestCase {
             expect.fulfill()
         }
         waitForExpectations(timeout: 2.0, handler: nil)
+    }
+
+    fileprivate func validateThumbnailExport(withExporter exporter: MediaThumbnailExporter, identifier: MediaThumbnailExporter.ThumbnailIdentifier, export: MediaImageExport) {
+        guard let availableThumbnail = exporter.availableThumbnail(with: identifier) else {
+            XCTFail("Thumbnail exported but was not detected as available.")
+            return
+        }
+        XCTAssertTrue(export.url == availableThumbnail, "Unexpected thumbnail export URL when comparing with availableThumbnail URL.")
     }
 }
