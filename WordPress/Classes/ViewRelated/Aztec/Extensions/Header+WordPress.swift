42520f5f675ad906b01329dd54a7c85a96d942cc
Jorge Leandro Perez <jorge@lantean.co>
2017-06-16 12:07:25 -0300

    Implements Header.HeaderType

---
+++ WordPress/Classes/ViewRelated/Aztec/Extensions/Header+WordPress.swift
@@ -0,0 +1,35 @@
+import Foundation
+import Aztec
+
+
+// MARK: - Header and List presentation extensions
+//
+extension Header.HeaderType {
+    var formattingIdentifier: FormattingIdentifier {
+        switch self {
+        case .none: return FormattingIdentifier.p
+        case .h1:   return FormattingIdentifier.header1
+        case .h2:   return FormattingIdentifier.header2
+        case .h3:   return FormattingIdentifier.header3
+        case .h4:   return FormattingIdentifier.header4
+        case .h5:   return FormattingIdentifier.header5
+        case .h6:   return FormattingIdentifier.header6
+        }
+    }
+
+    var description: String {
+        switch self {
+        case .none: return NSLocalizedString("Default", comment: "Description of the default paragraph formatting style in the editor.")
+        case .h1: return "Heading 1"
+        case .h2: return "Heading 2"
+        case .h3: return "Heading 3"
+        case .h4: return "Heading 4"
+        case .h5: return "Heading 5"
+        case .h6: return "Heading 6"
+        }
+    }
+
+    var iconImage: UIImage? {
+        return formattingIdentifier.iconImage
+    }
+}
