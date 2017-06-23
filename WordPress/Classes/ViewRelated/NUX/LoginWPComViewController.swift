c9f90c0ce6ea53a3f4b3f5afe296066fe0caf730
Nate Heagy <nate@heagy.com>
2017-06-23 16:10:42 -0600

    Implement new design for wpcom password screen - email is uneditable - instructional text added - next button changed - single sign-on button removed (this screen only)

--- WordPress/Classes/ViewRelated/NUX/LoginWPComViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/LoginWPComViewController.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 class LoginWPComViewController: SigninWPComViewController, LoginViewController {
+    @IBOutlet var emailLabel: UILabel?
+
     // let the storyboard's style stay
     override func setupStyles() {}
 
@@ -11,6 +13,14 @@ class LoginWPComViewController: SigninWPComViewController, LoginViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavBarIcon()
+        usernameField.isHidden = true
+        selfHostedButton.isHidden = true
+    }
+    
+    override func configureTextFields() {
+        super.configureTextFields()
+        passwordField.textInsets = WPStyleGuide.edgeInsetForLoginTextFields()
+        emailLabel?.text = usernameField.text
     }
 
     override func needsMultifactorCode() {
@@ -19,5 +29,19 @@ class LoginWPComViewController: SigninWPComViewController, LoginViewController {
 
         WPAppAnalytics.track(.twoFactorCodeRequested)
         self.performSegue(withIdentifier: .show2FA, sender: self)
+    }
+
+    override func localizeControls() {
+        passwordField.placeholder = NSLocalizedString("Password", comment: "Password placeholder")
+        passwordField.accessibilityIdentifier = "Password"
+        
+        let submitButtonTitle = NSLocalizedString("Next", comment: "Title of a button. The text should be capitalized.").localizedCapitalized
+        submitButton.setTitle(submitButtonTitle, for: UIControlState())
+        submitButton.setTitle(submitButtonTitle, for: .highlighted)
+        submitButton.accessibilityIdentifier = "Log In Button"
+        
+        let forgotPasswordTitle = NSLocalizedString("Lost your password?", comment: "Title of a button. ")
+        forgotPasswordButton.setTitle(forgotPasswordTitle, for: UIControlState())
+        forgotPasswordButton.setTitle(forgotPasswordTitle, for: .highlighted)
     }
 }
