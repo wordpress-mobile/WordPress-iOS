64f31618b92e8b0c86fa43db3bf8df7ed56aeb0e
James Frost <james.frost@automattic.com>
2017-06-24 21:41:17 +0100

    FancyAlerts: Add configurations for Aztec announcements

--- WordPress/Classes/ViewRelated/System/FancyAlerts+AztecAnnouncement.swift
+++ WordPress/Classes/ViewRelated/System/FancyAlerts+AztecAnnouncement.swift
@@ -2,24 +2,32 @@ import UIKit
 
 extension FancyAlertViewController {
     static func aztecAnnouncementController() -> FancyAlertViewController {
+        struct Strings {
+            static let titleText = NSLocalizedString("Try the New Editor", comment: "Title of alert prompting users to try the new Aztec editor")
+            static let bodyText = NSLocalizedString("The WordPress app now includes a beautiful new editor. Try it out by creating a new post!", comment: "Body text of alert prompting users to try the new Aztec editor")
+            static let tryIt = NSLocalizedString("Try It", comment: "Title of the primary button on alert prompting users to try the new Aztec editor")
+            static let notNow = NSLocalizedString("Not Now", comment: "Title of the cancel button on alert prompting users to try the new Aztec editor")
+            static let whatsNew = NSLocalizedString("What's new?", comment: "Title of more info button on alert prompting users to try the new Aztec editor")
+            static let beta = NSLocalizedString("Beta", comment: "Used to indicate a feature of the app currently in beta testing.")
+        }
+
         typealias Button = FancyAlertViewController.Config.ButtonConfig
 
-        let defaultButton = Button("Try It", { controller in
+        let defaultButton = Button(Strings.tryIt, { controller in
             controller.configuration = aztecAnnouncementSuccessConfig
         })
 
-        let cancelButton = Button("Now Now", { controller in
+        let cancelButton = Button(Strings.notNow, { controller in
             controller.dismiss(animated: true, completion: nil)
         })
 
-        let moreInfoButton = Button("What's new?", { _ in })
-
-        let titleAccessoryButton = Button("Beta", { _ in })
+        let moreInfoButton = Button(Strings.whatsNew, { _ in })
+        let titleAccessoryButton = Button(Strings.beta, { _ in })
 
         let image = UIImage(named: "wp-illustration-hand-write")
 
-        let config = FancyAlertViewController.Config(titleText: "Try the New Editor",
-                                                     bodyText: "The WordPress app now includes a beautiful new editor. Try it out by creating a new post!",
+        let config = FancyAlertViewController.Config(titleText: Strings.titleText,
+                                                     bodyText: Strings.bodyText,
                                                      headerImage: image,
                                                      defaultButton: defaultButton, cancelButton: cancelButton, moreInfoButton: moreInfoButton, titleAccessoryButton: titleAccessoryButton)
 
@@ -27,15 +35,24 @@ extension FancyAlertViewController {
     }
 
     private static let aztecAnnouncementSuccessConfig: FancyAlertViewController.Config = {
+        struct Strings {
+            static let titleText = NSLocalizedString("New Editor Enabled!", comment: "Title of alert informing users that the new Aztec editor has been enabled")
+            static let bodyText = NSLocalizedString("Thanks for trying it out! You can switch editor modes at any time in", comment: "Body text of alert informing users that the new Aztec editor has been enabled")
+            static let appSettings = NSLocalizedString("Me > App Settings", comment: "Text for button telling user where to find the App Settings section of the app")
+            static let beta = NSLocalizedString("Beta", comment: "Used to indicate a feature of the app currently in beta testing.")
+        }
+
         typealias Button = FancyAlertViewController.Config.ButtonConfig
 
-        let moreInfoButton = Button("Me > App Settings", { _ in })
+        let moreInfoButton = Button(Strings.appSettings, { _ in })
 
-        let titleAccessoryButton = Button("Beta", { _ in })
+        let titleAccessoryButton = Button(Strings.beta, { _ in })
 
-        return FancyAlertViewController.Config(titleText: "New Editor Enabled!",
-                                               bodyText: "Thanks for trying it out! You can switch editor modes at any time in",
-                                               headerImage: UIImage(named: "wp-illustration-thank-you"),
+        let image = #imageLiteral(resourceName: "wp-illustration-thank-you")
+
+        return FancyAlertViewController.Config(titleText: Strings.titleText,
+                                               bodyText: Strings.bodyText,
+                                               headerImage: image,
                                                defaultButton: nil, cancelButton: nil, moreInfoButton: moreInfoButton, titleAccessoryButton: titleAccessoryButton)
     }()
 }
