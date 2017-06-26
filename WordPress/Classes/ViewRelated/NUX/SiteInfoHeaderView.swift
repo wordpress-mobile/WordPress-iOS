0a9e981a9c5a1c9860d962b80265e738a807d65c
aerych <eric@automattic.com>
2017-06-26 17:34:05 -0500

    Login: Apply login styling to the SiteInfoHeaderView

--- WordPress/Classes/ViewRelated/NUX/SiteInfoHeaderView.swift
+++ WordPress/Classes/ViewRelated/NUX/SiteInfoHeaderView.swift
@@ -1,9 +1,30 @@
-//
-//  SiteInfoHeaderView.swift
-//  WordPress
-//
-//  Created by aerych on 6/26/17.
-//  Copyright © 2017 WordPress. All rights reserved.
-//
+import UIKit
 
-import Foundation
+/// A subclass of BlogDetailHeaderView styled for use in the login flow.
+///
+class SiteInfoHeaderView: BlogDetailHeaderView {
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        configureStyles()
+    }
+
+
+    func configureStyles() {
+
+        titleLabel.font = WPFontManager.systemSemiBoldFont(ofSize: 15.0)
+        titleLabel.sizeToFit()
+        titleLabel.textColor = WPStyleGuide.darkGrey()
+
+        subtitleLabel.font = UIFont.preferredFont(forTextStyle: .footnote)
+        subtitleLabel.sizeToFit()
+        subtitleLabel.textColor = WPStyleGuide.darkGrey()
+
+        blavatarImageView.layer.borderColor = WPStyleGuide.greyLighten20().cgColor
+        blavatarImageView.layer.borderWidth = 1
+        blavatarImageView.tintColor = WPStyleGuide.greyLighten10()
+    }
+
+}
