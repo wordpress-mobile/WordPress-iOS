f5a5dd949308343d87c3b317bff89eac4b36a83b
James Frost <james.frost@automattic.com>
2017-06-30 23:06:37 +0100

    Aztec Announcement: Hook up buttons to What's New webview

--- WordPress/Classes/ViewRelated/System/FancyAlerts+AztecAnnouncement.swift
+++ WordPress/Classes/ViewRelated/System/FancyAlerts+AztecAnnouncement.swift
@@ -1,5 +1,8 @@
 import UIKit
 
+let AztecAnnouncementWhatsNewURL = URL(string: "https://make.wordpress.org/mobile/whats-new-in-beta-ios-editor/")
+
+
 extension FancyAlertViewController {
     private enum Constants {
         static let successAnimationTransform: CGAffineTransform = {
@@ -12,6 +15,14 @@ extension FancyAlertViewController {
         }()
         static let successAnimationDuration: TimeInterval = 0.8
         static let successAnimationDampingRaio: CGFloat = 0.6
+    }
+
+    private func presentWhatsNewWebView() {
+        guard let webViewController = WPWebViewController(url: AztecAnnouncementWhatsNewURL) else { return }
+
+        let navigationController = UINavigationController(rootViewController: webViewController)
+
+        present(navigationController, animated: true, completion: nil)
     }
 
     static func aztecAnnouncementController() -> FancyAlertViewController {
@@ -50,8 +61,13 @@ extension FancyAlertViewController {
             controller.dismiss(animated: true, completion: nil)
         })
 
-        let moreInfoButton = Button(Strings.whatsNew, { _ in })
-        let titleAccessoryButton = Button(Strings.beta, { _ in })
+        let moreInfoButton = Button(Strings.whatsNew, { controller in
+            controller.presentWhatsNewWebView()
+        })
+
+        let titleAccessoryButton = Button(Strings.beta, { controller in
+            controller.presentWhatsNewWebView()
+        })
 
         let image = UIImage(named: "wp-illustration-hand-write")
 
@@ -75,7 +91,9 @@ extension FancyAlertViewController {
 
         let moreInfoButton = Button(Strings.appSettings, { _ in })
 
-        let titleAccessoryButton = Button(Strings.beta, { _ in })
+        let titleAccessoryButton = Button(Strings.beta, { controller in
+            controller.presentWhatsNewWebView()
+        })
 
         let image = UIImage(named: "wp-illustration-thank-you")
 
