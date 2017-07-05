e2fd958869a80b29099b15a121fc8e15dfb759e3
kurzee <brent@kurzee.com>
2017-07-05 12:36:28 -0500

    Media: support exporting remote video thumbnails.

--- WordPress/Classes/Utility/Media/MediaVideoExporter.swift
+++ WordPress/Classes/Utility/Media/MediaVideoExporter.swift
@@ -120,7 +120,7 @@ class MediaVideoExporter: MediaExporter {
         }
     }
 
-    /// Generate and export a preview image for a known video at the URL.
+    /// Generate and export a preview image for a known video at the URL, local file or remote resource.
     ///
     /// - Note: Generates the image asynchronously and could potentially take a bit.
     ///
