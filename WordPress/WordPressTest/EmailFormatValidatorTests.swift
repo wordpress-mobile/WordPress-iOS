2116ed5870c629133dd6bb8a8ac967b7c7421310
aerych <eric@automattic.com>
2017-07-07 12:00:03 -0500

    Tweak method signature

--- WordPress/WordPressTest/EmailFormatValidatorTests.swift
+++ WordPress/WordPressTest/EmailFormatValidatorTests.swift
@@ -4,28 +4,28 @@ import XCTest
 class EmailFormatValidatorTests: XCTestCase {
 
     func testValidEmailAddresses() {
-        XCTAssertTrue(EmailFormatValidator.validate(str: "e@example.com"))
-        XCTAssertTrue(EmailFormatValidator.validate(str: "example@example.com"))
-        XCTAssertTrue(EmailFormatValidator.validate(str: "example@example-example.com"))
-        XCTAssertTrue(EmailFormatValidator.validate(str: "example@example.example.example.com"))
-        XCTAssertTrue(EmailFormatValidator.validate(str: "example.example+example@example.com"))
+        XCTAssertTrue(EmailFormatValidator.validate(string: "e@example.com"))
+        XCTAssertTrue(EmailFormatValidator.validate(string: "example@example.com"))
+        XCTAssertTrue(EmailFormatValidator.validate(string: "example@example-example.com"))
+        XCTAssertTrue(EmailFormatValidator.validate(string: "example@example.example.example.com"))
+        XCTAssertTrue(EmailFormatValidator.validate(string: "example.example+example@example.com"))
     }
 
     func testInvalidEmailAddresses() {
-        XCTAssertFalse(EmailFormatValidator.validate(str: ""))
-        XCTAssertFalse(EmailFormatValidator.validate(str: "example"))
-        XCTAssertFalse(EmailFormatValidator.validate(str: "example@@example.com"))
-        XCTAssertFalse(EmailFormatValidator.validate(str: "example@example@.com"))
-        XCTAssertFalse(EmailFormatValidator.validate(str: "@example.com"))
-        XCTAssertFalse(EmailFormatValidator.validate(str: "example@example"))
-        XCTAssertFalse(EmailFormatValidator.validate(str: "example@.com"))
-        XCTAssertFalse(EmailFormatValidator.validate(str: "example@example..com"))
-        XCTAssertFalse(EmailFormatValidator.validate(str: "example@.example.com"))
-        XCTAssertFalse(EmailFormatValidator.validate(str: "example@example.com."))
-        XCTAssertFalse(EmailFormatValidator.validate(str: "example@examp?.com"))
-        XCTAssertFalse(EmailFormatValidator.validate(str: "example@exam_ple.com"))
-        XCTAssertFalse(EmailFormatValidator.validate(str: "examp***le@exam_ple.com"))
-        XCTAssertFalse(EmailFormatValidator.validate(str: "example@exam ple.com"))
+        XCTAssertFalse(EmailFormatValidator.validate(string: ""))
+        XCTAssertFalse(EmailFormatValidator.validate(string: "example"))
+        XCTAssertFalse(EmailFormatValidator.validate(string: "example@@example.com"))
+        XCTAssertFalse(EmailFormatValidator.validate(string: "example@example@.com"))
+        XCTAssertFalse(EmailFormatValidator.validate(string: "@example.com"))
+        XCTAssertFalse(EmailFormatValidator.validate(string: "example@example"))
+        XCTAssertFalse(EmailFormatValidator.validate(string: "example@.com"))
+        XCTAssertFalse(EmailFormatValidator.validate(string: "example@example..com"))
+        XCTAssertFalse(EmailFormatValidator.validate(string: "example@.example.com"))
+        XCTAssertFalse(EmailFormatValidator.validate(string: "example@example.com."))
+        XCTAssertFalse(EmailFormatValidator.validate(string: "example@examp?.com"))
+        XCTAssertFalse(EmailFormatValidator.validate(string: "example@exam_ple.com"))
+        XCTAssertFalse(EmailFormatValidator.validate(string: "examp***le@exam_ple.com"))
+        XCTAssertFalse(EmailFormatValidator.validate(string: "example@exam ple.com"))
     }
 
 }
