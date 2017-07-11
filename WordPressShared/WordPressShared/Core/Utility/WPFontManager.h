705e88fb43584f2a3637b628bce5d9f8fe4f34e6
Nate Heagy <nate@heagy.com>
2017-07-11 15:53:07 -0600

    Add documentation to the WPFontManager that they will not work with dynamic type

--- WordPressShared/WordPressShared/Core/Utility/WPFontManager.h
+++ WordPressShared/WordPressShared/Core/Utility/WPFontManager.h
@@ -5,10 +5,52 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface WPFontManager : NSObject
 
+/// Uses the system fonts of the specified size.
+/// As Apple says, "Think carefully before using these methods" as they do not respect the user's selected content size category.
+
+/**
+ Returns the system light font of the specified size.
+ 
+ @param size CGFloat specifying the size of font to return
+
+ @warning Font will not respect the user's selected content size category. Try WPStyleGuide.fontForTextStyle instead
+ */
 + (UIFont *)systemLightFontOfSize:(CGFloat)size;
+
+/**
+ Returns the system italic font of the specified size.
+
+ @param size CGFloat specifying the size of font to return
+
+ @warning Font will not respect the user's selected content size category. Try WPStyleGuide.fontForTextStyle instead
+ */
 + (UIFont *)systemItalicFontOfSize:(CGFloat)size;
+
+/**
+ Returns the system bold font of the specified size.
+
+ @param size CGFloat specifying the size of font to return
+
+ @warning Font will not respect the user's selected content size category. Try WPStyleGuide.fontForTextStyle instead
+ */
 + (UIFont *)systemBoldFontOfSize:(CGFloat)size;
+
+/**
+ Returns the system semibold font of the specified size.
+
+ @param size CGFloat specifying the size of font to return
+
+ @warning Font will not respect the user's selected content size category. Try WPStyleGuide.fontForTextStyle instead
+ */
 + (UIFont *)systemSemiBoldFontOfSize:(CGFloat)size;
+
+/**
+ Returns the system regular font of the specified size.
+
+ @param size CGFloat specifying the size of font to return
+
+ @warning Font will not respect the user's selected content size category. Try WPStyleGuide.fontForTextStyle instead
+ */
 + (UIFont *)systemRegularFontOfSize:(CGFloat)size;
 
 /// Loads the Noto font family for the life of the current process.
