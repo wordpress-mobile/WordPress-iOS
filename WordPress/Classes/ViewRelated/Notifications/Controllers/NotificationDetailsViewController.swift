ba8abbfd10013540c316393765e749cec774ae5f
Aaron Douglas <astralbodies@gmail.com>
2017-07-13 07:36:48 -0500

    Derrrrrp

--- WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationDetailsViewController.swift
+++ WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationDetailsViewController.swift
@@ -380,7 +380,7 @@ extension NotificationDetailsViewController {
     func setupReplyTextView() {
         let replyTextView = ReplyTextView(width: view.frame.width)
         replyTextView.placeholder = NSLocalizedString("Write a reply…", comment: "Placeholder text for inline compose view")
-        replyTextView.replyText = NSLocalizedString("Reply", comment: "").localizedUppercase()
+        replyTextView.replyText = NSLocalizedString("Reply", comment: "").localizedUppercase
         replyTextView.accessibilityIdentifier = NSLocalizedString("Reply Text", comment: "Notifications Reply Accessibility Identifier")
         replyTextView.delegate = self
         replyTextView.onReply = { [weak self] content in
