3c14edb47b504d99add76b9bd0f7fb15412f07a5
Matt Bumgardner <matt.bumgardner@gmail.com>
2017-07-17 15:02:04 -0500

    Fixed a lot of issues with reader remotes migrating to WPKit

---
+++ WordPress/WordPressTest/RichContentFormatterTests.swift
@@ -0,0 +1,56 @@
+import Foundation
+import UIKit
+import XCTest
+import WordPressShared
+@testable import WordPress
+
+class RichContentFormatterTests: XCTestCase {
+
+    func testResizeGalleryImageURLsForContentPublic() {
+        guard
+            let path = Bundle(for: type(of: self)).path(forResource: "gallery-reader-post-public", ofType: "json"),
+            let data = try? Data(contentsOf: URL(fileURLWithPath: path)),
+            let postDict = (try? JSONSerialization.jsonObject(with: data, options: [])) as? NSDictionary,
+            let content = postDict.object(forKey: "content") as? NSString,
+            let window = UIApplication.shared.keyWindow else {
+                XCTFail()
+                return
+        }
+
+        let resultContent = RichContentFormatter.resizeGalleryImageURL(content as String, isPrivateSite: false) as NSString
+        let imageSize = window.frame.size
+        let scale = UIScreen.main.scale
+        let scaledSize = imageSize.applying(CGAffineTransform(scaleX: scale, y: scale))
+
+        // Verify that the image source was updated with a Photon-friendly sized URL
+        let sourceStr = "src=\"https://lanteanartest.files.wordpress.com/2016/07/image217.png?w=1024&#038;h=1365\""
+        XCTAssertTrue(content.contains(sourceStr))
+        XCTAssertFalse(resultContent.contains(sourceStr))
+        let expectedURL = "src=\"https://i0.wp.com/lanteanartest.files.wordpress.com/2016/07/image217.png?quality=80&resize=\(Int(scaledSize.width)),\(Int(scaledSize.height))&ssl=1\""
+        XCTAssertTrue(resultContent.contains(expectedURL))
+    }
+
+    func testResizeGalleryImageURLsForContentPrivate() {
+        guard
+            let path = Bundle(for: type(of: self)).path(forResource: "gallery-reader-post-private", ofType: "json"),
+            let data = try? Data(contentsOf: URL(fileURLWithPath: path)),
+            let postDict = (try? JSONSerialization.jsonObject(with: data, options: [])) as? NSDictionary,
+            let content = postDict.object(forKey: "content") as? NSString,
+            let window = UIApplication.shared.keyWindow else {
+                XCTFail()
+                return
+        }
+
+        let resultContent = RichContentFormatter.resizeGalleryImageURL(content as String, isPrivateSite: true) as NSString
+        let imageSize = window.frame.size
+        let scale = UIScreen.main.scale
+        let scaledSize = imageSize.applying(CGAffineTransform(scaleX: scale, y: scale))
+
+        // Verify that the image source was updated with a Photon-friendly sized URL
+        let sourceStr = "src=\"https://picklessaltyporkvonhausen.files.wordpress.com/2016/07/img_8961.jpg?w=181&#038;h=135&#038;crop=1\""
+        XCTAssertTrue(content.contains(sourceStr))
+        XCTAssertFalse(resultContent.contains(sourceStr))
+        let expectedURL = "src=\"https://picklessaltyporkvonhausen.files.wordpress.com/2016/07/img_8961.jpg?h=\(Int(scaledSize.height))&w=\(Int(scaledSize.width))\""
+        XCTAssertTrue(resultContent.contains(expectedURL))
+    }
+}
--- WordPressShared/WordPressSharedTests/NSStringHelpersTests.m
+++ WordPressShared/WordPressSharedTests/NSStringHelpersTests.m
@@ -186,4 +186,13 @@
     XCTAssertTrue([expectedString isEqualToString:[sourceString stringByNormalizingWhitespace]]);
 }
 
+- (void)testSummaryForContent
+{
+    NSString *content = @"<p>Lorem ipsum dolor sit amet, [shortcode param=\"value\"]consectetur[/shortcode] adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p><p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>";
+    NSString *summary = [NSString summaryFromContent:content];
+    NSString *expectedSummary = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, …";
+
+    XCTAssertTrue([summary isEqualToString:expectedSummary], @"The expected summary was not derived from the content.");
+}
+
 @end
