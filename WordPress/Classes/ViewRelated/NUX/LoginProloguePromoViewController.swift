07de5b82c04b144028aceb6cd5c69708768bbfda
Nate Heagy <nate@heagy.com>
2017-07-24 16:31:19 -0600

    Convert login promo pages UI to be code-based

--- WordPress/Classes/ViewRelated/NUX/LoginProloguePromoViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/LoginProloguePromoViewController.swift
@@ -2,88 +2,141 @@ import UIKit
 import Lottie
 
 class LoginProloguePromoViewController: UIViewController {
-    @IBOutlet var stackView: UIStackView?
-    @IBOutlet var headingLabel: UILabel?
-    @IBOutlet var animationHolder: UIView?
-    @IBInspectable var pageNum: Int = 0
-    fileprivate var animationView: LOTAnimationView?
+    fileprivate let type: PromoType
+    fileprivate let stackView: UIStackView
+    fileprivate let headingLabel: UILabel
+    fileprivate let animationHolder: UIView
+    fileprivate var animationView: LOTAnimationView
+
     fileprivate struct Constants {
         static let stackSpacing: CGFloat = 36.0
         static let stackHeightMultiplier: CGFloat = 0.87
         static let stackWidthMultiplier: CGFloat = 0.8
     }
 
+    enum PromoType: String {
+        case post
+        case stats
+        case reader
+        case notifications
+        case jetpack
+
+        func animationKey() -> String {
+            return self.rawValue
+        }
+    }
+
+    init(as promoType: PromoType) {
+        type = promoType
+        stackView = UIStackView()
+        headingLabel = UILabel()
+        animationHolder = UIView()
+        guard let animation = Lottie.LOTAnimationView(name: type.animationKey()) else {
+            fatalError("animation could not be created for promo screen \(promoType)")
+        }
+        animationView = animation
+
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = UIColor.clear
 
-        headingLabel?.font = WPStyleGuide.mediumWeightFont(forStyle: .title3)
-        headingLabel?.text = headlineText()
-        headingLabel?.sizeToFit()
+        headingLabel.font = WPStyleGuide.mediumWeightFont(forStyle: .title3)
+        headingLabel.textColor = headlineColor()
+        headingLabel.text = headlineText()
+        headingLabel.textAlignment = .center
+        headingLabel.numberOfLines = 0
+        headingLabel.sizeToFit()
 
-        guard let holder = animationHolder,
-            let animation = Lottie.LOTAnimationView(name: animationKey()) else {
-                return
-        }
-
-        stackView?.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: Constants.stackWidthMultiplier).isActive = true
-        stackView?.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        stackView?.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: Constants.stackHeightMultiplier).isActive = true
-        stackView?.spacing = Constants.stackSpacing
-
-        animation.translatesAutoresizingMaskIntoConstraints = false
-        animation.contentMode = .scaleAspectFit
-        holder.addSubview(animation)
-
-        animation.leadingAnchor.constraint(equalTo: holder.leadingAnchor).isActive = true
-        animation.trailingAnchor.constraint(equalTo: holder.trailingAnchor).isActive = true
-        animation.topAnchor.constraint(equalTo: holder.topAnchor).isActive = true
-        animation.bottomAnchor.constraint(equalTo: holder.bottomAnchor).isActive = true
-        animationView = animation
+        setupLayout()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        animationView?.animationProgress = 0.0
+        animationView.animationProgress = 0.0
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        animationView?.play()
+        animationView.play()
     }
 
-    private func animationKey() -> String {
-        switch pageNum {
-        case 1:
-            return "post"
-        case 2:
-            return "stats"
-        case 3:
-            return "reader"
-        case 4:
-            return "notifications"
-        case 5:
-            fallthrough
+
+    // MARK: layout
+
+    private func setupLayout() {
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(headingLabel)
+        stackView.addArrangedSubview(animationHolder)
+        animationHolder.addSubview(animationView)
+
+        stackView.axis = .vertical
+        stackView.spacing = Constants.stackSpacing
+        if self.traitCollection.horizontalSizeClass == .regular {
+            stackView.alignment = .center
+        } else {
+            stackView.alignment = .fill
+        }
+        stackView.distribution = .fill
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        stackView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: Constants.stackWidthMultiplier).isActive = true
+        stackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: Constants.stackHeightMultiplier).isActive = true
+
+        headingLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: 21.0).isActive = true
+        headingLabel.widthAnchor.constraint(lessThanOrEqualToConstant: 600.0).isActive = true
+        headingLabel.translatesAutoresizingMaskIntoConstraints = false
+        headingLabel.setContentHuggingPriority(UILayoutPriorityDefaultLow, for: .vertical)
+        headingLabel.setContentCompressionResistancePriority(UILayoutPriorityRequired, for: .vertical)
+
+        animationHolder.translatesAutoresizingMaskIntoConstraints = false
+        animationHolder.widthAnchor.constraint(greaterThanOrEqualTo: animationHolder.heightAnchor, multiplier: 0.6667)
+        animationHolder.setContentHuggingPriority(UILayoutPriorityDefaultLow, for: .vertical)
+        animationHolder.setContentCompressionResistancePriority(UILayoutPriorityDefaultHigh, for: .vertical)
+
+        animationView.contentMode = .scaleAspectFit
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        animationView.leadingAnchor.constraint(equalTo: animationHolder.leadingAnchor).isActive = true
+        animationView.trailingAnchor.constraint(equalTo: animationHolder.trailingAnchor).isActive = true
+        animationView.topAnchor.constraint(equalTo: animationHolder.topAnchor).isActive = true
+        animationView.bottomAnchor.constraint(equalTo: animationHolder.bottomAnchor).isActive = true
+    }
+
+
+    // MARK: promo settings
+
+    private func headlineColor() -> UIColor {
+        switch type {
+        case .post, .reader, .jetpack:
+            return UIColor(hexString: "204E80")
         default:
-            return "jetpack"
+            return UIColor.white
         }
     }
 
     private func headlineText() -> String {
-        switch pageNum {
-        case 1:
+        switch type {
+        case .post:
             return NSLocalizedString("Publish from the park. Blog from the bus. Comment from the café. WordPress goes where you do.", comment: "shown in promotional screens during first launch")
-        case 2:
+        case .stats:
             return NSLocalizedString("Watch readers from around the world read and interact with your site — in real time.", comment: "shown in promotional screens during first launch")
-        case 3:
+        case .reader:
             return NSLocalizedString("Catch up with your favorite sites and join the conversation anywhere, any time.", comment: "shown in promotional screens during first launch")
-        case 4:
+        case .notifications:
             return NSLocalizedString("Your notifications travel with you — see comments and likes as they happen.", comment: "shown in promotional screens during first launch")
-        case 5:
-            fallthrough
-        default:
+        case .jetpack:
             return NSLocalizedString("Manage your Jetpack-powered site on the go — you‘ve got WordPress in your pocket.", comment: "shown in promotional screens during first launch")
         }
     }
