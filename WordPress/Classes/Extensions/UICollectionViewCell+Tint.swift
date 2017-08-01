f6b74ea386610f12607da102dfde5d852870d139
James Frost <james.frost@automattic.com>
2017-08-01 22:01:45 +0100

    Added `setCellTintColor` to UICollectionViewCell to set tint with UIAppearance
    * Fixes a bug caused by using non UI_APPEARANCE_SELECTOR `setTintColor`

---
+++ WordPress/Classes/Extensions/UICollectionViewCell+Tint.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+extension UICollectionViewCell {
+    // Allows cell tint color to be set via UIAppearance
+    func setCellTintColor(_ color: UIColor) {
+        tintColor = color
+    }
+}
