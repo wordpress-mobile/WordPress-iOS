5f031b30713cccad2cd38fc8eb593cb1f81ce32c
Jorge Bernal <jbernal@gmail.com>
2017-08-08 17:04:08 +0200

    Translation comment improvements

--- WordPressKit/WordPressKit/PluginState.swift
+++ WordPressKit/WordPressKit/PluginState.swift
@@ -13,11 +13,11 @@ public extension PluginState {
     var stateDescription: String {
         switch (active, autoupdate) {
         case (false, _):
-            return NSLocalizedString("Inactive", comment: "A plugin is not active on the site")
+            return NSLocalizedString("Inactive", comment: "The plugin is not active on the site")
         case (true, false):
-            return NSLocalizedString("Active, Autoupdates off", comment: "A plugin is active on the site and has not enabled automatic updates")
+            return NSLocalizedString("Active, Autoupdates off", comment: "The plugin is active on the site and has not enabled automatic updates")
         case (true, true):
-            return NSLocalizedString("Active, Autoupdates on", comment: "A plugin is active on the site and has enabled automatic updates")
+            return NSLocalizedString("Active, Autoupdates on", comment: "The plugin is active on the site and has enabled automatic updates")
         }
     }
 }
