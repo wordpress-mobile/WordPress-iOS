3ef6d4699a91e629700caf2c0aac049d6ba7fd39
Jorge Bernal <jbernal@gmail.com>
2017-08-08 17:03:52 +0200

    Better code organization in PluginListViewController

--- WordPress/Classes/ViewRelated/Plugins/PluginListViewController.swift
+++ WordPress/Classes/ViewRelated/Plugins/PluginListViewController.swift
@@ -2,9 +2,13 @@ import UIKit
 import WordPressKit
 
 class PluginListViewController: UITableViewController, ImmuTablePresenter {
+    let siteID: Int
+    let service: PluginServiceRemote
+
     fileprivate lazy var handler: ImmuTableViewHandler = {
         return ImmuTableViewHandler(takeOver: self)
     }()
+
     fileprivate var viewModel: PluginListViewModel = .loading {
         didSet {
             handler.viewModel = viewModel.tableViewModelWithPresenter(self)
@@ -13,6 +17,46 @@ class PluginListViewController: UITableViewController, ImmuTablePresenter {
     }
 
     fileprivate let noResultsView = WPNoResultsView()
+
+    init(siteID: Int, service: PluginServiceRemote) {
+        self.siteID = siteID
+        self.service = service
+        super.init(style: .grouped)
+        title = NSLocalizedString("Plugins", comment: "Title for the plugin manager")
+        noResultsView.delegate = self
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    convenience init?(blog: Blog) {
+        precondition(blog.dotComID != nil)
+        guard let api = blog.wordPressComRestApi(),
+            let service = PluginServiceRemote(wordPressComRestApi: api) else {
+                return nil
+        }
+
+        self.init(siteID: Int(blog.dotComID!), service: service)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        WPStyleGuide.configureColors(for: view, andTableView: tableView)
+        ImmuTable.registerRows([PluginListRow.self], tableView: tableView)
+        handler.viewModel = viewModel.tableViewModelWithPresenter(self)
+        updateNoResults()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        service.getPlugins(siteID: siteID, success: { result in
+            self.viewModel = .ready(result)
+        }, failure: { error in
+            self.viewModel = .error(String(describing: error))
+        })
+    }
 
     func updateNoResults() {
         if let noResultsViewModel = viewModel.noResultsViewModel {
@@ -34,49 +78,6 @@ class PluginListViewController: UITableViewController, ImmuTablePresenter {
     func hideNoResults() {
         noResultsView.removeFromSuperview()
     }
-
-    convenience init?(blog: Blog) {
-        precondition(blog.dotComID != nil)
-        guard let api = blog.wordPressComRestApi(),
-            let service = PluginServiceRemote(wordPressComRestApi: api) else {
-            return nil
-        }
-
-        self.init(siteID: Int(blog.dotComID!), service: service)
-    }
-
-    let siteID: Int
-    let service: PluginServiceRemote
-    init(siteID: Int, service: PluginServiceRemote) {
-        self.siteID = siteID
-        self.service = service
-        super.init(style: .grouped)
-        title = NSLocalizedString("Plugins", comment: "Title for the plugin manager")
-        noResultsView.delegate = self
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        WPStyleGuide.configureColors(for: view, andTableView: tableView)
-        ImmuTable.registerRows([PluginListRow.self], tableView: tableView)
-        handler.viewModel = viewModel.tableViewModelWithPresenter(self)
-        updateNoResults()
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-
-        service.getPlugins(siteID: siteID, success: { result in
-            self.viewModel = .ready(result)
-        }, failure: { error in
-            self.viewModel = .error(String(describing: error))
-        })
-    }
-
 }
 
 // MARK: - WPNoResultsViewDelegate
