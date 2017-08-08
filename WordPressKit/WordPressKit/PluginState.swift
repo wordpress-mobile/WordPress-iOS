192ba2e01601f23aaa87a5617cdc154e83b86974
Jorge Bernal <jbernal@gmail.com>
2017-08-08 17:05:09 +0200

    Include autoupdate state for inactive plugins description

--- WordPressKit/WordPressKit/PluginState.swift
+++ WordPressKit/WordPressKit/PluginState.swift
@@ -12,8 +12,10 @@ public struct PluginState {
 public extension PluginState {
     var stateDescription: String {
         switch (active, autoupdate) {
-        case (false, _):
-            return NSLocalizedString("Inactive", comment: "The plugin is not active on the site")
+        case (false, false):
+            return NSLocalizedString("Inactive, Autoupdates off", comment: "The plugin is not active on the site and has not enabled automatic updates")
+        case (false, true):
+            return NSLocalizedString("Inactive, Autoupdates on", comment: "The plugin is not active on the site and has enabled automatic updates")
         case (true, false):
             return NSLocalizedString("Active, Autoupdates off", comment: "The plugin is active on the site and has not enabled automatic updates")
         case (true, true):
