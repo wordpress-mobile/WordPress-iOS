eb367f6bf4112317bab21bd17a2f74037deb34ae
Diego Rey Mendez <diegoreymendez@gmail.com>
2017-08-08 11:00:23 -0300

    Adds methods for pattern matching replacements.

--- WordPress/Classes/Extensions/String+RegEx.swift
+++ WordPress/Classes/Extensions/String+RegEx.swift
@@ -7,18 +7,40 @@ extension String {
 
     /// Replaces all matches of a given RegEx, with a template String.
     ///
-    func replaceMatches(of regex: String, with template: String, options: NSRegularExpression.Options) -> String {
-        do {
-            let regex = try NSRegularExpression(pattern: regex, options: options)
-            let range = NSRange(location: 0, length: characters.count)
-            return regex.stringByReplacingMatches(in: self,
-                                                  options: [],
-                                                  range: range,
-                                                  withTemplate: template)
-        } catch {
-            assertionFailure()
+    func replaceMatches(of regex: String, with template: String, options: NSRegularExpression.Options = []) -> String {
+
+        let regex = try! NSRegularExpression(pattern: regex, options: options)
+        let fullRange = NSRange(location: 0, length: characters.count)
+
+        return regex.stringByReplacingMatches(in: self,
+                                              options: [],
+                                              range: fullRange,
+                                              withTemplate: template)
+    }
+
+    /// Replaces all matches of a given RegEx using a provided block.
+    ///
+    /// - Parameters:
+    ///     - regex: the regex to use for pattern matching.
+    ///     - options: the regex options.
+    ///     - block: the block that will be used for the replacement logic.
+    ///
+    /// - Returns: the new string.
+    ///
+    func replaceMatches(of regex: String, with options: NSRegularExpression.Options = [], using block: (String) -> String) -> String {
+
+        let regex = try! NSRegularExpression(pattern: regex, options: options)
+        let fullRange = NSRange(location: 0, length: characters.count)
+        let matches = regex.matches(in: self, options: [], range: fullRange)
+        var newString = self
+
+        for match in matches.reversed() {
+            let matchRange = range(from: match.range)
+            let matchString = substring(with: matchRange)
+
+            newString.replaceSubrange(matchRange, with: block(matchString))
         }
 
-        return self
+        return newString
     }
 }
