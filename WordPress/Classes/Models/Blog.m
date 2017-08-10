8fb8b0af903a4d03fc8b2319fe4e1ec7169bf05f
elibud <elibud@gmail.com>
2017-08-10 08:36:17 -0300

    Bury support for Premium Themes logic in Blog

--- WordPress/Classes/Models/Blog.m
+++ WordPress/Classes/Models/Blog.m
@@ -15,6 +15,8 @@ static NSInteger const ImageSizeMediumWidth = 480;
 static NSInteger const ImageSizeMediumHeight = 360;
 static NSInteger const ImageSizeLargeWidth = 640;
 static NSInteger const ImageSizeLargeHeight = 480;
+static NSInteger const JetpackProfessionalYearlyPlanId = 2004;
+static NSInteger const JetpackProfessionalMonthlyPlanId = 2001;
 
 NSString * const BlogEntityName = @"Blog";
 NSString * const PostFormatStandard = @"standard";
@@ -22,7 +24,6 @@ NSString * const ActiveModulesKeyPublicize = @"publicize";
 NSString * const ActiveModulesKeySharingButtons = @"sharedaddy";
 NSString * const OptionsKeyActiveModules = @"active_modules";
 NSString * const OptionsKeyPublicizeDisabled = @"publicize_permanently_disabled";
-
 
 @interface Blog ()
 
@@ -457,6 +458,9 @@ NSString * const OptionsKeyPublicizeDisabled = @"publicize_permanently_disabled"
             return [self supportsRestApi] && [self isAdmin];
         case BlogFeatureCustomThemes:
             return [self supportsRestApi] && [self isAdmin] && ![self isHostedAtWPcom];
+        case BlogFeaturePremiumThemes:
+            return [self supports:BlogFeatureCustomThemes] && (self.planID.integerValue == JetpackProfessionalYearlyPlanId
+                                                               || self.planID.integerValue == JetpackProfessionalMonthlyPlanId);
         case BlogFeatureMenus:
             return [self supportsRestApi] && [self isAdmin];
         case BlogFeaturePrivate:
