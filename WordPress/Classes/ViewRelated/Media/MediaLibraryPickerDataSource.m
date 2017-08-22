a701ab27ac571c601441cd9960dcb10f712e56c3
James Frost <james.frost@automattic.com>
2017-08-22 14:45:53 +0100

    Media library data source: Fix a crash if group view hasn't been loaded

--- WordPress/Classes/ViewRelated/Media/MediaLibraryPickerDataSource.m
+++ WordPress/Classes/ViewRelated/Media/MediaLibraryPickerDataSource.m
@@ -166,7 +166,9 @@
 
 -(void)unregisterChangeObserver:(id<NSObject>)blockKey
 {
-    [self.observers removeObjectForKey:blockKey];
+    if (blockKey) {
+        [self.observers removeObjectForKey:blockKey];
+    }
 }
 
 - (void)addImage:(UIImage *)image
