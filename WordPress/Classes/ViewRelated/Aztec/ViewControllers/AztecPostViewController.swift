c5f50cd06ac341fc0ef7945cd9ed2fce6aa7b16d
Jorge Leandro Perez <jorge@lantean.co>
2017-08-28 14:20:56 -0300

    AztecPostViewController: Wiring Header + List Accesibility Identifiers

--- WordPress/Classes/ViewRelated/Aztec/ViewControllers/AztecPostViewController.swift
+++ WordPress/Classes/ViewRelated/Aztec/ViewControllers/AztecPostViewController.swift
@@ -1492,8 +1492,11 @@ extension AztecPostViewController {
     }
 
     func toggleList(fromItem item: FormatBarItem) {
-        let listOptions = Constants.lists.map { (listType) -> OptionsTableViewOption in
-            return OptionsTableViewOption(image: listType.iconImage, title: NSAttributedString(string: listType.description, attributes: [:]))
+        let listOptions = Constants.lists.map { listType -> OptionsTableViewOption in
+            let title = NSAttributedString(string: listType.description, attributes: [:])
+            return OptionsTableViewOption(image: listType.iconImage,
+                                          title: title,
+                                          accessibilityIdentifier: listType.accessibilityIdentifier)
         }
 
         var index: Int? = nil
@@ -1827,11 +1830,17 @@ extension AztecPostViewController {
     func toggleHeader(fromItem item: FormatBarItem) {
         trackFormatBarAnalytics(stat: .editorTappedHeader)
 
-        let headerOptions = Constants.headers.map { (headerType) -> OptionsTableViewOption in
+        let headerOptions = Constants.headers.map { headerType -> OptionsTableViewOption in
+            let attributes = [
+                NSFontAttributeName: UIFont.systemFont(ofSize: headerType.fontSize),
+                NSForegroundColorAttributeName: WPStyleGuide.darkGrey()
+            ]
+
+            let title = NSAttributedString(string: headerType.description, attributes: attributes)
+
             return OptionsTableViewOption(image: headerType.iconImage,
-                                          title: NSAttributedString(string: headerType.description,
-                                                                    attributes:[NSFontAttributeName: UIFont.systemFont(ofSize: headerType.fontSize),
-                                                                                NSForegroundColorAttributeName: WPStyleGuide.darkGrey()]))
+                                          title: title,
+                                          accessibilityIdentifier: headerType.accessibilityIdentifier)
         }
 
         let selectedIndex = Constants.headers.index(of: self.headerLevelForSelectedText())
