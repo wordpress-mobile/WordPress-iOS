fd9c60d5947a6b79c7cabf60499e86fd479eba18
elibud <elibud@gmail.com>
2017-08-30 15:58:43 -0300

    Remove calls to context.perform

--- WordPress/Classes/Services/BlogJetpackSettingsService.swift
+++ WordPress/Classes/Services/BlogJetpackSettingsService.swift
@@ -13,66 +13,62 @@ struct BlogJetpackSettingsService {
     /// Sync ALL the Jetpack settings for a blog
     ///
     func syncJetpackSettingsForBlog(_ blog: Blog, success: @escaping () -> Void, failure: @escaping (Error?) -> Void) {
-
         guard blog.supports(.jetpackSettings) else {
             success()
             return
         }
+        guard let blogInContext = self.context.object(with: blog.objectID) as? Blog,
+            let remote = BlogJetpackSettingsServiceRemote(wordPressComRestApi: blog.wordPressComRestApi()),
+            let blogDotComId = blogInContext.dotComID as? Int,
+            let blogSettings = blogInContext.settings else {
+                success()
+                return
+        }
 
-        context.perform {
-            guard let blogInContext = self.context.object(with: blog.objectID) as? Blog,
-                let remote = BlogJetpackSettingsServiceRemote(wordPressComRestApi: blog.wordPressComRestApi()),
-                let blogDotComId = blogInContext.dotComID as? Int,
-                let blogSettings = blogInContext.settings else {
-                    success()
+        var fetchError: Error? = nil
+        var remoteJetpackSettings: RemoteBlogJetpackSettings? = nil
+        var remoteJetpackMonitorSettings: RemoteBlogJetpackMonitorSettings? = nil
+
+        // Create a dispatch group to wait for both calls.
+        let syncGroup = DispatchGroup()
+
+        syncGroup.enter()
+        remote.getJetpackSettingsForSite(blogDotComId,
+                                         success: { (remoteSettings) in
+                                             remoteJetpackSettings = remoteSettings
+                                             syncGroup.leave()
+                                         },
+                                         failure: { (error) in
+                                             fetchError = error
+                                             syncGroup.leave()
+                                         })
+
+        syncGroup.enter()
+        remote.getJetpackMonitorSettingsForSite(blogDotComId,
+                                                success: { (remoteMonitorSettings) in
+                                                    remoteJetpackMonitorSettings = remoteMonitorSettings
+                                                    syncGroup.leave()
+                                                },
+                                                failure: { (error) in
+                                                    fetchError = error
+                                                    syncGroup.leave()
+                                                })
+
+        syncGroup.notify(queue: DispatchQueue.main, execute: {
+            guard let remoteJetpackSettings = remoteJetpackSettings,
+                let remoteJetpackMonitorSettings = remoteJetpackMonitorSettings else {
+                    failure(fetchError)
                     return
             }
-
-            var fetchError: Error? = nil
-            var remoteJetpackSettings: RemoteBlogJetpackSettings? = nil
-            var remoteJetpackMonitorSettings: RemoteBlogJetpackMonitorSettings? = nil
-
-            // Create a dispatch group to wait for both calls.
-            let syncGroup = DispatchGroup()
-
-            syncGroup.enter()
-            remote.getJetpackSettingsForSite(blogDotComId,
-                                             success: { (remoteSettings) in
-                                                remoteJetpackSettings = remoteSettings
-                                                syncGroup.leave()
-                                             }, failure: { (error) in
-                                                fetchError = error
-                                                syncGroup.leave()
-                                             })
-
-            syncGroup.enter()
-            remote.getJetpackMonitorSettingsForSite(blogDotComId,
-                                                    success: { (remoteMonitorSettings) in
-                                                        remoteJetpackMonitorSettings = remoteMonitorSettings
-                                                        syncGroup.leave()
-                                                    }, failure: { (error) in
-                                                        fetchError = error
-                                                        syncGroup.leave()
-                                                    })
-
-            syncGroup.notify(queue: DispatchQueue.main, execute: {
-                guard let remoteJetpackSettings = remoteJetpackSettings,
-                    let remoteJetpackMonitorSettings = remoteJetpackMonitorSettings else {
-                        failure(fetchError)
-                        return
-                }
-                self.context.perform {
-                    self.updateJetpackSettings(blogSettings, remoteSettings: remoteJetpackSettings)
-                    self.updateJetpackMonitorSettings(blogSettings, remoteSettings: remoteJetpackMonitorSettings)
-                }
-                do {
-                    try self.context.save()
-                    success()
-                } catch let error as NSError {
-                    failure(error)
-                }
-            })
-        }
+            self.updateJetpackSettings(blogSettings, remoteSettings: remoteJetpackSettings)
+            self.updateJetpackMonitorSettings(blogSettings, remoteSettings: remoteJetpackMonitorSettings)
+            do {
+                try self.context.save()
+                success()
+            } catch let error as NSError {
+                failure(error)
+            }
+        })
     }
 
     // Jetpack settings have to be updated one by one because the API does not allow us to do it in any other way.
@@ -127,58 +123,53 @@ struct BlogJetpackSettingsService {
     }
 
     func updateJetpackMonitorSettinsForBlog(_ blog: Blog, success: @escaping () -> Void, failure: @escaping (Error?) -> Void) {
-        context.perform {
-
-            guard let blogInContext = self.context.object(with: blog.objectID) as? Blog,
-                let remote = BlogJetpackSettingsServiceRemote(wordPressComRestApi: blog.wordPressComRestApi()),
-                let blogDotComId = blogInContext.dotComID as? Int else {
-                    failure(nil)
-                    return
-            }
-
-            remote.updateJetpackMonitorSettingsForSite(blogDotComId,
-                                                       settings: self.jetpackMonitorsSettingsRemote(blogInContext.settings!),
-                                                       success: { () in
-                                                           do {
-                                                               try self.context.save()
-                                                               success()
-                                                           } catch let error as NSError {
-                                                               failure(error)
-                                                           }
-                                                       }, failure: { (error) in
-                                                           failure(error)
-                                                       })
+        guard let blogInContext = self.context.object(with: blog.objectID) as? Blog,
+            let remote = BlogJetpackSettingsServiceRemote(wordPressComRestApi: blog.wordPressComRestApi()),
+            let blogDotComId = blogInContext.dotComID as? Int else {
+                failure(nil)
+                return
         }
-    }
 
+        remote.updateJetpackMonitorSettingsForSite(blogDotComId,
+                                                   settings: self.jetpackMonitorsSettingsRemote(blogInContext.settings!),
+                                                   success: { () in
+                                                       do {
+                                                           try self.context.save()
+                                                           success()
+                                                       } catch let error as NSError {
+                                                           failure(error)
+                                                       }
+                                                   },
+                                                   failure: { (error) in
+                                                       failure(error)
+                                                   })
+    }
 }
 
 private extension BlogJetpackSettingsService {
 
     func updateJetpackSettingForBlog(_ blog: Blog, key: String, value: AnyObject, success: @escaping () -> Void, failure: @escaping (Error?) -> Void) {
-        context.perform {
-
-            guard let blogInContext = self.context.object(with: blog.objectID) as? Blog,
-                let remote = BlogJetpackSettingsServiceRemote(wordPressComRestApi: blog.wordPressComRestApi()),
-                let blogDotComId = blogInContext.dotComID as? Int else {
-                    failure(nil)
-                    return
-            }
-
-            remote.updateJetpackSetting(blogDotComId,
-                                        key: key,
-                                        value: value,
-                                        success: { () in
-                                            do {
-                                                try self.context.save()
-                                                success()
-                                            } catch let error as NSError {
-                                                failure(error)
-                                            }
-                                        }, failure: { (error) in
-                                            failure(error)
-                                        })
+        guard let blogInContext = self.context.object(with: blog.objectID) as? Blog,
+            let remote = BlogJetpackSettingsServiceRemote(wordPressComRestApi: blog.wordPressComRestApi()),
+            let blogDotComId = blogInContext.dotComID as? Int else {
+                failure(nil)
+                return
         }
+
+        remote.updateJetpackSetting(blogDotComId,
+                                    key: key,
+                                    value: value,
+                                    success: { () in
+                                        do {
+                                            try self.context.save()
+                                            success()
+                                        } catch let error as NSError {
+                                            failure(error)
+                                        }
+                                    },
+                                    failure: { (error) in
+                                        failure(error)
+                                    })
     }
 
     func updateJetpackSettings(_ settings: BlogSettings, remoteSettings: RemoteBlogJetpackSettings) {
