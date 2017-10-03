5e3ff75f2583683dd1db91fb17c0d7b65a1d60dd
Jorge Bernal <jbernal@gmail.com>
2017-10-03 14:08:55 +0200

    Include cookie logic in WebViewAuthenticatorTests

--- WordPress/WordPressTest/WebViewAuthenticatorTests.swift
+++ WordPress/WordPressTest/WebViewAuthenticatorTests.swift
@@ -9,6 +9,17 @@ private extension URLRequest {
     }
 }
 
+private class MockCookieJar: CookieJar {
+    let implementation: (URL, String) -> Bool
+    init(_ implementation: @escaping (URL, String) -> Bool) {
+        self.implementation = implementation
+    }
+
+    func hasCookie(url: URL, username: String, completion: @escaping (Bool) -> Void) {
+        completion(implementation(url, username))
+    }
+}
+
 class WebViewAuthenticatorTests: XCTestCase {
     let dotComLoginURL = URL(string: "https://wordpress.com/wp-login.php")!
     let dotComUser = "comuser"
@@ -26,10 +37,40 @@ class WebViewAuthenticatorTests: XCTestCase {
         return WebViewAuthenticator(credentials: .siteLogin(siteLoginURL, siteUser, sitePassword))
     }
 
-    func testAuthenticatedDotComRequest() {
+    func testAuthenticatedSiteRequestWithoutCookie() {
+        let url = URL(string: "http://example.com/some-page/?preview=true&preview_nonce=7ad6fc")!
+        let authenticator = siteAuthenticator
+
+        let cookieJar = MockCookieJar({ (url, username) in
+            return false
+        })
+        var authenticatedRequest: URLRequest? = nil
+        authenticator.request(url: url, cookieJar: cookieJar) {
+            authenticatedRequest = $0
+        }
+        guard let request = authenticatedRequest else {
+            XCTFail("The authenticator should return a valid request")
+            return
+        }
+
+        XCTAssertEqual(request.url, siteLoginURL)
+        XCTAssertEqual(request.value(forHTTPHeaderField: "Content-Type"), "application/x-www-form-urlencoded")
+        XCTAssertNil(request.value(forHTTPHeaderField: "Authorization"))
+        XCTAssertEqual(request.httpBodyString, "log=\(siteUser)&pwd=\(sitePasswordEncoded)&redirect_to=http%3A%2F%2Fexample.com%2Fsome-page%2F%3Fpreview%3Dtrue%26preview_nonce%3D7ad6fc")
+    }
+
+    func testAuthenticatedDotComRequestWithoutCookie() {
         let url = URL(string: "https://example.wordpress.com/some-page/")!
         let authenticator = dotComAuthenticator
-        guard let request = authenticator.authenticatedRequest(url: url) else {
+
+        let cookieJar = MockCookieJar({ (url, username) in
+            return false
+        })
+        var authenticatedRequest: URLRequest? = nil
+        authenticator.request(url: url, cookieJar: cookieJar) {
+            authenticatedRequest = $0
+        }
+        guard let request = authenticatedRequest else {
             XCTFail("The authenticator should return a valid request")
             return
         }
@@ -39,16 +80,23 @@ class WebViewAuthenticatorTests: XCTestCase {
         XCTAssertEqual(request.httpBodyString, "log=\(dotComUser)&redirect_to=https%3A%2F%2Fexample.wordpress.com%2Fsome-page%2F")
     }
 
-    func testAuthenticatedSiteRequest() {
-        let url = URL(string: "http://example.com/some-page/?preview=true&preview_nonce=7ad6fc")!
-        let authenticator = siteAuthenticator
-        guard let request = authenticator.authenticatedRequest(url: url) else {
+    func testUnauthenticatedDotComRequestWithCookie() {
+        let url = URL(string: "https://example.wordpress.com/some-page/")!
+        let authenticator = dotComAuthenticator
+
+        let cookieJar = MockCookieJar({ (url, username) in
+            return true
+        })
+        var authenticatedRequest: URLRequest? = nil
+        authenticator.request(url: url, cookieJar: cookieJar) {
+            authenticatedRequest = $0
+        }
+        guard let request = authenticatedRequest else {
             XCTFail("The authenticator should return a valid request")
             return
         }
-        XCTAssertEqual(request.url, siteLoginURL)
-        XCTAssertEqual(request.value(forHTTPHeaderField: "Content-Type"), "application/x-www-form-urlencoded")
+        XCTAssertEqual(request.url, url)
         XCTAssertNil(request.value(forHTTPHeaderField: "Authorization"))
-        XCTAssertEqual(request.httpBodyString, "log=\(siteUser)&pwd=\(sitePasswordEncoded)&redirect_to=http%3A%2F%2Fexample.com%2Fsome-page%2F%3Fpreview%3Dtrue%26preview_nonce%3D7ad6fc")
     }
+
 }
