ed03e21826bba305cc4f1f23a3955dff15ad5574
Jan Klausa <jan@klausa.pl>
2017-10-04 20:00:26 +0200

    add second state to the verification prompt, handling errors and showing success message

--- WordPress/Classes/ViewRelated/System/FancyAlerts+VerificationPrompt.swift
+++ WordPress/Classes/ViewRelated/System/FancyAlerts+VerificationPrompt.swift
@@ -10,10 +10,8 @@ import UIKit
 
 extension FancyAlertViewController {
 
-    public static func verificationPromptController() -> FancyAlertViewController {
+    public static func verificationPromptController(completion: (() -> Void)?) -> FancyAlertViewController {
         let resendEmailButton = FancyAlertViewController.Config.ButtonConfig(Strings.resendEmail) { controller, button in
-
-            //verify email
 
             let managedObjectContext = ContextManager.sharedInstance().mainContext
             let accountService = AccountService(managedObjectContext: managedObjectContext)
@@ -23,17 +21,16 @@ extension FancyAlertViewController {
             submitButton?.showActivityIndicator(true)
 
             accountService.requestVerificationEmail({ _ in
-                controller.dismiss(animated: true, completion: nil)
-                return
-
+                submitButton?.showActivityIndicator(false)
+                controller.setViewConfiguration(successfullySentVerificationEmailConfig(), animated: true)
             }, failure: { error in
                 submitButton?.showActivityIndicator(false)
+                controller.setViewConfiguration(failureSendingVerificationEmailConfig(), animated: true)
             })
-
-
         }
 
         let defaultButton = FancyAlertViewController.Config.ButtonConfig(Strings.ok) { controller, _ in
+            completion?()
             controller.dismiss(animated: true, completion: nil)
         }
 
@@ -50,19 +47,56 @@ extension FancyAlertViewController {
         return FancyAlertViewController.controllerWithConfiguration(configuration: config)
     }
 
+    private static func successfullySentVerificationEmailConfig() -> FancyAlertViewController.Config {
+        let okButton = FancyAlertViewController.Config.ButtonConfig(Strings.ok) { controller, _ in
+            controller.dismiss(animated: true, completion: nil)
+        }
+
+        return FancyAlertViewController.Config(titleText: Strings.titleText,
+                                               bodyText: Strings.emailSentSuccesfully,
+                                               headerImage: #imageLiteral(resourceName: "wp-illustration-hand-write"),
+                                               dividerPosition: .bottom,
+                                               defaultButton: okButton,
+                                               cancelButton: nil,
+                                               moreInfoButton: nil,
+                                               titleAccessoryButton: nil,
+                                               dismissAction: nil)
+    }
+
+    private static func failureSendingVerificationEmailConfig() -> FancyAlertViewController.Config {
+        let okButton = FancyAlertViewController.Config.ButtonConfig(Strings.ok) { controller, _ in
+            controller.dismiss(animated: true, completion: nil)
+        }
+
+        return FancyAlertViewController.Config(titleText: Strings.titleText,
+                                               bodyText: Strings.emailSendingFailed,
+                                               headerImage: #imageLiteral(resourceName: "wp-illustration-hand-write"),
+                                               dividerPosition: .bottom,
+                                               defaultButton: okButton,
+                                               cancelButton: nil,
+                                               moreInfoButton: nil,
+                                               titleAccessoryButton: nil,
+                                               dismissAction: nil)
+    }
+
     private struct Strings {
         static let titleText = NSLocalizedString("Confirm your email first",
                                                  comment: "Title of alert prompting users to verify their accounts while attempting to publish")
 
-        static let bodyText = NSLocalizedString("You need to verify your account before you can publish a post. Don’t worry, your post is safe and will be saved as a draft.",
+        static let bodyText = NSLocalizedString("You need to verify your account before you can publish a post.\nDon’t worry, your post is safe and will be saved as a draft.",
                                                 comment: "Body of alert prompting users to verify their accounts while attempting to publish")
 
-        static let resendEmail = NSLocalizedString("Resend email",
+        static let resendEmail = NSLocalizedString("Resend",
                                                    comment: "Title of secondary button on alert prompting verify their accounts while attempting to publish")
 
         static let ok = NSLocalizedString("OK",
                                           comment: "Title of primary button on alert prompting verify their accounts while attempting to publish")
 
+        static let emailSentSuccesfully = NSLocalizedString("Verification email sent, check your inbox.",
+                                                 comment: "Message shown when a verification email was re-sent succesfully")
+
+        static let emailSendingFailed = NSLocalizedString("Error sending verification email. Are you already verified?",
+                                                          comment: "Message shown when there was an error trying to re-send email verification")
     }
 
 
