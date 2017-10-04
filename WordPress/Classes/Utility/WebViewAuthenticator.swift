451a57fde84def4623349eab82b0ffa817b8c1af
Jorge Bernal <jbernal@gmail.com>
2017-10-04 13:06:28 +0200

    Adds documentation for WebViewAuthenticator

--- WordPress/Classes/Utility/WebViewAuthenticator.swift
+++ WordPress/Classes/Utility/WebViewAuthenticator.swift
@@ -1,12 +1,27 @@
 import Foundation
 
+/// Encapsulates all the authentication logic for web views.
+///
+/// This objects is in charge of deciding when a web view should be authenticated,
+/// and rewriting requests to do so.
+///
+/// Our current authentication system is based on posting to wp-login.php and
+/// taking advantage of the `redirect_to` parameter. In the specific case of
+/// WordPress.com, we sometimes want to pre-authenticate the user when visiting
+/// URLs that we're not sure if they are hosted there (e.g. mapped domains).
+///
+/// Since WordPress.com doesn't allow redirects to external URLs, we use a
+/// special WordPress.com URL that includes the target URL, and extract that on
+/// `interceptRedirect(request:)`. You should call that from your web view's
+/// delegate method, when deciding if a request or redirect should continue.
+///
 class WebViewAuthenticator: NSObject {
     enum Credentials {
         case dotCom(username: String, authToken: String)
         case siteLogin(loginURL: URL, username: String, password: String)
     }
 
-    let credentials: Credentials
+    fileprivate let credentials: Credentials
 
     init(credentials: Credentials) {
         self.credentials = credentials
@@ -34,17 +49,21 @@ class WebViewAuthenticator: NSObject {
         }
     }
 
-    func authenticatedRequest(url: URL) -> URLRequest? {
-        var request = URLRequest(url: loginURL)
-        request.httpMethod = "POST"
-        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        request.httpBody = body(url: url)
-        if let authToken = authToken {
-            request.setValue("Bearer \(authToken)", forHTTPHeaderField: "Authorization")
-        }
-        return request
-    }
-
+    /// Potentially rewrites a request for authentication.
+    ///
+    /// This method will call the completion block with the request to be used.
+    ///
+    /// - Warning: On WordPress.com, this uses a special redirect system. It
+    /// requires the web view to call `interceptRedirect(request:)` before
+    /// loading any request.
+    ///
+    /// - Parameters:
+    ///     - url: the URL to be loaded.
+    ///     - cookieJar: a CookieJar object where the authenticator will look
+    ///     for existing cookies.
+    ///     - completion: this will be called with either the request for
+    ///     authentication, or a request for the original URL.
+    ///
     func request(url: URL, cookieJar: CookieJar, completion: @escaping (URLRequest) -> Void) {
         cookieJar.hasCookie(url: loginURL, username: username) { [weak self] (hasCookie) in
             guard let authenticator = self else {
@@ -56,6 +75,18 @@ class WebViewAuthenticator: NSObject {
         }
     }
 
+    /// Intercepts and rewrites any potential redirect after login.
+    ///
+    /// This should be called whenever a web view needs to decide if it should
+    /// load a request. If this returns a non-nil value, the resulting request
+    /// should be loaded instead.
+    ///
+    /// - Parameters:
+    ///     - request: the request that was going to be loaded by the web view.
+    ///
+    /// - Returns: a request to be loaded instead. If `nil`, the original
+    /// request should continue loading.
+    ///
     func interceptRedirect(request: URLRequest) -> URLRequest? {
         guard let url = request.url,
             let components = URLComponents(url: url, resolvingAgainstBaseURL: false),
@@ -81,6 +112,17 @@ private extension WebViewAuthenticator {
             return unauthenticatedRequest(url: url)
         }
         return authenticatedRequest(url: url) ?? unauthenticatedRequest(url: url)
+    }
+
+    func authenticatedRequest(url: URL) -> URLRequest? {
+        var request = URLRequest(url: loginURL)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpBody = body(url: url)
+        if let authToken = authToken {
+            request.setValue("Bearer \(authToken)", forHTTPHeaderField: "Authorization")
+        }
+        return request
     }
 
     func unauthenticatedRequest(url: URL) -> URLRequest {
