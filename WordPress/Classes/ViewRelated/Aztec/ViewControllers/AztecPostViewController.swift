6e78ea2da2298b78f2a9378daee08edbc79f72a7
Jorge Leandro Perez <jorge@lantean.co>
2017-10-09 11:44:38 -0300

    AztecPostViewController: Preventing vertical offset glitch

--- WordPress/Classes/ViewRelated/Aztec/ViewControllers/AztecPostViewController.swift
+++ WordPress/Classes/ViewRelated/Aztec/ViewControllers/AztecPostViewController.swift
@@ -2022,9 +2022,8 @@ extension AztecPostViewController {
             return
         }
 
-        richTextView.resignFirstResponder()
         richTextView.inputView = to
-        richTextView.becomeFirstResponder()
+        richTextView.reloadInputView()
     }
 
     fileprivate func trackFormatBarAnalytics(stat: WPAnalyticsStat, action: String? = nil, headingStyle: String? = nil) {
