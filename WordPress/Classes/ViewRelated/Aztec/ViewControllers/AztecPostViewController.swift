c2b485c05608692a19348c061c81fc859aff30c3
Sergio Estevao <sergioestevao@gmail.com>
2017-10-10 14:19:36 +0100

    Don’t nil the picker when canceling.

--- WordPress/Classes/ViewRelated/Aztec/ViewControllers/AztecPostViewController.swift
+++ WordPress/Classes/ViewRelated/Aztec/ViewControllers/AztecPostViewController.swift
@@ -3090,8 +3090,6 @@ extension AztecPostViewController: WPMediaPickerViewControllerDelegate {
     func mediaPickerControllerDidCancel(_ picker: WPMediaPickerViewController) {
         if picker != mediaPickerInputViewController?.mediaPicker {
             dismiss(animated: true, completion: nil)
-        } else {
-            mediaPickerInputViewController = nil
         }
     }
 
