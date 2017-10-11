bc845e357cb4351ddaf0de4eedbef186e8e15920
Jorge Leandro Perez <jorge@lantean.co>
2017-10-11 14:10:58 -0300

    NoteBlockUserTableViewCell: Updates Colors

--- WordPress/Classes/ViewRelated/Notifications/Views/NoteBlockUserTableViewCell.swift
+++ WordPress/Classes/ViewRelated/Notifications/Views/NoteBlockUserTableViewCell.swift
@@ -73,7 +73,7 @@ class NoteBlockUserTableViewCell: NoteBlockTableViewCell {
         nameLabel.textColor = WPStyleGuide.Notifications.blockTextColor
 
         blogLabel.font = WPStyleGuide.Notifications.blockRegularFont
-        blogLabel.textColor = WPStyleGuide.allTAllShadeGrey()
+        blogLabel.textColor = WPStyleGuide.greyDarken20()
         blogLabel.adjustsFontSizeToFitWidth = false
     }
 
