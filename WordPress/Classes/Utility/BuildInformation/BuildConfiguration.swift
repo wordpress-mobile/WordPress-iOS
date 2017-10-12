522ac162e4e2363228086618de52c19c0162da7c
Diego Rey Mendez <diegoreymendez@gmail.com>
2017-10-12 15:46:33 -0300

    Removes some unnecessary unit tests. Removes Device.swift. Improves Build.swift.

---
+++ WordPress/Classes/Utility/BuildInformation/BuildConfiguration.swift
@@ -0,0 +1,39 @@
+enum BuildConfiguration {
+    /// Development build, usually run from Xcode
+    case localDeveloper
+    
+    /// Continuous integration builds for Automattic employees to test branches & PRs
+    case a8cBranchTest
+    
+    /// Beta released internally for Automattic employees
+    case a8cPrereleaseTesting
+    
+    /// Production build released in the app store
+    case appStore
+
+    static var current: BuildConfiguration {
+        #if DEBUG
+            return testingOverride ?? .localDeveloper
+        #elseif ALPHA_BUILD
+            return .a8cBranchTest
+        #elseif INTERNAL_BUILD
+            return .a8cPrereleaseTesting
+        #else
+            return .appStore
+        #endif
+    }
+    
+    static func ~=(a: BuildConfiguration, b: Set<BuildConfiguration>) -> Bool {
+        return b.contains(a)
+    }
+    
+    #if DEBUG
+    private static var testingOverride: BuildConfiguration?
+    
+    func test(_ closure: () -> ()) {
+        BuildConfiguration.testingOverride = self
+        closure()
+        BuildConfiguration.testingOverride = nil
+    }
+    #endif
+}
