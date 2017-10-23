c25b522cf252ad8a4831e9efedf5038b98cc7577
Diego Rey Mendez <diegoreymendez@gmail.com>
2017-10-23 14:41:59 -0300

    Run rake lint:autocorrect.

--- WordPress/WordPressTest/EditorSettingsTests.swift
+++ WordPress/WordPressTest/EditorSettingsTests.swift
@@ -13,29 +13,29 @@ class EditorSettingsTests: XCTestCase {
     func testAztecEnabledByDefaultButNotForcedAgain() {
         let testClosure: () -> () = { _ in
             let database = EphemeralKeyValueDatabase()
-            
+
             // This simulates the first launch
             let editorSettings = EditorSettings(database: database)
-            
+
             XCTAssertFalse(editorSettings.isEnabled(.legacy))
             XCTAssertFalse(editorSettings.isEnabled(.hybrid))
             XCTAssertTrue(editorSettings.isEnabled(.aztec))
-            
+
             // We pick another editor and try again
             editorSettings.enable(.hybrid)
-            
+
             XCTAssertFalse(editorSettings.isEnabled(.legacy))
             XCTAssertTrue(editorSettings.isEnabled(.hybrid))
             XCTAssertFalse(editorSettings.isEnabled(.aztec))
-            
+
             // This simulates a second launch
             let secondEditorSettings = EditorSettings(database: database)
-            
+
             XCTAssertFalse(secondEditorSettings.isEnabled(.legacy))
             XCTAssertTrue(secondEditorSettings.isEnabled(.hybrid))
             XCTAssertFalse(secondEditorSettings.isEnabled(.aztec))
         }
-        
+
         BuildConfiguration.localDeveloper.test(testClosure)
         BuildConfiguration.a8cBranchTest.test(testClosure)
         BuildConfiguration.a8cPrereleaseTesting.test(testClosure)
