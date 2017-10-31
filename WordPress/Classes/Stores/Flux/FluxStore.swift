647d45fc16304954c0c45433b1fb12193cacc5f6
Jorge Bernal <jbernal@gmail.com>
2017-10-31 15:35:28 +0100

    Extract FluxEmitter from FluxStore
    We can use FluxEmitter as a generic change notification system also for view
    models

--- WordPress/Classes/Stores/Flux/FluxStore.swift
+++ WordPress/Classes/Stores/Flux/FluxStore.swift
@@ -1,10 +1,10 @@
-open class FluxStore {
+open class FluxStore: FluxEmitter {
     let globalDispatcher: FluxDispatcher
     private var dispatchToken: FluxDispatcher.DispatchToken!
-    private let storeDispatcher = Dispatcher<Void>()
+    let dispatcher = Dispatcher<Void>()
 
     var listenerCount: Int {
-        return storeDispatcher.observerCount
+        return dispatcher.observerCount
     }
 
     deinit {
@@ -20,43 +20,5 @@ open class FluxStore {
 
     func onDispatch(_ action: FluxAction) {
         // Subclasses should override this
-    }
-
-    func onChange(_ handler: @escaping Action) -> Listener {
-        let token = storeDispatcher.register(callback: handler)
-        return Listener(dispatchToken: token, store: self)
-    }
-
-    func removeListener(_ listener: Listener) {
-        guard let token = listener.dispatchToken else {
-            assertionFailure("Attempting to remove a listener that has already stopped listening.")
-            return
-        }
-        storeDispatcher.unregister(token: token)
-        listener.dispatchToken = nil
-    }
-
-    func emitChange() {
-        storeDispatcher.dispatch()
-    }
-}
-
-extension FluxStore {
-    class Listener {
-        fileprivate var dispatchToken: Dispatcher<Void>.DispatchToken?
-        fileprivate weak var store: FluxStore?
-
-        fileprivate init(dispatchToken: Dispatcher<Void>.DispatchToken, store: FluxStore) {
-            self.dispatchToken = dispatchToken
-            self.store = store
-        }
-
-        deinit {
-            stopListening()
-        }
-
-        func stopListening() {
-            store?.removeListener(self)
-        }
     }
 }
