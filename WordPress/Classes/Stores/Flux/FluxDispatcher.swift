289a467945aeaa4b4d9910e6d25bab06c8a4b2b2
Jorge Bernal <jbernal@gmail.com>
2017-10-31 12:10:45 +0100

    Distribute Flux types in separate files

---
+++ WordPress/Classes/Stores/Flux/FluxDispatcher.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+class FluxDispatcher: Dispatcher<FluxAction> {
+    static let global = FluxDispatcher()
+
+    static func dispatch(_ action: FluxAction, dispatcher: FluxDispatcher = .global) {
+        dispatcher.dispatch(action)
+    }
+}
