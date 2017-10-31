2a17b5eb8af9e850a8aadf83c9e5fa0206df58bc
Jorge Bernal <jbernal@gmail.com>
2017-10-31 08:09:13 +0100

    Use a Listener wrapper object
    This removes the need to manually stop listening. When the listener gets
    released, it will stop listening automatically.

--- WordPress/Classes/Stores/FluxStore.swift
+++ WordPress/Classes/Stores/FluxStore.swift
@@ -1,28 +1,52 @@
 import Foundation
 
 open class FluxStore {
-    typealias Listener = NSObjectProtocol
     private let changeNotification = NSNotification.Name("FluxStoreChanged")
     var listenerCount = 0
 
     func onChange(_ handler: @escaping Action) -> Listener {
         listenerCount += 1
-        return NotificationCenter.default.addObserver(
+        let notificationObserver = NotificationCenter.default.addObserver(
             forName: changeNotification,
             object: self,
             queue: nil,
             using: { _ in
                 handler()
         })
+        return Listener(notificationObserver: notificationObserver, store: self)
     }
 
     func removeListener(_ listener: Listener) {
-        NotificationCenter.default.removeObserver(listener, name: changeNotification, object: self)
+        guard let notificationObserver = listener.notificationObserver else {
+            assertionFailure("Attempting to remove a listener that has already stopped listening.")
+            return
+        }
+        NotificationCenter.default.removeObserver(notificationObserver, name: changeNotification, object: self)
         listenerCount -= 1
     }
 
     func emitChange() {
         NotificationCenter.default.post(name: changeNotification, object: self)
+    }
+}
+
+extension FluxStore {
+    class Listener {
+        var notificationObserver: NSObjectProtocol?
+        weak var store: FluxStore?
+
+        fileprivate init(notificationObserver: NSObjectProtocol, store: FluxStore) {
+            self.notificationObserver = notificationObserver
+            self.store = store
+        }
+
+        deinit {
+            stopListening()
+        }
+
+        func stopListening() {
+            store?.removeListener(self)
+        }
     }
 }
 
