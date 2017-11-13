d30d9aeb85f285525fa865cfba519612032d7d68
atl009 <purplegraffiti@gmail.com>
2017-11-13 09:51:32 -0600

    Minor code formatting

--- WordPress/Classes/ViewRelated/Me/MeHeaderView.h
+++ WordPress/Classes/ViewRelated/Me/MeHeaderView.h
@@ -5,12 +5,12 @@ typedef void (^MeHeaderViewDropCallback)(UIImage *image);
 
 @interface MeHeaderView : UIView
 
-@property (nonatomic,   copy) NSString              *displayName;
-@property (nonatomic,   copy) NSString              *username;
-@property (nonatomic,   copy) NSString              *gravatarEmail;
-@property (nonatomic,   copy) MeHeaderViewCallback  onGravatarPress;
+@property (nonatomic,   copy) NSString                  *displayName;
+@property (nonatomic,   copy) NSString                  *username;
+@property (nonatomic,   copy) NSString                  *gravatarEmail;
+@property (nonatomic,   copy) MeHeaderViewCallback      onGravatarPress;
 @property (nonatomic,   copy) MeHeaderViewDropCallback  onDroppedImage;
-@property (nonatomic, assign) BOOL                  showsActivityIndicator;
+@property (nonatomic, assign) BOOL                      showsActivityIndicator;
 
 /// Overrides the current Gravatar Image (set via Email) with a given image reference.
 /// Plus, AFNetworking's internal cache is updated, to prevent undesired glitches upon refresh.
