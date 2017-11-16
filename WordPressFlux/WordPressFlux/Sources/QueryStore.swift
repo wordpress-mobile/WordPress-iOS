da2af9d9fda210edc2ed15bf0f2d956f26d4d9ef
Jorge Bernal <jbernal@gmail.com>
2017-11-16 09:27:48 +0100

    Adds QuerySubscription for automatic query stopping

--- WordPressFlux/WordPressFlux/Sources/QueryStore.swift
+++ WordPressFlux/WordPressFlux/Sources/QueryStore.swift
@@ -6,10 +6,42 @@ private struct QueryRef<QueryType> where QueryType: Query {
     }
 }
 
-open class QueryStore<State, QueryType>: ReducerStore<State> where QueryType: Query {
+protocol QueryProcessor: class {
+    func stop(query: QuerySubscription)
+}
+
+public class QuerySubscription {
+    private var dispatchToken: DispatchToken?
+    private weak var processor: QueryProcessor?
+
+    fileprivate init(dispatchToken: DispatchToken, processor: QueryProcessor) {
+        self.dispatchToken = dispatchToken
+        self.processor = processor
+    }
+
+    deinit {
+        if dispatchToken != nil {
+            stopListening()
+        }
+    }
+
+    private func stopListening() {
+        processor?.stop(query: self)
+    }
+}
+
+open class QueryStore<State: Equatable, QueryType>: ReducerStore<State> where QueryType: Query {
     fileprivate var activeQueries = [QueryRef<QueryType>]() {
         didSet {
-            processQueries(state: state, queries: activeQueries.map({$0.query}))
+            processQueries(state: state)
+        }
+    }
+
+    public override var state: State {
+        didSet {
+            if state != oldValue {
+                processQueries(state: state)
+            }
         }
     }
 
@@ -19,12 +51,16 @@ open class QueryStore<State, QueryType>: ReducerStore<State> where QueryType: Qu
         return queryRef.token
     }
 
-    public func stopQuery(token: DispatchToken) {
+    fileprivate func stopQuery(token: DispatchToken) {
         guard let index = activeQueries.index(where: { $0.token == token }) else {
             assertionFailure("Stopping a query that's not active")
             return
         }
         activeQueries.remove(at: index)
+    }
+
+    private func processQueries(state: State) {
+        processQueries(state: state, queries: activeQueries.map({ $0.query }))
     }
 
     open func processQueries(state: State, queries: [QueryType]) {
