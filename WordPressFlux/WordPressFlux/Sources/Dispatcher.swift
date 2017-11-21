ceeccc5bb9d8979e579843cfb476cefefb4084b5
Jorge Bernal <jbernal@gmail.com>
2017-11-21 11:23:48 +0100

    Added documentation for WordPressFlux

--- WordPressFlux/WordPressFlux/Sources/Dispatcher.swift
+++ WordPressFlux/WordPressFlux/Sources/Dispatcher.swift
@@ -1,3 +1,5 @@
+/// An opaque type used by Dispatcher to keep track of registered handlers.
+///
 public struct DispatchToken: Hashable, Equatable {
     private let uuid = UUID()
 
@@ -10,25 +12,51 @@ public struct DispatchToken: Hashable, Equatable {
     }
 }
 
+/// A Dispatcher broadcasts a Payload to all registered subscribers.
+///
+/// You can think of it as a strongly typed NotificationCenter, if it had been
+/// written for Swift instead of Objective-C.
+///
+/// Dispatcher is not thread safe yet, and it expects its methods to be called
+/// from the main thread only.
+///
 public class Dispatcher<Payload> {
-    public typealias Callback = (Payload) -> Void
 
+    /// Initializes a new Dispatcher.
+    ///
     public init() {}
 
-    private var observers = [DispatchToken: Callback]()
+    private var observers = [DispatchToken: (Payload) -> Void]()
 
-    public func register(_ callback: @escaping Callback) -> DispatchToken {
+    /// Register a new handler to call on dispatch.
+    ///
+    /// - Parameters:
+    ///     - callback: a closure to be called on dispatch with the payload as a
+    ///     parameter.
+    /// - Returns: a DispatchToken necessary to unregister the given callback.
+    ///
+    public func register(_ callback: @escaping (Payload) -> Void) -> DispatchToken {
         assertMainThread()
         let token = DispatchToken()
         observers[token] = callback
         return token
     }
 
+    /// Unregisters the callback associated with the given token.
+    ///
+    /// - Parameters:
+    ///     - token: the token that was returned by register.
+    ///
     public func unregister(token: DispatchToken) {
         assertMainThread()
         observers[token] = nil
     }
 
+    /// Dispatches the given payload to all registered handlers.
+    ///
+    /// - Parameters:
+    ///     - payload: the payload to broadcast to handlers.
+    ///
     public func dispatch(_ payload: Payload) {
         assertMainThread()
         self.observers.forEach { (_, callback) in
@@ -42,11 +70,27 @@ public class Dispatcher<Payload> {
 }
 
 extension Dispatcher: Unsubscribable {
-    public func subscribe(_ callback: @escaping Callback) -> Receipt {
+    /// Registers a new handler with a subscription bound to the lifetime of the
+    /// returned Receipt.
+    ///
+    /// When the returned Receipt is released from memory, the handler will be
+    /// automatically unregistered.
+    ///
+    /// - Parameters:
+    ///   - callback: a closure to be called on dispatch with the payload as a
+    ///               parameter.
+    /// - Returns: a Receipt bound to the handler subscription.
+    ///
+    public func subscribe(_ callback: @escaping (Payload) -> Void) -> Receipt {
         let token = register(callback)
         return Receipt(token: token, owner: self)
     }
 
+    /// Unregisters the callback associated with the given Receipt.
+    ///
+    /// - Parameters:
+    ///     - receipt: the receipt that was returned by subscribe.
+    ///
     public func unsubscribe(receipt: Receipt) {
         unregister(token: receipt.token)
     }
@@ -54,7 +98,9 @@ extension Dispatcher: Unsubscribable {
 
 // Cleaner syntax for Void payloads
 extension Dispatcher where Payload == Void {
-    func dispatch() {
+    /// Dispatches an event to all the registered handlers.
+    ///
+    public func dispatch() {
         dispatch(())
     }
 }
