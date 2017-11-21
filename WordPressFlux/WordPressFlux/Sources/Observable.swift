f4c413beb35f260ad6799df8dd5254c70c4afd6a
Jorge Bernal <jbernal@gmail.com>
2017-11-21 11:23:48 +0100

    Added documentation for WordPressFlux

--- WordPressFlux/WordPressFlux/Sources/Observable.swift
+++ WordPressFlux/WordPressFlux/Sources/Observable.swift
@@ -1,5 +1,28 @@
+/// A type that can be observed for changes.
+///
+/// Types that conform to Observable can be subscribed by observers to be
+/// notified of changes. Observers can call onChange and register a callback to
+/// handle any changes to the Observable, as long as they keep a reference to
+/// the returned Receipt.
+///
+/// ## Conforming to Observable
+///
+/// To allow a type to be observed, add a changeDispatcher property that will
+/// keep track of all the subscriptions. When the type needs to notify that it
+/// has changed, call emitChange().
+///
+///
 public protocol Observable {
+    /// The Dispatcher to keep track of all observers.
+    ///
     var changeDispatcher: Dispatcher<Void> { get }
+
+    /// Registers a new observer.
+    ///
+    /// When there's a change, the given handler will be called.
+    /// The observer should keep a copy of the returned Receipt for as long as
+    /// it desires to receive change notifications.
+    ///
     func onChange(_ handler: @escaping () -> Void) -> Receipt
 }
 
@@ -8,6 +31,7 @@ public extension Observable {
         return changeDispatcher.subscribe(handler)
     }
 
+    /// Notifies all registered observers of a change.
     func emitChange() {
         changeDispatcher.dispatch()
     }
--- WordPressFlux/WordPressFlux/Sources/StatefulStore.swift
+++ WordPressFlux/WordPressFlux/Sources/StatefulStore.swift
@@ -1,17 +1,45 @@
+/// A store that holds all of its internal state in a generic State property.
+///
+/// This class provides some common functionality for stores that want to keep
+/// all of their state in a single tree. When the state changes, it will
+/// automatically dispatch the change events.
+///
+/// Besides the existing change events provided by Store, consumers of a
+/// StatefulStore can also register with onStateChange(_:) and receive a tuple
+/// containing the old and new state.
+///
+/// ## Subclassing Notes
+///
+/// When subclassing StatefulStore, it is recommended that you implement a
+/// convenience initializer that sets an acceptable initialState.
+///
+/// StatefulStore provides a helper transaction method that's useful when you
+/// need to do multiple changes to the state property, but only want one change
+/// notification to be dispatched.
+///
 open class StatefulStore<State>: Store {
     private let stateDispatcher = Dispatcher<(State, State)>()
 
+    /// The internal state of the store.
+    ///
     public var state: State {
         didSet {
             emitStateChange(old: oldValue, new: state)
         }
     }
 
+    /// Initializes a store with an initial state.
+    ///
     public init(initialState: State, dispatcher: ActionDispatcher = .global) {
         state = initialState
         super.init(dispatcher: dispatcher)
     }
 
+    /// Groups several state changes into one transaction.
+    ///
+    /// You can modify the state multiple times from the passed closure, but
+    /// only one change event will be dispatched after the closure returns.
+    ///
     public func transaction(_ modify: (inout State) -> Void) {
         var copy = state
         modify(&copy)
@@ -23,6 +51,9 @@ open class StatefulStore<State>: Store {
         emitChange()
     }
 
+    /// Registers a new observer, that will receive a tuple with the old and new
+    /// state.
+    ///
     public func onStateChange(_ handler: @escaping ((State, State)) -> Void) -> Receipt {
         return stateDispatcher.subscribe(handler)
     }
