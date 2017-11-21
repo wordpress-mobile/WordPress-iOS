bf57a6c7f36baccbafa706e0c0f3bfddde81177e
Jorge Leandro Perez <jorge@lantean.co>
2017-11-21 11:36:22 -0300

    Swift 4: Adds missing @objc modifiers. Mark VI

--- WordPressKit/WordPressKit/WordPressComOAuthClient.swift
+++ WordPressKit/WordPressKit/WordPressComOAuthClient.swift
@@ -155,6 +155,7 @@ public final class WordPressComOAuthClient: NSObject {
     ///     - success: block to be called if authentication was successful.
     ///     - failure: block to be called if authentication failed. The error object is passed as a parameter.
     ///
+    @objc
     public func requestSocial2FACodeWithUserID(_ userID: Int,
                                      nonce: String,
                                       success: @escaping (_ newNonce: String) -> Void,
