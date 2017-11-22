31a8c5bed145f191d20e17f25f8daaf9a7307a41
Jorge Leandro Perez <jorge@lantean.co>
2017-11-22 08:51:34 -0300

    NoteBlockHeaderTableViewCell: Cleanup

--- WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationDetailsViewController.swift
+++ WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationDetailsViewController.swift
@@ -574,7 +574,7 @@ private extension NotificationDetailsViewController {
 
         // Download the Gravatar
         let mediaURL = gravatarBlock?.media.first?.mediaURL
-        cell.downloadGravatarWithURL(mediaURL)
+        cell.downloadGravatar(with: mediaURL)
     }
 
     func setupFooterCell(_ cell: NoteBlockTextTableViewCell, blockGroup: NotificationBlockGroup) {
