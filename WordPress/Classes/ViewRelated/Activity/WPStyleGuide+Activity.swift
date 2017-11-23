acd2d92ca60db5e628d6d29100b6dadef974926b
elibud <elibud@gmail.com>
2017-11-23 15:28:34 -0300

    Add icons to activities and color them according to the activity status

--- WordPress/Classes/ViewRelated/Activity/WPStyleGuide+Activity.swift
+++ WordPress/Classes/ViewRelated/Activity/WPStyleGuide+Activity.swift
@@ -1,10 +1,12 @@
 import Foundation
+import Gridicons
 import WordPressShared
 
 /// This class groups all of the styles used by all of the ActivityListViewController.
 ///
 extension WPStyleGuide {
-    public struct Activity {
+
+    public struct ActivityStyleGuide {
 
         // MARK: - Public Properties
 
@@ -41,6 +43,13 @@ extension WPStyleGuide {
             return WPStyleGuide.lightBlue()
         }
 
+        public static func getIconForActivity(_ activity: Activity) -> UIImage? {
+            if let gridicon = stringToGridiconMapping[activity.gridicon] {
+                return gridicon.imageWithTintColor(getColorByActivityStatus(activity))
+            }
+            return nil
+        }
+
         // MARK: - Private Properties
 
         fileprivate static let gravatar = UIImage(named: "gravatar")!
@@ -67,5 +76,48 @@ extension WPStyleGuide {
                                            lineBreakMode: .byTruncatingTail,
                                            alignment: .natural)
         }
+
+        private static func getColorByActivityStatus(_ activity: Activity) -> UIColor {
+            if activity.isStatusError {
+                return WPStyleGuide.errorRed()
+            }
+            if activity.isStatusSuccess {
+                return WPStyleGuide.validGreen()
+            }
+            if activity.isStatusWarning {
+                return WPStyleGuide.warningYellow()
+            }
+            return WPStyleGuide.greyLighten10()
+        }
+
+        // We will be able to get rid of this disgusting dictionary once we build the
+        // String->Gridicon mapping into the Gridicon module and we get a server side
+        // fix to have all the names correctly mapping.
+        private static let stringToGridiconMapping: [String: UIImage] = [
+            "checkmark": Gridicon.iconOfType(.checkmark),
+            "cog": Gridicon.iconOfType(.cog),
+            "comment": Gridicon.iconOfType(.comment),
+            "cross": Gridicon.iconOfType(.cross),
+            "domains": Gridicon.iconOfType(.domains),
+            "history": Gridicon.iconOfType(.history),
+            "image": Gridicon.iconOfType(.image),
+            "layout": Gridicon.iconOfType(.layout),
+            "lock": Gridicon.iconOfType(.lock),
+            "logout": Gridicon.iconOfType(.signOut),
+            "mail": Gridicon.iconOfType(.mail),
+            "menu": Gridicon.iconOfType(.menu),
+            "my-sites": Gridicon.iconOfType(.mySites),
+            "notice": Gridicon.iconOfType(.notice),
+            "notice-outline": Gridicon.iconOfType(.noticeOutline),
+            "pages": Gridicon.iconOfType(.pages),
+            "plugins": Gridicon.iconOfType(.plugins),
+            "posts": Gridicon.iconOfType(.posts),
+            "share": Gridicon.iconOfType(.share),
+            "shipping": Gridicon.iconOfType(.shipping),
+            "spam": Gridicon.iconOfType(.spam),
+            "themes": Gridicon.iconOfType(.themes),
+            "trash": Gridicon.iconOfType(.trash),
+            "user": Gridicon.iconOfType(.user),
+        ]
     }
 }
