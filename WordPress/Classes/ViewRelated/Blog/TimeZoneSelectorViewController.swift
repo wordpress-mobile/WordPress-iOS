5613eba8937cf42952cf458215a75c4c4705a30e
Mohd Asif <arshadasif0312@gmail.com>
2017-11-24 00:29:01 +0530

    Conform TimezoneSelectorVC to ImmuTablePresenter

--- WordPress/Classes/ViewRelated/Blog/TimeZoneSelectorViewController.swift
+++ WordPress/Classes/ViewRelated/Blog/TimeZoneSelectorViewController.swift
@@ -1,23 +1,47 @@
 import UIKit
 
-class TimeZoneSelectorViewController: UITableViewController {
+class TimeZoneSelectorViewController: UITableViewController, ImmuTablePresenter {
 
-    /// used to show section headers
-    var continentNames: [String] = []
-    /// used to show text in cell sorted by continent
-    var timezoneNamesSortedByContinent: [String: [String]] = [:]
-    /// all timezones from CoreData DB
-    var allTimezones: [TimezoneInfo] = []
     /// users current timezone passed by SiteSettingsVC, if empty means manual offset is to be used
     var usersCurrentTimeZone: String?
     /// users manual offset passed by SiteSettingsVC
     var usersManualOffset: NSNumber?
 
-    /// timezoneString will be set if user selects anything other than Manual Offset section
+    /// timezoneString will be set to a non empty string
+    /// if user selects anything other than Manual Offset section
     /// manualOffset will be set if users selects anything from Manual Offset section
     @objc var onChange: ((_ timezoneString: String, _ manualOffset: NSNumber?) -> Void)?
-    /// constant used below in string checks
-    let MANUAL_OFFSET: String = "Manual Offsets"
+
+    /// ImmuTableViewHandler, takes over the datasource, delegate from this VC
+    private lazy var handler: ImmuTableViewHandler = {
+        return ImmuTableViewHandler(takeOver: self)
+    }()
+
+    /// common action passed to TimezoneListRow cells, executed on select of a cell
+    private lazy var action: ImmuTableAction = { [weak self] (aRow) -> Void in
+        self?.navigationController?.popViewController(animated: true)
+        let timezoneValue: String = (aRow as? TimezoneListRow)?.timezoneValue ?? ""
+        var timezoneString: String = ""
+        var manualOffset: NSNumber?
+        if let numberString = timezoneValue.components(separatedBy: "UTC").last,
+            let floatVal = Float(numberString) {
+            let manualOffsetNumber: NSNumber = NSNumber(value: floatVal)
+            manualOffset = manualOffsetNumber
+        } else {
+            timezoneString = timezoneValue
+        }
+        self?.onChange?(timezoneString, manualOffset)
+    }
+
+    private var viewModel: TimezoneSelectorViewModel = .loading {
+        didSet {
+            handler.viewModel = viewModel.tableViewModel()
+            updateNoResults()
+        }
+    }
+
+    /// used to show an intermittent loading state
+    private let noResultsView = WPNoResultsView()
 
     override init(style: UITableViewStyle) {
         super.init(style: .grouped)
@@ -30,181 +54,59 @@ class TimeZoneSelectorViewController: UITableViewController {
     override func viewDidLoad() {
         self.tableView.rowHeight = UITableViewAutomaticDimension
         self.tableView.estimatedRowHeight = 45.0
-        self.loadDataFromDB()
-        if self.allTimezones.count == 0 {
+
+        title = NSLocalizedString("Timezones", comment: "Title for the timezone selector")
+        WPStyleGuide.configureColors(for: view, andTableView: tableView)
+        ImmuTable.registerRows([TimezoneListRow.self], tableView: tableView)
+        handler.viewModel = viewModel.tableViewModel()
+        updateNoResults()
+        loadAndShowData()
+    }
+
+    // MARK: Helper methods
+    /// If no data exists in DB, make an API call to fetch and save the data, then display
+    /// otherwise fetch data from DB and display
+    private func loadAndShowData() {
+        let allTimezones = self.viewModel.loadDataFromDB()
+        if allTimezones.count == 0 {
             self.loadDataFromAPIAndSaveToDB()
         } else {
-            self.setupVariables(with: self.allTimezones)
+            self.viewModel = .ready(allTimezones, self.usersCurrentTimeZone, self.usersManualOffset, self.action)
         }
     }
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        self.highlightCurrentSelection()
-    }
-
-    private func loadDataFromDB() {
-        let fetchRequest = NSFetchRequest<TimezoneInfo>(entityName: "TimezoneInfo")
-        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "continent", ascending: true),
-                                        NSSortDescriptor(key: "label", ascending: true)]
-        do {
-            self.allTimezones = try ContextManager.sharedInstance().mainContext.fetch(fetchRequest)
-        } catch {
-            print(error)
-        }
-    }
-
-    private func setupVariables(with result: [TimezoneInfo]) {
-        self.continentNames = result.reduce(into: Set<String>(), { (mySet, timezone) in
-            mySet.insert(timezone.continent)
-        }).sorted()
-        // remove manual offset's from it's sorted position and push it to the bottom
-        if let manualOffsetIndex = self.continentNames.index(of: MANUAL_OFFSET) {
-            self.continentNames.remove(at: manualOffsetIndex)
-            self.continentNames.append(MANUAL_OFFSET)
-        }
-        for continent in self.continentNames {
-            let allTimezonesInContinent = result.filter({ $0.continent == continent })
-
-            if continent == self.MANUAL_OFFSET {
-                // sort the UTC strings
-                let allLabelsAndValues = allTimezonesInContinent.map({ (label: $0.label, value: $0.value) })
-                self.timezoneNamesSortedByContinent[continent] = allLabelsAndValues.sorted(by: { (left, right) -> Bool in
-                    guard let leftNumString = left.value.components(separatedBy: "UTC").last,
-                        let rightNumString = right.value.components(separatedBy: "UTC").last,
-                        let floatLeftNum = Float(leftNumString),
-                        let floatRightNum = Float(rightNumString) else {
-                            return false
-                    }
-                    return floatLeftNum > floatRightNum
-                }).map({ $0.label })
-            } else {
-                let allLabels = allTimezonesInContinent.map({ $0.label })
-                self.timezoneNamesSortedByContinent[continent] = allLabels.sorted()
-            }
-        }
-    }
-
+    /// helper method to call API and save the data to DB
     private func loadDataFromAPIAndSaveToDB() {
         let api = ServiceRemoteWordPressComREST.anonymousWordPressComRestApi(withUserAgent: WPUserAgent.wordPress())!
         let remoteService = BlogServiceRemoteREST(wordPressComRestApi: api, siteID: NSNumber(value: 0))
         remoteService.fetchTimeZoneList(success: { [weak self] (resultsDict) in
-            self?.insertDataToDB(resultsDict: resultsDict)
-            self?.loadDataFromDB()
-            if let allTimezones = self?.allTimezones {
-                self?.setupVariables(with: allTimezones)
-            }
-            self?.tableView.reloadData()
-            self?.highlightCurrentSelection()
+            self?.viewModel.insertDataToDB(resultsDict: resultsDict)
+            self?.loadAndShowData()
             }, failure: { (error) in
-                print(error)
+                DDLogError("Error loading timezones: \(error)")
+                self.viewModel = .error(String(describing: error))
         })
     }
 
-    private func insertDataToDB(resultsDict: [String: [String: String]]) {
-        let manager = ContextManager.sharedInstance()
-        let context = manager.newDerivedContext()
-        context.performAndWait {
-            for (continent, timezonesInContinent) in resultsDict {
-                for (key, val) in timezonesInContinent {
-                    let timezoneInfo = NSEntityDescription.insertNewObject(forEntityName: "TimezoneInfo", into: context) as! TimezoneInfo
-                    timezoneInfo.label = key
-                    timezoneInfo.value = val
-                    timezoneInfo.continent = continent
-                }
-            }
-            manager.saveContextAndWait(context)
-        }
-    }
-
-    private func highlightCurrentSelection() {
-        guard self.allTimezones.count != 0,
-            let indexPath = self.getIndexPathToHighlight() else {
-            return
-        }
-        self.tableView.beginUpdates()
-        self.tableView.selectRow(at: indexPath, animated: true, scrollPosition: UITableViewScrollPosition.middle)
-        self.tableView.endUpdates()
-    }
-
-    private func getIndexPathToHighlight() -> IndexPath? {
-        if let usersTimeZone = self.usersCurrentTimeZone, !usersTimeZone.isEmpty {
-            for (continentIndex, continent) in self.continentNames.enumerated() {
-                let allTimezonesInContinent = self.allTimezones.filter({ $0.continent == continent })
-                guard let keyValPair = allTimezonesInContinent.first(where: { $0.value == usersTimeZone }),
-                    let index = self.timezoneNamesSortedByContinent[continent]?.index(of: keyValPair.label) else {
-                        continue
-                }
-                let indexPath = IndexPath(row: index, section: continentIndex)
-                return indexPath
-            }
-        } else if let manualOffset = self.usersManualOffset,
-            let section = self.continentNames.index(of: self.MANUAL_OFFSET),
-            let dict = self.timezoneNamesSortedByContinent[MANUAL_OFFSET] {
-            let hoursUTC = manualOffset.intValue
-            let minutesUTC = abs(Int((manualOffset.doubleValue - Double(hoursUTC)) * 60))
-            let utcString: String
-            if minutesUTC == 0 {
-                utcString = String(format: "UTC%+d", hoursUTC)
-            } else {
-                utcString = String(format: "UTC%+d:%d", hoursUTC, minutesUTC)
-            }
-            if let index = dict.index(where: { $0 == utcString }) {
-                let row = dict.distance(from: dict.startIndex, to: index)
-                let indexPath = IndexPath(row: row, section: section)
-                return indexPath
-            }
-        }
-        return nil
-    }
-
-    // MARK: UITableView Datasource
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return self.continentNames.count
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let sectionName = self.continentNames[section]
-        let sectionContinentNamesArray = self.timezoneNamesSortedByContinent[sectionName]
-        return sectionContinentNamesArray?.count ?? 0
-    }
-
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return self.continentNames[section]
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        let continentName = self.continentNames[indexPath.section]
-        let cellText = self.timezoneNamesSortedByContinent[continentName]?[indexPath.row]
-        cell.textLabel?.text = cellText
-        cell.selectionStyle = .none
-        if self.tableView.indexPathForSelectedRow == indexPath {
-            cell.accessoryType = .checkmark
-        }
-        return cell
-    }
-
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        self.navigationController?.popViewController(animated: true)
-        let continentName = self.continentNames[indexPath.section]
-        let timezoneLabel = self.timezoneNamesSortedByContinent[continentName]?[indexPath.row]
-        let timezoneValue = self.allTimezones.first(where: { $0.label == timezoneLabel })?.value
-        var manualOffset: NSNumber?
-        var timezoneString: String = ""
-        if continentName == self.MANUAL_OFFSET,
-            let numberString = timezoneValue?.components(separatedBy: "UTC").last,
-            let floatVal = Float(numberString) {
-            let manualOffsetNumber: NSNumber = NSNumber(value: floatVal)
-            manualOffset = manualOffsetNumber
+    // MARK: NoResultsView methods
+    private func updateNoResults() {
+        if let noResultsViewModel = viewModel.noResultsViewModel {
+            showNoResults(noResultsViewModel)
         } else {
-            timezoneString = timezoneValue ?? ""
+            hideNoResults()
         }
-        self.onChange?(timezoneString, manualOffset)
     }
 
-    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-        tableView.cellForRow(at: indexPath)?.accessoryType = .none
+    private func showNoResults(_ viewModel: WPNoResultsView.Model) {
+        noResultsView.bindViewModel(viewModel)
+        if noResultsView.isDescendant(of: tableView) {
+            noResultsView.centerInSuperview()
+        } else {
+            tableView.addSubview(withFadeAnimation: noResultsView)
+        }
+    }
+
+    private func hideNoResults() {
+        noResultsView.removeFromSuperview()
     }
 }
---
+++ WordPress/Classes/ViewRelated/Blog/TimezoneSelectorViewModel.swift
@@ -0,0 +1,194 @@
+import Foundation
+import CoreData
+
+enum TimezoneSelectorViewModel {
+    case loading
+    case ready([TimezoneInfo], String?, NSNumber?, ImmuTableAction)
+    case error(String)
+
+    /// the first param is a list of all continents, used as section headers
+    /// the second param is a mapping from continent to timezones in that continent
+    typealias ContinentsAndTimezones = ([String], [String: [(String, String)]])
+
+    var MANUAL_OFFSET: String { return "Manual Offsets" }
+    var noResultsViewModel: WPNoResultsView.Model? {
+        switch self {
+        case .loading:
+            return WPNoResultsView.Model(
+                title: NSLocalizedString("Loading Timezones...",
+                                         comment: "Text displayed while loading the activity feed for a site")
+            )
+        case .ready:
+            return nil
+        case .error:
+            let appDelegate = WordPressAppDelegate.sharedInstance()
+            if (appDelegate?.connectionAvailable)! {
+                return WPNoResultsView.Model(
+                    title: NSLocalizedString("Oops", comment: ""),
+                    message: NSLocalizedString("There was an error loading timezones",
+                                               comment: "Text displayed when there is a failure loading the timezone list"),
+                    buttonTitle: NSLocalizedString("Contact support",
+                                                   comment: "Button label for contacting support")
+                )
+            } else {
+                return WPNoResultsView.Model(
+                    title: NSLocalizedString("No connection", comment: ""),
+                    message: NSLocalizedString("An active internet connection is required to view timezones", comment: "")
+                )
+            }
+        }
+    }
+
+    func tableViewModel() -> ImmuTable {
+        switch self {
+        case .loading, .error:
+            return .Empty
+        case .ready(let timezoneInfoArray, let usersTimezoneString, let usersManualOffset, let action):
+            let (continents, continentToTimezones) = self.setupVariables(with: timezoneInfoArray)
+            let indexPathToHighlight = self.getIndexPathToHighlight(timezoneString: usersTimezoneString, manualOffset: usersManualOffset, data: (continents, continentToTimezones), allTimezones: timezoneInfoArray)
+            var sections: [ImmuTableSection] = []
+            for (sectionIndex, continent) in continents.enumerated() {
+                guard let allTimezones = continentToTimezones[continent] else {
+                    continue
+                }
+                var rows: [TimezoneListRow] = []
+                if let highlightedIndexPath = indexPathToHighlight,
+                    highlightedIndexPath.section == sectionIndex {
+                    for (rowIndex, timezoneInfo) in allTimezones.enumerated() {
+                        let isHighlighted: Bool = rowIndex == highlightedIndexPath.row
+                        rows.append(TimezoneListRow(timezoneLabel: timezoneInfo.0, timezoneValue: timezoneInfo.1, action: action, highlighted: isHighlighted))
+                    }
+                } else {
+                    rows = allTimezones.map({ TimezoneListRow(timezoneLabel: $0.0, timezoneValue: $0.1, action: action, highlighted: false) })
+                }
+                let section = ImmuTableSection(headerText: continent, rows: rows)
+                sections.append(section)
+            }
+            return ImmuTable(sections: sections)
+        }
+    }
+
+    // MARK: Helper methods
+
+    /// Save API data to core data DB
+    func insertDataToDB(resultsDict: [String: [String: String]]) {
+        let manager = ContextManager.sharedInstance()
+        let context = manager.newDerivedContext()
+        context.performAndWait {
+            for (continent, timezonesInContinent) in resultsDict {
+                for (key, val) in timezonesInContinent {
+                    let timezoneInfo = NSEntityDescription.insertNewObject(forEntityName: "TimezoneInfo", into: context) as! TimezoneInfo
+                    timezoneInfo.label = key
+                    timezoneInfo.value = val
+                    timezoneInfo.continent = continent
+                }
+            }
+            manager.saveContextAndWait(context)
+        }
+    }
+
+    /// Helper method to fetch all TimezoneInfo objects from DB
+    func loadDataFromDB() -> [TimezoneInfo] {
+        let fetchRequest = NSFetchRequest<TimezoneInfo>(entityName: "TimezoneInfo")
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "continent", ascending: true),
+                                        NSSortDescriptor(key: "label", ascending: true)]
+        do {
+            return try ContextManager.sharedInstance().mainContext.fetch(fetchRequest)
+        } catch {
+            print(error)
+            return []
+        }
+    }
+
+    /// Returns a sorted list of continents and timezones mapped to a continent
+    private func setupVariables(with result: [TimezoneInfo]) -> ContinentsAndTimezones {
+        var continentNames = result.reduce(into: Set<String>(), { (mySet, timezone) in
+            mySet.insert(timezone.continent)
+        }).sorted()
+
+        // remove manual offset's from it's sorted position and push it to the bottom
+        if let manualOffsetIndex = continentNames.index(of: self.MANUAL_OFFSET) {
+            continentNames.remove(at: manualOffsetIndex)
+            continentNames.append(self.MANUAL_OFFSET)
+        }
+        var timezoneNamesSortedByContinent: [String: [(String, String)]] = [:]
+        for continent in continentNames {
+            let allTimezonesInContinent = result.filter({ $0.continent == continent })
+
+            if continent == self.MANUAL_OFFSET {
+                // sort the UTC strings
+                let allLabelsAndValues = allTimezonesInContinent.map({ (label: $0.label, value: $0.value) })
+                timezoneNamesSortedByContinent[continent] = allLabelsAndValues.sorted(by: { (left, right) -> Bool in
+                    guard let leftNumString = left.value.components(separatedBy: "UTC").last,
+                        let rightNumString = right.value.components(separatedBy: "UTC").last,
+                        let floatLeftNum = Float(leftNumString),
+                        let floatRightNum = Float(rightNumString) else {
+                            return false
+                    }
+                    return floatLeftNum > floatRightNum
+                })
+            } else {
+                timezoneNamesSortedByContinent[continent] = allTimezonesInContinent.sorted(by: { (timezone1, timezone2) -> Bool in
+                    return timezone1.label < timezone2.label
+                }).map({ ($0.label, $0.value) })
+            }
+        }
+        return (headerNames: continentNames, sortedNamesByContinent: timezoneNamesSortedByContinent)
+    }
+
+    /// returns an IndexPath, which is the user's current selection
+    private func getIndexPathToHighlight(timezoneString: String?, manualOffset: NSNumber?, data: ContinentsAndTimezones, allTimezones: [TimezoneInfo]) -> IndexPath? {
+        if let usersTimeZone = timezoneString, !usersTimeZone.isEmpty {
+            for (continentIndex, continent) in data.0.enumerated() {
+                let allTimezonesInContinent = allTimezones.filter({ $0.continent == continent })
+                guard let keyValPair = allTimezonesInContinent.first(where: { $0.value == usersTimeZone }),
+                    let index = data.1[continent]?.index(where: { (labelValPair) -> Bool in
+                        return labelValPair.0 == keyValPair.label
+                    }) else {
+                        continue
+                }
+                let indexPath = IndexPath(row: index, section: continentIndex)
+                return indexPath
+            }
+        } else if let manualOffset = manualOffset,
+            let section = data.0.index(of: self.MANUAL_OFFSET),
+            let dict = data.1[self.MANUAL_OFFSET] {
+            let hoursUTC = manualOffset.intValue
+            let minutesUTC = abs(Int((manualOffset.doubleValue - Double(hoursUTC)) * 60))
+            let utcString: String
+            if minutesUTC == 0 {
+                utcString = String(format: "UTC%+d", hoursUTC)
+            } else {
+                utcString = String(format: "UTC%+d:%d", hoursUTC, minutesUTC)
+            }
+            if let index = dict.index(where: { $0.0 == utcString }) {
+                let row = dict.distance(from: dict.startIndex, to: index)
+                let indexPath = IndexPath(row: row, section: section)
+                return indexPath
+            }
+        }
+        return nil
+    }
+}
+
+/// Cell model used in TimezoneSelectorVC to display timezones
+struct TimezoneListRow: ImmuTableRow {
+    static let cell: ImmuTableCell = {
+        return ImmuTableCell.class(UITableViewCell.self)
+    }()
+
+    let timezoneLabel: String
+    let timezoneValue: String
+    let action: ImmuTableAction?
+    let highlighted: Bool
+
+    func configureCell(_ cell: UITableViewCell) {
+        cell.textLabel?.text = timezoneLabel
+        if highlighted {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        cell.selectionStyle = .none
+    }
+}
