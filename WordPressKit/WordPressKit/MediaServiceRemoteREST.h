4cdb201fb2eb41407f50ca9f881f6efabe284a22
Matt Bumgardner <matt.bumgardner@gmail.com>
2017-11-27 14:01:49 -0600

    WIP: uploading shared media item in separate call

--- WordPressKit/WordPressKit/MediaServiceRemoteREST.h
+++ WordPressKit/WordPressKit/MediaServiceRemoteREST.h
@@ -2,18 +2,23 @@
 #import "MediaServiceRemote.h"
 #import "SiteServiceRemoteWordPressComREST.h"
 
-NS_ASSUME_NONNULL_BEGIN
-
 @interface MediaServiceRemoteREST : SiteServiceRemoteWordPressComREST <MediaServiceRemote>
 
 - (RemoteMedia *)remoteMediaFromJSONDictionary:(NSDictionary *)jsonMedia;
 
-- (void)uploadMedia:(nonnull RemoteMedia *)media
-           progress:(NSProgress * __nullable __autoreleasing * __nullable)progress
-    requestEnqueued:(nullable void (^)(void))requestEnqueued
-            success:(nullable void (^)(RemoteMedia *remoteMedia))success
-            failure:(nullable void (^)(NSError *error))failure;
+/**
+ *  @brief      Upload a single media item to the remote site.
+ *
+ *  @discussion This purpose of this method is to give app extensions the ability to upload media via background sessions.
+ *
+ *  @param  media           The media item to create remotely.
+ *  @param  requestEnqueued The block that will be executed when the network request is queued.  Can be nil.
+ *  @param  success         The block that will be executed on success.  Can be nil.
+ *  @param  failure         The block that will be executed on failure.  Can be nil.
+ */
+- (void)uploadMedia:(RemoteMedia *)media
+    requestEnqueued:(void (^)(void))requestEnqueued
+            success:(void (^)(RemoteMedia *remoteMedia))success
+            failure:(void (^)(NSError *error))failure;
 
 @end
-
-NS_ASSUME_NONNULL_END
