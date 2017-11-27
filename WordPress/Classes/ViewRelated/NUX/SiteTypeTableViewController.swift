aec1c8f00066d33231babaa37ded2f9021e3a1ba
Stephenie Harris <stephenie@Stephenies-MacBook-Pro.local>
2017-11-27 12:24:03 -0700

    Cleaning up creating Site Type static rows. Adding Site Type enum. Showing empty collection view when type selected.

--- WordPress/Classes/ViewRelated/NUX/SiteTypeTableViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/SiteTypeTableViewController.swift
@@ -47,44 +47,56 @@ class SiteTypeTableViewController: UITableViewController, LoginWithLogoAndHelpVi
         let instr2 = NSLocalizedString("Choose an option below:", comment: "Choose site type prompt.")
         let instructionRow = InstructionRow(step: step, instr1: instr1, instr2: instr2, action: nil)
 
-        let blog = NSLocalizedString("Start with a Blog", comment: "Start with site type.")
-        let blogDescr = NSLocalizedString("To share your ideas, stories and photographs with your followers.", comment: "Site type description.")
-        let blogImage = UIImage(named: "site-creation-blog")
-        let blogRow = SiteTypeRow(startWith: blog, typeDescr: blogDescr, typeImage: blogImage, action: siteTypeRowSelected("Blog"))
-
-        let website = NSLocalizedString("Start with a Website", comment: "Start with site type.")
-        let websiteDescr = NSLocalizedString("To promote your business or brand, and connect with your audience.", comment: "Site type description.")
-        let websiteImage = UIImage(named: "site-creation-website")
-        let websiteRow = SiteTypeRow(startWith: website, typeDescr: websiteDescr, typeImage: websiteImage, action: siteTypeRowSelected("Website"))
-
-        let portfolio = NSLocalizedString("Start with a Portfolio", comment: "Start with site type.")
-        let portfolioDescr = NSLocalizedString("To present your creative projects in a visual showcase.", comment: "Site type description.")
-        let portfolioImage = UIImage(named: "site-creation-portfolio")
-        let portfolioRow = SiteTypeRow(startWith: portfolio, typeDescr: portfolioDescr, typeImage: portfolioImage, action: siteTypeRowSelected("Portfolio"))
-
         return ImmuTable(sections: [
             ImmuTableSection(
                 rows: [
                     instructionRow,
-                    blogRow,
-                    websiteRow,
-                    portfolioRow
+                    createRowFor(.blog),
+                    createRowFor(.website),
+                    createRowFor(.portfolio)
                 ])
             ])
     }
 
+    private func createRowFor(_ siteType: SiteType) -> SiteTypeRow {
+        let descr: String = {
+            switch siteType {
+            case .blog:
+                return "To share your ideas, stories and photographs with your followers."
+            case .website:
+                return "To promote your business or brand, and connect with your audience."
+            case .portfolio:
+                return "To present your creative projects in a visual showcase."
+            }
+        }()
+
+        let typeTitle = NSLocalizedString("Start with a \(siteType.rawValue.capitalized)", comment: "Start with site type.")
+        let typeDescr = NSLocalizedString(descr, comment: "Site type description.")
+        let typeImage = UIImage(named: "site-creation-\(siteType)")
+
+        return SiteTypeRow(siteType: siteType, startWith: typeTitle, typeDescr: typeDescr, typeImage: typeImage, action: siteTypeRowSelected())
+    }
+
+
     // MARK: - Row Handler
 
-    func siteTypeRowSelected(_ siteType: String) -> ImmuTableAction {
+    private func siteTypeRowSelected() -> ImmuTableAction {
         return { [unowned self] row in
-            self.tableView.deselectSelectedRowWithAnimation(true)
-            let message = "'\(siteType)' selected.\nThis is a work in progress. If you need to create a site, disable the siteCreation feature flag."
-            let alertController = UIAlertController(title: nil,
-                                                    message: message,
-                                                    preferredStyle: .alert)
-            alertController.addDefaultActionWithTitle("OK")
-            self.present(alertController, animated: true, completion: nil)
+            self.performSegue(withIdentifier: "showThemeSelection", sender: row)
         }
+    }
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        tableView.deselectSelectedRowWithAnimation(true)
+
+        guard let tableRow = sender as? SiteTypeRow else {
+            return
+        }
+
+        let selectedType = tableRow.siteType
+        // TODO: send to destination controller
     }
 
     // MARK: - Misc
