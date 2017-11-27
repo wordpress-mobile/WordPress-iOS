1fbd3212e8174cc4f9b97025e6dd09f807a797c9
Matt Bumgardner <matt.bumgardner@gmail.com>
2017-11-27 09:27:02 -0600

    Overloaded MediaServiceRemoteREST uploadMedia to include requestEnqueued block.

--- WordPressKit/WordPressKit/MediaServiceRemoteREST.h
+++ WordPressKit/WordPressKit/MediaServiceRemoteREST.h
@@ -8,6 +8,12 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (RemoteMedia *)remoteMediaFromJSONDictionary:(NSDictionary *)jsonMedia;
 
+- (void)uploadMedia:(nonnull RemoteMedia *)media
+           progress:(NSProgress * __nullable __autoreleasing * __nullable)progress
+    requestEnqueued:(nullable void (^)(void))requestEnqueued
+            success:(nullable void (^)(RemoteMedia *remoteMedia))success
+            failure:(nullable void (^)(NSError *error))failure;
+
 @end
 
 NS_ASSUME_NONNULL_END
