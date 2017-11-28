3826c9b28277dc3c58fa5aa108788898d7219217
Cesar Tardaguila <ctarda@me.com>
2017-11-28 16:50:16 +0800

    Add PostTag to the bridging header

--- WordPress/Classes/System/WordPress-Bridging-Header.h
+++ WordPress/Classes/System/WordPress-Bridging-Header.h
@@ -66,6 +66,7 @@
 #import "PostService.h"
 #import "PostServiceOptions.h"
 #import "PostSettingsViewController.h"
+#import "PostTag.h"
 #import "PostTagService.h"
 #import "PrivateSiteURLProtocol.h"
 
