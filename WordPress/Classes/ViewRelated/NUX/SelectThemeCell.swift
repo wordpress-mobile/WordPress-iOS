f17c3d0e7d23dfec1a45cf164f7321c95e18ef21
Stephenie Harris <stephenie@Stephenies-MacBook-Pro.local>
2017-11-29 13:12:47 -0700

    Fetching themes. Using hard-coded names until the endpoint is ready. Adding themes to collection view. Using StyleGuide for CV cell layout. Updating Step instruction with new text.

--- WordPress/Classes/ViewRelated/NUX/SelectThemeCell.swift
+++ WordPress/Classes/ViewRelated/NUX/SelectThemeCell.swift
@@ -2,10 +2,23 @@ import UIKit
 
 class SelectThemeCell: UICollectionViewCell {
 
+    // MARK: - Properties
+
     open static let reuseIdentifier = "themeSelectionCell"
+    fileprivate var placeholderImage = UIImage(named: "theme-loading")
+    fileprivate typealias Styles = WPStyleGuide.Themes
 
     @IBOutlet weak var themeImageView: UIImageView!
     @IBOutlet weak var themeTitleLabel: UILabel!
+    @IBOutlet weak var activityView: UIActivityIndicatorView!
+
+    var displayTheme: Theme? {
+        didSet {
+            updateTheme()
+        }
+    }
+
+    // MARK: - Init
 
     override open func awakeFromNib() {
         super.awakeFromNib()
@@ -14,4 +27,50 @@ class SelectThemeCell: UICollectionViewCell {
     override open func prepareForReuse() {
         super.prepareForReuse()
     }
+
+    // MARK: - Display
+
+    private func updateTheme() {
+        guard let displayTheme = displayTheme else {
+            return
+        }
+
+        themeTitleLabel.text = displayTheme.name
+
+        if let imageUrl = displayTheme.screenshotUrl, !imageUrl.isEmpty {
+            refreshScreenshotImage(imageUrl)
+        } else {
+            showPlaceholder()
+        }
+    }
+
+    private func refreshScreenshotImage(_ imageUrl: String) {
+        themeImageView.backgroundColor = Styles.placeholderColor
+        activityView.startAnimating()
+        themeImageView.downloadImage(URL(string: imageUrl),
+                                placeholderImage: nil,
+                                success: { [weak self] (image: UIImage) in
+                                    self?.showScreenshot()
+            }, failure: { [weak self] (error: Error?) in
+                if let error = error as NSError?, error.domain == NSURLErrorDomain && error.code == NSURLErrorCancelled {
+                    return
+                }
+                DDLogError("Error loading theme screenshot: \(String(describing: error?.localizedDescription))")
+                self?.showPlaceholder()
+        })
+    }
+
+    private func showScreenshot() {
+        themeImageView.contentMode = .scaleAspectFit
+        themeImageView.backgroundColor = UIColor.clear
+        activityView.stopAnimating()
+    }
+
+    private func showPlaceholder() {
+        themeImageView.contentMode = .center
+        themeImageView.backgroundColor = Styles.placeholderColor
+        themeImageView.image = placeholderImage
+        activityView.stopAnimating()
+    }
+
 }
--- WordPress/Classes/ViewRelated/NUX/ThemeSelectionViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/ThemeSelectionViewController.swift
@@ -1,17 +1,32 @@
 import UIKit
 
-class ThemeSelectionViewController: UICollectionViewController, LoginWithLogoAndHelpViewController {
+class ThemeSelectionViewController: UICollectionViewController, LoginWithLogoAndHelpViewController, NSFetchedResultsControllerDelegate, UICollectionViewDelegateFlowLayout {
 
     // MARK: - Properties
 
     var siteType: SiteType!
-    fileprivate typealias Styles = WPStyleGuide.Themes
+    private typealias Styles = WPStyleGuide.Themes
+
+    private let themeService = ThemeService(managedObjectContext: ContextManager.sharedInstance().mainContext)
+
+    private lazy var themesController: NSFetchedResultsController<NSFetchRequestResult> = {
+        return self.createThemesFetchedResultsController()
+    }()
+
+    private var themeCount: NSInteger {
+        return themesController.fetchedObjects?.count ?? 0
+    }
 
     // MARK: - View
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        configureView()
+        fetchThemes()
+    }
+
+    private func configureView() {
         WPStyleGuide.configureColors(for: view, collectionView: collectionView)
         _ = addHelpButtonToNavController()
         navigationItem.title = NSLocalizedString("Create New Site", comment: "Create New Site title.")
@@ -31,24 +46,78 @@ class ThemeSelectionViewController: UICollectionViewController, LoginWithLogoAnd
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        return themeCount
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SelectThemeCell.reuseIdentifier, for: indexPath) as! SelectThemeCell
-
+        cell.displayTheme = themeAtIndexPath(indexPath)
         return cell
     }
 
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let parentViewWidth = collectionView.frame.size.width
+    // MARK: - UICollectionViewDelegateFlowLayout
 
-        return Styles.cellSizeForFrameWidth(parentViewWidth)
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return Styles.cellSizeForFrameWidth(collectionView.frame.size.width)
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-            return Styles.themeMargins
+        return Styles.themeMargins
+    }
+
+    // MARK: - Theme Fetching
+
+    private func createThemesFetchedResultsController() -> NSFetchedResultsController<NSFetchRequestResult> {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: Theme.entityName())
+        fetchRequest.fetchBatchSize = 4
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "order", ascending: true)]
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                             managedObjectContext: self.themeService.managedObjectContext,
+                                             sectionNameKeyPath: nil,
+                                             cacheName: nil)
+        frc.delegate = self
+
+        return frc
+    }
+
+    private func fetchThemes() {
+        do {
+            themesController.fetchRequest.predicate = themesPredicate()
+            try themesController.performFetch()
+        } catch {
+            DDLogError("Error fetching themes: \(error)")
+        }
+    }
+
+    private func themesPredicate() -> NSPredicate? {
+        let blogThemes = ["Independent Publisher 2", "Penscratch 2", "Intergalactic 2", "Libre 2"]
+        let websiteThemes = ["Radcliffe 2", "Karuna", "Dara", "Twenty Seventeen"]
+        let portfolioThemes = ["Altofocus", "Rebalance", "Sketch", "Lodestar"]
+
+        var themes: [String]
+        switch siteType {
+        case .blog: themes = blogThemes
+        case .website: themes = websiteThemes
+        case .portfolio: themes = portfolioThemes
+        case .none:
+            return nil
+        case .some(_):
+            return nil
+        }
+
+        var predicates = [NSPredicate]()
+        for themeName in themes {
+            predicates.append(NSPredicate(format: "name = %@", themeName))
+        }
+
+        return NSCompoundPredicate(orPredicateWithSubpredicates: predicates)
+    }
+
+    // MARK: - Helpers
+
+    private func themeAtIndexPath(_ indexPath: IndexPath) -> Theme? {
+        return themesController.object(at: IndexPath(row: indexPath.row, section: 0)) as? Theme
     }
 
     // MARK: - Misc
