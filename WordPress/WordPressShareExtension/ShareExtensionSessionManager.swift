07ba12a78a1fa8b1c534597b8c551a6f68b93037
Matt Bumgardner <matt.bumgardner@gmail.com>
2017-11-29 10:51:42 -0600

    [WIP] ShareExtensionSessionManager updates

--- WordPress/WordPressShareExtension/ShareExtensionSessionManager.swift
+++ WordPress/WordPressShareExtension/ShareExtensionSessionManager.swift
@@ -59,16 +59,45 @@ import CoreData
 
     // MARK: - Private Functions
 
-    fileprivate func markAllSessionUploadOperationsWith(_ status: UploadOperation.UploadStatus) {
+    /// For the provided group ID:
+    ///
+    ///   1. Find the (not completed) post upload op (there should be one one)
+    ///   2. Find all of the completed media upload ops
+    ///   3. Append the media to the postContent property of the post upload op
+    ///   4. Save the post upload op and return it
+    ///
+    /// - Parameter groupID: Group ID representing all of the media upload ops and post upload op
+    /// - Returns: The updated RemotePost or nil
+    fileprivate func appendMediaToPostForGroupID(_ groupID: String) -> RemotePost? {
+        var remotePost: RemotePost?
+        if let postUploadOp = fetchPostUploadOpForGroup(groupID),
+            let mediaUploadOps = fetchMediaUploadOpsForGroup(groupID) {
+            // TODO: finish this!
+        }
+
+        return remotePost
+    }
+
+    /// Update *all* upload ops associated with the provided group ID with a status.
+    ///
+    /// - Parameters:
+    ///   - status: status to set
+    ///   - groupID: group ID for a set of upload ops
+    ///
+    fileprivate func markAllUploadOperationsWith(_ status: UploadOperation.UploadStatus, for groupID: String) {
         // Mark the items as complete in the shared core data instance
-        let predicate = NSPredicate(format: "(backgroundSessionIdentifier == %@)", backgroundSessionIdentifier)
+        let predicate = NSPredicate(format: "(groupID == %@)", groupID)
         for uploadOp in coreDataStack.managedContext.allObjects(ofType: UploadOperation.self, matching: predicate) {
-            uploadOp.currentStatus = .Complete
+            uploadOp.currentStatus = status
         }
         coreDataStack.saveContext()
     }
 
-    fileprivate func removeAllSessionTempFiles() {
+    /// Nukes all media files associated with the current NSURLSession. *Be careful* with this one because
+    /// some of the upload operations could still be in flight or errored out (which means you
+    /// may want to retry another time).
+    ///
+    fileprivate func removeAllMediaFilesForCurrentSession() {
         let predicate = NSPredicate(format: "(backgroundSessionIdentifier == %@ AND isMedia == true)", backgroundSessionIdentifier)
         for uploadOp in coreDataStack.managedContext.allObjects(ofType: UploadOperation.self, matching: predicate) {
             if let fileName = uploadOp.fileName {
@@ -77,22 +106,54 @@ import CoreData
         }
     }
 
-    fileprivate func fetchSessionPost(with groupID: String) -> RemotePost? {
-        var remotePost: RemotePost?
+    /// Fetch the upload op that represents a post for a given group ID.
+    ///
+    /// NOTE: There will only ever be one post associated with a group of upload ops.
+    ///
+    /// - Parameter groupID: group ID for a set of upload ops
+    /// - Returns: UploadOperation or nil
+    fileprivate func fetchPostUploadOpForGroup(_ groupID: String) -> UploadOperation? {
         var uploadOp: UploadOperation?
-        let predicate = NSPredicate(format: "(backgroundSessionIdentifier == %@ AND isMedia == false)", backgroundSessionIdentifier)
-        uploadOp = coreDataStack.managedContext.firstObject(ofType: UploadOperation.self, matching: predicate)
-        remotePost = uploadOp?.remotePost
-
-        return remotePost
-    }
-
-    fileprivate func fetchSessionUploadOp(taskID: Int) -> UploadOperation? {
-        var uploadOp: UploadOperation?
-        let predicate = NSPredicate(format: "(backgroundSessionTaskID == %d AND backgroundSessionIdentifier == %@)", taskID, backgroundSessionIdentifier)
+        let predicate = NSPredicate(format: "(groupID == %@ AND isMedia == false)", backgroundSessionIdentifier)
         uploadOp = coreDataStack.managedContext.firstObject(ofType: UploadOperation.self, matching: predicate)
 
         return uploadOp
+    }
+
+    /// Fetch the upload op that represents a post for a given URLSession taskIdentifier
+    ///
+    /// - Parameter taskIdentifier: the taskIdentifier from a URLSessionTask
+    /// - Returns: UploadOperation or nil
+    fileprivate func fetchSessionUploadOpForTask(taskIdentifier: Int) -> UploadOperation? {
+        var uploadOp: UploadOperation?
+        let predicate = NSPredicate(format: "(backgroundSessionTaskID == %d AND backgroundSessionIdentifier == %@)", taskIdentifier, backgroundSessionIdentifier)
+        uploadOp = coreDataStack.managedContext.firstObject(ofType: UploadOperation.self, matching: predicate)
+
+        return uploadOp
+    }
+
+    /// Fetch all of the media upload ops for the provided group id
+    ///
+    /// - Parameter groupID: group ID for a set of media upload ops
+    /// - Returns: An array of UploadOperations or nil
+    fileprivate func fetchMediaUploadOpsForGroup(_ groupID: String) -> [UploadOperation]? {
+        var mediaUploadOps: [UploadOperation]
+        let predicate = NSPredicate(format: "(groupID == %@ AND isMedia == true)", groupID)
+        mediaUploadOps = coreDataStack.managedContext.allObjects(ofType: UploadOperation.self, matching: predicate)
+
+        return mediaUploadOps
+    }
+
+    /// Fetches the group ID for the provided session ID.
+    ///
+    /// - Parameter sessionID: the session ID
+    /// - Returns: group ID or nil if session does not have an associated group
+    fileprivate func fetchGroupID(for sessionID: String) -> String? {
+        var uploadOp: UploadOperation?
+        let predicate = NSPredicate(format: "(backgroundSessionIdentifier == %@)", sessionID)
+        uploadOp = coreDataStack.managedContext.firstObject(ofType: UploadOperation.self, matching: predicate)
+
+        return uploadOp?.groupID
     }
 }
 
@@ -111,21 +172,33 @@ extension ShareExtensionSessionManager: URLSessionDelegate {
                 completionHandler()
             }
         }
-        DDLogInfo("Completed background media uploading for session \(session).")
+        DDLogInfo("Completed background uploading of all media for session \(session).")
+
+        // TODO: Check to see if there is no media OR no post upload ops. In these cases, no need to try to do anything here.
+
+        guard let groupID = fetchGroupID(for: self.backgroundSessionIdentifier), !groupID.isEmpty else {
+            DDLogError("Unable to find the Group ID for session with ID \(self.backgroundSessionIdentifier).")
+            return
+        }
+
+        guard let postUploadOp = appendMediaToPostForGroupID(groupID) else {
+            DDLogError("Unable to append media to the post for Group ID \(groupID).")
+            return
+        }
+
+        // TODO: POST the post!
+
+        // TODO: Cleanup?
     }
 
-    // Tells the URL session that the session has been invalidated. If you invalidate a session by calling its
-    // finishTasksAndInvalidate() method, the session waits until after the final task in the session finishes or
-    // fails before calling this delegate method. If you call the invalidateAndCancel() method, the session calls
-    // this delegate method immediately.
+    // Tells the delegate that the session has been invalidated.
     func urlSession(_ session: URLSession, didBecomeInvalidWithError error: Error?) {
         if let error = error {
-            DDLogError("Background session invalidated with error. Session:\(session) Error:\(error).")
+            DDLogError("Background session invalidated by the system with error. Session:\(session) Error:\(error).")
+            WPAppAnalytics.track(.shareExtensionError, error: error)
         } else {
             DDLogError("Background session explicitly invalidated. Session:\(session).")
         }
-        WPAppAnalytics.track(.shareExtensionError, error: error)
-        markAllSessionUploadOperationsWith(.Error)
     }
 }
 
@@ -135,31 +208,30 @@ extension ShareExtensionSessionManager: URLSessionTaskDelegate {
     
     // Periodically informs the delegate of the progress of sending body content to the server.
     func urlSession(_ session: URLSession, task: URLSessionTask, didSendBodyData bytesSent: Int64, totalBytesSent: Int64, totalBytesExpectedToSend: Int64) {
-        DDLogInfo("didSendBodyData [session:\(session) task:\(task.debugDescription) bytesSent:\(bytesSent) totalBytesSent:\(totalBytesSent) totalBytesExpectedToSend:\(totalBytesExpectedToSend)]")
+        DDLogDebug("didSendBodyData [session:\(session) task:\(task.debugDescription) bytesSent:\(bytesSent) totalBytesSent:\(totalBytesSent) totalBytesExpectedToSend:\(totalBytesExpectedToSend)]")
     }
 
     // Tells the delegate that the task finished transferring data. Server errors are not reported through the
     // error parameter. The only errors your delegate receives through the error parameter are client-side errors,
     // such as being unable to resolve the hostname or connect to the host.
     func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
-        if let error = error {
-            DDLogError("Background session task completed with error. Session:\(session) Task:\(task.debugDescription) Error:\(error).")
+        guard let uploadOp = fetchSessionUploadOpForTask(taskIdentifier: task.taskIdentifier) else {
+            DDLogError("Background session task was not found in shared database. Session:\(session) Task:\(task.debugDescription).")
             return
         }
 
-        if let uploadOp = fetchSessionUploadOp(taskID: task.taskIdentifier) {
+        if let error = error {
+            DDLogError("Background session task completed with error. Session:\(session) Task:\(task.debugDescription) Error:\(error).")
+            uploadOp.currentStatus = .Error
+            WPAppAnalytics.track(.shareExtensionError, error: error)
+        } else {
             uploadOp.currentStatus = .Complete
-            coreDataStack.saveContext()
-
-            // TODO: embed the media into the post content and then upload in another bg session
-            // var remotePost = fetchSessionPost(with: uploadOp.groupID)
-
             if let fileName = uploadOp.fileName {
                 ShareMediaFileManager.shared.removeFromUploadDirectory(fileName: fileName)
             }
+            DDLogInfo("Background session task completed. Session:\(session) Task:\(task.debugDescription).")
         }
-
-        DDLogInfo("Background session task completed. Session:\(session) Task:\(task.debugDescription).")
+        coreDataStack.saveContext()
     }
 }
 
@@ -167,6 +239,7 @@ extension ShareExtensionSessionManager: URLSessionTaskDelegate {
 
 extension ShareExtensionSessionManager: URLSessionDataDelegate {
 
+    // Tells the delegate that the data task has received some of the expected data.
     func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
         DDLogDebug("Received server response data for task:\(dataTask.debugDescription).")
         guard let object = try? JSONSerialization.jsonObject(with: data, options: []),
@@ -177,7 +250,7 @@ extension ShareExtensionSessionManager: URLSessionDataDelegate {
                 return
         }
 
-        if let uploadOp = fetchSessionUploadOp(taskID: dataTask.taskIdentifier) {
+        if let uploadOp = fetchSessionUploadOpForTask(taskIdentifier: dataTask.taskIdentifier) {
             if let remoteMediaID = mediaDict["ID"] as? NSNumber {
                 uploadOp.remoteMediaID = remoteMediaID.int64Value
             }
