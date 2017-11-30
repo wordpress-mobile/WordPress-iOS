3804fb83011f42de294c4e1278701edee56a2a9c
Cesar Tardaguila <ctarda@me.com>
2017-11-30 10:35:20 +0800

    Add a SiteTagsViewController in ViewRelated>SiteManagement

---
+++ WordPress/Classes/ViewRelated/Blog/SiteManagement/SiteTagsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SiteTagsViewController.swift
+//  WordPress
+//
+//  Created by Cesar Tardaguila Moro on 2017-11-30.
+//  Copyright © 2017 WordPress. All rights reserved.
+//
+
+import Foundation
