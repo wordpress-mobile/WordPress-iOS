ee32e87255cfd316b945636f6233e7d1a8abbf6a
Cesar Tardaguila <ctarda@me.com>
2017-11-30 14:38:18 +0800

    Add the view controller to present a single tag

---
+++ WordPress/Classes/ViewRelated/Blog/SiteManagement/SiteTagViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SiteTagViewController.swift
+//  WordPress
+//
+//  Created by Cesar Tardaguila Moro on 2017-11-30.
+//  Copyright © 2017 WordPress. All rights reserved.
+//
+
+import Foundation
+
+final class SiteTagViewController: UIViewController {
+    
+}
