84a626d46331718301a7fefce8ad17400cf9051f
James Frost <james.frost@automattic.com>
2017-11-30 10:20:36 +0000

    AppDelegate: Converted debug & logging related methods to Swift

--- WordPress/Classes/System/WordPressAppDelegate+Swift.swift
+++ WordPress/Classes/System/WordPressAppDelegate+Swift.swift
@@ -1,4 +1,128 @@
 import Foundation
+import UIDeviceIdentifier
+import CocoaLumberjack
+
+// MARK: - Helpers
+
+extension WordPressAppDelegate {
+    @objc var noSelfHostedBlogs: Bool {
+        let context = ContextManager.sharedInstance().mainContext
+        let blogService = BlogService(managedObjectContext: context)
+
+        return blogService.blogCountSelfHosted() == 0
+    }
+
+    @objc var noWordPressDotComAccount: Bool {
+        return !AccountHelper.isDotcomAvailable()
+    }
+}
+
+// MARK: - Debugging
+
+extension WordPressAppDelegate {
+    @objc func printDebugLaunchInfoWithLaunchOptions(_ launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) {
+        let unknown = "Unknown"
+
+        let device = UIDevice.current
+        let crashCount = UserDefaults.standard.integer(forKey: "crashCount")
+
+        let extraDebug = UserDefaults.standard.bool(forKey: "extra_debug")
+
+        let context = ContextManager.sharedInstance().mainContext
+        let blogService = BlogService(managedObjectContext: context)
+        let blogs = blogService.blogsForAllAccounts()
+
+        let accountService = AccountService(managedObjectContext: context)
+        let account = accountService.defaultWordPressComAccount()
+
+        let detailedVersionNumber = Bundle(for: type(of: self)).detailedVersionNumber() ?? unknown
+
+        DDLogInfo("===========================================================================")
+        DDLogInfo("Launching WordPress for iOS \(detailedVersionNumber)...")
+        DDLogInfo("Crash count: \(crashCount)")
+
+        #if DEBUG
+            DDLogInfo("Debug mode:  Debug")
+        #else
+            DDLogInfo("Debug mode:  Production")
+        #endif
+
+        DDLogInfo("Extra debug: \(extraDebug ? "YES" : "NO")")
+
+        let devicePlatform = UIDeviceHardware.platformString() ?? unknown
+        let architecture = UIDeviceHardware.platform() ?? unknown
+        let languages = UserDefaults.standard.array(forKey: "AppleLanguages")
+        let currentLanguage = languages?.first ?? unknown
+        let udid = device.wordPressIdentifier() ?? unknown
+
+        DDLogInfo("Device model: \(devicePlatform) (\(architecture))")
+        DDLogInfo("OS:        \(device.systemName), \(device.systemVersion)")
+        DDLogInfo("Language:  \(currentLanguage)")
+        DDLogInfo("UDID:      \(udid)")
+        DDLogInfo("APN token: \(PushNotificationsManager.shared.deviceToken ?? "None")")
+        DDLogInfo("Launch options: \(String(describing: launchOptions ?? [:]))")
+
+        if let account = account,
+            let username = account.username,
+            let userID = account.userID,
+            let verificationStatus = account.verificationStatus() {
+            DDLogInfo("wp.com account: \(username) (ID: \(userID)) (\(verificationStatus))")
+        }
+
+        if let blogs = blogs as? [Blog], blogs.count > 0 {
+            DDLogInfo("All blogs on device:")
+            blogs.forEach({ DDLogInfo("\($0.logDescription())") })
+        } else {
+            DDLogInfo("No blogs configured on device.")
+        }
+
+        DDLogInfo("===========================================================================")
+    }
+
+    @objc func removeCredentialsForDebug() {
+        #if DEBUG
+            URLCredentialStorage.shared.allCredentials.forEach { protectionSpace, credentials in
+                credentials.forEach { key, credential in
+                    let user = credential.user ?? "Unknown"
+                    DDLogVerbose("Removing credential \(user) for \(protectionSpace.host)")
+                    URLCredentialStorage.shared.remove(credential, for: protectionSpace)
+                }
+            }
+        #endif
+    }
+
+    @objc func toggleExtraDebuggingIfNeeded() {
+        if noSelfHostedBlogs && noWordPressDotComAccount {
+            // When there are no blogs in the app the settings screen is unavailable.
+            // In this case, enable extra_debugging by default to help troubleshoot any issues.
+            guard UserDefaults.standard.object(forKey: "orig_extra_debug") == nil else {
+                // Already saved. Don't save again or we could loose the original value.
+                return;
+            }
+
+            let origExtraDebug = UserDefaults.standard.bool(forKey: "extra_debug") ? "YES" : "NO"
+            UserDefaults.standard.set(origExtraDebug, forKey: "orig_extra_debug")
+            UserDefaults.standard.set(true, forKey: "extra_debug")
+            WordPressAppDelegate.setLogLevel(.verbose)
+            UserDefaults.resetStandardUserDefaults()
+        } else {
+            guard let origExtraDebug = UserDefaults.standard.string(forKey: "orig_extra_debug") else {
+                return
+            }
+
+            let origExtraDebugValue = (origExtraDebug as NSString).boolValue
+
+            // Restore the original setting and remove orig_extra_debug
+            UserDefaults.standard.set(origExtraDebugValue, forKey: "extra_debug")
+            UserDefaults.standard.removeObject(forKey: "orig_extra_debug")
+            UserDefaults.resetStandardUserDefaults()
+
+            if (origExtraDebugValue) {
+                WordPressAppDelegate.setLogLevel(.verbose)
+            }
+        }
+    }
+}
 
 // MARK: - Local Notification Helpers
 
