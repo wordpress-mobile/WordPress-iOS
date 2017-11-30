562268721790d26bf5882afde1d83f0bcc9c9fee
Cesar Tardaguila <ctarda@me.com>
2017-11-30 15:00:56 +0800

    Remove my name from the headers . Sorry!

--- WordPress/Classes/Models/PostTag+Comparable.swift
+++ WordPress/Classes/Models/PostTag+Comparable.swift
@@ -1,11 +1,3 @@
-//
-//  PostTag+Comparable.swift
-//  WordPress
-//
-//  Created by Cesar Tardaguila Moro on 2017-11-30.
-//  Copyright © 2017 WordPress. All rights reserved.
-//
-
 
 extension PostTag: Comparable {
     public static func <(lhs: PostTag, rhs: PostTag) -> Bool {
--- WordPress/Classes/ViewRelated/Blog/SiteManagement/SiteTagViewController.swift
+++ WordPress/Classes/ViewRelated/Blog/SiteManagement/SiteTagViewController.swift
@@ -1,12 +1,4 @@
-//
-//  SiteTagViewController.swift
-//  WordPress
-//
-//  Created by Cesar Tardaguila Moro on 2017-11-30.
-//  Copyright © 2017 WordPress. All rights reserved.
-//
-
-import Foundation
+import UIKit
 
 final class SiteTagViewController: UITableViewController {
     private let blog: Blog
