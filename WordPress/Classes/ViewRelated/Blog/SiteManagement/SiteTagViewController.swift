6866dfb670a86a751879ecc4cf5d2948c06e65ed
Cesar Tardaguila <ctarda@me.com>
2017-11-30 14:47:19 +0800

    Navigate to the SingleTag view

--- WordPress/Classes/ViewRelated/Blog/SiteManagement/SiteTagViewController.swift
+++ WordPress/Classes/ViewRelated/Blog/SiteManagement/SiteTagViewController.swift
@@ -8,6 +8,19 @@
 
 import Foundation
 
-final class SiteTagViewController: UIViewController {
+final class SiteTagViewController: UITableViewController {
+    private let blog: Blog
+    private let tagsService: PostTagService
+    private let tag: PostTag
     
+    public init(blog: Blog, tag: PostTag, tagsService: PostTagService) {
+        self.blog = blog
+        self.tag = tag
+        self.tagsService = tagsService
+        super.init(style: .grouped)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
