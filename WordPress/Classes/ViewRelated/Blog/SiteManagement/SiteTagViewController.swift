1d442a5b9bbad5775b31b8f567a7a5f65056e649
Cesar Tardaguila <ctarda@me.com>
2017-12-02 18:50:55 +0800

    That should remove all whitespacing

--- WordPress/Classes/ViewRelated/Blog/SiteManagement/SiteTagViewController.swift
+++ WordPress/Classes/ViewRelated/Blog/SiteManagement/SiteTagViewController.swift
@@ -3,13 +3,13 @@ import UIKit
 final class SiteTagViewController: UITableViewController {
     private let blog: Blog
     private let tag: PostTag
-    
+
     public init(blog: Blog, tag: PostTag) {
         self.blog = blog
         self.tag = tag
         super.init(style: .grouped)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
