43bf20a8919b4e7b9305ecbb8918ba78ac84588f
Cesar Tardaguila <ctarda@me.com>
2017-12-05 14:59:36 +0800

    Start building sections and section headers as an enumeration

--- WordPress/Classes/ViewRelated/Blog/SiteManagement/SiteTagViewController.swift
+++ WordPress/Classes/ViewRelated/Blog/SiteManagement/SiteTagViewController.swift
@@ -4,6 +4,34 @@ final class SiteTagViewController: UITableViewController {
     private let blog: Blog
     private let tag: PostTag
 
+    private enum Sections: Int, CustomStringConvertible {
+        case name
+        case description
+        case sectionCount
+
+        static var count: Int {
+            return sectionCount.rawValue
+        }
+
+        static func section(for index: Int) -> Sections {
+            guard index < sectionCount.rawValue else {
+                return .name
+            }
+            return Sections(rawValue: index)!
+        }
+
+        var description: String {
+            switch self {
+            case .name:
+                return NSLocalizedString("Tag", comment: "Section header for tag name in Tag Details View.").uppercased()
+            case .description:
+                return NSLocalizedString("Description", comment: "Section header for tag name in Tag Details View.").uppercased()
+            case .sectionCount:
+                return ""
+            }
+        }
+    }
+
     public init(blog: Blog, tag: PostTag) {
         self.blog = blog
         self.tag = tag
