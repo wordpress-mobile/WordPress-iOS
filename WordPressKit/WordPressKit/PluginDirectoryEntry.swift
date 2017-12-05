589ad25dd22727bc9142373999ac8ab3dff38aef
Jorge Bernal <jbernal@gmail.com>
2017-12-05 16:52:23 +0100

    Store lastUpdated date for plugin entries

--- WordPressKit/WordPressKit/PluginDirectoryEntry.swift
+++ WordPressKit/WordPressKit/PluginDirectoryEntry.swift
@@ -4,14 +4,16 @@ public struct PluginDirectoryEntry {
     public let name: String
     public let slug: String
     public let version: String
+    public let lastUpdated: Date
     public let icon: URL?
 }
 
 extension PluginDirectoryEntry: Decodable {
-    private enum CodingKeys : CodingKey {
+    private enum CodingKeys: String, CodingKey {
         case name
         case slug
         case version
+        case lastUpdated = "last_updated"
         case icons
     }
 
@@ -20,6 +22,7 @@ extension PluginDirectoryEntry: Decodable {
         name = try container.decode(String.self, forKey: .name)
         slug = try container.decode(String.self, forKey: .slug)
         version = try container.decode(String.self, forKey: .version)
+        lastUpdated = try container.decode(Date.self, forKey: .lastUpdated)
         let icons = try? container.decodeIfPresent([String: String].self, forKey: .icons)
         icon = icons??["2x"].flatMap(URL.init(string:))
     }
--- WordPressKit/WordPressKit/PluginDirectoryServiceRemote.swift
+++ WordPressKit/WordPressKit/PluginDirectoryServiceRemote.swift
@@ -2,29 +2,41 @@ import Foundation
 
 public struct PluginDirectoryServiceRemote {
     enum Errors: Error {
-        case decodingError
         case noData
     }
 
     let baseURL = URL(string: "https://api.wordpress.org/plugins/info/1.0/")!
+    static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "YYYY-MM-dd h:mma z"
+        return formatter
+    }()
 
     public func getPluginInformation(slug: String, success: @escaping (PluginDirectoryEntry) -> Void, failure: @escaping (Error) -> Void) {
         let url = baseURL
             .appendingPathComponent(slug)
             .appendingPathExtension("json")
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                failure(error)
-            } else if let data = data {
-                guard let plugin = try? JSONDecoder().decode(PluginDirectoryEntry.self, from: data) else {
-                    failure(Errors.decodingError)
-                    return
+            do {
+                if let error = error {
+                    throw error
+                } else if let data = data {
+                    success(try self.pluginEntry(fromData: data))
+                } else {
+                    throw Errors.noData
                 }
-                success(plugin)
-            } else {
-                failure(Errors.noData)
+            } catch {
+                failure(error)
             }
         }
         task.resume()
+    }
+}
+
+extension PluginDirectoryServiceRemote {
+    func pluginEntry(fromData data: Data) throws -> PluginDirectoryEntry {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .formatted(PluginDirectoryServiceRemote.dateFormatter)
+        return try decoder.decode(PluginDirectoryEntry.self, from: data)
     }
 }
