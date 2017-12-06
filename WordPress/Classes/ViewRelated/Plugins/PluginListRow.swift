69cd14b50daaf1fd3fb2ecc1d1825daae0bac10a
Jorge Bernal <jbernal@gmail.com>
2017-12-06 12:31:12 +0100

    Resize plugin icons before showing them

--- WordPress/Classes/ViewRelated/Plugins/PluginListRow.swift
+++ WordPress/Classes/ViewRelated/Plugins/PluginListRow.swift
@@ -6,17 +6,14 @@ struct PluginListRow: ImmuTableRow {
     let state: String
     let iconURL: URL?
     let action: ImmuTableAction?
+    private let iconSize = CGSize(width: 40, height: 40)
 
     func configureCell(_ cell: UITableViewCell) {
         WPStyleGuide.configureTableViewSmallSubtitleCell(cell)
         cell.textLabel?.text = name
         cell.detailTextLabel?.text = state
-        let iconPlaceholder = Gridicon.iconOfType(.plugins)
-        if let iconURL = iconURL {
-            cell.imageView?.setImageWith(iconURL, placeholderImage: iconPlaceholder)
-        } else {
-            cell.imageView?.image = iconPlaceholder
-        }
+        let iconPlaceholder = Gridicon.iconOfType(.plugins, withSize: iconSize)
+        cell.imageView?.downloadResizedImage(iconURL, placeholderImage: iconPlaceholder, pointSize: iconSize)
         cell.selectionStyle = .default
         cell.accessoryType = .disclosureIndicator
     }
