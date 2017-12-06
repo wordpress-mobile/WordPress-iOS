0fc7da9dfaae38feee4f0ed2167adcf126710604
Cesar Tardaguila <ctarda@me.com>
2017-12-06 16:19:20 +0800

    Pass literals for the confirmation alert

--- WordPress/Classes/ViewRelated/Blog/SiteManagement/SiteTagsViewController.swift
+++ WordPress/Classes/ViewRelated/Blog/SiteManagement/SiteTagsViewController.swift
@@ -242,7 +242,18 @@ extension SiteTagsViewController {
 extension SiteTagsViewController {
     fileprivate func navigate(to details: PostTag?) {
         let data = SettingsTitleSubtitleController.Data(title: details?.name, subtitle: details?.tagDescription)
-        let singleTag = SettingsTitleSubtitleController(data: data)
+
+        let confirmationTitle = NSLocalizedString("Delete this tag", comment: "Delete Tag confirmation action title")
+        let confirmationSubtitle = NSLocalizedString("Are you sure you want to delete this tag?", comment: "Message asking for confirmation on tag deletion")
+        let actionTitle = NSLocalizedString("Delete", comment: "Delete")
+        let cancelTitle = NSLocalizedString("Cancel", comment: "Alert dismissal title")
+
+        let confirmation = SettingsTitleSubtitleController.Confirmation(title: confirmationTitle,
+                                                                        subtitle: confirmationSubtitle,
+                                                                        actionTitle: actionTitle,
+                                                                        cancelTitle: cancelTitle)
+        
+        let singleTag = SettingsTitleSubtitleController(data: data, confirmation: confirmation)
         singleTag.setAction { updatedData in
             print("data to delete ", updatedData)
             self.navigationController?.popViewController(animated: true)
