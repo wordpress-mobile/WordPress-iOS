4e497586fe376bf89330f9de367f54f6c0394b61
Jorge Bernal <jbernal@gmail.com>
2017-12-11 12:37:50 +0100

    Fix lint warnings

--- WordPress/Classes/ViewRelated/Plugins/PluginListRow.swift
+++ WordPress/Classes/ViewRelated/Plugins/PluginListRow.swift
@@ -19,9 +19,9 @@ struct PluginListRow: ImmuTableRow {
         cell.imageView?.downloadResizedImage(iconURL, placeholderImage: iconPlaceholder, pointSize: iconSize)
         cell.selectionStyle = .default
         switch updateState {
-        case .available(_):
+        case .available:
             cell.accessoryView = UIImageView(image: #imageLiteral(resourceName: "gridicon-sync-circled"))
-        case .updating(_):
+        case .updating:
             cell.accessoryView = spinningImageView(image: #imageLiteral(resourceName: "gridicon-sync-circled"))
         case .updated:
             cell.accessoryType = .disclosureIndicator
