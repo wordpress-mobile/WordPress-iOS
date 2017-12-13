e96075bc2b9d98ac7284873ac951dd7c8eaa1345
James Frost <james.frost@automattic.com>
2017-12-13 10:20:26 +0000

    NoticePresenter: Remove isPresenting flag

--- WordPress/Classes/ViewRelated/System/NoticePresenter.swift
+++ WordPress/Classes/ViewRelated/System/NoticePresenter.swift
@@ -6,8 +6,6 @@ class NoticePresenter: NSObject {
     private let store: NoticeStore
     private var storeReceipt: Receipt?
 
-    private var isPresenting = false
-
     private init(store: NoticeStore) {
         self.store = store
         super.init()
@@ -22,24 +20,18 @@ class NoticePresenter: NSObject {
     }
 
     private func presentNextNoticeIfAvailable() {
-        if !isPresenting {
-            if let notice = store.nextNotice {
-                present(notice)
-            }
+        if let notice = store.nextNotice {
+            present(notice)
         }
     }
 
     private func present(_ notice: Notice) {
-        isPresenting = true
-
         UIApplication.shared.delegate?.window??.rootViewController?.present(alert(from: notice),
                                                                             animated: true,
                                                                             completion: nil)
     }
 
-    private func dismiss(_ notice: Notice) {
-        isPresenting = false
-
+    private func dismiss() {
         ActionDispatcher.dispatch(NoticeAction.dismiss)
     }
 
@@ -47,13 +39,13 @@ class NoticePresenter: NSObject {
         let alert = UIAlertController(title: notice.title, message: notice.message, preferredStyle: .alert)
 
         alert.addAction(UIAlertAction(title: NSLocalizedString("Dismiss", comment: ""), style: .cancel, handler: { _ in
-            self.dismiss(notice)
+            self.dismiss()
         }))
 
         if let actionTitle = notice.actionTitle,
             let actionHandler = notice.actionHandler {
             alert.addAction(UIAlertAction(title: actionTitle, style: .default, handler: { _ in
-                self.dismiss(notice)
+                self.dismiss()
                 actionHandler()
             }))
         }
