e740b1d067a78b84802f90f1f15f11bb4706fb46
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2017-12-15 20:48:24 +0700

    fix linting issues

--- WordPress/WordPressUITests/MainNavigationTests.swift
+++ WordPress/WordPressUITests/MainNavigationTests.swift
@@ -10,7 +10,7 @@ class MainNavigationTests: XCTestCase {
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
         BaseScreen.testCase = self
-        
+
         // Logout first if needed
         logoutIfNeeded()
     }
