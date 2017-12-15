2c849376b81f55955bd39c3b83e72b7d878b1980
elibud <elibud@gmail.com>
2017-12-15 13:20:14 -0300

    Support for editing a value in SettingsSelectionVC

--- WordPress/Classes/ViewRelated/Tools/SettingsSelectionViewController.m
+++ WordPress/Classes/ViewRelated/Tools/SettingsSelectionViewController.m
@@ -1,4 +1,5 @@
 #import "SettingsSelectionViewController.h"
+#import "SettingsTextViewController.h"
 #import "NSDictionary+SafeExpectations.h"
 #import <WordPressShared/NSString+XMLExtensions.h>
 #import <WordPressShared/WPStyleGuide.h>
@@ -10,6 +11,7 @@ NSString * const SettingsSelectionValuesKey = @"Values";
 NSString * const SettingsSelectionHintsKey = @"Hints";
 NSString * const SettingsSelectionDefaultValueKey = @"DefaultValue";
 NSString * const SettingsSelectionCurrentValueKey = @"CurrentValue";
+NSString * const SettingsSelectionEditableIndexKey = @"EditableIndex";
 
 CGFloat const SettingsSelectionDefaultTableViewCellHeight = 44.0f;
 
@@ -21,10 +23,20 @@ CGFloat const SettingsSelectionDefaultTableViewCellHeight = 44.0f;
         CurrentValue    : 0,
         DefaultValue    : 0,
         Title           : "Image Resize",
-        Titles          : [ "Always Ask", "Small", "Medium", "Large", "Disabled" ],
+        Titles          : [ "Always Ask", "Small", "Medium", "Large", "Enter new value" ],
         Values          : [ 0, 1, 2, 3, 4, 5 ]
+        EditableIndex   : 5
      }
  */
+
+- (instancetype)initWithStyle:(UITableViewStyle)style {
+    self = [super initWithStyle:style];
+    if (self) {
+        // By default no item is editable
+        self.editableIndex = -1;
+    }
+    return self;
+}
 
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary
 {
@@ -48,6 +60,9 @@ CGFloat const SettingsSelectionDefaultTableViewCellHeight = 44.0f;
     _hints = [dictionary arrayForKey:SettingsSelectionHintsKey];
     _defaultValue = dictionary[SettingsSelectionDefaultValueKey];
     _currentValue = dictionary[SettingsSelectionCurrentValueKey] ?: _defaultValue;
+    if ([dictionary valueForKey:SettingsSelectionEditableIndexKey]) {
+        _editableIndex = [[dictionary valueForKey:SettingsSelectionEditableIndexKey] integerValue];
+    }
 }
 
 - (void)setupRefreshControl
@@ -171,14 +186,37 @@ CGFloat const SettingsSelectionDefaultTableViewCellHeight = 44.0f;
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
-
     NSObject *val = self.values[indexPath.row];
-    self.currentValue = val;
-    
+
+    if (indexPath.row == self.editableIndex) {
+        SettingsTextViewController *siteTitleViewController = [[SettingsTextViewController alloc] initWithText:self.values[indexPath.row]
+                                                                                                   placeholder:NSLocalizedString(@"Enter a custom value", @"Enter a custom value")
+                                                                                                          hint:@""];
+        siteTitleViewController.onValueChanged = ^(id value) {
+            if (![value isEqualToString:self.values[indexPath.row]]) {
+                NSMutableArray *updatedValues = [NSMutableArray arrayWithArray:self.values];
+                updatedValues[indexPath.row] = value;
+                self.values = updatedValues;
+                NSMutableArray *updatedTitles = [NSMutableArray arrayWithArray:self.titles];
+                updatedTitles[indexPath.row] = value;
+                self.titles = updatedTitles;
+                [self updateSelectedValue:value];
+            }
+        };
+        [self.navigationController pushViewController:siteTitleViewController animated:YES];
+
+    } else {
+        [self updateSelectedValue:val];
+    }
+}
+
+- (void)updateSelectedValue:(NSObject *)newValue
+{
+    self.currentValue = newValue;
     [self.tableView reloadData];
 
     if (self.onItemSelected != nil) {
-        self.onItemSelected(val);
+        self.onItemSelected(newValue);
     }
 }
 
