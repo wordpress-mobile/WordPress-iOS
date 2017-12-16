427bcbd92f129db091561e61fb2c8db01d5fbc3e
aerych <eric@automattic.com>
2017-12-16 10:57:33 -0600

    Flag login attempt as being for Jetpack

--- WordPress/Classes/ViewRelated/NUX/LoginFields.swift
+++ WordPress/Classes/ViewRelated/NUX/LoginFields.swift
@@ -67,6 +67,9 @@ class SafariStoredCredentials {
 @objc
 class LoginFieldsMeta: NSObject {
 
+    /// Indicates whether a self-hosted user is attempting to log in to Jetpack
+    @objc var jetpackLogin = true
+
     /// Indicates whether a user is logging in via the wpcom flow or a self-hosted flow.  Used by the
     /// the LoginFacade in its branching logic.
     /// This is a good candidate to refactor out and call the proper login method directly.
