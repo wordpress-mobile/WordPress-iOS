ba56a17c549b51dccefc2d93c0d3dddd1a2a1bed
aerych <eric@automattic.com>
2017-12-19 14:20:29 -0600

    Fixes spelling.

--- WordPress/Classes/ViewRelated/NUX/LoginEmailViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/LoginEmailViewController.swift
@@ -103,9 +103,9 @@ class LoginEmailViewController: LoginViewController, SigninKeyboardResponder {
     ///
     @objc func localizeControls() {
         if loginFields.meta.jetpackLogin {
-            instructionLabel?.text = NSLocalizedString("To view your stats, log in to the WordPress.com account you used to connect Jetpack.", comment: "Instruction text on the login's email addresss screen.")
+            instructionLabel?.text = NSLocalizedString("To view your stats, log in to the WordPress.com account you used to connect Jetpack.", comment: "Instruction text on the login's email address screen.")
         } else {
-            instructionLabel?.text = NSLocalizedString("Log in to WordPress.com using an email address to manage all your WordPress sites.", comment: "Instruction text on the login's email addresss screen.")
+            instructionLabel?.text = NSLocalizedString("Log in to WordPress.com using an email address to manage all your WordPress sites.", comment: "Instruction text on the login's email address screen.")
         }
         emailTextField.placeholder = NSLocalizedString("Email address", comment: "Placeholder for a textfield. The user may enter their email address.")
         emailTextField.accessibilityIdentifier = "Email address"
