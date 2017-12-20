e1c2942317bb63e23f4373da714cc75556f095e3
Stephenie Harris <stephenie@Stephenies-MacBook-Pro.local>
2017-12-20 12:11:52 -0700

    Adding keyboard interaction.

--- WordPress/Classes/ViewRelated/NUX/SignupEmailViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/SignupEmailViewController.swift
@@ -1,6 +1,11 @@
 import UIKit
 
-class SignupEmailViewController: NUXAbstractViewController {
+class SignupEmailViewController: NUXAbstractViewController, SigninKeyboardResponder {
+
+    // MARK: - SigninKeyboardResponder Properties
+
+    @IBOutlet weak var bottomContentConstraint: NSLayoutConstraint?
+    @IBOutlet weak var verticalCenterConstraint: NSLayoutConstraint?
 
     // MARK: - Properties
 
@@ -16,6 +21,22 @@ class SignupEmailViewController: NUXAbstractViewController {
         setupNavBar()
         configureView()
         setupNextButton()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureViewForEditingIfNeeded()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        registerForKeyboardEvents(keyboardWillShowAction: #selector(handleKeyboardWillShow(_:)),
+                                  keyboardWillHideAction: #selector(handleKeyboardWillHide(_:)))
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unregisterForKeyboardEvents()
     }
 
     private func setupNavBar() {
@@ -42,6 +63,35 @@ class SignupEmailViewController: NUXAbstractViewController {
         nextButton?.setTitle(nextButtonTitle, for: UIControlState())
         nextButton?.setTitle(nextButtonTitle, for: .highlighted)
         nextButton?.accessibilityIdentifier = "Next Button"
+    }
+
+    /// Configure the view for an editing state. Should only be called from viewWillAppear
+    /// as this method skips animating any change in height.
+    ///
+    private func configureViewForEditingIfNeeded() {
+        // Check the helper to determine whether an editing state should be assumed.
+        adjustViewForKeyboard(SigninEditingState.signinEditingStateActive)
+        if SigninEditingState.signinEditingStateActive {
+            emailField.becomeFirstResponder()
+        }
+    }
+
+    // MARK: - Keyboard Notifications
+
+    @objc func handleKeyboardWillShow(_ notification: Foundation.Notification) {
+        keyboardWillShow(notification)
+    }
+
+    @objc func handleKeyboardWillHide(_ notification: Foundation.Notification) {
+        keyboardWillHide(notification)
+    }
+
+    // MARK: - LoginWithLogoAndHelpViewController
+
+    /// Override this to use the appropriate sourceTag.
+    ///
+    override func handleHelpButtonTapped(_ sender: AnyObject) {
+        displaySupportViewController(sourceTag: .wpComSignupEmail)
     }
 
     // MARK: - Misc
