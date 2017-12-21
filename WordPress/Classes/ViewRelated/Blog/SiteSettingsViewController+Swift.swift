70d4ef6440c422fff374e10cd46e9729cda763c8
elibud <elibud@gmail.com>
2017-12-21 17:27:41 -0300

    Add support for AMP site setting

--- WordPress/Classes/ViewRelated/Blog/SiteSettingsViewController+Swift.swift
+++ WordPress/Classes/ViewRelated/Blog/SiteSettingsViewController+Swift.swift
@@ -29,9 +29,47 @@ extension SiteSettingsViewController {
         navigationController?.pushViewController(pickerViewController, animated: true)
     }
 
+    // MARK: AMP footer
+
+    open override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        guard isTrafficSettingsSection(section) else {
+            return nil
+        }
+
+        let footer = UITableViewHeaderFooterView()
+        footer.textLabel?.text = NSLocalizedString("Your WordPress.com site supports the use of Accelerated Mobile Pages, a Google-led initiative that dramatically speeds up loading times on mobile devices.",
+                                                   comment: "Footer for AMP Traffic Site Setting, should match Calypso.")
+        footer.textLabel?.numberOfLines = 0
+        footer.textLabel?.font = UIFont.preferredFont(forTextStyle: .footnote)
+        footer.textLabel?.isUserInteractionEnabled = true
+
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleAMPFooterTap(_:)))
+        footer.addGestureRecognizer(tap)
+        return footer
+    }
+
+    @objc fileprivate func handleAMPFooterTap(_ sender: UITapGestureRecognizer) {
+        guard let url =  URL(string: self.ampSupportURL) else {
+            return
+        }
+        let webViewController = WebViewControllerFactory.controller(url: url)
+
+        if presentingViewController != nil {
+            navigationController?.pushViewController(webViewController, animated: true)
+        } else {
+            let navController = UINavigationController(rootViewController: webViewController)
+            present(navController, animated: true, completion: nil)
+        }
+    }
+
+    override open func tableView(_ tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
+        WPStyleGuide.configureTableViewSectionFooter(view)
+    }
+
     // MARK: Private Properties
 
     fileprivate var minNumberOfPostPerPage: Int { return 1 }
     fileprivate var maxNumberOfPostPerPage: Int { return 1000 }
+    fileprivate var ampSupportURL: String { return "https://support.wordpress.com/amp-accelerated-mobile-pages/" }
 
 }
