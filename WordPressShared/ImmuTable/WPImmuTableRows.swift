76847a2f2406c15c731ed1bf5cbce64691ff9a95
Will Kwon <will@kwon.co>
2018-01-03 14:55:57 -0800

    Copying over ImmuTable items to see if it'll work for us in this sharing UI

---
+++ WordPressShared/ImmuTable/WPImmuTableRows.swift
@@ -0,0 +1,205 @@
+import Foundation
+import WordPressShared
+
+
+struct NavigationItemRow: ImmuTableRow {
+    static let cell = ImmuTableCell.class(WPTableViewCellValue1.self)
+
+    let title: String
+    let detail: String?
+    let icon: UIImage?
+    let action: ImmuTableAction?
+    let accessoryType: UITableViewCellAccessoryType
+
+    init(title: String, detail: String? = nil, icon: UIImage? = nil, badgeCount: Int = 0, accessoryType: UITableViewCellAccessoryType = .disclosureIndicator, action: @escaping ImmuTableAction) {
+        self.title = title
+        self.detail = detail
+        self.icon = icon
+        self.accessoryType = accessoryType
+        self.action = action
+    }
+
+    func configureCell(_ cell: UITableViewCell) {
+        cell.textLabel?.text = title
+        cell.detailTextLabel?.text = detail
+        cell.accessoryType = accessoryType
+        cell.imageView?.image = icon
+
+        WPStyleGuide.configureTableViewCell(cell)
+    }
+}
+
+struct BadgeNavigationItemRow: ImmuTableRow {
+    static let cell = ImmuTableCell.class(WPTableViewCellBadge.self)
+
+    let title: String
+    let icon: UIImage?
+    let action: ImmuTableAction?
+    let badgeCount: Int
+    let accessoryType: UITableViewCellAccessoryType
+
+    init(title: String, icon: UIImage? = nil, badgeCount: Int = 0, accessoryType: UITableViewCellAccessoryType = .disclosureIndicator, action: @escaping ImmuTableAction) {
+        self.title = title
+        self.icon = icon
+        self.badgeCount = badgeCount
+        self.accessoryType = accessoryType
+        self.action = action
+    }
+
+    func configureCell(_ cell: UITableViewCell) {
+        let cell = cell as! WPTableViewCellBadge
+
+        cell.textLabel?.text = title
+        cell.accessoryType = accessoryType
+        cell.imageView?.image = icon
+        cell.badgeCount = badgeCount
+
+        WPStyleGuide.configureTableViewCell(cell)
+    }
+}
+
+struct EditableTextRow: ImmuTableRow {
+    static let cell = ImmuTableCell.class(WPTableViewCellValue1.self)
+
+    let title: String
+    let value: String
+    let action: ImmuTableAction?
+
+    func configureCell(_ cell: UITableViewCell) {
+        cell.textLabel?.text = title
+        cell.detailTextLabel?.text = value
+        cell.accessoryType = .disclosureIndicator
+
+        WPStyleGuide.configureTableViewCell(cell)
+    }
+}
+
+struct TextRow: ImmuTableRow {
+    static let cell = ImmuTableCell.class(WPTableViewCellValue1.self)
+
+    let title: String
+    let value: String
+    let action: ImmuTableAction? = nil
+
+    func configureCell(_ cell: UITableViewCell) {
+        cell.textLabel?.text = title
+        cell.detailTextLabel?.text = value
+        cell.selectionStyle = .none
+
+        WPStyleGuide.configureTableViewCell(cell)
+    }
+}
+
+struct CheckmarkRow: ImmuTableRow {
+    static let cell = ImmuTableCell.class(WPTableViewCellDefault.self)
+
+    let title: String
+    let checked: Bool
+    let action: ImmuTableAction?
+
+    func configureCell(_ cell: UITableViewCell) {
+        cell.textLabel?.text = title
+        cell.selectionStyle = .none
+        cell.accessoryType = (checked) ? .checkmark : .none
+
+        WPStyleGuide.configureTableViewCell(cell)
+    }
+}
+
+struct LinkRow: ImmuTableRow {
+    static let cell = ImmuTableCell.class(WPTableViewCellValue1.self)
+
+    let title: String
+    let action: ImmuTableAction?
+
+    func configureCell(_ cell: UITableViewCell) {
+        cell.textLabel?.text = title
+
+        WPStyleGuide.configureTableViewActionCell(cell)
+    }
+}
+
+struct LinkWithValueRow: ImmuTableRow {
+    static let cell = ImmuTableCell.class(WPTableViewCellValue1.self)
+
+    let title: String
+    let value: String
+    let action: ImmuTableAction?
+
+    func configureCell(_ cell: UITableViewCell) {
+        cell.textLabel?.text = title
+        cell.detailTextLabel?.text = value
+
+        WPStyleGuide.configureTableViewActionCell(cell)
+    }
+}
+
+struct ButtonRow: ImmuTableRow {
+    static let cell = ImmuTableCell.class(WPTableViewCellDefault.self)
+
+    let title: String
+    let action: ImmuTableAction?
+
+    func configureCell(_ cell: UITableViewCell) {
+        cell.textLabel?.text = title
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.lineBreakMode = .byWordWrapping
+
+        WPStyleGuide.configureTableViewActionCell(cell)
+        cell.textLabel?.textAlignment = .center
+    }
+}
+
+struct DestructiveButtonRow: ImmuTableRow {
+    static let cell = ImmuTableCell.class(WPTableViewCellDefault.self)
+
+    let title: String
+    let action: ImmuTableAction?
+    let accessibilityIdentifier: String
+
+    func configureCell(_ cell: UITableViewCell) {
+        cell.textLabel?.text = title
+        cell.accessibilityIdentifier = accessibilityIdentifier
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.lineBreakMode = .byWordWrapping
+        WPStyleGuide.configureTableViewDestructiveActionCell(cell)
+    }
+}
+
+struct TextWithButtonRow: ImmuTableRow {
+    static let cell = ImmuTableCell.class(TextWithAccessoryButtonCell.self)
+
+    let title: String
+    let actionLabel: String
+    let action: ImmuTableAction?
+
+    func configureCell(_ cell: UITableViewCell) {
+        let cell = cell as! TextWithAccessoryButtonCell
+
+        cell.textLabel?.text = title
+        cell.buttonText = actionLabel
+        cell.onButtonPressed = action.map({ action in
+            return {
+                action(self)
+            }
+        })
+    }
+}
+
+struct SwitchRow: ImmuTableRow {
+    static let cell = ImmuTableCell.class(SwitchTableViewCell.self)
+
+    let title: String
+    let value: Bool
+    let action: ImmuTableAction? = nil
+    let onChange: (Bool) -> Void
+
+    func configureCell(_ cell: UITableViewCell) {
+        let cell = cell as! SwitchTableViewCell
+
+        cell.textLabel?.text = title
+        cell.selectionStyle = .none
+        cell.on = value
+        cell.onChange = onChange
+    }
+}
