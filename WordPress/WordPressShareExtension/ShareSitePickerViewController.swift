faeb3dc50b9d0921293b759d3f3a8eb47a5602c3
Will Kwon <will@kwon.co>
2018-01-04 19:29:28 -0800

    Adding ShareSitePickerViewController

---
+++ WordPress/WordPressShareExtension/ShareSitePickerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ShareSitePickerViewController.swift
+//  WordPressShareExtension
+//
+//  Created by Will Kwon on 1/4/18.
+//  Copyright © 2018 WordPress. All rights reserved.
+//
+
+import UIKit
+
+class ShareSitePickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
