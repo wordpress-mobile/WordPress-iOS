0bdb0411885e9b674493ee1cd805cae35a03d3cd
James Frost <james.frost@automattic.com>
2018-01-05 15:00:55 +0000

    MediaProgressCoordinatorNoticeViewModel: Add failure notice

--- WordPress/Classes/ViewRelated/Media/MediaProgressCoordinatorNoticeViewModel.swift
+++ WordPress/Classes/ViewRelated/Media/MediaProgressCoordinatorNoticeViewModel.swift
@@ -9,39 +9,80 @@ struct MediaProgressCoordinatorNoticeViewModel {
                 return nil
         }
 
-        guard !mediaProgressCoordinator.hasFailedMedia else {
-            return nil
-        }
-
         self.mediaProgressCoordinator = mediaProgressCoordinator
         self.progress = progress
     }
 
+    private var uploadSuccessful: Bool {
+        return !mediaProgressCoordinator.hasFailedMedia
+    }
+
     var notice: Notice? {
-        if let blog = blogInContext {
-            return Notice(title: title,
-                          actionTitle: actionTitle,
-                          actionHandler: {
-                            let editor = EditPostViewController(blog: blog)
-                            editor.modalPresentationStyle = .fullScreen
-                            WPTabBarController.sharedInstance().present(editor, animated: false, completion: nil)
-                            WPAppAnalytics.track(.editorCreatedPost, withProperties: ["tap_source": "media_upload_notice"], with: blog)
-            })
+        if uploadSuccessful {
+            return successNotice
         } else {
+            return failureNotice
+        }
+    }
+
+    private var successNotice: Notice {
+        guard let blog = blogInContext else {
             return Notice(title: title)
         }
+
+        return Notice(title: title,
+                      actionTitle: actionTitle,
+                      actionHandler: {
+                        let editor = EditPostViewController(blog: blog)
+                        editor.modalPresentationStyle = .fullScreen
+                        WPTabBarController.sharedInstance().present(editor, animated: false, completion: nil)
+                        WPAppAnalytics.track(.editorCreatedPost, withProperties: ["tap_source": "media_upload_notice"], with: blog)
+        })
+    }
+
+    private var failureNotice: Notice {
+        return Notice(title: title, message: message)
     }
 
     var title: String {
-        let completedUnits = progress.completedUnitCount
-        if completedUnits == 1 {
-            return NSLocalizedString("Media uploaded (1 file)", comment: "Alert displayed to the user when a single media item has uploaded successfully.")
+        if uploadSuccessful {
+            let completedUnits = progress.completedUnitCount
+            if completedUnits == 1 {
+                return NSLocalizedString("Media uploaded (1 file)", comment: "Alert displayed to the user when a single media item has uploaded successfully.")
+            } else {
+                return String(format: NSLocalizedString("Media uploaded (%ld files)", comment: "Alert displayed to the user when multiple media items have uploaded successfully."), completedUnits)
+            }
         } else {
-            return String(format: NSLocalizedString("Media uploaded (%ld files)", comment: "Alert displayed to the user when multiple media items have uploaded successfully."), completedUnits)
+            let failedUnits = mediaProgressCoordinator.failedMediaIDs.count
+            if failedUnits == 1 {
+                return NSLocalizedString("1 file not uploaded", comment: "Alert displayed to the user when a single media item has failed to upload.")
+            } else {
+                return String(format: NSLocalizedString("%ld files not uploaded", comment: "Alert displayed to the user when multiple media items have failed to upload."), failedUnits)
+            }
         }
     }
 
-    let actionTitle: String = NSLocalizedString("Write Post", comment: "Button title. Opens the editor to write a new post.")
+    var message: String? {
+        guard !uploadSuccessful else {
+            return nil
+        }
+
+        switch progress.completedUnitCount {
+        case 1:
+            return NSLocalizedString("1 file successfully uploaded", comment: "Alert displayed to the user when a single media item has failed to upload.")
+        case 1...:
+            return String(format: NSLocalizedString("%ld files successfully uploaded", comment: "Alert displayed to the user when multiple media items have failed to upload."), progress.completedUnitCount)
+        default: return nil
+        }
+    }
+
+    var actionTitle: String {
+        if uploadSuccessful {
+            return NSLocalizedString("Write Post", comment: "Button title. Opens the editor to write a new post.")
+        } else {
+            return NSLocalizedString("Retry", comment: "Button title, displayed when media has failed to upload. Allows the user to try the upload again.")
+        }
+    }
 
     private var blogInContext: Blog? {
         guard let mediaID = mediaProgressCoordinator.inProgressMediaIDs.first,
