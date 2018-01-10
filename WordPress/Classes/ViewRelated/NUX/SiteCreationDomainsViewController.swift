043e8727dac8f729fbe7bbee437b44c98e290d21
Stephenie Harris <stephenie@Stephenies-MacBook-Pro.local>
2018-01-10 12:33:26 -0700

    Creating a new SiteCreationDomainsViewController. Making SiteCreationDomainsTableViewController a child via container view.

---
+++ WordPress/Classes/ViewRelated/NUX/SiteCreationDomainsViewController.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+class SiteCreationDomainsViewController: NUXAbstractViewController {
+
+    // MARK: - Properties
+
+    open var siteName: String?
+
+    override var sourceTag: SupportSourceTag {
+        get {
+            return .wpComCreateSiteDomain
+        }
+    }
+
+    private var domainsTableViewController: SiteCreationDomainsTableViewController?
+
+    // MARK: - View
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+    }
+
+    private func configureView() {
+        _ = addHelpButtonToNavController()
+        navigationItem.title = NSLocalizedString("Create New Site", comment: "Create New Site title.")
+        WPStyleGuide.configureColors(for: view, andTableView: nil)
+    }
+
+    // MARK: - Misc
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        if let vc = segue.destination as? SiteCreationDomainsTableViewController {
+            domainsTableViewController = vc
+            domainsTableViewController?.siteName = siteName
+        }
+    }
+
+}
