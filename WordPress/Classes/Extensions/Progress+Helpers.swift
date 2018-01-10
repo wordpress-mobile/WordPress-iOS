d2e623195316a2c3f136e33aeed4a1a98182f4f8
Sergio Estevao <sergioestevao@gmail.com>
2018-01-10 22:50:43 +0000

    Fix completed progress.

--- WordPress/Classes/Extensions/Progress+Helpers.swift
+++ WordPress/Classes/Extensions/Progress+Helpers.swift
@@ -4,7 +4,7 @@ extension Progress {
 
     static func discreteCompletedProgress() -> Progress {
         let progress = Progress.discreteProgress(totalUnitCount: 1)
-        progress.totalUnitCount = 1
+        progress.completedUnitCount = 1
         return progress
     }
 }
