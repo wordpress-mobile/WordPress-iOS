4adf35a6f70d5ba07b57abcf90a6534aa50e3783
Stephenie Harris <stephenie@Stephenies-MacBook-Pro.local>
2018-01-10 17:12:22 -0700

    Adding SiteCreationButtonView & controller. Child view controller of Domains VC via embed storyboard reference.

---
+++ WordPress/Classes/ViewRelated/NUX/SiteCreationButtonViewController.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class SiteCreationButtonViewController: UIViewController {
+
+    // MARK: - Properties
+
+    @IBOutlet var shadowView: UIView?
+    @IBOutlet var continueButton: UIButton?
+
+    // MARK: - View
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Misc
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
