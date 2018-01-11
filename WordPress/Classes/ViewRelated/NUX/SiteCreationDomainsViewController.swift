2f073e257ea286e7d4f7c8276ae4c51a4266bab2
Stephenie Harris <stephenie@Stephenies-MacBook-Pro.local>
2018-01-11 11:16:01 -0700

    Hide 'Create site' button initially. Show when a domain is selected. Hide when new search is initiated. Also, dismiss 'Loading domains' when load fails.

--- WordPress/Classes/ViewRelated/NUX/SiteCreationDomainsViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/SiteCreationDomainsViewController.swift
@@ -4,6 +4,10 @@ class SiteCreationDomainsViewController: NUXAbstractViewController {
 
     // MARK: - Properties
 
+    // Used to hide/show the Buttom View
+    @IBOutlet weak var buttonContainerViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var buttonContainerHeightConstraint: NSLayoutConstraint!
+
     open var siteName: String?
 
     override var sourceTag: SupportSourceTag {
@@ -29,6 +33,26 @@ class SiteCreationDomainsViewController: NUXAbstractViewController {
         WPStyleGuide.configureColors(for: view, andTableView: nil)
     }
 
+    private func showButtonView(show: Bool, withAnimation: Bool) {
+
+        let duration = withAnimation ? WPAnimationDurationDefault : 0
+
+        UIView.animate(withDuration: duration, animations: {
+            if show {
+                self.buttonContainerViewBottomConstraint.constant = 0
+            }
+            else {
+                // Move the view down double the height to ensure it's off the screen.
+                // i.e. to defy iPhone X bottom gap.
+                self.buttonContainerViewBottomConstraint.constant +=
+                    self.buttonContainerHeightConstraint.constant * 2
+            }
+
+            // Since the Button View uses auto layout, need to call this so the animation works properly.
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+
     // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -42,6 +66,7 @@ class SiteCreationDomainsViewController: NUXAbstractViewController {
         if let vc = segue.destination as? SiteCreationButtonViewController {
             buttonViewController = vc
             buttonViewController?.delegate = self
+            showButtonView(show: false, withAnimation: false)
         }
     }
 
@@ -59,6 +84,11 @@ class SiteCreationDomainsViewController: NUXAbstractViewController {
 extension SiteCreationDomainsViewController: SiteCreationDomainsTableViewControllerDelegate {
     func domainSelected(_ domain: String) {
         selectedDomain = domain
+        showButtonView(show: true, withAnimation: true)
+    }
+
+    func newSearchStarted() {
+        showButtonView(show: false, withAnimation: true)
     }
 }
 
