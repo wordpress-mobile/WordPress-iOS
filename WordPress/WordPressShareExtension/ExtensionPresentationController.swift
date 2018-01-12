71574e69301531d62785765db41fe798b2d5506d
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-01-12 07:11:12 -0600

    Keyboard handling (WIP)

--- WordPress/WordPressShareExtension/ExtensionPresentationController.swift
+++ WordPress/WordPressShareExtension/ExtensionPresentationController.swift
@@ -18,6 +18,12 @@ class ExtensionPresentationController: UIPresentationController {
     init(presentedViewController: UIViewController, presenting presentingViewController: UIViewController?, direction: Direction) {
         self.direction = direction
         super.init(presentedViewController: presentedViewController, presenting: presentingViewController)
+
+        self.registerKeyboardObservers()
+    }
+
+    deinit {
+        removeKeyboardObservers()
     }
 
     // MARK: - Presentation Controller Overrides
@@ -67,12 +73,64 @@ class ExtensionPresentationController: UIPresentationController {
     }
 }
 
+// MARK: - Keyboard Handling
+
+private extension ExtensionPresentationController {
+    func registerKeyboardObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown(notification:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: .UIKeyboardWillHide, object: nil)
+    }
+
+    func removeKeyboardObservers() {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+    }
+
+    @objc func keyboardWasShown(notification: Notification) {
+        let keyboardFrame = notification.keyboardEndFrame() ?? .zero
+        let duration = notification.keyboardAnimationDuration() ?? Constants.defaultAnimationDuration
+        animateForWithKeyboardFrame(presentedView!.convert(keyboardFrame, from: nil), duration: duration)
+    }
+
+    @objc func keyboardWillHide (notification: Notification) {
+        let keyboardFrame = notification.keyboardEndFrame() ?? .zero
+        let duration = notification.keyboardAnimationDuration() ?? Constants.defaultAnimationDuration
+        animateForWithKeyboardFrame(presentedView!.convert(keyboardFrame, from: nil), duration: duration)
+    }
+
+    func getTranslationFrame(keyboardFrame: CGRect, presentedFrame: CGRect) -> CGRect {
+        let keyboardTop = UIScreen.main.bounds.height - (keyboardFrame.size.height + Constants.bottomKeyboardMargin)
+        let presentedViewBottom = presentedFrame.origin.y + presentedFrame.height
+        let offset = presentedViewBottom - keyboardTop
+
+        guard offset > 0.0  else {
+            return presentedFrame
+        }
+
+        let newHeight = presentedFrame.size.height - offset
+        let frame = CGRect(x: presentedFrame.origin.x, y: presentedFrame.origin.y, width: presentedFrame.size.width, height: newHeight)
+        return frame
+    }
+
+    func animateForWithKeyboardFrame(_ keyboardFrame: CGRect, duration: Double) {
+        let presentedFrame = frameOfPresentedViewInContainerView
+        let translatedFrame = getTranslationFrame(keyboardFrame: keyboardFrame, presentedFrame: presentedFrame)
+        if translatedFrame != presentedFrame {
+            UIView.animate(withDuration: duration, animations: {
+                self.presentedView?.frame = translatedFrame
+            })
+        }
+    }
+}
+
 // MARK: - Constants
 
 private extension ExtensionPresentationController {
     struct Constants {
         static let fullAlpha: CGFloat = 1.0
         static let zeroAlpha: CGFloat = 0.0
+        static let defaultAnimationDuration: Double = 0.33
+        static let bottomKeyboardMargin: CGFloat = 10.0
     }
     
     struct Appearance {
@@ -80,5 +138,20 @@ private extension ExtensionPresentationController {
         static let cornerRadius: CGFloat = 13.0
         static let widthRatio: CGFloat = 0.97
         static let heightRatio: CGFloat = 0.92
+    }
+}
+
+// MARK: Notification + UIKeyboardInfo
+
+private extension Notification {
+
+    /// Gets the optional CGRect value of the UIKeyboardFrameEndUserInfoKey from a UIKeyboard notification
+    func keyboardEndFrame () -> CGRect? {
+        return (self.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+    }
+
+    /// Gets the optional AnimationDuration value of the UIKeyboardAnimationDurationUserInfoKey from a UIKeyboard notification
+    func keyboardAnimationDuration () -> Double? {
+        return (self.userInfo?[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue
     }
 }
