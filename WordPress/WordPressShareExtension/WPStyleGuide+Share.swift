f2640279211415e40d6635f02260e290e88b002e
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-01-13 13:21:28 -0600

    Modular VC updates

--- WordPress/WordPressShareExtension/WPStyleGuide+Share.swift
+++ WordPress/WordPressShareExtension/WPStyleGuide+Share.swift
@@ -16,5 +16,22 @@ extension WPStyleGuide {
             cell.detailTextLabel?.font = WPStyleGuide.subtitleFont()
             cell.detailTextLabel?.textColor = WPStyleGuide.greyDarken10()
         }
+
+        static func configureTableViewSiteCell(_ cell: UITableViewCell) {
+            cell.textLabel?.font = tableviewTextFont()
+            //cell.textLabel?.sizeToFit()
+            cell.textLabel?.textColor = darkGrey()
+
+            cell.detailTextLabel?.font = subtitleFont()
+            //cell.detailTextLabel?.sizeToFit()
+            cell.detailTextLabel?.textColor = greyDarken10()
+
+            cell.imageView?.layer.borderColor = UIColor.white.cgColor
+            cell.imageView?.layer.borderWidth = 1
+            cell.imageView?.tintColor = greyLighten10()
+
+            cell.backgroundColor = UIColor.white
+            cell.selectionStyle = .none
+        }
     }
 }
