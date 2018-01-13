6811f648c226482e19d90d7105b5701a080fc11d
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-01-13 13:21:28 -0600

    Modular VC updates

--- WordPress/WordPressShareExtension/ShareModularViewController.swift
+++ WordPress/WordPressShareExtension/ShareModularViewController.swift
@@ -4,81 +4,255 @@ import WordPressKit
 
 class ShareModularViewController: ShareExtensionAbstractViewController {
 
-    private let topStackView = UIStackView()
+    // MARK: - Private Properties
 
-    fileprivate let sitePickerTableViewController: SitePickerViewController = {
-        return SitePickerViewController()
-    }()
-
-    fileprivate let modulesTableViewController: UITableViewController = {
-        let storyboard = UIStoryboard(name: "ShareExtension", bundle: nil)
-        return storyboard.instantiateViewController(withIdentifier: "ModulesTableViewController") as! UITableViewController
-    }()
-
-    fileprivate let summaryLabel: UILabel = {
-        $0.text = "I'm a summary label!!"
+    @IBOutlet fileprivate var tableView: UITableView!
+    fileprivate lazy var activityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+    fileprivate lazy var noResultsView: WPNoResultsView = {
+        $0.accessoryView = activityIndicatorView
         return $0
-    }(UILabel())
+    }(WPNoResultsView())
 
-    override func viewDidLoad() {
+    fileprivate var sites: [RemoteBlog]?
+    fileprivate var hasSites: Bool {
+        get {
+            return sites == nil || sites?.isEmpty == false
+        }
+    }
+
+    // MARK: - View Lifecycle
+
+    open override func viewDidLoad() {
         super.viewDidLoad()
-        setupStackView()
-        addContentController(modulesTableViewController, to: topStackView)
-        topStackView.addArrangedSubview(summaryLabel)
-        addContentController(sitePickerTableViewController, to: topStackView)
-        sitePickerTableViewController.sitePickerDelegate = self
+
+        // Initialize Interface
+        setupTableView()
+        setupNoResultsView()
+
+        // Load Data
+        reloadSites()
+    }
+
+    open override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    // MARK: - Setup Helpers
+
+    fileprivate func setupTableView() {
+        // Register the cells
+        tableView.register(WPTableViewCell.self, forCellReuseIdentifier: Constants.sitesReuseIdentifier)
+
+        // Hide the separators, whenever the table is empty
+        tableView.tableFooterView = UIView()
+
+        // Style!
+        WPStyleGuide.configureColors(for: view, andTableView: tableView)
+        WPStyleGuide.configureAutomaticHeightRows(for: tableView)
+    }
+
+    fileprivate func setupNoResultsView() {
+        tableView.addSubview(noResultsView)
     }
 }
 
-// MARK: - Misc Private helpers
+// MARK: - UITableView DataSource Conformance
 
-extension ShareModularViewController: SitePickerDelegate{
-    func didSelectSite(siteId: Int, description: String?) {
-        selectedSiteID = siteId
+extension ShareModularViewController: UITableViewDataSource {
+    @objc func numberOfSections(in tableView: UITableView) -> Int {
+        return Section.count
+    }
+
+    @objc func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch Section(rawValue: section)! {
+        case .sites:
+            return rowCountForSitesSection
+        default:
+            // FIXME: This will change
+            return Constants.emptyCount
+        }
+    }
+
+    @objc func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier  = reusableIdentifierForIndexPath(indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier)!
+        configureCell(cell, indexPath: indexPath)
+
+        return cell
+    }
+
+    @objc func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let isSitesSection = indexPath.section == Section.sites.rawValue
+        return isSitesSection ? Constants.blogRowHeight : Constants.defaultRowHeight
+    }
+
+    @objc func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        // Hide when the section is empty!
+        if isSectionEmpty(section) {
+            return nil
+        }
+
+        let theSection = Section(rawValue: section)!
+        return theSection.headerText()
+    }
+
+    @objc func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        WPStyleGuide.configureTableViewSectionHeader(view)
+    }
+
+    @objc func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        // Hide when the section is empty!
+        if isSectionEmpty(section) {
+            return nil
+        }
+
+        let theSection = Section(rawValue: section)!
+        return theSection.footerText()
+    }
+
+    @objc func tableView(_ tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
+        WPStyleGuide.configureTableViewSectionFooter(view)
     }
 }
 
-// MARK: - Misc Private helpers
+// MARK: - UITableView Delegate Conformance
 
-private extension ShareModularViewController {
-
-    private func setupStackView() {
-
-        topStackView.axis = .vertical
-        topStackView.alignment = .fill
-        topStackView.distribution = .fill
-        topStackView.spacing = 8.0
-        topStackView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(topStackView)
-
-        let margins = view.layoutMarginsGuide
-        NSLayoutConstraint.activate([
-            topStackView.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
-            topStackView.trailingAnchor.constraint(equalTo: margins.trailingAnchor),
-            topStackView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8.0),
-            bottomLayoutGuide.topAnchor.constraint(equalTo: topStackView.bottomAnchor, constant: 8.0)
-            ])
+extension ShareModularViewController: UITableViewDelegate {
+    @objc func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath),
+            let site = siteForRowAtIndexPath(indexPath) else {
+            return
+        }
+        cell.accessoryType = .checkmark
+        selectedSiteID = site.blogID.intValue
     }
 
-    private func addContentController(_ child: UIViewController, to stackView: UIStackView) {
+    @objc func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) else {
+            return
+        }
+        cell.accessoryType = .none
+    }
+}
 
-        addChildViewController(child)
-        stackView.addArrangedSubview(child.view)
-        child.didMove(toParentViewController: self)
+// MARK: - UITableView Helpers
+
+fileprivate extension ShareModularViewController {
+    func reusableIdentifierForIndexPath(_ indexPath: IndexPath) -> String {
+        switch Section(rawValue: indexPath.section)! {
+        case .sites:
+            return Constants.sitesReuseIdentifier
+        default:
+            return Constants.defaultReuseIdentifier
+        }
     }
 
-    private func removeContentController(_ child: UIViewController, from stackView: UIStackView) {
+    func configureCell(_ cell: UITableViewCell, indexPath: IndexPath) {
+        guard let site = siteForRowAtIndexPath(indexPath) else {
+            return
+        }
 
-        child.willMove(toParentViewController: nil)
-        stackView.removeArrangedSubview(child.view)
-        child.view.removeFromSuperview()
-        child.removeFromParentViewController()
+        // Site's Details
+        let displayURL = URL(string: site.url)?.host ?? ""
+        if let name = site.name.nonEmptyString() {
+            cell.textLabel?.text = name
+            cell.detailTextLabel?.isEnabled = true
+            cell.detailTextLabel?.text = displayURL
+        } else {
+            cell.textLabel?.text = displayURL
+            cell.detailTextLabel?.isEnabled = false
+            cell.detailTextLabel?.text = nil
+        }
+
+        // Site's Blavatar
+        cell.imageView?.image = WPStyleGuide.Share.blavatarPlaceholderImage
+        if let siteIconPath = site.icon,
+            let siteIconUrl = URL(string: siteIconPath) {
+            cell.imageView?.downloadBlavatar(siteIconUrl)
+        } else {
+            cell.imageView?.image = WPStyleGuide.Share.blavatarPlaceholderImage
+        }
+
+        if site.blogID.intValue == selectedSiteID {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+
+        WPStyleGuide.Share.configureTableViewSiteCell(cell)
+    }
+
+    fileprivate var rowCountForSitesSection: Int {
+        return sites?.count ?? 0
+    }
+
+    func siteForRowAtIndexPath(_ indexPath: IndexPath) -> RemoteBlog? {
+        guard let sites = sites else {
+            return nil
+        }
+        return sites[indexPath.row]
+    }
+
+    func isSectionEmpty(_ sectionIndex: Int) -> Bool {
+        switch Section(rawValue: sectionIndex)! {
+        case .sites:
+            return hasSites
+        default:
+            return false
+        }
+    }
+}
+
+// MARK: - Data fetching helpers
+
+fileprivate extension ShareModularViewController {
+    fileprivate func reloadSites() {
+        guard let oauth2Token = oauth2Token else {
+            showEmptySitesIfNeeded()
+            return
+        }
+
+        let api = WordPressComRestApi(oAuthToken: oauth2Token, userAgent: nil)
+        let remote = AccountServiceRemoteREST.init(wordPressComRestApi: api)
+        remote?.getVisibleBlogs(success: { [weak self] blogs in
+            DispatchQueue.main.async {
+                self?.sites = (blogs as? [RemoteBlog]) ?? [RemoteBlog]()
+                self?.activityIndicatorView.stopAnimating()
+                self?.tableView.reloadData()
+                self?.showEmptySitesIfNeeded()
+            }
+            }, failure: { [weak self] error in
+                NSLog("Error retrieving blogs: \(String(describing: error))")
+                DispatchQueue.main.async {
+                    self?.sites = [RemoteBlog]()
+                    self?.activityIndicatorView.stopAnimating()
+                    self?.tableView.reloadData()
+                    self?.showEmptySitesIfNeeded()
+                }
+        })
+
+        showLoadingView()
+    }
+}
+
+// MARK: - No Results Helpers
+
+fileprivate extension ShareModularViewController {
+    fileprivate func showLoadingView() {
+        noResultsView.titleText = NSLocalizedString("Loading Sites...", comment: "Legend displayed when loading Sites")
+        activityIndicatorView.startAnimating()
+        noResultsView.isHidden = false
+    }
+
+    fileprivate func showEmptySitesIfNeeded() {
+        noResultsView.titleText = NSLocalizedString("No Sites", comment: "Legend displayed when the user has no sites")
+        noResultsView.isHidden = hasSites
     }
 }
 
 // MARK: - Backend Interaction
 
-private extension ShareModularViewController {
+fileprivate extension ShareModularViewController {
 
     func savePostToRemoteSite() {
         guard let _ = oauth2Token, let siteID = selectedSiteID else {
@@ -115,5 +289,51 @@ private extension ShareModularViewController {
                 self.extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
             })
         }
+    }
+}
+
+// MARK: - Table Sections
+
+fileprivate extension ShareModularViewController {
+    fileprivate enum Section: Int {
+        case modules       = 0
+        case sites         = 1
+
+        func headerText() -> String {
+            switch self {
+            case .modules:
+                return String()
+            case .sites:
+                return String()
+            }
+        }
+
+        func footerText() -> String {
+            switch self {
+            case .modules:
+                // FIXME: Placeholder for summary module
+                return String("Save XXX as a draft/published post on:")
+            case .sites:
+                return String()
+            }
+        }
+
+        static let count: Int = {
+            var max: Int = 0
+            while let _ = Section(rawValue: max) { max += 1 }
+            return max
+        }()
+    }
+}
+
+// MARK: - Constants
+
+fileprivate extension ShareModularViewController {
+    struct Constants {
+        static let sitesReuseIdentifier    = String(describing: WPTableViewCell.self)
+        static let defaultReuseIdentifier  = String(describing: ShareModularViewController.self)
+        static let blogRowHeight           = CGFloat(74.0)
+        static let defaultRowHeight        = CGFloat(44.0)
+        static let emptyCount              = 0
     }
 }
