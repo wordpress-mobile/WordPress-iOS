0f60f795461ac1ae25c8513d892e3dcfbc930fee
Jorge Bernal <jbernal@gmail.com>
2018-01-17 08:46:06 +0100

    Add WPTimeZone unit tests

--- WordPressKit/WordPressKitTests/TimeZoneServiceRemoteTests.swift
+++ WordPressKit/WordPressKitTests/TimeZoneServiceRemoteTests.swift
@@ -2,6 +2,39 @@ import XCTest
 import WordPressKit
 
 class TimeZoneServiceRemoteTests: RemoteTestCase, RESTTestable {
+    func testOffsetTimeZone() {
+        let utc = OffsetTimeZone(offset: 0)
+
+        XCTAssertEqual(utc.label, "UTC")
+        XCTAssertEqual(utc.value, "UTC")
+        XCTAssertEqual(utc.gmtOffset, 0)
+        XCTAssertEqual(utc.timezoneString, "UTC")
+
+        let utcPlusFour = OffsetTimeZone(offset: 4)
+        XCTAssertEqual(utcPlusFour.label, "UTC+4")
+        XCTAssertEqual(utcPlusFour.value, "UTC+4")
+        XCTAssertEqual(utcPlusFour.gmtOffset, 4)
+        XCTAssertEqual(utcPlusFour.timezoneString, "UTC+4")
+
+        let utcPlusFourThirty = OffsetTimeZone(offset: 4.5)
+        XCTAssertEqual(utcPlusFourThirty.label, "UTC+4:30")
+        XCTAssertEqual(utcPlusFourThirty.value, "UTC+4.5")
+        XCTAssertEqual(utcPlusFourThirty.gmtOffset, 4.5)
+        XCTAssertEqual(utcPlusFourThirty.timezoneString, "UTC+4.5")
+
+        let utcMinusTwo = OffsetTimeZone(offset: -2)
+        XCTAssertEqual(utcMinusTwo.label, "UTC-2")
+        XCTAssertEqual(utcMinusTwo.value, "UTC-2")
+        XCTAssertEqual(utcMinusTwo.gmtOffset, -2)
+        XCTAssertEqual(utcMinusTwo.timezoneString, "UTC-2")
+
+        let utcMinusTwoThirty = OffsetTimeZone(offset: -2.5)
+        XCTAssertEqual(utcMinusTwoThirty.label, "UTC-2:30")
+        XCTAssertEqual(utcMinusTwoThirty.value, "UTC-2.5")
+        XCTAssertEqual(utcMinusTwoThirty.gmtOffset, -2.5)
+        XCTAssertEqual(utcMinusTwoThirty.timezoneString, "UTC-2.5")
+    }
+
     func testGetTimeZones() {
         stubRemoteResponse("timezones", filename: "timezones.json", contentType: .ApplicationJSON)
 
