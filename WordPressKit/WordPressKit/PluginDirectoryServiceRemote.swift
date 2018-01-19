3f9750a49b19e5a6eb581376ab3d40e07cb4cb0b
Jan Klausa <jan@klausa.pl>
2018-01-19 02:50:21 +0100

    PluginDirectoryServiceRemote: refactor and cleanup

--- WordPressKit/WordPressKit/PluginDirectoryServiceRemote.swift
+++ WordPressKit/WordPressKit/PluginDirectoryServiceRemote.swift
@@ -1,7 +1,7 @@
 import Foundation
 import Alamofire
 
-public struct PluginDirectoryGetInformationEndpoint: Endpoint {
+private struct PluginDirectoryRemoteConstants {
     static let dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "en_US_POSIX")
@@ -9,43 +9,17 @@ public struct PluginDirectoryGetInformationEndpoint: Endpoint {
         return formatter
     }()
 
-    public enum Error: Swift.Error {
-        case pluginNotFound
-    }
-
-    let slug: String
-    public init(slug: String) {
-        self.slug = slug
-    }
-
-    public func buildRequest() throws -> URLRequest {
-        let baseURL = URL(string: "https://api.wordpress.org/plugins/info/1.0/")!
-
-        let url = baseURL
-            .appendingPathComponent(slug)
-            .appendingPathExtension("json")
-        let request = URLRequest(url: url)
-        let encodedRequest = try URLEncoding.default.encode(request, with: ["fields": "icons,banners"])
-        return encodedRequest
-    }
-
-    public func parseResponse(data: Data) throws -> PluginDirectoryEntry {
+    static let jsonDecoder: JSONDecoder = {
         let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = .formatted(PluginDirectoryGetInformationEndpoint.dateFormatter)
-        return try decoder.decode(PluginDirectoryEntry.self, from: data)
-    }
+        decoder.dateDecodingStrategy = .formatted(PluginDirectoryRemoteConstants.dateFormatter)
+        return decoder
+    }()
 
-    public func validate(request: URLRequest?, response: HTTPURLResponse, data: Data?) throws {
-        // api.wordpress.org has an odd way of responding to plugin info requests for
-        // plugins not in the directory: it will return `null` with an HTTP 200 OK.
-        // This turns that case into a `.pluginNotFound` error.
-        if response.statusCode == 200,
-            let data = data,
-            data.count == 4,
-            String(data: data, encoding: .utf8) == "null" {
-                throw Error.pluginNotFound
-        }
-    }
+    static let pluginsPerPage = 50
+
+    static let getInformationEndpoint = URL(string: "https://api.wordpress.org/plugins/info/1.0/")!
+    static let feedEndpoint = URL(string: "https://api.wordpress.org/plugins/info/1.1/")!
+    // note that this _isn't_ the same URL as PluginDirectoryGetInformationEndpoint.
 }
 
 public enum PluginDirectoryFeedType {
@@ -66,31 +40,53 @@ public enum PluginDirectoryFeedType {
 
 }
 
+public struct PluginDirectoryGetInformationEndpoint: Endpoint {
+    public enum Error: Swift.Error {
+        case pluginNotFound
+    }
+
+    let slug: String
+    public init(slug: String) {
+        self.slug = slug
+    }
+
+    public func buildRequest() throws -> URLRequest {
+        let url = PluginDirectoryRemoteConstants.getInformationEndpoint
+            .appendingPathComponent(slug)
+            .appendingPathExtension("json")
+        let request = URLRequest(url: url)
+        let encodedRequest = try URLEncoding.default.encode(request, with: ["fields": "icons,banners"])
+        return encodedRequest
+    }
+
+    public func parseResponse(data: Data) throws -> PluginDirectoryEntry {
+        return try PluginDirectoryRemoteConstants.jsonDecoder.decode(PluginDirectoryEntry.self, from: data)
+    }
+
+    public func validate(request: URLRequest?, response: HTTPURLResponse, data: Data?) throws {
+        // api.wordpress.org has an odd way of responding to plugin info requests for
+        // plugins not in the directory: it will return `null` with an HTTP 200 OK.
+        // This turns that case into a `.pluginNotFound` error.
+        if response.statusCode == 200,
+            let data = data,
+            data.count == 4,
+            String(data: data, encoding: .utf8) == "null" {
+                throw Error.pluginNotFound
+        }
+    }
+}
+
 public struct PluginDirectoryFeedEndpoint: Endpoint {
-    private let pluginsPerPage = 50
-
-
-
     public enum Error: Swift.Error {
         case genericError
     }
-
-    static let dateFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.locale = Locale(identifier: "en_US_POSIX")
-        formatter.dateFormat = "YYYY-MM-dd h:mma z"
-        return formatter
-    }()
 
     let feedType: PluginDirectoryFeedType
     let pageNumber: Int
 
     public func buildRequest() throws -> URLRequest {
-        // note that this _isn't_ the same URL as PluginDirectoryGetInformationEndpoint.
-        let baseURL = URL(string: "https://api.wordpress.org/plugins/info/1.1/")!
-
         var parameters: [String: Any] = ["action": "query_plugins",
-                                         "request[per_page]": pluginsPerPage,
+                                         "request[per_page]": PluginDirectoryRemoteConstants.pluginsPerPage,
                                          "request[fields][icons]": 1,
                                          "request[fields][banners]": 1,
                                          "request[fields][sections]": 0,
@@ -105,16 +101,14 @@ public struct PluginDirectoryFeedEndpoint: Endpoint {
 
         }
 
-        let request = URLRequest(url: baseURL)
+        let request = URLRequest(url: PluginDirectoryRemoteConstants.feedEndpoint)
         let encodedRequest = try URLEncoding.default.encode(request, with: parameters)
 
         return encodedRequest
     }
 
     public func parseResponse(data: Data) throws -> PluginDirectoryResponse {
-        let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = .formatted(PluginDirectoryGetInformationEndpoint.dateFormatter)
-        return try decoder.decode(PluginDirectoryResponse.self, from: data)
+        return try PluginDirectoryRemoteConstants.jsonDecoder.decode(PluginDirectoryResponse.self, from: data)
     }
 
    public func validate(request: URLRequest?, response: HTTPURLResponse, data: Data?) throws {
