a93944e5c0131fb5f45b72a1c100f00e4c597aca
Stephenie Harris <>
2018-01-22 10:32:39 -0700

    Remove unnecessary method.

--- WordPress/Classes/ViewRelated/NUX/SiteCreationButtonViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/SiteCreationButtonViewController.swift
@@ -27,11 +27,4 @@ class SiteCreationButtonViewController: UIViewController {
         delegate?.continueButtonPressed()
     }
 
-    // MARK: - Misc
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
