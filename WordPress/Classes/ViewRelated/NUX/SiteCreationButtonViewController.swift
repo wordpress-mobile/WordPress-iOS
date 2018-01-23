940cfff5cfb1dae08ecb4e5771bd850e87895f3c
Stephenie Harris <scoutharris@gmail.com>
2018-01-23 14:33:00 -0700

    Adding a second button to the button view. Making button view properties & methods generic.

--- WordPress/Classes/ViewRelated/NUX/SiteCreationButtonViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/SiteCreationButtonViewController.swift
@@ -1,7 +1,8 @@
 import UIKit
 
-protocol SiteCreationButtonViewControllerDelegate {
-    func continueButtonPressed()
+@objc protocol SiteCreationButtonViewControllerDelegate {
+    func primaryButtonPressed()
+    @objc optional func secondaryButtonPressed()
 }
 
 class SiteCreationButtonViewController: UIViewController {
@@ -9,22 +10,54 @@ class SiteCreationButtonViewController: UIViewController {
     // MARK: - Properties
 
     @IBOutlet var shadowView: UIView?
-    @IBOutlet var continueButton: UIButton?
+    @IBOutlet var primaryButton: UIButton?
+    @IBOutlet var secondaryButton: UIButton?
 
     open var delegate: SiteCreationButtonViewControllerDelegate?
 
+    private var primaryButtonTitle: String?
+    private var secondaryButtonTitle: String?
+
     // MARK: - View
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
 
-        continueButton?.titleLabel?.text = NSLocalizedString("Create site", comment: "Button text for creating a new site in the Site Creation process.")
+        primaryButton?.setTitle(primaryButtonTitle, for: UIControlState())
+        primaryButton?.accessibilityIdentifier = accessibilityIdentifierForString(primaryButtonTitle)
+
+        secondaryButton?.setTitle(secondaryButtonTitle, for: UIControlState())
+        secondaryButton?.accessibilityIdentifier = accessibilityIdentifierForString(secondaryButtonTitle)
+
+        // Hide secondary button if title is not provided.
+        secondaryButton?.isHidden = (secondaryButtonTitle ?? "").isEmpty
+    }
+
+
+    /// Public method to set the button titles.
+    ///
+    /// - Parameters:
+    ///   - primary: Title string for primary button. Required.
+    ///   - secondary: Title string for secondary button. Optional.
+    func setButtonTitles(primary: String, secondary: String? = nil) {
+        primaryButtonTitle = primary
+        secondaryButtonTitle = secondary
+    }
+
+    // MARK: - Helpers
+
+    private func accessibilityIdentifierForString(_ string: String?) -> String {
+        return "\(string ?? "") Button"
     }
 
     // MARK: - Button Handling
 
-    @IBAction func handleButtonPressed(_ sender: Any) {
-        delegate?.continueButtonPressed()
+    @IBAction func primaryButtonPressed(_ sender: Any) {
+        delegate?.primaryButtonPressed()
+    }
+
+    @IBAction func secondaryButtonPressed(_ sender: Any) {
+        delegate?.secondaryButtonPressed?()
     }
 
 }
