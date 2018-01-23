a30836b87a55cb2f97c1fe90bbf4496498698a0a
Jan Klausa <jan@klausa.pl>
2018-01-23 18:45:36 +0100

    Array: Document the `unique(by:)` extension.

--- WordPress/Classes/Extensions/Array.swift
+++ WordPress/Classes/Extensions/Array.swift
@@ -20,7 +20,14 @@ extension Array where Element: Hashable {
     }
 }
 
+
 extension Array {
+    /// Returns array containing only elements that have unique values for item at given KeyPath.
+    /// Useful when you want to `unique` objects by their property, e.g. find all unique `author`s for a list of `Comment`s.
+    ///
+    /// - Parameter filteringKeyPath: A KeyPath to a `Hashable` property that the elements should be uniqued by.
+    ///
+    /// - Returns: Array with values filtered by using the given KeyPath to ensure only unique values are present.
     public func unique<Value: Hashable>(by filteringKeyPath: KeyPath<Element, Value>) -> [Element] {
         let values = self.map { $0[keyPath: filteringKeyPath] }
 
