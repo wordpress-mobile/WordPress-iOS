02c40f78ca9de5ab9adfbc9cac500a6217bf1ae2
Jan Klausa <jan@klausa.pl>
2018-01-23 22:00:16 +0100

    PluginDirectoryServiceRemote: add tests.

--- WordPressKit/WordPressKitTests/PluginDirectoryTests.swift
+++ WordPressKit/WordPressKitTests/PluginDirectoryTests.swift
@@ -78,4 +78,69 @@ class PluginDirectoryTests: XCTestCase {
         }
     }
 
+    func testNewDirectoryFeedRequest() {
+        let endpoint = PluginDirectoryFeedEndpoint(feedType: .newest)
+        do {
+            let request = try endpoint.buildRequest()
+            XCTAssertEqual(request.httpMethod, "GET")
+            XCTAssertEqual(request.url?.absoluteString, "https://api.wordpress.org/plugins/info/1.1/?action=query_plugins&request%5Bbrowse%5D=new&request%5Bfields%5D%5Bbanners%5D=1&request%5Bfields%5D%5Bicons%5D=1&request%5Bfields%5D%5Bsections%5D=0&request%5Bpage%5D=1&request%5Bper_page%5D=50")
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+
+    func testPopularDirectoryFeedRequest() {
+        let endpoint = PluginDirectoryFeedEndpoint(feedType: .popular)
+        do {
+            let request = try endpoint.buildRequest()
+            XCTAssertEqual(request.httpMethod, "GET")
+            XCTAssertEqual(request.url?.absoluteString, "https://api.wordpress.org/plugins/info/1.1/?action=query_plugins&request%5Bbrowse%5D=popular&request%5Bfields%5D%5Bbanners%5D=1&request%5Bfields%5D%5Bicons%5D=1&request%5Bfields%5D%5Bsections%5D=0&request%5Bpage%5D=1&request%5Bper_page%5D=50")
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+
+    func testPopularDirectoryFeedDecoding() {
+        let popularFeedMockPath = Bundle(for: type(of: self)).path(forResource: "plugin-directory-popular", ofType: "json")!
+        let data = try! Data(contentsOf: URL(fileURLWithPath: popularFeedMockPath))
+        let endpoint = PluginDirectoryFeedEndpoint(feedType: .popular)
+
+        do {
+            let response = try endpoint.parseResponse(data: data)
+            XCTAssertEqual(response.pageMetadata.page, 1)
+            XCTAssertEqual(response.plugins.count, 50)
+            XCTAssertEqual(response.pageMetadata.pluginSlugs.count, 50)
+            XCTAssertEqual(response.plugins.first!.name, "Contact Form 7")
+            XCTAssertNotNil(response.plugins.first!.icon)
+
+            let slugs = response.plugins.map { $0.slug }
+            XCTAssertEqual(response.pageMetadata.pluginSlugs, slugs)
+
+        } catch {
+            XCTFail("Failed decoding plugin \(error)")
+        }
+    }
+
+    func testNewDirectoryFeedDecoding() {
+        // This also tests parsing the "broken" response where `plugins` is a [Int: Object] Dictionary, instead of an Array.
+
+        let newFeedMockPath = Bundle(for: type(of: self)).path(forResource: "plugin-directory-new", ofType: "json")!
+        let data = try! Data(contentsOf: URL(fileURLWithPath: newFeedMockPath))
+        let endpoint = PluginDirectoryFeedEndpoint(feedType: .newest)
+
+        do {
+            let response = try endpoint.parseResponse(data: data)
+            XCTAssertEqual(response.pageMetadata.page, 1)
+            XCTAssertEqual(response.plugins.count, 48)
+            XCTAssertEqual(response.pageMetadata.pluginSlugs.count, 48)
+            XCTAssertEqual(response.plugins.first!.name, "NapoleonCat Chat Widget for Facebook")
+            XCTAssertEqual(response.plugins.last!.name, "Woomizer")
+
+            let slugs = response.plugins.map { $0.slug }
+            XCTAssertEqual(response.pageMetadata.pluginSlugs, slugs)
+
+        } catch {
+            XCTFail("Failed decoding plugin \(error)")
+        }
+    }
 }
