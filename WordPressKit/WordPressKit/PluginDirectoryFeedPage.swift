b5b42613dc8aeefd1cd5f8169ec692a5564039a6
Jan Klausa <jan@klausa.pl>
2018-01-23 22:01:47 +0100

    PluginDirectoryFeedPage: add workaround for broken backend responses.

--- WordPressKit/WordPressKit/PluginDirectoryFeedPage.swift
+++ WordPressKit/WordPressKit/PluginDirectoryFeedPage.swift
@@ -16,7 +16,22 @@ public struct PluginDirectoryFeedPage: Decodable, Equatable {
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
 
-        plugins = try container.decode([PluginDirectoryEntry].self, forKey: .plugins)
+        // The API we're using has a bug where sometimes the `plugins` field is an Array, and sometimes
+        // it's a dictionary with numerical keys. Until the responsible parties can deploy a patch,
+        // here's a workaround.
+        pluginParsing: do {
+            if let parsedPlugins = try? container.decode([PluginDirectoryEntry].self, forKey: .plugins) {
+                self.plugins = parsedPlugins
+                break pluginParsing
+            }
+
+            let parsedPlugins = try container.decode([Int: PluginDirectoryEntry].self, forKey: .plugins)
+
+            self.plugins = parsedPlugins
+                .sorted { $0.key < $1.key }
+                .flatMap { $0.value }
+        }
+
         let info = try container.nestedContainer(keyedBy: InfoKeys.self, forKey: .info)
 
         let pageNumber = try info.decode(Int.self, forKey: .page)
