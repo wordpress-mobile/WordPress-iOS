026c0678f3b285b652e8ba22ece7bb8ae0b5b34f
Jan Klausa <jan@klausa.pl>
2018-02-05 14:21:42 +0000

    PluginDirectory: add PluginDirectoryViewController

---
+++ WordPress/Classes/ViewRelated/Plugins/PluginDirectoryViewController.swift
@@ -0,0 +1,84 @@
+import UIKit
+import WordPressFlux
+import Gridicons
+
+class PluginDirectoryViewController: UITableViewController {
+
+    private let viewModel: PluginDirectoryViewModel
+    private var viewModelReceipt: Receipt?
+    private var collectionView: UICollectionView?
+
+    init(site: JetpackSiteRef, store: PluginStore = StoreContainer.shared.plugin) {
+        viewModel = PluginDirectoryViewModel(site: site, store: store)
+
+        super.init(style: .plain)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    @objc convenience init?(blog: Blog) {
+        guard let site = JetpackSiteRef(blog: blog) else {
+            return nil
+        }
+
+        self.init(site: site)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.backgroundColor = .white
+
+        viewModelReceipt = viewModel.onChange { [weak self] in
+            self?.reloadTable()
+        }
+
+        tableView.rowHeight = 256
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+
+        ImmuTable.registerRows([CollectionViewContainerRow<PluginDirectoryCollectionViewCell, PluginDirectoryEntry>.self,
+                                TextRow.self],
+                               tableView: tableView)
+
+
+        let handler = ImmuTableViewHandler(takeOver: self)
+        handler.automaticallyDeselectCells = true
+        handler.viewModel = viewModel.tableViewModel(presenter: self)
+
+        immuHandler = handler
+    }
+
+    var immuHandler: ImmuTableViewHandler?
+ 
+
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0
+    }
+
+
+    func reloadTable() {
+        immuHandler?.viewModel = viewModel.tableViewModel(presenter: self)
+    }
+
+}
+
+extension PluginDirectoryViewController: PluginPresenter {
+    func present(plugin: Plugin, capabilities: SitePluginCapabilities) {
+        let pluginVC = PluginViewController(plugin: plugin, capabilities: capabilities, site: viewModel.site)
+        navigationController?.pushViewController(pluginVC, animated: true)
+    }
+
+    func present(directoryEntry: PluginDirectoryEntry) {
+        let pluginVC = PluginViewController(directoryEntry: directoryEntry, site: viewModel.site)
+        navigationController?.pushViewController(pluginVC, animated: true)
+    }
+
+
+}
+
