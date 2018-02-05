bf4d18eb8c26bdf41ef6ad7d610035c990b9995d
Jan Klausa <jan@klausa.pl>
2018-02-05 14:22:17 +0000

    PluginDirectory: show half stars in ratings.

--- WordPress/Classes/ViewRelated/Plugins/PluginDirectoryAccessoryView.swift
+++ WordPress/Classes/ViewRelated/Plugins/PluginDirectoryAccessoryView.swift
@@ -29,6 +29,14 @@ struct PluginDirectoryAccessoryView {
         return PluginDirectoryAccessoryView.label(with: icon, tintColor: color, text: text)
     }
 
+    static func updating() -> UIView {
+        let icon = Gridicon.iconOfType(.sync, withSize: PluginDirectoryAccessoryView.imageSize)
+        let color = WPStyleGuide.warningYellow()
+        let text = NSLocalizedString("Updating", comment: "Describes a status of a plugin")
+
+        return PluginDirectoryAccessoryView.label(with: icon, tintColor: color, text: text)
+    }
+
     private static func label(with icon: UIImage, tintColor: UIColor, text: String) -> UIView {
         let container = UIView(frame: .zero)
         container.translatesAutoresizingMaskIntoConstraints = false
@@ -57,17 +65,13 @@ struct PluginDirectoryAccessoryView {
         return container
     }
 
-    static func stars(count: Int) -> UIView {
+    static func stars(count: Double) -> UIView {
         let totalStars = 5
 
         let starImageSize = CGSize(width: 12, height: 12)
         let spacing: CGFloat = 0
 
         let totalWidth = (starImageSize.width * CGFloat(totalStars)) + (spacing * (CGFloat(totalStars) - 1))
-
-        let blueStar = Gridicon.iconOfType(.star, withSize: starImageSize)
-        let whiteStar = Gridicon.iconOfType(.starOutline, withSize: starImageSize)
-        let color = WPStyleGuide.mediumBlue()
 
         let container = UIView(frame: .zero)
         container.translatesAutoresizingMaskIntoConstraints = false
@@ -84,13 +88,25 @@ struct PluginDirectoryAccessoryView {
         stackView.bottomAnchor.constraint(equalTo: container.bottomAnchor).isActive = true
         stackView.widthAnchor.constraint(equalToConstant: totalWidth).isActive = true
 
-        for i in 1...totalStars {
-            let image: UIImage
+        let (wholeStars, half) = modf(count)
 
-            if i <= count {
-                image = blueStar
+        for i in 1...totalStars {
+
+            if i == Int(wholeStars) + 1, half > 0 {
+                // if this is the next iteration after the last "whole" star, and `half` is > 0, add a half-star.
+                stackView.addArrangedSubview(PluginDirectoryAccessoryView.halfStar(size: starImageSize))
+                continue
+            }
+
+            let image: UIImage
+            let color: UIColor
+
+            if i <= Int(wholeStars) {
+                image = Gridicon.iconOfType(.star, withSize: starImageSize)
+                color = WPStyleGuide.mediumBlue()
             } else {
-                image = whiteStar
+                image = Gridicon.iconOfType(.starOutline, withSize: starImageSize)
+                color = WPStyleGuide.greyLighten20()
             }
 
             let imageView = UIImageView(image: image)
@@ -102,6 +118,45 @@ struct PluginDirectoryAccessoryView {
         return container
     }
 
+    private static func halfStar(size:CGSize) -> UIView {
+        let color = WPStyleGuide.mediumBlue()
+
+        let container = UIView(frame: .zero)
+        container.translatesAutoresizingMaskIntoConstraints = false
+        container.heightAnchor.constraint(equalToConstant: size.height)
+        container.widthAnchor.constraint(equalToConstant: size.width)
+
+        let leftHalf = UIImageView(image: Gridicon.iconOfType(.star, withSize: size))
+        leftHalf.tintColor = color
+        leftHalf.translatesAutoresizingMaskIntoConstraints = false
+        leftHalf.contentMode = .left
+        leftHalf.clipsToBounds = true
+        leftHalf.setContentHuggingPriority(.required, for: .vertical)
+        leftHalf.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        leftHalf.addConstraint(NSLayoutConstraint(item: leftHalf, attribute: .width, relatedBy: .equal, toItem: leftHalf, attribute: .height, multiplier: 0.5, constant: 0))
+
+        let rightHalf = UIImageView(image: Gridicon.iconOfType(.starOutline, withSize: size))
+        rightHalf.tintColor = WPStyleGuide.greyLighten20()
+        rightHalf.translatesAutoresizingMaskIntoConstraints = false
+        rightHalf.contentMode = .right
+        rightHalf.clipsToBounds = true
+        rightHalf.setContentHuggingPriority(.required, for: .vertical)
+        rightHalf.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        rightHalf.addConstraint(NSLayoutConstraint(item: rightHalf, attribute: .width, relatedBy: .equal, toItem: rightHalf, attribute: .height, multiplier: 0.5, constant: 0))
+
+        container.addSubview(leftHalf)
+        container.addSubview(rightHalf)
+
+        leftHalf.topAnchor.constraint(equalTo: container.topAnchor).isActive = true
+        leftHalf.leadingAnchor.constraint(equalTo: container.leadingAnchor).isActive = true
+        leftHalf.bottomAnchor.constraint(equalTo: container.bottomAnchor).isActive = true
+
+        leftHalf.trailingAnchor.constraint(equalTo: rightHalf.leadingAnchor).isActive = true
+        rightHalf.trailingAnchor.constraint(equalTo: container.trailingAnchor).isActive = true
+        rightHalf.centerYAnchor.constraint(equalTo: leftHalf.centerYAnchor).isActive = true
+
+        return container
+    }
 
 }
 
