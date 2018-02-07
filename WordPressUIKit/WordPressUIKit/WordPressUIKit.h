200daea18a9de3785ee7170fe0e95e20f5c1211d
Jorge Leandro Perez <jorge@lantean.co>
2018-02-07 08:11:06 -0300

    New WordPressUIKit Framework

---
+++ WordPressUIKit/WordPressUIKit/WordPressUIKit.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for WordPressUIKit.
+FOUNDATION_EXPORT double WordPressUIKitVersionNumber;
+
+//! Project version string for WordPressUIKit.
+FOUNDATION_EXPORT const unsigned char WordPressUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WordPressUIKit/PublicHeader.h>
+
+
