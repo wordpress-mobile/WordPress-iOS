433bd28a7e8241d3efe4edcca09c2ca9ac24f470
Jorge Leandro Perez <jorge@lantean.co>
2018-02-07 12:50:58 -0300

    Wiring UIKitConstants

--- WordPressUIKit/WordPressUIKit/Constants/UIKitConstants.swift
+++ WordPressUIKit/WordPressUIKit/Constants/UIKitConstants.swift
@@ -2,7 +2,7 @@ import Foundation
 
 
 class UIKitConstants {
-    static let alphaHalf: CGFloat = 0.5
+    static let alphaMid: CGFloat = 0.5
     static let alphaZero: CGFloat = 0
     static let alphaFull: CGFloat = 1
 }
