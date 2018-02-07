f46b930fdb9851e97a1574a1b1bba135bc751340
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-02-07 11:15:40 -0600

    Added new action extension to WPiOS

---
+++ WordPress/WordPressDraftActionExtension/ActionViewController.swift
@@ -0,0 +1,49 @@
+import UIKit
+import MobileCoreServices
+
+class ActionViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        var imageFound = false
+        for item in self.extensionContext!.inputItems as! [NSExtensionItem] {
+            for provider in item.attachments! as! [NSItemProvider] {
+                if provider.hasItemConformingToTypeIdentifier(kUTTypeImage as String) {
+                    // This is an image. We'll load it, then place it in our image view.
+                    weak var weakImageView = self.imageView
+                    provider.loadItem(forTypeIdentifier: kUTTypeImage as String, options: nil, completionHandler: { (imageURL, error) in
+                        OperationQueue.main.addOperation {
+                            if let strongImageView = weakImageView {
+                                if let imageURL = imageURL as? URL {
+                                    strongImageView.image = UIImage(data: try! Data(contentsOf: imageURL))
+                                }
+                            }
+                        }
+                    })
+
+                    imageFound = true
+                    break
+                }
+            }
+
+            if imageFound {
+                // We only handle one image, so stop looking for more.
+                break
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func done() {
+        // Return any edited content to the host app.
+        // This template doesn't do anything, so we just echo the passed in items.
+        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+    }
+}
