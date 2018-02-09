e4846b0d6f4819acd2c976a856ba32cb543daf8a
Jorge Leandro Perez <jorge@lantean.co>
2018-02-09 13:39:19 -0300

    Renames SigninHelpers > WordPressAuthenticator

--- WordPress/Classes/ViewRelated/NUX/WordPressAuthenticator.swift
+++ WordPress/Classes/ViewRelated/NUX/WordPressAuthenticator.swift
@@ -5,7 +5,7 @@ import WordPressShared
 
 /// A collection of helper methods for NUX.
 ///
-@objc class SigninHelpers: NSObject {
+@objc class WordPressAuthenticator: NSObject {
     fileprivate static let WPComSuffix = ".wordpress.com"
     @objc static let WPSigninDidFinishNotification = "WPSigninDidFinishNotification"
 
@@ -396,7 +396,7 @@ import WordPressShared
     /// - Parameter loginFields: A LoginFields instance.
     ///
     @objc class func openForgotPasswordURL(_ loginFields: LoginFields) {
-        let baseURL = loginFields.meta.userIsDotCom ? "https://wordpress.com" : SigninHelpers.baseSiteURL(string: loginFields.siteAddress)
+        let baseURL = loginFields.meta.userIsDotCom ? "https://wordpress.com" : WordPressAuthenticator.baseSiteURL(string: loginFields.siteAddress)
         let forgotPasswordURL = URL(string: baseURL + "/wp-login.php?action=lostpassword&redirect_to=wordpress%3A%2F%2F")!
         UIApplication.shared.open(forgotPasswordURL)
     }
