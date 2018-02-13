55725d8d9b6a7ce51f9bf6eb5a36d20cab3376cc
Nate Heagy <nate@heagy.com>
2018-02-13 13:10:40 -0600

    Add new prologue buttons to new VC

---
+++ WordPress/Classes/ViewRelated/NUX/LoginPrologueSignupMethodViewController.swift
@@ -0,0 +1,37 @@
+class LoginPrologueSignupMethodViewController: UIViewController {
+    private var buttonViewController: NUXButtonViewController?
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+
+        if let vc = segue.destination as? NUXButtonViewController {
+            buttonViewController = vc
+        }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureButtonVC()
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+    private func configureButtonVC() {
+        guard let buttonViewController = buttonViewController else {
+            return
+        }
+
+        let loginTitle = NSLocalizedString("Sign up with Email", comment: "Button title.  Tapping begins our normal sign up process.")
+        let createTitle = NSLocalizedString("Sign up with Google", comment: "Button title. Tapping begins sign up using Google.")
+        buttonViewController.setupTopButton(title: loginTitle, isPrimary: false) { [weak self] in
+            self?.performSegue(withIdentifier: NUXViewController.SegueIdentifier.showEmailLogin.rawValue, sender: self)
+        }
+        buttonViewController.setupButtomButton(title: createTitle, isPrimary: false) { [weak self] in
+            // nil
+        }
+        let termsButton = WPStyleGuide.googleLoginButton()
+        buttonViewController.stackView?.insertArrangedSubview(termsButton, at: 0)
+        if #available(iOS 11, *) {
+            buttonViewController.stackView?.setCustomSpacing(0, after: termsButton)
+        }
+    }
+}
