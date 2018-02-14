d8f22f5759b1b5e0dceee99e61d26a9539f3520a
Jorge Leandro Perez <jorge@lantean.co>
2018-02-14 15:04:45 -0300

    WordPressAuthenticator+Support: Updates enum

--- WordPress/Classes/ViewRelated/NUX/WordPressAuthenticator+Support.swift
+++ WordPress/Classes/ViewRelated/NUX/WordPressAuthenticator+Support.swift
@@ -3,17 +3,17 @@ import Foundation
 
 // MARK: - Authentication Flow Event. Useful to relay internal Auth events over to activity trackers.
 //
-public extension WordPressAuthenticator {
-    public enum SupportOrigin {
-        case generalLogin
-        case jetpackLogin
-        case loginEmail
-        case login2FA
-        case loginMagicLink
-        case loginSiteAddress
-        case loginUsernamePassword
-        case loginWPComPassword
-        case wpComSignupEmail
-        case wpComSignup
-    }
+public enum WordPressSupportSourceTag {
+    case generalLogin
+    case jetpackLogin
+    case loginEmail
+    case login2FA
+    case loginMagicLink
+    case loginSiteAddress
+    case loginUsernamePassword
+    case loginWPComPassword
+    case wpComCreateSiteDomain
+    case wpComCreateSiteDetails
+    case wpComSignupEmail
+    case wpComSignup
 }
