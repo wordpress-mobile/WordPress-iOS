772db296a82e83677f55718037f1858c019e0096
Jorge Leandro Perez <jorge@lantean.co>
2018-02-14 17:07:05 -0300

    Implements WordPressAuthenticationManager

---
+++ WordPress/Classes/ViewRelated/NUX/WordPressAuthenticationManager.swift
@@ -0,0 +1,71 @@
+import Foundation
+
+
+// MARK: - WordPressAuthenticationManager
+//
+@objc
+class WordPressAuthenticationManager: NSObject {
+
+}
+
+
+// MARK: - WordPressAuthenticator Delegate
+//
+extension WordPressAuthenticationManager: WordPressAuthenticatorDelegate {
+
+    /// Indicates whether if the Support Action should be enabled, or not.
+    ///
+    var supportActionEnabled: Bool {
+        return true
+    }
+
+    /// Returns an instance of SupportViewController, configured to be displayed from a specified Support Source.
+    ///
+    func supportViewController(from source: WordPressSupportSourceTag) -> UIViewController {
+        let supportViewController = SupportViewController()
+        supportViewController.sourceTag = source.toSupportSourceTag()
+
+        let navController = UINavigationController(rootViewController: supportViewController)
+        navController.navigationBar.isTranslucent = false
+        navController.modalPresentationStyle = .formSheet
+
+        return supportViewController
+    }
+}
+
+
+// MARK: - WordPressSupportSourceTag ClientApp Helper Methods
+//
+extension WordPressSupportSourceTag {
+
+    /// Returns the matching SupportSourceTag enum case, matching for the current WordPressSupportSourceTag (Auth Framework) enum case.
+    ///
+    func toSupportSourceTag() -> SupportSourceTag {
+        switch self {
+        case .generalLogin:
+            return .generalLogin
+        case .jetpackLogin:
+            return  .jetpackLogin
+        case .loginEmail:
+            return .loginEmail
+        case .login2FA:
+            return .login2FA
+        case .loginMagicLink:
+            return .loginMagicLink
+        case .loginSiteAddress:
+            return .loginSiteAddress
+        case .loginUsernamePassword:
+            return .loginUsernamePassword
+        case .loginWPComPassword:
+            return .loginWPComPassword
+        case .wpComCreateSiteDomain:
+            return .wpComCreateSiteDomain
+        case .wpComCreateSiteDetails:
+            return .wpComCreateSiteDetails
+        case .wpComSignupEmail:
+            return .wpComSignupEmail
+        case .wpComSignup:
+            return .wpComSignup
+        }
+    }
+}
