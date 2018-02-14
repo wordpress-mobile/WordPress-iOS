7301df68ab5924395c8b2854a18f18dc8794aab2
James Frost <james.frost@automattic.com>
2018-02-14 16:48:38 +0000

    Media Library: Simplify progress reporting for deletion
    All media items marked for deletion now have their uploads cancelled
    straight away. Media library now uses MediaCoordinator for deletion
    instead of MediaService directly.

--- WordPress/Classes/ViewRelated/Media/MediaLibraryViewController.swift
+++ WordPress/Classes/ViewRelated/Media/MediaLibraryViewController.swift
@@ -309,7 +309,7 @@ class MediaLibraryViewController: WPMediaPickerViewController {
 
         let deletedItemsCount = assets.count
 
-        let updateProgress = { (media: Media?, progress: Progress?) in
+        let updateProgress = { (progress: Progress?) in
             let fractionCompleted = progress?.fractionCompleted ?? 0
             SVProgressHUD.showProgress(Float(fractionCompleted), status: NSLocalizedString("Deleting...", comment: "Text displayed in HUD while a media item is being deleted."))
         }
@@ -318,13 +318,16 @@ class MediaLibraryViewController: WPMediaPickerViewController {
         SVProgressHUD.setMinimumDismissTimeInterval(1.0)
 
         // Initialize the progress HUD before we start
-        updateProgress(nil, nil)
+        updateProgress(nil)
         isEditing = false
-        let service = MediaService(managedObjectContext: ContextManager.sharedInstance().mainContext)
-        service.deleteMedia(assets, progress: updateProgress, success: { [weak self] () in
+
+        MediaCoordinator.shared.delete(media: assets,
+                                       onProgress: updateProgress,
+                                       success: { [weak self] in
             WPAppAnalytics.track(.mediaLibraryDeletedItems, withProperties: ["number_of_items_deleted": deletedItemsCount], with: self?.blog)
             SVProgressHUD.showSuccess(withStatus: NSLocalizedString("Deleted!", comment: "Text displayed in HUD after successfully deleting a media item"))
-        }, failure: { () in
+        },
+                                       failure: {
             SVProgressHUD.showError(withStatus: NSLocalizedString("Unable to delete all media items.", comment: "Text displayed in HUD if there was an error attempting to delete a group of media items."))
         })
     }
