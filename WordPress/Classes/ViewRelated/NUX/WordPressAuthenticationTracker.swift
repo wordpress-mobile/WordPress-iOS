b55831eeab066606f99ca9aeceb9ebfdc4fcf2c7
Jorge Leandro Perez <jorge@lantean.co>
2018-02-14 09:47:37 -0300

    WordPressAuthenticationTracker: Adds missing event

--- WordPress/Classes/ViewRelated/NUX/WordPressAuthenticationTracker.swift
+++ WordPress/Classes/ViewRelated/NUX/WordPressAuthenticationTracker.swift
@@ -29,7 +29,7 @@ extension WordPressAuthenticationTracker {
 
         switch event {
         case .createAccountInitiated:
-            break
+            WPAppAnalytics.track(.createAccountInitiated)
         case .loginAutoFillCredentialsFilled:
             WPAppAnalytics.track(.loginAutoFillCredentialsFilled)
         case .loginAutoFillCredentialsUpdated:
