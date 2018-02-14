2ec4afce39da605f831f6c72e11821f03ed8f95a
etoledom <etoledom@icloud.com>
2018-02-14 17:26:03 -0300

    SwiftLint custom rules: Removed example warnings

--- WordPress/Classes/ViewRelated/NUX/LoginEmailViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/LoginEmailViewController.swift
@@ -45,11 +45,6 @@ class LoginEmailViewController: LoginViewController, NUXKeyboardResponder {
         setupOnePasswordButtonIfNeeded()
         configureForWPComOnlyIfNeeded()
         addGoogleButton()
-
-        instructionLabel?.textAlignment = .left
-        errorLabel?.textAlignment = .right
-        googleLoginButton?.contentHorizontalAlignment = .left
-        googleLoginButton?.contentHorizontalAlignment = .right
     }
 
 
