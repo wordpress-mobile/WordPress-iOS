167693361b439c57c464b14a36fbdbf3dfcb7372
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-02-14 08:49:23 -0600

    Style fixes for ExtensionEvents enum

--- WordPress/WordPressDraftActionExtension/Tracks+DraftAction.swift
+++ WordPress/WordPressDraftActionExtension/Tracks+DraftAction.swift
@@ -6,21 +6,21 @@ extension Tracks {
     // MARK: - Public Methods
     public func trackExtensionLaunched(_ wpcomAvailable: Bool) {
         let properties = ["is_configured_dotcom": wpcomAvailable]
-        trackExtensionEvent(.Launched, properties: properties as [String: AnyObject]?)
+        trackExtensionEvent(.launched, properties: properties as [String: AnyObject]?)
     }
 
     public func trackExtensionPosted(_ status: String) {
         let properties = ["post_status": status]
-        trackExtensionEvent(.Posted, properties: properties as [String: AnyObject]?)
+        trackExtensionEvent(.posted, properties: properties as [String: AnyObject]?)
     }
 
     public func trackExtensionError(_ error: NSError) {
         let properties = ["error_code": String(error.code), "error_domain": error.domain, "error_description": error.description]
-        trackExtensionEvent(.Error, properties: properties as [String: AnyObject]?)
+        trackExtensionEvent(.error, properties: properties as [String: AnyObject]?)
     }
 
     public func trackExtensionCancelled() {
-        trackExtensionEvent(.Canceled)
+        trackExtensionEvent(.canceled)
     }
 
     // MARK: - Private Helpers
@@ -32,9 +32,9 @@ extension Tracks {
     // MARK: - Private Enums
 
     fileprivate enum ExtensionEvents: String {
-        case Launched   = "wpios_draft_extension_launched"
-        case Posted     = "wpios_draft_extension_posted"
-        case Canceled   = "wpios_draft_extension_canceled"
-        case Error      = "wpios_draft_extension_error"
+        case launched   = "wpios_draft_extension_launched"
+        case posted     = "wpios_draft_extension_posted"
+        case canceled   = "wpios_draft_extension_canceled"
+        case error      = "wpios_draft_extension_error"
     }
 }
