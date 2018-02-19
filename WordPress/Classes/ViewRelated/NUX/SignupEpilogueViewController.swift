f214084d56dd1fbd779abc95cb62485d8fecc948
Stephenie Harris <scoutharris@gmail.com>
2018-02-19 15:28:11 -0700

    Adding button view.

--- WordPress/Classes/ViewRelated/NUX/SignupEpilogueViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/SignupEpilogueViewController.swift
@@ -2,8 +2,33 @@ import UIKit
 
 class SignupEpilogueViewController: NUXViewController {
 
+    // MARK: - Properties
+
+    private var buttonViewController: NUXButtonViewController?
+
+    // MARK: - View
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+
+        if let vc = segue.destination as? NUXButtonViewController {
+            buttonViewController = vc
+            buttonViewController?.delegate = self
+            buttonViewController?.setButtonTitles(primary: NSLocalizedString("Continue", comment: "Button text on site creation epilogue page to proceed to My Sites."))
+        }
+    }
+
+}
+
+// MARK: - NUXButtonViewControllerDelegate
+
+extension SignupEpilogueViewController: NUXButtonViewControllerDelegate {
+    func primaryButtonPressed() {
+    }
 }
