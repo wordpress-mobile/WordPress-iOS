8804290e5e2d76a469f281ef43c69a9126b88b4c
Nate Heagy <nate@heagy.com>
2018-02-20 11:23:09 -0600

    Restore login screens’ bg tap gesture recognizer

---
+++ WordPressUI/WordPressUI/Extensions/Events without selectors/UIGestureRecognizer+BlockEvents.swift
@@ -0,0 +1,33 @@
+// MARK: - Gesture Recognizer
+
+private final class UITapGestureRecognizerEventHandler<Sender: UITapGestureRecognizer>: NSObject {
+    let closure: (Sender) -> Void
+
+    init(sender: Sender, closure: @escaping (Sender) -> Void) {
+        self.closure = closure
+        super.init()
+
+        sender.addTarget(self, action: #selector(self.action))
+    }
+
+    @objc private func action(sender: UITapGestureRecognizer) {
+        guard let sender = sender as? Sender else { return }
+
+        self.closure(sender)
+    }
+}
+
+extension UITapGestureRecognizer: ControlEventBindable {}
+
+extension ControlEventBindable where Self: UITapGestureRecognizer {
+    private var controlEventHandlers: [UITapGestureRecognizerEventHandler<Self>] {
+        get { return (objc_getAssociatedObject(self, &BlockEventKeys.ControlEventHandlers) as? [UITapGestureRecognizerEventHandler<Self>]) ?? [] }
+        set { objc_setAssociatedObject(self, &BlockEventKeys.ControlEventHandlers, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC) }
+    }
+
+    /// Listen for `UIControlEvents` executing the provided closure when triggered
+    public func on(call closure: @escaping (Self) -> Void) {
+        let handler = UITapGestureRecognizerEventHandler<Self>(sender: self, closure: closure)
+        self.controlEventHandlers.append(handler)
+    }
+}
