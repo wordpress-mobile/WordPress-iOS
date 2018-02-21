b9e27be574bcd2a25b9be83b23eff5a76bba3ab1
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-02-21 06:42:07 -0800

    Add some header docs

--- WordPress/Classes/ViewRelated/Accessibility/Accessible.swift
+++ WordPress/Classes/ViewRelated/Accessibility/Accessible.swift
@@ -1,3 +1,7 @@
+
+
+/// Abstraction representing and enforcing better support for accessibility
 protocol Accessible {
+    /// Prepares and Accessible element to provide rich support for voice over
     func prepareForVoiceOver()
 }
