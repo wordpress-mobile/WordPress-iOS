ec6bf912a5af84882d1fca3bd843c30c0e18705a
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-02-21 06:36:01 -0800

    Extend the header in the same file.

--- WordPress/Classes/ViewRelated/Reader/ReaderFollowedSitesStreamHeader.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderFollowedSitesStreamHeader.swift
@@ -72,3 +72,14 @@ import WordPressShared.WPStyleGuide
         borderedView.backgroundColor = UIColor.white
     }
 }
+
+
+// MARK: - Accessibility
+extension ReaderFollowedSitesStreamHeader: Accessible {
+    func configureAccessibility() {
+        isAccessibilityElement = true
+        accessibilityLabel = NSLocalizedString("Manage", comment: "Button title. Tapping lets the user manage the sites they follow.")
+        accessibilityHint = NSLocalizedString("Tapping lets you manage the sites you follow.", comment: "Accessibility hint")
+        accessibilityTraits = UIAccessibilityTraitButton
+    }
+}
