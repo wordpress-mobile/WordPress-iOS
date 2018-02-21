655e3f1e8c6ae2ec2f792524b7640ed11c6b0850
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-02-21 10:27:23 -0800

    Add a (yet empty) test suit for the Reader followed sites header

---
+++ WordPress/WordPressTest/ReaderFollowedSitesStreamHeaderTests.swift
@@ -0,0 +1,36 @@
+//
+//  ReaderFollowedSitesStreamHeader.swift
+//  WordPressTest
+//
+//  Created by Cesar Tardaguila on 21/2/2018.
+//  Copyright © 2018 WordPress. All rights reserved.
+//
+
+import XCTest
+@testable import WordPress
+
+final class ReaderFollowedSitesStreamHeaderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
