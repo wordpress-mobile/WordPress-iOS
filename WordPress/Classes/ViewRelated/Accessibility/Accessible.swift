2444b7c25386490248e7a8639ea5f006df5dc10b
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-02-21 10:28:13 -0800

    Remove whitespacing

--- WordPress/Classes/ViewRelated/Accessibility/Accessible.swift
+++ WordPress/Classes/ViewRelated/Accessibility/Accessible.swift
@@ -1,5 +1,3 @@
-
-
 /// Abstraction representing and enforcing better support for accessibility
 protocol Accessible {
     /// Prepares and Accessible element to provide rich support for voice over
