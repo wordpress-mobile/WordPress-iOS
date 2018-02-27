98b552b33b6c646a134046d8f90d23d59a668be5
Jorge Leandro Perez <jorge@lantean.co>
2018-02-27 18:42:31 -0300

    WordPressAuthenticator: Nukes duplicated code

--- WordPressUI/WordPressUI/Extensions/UIWindow+Helpers.swift
+++ WordPressUI/WordPressUI/Extensions/UIWindow+Helpers.swift
@@ -3,12 +3,6 @@ import UIKit
 extension UIWindow {
     /// Returns the view controller at the top of the view hierarchy.
     @objc public var topmostPresentedViewController: UIViewController? {
-        guard var controller = rootViewController else {
-            return nil
-        }
-        while let presented = controller.presentedViewController {
-            controller = presented
-        }
-        return controller
+        return rootViewController?.leafViewController
     }
 }
