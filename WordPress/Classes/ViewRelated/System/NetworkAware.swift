3ab6a64f3f6ac41c1a6097c0ff84fb7ce8ff4e91
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-03-06 14:40:37 -0800

    Start extracting signatures of methods handling network alerts to a set of protocols and extensions

---
+++ WordPress/Classes/ViewRelated/System/NetworkAware.swift
@@ -0,0 +1,36 @@
+
+/// Abstracts UI elements that need to be aware of the network connection status.
+protocol NetworkAware {
+    func connectionAvailable() -> Bool
+    func handleConnectionError()
+}
+
+protocol NetworkAwareUI: NetworkAware {
+    func shouldPresentAlert() -> Bool
+    func contentIsEmpty() -> Bool
+    func presentNoNetworkAlert()
+}
+
+extension NetworkAware {
+    func connectionAvailable() -> Bool {
+        return ReachabilityUtils.isInternetReachable()
+    }
+}
+
+extension NetworkAwareUI {
+    func shouldPresentAlert() -> Bool {
+        return !connectionAvailable() && !contentIsEmpty()
+    }
+
+    func handleConnectionError() {
+        if shouldPresentAlert() {
+            presentNoNetworkAlert()
+        }
+    }
+
+    func presentNetworkAlert() {
+        let title = NSLocalizedString("Unable to Sync", comment: "Title of error prompt shown when a sync the user initiated fails.")
+        let message = NSLocalizedString("The Internet connection appears to be offline.", comment: "Message of error prompt shown when a sync the user initiated fails.")
+        WPError.showAlert(withTitle: title, message: message)
+    }
+}
