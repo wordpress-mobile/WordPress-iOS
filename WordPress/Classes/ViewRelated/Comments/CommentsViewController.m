02c0eb34ea9a681d41647c42a0cbe1e86289202f
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-03-06 13:16:13 -0800

    Do not trigger a refresh after presenting the network alert

--- WordPress/Classes/ViewRelated/Comments/CommentsViewController.m
+++ WordPress/Classes/ViewRelated/Comments/CommentsViewController.m
@@ -435,13 +435,6 @@ static NSString *CommentsLayoutIdentifier                       = @"CommentsLayo
                                             NSString *title = NSLocalizedString(@"Unable to Sync", @"Title of error prompt shown when a sync the user initiated fails.");
                                             [WPError showNetworkingAlertWithError:error title:title];
                                         }
-                                        if (userInteraction) {
-                                            double delayInSeconds = 0.2;
-                                            dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-                                            dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-                                                [weakSelf refreshPullToRefresh];
-                                            });
-                                        }
                                     }];
     }];
 }
