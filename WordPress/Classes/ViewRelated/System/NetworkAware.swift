fab588664e399e0e9d683b19a5a91a660f40ab93
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-03-06 15:37:31 -0800

    Add a method to return the no connection message

--- WordPress/Classes/ViewRelated/System/NetworkAware.swift
+++ WordPress/Classes/ViewRelated/System/NetworkAware.swift
@@ -9,6 +9,7 @@ protocol NetworkAwareUI: NetworkAware {
     func shouldPresentAlert() -> Bool
     func contentIsEmpty() -> Bool
     func presentNoNetworkAlert()
+    func noConnectionMessage() -> String
 }
 
 extension NetworkAware {
@@ -32,5 +33,9 @@ extension NetworkAwareUI {
         let title = NSLocalizedString("Unable to Sync", comment: "Title of error prompt shown when a sync the user initiated fails.")
         let message = NSLocalizedString("The Internet connection appears to be offline.", comment: "Message of error prompt shown when a sync the user initiated fails.")
         WPError.showAlert(withTitle: title, message: message)
+    }
+
+    func noConnectionMessage() -> String {
+        return ReachabilityUtils.noConnectionMessage()
     }
 }
