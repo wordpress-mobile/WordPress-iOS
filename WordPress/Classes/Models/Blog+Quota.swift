89214c7f0d893f1eb124aa30fc85e50fa277d658
Sergio Estevao <sergioestevao@gmail.com>
2018-03-06 11:55:46 +0000

    Simplifies code and add method comments.

--- WordPress/Classes/Models/Blog+Quota.swift
+++ WordPress/Classes/Models/Blog+Quota.swift
@@ -3,18 +3,21 @@ import Foundation
 /// This extension add methods to make the manipulation of disk quota values for a blog easier
 extension Blog {
 
+    /// Returns true if quota values are available for the site
     @objc var isQuotaAvailable: Bool {
         return quotaSpaceAllowed != nil && quotaSpaceUsed != nil
     }
 
+    /// Returns the percentage value (0 to 1) of the disk space quota being used
     @objc var quotaPercentageUsed: NSNumber? {
-        guard let quotaSpaceAllowed = quotaSpaceAllowed?.int64Value, let quotaSpaceUsed = quotaSpaceUsed?.int64Value else {
+        guard let quotaSpaceAllowed = quotaSpaceAllowed?.doubleValue, let quotaSpaceUsed = quotaSpaceUsed?.doubleValue else {
             return nil
         }
-        let quotaPercentageUsed = Double(quotaSpaceUsed) / Double(quotaSpaceAllowed)
+        let quotaPercentageUsed = quotaSpaceUsed / quotaSpaceAllowed
         return NSNumber(value: quotaPercentageUsed)
     }
 
+    /// Returns the quota space allowed in a string format using human readable units (kb, Mb, Gb). EX: 1.5 Gb
     @objc var quotaSpaceAllowedDescription: String? {
         guard let quotaSpaceAllowed = quotaSpaceAllowed?.int64Value else {
             return nil
@@ -22,6 +25,7 @@ extension Blog {
         return ByteCountFormatter.string(fromByteCount: quotaSpaceAllowed, countStyle: .binary)
     }
 
+    /// Returns the disk quota percentage in a human readable format. Ex: 5%
     @objc var quotaPercentageUsedDescription: String? {
         let percentageFormatter = NumberFormatter()
         percentageFormatter.numberStyle = .percent
