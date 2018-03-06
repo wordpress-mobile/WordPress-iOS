8382e9488ab01ed419f286044ca0834c65336e45
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-03-06 14:48:35 -0800

    Fix typo in method signature

--- WordPress/Classes/ViewRelated/System/NetworkAware.swift
+++ WordPress/Classes/ViewRelated/System/NetworkAware.swift
@@ -28,7 +28,7 @@ extension NetworkAwareUI {
         }
     }
 
-    func presentNetworkAlert() {
+    func presentNoNetworkAlert() {
         let title = NSLocalizedString("Unable to Sync", comment: "Title of error prompt shown when a sync the user initiated fails.")
         let message = NSLocalizedString("The Internet connection appears to be offline.", comment: "Message of error prompt shown when a sync the user initiated fails.")
         WPError.showAlert(withTitle: title, message: message)
