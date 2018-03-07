197cb2fd50d5414b1a3681704d356763f91ecb52
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-03-07 15:06:23 -0800

    Remove a poorly worded comment

--- WordPress/Classes/ViewRelated/Comments/CommentsViewController+NetworkAware.swift
+++ WordPress/Classes/ViewRelated/Comments/CommentsViewController+NetworkAware.swift
@@ -1,7 +1,5 @@
 import UIKit
 
-
-// CommentsViewController is an Objective-C class, so in order for interop to work, it looks like we need to override these methods.
 extension CommentsViewController: NetworkAwareUI {
     func contentIsEmpty() -> Bool {
         return tableViewHandler.resultsController.isEmpty()
