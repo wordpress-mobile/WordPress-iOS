0c72140d6db4d0f0ed679116c811cf0921a857b0
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-03-06 19:24:44 -0800

    First attempt at building an observer to reachability

--- WordPress/Classes/ViewRelated/System/NetworkAware.swift
+++ WordPress/Classes/ViewRelated/System/NetworkAware.swift
@@ -40,3 +40,43 @@ extension NetworkAwareUI {
         return ReachabilityUtils.noConnectionMessage()
     }
 }
+
+protocol NetworkStatusDelegate: class {
+    func observeNetworkStatus()
+    func networdStatusDidChange()
+}
+
+extension NetworkStatusDelegate where Self: UIViewController {
+    func observeNetworkStatus() {
+        let _ = ReachabilityObserver(delegate: self)
+    }
+}
+
+final fileprivate class ReachabilityObserver: NSObject {
+    private static var observerContext = 0
+    private weak var delegate: NetworkStatusDelegate?
+
+    init(delegate: NetworkStatusDelegate) {
+        self.delegate = delegate
+        super.init()
+
+        configureObserver()
+    }
+
+    private func configureObserver() {
+        if let appDelegate = UIApplication.shared.delegate as? WordPressAppDelegate {
+            appDelegate.addObserver(self, forKeyPath: #keyPath(WordPressAppDelegate.connectionAvailable), options: [.new], context: &type(of: self).observerContext)
+        }
+    }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == #keyPath(WordPressAppDelegate.connectionAvailable), let newValue = change?[.newKey] as? Bool {
+            //
+            print("======= rechability changed in my observer====")
+        }
+    }
+
+    deinit {
+        removeObserver(self, forKeyPath: #keyPath(WordPressAppDelegate.connectionAvailable))
+    }
+}
