81ee9ec65f5043aabe5e31c2596c376cf5632a63
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-03-06 17:31:06 -0800

    And delay refreshcontrol

--- WordPress/Classes/ViewRelated/Comments/CommentsViewController.m
+++ WordPress/Classes/ViewRelated/Comments/CommentsViewController.m
@@ -429,6 +429,11 @@ static NSString *CommentsLayoutIdentifier                       = @"CommentsLayo
                                                 failure(error);
                                             });
                                         }
+
+                                        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                                            [weakSelf refreshPullToRefresh];
+                                        });
+                                        
                                         dispatch_async(dispatch_get_main_queue(), ^{
                                             [weakSelf handleConnectionError];
                                         });
