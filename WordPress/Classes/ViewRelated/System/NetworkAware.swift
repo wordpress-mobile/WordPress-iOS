158eaab1d6486c20acacc164f4323cd8f06e460d
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-03-07 21:53:26 -0800

    Add some header docs

--- WordPress/Classes/ViewRelated/System/NetworkAware.swift
+++ WordPress/Classes/ViewRelated/System/NetworkAware.swift
@@ -41,10 +41,7 @@ extension NetworkAwareUI {
     }
 }
 
-fileprivate struct NetworkStatusAssociatedKeys {
-    static var associatedObjectKey = "com.later.error.notification.receiver"
-}
-
+/// Implementations of this protocol will be notified when the network connection status changes. Implementations of this protocol must call the observeNetworkStatus method.
 protocol NetworkStatusDelegate: class {
     func observeNetworkStatus()
 
@@ -65,56 +62,11 @@ extension NetworkStatusDelegate where Self: UIViewController {
             objc_setAssociatedObject(self, &NetworkStatusAssociatedKeys.associatedObjectKey, newValue, .OBJC_ASSOCIATION_RETAIN)
         }
     }
-
-//    private(set) var reachabilityObserver: ReachabilityObserver {
-//        get {
-//            guard let value = objc_getAssociatedObject(self, &NetworkStatusAssociatedKeys.observer) as? ReachabilityObserver else {
-//                return ReachabilityObserver(delegate: self)
-//            }
-//            return value
-//        }
-//        set(newValue) {
-//            objc_setAssociatedObject(self, &NetworkStatusAssociatedKeys.observer, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-//        }
-//    }
-//
-//    func observeNetworkStatus() {
-//        reachabilityObserver = ReachabilityObserver(delegate: self)
-//    }
 }
 
-//@objc final class ReachabilityObserver: NSObject {
-//    private static var observerContext = 0
-//    weak var delegate: NetworkStatusDelegate?
-//
-//    init(delegate: NetworkStatusDelegate) {
-//        self.delegate = delegate
-//        super.init()
-//
-//        configureObserver()
-//    }
-//
-//    private func configureObserver() {
-//        if let appDelegate = UIApplication.shared.delegate as? WordPressAppDelegate {
-//            appDelegate.addObserver(self, forKeyPath: #keyPath(WordPressAppDelegate.connectionAvailable), options: [.old, .new], context: &type(of: self).observerContext)
-//        }
-//    }
-//
-//    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
-//        if keyPath == #keyPath(WordPressAppDelegate.connectionAvailable),
-//            let oldValue = change?[.oldKey] as? Bool,
-//            let newValue = change?[.newKey] as? Bool {
-//            if oldValue != newValue {
-//                delegate?.networdStatusDidChange(active: newValue)
-//            }
-//        }
-//    }
-//
-//    deinit {
-//        removeObserver(self, forKeyPath: #keyPath(WordPressAppDelegate.connectionAvailable))
-//    }
-//}
-
+fileprivate struct NetworkStatusAssociatedKeys {
+    static var associatedObjectKey = "com.later.error.notification.receiver"
+}
 
 fileprivate final class ReachabilityNotificationObserver: NSObject {
     private weak var delegate: NetworkStatusDelegate?
@@ -122,9 +74,10 @@ fileprivate final class ReachabilityNotificationObserver: NSObject {
     init(delegate: NetworkStatusDelegate) {
         self.delegate = delegate
         super.init()
+        observeErrors()
     }
 
-    func observeErrors() {
+    private func observeErrors() {
         NotificationCenter.default.addObserver(self, selector: #selector(receive), name: .reachabilityChanged, object: nil)
     }
 
