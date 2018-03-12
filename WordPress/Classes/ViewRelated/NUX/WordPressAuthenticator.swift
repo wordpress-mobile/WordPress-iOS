d82f4c62353efc1b4ed8a5cc8446975fdbe3bcdb
Jorge Leandro Perez <jorge@lantean.co>
2018-03-12 13:18:50 -0300

    WordPress Auth: Updating outdated comments

--- WordPress/Classes/ViewRelated/NUX/WordPressAuthenticator.swift
+++ WordPress/Classes/ViewRelated/NUX/WordPressAuthenticator.swift
@@ -66,7 +66,7 @@ public protocol WordPressAuthenticatorDelegate: class {
     }
 
 
-    // MARK: - Public MethodsauthenticationInfoKey
+    // MARK: - Public Methods
 
     func supportBadgeCountWasUpdated() {
         NotificationCenter.default.post(name: .wordpressSupportBadgeUpdated, object: nil)
