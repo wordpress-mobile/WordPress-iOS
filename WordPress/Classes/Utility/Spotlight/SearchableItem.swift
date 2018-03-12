a5c02382a6f4241dfa480ac5cca0b3cf648a3f57
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-03-12 15:52:57 -0500

    WIP: Post/Pages indexing

---
+++ WordPress/Classes/Utility/Spotlight/SearchableItem.swift
@@ -0,0 +1,65 @@
+import CoreSpotlight
+import MobileCoreServices
+
+// MARK: - SearchableItemConvertable
+
+@objc protocol SearchableItemConvertable {
+
+    /// The value that uniquely identifies the searchable item
+    ///
+    var searchIdentifier: String {get}
+
+    /// The value that what class of items this searchable belongs to
+    ///
+    var searchDomain: String {get}
+
+    /// Item title to be displayed in spotlight search
+    ///
+    var searchTitle: String? {get}
+
+    /// Item description
+    ///
+    var searchDescription: String? {get}
+
+    // MARK: Optional Vars
+
+    /// An array of keywords associated with the item
+    ///
+    @objc optional var searchKeywords: [String]? {get}
+
+    /// Item thumbnail to be displayed in spotlight search
+    ///
+    @objc optional var searchImage: UIImage? {get}
+}
+
+extension SearchableItemConvertable {
+    var thumbnailImage: UIImage? { return nil }
+
+    internal var uniqueIdentifier: String {
+        return SearchIdentifierGenerator.composeUniqueIdentifier(domain: searchDomain, identifier: searchIdentifier)
+    }
+
+    internal func indexableItem() -> CSSearchableItem {
+        let searchableItemAttributeSet = CSSearchableItemAttributeSet(itemContentType: kUTTypeText as String)
+        searchableItemAttributeSet.title = searchTitle
+        searchableItemAttributeSet.contentDescription = searchDescription
+
+        if let keywords = searchKeywords {
+            searchableItemAttributeSet.keywords = keywords
+        }
+
+        if let img = searchImage {
+            searchableItemAttributeSet.thumbnailData = UIImageJPEGRepresentation(img!, 0.1)
+        }
+
+        return CSSearchableItem(uniqueIdentifier: uniqueIdentifier,
+                                domainIdentifier: searchDomain,
+                                attributeSet: searchableItemAttributeSet)
+    }
+}
+
+// MARK: - SearchableItemRetrievable
+
+protocol SearchableItemRetrievable: SearchableItemConvertable {
+    func retrieveItem(with identifier: String) throws -> Self?
+}
