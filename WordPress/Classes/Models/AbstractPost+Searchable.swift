1cfaa5bbd3470a08ec624bcf35b9fd354aac4ed6
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-03-13 21:39:32 -0500

    Now indexing all the viewable items on PostList VC

--- WordPress/Classes/Models/AbstractPost+Searchable.swift
+++ WordPress/Classes/Models/AbstractPost+Searchable.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-extension Post: SearchableItemConvertable {
+extension AbstractPost: SearchableItemConvertable {
     var searchIdentifier: String? {
         guard let postID = postID, postID.intValue > 0 else {
             return nil
@@ -26,8 +26,7 @@ extension Post: SearchableItemConvertable {
     }
 
     var searchDescription: String? {
-        let postPreview = contentPreviewForDisplay()
-        guard !postPreview.isEmpty else {
+        guard let postPreview = contentPreviewForDisplay() else {
             return String()
         }
         return postPreview
@@ -43,15 +42,11 @@ extension Post: SearchableItemConvertable {
 
     // MARK: - Helper Functions
 
-    private func generateKeywords() -> [String]? {
-        // Keywords defaults to tags
-        guard hasTags() else {
-           return generateKeywordsFromContent()
-        }
-        return tags?.arrayOfTags()
+    func generateKeywords() -> [String]? {
+       return generateKeywordsFromContent()
     }
 
-    private func generateKeywordsFromContent() -> [String]? {
+    func generateKeywordsFromContent() -> [String]? {
         var keywords: [String]? = nil
         if let postTitle = postTitle {
             // Try to generate some keywords from the title...
