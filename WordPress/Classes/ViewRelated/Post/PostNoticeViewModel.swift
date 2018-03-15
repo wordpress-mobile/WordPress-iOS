4d92bac594f5a23360a72e644b4c2d5da498aa67
James Frost <james.frost@automattic.com>
2018-03-15 13:41:24 +0000

    PostNoticeViewModel: Handle different post statuses

--- WordPress/Classes/ViewRelated/Post/PostNoticeViewModel.swift
+++ WordPress/Classes/ViewRelated/Post/PostNoticeViewModel.swift
@@ -4,24 +4,58 @@ struct PostNoticeViewModel {
     let post: AbstractPost
 
     private var title: String {
-        let isPage = post is Page
-        let isDraft = post.isDraft()
-
-        switch (isPage, isDraft) {
-        case (true, true):
-            return NSLocalizedString("Page draft uploaded.", comment: "Title of notification displayed when a page has been successfully saved as a draft.")
-        case (false, true):
-            return NSLocalizedString("Post draft uploaded.", comment: "Title of notification displayed when a post has been successfully saved as a draft.")
-        case (true, false):
-            return NSLocalizedString("Page published.", comment: "Title of notification displayed when a page has been successfully published.")
-        case (false, false):
-            return NSLocalizedString("Post published.", comment: "Title of notification displayed when a post has been successfully published.")
+        if let page = post as? Page {
+            return title(for: page)
+        } else {
+            return title(for: post)
         }
+    }
+
+    private func title(for page: Page) -> String {
+        let status = page.status ?? .publish
+
+        switch status {
+        case .draft:
+            return NSLocalizedString("Page draft uploaded", comment: "Title of notification displayed when a page has been successfully saved as a draft.")
+        case .scheduled:
+            return NSLocalizedString("Page scheduled", comment: "Title of notification displayed when a page has been successfully scheduled.")
+        case .pending:
+            return NSLocalizedString("Page pending review", comment: "Title of notification displayed when a page has been successfully saved as a draft.")
+        default:
+            return NSLocalizedString("Page published", comment: "Title of notification displayed when a page has been successfully published.")
+        }
+    }
+
+    private func title(for post: AbstractPost) -> String {
+        let status = post.status ?? .publish
+
+        switch status {
+        case .draft:
+            return NSLocalizedString("Post draft uploaded", comment: "Title of notification displayed when a post has been successfully saved as a draft.")
+        case .scheduled:
+            return NSLocalizedString("Post scheduled", comment: "Title of notification displayed when a post has been successfully scheduled.")
+        case .pending:
+            return NSLocalizedString("Post pending review", comment: "Title of notification displayed when a post has been successfully saved as a draft.")
+        default:
+            return NSLocalizedString("Post published", comment: "Title of notification displayed when a post has been successfully published.")
+        }
+    }
+
+    private var actionTitle: String {
+        if post.status == .draft {
+            return "Publish"
+        }
+
+        return "View"
     }
 
     var notice: Notice {
         return Notice(title: title,
                       message: nil,
-                      feedbackType: .success)
+                      feedbackType: .success,
+                      actionTitle: actionTitle,
+                      actionHandler: {
+                        
+        })
     }
 }
