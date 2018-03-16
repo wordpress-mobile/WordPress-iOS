e79b7135e27fac09463d4fe22fd2e45c90b56c16
James Frost <james.frost@automattic.com>
2018-03-16 11:46:38 +0000

    PostNoticeViewModel: Implement publish / view actions

--- WordPress/Classes/ViewRelated/Post/PostNoticeViewModel.swift
+++ WordPress/Classes/ViewRelated/Post/PostNoticeViewModel.swift
@@ -3,6 +3,27 @@ import UIKit
 struct PostNoticeViewModel {
     let post: AbstractPost
 
+    /// Returns the Notice represented by this view model.
+    ///
+    var notice: Notice {
+        let action = self.action
+
+        return Notice(title: title,
+                      message: message,
+                      feedbackType: .success,
+                      actionTitle: action.title,
+                      actionHandler: {
+                        switch action {
+                        case .publish:
+                            self.publishPost()
+                        case .view:
+                            self.viewPost()
+                        }
+        })
+    }
+
+    // MARK: - Display values for Notice
+
     private var title: String {
         if let page = post as? Page {
             return title(for: page)
@@ -41,21 +62,86 @@ struct PostNoticeViewModel {
         }
     }
 
-    private var actionTitle: String {
-        if post.status == .draft {
-            return "Publish"
+    private var message: String {
+        let title = post.postTitle ?? ""
+        if title.count > 0 {
+            return title
         }
 
-        return "View"
+        return post.blog.displayURL as String? ?? ""
     }
 
-    var notice: Notice {
-        return Notice(title: title,
-                      message: nil,
-                      feedbackType: .success,
-                      actionTitle: actionTitle,
-                      actionHandler: {
-                        
-        })
+    private enum Action {
+        case publish
+        case view
+
+        var title: String {
+            switch self {
+            case .publish:
+                return NSLocalizedString("Publish", comment: "Button title. Publishes a post.")
+            case .view:
+                return NSLocalizedString("View", comment: "Button title. Displays a summary / sharing screen for a specific post.")
+            }
+        }
+    }
+
+    private var action: Action {
+        return (post.status == .draft) ? .publish : .view
+    }
+
+    // MARK: - Actions
+
+    private func viewPost() {
+        if post is Page {
+            presentViewPage()
+        } else {
+            presentPostPost()
+        }
+    }
+
+    private func presentViewPage() {
+        guard let presenter = UIApplication.shared.delegate?.window??.topmostPresentedViewController,
+            let post = postInContext else {
+                return
+        }
+
+        let controller = PostPreviewViewController(post: post)
+        controller.navigationItem.title = NSLocalizedString("View", comment: "Verb. The screen title shown when viewing a post inside the app.")
+        controller.hidesBottomBarWhenPushed = true
+
+        let navigationController = UINavigationController(rootViewController: controller)
+        navigationController.modalPresentationStyle = .formSheet
+        presenter.present(navigationController, animated: true, completion: nil)
+    }
+
+    private func presentPostPost() {
+        guard let presenter = UIApplication.shared.delegate?.window??.topmostPresentedViewController,
+            let post = postInContext else {
+                return
+        }
+
+        let editor = EditPostViewController(post: post as! Post)
+        editor.modalPresentationStyle = .fullScreen
+        editor.openWithPostPost = true
+        editor.onClose = { _ in
+        }
+        presenter.present(editor, animated: true, completion: nil)
+    }
+
+    private func publishPost() {
+        guard let post = postInContext else {
+            return
+        }
+
+        post.status = .publish
+        PostCoordinator.shared.save(post: post)
+    }
+
+    private var postInContext: AbstractPost? {
+        let context = ContextManager.sharedInstance().mainContext
+        let objectInContext = try? context.existingObject(with: post.objectID)
+        let postInContext = objectInContext as? AbstractPost
+
+        return postInContext
     }
 }
