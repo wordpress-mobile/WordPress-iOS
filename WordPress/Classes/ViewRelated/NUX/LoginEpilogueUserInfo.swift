4cbc716c96ae941665e8202f5a0f4370e4163a2f
Jorge Leandro Perez <jorge@lantean.co>
2018-03-19 15:59:40 -0400

    LoginEpilogueUserInfo: New public methods

--- WordPress/Classes/ViewRelated/NUX/LoginEpilogueUserInfo.swift
+++ WordPress/Classes/ViewRelated/NUX/LoginEpilogueUserInfo.swift
@@ -1,6 +1,7 @@
 import Foundation
 import GoogleSignIn
 
+
 /// A simple container for the user info shown on the login epilogue screen.
 ///
 public struct LoginEpilogueUserInfo {
@@ -8,7 +9,6 @@ public struct LoginEpilogueUserInfo {
     var fullName = ""
     var email = ""
     var gravatarUrl: String?
-    var site: WordPressSite?
 
     init(account: WPAccount) {
         if let name = account.username {
@@ -35,6 +35,27 @@ public struct LoginEpilogueUserInfo {
     }
 
     init() {
+        // NO-OP
+    }
+}
+
+
+// MARK: - LoginEpilogueUserInfo
+//
+extension LoginEpilogueUserInfo {
+
+    /// Updates the Epilogue properties, given an UserProfile instance.
+    ///
+    mutating func update(with profile: UserProfile) {
+        username = profile.username
+        fullName = profile.displayName
+        email = profile.email
     }
 
+    /// Updates the Epilogue properties, given a GravatarProfile instance.
+    ///
+    mutating func update(with profile: GravatarProfile) {
+        gravatarUrl = profile.thumbnailUrl
+        fullName = profile.displayName
+    }
 }
