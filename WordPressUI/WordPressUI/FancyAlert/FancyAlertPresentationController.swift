3b26a2cbb02db4fb64f2eba3b1afd6fb6bf725d1
Jorge Leandro Perez <jorge@lantean.co>
2018-03-22 15:08:57 -0400

    FancyAlertPresentationController: Updates visibility

--- WordPressUI/WordPressUI/FancyAlert/FancyAlertPresentationController.swift
+++ WordPressUI/WordPressUI/FancyAlert/FancyAlertPresentationController.swift
@@ -3,7 +3,7 @@ import UIKit
 /// Presents a view controller with a dimming view behind that slowly fades in
 /// as the presented view controller slides up.
 ///
-class FancyAlertPresentationController: UIPresentationController, UIViewControllerTransitioningDelegate {
+open class FancyAlertPresentationController: UIPresentationController, UIViewControllerTransitioningDelegate {
     private struct Constants {
         static let dimmingViewAlpha: CGFloat = 0.5
     }
@@ -15,7 +15,7 @@ class FancyAlertPresentationController: UIPresentationController, UIViewControll
         return $0
     }(UIView())
 
-    override func presentationTransitionWillBegin() {
+    override open func presentationTransitionWillBegin() {
         guard let containerView = containerView else { return }
 
         containerView.addSubview(dimmingView)
@@ -31,13 +31,13 @@ class FancyAlertPresentationController: UIPresentationController, UIViewControll
         })
     }
 
-    override func presentationTransitionDidEnd(_ completed: Bool) {
+    override open func presentationTransitionDidEnd(_ completed: Bool) {
         if !completed {
             dimmingView.removeFromSuperview()
         }
     }
 
-    override func dismissalTransitionWillBegin() {
+    override open func dismissalTransitionWillBegin() {
         guard let coordinator = presentedViewController.transitionCoordinator else {
             dimmingView.alpha = UIKitConstants.alphaZero
             return
@@ -49,7 +49,7 @@ class FancyAlertPresentationController: UIPresentationController, UIViewControll
         })
     }
 
-    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+    open func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
         guard presented == self.presentedViewController,
             presenting == self.presentingViewController else {
                 return nil
