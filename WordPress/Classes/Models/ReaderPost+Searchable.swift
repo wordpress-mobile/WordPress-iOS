0e4f34a4d4c8e604e97f3d7102d80dcc4f06d79e
Matt Bumgardner <matt.bumgardner@gmail.com>
2018-03-23 10:09:46 -0500

    Added ReaderPost+Searchable; introduced exp date in SearchableItemConvertable protocol

---
+++ WordPress/Classes/Models/ReaderPost+Searchable.swift
@@ -0,0 +1,62 @@
+import Foundation
+
+extension ReaderPost: SearchableItemConvertable {
+    var searchItemType: SearchItemType {
+        return .readerPost
+    }
+
+    var isSearchable: Bool {
+        guard let title = searchTitle, !title.isEmpty else {
+            // If the title is empty or nil, don't index it
+            return false
+        }
+        return true
+    }
+
+    var searchIdentifier: String? {
+        guard let postID = postID, postID.intValue > 0 else {
+            return nil
+        }
+        return postID.stringValue
+    }
+
+    var searchDomain: String? {
+        return SearchItemType.readerPost.stringValue()
+    }
+
+    var searchTitle: String? {
+        return titleForDisplay()
+    }
+
+    var searchDescription: String? {
+        guard let readerPostPreview = contentPreviewForDisplay(), !readerPostPreview.isEmpty else {
+            return siteURLForDisplay() ?? contentForDisplay()
+        }
+        return readerPostPreview
+    }
+
+    var searchKeywords: [String]? {
+        return generateKeywordsFromContent()
+    }
+
+    var searchExpirationDate: Date? {
+        let sevenDaysFromNow = Calendar.current.date(byAdding: .day, value: 7, to: Date())
+        return sevenDaysFromNow
+    }
+}
+
+// MARK: - Private Helper Functions
+
+fileprivate extension ReaderPost {
+    func generateKeywordsFromContent() -> [String]? {
+        var keywords: [String]? = nil
+        if let postTitle = postTitle {
+            // Try to generate some keywords from the title...
+            keywords = postTitle.components(separatedBy: " ").map({ $0.trimmingCharacters(in: .whitespacesAndNewlines) })
+        } else if !contentPreviewForDisplay().isEmpty {
+            // ...otherwise try to generate some keywords from the content preview
+            keywords = contentPreviewForDisplay().components(separatedBy: " ").map({ $0.trimmingCharacters(in: .whitespacesAndNewlines) })
+        }
+        return keywords
+    }
+}
