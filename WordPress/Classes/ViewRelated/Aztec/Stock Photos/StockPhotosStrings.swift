0688a9767cb2f81244b34fbe896a75336317beeb
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-03-27 13:05:05 +0800

    Add a description to localized strings

--- WordPress/Classes/ViewRelated/Aztec/Stock Photos/StockPhotosStrings.swift
+++ WordPress/Classes/ViewRelated/Aztec/Stock Photos/StockPhotosStrings.swift
@@ -2,14 +2,14 @@
 extension String {
     // MARK: - Entry point: alert controller
     static var freePhotosLibrary: String {
-        return NSLocalizedString("Free Photo Library", comment: "")
+        return NSLocalizedString("Free Photo Library", comment: "One of the options when selecting More in the Post Editor's format bar")
     }
 
     static var files: String {
-        return NSLocalizedString("Files", comment: "")
+        return NSLocalizedString("Files", comment: "Label for the action Open Files in the post composer")
     }
 
     static var cancelMoreOptions: String {
-        return NSLocalizedString("Cancel", comment: "")
+        return NSLocalizedString("Cancel", comment: "Dismisses the Alert from Screen")
     }
 }
