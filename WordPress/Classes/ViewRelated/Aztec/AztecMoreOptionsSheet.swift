7def935a1c27d5fc5727531e2bbcb080f62e4db1
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-03-27 12:35:13 +0800

    Present both options

--- WordPress/Classes/ViewRelated/Aztec/AztecMoreOptionsSheet.swift
+++ WordPress/Classes/ViewRelated/Aztec/AztecMoreOptionsSheet.swift
@@ -2,18 +2,34 @@ import MobileCoreServices
 
 final class AztecMoreOptionsSheet {
     func present(origin: UIViewController, view: UIView) {
-        let alertController = UIAlertController(title: "Cesar", message: nil, preferredStyle: .actionSheet)
-        alertController.addActionWithTitle("Cancel",
-                                           style: .cancel,
-                                           handler: { (action) in
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
 
-        })
+        alertController.addAction(freePhoto())
+        alertController.addAction(files(origin: origin))
+        alertController.addAction(cancel())
 
 
         alertController.popoverPresentationController?.sourceView = view
         alertController.popoverPresentationController?.sourceRect = CGRect(origin: view.frame.origin, size: CGSize(width: 1, height: 1))
         alertController.popoverPresentationController?.permittedArrowDirections = .any
         origin.present(alertController, animated: true, completion: nil)
+    }
+
+    private func freePhoto() -> UIAlertAction {
+        return UIAlertAction(title: .freePhotosLibrary, style: .default, handler: { action in
+            print("going to free photos")
+        })
+    }
+
+    private func files(origin: UIViewController) -> UIAlertAction {
+        return UIAlertAction(title: .files, style: .default, handler: { [weak self] action in
+            print("going to free photos")
+            self?.showDocumentPicker(origin: origin)
+        })
+    }
+
+    private func cancel() -> UIAlertAction {
+        return UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
     }
 
     private func showDocumentPicker(origin: UIViewController) {
