698131d8f9aa71b4d91b7336314cb47750e827fb
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-03-27 12:34:49 +0800

    Add an extension to String to avoid littering the code with NSLocalizedString()

---
+++ WordPress/Classes/ViewRelated/Aztec/StockPhotosStrings.swift
@@ -0,0 +1,9 @@
+extension String {
+    static var freePhotosLibrary: String {
+        return NSLocalizedString("Free Photo Library", comment: "")
+    }
+
+    static var files: String {
+        return NSLocalizedString("Files", comment: "")
+    }
+}
