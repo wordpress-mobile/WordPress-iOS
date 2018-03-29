2f00133012be2e37a1a5031905373e479c193871
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-03-29 12:44:37 +0800

    Retain blog and initialize datasource lazily

--- WordPress/Classes/ViewRelated/Aztec/Stock Photos/StockPhotosPicker.swift
+++ WordPress/Classes/ViewRelated/Aztec/Stock Photos/StockPhotosPicker.swift
@@ -6,10 +6,15 @@ protocol StockPhotosPickerDelegate: AnyObject {
 
 /// Presents the Stock Photos main interface
 final class StockPhotosPicker: NSObject {
-    private let dataSource = StockPhotosDataSource(service: StockPhotosServiceMock())
+    private lazy var dataSource: StockPhotosDataSource = {
+        return StockPhotosDataSource(service: StockPhotosServiceMock())
+    }()
+
     weak var delegate: StockPhotosPickerDelegate?
+    private var blog: Blog?
 
     func presentPicker(origin: UIViewController, blog: Blog) {
+        self.blog = blog
         let options = WPMediaPickerOptions()
         options.showMostRecentFirst = true
         options.filter = [.all]
