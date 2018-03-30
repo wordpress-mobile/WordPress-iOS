9f9041dc2af157b675bc541fc4bea4f15f946ed2
elibud <elibud@gmail.com>
2018-03-30 18:21:11 -0300

    Remove dead classes WPAsyncBlockOperation and CreateNewBlogViewController

--- WordPress/Classes/ViewRelated/Blog/CreateNewBlog/CreateNewBlogViewController.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-
-FOUNDATION_EXPORT NSString * const NewWPComBlogCreatedNotification;
-FOUNDATION_EXPORT NSString * const NewWPComBlogCreatedNotificationBlogUserInfoKey;
-
-@interface CreateNewBlogViewController : UIViewController
-
-@end
