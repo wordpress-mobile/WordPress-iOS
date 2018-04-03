09d703f38ad8a37d5dcdd01f81abd3298332f708
Jorge Leandro Perez <jorge@lantean.co>
2018-04-03 17:10:27 -0300

    UIImageView+Networking: Splitting Resize and Download

--- WordPressUI/WordPressUI/Extensions/UIImageView+Networking.swift
+++ WordPressUI/WordPressUI/Extensions/UIImageView+Networking.swift
@@ -3,15 +3,43 @@ import Foundation
 
 public extension UIImageView {
 
+    ///
+    ///
+    public func downloadResizedImage(at url: URL, placeholderImage: UIImage? = nil, pointSize: CGSize) {
+        downloadImage(at: url, placeholderImage: placeholderImage, success: { [weak self] image in
+            var resizedImage = image
+            if image.size.height > pointSize.height || image.size.width > pointSize.width {
+                resizedImage = image.resizedImage(with: .scaleAspectFit, bounds: pointSize, interpolationQuality: .high)
+            }
+
+            self?.image = resizedImage
+        })
+    }
+
     /// Downloads an image and updates the UIImageView Instance
     ///
     /// - Parameter url: The URL of the target image
     ///
-    public func downloadImage(_ url: URL, pointSize: CGSize? = nil) {
+    public func downloadImage(at url: URL, placeholderImage: UIImage? = nil, success: ((UIImage) -> ())? = nil, failure: ((Error?) -> ())? = nil) {
+        // Let's wrap the onSuccess callback
+        let internalOnSuccess = { [weak self] (image: UIImage) in
+            guard let success = success else {
+                self?.image = image
+                return
+            }
+
+            success(image)
+        }
+
         // Hit the cache
         if let cachedImage = Downloader.cache.object(forKey: url as AnyObject) as? UIImage {
-            self.image = cachedImage
+            internalOnSuccess(cachedImage)
             return
+        }
+
+        // Placeholder?
+        if let placeholderImage = placeholderImage {
+            image = placeholderImage
         }
 
         // Cancel any previous OP's
@@ -28,19 +56,17 @@ public extension UIImageView {
         let session = URLSession.shared
         let task = session.dataTask(with: request, completionHandler: { [weak self] data, response, error in
             guard let data = data, let image = UIImage(data: data, scale: UIScreen.main.scale) else {
+                failure?(error)
                 return
             }
 
             DispatchQueue.main.async {
-                // Resize if needed!
-                var resizedImage = image
-                if let pointSize = pointSize, image.size.height > pointSize.height || image.size.width > pointSize.width {
-                    resizedImage = image.resizedImage(with: .scaleAspectFit, bounds: pointSize, interpolationQuality: .high)
-                }
-
                 // Update the Cache
-                Downloader.cache.setObject(resizedImage, forKey: url as AnyObject)
-                self?.image = resizedImage
+                Downloader.cache.setObject(image, forKey: url as AnyObject)
+                internalOnSuccess(image)
+
+                // Cleanup
+                self?.downloadTask = nil
             }
         })
 
