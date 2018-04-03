3695fc296f77f441516a9aeeda8fac556f52048e
Nate Heagy <nate@heagy.com>
2018-04-03 14:04:44 -0700

    Connect login’s email screen to signup flow

--- WordPress/Classes/ViewRelated/NUX/LoginEmailViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/LoginEmailViewController.swift
@@ -198,8 +198,8 @@ class LoginEmailViewController: LoginViewController, NUXKeyboardResponder {
 
         let button = WPStyleGuide.wpcomSignupButton()
         stackView.addArrangedSubview(button)
-        button.on(.touchUpInside) { (button) in
-            // pass
+        button.on(.touchUpInside) { [weak self] (button) in
+            self?.performSegue(withIdentifier: .showSignupEmail, sender: self)
         }
 
         stackView.addConstraints([
