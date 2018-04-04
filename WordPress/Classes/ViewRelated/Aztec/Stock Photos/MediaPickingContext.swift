4c52abff80938e673d8f43095fee8591d5e7f3ca
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-04-03 22:34:13 -0700

    Pass a UIBarButtonItem for kicks, giggles and popovers on iPad

--- WordPress/Classes/ViewRelated/Aztec/Stock Photos/MediaPickingContext.swift
+++ WordPress/Classes/ViewRelated/Aztec/Stock Photos/MediaPickingContext.swift
@@ -1,5 +1,13 @@
 struct MediaPickingContext {
     let origin: UIViewController & UIDocumentPickerDelegate
     let view: UIView
+    let barButtonItem: UIBarButtonItem?
     let blog: Blog
+
+    init(origin: UIViewController & UIDocumentPickerDelegate, view: UIView, barButtonItem: UIBarButtonItem? = nil, blog: Blog) {
+        self.origin = origin
+        self.view = view
+        self.barButtonItem = barButtonItem
+        self.blog = blog
+    }
 }
