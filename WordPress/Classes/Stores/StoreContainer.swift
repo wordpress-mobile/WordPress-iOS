a00fa8b872bf424a5b5abcfb1615048b13e09dfa
Jan Klausa <jan@klausa.pl>
2018-04-06 02:42:18 +0200

    StoreContainer: save plugins cache on app resigning `active` status.

--- WordPress/Classes/Stores/StoreContainer.swift
+++ WordPress/Classes/Stores/StoreContainer.swift
@@ -3,7 +3,17 @@ import Foundation
 class StoreContainer {
     static let shared = StoreContainer()
 
-    private init() {}
+    private init() {
+        NotificationCenter.default.addObserver(self, selector: #selector(applicationWillResignActive), name: .UIApplicationWillResignActive, object: nil)
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    @objc fileprivate func applicationWillResignActive() {
+        plugin.storeCachedState()
+    }
 
     let plugin = PluginStore()
     let notice = NoticeStore()
