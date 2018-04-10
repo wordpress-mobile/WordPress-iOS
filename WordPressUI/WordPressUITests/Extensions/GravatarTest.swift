5d76bae31073a48123826936b0d0213b531e16e4
Jorge Leandro Perez <jorge@lantean.co>
2018-04-10 07:47:58 -0300

    Moving GravatarTests from WordPressShared > WordPressUI

--- WordPressUI/WordPressUITests/Extensions/GravatarTest.swift
+++ WordPressUI/WordPressUITests/Extensions/GravatarTest.swift
@@ -1,5 +1,5 @@
 import XCTest
-@testable import WordPressShared
+@testable import WordPressUI
 
 class GravatarTest: XCTestCase {
     func testUnknownGravatarUrlMatchesURLWithSubdomainAndQueryParameters() {
