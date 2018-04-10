49856c59ccbebe2332f9d3ebf7452c19bffe1646
Jorge Leandro Perez <jorge@lantean.co>
2018-04-10 15:09:24 -0300

    Decoupling Authentication Flow: Mark XIX (#9074)
    * WordPressUI: NSString md5 Category
    
    * WordPressUI: UIImage+Assets
    
    * Moving Gravatar from WordPressShared > WordPressUI
    
    * WordPressUI: UIImageView+Gravatar
    
    * Updates Project(s)
    
    * UIImage+Assets: Renames property
    
    * WordPressUI: Updating Project
    
    * UIImage+Assets: Fixing bundle loading
    
    * Moving GravatarTests from WordPressShared > WordPressUI

--- WordPressShared/WordPressShared/Core/Utility/NSString+Helpers.m
+++ WordPressShared/WordPressShared/Core/Utility/NSString+Helpers.m
@@ -129,20 +129,6 @@ static NSString *const Ellipsis =  @"\u2026";
     return [self stringByAddingPercentEncodingWithAllowedCharacters:allowedCharacterSet];
 }
 
-- (NSString *)md5
-{
-    const char *cStr = [self UTF8String];
-    unsigned char result[CC_MD5_DIGEST_LENGTH];
-
-    CC_MD5(cStr, (CC_LONG)strlen(cStr), result);
-
-    return [NSString stringWithFormat:
-            @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
-            result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7],
-            result[8], result[9], result[10], result[11], result[12], result[13], result[14], result[15]
-            ];
-}
-
 - (NSMutableDictionary *)dictionaryFromQueryString
 {
     if (!self) {
