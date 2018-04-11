07e5c44a804be8a919416bb6ce38257e72c360eb
Jorge Leandro Perez <jorge@lantean.co>
2018-04-10 22:59:14 -0300

    WordPressAuthenticator+Events: New Internal API

--- WordPress/Classes/ViewRelated/NUX/WordPressAuthenticator+Events.swift
+++ WordPress/Classes/ViewRelated/NUX/WordPressAuthenticator+Events.swift
@@ -1,64 +1,36 @@
 import Foundation
-
+import WordPressShared
 
 // MARK: - Authentication Flow Event. Useful to relay internal Auth events over to activity trackers.
 //
-public extension WordPressAuthenticator {
-    public enum Event {
-        case createAccountInitiated
-        case createdAccount
-        case loginAutoFillCredentialsFilled
-        case loginAutoFillCredentialsUpdated
-        case loginEmailFormViewed
-        case loginEpilogueViewed
-        case loginFailed(error: Error)
-        case loginFailedToGuessXMLRPC(error: Error)
-        case loginForgotPasswordClicked
-        case loginMagicLinkFailed
-        case loginMagicLinkOpenEmailClientViewed
-        case loginMagicLinkRequested
-        case loginMagicLinkRequestFormViewed
-        case loginMagicLinkExited
-        case loginMagicLinkOpened
-        case loginMagicLinkSucceeded
-        case loginPasswordFormViewed
-        case loginProloguePaged
-        case loginPrologueViewed
-        case loginSocialAccountsNeedConnecting
-        case loginSocial2faNeeded
-        case loginSocialButtonClick
-        case loginSocialButtonFailure(error: Error?)
-        case loginSocialConnectSuccess
-        case loginSocialConnectFailure(error: Error)
-        case loginSocialErrorUnknownUser
-        case loginSocialSuccess
-        case loginTwoFactorFormViewed
-        case loginURLFormViewed
-        case loginURLHelpScreenViewed
-        case loginUsernamePasswordFormViewed
-        case onePasswordFailed
-        case onePasswordLogin
-        case onePasswordSignup
-        case openedLogin
-        case signupMagicLinkOpenEmailClientViewed
-        case signupMagicLinkOpened
-        case signupMagicLinkSucceeded
-        case signupMagicLinkFailed
-        case signupMagicLinkRequested
-        case signupSocialSuccess
-        case signedIn(properties: [String: String])
-        case twoFactorCodeRequested
-    }
-}
-
-
-// MARK: - Internal Helpers
-//
 extension WordPressAuthenticator {
 
-    /// Posts a `wordpressAuthenticationFlowEvent` notification, containing the specified Event.
+    /// Tracks the specified event.
     ///
-    static func post(event: Event) {
-        NotificationCenter.default.post(name: .wordpressAuthenticationFlowEvent, object: event)
+    @objc
+    static func track(_ event: WPAnalyticsStat) {
+        WordPressAuthenticator.shared.delegate?.track(event: event)
+    }
+
+    /// Tracks the specified event, with the specified properties.
+    ///
+    @objc
+    static func track(_ event: WPAnalyticsStat, properties: [AnyHashable: Any]) {
+        WordPressAuthenticator.shared.delegate?.track(event: event, properties: properties)
+    }
+
+    /// Tracks the specified event, with the associated Error.
+    ///
+    /// Note: Ideally speaking... `Error` is not optional. *However* this method is to be used in the ObjC realm, where not everything
+    /// has it's nullability specifier set. We're just covering unexpected scenarios.
+    ///
+    @objc
+    static func track(_ event: WPAnalyticsStat, error: Error?) {
+        guard let error = error else {
+            track(event)
+            return
+        }
+
+        WordPressAuthenticator.shared.delegate?.track(event: event, error: error)
     }
 }
