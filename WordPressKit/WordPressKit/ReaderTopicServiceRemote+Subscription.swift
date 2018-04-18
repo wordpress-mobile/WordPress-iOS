67a04d30b580654a0e2263900de087da863b7969
Daniele Bogo <me@bogodaniele.com>
2018-04-18 09:09:43 +0100

    #8950: ReaderService
    - Add a ServiceRequest
    - Add WordPressKit constants
    - Extend ReaderTopicServiceRemote implementing ReaderServiceSubscriptable protocols

---
+++ WordPressKit/WordPressKit/ReaderTopicServiceRemote+Subscription.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+
+extension ReaderTopicServiceRemote {
+    private func POST(with request: ReaderTopicServiceSubscriptionsRequest, parameters: [String: AnyObject]? = nil, success: @escaping SuccessBlock, failure: @escaping FailureBlock) {
+        let urlRequest = path(forEndpoint: request.path, withVersion: request.apiVersion)
+        
+        DDLogInfo("URL: \(urlRequest)")
+        
+        wordPressComRestApi.POST(urlRequest, parameters: parameters, success: { (_, response) in
+            DDLogInfo("Success \(response?.url?.absoluteString ?? "unknown response url")")
+            success()
+        }) { (error, _) in
+            DDLogError("Error: \(error.localizedDescription)")
+            failure(error)
+        }
+    }
+}
+
+
+extension ReaderTopicServiceRemote: SiteNotificationsSubscriptable {
+    @nonobjc public func subscribeSiteNotifications(with siteId: NSNumber, _ success: @escaping SuccessBlock, _ failure: @escaping FailureBlock) {
+        POST(with: .notifications(siteId: siteId, action: .subscribe), success: success, failure: failure)
+    }
+    
+    @nonobjc public func unsubscribeSiteNotifications(with siteId: NSNumber, _ success: @escaping SuccessBlock, _ failure: @escaping FailureBlock) {
+        POST(with: .notifications(siteId: siteId, action: .unsubscribe), success: success, failure: failure)
+    }
+}
+
+
+extension ReaderTopicServiceRemote: SiteCommentsSubscriptable {
+    @nonobjc public func subscribeSiteComments(with siteId: NSNumber, _ success: @escaping SuccessBlock, _ failure: @escaping FailureBlock) {
+        POST(with: .comments(siteId: siteId, action: .subscribe), success: success, failure: failure)
+    }
+    
+    @nonobjc public func unsubscribeSiteComments(with siteId: NSNumber, _ success: @escaping SuccessBlock, _ failure: @escaping FailureBlock) {
+        POST(with: .comments(siteId: siteId, action: .unsubscribe), success: success, failure: failure)
+    }
+}
+
+
+extension ReaderTopicServiceRemote: SitePostsSubscriptable {
+    @nonobjc public func subscribePostsEmail(with siteId: NSNumber, _ success: @escaping SuccessBlock, _ failure: @escaping FailureBlock) {
+        POST(with: .postsEmail(siteId: siteId, action: .subscribe), success: success, failure: failure)
+    }
+    
+    @nonobjc public func unsubscribePostsEmail(with siteId: NSNumber, _ success: @escaping SuccessBlock, _ failure: @escaping FailureBlock) {
+        POST(with: .postsEmail(siteId: siteId, action: .unsubscribe), success: success, failure: failure)
+    }
+    
+    @nonobjc public func updateFrequencyPostsEmail(with siteId: NSNumber, frequency: ReaderServiceDeliveryFrequency, _ success: @escaping SuccessBlock, _ failure: @escaping FailureBlock) {
+        let parameters = [WordPressKitConstants.SiteSubscription.Delivery.frequency: NSString(string: frequency.rawValue)]
+        POST(with: .postsEmail(siteId: siteId, action: .update), parameters: parameters, success: success, failure: failure)
+    }
+}
