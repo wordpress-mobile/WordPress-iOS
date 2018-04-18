8c940a20b037d7324fdee37e971c22cbb2376379
Daniele Bogo <me@bogodaniele.com>
2018-04-18 09:09:43 +0100

    #8950: ReaderService
    - Add a ServiceRequest
    - Add WordPressKit constants
    - Extend ReaderTopicServiceRemote implementing ReaderServiceSubscriptable protocols

---
+++ WordPressKit/WordPressKit/WordPressKitConstants.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+
+/// Constants definition for WordPressKit
+struct WordPressKitConstants {
+    struct SiteSubscription {
+        struct Delivery {
+            static let frequency = "delivery_frequency"
+        }
+    }
+}
