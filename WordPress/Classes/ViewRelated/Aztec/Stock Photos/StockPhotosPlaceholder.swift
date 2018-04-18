67b0d028cb54923641525ba9ef4228f3245b19ec
etoledom <etoledom@icloud.com>
2018-04-18 17:01:20 -0300

    Adding support for attributed text in NoResultsView

--- WordPress/Classes/ViewRelated/Aztec/Stock Photos/StockPhotosPlaceholder.swift
+++ WordPress/Classes/ViewRelated/Aztec/Stock Photos/StockPhotosPlaceholder.swift
@@ -1,5 +1,8 @@
 // Empty state for Stock Photos
 final class StockPhotosPlaceholder: WPNoResultsView {
+
+    private let pexelsUrl = "https://www.pexels.com"
+
     init() {
         super.init(frame: .zero)
         configure()
@@ -24,6 +27,32 @@ final class StockPhotosPlaceholder: WPNoResultsView {
     }
 
     private func configureSubtitle() {
-        messageText = .freePhotosPlaceholderSubtitle
+        do {
+            attributedMessageText = try createStringWIthLinkAttributes(in: .freePhotosPlaceholderSubtitle)
+        } catch {
+            // A translation error could make the link attributes to fail. (i.e. removing the '{')
+            // This will make sure that the message is still present, without any {}, but without the link.
+            messageText = removeCurlybraces(from: .freePhotosPlaceholderSubtitle)
+        }
+    }
+
+    private func createStringWIthLinkAttributes(in subtitle: String) throws -> NSAttributedString {
+        let htmlTaggedString = subtitle.replacingOccurrences(of: "{", with: "<a href=\"\(pexelsUrl)\">")
+            .replacingOccurrences(of: "}", with: "</a>")
+
+        guard let htmlTaggedData = htmlTaggedString.data(using: .utf8) else {
+            throw NSError()
+        }
+
+        let attributedString = try NSMutableAttributedString(
+            data: htmlTaggedData,
+            options:[.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue],
+            documentAttributes: nil)
+
+        return attributedString
+    }
+
+    private func removeCurlybraces(from string: String) -> String {
+        return string.replacingOccurrences(of: "{", with: "").replacingOccurrences(of: "}", with: "")
     }
 }
