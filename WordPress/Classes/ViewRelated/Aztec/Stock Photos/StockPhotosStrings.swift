9445feea4ad2e867473fab11467fdee59cd25658
etoledom <etoledom@icloud.com>
2018-04-19 19:50:15 -0300

    Stock Photos: Added no result message when search returns empty

--- WordPress/Classes/ViewRelated/Aztec/Stock Photos/StockPhotosStrings.swift
+++ WordPress/Classes/ViewRelated/Aztec/Stock Photos/StockPhotosStrings.swift
@@ -21,4 +21,8 @@ extension String {
     static var freePhotosPlaceholderSubtitle: String {
         return NSLocalizedString("Photos provided by Pexels", comment: "Subtitle for placeholder in Free Photos. The company name 'Pexels' should always be written as it is.")
     }
+
+    static var freePhotosSearchNoResult: String {
+        return NSLocalizedString("No media files match your search for", comment: "Phrase to show when the user search for images but there are no result to show. This will be followed by the phrase the user used to search. (i.e. No media files match your search for Ugly kitten). This search phrase will be always appended at the end.")
+    }
 }
