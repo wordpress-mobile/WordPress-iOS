19c2d9bcdc2205ccbc0b5ab3cf807fc0c7e28c84
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-04-18 17:28:06 -0700

    Downcast meta to [String: Int]. Otherwise, parsing via Codable will fail on the last pageable

--- WordPress/Classes/ViewRelated/Aztec/Stock Photos/StockPhotosService.swift
+++ WordPress/Classes/ViewRelated/Aztec/Stock Photos/StockPhotosService.swift
@@ -37,7 +37,7 @@ final class DefaultStockPhotosService: StockPhotosService {
 
     func search(params: StockPhotosSearchParams, completion: @escaping (StockPhotosResultsPage) -> Void) {
         api.GET(endPoint, parameters: parameters(params: params), success: { results, response in
-            if let media = results[ParsingKeys.media], let meta = results[ParsingKeys.meta] {
+            if let media = results[ParsingKeys.media], let meta = results[ParsingKeys.meta] as? [String: Int] {
                 do {
                     let mediaJSON = try JSONSerialization.data(withJSONObject: media as Any)
                     let parsedResponse = try JSONDecoder().decode([StockPhotosMedia].self, from: mediaJSON)
