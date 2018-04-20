0d38b8ee7aa114c1b62bffff0a921a7198012b20
Jorge Leandro Perez <jorge@lantean.co>
2018-04-20 13:51:54 -0300

    Relocating Model

--- WordPressAuthenticator/WordPressAuthenticator/Model/LoginFields.swift
+++ WordPressAuthenticator/WordPressAuthenticator/Model/LoginFields.swift
@@ -1,39 +1,41 @@
 import Foundation
 import GoogleSignIn
+import WordPressKit
+
 
 /// LoginFields is a state container for user textfield input on the login screens
 /// as well as other meta data regarding the nature of a login attempt.
 ///
 @objc
-class LoginFields: NSObject {
+public class LoginFields: NSObject {
     // These fields store user input from text fields.
 
     /// Stores the user's account identifier (either email address or username) that is
     /// entered in the login flow. By convention, even if the user is logging in
     /// via an email address this field should store that value.
-    @objc var username = ""
+    @objc public var username = ""
 
     /// The user's password.
-    @objc var password = ""
+    @objc public var password = ""
 
     /// The site address if logging in via the self-hosted flow.
-    @objc var siteAddress = ""
+    @objc public var siteAddress = ""
 
     /// The two factor code entered by a user.
-    @objc var multifactorCode = "" // 2fa code
+    @objc public var multifactorCode = "" // 2fa code
 
     /// Nonce info in the event of a social login with 2fa
-    @objc var nonceInfo: SocialLogin2FANonceInfo?
+    @objc public var nonceInfo: SocialLogin2FANonceInfo?
 
     /// User ID for use with the nonce for social login
-    @objc var nonceUserID: Int = 0
+    @objc public var nonceUserID: Int = 0
 
     /// Used by the SignupViewController. Signup currently asks for both a
     /// username and an email address.  This can be factored away when we revamp
     /// the signup flow.
-    @objc var emailAddress = ""
+    @objc public var emailAddress = ""
 
-    @objc var meta = LoginFieldsMeta()
+    @objc public var meta = LoginFieldsMeta()
     var storedCredentials: SafariStoredCredentials?
 
 
@@ -72,7 +74,7 @@ enum EmailMagicLinkSource: Int {
 }
 
 @objc
-class LoginFieldsMeta: NSObject {
+public class LoginFieldsMeta: NSObject {
 
     /// Indicates where the Magic Link Email was sent from.
     ///
@@ -80,47 +82,47 @@ class LoginFieldsMeta: NSObject {
 
     /// Indicates whether a self-hosted user is attempting to log in to Jetpack
     ///
-    @objc var jetpackLogin: Bool {
+    @objc public var jetpackLogin: Bool {
         return jetpackBlogXMLRPC != nil && jetpackBlogUsername != nil
     }
 
     /// Username of the selfhosted site for which we're attempting to perform a Jetpack Connection.
     ///
-    @objc var jetpackBlogUsername: String?
+    @objc public var jetpackBlogUsername: String?
 
     /// XMLRPC of the selfhosted site for which we're attempting to perform a Jetpack Connection.
     ///
-    @objc var jetpackBlogXMLRPC: String?
+    @objc public var jetpackBlogXMLRPC: String?
 
     /// Indicates whether a user is logging in via the wpcom flow or a self-hosted flow.  Used by the
     /// the LoginFacade in its branching logic.
     /// This is a good candidate to refactor out and call the proper login method directly.
     ///
-    @objc var userIsDotCom = true
+    @objc public var userIsDotCom = true
 
     /// Indicates a wpcom account created via social sign up that requires either a magic link, or a social log in option.
     /// If a user signed up via social sign up and subsequently reset their password this field will be false.
     ///
-    @objc var passwordless = false
+    @objc public var passwordless = false
 
     /// Should point to the site's xmlrpc.php for a self-hosted log in.  Should be the value returned via XML-RPC discovery.
     ///
-    @objc var xmlrpcURL: NSURL?
+    @objc public var xmlrpcURL: NSURL?
 
     /// Meta data about a site. This information is fetched and then displayed on the login epilogue.
     ///
-    var siteInfo: WordPressComSiteInfo?
+    @objc public var siteInfo: var siteInfo: WordPressComSiteInfo?
 
     /// Flags whether a 2fa challenge had to be satisfied before a log in could be complete.
     /// Included in analytics after a successful login.
     ///
-    @objc var requiredMultifactor = false // A 2fa prompt was needed.
+    @objc public var requiredMultifactor = false // A 2fa prompt was needed.
 
     /// Identifies a social login and the service used.
     ///
     var socialService: SocialServiceName?
 
-    @objc var socialServiceIDToken: String?
+    @objc public var socialServiceIDToken: String?
 
     var googleUser: GIDGoogleUser?
 }
