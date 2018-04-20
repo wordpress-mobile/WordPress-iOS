f2e481cc5a25bf1ca60ca73ee1035e85d8b9d29b
Jorge Leandro Perez <jorge@lantean.co>
2018-04-20 14:19:36 -0300

    Updates Bridging Header

--- WordPressAuthenticator/WordPressAuthenticator/WordPressAuthenticator.h
+++ WordPressAuthenticator/WordPressAuthenticator/WordPressAuthenticator.h
@@ -9,6 +9,10 @@ FOUNDATION_EXPORT const unsigned char WordPressAuthenticatorVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <WordPressAuthenticator/PublicHeader.h>
 
 
+#import <WordPressAuthenticator/LoginFacade.h>
+#import <WordPressAuthenticator/WordPressComOAuthClientFacade.h>
+#import <WordPressAuthenticator/WordPressXMLRPCAPIFacade.h>
+
 #import <WordPressAuthenticator/WPAuthenticatorLogging.h>
 #import <WordPressAuthenticator/WPNUXMainButton.h>
 #import <WordPressAuthenticator/WPNUXPrimaryButton.h>
