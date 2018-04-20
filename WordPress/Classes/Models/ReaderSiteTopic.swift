47d5cef03d13f18c98b43fb8d5c458ad54391ec7
Daniele Bogo <me@bogodaniele.com>
2018-04-20 15:26:51 +0100

    #8950: Notifications Reader
    - Modify coredata layer and data models

--- WordPress/Classes/Models/ReaderSiteTopic.swift
+++ WordPress/Classes/Models/ReaderSiteTopic.swift
@@ -1,6 +1,11 @@
 import Foundation
 
 @objc open class ReaderSiteTopic: ReaderAbstractTopic {
+    // Relations
+    @NSManaged open var postSubscription: ReaderSiteInfoSubscriptionPost?
+    @NSManaged open var emailSubscription: ReaderSiteInfoSubscriptionEmail?
+    
+    // Properties
     @NSManaged open var feedID: NSNumber
     @NSManaged open var feedURL: String
     @NSManaged open var isJetpack: Bool
@@ -21,5 +26,16 @@ import Foundation
         get {
             return siteID.intValue == 0
         }
+    }
+    
+    
+    //MARK: - Public methods
+    
+    @objc public func blogNameToDisplay() -> String {
+        return posts.first?.blogNameForDisplay() ?? title
+    }
+    
+    @objc public func isSubscribedForPostNotifications() -> Bool {
+        return postSubscription?.sendPosts ?? false
     }
 }
