9cf2a6b5798ef0d1f0bcd29b8d15ecb4545d78ca
Daniele Bogo <bogodaniele83@gmail.com>
2018-04-23 13:40:46 +0100

    Update WordPressKit for Reader notifications (#9126)
    * #8950: NSObject+ObjectValidation
    
    Add NSObject category to check if an object is valid and not nil
    
    * #8950: RemoteReaderSiteInfo
    
    - Add RemoteReaderSiteInfoSubscriptionPost and RemoteReaderSiteInfoSubscriptionEmail models
    - Add relationship into RemoteReaderSiteInfo
    
    * #8950: ReaderTopicServiceRemote
    
    - Update API version to 1.2
    - Map new response data into post and email subscriptions models
    
    * #8950: ReaderService
    
    - Create a delivery frequency enumeration
    - Create a Subscriptable protocol to manage site site notifications subscriptions
    
    * #8950: ReaderService
    
    - Add a ServiceRequest
    - Add WordPressKit constants
    - Extend ReaderTopicServiceRemote implementing ReaderServiceSubscriptable protocols
    
    * #8950: Test
    
    - Add RemoteReaderSiteInfoSubscriptionTests
    
    * #8950: Test
    
    - Create Mock Service Request
    - Test Mock Service Request and Reader Topic Service Subscription Request
    
    * #8950: Test
    
    - Test ReaderTopicServiceRemote extension
    
    * #8950: Fix Test
    
    - Fix test
    
    * #8950: Fix after review
    
    - Remove self
    
    * #8950: Fix after review
    
    - Remove ReaderServiceSubscriptable protocol and type alias
    
    * #8950: Fix after review
    
    - Remove header
    
    * #8950: Fix after review
    
    - Validate dictionary using SafeExpectations category
    
    * #8950: Fix after review
    
    - Remove unnecessary Struct
    
    * #8950: Fix after review
    
    - Lint fix
    
    * #8950: Fix after review
    
    - Remove NSObject+ObjectValidation category
    - Create ObjectValidation.swift
    
    * #8950: Fix after review
    
    - Remove NSObject+ObjectValidation files
    
    * #8950: Fix after review
    
    - Remove unused NSObject+ObjectValidation category
    
    * #8950: Fix after review
    
    - Remove protocol
    - Remove contants

---
+++ WordPressKit/WordPressKitTests/ReaderTopicServiceRemoteTest+Subscriptions.swift
@@ -0,0 +1,111 @@
+import XCTest
+@testable import WordPressKit
+
+
+class ReaderTopicServiceRemoteTestSubscriptions: XCTestCase {
+    let mockRemoteApi = MockWordPressComRestApi()
+    let response = [String: AnyObject]()
+    let siteId = NSNumber(value: 0)
+    var failure: ReaderServiceSubscriptable.FailureBlock!
+    var readerTopicServiceRemote: ReaderTopicServiceRemote!
+    
+    override func setUp() {
+        super.setUp()
+        readerTopicServiceRemote = ReaderTopicServiceRemote(wordPressComRestApi: mockRemoteApi)
+        failure = { _ in
+            fatalError()
+        }
+    }
+    
+    func testSubscribeNotification() {
+        var success = false
+        let expectedPath = "wpcom/v2/read/sites/0/notification-subscriptions/new/"
+        readerTopicServiceRemote.subscribeSiteNotifications(with: siteId, {
+            success = true
+        }, failure)
+        XCTAssertTrue(mockRemoteApi.postMethodCalled, "Wrong method")
+        XCTAssertEqual(mockRemoteApi.URLStringPassedIn, expectedPath, "Wrong path")
+        mockRemoteApi.successBlockPassedIn?(response as AnyObject, HTTPURLResponse())
+        XCTAssertTrue(success)
+    }
+    
+    func testUnsubscribeNotification() {
+        var success = false
+        let expectedPath = "wpcom/v2/read/sites/0/notification-subscriptions/delete/"
+        readerTopicServiceRemote.unsubscribeSiteNotifications(with: siteId, {
+            success = true
+        }, failure)
+        XCTAssertTrue(mockRemoteApi.postMethodCalled, "Wrong method")
+        XCTAssertEqual(mockRemoteApi.URLStringPassedIn, expectedPath, "Wrong path")
+        mockRemoteApi.successBlockPassedIn?(response as AnyObject, HTTPURLResponse())
+        XCTAssertTrue(success)
+    }
+    
+    func testSubscribeComments() {
+        var success = false
+        let expectedPath = "rest/v1.2/read/sites/0/comment_email_subscriptions/new/"
+        readerTopicServiceRemote.subscribeSiteComments(with: siteId, {
+            success = true
+        }, failure)
+        XCTAssertTrue(mockRemoteApi.postMethodCalled, "Wrong method")
+        XCTAssertEqual(mockRemoteApi.URLStringPassedIn, expectedPath, "Wrong path")
+        mockRemoteApi.successBlockPassedIn?(response as AnyObject, HTTPURLResponse())
+        XCTAssertTrue(success)
+    }
+    
+    func testUnsubscribeComments() {
+        var success = false
+        let expectedPath = "rest/v1.2/read/sites/0/comment_email_subscriptions/delete/"
+        readerTopicServiceRemote.unsubscribeSiteComments(with: siteId, {
+            success = true
+        }, failure)
+        XCTAssertTrue(mockRemoteApi.postMethodCalled, "Wrong method")
+        XCTAssertEqual(mockRemoteApi.URLStringPassedIn, expectedPath, "Wrong path")
+        mockRemoteApi.successBlockPassedIn?(response as AnyObject, HTTPURLResponse())
+        XCTAssertTrue(success)
+    }
+    
+    func testSubscribePostsEmail() {
+        var success = false
+        let expectedPath = "rest/v1.2/read/sites/0/post_email_subscriptions/new/"
+        readerTopicServiceRemote.subscribePostsEmail(with: siteId, {
+            success = true
+        }, failure)
+        XCTAssertTrue(mockRemoteApi.postMethodCalled, "Wrong method")
+        XCTAssertEqual(mockRemoteApi.URLStringPassedIn, expectedPath, "Wrong path")
+        mockRemoteApi.successBlockPassedIn?(response as AnyObject, HTTPURLResponse())
+        XCTAssertTrue(success)
+    }
+    
+    func testUnsubscribePostsEmail() {
+        var success = false
+        let expectedPath = "rest/v1.2/read/sites/0/post_email_subscriptions/delete/"
+        readerTopicServiceRemote.unsubscribePostsEmail(with: siteId, {
+            success = true
+        }, failure)
+        XCTAssertTrue(mockRemoteApi.postMethodCalled, "Wrong method")
+        XCTAssertEqual(mockRemoteApi.URLStringPassedIn, expectedPath, "Wrong path")
+        mockRemoteApi.successBlockPassedIn?(response as AnyObject, HTTPURLResponse())
+        XCTAssertTrue(success)
+    }
+    
+    func testUdatePostsEmail() {
+        var success = false
+        let expectedPath = "rest/v1.2/read/sites/0/post_email_subscriptions/update/"
+        
+        readerTopicServiceRemote.updateFrequencyPostsEmail(with: siteId, frequency: .weekly, {
+            success = true
+        }, failure)
+        XCTAssertTrue(mockRemoteApi.postMethodCalled, "Wrong method")
+        XCTAssertEqual(mockRemoteApi.URLStringPassedIn, expectedPath, "Wrong path")
+        mockRemoteApi.successBlockPassedIn?(response as AnyObject, HTTPURLResponse())
+        XCTAssertTrue(success)
+        
+        guard let parameters = mockRemoteApi.parametersPassedIn as? [String: AnyObject],
+            let frequency = parameters[WordPressKitConstants.SiteSubscription.Delivery.frequency] as? String else {
+            fatalError()
+        }
+        
+        XCTAssertEqual(frequency, ReaderServiceDeliveryFrequency.weekly.rawValue)
+    }
+}
