5d6b532b56b1246d4e2e78ac4d20277777aa4168
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-04-23 14:53:38 +0800

    Test results page

--- WordPress/WordPressTest/StockPhotosResultsPageTests.swift
+++ WordPress/WordPressTest/StockPhotosResultsPageTests.swift
@@ -2,14 +2,27 @@ import XCTest
 @testable import WordPress
 
 final class StockPhotosResultsPageTests: XCTestCase {
+    private struct Constants {
+        static let mockData = [mock(), mock()]
+    }
+
+    private static func mock() -> StockPhotosMedia {
+        let json = Bundle(for: StockPhotosResultsPageTests.self).url(forResource: "stock-photos-media", withExtension: "json")!
+        let data = try! Data(contentsOf: json)
+        let jsonDecoder = JSONDecoder()
+
+        return try! jsonDecoder.decode(StockPhotosMedia.self, from: data)
+    }
+
+    private var subject: StockPhotosResultsPage?
 
     override func setUp() {
         super.setUp()
-
+        subject = StockPhotosResultsPage(results: Constants.mockData, pageable: StockPhotosPageable.first())
     }
 
     override func tearDown() {
-
+        subject = nil
         super.tearDown()
     }
 
@@ -25,5 +38,11 @@ final class StockPhotosResultsPageTests: XCTestCase {
         let emptyResults = StockPhotosResultsPage.empty()
 
         XCTAssertNil(emptyResults.nextPageable())
+    }
+
+    func testPageContainsDataProvidedInInitializer() {
+        let data = subject?.content()
+
+        XCTAssertEqual(data, Constants.mockData)
     }
 }
