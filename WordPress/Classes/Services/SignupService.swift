fd68e8dba3cb668cc403400a84cca1ba22e76773
Jorge Leandro Perez <jorge@lantean.co>
2018-04-24 14:54:50 -0300

    Decoupling Authentication Flow: Mark XXV (#9110)
    * LoginPrologueSignupMethodViewController: Wiring SFSafariViewController
    
    * WordPressComOAuthClientFacade: Client + Secret are now parameters
    
    * WordPressXMLRPCAPIFacade: UserAgent is now a parameter
    
    * LoginFacade: New Initializer
    
    * WordPressAuthenticator: New Configuration API
    
    * WordPressAuthenticationManager: Wiring Authenticator Initialization
    
    * Wiring WordPressAuthenticatorConfiguration
    
    * LoginFacade: Updates documentation
    
    * LoginFacade: New constant
    
    * Removing WordPressAppError from Login-Y
    
    * WPError: Cleanup
    
    * LoginFacade: Fixing Unit Tests
    
    * LoginFacadeTests: Mocking tracking calls
    
    * LoginFacade: Updates documentation
    
    * Implements WordPressComBlogService
    
    * ServiceRemoteWordPressComREST: Adding nullability specifiers
    
    * Nuking quite a few Force Unwraps
    
    * Implements WordPressComAccountService
    
    * LoginViewController: Fixing Hound Error
    
    * Fixing Unit Tests
    
    * WordPressComOAuthClientFacade: Explicitly synthesizing property
    
    * Nukes socialSignup FeatureFlag
    
    * Nukes SignupViewController
    
    * SignupService: Nukes deprecated methods
    
    * Nukes NUXAbstractViewController
    
    * Nukes SigninKeyboardResponder
    
    * SignupService: Decoupling from main app. Part I
    
    * Removing FeatureFlag.supportsSocialSignup
    
    * SignupGoogleViewController: Splitting few methods
    
    * SignupService: Cleanup
    
    * WordPressAuthenticationManager: Fixing inverted mapping
    
    * Fixing Hound warning
    
    * Nuking dead service calls
    
    * SignupGoogleViewController: Renames Method
    
    * SignupGoogleViewController: Cleanup
    
    * SignupGoogleViewController: Adds missing dismiss
    
    * Addressing Review Feedback. Part I
    
    * Addressing Review Feedback. Part II
    
    * SignupService: Restoring documentation
    
    * SignupGoogleViewController: Preserving original behavior

--- WordPress/Classes/Services/SignupService.swift
+++ WordPress/Classes/Services/SignupService.swift
@@ -1,84 +1,71 @@
 import Foundation
 import CocoaLumberjack
 import WordPressShared
+import WordPressKit
 
 
 
-typealias SignupSocialSuccessBlock = (_ newAccount: Bool) -> Void
-typealias SignupFailureBlock = (_ error: Error?) -> Void
-
-
-/// SignupService is responsible for creating a new WPCom user and blog.
+/// SignupService: Responsible for creating a new WPCom user and blog.
 ///
-open class SignupService: LocalCoreDataService {
+class SignupService {
 
     /// Create a new WPcom account using Google signin token
     ///
     /// - Parameters:
-    ///   - token: the token from a successful Google login
+    ///   - googleToken: the token from a successful Google login
     ///   - success: block called when account is created successfully
     ///   - failure: block called when account creation fails
+    ///
     func createWPComUserWithGoogle(token: String,
-                                   success: @escaping SignupSocialSuccessBlock,
-                                   failure: @escaping SignupFailureBlock) {
+                                   success: @escaping (_ newAccount: Bool, _ username: String, _ wpcomToken: String) -> Void,
+                                   failure: @escaping (_ error: Error) -> Void) {
 
-        let remote = WordPressComServiceRemote(wordPressComRestApi: self.anonymousApi())
+        let remote = WordPressComServiceRemote(wordPressComRestApi: anonymousAPI)
         let locale = WordPressComLanguageDatabase().deviceLanguage.slug
 
         remote.createWPComAccount(withGoogle: token,
-                                   andLocale: locale,
-                                   andClientID: ApiCredentials.client(),
-                                   andClientSecret: ApiCredentials.secret(),
-                                   success: { (responseDictionary) in
-                                        guard let username = responseDictionary?[ResponseKeys.username] as? String,
-                                            let bearer_token = responseDictionary?[ResponseKeys.bearerToken] as? String else {
-                                                // without these we can't proceed.
-                                                failure(nil)
-                                                return
-                                        }
+                                  andLocale: locale,
+                                  andClientID: configuration.wpcomClientId,
+                                  andClientSecret: configuration.wpcomSecret,
+                                  success: { response in
 
-                                        // create the local account
-                                        let service = AccountService(managedObjectContext: self.managedObjectContext)
-                                        let account = service.createOrUpdateAccount(withUsername: username, authToken: bearer_token)
-                                        if service.defaultWordPressComAccount() == nil {
-                                            service.setDefaultWordPressComAccount(account)
-                                        }
+            guard let username = response?[ResponseKeys.username] as? String,
+                let bearer_token = response?[ResponseKeys.bearerToken] as? String else {
+                    failure(SignupError.unknown)
+                    return
+            }
 
-                                        let createdAccount = (responseDictionary?[ResponseKeys.createdAccount] as? Int ?? 0) == 1
-                                        if createdAccount {
-                                            defer {
-                                                WPAppAnalytics.track(.createdAccount, withProperties: ["source": "google"])
-                                            }
-                                            success(createdAccount)
-                                        } else {
-                                            defer {
-                                                WPAppAnalytics.track(.signupSocialToLogin)
-                                            }
-                                            // we need to sync the blogs for existing accounts to be able to display the Login Epilogue
-                                            BlogSyncFacade().syncBlogs(for: account, success: {
-                                                success(createdAccount)
-                                            }, failure: { (_) in
-                                                // the blog sync failed but the user is already logged in
-                                                success(createdAccount)
-                                            })
-                                        }
-                                    },
-                                    failure: failure)
+            let createdAccount = (response?[ResponseKeys.createdAccount] as? Int ?? 0) == 1
+            success(createdAccount, username, bearer_token)
+        }, failure: { error in
+            failure(error ?? SignupError.unknown)
+        })
+    }
+}
+
+
+// MARK: - Private
+//
+private extension SignupService {
+
+    var anonymousAPI: WordPressComRestApi {
+        return WordPressComRestApi(oAuthToken: nil, userAgent: configuration.userAgent)
     }
 
-
-
-    // MARK: Private Instance Methods
-
-    @objc func anonymousApi() -> WordPressComRestApi {
-        return WordPressComRestApi(userAgent: WPUserAgent.wordPress())
+    var configuration: WordPressAuthenticatorConfiguration {
+        return WordPressAuthenticator.shared.configuration
     }
 
-
-    /// A convenience struct for response keys
-    private struct ResponseKeys {
+    struct ResponseKeys {
         static let bearerToken = "bearer_token"
         static let username = "username"
         static let createdAccount = "created_account"
     }
+}
+
+
+// MARK: - Errors
+//
+enum SignupError: Error {
+    case unknown
 }
