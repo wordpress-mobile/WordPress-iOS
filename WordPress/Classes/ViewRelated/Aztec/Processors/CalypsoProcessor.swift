4f06abf4d13768c599ccc5fe1da7810fc8cbaeb6
Diego Rey Mendez <diegoreymendez@gmail.com>
2018-04-26 17:17:11 -0300

    rake lint:autocorrect

--- WordPress/Classes/ViewRelated/Aztec/Processors/CalypsoProcessor.swift
+++ WordPress/Classes/ViewRelated/Aztec/Processors/CalypsoProcessor.swift
@@ -5,7 +5,7 @@ protocol CalypsoProcessor: Processor {
 }
 
 extension CalypsoProcessor {
-    
+
     /// HACK: not a very good approach, but our APIs don't offer proper versioning info on `post_content`.
     /// Directly copied from here: https://github.com/WordPress/gutenberg/blob/5a6693589285363341bebad15bd56d9371cf8ecc/lib/register.php#L343
     ///
