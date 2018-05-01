7b3db113c6612ee3ddd0031df70ddeacfa4cbf1c
etoledom <etoledom@icloud.com>
2018-05-01 14:53:41 -0300

    Gif: Removed `FLAnimatedImage` dependency from podfile

--- WordPress/Classes/Extensions/FLAnimatedImageView+Cache.swift
+++ WordPress/Classes/Extensions/FLAnimatedImageView+Cache.swift
@@ -13,25 +13,28 @@ public class CachedAnimatedImageView: UIImageView, GIFAnimatable {
 
     @objc func setAnimatedImage(_ urlRequest: URLRequest,
                        placeholderImage: UIImage?,
-                       success: ((FLAnimatedImage) -> ())? ,
-                       failure: ((NSError?) -> ())? ) {
+                       success: (() -> Void)? ,
+                       failure: ((NSError?) -> Void)? ) {
 
         if let ongoingTask = currentTask {
             ongoingTask.cancel()
         }
 
-        currentTask = AnimatedImageCache.shared.animatedImage(
-            urlRequest,
-            placeholderImage: placeholderImage,
-            success: { [weak self] (animatedImageData) in
-                guard let strongSelf = self else {
-                    return
-                }
-                DispatchQueue.main.async(execute: {
-                    strongSelf.animate(withGIFData: animatedImageData)
+        let successBlock: (Data) -> Void = { [weak self] animatedImageData in
+            guard let strongSelf = self else {
+                return
+            }
+            DispatchQueue.main.async(execute: {
+                strongSelf.animate(withGIFData: animatedImageData, loopCount: 0, completionHandler: {
+                    success?()
                 })
-            },
-            failure: failure)
+            })
+        }
+
+        currentTask = AnimatedImageCache.shared.animatedImage(urlRequest,
+                                                              placeholderImage: placeholderImage,
+                                                              success: successBlock,
+                                                              failure: failure)
     }
 
 
@@ -48,19 +51,22 @@ class AnimatedImageCache {
         return session
     }()
 
-    fileprivate lazy var cache: NSCache = {
-        return NSCache<AnyObject, AnyObject>()
+    fileprivate lazy var cache: NSCache<NSURL, NSData> = {
+        return NSCache<NSURL, NSData>()
     }()
 
     func animatedImage(_ urlRequest: URLRequest,
                        placeholderImage: UIImage?,
-                       success: ((Data) -> ())? ,
-                       failure: ((NSError?) -> ())? ) -> URLSessionTask? {
+                       success: ((Data) -> Void)? ,
+                       failure: ((NSError?) -> Void)? ) -> URLSessionTask? {
+
         if  let key = urlRequest.url,
-            let animatedImage = cache.object(forKey: key as AnyObject) as? Data {
-            success?(animatedImage)
+            let animatedImageData = cache.object(forKey: key as NSURL) {
+
+            success?(animatedImageData as Data)
             return nil
         }
+
         let task = session.dataTask(with: urlRequest, completionHandler: { [weak self](data, response, error) in
             //check if view is still here
             guard let strongSelf = self else {
@@ -68,24 +74,22 @@ class AnimatedImageCache {
             }
             // check if there is an error
             if let error = error {
-                failure?(error as NSError?)
+                failure?(error as NSError)
                 return
             }
             // check if data is here and is animated gif
             guard let data = data else {
-                    failure?(nil)
-                    return
+                failure?(nil)
+                return
             }
 
             if  let key = urlRequest.url {
-                strongSelf.cache.setObject(data as NSData, forKey: key as AnyObject)
+                strongSelf.cache.setObject(data as NSData, forKey: key as NSURL)
             }
             success?(data)
+        })
 
-            })
         task.resume()
         return task
     }
-
-
 }
