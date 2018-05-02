d4e311fc70c074c59d3036fea354798dc6d3bf39
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-05-02 14:28:09 +0800

    Update the mock topic to be created in an in-memory context, so that the app does not crash for now

--- WordPress/Classes/Models/ReaderSaveForLaterTopic.swift
+++ WordPress/Classes/Models/ReaderSaveForLaterTopic.swift
@@ -1,12 +1,32 @@
-/// This is basically a mock at the moment. It models a mock topic, so that I can test that the topic gets renderd in the UI
+/// Plese do not review this class. This is basically a mock at the moment. It models a mock topic, so that I can test that the topic gets rendered in the UI
 final class ReaderSaveForLaterTopic: ReaderAbstractTopic {
     init(title: String) {
-        super.init(entity: NSEntityDescription(), insertInto: nil)
+        let managedObjectContext = ReaderSaveForLaterTopic.setUpInMemoryManagedObjectContext()
+        let entity = NSEntityDescription.entity(forEntityName: "ReaderListTopic", in: managedObjectContext)
+        super.init(entity: entity!, insertInto: managedObjectContext)
         self.title = title
         self.path = "/read/saved"
     }
 
     override open class var TopicType: String {
         return "saveForLater"
+    }
+
+    /// TODO. This function will have to go away
+    static func setUpInMemoryManagedObjectContext() -> NSManagedObjectContext {
+        let managedObjectModel = NSManagedObjectModel.mergedModel(from: [Bundle.main])!
+
+        let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+
+        do {
+            try persistentStoreCoordinator.addPersistentStore(ofType: NSInMemoryStoreType, configurationName: nil, at: nil, options: nil)
+        } catch {
+            print("Adding in-memory persistent store failed")
+        }
+
+        let managedObjectContext = NSManagedObjectContext()
+        managedObjectContext.persistentStoreCoordinator = persistentStoreCoordinator
+
+        return managedObjectContext
     }
 }
