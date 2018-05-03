d3fc1541156bfd95a61ab61cf2731bc5503d4f8e
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-05-03 11:39:26 +0800

    Extract title for SavedPosts section to an extension of String

---
+++ WordPress/Classes/ViewRelated/Reader/ReaderStrings.swift
@@ -0,0 +1,6 @@
+/// Encapsulates user facing strings in Reader
+extension String {
+    static var savedForLaterMenuTitle: String {
+        return NSLocalizedString("Saved Posts", comment: "Section title for Saved Posts in Reader")
+    }
+}
