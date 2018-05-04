50376e446e79820a461c6d1beb534c7353f58a6c
Daniele Bogo <me@bogodaniele.com>
2018-05-04 16:19:26 +0100

    #8950: Settings - Woking on site notifications layout
    - Create new view controller
    - Implement models to map and UI elements

---
+++ WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationSiteSubscriptionViewController.swift
@@ -0,0 +1,265 @@
+import UIKit
+import WordPressKit
+
+
+/// The purpose of this class is to render a collection of notifications sunscriptions for a Site Topic,
+/// and to provide the user a simple interface to update those settings, as needed.
+///
+class NotificationSiteSubscriptionViewController: UITableViewController {
+    private class Section {
+        enum SectionType {
+            case posts
+            case emails
+            case comments
+        }
+
+        var type: SectionType
+        var rows: [Row]
+        var footerText: String?
+
+        init(type: SectionType, rows: [Row], footerText: String? = nil) {
+            self.type = type
+            self.rows = rows
+            self.footerText = footerText
+        }
+    }
+
+    private class Row {
+        enum Kind: String {
+            case setting = "kSwitchCellIdentifier"
+            case checkmark = "kCheckmarkCellIdentifier"
+        }
+
+        let title: String
+        let kind: Kind
+        let frequency: ReaderServiceDeliveryFrequency?
+
+
+        init(kind: Kind = .setting, title: String, frequency: ReaderServiceDeliveryFrequency? = nil) {
+            self.title = NSLocalizedString(title, comment: "The title will be displayed by the row cell")
+            self.kind = kind
+            self.frequency = frequency
+        }
+    }
+
+    private class SiteSubscription {
+        var postsNotification: Bool = false
+        var emailsNotification: Bool = false {
+            didSet {
+                if emailsNotification {
+                    frequency = .instantly
+                }
+            }
+        }
+        var commentsNotification: Bool = false
+        var frequency: ReaderServiceDeliveryFrequency?
+
+        func boolValue(for sectionType: Section.SectionType) -> Bool {
+            switch sectionType {
+            case .posts: return postsNotification
+            case .emails: return emailsNotification
+            case .comments: return commentsNotification
+            }
+        }
+    }
+
+    private var sections: [Section] = []
+    private let service = ReaderTopicService(managedObjectContext: ContextManager.sharedInstance().mainContext)
+    private let siteId: Int
+    private var siteTopic: ReaderSiteTopic?
+    private let siteSubscription = SiteSubscription()
+
+
+    required init(siteId: Int) {
+        self.siteId = siteId
+        super.init(style: .grouped)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    deinit {
+//        stopListeningToNotifications()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupData()
+        setupTitle()
+        setupTableView()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        saveSettingsIfNeeded()
+    }
+
+
+    // MARK: - Private methods
+
+    private func setupData() {
+        siteTopic = service.findSiteTopic(withSiteID: NSNumber(value: siteId))
+
+        siteSubscription.postsNotification = siteTopic?.postSubscription?.sendPosts ?? false
+        siteSubscription.emailsNotification = siteTopic?.emailSubscription?.sendPosts ?? false
+        siteSubscription.commentsNotification = siteTopic?.emailSubscription?.sendComments ?? false
+        if let postDeliveryFrequency = siteTopic?.emailSubscription?.postDeliveryFrequency,
+            let frequency = ReaderServiceDeliveryFrequency(rawValue: postDeliveryFrequency) {
+            siteSubscription.frequency = frequency
+        }
+
+        let instantly = ReaderServiceDeliveryFrequency.instantly
+        let daily = ReaderServiceDeliveryFrequency.daily
+        let weekly = ReaderServiceDeliveryFrequency.weekly
+
+        let post = Section(type: .posts,
+                           rows: [Row(title: "New posts")],
+                           footerText: "Receive notifications for new posts from this site")
+        let email = Section(type: .emails, rows: [Row(title: "Email me new posts"),
+                                                  Row(kind: .checkmark, title: instantly.rawValue.capitalized, frequency: instantly),
+                                                  Row(kind: .checkmark, title: daily.rawValue.capitalized, frequency: daily),
+                                                  Row(kind: .checkmark, title: weekly.rawValue.capitalized, frequency: weekly)])
+        let comments = Section(type: .comments, rows: [Row(title: "Email me new comments")])
+
+        sections = [post, email, comments]
+    }
+
+    private func setupTitle() {
+        title = siteTopic?.title
+    }
+
+    private func setupTableView() {
+        // Register the cells
+        tableView.register(SwitchTableViewCell.self, forCellReuseIdentifier: Row.Kind.setting.rawValue)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: Row.Kind.checkmark.rawValue)
+
+        // Hide the separators, whenever the table is empty
+        tableView.tableFooterView = UIView()
+
+        // Style!
+        WPStyleGuide.configureColors(for: view, andTableView: tableView)
+        WPStyleGuide.configureAutomaticHeightRows(for: tableView)
+    }
+
+    private func switchCell(for section: Section, row: Row, at index: IndexPath) -> SwitchTableViewCell {
+        let cell: SwitchTableViewCell = self.cell(for: tableView, at: index, identifier: row.kind.rawValue)
+        cell.name = row.title
+        cell.on = siteSubscription.boolValue(for: section.type)
+        cell.onChange = { [weak self] (newValue: Bool) in
+            switch section.type {
+            case .posts:
+                self?.siteSubscription.postsNotification = newValue
+
+            case .emails:
+                self?.siteSubscription.emailsNotification = newValue
+                self?.reloadData(at: index.section, animation: .fade)
+
+            case .comments:
+                self?.siteSubscription.commentsNotification = newValue
+            }
+        }
+        return cell
+    }
+
+    private func reloadData(at section: Int, animation: UITableViewRowAnimation = .none) {
+        let sections = IndexSet(integer: section)
+        tableView.reloadSections(sections, with: animation)
+    }
+
+    private func saveSettingsIfNeeded() {
+//        service.updateSettings(settings!,
+//                               stream: stream!,
+//                               newValues: newValues,
+//                               success: {
+//                                WPAnalytics.track(.notificationsSettingsUpdated, withProperties: ["success": true])
+//        },
+//                               failure: { (error: Error?) in
+//                                WPAnalytics.track(.notificationsSettingsUpdated, withProperties: ["success": false])
+//                                self.handleUpdateError()
+//        })
+    }
+
+    private func handleUpdateError() {
+        let title = NSLocalizedString("Oops!", comment: "")
+        let message = NSLocalizedString("There has been an unexpected error while updating your followed sites Notification Settings",
+                                            comment: "Displayed after a failed Notification Settings call")
+        let cancelText = NSLocalizedString("Cancel", comment: "Cancel. Action.")
+        let retryText = NSLocalizedString("Retry", comment: "Retry. Action")
+
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addCancelActionWithTitle(cancelText, handler: nil)
+        alertController.addDefaultActionWithTitle(retryText) { (action: UIAlertAction) in
+            self.saveSettingsIfNeeded()
+        }
+        alertController.presentFromRootViewController()
+    }
+
+    private func cell<T: UITableViewCell>(for tableView: UITableView, at indexPath: IndexPath, identifier: String) -> T {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: identifier) as? T else {
+            fatalError("A cell must be found for identifier: \(identifier)")
+        }
+
+        return cell
+    }
+
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let section = sections[section]
+        switch section.type {
+        case .emails: return siteSubscription.emailsNotification ? section.rows.count : 1
+        default: return section.rows.count
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let section = sections[indexPath.section]
+        let row = section.rows[indexPath.row]
+
+        switch row.kind {
+        case .checkmark:
+            let cell: UITableViewCell = self.cell(for: tableView, at: indexPath, identifier: row.kind.rawValue)
+            cell.accessoryType = siteSubscription.frequency == row.frequency ? .checkmark : .none
+            cell.textLabel?.text = row.title
+            WPStyleGuide.configureTableViewCell(cell)
+            return cell
+
+        case .setting:
+            return switchCell(for: section, row: row, at: indexPath)
+        }
+    }
+
+    // MARK: - Table view delegate
+
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        WPStyleGuide.configureTableViewSectionHeader(view)
+    }
+
+    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return sections[section].footerText
+    }
+
+    override func tableView(_ tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
+        WPStyleGuide.configureTableViewSectionFooter(view)
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectSelectedRowWithAnimation(true)
+
+        let section = sections[indexPath.section]
+        let row = section.rows[indexPath.row]
+        if row.kind == .checkmark {
+            let row = section.rows[indexPath.row]
+            siteSubscription.frequency = row.frequency
+            reloadData(at: indexPath.section)
+        }
+    }
+}
