e557a9a81d86cf6a1d3fab3f1b20c8b7f9edcacb
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-05-04 12:49:19 +0800

    Update description of event chain

--- WordPress/Classes/ViewRelated/Reader/SaveForLaterAction.swift
+++ WordPress/Classes/ViewRelated/Reader/SaveForLaterAction.swift
@@ -1,7 +1,8 @@
 final class SaveForLaterAction: PostAction {
     func execute(with post: ReaderPost) {
         //1.- Prepare toast
-        //2.- Send post to the service
-        //3.- Present toast
+        //2.- Mark post as saved for later (boolean flag in ReaderPost)
+        //3.- Send post to the SaveForLaterService
+        //4.- Present toast
     }
 }
