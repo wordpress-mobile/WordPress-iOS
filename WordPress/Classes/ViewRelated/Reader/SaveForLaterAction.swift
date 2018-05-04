d70081a1023720011b908c6a6772b12774a8ae47
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-05-04 13:13:40 +0800

    Present notice

--- WordPress/Classes/ViewRelated/Reader/SaveForLaterAction.swift
+++ WordPress/Classes/ViewRelated/Reader/SaveForLaterAction.swift
@@ -1,8 +1,26 @@
+import WordPressFlux
+
 final class SaveForLaterAction: PostAction {
+    private struct Strings {
+        static let postSaved = NSLocalizedString("Post saved.", comment: "Title of the notification presented in Reader when a post is saved for later")
+        static let viewAll = NSLocalizedString("View All", comment: "Button in the notification presented in Reader when a post is saved for later")
+    }
+
     func execute(with post: ReaderPost) {
-        //1.- Prepare toast
+        //1.- Present notice
+        presentNotice()
         //2.- Mark post as saved for later (boolean flag in ReaderPost)
         //3.- Send post to the SaveForLaterService
         //4.- Present toast
+    }
+
+    private func presentNotice() {
+        let notice = Notice(title: Strings.postSaved,
+                            actionTitle: Strings.viewAll,
+                            actionHandler: {
+                                print("Howdy!")
+        })
+
+        ActionDispatcher.dispatch(NoticeAction.post(notice))
     }
 }
