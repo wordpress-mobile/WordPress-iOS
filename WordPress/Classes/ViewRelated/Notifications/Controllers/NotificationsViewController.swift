632c9b5920d9319f3362ef20139eae6c5a662353
Nate Heagy <nate@heagy.com>
2018-05-08 15:27:45 -0600

    Abstract app rating table header for reuse

--- WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationsViewController.swift
+++ WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationsViewController.swift
@@ -403,11 +403,11 @@ private extension NotificationsViewController {
     func setupRatingsView() {
         precondition(ratingsView != nil)
 
-        ratingsView.delegate = self
         ratingsView.alpha = WPAlphaZero
 
         // this allows the selector to move to the top
         ratingsSpaceConstraint.isActive = false
+        setupForAppRatings()
     }
 
     func setupRefreshControl() {
@@ -1207,9 +1207,9 @@ extension NotificationsViewController: WPNoResultsViewDelegate {
 //
 private extension NotificationsViewController {
     func showRatingViewIfApplicable() {
-        guard AppRatingUtility.shared.shouldPromptForAppReview(section: Ratings.section) else {
-            return
-        }
+//        guard AppRatingUtility.shared.shouldPromptForAppReview(section: Ratings.section) else {
+//            return
+//        }
 
         guard ratingsView.alpha != WPAlphaFull else {
             return
@@ -1385,67 +1385,6 @@ extension NotificationsViewController: WPSplitViewControllerDetailProvider {
     }
 }
 
-// MARK: - AppFeedbackPromptViewDelegate Methods
-//
-extension NotificationsViewController: AppFeedbackPromptViewDelegate {
-    @objc func likedApp() {
-        WPAnalytics.track(.appReviewsLikedApp)
-        AppRatingUtility.shared.likedCurrentVersion()
-        hideRatingViewWithDelay(3.0)
-        // Optimistically assuming our prompting succeeds since we try to stay
-        // in line and not prompt more than three times a year
-        AppRatingUtility.shared.ratedCurrentVersion()
-        AppRatingUtility.shared.userWasPromptedToReview()
-        DispatchQueue.main.async {
-            if #available(iOS 10.3, *) {
-                SKStoreReviewController.requestReview()
-            } else {
-                UIApplication.shared.open(AppRatingUtility.shared.appReviewUrl)
-            }
-        }
-
-    }
-
-    @objc func dislikedApp() {
-        WPAnalytics.track(.appReviewsDidntLikeApp)
-        AppRatingUtility.shared.dislikedCurrentVersion()
-    }
-
-    @objc func gatherFeedback() {
-        WPAnalytics.track(.appReviewsOpenedFeedbackScreen)
-        AppRatingUtility.shared.gaveFeedbackForCurrentVersion()
-        hideRatingViewWithDelay(0.0)
-
-        if FeatureFlag.zendeskMobile.enabled {
-            if ZendeskUtils.zendeskEnabled {
-                ZendeskUtils.sharedInstance.showNewRequestIfPossible(from: self)
-            } else {
-                if let contact = URL(string: NotificationsViewController.contactURL) {
-                    UIApplication.shared.open(contact)
-                }
-            }
-        } else {
-            if HelpshiftUtils.isHelpshiftEnabled() {
-                let presenter = HelpshiftPresenter.init()
-                presenter.sourceTag = SupportSourceTag.inAppFeedback
-                presenter.presentHelpshiftConversationWindowFromViewController(self,
-                                                                               refreshUserDetails: true,
-                                                                               completion: nil)
-            } else {
-                if let contact = URL(string: NotificationsViewController.contactURL) {
-                    UIApplication.shared.open(contact)
-                }
-            }
-        }
-    }
-
-    @objc func dismissPrompt() {
-        WPAnalytics.track(.appReviewsDeclinedToRateApp)
-        AppRatingUtility.shared.declinedToRateCurrentVersion()
-        hideRatingViewWithDelay(0.0)
-    }
-}
-
 // MARK: - Details Navigation Datasource
 //
 extension NotificationsViewController: NotificationsNavigationDataSource {
@@ -1483,6 +1422,108 @@ extension NotificationsViewController: SearchableActivityConvertable {
     }
 }
 
+// MARK: - App Ratings
+//
+extension NotificationsViewController {
+    func setupForAppRatings() {
+        ratingsView.setupHeading(NSLocalizedString("What do you think about WordPress?",
+                                                   comment: "This is the string we display when prompting the user to review the app"))
+        let yesTitle = NSLocalizedString("I Like It",
+                                         comment: "This is one of the buttons we display inside of the prompt to review the app")
+        let noTitle = NSLocalizedString("Could Be Better",
+                                        comment: "This is one of the buttons we display inside of the prompt to review the app")
+
+        ratingsView.setupYesButton(title: yesTitle) { [weak self] button in
+            self?.likedApp()
+        }
+
+        ratingsView.setupNoButton(title: noTitle) { [weak self] button in
+            self?.dislikedApp()
+        }
+    }
+
+    private func likedApp() {
+        defer {
+            WPAnalytics.track(.appReviewsLikedApp)
+        }
+        AppRatingUtility.shared.likedCurrentVersion()
+
+        // 1. Show the thank-you, then hide it after a few seconds
+        hideRatingViewWithDelay(3.0)
+        ratingsView.showBigHeading(title: NSLocalizedString("Great!\n We love to hear from happy users \n😁",
+                                                                  comment: "This is the text we display to the user after they've indicated they like the app"))
+
+        // 2. Show the app store ratings alert
+        // Note: Optimistically assuming our prompting succeeds since we try to stay
+        // in line and not prompt more than three times a year
+        AppRatingUtility.shared.ratedCurrentVersion()
+        AppRatingUtility.shared.userWasPromptedToReview()
+        DispatchQueue.main.async {
+            if #available(iOS 10.3, *) {
+                SKStoreReviewController.requestReview()
+            } else {
+                UIApplication.shared.open(AppRatingUtility.shared.appReviewUrl)
+            }
+        }
+    }
+
+    private func dislikedApp() {
+        defer {
+            WPAnalytics.track(.appReviewsDidntLikeApp)
+        }
+        AppRatingUtility.shared.dislikedCurrentVersion()
+
+        // Let's try to find out why they don't like the app
+        UIView.animate(withDuration: 0.3) { [weak self] in
+            self?.ratingsView.setupHeading(NSLocalizedString("Could you tell us how we could improve?",
+                                                             comment: "This is the text we display to the user when we ask them for a review and they've indicated they don't like the app"))
+            let yesTitle = NSLocalizedString("Send Feedback",
+                                             comment: "This is one of the buttons we display when prompting the user for a review")
+            let noTitle = NSLocalizedString("No Thanks",
+                                            comment: "This is one of the buttons we display when prompting the user for a review")
+            self?.ratingsView.setupYesButton(title: yesTitle) { [weak self] button in
+                self?.gatherFeedback()
+            }
+            self?.ratingsView.setupNoButton(title: noTitle) { [weak self] button in
+                self?.dismissPrompt()
+            }
+        }
+    }
+
+    private func gatherFeedback() {
+        WPAnalytics.track(.appReviewsOpenedFeedbackScreen)
+        AppRatingUtility.shared.gaveFeedbackForCurrentVersion()
+        hideRatingViewWithDelay(0.0)
+
+        if FeatureFlag.zendeskMobile.enabled {
+            if ZendeskUtils.zendeskEnabled {
+                ZendeskUtils.sharedInstance.showNewRequestIfPossible(from: self)
+            } else {
+                if let contact = URL(string: NotificationsViewController.contactURL) {
+                    UIApplication.shared.open(contact)
+                }
+            }
+        } else {
+            if HelpshiftUtils.isHelpshiftEnabled() {
+                let presenter = HelpshiftPresenter.init()
+                presenter.sourceTag = SupportSourceTag.inAppFeedback
+                presenter.presentHelpshiftConversationWindowFromViewController(self,
+                                                                               refreshUserDetails: true,
+                                                                               completion: nil)
+            } else {
+                if let contact = URL(string: NotificationsViewController.contactURL) {
+                    UIApplication.shared.open(contact)
+                }
+            }
+        }
+    }
+
+    private func dismissPrompt() {
+        WPAnalytics.track(.appReviewsDeclinedToRateApp)
+        AppRatingUtility.shared.declinedToRateCurrentVersion()
+        hideRatingViewWithDelay(0.0)
+    }
+}
 
 // MARK: - Private Properties
 //
