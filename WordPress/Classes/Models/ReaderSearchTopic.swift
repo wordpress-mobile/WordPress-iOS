529d0250035853c70202c7c2a7c7321472a5077a
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-05-10 12:35:57 +0800

    Override posts to return the results of a fetch request

--- WordPress/Classes/Models/ReaderSearchTopic.swift
+++ WordPress/Classes/Models/ReaderSearchTopic.swift
@@ -4,4 +4,21 @@ import Foundation
     override open class var TopicType: String {
         return "search"
     }
+
+    open override var posts: [ReaderPost] {
+        set {}
+        get {
+            if let context = managedObjectContext {
+                let savedPostsRequest = NSFetchRequest<NSFetchRequestResult>(entityName: ReaderPost.classNameWithoutNamespaces())
+                savedPostsRequest.predicate = NSPredicate(format: "isSavedForLater = %@", NSNumber(value: true))
+                guard let results = (try? context.fetch(savedPostsRequest)) as? [ReaderPost] else {
+                    return []
+                }
+
+                return results
+            }
+
+            return []
+        }
+    }
 }
