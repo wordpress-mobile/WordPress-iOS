fefcbb848d014f28b45f8f5faba5c38b0c59ef2f
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-05-10 12:37:27 +0800

    Wrong object! ReaderSaveForLaterTopic is the one that will override posts

--- WordPress/Classes/Models/ReaderSaveForLaterTopic.swift
+++ WordPress/Classes/Models/ReaderSaveForLaterTopic.swift
@@ -2,4 +2,21 @@
     override open class var TopicType: String {
         return "saveForLater"
     }
+
+    open override var posts: [ReaderPost] {
+        set {}
+        get {
+            if let context = managedObjectContext {
+                let savedPostsRequest = NSFetchRequest<NSFetchRequestResult>(entityName: ReaderPost.classNameWithoutNamespaces())
+                savedPostsRequest.predicate = NSPredicate(format: "isSavedForLater = %@", NSNumber(value: true))
+                guard let results = (try? context.fetch(savedPostsRequest)) as? [ReaderPost] else {
+                    return []
+                }
+
+                return results
+            }
+
+            return []
+        }
+    }
 }
--- WordPress/Classes/Models/ReaderSearchTopic.swift
+++ WordPress/Classes/Models/ReaderSearchTopic.swift
@@ -4,21 +4,4 @@ import Foundation
     override open class var TopicType: String {
         return "search"
     }
-
-    open override var posts: [ReaderPost] {
-        set {}
-        get {
-            if let context = managedObjectContext {
-                let savedPostsRequest = NSFetchRequest<NSFetchRequestResult>(entityName: ReaderPost.classNameWithoutNamespaces())
-                savedPostsRequest.predicate = NSPredicate(format: "isSavedForLater = %@", NSNumber(value: true))
-                guard let results = (try? context.fetch(savedPostsRequest)) as? [ReaderPost] else {
-                    return []
-                }
-
-                return results
-            }
-
-            return []
-        }
-    }
 }
