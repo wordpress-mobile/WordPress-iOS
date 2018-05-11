8d6ef29f16a72ff50001c06f87420cf86cd1892f
etoledom <etoledom@icloud.com>
2018-05-11 18:58:50 -0300

    FeaturedImageViewController: Added support for animated gifs

--- WordPress/Classes/ViewRelated/Post/FeaturedImageViewController.h
+++ WordPress/Classes/ViewRelated/Post/FeaturedImageViewController.h
@@ -1,8 +1,15 @@
 #import "WPImageViewController.h"
 
 @class AbstractPost;
+@class FeaturedImageViewController;
+
+@protocol FeaturedImageViewControllerDelegate <NSObject>
+- (void)FeaturedImageViewControllerOnRemoveImageButtonPressed:(FeaturedImageViewController *)controller;
+@end
 
 @interface FeaturedImageViewController : WPImageViewController
+
+@property (weak, nonatomic) id<FeaturedImageViewControllerDelegate> delegate;
 
 - (id)initWithPost:(AbstractPost *)post;
 
