cd46c2181b5efb256ee7d9efa775963b240e765d
Daniele Bogo <me@bogodaniele.com>
2018-05-13 10:30:26 +0100

    #8950: Settings - Localize strings
    - Localize Remote Service error message
    - Localize ReaderTopicServiceError cases

--- WordPressKit/WordPressKit/ReaderTopicServiceError.swift
+++ WordPressKit/WordPressKit/ReaderTopicServiceError.swift
@@ -12,10 +12,14 @@ public enum ReaderTopicServiceError: Error {
             return "Invalid id: an id must be valid or not nil"
             
         case .topicNotfound(let id):
-            return "Topic not found for id: \(id)"
+            let localizedString = NSLocalizedString("Topic not found for id:",
+                                                    comment: "Used when a Reader Topic is not found for a specific id")
+            return localizedString + " \(id)"
             
         case .remoteResponse(let message, let url):
-            return message ?? "an error occurred while processing your request \(url)"
+            let localizedString = NSLocalizedString("An error occurred while processing your request: ",
+                                                    comment: "Used when a remote response doesn't have a specific message for a specific request")
+            return message ?? localizedString + " \(url)"
         }
     }
 }
