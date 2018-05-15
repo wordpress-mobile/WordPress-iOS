83f1575d8a2ece6e4b49f2013ac77d313dfab509
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-05-14 19:23:43 -0700

    Extract ShowAttributionAction

---
+++ WordPress/Classes/ViewRelated/Reader/ShowAttributionAction.swift
@@ -0,0 +1,28 @@
+final class ShowAttributionAction {
+    func execute(with post: ReaderPost, context: NSManagedObjectContext, origin: UIViewController) {
+        // Fail safe. If there is no attribution exit.
+        guard let sourceAttribution = post.sourceAttribution else {
+            return
+        }
+
+        // If there is a blogID preview the site
+        if let blogID = sourceAttribution.blogID {
+            let controller = ReaderStreamViewController.controllerWithSiteID(blogID, isFeed: false)
+            origin.navigationController?.pushViewController(controller, animated: true)
+            return
+        }
+
+        if sourceAttribution.attributionType != SourcePostAttributionTypeSite {
+            return
+        }
+
+        guard let linkURL = URL(string: sourceAttribution.blogURL) else {
+            return
+        }
+        let configuration = WebViewControllerConfiguration(url: linkURL)
+        configuration.addsWPComReferrer = true
+        let controller = WebViewControllerFactory.controller(configuration: configuration)
+        let navController = UINavigationController(rootViewController: controller)
+        origin.present(navController, animated: true, completion: nil)
+    }
+}
