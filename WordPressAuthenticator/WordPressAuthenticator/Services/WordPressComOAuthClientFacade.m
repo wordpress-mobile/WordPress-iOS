061d46f6401c4049bee790b96973dcc71d52eef2
Jorge Leandro Perez <jorge@lantean.co>
2018-05-16 12:01:06 -0300

    Updating WordPressKit Imports

--- WordPressAuthenticator/WordPressAuthenticator/Services/WordPressComOAuthClientFacade.m
+++ WordPressAuthenticator/WordPressAuthenticator/Services/WordPressComOAuthClientFacade.m
@@ -1,6 +1,8 @@
 #import "WordPressComOAuthClientFacade.h"
 #import <WordPressKit/WordPressKit.h>
+#import <WordPressKit/WordPressKit-Swift.h>
 #import <WordPressAuthenticator/WordPressAuthenticator-Swift.h>
+
 
 @interface WordPressComOAuthClientFacade ()
 
