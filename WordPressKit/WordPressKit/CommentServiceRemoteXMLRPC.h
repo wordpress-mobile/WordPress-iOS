0893314992c1716865aeeef9d266930dc8c97675
Jorge Leandro Perez <jorge@lantean.co>
2018-05-16 10:30:34 -0300

    Nuking WordPressKit Folder

--- WordPressKit/WordPressKit/CommentServiceRemoteXMLRPC.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "CommentServiceRemote.h"
-#import "ServiceRemoteWordPressXMLRPC.h"
-
-@interface CommentServiceRemoteXMLRPC : ServiceRemoteWordPressXMLRPC<CommentServiceRemote>
-
-@end
--- WordPressKit/WordPressKit/RemoteReaderPost.m
+++
@@ -1,7 +0,0 @@
-#import "RemoteReaderPost.h"
-#import "RemoteSourcePostAttribution.h"
-#import <WordPressKit/WordPressKit-Swift.h>
-
-@implementation RemoteReaderPost
-
-@end
