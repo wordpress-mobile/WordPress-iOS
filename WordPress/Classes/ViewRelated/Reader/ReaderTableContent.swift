64fa48b4609831d64b8b24e33a9d721b390d1971
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-05-15 20:06:41 -0700

    Initialize content handler

--- WordPress/Classes/ViewRelated/Reader/ReaderTableContent.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderTableContent.swift
@@ -1,3 +1,10 @@
 final class ReaderTableContent {
+    private var tableViewHandler: WPTableViewHandler?
 
+    func initializeContent(tableView: UITableView, delegate: WPTableViewHandlerDelegate) {
+        tableViewHandler = WPTableViewHandler(tableView: tableView)
+        tableViewHandler?.cacheRowHeights = false
+        tableViewHandler?.updateRowAnimation = .none
+        tableViewHandler?.delegate = delegate
+    }
 }
