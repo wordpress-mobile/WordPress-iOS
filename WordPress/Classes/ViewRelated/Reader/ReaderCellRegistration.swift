fc6fa53180dceb226ae4d419ab7124de262bda85
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-05-15 18:54:26 -0700

    Add ReaderCellRegistration

---
+++ WordPress/Classes/ViewRelated/Reader/ReaderCellRegistration.swift
@@ -0,0 +1,12 @@
+struct ReaderCellRegistration {
+    private let footerViewNibName = "PostListFooterView"
+    private let readerCardCellNibName = "ReaderPostCardCell"
+    private let readerCardCellReuseIdentifier = "ReaderCardCellReuseIdentifier"
+    private let readerBlockedCellNibName = "ReaderBlockedSiteCell"
+    private let readerBlockedCellReuseIdentifier = "ReaderBlockedCellReuseIdentifier"
+    private let readerGapMarkerCellNibName = "ReaderGapMarkerCell"
+    private let readerGapMarkerCellReuseIdentifier = "ReaderGapMarkerCellReuseIdentifier"
+    private let readerCrossPostCellNibName = "ReaderCrossPostCell"
+    private let readerCrossPostCellReuseIdentifier = "ReaderCrossPostCellReuseIdentifier"
+
+}
