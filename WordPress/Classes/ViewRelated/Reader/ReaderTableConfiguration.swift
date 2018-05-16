63baa20f4d8ea45bf9eddd015a7025a770385516
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-05-15 19:19:07 -0700

    Begin extracting cell configuration

--- WordPress/Classes/ViewRelated/Reader/ReaderTableConfiguration.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderTableConfiguration.swift
@@ -9,4 +9,40 @@ struct ReaderTableConfiguration {
     private let readerCrossPostCellNibName = "ReaderCrossPostCell"
     private let readerCrossPostCellReuseIdentifier = "ReaderCrossPostCellReuseIdentifier"
 
+    func setup(_ tableView: UITableView) {
+        setUpAccesibility(tableView)
+        setUpSeparator(tableView)
+        setUpCardCell(tableView)
+        setUpBlockerCell(tableView)
+        setUpGapMarkerCell(tableView)
+        setUpCrossPostCell(tableView)
+    }
+
+    private func setUpAccesibility(_ tableView: UITableView) {
+        tableView.accessibilityIdentifier = "Reader"
+    }
+
+    private func setUpSeparator(_ tableView: UITableView) {
+        tableView.separatorStyle = .none
+    }
+
+    private func setUpCardCell(_ tableView: UITableView) {
+        let nib = UINib(nibName: readerCardCellNibName, bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: readerCardCellReuseIdentifier)
+    }
+
+    private func setUpBlockerCell(_ tableView: UITableView) {
+        let nib = UINib(nibName: readerBlockedCellNibName, bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: readerBlockedCellReuseIdentifier)
+    }
+
+    private func setUpGapMarkerCell(_ tableView: UITableView) {
+        let nib = UINib(nibName: readerGapMarkerCellNibName, bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: readerGapMarkerCellReuseIdentifier)
+    }
+
+    private func setUpCrossPostCell(_ tableView: UITableView) {
+        let nib = UINib(nibName: readerCrossPostCellNibName, bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: readerCrossPostCellReuseIdentifier)
+    }
 }
