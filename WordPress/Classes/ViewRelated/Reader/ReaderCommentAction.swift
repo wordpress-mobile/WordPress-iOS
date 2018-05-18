df3c9b059b5f633339880f0fb691306b2435ea85
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-05-17 18:29:21 -0700

    Rename MenuAction, HeaderAction, CommentAction

--- WordPress/Classes/ViewRelated/Reader/ReaderCommentAction.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderCommentAction.swift
@@ -1,5 +1,5 @@
 /// Encapsulates a command to navigate to a post's comments
-final class CommentAction {
+final class ReaderCommentAction {
     func execute(post: ReaderPost, origin: UIViewController) {
         guard let postInMainContext = ReaderActionHelpers.postInMainContext(post),
             let controller = ReaderCommentsViewController(post: postInMainContext) else {
