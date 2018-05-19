cf84ce1812595b8cba13ebc9667e2a8364844b26
etoledom <etoledom@icloud.com>
2018-05-19 07:54:18 -0400

    ImageLoader: Code organization

---
+++ WordPress/Classes/Extensions/MediaCellProgressView+ActivityIndicatorType.swift
@@ -0,0 +1,12 @@
+
+extension MediaCellProgressView: ActivityIndicatorType {
+    func startAnimating() {
+        isHidden = false
+        state = .indeterminate
+    }
+
+    func stopAnimating() {
+        isHidden = true
+        state = .stopped
+    }
+}
