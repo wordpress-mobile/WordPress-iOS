c9d9db5beeeda69eb6ba487c52025e798b256a91
etoledom <etoledom@icloud.com>
2018-05-19 07:54:18 -0400

    ImageLoader: Code organization

--- WordPress/Classes/Utility/Media/ImageLoader.swift
+++ WordPress/Classes/Utility/Media/ImageLoader.swift
@@ -13,18 +13,6 @@
     var isSelfHostedWithCredentials: Bool { get }
 }
 
-extension MediaCellProgressView: ActivityIndicatorType {
-    func startAnimating() {
-        isHidden = false
-        state = .indeterminate
-    }
-
-    func stopAnimating() {
-        isHidden = true
-        state = .stopped
-    }
-}
-
 /// Class used together with `CachedAnimatedImageView` to facilitate the loading of both
 /// still images and animated gifs.
 ///
@@ -65,37 +53,6 @@ extension MediaCellProgressView: ActivityIndicatorType {
         imageView.prepForReuse()
     }
 
-    @objc(loadImageFromMedia:preferredSize:placeholder:success:error:)
-    /// Load an image from the given Media object. If it's a gif, it will animate it.
-    /// For any other type of media, this will load the corresponding static image.
-    ///
-    /// - Parameters:
-    ///   - media: The media object
-    ///   - placeholder: A placeholder to show while the image is loading.
-    ///   - size: The prefered size of the image to load.
-    ///   - success: A closure to be called if the image was loaded successfully.
-    ///   - error: A closure to be called if there was an error loading the image.
-    ///
-    func loadImage(media: Media, preferredSize size: CGSize = .zero, placeholder: UIImage?, success: (() -> Void)?, error: ((Error?) -> Void)?) {
-
-        self.placeholder = placeholder
-        successHandler = success
-        errorHandler = error
-
-        guard let url = url(from: media) else {
-            let error = NSError(domain: NSURLErrorDomain, code: NSURLErrorBadURL, userInfo: nil)
-            callErrorHandler(with: error)
-            return
-        }
-
-        if url.isGif {
-            loadGif(with: url, from: media.blog)
-        } else if imageView.image == nil {
-            imageView.clean()
-            loadImage(from: media, preferredSize: size)
-        }
-    }
-
     @objc(loadImageWithURL:fromPost:andPreferedSize:)
     /// Load an image from a specific post, using the given URL. Supports animated images (gifs) as well.
     ///
@@ -104,12 +61,12 @@ extension MediaCellProgressView: ActivityIndicatorType {
     ///   - post: The post where the image is loaded from.
     ///   - size: The prefered size of the image to load.
     ///
-    func loadImage(with url: URL, from post: ImageSourceInformation, preferedSize size: CGSize = .zero) {
+    func loadImage(with url: URL, from source: ImageSourceInformation, preferedSize size: CGSize = .zero) {
         if url.isGif {
-            loadGif(with: url, from: post)
+            loadGif(with: url, from: source)
         } else {
             imageView.clean()
-            loadStaticImage(with: url, from: post, preferedSize: size)
+            loadStaticImage(with: url, from: source, preferedSize: size)
         }
     }
 
@@ -123,22 +80,22 @@ extension MediaCellProgressView: ActivityIndicatorType {
     ///   - placeholder: A placeholder to show while the image is loading.
     ///   - success: A closure to be called if the image was loaded successfully.
     ///   - error: A closure to be called if there was an error loading the image.
-    func loadImage(with url: URL, from post: ImageSourceInformation, preferedSize size: CGSize = .zero, placeholder: UIImage?, success: (() -> Void)?, error: ((Error?) -> Void)?) {
+    func loadImage(with url: URL, from source: ImageSourceInformation, preferedSize size: CGSize = .zero, placeholder: UIImage?, success: (() -> Void)?, error: ((Error?) -> Void)?) {
 
         self.placeholder = placeholder
         successHandler = success
         errorHandler = error
 
-        loadImage(with: url, from: post, preferedSize: size)
+        loadImage(with: url, from: source, preferedSize: size)
     }
 
     // MARK: - Private helpers
 
     /// Load an animated image from the given URL.
     ///
-    private func loadGif(with url: URL, from post: ImageSourceInformation) {
+    private func loadGif(with url: URL, from source: ImageSourceInformation) {
         let request: URLRequest
-        if post.isPrivateOnWPCom {
+        if source.isPrivateOnWPCom {
             request = PrivateSiteURLProtocol.requestForPrivateSite(from: url)
         } else {
             request = URLRequest(url: url)
@@ -148,12 +105,12 @@ extension MediaCellProgressView: ActivityIndicatorType {
 
     /// Load a static image from the given URL.
     ///
-    private func loadStaticImage(with url: URL, from post: ImageSourceInformation, preferedSize size: CGSize) {
+    private func loadStaticImage(with url: URL, from source: ImageSourceInformation, preferedSize size: CGSize) {
         if url.isFileURL {
             downloadImage(from: url)
-        } else if post.isPrivateOnWPCom {
-            loadPrivateImage(with: url, from: post, preferedSize: size)
-        } else if post.isSelfHostedWithCredentials {
+        } else if source.isPrivateOnWPCom {
+            loadPrivateImage(with: url, from: source, preferedSize: size)
+        } else if source.isSelfHostedWithCredentials {
             downloadImage(from: url)
         } else {
             loadProtonUrl(with: url, preferedSize: size)
@@ -162,7 +119,7 @@ extension MediaCellProgressView: ActivityIndicatorType {
 
     /// Loads the image from a private post hosted in WPCom.
     ///
-    private func loadPrivateImage(with url: URL, from post: ImageSourceInformation, preferedSize size: CGSize) {
+    private func loadPrivateImage(with url: URL, from source: ImageSourceInformation, preferedSize size: CGSize) {
         let scale = UIScreen.main.scale
         let scaledSize = CGSize(width: size.width * scale, height: size.height * scale)
         let scaledURL = WPImageURLHelper.imageURLWithSize(scaledSize, forImageURL: url)
@@ -179,19 +136,6 @@ extension MediaCellProgressView: ActivityIndicatorType {
             return
         }
         downloadImage(from: protonURL)
-    }
-
-    private func loadImage(from media: Media, preferredSize size: CGSize) {
-        imageView.image = placeholder
-        imageView.startLoadingAnimation()
-        media.image(with: size) {  [weak self] (image, error) in
-            if let image = image {
-                self?.imageView.image = image
-                self?.callSuccessHandler()
-            } else {
-                self?.callErrorHandler(with: error)
-            }
-        }
     }
 
     /// Download the animated image from the given URL Request.
@@ -246,6 +190,55 @@ extension MediaCellProgressView: ActivityIndicatorType {
         DispatchQueue.main.async {
             self.loadingIndicator.state = .error
             self.errorHandler?(error)
+        }
+    }
+}
+
+//MARK: - Loading Media object
+
+extension ImageLoader {
+
+    @objc(loadImageFromMedia:preferredSize:placeholder:success:error:)
+    /// Load an image from the given Media object. If it's a gif, it will animate it.
+    /// For any other type of media, this will load the corresponding static image.
+    ///
+    /// - Parameters:
+    ///   - media: The media object
+    ///   - placeholder: A placeholder to show while the image is loading.
+    ///   - size: The prefered size of the image to load.
+    ///   - success: A closure to be called if the image was loaded successfully.
+    ///   - error: A closure to be called if there was an error loading the image.
+    ///
+    func loadImage(media: Media, preferredSize size: CGSize = .zero, placeholder: UIImage?, success: (() -> Void)?, error: ((Error?) -> Void)?) {
+
+        self.placeholder = placeholder
+        successHandler = success
+        errorHandler = error
+
+        guard let url = url(from: media) else {
+            let error = NSError(domain: NSURLErrorDomain, code: NSURLErrorBadURL, userInfo: nil)
+            callErrorHandler(with: error)
+            return
+        }
+
+        if url.isGif {
+            loadGif(with: url, from: media.blog)
+        } else if imageView.image == nil {
+            imageView.clean()
+            loadImage(from: media, preferredSize: size)
+        }
+    }
+
+    private func loadImage(from media: Media, preferredSize size: CGSize) {
+        imageView.image = placeholder
+        imageView.startLoadingAnimation()
+        media.image(with: size) {  [weak self] (image, error) in
+            if let image = image {
+                self?.imageView.image = image
+                self?.callSuccessHandler()
+            } else {
+                self?.callErrorHandler(with: error)
+            }
         }
     }
 
