207fd68f76df8c2b8aace657a15d14154d08ddf8
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-05-20 17:01:56 -0700

    Expose a new notification to navigate to all saved for later

--- WordPress/Classes/Extensions/Notifications/NotificationName+Names.swift
+++ WordPress/Classes/Extensions/Notifications/NotificationName+Names.swift
@@ -6,6 +6,10 @@ extension Foundation.Notification.Name {
     static var reachabilityChanged: Foundation.NSNotification.Name {
         return Foundation.Notification.Name("org.wordpress.reachability.changed")
     }
+
+    static var showAllSavedForLaterPosts: Foundation.NSNotification.Name {
+        return Foundation.Notification.Name("org.wordpress.reader.savedforlaterposts.showall")
+    }
 }
 
 /// Keys for Notification's userInfo dictionary
