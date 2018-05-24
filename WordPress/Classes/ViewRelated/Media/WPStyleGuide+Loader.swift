29633224eab54d58e7934e6eb0e655294faf3edb
etoledom <etoledom@icloud.com>
2018-05-24 12:42:44 -0400

    CircularProgressView: Enum based style

--- WordPress/Classes/ViewRelated/Media/WPStyleGuide+Loader.swift
+++ WordPress/Classes/ViewRelated/Media/WPStyleGuide+Loader.swift
@@ -3,14 +3,11 @@ import UIKit
 
 extension WPStyleGuide {
     static func styleProgressViewWhite(_ progressView: CircularProgressView) {
-        progressView.loaderAppearance = ProgressIndicatorView.Appearance(lineColor: WPStyleGuide.lightBlue())
         progressView.backgroundColor = .clear
     }
 
     static func styleProgressViewForMediaCell(_ progressView: CircularProgressView) {
         progressView.backgroundColor = WPStyleGuide.darkGrey()
-        let appearance = ProgressIndicatorView.Appearance(lineColor: .white, trackColor: WPStyleGuide.grey())
-        progressView.loaderAppearance = appearance
         progressView.retryView.tintColor = .white
     }
 
