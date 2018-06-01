d3a591bb51b0053a6a52a9b828b3ed79ea10b1c1
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-05-31 17:24:54 -0700

    Start by adding an empty cell for Undo

---
+++ WordPress/Classes/ViewRelated/Reader/ReaderSavedPostUndoCell.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+final class ReaderSavedPostUndoCell: UITableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
