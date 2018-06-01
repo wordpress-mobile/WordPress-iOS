4b707f804e83a6c3ce8d7c95903de0afe887434c
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-05-31 17:39:31 -0700

    Temporary layout and action, to get things rolling

--- WordPress/Classes/ViewRelated/Reader/ReaderSavedPostUndoCell.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderSavedPostUndoCell.swift
@@ -1,14 +1,19 @@
 import UIKit
 
 final class ReaderSavedPostUndoCell: UITableViewCell {
+    @IBOutlet weak var removed: UILabel!
+    @IBOutlet weak var title: UILabel!
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    @IBAction func undo(_ sender: Any) {
+    }
+
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        setHighlighted(selected, animated: animated)
     }
 }
