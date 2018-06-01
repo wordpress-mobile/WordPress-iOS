2b049a61310f472e8660b0d12e73eeae113ba36d
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-05-31 17:58:50 -0700

    Initial implementation of removed posts

--- WordPress/Classes/ViewRelated/Reader/ReaderSaveForLaterRemovedPosts.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderSaveForLaterRemovedPosts.swift
@@ -1,3 +1,23 @@
 final class ReaderSaveForLaterRemovedPosts {
+    private var removedPosts: [ReaderPost]
 
+    init() {
+        removedPosts = []
+    }
+
+    func add(_ post: ReaderPost) {
+        removedPosts.append(post)
+    }
+
+    func remove(_ post: ReaderPost) {
+        guard let index = removedPosts.index(of: post) else {
+            return
+        }
+
+        removedPosts.remove(at: index)
+    }
+
+    func contains(_ post: ReaderPost) -> Bool {
+        return removedPosts.contains(post)
+    }
 }
