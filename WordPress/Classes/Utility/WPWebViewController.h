635ce0d34f3eda627aeb09373dd4dfc3066e2555
Stephenie Harris <scoutharris@gmail.com>
2018-06-07 12:14:03 -0600

    Adding hide masterbar option for older versions of iOS.

--- WordPress/Classes/Utility/WPWebViewController.h
+++ WordPress/Classes/Utility/WPWebViewController.h
@@ -33,6 +33,11 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @property (nonatomic, assign) BOOL addsWPComReferrer;
 
+/**
+ *  @brief  When true adds parameters to hide the wordpress.com masterbar.
+ */
+@property (nonatomic, assign) BOOL addsHideDotComMasterbarParameters;
+
 @property (nonatomic, strong, nullable) WebViewAuthenticator *authenticator;
 
 /**
