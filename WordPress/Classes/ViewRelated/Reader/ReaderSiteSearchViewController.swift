919ac6170d1b00ffe2d73833c483736cdce77d18
James Frost <james.frost@automattic.com>
2018-06-07 23:20:28 +0100

    Reader Site Search: Add No Results view

--- WordPress/Classes/ViewRelated/Reader/ReaderSiteSearchViewController.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderSiteSearchViewController.swift
@@ -11,13 +11,25 @@ class ReaderSiteSearchViewController: UITableViewController {
         return tableHandler
     }()
 
-    var feeds: [ReaderFeed] = []
+    var feeds: [ReaderFeed] = [] {
+        didSet {
+            reloadData()
+        }
+    }
 
     var searchTerm: String? = nil {
         didSet {
+            feeds = []
+
             performSearch(for: searchTerm)
         }
     }
+
+    let statusView: WPNoResultsView = {
+        let view = WPNoResultsView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
 
     // MARK: - View lifecycle
 
@@ -36,6 +48,8 @@ class ReaderSiteSearchViewController: UITableViewController {
 
         tableView.register(WPBlogTableViewCell.self, forCellReuseIdentifier: WPBlogTableViewCell.reuseIdentifier())
         tableView.tableFooterView = UIView()
+
+        view.backgroundColor = .clear
     }
 
     // MARK: - Actions
@@ -46,15 +60,70 @@ class ReaderSiteSearchViewController: UITableViewController {
                 return
         }
 
+        showLoadingView()
+
         let context = ContextManager.sharedInstance().mainContext
         let service = ReaderSiteSearchService(managedObjectContext: context)
         service.performSearch(withQuery: term,
                               page: 0,
                               success: { [weak self] (feeds, _, _) in
             self?.feeds = feeds
-            self?.tableView.reloadData()
-            }, failure: { _ in
+            self?.reloadData()
+            }, failure: { [weak self] _ in
+                self?.showLoadingFailedView()
         })
+    }
+
+    private func reloadData() {
+        tableView.reloadData()
+
+        if feeds.count == 0 {
+            showNoResultsView()
+        } else {
+            hideStatusView()
+        }
+    }
+
+    // MARK: - Status View
+
+    private func showNoResultsView() {
+        statusView.titleText = NSLocalizedString("No sites found", comment: "A message title")
+
+        let localizedMessageText = NSLocalizedString("No sites found matching %@ in your language.", comment: "Message shown when the reader finds no sites for the specified search phrase. The %@ is a placeholder for the search phrase.")
+        statusView.messageText = NSString(format: localizedMessageText as NSString, searchTerm ?? "") as String
+
+        statusView.accessoryView = nil
+        statusView.buttonTitle = nil
+
+        showStatusView()
+    }
+
+    private func showLoadingView() {
+        statusView.titleText = NSLocalizedString("Fetching sites...", comment: "A brief prompt when the user is searching for sites in the Reader.")
+        statusView.messageText = ""
+        statusView.buttonTitle = nil
+
+        let boxView = WPAnimatedBox()
+        statusView.accessoryView = boxView
+        showStatusView()
+        boxView.animate(afterDelay: 0.3)
+    }
+
+    private func showLoadingFailedView() {
+        statusView.titleText = NSLocalizedString("Problem loading sites", comment: "Error message title informing the user that a search for sites in the Reader could not be loaded.")
+        statusView.messageText = NSLocalizedString("Sorry. Your search results could not be loaded.", comment: "A short error message leting the user know the requested search could not be performed.")
+        showStatusView()
+    }
+
+    private func showStatusView() {
+        if !statusView.isDescendant(of: tableView) {
+            tableView.addSubview(withFadeAnimation: statusView)
+            tableView.pinSubviewAtCenter(statusView)
+        }
+    }
+
+    private func hideStatusView() {
+        statusView.removeFromSuperview()
     }
 
     // MARK: - UITableViewDataSource
@@ -80,7 +149,8 @@ class ReaderSiteSearchViewController: UITableViewController {
         WPStyleGuide.configureTableViewBlogCell(cell)
 
         cell.textLabel?.text = feed.title
-        cell.detailTextLabel?.text = feed.url.absoluteString
+        cell.detailTextLabel?.text =  feed.urlForDisplay
+
         cell.accessoryType = .disclosureIndicator
 
         if let blavatarURL = feed.blavatarURL {
@@ -111,5 +181,21 @@ class ReaderSiteSearchViewController: UITableViewController {
         }
 
         return nil
+    }
+}
+
+extension ReaderFeed {
+    var urlForDisplay: String {
+        guard let components = URLComponents(url: url, resolvingAgainstBaseURL: false),
+            let host = components.host else {
+            return url.absoluteString
+        }
+
+        let path = components.path
+        if path.isEmpty && path != "/" {
+            return host + path
+        } else {
+            return host
+        }
     }
 }
