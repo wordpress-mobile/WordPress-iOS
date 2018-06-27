531b004e7dbb9c0004990a020ed23d0373d7b8bc
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-06-27 12:54:28 +0800

    Encapsulate comment approval

--- WordPress/Classes/Models/Notifications/Actions/ApproveComment.swift
+++ WordPress/Classes/Models/Notifications/Actions/ApproveComment.swift
@@ -1,6 +1,11 @@
 import MGSwipeTableCell
 
 final class ApproveComment: DefaultNotificationAction {
+    private enum TitleStrings {
+        static let approve = NSLocalizedString("Approve", comment: "Approves a Comment")
+        static let unapprove = NSLocalizedString("Unapprove", comment: "Unapproves a Comment")
+    }
+
     let approveIcon: UIButton = {
         let title = NSLocalizedString("Approve", comment: "Approves a Comment")
         return MGSwipeButton(title: title, backgroundColor: WPStyleGuide.wordPressBlue())
@@ -10,7 +15,31 @@ final class ApproveComment: DefaultNotificationAction {
         return approveIcon
     }
 
-    func execute() {
+    func execute(block: NotificationBlock, onCompletion: ((NotificationDeletionRequest) -> Void)? = nil) {
+        if enabled {
+            unApprove(block: block)
+        } else {
+            approve(block: block)
+        }
+    }
 
+    private func unApprove(block: NotificationBlock) {
+        setIconTitle(TitleStrings.unapprove)
+
+        ReachabilityUtils.onAvailableInternetConnectionDo {
+            actionsService?.unapproveCommentWithBlock(block)
+        }
+    }
+
+    private func approve(block: NotificationBlock) {
+        setIconTitle(TitleStrings.approve)
+
+        ReachabilityUtils.onAvailableInternetConnectionDo {
+            actionsService?.approveCommentWithBlock(block)
+        }
+    }
+
+    private func setIconTitle(_ title: String) {
+        icon?.setTitle(title, for: .normal)
     }
 }
