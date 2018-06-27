52f232379719dadf9ca4cbb99f2c9bf7a44bae42
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-06-27 12:37:30 +0800

    Pass callbacks to buttons

--- WordPress/Classes/Models/Notifications/Actions/TrashComment.swift
+++ WordPress/Classes/Models/Notifications/Actions/TrashComment.swift
@@ -3,14 +3,60 @@ import MGSwipeTableCell
 final class TrashComment: DefaultNotificationAction {
     let trashIcon: UIButton = {
         let title = NSLocalizedString("Trash", comment: "Trashes a comment")
-        return MGSwipeButton(title: title, backgroundColor: WPStyleGuide.wordPressBlue())
+        return MGSwipeButton(title: title, backgroundColor: WPStyleGuide.errorRed())
+//        let button = MGSwipeButton(title: title, backgroundColor: WPStyleGuide.errorRed(), callback: { [weak self] _ in
+//            ReachabilityUtils.onAvailableInternetConnectionDo {
+//                let request = NotificationDeletionRequest(kind: .deletion, action: { [weak self] onCompletion in
+//                    self?.actionsService.deleteCommentWithBlock(block) { success in
+//                        onCompletion(success)
+//                    }
+//                })
+//
+//                self?.showUndeleteForNoteWithID(note.objectID, request: request)
+//            }
+//            return true
+//        })
+//
+//        return button
+//        let button = MGSwipeButton(title: title, backgroundColor: WPStyleGuide.errorRed(), callback: { [weak self] _ in
+//            ReachabilityUtils.onAvailableInternetConnectionDo {
+//                let request = NotificationDeletionRequest(kind: .deletion, action: { [weak self] onCompletion in
+//                    self?.actionsService.deleteCommentWithBlock(block) { success in
+//                        onCompletion(success)
+//                    }
+//                })
+//
+//                self?.showUndeleteForNoteWithID(note.objectID, request: request)
+//            }
+//        })
+//        return button
     }()
 
     override var icon: UIButton? {
         return trashIcon
     }
 
-    func execute() {
+    func execute(block: NotificationBlock, onCompletion: ((NotificationDeletionRequest) -> Void)?) {
+        ReachabilityUtils.onAvailableInternetConnectionDo {
+            let request = NotificationDeletionRequest(kind: .deletion, action: { [weak self] requestCompletion in
+                self?.actionsService?.deleteCommentWithBlock(block, completion: { success in
+                    requestCompletion(success)
+                })
+            })
 
+            onCompletion?(request)
+        }
     }
+
+//    func execute(block: NotificationBlock, compl) {
+//        ReachabilityUtils.onAvailableInternetConnectionDo {
+//            let request = NotificationDeletionRequest(kind: .deletion, action: { [weak self] onCompletion in
+//                self?.actionsService.deleteCommentWithBlock(block) { success in
+//                    onCompletion(success)
+//                }
+//            })
+//
+//            self?.showUndeleteForNoteWithID(note.objectID, request: request)
+//        }
+//    }
 }
