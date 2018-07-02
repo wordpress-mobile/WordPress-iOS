f2bd0de25de8447e5fede806913d4da14d840f1b
etoledom <etoledom@icloud.com>
2018-07-02 09:12:32 -0400

    Fix hound style checks

--- WordPress/Classes/ViewRelated/Notifications/FormattableContent/Styles/SnipetsContentStyles.swift
+++ WordPress/Classes/ViewRelated/Notifications/FormattableContent/Styles/SnipetsContentStyles.swift
@@ -1,12 +1,12 @@
 
 class SnipetsContentStyles: FormattableContentStyles {
-    var attributes: [NSAttributedStringKey : Any] {
+    var attributes: [NSAttributedStringKey: Any] {
         return WPStyleGuide.Notifications.snippetRegularStyle
     }
 
-    var quoteStyles: [NSAttributedStringKey : Any]? = nil
+    var quoteStyles: [NSAttributedStringKey: Any]? = nil
 
-    var rangeStylesMap: [FormattableContentRange.Kind : [NSAttributedStringKey : Any]]? = nil
+    var rangeStylesMap: [FormattableContentRange.Kind: [NSAttributedStringKey: Any]]? = nil
 
     var linksColor: UIColor? = nil
 
