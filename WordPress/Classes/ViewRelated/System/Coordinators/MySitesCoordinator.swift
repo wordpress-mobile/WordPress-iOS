805963bafb2496c1fafd8b537d853e4afe4a9414
James Frost <james.frost@automattic.com>
2018-07-10 12:09:45 +0100

    Universal Links: Add routes for Stats and Stats for a specific blog

---
+++ WordPress/Classes/ViewRelated/System/Coordinators/MySitesCoordinator.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+@objc
+class MySitesCoordinator: NSObject {
+    let mySitesNavigationController: UINavigationController
+    let blogListViewController: BlogListViewController
+
+    @objc
+    init(mySitesNavigationController: UINavigationController,
+         blogListViewController: BlogListViewController) {
+        self.mySitesNavigationController = mySitesNavigationController
+        self.blogListViewController = blogListViewController
+
+        super.init()
+    }
+
+    private func prepareToNavigate() {
+        WPTabBarController.sharedInstance().showMySitesTab()
+
+        mySitesNavigationController.viewControllers = [blogListViewController]
+    }
+
+    func showBlogDetails(for blog: Blog) {
+        prepareToNavigate()
+
+        blogListViewController.setSelectedBlog(blog, animated: false)
+    }
+
+    func showStats(for blog: Blog) {
+        showBlogDetails(for: blog)
+
+        if let blogDetailsViewController = mySitesNavigationController.topViewController as? BlogDetailsViewController {
+            blogDetailsViewController.showDetailView(for: .stats)
+        }
+    }
+}
