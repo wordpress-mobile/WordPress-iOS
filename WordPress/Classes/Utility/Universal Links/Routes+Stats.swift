88db50748fdac3792e861bb97b7b73b94ceeaf59
James Frost <james.frost@automattic.com>
2018-07-11 12:34:06 +0100

    Universal Links: Add day/week/month/year/insights Stats routes

--- WordPress/Classes/Utility/Universal Links/Routes+Stats.swift
+++ WordPress/Classes/Utility/Universal Links/Routes+Stats.swift
@@ -1,8 +1,14 @@
 import Foundation
+import WordPressComStatsiOS
 
 enum StatsRoute {
     case root
     case site
+    case daySite
+    case weekSite
+    case monthSite
+    case yearSite
+    case insights
     case activityLog
 }
 
@@ -17,6 +23,16 @@ extension StatsRoute: Route {
             return "/stats"
         case .site:
             return "/stats/:domain"
+        case .daySite:
+            return "/stats/day/:domain"
+        case .weekSite:
+            return "/stats/week/:domain"
+        case .monthSite:
+            return "/stats/month/:domain"
+        case .yearSite:
+            return "/stats/year/:domain"
+        case .insights:
+            return "/stats/insights/:domain"
         case .activityLog:
             return "/stats/activity/:domain"
         }
@@ -37,6 +53,31 @@ extension StatsRoute: NavigationAction {
         case .site:
             if let blog = blog(from: values) {
                 coordinator.showStats(for: blog)
+            }
+        case .daySite:
+            if let blog = blog(from: values) {
+                coordinator.showStats(for: blog,
+                                      timePeriod: StatsPeriodType.days)
+            }
+        case .weekSite:
+            if let blog = blog(from: values) {
+                coordinator.showStats(for: blog,
+                                      timePeriod: StatsPeriodType.weeks)
+            }
+        case .monthSite:
+            if let blog = blog(from: values) {
+                coordinator.showStats(for: blog,
+                                      timePeriod: StatsPeriodType.months)
+            }
+        case .yearSite:
+            if let blog = blog(from: values) {
+                coordinator.showStats(for: blog,
+                                      timePeriod: StatsPeriodType.years)
+            }
+        case .insights:
+            if let blog = blog(from: values) {
+                coordinator.showStats(for: blog,
+                                      timePeriod: StatsPeriodType.insights)
             }
         case .activityLog:
             if let blog = blog(from: values) {
