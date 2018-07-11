69732d37f4ca1d7cc2b33fbba5dd570c1afcbade
etoledom <etoledom@icloud.com>
2018-07-11 17:10:31 -0400

    Modified FormatteableContent to insolate it from the ranges construction

--- WordPress/Classes/ViewRelated/Notifications/FormattableContent/NotificationContentFactory.swift
+++ WordPress/Classes/ViewRelated/Notifications/FormattableContent/NotificationContentFactory.swift
@@ -2,6 +2,7 @@
 private enum Constants {
     static let Actions      = "actions"
     static let RawType      = "type"
+    static let Ranges       = "ranges"
 }
 
 extension Notification {
@@ -15,29 +16,37 @@ extension Notification {
 
 class NotificationContentFactory: FormattableContentFactory {
     static func content(from blocks: [[String: AnyObject]], actionsParser parser: FormattableContentActionParser, parent: FormattableContentParent) -> [FormattableContent] {
+
         return blocks.compactMap { rawBlock in
             let actions = parser.parse(rawBlock[Constants.Actions] as? [String: AnyObject])
+            let ranges = rangesFrom(rawBlock)
             guard let type = rawBlock[Constants.RawType] as? String else {
-                return NotificationTextContent(dictionary: rawBlock, actions: actions, parent: parent)
+                return NotificationTextContent(dictionary: rawBlock, actions: actions, ranges: ranges, parent: parent)
             }
-            return content(for: type, with: rawBlock, actions: actions, parent: parent)
+            return content(for: type, with: rawBlock, actions: actions, ranges: ranges, parent: parent)
         }
     }
 
-    private static func content(for type: String, with rawBlock: [String: AnyObject], actions: [FormattableContentAction], parent: FormattableContentParent) -> FormattableContent? {
+    private static func rangesFrom(_ rawBlock: [String: AnyObject]) -> [FormattableContentRange] {
+        let rawRanges   = rawBlock[Constants.Ranges] as? [[String: AnyObject]]
+        let parsed = rawRanges?.compactMap(NotificationContentRangeFactory.contentRange)
+        return parsed ?? []
+    }
+
+    private static func content(for type: String, with rawBlock: [String: AnyObject], actions: [FormattableContentAction], ranges: [FormattableContentRange], parent: FormattableContentParent) -> FormattableContent? {
         guard let type = Notification.ContentType(rawValue: type) else {
-            return NotificationTextContent(dictionary: rawBlock, actions: actions, parent: parent)
+            return NotificationTextContent(dictionary: rawBlock, actions: actions, ranges: ranges, parent: parent)
         }
 
         switch type {
         case .comment:
-            return FormattableCommentContent(dictionary: rawBlock, actions: actions, parent: parent)
+            return FormattableCommentContent(dictionary: rawBlock, actions: actions, ranges: ranges, parent: parent)
         case .user:
-            return FormattableUserContent(dictionary: rawBlock, actions: actions, parent: parent)
+            return FormattableUserContent(dictionary: rawBlock, actions: actions, ranges: ranges, parent: parent)
         case .text:
-            return NotificationTextContent(dictionary: rawBlock, actions: actions, parent: parent)
+            return NotificationTextContent(dictionary: rawBlock, actions: actions, ranges: ranges, parent: parent)
         case .image:
-            return NotificationTextContent(dictionary: rawBlock, actions: actions, parent: parent)
+            return NotificationTextContent(dictionary: rawBlock, actions: actions, ranges: ranges, parent: parent)
         }
     }
 }
