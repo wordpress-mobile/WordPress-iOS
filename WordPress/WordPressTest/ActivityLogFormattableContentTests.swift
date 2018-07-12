ade25f02bde84bc0083ee71c61afc02f496093e2
etoledom <etoledom@icloud.com>
2018-07-12 17:23:29 -0400

    FormattableContentFactory: Code organization

--- WordPress/WordPressTest/ActivityLogFormattableContentTests.swift
+++ WordPress/WordPressTest/ActivityLogFormattableContentTests.swift
@@ -18,20 +18,7 @@ class MockActivityParent: FormattableContentParent {
 
 final class ActivityLogFormattableContentTests: XCTestCase {
 
-    let testPostID = 347
-    let testSiteID = 137726971
-    var testPostURL: String {
-        return "https://wordpress.com/read/blogs/\(testSiteID)/posts/\(testPostID)"
-    }
-    let pingbackText = "Pingback to Camino a Machu Picchu from Tren de Machu Picchu a Cusco – eToledo"
-    let postText = "Tren de Machu Picchu a Cusco"
-    let commentText = "Comment by levitoledo on Hola Lima! 🇵🇪: Great post! True talent!"
-    let themeText = "Spatial"
-    let settingsText = "Default post category changed from \"subcategory\" to \"viajes\""
-    let siteText = "Atomic"
-    let pluginText = "WP Job Manager 1.31.1"
-
-    let activityLogJSON = ActivityLogJSON()
+    let testData = ActivityLogTestData()
     let parent = MockActivityParent()
     let actionsParser = ActivityActionsParser()
 
@@ -44,7 +31,7 @@ final class ActivityLogFormattableContentTests: XCTestCase {
     }
 
     func testPingbackContentIsParsedCorrectly() {
-        let dictionary = activityLogJSON.getPingbackDictionary()
+        let dictionary = testData.getPingbackDictionary()
 
         let pingbackContent = ActivityFormattableContentFactory.content(from: [dictionary], actionsParser: actionsParser, parent: parent)
 
@@ -52,165 +39,89 @@ final class ActivityLogFormattableContentTests: XCTestCase {
 
         let pingback = pingbackContent[0]
 
-        XCTAssertEqual(pingback.text, pingbackText)
+        XCTAssertEqual(pingback.text, testData.pingbackText)
         XCTAssertEqual(pingback.ranges.count, 2)
         XCTAssertEqual(pingback.ranges.first?.kind, .default)
         XCTAssertEqual(pingback.ranges.last?.kind, .post)
     }
 
     func testPostContentIsParsedCorrectly() {
-        let dictionary = activityLogJSON.getPostContentDictionary()
+        let dictionary = testData.getPostContentDictionary()
         let postContent = ActivityFormattableContentFactory.content(from: [dictionary], actionsParser: actionsParser, parent: parent)
 
         XCTAssertEqual(postContent.count, 1)
 
         let post = postContent[0]
 
-        XCTAssertEqual(post.text, postText)
+        XCTAssertEqual(post.text, testData.postText)
         XCTAssertEqual(post.ranges.count, 1)
         XCTAssertEqual(post.ranges.first?.kind, .post)
     }
 
     func testCommentContentIsParsedCorrectly() {
-        let dictionary = activityLogJSON.getCommentContentDictionary()
+        let dictionary = testData.getCommentContentDictionary()
         let commentContent = ActivityFormattableContentFactory.content(from: [dictionary], actionsParser: actionsParser, parent: parent)
 
         XCTAssertEqual(commentContent.count, 1)
 
         let comment = commentContent[0]
 
-        XCTAssertEqual(comment.text, commentText)
+        XCTAssertEqual(comment.text, testData.commentText)
         XCTAssertEqual(comment.ranges.count, 2)
         XCTAssertEqual(comment.ranges.first?.kind, .comment)
         XCTAssertEqual(comment.ranges.last?.kind, .post)
     }
 
     func testThemeContentIsParsedCorrectly() {
-        let dictionary = activityLogJSON.getThemeContentDictionary()
+        let dictionary = testData.getThemeContentDictionary()
         let themeContent = ActivityFormattableContentFactory.content(from: [dictionary], actionsParser: actionsParser, parent: parent)
 
         XCTAssertEqual(themeContent.count, 1)
 
         let theme = themeContent[0]
 
-        XCTAssertEqual(theme.text, themeText)
+        XCTAssertEqual(theme.text, testData.themeText)
         XCTAssertEqual(theme.ranges.count, 1)
         XCTAssertEqual(theme.ranges.first?.kind, .theme)
     }
 
     func testSettingContentIsParsedCorrectly() {
-        let dictionary = activityLogJSON.getSettingsContentDictionary()
+        let dictionary = testData.getSettingsContentDictionary()
         let settingsContent = ActivityFormattableContentFactory.content(from: [dictionary], actionsParser: actionsParser, parent: parent)
 
         XCTAssertEqual(settingsContent.count, 1)
 
         let settings = settingsContent[0]
 
-        XCTAssertEqual(settings.text, settingsText)
+        XCTAssertEqual(settings.text, testData.settingsText)
         XCTAssertEqual(settings.ranges.count, 2)
         XCTAssertEqual(settings.ranges.first?.kind, .italic)
         XCTAssertEqual(settings.ranges.last?.kind, .italic)
     }
 
     func testSiteContentIsParsedCorreclty() {
-        let dictionary = activityLogJSON.getSiteContentDictionary()
+        let dictionary = testData.getSiteContentDictionary()
         let siteContent = ActivityFormattableContentFactory.content(from: [dictionary], actionsParser: actionsParser, parent: parent)
 
         XCTAssertEqual(siteContent.count, 1)
 
         let site = siteContent[0]
 
-        XCTAssertEqual(site.text, siteText)
+        XCTAssertEqual(site.text, testData.siteText)
         XCTAssertEqual(site.ranges.count, 1)
         XCTAssertEqual(site.ranges.first?.kind, .site)
     }
 
     func testPluginContentIsParsedCorreclty() {
-        let dictionary = activityLogJSON.getPluginContentDictionary()
+        let dictionary = testData.getPluginContentDictionary()
         let pluginContent = ActivityFormattableContentFactory.content(from: [dictionary], actionsParser: actionsParser, parent: parent)
 
         XCTAssertEqual(pluginContent.count, 1)
 
         let plugin = pluginContent[0]
 
-        XCTAssertEqual(plugin.text, pluginText)
+        XCTAssertEqual(plugin.text, testData.pluginText)
         XCTAssertEqual(plugin.ranges.count, 1)
         XCTAssertEqual(plugin.ranges.first?.kind, .plugin)
-    }
-}
-
-class ActivityLogJSON {
-
-    let contextManager = TestContextManager()
-
-    private func getDictionaryFromFile(named fileName: String) -> [String : AnyObject] {
-        return contextManager.object(withContentOfFile: fileName) as! [String : AnyObject]
-    }
-
-    func getPingbackDictionary() -> [String: AnyObject] {
-        return getDictionaryFromFile(named: "activity-log-pingback-content.json")
-    }
-
-    func getPostContentDictionary() -> [String: AnyObject] {
-        return getDictionaryFromFile(named: "activity-log-post-content.json")
-    }
-
-    func getCommentContentDictionary() -> [String: AnyObject] {
-        return getDictionaryFromFile(named: "activity-log-comment-content.json")
-    }
-
-    func getThemeContentDictionary() -> [String: AnyObject] {
-        return getDictionaryFromFile(named: "activity-log-theme-content.json")
-    }
-
-    func getSettingsContentDictionary() -> [String: AnyObject] {
-        return getDictionaryFromFile(named: "activity-log-settings-content.json")
-    }
-
-    func getSiteContentDictionary() -> [String: AnyObject] {
-        return getDictionaryFromFile(named: "activity-log-site-content.json")
-    }
-
-    func getPluginContentDictionary() -> [String: AnyObject] {
-        return getDictionaryFromFile(named: "activity-log-plugin-content.json")
-    }
-
-    func getCommentRangeDictionary() -> [String: AnyObject] {
-        let dictionary = getCommentContentDictionary()
-        return getRange(at: 0, from: dictionary)
-    }
-
-    func getPostRangeDictionary() -> [String: AnyObject] {
-        let dictionary = getPostContentDictionary()
-        return getRange(at: 0, from: dictionary)
-    }
-
-    func getThemeRangeDictionary() -> [String: AnyObject] {
-        let dictionary = getThemeContentDictionary()
-        return getRange(at: 0, from: dictionary)
-    }
-
-    func getItalicRangeDictionary() -> [String: AnyObject] {
-        let dictionary = getSettingsContentDictionary()
-        return getRange(at: 0, from: dictionary)
-    }
-
-    func getSiteRangeDictionary() -> [String: AnyObject] {
-        let dictionary = getSiteContentDictionary()
-        return getRange(at: 0, from: dictionary)
-    }
-
-    func getPluginRangeDictionary() -> [String: AnyObject] {
-        let dictionary = getPluginContentDictionary()
-        return getRange(at: 0, from: dictionary)
-    }
-
-    private func getRange(at index: Int, from dictionary: [String: AnyObject]) -> [String: AnyObject] {
-        let ranges = getRanges(from: dictionary)
-        return ranges[index]
-    }
-
-    private func getRanges(from dictionary: [String: AnyObject]) -> [[String: AnyObject]] {
-        return dictionary["ranges"] as! [[String: AnyObject]]
     }
 }
