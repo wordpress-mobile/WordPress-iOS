5b2ddb3e69c23223882cfd8be70f8eb47381d837
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-07-13 10:10:56 +0200

    Unit test TrashCommentAction

--- WordPress/Classes/Models/Notifications/Actions/TrashComment.swift
+++ WordPress/Classes/Models/Notifications/Actions/TrashComment.swift
@@ -1,7 +1,7 @@
 import MGSwipeTableCell
 
 /// Encapsulates logic to trash a comment
-final class TrashComment: DefaultNotificationActionCommand {
+class TrashComment: DefaultNotificationActionCommand {
     static let title = NSLocalizedString("Trash", comment: "Trashes the comment")
     static let hint = NSLocalizedString("Moves the comment to the Trash.", comment: "VoiceOver accessibility hint, informing the user the button can be used to Move a comment to the Trash.")
 
