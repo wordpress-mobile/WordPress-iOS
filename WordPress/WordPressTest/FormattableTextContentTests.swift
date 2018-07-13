fa9ef419dc3825630518b08fe4ea331a6aac4bba
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-07-13 12:11:37 +0200

    Add empty FormattableContentTests

---
+++ WordPress/WordPressTest/FormattableTextContentTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import WordPress
+
+final class FormattableTextContentTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+}
