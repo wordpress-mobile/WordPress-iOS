e68fe041605dc4d82ff310f49543c09f8ce3c992
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-07-13 12:41:40 +0200

    Start testing Activity and Notification content factory

--- WordPress/Classes/ViewRelated/Activity/FormattableContent/Factory/ActivityContentFactory.swift
+++ WordPress/Classes/ViewRelated/Activity/FormattableContent/Factory/ActivityContentFactory.swift
@@ -8,6 +8,8 @@ struct ActivityContentFactory: FormattableContentFactory {
             let rawActions = getRawActions(from: $0)
             let actions = parser.parse(rawActions)
             let ranges = rangesFrom($0)
+            print("rawActions ", rawActions)
+            print("ranges ", ranges)
             return FormattableTextContent(dictionary: $0, actions: actions, ranges: ranges, parent: parent)
         }
     }
