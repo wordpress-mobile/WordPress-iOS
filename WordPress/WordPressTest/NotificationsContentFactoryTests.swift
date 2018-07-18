f79be5dcf810fd2d48b48f7aa02d5a60659ec6bc
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-07-18 12:38:40 +0800

    Test NotificationsContentFactory

--- WordPress/WordPressTest/NotificationsContentFactoryTests.swift
+++ WordPress/WordPressTest/NotificationsContentFactoryTests.swift
@@ -5,14 +5,34 @@ final class NotificationsContentFactoryTests: XCTestCase {
     private let contextManager = TestContextManager()
     private let entityName = Notification.classNameWithoutNamespaces()
 
-    func testTextNotificationReturnsProperImplementationOfFormattableContent() {
-        let subject = NotificationContentFactory.content(from: [mockDictionary()], actionsParser: NotificationActionParser(), parent: loadLikeNotification()).first as? NotificationTextContent
+    func testTextNotificationReturnsExpectedImplementationOfFormattableContent() {
+        let subject = NotificationContentFactory.content(from: [mockTextContentDictionary()], actionsParser: NotificationActionParser(), parent: loadLikeNotification()).first as? NotificationTextContent
 
         XCTAssertNotNil(subject)
     }
 
-    private func mockDictionary() -> [String: AnyObject] {
+    func testCommentNotificationReturnsExpectedImplementationOfFormattableContent() {
+        let subject = NotificationContentFactory.content(from: [mockCommentContentDictionary()], actionsParser: NotificationActionParser(), parent: loadLikeNotification()).first as? FormattableCommentContent
+
+        XCTAssertNotNil(subject)
+    }
+
+    func testUserNotificationReturnsExpectedImplementationOfFormattableContent() {
+        let subject = NotificationContentFactory.content(from: [mockUserContentDictionary()], actionsParser: NotificationActionParser(), parent: loadLikeNotification()).first as? FormattableUserContent
+
+        XCTAssertNotNil(subject)
+    }
+
+    private func mockTextContentDictionary() -> [String: AnyObject] {
         return getDictionaryFromFile(named: "notifications-text-content.json")
+    }
+
+    private func mockCommentContentDictionary() -> [String: AnyObject] {
+        return getDictionaryFromFile(named: "notifications-comment-content.json")
+    }
+
+    private func mockUserContentDictionary() -> [String: AnyObject] {
+        return getDictionaryFromFile(named: "notifications-user-content.json")
     }
 
     private func getDictionaryFromFile(named fileName: String) -> [String: AnyObject] {
