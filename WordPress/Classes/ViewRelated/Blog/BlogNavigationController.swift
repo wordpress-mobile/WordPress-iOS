d501923d4c950eed6abf2fc315147f0b4e820bb7
Nate Heagy <nate@heagy.com>
2018-07-19 14:03:11 -0600

    Refactor NavigationControllerDelegateRepeater to use NSHashTable

--- WordPress/Classes/ViewRelated/Blog/BlogNavigationController.swift
+++ WordPress/Classes/ViewRelated/Blog/BlogNavigationController.swift
@@ -2,28 +2,28 @@ import WordPressFlux
 
 /// Allows the use of multiple UINavigationControllerDelegate by resending each message
 private class NavigationControllerDelegateRepeater: NSObject, UINavigationControllerDelegate {
-    private var delegates: [UINavigationControllerDelegate] = []
+    private var delegates: NSHashTable<UINavigationControllerDelegate> = NSHashTable<UINavigationControllerDelegate>.weakObjects()
 
     func add(delegate: UINavigationControllerDelegate) {
-        delegates.append(delegate)
+        delegates.add(delegate)
     }
 
     // MARK: UINavigationControllerDelegate
 
     func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
-        for delegate in delegates {
+        for delegate in delegates.allObjects {
             delegate.navigationController?(navigationController, willShow: viewController, animated: animated)
         }
     }
 
     func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
-        for delegate in delegates {
+        for delegate in delegates.allObjects {
             delegate.navigationController?(navigationController, didShow: viewController, animated: animated)
         }
     }
 
     func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationControllerOperation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        for delegate in delegates {
+        for delegate in delegates.allObjects {
             if let transitioning = delegate.navigationController?(navigationController, animationControllerFor: operation, from: fromVC, to: toVC) {
                 return transitioning
             }
