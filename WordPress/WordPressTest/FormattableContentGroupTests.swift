c76ac012d401e2c6ff95ab17c8ef1102d1e243ba
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-07-19 11:49:35 +0800

    Unit test FormattableContentGroup

--- WordPress/WordPressTest/FormattableContentGroupTests.swift
+++ WordPress/WordPressTest/FormattableContentGroupTests.swift
@@ -2,14 +2,66 @@ import XCTest
 @testable import WordPress
 
 final class FormattableContentGroupTests: XCTestCase {
+    private let contextManager = TestContextManager()
+    private var subject: FormattableContentGroup?
+
+    private struct Constants {
+        static let kind: FormattableContentGroup.Kind = .activity
+    }
 
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        subject = FormattableContentGroup(blocks: [mockContent()], kind: Constants.kind)
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        subject = nil
         super.tearDown()
+    }
+
+    func testKindRemainsAsInitialised() {
+        XCTAssertEqual(subject?.kind, Constants.kind)
+    }
+
+    func testBlocksRemainAsInitialised() {
+        let groupBlocks = subject?.blocks as? [FormattableTextContent]
+        let mockBlocks = [mockContent()]
+
+        /// Compare by the blocks' text
+        let groupBlocksText = groupBlocks!.map { $0.text }
+        let mockBlocksText = mockBlocks.map { $0.text }
+
+        XCTAssertEqual(groupBlocksText, mockBlocksText)
+    }
+
+    func testBlockOfKindReturnsExpectation() {
+        let obtainedBlock: FormattableTextContent? = subject?.blockOfKind(.text)
+        let obtainedBlockText = obtainedBlock?.text
+
+        let mockText = mockContent().text
+
+        XCTAssertEqual(obtainedBlockText, mockText)
+    }
+
+    func testBlockOfKindReturnsNilWhenNotFound() {
+        let obtainedBlock: FormattableTextContent? = subject?.blockOfKind(.image)
+
+        XCTAssertNil(obtainedBlock)
+    }
+
+    private func mockContent() -> FormattableTextContent {
+        return FormattableTextContent(dictionary: mockActivity(), actions: [], ranges: [], parent: mockParent())
+    }
+
+    private func mockActivity() -> [String: AnyObject] {
+        return getDictionaryFromFile(named: "activity-log-activity-content.json")
+    }
+
+    private func getDictionaryFromFile(named fileName: String) -> [String: AnyObject] {
+        return contextManager.object(withContentOfFile: fileName) as! [String: AnyObject]
+    }
+
+    func mockParent() -> FormattableContentParent {
+        return MockActivityParent()
     }
 }
