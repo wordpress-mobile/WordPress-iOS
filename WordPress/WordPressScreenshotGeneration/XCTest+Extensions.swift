56cb1b0e1465688db7e21b5b1d855f63bc7e807b
James Treanor <jtreanor3@gmail.com>
2018-07-20 16:14:35 +0300

    Add some XCTest helpers for the screenshot tests

---
+++ WordPress/WordPressScreenshotGeneration/XCTest+Extensions.swift
@@ -0,0 +1,24 @@
+import XCTest
+
+extension XCTestCase {
+    
+    public func waitForElementToExist(element: XCUIElement, timeout: TimeInterval? = nil) {
+        let timeoutValue = timeout ?? 30
+        guard element.waitForExistence(timeout: timeoutValue) else {
+            XCTFail("Failed to find \(element) after \(timeoutValue) seconds.")
+            return
+        }
+    }
+    
+    public func waitForElementToNotExist(element: XCUIElement, timeout: TimeInterval? = nil) {
+        let notExistsPredicate = NSPredicate(format: "exists == false")
+        let expectation = XCTNSPredicateExpectation(predicate: notExistsPredicate,
+                                                    object: element)
+
+        let timeoutValue = timeout ?? 30
+        guard XCTWaiter().wait(for: [expectation], timeout: timeoutValue) == .completed else {
+            XCTFail("\(element) still exists after \(timeoutValue) seconds.")
+            return
+        }
+    }
+}
