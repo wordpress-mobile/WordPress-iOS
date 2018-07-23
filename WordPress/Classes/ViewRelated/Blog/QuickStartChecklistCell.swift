ed7626095ef5c9e94c345e038dbb5981054b66f3
Nate Heagy <nate@heagy.com>
2018-07-23 16:39:37 -0600

    Add QuickStart checklist screen

--- WordPress/Classes/ViewRelated/Blog/QuickStartChecklistCell.swift
+++ WordPress/Classes/ViewRelated/Blog/QuickStartChecklistCell.swift
@@ -2,4 +2,14 @@ class QuickStartChecklistCell: UITableViewCell {
     @IBOutlet var titleLabel: UILabel?
     @IBOutlet var descriptionLabel: UILabel?
     @IBOutlet var iconView: UIImageView?
+
+    public var tour: QuickStartTour? {
+        didSet {
+            titleLabel?.text = tour?.title
+            descriptionLabel?.text = tour?.description
+            iconView?.image = tour?.icon
+        }
+    }
+
+    static let reuseIdentifier = "QuickStartChecklistCell"
 }
