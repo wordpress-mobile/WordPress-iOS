f5d7837b4330775c21d3b653a67cda0749eed869
Nate Heagy <nate@heagy.com>
2018-07-24 10:49:13 -0600

    Add dotComID arg to QuickStartChecklistDataSource

--- WordPress/Classes/ViewRelated/Blog/QuickStartChecklistView.swift
+++ WordPress/Classes/ViewRelated/Blog/QuickStartChecklistView.swift
@@ -1,19 +1,34 @@
 class QuickStartChecklistView: UITableViewController {
-    private let dataSource = QuickStartChecklistDataSource()
+    private var dataSource: QuickStartChecklistDataSource? {
+        didSet {
+            self.tableView?.dataSource = dataSource
+        }
+    }
+    @objc public var dotComID: NSNumber? {
+        didSet {
+            guard let dotComID = dotComID else {
+                return
+            }
+            dataSource = QuickStartChecklistDataSource(blogID: dotComID.intValue)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let tableView = UITableView(frame: .zero)
-        tableView.dataSource = dataSource
         self.tableView = tableView
-        
+
         let cellNib = UINib(nibName: "QuickStartChecklistCell", bundle: Bundle(for: QuickStartChecklistCell.self))
         tableView.register(cellNib, forCellReuseIdentifier: QuickStartChecklistCell.reuseIdentifier)
     }
 }
 
 private class QuickStartChecklistDataSource: NSObject, UITableViewDataSource {
+    init(blogID: Int) {
+        // load storage here
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return QuickStartTourGuide.checklistTours.count
     }
