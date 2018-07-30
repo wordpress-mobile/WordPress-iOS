4bbbd833c9a593462cce552017980cf9f6efd1e6
stevebaranski <steven.baranski@icloud.com>
2018-07-30 11:20:07 -0700

    Addressed Hound warnings re: trailing whitespace

--- WordPress/WordPressNotificationServiceExtension/Sources/NotificationService.swift
+++ WordPress/WordPressNotificationServiceExtension/Sources/NotificationService.swift
@@ -8,15 +8,15 @@ class NotificationService: UNNotificationServiceExtension {
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
         bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        
+
         if let bestAttemptContent = bestAttemptContent {
             // Modify the notification content here...
             bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
+
             contentHandler(bestAttemptContent)
         }
     }
-    
+
     override func serviceExtensionTimeWillExpire() {
         // Called just before the extension will be terminated by the system.
         // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
