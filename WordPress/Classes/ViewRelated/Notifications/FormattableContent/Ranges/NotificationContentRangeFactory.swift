f3748f826e1f5a7799b233221b3fce27afa17344
etoledom <etoledom@icloud.com>
2018-07-31 13:37:04 -0400

    FormattableRangesFactory moved to FormattableContent folder

--- WordPress/Classes/ViewRelated/Notifications/FormattableContent/Ranges/NotificationContentRangeFactory.swift
+++ WordPress/Classes/ViewRelated/Notifications/FormattableContent/Ranges/NotificationContentRangeFactory.swift
@@ -1,34 +1,5 @@
 
-protocol ContentRangeFactory {
-    static func contentRange(from dictionary: [String: AnyObject]) -> FormattableContentRange?
-}
-
-extension ContentRangeFactory {
-    static func rangeFrom(_ dictionary: [String: AnyObject]) -> NSRange? {
-        guard let indices = dictionary[RangeKeys.indices] as? [Int],
-            let start = indices.first,
-            let end = indices.last else {
-                return nil
-        }
-        return NSMakeRange(start, end - start)
-    }
-
-    static func kindString(from dictionary: [String: AnyObject]) -> String? {
-        return dictionary[RangeKeys.rawType] as? String
-    }
-}
-
-private enum RangeKeys {
-    static let rawType = "type"
-    static let url = "url"
-    static let indices = "indices"
-    static let id = "id"
-    static let value = "value"
-    static let siteId = "site_id"
-    static let postId = "post_id"
-}
-
-struct NotificationContentRangeFactory: ContentRangeFactory {
+struct NotificationContentRangeFactory: FormattableRangesFactory {
     static func contentRange(from dictionary: [String: AnyObject]) -> FormattableContentRange? {
         guard let range = rangeFrom(dictionary) else {
             return nil
