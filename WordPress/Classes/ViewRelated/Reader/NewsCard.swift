65719c35950dd31da5310767897e56e9f10b24a0
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-08-02 15:33:39 +0800

    And inject the manager into the card

--- WordPress/Classes/ViewRelated/Reader/NewsCard.swift
+++ WordPress/Classes/ViewRelated/Reader/NewsCard.swift
@@ -2,6 +2,16 @@ import UIKit
 
 /// UI of the New Card
 final class NewsCard: UIViewController {
+    private let manager: NewsManager
+
+    init(manager: NewsManager) {
+        self.manager = manager
+        super.init(nibName: "NewsCard", bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
