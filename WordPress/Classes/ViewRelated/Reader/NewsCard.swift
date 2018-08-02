b96cd6025586e415b1cc154dd53a0f7617292f8a
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-08-02 14:07:30 +0800

    Add New Card

---
+++ WordPress/Classes/ViewRelated/Reader/NewsCard.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+/// UI of the New Card
+final class NewsCard: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
