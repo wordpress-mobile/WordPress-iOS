d012f0cd7e38e58a8c564774bcaf8a9189e4eb34
Thuy.Copeland <thuycopeland@gmail.com>
2018-08-03 13:49:37 -0500

    Protect against out-of-bound ranges occurring due to the text translation

--- WordPress/Classes/ViewRelated/Notifications/FormattableContent/FooterTextContent.swift
+++ WordPress/Classes/ViewRelated/Notifications/FormattableContent/FooterTextContent.swift
@@ -5,8 +5,26 @@ class FooterTextContent: FormattableTextContent {
         if text == "You replied to this comment." {
             let localizedText = NSLocalizedString(text, comment: "Notification text - below a comment notification detail")
 
-            // FIXME: Now that we have localized the text, the clickable link range for NSAttributedString will be wrong.
-            super.init(text: localizedText, ranges: ranges, actions: actions)
+            /// Now that we have localized the text, the clickable link range is not accurate
+            /// and could cause an out-of-bounds error. Recalculate the range as the entire phrase.
+            ///
+            var modifiedRanges = [FormattableContentRange]()
+            if let firstRange = ranges.first {
+                modifiedRanges.append(firstRange)
+            }
+
+            if let secondItem = ranges[1] as? NotificationCommentRange {
+                let modifiedSecondRange = secondItem.rangeShifted(by: -secondItem.range.location)
+                var properties = NotificationContentRange.Properties(range: modifiedSecondRange)
+                    properties.url = secondItem.url
+                    properties.siteID = secondItem.siteID
+                    properties.userID = secondItem.userID
+                    properties.postID = secondItem.postID
+
+                let secondRange = NotificationContentRange(kind: .comment, properties: properties)
+                modifiedRanges.append(secondRange)
+            }
+            super.init(text: localizedText, ranges: modifiedRanges, actions: actions)
         } else {
             super.init(text: text, ranges: ranges, actions: actions)
         }
