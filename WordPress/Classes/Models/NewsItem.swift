7b46b139912964b8f382beee91a7d6a26c72794b
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-08-03 09:53:54 +0800

    Update NewsItem object with a title, content and url

--- WordPress/Classes/Models/NewsItem.swift
+++ WordPress/Classes/Models/NewsItem.swift
@@ -1,4 +1,6 @@
 /// Encapsulates the content of the message to be presented in the "New" Card
 struct NewsItem {
+    let title: String
     let content: String
+    let extendedInfoURL: URL
 }
--- WordPress/WordPressTest/NewsItemTests.swift
+++ WordPress/WordPressTest/NewsItemTests.swift
@@ -3,14 +3,16 @@ import XCTest
 
 final class NewsItemTests: XCTestCase {
     private struct Constants {
-        static let message = "🦄"
+        static let title = "🦄"
+        static let content = "🦄"
+        static let infoURL = URL(string: "https://wordpress.com")!
     }
 
     private var subject: NewsItem?
 
     override func setUp() {
         super.setUp()
-        subject = NewsItem(content: Constants.message)
+        subject = NewsItem(title: Constants.title, content: Constants.content, extendedInfoURL: Constants.infoURL)
     }
 
     override func tearDown() {
@@ -18,7 +20,15 @@ final class NewsItemTests: XCTestCase {
         super.tearDown()
     }
 
-    func testMessageMatchesExpectation() {
-        XCTAssertEqual(subject?.content, Constants.message)
+    func testTitleMatchesExpectation() {
+        XCTAssertEqual(subject?.title, Constants.title)
+    }
+
+    func testContentMatchesExpectation() {
+        XCTAssertEqual(subject?.content, Constants.content)
+    }
+
+    func testURLMatchesExpectation() {
+        XCTAssertEqual(subject?.extendedInfoURL, Constants.infoURL)
     }
 }
