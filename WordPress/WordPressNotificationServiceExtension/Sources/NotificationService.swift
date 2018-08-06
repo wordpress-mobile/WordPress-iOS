8394b5b115454a411c170f4703c8ea0123ec833e
stevebaranski <steven.baranski@icloud.com>
2018-08-06 14:56:38 -0700

    Service extension now retrieves remote notification details

--- WordPress/WordPressNotificationServiceExtension/Sources/NotificationService.swift
+++ WordPress/WordPressNotificationServiceExtension/Sources/NotificationService.swift
@@ -1,27 +1,100 @@
 import UserNotifications
 
+import WordPressKit
+
 class NotificationService: UNNotificationServiceExtension {
 
-    var contentHandler: ((UNNotificationContent) -> Void)?
-    var bestAttemptContent: UNMutableNotificationContent?
+    // MARK: Properties
+
+    private var notificationService: NotificationSyncServiceRemote?
+
+    private var contentHandler: ((UNNotificationContent) -> Void)?
+    private var bestAttemptContent: UNMutableNotificationContent?
+
+    // MARK: UNNotificationServiceExtension
 
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        self.bestAttemptContent = request.content.mutableCopy() as? UNMutableNotificationContent
 
-        if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
+        guard
+            let notificationContent = self.bestAttemptContent,
+            let noteID = notificationContent.userInfo["note_id"] as? Int,
+            let token = readExtensionToken()
+        else
+        {
+            contentHandler(request.content)
+            return
+        }
 
-            contentHandler(bestAttemptContent)
+        let api = WordPressComRestApi(oAuthToken: token)
+        let service = NotificationSyncServiceRemote(wordPressComRestApi: api)
+        self.notificationService = service
+
+        let identifiers = [ String(noteID) ]
+        service.loadNotes(noteIds: identifiers) { error, notifications in
+            defer {
+                contentHandler(notificationContent)
+            }
+
+            if let error = error {
+                debugPrint("Unable to retrieve notifications for Note ID : \(noteID) | Error : \(error.localizedDescription)")
+                return
+            }
+
+            if let remoteNotifications = notifications,
+                remoteNotifications.count == 1,
+                let notification = remoteNotifications.first
+            {
+                let identifier = notification.notificationId
+                if
+                    let title = notification.title,
+                    let type = notification.type
+                {
+                    notificationContent.title = "\(title) (\(identifier)) | \(type)"
+                }
+
+                if let subtitle = notification.timestamp {
+                    notificationContent.subtitle = subtitle
+                }
+
+                if let body = notification.url {
+                    notificationContent.body = body
+                }
+
+                if let metadata = notification.meta {
+                    notificationContent.userInfo["meta"] = metadata
+                }
+            }
         }
     }
 
     override func serviceExtensionTimeWillExpire() {
-        // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent = bestAttemptContent {
+        notificationService?.wordPressComRestApi.invalidateAndCancelTasks()
+        
+        if let contentHandler = contentHandler,
+            let bestAttemptContent = bestAttemptContent
+        {
             contentHandler(bestAttemptContent)
         }
+    }
+}
+
+private extension NotificationService {
+    /// Retrieves the WPCOM OAuth Token, meant for Extension usage.
+    ///
+    /// - Returns: the token if found; `nil` otherwise
+    ///
+    func readExtensionToken() -> String? {
+        guard
+            let oauthToken = try? SFHFKeychainUtils.getPasswordForUsername(WPNotificationServiceExtensionKeychainTokenKey,
+                                                                           andServiceName: WPNotificationServiceExtensionKeychainServiceName,
+                                                                           accessGroup: WPAppKeychainAccessGroup)
+            else {
+                debugPrint("Unable to retrieve Notification Service Extension OAuth token")
+                return nil
+        }
+
+        return oauthToken
     }
 }
