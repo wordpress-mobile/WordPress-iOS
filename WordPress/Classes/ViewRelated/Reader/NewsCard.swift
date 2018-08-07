ce49e356a78779ed7039531bc8a568d3363baa0a
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-08-07 13:33:12 +0800

    Add first iteration of the UI

--- WordPress/Classes/ViewRelated/Reader/NewsCard.swift
+++ WordPress/Classes/ViewRelated/Reader/NewsCard.swift
@@ -1,8 +1,33 @@
 import UIKit
 
 /// UI of the New Card
-final class NewsCard: UIViewController {
+final class NewsCard: UIViewController, ReaderStreamHeader {
+    @IBOutlet weak var dismiss: UIButton!
+    @IBOutlet weak var illustration: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var subtitle: UILabel!
+    @IBOutlet weak var action: UILabel!
+
+    var delegate: ReaderStreamHeaderDelegate? {
+        get {
+            return decorated?.delegate
+        }
+        set {
+            decorated?.delegate = delegate
+        }
+    }
+
+    func enableLoggedInFeatures(_ enable: Bool) {
+        decorated?.enableLoggedInFeatures(enable)
+    }
+
+    func configureHeader(_ topic: ReaderAbstractTopic) {
+        decorated?.configureHeader(topic)
+    }
+
     private let manager: NewsManager
+
+    var decorated: ReaderStreamHeader?
 
     init(manager: NewsManager) {
         self.manager = manager
