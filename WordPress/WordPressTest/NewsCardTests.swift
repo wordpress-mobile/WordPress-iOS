2dce57f7185a5dfb38e674505dd55f9011ff7707
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-08-08 13:07:46 +0800

    Uniut test outlets

--- WordPress/WordPressTest/NewsCardTests.swift
+++ WordPress/WordPressTest/NewsCardTests.swift
@@ -4,15 +4,38 @@ import XCTest
 
 final class NewsCardTests: XCTestCase {
     private struct Constants {
-        static let content = "😳"
+        static let title = "😳"
+        static let content = "😳😳"
+        static let url = URL(string: "http://wordpress.com")!
     }
 
+    private class MockNewsManager: NewsManager {
+        var dismissed: Bool = false
+        var cardIsVisible: Bool = true
+
+        func dismiss() {
+            dismissed = true
+        }
+
+        func shouldPresentCard() -> Bool {
+            return cardIsVisible
+        }
+
+        func load(then completion: @escaping (Result<NewsItem>) -> Void) {
+            let newsItem = NewsItem(title: Constants.title, content: Constants.content, extendedInfoURL: Constants.url)
+            let result: Result<NewsItem> = .success(newsItem)
+
+            completion(result)
+        }
+    }
+
+
     private var subject: NewsCard?
-    private var manager: NewsManager?
+    private var manager: MockNewsManager?
 
     override func setUp() {
         super.setUp()
-        manager = DefaultNewsManager(service: LocalNewsService(fileName: Constants.content))
+        manager = MockNewsManager()
         subject = NewsCard(manager: manager!)
         let _ = subject?.view
     }
@@ -25,5 +48,19 @@ final class NewsCardTests: XCTestCase {
 
     func testViewHasTheExpectedBackgroundColor() {
         XCTAssertEqual(subject?.view.backgroundColor, WPStyleGuide.greyLighten30())
+    }
+
+    func testNewsTitleContainsTheExpectedText() {
+        XCTAssertEqual(subject?.newsTitle.text, Constants.title)
+    }
+
+    func testNewsSubtitleContainsTheExpectedText() {
+        XCTAssertEqual(subject?.newsSubtitle.text, Constants.content)
+    }
+
+    func testTappingDismissButtonCallsDismissInManager() {
+        subject?.dismiss.sendActions(for: .touchUpInside)
+
+        XCTAssertTrue(manager!.dismissed)
     }
 }
