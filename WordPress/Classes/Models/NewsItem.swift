d824ef6f68038323fa33b15a82e0ae64734bdd76
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-08-09 12:14:00 +0800

    Add a version property and tests

--- WordPress/Classes/Models/NewsItem.swift
+++ WordPress/Classes/Models/NewsItem.swift
@@ -3,6 +3,7 @@ struct NewsItem {
     let title: String
     let content: String
     let extendedInfoURL: URL
+    let version: Decimal
 }
 
 extension NewsItem {
@@ -10,17 +11,20 @@ extension NewsItem {
         static let title = "Title"
         static let content = "Content"
         static let URL = "URL"
+        static let version = "version"
     }
 
     init?(fileContent: [String: String]) {
         guard let title = fileContent[FileKeys.title],
             let content = fileContent[FileKeys.content],
             let urlString = fileContent[FileKeys.URL],
-            let url = URL(string: urlString) else {
+            let url = URL(string: urlString),
+            let versionString = fileContent[FileKeys.version],
+            let version = Decimal(string: versionString) else {
                 return nil
         }
 
-        self.init(title: title, content: content, extendedInfoURL: url)
+        self.init(title: title, content: content, extendedInfoURL: url, version: version)
     }
 }
 
