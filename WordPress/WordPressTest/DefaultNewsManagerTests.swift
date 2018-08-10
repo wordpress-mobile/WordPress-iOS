3116fea1ee4e0a91ccf77a44371edb1390942fc6
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-08-10 12:52:12 +0800

    Unit test all the logic

--- WordPress/WordPressTest/DefaultNewsManagerTests.swift
+++ WordPress/WordPressTest/DefaultNewsManagerTests.swift
@@ -5,20 +5,59 @@ final class DefaultNewsManagerTests: XCTestCase {
     private struct Constants {
         static let title = "This is an awesome new feature!"
         static let contextId = "context"
+        static let buildNumber = "10.6"
+        static let cardBuildNumber = "10.6"
+        static let previousCardBuildNumber = "10.5"
+        static let cardTitle = "Hello"
+        static let cardContent = "How is your day going so far?"
+        static let cardURL = URL(string: "http://wordpress.com")!
+    }
+
+    private final class MockNewsService: NewsService {
+        func load(then completion: @escaping (Result<NewsItem>) -> Void) {
+            let newsItem = NewsItem(title: Constants.title,
+                                    content: Constants.contextId,
+                                    extendedInfoURL: Constants.cardURL,
+                                    version: Decimal(string: Constants.cardBuildNumber)!)
+
+            let result: Result<NewsItem> = .success(newsItem)
+            completion(result)
+        }
+    }
+
+    private final class MockInMemoryDefaults: NullMockUserDefaults {
+        var previousVersion: Decimal?
+        var previousContext: String?
+        override func object(forKey defaultName: String) -> Any? {
+            if defaultName == DefaultNewsManager.DatabaseKeys.lastDismissedCardVersion {
+                return previousVersion
+            } else if defaultName == DefaultNewsManager.DatabaseKeys.cardContainerIdentifier {
+                return previousContext
+            } else {
+                return nil
+            }
+        }
+
+        override func set(_ value: Any?, forKey defaultName: String) {
+            //
+        }
     }
 
     private var manager: NewsManager?
     private var service: NewsService?
+    private var database: MockInMemoryDefaults?
 
     override func setUp() {
         super.setUp()
-        service = LocalNewsService(fileName: "News")
-        manager = DefaultNewsManager(service: service!, database: NullMockUserDefaults())
+        service = MockNewsService()
+        database = MockInMemoryDefaults()
+        manager = DefaultNewsManager(service: service!, database: database!)
     }
 
     override func tearDown() {
         manager = nil
         service = nil
+        database = nil
         super.tearDown()
     }
 
@@ -33,7 +72,37 @@ final class DefaultNewsManagerTests: XCTestCase {
         })
     }
 
-    func testManagerShouldPresentUI() {
+    func testManagerShouldPresentCardIfContextIsEmptyAndThereIsNoPreviousCard() {
         XCTAssertTrue(manager!.shouldPresentCard(contextId: Identifier.empty()))
+    }
+
+    func testManagerShouldPresentCardIfCardVersionIsGreaterThanPreviousCardVersion() {
+        database?.previousVersion = Decimal(string: Constants.previousCardBuildNumber)
+        let contextIdentifier = Identifier.empty()
+
+        XCTAssertTrue(manager!.shouldPresentCard(contextId: contextIdentifier))
+    }
+
+    func testManagerShouldPresentUIIfCardVersionIsEqualToPreviousCardVersionAndIsHasNotBeenDismissed() {
+        let contextIdentifier = Identifier.empty()
+
+        XCTAssertTrue(manager!.shouldPresentCard(contextId: contextIdentifier))
+    }
+
+    func testManagerShouldNotPresentUIWhenIsItNotPresentedInTheRightContext() {
+        database?.previousContext = Constants.contextId
+
+        let newContext = Identifier.empty()
+
+        XCTAssertFalse(manager!.shouldPresentCard(contextId: newContext))
+    }
+
+    func testManagerShouldPresentUIWhenIsItPresentedInTheRightContextAndTheCardVersionIsGreaterThanLastSavedVersion() {
+        database?.previousContext = Constants.contextId
+        database?.previousVersion = Decimal(string: Constants.previousCardBuildNumber)
+
+        let newContext = Identifier(value: Constants.contextId)
+
+        XCTAssertTrue(manager!.shouldPresentCard(contextId: newContext))
     }
 }
