da0494998ea2e38c28b8602157f309c736b8e7f4
stevebaranski <steven.baranski@icloud.com>
2018-08-13 16:18:46 -0700

    Address SwiftLint violation reported by Hound

--- WordPress/WordPressNotificationServiceExtension/Sources/FormattableContent/RemoteNotificationActionParser.swift
+++ WordPress/WordPressNotificationServiceExtension/Sources/FormattableContent/RemoteNotificationActionParser.swift
@@ -2,7 +2,7 @@ import Foundation
 
 /// This class is analagous to `NotificationActionParser`, with extension-specific behavior.
 class RemoteNotificationActionParser: FormattableContentActionParser {
-    func parse(_ dictionary: [String : AnyObject]?) -> [FormattableContentAction] {
+    func parse(_ dictionary: [String: AnyObject]?) -> [FormattableContentAction] {
         return []
     }
 }
