0be9e12d7e07d2043051e71d4711b141d3837ad8
James Frost <james.frost@automattic.com>
2018-08-14 16:16:56 +0100

    Add scaffolding for GiphyPicker and associated classes

---
+++ WordPress/Classes/ViewRelated/Media/Giphy/GiphyStrings.swift
@@ -0,0 +1,7 @@
+/// Extension on String containing the literals for the Giphy feature
+extension String {
+    // MARK: - Entry point: alert controller
+    static var giphy: String {
+        return NSLocalizedString("Giphy", comment: "One of the options when selecting More in the Post Editor's format bar")
+    }
+}
