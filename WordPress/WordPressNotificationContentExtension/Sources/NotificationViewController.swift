0e47499f4a3ca69062b823cb7328e68ed8f611aa
stevebaranski <steven.baranski@icloud.com>
2018-08-16 15:26:13 -0700

    Removed trailing whitespace

--- WordPress/WordPressNotificationContentExtension/Sources/NotificationViewController.swift
+++ WordPress/WordPressNotificationContentExtension/Sources/NotificationViewController.swift
@@ -5,7 +5,7 @@ import UserNotificationsUI
 class NotificationViewController: UIViewController, UNNotificationContentExtension {
 
     @IBOutlet var label: UILabel?
-    
+
     func didReceive(_ notification: UNNotification) {
         self.label?.text = notification.request.content.body
     }
