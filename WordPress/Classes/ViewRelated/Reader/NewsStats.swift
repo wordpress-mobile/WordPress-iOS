7c7d4dabe0443a9ce96a59f22b490c6a60200f96
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-08-20 16:46:26 +0800

    Extract stats tracking to a separate class.

---
+++ WordPress/Classes/ViewRelated/Reader/NewsStats.swift
@@ -0,0 +1,49 @@
+protocol NewsStats {
+    func trackPresented(news: Result<NewsItem>?)
+    func trackDismissed(news: Result<NewsItem>?)
+    func trackRequestedExtendedInfo(news: Result<NewsItem>?)
+}
+
+final class TracksNewsStats: NewsStats {
+    private let origin: String
+
+    enum StatsKeys {
+        static let origin = "origin"
+        static let version = "version"
+    }
+
+    init(origin: String) {
+        self.origin = origin
+    }
+
+    func trackPresented(news: Result<NewsItem>?) {
+        track(event: .newsCardViewed, news: news)
+    }
+
+    func trackDismissed(news: Result<NewsItem>?) {
+        track(event: .newsCardDismissed, news: news)
+    }
+
+    func trackRequestedExtendedInfo(news: Result<NewsItem>?) {
+        track(event: .newsCardRequestedExtendedInfo, news: news)
+    }
+
+    private func eventProperties(version: Decimal) -> [AnyHashable: Any] {
+        return [StatsKeys.origin: origin,
+                StatsKeys.version: version.description]
+    }
+
+    private func track(event: WPAnalyticsStat, news: Result<NewsItem>?) {
+        guard let actualNews = news else {
+            return
+        }
+
+        switch actualNews {
+        case .error:
+            return
+        case .success(let newsItem):
+            WPAppAnalytics.track(event, withProperties: eventProperties(version: newsItem.version))
+        }
+    }
+
+}
