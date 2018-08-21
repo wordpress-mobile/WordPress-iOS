ce99ee21183f937f1f4d9521a3d0b3d9de04e1db
Nate Heagy <nate@heagy.com>
2018-08-21 11:47:01 -0700

    Refactor NoticeStyle to be more useful

--- WordPress/Classes/ViewRelated/Blog/QuickStartTourGuide.swift
+++ WordPress/Classes/ViewRelated/Blog/QuickStartTourGuide.swift
@@ -16,8 +16,8 @@ open class QuickStartTourGuide: NSObject, UINavigationControllerDelegate {
     // MARK: Quick Start methods
     @objc
     func showTestQuickStartNotice() {
-        let exampleLabelStr = QuickStartNoticeView.makeHighlightMessage(base: "Tap %@ to see your checklist", highlight: "Quick Start", icon: Gridicon.iconOfType(.listCheckmark))
-        let notice = Notice(title: "Test Quick Start Notice", style: .quickStart(exampleLabelStr))
+        let exampleMessage = "Tap %@ to see your checklist".highlighting(phrase: "Quick Start", icon: Gridicon.iconOfType(.listCheckmark))
+        let notice = Notice(title: "Test Quick Start Notice", style: QuickStartNoticeStyle(attributedMessage: exampleMessage))
         ActionDispatcher.dispatch(NoticeAction.post(notice))
     }
 
@@ -42,4 +42,52 @@ open class QuickStartTourGuide: NSObject, UINavigationControllerDelegate {
         QuickStartPublishTour(),
         QuickStartFollowTour()
     ]
+}
+
+private extension String {
+    func highlighting(phrase: String, icon: UIImage) -> NSAttributedString {
+        let normalParts = components(separatedBy: "%@")
+        guard normalParts.count > 0 else {
+            // if the provided base doesn't contain %@ then we don't know where to place the highlight
+            return NSAttributedString(string: self)
+        }
+        let resultString = NSMutableAttributedString(string: normalParts[0])
+
+        let font = WPStyleGuide.mediumWeightFont(forStyle: .subheadline)
+
+        let iconAttachment = NSTextAttachment()
+        iconAttachment.image = icon.imageWithTintColor(Constants.highlightColor)
+        iconAttachment.bounds = CGRect(x: 0.0, y: font.descender + Constants.iconOffset, width: Constants.iconSize, height: Constants.iconSize)
+        let iconStr = NSAttributedString(attachment: iconAttachment)
+
+        let highlightStr = NSAttributedString(string: phrase, attributes: [.foregroundColor: Constants.highlightColor, .font: Constants.highlightFont])
+
+        switch UIView.userInterfaceLayoutDirection(for: .unspecified) {
+        case .rightToLeft:
+            resultString.append(highlightStr)
+            resultString.append(NSAttributedString(string: " "))
+            resultString.append(iconStr)
+        default:
+            resultString.append(iconStr)
+            resultString.append(NSAttributedString(string: " "))
+            resultString.append(highlightStr)
+        }
+
+        if normalParts.count > 1 {
+            resultString.append(NSAttributedString(string: normalParts[1]))
+        }
+
+        return resultString
+    }
+
+    private enum Constants {
+        static let iconOffset: CGFloat = 1.0
+        static let iconSize: CGFloat = 16.0
+        static let highlightColor = WPStyleGuide.lightBlue()
+        static var highlightFont: UIFont {
+            get {
+                return WPStyleGuide.fontForTextStyle(.subheadline, fontWeight: .semibold)
+            }
+        }
+    }
 }
