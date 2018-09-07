a5ef5d1e4ed93804d9b10b457df50afb258398fc
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-09-07 08:56:38 +0800

    Activate feature for all builds

--- WordPress/Classes/Utility/BuildInformation/FeatureFlag.swift
+++ WordPress/Classes/Utility/BuildInformation/FeatureFlag.swift
@@ -22,7 +22,7 @@ enum FeatureFlag: Int {
         case .quickStart:
             return BuildConfiguration.current == .localDeveloper
         case .newsCard:
-            return true
+            return BuildConfiguration.current ~= [.localDeveloper, .a8cBranchTest, .a8cPrereleaseTesting]
         case .giphy:
             return BuildConfiguration.current == .localDeveloper
         case .automatedTransfer:
