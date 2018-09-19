9b0dfaa0a65cea4dac9e06abade375543b03bbc5
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-09-19 15:45:24 +0800

    Disable a test

--- WordPress/WordPressTest/NotificationSyncMediatorTests.swift
+++ WordPress/WordPressTest/NotificationSyncMediatorTests.swift
@@ -21,7 +21,7 @@ class NotificationSyncMediatorTests: XCTestCase {
 
     /// Expectation's Timeout
     ///
-    fileprivate let timeout = TimeInterval(40)
+    fileprivate let timeout = TimeInterval(3)
 
 
     // MARK: - Overriden Methods
