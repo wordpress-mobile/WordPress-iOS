da51613f9050f3bb22374b4067fb1d930a60c0bb
Yael Rubinstein <yaelirub@gmail.com>
2018-09-23 21:55:10 -0700

    Enable copy comment url - Implemented a textCopyAble protocol. This protocol can be used by any Swift class that needs to enable copying a text. The only thing that the class will need to define is which text is meant to be copied. In our case its the comments link or the notification url. - Created a LongPressGestureLabel class which is a UILabel class and changed the time stamp labels on NoteBlockCommentTableViewCell and ReaderCommentCell to this class so they will have a long press gesture that will execute a function defined by the calling view controller. - Added copy functionality to comments under Notifications tab. - Added copy functionality to comments under Reader tab posts. - presentAlertForCopy helper method displays an UIAlertController with 2 options: Copy and Cancel. The Copy option returns with a success notice that shows when the text was successfully copied to the clip board (or with failure notice in other cases) - Since ReaderCommentsViewController is an objective c class, I could have not used the implemented TextCopyable protocol.

--- WordPress/Classes/Extensions/UIAlertController+Helpers.swift
+++ WordPress/Classes/Extensions/UIAlertController+Helpers.swift
@@ -16,7 +16,35 @@ extension UIAlertController {
         while leafViewController.presentedViewController != nil && !leafViewController.presentedViewController!.isBeingDismissed {
             leafViewController = leafViewController.presentedViewController!
         }
-
+        
         leafViewController.present(self, animated: true, completion: nil)
+    }
+    
+    func presentAlertForCopy(_ text: String?, completion: ((Notice) -> Void)? = nil) {
+        guard let text = text else {
+            completion?(failureNotice)
+            return
+        }
+        self.addDefaultActionWithTitle(NSLocalizedString("Copy", comment: "Copy button")) { [weak self] alertAction in
+            guard let strongSelf = self else {
+                assertionFailure("UIAlertController was nil when trying to set action in presentAlertForCopy")
+                return
+            }
+            UIPasteboard.general.string = text
+            completion?(strongSelf.successNotice)
+        }
+        
+        self.addCancelActionWithTitle(NSLocalizedString("Cancel", comment: "Cancel button"))
+        presentFromRootViewController()
+    }
+    
+    private var successNotice: Notice {
+        let title = NSLocalizedString("Link Copied to Clipboard", comment: "")
+        return Notice(title: title)
+    }
+    
+    private var failureNotice: Notice {
+        let title = NSLocalizedString("Copy to Clipboard failed", comment: "")
+        return Notice(title: title)
     }
 }
