e01a2202983ab229098ae6c8854fc8b433cdc4d7
Nate Heagy <nate@heagy.com>
2018-09-26 14:53:27 -0700

    Rework the quick start core data model

---
+++ WordPress/Classes/Models/Blog+QuickStart.swift
@@ -0,0 +1,48 @@
+/// This extension contains all the methods for working with QuickStartTourStates
+@nonobjc
+extension Blog {
+    public var completedQuickStartTours: [QuickStartTourState]? {
+        get {
+            return quickStartTours?.filter { $0.completed }
+        }
+    }
+
+    public var skippedQuickStartTours: [QuickStartTourState]? {
+        get {
+            return quickStartTours?.filter { $0.skipped }
+        }
+    }
+
+    public func skipTour(_ tourID: String) {
+        let tourState = findOrCreate(tour: tourID)
+        tourState.skipped = true
+
+        let context = managedObjectContext ?? ContextManager.sharedInstance().mainContext
+        ContextManager.sharedInstance().saveContextAndWait(context)
+    }
+
+    public func completeTour(_ tourID: String) {
+        let tourState = findOrCreate(tour: tourID)
+        tourState.completed = true
+
+        let context = managedObjectContext ?? ContextManager.sharedInstance().mainContext
+        ContextManager.sharedInstance().saveContextAndWait(context)
+    }
+
+    public func tourState(for tourID: String) -> QuickStartTourState? {
+        return quickStartTours?.filter { $0.tourID == tourID }.first
+    }
+
+    func findOrCreate(tour tourID: String) -> QuickStartTourState {
+        guard let existingTourState = tourState(for: tourID) else {
+            let context = managedObjectContext ?? ContextManager.sharedInstance().mainContext
+
+            let newTourState = NSEntityDescription.insertNewObject(forEntityName: QuickStartTourState.entityName(), into: context) as! QuickStartTourState
+            newTourState.blog = self
+            newTourState.tourID = tourID
+            return newTourState
+        }
+
+        return existingTourState
+    }
+}
--- WordPress/Classes/Models/QuickStartCompletedTour.swift
+++
@@ -1,9 +0,0 @@
-@objc(QuickStartCompletedTour)
-open class QuickStartCompletedTour: NSManagedObject {
-    // Relations
-    @NSManaged open var completedBlog: Blog?
-    @NSManaged open var skippedBlog: Blog?
-
-    // Properties
-    @NSManaged open var tourID: String
-}
