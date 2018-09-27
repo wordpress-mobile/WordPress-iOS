b0f470ec55ad8f38e55b5e78c7087e690b68dd91
stevebaranski <steven.baranski@icloud.com>
2018-09-26 20:09:58 -0700

    Addressed existing SwiftLint warnings

--- WordPress/Classes/Utility/Debouncer.swift
+++ WordPress/Classes/Utility/Debouncer.swift
@@ -6,18 +6,18 @@ final class Debouncer {
     var callback: (() -> Void)?
     var delay: Double
     weak var timer: Timer?
-    
+
     init(delay: Double, callback: (() -> Void)? = nil) {
         self.delay = delay
         self.callback = callback
     }
-    
+
     func call() {
         timer?.invalidate()
         let nextTimer = Timer.scheduledTimer(timeInterval: delay, target: self, selector: #selector(Debouncer.fireNow), userInfo: nil, repeats: false)
         timer = nextTimer
     }
-    
+
     @objc func fireNow() {
         self.callback?()
     }
