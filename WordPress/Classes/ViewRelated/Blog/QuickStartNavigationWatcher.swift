5c3b00ed1f948730b81ad7dd83b6bbd23f7606bd
Nate Heagy <nate@heagy.com>
2018-10-05 14:04:44 -0700

    Add theme quick start tour and refactor nav watcher

---
+++ WordPress/Classes/ViewRelated/Blog/QuickStartNavigationWatcher.swift
@@ -0,0 +1,23 @@
+@objc
+class QuickStartNavigationWatcher: NSObject, UINavigationControllerDelegate {
+    public func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
+        guard let tourGuide = QuickStartTourGuide.find() else {
+            return
+        }
+
+        switch viewController {
+        case is QuickStartChecklistViewController:
+            tourGuide.visited(.checklist)
+        case is BlogListViewController:
+            tourGuide.visited(.noSuchElement)
+        case is WPWebViewController:
+            fallthrough
+        case is WebKitViewController:
+            tourGuide.visited(.viewSite)
+        case is ThemeBrowserViewController:
+            tourGuide.visited(.themes)
+        default:
+            break
+        }
+    }
+}
