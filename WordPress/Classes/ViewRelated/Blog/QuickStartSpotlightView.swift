98ee2eb2261374c5d4d8700543ae87b1260f4bf4
Nate Heagy <nate@heagy.com>
2018-10-10 20:42:11 -0700

    Update the quick start two dot animation

--- WordPress/Classes/ViewRelated/Blog/QuickStartSpotlightView.swift
+++ WordPress/Classes/ViewRelated/Blog/QuickStartSpotlightView.swift
@@ -1,8 +1,7 @@
-
 //  Exported from Kite Compositor for Mac 1.9.4
+
 import UIKit
 
-@objc
 class QuickStartSpotlightView: UIView
 {
 
@@ -10,7 +9,7 @@ class QuickStartSpotlightView: UIView
 
     init()
     {
-        super.init(frame: CGRect(x: 0, y: 0, width: 26, height: 26))
+        super.init(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
         self.setupLayers()
     }
 
@@ -26,141 +25,181 @@ class QuickStartSpotlightView: UIView
     {
         // Colors
         //
-        let backgroundColor = UIColor(red: 0.096, green: 0.44875, blue: 0.64, alpha: 1)
+        let backgroundColor = UIColor(red: 0.243137, green: 0.517647, blue: 0.682353, alpha: 1)
         let borderColor = UIColor(red: 0.126, green: 0.588984, blue: 0.84, alpha: 1)
-        let backgroundColor1 = UIColor(red: 0.243137, green: 0.517647, blue: 0.682353, alpha: 1)
+        let backgroundColor1 = UIColor(red: 0.096, green: 0.44875, blue: 0.64, alpha: 1)
 
-        // Layer
+        // Big Circle
         //
-        let layerLayer = CALayer()
-        layerLayer.name = "Layer"
-        layerLayer.bounds = CGRect(x: 0, y: 0, width: 12, height: 12)
-        layerLayer.position = CGPoint(x: 13, y: 13)
-        layerLayer.contentsGravity = kCAGravityCenter
-        layerLayer.backgroundColor = backgroundColor.cgColor
-        layerLayer.cornerRadius = 6
-        layerLayer.borderColor = borderColor.cgColor
-        layerLayer.shadowOffset = CGSize(width: 0, height: 1)
-        layerLayer.fillMode = kCAFillModeForwards
-        layerLayer.sublayerTransform = CATransform3D( m11: -5, m12: -0, m13: -0, m14: -0,
-                                                      m21: -0, m22: 5, m23: -0, m24: -0,
-                                                      m31: -0, m32: -0, m33: 1, m34: -0,
-                                                      m41: 0, m42: 0, m43: 0, m44: 1 )
+        let bigCircleLayer = CALayer()
+        bigCircleLayer.name = "Big Circle"
+        bigCircleLayer.bounds = CGRect(x: 0, y: 0, width: 40, height: 40)
+        bigCircleLayer.position = CGPoint(x: 20, y: 20)
+        bigCircleLayer.contentsGravity = CALayerContentsGravity.center
+        bigCircleLayer.opacity = 0.5
+        bigCircleLayer.backgroundColor = backgroundColor.cgColor
+        bigCircleLayer.cornerRadius = 20
+        bigCircleLayer.borderColor = borderColor.cgColor
+        bigCircleLayer.shadowOffset = CGSize(width: 0, height: 1)
+        bigCircleLayer.allowsEdgeAntialiasing = true
+        bigCircleLayer.allowsGroupOpacity = true
+        bigCircleLayer.fillMode = CAMediaTimingFillMode.forwards
+        bigCircleLayer.transform = CATransform3D( m11: 0, m12: 0, m13: 0, m14: 0,
+                                                  m21: 0, m22: 0, m23: 0, m24: 0,
+                                                  m31: 0, m32: 0, m33: 1, m34: 0,
+                                                  m41: 0, m42: 0, m43: 0, m44: 1 )
+        bigCircleLayer.sublayerTransform = CATransform3D( m11: -5, m12: -0, m13: -0, m14: -0,
+                                                          m21: -0, m22: 5, m23: -0, m24: -0,
+                                                          m31: -0, m32: -0, m33: 1, m34: -0,
+                                                          m41: 0, m42: 0, m43: 0, m44: 1 )
 
-        // Layer Animations
-        //
-
-        // transform.scale.xy
-        //
-        let transformScaleXyAnimation = CABasicAnimation()
-        transformScaleXyAnimation.beginTime = self.layer.convertTime(CACurrentMediaTime(), from: nil) + 0.000001
-        transformScaleXyAnimation.duration = 0.497999
-        transformScaleXyAnimation.fillMode = kCAFillModeForwards
-        transformScaleXyAnimation.isRemovedOnCompletion = false
-        transformScaleXyAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        transformScaleXyAnimation.keyPath = "transform.scale.xy"
-        transformScaleXyAnimation.toValue = 1
-        transformScaleXyAnimation.fromValue = 0
-
-        layerLayer.add(transformScaleXyAnimation, forKey: "transformScaleXyAnimation")
-
-        // opacity
-        //
-        let opacityAnimation = CAKeyframeAnimation()
-        opacityAnimation.beginTime = self.layer.convertTime(CACurrentMediaTime(), from: nil) + 0.498
-        opacityAnimation.duration = 2.002
-        opacityAnimation.repeatCount = 99999
-        opacityAnimation.fillMode = kCAFillModeForwards
-        opacityAnimation.isRemovedOnCompletion = false
-        opacityAnimation.keyPath = "opacity"
-        opacityAnimation.values = [ 1, 1, 0, 0, 1 ]
-        opacityAnimation.keyTimes = [ 0, 0.252831, 0.750944, 0.7385, 1 ]
-        opacityAnimation.calculationMode = kCAAnimationLinear
-
-        layerLayer.add(opacityAnimation, forKey: "opacityAnimation")
-
-        // transform.scale.xy
-        //
-        let transformScaleXyAnimation1 = CAKeyframeAnimation()
-        transformScaleXyAnimation1.beginTime = self.layer.convertTime(CACurrentMediaTime(), from: nil) + 0.498
-        transformScaleXyAnimation1.duration = 2.002
-        transformScaleXyAnimation1.repeatCount = 99999
-        transformScaleXyAnimation1.fillMode = kCAFillModeForwards
-        transformScaleXyAnimation1.isRemovedOnCompletion = false
-        transformScaleXyAnimation1.keyPath = "transform.scale.xy"
-        transformScaleXyAnimation1.values = [ 1, 1, 2, 0, 1 ]
-        transformScaleXyAnimation1.keyTimes = [ 0, 0.252831, 0.7385, 0.750943, 1 ]
-        transformScaleXyAnimation1.calculationMode = kCAAnimationLinear
-
-        layerLayer.add(transformScaleXyAnimation1, forKey: "transformScaleXyAnimation1")
-
-        self.layer.addSublayer(layerLayer)
-
-        // Layer
-        //
-        let layerLayer1 = CALayer()
-        layerLayer1.name = "Layer"
-        layerLayer1.bounds = CGRect(x: 0, y: 0, width: 12, height: 12)
-        layerLayer1.position = CGPoint(x: 13, y: 13)
-        layerLayer1.contentsGravity = kCAGravityCenter
-        layerLayer1.backgroundColor = backgroundColor1.cgColor
-        layerLayer1.cornerRadius = 6
-        layerLayer1.borderColor = borderColor.cgColor
-        layerLayer1.shadowOffset = CGSize(width: 0, height: 1)
-        layerLayer1.fillMode = kCAFillModeForwards
-        layerLayer1.sublayerTransform = CATransform3D( m11: -5, m12: -0, m13: -0, m14: -0,
-                                                       m21: -0, m22: 5, m23: -0, m24: -0,
-                                                       m31: -0, m32: -0, m33: 1, m34: -0,
-                                                       m41: 0, m42: 0, m43: 0, m44: 1 )
-
-        // Layer Animations
+        // Big Circle Animations
         //
 
-        // transform.scale.xy
+        // pop big
         //
-        let transformScaleXyAnimation2 = CAKeyframeAnimation()
-        transformScaleXyAnimation2.beginTime = self.layer.convertTime(CACurrentMediaTime(), from: nil) + 0.998684
-        transformScaleXyAnimation2.duration = 2.001316
-        transformScaleXyAnimation2.repeatCount = 99999
-        transformScaleXyAnimation2.fillMode = kCAFillModeForwards
-        transformScaleXyAnimation2.isRemovedOnCompletion = false
-        transformScaleXyAnimation2.keyPath = "transform.scale.xy"
-        transformScaleXyAnimation2.values = [ 0, 1, 1, 2, 0 ]
-        transformScaleXyAnimation2.keyTimes = [ 0, 0.251187, 0.501023, 0.9858, 1 ]
-        transformScaleXyAnimation2.calculationMode = kCAAnimationLinear
+        let popBigAnimation = CASpringAnimation()
+        popBigAnimation.beginTime = self.layer.convertTime(CACurrentMediaTime(), from: nil) + 0.05
+        popBigAnimation.duration = 0.99321
+        popBigAnimation.fillMode = CAMediaTimingFillMode.forwards
+        popBigAnimation.isRemovedOnCompletion = false
+        popBigAnimation.keyPath = "transform.scale.xy"
+        popBigAnimation.toValue = 1
+        popBigAnimation.stiffness = 200
+        popBigAnimation.damping = 10
+        popBigAnimation.mass = 0.7
+        popBigAnimation.initialVelocity = 4
 
-        layerLayer1.add(transformScaleXyAnimation2, forKey: "transformScaleXyAnimation2")
+        bigCircleLayer.add(popBigAnimation, forKey: "popBigAnimation")
 
-        // transform.scale.xy
+        // Group Animation
         //
-        let transformScaleXyAnimation3 = CABasicAnimation()
-        transformScaleXyAnimation3.beginTime = self.layer.convertTime(CACurrentMediaTime(), from: nil) + 0.000001
-        transformScaleXyAnimation3.duration = 0.998683
-        transformScaleXyAnimation3.fillMode = kCAFillModeForwards
-        transformScaleXyAnimation3.isRemovedOnCompletion = false
-        transformScaleXyAnimation3.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        transformScaleXyAnimation3.keyPath = "transform.scale.xy"
-        transformScaleXyAnimation3.toValue = 0
-        transformScaleXyAnimation3.fromValue = 0
+        let groupAnimationAnimation = CAAnimationGroup()
+        groupAnimationAnimation.beginTime = self.layer.convertTime(CACurrentMediaTime(), from: nil) + 1.04321
+        groupAnimationAnimation.duration = 2
+        groupAnimationAnimation.repeatCount = 9999
+        groupAnimationAnimation.fillMode = CAMediaTimingFillMode.forwards
+        groupAnimationAnimation.isRemovedOnCompletion = false
+        groupAnimationAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
 
-        layerLayer1.add(transformScaleXyAnimation3, forKey: "transformScaleXyAnimation3")
-
-        // opacity
+        // Group Animation Animations
         //
-        let opacityAnimation1 = CAKeyframeAnimation()
-        opacityAnimation1.beginTime = self.layer.convertTime(CACurrentMediaTime(), from: nil) + 0.998684
-        opacityAnimation1.duration = 2.001316
-        opacityAnimation1.repeatCount = 99999
-        opacityAnimation1.fillMode = kCAFillModeForwards
-        opacityAnimation1.isRemovedOnCompletion = false
-        opacityAnimation1.keyPath = "opacity"
-        opacityAnimation1.values = [ 1, 1, 0, 0 ]
-        opacityAnimation1.keyTimes = [ 0, 0.501023, 0.9846, 1 ]
-        opacityAnimation1.calculationMode = kCAAnimationLinear
+        // get small
+        //
+        let getSmallAnimation = CABasicAnimation()
+        getSmallAnimation.beginTime = 0.002777
+        getSmallAnimation.duration = 0.253381
+        getSmallAnimation.fillMode = CAMediaTimingFillMode.forwards
+        getSmallAnimation.isRemovedOnCompletion = false
+        getSmallAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.default)
+        getSmallAnimation.keyPath = "transform.scale.xy"
+        getSmallAnimation.toValue = 0.5
+        // pop big
+        //
+        let popBigAnimation1 = CASpringAnimation()
+        popBigAnimation1.beginTime = 0.333027
+        popBigAnimation1.duration = 0.99321
+        popBigAnimation1.fillMode = CAMediaTimingFillMode.forwards
+        popBigAnimation1.isRemovedOnCompletion = false
+        popBigAnimation1.keyPath = "transform.scale.xy"
+        popBigAnimation1.toValue = 1
+        popBigAnimation1.stiffness = 200
+        popBigAnimation1.damping = 10
+        popBigAnimation1.mass = 0.7
+        popBigAnimation1.initialVelocity = 4
+        groupAnimationAnimation.animations = [ getSmallAnimation, popBigAnimation1 ]
 
-        layerLayer1.add(opacityAnimation1, forKey: "opacityAnimation1")
+        bigCircleLayer.add(groupAnimationAnimation, forKey: "groupAnimationAnimation")
 
-        self.layer.addSublayer(layerLayer1)
+        self.layer.addSublayer(bigCircleLayer)
+
+        // Small Circle
+        //
+        let smallCircleLayer = CALayer()
+        smallCircleLayer.name = "Small Circle"
+        smallCircleLayer.bounds = CGRect(x: 0, y: 0, width: 16, height: 16)
+        smallCircleLayer.position = CGPoint(x: 20, y: 20)
+        smallCircleLayer.contentsGravity = CALayerContentsGravity.center
+        smallCircleLayer.backgroundColor = backgroundColor1.cgColor
+        smallCircleLayer.cornerRadius = 8
+        smallCircleLayer.borderColor = borderColor.cgColor
+        smallCircleLayer.shadowOffset = CGSize(width: 0, height: 1)
+        smallCircleLayer.allowsEdgeAntialiasing = true
+        smallCircleLayer.allowsGroupOpacity = true
+        smallCircleLayer.fillMode = CAMediaTimingFillMode.forwards
+        smallCircleLayer.transform = CATransform3D( m11: 0, m12: 0, m13: 0, m14: 0,
+                                                    m21: 0, m22: 0, m23: 0, m24: 0,
+                                                    m31: 0, m32: 0, m33: 1, m34: 0,
+                                                    m41: 0, m42: 0, m43: 0, m44: 1 )
+        smallCircleLayer.sublayerTransform = CATransform3D( m11: -5, m12: -0, m13: -0, m14: -0,
+                                                            m21: -0, m22: 5, m23: -0, m24: -0,
+                                                            m31: -0, m32: -0, m33: 1, m34: -0,
+                                                            m41: 0, m42: 0, m43: 0, m44: 1 )
+
+        // Small Circle Animations
+        //
+
+        // breathOUT
+        //
+        let breathOUTAnimation = CASpringAnimation()
+        breathOUTAnimation.beginTime = self.layer.convertTime(CACurrentMediaTime(), from: nil) + 0.000001
+        breathOUTAnimation.duration = 1
+        breathOUTAnimation.repeatDuration = 1
+        breathOUTAnimation.fillMode = CAMediaTimingFillMode.forwards
+        breathOUTAnimation.isRemovedOnCompletion = false
+        breathOUTAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        breathOUTAnimation.keyPath = "transform.scale.xy"
+        breathOUTAnimation.toValue = 1
+        breathOUTAnimation.stiffness = 200
+        breathOUTAnimation.damping = 10
+        breathOUTAnimation.mass = 0.7
+        breathOUTAnimation.initialVelocity = 4
+
+        smallCircleLayer.add(breathOUTAnimation, forKey: "breathOUTAnimation")
+
+        // Group Animation
+        //
+        let groupAnimationAnimation1 = CAAnimationGroup()
+        groupAnimationAnimation1.beginTime = self.layer.convertTime(CACurrentMediaTime(), from: nil) + 1
+        groupAnimationAnimation1.duration = 2
+        groupAnimationAnimation1.repeatCount = 99999
+        groupAnimationAnimation1.fillMode = CAMediaTimingFillMode.forwards
+        groupAnimationAnimation1.isRemovedOnCompletion = false
+        groupAnimationAnimation1.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
+
+        // Group Animation Animations
+        //
+        // breathIN
+        //
+        let breathINAnimation = CABasicAnimation()
+        breathINAnimation.beginTime = 0.000001
+        breathINAnimation.duration = 0.25
+        breathINAnimation.fillMode = CAMediaTimingFillMode.forwards
+        breathINAnimation.isRemovedOnCompletion = false
+        breathINAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.default)
+        breathINAnimation.keyPath = "transform.scale.xy"
+        breathINAnimation.toValue = 0.5
+        breathINAnimation.fromValue = 1
+        // breathOUT
+        //
+        let breathOUTAnimation1 = CASpringAnimation()
+        breathOUTAnimation1.beginTime = 0.307227
+        breathOUTAnimation1.duration = 1
+        breathOUTAnimation1.repeatDuration = 1
+        breathOUTAnimation1.fillMode = CAMediaTimingFillMode.forwards
+        breathOUTAnimation1.isRemovedOnCompletion = false
+        breathOUTAnimation1.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        breathOUTAnimation1.keyPath = "transform.scale.xy"
+        breathOUTAnimation1.toValue = 1
+        breathOUTAnimation1.stiffness = 200
+        breathOUTAnimation1.damping = 10
+        breathOUTAnimation1.mass = 0.7
+        breathOUTAnimation1.initialVelocity = 4
+        groupAnimationAnimation1.animations = [ breathINAnimation, breathOUTAnimation1 ]
+
+        smallCircleLayer.add(groupAnimationAnimation1, forKey: "groupAnimationAnimation1")
+
+        self.layer.addSublayer(smallCircleLayer)
 
     }
 
