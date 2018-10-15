8045716313d78b46a576bc9e87141fcc2d265090
Nate Heagy <nate@heagy.com>
2018-10-14 21:38:10 -0700

    Add first step of quick start tour for following

--- WordPress/Classes/ViewRelated/System/WPTabBarController+QuickStart.swift
+++ WordPress/Classes/ViewRelated/System/WPTabBarController+QuickStart.swift
@@ -5,25 +5,42 @@ private var quickStartObserver: NSObject?
 extension WPTabBarController {
     @objc func startWatchingQuickTours() {
         let observer = NotificationCenter.default.addObserver(forName: .QuickStartTourElementChangedNotification, object: nil, queue: nil) { [weak self] (notification) in
+            spotlightView?.removeFromSuperview()
+            spotlightView = nil
+
             guard let userInfo = notification.userInfo,
                 let element = userInfo[QuickStartTourGuide.notificationElementKey] as? QuickStartTourElement,
-                element == .newpost,
+                [.newpost, .readerTab].contains(element),
                 let tabBar = self?.tabBar else {
-                    spotlightView?.removeFromSuperview()
-                    spotlightView = nil
                     return
             }
 
             let newSpotlight = QuickStartSpotlightView()
             tabBar.addSubview(newSpotlight)
 
-            let x = tabBar.bounds.size.width / 2.0
-
+            let x: CGFloat
+            if element == .newpost {
+                x = tabBar.bounds.size.width / 2.0
+            } else {
+                x = tabBar.bounds.size.width * 0.40 - newSpotlight.frame.size.width
+            }
             newSpotlight.frame = CGRect(x: x, y: spotlightViewOffset, width: newSpotlight.frame.width, height: newSpotlight.frame.height)
             spotlightView = newSpotlight
         }
 
         quickStartObserver = observer as? NSObject
+    }
+
+    private func findTabView(index: Int) -> UIView? {
+        var tabs: [UIView] = []
+        for subview in tabBar.subviews {
+            tabs.append(subview)
+        }
+        tabs.sort {
+            $0.frame.origin.x < $1.frame.origin.x
+        }
+
+        return tabs[index]
     }
 
     @objc func alertQuickStartThatWriteWasTapped() {
