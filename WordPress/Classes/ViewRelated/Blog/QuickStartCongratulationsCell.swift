a43cdbbac4ea691d45d5c0177821913fc46247b8
Nate Heagy <nate@heagy.com>
2018-10-19 12:56:46 -0700

    Cleanup congratulations tour

--- WordPress/Classes/ViewRelated/Blog/QuickStartCongratulationsCell.swift
+++ WordPress/Classes/ViewRelated/Blog/QuickStartCongratulationsCell.swift
@@ -8,8 +8,10 @@ class QuickStartCongratulationsCell: UITableViewCell {
         topLabel?.font = WPStyleGuide.fontForTextStyle(.subheadline, fontWeight: .semibold)
         bottomLabel?.font = WPStyleGuide.fontForTextStyle(.subheadline, fontWeight: .regular)
 
-        topLabel?.text = NSLocalizedString("Congrats on finishing Quick Start  🎉", comment: "Headline shown to users when they complete all Quick Start items")
-        bottomLabel?.text = NSLocalizedString("doesn’t it feel good to cross things off a list?", comment: "subhead shown to users when they complete all Quick Start items")
+        let tour = QuickStartCongratulationsTour()
+
+        topLabel?.text = tour.title
+        bottomLabel?.text = tour.description
     }
 
     static let reuseIdentifier = "QuickStartCongratulationsCell"
