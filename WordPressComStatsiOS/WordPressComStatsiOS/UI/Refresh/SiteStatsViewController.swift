88969643e858334ba59094fde6022b12e22d5645
Stephenie Harris <scoutharris@gmail.com>
2018-10-24 14:03:54 -0600

    Renaming Filter enum to StatsPeriodType.

--- WordPressComStatsiOS/WordPressComStatsiOS/UI/Refresh/SiteStatsViewController.swift
+++ WordPressComStatsiOS/WordPressComStatsiOS/UI/Refresh/SiteStatsViewController.swift
@@ -7,7 +7,38 @@ public class SiteStatsViewController: UIViewController {
     @IBOutlet weak var filterTabBar: FilterTabBar!
     @IBOutlet weak var insightsContainerView: UIView!
     @IBOutlet weak var statsContainerView: UIView!
+    
+    enum StatsPeriodType: Int {
+        case insights = 0
+        case days = 1
+        case weeks = 2
+        case months = 3
+        case years = 4
+        
+        static let allPeriods = [StatsPeriodType.insights, .days, .weeks, .months, .years]
+        
+        var filterTitle: String {
+            switch self {
+            case .insights: return NSLocalizedString("Insights", comment: "Title of Insights stats filter.")
+            case .days: return NSLocalizedString("Days", comment: "Title of Days stats filter.")
+            case .weeks: return NSLocalizedString("Weeks", comment: "Title of Weeks stats filter.")
+            case .months: return NSLocalizedString("Months", comment: "Title of Months stats filter.")
+            case .years: return NSLocalizedString("Years", comment: "Title of Years stats filter.")
+            }
+        }
+    }
 
+    var currentSelectedPeriod: StatsPeriodType {
+        get {
+            let selectedIndex = filterTabBar?.selectedIndex ?? StatsPeriodType.insights.rawValue
+            return StatsPeriodType(rawValue: selectedIndex) ?? .insights
+        }
+        set {
+            filterTabBar?.setSelectedIndex(newValue.rawValue)
+            // TODO: reload view based on selected tab
+        }
+    }
+    
     // MARK: - View
 
     public override func viewDidLoad() {
@@ -30,44 +61,10 @@ private extension SiteStatsViewController {
         filterTabBar.tintColor = WPStyleGuide.wordPressBlue()
         filterTabBar.deselectedTabColor = WPStyleGuide.greyDarken10()
         filterTabBar.dividerColor = WPStyleGuide.greyLighten20()
-
-        filterTabBar.items = Filter.allFilters.map { $0.title }
+        
+        filterTabBar.items = StatsPeriodType.allPeriods.map { $0.filterTitle }
         filterTabBar.addTarget(self, action: #selector(selectedFilterDidChange(_:)), for: .valueChanged)
     }
-
-    var filter: Filter {
-        get {
-            let selectedIndex = filterTabBar?.selectedIndex ?? Filter.insights.rawValue
-            return Filter(rawValue: selectedIndex) ?? .insights
-        }
-        set {
-            filterTabBar?.setSelectedIndex(newValue.rawValue)
-            // TODO: reload view based on selected tab
-        }
-    }
-
-    enum Filter: Int {
-        case insights = 0
-        case daily = 1
-        case weekly = 2
-        case monthly = 3
-        case yearly = 4
-
-        static let allFilters = [Filter.insights, .daily, .weekly, .monthly, .yearly]
-
-        var title: String {
-            switch self {
-            case .insights: return NSLocalizedString("Insights", comment: "Title of Insights stats filter.")
-            case .daily: return NSLocalizedString("Days", comment: "Title of Days stats filter.")
-            case .weekly: return NSLocalizedString("Weeks", comment: "Title of Weeks stats filter.")
-            case .monthly: return NSLocalizedString("Months", comment: "Title of Months stats filter.")
-            case .yearly: return NSLocalizedString("Years", comment: "Title of Years stats filter.")
-            }
-        }
-    }
-
-}
-
     
     @objc func selectedFilterDidChange(_ filterBar: FilterTabBar) {
         // TODO: reload view based on selected tab
