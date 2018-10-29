ea87bd79907a9431377786791b4ade90fd7e13ad
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-10-29 12:32:29 +0800

    Remove an implementation of an step that we don’t need now

--- WordPress/Classes/ViewRelated/Wizards/WizardStep.swift
+++ WordPress/Classes/ViewRelated/Wizards/WizardStep.swift
@@ -12,19 +12,3 @@ protocol WizardStep {
 
     func setWizard(_ wizard : Wizard)
 }
-
-final class WizardStepViewController: UIViewController {
-    /// The injected WizardStep
-    private let step: WizardStep
-
-    required init(step: WizardStep) {
-        self.step = step
-
-        super.init(nibName: nil, bundle: nil)
-        // Additional customization TBD
-    }
-
-    required init(coder decoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
