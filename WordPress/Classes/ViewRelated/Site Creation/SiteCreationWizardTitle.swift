35fef1c7249bdee6390013f4435b75f253796eaa
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-10-29 15:52:15 +0800

    Remove a header

--- WordPress/Classes/ViewRelated/Site Creation/SiteCreationWizardTitle.swift
+++ WordPress/Classes/ViewRelated/Site Creation/SiteCreationWizardTitle.swift
@@ -1,11 +1,3 @@
-//
-//  SiteCreationWizardTitle.swift
-//  WordPress
-//
-//  Created by Cesar Tardaguila on 29/10/2018.
-//  Copyright © 2018 WordPress. All rights reserved.
-//
-
 import UIKit
 
 final class SiteCreationWizardTitle: UIViewController {
