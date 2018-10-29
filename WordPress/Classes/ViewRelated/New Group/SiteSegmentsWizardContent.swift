e8e3aa699d8003a11e42cc472c11d8ad07e18c4c
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-10-29 15:38:11 +0800

    Add view controller for title / subtitle and view controller for SiteType content

---
+++ WordPress/Classes/ViewRelated/New Group/SiteSegmentsWizardContent.swift
@@ -0,0 +1,29 @@
+//
+//  SiteSegmentsWizardContent.swift
+//  
+//
+//  Created by Cesar Tardaguila on 29/10/2018.
+//
+
+import UIKit
+
+class SiteSegmentsWizardContent: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WordPress/Classes/ViewRelated/Site Creation/SiteCreationWizardTitle.swift
@@ -0,0 +1,30 @@
+//
+//  SiteCreationWizardTitle.swift
+//  WordPress
+//
+//  Created by Cesar Tardaguila on 29/10/2018.
+//  Copyright © 2018 WordPress. All rights reserved.
+//
+
+import UIKit
+
+class SiteCreationWizardTitle: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
