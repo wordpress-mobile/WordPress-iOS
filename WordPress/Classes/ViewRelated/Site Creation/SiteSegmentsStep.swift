a8319409a4eb710654bd4cdb6f663ab8b178fb83
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-10-30 14:56:23 +0800

    Return header

--- WordPress/Classes/ViewRelated/Site Creation/SiteSegmentsStep.swift
+++ WordPress/Classes/ViewRelated/Site Creation/SiteSegmentsStep.swift
@@ -7,9 +7,13 @@ final class SiteSegmentsStep: WizardStep {
         return Identifier(value: String(describing: self))
     }
 
-    var header: UIViewController {
-        return UIViewController()
-    }
+    lazy var header: UIViewController = {
+        let title = NSLocalizedString("Tell us what kind of site you'd like to make", comment: "Create site, step 1. Select type of site. Title")
+        let subtitle = NSLocalizedString("This helps us suggest a solid foundation. But you're never locked in -- all sites evolve!", comment: "Create site, step 1. Select type of site. Subtitle")
+        let headerData = SiteCreationHeaderData(title: title, subtitle: subtitle)
+        
+        return SiteCreationWizardTitle(data: headerData)
+    }()
 
     var content: UIViewController {
         return UIViewController()
