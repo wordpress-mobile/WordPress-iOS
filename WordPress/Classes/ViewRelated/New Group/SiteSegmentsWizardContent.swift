a2114e4ffb4c5839b748487b95fe5c002a495a6a
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-10-30 15:10:05 +0800

    Prepare content VC to pass it the service

--- WordPress/Classes/ViewRelated/New Group/SiteSegmentsWizardContent.swift
+++ WordPress/Classes/ViewRelated/New Group/SiteSegmentsWizardContent.swift
@@ -2,12 +2,19 @@
 import UIKit
 
 final class SiteSegmentsWizardContent: UIViewController {
+    private let service: SiteSegmentsService
 
+    init(service: SiteSegmentsService) {
+        self.service = service
+        super.init(nibName: String(describing: type(of: self)), bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
 
 }
