6e6e9197c67181ba5685e45b877f8e1e7465f7c8
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-10-30 15:09:53 +0800

    Make lazy vars private set

--- WordPress/Classes/ViewRelated/Site Creation/SiteSegmentsStep.swift
+++ WordPress/Classes/ViewRelated/Site Creation/SiteSegmentsStep.swift
@@ -7,17 +7,17 @@ final class SiteSegmentsStep: WizardStep {
         return Identifier(value: String(describing: self))
     }
 
-    lazy var header: UIViewController = {
+    private(set) lazy var header: UIViewController = {
         let title = NSLocalizedString("Tell us what kind of site you'd like to make", comment: "Create site, step 1. Select type of site. Title")
         let subtitle = NSLocalizedString("This helps us suggest a solid foundation. But you're never locked in -- all sites evolve!", comment: "Create site, step 1. Select type of site. Subtitle")
         let headerData = SiteCreationHeaderData(title: title, subtitle: subtitle)
-        
+
         return SiteCreationWizardTitle(data: headerData)
     }()
 
-    var content: UIViewController {
+    private(set) lazy var content: UIViewController = {
         return UIViewController()
-    }
+    }()
 
     var delegate: WizardDelegate?
 
