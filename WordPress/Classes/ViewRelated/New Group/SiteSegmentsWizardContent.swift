c142f07ec45e669038eb4e1ad0318806ed438f9d
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-10-30 15:46:03 +0800

    Add table view outlet. Start fetching data

--- WordPress/Classes/ViewRelated/New Group/SiteSegmentsWizardContent.swift
+++ WordPress/Classes/ViewRelated/New Group/SiteSegmentsWizardContent.swift
@@ -4,6 +4,8 @@ import UIKit
 final class SiteSegmentsWizardContent: UIViewController {
     private let service: SiteSegmentsService
 
+    @IBOutlet weak var table: UITableView!
+
     init(service: SiteSegmentsService) {
         self.service = service
         super.init(nibName: String(describing: type(of: self)), bundle: nil)
@@ -15,6 +17,42 @@ final class SiteSegmentsWizardContent: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        fetchSegments()
     }
 
+    private func fetchSegments() {
+        service.siteSegments(for: Locale.current) { [weak self] results in
+            switch results {
+            case .error(let error):
+                self?.handleError(error)
+            case .success(let data):
+                self?.handleData(data)
+            }
+        }
+    }
+
+    private func handleError(_ error: Error) {
+        print("=== handling error===")
+    }
+
+    private func handleData(_ data: [SiteSegment]) {
+        print("handling data")
+    }
+}
+
+final class SegmentsDataSource: NSObject, UITableViewDataSource {
+    private let data: [SiteSegment]
+
+    init(data: [SiteSegment]) {
+        self.data = data
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
