cac71d8ff5b65240f242110d8173f0533bc428a2
Daniele Bogo <daniele.bogo@automattic.com>
2018-10-31 12:59:45 +0000

    Add accessors

--- WordPress/Classes/Models/Revisions/RevisionDiff.swift
+++ WordPress/Classes/Models/Revisions/RevisionDiff.swift
@@ -9,8 +9,42 @@ class RevisionDiff: NSManagedObject {
     @NSManaged var totalAdditions: NSNumber
     @NSManaged var totalDeletions: NSNumber
 
-    @NSManaged var contentDiffs: [DiffContentValue]?
-    @NSManaged var titleDiffs: [DiffTitleValue]?
+    @NSManaged var contentDiffs: NSSet?
+    @NSManaged var titleDiffs: NSSet?
 
     @NSManaged var revision: Revision?
+}
+
+
+// MARK: Generated accessors for contentDiffs
+//
+extension RevisionDiff {
+    @objc(addContentDiffsObject:)
+    @NSManaged public func addToContentDiffs(_ value: DiffContentValue)
+
+    @objc(removeContentDiffsObject:)
+    @NSManaged public func removeFromContentDiffs(_ value: DiffContentValue)
+
+    @objc(addContentDiffs:)
+    @NSManaged public func addToContentDiffs(_ values: NSSet)
+
+    @objc(removeContentDiffs:)
+    @NSManaged public func removeFromContentDiffs(_ values: NSSet)
+}
+
+
+// MARK: Generated accessors for titleDiffs
+//
+extension RevisionDiff {
+    @objc(addTitleDiffsObject:)
+    @NSManaged public func addToTitleDiffs(_ value: DiffTitleValue)
+
+    @objc(removeTitleDiffsObject:)
+    @NSManaged public func removeFromTitleDiffs(_ value: DiffTitleValue)
+
+    @objc(addTitleDiffs:)
+    @NSManaged public func addToTitleDiffs(_ values: NSSet)
+
+    @objc(removeTitleDiffs:)
+    @NSManaged public func removeFromTitleDiffs(_ values: NSSet)
 }
