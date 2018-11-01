624b878e1684da46d97acc4a6b7c348ab0ff1607
Daniele Bogo <daniele.bogo@automattic.com>
2018-11-01 22:13:57 +0000

    Modify table view controller using WPTableViewHandler and implementing the local service

--- WordPress/Classes/ViewRelated/Post/Revisions/RevisionsTableViewController.swift
+++ WordPress/Classes/ViewRelated/Post/Revisions/RevisionsTableViewController.swift
@@ -1,10 +1,13 @@
 class RevisionsTableViewController: UITableViewController {
-    private var dataSource: RevisionsDataSource? {
-        didSet {
-            self.tableView?.dataSource = dataSource
-        }
-    }
     private var post: AbstractPost?
+    private lazy var tableViewHandler: WPTableViewHandler = {
+        let tableViewHandler = WPTableViewHandler(tableView: self.tableView)
+        tableViewHandler.cacheRowHeights = false
+        tableViewHandler.delegate = self
+        tableViewHandler.updateRowAnimation = .none
+        return tableViewHandler
+    }()
+
 
     convenience init(post: AbstractPost) {
         self.init()
@@ -19,45 +22,70 @@ class RevisionsTableViewController: UITableViewController {
         let cellNib = UINib(nibName: "RevisionsTableViewCell", bundle: Bundle(for: RevisionsTableViewCell.self))
         tableView.register(cellNib, forCellReuseIdentifier: RevisionsTableViewCell.reuseIdentifier)
 
+        tableViewHandler.refreshTableView()
+
         guard let post = post else {
             return
         }
-        dataSource = RevisionsDataSource(post: post)
+
+        let managedObjectContext = ContextManager.sharedInstance().mainContext
+        let postService = PostService(managedObjectContext: managedObjectContext)
+        postService.getPostRevisions(for: post, success: { revisions in
+            DispatchQueue.main.async {
+                self.tableViewHandler.refreshTableView()
+            }
+        }) { (error) in
+            DDLogError(error?.localizedDescription ?? "unkown error")
+        }
     }
 }
 
-private class RevisionsDataSource: NSObject, UITableViewDataSource {
-    let post: AbstractPost
-    var revisions: [Int] = []
 
-    init(post: AbstractPost) {
-        self.post = post
-
-        super.init()
-        fetchRevisions()
+extension RevisionsTableViewController: WPTableViewHandlerDelegate {
+    func managedObjectContext() -> NSManagedObjectContext {
+        return ContextManager.sharedInstance().mainContext
     }
 
-    func fetchRevisions() {
-        guard let postRevisions = post.revisions as? [Int] else {
-            return
-        }
-        revisions = postRevisions
+    func fetchRequest() -> NSFetchRequest<NSFetchRequestResult> {
+        let predicate = NSPredicate(format: "\(#keyPath(Revision.postId)) = %@ && \(#keyPath(Revision.siteId)) = %@", post!.postID!, post!.blog.dotComID!)
+        let descriptor = NSSortDescriptor(key: #keyPath(Revision.revisionId), ascending: true)
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: Revision.entityName())
+        fetchRequest.predicate = predicate
+        fetchRequest.sortDescriptors = [descriptor]
+        return fetchRequest
     }
 
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+    func sectionNameKeyPath() -> String {
+        return #keyPath(Revision.revisionDate)
     }
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return revisions.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: RevisionsTableViewCell.reuseIdentifier) as? RevisionsTableViewCell else {
-            return UITableViewCell()
+    func configureCell(_ cell: UITableViewCell, at indexPath: IndexPath) {
+        guard let cell = cell as? RevisionsTableViewCell else {
+            preconditionFailure("The cell should be of class \(String(describing: RevisionsTableViewCell.self))")
         }
 
-        cell.revisionNum = revisions[indexPath.row]
+        let revision = self.revision(at: indexPath)
+        cell.revisionNum = revision.revisionId.intValue
+    }
+
+    func revision(at indexPath: IndexPath) -> Revision {
+        guard let revision = tableViewHandler.resultsController.object(at: indexPath) as? Revision else {
+            fatalError("Expected a Revision object.")
+        }
+
+        return revision
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: RevisionsTableViewCell.reuseIdentifier, for: indexPath) as? RevisionsTableViewCell else {
+            preconditionFailure("The cell should be of class \(String(describing: RevisionsTableViewCell.self))")
+        }
+
+        configureCell(cell, at: indexPath)
         return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
     }
 }
