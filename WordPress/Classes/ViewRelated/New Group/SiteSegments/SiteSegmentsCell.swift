afb7a45b14f18e55c8d0c0fccba16d131453110e
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-10-31 20:22:58 -0700

    Draft implementation of the cell

--- WordPress/Classes/ViewRelated/New Group/SiteSegments/SiteSegmentsCell.swift
+++ WordPress/Classes/ViewRelated/New Group/SiteSegments/SiteSegmentsCell.swift
@@ -1,12 +1,33 @@
 import UIKit
 
 final class SiteSegmentsCell: UITableViewCell {
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
-    }
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var subtitle: UILabel!
 
     func set(segment: SiteSegment) {
-        
+        title.text = segment.title
+        subtitle.text = segment.subtitle
+        icon.setImageWith(segment.icon)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        styleTitle()
+        styleSubtitle()
+    }
+
+    override func prepareForReuse() {
+        title.text = ""
+        subtitle.text = ""
+        icon.image = nil
+    }
+
+    private func styleTitle() {
+
+    }
+
+    private func styleSubtitle() {
+
     }
 }
