76ad2089067c91ce0f206026fa7f27fabbc5a1b8
Daniele Bogo <daniele.bogo@automattic.com>
2018-11-02 11:03:24 +0000

    Add formatter to Revision

--- WordPress/Classes/Models/Revisions/Revision.swift
+++ WordPress/Classes/Models/Revisions/Revision.swift
@@ -18,16 +18,22 @@ class Revision: NSManagedObject {
 
     @NSManaged var diff: RevisionDiff?
 
-    @objc var revisionDate: String {
-        let date = formatter.date(from: postDateGmt ?? "") ?? Date()
-        return date.toStringForPageSections()
-    }
 
-    private lazy var formatter: DateFormatter = {
+    private lazy var revisionFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "en_US_POSIX")
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ssZ"
         formatter.timeZone = TimeZone(secondsFromGMT: 0)
         return formatter
     }()
+
+    @objc var revisionDate: String {
+        let date = revisionFormatter.date(from: postDateGmt ?? "") ?? Date()
+        return date.toStringForPageSections()
+    }
+
+    @objc var revisionModifiedDate: String {
+        let date = revisionFormatter.date(from: postModifiedGmt ?? "") ?? Date()
+        return date.toStringForPageSections()
+    }
 }
