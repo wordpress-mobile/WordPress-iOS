087a18474986e3779d1f2f6a48db185791e67023
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-11-05 13:19:08 +0800

    Fix a typo

--- WordPress/Classes/ViewRelated/New Group/SiteCreationOutput.swift
+++ WordPress/Classes/ViewRelated/New Group/SiteCreationOutput.swift
@@ -1,3 +1,3 @@
-// The final output of the site creation wizard, is an endocable model object that can be submitted to the backend
+// The final output of the site creation wizard, is an encodable model object that can be submitted to the backend
 struct SiteCreationOutput {
 }
