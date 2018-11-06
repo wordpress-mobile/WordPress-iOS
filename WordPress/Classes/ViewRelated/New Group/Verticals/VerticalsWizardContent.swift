5db1116d8a10b3126a5577d9bee55b88ebd445cb
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-11-06 14:08:22 +0800

    Setup outlets

--- WordPress/Classes/ViewRelated/New Group/Verticals/VerticalsWizardContent.swift
+++ WordPress/Classes/ViewRelated/New Group/Verticals/VerticalsWizardContent.swift
@@ -4,6 +4,10 @@ final class VerticalsWizardContent: UIViewController {
     private let service: SiteVerticalsService
     private var dataSource: UITableViewDataSource?
 
+    @IBOutlet weak var search: UITextField!
+    @IBOutlet weak var table: UITableView!
+
+
     init(service: SiteVerticalsService) {
         self.service = service
         super.init(nibName: String(describing: type(of: self)), bundle: nil)
