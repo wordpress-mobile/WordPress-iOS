f98fcab98c65444bf154d45335243ab7d247405e
Daniele Bogo <daniele.bogo@automattic.com>
2018-11-07 09:56:43 +0000

    Fix date formatter type

--- WordPress/Classes/Models/Revisions/Revision.swift
+++ WordPress/Classes/Models/Revisions/Revision.swift
@@ -36,6 +36,6 @@ class Revision: NSManagedObject {
     }
 
     @objc var revisionDateForSection: String {
-        return revisionDate.toStringForPageSections()
+        return revisionDate.longUTCStringWithoutTime()
     }
 }
