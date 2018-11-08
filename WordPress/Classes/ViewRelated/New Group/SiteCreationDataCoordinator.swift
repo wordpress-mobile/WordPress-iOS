5d5745ada0568a2f0b3ae55145e2074fb420f659
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-11-08 18:29:22 +0800

    Implement a header

--- WordPress/Classes/ViewRelated/New Group/SiteCreationDataCoordinator.swift
+++ WordPress/Classes/ViewRelated/New Group/SiteCreationDataCoordinator.swift
@@ -1,6 +1,12 @@
+// Decorates a UITableViewDataSource & UITableViewDelegate to implement a table view header
 final class SiteCreationDataCoordinator: NSObject, UITableViewDataSource, UITableViewDelegate {
     private let decorated: UITableViewDataSource & UITableViewDelegate
     private let headerData: SiteCreationHeaderData
+
+    private struct Constants {
+        // Arbitrary value, will have to be updated
+        static let headerHeight: CGFloat = 80
+    }
 
     init(decorated: UITableViewDataSource & UITableViewDelegate, headerData: SiteCreationHeaderData) {
         self.decorated = decorated
@@ -18,5 +24,26 @@ final class SiteCreationDataCoordinator: NSObject, UITableViewDataSource, UITabl
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //Avoid force unwrapping
         decorated.tableView!(tableView, didSelectRowAt: indexPath)
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let title = UILabel(frame: .zero)
+        title.translatesAutoresizingMaskIntoConstraints = false
+        title.text = headerData.title
+
+        let subTitle = UILabel(frame: .zero)
+        subTitle.translatesAutoresizingMaskIntoConstraints = false
+        subTitle.text = headerData.subtitle
+
+        let stackView = UIStackView(arrangedSubviews: [title, subTitle])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+
+        return stackView
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return Constants.headerHeight
     }
 }
