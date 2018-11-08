3468f2f422edbca6389181444b710b76e243ce83
Stephenie Harris <scoutharris@gmail.com>
2018-11-08 11:42:20 -0700

    Adding example conversions.

--- WordPress/Classes/ViewRelated/Stats/Double+Stats.swift
+++ WordPress/Classes/ViewRelated/Stats/Double+Stats.swift
@@ -2,6 +2,24 @@ import Foundation
 
 extension Double {
 
+    /// Provides a short, friendly representation of the current Double value. If the value is
+    /// below 1000, the decimal is stripped and the string returned will look like an Int. If the value
+    /// is above 1000, the value is rounded to the nearest tenth and the appropriate abbreviation
+    /// will be appended (k, m, b, t, p, e).
+    ///
+    /// Examples:
+    ///  - 0 becomes "0"
+    ///  - 999 becomes "999"
+    ///  - 1000 becomes "1k"
+    ///  - 987654 becomes "987.7k"
+    ///  - 999999 becomes "1m"
+    ///  - 1000000 becomes "1m"
+    ///  - 1234324 becomes "1.2m"
+    ///  - 5800199 becomes "5.8m"
+    ///  - 5897459 becomes "5.9m"
+    ///  - 1000000000 becomes "1b"
+    ///  - 1000000000000 becomes "1t"
+
     func abbreviatedString() -> String {
         var num = self
         let sign = num < 0 ? "-" : ""
