ab1cdeedb21abab442b190feee45736848c75be4
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-11-08 16:39:12 +0800

    Add a cancel button

--- WordPress/Classes/ViewRelated/Blog/BlogListViewController+SiteCreation.swift
+++ WordPress/Classes/ViewRelated/Blog/BlogListViewController+SiteCreation.swift
@@ -3,6 +3,18 @@ extension BlogListViewController {
     func enhancedSiteCreation() {
         let wizardLauncher = SiteCreationWizardLauncher()
         let wizard = wizardLauncher.ui
+        wizard.navigationItem.leftBarButtonItem = cancelButton()
+
         present(wizard, animated: true)
+    }
+
+    private func cancelButton() -> UIBarButtonItem {
+        let literal = NSLocalizedString("Cancel", comment: "Cancel button. Site creation modal popover.")
+        return UIBarButtonItem(title: literal, style: .plain, target: self, action: #selector(cancel))
+    }
+
+    @objc
+    private func cancel() {
+        dismiss(animated: true, completion: nil)
     }
 }
