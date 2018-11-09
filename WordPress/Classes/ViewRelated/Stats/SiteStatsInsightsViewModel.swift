09763017489cc69d4817886ae5857152e513b32e
Stephenie Harris <scoutharris@gmail.com>
2018-11-09 14:41:22 -0700

    Adding view model for Stats Insights.

---
+++ WordPress/Classes/ViewRelated/Stats/SiteStatsInsightsViewModel.swift
@@ -0,0 +1,61 @@
+import Foundation
+import WordPressComStatsiOS
+
+/// The view model used by Stats Insights.
+///
+class SiteStatsInsightsViewModel: NSObject {
+
+    // MARK: - Properties
+
+    private var statsService: WPStatsService?
+    private(set) var refreshing = false
+    private var siteStatsInsightsDelegate: SiteStatsInsightsDelegate?
+
+    // MARK: - Constructor
+
+    init(statsService: WPStatsService?, insightsDelegate: SiteStatsInsightsDelegate) {
+        super.init()
+        self.statsService = statsService
+        self.siteStatsInsightsDelegate = insightsDelegate
+    }
+
+    // MARK: - Data Fetching
+
+    func fetchStats() {
+
+        refreshing = true
+
+        statsService?.retrieveInsightsStats(allTimeStatsCompletionHandler: { (allTimeStats, error) in
+
+        }, insightsCompletionHandler: { (mostPopularStats, error) in
+
+        }, todaySummaryCompletionHandler: { (todaySummary, error) in
+
+        }, latestPostSummaryCompletionHandler: { (latestPostSummary, error) in
+            if error != nil {
+                DDLogDebug("Error fetching latest post summary: \(String(describing: error?.localizedDescription))")
+            }
+            self.refreshing = false
+            self.siteStatsInsightsDelegate?.latestPostSummaryLoaded?(latestPostSummary)
+        }, commentsAuthorCompletionHandler: { (commentsAuthors, error) in
+
+        }, commentsPostsCompletionHandler: { (commentsPosts, error) in
+
+        }, tagsCategoriesCompletionHandler: { (tagsCategories, error) in
+
+        }, followersDotComCompletionHandler: { (followersDotCom, error) in
+
+        }, followersEmailCompletionHandler: { (followersEmail, error) in
+
+        }, publicizeCompletionHandler: { (publicize, error) in
+
+        }, streakCompletionHandler: { (statsStreak, error) in
+
+        }, progressBlock: { (numberOfFinishedOperations, totalNumberOfOperations) in
+
+        }, andOverallCompletionHandler: {
+
+        })
+    }
+
+}
