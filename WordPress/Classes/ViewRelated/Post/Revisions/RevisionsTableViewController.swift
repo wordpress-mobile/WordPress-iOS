08236a87ab5049cb3c52738056268f503bb3de30
Daniele Bogo <daniele.bogo@automattic.com>
2018-11-09 10:20:08 +0000

    Implement didSelectRowAt due to a mandatory implementation

--- WordPress/Classes/ViewRelated/Post/Revisions/RevisionsTableViewController.swift
+++ WordPress/Classes/ViewRelated/Post/Revisions/RevisionsTableViewController.swift
@@ -142,6 +142,13 @@ extension RevisionsTableViewController: WPTableViewHandlerDelegate {
         configureCell(cell, at: indexPath)
         return cell
     }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        let revision = getRevision(at: indexPath)
+        print("Select revision \(revision.revisionId.stringValue)")
+    }
 }
 
 
