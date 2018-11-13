79dc30e0a5d9a7b5b393d11b090dc97f6d4ca979
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-11-13 15:40:12 +0800

    Provide a mock implementation of the SiteInformationService

--- WordPress/Classes/Services/SiteInformationService.swift
+++ WordPress/Classes/Services/SiteInformationService.swift
@@ -1,3 +1,23 @@
-protocol SiteInformationService {
+typealias SiteInformationServiceCompletion = (Result<SiteInformation>) -> Void
 
+protocol SiteInformationService {
+    func information(for: Locale, completion: @escaping SiteInformationServiceCompletion)
+}
+
+final class MockSiteInformationService: SiteInformationService {
+    func information(for: Locale, completion: @escaping SiteInformationServiceCompletion) {
+
+        let result = Result.success(mockSiteInformation())
+
+        completion(result)
+    }
+
+    private func mockSiteInformation() -> SiteInformation {
+        let json = Bundle(for: MockSiteInformationService.self).url(forResource: "site-info-need", withExtension: "json")!
+        let data = try! Data(contentsOf: json)
+        let jsonDecoder = JSONDecoder()
+
+        let value = try! jsonDecoder.decode(SiteInformation.self, from: data)
+        return value
+    }
 }
