ecf2590f22f0d0769d82d6c73932cc7161c88004
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-11-14 15:23:58 +0800

    Make SiteAddressService return a DomainSuggestion instead of SiteAddress

--- WordPress/Classes/Models/SiteAddress.swift
+++
@@ -1,5 +0,0 @@
-/// Models a Site Address
-struct SiteAddress {
-    let identifier: Identifier
-    let title: String
-}
--- WordPress/Classes/Services/SiteAddressService.swift
+++ WordPress/Classes/Services/SiteAddressService.swift
@@ -1,4 +1,6 @@
-typealias SiteAddressServiceCompletion = (Result<[SiteAddress]>) -> Void
+import WordPressKit
+
+typealias SiteAddressServiceCompletion = (Result<[DomainSuggestion]>) -> Void
 
 protocol SiteAddressService {
     func addresses(for: Locale, completion: @escaping SiteAddressServiceCompletion)
@@ -11,9 +13,15 @@ final class MockSiteAddressService: SiteAddressService {
         completion(result)
     }
 
-    private func mockAddresses() -> [SiteAddress] {
-        return [ SiteAddress(identifier: Identifier(value: "SA 1"), title: "ravenclaw.wordpress.com"),
-                 SiteAddress(identifier: Identifier(value: "SA 2"), title: "ravenclaw.com"),
-                 SiteAddress(identifier: Identifier(value: "SA 3"), title: "team.ravenclaw.com") ]
+    private func mockAddresses() -> [DomainSuggestion] {
+        return [ DomainSuggestion(name: "ravenclaw.wordpress.com"),
+                 DomainSuggestion(name: "ravenclaw.com"),
+                 DomainSuggestion(name: "team.ravenclaw.com")]
+    }
+}
+
+private extension DomainSuggestion {
+    init(name: String) {
+        try! self.init(json: ["domain_name": name as AnyObject])
     }
 }
