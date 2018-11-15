659b6795e52c88370f4133edd3f67c50e98b897a
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-11-15 17:18:08 +0800

    Draft implementation of the Site Info UI

--- WordPress/Classes/ViewRelated/Site Creation/SiteInfo/SiteInformationWizardContent.swift
+++ WordPress/Classes/ViewRelated/Site Creation/SiteInfo/SiteInformationWizardContent.swift
@@ -1,10 +1,30 @@
 import UIKit
 
 final class SiteInformationWizardContent: UIViewController {
+    fileprivate enum Rows: Int, CaseIterable {
+        case title = 0
+        case tagline = 1
+
+        static func count() -> Int {
+            return allCases.count
+        }
+
+        func matches(_ row: Int) -> Bool {
+            return row == self.rawValue
+        }
+    }
+
     private let completion: (SiteInformation) -> Void
 
     @IBOutlet weak var table: UITableView!
     @IBOutlet weak var nextStep: UIButton!
+
+    private lazy var headerData: SiteCreationHeaderData = {
+        let title = NSLocalizedString("Basic information", comment: "Create site, step 3. Select basic information. Title")
+        let subtitle = NSLocalizedString("Tell us more about the site you are creating.", comment: "Create site, step 3. Select basic information. Subtitle")
+
+        return SiteCreationHeaderData(title: title, subtitle: subtitle)
+    }()
 
     init(completion: @escaping (SiteInformation) -> Void) {
         self.completion = completion
@@ -22,7 +42,6 @@ final class SiteInformationWizardContent: UIViewController {
         setupBackground()
         setupTable()
         setupNextButton()
-        render()
     }
 
     private func applyTitle() {
@@ -35,15 +54,21 @@ final class SiteInformationWizardContent: UIViewController {
 
     private func setupTable() {
         setupTableBackground()
-        hideSeparators()
+        registerCell()
+        setupHeader()
+
+        table.dataSource = self
+        table.delegate = self
     }
 
     private func setupTableBackground() {
         table.backgroundColor = WPStyleGuide.greyLighten30()
     }
-
-    private func hideSeparators() {
-        table.tableFooterView = UIView(frame: .zero)
+    private func registerCell() {
+        table.register(
+            InlineEditableNameValueCell.defaultNib,
+            forCellReuseIdentifier: InlineEditableNameValueCell.defaultReuseID
+        )
     }
 
     private func setupNextButton() {
@@ -55,20 +80,18 @@ final class SiteInformationWizardContent: UIViewController {
         nextStep.accessibilityHint = NSLocalizedString("Navigates to the next step", comment: "Site creation. Navigates tot he next step")
     }
 
-    private func render() {
-    }
-
-    private func setupHeader(_ headerData: SiteCreationHeaderData) {
+    private func setupHeader() {
         let header = TitleSubtitleHeader(frame: .zero)
         header.setTitle(headerData.title)
         header.setSubtitle(headerData.subtitle)
 
         table.tableHeaderView = header
 
-        // This is the only way I found to insert a stack view into the header without breaking the autolayout constraints. We do something similar in Reader
-        header.centerXAnchor.constraint(equalTo: table.centerXAnchor).isActive = true
-        header.widthAnchor.constraint(equalTo: table.layoutMarginsGuide.widthAnchor).isActive = true
-        header.topAnchor.constraint(equalTo: table.layoutMarginsGuide.topAnchor).isActive = true
+        NSLayoutConstraint.activate([
+            header.centerXAnchor.constraint(equalTo: table.centerXAnchor),
+            header.widthAnchor.constraint(equalTo: table.layoutMarginsGuide.widthAnchor),
+            header.topAnchor.constraint(equalTo: table.layoutMarginsGuide.topAnchor)
+            ])
 
         table.tableHeaderView?.layoutIfNeeded()
         table.tableHeaderView = table.tableHeaderView
@@ -76,7 +99,61 @@ final class SiteInformationWizardContent: UIViewController {
 
     @objc
     private func goNext() {
-        let collectedData = SiteInformation(title: "Change me", tagLine: "Change me")
+        guard let titleCell = cell(at: IndexPath(row: Rows.title.rawValue, section: 0)),
+            let taglineCell = cell(at: IndexPath(row: Rows.tagline.rawValue, section: 0)) else {
+            return
+        }
+
+        let collectedData = SiteInformation(title: titleCell.valueTextField.text ?? "", tagLine: taglineCell.valueTextField.text)
         completion(collectedData)
+    }
+
+    private func cell(at: IndexPath) -> InlineEditableNameValueCell? {
+        return table.cellForRow(at: at) as? InlineEditableNameValueCell
+    }
+}
+
+extension SiteInformationWizardContent: UITableViewDataSource {
+    private enum TableStrings {
+        static let site = NSLocalizedString("Site Title", comment: "Site info. Title")
+        static let tagline = NSLocalizedString("Tagline", comment: "Site info. Tagline")
+        static let taglinePlaceholder = NSLocalizedString("Optional Tagline", comment: "Site info. Tagline placeholder")
+        static let footer = NSLocalizedString("The tagline is a short line of text shown right below the title", comment: "Site info. Table footer.")
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Rows.count()
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: InlineEditableNameValueCell.defaultReuseID, for: indexPath) as? InlineEditableNameValueCell {
+            configure(cell, index: indexPath)
+
+            return cell
+        }
+
+        return UITableViewCell()
+    }
+
+    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return TableStrings.footer
+    }
+
+    private func configure(_ cell: InlineEditableNameValueCell, index: IndexPath) {
+        if Rows.title.matches(index.row) {
+            cell.nameLabel.text = TableStrings.site
+            cell.valueTextField.placeholder = TableStrings.site
+        }
+
+        if Rows.tagline.matches(index.row) {
+            cell.nameLabel.text = TableStrings.tagline
+            cell.valueTextField.placeholder = TableStrings.taglinePlaceholder
+        }
+    }
+}
+
+extension SiteInformationWizardContent: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
+        WPStyleGuide.configureTableViewSectionFooter(view)
     }
 }
