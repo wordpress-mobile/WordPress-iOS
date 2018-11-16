9af420bbf66658f87ace2e0bdd28f3e43d9d4155
Nate Heagy <nate@heagy.com>
2018-11-16 11:08:18 -0800

    Refactor revision diff VC to recreate textviews

--- WordPress/Classes/ViewRelated/Post/Revisions/Views/RevisionDiffViewController.swift
+++ WordPress/Classes/ViewRelated/Post/Revisions/Views/RevisionDiffViewController.swift
@@ -3,10 +3,19 @@ import Aztec
 
 class RevisionDiffViewController: UIViewController {
     @IBOutlet private var webView: WKWebView?
+    private let aztext = Aztec.TextView(defaultFont: UIFont.systemFont(ofSize: 15.0), defaultMissingImage: UIImage())
     var revision: Revision? {
         didSet {
             showRevision()
         }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        aztext.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(aztext)
+        view.pinSubviewToAllEdges(aztext)
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -14,17 +23,14 @@ class RevisionDiffViewController: UIViewController {
 
         showRevision()
     }
+
     private func showRevision() {
         guard let revision = revision else {
             return
         }
 
-        let aztext = Aztec.TextView(defaultFont: UIFont.systemFont(ofSize: 15.0), defaultMissingImage: UIImage())
-        aztext.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(aztext)
-        view.pinSubviewToAllEdges(aztext)
-
         let html = revision.postContent ?? ""
+        aztext.setHTML("")
         aztext.insertText(html)
      }
 }
