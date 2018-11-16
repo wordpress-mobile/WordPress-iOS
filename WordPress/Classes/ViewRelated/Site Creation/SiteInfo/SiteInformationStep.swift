9329d518568ce6a9bc62af8271b80ddedef2069b
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-11-16 08:57:20 +0800

    Rename completion loc

--- WordPress/Classes/ViewRelated/Site Creation/SiteInfo/SiteInformationStep.swift
+++ WordPress/Classes/ViewRelated/Site Creation/SiteInfo/SiteInformationStep.swift
@@ -2,7 +2,7 @@ final class SiteInformationStep: WizardStep {
     private let creator: SiteCreator
 
     private(set) lazy var content: UIViewController = {
-        return SiteInformationWizardContent(completion: didGoNext)
+        return SiteInformationWizardContent(completion: didSelect)
     }()
 
     var delegate: WizardDelegate?
@@ -11,7 +11,7 @@ final class SiteInformationStep: WizardStep {
         self.creator = creator
     }
 
-    private func didGoNext(_ data: SiteInformation) {
+    private func didSelect(_ data: SiteInformation) {
         creator.information = data
         delegate?.wizard(self, willNavigateTo: WebAddressStep.identifier)
     }
