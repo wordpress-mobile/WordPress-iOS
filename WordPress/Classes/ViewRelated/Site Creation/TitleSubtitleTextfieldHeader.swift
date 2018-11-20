bba67aefec262b77e29d2ae6c5f7859890c6d338
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-11-20 12:24:50 +0800

    Animate showing / hiding the title and subtitle when the textifield starts / ends editing

--- WordPress/Classes/ViewRelated/Site Creation/TitleSubtitleTextfieldHeader.swift
+++ WordPress/Classes/ViewRelated/Site Creation/TitleSubtitleTextfieldHeader.swift
@@ -2,6 +2,13 @@ import UIKit
 import Gridicons
 
 final class TitleSubtitleTextfieldHeader: UIView {
+    private struct Animation {
+        static let duration: TimeInterval = 0.40
+        static let delay: TimeInterval = 0.0
+        static let damping: CGFloat = 0.9
+        static let spring: CGFloat = 1.0
+    }
+
     private lazy var titleSubtitle: TitleSubtitleHeader = {
         let returnValue = TitleSubtitleHeader(frame: .zero)
         returnValue.translatesAutoresizingMaskIntoConstraints = false
@@ -14,6 +21,8 @@ final class TitleSubtitleTextfieldHeader: UIView {
         returnValue.translatesAutoresizingMaskIntoConstraints = false
         returnValue.leftViewMode = .always
         returnValue.backgroundColor = .white
+
+        returnValue.delegate = self
 
         let loupeIcon = Gridicon.iconOfType(.search)
         let imageView = UIImageView(image: loupeIcon)
@@ -65,5 +74,36 @@ final class TitleSubtitleTextfieldHeader: UIView {
 
     func setSubtitle(_ text: String) {
         titleSubtitle.setSubtitle(text)
+    }
+
+    func hideTitleSubtitle() {
+        updateTitleSubtitle(visibility: true)
+    }
+
+    func showTitleSubtitle() {
+        updateTitleSubtitle(visibility: false)
+    }
+
+    private func updateTitleSubtitle(visibility: Bool) {
+        //stackView.arrangedSubviews.first?.isHidden = visibility
+
+        UIView.animate(withDuration: Animation.duration,
+                       delay: Animation.delay,
+                       usingSpringWithDamping: Animation.damping,
+                       initialSpringVelocity: Animation.spring,
+                       options: [],
+                       animations: { [weak self] in
+                            self?.stackView.arrangedSubviews.first?.isHidden = visibility
+        }, completion: nil)
+    }
+}
+
+extension TitleSubtitleTextfieldHeader: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        hideTitleSubtitle()
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        showTitleSubtitle()
     }
 }
