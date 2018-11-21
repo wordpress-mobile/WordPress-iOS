febace17f02a5389d33d9c2768bc92a42789923d
Stephenie Harris <scoutharris@gmail.com>
2018-11-21 16:45:46 -0700

    Adding MostPopularStatsRow.

--- WordPress/Classes/ViewRelated/Stats/SiteStatsTableViewCells.swift
+++ WordPress/Classes/ViewRelated/Stats/SiteStatsTableViewCells.swift
@@ -47,6 +47,29 @@ struct AllTimeStatsRow: ImmuTableRow {
     }
 }
 
+struct MostPopularStatsRow: ImmuTableRow {
+
+    typealias CellType = SimpleTotalsCell
+
+    static let cell: ImmuTableCell = {
+        return ImmuTableCell.nib(CellType.defaultNib, CellType.self)
+    }()
+
+    let itemSubtitle: String
+    let dataSubtitle: String
+    let dataRows: [StatsTotalRowData]
+    let action: ImmuTableAction? = nil
+
+    func configureCell(_ cell: UITableViewCell) {
+
+        guard let cell = cell as? CellType else {
+            return
+        }
+
+        cell.configure(dataRows: dataRows, itemSubtitle: itemSubtitle, dataSubtitle: dataSubtitle)
+    }
+}
+
 struct CellHeaderRow: ImmuTableRow {
 
     typealias CellType = StatsCellHeader
