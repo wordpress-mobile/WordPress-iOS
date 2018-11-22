d595d10a3200fb34bbe236fe2027b6d999174192
Nate Heagy <nate@heagy.com>
2018-11-22 11:10:07 -0800

    Add demonstration code for notices from modals

--- WordPress/Classes/ViewRelated/Post/Revisions/Browser/RevisionDiffsBrowserViewController.swift
+++ WordPress/Classes/ViewRelated/Post/Revisions/Browser/RevisionDiffsBrowserViewController.swift
@@ -1,4 +1,5 @@
 import Gridicons
+import WordPressFlux
 
 class RevisionBrowserState {
     typealias RevisionSelectedBlock = (Revision) -> Void
@@ -128,6 +129,12 @@ class RevisionDiffsBrowserViewController: UIViewController {
         dismiss(animated: true) {
             self.revisionState?.onRevisionSelected(revision)
         }
+
+        // Temp code to demonstrate how this will look
+        let notice = Notice(title: "Revision loaded", message: nil, feedbackType: .success, notificationInfo: nil, actionTitle: "Undo", cancelTitle: nil) { (happened) in
+
+        }
+        ActionDispatcher.dispatch(NoticeAction.post(notice))
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
