08de6be3c0d5ac5c12a225f2f66f8ee1366e8e70
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-11-26 13:50:29 +0800

    Extract animation duration

--- WordPress/Classes/ViewRelated/Site Creation/SiteInfo/SiteInformationWizardContent.swift
+++ WordPress/Classes/ViewRelated/Site Creation/SiteInfo/SiteInformationWizardContent.swift
@@ -289,27 +289,3 @@ extension SiteInformationWizardContent {
         bottomConstraint.constant = Constants.bottomMargin
     }
 }
-
-private struct KeyboardInfo {
-    var animationCurve: UIView.AnimationCurve
-    var animationDuration: Double
-    var isLocal: Bool
-    var frameBegin: CGRect
-    var frameEnd: CGRect
-}
-
-extension KeyboardInfo {
-    init?(_ notification: Foundation.Notification) {
-        guard notification.name == UIResponder.keyboardWillShowNotification || notification.name == UIResponder.keyboardWillHideNotification else {
-            return nil
-        }
-
-        let u = notification.userInfo!
-
-        animationCurve = UIView.AnimationCurve(rawValue: u[UIWindow.keyboardAnimationCurveUserInfoKey] as! Int)!
-        animationDuration = u[UIWindow.keyboardAnimationDurationUserInfoKey] as! Double
-        isLocal = u[UIWindow.keyboardIsLocalUserInfoKey] as! Bool
-        frameBegin = u[UIWindow.keyboardFrameBeginUserInfoKey] as! CGRect
-        frameEnd = u[UIWindow.keyboardFrameEndUserInfoKey] as! CGRect
-    }
-}
