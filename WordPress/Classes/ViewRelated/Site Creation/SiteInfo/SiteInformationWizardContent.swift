3de2ef5575b2add4d849e79f888d8bd8ad627098
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-11-26 09:39:46 +0800

    Assign header constraints in other wizard content VCs

--- WordPress/Classes/ViewRelated/Site Creation/SiteInfo/SiteInformationWizardContent.swift
+++ WordPress/Classes/ViewRelated/Site Creation/SiteInfo/SiteInformationWizardContent.swift
@@ -91,7 +91,7 @@ final class SiteInformationWizardContent: UIViewController {
 
         NSLayoutConstraint.activate([
             header.centerXAnchor.constraint(equalTo: table.centerXAnchor),
-            header.widthAnchor.constraint(equalTo: table.widthAnchor),
+            header.widthAnchor.constraint(lessThanOrEqualTo: table.widthAnchor, multiplier: 1.0),
             header.topAnchor.constraint(equalTo: table.topAnchor)
             ])
 
