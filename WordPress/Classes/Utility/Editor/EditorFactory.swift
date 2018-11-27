30bb1edb4efdc3a282afe764bbeb98058eecad14
Diego Rey Mendez <diegoreymendez@gmail.com>
2018-11-27 13:55:18 -0300

    Adds documentation.

--- WordPress/Classes/Utility/Editor/EditorFactory.swift
+++ WordPress/Classes/Utility/Editor/EditorFactory.swift
@@ -1,7 +1,12 @@
 import Foundation
 
+/// This class takes care of instantiating the correct editor based on the App settings, feature flags,
+/// etc.
+///
 class EditorFactory {
 
+    /// Settings for the Gutenberg logic.
+    ///
     private let gutenbergSettings = GutenbergSettings()
 
     // MARK: - Editor: Editor Choice Logic
