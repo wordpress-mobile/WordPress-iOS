91ced04b110071b5fe66b69a10527d6f4d1ca648
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2018-11-27 17:31:38 +0800

    Extract code to refresh the header to an extension

---
+++ WordPress/Classes/ViewRelated/Site Creation/RedrawableTableHeader.swift
@@ -0,0 +1,37 @@
+protocol RedrawableTableHeader {
+    func refreshTableViewHeaderLayout(_ table: UITableView)
+}
+
+extension RedrawableTableHeader where Self: UIViewController {
+    func refreshTableViewHeaderLayout(_ table: UITableView) {
+        guard let headerView = table.tableHeaderView else {
+            return
+        }
+
+        // The tableView may need to layout, run this layout now, if needed.
+        // This ensures the proper margins, such as readable margins, are
+        // inherited and calculated by the headerView.
+        table.layoutIfNeeded()
+
+        // Start with the provided UILayoutFittingCompressedSize to let iOS handle its own magic
+        // number for a "compressed" height, meaning we want our fitting size to be the minimal height.
+        var fittingSize = UIView.layoutFittingCompressedSize
+
+        // Set the width to the tableView's width since this is a known width for the headerView.
+        // Otherwise, the layout will try and adopt 'any' width and may break based on the how
+        // the constraints are set up in the nib.
+        fittingSize.width = table.frame.size.width
+
+        // Require horizontal fitting since our width is known.
+        // Use the lower fitting size priority as we want to minimize our height consumption
+        // according to the layout's contraints and intrinsic size.
+        let size = headerView.systemLayoutSizeFitting(fittingSize,
+                                                      withHorizontalFittingPriority: .required,
+                                                      verticalFittingPriority: .fittingSizeLevel)
+        // Update the tableHeaderView itself. Classic.
+        var headerFrame = headerView.frame
+        headerFrame.size.height = size.height
+        headerView.frame = headerFrame
+        table.tableHeaderView = headerView
+    }
+}
--- WordPress/Classes/ViewRelated/Site Creation/SiteInfo/SiteInformationWizardContent.swift
+++ WordPress/Classes/ViewRelated/Site Creation/SiteInfo/SiteInformationWizardContent.swift
@@ -1,9 +1,9 @@
 import UIKit
 import WordPressAuthenticator
 
-typealias SIteInformationCompletion = (SiteInformation) -> Void
+typealias SiteInformationCompletion = (SiteInformation) -> Void
 
-final class SiteInformationWizardContent: UIViewController {
+final class SiteInformationWizardContent: UIViewController, RedrawableTableHeader {
     private enum Rows: Int, CaseIterable {
         case title = 0
         case tagline = 1
@@ -24,7 +24,7 @@ final class SiteInformationWizardContent: UIViewController {
         static let footerHorizontalMargin: CGFloat = 16.0
     }
 
-    private let completion: SIteInformationCompletion
+    private let completion: SiteInformationCompletion
 
     @IBOutlet weak var table: UITableView!
     @IBOutlet weak var nextStep: NUXButton!
@@ -38,7 +38,7 @@ final class SiteInformationWizardContent: UIViewController {
         return SiteCreationHeaderData(title: title, subtitle: subtitle)
     }()
 
-    init(completion: @escaping SIteInformationCompletion) {
+    init(completion: @escaping SiteInformationCompletion) {
         self.completion = completion
         super.init(nibName: String(describing: type(of: self)), bundle: nil)
     }
@@ -138,39 +138,7 @@ final class SiteInformationWizardContent: UIViewController {
         table.tableHeaderView?.layoutIfNeeded()
         table.tableHeaderView = table.tableHeaderView
 
-        refreshTableViewHeaderLayout()
-    }
-
-    func refreshTableViewHeaderLayout() {
-        guard let headerView = table.tableHeaderView else {
-            return
-        }
-
-        // The tableView may need to layout, run this layout now, if needed.
-        // This ensures the proper margins, such as readable margins, are
-        // inherited and calculated by the headerView.
-        table.layoutIfNeeded()
-
-        // Start with the provided UILayoutFittingCompressedSize to let iOS handle its own magic
-        // number for a "compressed" height, meaning we want our fitting size to be the minimal height.
-        var fittingSize = UIView.layoutFittingCompressedSize
-
-        // Set the width to the tableView's width since this is a known width for the headerView.
-        // Otherwise, the layout will try and adopt 'any' width and may break based on the how
-        // the constraints are set up in the nib.
-        fittingSize.width = table.frame.size.width
-
-        // Require horizontal fitting since our width is known.
-        // Use the lower fitting size priority as we want to minimize our height consumption
-        // according to the layout's contraints and intrinsic size.
-        let size = headerView.systemLayoutSizeFitting(fittingSize,
-                                                      withHorizontalFittingPriority: .required,
-                                                      verticalFittingPriority: .fittingSizeLevel)
-        // Update the tableHeaderView itself. Classic.
-        var headerFrame = headerView.frame
-        headerFrame.size.height = size.height
-        headerView.frame = headerFrame
-        table.tableHeaderView = headerView
+        refreshTableViewHeaderLayout(table)
     }
 
     private func setupFooter() {
