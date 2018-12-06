1bb50d4efde0b683b85f8fe42b7ab67e82198983
Stephenie Harris <scoutharris@gmail.com>
2018-12-05 19:01:44 -0700

    Adding ‘View more’ with temporary alert.

--- WordPress/Classes/ViewRelated/Stats/Insights/Posting Activity/PostingActivityCell.swift
+++ WordPress/Classes/ViewRelated/Stats/Insights/Posting Activity/PostingActivityCell.swift
@@ -3,6 +3,12 @@ import UIKit
 class PostingActivityCell: UITableViewCell, NibLoadable {
 
     @IBOutlet weak var monthsStackView: UIStackView!
+    @IBOutlet weak var viewMoreLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        applyStyles()
+    }
 
     func configure(monthsData: [[PostingActivityDayData]]) {
         addMonths(monthsData: monthsData)
@@ -11,6 +17,11 @@ class PostingActivityCell: UITableViewCell, NibLoadable {
 }
 
 private extension PostingActivityCell {
+
+    func applyStyles() {
+        viewMoreLabel.text = NSLocalizedString("View more", comment: "Label for viewing more posting activity.")
+        viewMoreLabel.textColor = WPStyleGuide.Stats.actionTextColor
+    }
 
     func addMonths(monthsData: [[PostingActivityDayData]]) {
 
@@ -28,6 +39,15 @@ private extension PostingActivityCell {
             monthsStackView.removeArrangedSubview($0)
             $0.removeFromSuperview()
         }
+    }
+
+    @IBAction func didTapViewMoreButton(_ sender: UIButton) {
+        // TODO: show Posting Activity details
+        let alertController =  UIAlertController(title: "Posting Activity will be shown here.",
+                                                 message: nil,
+                                                 preferredStyle: .alert)
+        alertController.addCancelActionWithTitle("OK")
+        alertController.presentFromRootViewController()
     }
 
 }
