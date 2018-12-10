4a80511bb84ffdc7274ca03b82656bd8831ac16f
Nate Heagy <nate@heagy.com>
2018-12-10 13:59:39 -0800

    Add undo notice for loading revisions
    Works by grabbing a clone of the post’s `.original`, then setting that as the post.

--- WordPress/Classes/ViewRelated/Post/PostEditor+MoreOptions.swift
+++ WordPress/Classes/ViewRelated/Post/PostEditor+MoreOptions.swift
@@ -1,5 +1,5 @@
 import Foundation
-
+import WordPressFlux
 
 extension PostEditor where Self: UIViewController {
 
@@ -22,10 +22,28 @@ extension PostEditor where Self: UIViewController {
 
     func displayHistory() {
         let revisionsViewController = RevisionsTableViewController(post: post) { [weak self] revision in
-            if let post = self?.post.update(from: revision) {
-                DispatchQueue.main.async {
-                    self?.post = post
+            guard let self = self else {
+                return
+            }
+            let post = self.post.update(from: revision)
+
+            // show the notice with undo button
+            let notice = Notice(title: "Revision loaded", message: nil, feedbackType: .success, notificationInfo: nil, actionTitle: "Undo", cancelTitle: nil) { [weak self] (happened) in
+                guard happened, let self = self else {
+                    return
                 }
+                DispatchQueue.main.async { [weak self] in
+                    guard let self = self, let original = self.post.original else {
+                        return
+                    }
+                    let clone = self.post.clone(from: original)
+                    self.post = clone
+                }
+            }
+            ActionDispatcher.dispatch(NoticeAction.post(notice))
+
+            DispatchQueue.main.async {
+                self.post = post
             }
         }
         navigationController?.pushViewController(revisionsViewController, animated: true)
