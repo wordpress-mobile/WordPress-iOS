5a8be7325576613b355f034fe62a31fbceb870e3
aerych <aerychj@gmail.com>
2018-12-21 08:13:25 -0600

    Plans: Removes remaining boilerplate from models.

--- WordPress/Classes/Models/PlanFeature.swift
+++ WordPress/Classes/Models/PlanFeature.swift
@@ -2,13 +2,7 @@ import Foundation
 import CoreData
 
 public class PlanFeature: NSManagedObject {
-
-    @nonobjc public class func fetchRequest() -> NSFetchRequest<PlanFeature> {
-        return NSFetchRequest<PlanFeature>(entityName: "PlanFeature")
-    }
-
     @NSManaged public var summary: String
     @NSManaged public var title: String
     @NSManaged public var slug: String
-
 }
--- WordPress/Classes/Models/PlanGroup.swift
+++ WordPress/Classes/Models/PlanGroup.swift
@@ -2,13 +2,7 @@ import Foundation
 import CoreData
 
 public class PlanGroup: NSManagedObject {
-
-    @nonobjc public class func fetchRequest() -> NSFetchRequest<PlanGroup> {
-        return NSFetchRequest<PlanGroup>(entityName: "PlanGroup")
-    }
-
     @NSManaged public var order: Int16
     @NSManaged public var name: String
     @NSManaged public var slug: String
-
 }
