b73753727a67a2d05739a33a80848a2d0f97fa16
Daniele Bogo <daniele.bogo@automattic.com>
2019-01-11 10:20:38 +0000

    Use media utility to reduce code implementation

--- WordPress/Classes/ViewRelated/Post/Revisions/Browser/Preview/RevisionPreviewTextViewManager.swift
+++ WordPress/Classes/ViewRelated/Post/Revisions/Browser/Preview/RevisionPreviewTextViewManager.swift
@@ -3,9 +3,17 @@ import Gridicons
 
 
 class RevisionPreviewTextViewManager: NSObject {
-    var blog: Blog?
+    var post: AbstractPost?
 
+    private let mediaUtility = EditorMediaUtility()
     private var activeMediaRequests = [ImageDownloader.Task]()
+
+    private enum Constants {
+        static let mediaPlaceholderImageSize = CGSize(width: 128, height: 128)
+
+        static let placeholderMediaLink = URL(string: "placeholder://")
+        static let placeholderDocumentLink = URL(string: "documentUploading://")
+    }
 }
 
 
@@ -30,31 +38,7 @@ extension RevisionPreviewTextViewManager: TextViewAttachmentDelegate {
     }
 
     func textView(_ textView: TextView, placeholderFor attachment: NSTextAttachment) -> UIImage {
-        return placeholderImage(for: attachment)
-    }
-
-    func fetchPosterImageFor(videoAttachment: VideoAttachment, onSuccess: @escaping (UIImage) -> (), onFailure: @escaping () -> ()) {
-        guard let videoSrcURL = videoAttachment.url,
-            let placeholderURL = URL(string: "placeholder://"),
-            videoSrcURL != placeholderURL,
-            videoAttachment.posterURL == nil else {
-            onFailure()
-            return
-        }
-
-        let thumbnailGenerator = MediaVideoExporter(url: videoSrcURL)
-        thumbnailGenerator.exportPreviewImageForVideo(atURL: videoSrcURL, imageOptions: nil, onCompletion: { (exportResult) in
-            guard let image = UIImage(contentsOfFile: exportResult.url.path) else {
-                onFailure()
-                return
-            }
-            DispatchQueue.main.async {
-                onSuccess(image)
-            }
-        }, onError: { (error) in
-            DDLogError("Unable to grab frame from video = \(videoSrcURL). Details: \(error.localizedDescription)")
-            onFailure()
-        })
+        return mediaUtility.placeholderImage(for: attachment, size: Constants.mediaPlaceholderImageSize)
     }
 
     /* These 3 functions are mandatory implemented but not needed
@@ -67,73 +51,22 @@ extension RevisionPreviewTextViewManager: TextViewAttachmentDelegate {
 
 
 private extension RevisionPreviewTextViewManager {
+    private func fetchPosterImageFor(videoAttachment: VideoAttachment, onSuccess: @escaping (UIImage) -> (), onFailure: @escaping () -> ()) {
+        guard let videoSrcURL = videoAttachment.url,
+            videoSrcURL != Constants.placeholderMediaLink,
+            videoAttachment.posterURL == nil else {
+                onFailure()
+                return
+        }
+        mediaUtility.fetchPosterImage(for: videoSrcURL, onSuccess: onSuccess, onFailure: onFailure)
+    }
+
     private func downloadImage(from url: URL, success: @escaping (UIImage) -> Void, onFailure failure: @escaping () -> Void) {
-        guard let blog = blog else {
+        guard let post = post else {
             return
         }
 
-        var requestURL = url
-        let imageMaxDimension = max(UIScreen.main.bounds.size.width, UIScreen.main.bounds.size.height)
-        //use height zero to maintain the aspect ratio when fetching
-        var size = CGSize(width: imageMaxDimension, height: 0)
-        let request: URLRequest
-
-        if url.isFileURL {
-            request = URLRequest(url: url)
-        } else if blog.isPrivate() {
-            // private wpcom image needs special handling.
-            // the size that WPImageHelper expects is pixel size
-            size.width = size.width * UIScreen.main.scale
-            requestURL = WPImageURLHelper.imageURLWithSize(size, forImageURL: requestURL)
-            request = PrivateSiteURLProtocol.requestForPrivateSite(from: requestURL)
-        } else if !blog.isHostedAtWPcom && blog.isBasicAuthCredentialStored() {
-            size.width = size.width * UIScreen.main.scale
-            requestURL = WPImageURLHelper.imageURLWithSize(size, forImageURL: requestURL)
-            request = URLRequest(url: requestURL)
-        } else {
-            // the size that PhotonImageURLHelper expects is points size
-            requestURL = PhotonImageURLHelper.photonURL(with: size, forImageURL: requestURL)
-            request = URLRequest(url: requestURL)
-        }
-
-        let receipt = ImageDownloader.shared.downloadImage(for: request) { [weak self] (image, error) in
-            guard let _ = self else {
-                return
-            }
-
-            DispatchQueue.main.async {
-                guard let image = image else {
-                    DDLogError("Unable to download image for attachment with url = \(url). Details: \(String(describing: error?.localizedDescription))")
-                    failure()
-                    return
-                }
-
-                success(image)
-            }
-        }
-
+        let receipt = mediaUtility.downloadImage(from: url, post: post, success: success, onFailure: failure)
         activeMediaRequests.append(receipt)
-    }
-
-    private func placeholderImage(for attachment: NSTextAttachment) -> UIImage {
-        let size = CGSize(width: 128, height: 128)
-        let iconType = getIconType(for: attachment)
-        let icon = Gridicon.iconOfType(iconType, withSize: size)
-        icon.addAccessibilityForAttachment(attachment)
-        return icon
-    }
-
-    private func getIconType(for attachment: NSTextAttachment) -> GridiconType {
-        guard let url = URL(string: "documentUploading://") else {
-            preconditionFailure("Invalid static URL string: documentUploading://")
-        }
-        switch attachment {
-        case let imageAttachment as ImageAttachment:
-            return (imageAttachment.url == url) ? .pages : .image
-        case _ as VideoAttachment:
-            return .video
-        default:
-            return .attachment
-        }
     }
 }
