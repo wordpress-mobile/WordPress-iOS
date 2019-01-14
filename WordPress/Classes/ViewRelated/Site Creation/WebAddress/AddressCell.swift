e28ced40a6e69aef53f4ac4ae574a177d6638dc6
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2019-01-14 09:33:37 +0800

    First iteration of styling the AddressCell

--- WordPress/Classes/ViewRelated/Site Creation/WebAddress/AddressCell.swift
+++ WordPress/Classes/ViewRelated/Site Creation/WebAddress/AddressCell.swift
@@ -6,7 +6,7 @@ final class AddressCell: UITableViewCell, ModelSettableCell {
 
     var model: DomainSuggestion? {
         didSet {
-            title.text = model?.domainName
+            title.attributedText = processName(model?.domainName)
         }
     }
 
@@ -16,7 +16,33 @@ final class AddressCell: UITableViewCell, ModelSettableCell {
     }
 
     override func prepareForReuse() {
-        title.text = ""
+        title.attributedText = nil
+    }
+
+    private func processName(_ domainName: String?) -> NSAttributedString? {
+        guard let name = domainName else {
+            return nil
+        }
+
+        let components = name.components(separatedBy: ".")
+
+        guard let customName = components.first else {
+            return nil
+        }
+
+        let regularAttributes: [NSAttributedString.Key: Any] = [.font: WPStyleGuide.fontForTextStyle(.body, fontWeight: .regular),
+                                                                   .foregroundColor: WPStyleGuide.grey()]
+        let fullString = NSMutableAttributedString(string: name, attributes: regularAttributes)
+
+        let rangeOfDomain = NSRange(location: 0, length: customName.count)
+
+
+        let customNameAttributes: [NSAttributedString.Key: Any] = [.font: WPStyleGuide.fontForTextStyle(.body, fontWeight: .regular),
+                                                                   .foregroundColor: WPStyleGuide.darkGrey()]
+
+        fullString.setAttributes(customNameAttributes, range: rangeOfDomain)
+
+        return fullString
     }
 
     private func styleTitle() {
