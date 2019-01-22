a1cc5e5fb44b7b6b77cbefee9ee9fe0d37cdd542
Daniele Bogo <daniele.bogo@automattic.com>
2019-01-22 14:54:03 +0000

    Add new classes

---
+++ WordPress/Classes/ViewRelated/Blog/QuickStartChecklistManager.swift
@@ -0,0 +1,9 @@
+//
+//  QuickStartChecklistManager.swift
+//  WordPress
+//
+//  Created by Daniele Bogo on 22/01/2019.
+//  Copyright © 2019 WordPress. All rights reserved.
+//
+
+import Foundation
