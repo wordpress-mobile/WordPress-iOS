e281533e29d68f8fea80b2d63c6a1fb5fea02541
Daniele Bogo <daniele.bogo@automattic.com>
2019-02-06 16:18:32 +0000

    Move extension into Manager file

--- WordPress/Classes/ViewRelated/Blog/QuickStartChecklistManager.swift
+++ WordPress/Classes/ViewRelated/Blog/QuickStartChecklistManager.swift
@@ -202,9 +202,35 @@ private extension QuickStartChecklistManager {
     }
 }
 
+private extension UITableView {
+    /// Allows multiple insert/delete/reload/move calls to be animated simultaneously.
+    ///
+    /// - Parameters:
+    ///   - update: The block that performs the relevant insert, delete, reload, or move operations.
+    ///   - completion: A completion handler block to execute when all of the operations are finished. The Boolean value indicating whether the animations completed successfully. The value of this parameter is false if the animations were interrupted for any reason. On iOS 10 the value is always true.
+    func perform(update: (UITableView) -> Void, _ completion: ((UITableView, Bool) -> Void)? = nil) {
+        if #available(iOS 11.0, *) {
+            performBatchUpdates({
+                update(self)
+            }) { success in
+                completion?(self, success)
+            }
+        } else {
+            CATransaction.begin()
+            CATransaction.setCompletionBlock {
+                completion?(self, true)
+            }
+            beginUpdates()
+            update(self)
+            endUpdates()
+            CATransaction.commit()
+        }
+    }
+}
+
 private enum Sections: Int, CaseIterable {
-    static let footerHeight = CGFloat(20.0)
-    static let headerHeight = CGFloat(44.0)
+    static let footerHeight: CGFloat = 20.0
+    static let headerHeight: CGFloat = 44.0
 
     case todo
     case completed
