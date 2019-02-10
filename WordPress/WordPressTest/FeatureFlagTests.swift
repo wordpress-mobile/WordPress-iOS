bc1128130c70f7abb4d41050021a1a029c4f33e3
stevebaranski <steven.baranski@icloud.com>
2019-02-10 09:55:33 -0800

    Removed obsolete bottom sheet demo

--- WordPress/WordPressTest/FeatureFlagTests.swift
+++ WordPress/WordPressTest/FeatureFlagTests.swift
@@ -32,34 +32,4 @@ final class FeatureFlagTests: XCTestCase {
             XCTAssertFalse(actualValue, "Enhanced site creation should be disabled for .appStore BuildConfiguration")
         }
     }
-
-    // MARK: Bottom Sheet Demo
-
-    func testFeatureFlag_BottomSheetDemo_Enabled_ForLocalDeveloperBuildConfiguration() {
-        BuildConfiguration.localDeveloper.test {
-            let actualValue = FeatureFlag.bottomSheetDemo.enabled
-            XCTAssertTrue(actualValue, "Bottom sheet demo should be enabled for .localDeveloper BuildConfiguration")
-        }
-    }
-
-    func testFeatureFlag_BottomSheetDemo_Disabled_ForBranchTestBuildConfiguration() {
-        BuildConfiguration.a8cBranchTest.test {
-            let actualValue = FeatureFlag.bottomSheetDemo.enabled
-            XCTAssertFalse(actualValue, "Bottom sheet demo should be should be disabled for .a8cBranchTest BuildConfiguration")
-        }
-    }
-
-    func testFeatureFlag_BottomSheetDemo_Disabled_ForPrereleaseTestingBuildConfiguration() {
-        BuildConfiguration.a8cPrereleaseTesting.test {
-            let actualValue = FeatureFlag.bottomSheetDemo.enabled
-            XCTAssertFalse(actualValue, "Bottom sheet demo should be should be disabled for .a8cPrereleaseTesting BuildConfiguration")
-        }
-    }
-
-    func testFeatureFlag_BottomSheetDemo_Disabled_ForAppStoreBuildConfiguration() {
-        BuildConfiguration.appStore.test {
-            let actualValue = FeatureFlag.bottomSheetDemo.enabled
-            XCTAssertFalse(actualValue, "Bottom sheet demo should be should be disabled for .appStore BuildConfiguration")
-        }
-    }
 }
