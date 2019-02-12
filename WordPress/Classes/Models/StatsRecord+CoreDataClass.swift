49a7d36b3f0015d1bc213cfb7383b4822a4e6a26
Jan Klausa <jan@klausa.pl>
2019-02-12 20:32:30 +0100

    Add `TopCommentedPostStatsRecordValue` entity.

--- WordPress/Classes/Models/StatsRecord+CoreDataClass.swift
+++ WordPress/Classes/Models/StatsRecord+CoreDataClass.swift
@@ -27,6 +27,7 @@ import CoreData
 public enum StatsRecordType: Int16 {
     case lastPostInsight
     case allTimeStatsInsight
+    case topCommentedPosts
 
     case postStats
     case blogStats
@@ -37,7 +38,7 @@ public enum StatsRecordType: Int16 {
         // lot of sense to hold on to Insights from the past...).
         // This lets us disambiguate between which is which.
         switch self {
-        case .lastPostInsight, .allTimeStatsInsight:
+        case .lastPostInsight, .allTimeStatsInsight, .topCommentedPosts:
             return false
         case .postStats, .blogStats:
             return true
