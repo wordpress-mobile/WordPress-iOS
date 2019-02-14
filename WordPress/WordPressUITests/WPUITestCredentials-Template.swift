cb6a0e7018a9e89df8071052c1ca82c2110b4b4e
rachelmcr <rachel@automattic.com>
2019-02-14 13:27:10 +0000

    Use mobile secrets credentials for UI tests

---
+++ WordPress/WordPressUITests/WPUITestCredentials-Template.swift
@@ -0,0 +1,8 @@
+// Use the same login here that you did when you created an App
+// in the WordPress.com Application Manager. If you're not sure how to do that,
+// you can read more here: https://github.com/wordpress-mobile/WordPress-iOS#setup-credentials
+struct WPUITestCredentials {
+    let testUserEmail: String = <# Your WordPress.com Email #>
+    let testUserPassword: String = <# Your WordPress.com Password #>
+    let testSiteAddress: String =  <# Your WordPress.com Site Address #>
+}
