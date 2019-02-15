3859d304e65c0ae991bd2f0f27782331494d34c7
Daniele Bogo <daniele.bogo@automattic.com>
2019-02-15 17:28:47 +0000

    Add new events name

--- WordPress/Classes/Utility/Analytics/WPAnalyticsTrackerAutomatticTracks.m
+++ WordPress/Classes/Utility/Analytics/WPAnalyticsTrackerAutomatticTracks.m
@@ -1225,6 +1225,33 @@ NSString *const TracksUserDefaultsLoggedInUserIDKey = @"TracksLoggedInUserID";
         case WPAnalyticsStatQuickStartTourCompleted:
             eventName = @"quick_start_task_completed";
             break;
+        case WPAnalyticsStatQuickStartMigrationDialogViewed:
+            eventName = @"quick_start_migration_dialog_viewed";
+            break;
+        case WPAnalyticsStatQuickStartMigrationDialogPositiveTapped:
+            eventName = @"quick_start_migration_dialog_positive_tapped";
+            break;
+        case WPAnalyticsStatQuickStartRemoveDialogButtonTapped:
+            eventName = @"quick_start_remove_dialog_button_tapped";
+            break;
+        case WPAnalyticsStatQuickStartTypeDismissed:
+            eventName = @"quick_start_type_dismissed";
+            break;
+        case WPAnalyticsStatQuickStartListCollapsed:
+            eventName = @"quick_start_list_collapsed";
+            break;
+        case WPAnalyticsStatQuickStartListExpanded:
+            eventName = @"quick_start_list_expanded";
+            break;
+        case WPAnalyticsStatQuickStartItemSkipped:
+            eventName = @"quick_start_list_item_skipped";
+            break;
+        case WPAnalyticsStatQuickStartNotificationStarted:
+            eventName = @"quick_start_notification_started";
+            break;
+        case WPAnalyticsStatQuickStartNotificationTapped:
+            eventName = @"quick_start_notification_tapped";
+            break;
         case WPAnalyticsStatReaderAccessed:
             eventName = @"reader_accessed";
             break;
