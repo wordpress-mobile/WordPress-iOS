560591779f1044dee4941a93e99d42464b79369b
Jeremy Massel <jeremy.massel@gmail.com>
2019-02-15 15:41:06 -0700

    Remove isAccessibilityElement on page/post tables

--- WordPress/Classes/ViewRelated/Post/PostListViewController.swift
+++ WordPress/Classes/ViewRelated/Post/PostListViewController.swift
@@ -221,7 +221,8 @@ class PostListViewController: AbstractPostListViewController, UIViewControllerRe
     override func configureTableView() {
         tableView.accessibilityIdentifier = "PostsTable"
         tableView.isAccessibilityElement = true
-        tableView.estimatedRowHeight = postCardEstimatedRowHeight
+        tableView.separatorStyle = .none
+        tableView.estimatedRowHeight = type(of: self).postCardEstimatedRowHeight
         tableView.rowHeight = UITableView.automaticDimension
         tableView.separatorStyle = .none
 
