726d6cdeedf74ecfddb85376bcea382d7f3c3b6a
Tugdual de Kerviler <dekervit@gmail.com>
2019-02-15 10:52:35 +0100

    Move gutenberg strings to InfoPListTranslator

--- WordPress/Classes/Utility/InfoPListTranslator.m
+++ WordPress/Classes/Utility/InfoPListTranslator.m
@@ -11,6 +11,24 @@
     NSLocalizedString(@"To add photos or videos to your posts.", @"NSPhotoLibraryAddUsageDescription: Sentence to justify why the app asks permission from the user to access is Media Library.");
     NSLocalizedString(@"Enable microphone access to record sound in your videos.", @"NSMicrophoneUsageDescription: Sentence to justify why the app asks permission from the user to access the device microphone.");
     NSLocalizedString(@"Save as Draft", @"WordPressDraftActionExtension.CFBundleDisplayName and WordPressDraftActionExtension.CFBundleName: The localised name of the Draft Extension. It will be displayed in the system Share Sheets.");
+
+    // gutenberg mobile strings
+    NSLocalizedString(@"Add text or type / to add content");
+    NSLocalizedString(@"Add URL");
+    NSLocalizedString(@"Alt Text");
+    NSLocalizedString(@"Reset to Original");
+    NSLocalizedString(@"Failed to insert media.\nPlease tap for options.");
+    NSLocalizedString(@"Upload a new image or select a file from your library.");
+    NSLocalizedString(@"Device Library");
+    NSLocalizedString(@"Take photo");
+    NSLocalizedString(@"Start writing or press ⊕ to add content");
+    NSLocalizedString(@"Unsupported Block");
+    NSLocalizedString(@"Unsupported block type.");
+    NSLocalizedString(@"ADD BLOCK");
+    NSLocalizedString(@"ADD BLOCK HERE");
+    NSLocalizedString(@"Move block up");
+    NSLocalizedString(@"Move block down" );
+    NSLocalizedString(@"Remove content");
 }
 
 @end
