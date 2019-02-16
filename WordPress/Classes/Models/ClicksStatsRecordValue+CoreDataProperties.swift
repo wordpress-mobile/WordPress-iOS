7f4fc4629896be09362b02fe221cb378b944eac9
Jan Klausa <jan@klausa.pl>
2019-02-16 03:47:49 +0100

    fix model to have ordered relations

--- WordPress/Classes/Models/ClicksStatsRecordValue+CoreDataProperties.swift
+++ WordPress/Classes/Models/ClicksStatsRecordValue+CoreDataProperties.swift
@@ -10,15 +10,32 @@ extension ClicksStatsRecordValue {
 
     @NSManaged public var clicksCount: Int64
     @NSManaged public var label: String?
-    @NSManaged public var countryName: String?
     @NSManaged public var urlString: String?
-    @NSManaged public var children: NSSet?
+    @NSManaged public var children: NSOrderedSet?
     @NSManaged public var parent: ClicksStatsRecordValue?
 
 }
 
 // MARK: Generated accessors for children
 extension ClicksStatsRecordValue {
+
+    @objc(insertObject:inChildrenAtIndex:)
+    @NSManaged public func insertIntoChildren(_ value: ClicksStatsRecordValue, at idx: Int)
+
+    @objc(removeObjectFromChildrenAtIndex:)
+    @NSManaged public func removeFromChildren(at idx: Int)
+
+    @objc(insertChildren:atIndexes:)
+    @NSManaged public func insertIntoChildren(_ values: [ClicksStatsRecordValue], at indexes: NSIndexSet)
+
+    @objc(removeChildrenAtIndexes:)
+    @NSManaged public func removeFromChildren(at indexes: NSIndexSet)
+
+    @objc(replaceObjectInChildrenAtIndex:withObject:)
+    @NSManaged public func replaceChildren(at idx: Int, with value: ClicksStatsRecordValue)
+
+    @objc(replaceChildrenAtIndexes:withChildren:)
+    @NSManaged public func replaceChildren(at indexes: NSIndexSet, with values: [ClicksStatsRecordValue])
 
     @objc(addChildrenObject:)
     @NSManaged public func addToChildren(_ value: ClicksStatsRecordValue)
@@ -27,9 +44,9 @@ extension ClicksStatsRecordValue {
     @NSManaged public func removeFromChildren(_ value: ClicksStatsRecordValue)
 
     @objc(addChildren:)
-    @NSManaged public func addToChildren(_ values: NSSet)
+    @NSManaged public func addToChildren(_ values: NSOrderedSet)
 
     @objc(removeChildren:)
-    @NSManaged public func removeFromChildren(_ values: NSSet)
+    @NSManaged public func removeFromChildren(_ values: NSOrderedSet)
 
 }
--- WordPress/Classes/Models/ReferrerStatsRecordValue+CoreDataProperties.swift
+++ WordPress/Classes/Models/ReferrerStatsRecordValue+CoreDataProperties.swift
@@ -1,4 +1,3 @@
-
 import Foundation
 import CoreData
 
@@ -9,17 +8,35 @@ extension ReferrerStatsRecordValue {
         return NSFetchRequest<ReferrerStatsRecordValue>(entityName: "ReferrerStatsRecordValue")
     }
 
-    @NSManaged public var viewsCount: Int64
     @NSManaged public var iconURLString: String?
     @NSManaged public var label: String?
     @NSManaged public var urlString: String?
-    @NSManaged public var children: NSSet?
+    @NSManaged public var viewsCount: Int64
+    @NSManaged public var children: NSOrderedSet?
     @NSManaged public var parent: ReferrerStatsRecordValue?
 
 }
 
 // MARK: Generated accessors for children
 extension ReferrerStatsRecordValue {
+
+    @objc(insertObject:inChildrenAtIndex:)
+    @NSManaged public func insertIntoChildren(_ value: ReferrerStatsRecordValue, at idx: Int)
+
+    @objc(removeObjectFromChildrenAtIndex:)
+    @NSManaged public func removeFromChildren(at idx: Int)
+
+    @objc(insertChildren:atIndexes:)
+    @NSManaged public func insertIntoChildren(_ values: [ReferrerStatsRecordValue], at indexes: NSIndexSet)
+
+    @objc(removeChildrenAtIndexes:)
+    @NSManaged public func removeFromChildren(at indexes: NSIndexSet)
+
+    @objc(replaceObjectInChildrenAtIndex:withObject:)
+    @NSManaged public func replaceChildren(at idx: Int, with value: ReferrerStatsRecordValue)
+
+    @objc(replaceChildrenAtIndexes:withChildren:)
+    @NSManaged public func replaceChildren(at indexes: NSIndexSet, with values: [ReferrerStatsRecordValue])
 
     @objc(addChildrenObject:)
     @NSManaged public func addToChildren(_ value: ReferrerStatsRecordValue)
@@ -28,9 +45,9 @@ extension ReferrerStatsRecordValue {
     @NSManaged public func removeFromChildren(_ value: ReferrerStatsRecordValue)
 
     @objc(addChildren:)
-    @NSManaged public func addToChildren(_ values: NSSet)
+    @NSManaged public func addToChildren(_ values: NSOrderedSet)
 
     @objc(removeChildren:)
-    @NSManaged public func removeFromChildren(_ values: NSSet)
+    @NSManaged public func removeFromChildren(_ values: NSOrderedSet)
 
 }
