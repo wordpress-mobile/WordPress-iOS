584ca47a1b102456050976bbcfacad2bc4849b0c
Diego Rey Mendez <diegoreymendez@gmail.com>
2019-02-19 18:35:50 -0300

    Increments the error margin of a test.

--- WordPress/WordPressTest/DebouncerTests.swift
+++ WordPress/WordPressTest/DebouncerTests.swift
@@ -7,7 +7,7 @@ class DebouncerTests: XCTestCase {
     ///
     func testDebouncerRunsNormally() {
         let timerDelay = 0.5
-        let allowedError = 0.25
+        let allowedError = 0.3
         let minDelay = timerDelay * (1 - allowedError)
         let maxDelay = timerDelay * (1 + allowedError)
         let testTimeout = maxDelay + 0.01
