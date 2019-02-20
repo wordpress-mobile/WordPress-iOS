f7470038e6011b15ca617ce1d823d8c551192b6d
Nate Heagy <nate@heagy.com>
2019-02-20 13:18:34 -0800

    Update QS fancy alert

--- WordPress/Classes/ViewRelated/System/FancyAlertViewController+QuickStart.swift
+++ WordPress/Classes/ViewRelated/System/FancyAlertViewController+QuickStart.swift
@@ -1,6 +1,14 @@
 extension FancyAlertViewController {
     private struct Strings {
         static let titleText = NSLocalizedString("Want a little help getting started?", comment: "Title of alert asking if users want to try out the quick start checklist.")
+        static let bodyText = NSLocalizedString("We’ll walk you through the basics of building and growing your site", comment: "Body text of alert asking if users want to try out the quick start checklist.")
+        static let allowButtonText = NSLocalizedString("Yes, help me", comment: "Allow button title shown in alert asking if users want to try out the quick start checklist.")
+        static let notNowText = NSLocalizedString("Not This Time", comment: "Not this time button title shown in alert asking if users want to try out the quick start checklist.")
+    }
+
+    // MARK: TODO: delete these when the .quickStartV2 feature flag is removed
+    private struct StringsV1 {
+        static let titleText = NSLocalizedString("Want a little help getting started?", comment: "Title of alert asking if users want to try out the quick start checklist.")
         static let bodyText = NSLocalizedString("Our Quick Start checklist walks you through the basics of setting up a new website.", comment: "Body text of alert asking if users want to try out the quick start checklist.")
         static let allowButtonText = NSLocalizedString("Yes", comment: "Allow button title shown in alert asking if users want to try out the quick start checklist.")
         static let notNowText = NSLocalizedString("Not This Time", comment: "Not this time button title shown in alert asking if users want to try out the quick start checklist.")
@@ -17,6 +25,10 @@ extension FancyAlertViewController {
     /// - Parameter approveAction: block to call when approve is tapped
     /// - Returns: FancyAlertViewController of the primer
     @objc static func makeQuickStartAlertController(blog: Blog) -> FancyAlertViewController {
+        guard Feature.enabled(.quickStartV2) else {
+            return makeQuickStartAlertControllerV1(blog: blog)
+        }
+
         WPAnalytics.track(.quickStartRequestAlertViewed)
 
         let allowButton = ButtonConfig(Strings.allowButtonText) { controller, _ in
@@ -37,7 +49,44 @@ extension FancyAlertViewController {
             WPAnalytics.track(.quickStartRequestAlertButtonTapped, withProperties: ["type": "neutral"])
         }
 
-        let neverButton = ButtonConfig(Strings.neverText) { controller, _ in
+        let image = UIImage(named: "wp-illustration-tasks-complete-audience")
+
+        let config = FancyAlertViewController.Config(titleText: Strings.titleText,
+                                                     bodyText: Strings.bodyText,
+                                                     headerImage: image,
+                                                     dividerPosition: .bottom,
+                                                     defaultButton: allowButton,
+                                                     cancelButton: notNowButton,
+                                                     appearAction: {},
+                                                     dismissAction: {})
+
+        let controller = FancyAlertViewController.controllerWithConfiguration(configuration: config)
+        return controller
+    }
+
+    // MARK: TODO: delete this when the .quickStartV2 feature flag is removed
+    static func makeQuickStartAlertControllerV1(blog: Blog) -> FancyAlertViewController {
+        WPAnalytics.track(.quickStartRequestAlertViewed)
+
+        let allowButton = ButtonConfig(StringsV1.allowButtonText) { controller, _ in
+            controller.dismiss(animated: true)
+
+            guard let tourGuide = QuickStartTourGuide.find() else {
+                return
+            }
+
+            tourGuide.setup(for: blog)
+
+            WPAnalytics.track(.quickStartRequestAlertButtonTapped, withProperties: ["type": "positive"])
+        }
+
+        let notNowButton = ButtonConfig(StringsV1.notNowText) { controller, _ in
+            controller.dismiss(animated: true)
+            PushNotificationsManager.shared.deletePendingLocalNotifications()
+            WPAnalytics.track(.quickStartRequestAlertButtonTapped, withProperties: ["type": "neutral"])
+        }
+
+        let neverButton = ButtonConfig(StringsV1.neverText) { controller, _ in
             UserDefaults.standard.quickStartWasDismissedPermanently = true
             controller.dismiss(animated: true)
             PushNotificationsManager.shared.deletePendingLocalNotifications()
@@ -46,8 +95,8 @@ extension FancyAlertViewController {
 
         let image = UIImage(named: "wp-illustration-checklist")
 
-        let config = FancyAlertViewController.Config(titleText: Strings.titleText,
-                                                     bodyText: Strings.bodyText,
+        let config = FancyAlertViewController.Config(titleText: StringsV1.titleText,
+                                                     bodyText: StringsV1.bodyText,
                                                      headerImage: image,
                                                      dividerPosition: .bottom,
                                                      defaultButton: allowButton,
