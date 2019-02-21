c55331d494d102d09e419506202ba711671a5818
stevebaranski <steven.baranski@icloud.com>
2019-02-21 13:48:00 -0800

    Configured bar chart to match visual spec

--- WordPress/Classes/ViewRelated/Stats/Charts/StatsBarChartView.swift
+++ WordPress/Classes/ViewRelated/Stats/Charts/StatsBarChartView.swift
@@ -39,10 +39,73 @@ class StatsBarChartView: BarChartView {
     // MARK: Private behavior
 
     private func applyStyling() {
-        debugPrint(#function)
+        configureBarChartViewProperties()
+        configureBarLineChartViewBaseProperties()
+        configureChartViewBaseProperties()
+
+        configureXAxis()
+        configureYAxis()
     }
 
-    private func populateData() {
+    private func configureBarChartViewProperties() {
+        drawBarShadowEnabled = false
+        drawValueAboveBarEnabled = false
+        fitBars = true
+    }
+
+    private func configureBarLineChartViewBaseProperties() {
+        doubleTapToZoomEnabled = false
+        dragXEnabled = false
+        dragYEnabled = false
+        pinchZoomEnabled = false
+
+        rightAxis.enabled = false
+
+        drawBordersEnabled = false
+        drawGridBackgroundEnabled = false
+
+        minOffset = CGFloat(0)
+
+        scaleXEnabled = false
+        scaleYEnabled = false
+    }
+
+    private func configureChartViewBaseProperties() {
+        dragDecelerationEnabled = false
+        drawMarkers = false
+        highlightPerTapEnabled = false
+
+        extraRightOffset = CGFloat(20)
+
+        legend.enabled = false
+
+        let animationDuration = TimeInterval(1)
+        animate(yAxisDuration: animationDuration)
+    }
+
+    private func configureXAxis() {
+        xAxis.axisLineColor = styling.adornmentColor
+        xAxis.drawAxisLineEnabled = true
+        xAxis.drawGridLinesEnabled = false
+        xAxis.drawLabelsEnabled = true
+        xAxis.labelPosition = .bottom
+        xAxis.labelTextColor = styling.adornmentColor
+        xAxis.setLabelCount(2, force: true)
+        xAxis.valueFormatter = styling.xAxisValueFormatter
+    }
+
+    private func configureYAxis() {
+        let yAxis = leftAxis
+
+        yAxis.gridColor = styling.adornmentColor
+        yAxis.drawAxisLineEnabled = false
+        yAxis.drawLabelsEnabled = true
+        yAxis.drawZeroLineEnabled = true
+        yAxis.labelTextColor = styling.adornmentColor
+        yAxis.valueFormatter = styling.yAxisValueFormatter
+    }
+
+    private func configureAndPopulateData() {
         let barChartData = self.barChartData.barChartData
 
         if let dataSets = barChartData.dataSets as? [BarChartDataSet] {
@@ -61,6 +124,6 @@ class StatsBarChartView: BarChartView {
         translatesAutoresizingMaskIntoConstraints = false
 
         applyStyling()
-        populateData()
+        configureAndPopulateData()
     }
 }
