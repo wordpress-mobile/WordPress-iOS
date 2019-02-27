3b12f4c33dc72beaa999450fcab93d1d539eddb7
Shiki <jayson@basanes.net>
2019-02-27 06:46:31 -0700

    ReachabilityUtils: Add info about failing tests

--- WordPress/Classes/Utility/ReachabilityUtils.h
+++ WordPress/Classes/Utility/ReachabilityUtils.h
@@ -2,6 +2,19 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ * TODO This can be converted to Swift but these tests fail when we do:
+ *
+ * - ApproveCommentActionTests.testExecuteCallsApproveWhenIconIsOff()
+ * - ApproveCommentActionTests.testExecuteCallsUnapproveWhenIconIsOn()
+ * - ApproveCommentActionTests.testExecuteUpdatesIconAccessibilityHintWhenIconIsOff()
+ * - ApproveCommentActionTests.testExecuteUpdatesIconAccessibilityHintWhenIconIsOn()
+ * - ApproveCommentActionTests.testExecuteUpdatesIconAccessibilityLabelWhenIconIsOff()
+ * - ApproveCommentActionTests.testExecuteUpdatesIconAccessibilityLabelWhenIconIsOn()
+ * - ApproveCommentActionTests.testExecuteUpdatesIconTitleWhenIconIsOff()
+ * - ApproveCommentActionTests.testExecuteUpdatesIconTitleWhenIconIsOn()
+ * - TrashCommentActionTests.testExecuteCallsTrash()
+ */
 @interface ReachabilityUtils : NSObject
 
 + (BOOL)isInternetReachable;
