3560893edb09aee5fa83c5741ec9961a10c5f43f
Jeremy Massel <jeremy.massel@gmail.com>
2019-03-05 23:10:14 -0700

    Add gutenberg support

--- WordPress/Classes/ViewRelated/Pages/PageListTableViewCell.m
+++ WordPress/Classes/ViewRelated/Pages/PageListTableViewCell.m
@@ -95,6 +95,7 @@ static CGFloat const FeaturedImageSize = 120.0;
     [self configureBadges];
     [self configureTimeStamp];
     [self configureFeaturedImage];
+    [self configureAccessibility];
 }
 
 #pragma mark - Configuration
@@ -175,6 +176,11 @@ static CGFloat const FeaturedImageSize = 120.0;
                                              success:nil
                                                error:nil];
     }
+}
+
+- (void)configureAccessibility
+{
+    self.accessibilityIdentifier = self.post.slugForDisplay;
 }
 
 @end
