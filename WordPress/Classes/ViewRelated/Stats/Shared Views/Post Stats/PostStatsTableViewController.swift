09c9e4bf4b529566e244f9d5f5896afd14c0a615
Stephenie Harris <scoutharris@gmail.com>
2019-03-07 12:33:41 -0700

    Adding initial Post Stats table.

---
+++ WordPress/Classes/ViewRelated/Stats/Shared Views/Post Stats/PostStatsTableViewController.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+class PostStatsTableViewController: UITableViewController, StoryboardLoadable {
+
+    // MARK: - StoryboardLoadable Protocol
+
+    static var defaultStoryboardName = defaultControllerID
+
+    // MARK: - Properties
+
+    private typealias Style = WPStyleGuide.Stats
+
+    // MARK: - View
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = NSLocalizedString("Stats", comment: "Window title for Post Stats view.")
+        Style.configureTable(tableView)
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = UITableViewCell()
+
+
+        // Configure the cell...
+
+        return cell
+    }
+
+}
