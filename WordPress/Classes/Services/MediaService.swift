58e50e1079d18146a478d91f092d65883d8183ce
Sergio Estevao <sergioestevao@gmail.com>
2019-03-12 09:29:10 +0000

    Delete media object that don’t have a local file set and aren’t published.

--- WordPress/Classes/Services/MediaService.swift
+++ WordPress/Classes/Services/MediaService.swift
@@ -14,11 +14,17 @@ extension MediaService {
             let fetch = NSFetchRequest<Media>(entityName: Media.classNameWithoutNamespaces())
             let pushingPredicate = NSPredicate(format: "remoteStatusNumber = %@", NSNumber(value: MediaRemoteStatus.pushing.rawValue))
             let processingPredicate = NSPredicate(format: "remoteStatusNumber = %@", NSNumber(value: MediaRemoteStatus.processing.rawValue))
-            fetch.predicate = NSCompoundPredicate(orPredicateWithSubpredicates: [pushingPredicate, processingPredicate])
+            let errorPredicate = NSPredicate(format: "remoteStatusNumber = %@", NSNumber(value: MediaRemoteStatus.failed.rawValue))
+            fetch.predicate = NSCompoundPredicate(orPredicateWithSubpredicates: [pushingPredicate, processingPredicate, errorPredicate])
             do {
                 let mediaPushing = try self.managedObjectContext.fetch(fetch)
                 for media in mediaPushing {
-                    media.remoteStatus = .failed
+                    if (media.remoteStatus == .pushing || media.remoteStatus == .processing) {
+                        media.remoteStatus = .failed
+                    } else if media.remoteStatus == .failed,
+                        let error = media.error as NSError?, error.domain == MediaServiceErrorDomain && error.code == MediaServiceError.fileDoesNotExist.rawValue {
+                        self.managedObjectContext.delete(media)
+                    }
                 }
 
                 ContextManager.sharedInstance().save(self.managedObjectContext, withCompletionBlock: {
