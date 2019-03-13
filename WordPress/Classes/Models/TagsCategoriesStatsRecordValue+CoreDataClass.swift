61713d3b39a1c051a312ed7c807fba470623581a
Jan Klausa <jan@klausa.pl>
2019-03-13 04:18:47 +0100

    add support for changing publicize/tags and categories insights into core data entities

--- WordPress/Classes/Models/TagsCategoriesStatsRecordValue+CoreDataClass.swift
+++ WordPress/Classes/Models/TagsCategoriesStatsRecordValue+CoreDataClass.swift
@@ -30,5 +30,47 @@ public class TagsCategoriesStatsRecordValue: StatsRecordValue {
             }
         }
     }
+}
+
+fileprivate extension TagsCategoriesStatsRecordValue {
+    convenience init?(context: NSManagedObjectContext, tagCategory: StatsTagAndCategory) {
+        self.init(context: context)
+
+        self.name = tagCategory.name
+        self.urlString = tagCategory.url?.absoluteString
+        self.viewsCount = Int64(tagCategory.viewsCount ?? 0)
+
+        switch tagCategory.kind {
+        case .category:
+            self.type = TagsCategoriesType.category.rawValue
+        case .folder:
+            self.type = TagsCategoriesType.folder.rawValue
+        case .tag:
+            self.type = TagsCategoriesType.tag.rawValue
+        }
+
+        let children = tagCategory.children.compactMap { TagsCategoriesStatsRecordValue(context: context, tagCategory: $0) }
+
+        self.children = NSOrderedSet(array: children)
+    }
+}
+
+extension StatsTagsAndCategoriesInsight: StatsRecordValueConvertible {
+    func statsRecordValues(in context: NSManagedObjectContext) -> [StatsRecordValue] {
+        return topTagsAndCategories.compactMap {
+            return TagsCategoriesStatsRecordValue(context: context, tagCategory: $0)
+        }
+    }
+
+    init(statsRecordValue: StatsRecordValue) {
+        // We won't be needing those until later. I added them to protocol to show the intended design
+        // but it doesn't make sense to implement it yet.
+        fatalError("This shouldn't be called yet — implementation of StatsRecordValueConvertible is still in progres. This method was added to illustrate intended design, but isn't ready yet.")
+    }
+
+    static var recordType: StatsRecordType {
+        return .tagsAndCategories
+    }
+
 
 }
--- WordPress/WordPressTest/TagsCategoriesStatsRecordValueTests.swift
+++ WordPress/WordPressTest/TagsCategoriesStatsRecordValueTests.swift
@@ -1,4 +1,5 @@
 @testable import WordPress
+@testable import WordPressKit
 
 class TagsCategoriesStatsRecordValueTests: StatsTestCase {
 
@@ -134,5 +135,73 @@ class TagsCategoriesStatsRecordValueTests: StatsTestCase {
         XCTAssertNotNil(fetchedValue.linkURL)
     }
 
+    func testCoreDataConversionWorks() {
+        let standaloneTag = StatsTagAndCategory(name: "standalone", kind: .tag, url: "wp.com/tags/tag", viewsCount: 0, children: [])
+
+        let childTag = StatsTagAndCategory(name: "tag", kind: .tag, url: "whatever", viewsCount: 9001, children: [])
+        let childCategory = StatsTagAndCategory(name: "category", kind: .category, url: "", viewsCount: 9002, children: [])
+
+        let folderInsight = StatsTagAndCategory(name: "folder",
+                                                kind: .folder,
+                                                url: "https://wordpress.com",
+                                                viewsCount: 18003,
+                                                children: [childTag, childCategory])
+
+        let insight = StatsTagsAndCategoriesInsight(topTagsAndCategories: [folderInsight, standaloneTag])
+
+        let blog = defaultBlog()
+
+        _ = StatsRecord.record(from: insight, for: blog)
+
+        XCTAssertNoThrow(try mainContext.save())
+
+        let fetchRequest = StatsRecord.fetchRequest(for: .tagsAndCategories)
+
+        let result = try! mainContext.fetch(fetchRequest)
+        let statsRecord = result.first!
+
+        XCTAssertEqual(statsRecord.blog, blog)
+        XCTAssertEqual(statsRecord.period, StatsRecordPeriodType.notApplicable.rawValue)
+
+        let castedResults = statsRecord.values?.array as! [TagsCategoriesStatsRecordValue]
+
+        XCTAssertEqual(castedResults.count, 2)
+
+        let tag = castedResults.first { $0.type == TagsCategoriesType.tag.rawValue }
+        XCTAssertNotNil(tag)
+
+        XCTAssertEqual(tag?.name, "standalone")
+        XCTAssertEqual(tag?.type, TagsCategoriesType.tag.rawValue)
+        XCTAssertEqual(tag?.viewsCount, 0)
+        XCTAssertEqual(tag?.linkURL, URL(string: "wp.com/tags/tag"))
+        XCTAssertEqual(tag?.children?.count, 0)
+
+        let folder = castedResults.first { $0.type == TagsCategoriesType.folder.rawValue }
+
+        XCTAssertNotNil(folder)
+
+        XCTAssertEqual(folder?.name, "folder")
+        XCTAssertEqual(folder?.type, TagsCategoriesType.folder.rawValue)
+        XCTAssertEqual(folder?.viewsCount, 18003)
+        XCTAssertEqual(folder?.linkURL, URL(string: "https://wordpress.com"))
+
+        XCTAssertEqual(folder?.children?.count, 2)
+
+        let firstChild = folder?.children?[0] as? TagsCategoriesStatsRecordValue
+        let secondChild = folder?.children?[1] as? TagsCategoriesStatsRecordValue
+
+        XCTAssertNotNil(firstChild)
+        XCTAssertNotNil(secondChild)
+
+        XCTAssertEqual(firstChild?.name, "tag")
+        XCTAssertEqual(firstChild?.type, TagsCategoriesType.tag.rawValue)
+        XCTAssertEqual(firstChild?.viewsCount, 9001)
+        XCTAssertEqual(firstChild?.linkURL, URL(string: "whatever"))
+
+        XCTAssertEqual(secondChild?.name, "category")
+        XCTAssertEqual(secondChild?.type, TagsCategoriesType.category.rawValue)
+        XCTAssertEqual(secondChild?.viewsCount, 9002)
+        XCTAssertEqual(secondChild?.linkURL, nil)
+    }
 
 }
