30bb6546491e9a3457f3d1ef8d5773912abe2e6f
Jan Klausa <jan@klausa.pl>
2019-03-26 05:31:14 +0100

    add code to map TopViewedPostStatsRecordValue into both StatsPublishedPostsTimeIntervalData and StatsTopPostsTimeIntervalData

--- WordPress/Classes/Models/TopViewedPostStatsRecordValue+CoreDataClass.swift
+++ WordPress/Classes/Models/TopViewedPostStatsRecordValue+CoreDataClass.swift
@@ -50,4 +50,117 @@ public class TopViewedPostStatsRecordValue: StatsRecordValue {
         }
     }
 }
+
+extension TopViewedPostStatsRecordValue {
+    convenience init(managedObjectContext: NSManagedObjectContext, remotePost: StatsTopPost) {
+        self.init(context: managedObjectContext)
+
+        self.postID = Int64(remotePost.postID)
+        self.viewsCount = Int64(remotePost.viewsCount)
+        self.title = remotePost.title
+        self.postURLString = remotePost.postURL?.absoluteString
+        self.type = TopViewsPostType(kind: remotePost.kind).rawValue
+    }
+}
+
+extension StatsTopPost {
+    init?(topViewedPostStatsRecordValue: TopViewedPostStatsRecordValue) {
+        guard
+            let title = topViewedPostStatsRecordValue.title,
+            let postType = TopViewsPostType(rawValue: topViewedPostStatsRecordValue.type)
+            else {
+                return nil
+        }
+
+        self = StatsTopPost(title: title,
+                            postID: Int(topViewedPostStatsRecordValue.postID),
+                            postURL: topViewedPostStatsRecordValue.postURL,
+                            viewsCount: Int(topViewedPostStatsRecordValue.viewsCount),
+                            kind: postType.statsTopPostKind)
+    }
+}
+
+extension StatsPublishedPostsTimeIntervalData: TimeIntervalStatsRecordValueConvertible {
+    var recordPeriodType: StatsRecordPeriodType {
+        return StatsRecordPeriodType(remoteStatus: period)
+    }
+
+    var date: Date {
+        return periodEndDate
+    }
+
+    func statsRecordValues(in context: NSManagedObjectContext) -> [StatsRecordValue] {
+        return publishedPosts.compactMap { TopViewedPostStatsRecordValue(managedObjectContext: context, remotePost: $0) }
+    }
+
+    init?(statsRecordValues: [StatsRecordValue]) {
+        guard
+            let firstParent = statsRecordValues.first?.statsRecord,
+            let period = StatsRecordPeriodType(rawValue: firstParent.period),
+            let date = firstParent.date
+            else {
+                return nil
+        }
+
+        let posts = statsRecordValues
+            .compactMap { return $0 as? TopViewedPostStatsRecordValue }
+            .compactMap { StatsTopPost(topViewedPostStatsRecordValue: $0) }
+
+        self = StatsPublishedPostsTimeIntervalData(period: period.statsPeriodUnitValue,
+                                                   periodEndDate: date as Date,
+                                                   publishedPosts: posts)
+    }
+
+    static var recordType: StatsRecordType {
+        return .publishedPosts
+    }
+}
+
+extension StatsTopPostsTimeIntervalData: TimeIntervalStatsRecordValueConvertible {
+    var recordPeriodType: StatsRecordPeriodType {
+        return StatsRecordPeriodType(remoteStatus: period)
+    }
+
+    var date: Date {
+        return periodEndDate
+    }
+
+    func statsRecordValues(in context: NSManagedObjectContext) -> [StatsRecordValue] {
+        var posts: [StatsRecordValue] = topPosts.map { TopViewedPostStatsRecordValue(managedObjectContext: context, remotePost: $0) }
+
+        let otherAndTotalCount = OtherAndTotalViewsCountStatsRecordValue(context: context,
+                                                                         otherCount: otherViewsCount,
+                                                                         totalCount: totalViewsCount)
+
+        posts.append(otherAndTotalCount)
+
+        return posts
+    }
+
+    init?(statsRecordValues: [StatsRecordValue]) {
+        guard
+            let firstParent = statsRecordValues.first?.statsRecord,
+            let period = StatsRecordPeriodType(rawValue: firstParent.period),
+            let date = firstParent.date,
+            let otherAndTotalCount = statsRecordValues.compactMap({ $0 as? OtherAndTotalViewsCountStatsRecordValue }).first
+            else {
+                return nil
+        }
+
+
+        let posts = statsRecordValues
+            .compactMap { $0 as? TopViewedPostStatsRecordValue }
+            .compactMap { StatsTopPost(topViewedPostStatsRecordValue: $0) }
+
+        self = StatsTopPostsTimeIntervalData(period: period.statsPeriodUnitValue,
+                                             periodEndDate: date as Date,
+                                             topPosts: posts,
+                                             totalViewsCount: Int(otherAndTotalCount.totalCount),
+                                             otherViewsCount: Int(otherAndTotalCount.otherCount))
+    }
+
+    static var recordType: StatsRecordType {
+        return .topViewedPost
+    }
+
 }
