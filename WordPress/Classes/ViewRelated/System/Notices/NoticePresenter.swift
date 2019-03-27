wordpress-mobile/WordPress-iOS

commit 4cac38f8671cb7bc4b3265d036ff50f22d13abc8

Make second UIWindow hidden by default
This is the right way to do it in the first place.

WordPress/Classes/ViewRelated/System/Notices/NoticePresenter.swift
@@ -33,7 +33,6 @@ class NoticePresenter: NSObject {
         // however, since the alerts aren't permanently on screen, this isn't
         // often a problem.
         window.windowLevel = .alert
-        window.isHidden = false
 
         super.init()
 
@@ -107,6 +106,8 @@ class NoticePresenter: NSObject {
         if let feedbackType = notice.feedbackType {
             generator.notificationOccurred(feedbackType)
         }
+
+        window.isHidden = false
 
         animatePresentation(fromState: fromState, toState: toState, completion: {
             // Quick Start notices don't get automatically dismissed

commit 4762a9e354f43db9deb6d430d56bec33d4a020d2

Fix UntouchableWindow not auto-resizing on iPads
This regression happened in 4cac38f8. The `UntouchableWindow` rotation is fixed but this causes a bug in `MaskView` which will be fixed in a later commit.

WordPress/Classes/ViewRelated/System/Notices/NoticePresenter.swift
@@ -70,6 +70,14 @@ class NoticePresenter: NSObject {
 
         super.init()
 
+        // Make the window key and visible at first but hide it on the next UI loop. If this is not
+        // done, the window is not automatically resized when the device is rotated. This issue
+        // only happens on iPad devices.
+        window.makeKeyAndVisible()
+        DispatchQueue.main.async {
+            self.window.isHidden = true
+        }
+
         // Keep the storeReceipt to prevent the `onChange` subscription from being deactivated.
         storeReceipt = store.onChange { [weak self] in
             self?.onStoreChange()