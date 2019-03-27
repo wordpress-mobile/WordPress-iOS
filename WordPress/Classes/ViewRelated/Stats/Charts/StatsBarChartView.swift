e41d35fd53ae2b40fe557e84bf1e314d0da48a6e
stevebaranski <steven.baranski@icloud.com>
2019-03-27 13:07:54 -0700

    Adjusted top offset to prevent axis label clipping

--- WordPress/Classes/ViewRelated/Stats/Charts/StatsBarChartView.swift
+++ WordPress/Classes/ViewRelated/Stats/Charts/StatsBarChartView.swift
@@ -14,6 +14,7 @@ class StatsBarChartView: BarChartView {
         static let intrinsicHeight      = CGFloat(170)      // height via Zeplin
         static let highlightAlpha       = CGFloat(1)
         static let markerAlpha          = CGFloat(0.2)
+        static let topOffsetSansLegend  = CGFloat(5)
         static let trailingOffset       = CGFloat(20)
     }
 
@@ -195,7 +196,7 @@ class StatsBarChartView: BarChartView {
         NSLayoutConstraint.activate([
             chartLegend.widthAnchor.constraint(equalTo: widthAnchor)
         ])
-        extraTopOffset += chartLegend.intrinsicContentSize.height
+        extraTopOffset = chartLegend.intrinsicContentSize.height
 
         self.legendView = chartLegend
     }
@@ -214,13 +215,17 @@ class StatsBarChartView: BarChartView {
     private func configureYAxis() {
         let yAxis = leftAxis
 
-        xAxis.axisLineColor = styling.lineColor
+        yAxis.axisLineColor = styling.lineColor
         yAxis.gridColor = styling.lineColor
         yAxis.drawAxisLineEnabled = false
         yAxis.drawLabelsEnabled = true
         yAxis.drawZeroLineEnabled = true
         yAxis.labelTextColor = styling.labelColor
         yAxis.valueFormatter = styling.yAxisValueFormatter
+
+        // This adjustment is intended to prevent clipping observed with some labels
+        // Potentially relevant : https://github.com/danielgindi/Charts/issues/992
+        extraTopOffset = Constants.topOffsetSansLegend
     }
 
     private func drawChartMarker(for entry: ChartDataEntry) {
