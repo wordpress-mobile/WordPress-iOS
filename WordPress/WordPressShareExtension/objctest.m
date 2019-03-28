a2a01a29b08e0d714d135a9e746b39a747565754
Nate Heagy <nate@heagy.com>
2019-03-28 16:31:46 -0700

    Removing test file

--- WordPress/WordPressShareExtension/objctest.m
+++
@@ -1,9 +0,0 @@
-//
-//  objctest.m
-//  WordPressShareExtension
-//
-//  Created by Nate Heagy on 2019-03-14.
-//  Copyright © 2019 WordPress. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
