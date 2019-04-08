6ffcfbee724a233deac178bb75705cc4cf59eba5
Daniele Bogo <daniele.bogo@automattic.com>
2019-04-08 16:23:52 +0100

    Force to don’t load the nib

--- WordPress/Classes/ViewRelated/Jetpack/Install/JetpackRemoteInstallViewController.swift
+++ WordPress/Classes/ViewRelated/Jetpack/Install/JetpackRemoteInstallViewController.swift
@@ -26,6 +26,10 @@ class JetpackRemoteInstallViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
+    override func loadView() {
+        view = UIView()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
