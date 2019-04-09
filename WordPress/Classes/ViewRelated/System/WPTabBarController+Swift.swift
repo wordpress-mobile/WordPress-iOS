d7302d5076fe2a32899e22adcddd763a2c32aed7
Shiki <jayson@basanes.net>
2019-04-09 13:12:57 -0600

    Do not track tab access the VC is not visible

--- WordPress/Classes/ViewRelated/System/WPTabBarController+Swift.swift
+++ WordPress/Classes/ViewRelated/System/WPTabBarController+Swift.swift
@@ -21,13 +21,22 @@ extension WPTabBarController {
 
     /// Count the current tab as "accessed" in analytics.
     ///
-    /// We want to call this on:
+    /// We want to call this when:
     ///
     /// - The app has been placed in the foreground
     /// - The app was just launched and we restored the previously selected tab
     ///   (in `decodeRestorableStateWithCoder`)
     /// - The user selected a different tab
     @objc func trackAccessStatForTabIndex(_ tabIndex: Int) {
+        // Since this ViewController is a singleton, it can be active **behind** the login view.
+        // The `isViewonScreen()` prevents us from tracking this.
+        //
+        // The `presentedViewController` check is to avoid tracking while a modal dialog is shown
+        // and the app is placed in the background and back to foreground.
+        guard isViewOnScreen(), presentedViewController == nil else {
+            return
+        }
+
         guard let tabType = WPTabType(rawValue: UInt(tabIndex)),
             let stat = WPTabBarController.tabIndexToStatMap[tabType] else {
                 return
