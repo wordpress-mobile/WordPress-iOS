bc8d15963e3cf70fd78df837c3aa5351bebe15ce
James Treanor <jtreanor3@gmail.com>
2019-04-11 09:21:53 +0100

    Fix loading WordPressAuthenticator xibs

--- WordPress/Classes/ViewRelated/NUX/DomainSuggestionsTableViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/DomainSuggestionsTableViewController.swift
@@ -60,7 +60,7 @@ class DomainSuggestionsTableViewController: NUXTableViewController {
     override func awakeFromNib() {
         super.awakeFromNib()
 
-        let bundle = Bundle(for: WordPressAuthenticator.self)
+        let bundle = WordPressAuthenticator.bundle
         tableView.register(UINib(nibName: "SearchTableViewCell", bundle: bundle), forCellReuseIdentifier: SearchTableViewCell.reuseIdentifier)
         setupBackgroundTapGestureRecognizer()
     }
