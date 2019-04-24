9b8f23e1761bf82dbeb589b475768b78ce2bc13c
Jaclyn Chen <watertranquil@gmail.com>
2019-04-24 12:24:00 +0800

    Add domain credit view model helper and placeholder action handler on domain credit redemption.

--- WordPress/Classes/ViewRelated/Blog/BlogDetailsViewController.m
+++ WordPress/Classes/ViewRelated/Blog/BlogDetailsViewController.m
@@ -364,6 +364,7 @@ NSString * const WPCalypsoDashboardPath = @"https://wordpress.com/stats/";
             [self.tableView selectRowAtIndexPath:indexPath
                                         animated:NO
                                   scrollPosition:[self optimumScrollPositionForIndexPath:indexPath]];
+            [self showDomainCreditRedemption];
             break;
         case BlogDetailsSubsectionQuickStart:
             self.restorableSelectedIndexPath = indexPath;
