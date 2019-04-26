a4871747a1e6aa06d99b7dfe3cf9d56582c098cc
Jaclyn Chen <watertranquil@gmail.com>
2019-04-26 21:24:50 +0800

    Show success screen with action delegate to dismiss view controller and then show notice.

--- WordPress/Classes/ViewRelated/Blog/DomainCredit/BlogDetailsViewController+DomainCredit.swift
+++ WordPress/Classes/ViewRelated/Blog/DomainCredit/BlogDetailsViewController+DomainCredit.swift
@@ -1,4 +1,5 @@
 import Gridicons
+import WordPressFlux
 
 extension BlogDetailsViewController {
     @objc func domainCreditSectionViewModel() -> BlogDetailsSection {
@@ -18,5 +19,34 @@ extension BlogDetailsViewController {
 
     @objc func showDomainCreditRedemption() {
         // TODO-#11467 - subtask - integration with domain registration.
+        // Temporarily shows success screen before domain registration is integrated.
+        let domain = "lifeoftea.com"
+        presentDomainCreditRedemptionSuccess(domain: domain)
+    }
+
+    private func presentDomainCreditRedemptionSuccess(domain: String) {
+        let controller = DomainCreditRedemptionSuccessViewController(domain: domain, delegate: self)
+        present(controller, animated: true, completion: nil)
+    }
+}
+
+extension BlogDetailsViewController: DomainCreditRedemptionSuccessViewControllerDelegate {
+    func continueButtonPressed() {
+        dismiss(animated: true) { [weak self] in
+            guard let email = self?.accountEmail() else {
+                return
+            }
+            let title = String(format: NSLocalizedString("Verify your email address - instructions sent to %@", comment: "Notice displayed after domain credit redemption success."), email)
+            ActionDispatcher.dispatch(NoticeAction.post(Notice(title: title)))
+        }
+    }
+
+    private func accountEmail() -> String? {
+        let context = ContextManager.sharedInstance().mainContext
+        let accountService = AccountService(managedObjectContext: context)
+        guard let defaultAccount = accountService.defaultWordPressComAccount() else {
+            return nil
+        }
+        return defaultAccount.email
     }
 }
