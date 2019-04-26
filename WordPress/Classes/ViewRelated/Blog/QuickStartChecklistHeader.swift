94a24f092f6fe88847101156d07ee4e76994c25f
stevebaranski <steven.baranski@icloud.com>
2019-04-26 12:05:13 -0700

    Improved accessibility of Quick Start checklist header

--- WordPress/Classes/ViewRelated/Blog/QuickStartChecklistHeader.swift
+++ WordPress/Classes/ViewRelated/Blog/QuickStartChecklistHeader.swift
@@ -1,6 +1,7 @@
 import Gridicons
 
 class QuickStartChecklistHeader: UIView {
+
     var collapseListener: ((Bool) -> Void)?
     var collapse: Bool = false {
         didSet {
@@ -15,11 +16,29 @@ class QuickStartChecklistHeader: UIView {
                 self?.bottomStroke.alpha = CGFloat(alpha)
                 self?.chevronView.transform = CGAffineTransform(rotationAngle: rotate)
             })
+
+            let hintToken: String
+            if collapse {
+                hintToken = NSLocalizedString("Collapses", comment: "This token is used with ~ to format the accessibility hint to reflect the expanded and collapsed state of the list.")
+            } else {
+                hintToken = NSLocalizedString("Expands", comment: "This token is used with ~ to format the accessibility hint to reflect the expanded and collapsed state of the list.")
+            }
+
+            let accessibilityFormat = NSLocalizedString("%@ the list of completed tasks.", comment: "Accessibility hint for the list of completed tasks presented during Quick Start.")
+            let localizedAccessibilityHint = String(format: accessibilityFormat, hintToken)
+
+            accessibilityHint = localizedAccessibilityHint
         }
     }
     var count: Int = 0 {
         didSet {
             titleLabel.text = String(format: Constant.title, count)
+
+            // Admittedly pluralization support could be improved here. See also: `paaHJt-c3-p2`
+            let accessibilityFormat = NSLocalizedString("%i completed tasks, toggling expands & contracts the list of these tasks", comment: "Accessibility description for the list of completed tasks presented during Quick Start.")
+            let localizedAccessibilityDescription = String(format: accessibilityFormat, arguments: [count])
+
+            accessibilityLabel = localizedAccessibilityDescription
         }
     }
 
@@ -60,8 +79,31 @@ class QuickStartChecklistHeader: UIView {
     @IBAction private func headerDidTouch(_ sender: UIButton) {
         collapse.toggle()
     }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        prepareForVoiceOver()
+    }
 }
 
 private enum Constant {
     static let title = NSLocalizedString("Complete (%i)", comment: "The table view header title that displays the number of completed tasks")
+}
+
+// MARK: - Accessible
+
+extension QuickStartChecklistHeader: Accessible {
+    func prepareForVoiceOver() {
+        // Here we explicit configure the subviews, to prepare for the desired composite behavior
+        bottomStroke.isAccessibilityElement = false
+        contentView.isAccessibilityElement = false
+        titleLabel.isAccessibilityElement = false
+        chevronView.isAccessibilityElement = false
+
+        // Neither the top stroke nor the button (overlay) are outlets, so we configured them in the nib
+
+        // From an accessibility perspective, this view is essentially monolithic, so we configure it accordingly
+        isAccessibilityElement = true
+        accessibilityTraits = [ .header, .button ]
+    }
 }
