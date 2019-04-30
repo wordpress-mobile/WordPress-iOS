7bdf7b4fcc88724c3f60fcdccbc7f097d3bbaffe
Daniele Bogo <daniele.bogo@automattic.com>
2019-04-30 15:41:46 +0100

    Use protocol + extension to avoid code duplications

--- WordPress/Classes/Extensions/UIViewController+NoResults.swift
+++ WordPress/Classes/Extensions/UIViewController+NoResults.swift
@@ -1,18 +1,36 @@
-protocol NoResultsViewDisplayable: class {
+protocol NoResultsViewHost: class {
+    typealias NoResultsCustomizationBlock = (NoResultsViewController) -> Void
+
     var noResultsViewController: NoResultsViewController { get }
 }
 
-extension NoResultsViewDisplayable where Self: UIViewController {
+extension NoResultsViewHost where Self: UIViewController {
     func configureAndDisplayNoResults(on view: UIView,
                                       title: String,
                                       subtitle: String? = nil,
                                       buttonTitle: String? = nil,
-                                      accessoryView: UIView? = nil) {
+                                      accessoryView: UIView? = nil,
+                                      animated: Bool = true,
+                                      customizationBlock: NoResultsCustomizationBlock? = nil) {
         noResultsViewController.configure(title: title,
                                           buttonTitle: buttonTitle,
                                           subtitle: subtitle,
                                           accessoryView: accessoryView)
-        displayNoResults(on: view)
+        displayNoResults(on: view,
+                         animated: animated,
+                         customizationBlock: customizationBlock)
+    }
+
+    func updateNoResults(title: String,
+                         subtitle: String? = nil,
+                         buttonTitle: String? = nil,
+                         accessoryView: UIView? = nil) {
+        noResultsViewController.configure(title: title,
+                                          buttonTitle: buttonTitle,
+                                          subtitle: subtitle,
+                                          accessoryView: accessoryView)
+        noResultsViewController.hideImageView(accessoryView == nil)
+        noResultsViewController.updateView()
     }
 
     func hideNoResults(_ completion: (() -> Void)? = nil) {
@@ -24,16 +42,21 @@ extension NoResultsViewDisplayable where Self: UIViewController {
         completion?()
     }
 
-    func displayNoResults(on view: UIView) {
+    func displayNoResults(on view: UIView,
+                          animated: Bool = true,
+                          customizationBlock: NoResultsCustomizationBlock? = nil) {
         if noResultsViewController.view.superview != nil {
             return
         }
 
-        addChild(noResultsViewController)
         noResultsViewController.view.frame = view.frame
         noResultsViewController.view.frame.origin.y = 0
+        customizationBlock?(noResultsViewController)
+        addChild(noResultsViewController)
 
-        view.addSubview(withFadeAnimation: noResultsViewController.view)
+        if animated {
+            view.addSubview(withFadeAnimation: noResultsViewController.view)
+        }
         noResultsViewController.didMove(toParent: self)
     }
 }
