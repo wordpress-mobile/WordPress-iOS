ef241b8a52a3de47809138efd8e7495a77cec16c
Daniele Bogo <daniele.bogo@automattic.com>
2019-05-01 12:40:08 +0100

    Use all the arguments to customise the no results screen

--- WordPress/Classes/Extensions/UIViewController+NoResults.swift
+++ WordPress/Classes/Extensions/UIViewController+NoResults.swift
@@ -1,5 +1,6 @@
 protocol NoResultsViewHost: class {
     typealias NoResultsCustomizationBlock = (NoResultsViewController) -> Void
+    typealias NoResultsAttributedSubtitleConfiguration = NoResultsViewController.AttributedSubtitleConfiguration
 
     var noResultsViewController: NoResultsViewController { get }
 }
@@ -13,19 +14,31 @@ extension NoResultsViewHost where Self: UIViewController {
     ///   - title: Main descriptive text. Required.
     ///   - subtitle: Secondary descriptive text. Optional.
     ///   - buttonTitle: Title of action button. Optional.
-    ///   - accessoryView: View to show instead of the image. Optional.
+    ///   - attributedSubtitle: Secondary descriptive attributed text. Optional.
+    ///   - attributedSubtitleConfiguration: Called after default styling, for subtitle attributed text customization.
+    ///   - image:              Name of image file to use. Optional.
+    ///   - subtitleImage:      Name of image file to use in place of subtitle. Optional.
+    ///   - accessoryView:      View to show instead of the image. Optional.
     ///   - animated: Enable the fade in transition. True by default.
     ///   - customizationBlock: Used to customize the view controller before being added. Optional.
     func configureAndDisplayNoResults(on view: UIView,
                                       title: String,
                                       subtitle: String? = nil,
                                       buttonTitle: String? = nil,
+                                      attributedSubtitle: NSAttributedString? = nil,
+                                      attributedSubtitleConfiguration: NoResultsAttributedSubtitleConfiguration? = nil,
+                                      image: String? = nil,
+                                      subtitleImage: String? = nil,
                                       accessoryView: UIView? = nil,
                                       animated: Bool = true,
                                       customizationBlock: NoResultsCustomizationBlock? = nil) {
         noResultsViewController.configure(title: title,
                                           buttonTitle: buttonTitle,
                                           subtitle: subtitle,
+                                          attributedSubtitle: attributedSubtitle,
+                                          attributedSubtitleConfiguration: attributedSubtitleConfiguration,
+                                          image: image,
+                                          subtitleImage: subtitleImage,
                                           accessoryView: accessoryView)
         displayNoResults(on: view,
                          animated: animated,
@@ -35,17 +48,30 @@ extension NoResultsViewHost where Self: UIViewController {
     /// Used to update the no results view controller
     ///
     /// - Parameters:
+    ///   - view: The no results view parentView. Required.
     ///   - title: Main descriptive text. Required.
     ///   - subtitle: Secondary descriptive text. Optional.
     ///   - buttonTitle: Title of action button. Optional.
-    ///   - accessoryView: View to show instead of the image. Optional.
+    ///   - attributedSubtitle: Secondary descriptive attributed text. Optional.
+    ///   - attributedSubtitleConfiguration: Called after default styling, for subtitle attributed text customization.
+    ///   - image:              Name of image file to use. Optional.
+    ///   - subtitleImage:      Name of image file to use in place of subtitle. Optional.
+    ///   - accessoryView:      View to show instead of the image. Optional.
     func updateNoResults(title: String,
                          subtitle: String? = nil,
                          buttonTitle: String? = nil,
+                         attributedSubtitle: NSAttributedString? = nil,
+                         attributedSubtitleConfiguration: NoResultsAttributedSubtitleConfiguration? = nil,
+                         image: String? = nil,
+                         subtitleImage: String? = nil,
                          accessoryView: UIView? = nil) {
         noResultsViewController.configure(title: title,
                                           buttonTitle: buttonTitle,
                                           subtitle: subtitle,
+                                          attributedSubtitle: attributedSubtitle,
+                                          attributedSubtitleConfiguration: attributedSubtitleConfiguration,
+                                          image: image,
+                                          subtitleImage: subtitleImage,
                                           accessoryView: accessoryView)
         noResultsViewController.hideImageView(accessoryView == nil)
         noResultsViewController.updateView()
