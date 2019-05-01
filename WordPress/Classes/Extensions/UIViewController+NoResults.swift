e7fb2bc4af041af53e0912deeecc470e5f2eda8a
Daniele Bogo <daniele.bogo@automattic.com>
2019-05-01 09:54:00 +0100

    Add documentation

--- WordPress/Classes/Extensions/UIViewController+NoResults.swift
+++ WordPress/Classes/Extensions/UIViewController+NoResults.swift
@@ -5,6 +5,17 @@ protocol NoResultsViewHost: class {
 }
 
 extension NoResultsViewHost where Self: UIViewController {
+
+    /// Configure and display the no results view controller
+    ///
+    /// - Parameters:
+    ///   - view: The no results view parentView. Required.
+    ///   - title: Main descriptive text. Required.
+    ///   - subtitle: Secondary descriptive text. Optional.
+    ///   - buttonTitle: Title of action button. Optional.
+    ///   - accessoryView: View to show instead of the image. Optional.
+    ///   - animated: Enable the fade in transition. True by default.
+    ///   - customizationBlock: Used to customize the view controller before being added. Optional.
     func configureAndDisplayNoResults(on view: UIView,
                                       title: String,
                                       subtitle: String? = nil,
@@ -21,6 +32,13 @@ extension NoResultsViewHost where Self: UIViewController {
                          customizationBlock: customizationBlock)
     }
 
+    /// Used to update the no results view controller
+    ///
+    /// - Parameters:
+    ///   - title: Main descriptive text. Required.
+    ///   - subtitle: Secondary descriptive text. Optional.
+    ///   - buttonTitle: Title of action button. Optional.
+    ///   - accessoryView: View to show instead of the image. Optional.
     func updateNoResults(title: String,
                          subtitle: String? = nil,
                          buttonTitle: String? = nil,
@@ -33,6 +51,9 @@ extension NoResultsViewHost where Self: UIViewController {
         noResultsViewController.updateView()
     }
 
+    /// Hide and remove the no results view controller
+    ///
+    /// - Parameter completion: Block called as soon the view controller has been removed.
     func hideNoResults(_ completion: (() -> Void)? = nil) {
         if noResultsViewController.view.superview == nil {
             return
@@ -42,6 +63,12 @@ extension NoResultsViewHost where Self: UIViewController {
         completion?()
     }
 
+    /// Display the no result view controller
+    ///
+    /// - Parameters:
+    ///   - view: The no results view parentView. Required.
+    ///   - animated: Enable the fade in transition. True by default.
+    ///   - customizationBlock: Used to customize the view controller before being added. Optional.
     func displayNoResults(on view: UIView,
                           animated: Bool = true,
                           customizationBlock: NoResultsCustomizationBlock? = nil) {
