5eaf0aa0aef23284d3c76db611618ebc4d9132a4
Leandro Alonso <contato@leandroalonso.com>
2019-05-07 20:22:19 -0300

    Creates a PostActionSheet to show post options
    Given a view controller and a Post the Action Sheet with options is displayed

---
+++ WordPress/WordPressTest/PostActionSheetTests.swift
@@ -0,0 +1,36 @@
+import UIKit
+import XCTest
+
+@testable import WordPress
+
+class PostActionSheetTests: XCTestCase {
+
+    var postActionSheet: PostActionSheet!
+    var viewControllerMock: UIViewControllerMock!
+
+    override func setUp() {
+        viewControllerMock = UIViewControllerMock()
+        postActionSheet = PostActionSheet(viewController: viewControllerMock)
+    }
+
+    func testPublishedPostOptions() {
+        let post = PostBuilder().published().build()
+
+        postActionSheet.show(for: post)
+
+        let options = viewControllerMock.viewControllerPresented?.actions.compactMap { $0.title }
+        XCTAssertEqual(["Cancel", "Stats", "Move to drafts", "Move to trash"], options)
+    }
+
+}
+
+class UIViewControllerMock: UIViewController {
+
+    var didCallPresent = false
+    var viewControllerPresented: UIAlertController?
+
+    override func present(_ viewControllerToPresent: UIViewController, animated flag: Bool, completion: (() -> Void)? = nil) {
+        didCallPresent = true
+        viewControllerPresented = viewControllerToPresent as? UIAlertController
+    }
+}
