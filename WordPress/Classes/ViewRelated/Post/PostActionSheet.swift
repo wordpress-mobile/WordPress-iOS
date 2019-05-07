a0fd91c37c13c93dc553d00ea6c85d41d5ad107f
Leandro Alonso <contato@leandroalonso.com>
2019-05-07 20:22:19 -0300

    Creates a PostActionSheet to show post options
    Given a view controller and a Post the Action Sheet with options is displayed

---
+++ WordPress/Classes/ViewRelated/Post/PostActionSheet.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+class PostActionSheet {
+
+    weak var viewController: UIViewController?
+    weak var interactivePostViewDelegate: InteractivePostViewDelegate?
+
+    init(viewController: UIViewController) {
+        self.viewController = viewController
+    }
+
+    func show(for post: Post) {
+        let actionSheetController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+
+        let cancelActionButton = UIAlertAction(title: "Cancel", style: .cancel) { _ in }
+        actionSheetController.addAction(cancelActionButton)
+
+        let statsActionButton = UIAlertAction(title: "Stats", style: .default) { _ in
+            // show stats
+        }
+        actionSheetController.addAction(statsActionButton)
+
+        let draftsActionButton = UIAlertAction(title: "Move to drafts", style: .default) { _ in
+            // move to drafts
+        }
+        actionSheetController.addAction(draftsActionButton)
+
+        let trashActionButton = UIAlertAction(title: "Move to trash", style: .destructive) { _ in
+            // move to trash
+        }
+        actionSheetController.addAction(trashActionButton)
+
+        viewController?.present(actionSheetController, animated: true)
+    }
+}
---
+++ WordPress/WordPressTest/PostBuilder.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+@testable import WordPress
+
+class PostBuilder {
+    private let inMemoryManagedObjectContext: NSManagedObjectContext = {
+        let managedObjectModel = NSManagedObjectModel.mergedModel(from: [Bundle.main])!
+
+        let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+
+        do {
+            try persistentStoreCoordinator.addPersistentStore(ofType: NSInMemoryStoreType, configurationName: nil, at: nil, options: nil)
+        } catch {
+            print("Adding in-memory persistent store failed")
+        }
+
+        let managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = persistentStoreCoordinator
+
+        return managedObjectContext
+    }()
+
+    lazy var post = {
+        return NSEntityDescription.insertNewObject(forEntityName: "Post", into: inMemoryManagedObjectContext) as! Post
+    }()
+
+    func published() -> PostBuilder {
+        post.status = .publish
+        return self
+    }
+
+    func build() -> Post {
+        return NSEntityDescription.insertNewObject(forEntityName: "Post", into: inMemoryManagedObjectContext) as! Post
+    }
+}
