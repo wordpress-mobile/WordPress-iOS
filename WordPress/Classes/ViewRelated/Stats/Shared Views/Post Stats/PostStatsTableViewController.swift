bc508c91456d3c56dd804369ab24bc32c1d18152
Daniele Bogo <daniele.bogo@automattic.com>
2019-05-20 16:42:54 +0100

    Implement loading view

--- WordPress/Classes/ViewRelated/Stats/Shared Views/Post Stats/PostStatsTableViewController.swift
+++ WordPress/Classes/ViewRelated/Stats/Shared Views/Post Stats/PostStatsTableViewController.swift
@@ -39,6 +39,7 @@ class PostStatsTableViewController: UITableViewController, StoryboardLoadable {
         tableView.register(SiteStatsTableHeaderView.defaultNib,
                            forHeaderFooterViewReuseIdentifier: SiteStatsTableHeaderView.defaultNibName)
         initViewModel()
+        displayLoadingViewIfNecessary()
     }
 
     func configure(postID: Int, postTitle: String?, postURL: URL?) {
@@ -108,6 +109,12 @@ private extension PostStatsTableViewController {
 
         tableHandler.viewModel = viewModel.tableViewModel()
         refreshControl?.endRefreshing()
+
+        if viewModel.fetchDataHasFailed() {
+            displayFailureViewIfNecessary()
+        } else {
+            hideNoResults()
+        }
     }
 
     @objc func userInitiatedRefresh() {
@@ -115,12 +122,17 @@ private extension PostStatsTableViewController {
         refreshData()
     }
 
-    func refreshData() {
-        guard let postID = postID else {
+    func refreshData(forceUpdate: Bool = false) {
+        guard let viewModel = viewModel,
+            let postID = postID else {
             return
         }
 
-        viewModel?.refreshPostStats(postID: postID, selectedDate: selectedDate)
+        viewModel.refreshPostStats(postID: postID, selectedDate: selectedDate)
+        if forceUpdate {
+            tableHandler.viewModel = viewModel.tableViewModel()
+        }
+        displayLoadingViewIfNecessary()
     }
 
     func applyTableUpdates() {
@@ -166,7 +178,60 @@ extension PostStatsTableViewController: SiteStatsTableHeaderDelegate {
         }
 
         selectedDate = newDate
-        refreshData()
+        refreshData(forceUpdate: true)
+    }
+}
+
+// MARK: - NoResultsViewHost
+
+extension PostStatsTableViewController: NoResultsViewHost {
+    private func displayLoadingViewIfNecessary() {
+        guard tableHandler.viewModel.sections.isEmpty else {
+            return
+        }
+
+        if noResultsViewController.view.superview != nil {
+            updateNoResults(title: NoResultConstants.successTitle,
+                            accessoryView: NoResultsViewController.loadingAccessoryView()) { [weak self] noResults in
+                                noResults.delegate = self
+                                noResults.hideImageView(false)
+            }
+            return
+        }
+
+        configureAndDisplayNoResults(on: tableView,
+                                     title: NoResultConstants.successTitle,
+                                     accessoryView: NoResultsViewController.loadingAccessoryView()) { [weak self] noResults in
+                                        noResults.delegate = self
+                                        noResults.hideImageView(false)
+        }
     }
 
+    private func displayFailureViewIfNecessary() {
+        guard tableHandler.viewModel.sections.isEmpty else {
+            return
+        }
+
+        updateNoResults(title: NoResultConstants.errorTitle,
+                        subtitle: NoResultConstants.errorSubtitle,
+                        buttonTitle: NoResultConstants.refreshButtonTitle) { [weak self] noResults in
+                            noResults.delegate = self
+                            noResults.hideImageView()
+        }
+    }
+
+    private enum NoResultConstants {
+        static let successTitle = NSLocalizedString("Loading Stats...", comment: "The loading view title displayed while the service is loading")
+        static let errorTitle = NSLocalizedString("Stats not loaded", comment: "The loading view title displayed when an error occurred")
+        static let errorSubtitle = NSLocalizedString("There was a problem loading your data, refresh your page to try again.", comment: "The loading view subtitle displayed when an error occurred")
+        static let refreshButtonTitle = NSLocalizedString("Refresh", comment: "The loading view button title displayed when an error occurred")
+    }
+}
+
+// MARK: - NoResultsViewControllerDelegate methods
+
+extension PostStatsTableViewController: NoResultsViewControllerDelegate {
+    func actionButtonPressed() {
+        refreshData()
+    }
 }
