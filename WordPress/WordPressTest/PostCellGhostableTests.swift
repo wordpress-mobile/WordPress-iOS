776dee776a32eb3a5fb62d84906f857d8d94fd13
Leandro Alonso <contato@leandroalonso.com>
2019-05-20 21:32:01 -0300

    Configure the post cell before showing it as a ghost

--- WordPress/WordPressTest/PostCellGhostableTests.swift
+++ WordPress/WordPressTest/PostCellGhostableTests.swift
@@ -24,6 +24,10 @@ class PostCellGhostableTests: XCTestCase {
         XCTAssertFalse(postCell.snippetLabel.isHidden)
     }
 
+    func testHideAuthorLabel() {
+        XCTAssertTrue(postCell.authorLabel.isHidden)
+    }
+
     func testHideStatusView() {
         XCTAssertTrue(postCell.statusView.isHidden)
     }
@@ -44,6 +48,15 @@ class PostCellGhostableTests: XCTestCase {
         let margin: CGFloat = WPDeviceIdentification.isiPad() ? 20 : 16
 
         XCTAssertEqual(postCell.topPadding.constant, margin)
+    }
+
+    func testSpaceBetweenTitleAndSnippet() {
+        XCTAssertEqual(postCell.titleAndSnippetView.spacing, 16)
+    }
+
+    func testTopAndBottomOfTitleAndSnippet() {
+        XCTAssertEqual(postCell.titleAndSnippetView.layoutMargins.top, 0)
+        XCTAssertEqual(postCell.titleAndSnippetView.layoutMargins.bottom, 8)
     }
 
     func testActionBarIsNotGhostable() {
