155af4d9cf2da36e3c72a1c3b6e4fc6e32c37c9e
Leandro Alonso <contato@leandroalonso.com>
2019-05-23 22:26:39 -0300

    Move RestorePostTableViewCell to Swift

--- WordPress/Classes/ViewRelated/Post/RestorePostTableViewCell.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-#import "ConfigurablePostView.h"
-#import "WordPress-Swift.h"
-
-@protocol ConfigurablePostView;
-
-@interface RestorePostTableViewCell : UITableViewCell <ConfigurablePostView, InteractivePostView>
-
-@end
