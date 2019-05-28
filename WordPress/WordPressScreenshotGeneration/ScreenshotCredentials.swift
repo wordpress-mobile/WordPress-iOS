01747fd9a6eceae24b6807ece463d66fd56148e1
James Treanor <jtreanor3@gmail.com>
2019-05-28 12:57:37 +0100

    Use fake credentials for UI test and screenshot mocking

---
+++ WordPress/WordPressScreenshotGeneration/ScreenshotCredentials.swift
@@ -0,0 +1,5 @@
+// // These are fake credentials used for the mocked screenshot tests
+struct ScreenshotCredentials {
+    static let username: String = "e2eflowtestingmobile"
+    static let password: String = "mocked_password"
+}
