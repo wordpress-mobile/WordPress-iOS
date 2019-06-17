c85ce4353225aefd330968f658c138bc30322577
Daniele Bogo <daniele.bogo@automattic.com>
2019-06-17 09:37:03 +0100

    Add separator line

--- WordPress/Classes/ViewRelated/Stats/Period Stats/Countries/Map/CountriesMapCell.swift
+++ WordPress/Classes/ViewRelated/Stats/Period Stats/Countries/Map/CountriesMapCell.swift
@@ -2,7 +2,13 @@ import UIKit
 
 class CountriesMapCell: UITableViewCell, NibLoadable {
     private let countriesMapView = CountriesMapView.loadFromNib()
+    private typealias Style = WPStyleGuide.Stats
 
+    @IBOutlet private var separatorLine: UIView! {
+        didSet {
+            Style.configureViewAsSeparator(separatorLine)
+        }
+    }
     @IBOutlet private var countriesMapContainer: UIStackView! {
         didSet {
             countriesMapContainer.addArrangedSubview(countriesMapView)
