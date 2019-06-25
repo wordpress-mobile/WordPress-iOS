8f0a5464f415a3b07f327e4f00cb6f1e48db2605
rachelmcr <rachel@automattic.com>
2019-06-25 13:18:43 +0100

    Take screenshot at the end of failed tests

--- WordPress/WordPressUITests/Tests/MainNavigationTests.swift
+++ WordPress/WordPressUITests/Tests/MainNavigationTests.swift
@@ -14,6 +14,7 @@ class MainNavigationTests: XCTestCase {
     }
 
     override func tearDown() {
+        takeScreenshotOfFailedTest()
         LoginFlow.logoutIfNeeded()
         super.tearDown()
     }
