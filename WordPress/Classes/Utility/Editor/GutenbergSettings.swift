6f029b88346bb72c20b3ee7c8bc96513a5836bde
etoledom <etoledom@icloud.com>
2019-07-15 13:56:35 +0200

    Implement POST editor settings

--- WordPress/Classes/Utility/Editor/GutenbergSettings.swift
+++ WordPress/Classes/Utility/Editor/GutenbergSettings.swift
@@ -10,6 +10,7 @@ class GutenbergSettings {
 
     // MARK: - Internal variables
     fileprivate let database: KeyValueDatabase
+    private let queue = DispatchQueue(label: "org.wordpress.post_editor_settings", qos: .background)
 
     // MARK: - Initialization
     init(database: KeyValueDatabase) {
@@ -53,6 +54,42 @@ class GutenbergSettings {
         } else {
             // It's an existing post
             return post.containsGutenbergBlocks()
+        }
+    }
+
+    // MARK: - Editor settings sync with remote
+
+    /// Posts the current local editor setting to remote. This local setting will be set to all the user's blogs.
+    ///
+    func setToRemote() {
+        let currentSettings: EditorSettings = isGutenbergEnabled() ? .gutenberg : .aztec
+        let allBlogs = getAllBlogs()
+        let delay: TimeInterval = allBlogs.count > 5 ? 0.3 : 0;
+
+        queue.async {
+            allBlogs.forEach({ (blog) in
+                self.postEditorSettings(currentSettings, to: blog)
+                Thread.sleep(forTimeInterval: delay)
+            })
+        }
+    }
+
+    private func getAllBlogs() -> [Blog] {
+        let blogService = BlogService(managedObjectContext: ContextManager.sharedInstance().mainContext)
+        return blogService.blogsForAllAccounts() as? [Blog] ?? []
+    }
+
+    private func postEditorSettings(_ settings: EditorSettings, to blog: Blog) {
+        guard
+            let blogDotComId = blog.dotComID as? Int,
+            let remoteAPI = blog.wordPressComRestApi()
+        else {
+            return
+        }
+
+        let service = EditorServiceRemote(wordPressComRestApi: remoteAPI)
+        service.postDesignateMobileEditor(blogDotComId, editor: settings, success: { _ in }) { (error) in
+            DDLogError("Failed to syncronize editor settings with error: \(error)")
         }
     }
 }
