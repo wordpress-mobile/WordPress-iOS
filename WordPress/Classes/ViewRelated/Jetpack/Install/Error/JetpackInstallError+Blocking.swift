ef540f4ca08f25586f6007407bb13200f755e2a9
Daniele Bogo <daniele.bogo@automattic.com>
2019-07-24 12:19:08 +0100

    Update error extension

--- WordPress/Classes/ViewRelated/Jetpack/Install/Error/JetpackInstallError+Blocking.swift
+++ WordPress/Classes/ViewRelated/Jetpack/Install/Error/JetpackInstallError+Blocking.swift
@@ -3,11 +3,25 @@ extension JetpackInstallError {
     /// If the error is blocking,
     /// use the webview as fallback to install Jetpack
     var isBlockingError: Bool {
-        switch self {
+        switch self.type {
         case .loginFailure, .siteIsJetpack, .unknown:
             return false
         default:
             return true
+        }
+    }
+
+    var message: String {
+        switch self.type {
+        case .loginFailure:
+            return NSLocalizedString("Jetpack could not be installed at this time.",
+                                     comment: "The default Jetpack view message used when a 'login failure' error occurred")
+        case .siteIsJetpack:
+            return NSLocalizedString("Jetpack could not be installed at this time.",
+                                     comment: "The default Jetpack view message used when a 'site is Jetpack' error occurred")
+        default:
+            return NSLocalizedString("Jetpack could not be installed at this time.",
+                                     comment: "The default Jetpack view message used when an error occurred")
         }
     }
 }
