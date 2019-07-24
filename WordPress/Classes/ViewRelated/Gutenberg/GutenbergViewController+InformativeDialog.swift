256cc8d054b6e50c46760764b2bc50edf204efd0
etoledom <etoledom@icloud.com>
2019-07-24 12:51:11 +0200

    Gutenberg: Change auto-enable dialog to show the new switch position

--- WordPress/Classes/ViewRelated/Gutenberg/GutenbergViewController+InformativeDialog.swift
+++ WordPress/Classes/ViewRelated/Gutenberg/GutenbergViewController+InformativeDialog.swift
@@ -5,7 +5,7 @@ extension GutenbergViewController {
 
     enum InfoDialog {
         static let message = NSLocalizedString(
-            "This post uses the block editor, which is the default editor for new posts. To enable the classic editor, go to Me > App Settings.",
+            "This post uses the block editor, which is the default editor for new posts. To enable the classic editor, go to Site Settings.",
             comment: "Popup content about why this post is being opened in block editor"
         )
         static let title = NSLocalizedString("Block Editor Enabled", comment: "Popup title about why this post is being opened in Block Editor")
