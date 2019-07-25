0872ee257eb5cebc7460ffbe20f7ad6d5032715a
Nate Heagy <nate@heagy.com>
2019-07-25 16:15:08 -0700

    Add specific reasons for ContextManager save: errors

--- WordPress/Classes/Utility/ContextManager.m
+++ WordPress/Classes/Utility/ContextManager.m
@@ -305,8 +305,72 @@ static ContextManager *_override;
     
     if ([context hasChanges] && ![context save:&error]) {
         DDLogError(@"Unresolved core data error\n%@:", error);
+
+        // error handling code from https://stackoverflow.com/a/3510918
+        NSArray *errors = nil;
+        NSString *reasons = @"Reasons: ";
+        if ([error code] == NSValidationMultipleErrorsError) {
+            errors = [[error userInfo] objectForKey:NSDetailedErrorsKey];
+        } else {
+            errors = [NSArray arrayWithObject:error];
+        }
+
+        if (errors && [errors count] > 0) {
+            for (NSError * error in errors) {
+                NSString *entityName = [[[[error userInfo] objectForKey:@"NSValidationErrorObject"] entity] name];
+                NSString *attributeName = [[error userInfo] objectForKey:@"NSValidationErrorKey"];
+                NSString *msg;
+                switch ([error code]) {
+                    case NSManagedObjectValidationError:
+                        msg = @"Generic validation error.";
+                        break;
+                    case NSValidationMissingMandatoryPropertyError:
+                        msg = [NSString stringWithFormat:@"The attribute '%@' mustn't be empty.", attributeName];
+                        break;
+                    case NSValidationRelationshipLacksMinimumCountError:
+                        msg = [NSString stringWithFormat:@"The relationship '%@' doesn't have enough entries.", attributeName];
+                        break;
+                    case NSValidationRelationshipExceedsMaximumCountError:
+                        msg = [NSString stringWithFormat:@"The relationship '%@' has too many entries.", attributeName];
+                        break;
+                    case NSValidationRelationshipDeniedDeleteError:
+                        msg = [NSString stringWithFormat:@"To delete, the relationship '%@' must be empty.", attributeName];
+                        break;
+                    case NSValidationNumberTooLargeError:
+                        msg = [NSString stringWithFormat:@"The number of the attribute '%@' is too large.", attributeName];
+                        break;
+                    case NSValidationNumberTooSmallError:
+                        msg = [NSString stringWithFormat:@"The number of the attribute '%@' is too small.", attributeName];
+                        break;
+                    case NSValidationDateTooLateError:
+                        msg = [NSString stringWithFormat:@"The date of the attribute '%@' is too late.", attributeName];
+                        break;
+                    case NSValidationDateTooSoonError:
+                        msg = [NSString stringWithFormat:@"The date of the attribute '%@' is too soon.", attributeName];
+                        break;
+                    case NSValidationInvalidDateError:
+                        msg = [NSString stringWithFormat:@"The date of the attribute '%@' is invalid.", attributeName];
+                        break;
+                    case NSValidationStringTooLongError:
+                        msg = [NSString stringWithFormat:@"The text of the attribute '%@' is too long.", attributeName];
+                        break;
+                    case NSValidationStringTooShortError:
+                        msg = [NSString stringWithFormat:@"The text of the attribute '%@' is too short.", attributeName];
+                        break;
+                    case NSValidationStringPatternMatchingError:
+                        msg = [NSString stringWithFormat:@"The text of the attribute '%@' doesn't match the required pattern.", attributeName];
+                        break;
+                    default:
+                        msg = [NSString stringWithFormat:@"Unknown error (code %i).", [error code]];
+                        break;
+                }
+
+                reasons = [reasons stringByAppendingFormat:@"%@%@%@\n", (entityName?:@""),(entityName?@": ":@""),msg];
+            }
+        }
+
         @throw [NSException exceptionWithName:@"Unresolved Core Data save error"
-                                       reason:@"Unresolved Core Data save error"
+                                       reason:reasons
                                      userInfo:error.userInfo];
     }
 }
