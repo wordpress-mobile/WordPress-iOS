171a3811624168794d7a106ac3ee4bd6d21bb253
Daniele Bogo <daniele.bogo@automattic.com>
2019-07-29 12:00:54 +0100

    Add strokes programmatically

--- WordPress/Classes/ViewRelated/Me/Change Username/Textfield/ChangeUsernameTextfield.swift
+++ WordPress/Classes/ViewRelated/Me/Change Username/Textfield/ChangeUsernameTextfield.swift
@@ -1,3 +1,8 @@
 class ChangeUsernameTextfield: UIView, NibLoadable {
+    override func awakeFromNib() {
+        super.awakeFromNib()
 
+        addTopBorder(withColor: .neutral(shade: .shade10))
+        addBottomBorder(withColor: .neutral(shade: .shade10))
+    }
 }
