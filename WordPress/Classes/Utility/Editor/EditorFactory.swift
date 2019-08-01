a67acdc9ac865603687ec8b9325f857334709dba
etoledom <etoledom@icloud.com>
2019-08-01 20:16:06 +0300

    Gutenberg opt-in: Adding Tracks properties to gutenberg enabled/disabled events

--- WordPress/Classes/Utility/Editor/EditorFactory.swift
+++ WordPress/Classes/Utility/Editor/EditorFactory.swift
@@ -24,7 +24,7 @@ class EditorFactory {
         let gutenbergVC = GutenbergViewController(post: post, replaceEditor: replaceEditor)
 
         if gutenbergSettings.shouldAutoenableGutenberg(for: post) {
-            gutenbergSettings.setGutenbergEnabled(true, for: post.blog)
+            gutenbergSettings.setGutenbergEnabled(true, for: post.blog, source: .onBlockPostOpening)
             gutenbergSettings.postSettingsToRemote(for: post.blog)
             gutenbergVC.shouldPresentInformativeDialog = true
         }
