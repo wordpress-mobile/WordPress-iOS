9f49506877180976e78977e980f170e3e2254a3d
aerych <aerychj@gmail.com>
2019-08-28 17:51:47 -0500

    Posts: Modfies predicate to purge trashed posts with local changes.

--- WordPress/Classes/Services/PostService.m
+++ WordPress/Classes/Services/PostService.m
@@ -566,7 +566,7 @@ const NSUInteger PostServiceDefaultNumberToSync = 40;
     
     if (purge) {
         // Set up predicate for fetching any posts that could be purged for the sync.
-        NSPredicate *predicate  = [NSPredicate predicateWithFormat:@"(remoteStatusNumber = %@) AND (postID != NULL) AND (original = NULL) AND (revision = NULL) AND (blog = %@)", @(AbstractPostRemoteStatusSync), blog];
+        NSPredicate *predicate  = [NSPredicate predicateWithFormat:@"(remoteStatusNumber = %@) AND (postID != NULL) AND (original = NULL) AND (revision = NULL OR status = 'trash') AND (blog = %@)", @(AbstractPostRemoteStatusSync), blog];
         if ([statuses count] > 0) {
             NSPredicate *statusPredicate = [NSPredicate predicateWithFormat:@"status IN %@", statuses];
             predicate = [NSCompoundPredicate andPredicateWithSubpredicates:@[predicate, statusPredicate]];
