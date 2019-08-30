f9a809edadc567e2a768ef7c6d12d21b766d77f8
Shiki <jayson@basanes.net>
2019-08-30 13:36:42 -0600

    PostCardStatusViewModelTests: Readable failures

--- WordPress/WordPressTest/ViewRelated/Post/Views/PostCardStatusViewModelTests.swift
+++ WordPress/WordPressTest/ViewRelated/Post/Views/PostCardStatusViewModelTests.swift
@@ -23,29 +23,63 @@ class PostCardStatusViewModelTests: XCTestCase {
 
     func testExpectedButtonGroupsForVariousPostAttributeCombinations() {
         // Arrange
-        let expectations: [Post: ButtonGroups] = [
-            // Draft with remote
-            PostBuilder(context).drafted().withRemote().build():
-                ButtonGroups(primary: [.edit, .view, .more], secondary: [.publish, .trash]),
-            // Draft that was not uploaded to the server
-            PostBuilder(context).drafted().with(remoteStatus: .failed).build():
-                ButtonGroups(primary: [.edit, .publish, .trash], secondary: []),
-            // Local published draft with confirmed auto-upload.
-            PostBuilder(context).published().with(remoteStatus: .failed).confirmedAutoUpload().build():
-                ButtonGroups(primary: [.edit, .cancelAutoUpload, .more], secondary: [.moveToDraft, .trash]),
-            // Local published draft with canceled auto-upload.
-            PostBuilder(context).published().with(remoteStatus: .failed).build():
-                ButtonGroups(primary: [.edit, .publish, .more], secondary: [.moveToDraft, .trash]),
-            // Published post
-            PostBuilder(context).published().withRemote().build():
-                ButtonGroups(primary: [.edit, .view, .more], secondary: [.stats, .moveToDraft, .trash]),
+        print(String(describing: PostCardStatusViewModel.Button.edit))
+
+        let expectations: [(String, Post, ButtonGroups)] = [
+            (
+                "Draft with remote",
+                PostBuilder(context).drafted().withRemote().build(),
+                ButtonGroups(primary: [.edit, .view, .more], secondary: [.publish, .trash])
+            ),
+            (
+                "Draft that was not uploaded to the server",
+                PostBuilder(context).drafted().with(remoteStatus: .failed).build(),
+                ButtonGroups(primary: [.edit, .publish, .trash], secondary: [])
+            ),
+            (
+                "Local published draft with confirmed auto-upload",
+                PostBuilder(context).published().with(remoteStatus: .failed).confirmedAutoUpload().build(),
+                ButtonGroups(primary: [.edit, .cancelAutoUpload, .more], secondary: [.moveToDraft, .trash])
+            ),
+            (
+                "Local published draft with canceled auto-upload",
+                PostBuilder(context).published().with(remoteStatus: .failed).build(),
+                ButtonGroups(primary: [.edit, .publish, .more], secondary: [.moveToDraft, .trash])
+            ),
+            (
+                "Published post",
+                PostBuilder(context).published().withRemote().build(),
+                ButtonGroups(primary: [.edit, .view, .more], secondary: [.stats, .moveToDraft, .trash])
+            )
         ]
 
         // Act and Assert
-        expectations.forEach { post, expectedButtonGroups in
+        expectations.forEach { scenario, post, expectedButtonGroups in
             let viewModel = PostCardStatusViewModel(post: post)
 
-            expect(viewModel.buttonGroups).to(equal(expectedButtonGroups))
+            expect({
+                guard viewModel.buttonGroups == expectedButtonGroups else {
+                    let reason = "The scenario \"\(scenario)\" failed. "
+                        + " Expected buttonGroups to be: \(expectedButtonGroups.prettifiedDescription)."
+                        + " Actual: \(viewModel.buttonGroups.prettifiedDescription)"
+
+                    return .failed(reason: reason)
+                }
+
+                return .succeeded
+            }).to(succeed())
         }
+    }
+}
+
+private extension ButtonGroups {
+    var prettifiedDescription: String {
+        return "{ primary: \(primary.prettifiedDescription), secondary: \(secondary.prettifiedDescription) }"
+    }
+}
+
+private extension Array where Element == PostCardStatusViewModel.Button {
+    var prettifiedDescription: String {
+        return "[" + map { String(describing: $0) }.joined(separator: ", ") + "]"
     }
 }
