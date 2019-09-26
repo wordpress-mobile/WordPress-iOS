3aa91c5c969eb6050489a12e20fd6579e801911d
Daniele Bogo <daniele.bogo@automattic.com>
2019-09-26 11:43:28 +0100

    Fix pending comments color and editor

--- WordPress/Classes/ViewRelated/Notifications/Style/WPStyleGuide+Notifications.swift
+++ WordPress/Classes/ViewRelated/Notifications/Style/WPStyleGuide+Notifications.swift
@@ -127,7 +127,7 @@ extension WPStyleGuide {
         public static let blockApprovedBgColor      = UIColor.clear
 
         public static let blockUnapprovedSideColor  = UIColor.warning(.shade60)
-        public static let blockUnapprovedBgColor    = UIColor.warning(.shade0)
+        public static let blockUnapprovedBgColor    = UIColor(light: .warning(.shade0), dark: .warning(.shade90))
         public static let blockUnapprovedTextColor  = UIColor.errorDark
         public static let blockUnapprovedLinkColor  = UIColor.primary
 
