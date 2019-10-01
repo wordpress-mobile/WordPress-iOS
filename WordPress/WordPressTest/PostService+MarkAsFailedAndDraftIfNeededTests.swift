3213e769a98d47499686338eed2238b5918efb92
Leandro Alonso <contato@leandroalonso.com>
2019-10-01 18:07:52 -0300

    Do not change the status to draft when marking post as failed

--- WordPress/WordPressTest/PostService+MarkAsFailedAndDraftIfNeededTests.swift
+++ WordPress/WordPressTest/PostService+MarkAsFailedAndDraftIfNeededTests.swift
@@ -6,28 +6,6 @@ import Nimble
 
 /// Test cases for PostService.markAsFailedAndDraftIfNeeded()
 class PostServiceMarkAsFailedAndDraftIfNeededTests: XCTestCase {
-    func testMarkAPostAsFailedAndDraftIt() {
-        let post = PostBuilder()
-            .with(status: .pending)
-            .build()
-        let postService = PostService()
-
-        postService.markAsFailedAndDraftIfNeeded(post: post)
-
-        expect(post.remoteStatus).to(equal(.failed))
-        expect(post.status).to(equal(.draft))
-    }
-
-    func testItWillMarkLocallyPublishedPostsAsFailedButKeepTheStatus() {
-        let post = PostBuilder().published().with(remoteStatus: .local).build()
-        let postService = PostService()
-
-        postService.markAsFailedAndDraftIfNeeded(post: post)
-
-        expect(post.remoteStatus).to(equal(.failed))
-        expect(post.status).to(equal(.publish))
-    }
-
     func testMarkAPostAsFailedAndKeepItsStatus() {
         let post = PostBuilder()
             .with(status: .pending)
