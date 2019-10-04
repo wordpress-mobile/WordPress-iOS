ab351bf1e01697103c396f00f82b463fcacee406
Stephenie Harris <scoutharris@gmail.com>
2019-10-04 16:19:07 -0600

    Adding default Insights. Adding methods to facilitate writing to / reading from User Defaults.

--- WordPress/Classes/ViewRelated/Stats/Insights/SiteStatsInsightsTableViewController.swift
+++ WordPress/Classes/ViewRelated/Stats/Insights/SiteStatsInsightsTableViewController.swift
@@ -15,19 +15,24 @@ enum InsightType: Int {
     case postingActivity
     case publicize
 
-    // TODO: remove when Manage Insights is implemented.
-    static let allValues = [InsightType.latestPostSummary,
-                            .todaysStats,
-                            .annualSiteStats,
-                            .allTimeStats,
-                            .mostPopularTime,
-                            .postingActivity,
-                            .comments,
-                            .tagsAndCategories,
-                            .followersTotals,
-                            .followers,
-                            .publicize
+    // These Insights will be displayed in this order if a site's Insights have not been customized.
+    static let defaultInsights = [InsightType.postingActivity,
+                                  .todaysStats,
+                                  .allTimeStats,
+                                  .mostPopularTime,
+                                  .comments
     ]
+
+    static let defaultInsightsValues = InsightType.defaultInsights.map { $0.rawValue }
+
+    static func typesForValues(_ values: [Int]) -> [InsightType] {
+        return values.compactMap { InsightType(rawValue: $0) }
+    }
+
+    static func valuesForTypes(_ types: [InsightType]) -> [Int] {
+        return types.compactMap { $0.rawValue }
+    }
+
 }
 
 @objc protocol SiteStatsInsightsDelegate {
