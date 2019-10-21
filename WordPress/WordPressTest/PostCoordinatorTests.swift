21f667a4636812cd565c26c770d2d9390beab4f1
Shiki <jayson@basanes.net>
2019-10-21 10:59:18 -0600

    PostCoordinator: Completion block is called once

--- WordPress/WordPressTest/PostCoordinatorTests.swift
+++ WordPress/WordPressTest/PostCoordinatorTests.swift
@@ -379,6 +379,37 @@ class PostCoordinatorTests: XCTestCase {
         expect(notice.feedbackType).to(equal(.error))
         expect(notice.message).to(contain("Ipsa aliquam"))
     }
+
+    func testFailingToSaveBecauseOfMediaErrorsWillWillCallCompletionBlockOnlyOnce() {
+        // Arrange
+        let post = PostBuilder(context)
+            .with(image: "test.jpeg", status: .failed)
+            .with(image: "test-002.jpeg", status: .failed)
+            .build()
+
+        let onUpdateParameters = post.media.reduce(into: [Media: MediaCoordinator.MediaState]()) { dict, media in
+            dict[media] = MediaCoordinator.MediaState.failed(error: NSError())
+        }
+        let mediaCoordinatorMock = MediaCoordinatorMock(onUpdateParameters: onUpdateParameters)
+
+        let postServiceMock = PostServiceMock(managedObjectContext: context)
+
+        let postCoordinator = PostCoordinator(mainService: postServiceMock,
+                                              backgroundService: postServiceMock,
+                                              mediaCoordinator: mediaCoordinatorMock)
+
+        // Act
+        var results = [Result<AbstractPost>]()
+        waitUntil(timeout: 2) { done in
+            postCoordinator.save(post) { aResult in
+                results.append(aResult)
+                done()
+            }
+        }
+
+        // Assert
+        expect(results).to(haveCount(1))
+    }
 }
 
 private class PostServiceMock: PostService {
@@ -413,12 +444,14 @@ private class PostServiceMock: PostService {
 }
 
 private class MediaCoordinatorMock: MediaCoordinator {
-    private var media: Media
-    private var mediaState: MediaState
+    private var onUpdateParameters = [Media: MediaState]()
 
     init(media: Media, mediaState: MediaState) {
-        self.media = media
-        self.mediaState = mediaState
+        onUpdateParameters[media] = mediaState
+    }
+
+    init(onUpdateParameters: [Media: MediaState]) {
+        self.onUpdateParameters = onUpdateParameters
     }
 
     override func addObserver(_ onUpdate: @escaping MediaCoordinator.ObserverBlock, for media: Media? = nil) -> UUID {
@@ -427,7 +460,9 @@ private class MediaCoordinatorMock: MediaCoordinator {
 
     override func addObserver(_ onUpdate: @escaping MediaCoordinator.ObserverBlock, forMediaFor post: AbstractPost) -> UUID {
         DispatchQueue.main.async {
-            onUpdate(self.media, self.mediaState)
+            self.onUpdateParameters.forEach { media, mediaState in
+                onUpdate(media, mediaState)
+            }
         }
         return UUID()
     }
