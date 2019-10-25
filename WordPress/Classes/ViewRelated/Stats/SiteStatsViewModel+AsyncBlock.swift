f09986d524f77950e732dc36798dba6a21752a68
Daniele Bogo <daniele.bogo@automattic.com>
2019-10-25 14:52:56 +0100

    Update protocol

--- WordPress/Classes/ViewRelated/Stats/SiteStatsViewModel+AsyncBlock.swift
+++ WordPress/Classes/ViewRelated/Stats/SiteStatsViewModel+AsyncBlock.swift
@@ -1,28 +1,43 @@
 protocol AsyncBlocksLoadable {
-    typealias CompletionBlock<Value> = () -> Value
+    typealias AsyncBlock<Value> = () -> Value
+    typealias CacheBlock = () -> Bool
 
     associatedtype CurrentStore
     associatedtype RowType
 
+    /// The current Store used
     var currentStore: CurrentStore { get }
 
+    /// This function returns the blocks to display during the Stats loading event
+    /// - Parameter blockType: The block type
+    /// - Parameter type: The Stats type
+    /// - Parameter status: The block status
+    /// - Parameter checkingCache: The block used to check if the data is cached and needs to be checked in a different way from the default implementation
+    /// - Parameter block: The main block to display
+    /// - Parameter loading: The loading block to display
+    /// - Parameter error: The error block to display
     func blocks<Value>(for blockType: RowType,
                        type: StatType,
                        status: StoreFetchingStatus,
-                       block: CompletionBlock<Value>,
-                       loading: CompletionBlock<Value>,
-                       error: CompletionBlock<Value>) -> Value
+                       checkingCache: CacheBlock?,
+                       block: AsyncBlock<Value>,
+                       loading: AsyncBlock<Value>,
+                       error: AsyncBlock<Value>) -> Value
 }
 
 extension AsyncBlocksLoadable where CurrentStore: StatsStoreCacheable, RowType == CurrentStore.StatsStoreType {
     func blocks<Value>(for blockType: RowType,
                        type: StatType,
                        status: StoreFetchingStatus,
-                       block: CompletionBlock<Value>,
-                       loading: CompletionBlock<Value>,
-                       error: CompletionBlock<Value>) -> Value {
+                       checkingCache: CacheBlock? = nil,
+                       block: AsyncBlock<Value>,
+                       loading: AsyncBlock<Value>,
+                       error: AsyncBlock<Value>) -> Value {
         let featureFlag: FeatureFlag = type == .insights ? .statsAsyncLoading : .statsAsyncLoadingDWMY
-        if currentStore.containsCachedData(for: blockType) || !Feature.enabled(featureFlag) {
+        let isFeatureEnabled = Feature.enabled(featureFlag)
+        let containsCachedData = checkingCache != nil ? checkingCache!() : currentStore.containsCachedData(for: blockType)
+
+        if containsCachedData || !isFeatureEnabled {
             return block()
         }
 
