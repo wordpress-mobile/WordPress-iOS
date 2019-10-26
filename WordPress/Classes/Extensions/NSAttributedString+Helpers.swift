16fe05f4676ace9d9811c595b1c171b8caa70f40
Stephenie Harris <scoutharris@gmail.com>
2019-10-25 18:48:41 -0600

    Remove local method, let the app use the same method from WPKit.

--- WordPress/Classes/Extensions/NSAttributedString+Helpers.swift
+++ WordPress/Classes/Extensions/NSAttributedString+Helpers.swift
@@ -44,32 +44,4 @@ public extension NSAttributedString {
         return theString
     }
 
-    /// This helper method returns a new NSAttributedString instance, with all of the the leading / trailing newLines
-    /// characters removed.
-    ///
-    func trimNewlines() -> NSAttributedString {
-        guard let trimmed = mutableCopy() as? NSMutableAttributedString else {
-            return self
-        }
-
-        let characterSet = CharacterSet.newlines
-
-        // Trim: Leading
-        var range = (trimmed.string as NSString).rangeOfCharacter(from: characterSet)
-
-        while range.length != 0 && range.location == 0 {
-            trimmed.replaceCharacters(in: range, with: String())
-            range = (trimmed.string as NSString).rangeOfCharacter(from: characterSet)
-        }
-
-        // Trim Trailing
-        range = (trimmed.string as NSString).rangeOfCharacter(from: characterSet, options: .backwards)
-
-        while range.length != 0 && NSMaxRange(range) == trimmed.length {
-            trimmed.replaceCharacters(in: range, with: String())
-            range = (trimmed.string as NSString).rangeOfCharacter(from: characterSet, options: .backwards)
-        }
-
-        return trimmed
-    }
 }
