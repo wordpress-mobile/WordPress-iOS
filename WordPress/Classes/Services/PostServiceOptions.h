bd5be37acd8a1408b95f97404c206e31245850b1
Paul Von Schrottky <paulvonschrottky@gmail.com>
2019-10-27 23:44:23 -0300

    Removed incorrect post meta attribute comment

--- WordPress/Classes/Services/PostServiceOptions.h
+++ WordPress/Classes/Services/PostServiceOptions.h
@@ -24,9 +24,6 @@
 @property (nonatomic, assign) PostServiceResultsOrdering orderBy;
 @property (nonatomic, strong) NSNumber *authorID;
 @property (nonatomic, copy) NSString *search;
-
-// Undocumented as of 2019/10/26 - exclusive to REST API.
-// Use cases include fetching autosaved post attributes.
 @property (nonatomic, copy) NSString *meta;
 
 @end
