fd6b296c4b6ded7ac78942e7b4ec4b0c2f1dbdac
Leandro Alonso <contato@leandroalonso.com>
2019-11-01 16:30:30 -0300

    Configure MediaEditor project as a framework
    We'd like to extract this project in the future. However, right now it's easier to add the project as a framework and take advantage of the current infrasctructure we have. All of this while making things decoupled and the project as a component.

---
+++ WordPress/Classes/MediaEditor/MediaEditor.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for MediaEditor.
+FOUNDATION_EXPORT double MediaEditorVersionNumber;
+
+//! Project version string for MediaEditor.
+FOUNDATION_EXPORT const unsigned char MediaEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MediaEditor/PublicHeader.h>
+
+
