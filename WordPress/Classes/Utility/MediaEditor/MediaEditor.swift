9927feae54fd52464d79a223a97316efb6e429e3
Leandro Alonso <contato@leandroalonso.com>
2019-11-11 14:04:42 -0300

    Adds MediaEditor into WPiOS project

---
+++ WordPress/Classes/Utility/MediaEditor/MediaEditor.swift
@@ -0,0 +1,36 @@
+import UIKit
+import TOCropViewController
+
+public class MediaEditor: NSObject {
+
+    let cropViewControllerFactory: (UIImage) -> TOCropViewController
+
+    private(set) var cropViewController: TOCropViewController?
+    private var callback: ((UIImage?) -> ())?
+
+    public init(cropViewControllerFactory: @escaping (UIImage) -> TOCropViewController = TOCropViewController.init) {
+        self.cropViewControllerFactory = cropViewControllerFactory
+        super.init()
+    }
+
+    public func edit(_ image: UIImage, from viewController: UIViewController? = nil, callback: @escaping (UIImage?) -> ()) {
+        self.callback = callback
+        let cropViewController = self.cropViewControllerFactory(image)
+        cropViewController.delegate = self
+        cropViewController.toolbar.rotateCounterclockwiseButtonHidden = true
+        viewController?.present(cropViewController, animated: true)
+        self.cropViewController = cropViewController
+    }
+
+}
+
+extension MediaEditor: TOCropViewControllerDelegate {
+    public func cropViewController(_ cropViewController: TOCropViewController, didCropTo image: UIImage, with cropRect: CGRect, angle: Int) {
+        callback?(image)
+    }
+
+    public func cropViewController(_ cropViewController: TOCropViewController, didFinishCancelled cancelled: Bool) {
+        self.cropViewController?.dismiss(animated: true)
+        callback?(nil)
+    }
+}
