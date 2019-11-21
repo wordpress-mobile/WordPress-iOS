49149b77843755ccd59ec82e70e49fa0dd90425c
Jorge Bernal <jorge@automattic.com>
2019-11-21 16:43:08 +0100

    Remove old Result type and tests

--- WordPress/Classes/Models/Result.swift
+++
@@ -1,9 +0,0 @@
-
-/// Result type, encapsulates the result of an operation that either returns a value or an error
-///
-/// - success: the operation was a success, and it returned this value
-/// - error: the error returned by the operation
-enum Result<Value, Error> {
-    case success(Value)
-    case failure(Error)
-}
