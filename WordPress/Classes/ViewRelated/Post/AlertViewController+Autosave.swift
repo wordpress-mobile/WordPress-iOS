71cc8cc9a698ee9d94ad4ab077019d603221200b
Paul Von Schrottky <paulvonschrottky@gmail.com>
2019-11-24 20:57:32 -0300

    Join  autosave localized copy into one string
    Using one string gives translators more freedom in structuring the copy
    for each locale.

--- WordPress/Classes/ViewRelated/Post/AlertViewController+Autosave.swift
+++ WordPress/Classes/ViewRelated/Post/AlertViewController+Autosave.swift
@@ -24,22 +24,13 @@ extension UIAlertController {
     static func autosaveOptionsViewController(forSaveDate saveDate: Date, autosaveDate: Date, didTapSaveOption: @escaping () -> Void, didTapAutosaveOption: @escaping () -> Void) -> UIAlertController {
 
         let title = NSLocalizedString("Which version would you like to edit?", comment: "Title displayed in popup when user has the option to load unsaved changes")
-        let body = NSLocalizedString("You recently made changes to this post but didn't save them. Choose a version to load:", comment: "Message displayed in popup when user has the option to load unsaved changes")
-        let saveLabel = NSLocalizedString("From this device", comment: "Message displayed in popup indicating date of change on device")
-        let autosaveLabel = NSLocalizedString("From another device", comment: "Message displayed in popup indicating date of change on another device")
 
-        let savedOnLabel = NSLocalizedString("Saved on", comment: "Label shown on post save dates when user has the option to load unsaved changes")
-        let saveDateFormatted = savedOnLabel + " " + dateAndTime(for: saveDate)
-        let autosaveDateFormatted = savedOnLabel + " " + dateAndTime(for: autosaveDate)
+        let saveDateFormatted = dateAndTime(for: saveDate)
+        let autosaveDateFormatted = dateAndTime(for: autosaveDate)
+        let message = String(format: NSLocalizedString("You recently made changes to this post but didn't save them. Choose a version to load:\n\nFrom this device\nSaved on %@\n\nFrom another device\nSaved on %@\n", comment: "Message displayed in popup when user has the option to load unsaved changes"), saveDateFormatted, autosaveDateFormatted)
+        
         let loadSaveButtonTitle = NSLocalizedString("From this device", comment: "Button title displayed in popup indicating date of change on device")
         let fromAutosaveButtonTitle = NSLocalizedString("From another device", comment: "Button title displayed in popup indicating date of change on another device")
-
-        let message =
-            body + "\n\n" +
-            saveLabel + "\n" +
-            saveDateFormatted + "\n\n" +
-            autosaveLabel + "\n" +
-            autosaveDateFormatted + "\n"
 
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: loadSaveButtonTitle, style: .default) { _ in
