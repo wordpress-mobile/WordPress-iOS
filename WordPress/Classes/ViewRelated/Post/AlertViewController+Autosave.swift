1fded19bd464bb2fe934cdb2abfcdc3a51ebe4c7
Paul Von Schrottky <paulvonschrottky@gmail.com>
2019-11-24 21:08:48 -0300

    Clarified autosave copy comment for translators

--- WordPress/Classes/ViewRelated/Post/AlertViewController+Autosave.swift
+++ WordPress/Classes/ViewRelated/Post/AlertViewController+Autosave.swift
@@ -27,7 +27,7 @@ extension UIAlertController {
 
         let saveDateFormatted = dateAndTime(for: saveDate)
         let autosaveDateFormatted = dateAndTime(for: autosaveDate)
-        let message = String(format: NSLocalizedString("You recently made changes to this post but didn't save them. Choose a version to load:\n\nFrom this device\nSaved on %@\n\nFrom another device\nSaved on %@\n", comment: "Message displayed in popup when user has the option to load unsaved changes"), saveDateFormatted, autosaveDateFormatted)
+        let message = String(format: NSLocalizedString("You recently made changes to this post but didn't save them. Choose a version to load:\n\nFrom this device\nSaved on %@\n\nFrom another device\nSaved on %@\n", comment: "Message displayed in popup when user has the option to load unsaved changes. \n is a placeholder for a new line, and the two %@ are placeholders for the date of last save on this device, and date of last autosave on another device, respectively."), saveDateFormatted, autosaveDateFormatted)
         
         let loadSaveButtonTitle = NSLocalizedString("From this device", comment: "Button title displayed in popup indicating date of change on device")
         let fromAutosaveButtonTitle = NSLocalizedString("From another device", comment: "Button title displayed in popup indicating date of change on another device")
