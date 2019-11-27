c68e7e987ca0f66bc810419092630e8b7ff11270
James Frost <james.frost@automattic.com>
2019-11-27 19:08:17 +0000

    DebugMenu: Allow overriding feature flags

--- WordPress/Classes/ViewRelated/Me/DebugMenuViewController.swift
+++ WordPress/Classes/ViewRelated/Me/DebugMenuViewController.swift
@@ -22,7 +22,7 @@ class DebugMenuViewController: UITableViewController {
         super.viewDidLoad()
 
         ImmuTable.registerRows([
-            SwitchRow.self
+            SwitchWithSubtitleRow.self
         ], tableView: tableView)
 
         handler = ImmuTableViewHandler(takeOver: self)
@@ -42,11 +42,18 @@ class DebugMenuViewController: UITableViewController {
     }
 
     private func makeRow(for flag: FeatureFlag) -> ImmuTableRow {
-        return SwitchRow(title: String(describing: flag), value: flag.enabled, onChange: { isOn in
+        let store = FeatureFlagOverrideStore()
+
+        let overridden: String? = store.isOverridden(flag) ? Strings.overridden : nil
+
+        return SwitchWithSubtitleRow(title: String(describing: flag), value: flag.enabled, subtitle: overridden, onChange: { isOn in
+            try? store.override(flag, withValue: isOn)
+            self.reloadViewModel()
         })
     }
 
     enum Strings {
+        static let overridden = NSLocalizedString("Overridden", comment: "Used to indicate a setting is overridden in debug builds of the app")
         static let featureFlags = NSLocalizedString("Feature flags", comment: "Title of the Feature Flags screen used in debug builds of the app")
     }
 }
