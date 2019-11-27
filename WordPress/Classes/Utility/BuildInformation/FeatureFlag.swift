11b7717eb15d13fa2f1bc9fc63934e8ab4424c2d
James Frost <james.frost@automattic.com>
2019-11-27 19:07:41 +0000

    FeatureFlags: Allow overriding of feature flags

--- WordPress/Classes/Utility/BuildInformation/FeatureFlag.swift
+++ WordPress/Classes/Utility/BuildInformation/FeatureFlag.swift
@@ -11,6 +11,10 @@ enum FeatureFlag: Int, CaseIterable {
 
     /// Returns a boolean indicating if the feature is enabled
     var enabled: Bool {
+        if let overriddenValue = FeatureFlagOverrideStore().overriddenValue(for: self) {
+            return overriddenValue
+        }
+
         switch self {
         case .exampleFeature:
             return true
@@ -69,7 +73,15 @@ extension FeatureFlag: CustomStringConvertible {
 
 // MARK: - Overriding Feature Flags
 
-extension FeatureFlag {
+// Protocol allows easier unit testing, so we can implement mock
+// feature flags to use in test cases.
+//
+protocol OverrideableFlag: CustomStringConvertible {
+    var enabled: Bool { get }
+    var canOverride: Bool { get }
+}
+
+extension FeatureFlag: OverrideableFlag {
     var canOverride: Bool {
         switch self {
         case .debugMenu, .exampleFeature:
@@ -78,4 +90,57 @@ extension FeatureFlag {
             return true
         }
     }
+}
+
+/// Used to override values for feature flags at runtime in debug builds
+///
+struct FeatureFlagOverrideStore {
+    private let store: KeyValueDatabase
+
+    init(store: KeyValueDatabase = UserDefaults.standard) {
+        self.store = store
+    }
+
+    private func key(for flag: OverrideableFlag) -> String {
+        return "ff-override-\(String(describing: flag))"
+    }
+
+    /// - returns: True if the specified feature flag is overridden
+    ///
+    func isOverridden(_ featureFlag: OverrideableFlag) -> Bool {
+        return overriddenValue(for: featureFlag) != nil
+    }
+
+    /// Removes any existing overridden value and stores the new value
+    ///
+    func override(_ featureFlag: OverrideableFlag, withValue value: Bool) throws {
+        guard featureFlag.canOverride == true else {
+            throw FeatureFlagError.cannotBeOverridden
+        }
+
+        let key = self.key(for: featureFlag)
+
+        if isOverridden(featureFlag) {
+            store.removeObject(forKey: key)
+        }
+
+        if value != featureFlag.enabled {
+            store.set(value, forKey: key)
+        }
+    }
+
+    /// - returns: The overridden value for the specified feature flag, if one exists.
+    /// If no override exists, returns `nil`.
+    ///
+    func overriddenValue(for featureFlag: OverrideableFlag) -> Bool? {
+        guard let value = store.object(forKey: key(for: featureFlag)) as? Bool else {
+            return nil
+        }
+
+        return value
+    }
+}
+
+enum FeatureFlagError: Error {
+    case cannotBeOverridden
 }
---
+++ WordPress/WordPressTest/FeatureFlagTests.swift
@@ -0,0 +1,108 @@
+//
+//  FeatureFlagTests.swift
+//  WordPressTest
+//
+//  Created by James Frost on 27/11/2019.
+//  Copyright © 2019 WordPress. All rights reserved.
+//
+
+import XCTest
+@testable import WordPress
+
+fileprivate enum MockFeatureFlag: OverrideableFlag {
+    case enabledFeature
+    case disabledFeature
+    case nonOverrideableFeature
+
+    var enabled: Bool {
+        switch self {
+        case .enabledFeature:
+            return true
+        case .disabledFeature:
+            return false
+        case .nonOverrideableFeature:
+            return true
+        }
+    }
+
+    var canOverride: Bool {
+        return self != .nonOverrideableFeature
+    }
+
+    var description: String {
+        switch self {
+        case .enabledFeature:
+            return "Enabled feature"
+        case .disabledFeature:
+            return "Disabled feature"
+        case .nonOverrideableFeature:
+            return "Non overrideable feature"
+        }
+    }
+}
+
+class FeatureFlagTests: XCTestCase {
+    var store: FeatureFlagOverrideStore!
+
+    override func setUp() {
+        store = FeatureFlagOverrideStore(store: EphemeralKeyValueDatabase())
+    }
+
+    func testFeatureFlagIsNotOverriddenByDefault() {
+        let flag = MockFeatureFlag.enabledFeature
+        XCTAssertFalse(store.isOverridden(flag))
+    }
+
+    func testEnabledFeatureFlagValueIsOverridden() {
+        let flag = MockFeatureFlag.enabledFeature
+
+        XCTAssertNil(store.overriddenValue(for: flag))
+        try? store.override(flag, withValue: false)
+
+        let value = store.overriddenValue(for: flag)
+        XCTAssertNotNil(value)
+        XCTAssert(value == false)
+    }
+
+    func testDisabledFeatureFlagValueIsOverridden() {
+        let flag = MockFeatureFlag.disabledFeature
+
+        XCTAssertNil(store.overriddenValue(for: flag))
+        try? store.override(flag, withValue: true)
+
+        let value = store.overriddenValue(for: flag)
+        XCTAssertNotNil(value)
+        XCTAssert(value == true)
+    }
+
+    func testNonOverrideableFeatureFlagCannotBeOverridden() {
+        let flag = MockFeatureFlag.nonOverrideableFeature
+
+        try? store.override(flag, withValue: false)
+        XCTAssertFalse(store.isOverridden(flag))
+    }
+
+    func testEnabledFeatureFlagValueIsNotOverriddenWhenResetToNormalState() {
+        let flag = MockFeatureFlag.enabledFeature
+
+        XCTAssertFalse(store.isOverridden(flag))
+
+        try? store.override(flag, withValue: false)
+        XCTAssertTrue(store.isOverridden(flag))
+
+        try? store.override(flag, withValue: true)
+        XCTAssertFalse(store.isOverridden(flag))
+    }
+
+    func testDisabledFeatureFlagValueIsNotOverriddenWhenResetToNormalState() {
+        let flag = MockFeatureFlag.disabledFeature
+
+        XCTAssertFalse(store.isOverridden(flag))
+
+        try? store.override(flag, withValue: true)
+        XCTAssertTrue(store.isOverridden(flag))
+
+        try? store.override(flag, withValue: false)
+        XCTAssertFalse(store.isOverridden(flag))
+    }
+}
