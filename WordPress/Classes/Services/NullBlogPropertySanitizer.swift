7933635b4a68eb8297b09430badbe349dcf9f07d
Shiki <jayson@basanes.net>
2019-11-28 12:24:34 -0700

    NullBlogPropertySanitizer: Use guard

--- WordPress/Classes/Services/NullBlogPropertySanitizer.swift
+++ WordPress/Classes/Services/NullBlogPropertySanitizer.swift
@@ -9,31 +9,33 @@ struct NullBlogPropertySanitizer {
     }
 
     func sanitize() {
-        if appWasUpdated() {
-            let context = ContextManager.shared.mainContext
-            context.perform {
-                [Post.entityName(),
-                 Page.entityName(),
-                 Media.entityName(),
-                 PostCategory.entityName()].forEach { entityName in
-                    let request = NSFetchRequest<NSManagedObject>(entityName: entityName)
-                    let predicate = NSPredicate(format: "blog == NULL")
-                    request.predicate = predicate
-
-                    if let results = try? context.fetch(request), !results.isEmpty {
-                        results.forEach(context.delete)
-
-                        WPAnalytics.track(.debugDeletedOrphanedEntities, withProperties: [
-                            "entity_name": entityName,
-                            "deleted_count": results.count
-                        ])
-                    }
-                }
-                try? context.save()
-            }
-
-            store.set(currentBuildVersion(), forKey: key)
+        guard appWasUpdated() else {
+            return
         }
+
+        let context = ContextManager.shared.mainContext
+        context.perform {
+            [Post.entityName(),
+             Page.entityName(),
+             Media.entityName(),
+             PostCategory.entityName()].forEach { entityName in
+                let request = NSFetchRequest<NSManagedObject>(entityName: entityName)
+                let predicate = NSPredicate(format: "blog == NULL")
+                request.predicate = predicate
+
+                if let results = try? context.fetch(request), !results.isEmpty {
+                    results.forEach(context.delete)
+
+                    WPAnalytics.track(.debugDeletedOrphanedEntities, withProperties: [
+                        "entity_name": entityName,
+                        "deleted_count": results.count
+                    ])
+                }
+            }
+            try? context.save()
+        }
+
+        store.set(currentBuildVersion(), forKey: key)
     }
 
     private func appWasUpdated() -> Bool {
