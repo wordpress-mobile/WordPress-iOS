84d3cb3e313ef8b2e2214be9d03b8c848d0150e0
Leandro Alonso <contato@leandroalonso.com>
2019-11-28 12:45:47 -0300

    Only sanitize enitites if the app was recently updated

--- WordPress/Classes/Services/NullBlogPropertySanitizer.swift
+++ WordPress/Classes/Services/NullBlogPropertySanitizer.swift
@@ -1,20 +1,44 @@
 import Foundation
 
 struct NullBlogPropertySanitizer {
+    private let store: UserDefaults
+    private let key = "null-property-sanitization"
+
+    init(store: UserDefaults = UserDefaults.standard) {
+        self.store = store
+    }
+
     func sanitize() {
-        let context = ContextManager.shared.mainContext
-        context.perform {
-            [Post.entityName(),
-             Page.entityName(),
-             Media.entityName(),
-             PostCategory.entityName()].forEach { entityName in
-                let request = NSFetchRequest<NSManagedObject>(entityName: entityName)
-                let predicate = NSPredicate(format: "blog == NULL")
-                request.predicate = predicate
-                let results = try? context.fetch(request)
-                results?.forEach(context.delete)
+        if appWasUpdated() {
+            let context = ContextManager.shared.mainContext
+            context.perform {
+                [Post.entityName(),
+                 Page.entityName(),
+                 Media.entityName(),
+                 PostCategory.entityName()].forEach { entityName in
+                    let request = NSFetchRequest<NSManagedObject>(entityName: entityName)
+                    let predicate = NSPredicate(format: "blog == NULL")
+                    request.predicate = predicate
+                    let results = try? context.fetch(request)
+                    results?.forEach(context.delete)
+                }
+                try? context.save()
             }
-            try? context.save()
+
+            store.set(currentBuildVersion(), forKey: key)
         }
+    }
+
+    private func appWasUpdated() -> Bool {
+        return store.double(forKey: key) < currentBuildVersion()
+    }
+
+    private func currentBuildVersion() -> Double {
+        guard let shortVersionString = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String,
+            let version = Double(shortVersionString) else {
+            return 0
+        }
+
+        return version
     }
 }
