f0e435c950c48d2a5ba3ef3e61890734111cb48f
Stephenie Harris <scoutharris@gmail.com>
2019-12-03 14:47:25 -0700

    Adding Tracks extension.

---
+++ WordPress/WordPressAllTimeWidget/Tracks+AllTimeWidget.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+
+/// This extension implements helper tracking methods, meant for Today Widget Usage.
+///
+extension Tracks {
+    // MARK: - Public Methods
+    public func trackExtensionAccessed() {
+        trackExtensionEvent(.Accessed)
+    }
+
+    public func trackExtensionStatsLaunched(_ siteID: Int) {
+        let properties = ["site_id": siteID]
+        trackExtensionEvent(.StatsLaunched, properties: properties as [String: AnyObject]?)
+    }
+
+    public func trackExtensionConfigureLaunched() {
+        trackExtensionEvent(.ConfigureLaunched)
+    }
+
+    public func trackDisplayModeChanged(properties: [String: Bool]) {
+        trackExtensionEvent(.DisplayModeChanged, properties: properties as [String: AnyObject])
+    }
+
+    // MARK: - Private Helpers
+    fileprivate func trackExtensionEvent(_ event: ExtensionEvents, properties: [String: AnyObject]? = nil) {
+        track(event.rawValue, properties: properties)
+    }
+
+
+    // MARK: - Private Enums
+    fileprivate enum ExtensionEvents: String {
+        case Accessed           = "wpios_all_time_extension_accessed"
+        case StatsLaunched      = "wpios_all_time_extension_stats_launched"
+        case ConfigureLaunched  = "wpios_all_time_extension_configure_launched"
+        case DisplayModeChanged = "wpios_all_time_extension_display_mode_changed"
+    }
+}
