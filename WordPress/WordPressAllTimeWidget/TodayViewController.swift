369fb6c148e6d6fa06fcf6d531cf6a2ba32f87e4
Stephenie Harris <scoutharris@gmail.com>
2019-12-03 13:03:37 -0700

    Adding boilerplate widget.

---
+++ WordPress/WordPressAllTimeWidget/TodayViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TodayViewController.swift
+//  WordPressAllTimeWidget
+//
+//  Created by Stephenie Harris on 12/3/19.
+//  Copyright © 2019 WordPress. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+        
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
