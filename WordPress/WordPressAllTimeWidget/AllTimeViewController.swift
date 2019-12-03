6d95ac091dffc9c78de4f538bc8e71cc7d17a3ea
Stephenie Harris <scoutharris@gmail.com>
2019-12-03 13:20:24 -0700

    Adding bridging header.

--- WordPress/WordPressAllTimeWidget/AllTimeViewController.swift
+++ WordPress/WordPressAllTimeWidget/AllTimeViewController.swift
@@ -5,16 +5,9 @@ class AllTimeViewController: UIViewController, NCWidgetProviding {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
-        // Perform any setup necessary in order to update the view.
-
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
-
         completionHandler(NCUpdateResult.newData)
     }
 
