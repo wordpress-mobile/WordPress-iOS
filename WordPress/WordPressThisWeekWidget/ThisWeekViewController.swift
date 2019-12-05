21015bc5152b51b5584da33071a3f3b00af90888
Stephenie Harris <scoutharris@gmail.com>
2019-12-05 15:09:41 -0700

    Adding precompiled header file.

--- WordPress/WordPressThisWeekWidget/ThisWeekViewController.swift
+++ WordPress/WordPressThisWeekWidget/ThisWeekViewController.swift
@@ -2,13 +2,13 @@ import UIKit
 import NotificationCenter
 
 class ThisWeekViewController: UIViewController, NCWidgetProviding {
-        
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-        
+
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
         completionHandler(NCUpdateResult.newData)
     }
-    
+
 }
