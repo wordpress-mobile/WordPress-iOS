c54a101249d26762528d390238740aa0c75cb349
Stephenie Harris <scoutharris@gmail.com>
2019-12-09 15:52:28 -0700

    Adding methods to load & save data.

--- WordPress/WordPressThisWeekWidget/ThisWeekViewController.swift
+++ WordPress/WordPressThisWeekWidget/ThisWeekViewController.swift
@@ -1,14 +1,44 @@
 import UIKit
 import NotificationCenter
 
+    // MARK: - Properties
+
+    private var statsValues: ThisWeekWidgetStats?
+
 class ThisWeekViewController: UIViewController, NCWidgetProviding {
+
+
+    // MARK: - View
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        loadSavedData()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        saveData()
+    }
+
+}
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
         completionHandler(NCUpdateResult.newData)
+// MARK: - Private Extension
+
+private extension ThisWeekViewController {
+
+    // MARK: - Data Management
+
+    func loadSavedData() {
+        statsValues = ThisWeekWidgetStats.loadSavedData()
+    }
+
+    func saveData() {
+        statsValues?.saveData()
     }
 
 }
