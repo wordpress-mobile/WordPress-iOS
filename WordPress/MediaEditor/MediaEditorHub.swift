5cf1fc8fa96e4038185eeb2ed1640be7577a1bc0
Leandro Alonso <contato@leandroalonso.com>
2019-12-11 13:18:44 -0300

    Adds image and cancel button to Hub

--- WordPress/MediaEditor/MediaEditorHub.swift
+++ WordPress/MediaEditor/MediaEditorHub.swift
@@ -2,9 +2,21 @@ import UIKit
 
 class MediaEditorHub: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = UIColor(white: 0.12, alpha: 1)
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cancelButton: UIButton!
+
+    var onCancel: (() -> ())?
+
+    @IBAction func cancel(_ sender: Any) {
+        onCancel?()
+    }
+
+    func show(image: UIImage) {
+        imageView.image = image
+    }
+
+    static func initialize() -> MediaEditorHub {
+        return UIStoryboard(name: "MediaEditorHub", bundle: nil).instantiateViewController(withIdentifier: "hubViewController") as! MediaEditorHub
     }
 
 }
---
+++ WordPress/WordPressTest/MediaEditor/MediaEditorHubTests.swift
@@ -0,0 +1,37 @@
+import XCTest
+import Nimble
+
+@testable import WordPress
+
+class MediaEditorHubTests: XCTestCase {
+
+    func testInitializeFromStoryboard() {
+        let hub: MediaEditorHub = MediaEditorHub.initialize()
+
+        expect(hub).toNot(beNil())
+    }
+
+    func testShowImage() {
+        let hub: MediaEditorHub = MediaEditorHub.initialize()
+        _ = hub.view
+        let image = UIImage()
+
+        hub.show(image: image)
+
+        expect(hub.imageView.image).to(equal(image))
+    }
+
+    func testTappingCancelButtonCallsOnCancel() {
+        var didCallOnCancel = false
+        let hub: MediaEditorHub = MediaEditorHub.initialize()
+        _ = hub.view
+        hub.onCancel = {
+            didCallOnCancel = true
+        }
+
+        hub.cancelButton.sendActions(for: .touchUpInside)
+
+        expect(didCallOnCancel).to(beTrue())
+    }
+
+}
