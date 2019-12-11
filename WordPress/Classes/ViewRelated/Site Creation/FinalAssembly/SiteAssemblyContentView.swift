67786b7a1481733f1f64ec0bee0940729f1eff21
Emily Laguna <the.emilylaguna@gmail.com>
2019-12-10 20:15:03 -0800

    Adds the title/subtitle labels to the site creation loading view

--- WordPress/Classes/ViewRelated/Site Creation/FinalAssembly/SiteAssemblyContentView.swift
+++ WordPress/Classes/ViewRelated/Site Creation/FinalAssembly/SiteAssemblyContentView.swift
@@ -23,6 +23,15 @@ final class SiteAssemblyContentView: UIView {
 
     /// This advises the user that the site creation request completed successfully.
     private(set) var completionLabel: UILabel
+    
+    /// This provides the user with some playful words while their site is being assembled
+    private let statusTitleLabel: UILabel
+    
+    /// This provides the user with some expectation while the site is being assembled
+    private let statusSubtitleLabel: UILabel
+    
+    /// This displays an image while the site is being assembled
+    private let statusImageView: UIImageView
 
     /// This advises the user that the site creation request is underway.
     private let statusLabel: UILabel
@@ -100,7 +109,50 @@ final class SiteAssemblyContentView: UIView {
 
             return label
         }()
+        
+        self.statusTitleLabel = {
+            let label = UILabel()
 
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.numberOfLines = 0
+
+            label.font = WPStyleGuide.fontForTextStyle(.largeTitle, fontWeight: .bold)
+            label.textColor = .text
+            label.textAlignment = .center
+
+            let statusText = NSLocalizedString("Hooray!\nAlmost done",
+                                               comment: "User-facing string, presented to reflect that site assembly is underway.")
+            label.text = statusText
+            label.accessibilityLabel = statusText
+
+            return label
+        }()
+        
+        self.statusSubtitleLabel = {
+            let label = UILabel()
+
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.numberOfLines = 0
+
+            label.font = WPStyleGuide.fontForTextStyle(.title2)
+            label.textColor = .textSubtle
+            label.textAlignment = .center
+
+            let statusText = NSLocalizedString("Your site will be ready shortly",
+                                               comment: "User-facing string, presented to reflect that site assembly is underway.")
+            label.text = statusText
+            label.accessibilityLabel = statusText
+
+            return label
+        }()
+        
+        self.statusImageView = {
+            let image = UIImage(named: "site-creation-loading")
+            let imageView = UIImageView(image: image);
+            
+            return imageView;
+        }()
+        
         self.statusLabel = {
             let label = UILabel()
 
@@ -187,7 +239,7 @@ final class SiteAssemblyContentView: UIView {
 
         backgroundColor = .listBackground
 
-        statusStackView.addArrangedSubviews([ statusLabel, activityIndicator ])
+        statusStackView.addArrangedSubviews([ statusTitleLabel, statusSubtitleLabel, statusImageView, statusLabel, activityIndicator ])
         addSubviews([ completionLabel, statusStackView ])
 
         let completionLabelTopInsetInitial = Parameters.verticalSpacing * 2
