48b545455da38aee733a28f5748a7328e62864d9
Leandro Alonso <contato@leandroalonso.com>
2019-12-11 13:44:12 -0300

    Creates a protocol to define an AsyncImage

---
+++ WordPress/MediaEditor/AsyncImage.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+protocol AsyncImage {
+    var thumb: UIImage? { get }
+
+    func thumbnail(finishedRetrievingThumbnail: @escaping (UIImage?) -> ())
+
+    func full(finishedRetrievingFullImage: @escaping (UIImage?) -> ())
+}
