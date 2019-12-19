4f32a1bb633c27579c7864cfe7cb04217afd8652
Brandon Titus <b@titus.io>
2019-12-19 12:58:17 -0700

    Add Release Notes line and comments

--- WordPress/WordPressShareExtension/MainShareViewController.swift
+++ WordPress/WordPressShareExtension/MainShareViewController.swift
@@ -78,8 +78,8 @@ private extension MainShareViewController {
         let shareNavController = UINavigationController(rootViewController: editorController)
 
         if #available(iOS 13, *), editorController.originatingExtension == .saveToDraft {
-            // iOS 13 has proper animations and presentations for share and action sheets.
-            // We just need to make sure we don't end up with stacked modal view controllers by using this:
+            // iOS 13 has proper animations and presentations for share and action sheets. So the `else` case should be removed when iOS 13 is minimum.
+            // We  need to make sure we don't end up with stacked modal view controllers by using this:
             shareNavController.modalPresentationStyle = .overFullScreen
         } else {
             shareNavController.transitioningDelegate = extensionTransitioningManager
