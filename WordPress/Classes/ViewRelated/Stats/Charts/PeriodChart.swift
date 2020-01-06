067f1486f5edf3fd41d2349c687e77861fcdec59
Shiki <jayson@basanes.net>
2020-01-06 14:32:24 -0700

    PeriodChart: Add label to BarChartDataSet
    These labels are read by VoiceOver when hovering over one of the bars in the Views bar chart. This makes it easier for the user to distinguish between the stacked Views and Visitors numbers.

--- WordPress/Classes/ViewRelated/Stats/Charts/PeriodChart.swift
+++ WordPress/Classes/ViewRelated/Stats/Charts/PeriodChart.swift
@@ -104,8 +104,10 @@ private final class PeriodChartDataTransformer {
 
         var chartData = [BarChartData]()
 
-        let viewsDataSet = BarChartDataSet(values: viewEntries)
-        let visitorsDataSet = BarChartDataSet(values: visitorEntries)
+        let viewsDataSet = BarChartDataSet(values: viewEntries,
+                                           label: NSLocalizedString("Views", comment: "Accessibility label used for distinguishing Views and Visitors in the Stats → Views bar chart."))
+        let visitorsDataSet = BarChartDataSet(values: visitorEntries,
+                                              label: NSLocalizedString("Visitors", comment: "Accessibility label used for distinguishing Views and Visitors in the Stats → Views bar chart."))
         let viewsDataSets = [ viewsDataSet, visitorsDataSet ]
         let viewsChartData = BarChartData(dataSets: viewsDataSets)
         chartData.append(viewsChartData)
