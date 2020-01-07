4f61ee112b754ef8cc0fc6f0851905485c4bcf00
Shiki <jayson@basanes.net>
2020-01-07 15:39:05 -0700

    StatsCellHeader: Fix VoiceOver navigation order
    Previously, the manage insights button was accessed first before the title.

--- WordPress/Classes/ViewRelated/Stats/Shared Views/StatsCellHeader.swift
+++ WordPress/Classes/ViewRelated/Stats/Shared Views/StatsCellHeader.swift
@@ -47,6 +47,8 @@ class StatsCellHeader: UITableViewCell, NibLoadable, Accessible {
         manageInsightButton.accessibilityTraits = .button
         manageInsightButton.accessibilityLabel = NSLocalizedString("Manage Insight", comment: "Accessibility label for button that displays Manage Insight options.")
         manageInsightButton.accessibilityHint = NSLocalizedString("Select to manage this Insight.", comment: "Accessibility hint for Manage Insight button.")
+
+        accessibilityElements = [headerLabel, manageInsightButton].compactMap { $0 }
     }
 }
 
