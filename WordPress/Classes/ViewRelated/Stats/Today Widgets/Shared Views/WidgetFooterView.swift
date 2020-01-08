d39f1697ec2e188b985fd8fc5d03f3d72cfd3475
Stephenie Harris <scoutharris@gmail.com>
2020-01-08 15:52:48 -0700

    Widget footer view: Adding a Visual Effect View for the separator line.

--- WordPress/Classes/ViewRelated/Stats/Today Widgets/Shared Views/WidgetFooterView.swift
+++ WordPress/Classes/ViewRelated/Stats/Today Widgets/Shared Views/WidgetFooterView.swift
@@ -1,4 +1,5 @@
 import UIKit
+import NotificationCenter
 
 class WidgetFooterView: UITableViewHeaderFooterView {
 
@@ -7,6 +8,7 @@ class WidgetFooterView: UITableViewHeaderFooterView {
     static let reuseIdentifier = "WidgetFooterView"
 
     @IBOutlet private var separatorLine: UIView!
+    @IBOutlet private var separatorVisualEffect: UIVisualEffectView!
     @IBOutlet private var siteUrlLabel: UILabel!
     @IBOutlet private var backgroundColorView: UIView!
 
@@ -27,8 +29,17 @@ class WidgetFooterView: UITableViewHeaderFooterView {
 
 private extension WidgetFooterView {
     func configureColors() {
-        separatorLine.backgroundColor = UIColor(light: .divider, dark: .textSubtle)
         siteUrlLabel.textColor = .textSubtle
         backgroundColorView.backgroundColor = .clear
+
+        if #available(iOS 13, *) {
+            separatorLine.backgroundColor = UIColor(white: 1.0, alpha: 0.5)
+            separatorLine.tintColor = UIColor(white: 1.0, alpha: 0.5)
+            separatorVisualEffect.effect = UIVibrancyEffect.widgetEffect(forVibrancyStyle: .separator)
+        } else {
+            separatorLine.backgroundColor = .divider
+            separatorLine.tintColor = .divider
+            separatorVisualEffect.effect = UIVibrancyEffect.widgetSecondary()
+        }
     }
 }
