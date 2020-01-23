aab290d48c0c647b6187aae7df778e851a647035
Stephenie Harris <scoutharris@gmail.com>
2020-01-23 12:34:02 -0700

    Remove support for noConnection.

--- WordPress/Classes/ViewRelated/Stats/Today Widgets/Shared Views/WidgetUnconfiguredCell.swift
+++ WordPress/Classes/ViewRelated/Stats/Today Widgets/Shared Views/WidgetUnconfiguredCell.swift
@@ -4,16 +4,6 @@ enum WidgetType {
     case today
     case allTime
     case thisWeek
-    case noConnection
-
-    var configureLabelFont: UIFont {
-        switch self {
-        case .noConnection:
-            return UIFont.systemFont(ofSize: UIFont.preferredFont(forTextStyle: .headline).pointSize)
-        default:
-            return UIFont.systemFont(ofSize: UIFont.preferredFont(forTextStyle: .footnote).pointSize)
-        }
-    }
 }
 
 class WidgetUnconfiguredCell: UITableViewCell {
@@ -27,7 +17,7 @@ class WidgetUnconfiguredCell: UITableViewCell {
     @IBOutlet private var separatorVisualEffectView: UIVisualEffectView!
     @IBOutlet private var actionLabel: UILabel!
 
-    private(set) var widgetType: WidgetType?
+    private var widgetType: WidgetType?
 
     // MARK: - View
 
@@ -47,8 +37,6 @@ private extension WidgetUnconfiguredCell {
             return
         }
 
-        configureLabel.font = widgetType.configureLabelFont
-
         configureLabel.text = {
             switch widgetType {
             case .today:
@@ -57,12 +45,10 @@ private extension WidgetUnconfiguredCell {
                 return LocalizedText.configureAllTime
             case .thisWeek:
                 return LocalizedText.configureThisWeek
-            case .noConnection:
-                return LocalizedText.noConnection
             }
         }()
 
-        actionLabel.text = widgetType == .noConnection ? LocalizedText.retry : LocalizedText.openWordPress
+        actionLabel.text = LocalizedText.openWordPress
         configureLabel.textColor = WidgetStyles.primaryTextColor
         actionLabel.textColor = WidgetStyles.primaryTextColor
         WidgetStyles.configureSeparator(separatorLine)
@@ -74,8 +60,6 @@ private extension WidgetUnconfiguredCell {
         static let configureAllTime = NSLocalizedString("Display your all-time site stats here. Configure in the WordPress app in your site stats.", comment: "Unconfigured stats all-time widget helper text")
         static let configureThisWeek = NSLocalizedString("Display your site stats for this week here. Configure in the WordPress app in your site stats.", comment: "Unconfigured stats this week widget helper text")
         static let openWordPress = NSLocalizedString("Open WordPress", comment: "Today widget label to launch WP app")
-        static let noConnection = NSLocalizedString("No network available", comment: "Displayed in the Stats widgets when there is no network")
-        static let retry = NSLocalizedString("Retry", comment: "Stats widgets label to reload the widget")
     }
 
 }
