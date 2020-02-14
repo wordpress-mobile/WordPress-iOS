02a50836fa7bce0c5d7d429a11aae01b55433b92
Emily Laguna <emily.laguna@automattic.com>
2020-02-14 09:59:20 -0800

    Add PSI test placeholder

---
+++ WordPress/WordPressTest/NUX/Post Signup Interstitial/PostSignUpInterstitialCoordinatorTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+
+@testable import WordPress
+
+class PostSignUpInterstitialCoordinatorTests: XCTestCase {
+    func testShouldNotDisplay() {
+//        let coordinator = PostSignUpInterstitialCoordinator()
+//        XCTAssertFalse(coordinator.shouldDisplay(numberOfBlogs: 10))
+    }
+
+    func testShouldDisplay() {
+//        let coordinator = PostSignUpInterstitialCoordinator()
+//        XCTAssertFalse(coordinator.shouldDisplay(numberOfBlogs: 0))
+    }
+}
