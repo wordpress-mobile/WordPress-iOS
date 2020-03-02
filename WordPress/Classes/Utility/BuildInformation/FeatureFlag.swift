28f5441b27d9cf488196e66fb75395483c6c000e
Brandon Titus <b@titus.io>
2020-03-02 07:09:39 -0600

    Enable create button on Feature Branch

--- WordPress/Classes/Utility/BuildInformation/FeatureFlag.swift
+++ WordPress/Classes/Utility/BuildInformation/FeatureFlag.swift
@@ -34,7 +34,7 @@ enum FeatureFlag: Int, CaseIterable {
         case .meMove:
             return BuildConfiguration.current == .localDeveloper
         case .floatingCreateButton:
-            return BuildConfiguration.current == .localDeveloper
+            return BuildConfiguration.current ~= [.localDeveloper, .a8cBranchTest]
         }
     }
 }
