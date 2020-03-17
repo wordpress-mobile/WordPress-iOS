3271f1de2283202536a5d5014ec2c542a8d9d797
Leandro Alonso <contato@leandroalonso.com>
2020-03-17 10:44:26 -0300

    Create Prepublishing VC

---
+++ WordPress/Classes/ViewRelated/Post/PrepublishingViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class PrepublishingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
