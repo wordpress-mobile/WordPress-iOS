e9d06e3dd58d4de676933b28aea90c6d4429d4ac
James Frost <james.frost@automattic.com>
2020-03-19 11:12:57 +0000

    Migrate deprecated Gridicons method calls

--- WordPress/Classes/ViewRelated/System/Floating Create Button/CreateButtonCoordinator.swift
+++ WordPress/Classes/ViewRelated/System/Floating Create Button/CreateButtonCoordinator.swift
@@ -70,12 +70,12 @@ import Gridicons
 
     private func actionSheetController(for traitCollection: UITraitCollection) -> UIViewController {
         let postsButton = ActionSheetButton(title: NSLocalizedString("Blog post", comment: "Create new Blog Post button title"),
-                                            image: Gridicon.iconOfType(.posts),
+                                            image: .gridicon(.posts),
                                             identifier: "blogPostButton",
                                             target: self,
                                             selector: #selector(showNewPost))
         let pagesButton = ActionSheetButton(title: NSLocalizedString("Site page", comment: "Create new Site Page button title"),
-                                            image: Gridicon.iconOfType(.pages),
+                                            image: .gridicon(.pages),
                                             identifier: "sitePageButton",
                                             target: self,
                                             selector: #selector(showNewPage))
