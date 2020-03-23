2096f295f39c9349d8ae3c34da7e6bd1a66e24cf
Leandro Alonso <contato@leandroalonso.com>
2020-03-23 17:47:12 -0300

    Fix unit tests

--- WordPress/WordPressTest/Bottom Sheet/BottomSheetViewControllerTests.swift
+++ WordPress/WordPressTest/Bottom Sheet/BottomSheetViewControllerTests.swift
@@ -8,7 +8,7 @@ class BottomSheetViewControllerTests: XCTestCase {
     /// - Add the given ViewController as a child View Controller
     ///
     func testAddTheGivenViewControllerAsAChildViewController() {
-        let viewController = UIViewController()
+        let viewController = BottomSheetPresentableViewController()
         let bottomSheet = BottomSheetViewController(childViewController: viewController)
 
         bottomSheet.viewDidLoad()
@@ -19,11 +19,15 @@ class BottomSheetViewControllerTests: XCTestCase {
     /// - Add the given ViewController view to the subviews of the Bottom Sheet
     ///
     func testAddGivenVCViewToTheBottomSheetSubviews() {
-        let viewController = UIViewController()
+        let viewController = BottomSheetPresentableViewController()
         let bottomSheet = BottomSheetViewController(childViewController: viewController)
 
         bottomSheet.viewDidLoad()
 
         expect(bottomSheet.view.subviews.flatMap { $0.subviews }).to(contain(viewController.view))
     }
+}
+
+private class BottomSheetPresentableViewController: UIViewController, BottomSheetPresentable {
+    var initialHeight: CGFloat = 0
 }
