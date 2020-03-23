13676e5e34e8d8a7bfb3ebb7d71b4b9bd61a0ca6
Leandro Alonso <contato@leandroalonso.com>
2020-03-23 18:02:27 -0300

    Make sure the callback works

---
+++ WordPress/WordPressTest/PrepublishingNudgesViewControllerTests.swift
@@ -0,0 +1,31 @@
+import XCTest
+import Nimble
+
+@testable import WordPress
+
+class PrepublishingNudgesViewControllerTests: XCTestCase {
+
+    override class func setUp() {
+        super.setUp()
+
+        /// We need that in order to initialize the Authenticator, otherwise this test crashes
+        /// This is because we're using the NUXButton. Ideally, that component should be extracted
+        WordPressAuthenticationManager().initializeWordPressAuthenticator()
+    }
+
+    /// Call the completion block when the "Publish" button is pressed
+    ///
+    func testCallCompletionBlockWhenButtonTapped() {
+        let post = PostBuilder().build()
+        var returnedPost: AbstractPost?
+        let prepublishingViewController = PrepublishingViewController(post: post) { post in
+            returnedPost = post
+        }
+        prepublishingViewController.viewDidLoad()
+
+        prepublishingViewController.nuxButton.sendActions(for: .touchUpInside)
+
+        expect(returnedPost).to(equal(post))
+    }
+
+}
