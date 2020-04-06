9ce7291bdbd11621baf049d53248605f202e9d7c
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2020-04-06 11:54:54 -0500

    Update ReaderTabViewModel.swift, refactor constants in a private enum, add applicationWillTerminate and accountChanged notification observers to handle cleanup actions

--- WordPress/Classes/ViewRelated/Reader/Tab Navigation/ReaderTabViewModel.swift
+++ WordPress/Classes/ViewRelated/Reader/Tab Navigation/ReaderTabViewModel.swift
@@ -1,17 +1,21 @@
 
 class ReaderTabViewModel {
 
-    var navigationClosure: ((ReaderAbstractTopic) -> Void)?
+    var tabSelectionCallback: ((ReaderAbstractTopic) -> Void)?
 
-    // TODO: - READERNAV - Methods to be implemented. Signature will likely change
+    init() {
+        addNotificationsObservers()
+    }
+
     func showTab(for item: FilterTabBarItem) {
         guard let readerItem = item as? ReaderTabItem,
             let topic = readerItem.topic else {
-            return
+                return
         }
-        navigationClosure?(topic)
+        tabSelectionCallback?(topic)
     }
-
+    
+    // TODO: - READERNAV - Methods to be implemented. Signature will likely change
     func performSearch() { }
 
     func presentFilter() { }
@@ -27,12 +31,11 @@ extension ReaderTabViewModel {
 
     /// Fetch request to extract reader menu topics from Core Data
     private var topicsFetchRequest: NSFetchRequest<NSFetchRequestResult> {
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "ReaderAbstractTopic")
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: ReaderTopics.entityName)
 
-        fetchRequest.predicate = NSPredicate(format: "following == %@ AND showInMenu == YES AND type == 'default' OR type == 'list' OR type == 'team'",
-                                             NSNumber(value: ReaderHelpers.isLoggedIn()))
+        fetchRequest.predicate = NSPredicate(format: ReaderTopics.predicate, NSNumber(value: ReaderHelpers.isLoggedIn()))
 
-        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "type", ascending: true)]
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: ReaderTopics.sortByKey, ascending: true)]
         return fetchRequest
     }
 
@@ -47,7 +50,7 @@ extension ReaderTabViewModel {
             completion(ReaderHelpers.rearrange(items: topics.map { ReaderTabItem(topic: $0) }))
 
         } catch {
-            DDLogError("There was a problem fetching topics for the menu." + error.localizedDescription)
+            DDLogError(ReaderTopics.fetchRequestError + error.localizedDescription)
             completion(nil)
         }
     }
@@ -60,9 +63,19 @@ extension ReaderTabViewModel {
         service.fetchReaderMenu(success: { [weak self] in
             self?.fetchTabBarItems(completion: completion)
             }, failure: { error in
-                DDLogError("Error syncing menu: \(String(describing: error))")
+                DDLogError(ReaderTopics.remoteFetchError + "\(String(describing: error))")
                 completion(nil)
         })
+    }
+
+    private enum ReaderTopics {
+        static let predicate = "following == %@ AND showInMenu == YES AND type == 'default' OR type == 'list' OR type == 'team'"
+
+        static let entityName = "ReaderAbstractTopic"
+        static let sortByKey = "type"
+
+        static let fetchRequestError = "There was a problem fetching topics for the menu. "
+        static let remoteFetchError = "Error syncing menu: "
     }
 }
 
@@ -74,13 +87,65 @@ extension ReaderTabViewModel {
         guard let topic = item.topic else {
             return nil
         }
-
         let controller = ReaderStreamViewController.controllerWithTopic(topic)
 
-        self.navigationClosure = { topic in
-            controller.readerTopic = topic
+        self.tabSelectionCallback = { [weak controller] topic in
+            controller?.readerTopic = topic
+        }
+        return controller
+    }
+}
+
+
+// MARK: - Logout and Termination Cleanup
+extension ReaderTabViewModel {
+
+    private func addNotificationsObservers() {
+        NotificationCenter.default.addObserver(forName: UIApplication.willTerminateNotification,
+                                               object: nil,
+                                               queue: nil) { notification in
+                                                self.cleanupStaleContent(removeAllTopics: false)
+                                                self.unflagInUseContent()
         }
 
-        return controller
+        NotificationCenter.default.addObserver(forName: .WPAccountDefaultWordPressComAccountChanged,
+                                               object: nil,
+                                               queue: nil) { notification in
+                                                self.unflagInUseContent()
+                                                self.clearSavedPosts()
+                                                self.cleanupStaleContent(removeAllTopics: true)
+                                                self.clearSearchSuggestions()
+        }
+    }
+
+    /// Clears the inUse flag from any topics or posts so marked.
+    private func unflagInUseContent() {
+        let context = ContextManager.sharedInstance().mainContext
+        ReaderPostService(managedObjectContext: context).clearInUseFlags()
+        ReaderTopicService(managedObjectContext: context).clearInUseFlags()
+    }
+
+    /// Clean up topics that do not belong in the menu and posts that have no topic
+    /// This is merely a convenient place to perform this task.
+    private func cleanupStaleContent(removeAllTopics removeAll: Bool) {
+        let context = ContextManager.sharedInstance().mainContext
+        ReaderPostService(managedObjectContext: context).deletePostsWithNoTopic()
+
+        if removeAll {
+            ReaderTopicService(managedObjectContext: context).deleteAllTopics()
+        } else {
+            ReaderTopicService(managedObjectContext: context).deleteNonMenuTopics()
+        }
+    }
+
+    /// Clears all saved posts, so they can be deleted by cleanup methods.
+    private func clearSavedPosts() {
+        let context = ContextManager.sharedInstance().mainContext
+        ReaderPostService(managedObjectContext: context).clearSavedPostFlags()
+    }
+
+    private func clearSearchSuggestions() {
+        let context = ContextManager.sharedInstance().mainContext
+        ReaderSearchSuggestionService(managedObjectContext: context).deleteAllSuggestions()
     }
 }
