e70901ca2d8185814ba2e8c8056d9868d4440f5a
Leandro Alonso <contato@leandroalonso.com>
2020-04-07 11:45:12 -0300

    Change navigation header to display title

--- WordPress/Classes/ViewRelated/Post/Prepublishing Nudge/PrepublishingNavigationController.swift
+++ WordPress/Classes/ViewRelated/Post/Prepublishing Nudge/PrepublishingNavigationController.swift
@@ -97,7 +97,13 @@ extension PrepublishingNavigationController: UINavigationControllerDelegate {
     /// Animated the back button based on what View Controller will be shown
     func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
         transitionCoordinator?.animate(alongsideTransition: { context in
-            viewController is PrepublishingViewController ? self.header.hideBackButton() : self.header.showBackButton()
+            if viewController is PrepublishingViewController {
+                self.header.hideBackButton()
+                self.header.hideTitle()
+            } else {
+                self.header.showBackButton()
+                self.header.showTitle()
+            }
 
             self.header.setTitle(viewController.title, transitionDuration: context.transitionDuration)
         }, completion: nil)
