445da7d5d824bef2be47c940e17a5ff350b64a97
Leandro Alonso <contato@leandroalonso.com>
2020-04-07 16:25:41 -0300

    Use LightNavigationController

--- WordPress/Classes/ViewRelated/Post/Prepublishing Nudge/PrepublishingNavigationController.swift
+++ WordPress/Classes/ViewRelated/Post/Prepublishing Nudge/PrepublishingNavigationController.swift
@@ -1,30 +1,12 @@
 import UIKit
 
-
-class PrepublishingNavigationController: UINavigationController {
+class PrepublishingNavigationController: LightNavigationController {
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
-        configureNavigationBar()
 
         // Set the height for iPad
         if UIDevice.isPad() {
             view.heightAnchor.constraint(equalToConstant: Constants.height).isActive = true
-        }
-    }
-
-    private func configureNavigationBar() {
-        if #available(iOS 13.0, *) {
-            let appearance = UINavigationBarAppearance()
-            appearance.configureWithOpaqueBackground()
-//            appearance.backgroundColor = .white
-
-            navigationBar.standardAppearance = appearance
-            
-        } else {
-            let clearImage = UIImage(color: .clear, havingSize: CGSize(width: 1, height: 1))
-            navigationBar.shadowImage = clearImage
         }
     }
 
