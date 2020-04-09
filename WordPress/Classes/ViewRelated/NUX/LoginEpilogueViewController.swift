829ed690f9fe32387fbc946616e4a254bac8e3ec
Stephenie Harris <scoutharris@gmail.com>
2020-04-09 14:48:10 -0600

    Set table view margins based on size class and device orientation.

--- WordPress/Classes/ViewRelated/NUX/LoginEpilogueViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/LoginEpilogueViewController.swift
@@ -20,6 +20,11 @@ class LoginEpilogueViewController: UIViewController {
     ///
     @IBOutlet var doneButton: UIButton!
 
+    /// Constraints on the table view container.
+    /// Used to adjust the table width on iPad.
+    @IBOutlet var tableViewLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet var tableViewTrailingConstraint: NSLayoutConstraint!
+
     /// Links to the Epilogue TableViewController
     ///
     private var tableViewController: LoginEpilogueTableViewController?
@@ -50,8 +55,9 @@ class LoginEpilogueViewController: UIViewController {
             fatalError()
         }
 
-        view.backgroundColor = .basicBackground
+        view.backgroundColor = .yellow// .basicBackground
         topLine.backgroundColor = .divider
+        setTableViewMargins()
         refreshInterface(with: credentials)
     }
 
@@ -91,8 +97,18 @@ class LoginEpilogueViewController: UIViewController {
         super.viewDidLayoutSubviews()
         configurePanelBasedOnTableViewContents()
     }
-}
 
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        setTableViewMargins()
+    }
+
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        setTableViewMargins()
+    }
+
+}
 
 // MARK: - Configuration
 //
@@ -132,6 +148,29 @@ private extension LoginEpilogueViewController {
             topLine.isHidden = true
         }
     }
+
+    func setTableViewMargins() {
+        guard traitCollection.horizontalSizeClass == .regular &&
+            traitCollection.verticalSizeClass == .regular else {
+                tableViewLeadingConstraint.constant = 0
+                tableViewTrailingConstraint.constant = 0
+                return
+        }
+
+        let marginMultiplier = UIDevice.current.orientation.isLandscape ?
+            TableViewMarginMultipliers.ipadLandscape :
+            TableViewMarginMultipliers.ipadPortrait
+
+        let margin = view.frame.width * marginMultiplier
+        tableViewLeadingConstraint.constant = margin
+        tableViewTrailingConstraint.constant = margin
+    }
+
+    private enum TableViewMarginMultipliers {
+        static let ipadPortrait: CGFloat = 0.16
+        static let ipadLandscape: CGFloat = 0.25
+    }
+
 }
 
 
