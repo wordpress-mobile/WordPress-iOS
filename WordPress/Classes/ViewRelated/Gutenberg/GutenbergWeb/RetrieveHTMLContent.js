a7684f960e579278832f6db533bbceb4ae950db3
eToledo <etoledom@icloud.com>
2020-04-28 21:35:58 +0200

    [Gutenberg Web] Avoid creating drafts on remote

--- WordPress/Classes/ViewRelated/Gutenberg/GutenbergWeb/RetrieveHTMLContent.js
+++ WordPress/Classes/ViewRelated/Gutenberg/GutenbergWeb/RetrieveHTMLContent.js
@@ -3,3 +3,23 @@ window.getHTMLPostContent = () => {
     const HTML = window.wp.blocks.serialize( blocks );
     window.webkit.messageHandlers.htmlPostContent.postMessage(HTML);
 };
+
+(function(open) {
+  XMLHttpRequest.prototype.open = function(arg1, arg2) {
+    this.URL = arg2;
+    this.method = arg1;
+    open.apply(this, arguments);
+  };
+})(XMLHttpRequest.prototype.open);
+
+(function(send) {
+  XMLHttpRequest.prototype.send = function(arg1) {
+      window.webkit.messageHandlers.log.postMessage(String(this.method + ': ' + this.URL));
+    if (this.URL.includes('/autosaves') && this.method === "POST") {
+      window.webkit.messageHandlers.log.postMessage("Prevented autosave");
+      this.abort();
+      return;
+    }
+    send.apply(this, arguments);
+  };
+})(XMLHttpRequest.prototype.send);
