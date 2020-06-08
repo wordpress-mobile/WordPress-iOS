3249f7eded6c8ac62b92f76b4223d5f268b9c0d9
Leandro Alonso <contato@leandroalonso.com>
2020-06-08 10:02:53 -0300

    Remove self

--- WordPress/Classes/ViewRelated/Reader/DetailWebView/ReaderDetailToolbar.swift
+++ WordPress/Classes/ViewRelated/Reader/DetailWebView/ReaderDetailToolbar.swift
@@ -79,7 +79,7 @@ class ReaderDetailToolbar: UIView, NibLoadable {
     }
 
     @IBAction func didTapComment(_ sender: Any) {
-        guard let post = self.post, let viewController = viewController else {
+        guard let post = post, let viewController = viewController else {
             return
         }
 
