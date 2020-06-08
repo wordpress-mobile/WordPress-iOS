844b53d73bcd52fe33a2ae025505133c6f10a220
Emily Laguna <emily.laguna@automattic.com>
2020-06-08 11:44:07 -0700

    Move the round method to a private extension

--- WordPress/Classes/Extensions/Math.swift
+++ WordPress/Classes/Extensions/Math.swift
@@ -1,25 +1,3 @@
-extension Int {
-    /// Rounds self to the nearest multiple of the given argument
-    ///
-    /// In case of a tie, it rounds to the multiple with the highest absolute value.
-    /// That is, `-3.round(5)` rounds to `-5` and `3.round(5)` rounds to `5`
-    ///
-    /// - Parameter divisor: a positive number that's a divisor of the result.
-    /// - Precondition: divisor must be > 0
-    /// - returns: an Int rounded to the nearest integer that's a multiple of the argument.
-    ///
-    func round(_ divisor: UInt) -> Int {
-        assert(divisor > 0)
-        if self == 0 || divisor == 0 {
-            return self
-        }
-        let sign = self / abs(self)
-        let divisor = Int(divisor)
-        let half = divisor / 2
-        return self + sign * (half - (abs(self) as Int + half) % divisor)
-    }
-}
-
 extension Comparable {
     /// Clamps self between a minimum and maximum value
     ///
--- WordPress/Classes/ViewRelated/Cells/MediaSizeSliderCell.swift
+++ WordPress/Classes/ViewRelated/Cells/MediaSizeSliderCell.swift
@@ -106,6 +106,29 @@ class MediaSizeSliderCell: WPTableViewCell {
 
 }
 
+private extension Int {
+    /// Rounds self to the nearest multiple of the given argument
+    ///
+    /// In case of a tie, it rounds to the multiple with the highest absolute value.
+    /// That is, `-3.round(5)` rounds to `-5` and `3.round(5)` rounds to `5`
+    ///
+    /// - Parameter divisor: a positive number that's a divisor of the result.
+    /// - Precondition: divisor must be > 0
+    /// - returns: an Int rounded to the nearest integer that's a multiple of the argument.
+    ///
+    func round(_ divisor: UInt) -> Int {
+        assert(divisor > 0)
+        if self == 0 || divisor == 0 {
+            return self
+        }
+        let absoluteValue: Int = abs(self) as Int
+        let sign: Int = self / absoluteValue
+        let divisor: Int = Int(divisor)
+        let half: Int = divisor / 2
+        return self + sign * (half - (absoluteValue + half) % divisor)
+    }
+}
+
 struct ImageSizeModel: MediaSizeModel {
     var value: Int {
         didSet {
