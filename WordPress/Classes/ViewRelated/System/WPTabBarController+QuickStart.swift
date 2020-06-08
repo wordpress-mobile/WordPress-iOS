9077b115a80e87c9f77df9511ad9ea826f389044
Thuy.Copeland <thuycopeland@gmail.com>
2020-06-08 16:37:30 -0500

    Delete references to the .newpost element
    The new post button no longer lives here.

--- WordPress/Classes/ViewRelated/System/WPTabBarController+QuickStart.swift
+++ WordPress/Classes/ViewRelated/System/WPTabBarController+QuickStart.swift
@@ -10,7 +10,7 @@ extension WPTabBarController {
 
             guard let userInfo = notification.userInfo,
                 let element = userInfo[QuickStartTourGuide.notificationElementKey] as? QuickStartTourElement,
-                [.newpost, .readerTab].contains(element),
+                [.readerTab].contains(element),
                 let tabBar = self?.tabBar else {
                     return
             }
@@ -18,21 +18,13 @@ extension WPTabBarController {
             let newSpotlight = QuickStartSpotlightView()
             tabBar.addSubview(newSpotlight)
 
-            let x: CGFloat
-            if element == .readerTab {
-                x = tabBar.bounds.size.width / 2.0
-            } else {
-                x = tabBar.bounds.size.width * 0.40 - newSpotlight.frame.size.width
-            }
+            let x = tabBar.bounds.size.width / 2.0
+
             newSpotlight.frame = CGRect(x: x, y: spotlightViewOffset, width: newSpotlight.frame.width, height: newSpotlight.frame.height)
             spotlightView = newSpotlight
         }
 
         quickStartObserver = observer as? NSObject
-    }
-
-    @objc func alertQuickStartThatWriteWasTapped() {
-        tourGuide.visited(.newpost)
     }
 
     @objc func alertQuickStartThatReaderWasTapped() {
