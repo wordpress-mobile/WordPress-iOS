58a6f44edfa03523d6d593ff797792b8ca6c85a1
Emily Laguna <emily.laguna@automattic.com>
2020-06-11 14:38:36 -0700

    Whitespace

--- WordPress/Packages/Extensions/Tests/ExtensionsTests/ExtensionsTests.swift
+++ WordPress/Packages/Extensions/Tests/ExtensionsTests/ExtensionsTests.swift
@@ -5,7 +5,7 @@ final class ExtensionsTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
+        // results. 
         XCTAssertEqual(Extensions().text, "Hello, World!")
     }
 
