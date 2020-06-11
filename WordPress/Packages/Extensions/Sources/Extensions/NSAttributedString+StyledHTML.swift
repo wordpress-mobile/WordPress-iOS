d3c770a253f23648c4f70c45286f52605fc31836
Emily Laguna <emily.laguna@automattic.com>
2020-06-11 14:25:51 -0700

    Fix lint errors

--- WordPress/Packages/Extensions/Sources/Extensions/NSAttributedString+StyledHTML.swift
+++ WordPress/Packages/Extensions/Sources/Extensions/NSAttributedString+StyledHTML.swift
@@ -2,16 +2,16 @@ import UIKit
 
 private extension UIColor {
     func hexString() -> String? {
-        var r:CGFloat = 0
-        var g:CGFloat = 0
-        var b:CGFloat = 0
-        var a:CGFloat = 0
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var a: CGFloat = 0
 
         getRed(&r, green: &g, blue: &b, alpha: &a)
 
-        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        let rgb: Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
 
-        return String(format:"#%06x", rgb)
+        return String(format: "#%06x", rgb)
     }
 }
 
