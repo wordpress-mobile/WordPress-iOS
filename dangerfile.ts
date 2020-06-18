146cc4891cb7df595c3a7acb804980904c9f265c
Gio Lodi <giovanni.lodi42@gmail.com>
2020-06-18 15:59:26 +1000

    Add a local Dangerfile to test behavior on forked repos

---
+++ dangerfile.ts
@@ -0,0 +1,16 @@
+// TODO: would need a check to make sure that we are running from GitHub
+// Actions, too
+const headRepoName = danger.github.pr.head.repo.full_name
+const baseRepoName = danger.github.pr.base.repo.full_name
+
+if headRepoName != baseRepoName {
+  // TODO:
+  // - Add link to the docs
+  // - Maybe there's a way to see if commenting is possible? Maybe this type of
+  //   error could be printed only if the comment API returns a 403?
+  console.log("⚠️ Running from a forked repo. Danger won't be able to post comments on the main repo unless GitHub Actions are enabled on the fork, too.")
+}
+
+// I'm not expecting this comment to be posted because the forked repo where
+// this Dangerfile lives doesn't have GitHub Actions enabled.
+warn("This is a test message.")
