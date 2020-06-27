cd5501205595f373d6bccb34b702bd97afc2b39c
James Frost <james.frost@automattic.com>
2020-06-27 08:54:47 +0100

    Roll back the site title if we failed to update it remotely

--- WordPress/Classes/ViewRelated/Blog/Blog Details/BlogDetailsViewController+Header.swift
+++ WordPress/Classes/ViewRelated/Blog/Blog Details/BlogDetailsViewController+Header.swift
@@ -60,6 +60,8 @@ extension BlogDetailsViewController {
             return
         }
 
+        // Save the old value in case we need to roll back
+        let existingBlogTitle = blog.settings?.name ?? SiteTitleStrings.defaultSiteTitle
         blog.settings?.name = title
 
         let service = BlogService(managedObjectContext: context)
@@ -72,7 +74,9 @@ extension BlogDetailsViewController {
             ActionDispatcher.global.dispatch(NoticeAction.post(notice))
 
             self?.headerView.refreshSiteTitle()
-        }, failure: { error in
+        }, failure: { [weak self] error in
+            self?.blog.settings?.name = existingBlogTitle
+            
             let notice = Notice(title: SiteTitleStrings.settingsSaveErrorTitle,
                                 message: SiteTitleStrings.settingsSaveErrorMessage,
                                 feedbackType: .error)
@@ -86,6 +90,7 @@ extension BlogDetailsViewController {
         static let siteTitleHint = NSLocalizedString("The Site Title is displayed in the title bar of a web browser and is displayed in the header for most themes.", comment: "Description of the purpose of a site's title.")
         static let notAnAdminHint = NSLocalizedString("The Site Title can only be changed by a user with the administrator role.", comment: "Message informing the user that the site title can only be changed by an administrator user.")
         static let placeholderText = NSLocalizedString("A title for the site", comment: "Placeholder text for the title of a site")
+        static let defaultSiteTitle = NSLocalizedString("Site Title", comment: "Default title for a site")
         static let settingsViewControllerTitle = NSLocalizedString("Site Title", comment: "Title for screen that show site title editor")
         static let titleChangeSuccessfulMessage = NSLocalizedString("Site title changed successfully", comment: "Confirmation that the user successfully changed the site's title")
         static let settingsSaveErrorTitle = NSLocalizedString("Error updating site title", comment: "Error message informing the user that their site's title could not be changed")
