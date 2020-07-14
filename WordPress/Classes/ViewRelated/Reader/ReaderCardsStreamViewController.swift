03ba076a452547ccd09dd4dcdd75c7304f10b278
Leandro Alonso <contato@leandroalonso.com>
2020-07-14 13:40:56 -0300

    Reset currentPage when fetching the first page

--- WordPress/Classes/ViewRelated/Reader/ReaderCardsStreamViewController.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderCardsStreamViewController.swift
@@ -34,6 +34,7 @@ class ReaderCardsStreamViewController: ReaderStreamViewController {
     // MARK: - Sync
 
     override func fetch(for topic: ReaderAbstractTopic, success: @escaping ((Int, Bool) -> Void), failure: @escaping ((Error?) -> Void)) {
+        currentPage = 1
         cardsService.fetch(page: 1, success: success, failure: failure)
     }
 
