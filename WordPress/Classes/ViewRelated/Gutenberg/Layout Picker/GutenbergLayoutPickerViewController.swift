a1deab82796e37c0608ec873fcffce0f60bc2c7c
Chip Snyder <chip.snyder3@gmail.com>
2020-07-16 09:29:51 -0400

    Work on snapping to position

--- WordPress/Classes/ViewRelated/Gutenberg/Layout Picker/GutenbergLayoutPickerViewController.swift
+++ WordPress/Classes/ViewRelated/Gutenberg/Layout Picker/GutenbergLayoutPickerViewController.swift
@@ -6,8 +6,9 @@ class GutenbergLayoutPickerViewController: UIViewController {
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var headerBar: UIView!
     @IBOutlet weak var closeButton: UIButton!
-    @IBOutlet weak var largeTitleView: UILabel!
     @IBOutlet weak var titleView: UILabel!
+    @IBOutlet weak var largeTitleView: UILabel!
+    @IBOutlet weak var promptView: UILabel!
     @IBOutlet weak var categoryBar: UICollectionView!
     @IBOutlet weak var headerHeightConstraint: NSLayoutConstraint!
 
@@ -20,6 +21,7 @@ class GutenbergLayoutPickerViewController: UIViewController {
     let minTitleFontSize: CGFloat = 17
     let maxTitleFontSize: CGFloat = 34
     var maxHeaderHeight: CGFloat = 285
+    var midHeaderHeight: CGFloat = 212
     var minHeaderHeight: CGFloat {
         return headerBar.frame.height + 20 + categoryBar.frame.height + 9
     }
@@ -31,17 +33,25 @@ class GutenbergLayoutPickerViewController: UIViewController {
 
         let tableFooterFrame = footerView.frame
         let bottomInset = tableFooterFrame.size.height - (UIApplication.shared.keyWindow?.safeAreaInsets.bottom ?? 44)
+
         tableView.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: maxHeaderHeight))
+        tableView.tableHeaderView?.backgroundColor = UIColor.orange
         tableView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: bottomInset))
 
         closeButton.setImage(UIImage.gridicon(.crossSmall), for: .normal)
         largeTitleView.font = titleViewFont(withSize: maxTitleFontSize)
         titleView.font = titleViewFont(withSize: minTitleFontSize)
+
+        midHeaderHeight = maxHeaderHeight - promptView.frame.minY
     }
 
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.isNavigationBarHidden = true
         super.viewWillAppear(animated)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     }
 
     override func viewDidDisappear(_ animated: Bool) {
@@ -127,6 +137,36 @@ extension GutenbergLayoutPickerViewController: UITableViewDelegate {
         }
 
         titleView.isHidden = largeTitleView.frame.maxY > headerBar.frame.height
+    }
+
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        let scrollComparisonPoint = headerBar.frame.maxY
+
+        if largeTitleView.frame.midY > scrollComparisonPoint {
+            snapToMaxHeight(scrollView)
+        } else if promptView.frame.midY > scrollComparisonPoint {
+            snapToMidHeight(scrollView)
+        } else if headerHeightConstraint.constant != minHeaderHeight {
+            snapToMinHeight(scrollView)
+        }
+    }
+
+    private func snapToMaxHeight(_ scrollView: UIScrollView) {
+        scrollView.contentOffset.y = 0
+        headerHeightConstraint.constant = maxHeaderHeight
+        titleView.isHidden = true
+    }
+
+    private func snapToMidHeight(_ scrollView: UIScrollView) {
+        scrollView.contentOffset.y = maxHeaderHeight - midHeaderHeight
+        headerHeightConstraint.constant = midHeaderHeight
+        titleView.isHidden = false
+    }
+
+    private func snapToMinHeight(_ scrollView: UIScrollView) {
+        scrollView.contentOffset.y = maxHeaderHeight - minHeaderHeight
+        headerHeightConstraint.constant = minHeaderHeight
+        titleView.isHidden = false
     }
 }
 
