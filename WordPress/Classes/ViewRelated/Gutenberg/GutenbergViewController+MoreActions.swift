1a93576c8e4da1c38ff8cd550262784993cb7f20
James Frost <james.frost@automattic.com>
2020-07-20 10:29:01 +0100

    Editor More popover iOS 14 fix: dismiss on rotation / size change

--- WordPress/Classes/ViewRelated/Gutenberg/GutenbergViewController+MoreActions.swift
+++ WordPress/Classes/ViewRelated/Gutenberg/GutenbergViewController+MoreActions.swift
@@ -65,6 +65,7 @@ extension GutenbergViewController {
             // https://github.com/wordpress-mobile/WordPress-iOS/issues/14460
             alert.popoverPresentationController?.sourceRect = button.convert(button.bounds, to: navigationController?.navigationBar)
             alert.popoverPresentationController?.sourceView = navigationController?.navigationBar
+            alert.view.accessibilityIdentifier = MoreSheetAlert.accessibilityIdentifier
         } else {
             alert.popoverPresentationController?.barButtonItem = navigationBarManager.moreBarButtonItem
         }
@@ -100,7 +101,7 @@ extension GutenbergViewController {
 // MARK: - Constants
 
 extension GutenbergViewController {
-    private struct MoreSheetAlert {
+    struct MoreSheetAlert {
         static let classicTitle = NSLocalizedString(
             "Switch to classic editor",
             comment: "Switches from Gutenberg mobile to the classic editor"
@@ -111,5 +112,6 @@ extension GutenbergViewController {
         static let historyTitle = NSLocalizedString("History", comment: "Displays the History screen from the editor's alert sheet")
         static let postSettingsTitle = NSLocalizedString("Post Settings", comment: "Name of the button to open the post settings")
         static let keepEditingTitle = NSLocalizedString("Keep Editing", comment: "Goes back to editing the post.")
+        static let accessibilityIdentifier = "MoreSheetAccessibilityIdentifier"
     }
 }
