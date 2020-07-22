cd14721adf1cb5e89a90110aee2ab8aa914cf8ac
Thuy.Copeland <thuycopeland@gmail.com>
2020-07-22 18:13:23 -0500

    Enable siteAddress feature flag

--- WordPress/Classes/Utility/BuildInformation/FeatureFlag.swift
+++ WordPress/Classes/Utility/BuildInformation/FeatureFlag.swift
@@ -31,7 +31,7 @@ enum FeatureFlag: Int, CaseIterable {
         case .unifiedAuth:
             return BuildConfiguration.current ~= [.localDeveloper, .a8cBranchTest]
         case .unifiedSiteAddress:
-            return false
+            return BuildConfiguration.current == .localDeveloper
         case .unifiedGoogle:
             return BuildConfiguration.current ~= [.localDeveloper, .a8cBranchTest]
         case .meMove:
