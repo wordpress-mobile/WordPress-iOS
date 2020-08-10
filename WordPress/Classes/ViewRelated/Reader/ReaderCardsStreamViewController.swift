e68e7daeec3abdedbb3d25954071efb4f03961df
Leandro Alonso <contato@leandroalonso.com>
2020-08-10 11:40:52 -0300

    Remove interests presentation from Discover section

--- WordPress/Classes/ViewRelated/Reader/ReaderCardsStreamViewController.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderCardsStreamViewController.swift
@@ -11,18 +11,10 @@ class ReaderCardsStreamViewController: ReaderStreamViewController {
         return ReaderCardService()
     }()
 
-    private var selectInterestsViewController: ReaderSelectInterestsViewController? = ReaderSelectInterestsViewController()
-
     override func viewDidLoad() {
         super.viewDidLoad()
         ReaderWelcomeBanner.displayIfNeeded(in: tableView)
         tableView.register(ReaderTopicsCardCell.self, forCellReuseIdentifier: readerCardTopicsIdentifier)
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-
-        displaySelectInterestsIfNeeded()
     }
 
     // MARK: - TableView Related
@@ -127,43 +119,5 @@ extension ReaderCardsStreamViewController: ReaderTopicsCardCellDelegate {
     func didSelect(topic: ReaderTagTopic) {
         let topicStreamViewController = ReaderStreamViewController.controllerWithTopic(topic)
         navigationController?.pushViewController(topicStreamViewController, animated: true)
-    }
-}
-
-// MARK: - Select Interests Display
-private extension ReaderCardsStreamViewController {
-    func displaySelectInterestsIfNeeded() {
-        selectInterestsViewController?.userIsFollowingTopics { [unowned self] isFollowing in
-            if isFollowing {
-                self.selectInterestsViewController = nil
-            } else {
-                self.showSelectInterestsView()
-            }
-        }
-    }
-
-    func showSelectInterestsView() {
-        guard let controller = selectInterestsViewController else {
-            return
-        }
-
-        controller.view.frame = self.view.bounds
-        self.add(controller)
-
-        controller.didSaveInterests = { [unowned self] in
-            guard let controller = self.selectInterestsViewController else {
-                return
-            }
-
-            self.displayLoadingStream()
-            self.syncIfAppropriate()
-
-            UIView.animate(withDuration: 0.2, animations: {
-                controller.view.alpha = 0
-            }) { [unowned self] _ in
-                controller.remove()
-                self.selectInterestsViewController = nil
-            }
-        }
     }
 }
