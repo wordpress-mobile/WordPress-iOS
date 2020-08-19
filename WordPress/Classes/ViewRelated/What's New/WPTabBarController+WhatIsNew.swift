efa8f34123bb524c8549646113c874bea429d881
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2020-08-19 12:08:59 -0500

    changes for PR 14672

--- WordPress/Classes/ViewRelated/What's New/WPTabBarController+WhatIsNew.swift
+++ WordPress/Classes/ViewRelated/What's New/WPTabBarController+WhatIsNew.swift
@@ -7,19 +7,21 @@ extension WPTabBarController {
 
     private func makeWhatIsNewView() -> WhatIsNewView {
 
-        var version = ""
-        if let versionNumber = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
-            version = NSLocalizedString("Version ", comment: "Description for the version label in the What's new page.") + versionNumber
-        }
+        let version = Bundle.main.shortVersionString() != nil ?
+            WhatIsNewStrings.versionPrefix + Bundle.main.shortVersionString() : ""
 
-        let textContent = WhatIsNewTextContent(title: NSLocalizedString("What's New in WordPress",
-                                                                        comment: "Title of the What's new page."),
+        let textContent = WhatIsNewTextContent(title: WhatIsNewStrings.title,
                                                version: version,
-                                               moreContentButtonTitle: NSLocalizedString("Find out more",
-                                                                                         comment: "Title for the Find out more button in the What's new page."),
-                                               continueButtonTitle: NSLocalizedString("Continue",
-                                                                                      comment: "Title for the continue button in the What's New page."))
+                                               moreContentButtonTitle: WhatIsNewStrings.moreContentButtonTitle,
+                                               continueButtonTitle: WhatIsNewStrings.continueButtonTitle)
 
         return WhatIsNewView(textContent: textContent)
+    }
+
+    private enum WhatIsNewStrings {
+        static let title = NSLocalizedString("What's New in WordPress", comment: "Title of the What's new page.")
+        static let versionPrefix = NSLocalizedString("Version ", comment: "Description for the version label in the What's new page.")
+        static let moreContentButtonTitle = NSLocalizedString("Find out more", comment: "Title for the Find out more button in the What's new page.")
+        static let continueButtonTitle = NSLocalizedString("Continue", comment: "Title for the continue button in the What's New page.")
     }
 }
