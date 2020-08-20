68e16e8ac274a5220f6d1f74904bb765ead36d95
Leandro Alonso <contato@leandroalonso.com>
2020-08-20 13:50:37 -0300

    Moce constants to an enum

--- WordPress/Classes/ViewRelated/Reader/Analytics/ReaderTracker.swift
+++ WordPress/Classes/ViewRelated/Reader/Analytics/ReaderTracker.swift
@@ -1,20 +1,22 @@
 import Foundation
 
 class ReaderTracker {
-    /// Time spent in the main Reader view (the one with the tabs)
-    let timeInReader = "time_in_main_reader"
+    enum Section: String {
+        /// Time spent in the main Reader view (the one with the tabs)
+        case main = "time_in_main_reader"
 
-    /// Time spent in the Following tab with an active filter
-    let timeInReaderFilteredList = "time_in_reader_filtered_list"
+        /// Time spent in the Following tab with an active filter
+        case filteredList = "time_in_reader_filtered_list"
 
-    /// Time spent reading article
-    let timeInReaderPost = "time_in_reader_post"
+        /// Time spent reading article
+        case readerPost = "time_in_reader_post"
+    }
 
     func data() -> [String: Double] {
         return [
-            timeInReader: 0,
-            timeInReaderFilteredList: 0,
-            timeInReaderPost: 0
+            Section.main.rawValue: 0,
+            Section.filteredList.rawValue: 0,
+            Section.readerPost.rawValue: 0
         ]
     }
 }
