e4fccb6038e20ed6eb890b38abfa8bfa13aaa30e
Chip Snyder <chip.snyder3@gmail.com>
2020-08-25 15:36:33 -0400

    Fix display issue on iOS 13 devices

--- WordPress/Classes/Services/PageCoordinator.swift
+++ WordPress/Classes/Services/PageCoordinator.swift
@@ -20,8 +20,13 @@ class PageCoordinator {
         }
         rootView.completion = completion
 
-        // Specifically using fullScreen instead of pageSheet to get the desired behavior on Max devices running iOS 12 and below.
-        navigationController.modalPresentationStyle = UIDevice.current.userInterfaceIdiom == .pad ? .pageSheet : .fullScreen
+        if #available(iOS 13.0, *) {
+            navigationController.modalPresentationStyle = .pageSheet
+        } else {
+            // Specifically using fullScreen instead of pageSheet to get the desired behavior on Max devices running iOS 12 and below.
+             navigationController.modalPresentationStyle = UIDevice.current.userInterfaceIdiom == .pad ? .pageSheet : .fullScreen
+        }
+
         controller.present(navigationController, animated: true, completion: nil)
     }
 }
