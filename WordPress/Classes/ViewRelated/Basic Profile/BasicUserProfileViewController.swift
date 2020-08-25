878e1916dd2f15027cb672d4ecb7d1b0d7698089
Ugur Kilic <ughuro@gmail.com>
2020-08-25 21:29:29 +0300

    Fix the ghost animation

--- WordPress/Classes/ViewRelated/Basic Profile/BasicUserProfileViewController.swift
+++ WordPress/Classes/ViewRelated/Basic Profile/BasicUserProfileViewController.swift
@@ -3,9 +3,13 @@ import Foundation
 final class BasicUserProfileViewController: UIViewController, DrawerPresentable, StoryboardLoadable {
     static var defaultStoryboardName: String = "BasicUserProfile"
 
+    let allowsUserTransition = false
+
     var expandedHeight: DrawerHeight = DrawerHeight.intrinsicHeight
 
-    let allowsUserTransition = false
+    var viewModel: BasicUserProfileViewModel?
+
+    private var isInitialDisplay = true
 
     @IBOutlet private weak var gravatarImageView: CircularImageView!
     @IBOutlet private weak var nameLabel: UILabel!
@@ -13,10 +17,75 @@ final class BasicUserProfileViewController: UIViewController, DrawerPresentable,
     @IBOutlet private weak var locationLabel: UILabel!
     @IBOutlet private weak var aboutMeLabel: UILabel!
     @IBOutlet private weak var contentView: UIView!
-    @IBOutlet private weak var sitesTableView: UITableView!
 
-    var viewModel: BasicUserProfileViewModel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
+        // TODO - BASICPROFILES: Tracking
+
+        applyStyling()
+        downloadGravatarImageView()
+
+        viewModel?.fetchUserDetails { [weak self] profile in
+            guard let self = self else {
+                return
+            }
+
+            self.nameLabel.text = profile?.formattedName
+            self.usernameLabel.text = "@\(profile?.preferredUsername ?? "")"
+            if let location = profile?.currentLocation {
+                self.locationLabel.text = location
+            } else {
+                self.locationLabel.isHidden = true
+            }
+            if let aboutMe = profile?.aboutMe {
+                self.aboutMeLabel.text = aboutMe
+            } else {
+                self.aboutMeLabel.isHidden = true
+            }
+
+            self.contentView.stopGhostAnimation()
+
+            // TODO - BASICPROFILES: Update sites
+        }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if isInitialDisplay {
+            contentView.startGhostAnimation()
+            isInitialDisplay = false
+        }
+    }
+
+    private func applyStyling() {
+        nameLabel.font = WPStyleGuide.fontForTextStyle(.headline, fontWeight: .semibold)
+        usernameLabel.font = WPStyleGuide.fontForTextStyle(.callout)
+        locationLabel.font = WPStyleGuide.fontForTextStyle(.caption1)
+        aboutMeLabel.font = WPStyleGuide.fontForTextStyle(.caption1)
+
+        nameLabel.textColor = .text
+        usernameLabel.textColor = .textSubtle
+        locationLabel.textColor = .textSubtle
+        aboutMeLabel.textColor = .text
+    }
+
+    private func downloadGravatarImageView() {
+        guard let viewModel = viewModel else {
+            return
+        }
+        if let email = viewModel.email {
+            gravatarImageView.downloadGravatarWithEmail(email)
+        } else if let url = viewModel.avatarURL {
+            let placeholder = UIImage(named: "gravatar")
+            gravatarImageView.downloadImage(from: url, placeholderImage: placeholder)
+        }
+    }
+}
+
+// MARK: - Presentation
+
+extension BasicUserProfileViewController {
     @objc class func present(context: UIViewController, view: UIView, email: String?, avatarURL: URL?) {
         guard let viewModel = BasicUserProfileViewModel(email: email, avatarURL: avatarURL) else {
             return
@@ -27,60 +96,5 @@ final class BasicUserProfileViewController: UIViewController, DrawerPresentable,
 
         let bottomSheet = BottomSheetViewController(childViewController: controller)
         bottomSheet.show(from: context, sourceView: view, arrowDirections: .up)
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        contentView.startGhostAnimation()
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // TODO - BASICPROFILES: Tracking
-
-        nameLabel.font = WPStyleGuide.fontForTextStyle(.title3, fontWeight: .semibold)
-        usernameLabel.font = WPStyleGuide.fontForTextStyle(.callout)
-        locationLabel.font = WPStyleGuide.fontForTextStyle(.caption1)
-        aboutMeLabel.font = WPStyleGuide.fontForTextStyle(.caption1)
-
-        nameLabel.textColor = .text
-        usernameLabel.textColor = .textSubtle
-        locationLabel.textColor = .textSubtle
-        aboutMeLabel.textColor = .text
-
-        downloadGravatarImageView()
-
-        viewModel.fetchUserDetails { [weak self] profile in
-            guard let strongSelf = self else {
-                return
-            }
-
-            strongSelf.nameLabel.text = profile?.formattedName
-            strongSelf.usernameLabel.text = "@\(profile?.preferredUsername ?? "")"
-            if let location = profile?.currentLocation {
-                strongSelf.locationLabel.text = location
-            } else {
-                strongSelf.locationLabel.isHidden = true
-            }
-            if let aboutMe = profile?.aboutMe {
-                strongSelf.aboutMeLabel.text = aboutMe
-            } else {
-                strongSelf.aboutMeLabel.isHidden = true
-            }
-
-            strongSelf.contentView.stopGhostAnimation()
-
-            // TODO - BASICPROFILES: Update sites
-        }
-    }
-
-    private func downloadGravatarImageView() {
-        if let email = viewModel.email {
-            gravatarImageView.downloadGravatarWithEmail(email)
-        } else if let url = viewModel.avatarURL {
-            let placeholder = UIImage(named: "gravatar")
-            gravatarImageView.downloadImage(from: url, placeholderImage: placeholder)
-        }
     }
 }
