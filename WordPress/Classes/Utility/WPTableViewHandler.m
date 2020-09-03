75142ddb252dc26dbdad3b00fa2d8e77e607bdc6
Leandro Alonso <contato@leandroalonso.com>
2020-09-03 17:19:42 -0300

    Break the loop if the visible cell doesn't exist
    Co-authored-by: Emily Laguna <emily.laguna@automattic.com>

--- WordPress/Classes/Utility/WPTableViewHandler.m
+++ WordPress/Classes/Utility/WPTableViewHandler.m
@@ -151,7 +151,10 @@ static CGFloat const DefaultCellHeight = 44.0;
         }
         newIndexPath = [self.resultsController indexPathForObject:obj];
 
-        if (visibleCellFrames.count >= i) {
+            if (i > visibleCellFrames.count) {
+                break;
+            }
+            
             CGRect originalCellFrame = [[visibleCellFrames objectAtIndex:i] CGRectValue];
             if (newIndexPath) {
                 // Since we still have one of the originally visible objects,
@@ -161,8 +164,6 @@ static CGFloat const DefaultCellHeight = 44.0;
             } else {
                 originalCellOriginOffsetDelta -= originalCellFrame.size.height;
             }
-        }
-
         i++;
     }
 
