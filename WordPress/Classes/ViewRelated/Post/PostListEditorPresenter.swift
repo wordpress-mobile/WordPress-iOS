9f061192fceea096bac9b6f1b1b1285d9804f05f
Declan McKenna <deco354@gmail.com>
2020-09-18 16:28:41 +0100

    Update text for existing Conflict Dialog
    The text now decides between Remote and Local as we now handle conflicts
    for remote and local autosaves.

--- WordPress/Classes/ViewRelated/Post/PostListEditorPresenter.swift
+++ WordPress/Classes/ViewRelated/Post/PostListEditorPresenter.swift
@@ -47,14 +47,26 @@ struct PostListEditorPresenter {
     /// A dialog giving the user the choice between loading the current version a post or its autosaved version.
     private static func autosaveOptionsViewController(forSaveDate saveDate: Date, autosaveDate: Date, didTapOption: @escaping (_ loadAutosaveRevision: Bool) -> Void) -> UIAlertController {
 
-        let title = NSLocalizedString("Which version would you like to edit?", comment: "Title displayed in popup when user has the option to load unsaved changes")
+        let title = NSLocalizedString("Resolve sync conflict", comment: "Title displayed in popup when user has to resolve a verion conflict due to unsaved/autosaved changes")
 
         let saveDateFormatted = dateAndTime(for: saveDate)
         let autosaveDateFormatted = dateAndTime(for: autosaveDate)
-        let message = String(format: NSLocalizedString("You recently made changes to this post but didn't save them. Choose a version to load:\n\nFrom this device\nSaved on %@\n\nFrom another device\nSaved on %@\n", comment: "Message displayed in popup when user has the option to load unsaved changes. \n is a placeholder for a new line, and the two %@ are placeholders for the date of last save on this device, and date of last autosave on another device, respectively."), saveDateFormatted, autosaveDateFormatted)
 
-        let loadSaveButtonTitle = NSLocalizedString("From this device", comment: "Button title displayed in popup indicating date of change on device")
-        let fromAutosaveButtonTitle = NSLocalizedString("From another device", comment: "Button title displayed in popup indicating date of change on another device")
+        let alertMessageText = """
+    This post has two versions that are in conflict. Select the version you would like to keep:
+
+    Local
+    Saved on \(saveDateFormatted)
+
+    Remote
+    Saved on \(autosaveDateFormatted)
+
+    """
+
+        let message = NSLocalizedString(alertMessageText, comment: "Message displayed in popup when user has the option to load unsaved changes")
+
+        let loadSaveButtonTitle = NSLocalizedString("Keep Local", comment: "Button title displayed in popup indicating date of change on device")
+        let fromAutosaveButtonTitle = NSLocalizedString("Keep Remote", comment: "Button title displayed in popup indicating date of change on another device")
 
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: loadSaveButtonTitle, style: .default) { _ in
