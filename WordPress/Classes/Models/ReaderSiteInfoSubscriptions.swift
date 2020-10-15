575c79a8fac22e1d228b8565e8b958b4c3fdcab4
Emily Laguna <emily.laguna@automattic.com>
2020-10-15 14:34:34 -0400

    Move ReaderSiteInfoSubscriptionPost, ReaderSiteInfoSubscriptionEmail creation logic to the models

--- WordPress/Classes/Models/ReaderSiteInfoSubscriptions.swift
+++ WordPress/Classes/Models/ReaderSiteInfoSubscriptions.swift
@@ -5,6 +5,22 @@ import CoreData
 @objc public class ReaderSiteInfoSubscriptionPost: NSManagedObject {
     @NSManaged open var siteTopic: ReaderSiteTopic
     @NSManaged open var sendPosts: Bool
+
+    class func createOrUpdate(from remoteSiteInfo: RemoteReaderSiteInfo, topic: ReaderSiteTopic, context: NSManagedObjectContext) -> ReaderSiteInfoSubscriptionPost? {
+        guard remoteSiteInfo.postSubscription.wp_isValidObject() else {
+            return nil
+        }
+
+        var subscription = topic.postSubscription
+        if !(subscription?.wp_isValidObject() ?? false)  {
+            subscription = ReaderSiteInfoSubscriptionPost(context: context)
+        }
+
+        subscription?.siteTopic = topic
+        subscription?.sendPosts = remoteSiteInfo.postSubscription.sendPosts
+
+        return subscription
+    }
 }
 
 
@@ -13,4 +29,22 @@ import CoreData
     @NSManaged open var sendPosts: Bool
     @NSManaged open var sendComments: Bool
     @NSManaged open var postDeliveryFrequency: String
+
+    class func createOrUpdate(from remoteSiteInfo: RemoteReaderSiteInfo, topic: ReaderSiteTopic, context: NSManagedObjectContext) -> ReaderSiteInfoSubscriptionEmail? {
+        guard (remoteSiteInfo.emailSubscription?.wp_isValidObject() ?? false) else {
+            return nil
+        }
+
+        var subscription = topic.emailSubscription
+        if !(subscription?.wp_isValidObject() ?? false)  {
+            subscription = ReaderSiteInfoSubscriptionEmail(context: context)
+        }
+
+        subscription?.siteTopic = topic
+        subscription?.sendPosts = remoteSiteInfo.emailSubscription.sendPosts
+        subscription?.sendComments = remoteSiteInfo.emailSubscription.sendComments
+        subscription?.postDeliveryFrequency = remoteSiteInfo.emailSubscription.postDeliveryFrequency
+
+        return subscription
+    }
 }
