bd62fef25f434fe510ce1eb4e2aa1a0199d458fc
Brandon Titus <b@titus.io>
2020-10-15 12:47:45 -0600

    Add swift version of createButtonCoordinator

--- WordPress/Classes/ViewRelated/Blog/Blog Details/BlogDetailsViewController.m
+++ WordPress/Classes/ViewRelated/Blog/Blog Details/BlogDetailsViewController.m
@@ -435,24 +435,7 @@ NSString * const WPCalypsoDashboardPath = @"https://wordpress.com/stats/";
 - (CreateButtonCoordinator *)createButtonCoordinator
 {
     if (!_createButtonCoordinator) {
-        __weak __typeof(self) weakSelf = self;
-
-        BOOL showNewStory = [Feature enabled:FeatureFlagStories] && [self.blog supports:BlogFeatureStories];
-        void (^newStory)(void) = ^void() {
-            WPTabBarController *controller = (WPTabBarController *)weakSelf.tabBarController;
-            Blog *blog = [controller currentOrLastBlog];
-            [controller showStoryEditorForBlog:blog];
-        };
-
-        _createButtonCoordinator = [[CreateButtonCoordinator alloc] init:self newPost:^{
-            [((WPTabBarController *)weakSelf.tabBarController) showPostTabWithCompletion:^(void) {
-                [weakSelf startAlertTimer];
-            }];
-        } newPage:^{
-            WPTabBarController *controller = (WPTabBarController *)weakSelf.tabBarController;
-            Blog *blog = [controller currentOrLastBlog];
-            [controller showPageEditorForBlog:blog];
-        } newStory: (showNewStory) ? newStory : nil];
+        _createButtonCoordinator = [self makeCreateButtonCoordinator];
     }
     
     return _createButtonCoordinator;
