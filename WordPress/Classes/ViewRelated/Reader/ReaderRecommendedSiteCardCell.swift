abfe78644c3c548936e7ba13fb36b8274d740aaf
Emily Laguna <emily.laguna@automattic.com>
2020-10-16 14:31:07 -0400

    Adds iPad follow button

--- WordPress/Classes/ViewRelated/Reader/ReaderRecommendedSiteCardCell.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderRecommendedSiteCardCell.swift
@@ -5,7 +5,9 @@ class ReaderRecommendedSiteCardCell: UITableViewCell {
     @IBOutlet weak var blogNameLabel: UILabel!
     @IBOutlet weak var hostNameLabel: UILabel!
     @IBOutlet weak var followButton: UIButton!
+    @IBOutlet weak var iPadFollowButton: UIButton!
     @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var infoTrailingConstraint: NSLayoutConstraint!
 
     var delegate: ReaderRecommendedSitesCardCellDelegate?
 
@@ -19,7 +21,7 @@ class ReaderRecommendedSiteCardCell: UITableViewCell {
         separatorInset = UIEdgeInsets.zero
 
         followButton.isSelected = topic.following
-        followButton.isHidden = !ReaderHelpers.isLoggedIn()
+        iPadFollowButton.isSelected = topic.following
 
         blogNameLabel.text = topic.title
         hostNameLabel.text = URL(string: topic.siteURL)?.host
@@ -27,13 +29,37 @@ class ReaderRecommendedSiteCardCell: UITableViewCell {
         descriptionLabel.isHidden = topic.siteDescription.isEmpty
 
         configureSiteIcon(topic)
+        configureFollowButtonVisibility()
     }
 
     @IBAction func didTapFollowButton(_ sender: Any) {
         // Optimistically change the value
         followButton.isSelected = !followButton.isSelected
+        iPadFollowButton.isSelected = !iPadFollowButton.isSelected
+
+        configureFollowButtonVisibility()
 
         delegate?.handleFollowActionForCell(self)
+    }
+
+    private func configureFollowButtonVisibility() {
+        let isLoggedIn = ReaderHelpers.isLoggedIn()
+
+        guard isLoggedIn else {
+            followButton.isHidden = true
+            iPadFollowButton.isHidden = true
+            return
+        }
+
+        let isCompact = traitCollection.horizontalSizeClass == .compact
+
+        followButton.isHidden = !isCompact
+        iPadFollowButton.isHidden = isCompact
+
+        // Update the info trailing constraint to prevent clipping
+        let button = isCompact ? followButton : iPadFollowButton
+        let width = button?.frame.size.width ?? 0
+        infoTrailingConstraint.constant = width + Constants.buttonMargin
     }
 
     private func configureSiteIcon(_ topic: ReaderSiteTopic) {
@@ -48,6 +74,10 @@ class ReaderRecommendedSiteCardCell: UITableViewCell {
         }
 
         iconImageView.downloadImage(from: url, placeholderImage: placeholder)
+    }
+
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        configureFollowButtonVisibility()
     }
 
     private func applyStyles() {
@@ -65,7 +95,12 @@ class ReaderRecommendedSiteCardCell: UITableViewCell {
         descriptionLabel.font = WPStyleGuide.fontForTextStyle(.subheadline)
         descriptionLabel.textColor = .text
 
+        WPStyleGuide.applyReaderFollowButtonStyle(iPadFollowButton)
         WPStyleGuide.applyReaderIconFollowButtonStyle(followButton)
+    }
+
+    private struct Constants {
+        static let buttonMargin: CGFloat = 8
     }
 }
 
