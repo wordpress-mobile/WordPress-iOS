cdf156997bb03f60184298c2e4fe6b2b57566a03
Chip Snyder <chip.snyder3@gmail.com>
2020-10-28 13:43:23 -0400

    Include Ghost cells and API call for site creation

--- WordPress/Classes/ViewRelated/Site Creation/DesignSelection/SiteDesignContentCollectionViewController.swift
+++ WordPress/Classes/ViewRelated/Site Creation/DesignSelection/SiteDesignContentCollectionViewController.swift
@@ -1,6 +1,5 @@
 import UIKit
-
-private let reuseIdentifier = "Cell"
+import WordPressKit
 
 class SiteDesignContentCollectionViewController: UICollectionViewController, CollapsableHeaderDataSource {
     let mainTitle = NSLocalizedString("Choose a design", comment: "Title for the screen to pick a design and homepage for a site.")
@@ -13,6 +12,15 @@ class SiteDesignContentCollectionViewController: UICollectionViewController, Col
         return collectionView
     }
 
+    var siteDesigns: [RemoteSiteDesign] = [] {
+        didSet {
+            collectionView.reloadData()
+        }
+    }
+    var isLoading = true
+    let cellSize = CGSize(width: 150, height: 230)
+    let restAPI = WordPressComRestApi.anonymousApi(userAgent: WPUserAgent.wordPress())
+
     init() {
         super.init(nibName: "\(SiteDesignContentCollectionViewController.self)", bundle: .main)
     }
@@ -23,38 +31,69 @@ class SiteDesignContentCollectionViewController: UICollectionViewController, Col
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.register(CollapsableHeaderCollectionViewCell.nib, forCellWithReuseIdentifier: CollapsableHeaderCollectionViewCell.cellReuseIdentifier)
     }
 
     func estimatedContentSize() -> CGSize {
-        return .zero
+        guard isLoading || siteDesigns.count > 1 else { return .zero }
+        let cellCount = isLoading ? 1 : siteDesigns.count
+        let cellsPerRow = floor(collectionView.frame.size.width / cellSize.width)
+        let rows = ceil(CGFloat(cellCount) / cellsPerRow)
+        let flowLayout = (collectionView.collectionViewLayout as? UICollectionViewFlowLayout)
+        let topSpacing = (flowLayout?.sectionInset.top ?? 1)
+        let lineSpacing = (flowLayout?.minimumLineSpacing ?? 01) * max(rows - 2, 0)
+        let estimatedHeight = rows * cellSize.height + topSpacing + lineSpacing
+        return CGSize(width: collectionView.frame.width, height: estimatedHeight)
+    }
+
+    private func fetchSiteDesigns() {
+        SiteDesignServiceRemote.fetchSiteDesigns(restAPI) { [weak self] (response) in
+            DispatchQueue.main.async {
+                switch response {
+                case .success(let result):
+                    self?.siteDesigns = result
+                case .failure(let error):
+                    self?.handleError(error)
+                }
+            }
+        }
+    }
+
+    private func handleError(_ error: Error) {
+        /* ToDo */
     }
 
     // MARK: UICollectionViewDataSource
-
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return isLoading ? 1 : siteDesigns.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let cellReuseIdentifier = CollapsableHeaderCollectionViewCell.cellReuseIdentifier
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellReuseIdentifier, for: indexPath) as? CollapsableHeaderCollectionViewCell else {
+            fatalError("Expected the cell with identifier \"\(cellReuseIdentifier)\" to be a \(CollapsableHeaderCollectionViewCell.self). Please make sure the collection view is registering the correct nib before loading the data")
+        }
 
-        // This is placeholder content to help test the content area.
-        cell.layer.borderWidth = 1
-        cell.layer.borderColor = UIColor.darkGray.cgColor
-        cell.backgroundColor = .lightGray
-        cell.layer.cornerRadius = 3
+        guard !isLoading else {
+            cell.startGhostAnimation(style: GhostCellStyle.muriel)
+            return cell
+        }
+
+//        let layout = layouts[indexPath.row]
+//        cell.previewURL = layout.preview
+//        cell.isAccessibilityElement = true
+//        cell.accessibilityLabel = layout.slug
 
         return cell
     }
 
     // MARK: UICollectionViewFlowLayout
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 150, height: 230)
+        return cellSize
     }
 }
 
@@ -63,4 +102,8 @@ extension SiteDesignContentCollectionViewController: CollapsableHeaderDelegate {
     func primaryActionSelected() {
         /* TODO - connect to choose */
     }
+}
+
+extension SiteDesignContentCollectionViewController: UICollectionViewDelegate {
+
 }
