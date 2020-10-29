b39e59a1a6d2666a7cfa3a86898462a97ed1e0fc
Diego Rey Mendez <diego@reymendez.com>
2020-10-29 10:51:27 -0300

    Adds a workaround for a crashing bug with Bulgarian keyboards.

---
+++ WordPress/Classes/Extensions/UITextField+WorkaroundContinueIssue.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+@objc
+extension UITextField {
+
+    /// This method takes care of resolving whether the iOS version is vulnerable to the Bulgarian / Icelandic keyboard crash issue
+    /// by Apple.  Once the issue is resolved by Apple we should consider setting an upper iOS version to limit this workaround.
+    ///
+    /// Once we drop support for iOS 14, we could remove this extension entirely.
+    ///
+    public class func shouldActivateWorkaroundForBulgarianKeyboardCrash() -> Bool {
+        if #available(iOS 14.0, *) {
+            return true
+        }
+
+        return false
+    }
+
+    /// We're swizzling `UITextField.becomeFirstResponder()` so that we can fix an issue with
+    /// Bulgarian and Icelandic keyboards when appropriate.
+    ///
+    /// Ref: https://github.com/wordpress-mobile/WordPress-iOS/issues/15187
+    ///
+    @objc
+    class func activateWorkaroundForBulgarianKeyboardCrash() {
+        let original = class_getInstanceMethod(
+            UITextField.self,
+            #selector(UITextField.becomeFirstResponder))!
+        let new = class_getInstanceMethod(
+            UITextField.self,
+            #selector(UITextField.swizzledBecomeFirstResponder))!
+
+        method_exchangeImplementations(original, new)
+    }
+
+    /// This method simply replaces the `returnKeyType == .continue` with
+    /// `returnKeyType == .next`when the Bulgarian Keyboard crash workaround is needed.
+    ///
+    public func swizzledBecomeFirstResponder() {
+        if UITextField.shouldActivateWorkaroundForBulgarianKeyboardCrash(),
+           returnKeyType == .continue {
+            returnKeyType = .next
+        }
+
+        // This can look confusing - it's basically calling the original method to
+        // make sure we don't disrupt anything.
+        swizzledBecomeFirstResponder()
+    }
+}
