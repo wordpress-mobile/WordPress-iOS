bdc805a05fad4d3e322304435f713659a79c141d
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2020-10-29 18:27:00 -0500

    remove unwanted file

--- WordPress/Classes/Services/TodayExtensionService+iOS14.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TodayExtensionService+iOS14.swift
-//  WordPress
-//
-//  Created by Giorgio Ruscigno on 10/28/20.
-//  Copyright © 2020 WordPress. All rights reserved.
-//
-
-import Foundation
