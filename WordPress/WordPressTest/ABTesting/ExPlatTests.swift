4d31e6c15d5c92932c1a506b235684da768c519f
Leandro Alonso <contato@leandroalonso.com>
2020-11-04 11:26:27 -0300

    Fix tests

--- WordPress/WordPressTest/ABTesting/ExPlatTests.swift
+++ WordPress/WordPressTest/ABTesting/ExPlatTests.swift
@@ -4,19 +4,32 @@ import Nimble
 @testable import WordPress
 
 class ExPlatTests: XCTestCase {
-    // Save the returned assignments
+    // Save the returned experiments variation
     //
     func testRefresh() {
-        let abTesting = ExPlat(service: ExPlatServiceMock.withDefaultApi())
+        let abTesting = ExPlat(service: ExPlatServiceMock())
 
         abTesting.refresh()
 
-        expect(abTesting.experiment("experiment")).to(equal("control"))
+        expect(abTesting.experiment("experiment")).toEventually(equal("control"))
+        expect(abTesting.experiment("another_experiment")).toEventually(equal("treatment"))
     }
 }
 
 private class ExPlatServiceMock: ExPlatService {
+    init() {
+        super.init(wordPressComRestApi: WordPressComMockRestApi())
+    }
+
     override func getAssignments(completion: @escaping (Assignments?) -> Void) {
-        completion(Assignments.init(ttl: 60, variations: ["experiment": "control"]))
+        completion(
+            Assignments(
+                ttl: 60,
+                variations: [
+                    "experiment": "control",
+                    "another_experiment": "treatment"
+                ]
+            )
+        )
     }
 }
