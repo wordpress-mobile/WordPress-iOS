3465d73bd6d70a3cac29a3dbc8953d4f74f9eb96
Diego Rey Mendez <diego@reymendez.com>
2020-11-10 17:43:38 -0300

    Removes some commented out code.

--- WordPress/Classes/Utility/BuildInformation/FeatureFlag.swift
+++ WordPress/Classes/Utility/BuildInformation/FeatureFlag.swift
@@ -45,7 +45,7 @@ enum FeatureFlag: Int, CaseIterable, OverrideableFlag {
         case .whatIsNew:
             return true
         case .newNavBarAppearance:
-            return false //BuildConfiguration.current == .localDeveloper
+            return false
         case .unifiedPrologueCarousel:
             return false
         case .stories:
