1616309af19d2c68f5cad39afc3f3ab5a104d223
aerych <aerychj@gmail.com>
2020-11-16 13:29:32 -0600

    Previews: Don't add preview param if not needead.

--- WordPress/Classes/ViewRelated/Post/Preview/PreviewNonceHandler.swift
+++ WordPress/Classes/ViewRelated/Post/Preview/PreviewNonceHandler.swift
@@ -1,17 +1,22 @@
 struct PreviewNonceHandler {
     static func nonceURL(post: AbstractPost, previewURL: URL?) -> URL? {
         let permalink = post.permaLink.flatMap(URL.init(string:))
-        guard
-            let url = previewURL ?? permalink,
-            let unmappedURL = unmapURL(post: post, url: url)
-        else {
+        guard var url = previewURL ?? permalink else {
             return nil
         }
 
+        if postNeedsPreviewParam(post: post) {
+            url = addPreviewIfNecessary(url: url) ?? url
+        }
+
+        if shouldUseUnmappedDomain(url: url, for: post) {
+            url = unmapURL(post: post, url: url) ?? url
+        }
+
         if post.blog.supports(.noncePreviews), let nonce = post.blog.getOptionValue("frame_nonce") as? String {
-            return addNonce(nonce, to: unmappedURL)
+            return addNonce(nonce, to: url)
         } else {
-            return unmappedURL
+            return url
         }
     }
 
@@ -21,7 +26,7 @@ struct PreviewNonceHandler {
         }
         var queryItems = components.queryItems ?? []
         queryItems.append(URLQueryItem(name: "frame-nonce", value: nonce))
-        components.queryItems = addPreviewIfNecessary(items: queryItems)
+        components.queryItems = queryItems
         return components.url
     }
 
@@ -33,11 +38,53 @@ struct PreviewNonceHandler {
         else {
             return url
         }
-        let queryItems = components.queryItems ?? []
-        components.queryItems = addPreviewIfNecessary(items: queryItems)
+
         components.scheme = unmappedSiteURL.scheme
         components.host = unmappedSiteURL.host
 
+        return components.url
+    }
+
+    private static func postNeedsPreviewParam(post: AbstractPost) -> Bool {
+        // If the post is not published, add the preview param.
+        if post.status!.rawValue != PostStatusPublish {
+            return true
+        }
+
+        // If the post is published, but has changes, add the preview param.
+        if post.hasUnsavedChanges() || post.hasRemoteChanges() {
+            return true
+        }
+
+        return false
+    }
+
+    private static func shouldUseUnmappedDomain(url: URL, for post: AbstractPost) -> Bool {
+        // Always used the unmapped domain when previewing.
+        if postNeedsPreviewParam(post: post) {
+            return true
+        }
+
+        // Now it gets a little tricky. We'd like to have users authenticated,
+        // but we also want use a custom domain that smoeone has bought.
+        // RequestAuthenticator currently can't authenticate a custom domain.
+        // So until it can, unmap the domain when authentication is required,
+        // e.g. for private sites, or coming soon..
+        if post.blog.isPrivate() {
+            return true
+        }
+
+        return false
+    }
+
+    private static func addPreviewIfNecessary(url: URL) -> URL? {
+        guard
+            var components = URLComponents(url: url, resolvingAgainstBaseURL: false)
+        else {
+            return url
+        }
+        let queryItems = components.queryItems ?? []
+        components.queryItems = addPreviewIfNecessary(items: queryItems)
         return components.url
     }
 
