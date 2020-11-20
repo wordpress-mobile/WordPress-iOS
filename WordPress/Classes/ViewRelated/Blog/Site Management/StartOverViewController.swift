d0378b9ab67a2ac054673b188645bdc16c776ad1
Thuy.Copeland <thuycopeland@gmail.com>
2020-11-20 10:57:49 -0600

    Remove contraction
    Per App's writing guidelines, begin removing contractions wherever they are found in user-facing strings

--- WordPress/Classes/ViewRelated/Blog/Site Management/StartOverViewController.swift
+++ WordPress/Classes/ViewRelated/Blog/Site Management/StartOverViewController.swift
@@ -15,7 +15,7 @@ open class StartOverViewController: UITableViewController, MFMailComposeViewCont
 
     @objc let headerView: TableViewHeaderDetailView = {
         let header = NSLocalizedString("Let Us Help", comment: "Heading for instructions on Start Over settings page")
-        let detail1 = NSLocalizedString("If you want a site but don't want any of the posts and pages you have now, " +
+        let detail1 = NSLocalizedString("If you want a site but do not want any of the posts and pages you have now, " +
                                         "our support team can delete your posts, pages, media, and comments for you.",
                                         comment: "Detailed instructions on Start Over settings page. This is the first paragraph.")
         let doubleNewline = "\n\n"
