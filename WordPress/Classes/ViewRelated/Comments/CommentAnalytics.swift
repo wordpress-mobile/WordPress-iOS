4a50b13ff344bfcde4cac2edc07f75dee7281cdf
aerych <aerychj@gmail.com>
2020-11-22 21:49:43 -0600

    Analytics: Adds site comments analytics.

--- WordPress/Classes/ViewRelated/Comments/CommentAnalytics.swift
+++ WordPress/Classes/ViewRelated/Comments/CommentAnalytics.swift
@@ -1,9 +1,77 @@
-//
-//  CommentAnalytics.swift
-//  WordPress
-//
-//  Created by aerych on 11/22/20.
-//  Copyright © 2020 WordPress. All rights reserved.
-//
-
 import Foundation
+
+@objc class CommentAnalytics: NSObject{
+
+    static func trackingContext() -> String {
+        let screen = WPTabBarController.sharedInstance()?.currentlySelectedScreen() ?? "unknown"
+        switch screen {
+        case WPTabBarCurrentlySelectedScreenSites:
+            return "sites"
+        case WPTabBarCurrentlySelectedScreenReader:
+            return "reader"
+        case WPTabBarCurrentlySelectedScreenNotifications:
+            return "notifications"
+        default:
+            return "unknown"
+        }
+    }
+
+    static func defaultProperties(comment: Comment) -> [AnyHashable: Any] {
+        return [
+            "context": trackingContext(),
+            "post_id": comment.postID.intValue,
+            "comment_id": comment.commentID.intValue
+        ]
+    }
+
+    @objc static func trackCommentViewed(comment: Comment) {
+        let properties = defaultProperties(comment: comment)
+        WPAnalytics.track(.commentViewed, properties: properties, blog: comment.blog)
+    }
+
+    @objc static func trackCommentEditorOpened(comment: Comment) {
+        let properties = defaultProperties(comment: comment)
+        WPAnalytics.track(.commentEditorOpened, properties: properties, blog: comment.blog)
+    }
+
+    @objc static func trackCommentEdited(comment: Comment) {
+        let properties = defaultProperties(comment: comment)
+        WPAnalytics.track(.commentEdited, properties: properties, blog: comment.blog)
+    }
+
+    @objc static func trackCommentApproved(comment: Comment) {
+        let properties = defaultProperties(comment: comment)
+        WPAnalytics.track(.commentApproved, properties: properties, blog: comment.blog)
+    }
+
+    @objc static func trackCommentUnApproved(comment: Comment) {
+        let properties = defaultProperties(comment: comment)
+        WPAnalytics.track(.commentUnApproved, properties: properties, blog: comment.blog)
+    }
+
+    @objc static func trackCommentTrashed(comment: Comment) {
+        let properties = defaultProperties(comment: comment)
+        WPAnalytics.track(.commentTrashed, properties: properties, blog: comment.blog)
+    }
+
+    @objc static func trackCommentSpammed(comment: Comment) {
+        let properties = defaultProperties(comment: comment)
+        WPAnalytics.track(.commentSpammed, properties: properties, blog: comment.blog)
+    }
+
+    @objc static func trackCommentLiked(comment: Comment) {
+        let properties = defaultProperties(comment: comment)
+        WPAnalytics.track(.commentLiked, properties: properties, blog: comment.blog)
+    }
+
+    @objc static func trackCommentUnLiked(comment: Comment) {
+        let properties = defaultProperties(comment: comment)
+        WPAnalytics.track(.commentUnliked, properties: properties, blog: comment.blog)
+    }
+
+    @objc static func trackCommentRepliedTo(comment: Comment) {
+        let properties = defaultProperties(comment: comment)
+        WPAnalytics.track(.commentRepliedTo, properties: properties, blog: comment.blog)
+    }
+
+}
