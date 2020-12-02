4d108dfc7cf07f9b433dd2e1a829cbd840cffd86
Leandro Alonso <contato@leandroalonso.com>
2020-12-02 17:25:16 -0300

    Fix the logic to display year

--- WordPress/Classes/ViewRelated/Activity/ActivityListViewModel.swift
+++ WordPress/Classes/ViewRelated/Activity/ActivityListViewModel.swift
@@ -140,7 +140,7 @@ class ActivityListViewModel: Observable {
             return nil
         }
 
-        let format = isYearDifferent(between: after, and: before) ? "MMM d, yyyy" : "MMM d"
+        let format = shouldDisplayFullYear(with: after, and: before) ? "MMM d, yyyy" : "MMM d"
         dateFormatter.setLocalizedDateFormatFromTemplate(format)
 
         var formattedDateRanges: [String] = []
@@ -156,15 +156,16 @@ class ActivityListViewModel: Observable {
         return formattedDateRanges.joined(separator: " - ")
     }
 
-    private func isYearDifferent(between firstDate: Date?, and secondDate: Date?) -> Bool {
+    private func shouldDisplayFullYear(with firstDate: Date?, and secondDate: Date?) -> Bool {
         guard let firstDate = firstDate, let secondDate = secondDate else {
             return false
         }
 
-        let firstDateComponents = Calendar.current.dateComponents([.year], from: firstDate)
-        let secondDateComponents = Calendar.current.dateComponents([.year], from: secondDate)
+        let currentYear = Calendar.current.dateComponents([.year], from: Date()).year
+        let firstYear = Calendar.current.dateComponents([.year], from: firstDate).year
+        let secondYear = Calendar.current.dateComponents([.year], from: secondDate).year
 
-        return firstDateComponents.year != secondDateComponents.year
+        return firstYear != currentYear || secondYear != currentYear
     }
 
     private func restoreStatusSection() -> ImmuTableSection? {
