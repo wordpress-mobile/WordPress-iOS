b16f50940f1e4837031694f2fee13873c75a6cfe
Stephenie Harris <scoutharris@gmail.com>
2020-12-07 16:53:23 -0700

    Remove unused properties.

--- WordPress/Classes/Models/ReaderTeamTopic.swift
+++ WordPress/Classes/Models/ReaderTeamTopic.swift
@@ -7,17 +7,5 @@ import Foundation
         return "organization"
     }
 
-    @objc open var icon: UIImage? {
-        guard bundledTeamIcons.contains(slug) else {
-            return nil
-        }
-
-        return UIImage(named: slug)
-    }
-
-    fileprivate let bundledTeamIcons: [String] = [
-        ReaderTeamTopic.a8cTeamSlug
-    ]
-
     static let a8cTeamSlug = "a8c"
 }
