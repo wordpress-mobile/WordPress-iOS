48b1da88fbf07364da9f20838f7997e71cd82528
Emily Laguna <emily.laguna@automattic.com>
2020-12-10 09:40:17 -0500

    Add base threat cell

---
+++ WordPress/Classes/ViewRelated/Jetpack/Jetpack Scan/JetpackScanThreatCell.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+class JetpackScanThreatCell: UITableViewCell {
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+}
