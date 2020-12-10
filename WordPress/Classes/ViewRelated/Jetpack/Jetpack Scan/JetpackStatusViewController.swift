048b83bfba740aa5f02b5ae1bdebb1a027f37b43
Emily Laguna <emily.laguna@automattic.com>
2020-12-10 09:53:01 -0500

    Add base scan status view controller

---
+++ WordPress/Classes/ViewRelated/Jetpack/Jetpack Scan/JetpackStatusViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class JetpackStatusViewController: UIViewController {
+    @IBOutlet weak var iconContainerView: UIView!
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var defaultButton: FancyButton!
+    @IBOutlet weak var secondaryButton: FancyButton!
+}
