13537aecc3c0fe7de2cabf10f3e1c562767b920b
Yael Rubinstein <yaelirub@gmail.com>
2020-12-16 13:25:35 -0800

    Reverting previous fix and instead checking the bounds width > 0 before setting the tabs

--- WordPress/Classes/ViewRelated/Reader/Tab Navigation/ReaderTabItemsStore.swift
+++ WordPress/Classes/ViewRelated/Reader/Tab Navigation/ReaderTabItemsStore.swift
@@ -87,6 +87,8 @@ extension ReaderTabItemsStore {
         }
         state = .loading
 
+        // Return the tab bar items right away to avoid waiting for the request to finish
+        fetchTabBarItems()
 
         // Sync the reader menu
         service.fetchReaderMenu(success: { [weak self] in
