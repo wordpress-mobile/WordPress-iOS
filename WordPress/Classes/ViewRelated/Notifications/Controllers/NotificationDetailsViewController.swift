4266ba42d34623081d7e31137ef49922e1a743cf
Emily Laguna <emily.laguna@automattic.com>
2020-12-16 09:50:54 -0500

    Change the background to white on the notification details view

--- WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationDetailsViewController.swift
+++ WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationDetailsViewController.swift
@@ -383,13 +383,12 @@ extension NotificationDetailsViewController {
     }
 
     func setupMainView() {
-        view.backgroundColor = .listBackground
+        view.backgroundColor = note.isBadge ? .ungroupedListBackground : .listBackground
     }
 
     func setupTableView() {
         tableView.separatorStyle            = .none
         tableView.keyboardDismissMode       = .interactive
-        tableView.backgroundColor           = .neutral(.shade5)
         tableView.accessibilityIdentifier   = NSLocalizedString("Notification Details Table", comment: "Notifications Details Accessibility Identifier")
         tableView.backgroundColor           = note.isBadge ? .ungroupedListBackground : .listBackground
     }
