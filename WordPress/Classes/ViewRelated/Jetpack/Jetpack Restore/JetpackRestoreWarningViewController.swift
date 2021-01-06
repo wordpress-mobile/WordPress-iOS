4098feccdc2ab26589fccb07b26d260fb803a07c
Momo Ozawa <momo.ozawa@automattic.com>
2021-01-06 14:15:07 +0900

    Add Jetpack Restore Warning screen

---
+++ WordPress/Classes/ViewRelated/Jetpack/Jetpack Restore/JetpackRestoreWarningViewController.swift
@@ -0,0 +1,12 @@
+import Foundation
+import CocoaLumberjack
+import WordPressShared
+
+class JetpackRestoreWarningViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = NSLocalizedString("Warning", comment: "Title for Jetpack Restore Warning screen")
+    }
+
+}
