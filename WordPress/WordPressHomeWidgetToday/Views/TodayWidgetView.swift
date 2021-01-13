0c1de986211f5f9e114aa2452194a94653824b2a
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2021-01-13 09:52:39 -0600

    move LocalizedStringKey extension to a separate file

--- WordPress/WordPressHomeWidgetToday/Views/TodayWidgetView.swift
+++ WordPress/WordPressHomeWidgetToday/Views/TodayWidgetView.swift
@@ -52,14 +52,3 @@ extension TodayWidgetView {
         static let commentsTitle = LocalizedStringKey("widget.today.comments.label", defaultValue: "Comments", comment: "Title of comments label in today widget")
     }
 }
-
-
-extension LocalizedStringKey {
-    static let defaultBundle = Bundle(for: HomeWidgetTodayRemoteService.self)
-
-    /// TODO - TODAYWIDGET - LocalizedStringKey is used as a wrapper of NSLocalizedString, in order to use synthetic keys and assing a default value
-    /// in case of missing localization. This will need to be updated (if and) as soon as LocalizedStringKey supports default values
-    init(_ key: String, defaultValue: String, comment: String) {
-        self.init(NSLocalizedString(key, tableName: nil, bundle: Self.defaultBundle, value: defaultValue, comment: comment))
-    }
-}
