45ac742bf50441994dee453d793541fa3db94420
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2021-01-12 19:24:50 -0600

    Add convenience initializer to LocalizedStringKey to use a default value if localization is missing, TodayWidgetView

--- WordPress/WordPressHomeWidgetToday/Views/TodayWidgetView.swift
+++ WordPress/WordPressHomeWidgetToday/Views/TodayWidgetView.swift
@@ -45,10 +45,19 @@ extension TodayWidgetView {
 
     private enum Constants {
         // Titles
-        static let widgetTitle: LocalizedStringKey = "TODAY_WIDGET_TODAY_LABEL"
-        static let viewsTitle: LocalizedStringKey = "TODAY_WIDGET_VIEWS_LABEL"
-        static let visitorsTitle: LocalizedStringKey = "TODAY_WIDGET_VISITORS_LABEL"
-        static let likesTitle: LocalizedStringKey = "TODAY_WIDGET_LIKES_LABEL"
-        static let commentsTitle: LocalizedStringKey = "TODAY_WIDGET_COMMENTS_LABEL"
+        static let widgetTitle = LocalizedStringKey("TODAY_WIDGET_TODAY_LABEL", defaultValue: "Today", comment: "Title of today widget")
+        static let viewsTitle = LocalizedStringKey("TODAY_WIDGET_VIEWS_LABEL", defaultValue: "Views", comment: "Title of views label in today widget")
+        static let visitorsTitle = LocalizedStringKey("TODAY_WIDGET_VISITORS_LABEL", defaultValue: "Visitors", comment: "Title of visitors label in today widget")
+        static let likesTitle = LocalizedStringKey("TODAY_WIDGET_LIKES_LABEL", defaultValue: "Likes", comment: "Title of likes label in today widget")
+        static let commentsTitle = LocalizedStringKey("TODAY_WIDGET_COMMENTS_LABEL", defaultValue: "Comments", comment: "Title of comments label in today widget")
+    }
+}
+
+
+extension LocalizedStringKey {
+    static let defaultBundle = Bundle(for: HomeWidgetTodayRemoteService.self)
+
+    init(_ key: String, defaultValue: String, comment: String) {
+        self.init(NSLocalizedString(key, tableName: nil, bundle: Self.defaultBundle, value: defaultValue, comment: comment))
     }
 }
