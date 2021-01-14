086c1a66f852e2f5dbece611a4e79fd65b95018f
Stephenie Harris <scoutharris@gmail.com>
2021-01-14 13:40:28 -0700

    Reader post card: add seen toggle to options menu.

--- WordPress/Classes/ViewRelated/Reader/ReaderShowMenuAction.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderShowMenuAction.swift
@@ -67,6 +67,15 @@ final class ReaderShowMenuAction {
                                                })
         }
 
+        // Seen
+        alertController.addActionWithTitle(post.isSeen ? ReaderPostMenuButtonTitles.markUnseen : ReaderPostMenuButtonTitles.markSeen,
+                                           style: .default,
+                                           handler: { (action: UIAlertAction) in
+                                            if let post: ReaderPost = ReaderActionHelpers.existingObject(for: post.objectID, in: context) {
+                                                ReaderSeenAction().execute(with: post, context: context)
+                                            }
+                                           })
+
         // Visit
         alertController.addActionWithTitle(ReaderPostMenuButtonTitles.visit,
                                            style: .default,
