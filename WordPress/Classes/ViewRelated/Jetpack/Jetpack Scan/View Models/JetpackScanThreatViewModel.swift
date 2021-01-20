319af0673af9249cfd2eded4919de14a37701972
Emily Laguna <emily.laguna@automattic.com>
2021-01-20 14:50:01 -0500

    Add logic to the threat view model

--- WordPress/Classes/ViewRelated/Jetpack/Jetpack Scan/View Models/JetpackScanThreatViewModel.swift
+++ WordPress/Classes/ViewRelated/Jetpack/Jetpack Scan/View Models/JetpackScanThreatViewModel.swift
@@ -1,4 +1,156 @@
 import Foundation
 
 struct JetpackScanThreatViewModel {
+    let iconImage: UIImage?
+    let iconImageColor: UIColor
+    let title: String
+    let description: String?
+
+    init(threat: JetpackScanThreat) {
+        let status = threat.status
+
+        iconImage = Self.iconImage(for: status)
+        iconImageColor = Self.iconColor(for: status)
+        title = Self.title(for: threat)
+        description = Self.description(for: threat)
+    }
+
+    private static func description(for threat: JetpackScanThreat) -> String? {
+        let type = threat.type
+        let description: String?
+
+        switch type {
+        case .core:
+            description = Strings.description.core
+        case .file:
+            let signature = threat.signature
+            description = String(format: Strings.description.file, signature)
+        case .plugin:
+            description = Strings.description.plugin
+        case .theme:
+            description = Strings.description.theme
+        case .database:
+            description = Strings.description.database
+        default:
+            description = Strings.description.unknown
+        }
+
+        return description
+    }
+
+    private static func title(for threat: JetpackScanThreat) -> String {
+        let type = threat.type
+        let title: String
+
+        switch type {
+        case .core:
+            if let fileName = threat.fileName?.fileName() {
+                title = String(format: Strings.titles.core.multiple, fileName)
+            } else {
+                title = Strings.titles.core.singular
+            }
+
+        case .file:
+            if let fileName = threat.fileName?.fileName() {
+                title = String(format: Strings.titles.file.multiple, fileName)
+            } else {
+                title = Strings.titles.file.singular
+            }
+
+        case .plugin:
+            if let plugin = threat.`extension` {
+                title = String(format: Strings.titles.plugin.multiple, plugin.name, plugin.version)
+            } else {
+                title = Strings.titles.plugin.singular
+            }
+
+        case .theme:
+            if let plugin = threat.`extension` {
+                title = String(format: Strings.titles.theme.multiple, plugin.name, plugin.version)
+            } else {
+                title = Strings.titles.theme.singular
+            }
+
+        case .database:
+            if let rowCount = threat.rows?.count, rowCount > 0 {
+                title = String(format: Strings.titles.database.multiple, "\(rowCount)")
+            } else {
+                title = Strings.titles.database.singular
+            }
+
+        default:
+            title = Strings.titles.unknown
+        }
+
+        return title
+    }
+
+    private static func iconColor(for status: JetpackScanThreat.ThreatStatus?) -> UIColor {
+        switch status {
+        case .current:
+            return .error
+        case .fixed:
+            return .success
+        default:
+            return .neutral(.shade20)
+        }
+    }
+
+    private static func iconImage(for status: JetpackScanThreat.ThreatStatus?) -> UIImage? {
+        var image: UIImage = .gridicon(.notice)
+
+        if status == .fixed {
+            if let icon = UIImage(named: "jetpack-scan-threat-fixed") {
+                image = icon
+            }
+        }
+
+        return image.imageWithTintColor(.white)
+    }
+
+    private struct Strings {
+        struct titles {
+            struct core {
+                static let singular = NSLocalizedString("Infected core file", comment: "Title of a ")
+                static let multiple = NSLocalizedString("Infected core file: %1$@", comment: "Title TODO")
+            }
+
+            struct file {
+                static let singular = NSLocalizedString("A file contains a malicious code pattern", comment: "Title TODO")
+                static let multiple = NSLocalizedString("The file %1$@ contains a malicious code pattern", comment: "Title TODO")
+            }
+
+            struct plugin {
+                static let singular = NSLocalizedString("Vulnerable Plugin", comment: "Title TODO")
+                static let multiple = NSLocalizedString("Vulnerable Plugin: %1$@ (version %2$@)", comment: "Title TODO")
+            }
+
+            struct theme {
+                static let singular = NSLocalizedString("Vulnerable Theme", comment: "Title TODO")
+                static let multiple = NSLocalizedString("Vulnerable Theme %1$@ (version %2$@)", comment: "Title TODO")
+            }
+
+            struct database {
+                static let singular = NSLocalizedString("Database threat", comment: "Title TODO")
+                static let multiple = NSLocalizedString("Database %1$d threats", comment: "Title TODO")
+            }
+
+            static let unknown = NSLocalizedString("Threat Found", comment: "Title TODO")
+        }
+
+        struct description {
+            static let core = NSLocalizedString("Vulnerability found in WordPress", comment: "TODO")
+            static let file = NSLocalizedString("Threat found %1$@", comment: "TODO")
+            static let plugin = NSLocalizedString("Vulnerability found in plugin", comment: "TODO")
+            static let theme = NSLocalizedString("Vulnerability found in theme", comment: "TODO")
+            static let database: String? = nil
+            static let unknown = NSLocalizedString("Miscellaneous vulnerability", comment: "TODO")
+        }
+    }
+}
+
+private extension String {
+    func fileName() -> String {
+        return (self as NSString).lastPathComponent
+    }
 }
