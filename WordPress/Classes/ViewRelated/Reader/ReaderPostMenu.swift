6ed2c95ce322c3e138abd7a48bfe05995b1d4724
Stephenie Harris <scoutharris@gmail.com>
2021-01-20 14:19:19 -0700

    When a post's seen status is toggled from the post details menu, post a notification.

--- WordPress/Classes/ViewRelated/Reader/ReaderPostMenu.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderPostMenu.swift
@@ -205,7 +205,9 @@ open class ReaderPostMenu {
         WPAnalytics.track(event, properties: ["source": "post_details"])
 
         let postService = ReaderPostService(managedObjectContext: context)
-        postService.toggleSeen(for: post, success: nil, failure: nil)
+        postService.toggleSeen(for: post, success: {
+            NotificationCenter.default.post(name: .ReaderPostSeenToggled, object: nil, userInfo: [postUserInfoKey: post])
+        }, failure: nil)
     }
 
     fileprivate class func visitSiteForPost(_ post: ReaderPost, presentingViewController viewController: UIViewController) {
