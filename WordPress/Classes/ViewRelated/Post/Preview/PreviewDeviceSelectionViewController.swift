1e9aba6ec18b56d2bba9901982a74b3f98ae48a6
Matthew Kevins <mmkevins@yahoo.com>
2021-01-22 11:02:31 +1000

    Fix whitespace violation

--- WordPress/Classes/ViewRelated/Post/Preview/PreviewDeviceSelectionViewController.swift
+++ WordPress/Classes/ViewRelated/Post/Preview/PreviewDeviceSelectionViewController.swift
@@ -35,7 +35,7 @@ class PreviewDeviceSelectionViewController: UIViewController {
         static var available: [PreviewDevice] {
             return [.mobile, .tablet, .desktop]
         }
-        
+
         var viewportScript: String {
             return String(format: "let parent = document.querySelector('meta[name=viewport]'); parent.setAttribute('content', 'width=%1$d, initial-scale=0');", NSInteger(width))
         }
