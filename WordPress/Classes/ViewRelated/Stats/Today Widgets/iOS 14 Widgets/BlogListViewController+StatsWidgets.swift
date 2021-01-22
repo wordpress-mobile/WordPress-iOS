6bbca846618462085cb9f48252451773a16a29b0
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2021-01-22 11:16:40 -0600

    update site list in iOS 14 cache when syncBlogs completes

---
+++ WordPress/Classes/ViewRelated/Stats/Today Widgets/iOS 14 Widgets/BlogListViewController+StatsWidgets.swift
@@ -0,0 +1,43 @@
+extension BlogListViewController {
+
+    @objc func refreshStatsWidgetsSiteList() {
+
+        guard let currentData = HomeWidgetTodayData.read() else {
+            return
+        }
+        let blogService = BlogService(managedObjectContext: ContextManager.shared.mainContext)
+        let updatedSiteList = blogService.visibleBlogsForWPComAccounts()
+
+        let newData = updatedSiteList.reduce(into: [Int: HomeWidgetTodayData]()) { result, element in
+            guard let blogID = element.dotComID else {
+                return
+            }
+            let existingSite = currentData[blogID.intValue]
+
+            let siteURL = element.url ?? existingSite?.url ?? ""
+            let siteName = (element.title ?? siteURL).isEmpty ? siteURL : element.title ?? siteURL
+
+            var iconURL = existingSite?.iconURL
+            var timeZone = existingSite?.timeZone ?? TimeZone.current
+
+            if let blog = blogService.blog(byBlogId: blogID) {
+                iconURL = blog.icon
+                timeZone = blogService.timeZone(for: blog)
+            }
+            let date = existingSite?.date ?? Date()
+            let stats = existingSite?.stats ?? TodayWidgetStats()
+
+            result[blogID.intValue] = HomeWidgetTodayData(siteID: blogID.intValue,
+                                                          siteName: siteName,
+                                                          iconURL: iconURL,
+                                                          url: siteURL,
+                                                          timeZone: timeZone,
+                                                          date: date,
+                                                          stats: stats)
+
+
+        }
+
+        HomeWidgetTodayData.write(items: newData)
+    }
+}
