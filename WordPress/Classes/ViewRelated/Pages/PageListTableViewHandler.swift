46d8201dad4786bab44e235c32ae1787a473e99a
Chip Snyder <chip.snyder3@gmail.com>
2021-01-27 13:17:55 -0500

    Mark the edit homepage tour as complete if the homepage has been deleted

--- WordPress/Classes/ViewRelated/Pages/PageListTableViewHandler.swift
+++ WordPress/Classes/ViewRelated/Pages/PageListTableViewHandler.swift
@@ -71,7 +71,6 @@ final class PageListTableViewHandler: WPTableViewHandler {
         }
     }
 
-
     // MARK: - Public methods
 
     func page(at indexPath: IndexPath) -> Page {
@@ -164,6 +163,15 @@ final class PageListTableViewHandler: WPTableViewHandler {
             return []
         }
 
+        cancelTourIfNeeded()
         return status == .published ? pages.setHomePageFirst().hierarchySort() : pages
     }
+
+    private func cancelTourIfNeeded() {
+        if QuickStartTourGuide.shared.isCurrentElement(.editHomepage) && blog.homepagePageID == nil {
+            // Ends the tour Edit Homepage if the site doesn't have a homepage set or uses the blog.
+            QuickStartTourGuide.shared.visited(.editHomepage)
+        }
+    }
+
 }
