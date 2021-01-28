cb10f792b8a70c9902a2b1bfb58d703d2a159cde
Momo Ozawa <momo.ozawa@automattic.com>
2021-01-28 17:12:36 +0900

    Delete Threat Details view
    Move view code into VC

--- WordPress/Classes/ViewRelated/Jetpack/Jetpack Scan/JetpackScanThreatDetailsViewController.swift
+++ WordPress/Classes/ViewRelated/Jetpack/Jetpack Scan/JetpackScanThreatDetailsViewController.swift
@@ -2,6 +2,37 @@ import UIKit
 
 class JetpackScanThreatDetailsViewController: UIViewController {
 
+    // MARK: - IBOutlets
+
+    /// General info
+    @IBOutlet private weak var generalInfoStackView: UIStackView!
+    @IBOutlet private weak var icon: UIImageView!
+    @IBOutlet private weak var generalInfoTitleLabel: UILabel!
+    @IBOutlet private weak var generalInfoDescriptionLabel: UILabel!
+
+    /// Problem
+    @IBOutlet private weak var problemStackView: UIStackView!
+    @IBOutlet private weak var problemTitleLabel: UILabel!
+    @IBOutlet private weak var problemDescriptionLabel: UILabel!
+
+    /// Technical details
+    @IBOutlet private weak var technicalDetailsStackView: UIStackView!
+    @IBOutlet private weak var technicalDetailsTitleLabel: UILabel!
+    @IBOutlet private weak var technicalDetailsDescriptionLabel: UILabel!
+    @IBOutlet private weak var technicalDetailsFileContainerView: UIView!
+    @IBOutlet private weak var technicalDetailsFileLabel: UILabel!
+    @IBOutlet private weak var technicalDetailsContextLabel: UILabel!
+
+    /// Fix
+    @IBOutlet private weak var fixStackView: UIStackView!
+    @IBOutlet private weak var fixTitleLabel: UILabel!
+    @IBOutlet private weak var fixDescriptionLabel: UILabel!
+
+    /// Buttons
+    @IBOutlet private weak var buttonsStackView: UIStackView!
+    @IBOutlet private weak var primaryActionButton: FancyButton!
+    @IBOutlet private weak var secondaryActionButton: FancyButton!
+
     // MARK: - Properties
 
     private let threat: JetpackScanThreat
@@ -22,19 +53,114 @@ class JetpackScanThreatDetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = Strings.title
-        configureThreatDetailsView()
+
+        let viewModel = JetpackScanThreatViewModel(threat: threat)
+        configure(with: viewModel)
     }
+
+    // MARK: - IBActions
+
+    @IBAction private func primaryActionButtonTapped(_ sender: Any) {
+    }
+
+    @IBAction private func secondaryActionButtonTapped(_ sender: Any) {
+    }
+}
+
+extension JetpackScanThreatDetailsViewController {
 
     // MARK: - Configure
 
-    private func configureThreatDetailsView() {
-        let threatDetailsView = ThreatDetailsView.loadFromNib()
-        let viewModel = JetpackScanThreatViewModel(threat: threat)
-        threatDetailsView.configure(with: viewModel)
+    func configure(with viewModel: JetpackScanThreatViewModel) {
+        icon.image = viewModel.detailIconImage
+        icon.tintColor = viewModel.detailIconImageColor
 
-        threatDetailsView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(threatDetailsView)
-        view.pinSubviewToAllEdges(threatDetailsView)
+        generalInfoTitleLabel.text = viewModel.title
+        generalInfoDescriptionLabel.text = viewModel.description
+
+        problemTitleLabel.text = viewModel.problemTitle
+        problemDescriptionLabel.text = viewModel.problemDescription
+
+        if viewModel.fileContext != nil {
+            technicalDetailsTitleLabel.text = viewModel.technicalDetailsTitle
+            technicalDetailsDescriptionLabel.text = viewModel.technicalDetailsDescription
+            technicalDetailsFileLabel.text = viewModel.fileName
+            technicalDetailsContextLabel.text = "" // FIXME
+            technicalDetailsStackView.isHidden = false
+        } else {
+            technicalDetailsStackView.isHidden = true
+        }
+
+        fixTitleLabel.text = viewModel.fixTitle
+        fixDescriptionLabel.text = viewModel.fixDescription
+
+        primaryActionButton.setTitle(viewModel.primaryButtonTitle, for: .normal)
+        secondaryActionButton.setTitle(viewModel.secondaryButtonTitle, for: .normal)
+
+        applyStyles()
+    }
+
+    // MARK: - Styling
+
+    private func applyStyles() {
+        view.backgroundColor = .basicBackground
+        styleGeneralInfoSection()
+        styleProblemSection()
+        styleTechnicalDetailsSection()
+        styleFixSection()
+        styleButtons()
+    }
+
+    private func styleGeneralInfoSection() {
+        generalInfoTitleLabel.font = WPStyleGuide.fontForTextStyle(.title3, fontWeight: .semibold)
+        generalInfoTitleLabel.textColor = .error
+        generalInfoTitleLabel.numberOfLines = 0
+
+        generalInfoDescriptionLabel.font = WPStyleGuide.fontForTextStyle(.body)
+        generalInfoDescriptionLabel.textColor = .text
+        generalInfoDescriptionLabel.numberOfLines = 0
+    }
+
+    private func styleProblemSection() {
+        problemTitleLabel.font = WPStyleGuide.fontForTextStyle(.title3, fontWeight: .semibold)
+        problemTitleLabel.textColor = .text
+        problemTitleLabel.numberOfLines = 0
+
+        problemDescriptionLabel.font = WPStyleGuide.fontForTextStyle(.body)
+        problemDescriptionLabel.textColor = .text
+        problemDescriptionLabel.numberOfLines = 0
+    }
+
+    private func styleTechnicalDetailsSection() {
+        technicalDetailsTitleLabel.font = WPStyleGuide.fontForTextStyle(.title3, fontWeight: .semibold)
+        technicalDetailsTitleLabel.textColor = .text
+        technicalDetailsTitleLabel.numberOfLines = 0
+
+        technicalDetailsFileContainerView.backgroundColor = .listBackground
+
+        technicalDetailsFileLabel.font = WPStyleGuide.fontForTextStyle(.footnote)
+        technicalDetailsFileLabel.textColor = .text
+        technicalDetailsFileLabel.numberOfLines = 0
+
+        technicalDetailsDescriptionLabel.font = WPStyleGuide.fontForTextStyle(.body)
+        technicalDetailsDescriptionLabel.textColor = .text
+        technicalDetailsDescriptionLabel.numberOfLines = 0
+    }
+
+    private func styleFixSection() {
+        fixTitleLabel.font = WPStyleGuide.fontForTextStyle(.title3, fontWeight: .semibold)
+        fixTitleLabel.textColor = .text
+        fixTitleLabel.numberOfLines = 0
+
+        fixDescriptionLabel.font = WPStyleGuide.fontForTextStyle(.body)
+        fixDescriptionLabel.textColor = .text
+        fixDescriptionLabel.numberOfLines = 0
+    }
+
+    private func styleButtons() {
+        primaryActionButton.isPrimary = true
+
+        secondaryActionButton.isPrimary = false
     }
 }
 
