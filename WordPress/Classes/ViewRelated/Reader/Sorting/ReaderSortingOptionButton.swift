f8ff53aaffb15d75030fdb8faf790cac3d9ba254
Božidar Ševo <bozidarsevo@gmail.com>
2021-01-31 22:25:28 +0100

    Added ReaderSortingOptionButton used for opening sorting options sheet and use sortingOption parameter for fetching cards

---
+++ WordPress/Classes/ViewRelated/Reader/Sorting/ReaderSortingOptionButton.swift
@@ -0,0 +1,118 @@
+import UIKit
+
+enum ReaderSortingOption: String, CaseIterable {
+    case date, popularity
+    
+    var parameterValue: String {
+        return rawValue
+    }
+    
+    var localizedDescription: String {
+        // TODO: check if strings are localized
+        switch self {
+        case .date:
+            return NSLocalizedString("Recent", comment: "Sorting option description")
+        case .popularity:
+            return NSLocalizedString("Popular", comment: "Sorting option description")
+        }
+    }
+    
+    var image: UIImage {
+        switch self {
+        case .date:
+            // TODO: use proper icon
+            return .gridicon(.calendar)
+        case .popularity:
+            // TODO: use proper icon
+            return .gridicon(.lineGraph)
+        }
+    }
+}
+
+class ReaderSortingOptionButton: UIControl {
+    var sourceView: UIView {
+        return chevronView
+    }
+    
+    private lazy var iconView: UIImageView = {
+        let view = UIImageView()
+        view.tintColor = UIColor(light: UIColor(hexString: "4D4D4D"), dark: UIColor(hexString: "BFBFBF"))
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var label: UILabel = {
+        let view = UILabel()
+        view.textColor = UIColor(light: .black, dark: .white)
+        view.font = WPFontManager.systemSemiBoldFont(ofSize: 15.0)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var chevronView: UIImageView = {
+        let view = UIImageView()
+        view.image = .gridicon(.chevronDown)
+        view.tintColor = UIColor(light: UIColor(hexString: "4D4D4D"), dark: UIColor(hexString: "BFBFBF"))
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    var sortingOption: ReaderSortingOption? {
+        didSet {
+            label.text = sortingOption?.localizedDescription
+            iconView.image = sortingOption?.image
+        }
+    }
+    
+    // TODO: check how should it behave when touched/highlighted
+    override var isHighlighted: Bool {
+        didSet {
+            alpha = isHighlighted ? 0.5 : 1.0
+        }
+    }
+    
+    // MARK: - setup
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setup()
+    }
+    
+    convenience init() {
+        self.init(frame: CGRect.zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setup()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setup()
+    }
+    
+    private func setup() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        // TODO: check if accessibility is set correctly
+        accessibilityIdentifier = "Reader sorting option button"
+        accessibilityLabel = NSLocalizedString("Sorting option button", comment: "Accessibility label for sorting option button")
+        
+        addSubview(iconView)
+        addSubview(label)
+        addSubview(chevronView)
+        
+        NSLayoutConstraint.activate([
+            iconView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            iconView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 18.0),
+            label.centerYAnchor.constraint(equalTo: centerYAnchor),
+            label.leadingAnchor.constraint(equalTo: iconView.trailingAnchor, constant: 6.0),
+            chevronView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            chevronView.leadingAnchor.constraint(equalTo: label.trailingAnchor, constant: 12.0),
+        ])
+    }
+}
