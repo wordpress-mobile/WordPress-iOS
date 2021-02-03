1c8cb9ec340ceb8650dc25c1243cd1d02bd9d617
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2021-02-03 12:56:40 -0600

    Add row view for this week widget

---
+++ WordPress/WordPressStatsWidgets/Views/ThisWeekContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ThisWeekContentView.swift
+//  WordPressStatsWidgets
+//
+//  Created by Giorgio Ruscigno on 2/3/21.
+//  Copyright © 2021 WordPress. All rights reserved.
+//
+
+import SwiftUI
+
+struct ThisWeekContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ThisWeekContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThisWeekContentView()
+    }
+}
---
+++ WordPress/WordPressStatsWidgets/Views/ThisWeekRow.swift
@@ -0,0 +1,97 @@
+
+import SwiftUI
+import WidgetKit
+
+struct ThisWeekRow: View {
+
+    let date = Date()
+    let todayPercentValue = 0.9
+    let todayData = 130
+    let isToday = false
+
+    private var percentFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .percent
+        formatter.positivePrefix = "+"
+        return formatter
+    }()
+
+    private var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.setLocalizedDateFormatFromTemplate("MMM d, yyyy")
+        return formatter
+    }()
+
+    var differenceBackgroundColor: Color {
+        guard !isToday else {
+            return Constants.neutralColor
+        }
+        return todayPercentValue < 0 ? Constants.negativeColor : Constants.positiveColor
+    }
+
+    var differenceLabelText: LocalizedStringKey {
+        guard !isToday else {
+            return LocalizableStrings.todayWidgetTitle
+        }
+
+        return LocalizedStringKey(dateFormatter.string(from: date))
+    }
+
+    var body: some View {
+        HStack {
+            Text(differenceLabelText)
+                .font(Constants.dateViewFont)
+                .fontWeight(Constants.dateViewFontWeight)
+                .foregroundColor(Constants.dateViewFontColor)
+            Spacer()
+            Text("\(todayData)")
+                .font(Constants.dataViewFont)
+                .foregroundColor(Constants.dataViewFontColor)
+
+            Text(percentFormatter.string(for: todayPercentValue) ?? "0")
+
+                .frame(minWidth: Constants.differenceViewMinWidth,
+                       idealHeight: Constants.differenceViewIdealHeight,
+                       alignment: Constants.differenceViewAlignment)
+                .padding(Constants.differenceViewInsets)
+                .font(Constants.differenceViewFont)
+                .foregroundColor(Constants.differenceTextColor)
+                .background(differenceBackgroundColor)
+                .cornerRadius(Constants.differenceCornerRadius)
+        }
+        .padding()
+    }
+}
+
+struct ThisWeekRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ThisWeekRow()
+            .previewContext(WidgetPreviewContext(family: .systemMedium))
+    }
+}
+
+
+private enum Constants {
+    // difference view
+    static let positiveColor = Color("Green50")
+    static let negativeColor = Color("Red50")
+    static let neutralColor = Color(UIColor.systemGray)
+    static let differenceTextColor = Color.white
+
+    static let differenceViewInsets = EdgeInsets(top: 4, leading: 8, bottom: 4, trailing: 8)
+
+    static let differenceViewMinWidth: CGFloat = 44.0
+    static let differenceViewIdealHeight: CGFloat = 24.0
+    static let differenceViewAlignment = Alignment.trailing
+
+    static let differenceCornerRadius: CGFloat = 4.0
+
+    static let differenceViewFont = Font.footnote
+    // date view
+    static let dateViewFont = Font.subheadline
+    static let dateViewFontColor = Color(.label)
+    static let dateViewFontWeight = Font.Weight.regular
+    // data view
+    static let dataViewFont = Font.caption
+    static let dataViewFontColor = Color(.label)
+}
