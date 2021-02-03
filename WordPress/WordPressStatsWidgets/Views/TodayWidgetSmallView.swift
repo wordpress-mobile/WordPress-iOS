7b7f12273043c1b296524a76942aa151ed9fa6f7
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2021-02-03 14:59:16 -0600

    add computed property for views in TodayWidgetSmallView

--- WordPress/WordPressStatsWidgets/Views/TodayWidgetSmallView.swift
+++ WordPress/WordPressStatsWidgets/Views/TodayWidgetSmallView.swift
@@ -5,13 +5,17 @@ struct TodayWidgetSmallView: View {
     let widgetTitle: LocalizedStringKey
     let viewsTitle: LocalizedStringKey
 
+    private var views: Int {
+        (content as? HomeWidgetTodayData)?.stats.views ?? (content as? HomeWidgetAllTimeData)?.stats.views ?? 0
+    }
+
     var body: some View {
         HStack {
             VStack(alignment: .leading) {
                 FlexibleCard(axis: .vertical, title: widgetTitle, value: .description(content.siteName))
 
                 Spacer()
-                VerticalCard(title: viewsTitle, value: (content as? HomeWidgetTodayData)?.stats.views ?? (content as? HomeWidgetAllTimeData)?.stats.views ?? 0, largeText: true)
+                VerticalCard(title: viewsTitle, value: views, largeText: true)
             }
             Spacer()
         }
