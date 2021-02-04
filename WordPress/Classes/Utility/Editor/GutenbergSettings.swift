16b22f89887acbf295e593eefe45a3500e0003ab
Paul Von Schrottky <paul.von.schrottky@automattic.com>
2021-02-04 15:43:27 -0300

    Don't check editor pref on new Simple site posts
    Before this commit, when creating a new post the app would check the site's mobile editor preference (which is different to their web editor preference) and open the corresponding editor (Block editor or Classic editor).
    
    With this commit we now do not look at the preference when the user creates a new post and we will now always use the Block editor in this scenario).

--- WordPress/Classes/Utility/Editor/GutenbergSettings.swift
+++ WordPress/Classes/Utility/Editor/GutenbergSettings.swift
@@ -181,7 +181,8 @@ class GutenbergSettings {
         let blog = post.blog
 
         if post.isContentEmpty() {
-            return blog.isGutenbergEnabled
+            let isSimpleWPComSite = blog.isAtomic() || !blog.isHostedAtWPcom
+            return isSimpleWPComSite || blog.isGutenbergEnabled
         } else {
             // It's an existing post
             return post.containsGutenbergBlocks()
