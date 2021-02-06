20e882bc529f2724262a27db6edaded1ac0e1aed
Joel Dean <jdeanjj1000@gmail.com>
2021-02-05 22:05:35 -0500

    The label on the Choose audio picker is now Choose from device.

--- WordPress/Classes/ViewRelated/Media/StockPhotos/StockPhotosStrings.swift
+++ WordPress/Classes/ViewRelated/Media/StockPhotos/StockPhotosStrings.swift
@@ -6,7 +6,7 @@ extension String {
     }
 
     static var otherApps: String {
-        return NSLocalizedString("Other Apps", comment: "Menu option used for adding media from other applications.")
+        return NSLocalizedString("Choose from device", comment: "Menu option used for adding media from other applications.")
     }
 
     static var allFiles: String {
