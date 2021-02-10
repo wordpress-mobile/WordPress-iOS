f66b1b6682286e9a3ee4b8e4d300b08be856c41a
Antonis Lilis <antonis.lilis@automattic.com>
2021-02-10 19:29:07 +0200

    Sets the isFirstTimePublish value for newly published posts

--- WordPress/Classes/Services/PostService.m
+++ WordPress/Classes/Services/PostService.m
@@ -260,6 +260,8 @@ forceDraftIfCreating:(BOOL)forceDraftIfCreating
 
     // Add the post to the uploading queue list
     [self.uploadingList uploading:postObjectID];
+    
+    BOOL isFirstTimePublish = post.isFirstTimePublish;
 
     void (^successBlock)(RemotePost *post) = ^(RemotePost *post) {
         [self.managedObjectContext performBlock:^{
@@ -277,6 +279,7 @@ forceDraftIfCreating:(BOOL)forceDraftIfCreating
                     }
                 }
                 
+                postInContext.isFirstTimePublish = isFirstTimePublish;
                 [self updatePost:postInContext withRemotePost:post];
                 postInContext.remoteStatus = AbstractPostRemoteStatusSync;
 
