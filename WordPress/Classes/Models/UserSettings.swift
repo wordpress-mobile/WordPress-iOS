b8a4ee116b5fd1c5f8620f5e6e27bacb11b88a90
Jeremy Massel <jeremy.massel@gmail.com>
2021-02-28 21:45:34 -0700

    Add documentation

--- WordPress/Classes/Models/UserSettings.swift
+++ WordPress/Classes/Models/UserSettings.swift
@@ -2,13 +2,31 @@ import Foundation
 
 class UserSettings {
 
+    /// Stores all `UserSettings` keys.
+    ///
+    /// The additional level of indirection allows these keys to be retrieved from tests.
+    ///
+    /// **IMPORTANT NOTE:**
+    ///
+    /// Any change to these keys is a breaking change without some kind of migration.
+    /// It's probably best never to change them.
+    struct Keys {
+        static let crashLoggingOptOutKey = "crashlytics_opt_out"
+        static let forceCrashLoggingKey = "force-crash-logging"
+        static let defaultDotComUUIDKey = "AccountDefaultDotcomUUID"
+    }
+
     @objc
-    @UserDefault("crashlytics_opt_out", defaultValue: false)
+    @UserDefault(Keys.crashLoggingOptOutKey, defaultValue: false)
     static var userHasOptedOutOfCrashLogging: Bool
 
     @objc
-    @UserDefault("force-crash-logging", defaultValue: false)
+    @UserDefault(Keys.forceCrashLoggingKey, defaultValue: false)
     static var userHasForcedCrashLoggingEnabled: Bool
+
+    @objc
+    @EraseableUserDefault(Keys.defaultDotComUUIDKey)
+    static var defaultDotComUUID: String?
 }
 
 /// A property wrapper for UserDefaults access
@@ -28,6 +46,29 @@ struct UserDefault<T> {
         }
         set {
             UserDefaults.standard.set(newValue, forKey: key)
+        }
+    }
+}
+
+/// A property wrapper for optional UserDefaults that return `nil` by default
+@propertyWrapper
+struct EraseableUserDefault<T> {
+    let key: String
+
+    init(_ key: String) {
+        self.key = key
+    }
+
+    var wrappedValue: T? {
+        get {
+            return UserDefaults.standard.object(forKey: key) as? T
+        }
+        set {
+            if let newValue = newValue {
+                UserDefaults.standard.set(newValue, forKey: key)
+            } else {
+                UserDefaults.standard.removeObject(forKey: key)
+            }
         }
     }
 }
--- WordPress/Classes/Models/WPAccount+Lookup.swift
+++ WordPress/Classes/Models/WPAccount+Lookup.swift
@@ -2,49 +2,56 @@ import CoreData
 
 public extension WPAccount {
 
-    /// Is this `WPAccount` instance the default WordPress.com account?
+    //MARK: - Relationship Lookups
+
+    /// Is this `WPAccount` object the default WordPress.com account?
+    ///
     @objc
     var isDefaultWordPressComAccount: Bool {
-        guard let uuid = WPAccount.defaultDotComAccountUUID, uuid.count > 0 else {
+        guard let uuid = UserSettings.defaultDotComUUID, uuid.count > 0 else {
             return false
         }
 
         return self.uuid == uuid
     }
 
+    /// Does this `WPAccount` object have any associated blogs?
+    ///
     @objc
     var hasBlogs: Bool {
         return !blogs.isEmpty
     }
 
-    /// An Objective-C wrapper around the `lookupDefaultWordPressComAccount` method.
-    ///
-    /// Prefer using `lookupDefaultWordPressComAccount` directly
-    @objc(lookupDefaultWordPressComAccountInContext:)
-    static func objc_lookupDefaultWordPressComAccount(in context: NSManagedObjectContext) -> WPAccount? {
-        return try? lookupDefaultWordPressComAccount(in: context)
-    }
+    //MARK: - Object Lookups
 
-    /// Returns the default WordPress.com account
-    /// The default WordPress.com account is the one used for Reader and Notifications
+    /// Returns the default WordPress.com account, if one exists
     ///
-    /// @return the default WordPress.com account
-    /// @see setDefaultWordPressComAccount
-    /// @see removeDefaultWordPressComAccount
+    /// The default WordPress.com account is the one used for Reader and Notifications.
+    ///
     static func lookupDefaultWordPressComAccount(in context: NSManagedObjectContext) throws -> WPAccount? {
-        guard let uuid = defaultDotComAccountUUID, uuid.count > 0 else {
+        guard let uuid = UserSettings.defaultDotComUUID, uuid.count > 0 else {
             // No account, or no default account set. Clear the defaults key.
-            defaultDotComAccountUUID = nil
+            UserSettings.defaultDotComUUID = nil
             return nil
         }
 
         return try lookup(withUUIDString: uuid, in: context)
     }
 
+    /// Does a default WordPress.com account exist?
+    /// The default WordPress.com account is the one used for Reader and Notifications.
+    ///
     static func lookupHasDefaultWordPressComAccount(in context: NSManagedObjectContext) throws -> Bool {
         return try lookupDefaultWordPressComAccount(in: context) != nil
     }
 
+    /// Lookup a WPAccount by its local uuid
+    ///
+    /// - Parameters:
+    ///   - uuidString: The UUID (in string form) associated with the account
+    ///   - context:  An NSManagedObjectContext containing the `WPAccount` object with the given `uuidString`.
+    /// - Returns: The `WPAccount` object associated with the given `uuidString`, if it exists.
+    ///
     static func lookup(withUUIDString uuidString: String, in context: NSManagedObjectContext) throws -> WPAccount? {
         let fetchRequest = NSFetchRequest<Self>(entityName: WPAccount.entityName())
         fetchRequest.predicate = NSPredicate(format: "uuid = %@", uuidString)
@@ -61,32 +68,35 @@ public extension WPAccount {
         return defaultAccount
     }
 
-    /// An Objective-C wrapper around the `lookupDefaultWordPressComAccount` method.
+    /// Lookup the total number of `WPAccount` objects in the given `context`.
     ///
-    /// Prefer using `lookupDefaultWordPressComAccount` directly
-    @objc(lookupNumberOfAccountsInContext:)
-    static func objc_lookupNumberOfAccounts(in context: NSManagedObjectContext) -> Int {
-        return (try? lookupNumberOfAccounts(in: context)) ?? 0
-    }
-
+    /// If none exist, this method returns `0`.
+    ///
+    /// - Parameters:
+    ///   - context:  An NSManagedObjectContext that may or may not contain `WPAccount` objects.
+    /// - Returns: The number of `WPAccount` objects in the given `context`.
+    ///
     static func lookupNumberOfAccounts(in context: NSManagedObjectContext) throws -> Int {
         let fetchRequest = NSFetchRequest<Self>(entityName: WPAccount.entityName())
         fetchRequest.includesSubentities = false
         return try context.count(for: fetchRequest)
     }
 
-    private static let DefaultDotcomAccountUUIDDefaultsKey = "AccountDefaultDotcomUUID"
+    //MARK: - Objective-C Compatibility Wrappers
 
-    private static var defaultDotComAccountUUID: String? {
-        get {
-            return UserDefaults.standard.string(forKey: DefaultDotcomAccountUUIDDefaultsKey)
-        }
-        set {
-            if newValue == nil {
-                UserDefaults.standard.removeObject(forKey: DefaultDotcomAccountUUIDDefaultsKey)
-            } else {
-                UserDefaults.standard.setValue(newValue, forKey: DefaultDotcomAccountUUIDDefaultsKey)
-            }
-        }
+    /// An Objective-C wrapper around the `lookupDefaultWordPressComAccount` method.
+    ///
+    /// Prefer using `lookupDefaultWordPressComAccount` directly
+    @objc(lookupDefaultWordPressComAccountInContext:)
+    static func objc_lookupDefaultWordPressComAccount(in context: NSManagedObjectContext) -> WPAccount? {
+        return try? lookupDefaultWordPressComAccount(in: context)
+    }
+
+    /// An Objective-C wrapper around the `lookupDefaultWordPressComAccount` method.
+    ///
+    /// Prefer using `lookupDefaultWordPressComAccount` directly
+    @objc(lookupNumberOfAccountsInContext:)
+    static func objc_lookupNumberOfAccounts(in context: NSManagedObjectContext) -> Int {
+        return (try? lookupNumberOfAccounts(in: context)) ?? 0
     }
 }
