e950e36b761ed1335b064861bf07536581423f40
bitdc <andzeroo@gmail.com>
2021-03-04 02:18:13 +0700

    Minor adjustments based on previous review
    - https://github.com/wordpress-mobile/WordPress-iOS/pull/15977#discussion_r585114909
    - https://github.com/wordpress-mobile/WordPress-iOS/pull/15994#discussion_r585122810

--- WordPress/Classes/Services/PostServiceRemoteFactory.swift
+++ WordPress/Classes/Services/PostServiceRemoteFactory.swift
@@ -5,16 +5,18 @@ import WordPressKit
 @objc class PostServiceRemoteFactory: NSObject {
     @objc func forBlog(_ blog: Blog) -> PostServiceRemote? {
         if blog.supports(.wpComRESTAPI),
-            let api = blog.wordPressComRestApi(),
-            let dotComID = blog.dotComID {
+           let api = blog.wordPressComRestApi(),
+           let dotComID = blog.dotComID {
             return PostServiceRemoteREST(wordPressComRestApi: api, siteID: dotComID)
-        } else if let api = blog.xmlrpcApi,
-            let username = blog.username,
-            let password = blog.password {
-            return PostServiceRemoteXMLRPC(api: api, username: username, password: password)
-        } else {
-            return nil
         }
+
+        if let api = blog.xmlrpcApi,
+           let username = blog.username,
+           let password = blog.password {
+            return PostServiceRemoteXMLRPC(api: api, username: username, password: password)
+        }
+
+        return nil
     }
 
     @objc func restRemoteFor(siteID: NSNumber, context: NSManagedObjectContext) -> PostServiceRemoteREST? {
