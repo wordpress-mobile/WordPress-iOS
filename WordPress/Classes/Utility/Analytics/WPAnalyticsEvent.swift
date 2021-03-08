f136f09311f06dfe90f36f6b15ea54df467276b8
Chip <chip.snyder@automattic.com>
2021-03-08 10:02:48 -0500

    Add Analytics events to category selection (#16024)

--- WordPress/Classes/Utility/Analytics/WPAnalyticsEvent.swift
+++ WordPress/Classes/Utility/Analytics/WPAnalyticsEvent.swift
@@ -153,6 +153,10 @@ import Foundation
     case inviteLinksShare
     case inviteLinksDisable
 
+    // Page Layout and Site Design Picker
+    case categoryFilterSelected
+    case categoryFilterDeselected
+
     /// A String that represents the event
     var value: String {
         switch self {
@@ -421,6 +425,12 @@ import Foundation
             return "invite_links_share"
         case .inviteLinksDisable:
             return "invite_links_disable"
+
+        // Page Layout and Site Design Picker
+        case .categoryFilterSelected:
+            return "category_filter_selected"
+        case .categoryFilterDeselected:
+            return "category_filter_deselected"
         }
     }
 
