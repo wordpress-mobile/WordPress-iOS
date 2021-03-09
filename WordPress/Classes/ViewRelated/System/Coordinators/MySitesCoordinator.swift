67b8582195e7809d494a40feac827fc1c54a5801
James Frost <james.frost@automattic.com>
2021-03-09 16:41:29 +0000

    Hound fixes

--- WordPress/Classes/ViewRelated/System/Coordinators/MySitesCoordinator.swift
+++ WordPress/Classes/ViewRelated/System/Coordinators/MySitesCoordinator.swift
@@ -117,12 +117,12 @@ class MySitesCoordinator: NSObject {
     func showActivityLog(for blog: Blog) {
         showBlogDetails(for: blog, then: .activity)
     }
-    
+
     // MARK: - Adding a new site
     @objc
     func showAddNewSite(from view: UIView) {
         showMySites()
-        
+
         blogListViewController.presentInterfaceForAddingNewSite(from: view)
     }
 
