8a1af271e939a024de148f6aae27516e4108052c
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2021-03-24 18:00:26 -0500

    Add assets for prologue editor page

---
+++ WordPress/Classes/ViewRelated/NUX/ContentViews/UnifiedPrologueEditorContentView.swift
@@ -0,0 +1,9 @@
+//
+//  UnifiedPrologueEditorContentView.swift
+//  WordPress
+//
+//  Created by Giorgio Ruscigno on 3/24/21.
+//  Copyright © 2021 WordPress. All rights reserved.
+//
+
+import Foundation
