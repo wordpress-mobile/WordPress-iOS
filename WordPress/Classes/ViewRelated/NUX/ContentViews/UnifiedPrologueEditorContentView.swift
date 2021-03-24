50aa194588b48d500885e35f9566b4a9470f84cc
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2021-03-24 18:01:57 -0500

    Add SwiftUI view for prologue editor page

--- WordPress/Classes/ViewRelated/NUX/ContentViews/UnifiedPrologueEditorContentView.swift
+++ WordPress/Classes/ViewRelated/NUX/ContentViews/UnifiedPrologueEditorContentView.swift
@@ -1,9 +1,87 @@
-//
-//  UnifiedPrologueEditorContentView.swift
-//  WordPress
-//
-//  Created by Giorgio Ruscigno on 3/24/21.
-//  Copyright © 2021 WordPress. All rights reserved.
-//
+import UIKit
+import SwiftUI
 
-import Foundation
+class UnifiedPrologueEditorContentView: UIView {
+
+    init() {
+        super.init(frame: .zero)
+        let controller = UIHostingController(rootView: UnifiedPrologueEditorView())
+        controller.view.translatesAutoresizingMaskIntoConstraints = false
+        controller.view.backgroundColor = .clear
+        addSubview(controller.view)
+        pinSubviewToAllEdges(controller.view)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+struct UnifiedPrologueEditorView: View {
+
+    var body: some View {
+
+        VStack {
+            Spacer()
+            ZStack {
+                RoundedRectangle(cornerRadius: 4, style: .continuous)
+                    .foregroundColor(Appearance.textBackgroundColor)
+                Text(Appearance.topElementTitle)
+                    .font(Appearance.largeTextFont)
+            }
+            .fixedSize(horizontal: false, vertical: true)
+            Spacer(minLength: 16)
+                .fixedSize(horizontal: false, vertical: true)
+            ZStack {
+                RoundedRectangle(cornerRadius: 4, style: .continuous)
+                    .foregroundColor(Appearance.textBackgroundColor)
+                (Text(Appearance.middleElementTitle)
+                    + Text(Appearance.middleElementTerminator)
+                        .foregroundColor(.blue))
+                    .lineLimit(.none)
+                    .padding()
+
+            }
+            .fixedSize(horizontal: false, vertical: true)
+            Spacer(minLength: 16)
+                .fixedSize(horizontal: false, vertical: true)
+            ZStack {
+                RoundedRectangle(cornerRadius: 4, style: .continuous)
+                    .foregroundColor(Appearance.textBackgroundColor)
+                HStack {
+                    Image("page2Img1Sea")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    Image("page2Img2Trees")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    Image("page2Img3Food")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                }
+                .padding(.all, 8)
+            }
+            .fixedSize(horizontal: false, vertical: true)
+            Spacer()
+        }
+    }
+}
+
+private extension UnifiedPrologueEditorView {
+
+    enum Appearance {
+        static let topElementTitle: LocalizedStringKey = "Getting Inspired"
+        static let middleElementShortTitle: LocalizedStringKey = "I am so inspired by photographer Cameron Karsten's work. I will be trying these techniques on my"
+        static let middleElementTerminator = "|"
+        static let textBackgroundColor = Color(UIColor(light: .white, dark: .gray(.shade90)))
+        static let largeTextFont = Font(WPStyleGuide.serifFontForTextStyle(.title1))
+        static let normalTextFont = Font.subheadline
+        /// - TODO: This needs to be updated with actual text
+        static let middleElementLongTitle: LocalizedStringKey = "I am so inspired by photographer Cameron Karsten's work. I will be trying these techniques on my I am so inspired by photographer Cameron Karsten's work. I will be trying these techniques on my I am so inspired by photographer Cameron Karsten's work. I will be trying these techniques on my"
+
+        static var middleElementTitle: LocalizedStringKey {
+            WPDeviceIdentification.isiPad() ? middleElementLongTitle : middleElementShortTitle
+        }
+    }
+}
