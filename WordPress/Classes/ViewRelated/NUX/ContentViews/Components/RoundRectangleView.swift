c2c5618f2d3347b6f1bb0cc1ab66f7c5f3608d30
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2021-03-25 16:23:49 -0500

    Add alignment property to RoundRectangleView

--- WordPress/Classes/ViewRelated/NUX/ContentViews/Components/RoundRectangleView.swift
+++ WordPress/Classes/ViewRelated/NUX/ContentViews/Components/RoundRectangleView.swift
@@ -7,12 +7,15 @@ struct RoundRectangleView<Content: View>: View {
     private let rectangleFillColor = Color(UIColor(light: .white, dark: .gray(.shade90)))
     private let cornerRadius: CGFloat = 4
 
-    init(@ViewBuilder content: @escaping () -> Content) {
+    private var alignment: Alignment
+
+    init(alignment: Alignment = .center, @ViewBuilder content: @escaping () -> Content) {
+        self.alignment = alignment
         self.content = content()
     }
 
     var body: some View {
-        ZStack {
+        ZStack(alignment: alignment) {
             RoundedRectangle(cornerRadius: cornerRadius, style: .continuous)
                 .foregroundColor(rectangleFillColor)
             content
