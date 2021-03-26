a2910f743e0933f54a7f70208ef1f5cfcef7e3e2
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2021-03-26 13:31:51 -0500

    Remove unused parameters from UnifiedPrologueEditorContentView

--- WordPress/Classes/ViewRelated/NUX/ContentViews/Editor/UnifiedPrologueEditorContentView.swift
+++ WordPress/Classes/ViewRelated/NUX/ContentViews/Editor/UnifiedPrologueEditorContentView.swift
@@ -5,8 +5,10 @@ struct UnifiedPrologueEditorContentView: View {
 
     var body: some View {
         GeometryReader { content in
+
             VStack {
                 Spacer(minLength: content.size.height * 0.18)
+
                 RoundRectangleView {
                     HStack {
                         Text(Appearance.topElementTitle)
@@ -21,6 +23,7 @@ struct UnifiedPrologueEditorContentView: View {
 
                 Spacer(minLength: content.size.height * 0.03)
                     .fixedSize(horizontal: false, vertical: true)
+
                 RoundRectangleView(alignment: .top) {
                     (Text(Appearance.middleElementTitle)
                         + Text(Appearance.middleElementTerminator)
@@ -31,6 +34,7 @@ struct UnifiedPrologueEditorContentView: View {
                         .fixedSize(horizontal: false, vertical: true)
                         .lineLimit(.none)
                         .padding(.all, content.size.height * 0.06)
+
                     HStack {
                         let alignImageLeftIconSize = content.size.height * 0.15
                         CircledIcon(size: alignImageLeftIconSize,
@@ -38,7 +42,9 @@ struct UnifiedPrologueEditorContentView: View {
                                     yOffset: alignImageLeftIconSize  * 3 / 4,
                                     iconType: .alignImageLeft,
                                     backgroundColor: Color(UIColor.muriel(name: .purple, .shade50)))
+
                         Spacer()
+
                         let plusIconSize = content.size.height * 0.2
                         CircledIcon(size: plusIconSize,
                                     xOffset: plusIconSize * 2 / 3,
@@ -47,19 +53,22 @@ struct UnifiedPrologueEditorContentView: View {
                                     backgroundColor: Color(UIColor.muriel(name: .blue, .shade50)))
                     }
                 }
-
                 .fixedSize(horizontal: false, vertical: true)
+
                 Spacer(minLength: content.size.height * 0.03)
                     .fixedSize(horizontal: false, vertical: true)
+
                 RoundRectangleView {
                     HStack(spacing: content.size.height * 0.03) {
                         Image("page2Img1Sea")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
+
                         ZStack(alignment: .bottomLeading) {
                             Image("page2Img2Trees")
                                 .resizable()
                                 .aspectRatio(contentMode: .fit)
+
                             let imageMultipleIconSize = content.size.height * 0.18
                             CircledIcon(size: imageMultipleIconSize,
                                         xOffset: -imageMultipleIconSize / 2,
@@ -67,14 +76,15 @@ struct UnifiedPrologueEditorContentView: View {
                                         iconType: .imageMultiple,
                                         backgroundColor: Color(UIColor.muriel(name: .pink, .shade40)))
                         }
+
                         Image("page2Img3Food")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                     }
                     .padding(.all, content.size.height * 0.03)
                 }
-
                 .fixedSize(horizontal: false, vertical: true)
+
                 // avoid bottom overlapping due to the icon offset
                 Spacer(minLength: content.size.height * 0.1)
             }
@@ -86,35 +96,7 @@ private extension UnifiedPrologueEditorContentView {
 
     enum Appearance {
         static let topElementTitle: LocalizedStringKey = "Getting Inspired"
-        static let middleElementShortTitle: LocalizedStringKey = "I am so inspired by photographer Cameron Karsten's work. I will be trying these techniques on my next"
+        static let middleElementTitle: LocalizedStringKey = "I am so inspired by photographer Cameron Karsten's work. I will be trying these techniques on my next"
         static let middleElementTerminator = "|"
-        static let largeTextFont = Font.system(size: 32, weight: .semibold, design: .serif)
-        static let normalTextFont = Font.system(size: 15, weight: .regular, design: .default)
-        /// - TODO: This needs to be updated with actual text
-        static let middleElementLongTitle: LocalizedStringKey = "I am so inspired by photographer Cameron Karsten's work. I will be trying these techniques on my I am so inspired by photographer Cameron Karsten's work. I will be trying these techniques on my I am so inspired by photographer Cameron Karsten's work. I will be trying these techniques on my"
-
-        static var middleElementTitle: LocalizedStringKey {
-            //            WPDeviceIdentification.isiPad() ?
-            //                middleElementLongTitle :
-            middleElementShortTitle
-        }
-
-        // Spacing
-        static let internalSpacerMinHeight: CGFloat = 8.0
-        static let imageGalleryPadding: CGFloat = 8.0
-        static let topSpacing: CGFloat = 60.0
-
-        // Icons
-        static let plusIconSize: CGFloat = 52.0
-        static let plusIconXOffset = plusIconSize * 2 / 3
-        static let plusIconYOffset = -plusIconSize * 2 / 3
-
-        static let alignImageLeftIconSize: CGFloat = 40.0
-        static let alignImageLeftIconXOffset = -alignImageLeftIconSize * 3 / 4
-        static let alignImageLeftIconYOffset = alignImageLeftIconSize
-
-        static let imageMultipleIconSize: CGFloat = 48.0
-        static let imageMultipleIconXOffset = -imageMultipleIconSize / 2
-        static let imageMultipleIconYOffset = imageMultipleIconSize / 2
     }
 }
