e7ab1b0d861866ca57f9b8d2d9ded0315ebbde14
Jeremy Massel <jeremy.massel@automattic.com>
2021-03-25 23:44:40 -0600

    Remove AccountService from CommentService

--- WordPress/Classes/Services/CommentService.m
+++ WordPress/Classes/Services/CommentService.m
@@ -887,8 +887,8 @@ static NSTimeInterval const CommentsRefreshTimeoutInSeconds = 60 * 5; // 5 minut
     // In theory a sync could include a newly created comment before the request that created it returned.
     Comment *comment = [NSEntityDescription insertNewObjectForEntityForName:NSStringFromClass([Comment class]) inManagedObjectContext:self.managedObjectContext];
 
-    AccountService *service = [[AccountService alloc] initWithManagedObjectContext:self.managedObjectContext];
-    comment.author = [[service defaultWordPressComAccount] username];
+    WPAccount *account = [WPAccount lookupDefaultWordPressComAccountInContext:self.managedObjectContext];
+    comment.author = [account username];
     comment.content = content;
     comment.dateCreated = [NSDate date];
     comment.parentID = parentID;
@@ -1135,8 +1135,7 @@ static NSTimeInterval const CommentsRefreshTimeoutInSeconds = 60 * 5; // 5 minut
  */
 - (WordPressComRestApi *)apiForRESTRequest
 {
-    AccountService *accountService = [[AccountService alloc] initWithManagedObjectContext:self.managedObjectContext];
-    WPAccount *defaultAccount = [accountService defaultWordPressComAccount];
+    WPAccount *defaultAccount = [WPAccount lookupDefaultWordPressComAccountInContext:self.managedObjectContext];
     WordPressComRestApi *api = [defaultAccount wordPressComRestApi];
     //Sergio Estevao: Do we really want to do this? If the call going to be valid if no credential is available?
     if (![api hasCredentials]) {
