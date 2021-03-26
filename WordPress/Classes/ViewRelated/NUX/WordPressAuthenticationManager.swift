ad333d832b267d0c4a7197c74e520b6a7b9908e7
Jeremy Massel <jeremy.massel@automattic.com>
2021-03-25 23:32:37 -0600

    Remove AccountService from WordPressAuthenticationManager
    Adjust WordPressAuthenticationManager

--- WordPress/Classes/ViewRelated/NUX/WordPressAuthenticationManager.swift
+++ WordPress/Classes/ViewRelated/NUX/WordPressAuthenticationManager.swift
@@ -115,8 +115,7 @@ extension WordPressAuthenticationManager {
     @objc
     class func signinForWPComFixingAuthToken(_ onDismissed: ((_ cancelled: Bool) -> Void)? = nil) -> UIViewController {
         let context = ContextManager.sharedInstance().mainContext
-        let service = AccountService(managedObjectContext: context)
-        let account = service.defaultWordPressComAccount()
+        let account = try? WPAccount.lookupDefaultWordPressComAccount(in: context)
 
         return WordPressAuthenticator.signinForWPCom(dotcomEmailAddress: account?.email, dotcomUsername: account?.username, onDismissed: onDismissed)
     }
@@ -203,8 +202,7 @@ extension WordPressAuthenticationManager: WordPressAuthenticatorDelegate {
 
     /// We allow to connect with WordPress.com account only if there is no default account connected already.
     var allowWPComLogin: Bool {
-        let accountService = AccountService(managedObjectContext: ContextManager.shared.mainContext)
-        return accountService.defaultWordPressComAccount() == nil
+        (try? WPAccount.lookupDefaultWordPressComAccount(in: ContextManager.shared.mainContext)) == nil
     }
 
     /// Returns an instance of a SupportView, configured to be displayed from a specified Support Source.
@@ -317,8 +315,7 @@ extension WordPressAuthenticationManager: WordPressAuthenticatorDelegate {
         }
 
         let context = ContextManager.sharedInstance().mainContext
-        let service = AccountService(managedObjectContext: context)
-        let numberOfBlogs = service.defaultWordPressComAccount()?.blogs?.count ?? 0
+        let numberOfBlogs = (try? WPAccount.lookupDefaultWordPressComAccount(in: context))?.blogs?.count ?? 0
 
         return numberOfBlogs > 0
     }
@@ -338,9 +335,7 @@ extension WordPressAuthenticationManager: WordPressAuthenticatorDelegate {
         let service = AccountService(managedObjectContext: context)
 
         let account = service.createOrUpdateAccount(withUsername: username, authToken: authToken)
-        if service.defaultWordPressComAccount() == nil {
-            service.setDefaultWordPressComAccount(account)
-        }
+        service.setDefaultWordPressComAccount(account)
     }
 
     /// When an Apple account is used during the Auth flow, save the Apple user id to the keychain.
