a7d890b7d10c7d9f5b1e1b5d33edb661a7bd2364
Jeremy Massel <jeremy.massel@automattic.com>
2021-03-25 23:44:01 -0600

    Remove AccountService from PushAuthenticationService

--- WordPress/Classes/Services/PushAuthenticationService.swift
+++ WordPress/Classes/Services/PushAuthenticationService.swift
@@ -47,8 +47,7 @@ import Foundation
 
         var api: WordPressComRestApi? = nil
 
-        let accountService = AccountService(managedObjectContext: managedObjectContext)
-        if let unwrappedRestApi = accountService.defaultWordPressComAccount()?.wordPressComRestApi {
+        if let unwrappedRestApi = (try? WPAccount.lookupDefaultWordPressComAccount(in: managedObjectContext))?.wordPressComRestApi {
             if unwrappedRestApi.hasCredentials() {
                 api = unwrappedRestApi
             }
