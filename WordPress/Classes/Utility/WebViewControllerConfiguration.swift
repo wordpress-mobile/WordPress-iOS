8dcd0b58f1a8b23a17e890777a40f50edae9f362
Jeremy Massel <jeremy.massel@automattic.com>
2021-03-25 23:40:02 -0600

    Remove AccountService from WebViewControllerConfiguration

--- WordPress/Classes/Utility/WebViewControllerConfiguration.swift
+++ WordPress/Classes/Utility/WebViewControllerConfiguration.swift
@@ -31,9 +31,7 @@ class WebViewControllerConfiguration: NSObject {
     }
 
     @objc func authenticateWithDefaultAccount() {
-        let context = ContextManager.sharedInstance().mainContext
-        let service = AccountService(managedObjectContext: context)
-        guard let account = service.defaultWordPressComAccount() else {
+        guard let account = try? WPAccount.lookupDefaultWordPressComAccount(in: ContextManager.shared.mainContext) else {
             return
         }
         authenticate(account: account)
