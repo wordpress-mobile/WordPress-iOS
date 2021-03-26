95f8499e95e9406554abf2528201830b1e2c9a55
Jeremy Massel <jeremy.massel@automattic.com>
2021-03-25 23:38:57 -0600

    Remove AccountService from BlogListViewController

--- WordPress/Classes/ViewRelated/Blog/Blog List/BlogListViewController.m
+++ WordPress/Classes/ViewRelated/Blog/Blog List/BlogListViewController.m
@@ -389,8 +389,7 @@ static NSInteger HideSearchMinSites = 3;
     }
 
     NSManagedObjectContext *context = [[ContextManager sharedInstance] mainContext];
-    AccountService *accountService = [[AccountService alloc] initWithManagedObjectContext:context];
-    WPAccount *defaultAccount = [accountService defaultWordPressComAccount];
+    WPAccount *defaultAccount = [WPAccount lookupDefaultWordPressComAccountInContext:context];
 
     if (!defaultAccount) {
         [self handleSyncEnded];
@@ -986,8 +985,7 @@ static NSInteger HideSearchMinSites = 3;
 - (WPAccount *)defaultWordPressComAccount
 {
     NSManagedObjectContext *context = [[ContextManager sharedInstance] mainContext];
-    AccountService *accountService = [[AccountService alloc] initWithManagedObjectContext:context];
-    return [accountService defaultWordPressComAccount];
+    return [WPAccount lookupDefaultWordPressComAccountInContext:context];
 }
 
 - (void)showLoginControllerForAddingSelfHostedSite
@@ -1023,7 +1021,7 @@ static NSInteger HideSearchMinSites = 3;
                                                                      NSManagedObjectContext *context = [[ContextManager sharedInstance] newDerivedContext];
                                                                      [context performBlock:^{
                                                                          AccountService *accountService = [[AccountService alloc] initWithManagedObjectContext:context];
-                                                                         WPAccount *account = [accountService defaultWordPressComAccount];
+                                                                         WPAccount *account = [WPAccount lookupDefaultWordPressComAccountInContext:context];
                                                                          [accountService setVisibility:visible forBlogs:[account.blogs allObjects]];
                                                                          [[ContextManager sharedInstance] saveContext:context];
                                                                      }];
