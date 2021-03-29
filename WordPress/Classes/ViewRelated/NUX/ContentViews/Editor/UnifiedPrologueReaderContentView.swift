69ce422cbae83b53e9aa19c89c565f30b7d8d953
James Frost <james.frost@automattic.com>
2021-03-29 21:59:48 +0100

    Prologue Reader content view: Refactor and extract subviews

--- WordPress/Classes/ViewRelated/NUX/ContentViews/Editor/UnifiedPrologueReaderContentView.swift
+++ WordPress/Classes/ViewRelated/NUX/ContentViews/Editor/UnifiedPrologueReaderContentView.swift
@@ -7,13 +7,12 @@ struct UnifiedPrologueReaderContentView: View {
             let spacingUnit = content.size.height * 0.06
             let feedSize = content.size.height * 0.1
 
-            let notificationIconSize = content.size.height * 0.2
-            let smallIconSize = content.size.height * 0.15
-            let largerIconSize = content.size.height * 0.2
+            let smallIconSize = content.size.height * 0.175
+            let largerIconSize = content.size.height * 0.25
+
             let fontSize = content.size.height * 0.05
-            let notificationFont = Font.system(size: fontSize,
-                                               weight: .regular,
-                                               design: .default)
+            let smallFontSize = content.size.height * 0.045
+            let smallFont = Font.system(size: smallFontSize, weight: .regular, design: .default)
             let feedFont = Font.system(size: fontSize,
                                        weight: .regular,
                                        design: .default)
@@ -24,37 +23,31 @@ struct UnifiedPrologueReaderContentView: View {
                 RoundRectangleView {
                     HStack {
                         VStack {
-                            Feed(image: Appearance.feedTopImage,
+                            FeedRow(image: Appearance.feedTopImage,
                                  imageSize: feedSize,
-                                 title: Appearance.feedTopTitle,
+                                 title: Strings.feedTopTitle,
                                  font: feedFont)
-                            Feed(image: Appearance.feedMiddleImage,
+                            FeedRow(image: Appearance.feedMiddleImage,
                                  imageSize: feedSize,
-                                 title: Appearance.feedMiddleTitle,
+                                 title: Strings.feedMiddleTitle,
                                  font: feedFont)
-                            Feed(image: Appearance.feedBottomImage,
+                            FeedRow(image: Appearance.feedBottomImage,
                                  imageSize: feedSize,
-                                 title: Appearance.feedBottomTitle,
+                                 title: Strings.feedBottomTitle,
                                  font: feedFont)
                         }
                     }
                     .padding(spacingUnit / 2)
 
-//                    HStack {
-//                        CircledIcon(size: smallIconSize,
-//                                    xOffset: -smallIconSize * 0.75,
-//                                    yOffset: smallIconSize  * 0.75,
-//                                    iconType: .reply,
-//                                    backgroundColor: Color(UIColor.muriel(name: .celadon, .shade30)))
-//
-//                        Spacer()
-//
-//                        CircledIcon(size: smallIconSize,
-//                                    xOffset: smallIconSize * 0.25,
-//                                    yOffset: -smallIconSize  * 0.75,
-//                                    iconType: .star,
-//                                    backgroundColor: Color(UIColor.muriel(name: .yellow, .shade20)))
-//                    }
+                    HStack {
+                        Spacer()
+
+                        CircledIcon(size: largerIconSize,
+                                    xOffset: largerIconSize * 0.7,
+                                    yOffset: -largerIconSize * 0.05,
+                                    iconType: .readerFollow,
+                                    backgroundColor: Color(UIColor.muriel(name: .red, .shade40)))
+                    }
                 }
                 .fixedSize(horizontal: false, vertical: true)
                 .padding(.horizontal, spacingUnit)
@@ -66,84 +59,60 @@ struct UnifiedPrologueReaderContentView: View {
                     ScrollView(.horizontal) {
                         VStack(alignment: .leading) {
                             HStack {
-                                ForEach(["Art", "Cooking", "Football"], id: \.self) { item in
+                                ForEach([Strings.tagArt, Strings.tagCooking, Strings.tagFootball], id: \.self) { item in
                                     Text(item)
-                                        .font(notificationFont)
-                                        .lineLimit(1)
-                                        .truncationMode(.tail)
-                                        .padding(.horizontal, spacingUnit)
-                                        .padding(.vertical, spacingUnit * 0.4)
-                                        .background(Color(UIColor.muriel(name: .gray, .shade0)))
-                                        .clipShape(Capsule())
+                                        .tagItemStyle(with: smallFont, horizontalPadding: spacingUnit, verticalPadding: spacingUnit * 0.25)
                                 }
                             }
                             HStack {
-                                ForEach(["Gardening", "Music", "Politics"], id: \.self) { item in
+                                ForEach([Strings.tagGardening, Strings.tagMusic, Strings.tagPolitics], id: \.self) { item in
                                     Text(item)
-                                        .font(notificationFont)
-                                        .lineLimit(1)
-                                        .truncationMode(.tail)
-                                        .padding(.horizontal, spacingUnit)
-                                        .padding(.vertical, spacingUnit * 0.4)
-                                        .background(Color(UIColor.muriel(name: .gray, .shade0)))
-                                        .clipShape(Capsule())
+                                        .tagItemStyle(with: smallFont, horizontalPadding: spacingUnit, verticalPadding: spacingUnit * 0.25)
                                 }
                             }
                         }
                         .padding(spacingUnit / 2)
                     }
                     .disabled(true)
+
+                    HStack {
+                        CircledIcon(size: smallIconSize,
+                                    xOffset: -smallIconSize * 0.7,
+                                    yOffset: -smallIconSize  * 0.7,
+                                    iconType: .star,
+                                    backgroundColor: Color(UIColor.muriel(name: .yellow, .shade20)))
+
+                        Spacer()
+                    }
                 }
                 .fixedSize(horizontal: false, vertical: true)
 
                 Spacer(minLength: spacingUnit / 2)
                     .fixedSize(horizontal: false, vertical: true)
 
+                let postWidth = content.size.width * 0.3
+
                 RoundRectangleView {
                     ScrollView(.horizontal) {
                         HStack {
-                            VStack {
-                                Image("page5Img1Coffee")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                Text("My Top Ten Cafes")
-                                    .lineLimit(2)
-                                    .font(notificationFont)
-                            }
-                            .frame(width: content.size.width * 0.3)
-                            VStack {
-                                Image("page5Img2Stadium")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                Text("The World's Best Fans")
-                                    .lineLimit(2)
-                                    .font(notificationFont)
-                            }
-                            .frame(width: content.size.width * 0.3)
-                            VStack {
-                                Image("page5Img3Museum")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                Text("Museums to See In London")
-                                    .lineLimit(2)
-                                    .font(notificationFont)
-                            }
-                            .frame(width: content.size.width * 0.3)
+                            PostView(image: Appearance.firstPostImage, title: Strings.firstPostTitle, size: postWidth, font: smallFont)
+                            PostView(image: Appearance.secondPostImage, title: Strings.secondPostTitle, size: postWidth, font: smallFont)
+                            PostView(image: Appearance.thirdPostImage, title: Strings.thirdPostTitle, size: postWidth, font: smallFont)
                         }
                         .padding(spacingUnit / 2)
 
                     }
                     .disabled(true)
 
-//                    HStack {
-//                        Spacer()
-//
-//                        CircledIcon(size: largerIconSize,
-//                                    xOffset: largerIconSize * 0.6,
-//                                    yOffset: largerIconSize  * 0.3,
-//                                    iconType: .comment,
-//                                    backgroundColor: Color(UIColor.muriel(name: .blue, .shade50)))
-//                    }
+                    HStack {
+                        Spacer()
+
+                        CircledIcon(size: smallIconSize,
+                                    xOffset: smallIconSize * 0.75,
+                                    yOffset: -smallIconSize * 0.85,
+                                    iconType: .bookmarkOutline,
+                                    backgroundColor: Color(UIColor.muriel(name: .purple, .shade50)))
+                    }
                 }
                 .fixedSize(horizontal: false, vertical: true)
                 .padding(.horizontal, spacingUnit)
@@ -155,7 +124,40 @@ struct UnifiedPrologueReaderContentView: View {
     }
 }
 
-private struct Feed: View {
+private extension UnifiedPrologueReaderContentView {
+    enum Appearance {
+        static let feedTopImage = "page5Avatar1"
+        static let feedMiddleImage = "page5Avatar2"
+        static let feedBottomImage = "page5Avatar3"
+
+        static let firstPostImage = "page5Img1Coffee"
+        static let secondPostImage = "page5Img2Stadium"
+        static let thirdPostImage = "page5Img3Museum"
+    }
+
+    enum Strings {
+        static let feedTopTitle: String = "Pamela Nguyen"
+        static let feedMiddleTitle: String = NSLocalizedString("Web News", comment: "Example Reader feed title")
+        static let feedBottomTitle: String = NSLocalizedString("Rock 'n Roll Weekly", comment: "Example Reader feed title")
+
+        static let tagArt: String = NSLocalizedString("Art", comment: "An example tag used in the login prologue screens.")
+        static let tagCooking: String = NSLocalizedString("Cooking", comment: "An example tag used in the login prologue screens.")
+        static let tagFootball: String = NSLocalizedString("Football", comment: "An example tag used in the login prologue screens.")
+        static let tagGardening: String = NSLocalizedString("Gardening", comment: "An example tag used in the login prologue screens.")
+        static let tagMusic: String = NSLocalizedString("Music", comment: "An example tag used in the login prologue screens.")
+        static let tagPolitics: String = NSLocalizedString("Politics", comment: "An example tag used in the login prologue screens.")
+
+        static let firstPostTitle: LocalizedStringKey = "My Top Ten Cafes"
+        static let secondPostTitle: LocalizedStringKey = "The World's Best Fans"
+        static let thirdPostTitle: LocalizedStringKey = "Museums to See In London"
+    }
+}
+
+// MARK: - Views
+
+/// A view showing an icon followed by a title for an example feed.
+///
+private struct FeedRow: View {
     let image: String
     let imageSize: CGFloat
     let title: String
@@ -177,14 +179,48 @@ private struct Feed: View {
     }
 }
 
-private extension UnifiedPrologueReaderContentView {
-    enum Appearance {
-        static let feedTopImage = "page5Avatar1"
-        static let feedMiddleImage = "page5Avatar2"
-        static let feedBottomImage = "page5Avatar3"
+/// A view modifier that applies style to a text to make it look like a tag token.
+///
+private struct TagItem: ViewModifier {
+    let font: Font
+    let horizontalPadding: CGFloat
+    let verticalPadding: CGFloat
 
-        static let feedTopTitle: String = "Pamela Nguyen"
-        static let feedMiddleTitle: String = "Web News"
-        static let feedBottomTitle: String = "Rock 'n Roll Weekly"
+    func body(content: Content) -> some View {
+        content
+            .font(font)
+            .lineLimit(1)
+            .truncationMode(.tail)
+            .padding(.horizontal, horizontalPadding)
+            .padding(.vertical, verticalPadding)
+            .background(Color(UIColor.muriel(name: .gray, .shade0)))
+            .clipShape(Capsule())
+    }
+}
+
+extension View {
+    func tagItemStyle(with font: Font, horizontalPadding: CGFloat, verticalPadding: CGFloat) -> some View {
+        self.modifier(TagItem(font: font, horizontalPadding: horizontalPadding, verticalPadding: verticalPadding))
+    }
+}
+
+/// A view showing an image with title below for an example post.
+///
+private struct PostView: View {
+    let image: String
+    let title: LocalizedStringKey
+    let size: CGFloat
+    let font: Font
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Image(image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Text(title)
+                .lineLimit(2)
+                .font(font)
+        }
+        .frame(width: size)
     }
 }
