f75b8b440e3abdff04f1e995194e51d4715748ea
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2021-03-29 14:10:08 -0500

    Add assets for unified prologue analytics page - Also add new UnifiedPrologueStatsContentView

---
+++ WordPress/Classes/ViewRelated/NUX/ContentViews/Stats/UnifiedPrologueStatsContentView.swift
@@ -0,0 +1,9 @@
+//
+//  UnifiedPrologueStatsContentView.swift
+//  WordPress
+//
+//  Created by Giorgio Ruscigno on 3/29/21.
+//  Copyright © 2021 WordPress. All rights reserved.
+//
+
+import Foundation
