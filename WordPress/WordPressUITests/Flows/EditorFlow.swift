82ecea5d43f7ead050309e78eb2a4b07e883a317
Cameron Voell <cameronvoell@gmail.com>
2021-04-01 16:11:59 -0700

    Disable test for adding new aztec post. Fix Gutenberg test.

--- WordPress/WordPressUITests/Flows/EditorFlow.swift
+++ WordPress/WordPressUITests/Flows/EditorFlow.swift
@@ -7,6 +7,10 @@ class EditorFlow {
         }
     }
 
+    static func goToMySiteScreen() -> MySiteScreen {
+        return TabNavComponent().gotoMySiteScreen()
+    }
+
     static func toggleBlockEditor(to state: SiteSettingsScreen.Toggle) -> SiteSettingsScreen {
         if !SiteSettingsScreen.isLoaded() {
             _ = TabNavComponent()
