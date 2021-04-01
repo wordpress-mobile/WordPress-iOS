e7fcf6ae79728e1af0da86c73baf3f30c6c16b07
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2021-04-01 16:45:48 -0500

    Add UnifiedPrologueBackgroundView - a SwiftUI view with partial circles to be used as background for unified prologue

---
+++ WordPress/Classes/ViewRelated/NUX/ContentViews/Background/UnifiedPrologueBackgroundView.swift
@@ -0,0 +1,9 @@
+//
+//  UnifiedPrologueBackgroundView.swift
+//  WordPress
+//
+//  Created by Giorgio Ruscigno on 3/31/21.
+//  Copyright © 2021 WordPress. All rights reserved.
+//
+
+import Foundation
