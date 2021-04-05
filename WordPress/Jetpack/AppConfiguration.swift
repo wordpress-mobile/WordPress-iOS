3b326b3d6aadd7792e65da33a76c103595a50066
Leandro Alonso <contato@leandroalonso.com>
2021-04-05 15:37:05 -0300

    Fix: does not allow site creation for Jetpack
    Co-authored-by: Emily Laguna <emily.laguna@automattic.com>

--- WordPress/Jetpack/AppConfiguration.swift
+++ WordPress/Jetpack/AppConfiguration.swift
@@ -2,5 +2,5 @@ import Foundation
 
 @objc class AppConfiguration: NSObject {
     @objc static let isJetpack: Bool = true
-    @objc static let allowSiteCreation: Bool = true
+    @objc static let allowSiteCreation: Bool = false
 }
