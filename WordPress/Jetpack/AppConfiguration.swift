96e6364be5746ff4f4f11ed01ce6c489f6032b82
Momo Ozawa <momo.ozawa@automattic.com>
2021-04-05 12:33:28 +0900

    Add allowsConnectSite property to app config

--- WordPress/Jetpack/AppConfiguration.swift
+++ WordPress/Jetpack/AppConfiguration.swift
@@ -2,4 +2,5 @@ import Foundation
 
 @objc class AppConfiguration: NSObject, TargetConfiguration {
     @objc static let isJetpack: Bool = true
+    @objc static let allowsConnectSite: Bool = false
 }
