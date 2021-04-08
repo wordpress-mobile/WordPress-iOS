c3fc65dd0634f616e000ca5b540ee16cd04b6456
James Frost <james.frost@automattic.com>
2021-04-08 09:32:19 +0100

    Update blue icons and update AppIconViewController for new assets

--- WordPress/Classes/ViewRelated/Me/App Settings/AppIconViewController.swift
+++ WordPress/Classes/ViewRelated/Me/App Settings/AppIconViewController.swift
@@ -4,20 +4,20 @@ import WordPressShared
 open class AppIconViewController: UITableViewController {
 
     private enum Constants {
-        static let rowHeight: CGFloat = 58.0
+        static let rowHeight: CGFloat = 76.0
         static let cornerRadius: CGFloat = 4.0
         static let iconBorderColor: UIColor? = UITableView().separatorColor
         static let iconBorderWidth: CGFloat = 0.5
 
         static let cellIdentifier = "IconCell"
 
-        static let iconPreviewBaseName = "icon_40pt"
-        static let defaultIconName = "WordPress"
-        static let jetpackIconName = "Jetpack Green"
+        static let iconPreviewBaseName = "icon-app-60x60"
+        static let defaultIconName = "Blue"
 
         static let infoPlistBundleIconsKey = "CFBundleIcons"
         static let infoPlistAlternateIconsKey = "CFBundleAlternateIcons"
         static let infoPlistRequiresBorderKey = "WPRequiresBorder"
+        static let infoPlistLegacyIconKey = "WPLegacyIcon"
     }
 
     private var icons: [String] = []
@@ -73,8 +73,8 @@ open class AppIconViewController: UITableViewController {
     }
 
     private func previewImageName(for icon: String) -> String {
-        let lowered = icon.lowercased().replacingMatches(of: " ", with: "_")
-        return "\(lowered)_\(Constants.iconPreviewBaseName)"
+        let lowered = icon.lowercased().replacingMatches(of: " ", with: "-")
+        return "\(lowered)-\(Constants.iconPreviewBaseName)"
     }
 
     open override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -102,26 +102,13 @@ open class AppIconViewController: UITableViewController {
             return
         }
 
-        // Add them (sorted) to the default key – first any prefixed with WordPress, then the rest.
+        // Add them (sorted) to the default key – first any prefixed with Blue, then the rest.
         let keys = Set(iconDict.keys)
-        let wordPressKeys = keys.filter({$0.hasPrefix("WordPress")}).sorted()
-        let otherKeys = keys.subtracting(wordPressKeys).sorted()
-        icons.append(contentsOf: (wordPressKeys + otherKeys))
-
-        // Only show the Jetpack icon if the user has a Jetpack-connected site in the app.
-        if !shouldShowJetpackIcon {
-            icons.removeAll(where: { $0 == Constants.jetpackIconName })
-        }
+        let blueKeys = keys.filter({$0.hasPrefix(Constants.defaultIconName)}).sorted()
+        let otherKeys = keys.subtracting(blueKeys).sorted()
+        icons.append(contentsOf: (blueKeys + otherKeys))
 
         self.icons = icons
-    }
-
-    private var shouldShowJetpackIcon: Bool {
-        let context = ContextManager.shared.mainContext
-        let hasJetpackSite = BlogService(managedObjectContext: context).hasAnyJetpackBlogs()
-        let hasWPComSite = AccountService(managedObjectContext: context).defaultWordPressComAccount() != nil
-
-        return hasJetpackSite || hasWPComSite
     }
 
     private func loadBorderedIcons() {
@@ -149,6 +136,13 @@ open class AppIconViewController: UITableViewController {
                 return nil
         }
 
-        return iconDict
+        // Remove any legacy icons
+        return iconDict.filter { (key, value) -> Bool in
+            if let value = value as? [String: Any] {
+                return value[Constants.infoPlistLegacyIconKey] == nil
+            }
+
+            return true
+        }
     }
 }
