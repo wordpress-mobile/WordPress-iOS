103c65ed57041f0d9577729789606c681c52ca35
Emily Laguna <emily.laguna@automattic.com>
2021-04-08 11:11:15 -0400

    Update AppConfiguration to include new options

--- WordPress/Classes/Utility/App Configuration/AppConfiguration.swift
+++ WordPress/Classes/Utility/App Configuration/AppConfiguration.swift
@@ -1,6 +1,11 @@
 import Foundation
 
+/**
+ * WordPress Configuration
+ */
 @objc class AppConfiguration: NSObject {
     @objc static let isJetpack: Bool = false
     @objc static let allowSiteCreation: Bool = true
+    @objc static let jetpackLogin: Bool = false
+    @objc static let allowSignUp: Bool = true
 }
--- WordPress/Jetpack/AppConfiguration.swift
+++ WordPress/Jetpack/AppConfiguration.swift
@@ -1,6 +1,11 @@
 import Foundation
 
+/**
+ * Jetpack Configuration
+ */
 @objc class AppConfiguration: NSObject {
     @objc static let isJetpack: Bool = true
     @objc static let allowSiteCreation: Bool = false
+    @objc static let jetpackLogin: Bool = true
+    @objc static let allowSignUp: Bool = false
 }
