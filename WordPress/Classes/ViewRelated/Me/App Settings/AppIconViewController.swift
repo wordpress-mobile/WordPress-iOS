7dd741b8dca2502376971597d1af136ad81e7f72
James Frost <james.frost@automattic.com>
2021-04-09 15:16:47 +0100

    Refactor AppIconViewController to use Icon struct and show legacy icons

--- WordPress/Classes/ViewRelated/Me/App Settings/AppIconViewController.swift
+++ WordPress/Classes/ViewRelated/Me/App Settings/AppIconViewController.swift
@@ -3,31 +3,29 @@ import WordPressShared
 
 open class AppIconViewController: UITableViewController {
 
-    private enum Constants {
-        static let rowHeight: CGFloat = 76.0
-        static let cornerRadius: CGFloat = 13.0
-        static let iconBorderColor: UIColor? = UITableView().separatorColor
-        static let iconBorderWidth: CGFloat = 0.5
+    private enum Section: Int {
+        case current
+        // Legacy icons are our v1 custom icons,
+        // which will be removed at some point in the future.
+        case legacy
 
-        static let cellIdentifier = "IconCell"
-
-        static let iconPreviewBaseName = "icon-app-60x60"
-        static let defaultIconName = "Blue"
-
-        static let infoPlistBundleIconsKey = "CFBundleIcons"
-        static let infoPlistAlternateIconsKey = "CFBundleAlternateIcons"
-        static let infoPlistRequiresBorderKey = "WPRequiresBorder"
-        static let infoPlistLegacyIconKey = "WPLegacyIcon"
+        var title: String? {
+            switch self {
+            case .current:
+                return nil
+            case .legacy:
+                return NSLocalizedString("Legacy Icons", comment: "")
+            }
+        }
     }
 
-    private var icons: [String] = []
-    private var borderedIcons: [String] = []
+    private var icons = [[AppIcon]]()
+    private var borderedIcons = [String]()
 
     convenience init() {
         self.init(style: .grouped)
 
         loadIcons()
-        loadBorderedIcons()
     }
 
     open override func viewDidLoad() {
@@ -42,24 +40,31 @@ open class AppIconViewController: UITableViewController {
 
     // MARK: - UITableview Data Source
 
-    open override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    open override func numberOfSections(in tableView: UITableView) -> Int {
         return icons.count
     }
 
+    open override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return Section(rawValue: section)?.title
+    }
+
+    open override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return icons[section].count
+    }
+
     open override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let icon = icons[indexPath.row]
+        let icon = icons[indexPath.section][indexPath.row]
 
         let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath)
 
-        cell.textLabel?.text = icon
+        cell.textLabel?.text = icon.name
 
         if let imageView = cell.imageView {
-            let image = UIImage(named: previewImageName(for: icon))
-            imageView.image = image
+            imageView.image = UIImage(named: icon.imageName)
             imageView.layer.cornerRadius = Constants.cornerRadius
             imageView.layer.masksToBounds = true
             imageView.layer.borderColor = Constants.iconBorderColor?.cgColor
-            imageView.layer.borderWidth = borderedIcons.contains(icon) ? Constants.iconBorderWidth : 0
+            imageView.layer.borderWidth = icon.isBordered ? Constants.iconBorderWidth : 0
             imageView.layer.cornerCurve = .continuous
         }
 
@@ -73,29 +78,17 @@ open class AppIconViewController: UITableViewController {
 
         // If there's no custom icon in use and we're checking the top (default) row
         let isDefaultIconInUse = currentIconName == nil
-        if isDefaultIconInUse && indexPath.row == 0 {
+        if isDefaultIconInUse && indexPath.section == Section.current.rawValue && indexPath.row == 0 {
             return true
         }
 
-        // If the current custom icon is a legacy icon and it matches this row
-        let icon = icons[indexPath.row]
-        if let currentIconName = currentIconName,
-           let legacyName = legacyMappings[currentIconName],
-           legacyName == icon {
-            return true
-        }
-
-        return currentIconName == icon
-    }
-
-    private func previewImageName(for icon: String) -> String {
-        let lowered = icon.lowercased().replacingMatches(of: " ", with: "-")
-        return "\(lowered)-\(Constants.iconPreviewBaseName)"
+        let icon = icons[indexPath.section][indexPath.row]
+        return currentIconName == icon.name
     }
 
     open override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let isOriginalIconRow = (indexPath.row == 0)
-        let icon = isOriginalIconRow ? nil : icons[indexPath.row]
+        let isOriginalIconRow = (indexPath.section == Section.current.rawValue && indexPath.row == 0)
+        let icon = isOriginalIconRow ? nil : icons[indexPath.section][indexPath.row].name
 
         UIApplication.shared.setAlternateIconName(icon, completionHandler: { [weak self] error in
             if error == nil {
@@ -110,64 +103,80 @@ open class AppIconViewController: UITableViewController {
     // MARK: - Private helpers
 
     private func loadIcons() {
-        var icons = [Constants.defaultIconName]
+        let defaultIcon = AppIcon(name: Constants.defaultIconName, isBordered: false, isLegacy: false)
+        let iconDict = [defaultIcon] + infoPlistIconsDict
 
-        // Load the names of the alternative app icons from the info plist
-        guard let iconDict = infoPlistIconsDict else {
-            self.icons = icons
-            return
-        }
+        // Produces a closure which sorts alphabetically, giving priority to items
+        // beginning with the specified prefix.
+        func sortWithPriority(toItemsWithPrefix prefix: String) -> ((AppIcon, AppIcon) -> Bool) {
+            return { (first, second) in
+                let firstIsDefault = first.name.hasPrefix(prefix)
+                let secondIsDefault = second.name.hasPrefix(prefix)
 
-        // Add them (sorted) to the default key – first any prefixed with Blue, then the rest.
-        let keys = Set(iconDict.keys)
-        let blueKeys = keys.filter({$0.hasPrefix(Constants.defaultIconName)}).sorted()
-        let otherKeys = keys.subtracting(blueKeys).sorted()
-        icons.append(contentsOf: (blueKeys + otherKeys))
+                if firstIsDefault && !secondIsDefault {
+                    return true
+                } else if !firstIsDefault && secondIsDefault {
+                    return false
+                }
 
-        self.icons = icons
-    }
-
-    private func loadBorderedIcons() {
-        guard let iconDict = infoPlistIconsDict else {
-            return
-        }
-
-        var icons: [String] = []
-
-        // Find any icons that require a border – they have the `WPRequiresBorder` key set to YES.
-        for (key, value) in iconDict {
-            if let value = value as? [String: Any],
-                let requiresBorder = value[Constants.infoPlistRequiresBorderKey] as? Bool,
-                requiresBorder == true {
-                icons.append(key)
+                return first.name < second.name
             }
         }
 
-        self.borderedIcons = icons
+        // Filter out the current and legacy icon groups, with the Blue icons sorted to the top.
+        let currentIcons = iconDict.filter({ $0.isLegacy == false })
+                                   .sorted(by: sortWithPriority(toItemsWithPrefix: Constants.defaultIconName))
+        let legacyIcons = iconDict.filter({ $0.isLegacy == true })
+                                  .sorted(by: sortWithPriority(toItemsWithPrefix: Constants.defaultLegacyIconName))
+
+        self.icons = [currentIcons, legacyIcons]
     }
 
-    private var infoPlistIconsDict: [String: Any]? {
+    private var infoPlistIconsDict: [AppIcon] {
         guard let bundleDict = Bundle.main.object(forInfoDictionaryKey: Constants.infoPlistBundleIconsKey) as? [String: Any],
             let iconDict = bundleDict[Constants.infoPlistAlternateIconsKey] as? [String: Any] else {
-                return nil
+                return []
         }
 
-        // Remove any legacy icons
-        return iconDict.filter { (key, value) -> Bool in
-            if let value = value as? [String: Any] {
-                return value[Constants.infoPlistLegacyIconKey] == nil
+        return iconDict.compactMap { (key, value) -> AppIcon? in
+            guard let value = value as? [String: Any] else {
+                return nil
             }
 
-            return true
+            let isBordered = value[Constants.infoPlistRequiresBorderKey] as? Bool == true
+            let isLegacy = value[Constants.infoPlistLegacyIconKey] as? Bool == true
+            return AppIcon(name: key, isBordered: isBordered, isLegacy: isLegacy)
         }
     }
 
-    private let legacyMappings: [String: String] = [
-        "WordPress Dark": "Black",
-        "Jetpack Green": "Celadon",
-        "Hot Pink": "Pink",
-        "Open Source": "Black Classic",
-        "Open Source Dark": "Black",
-        "Pride": "Spectrum"
-    ]
+    private enum Constants {
+        static let rowHeight: CGFloat = 76.0
+        static let cornerRadius: CGFloat = 13.0
+        static let iconBorderColor: UIColor? = UITableView().separatorColor
+        static let iconBorderWidth: CGFloat = 0.5
+
+        static let cellIdentifier = "IconCell"
+
+        static let iconPreviewBaseName = "icon-app-60x60"
+        static let defaultIconName = "Blue"
+        static let defaultLegacyIconName = "WordPress"
+
+        static let infoPlistBundleIconsKey = "CFBundleIcons"
+        static let infoPlistAlternateIconsKey = "CFBundleAlternateIcons"
+        static let infoPlistRequiresBorderKey = "WPRequiresBorder"
+        static let infoPlistLegacyIconKey = "WPLegacyIcon"
+    }
+}
+
+private struct AppIcon {
+    let name: String
+    let isBordered: Bool
+    let isLegacy: Bool
+
+    var imageName: String {
+        let lowered = name.lowercased().replacingMatches(of: " ", with: "-")
+        return "\(lowered)-\(AppIcon.imageBaseName)"
+    }
+
+    private static let imageBaseName = "icon-app-60x60"
 }
