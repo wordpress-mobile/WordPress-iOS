1cf96487e30fa84bbd52f59ebd49d33bc580b0bc
Tanner W. Stokes <tanner@tannr.com>
2021-04-13 15:53:40 -0400

    Add the BlogAuthor type to the authors set and introduce an author query function.

--- WordPress/Classes/Models/Blog+BlogAuthors.swift
+++ WordPress/Classes/Models/Blog+BlogAuthors.swift
@@ -3,7 +3,7 @@ import CoreData
 
 
 extension Blog {
-    @NSManaged public var authors: NSSet?
+    @NSManaged public var authors: Set<BlogAuthor>?
 
 
     @objc(addAuthorsObject:)
@@ -17,4 +17,9 @@ extension Blog {
 
     @objc(removeAuthors:)
     @NSManaged public func removeFromAuthors(_ values: NSSet)
+
+    @objc
+    func getAuthorWith(id: NSNumber) -> BlogAuthor? {
+        return authors?.first(where: { $0.userID == id })
+    }
 }
