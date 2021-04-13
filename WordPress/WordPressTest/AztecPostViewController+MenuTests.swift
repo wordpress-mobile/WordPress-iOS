3f16c835cb1c177462a771234728e94310c21465
Paul Von Schrottky <paul.von.schrottky@automattic.com>
2021-04-13 19:18:06 -0400

    Add unit tests for AztecPostViewController menu

---
+++ WordPress/WordPressTest/AztecPostViewController+MenuTests.swift
@@ -0,0 +1,33 @@
+//
+//  AztecPostViewController+MenuTests.swift
+//  WordPressTest
+//
+//  Created by Paul Von Schrottky on 4/13/21.
+//  Copyright © 2021 WordPress. All rights reserved.
+//
+
+import XCTest
+
+class AztecPostViewController_MenuTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
