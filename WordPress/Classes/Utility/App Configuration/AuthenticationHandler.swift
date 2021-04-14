1a5d8da6e215859b3a5948fbba94bf5ceca957ed
Emily Laguna <emily.laguna@automattic.com>
2021-04-14 11:08:45 -0400

    Move AuthenticationHandler to WordPress files

--- WordPress/Classes/Utility/App Configuration/AuthenticationHandler.swift
+++ WordPress/Classes/Utility/App Configuration/AuthenticationHandler.swift
@@ -1,7 +1,6 @@
 import WordPressAuthenticator
 
 protocol AuthenticationHandler {
-
     func shouldPresentUsernamePasswordController(for siteInfo: WordPressComSiteInfo?, onCompletion: @escaping (WordPressAuthenticatorResult) -> Void)
 
     func presentLoginEpilogue(in navigationController: UINavigationController, for credentials: AuthenticatorCredentials, onDismiss: @escaping () -> Void) -> Bool
