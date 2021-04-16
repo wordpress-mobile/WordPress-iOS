2266499b53abe881967948176894c7f4bf6257d0
Stephenie Harris <scoutharris@gmail.com>
2021-04-16 13:20:24 -0600

    Track user profile shown.

--- WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationDetailsViewController.swift
+++ WordPress/Classes/ViewRelated/Notifications/Controllers/NotificationDetailsViewController.swift
@@ -988,6 +988,8 @@ private extension NotificationDetailsViewController {
 
         let sourceView = tableView.cellForRow(at: indexPath) ?? view
         bottomSheet.show(from: self, sourceView: sourceView)
+
+        WPAnalytics.track(.userProfileShown, properties: ["source": "like_notification_list"])
     }
 
 }
