5a1f82c83067649ea17157b8eb71abd13bf509e3
Stephenie Harris <scoutharris@gmail.com>
2021-04-26 15:05:52 -0600

    Add methods to create Core Data objects.

--- WordPress/Classes/Models/Notifications/Likes/LikeUserPreferredBlog+CoreDataClass.swift
+++ WordPress/Classes/Models/Notifications/Likes/LikeUserPreferredBlog+CoreDataClass.swift
@@ -3,4 +3,21 @@ import CoreData
 @objc(LikeUserPreferredBlog)
 public class LikeUserPreferredBlog: NSManagedObject {
 
+    static func createBlogFrom(remoteBlog: RemoteLikeUserPreferredBlog,
+                               forUser user: LikeUser,
+                               context: NSManagedObjectContext) -> LikeUserPreferredBlog? {
+
+        guard let preferredBlog = NSEntityDescription.insertNewObject(forEntityName: "LikeUserPreferredBlog", into: context) as? LikeUserPreferredBlog else {
+            return nil
+        }
+
+        preferredBlog.blogUrl = remoteBlog.blogUrl
+        preferredBlog.blogName = remoteBlog.blogName
+        preferredBlog.iconUrl = remoteBlog.iconUrl
+        preferredBlog.blogID = remoteBlog.blogID?.int64Value ?? 0
+        preferredBlog.user = user
+
+        return preferredBlog
+    }
+
 }
