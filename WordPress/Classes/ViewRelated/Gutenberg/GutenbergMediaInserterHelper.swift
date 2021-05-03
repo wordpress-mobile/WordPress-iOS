696084788d0113be740a5017da4591bb3546a039
Siobhan <siobhan@automattic.com>
2021-05-03 10:36:33 +0100

    Build out notices to send when a new featured image is set
    This commit includes different notices that will be sent to Gutenberg, depending on whether an image is being removed (which will be the case when the media ID is zero) or set.

--- WordPress/Classes/ViewRelated/Gutenberg/GutenbergMediaInserterHelper.swift
+++ WordPress/Classes/ViewRelated/Gutenberg/GutenbergMediaInserterHelper.swift
@@ -186,6 +186,14 @@ class GutenbergMediaInserterHelper: NSObject {
     func setFeaturedImage(mediaID: Int32) {
         let media = Media.existingMediaWith(mediaID: NSNumber(value: mediaID), inBlog: post.blog)
         post.featuredImage = media
+        
+        if mediaID == 0 {
+            gutenberg.showNotice(NSLocalizedString("Removed as Featured Image", comment: "Featured image removed confirmation message"))
+
+        } else {
+            gutenberg.showNotice(NSLocalizedString("Set as Featured Image", comment: "Featured image set confirmation message"))
+        }
+        
         gutenberg.featuredImageIdNativeUpdated(mediaId: mediaID)
     }
 
