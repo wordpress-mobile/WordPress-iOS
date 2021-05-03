89a4aa9c73e0600bda282fbbb26f42c6d9dcf640
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2021-05-03 15:47:29 -0500

    Enable Editor Onboarding features for dev and test environments
    This should be removed once the feature is finished.

--- WordPress/Classes/Utility/Editor/GutenbergOnboardingRollout.swift
+++ WordPress/Classes/Utility/Editor/GutenbergOnboardingRollout.swift
@@ -4,6 +4,6 @@ struct GutenbergOnboardingRollout {
     private let phasePercentage = 0
 
     func isUserIdInPhaseRolloutPercentage(_ userId: Int) -> Bool {
-        return userId % 100 >= (100 - phasePercentage)
+        return userId % 100 >= (100 - phasePercentage) || BuildConfiguration.current ~= [.localDeveloper, .a8cBranchTest]
     }
 }
