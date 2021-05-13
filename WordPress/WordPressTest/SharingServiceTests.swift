8c08b3d8773c67f0d67c347ba7fc8e028c10ddc3
James Frost <james@jameswfrost.co.uk>
2021-05-13 15:55:08 +0100

    Add sharing service test

---
+++ WordPress/WordPressTest/SharingServiceTests.swift
@@ -0,0 +1,31 @@
+import XCTest
+@testable import WordPress
+
+class SharingServiceTests: XCTestCase {
+    var contextManager: TestContextManager!
+    var context: NSManagedObjectContext {
+        return contextManager.mainContext
+    }
+
+    override func setUp() {
+        super.setUp()
+
+        contextManager = TestContextManager()
+    }
+
+    func testSyncingPublicizeConnectionsForNonDotComBlogCallsACompletionBlock() throws {
+        let blogService = BlogService(managedObjectContext: context)
+        let blog = blogService.createBlog()
+        let sharingService = SharingService(managedObjectContext: context)
+
+        let expect = expectation(description: "Sharing service completion block called.")
+
+        sharingService.syncPublicizeConnectionsForBlog(blog) {
+            expect.fulfill()
+        } failure: { (error) in
+            expect.fulfill()
+        }
+
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+}
