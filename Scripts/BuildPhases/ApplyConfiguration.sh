ad747b460663716779e4171707f504a6a1792baf
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2021-05-14 10:12:22 -0600

    Fix paths that have spaces in them

--- Scripts/BuildPhases/ApplyConfiguration.sh
+++ Scripts/BuildPhases/ApplyConfiguration.sh
@@ -15,35 +15,35 @@ mkdir -p "basename $SECRETS_DESTINATION_FILE"
 # If the WordPress Production Secrets are available for WordPress, use them
 if [ -f "$PRODUCTION_SECRETS_FILE" ] && [ "$BUILD_SCHEME" == "WordPress" ]; then
     echo "Applying Production Secrets"
-    cp -v $PRODUCTION_SECRETS_FILE $SECRETS_DESTINATION_FILE
+    cp -v $PRODUCTION_SECRETS_FILE "${SECRETS_DESTINATION_FILE}"
     exit 0
 fi
 
 # If the WordPress Internal Secrets are available, use them
 if [ -f "$INTERNAL_SECRETS_FILE" ] && [ "${BUILD_SCHEME}" == "WordPress Internal" ]; then
     echo "Applying Internal Secrets"
-    cp -v $INTERNAL_SECRETS_FILE $SECRETS_DESTINATION_FILE
+    cp -v $INTERNAL_SECRETS_FILE "${SECRETS_DESTINATION_FILE}"
     exit 0
 fi
 
 # If the WordPress Alpha Secrets are available, use them
 if [ -f "$INTERNAL_SECRETS_FILE" ] && [ "${BUILD_SCHEME}" == "WordPress Alpha" ]; then
     echo "Applying Alpha Secrets"
-    cp -v $ALPHA_SECRETS_FILE $SECRETS_DESTINATION_FILE
+    cp -v $ALPHA_SECRETS_FILE "${SECRETS_DESTINATION_FILE}"
     exit 0
 fi
 
 # If the Jetpack Secrets are available (and if we're building Jetpack) use them
 if [ -f "$JETPACK_SECRETS_FILE" ] && [ "${BUILD_SCHEME}" == "Jetpack" ]; then
     echo "Applying Jetpack Secrets"
-    cp -v $JETPACK_SECRETS_FILE $SECRETS_DESTINATION_FILE
+    cp -v $JETPACK_SECRETS_FILE "${SECRETS_DESTINATION_FILE}"
     exit 0
 fi
 
 # If the developer has a local secrets file, use it
 if [ -f "$LOCAL_SECRETS_FILE" ]; then
     echo "Applying Local Secrets"
-    cp -v $LOCAL_SECRETS_FILE $SECRETS_DESTINATION_FILE
+    cp -v $LOCAL_SECRETS_FILE "${SECRETS_DESTINATION_FILE}"
     exit 0
 fi
 
