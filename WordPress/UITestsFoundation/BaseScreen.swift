aa2204f0379a769904c2a628acb4479c927b48f4
Gio Lodi <giovanni.lodi42@gmail.com>
2021-06-15 12:54:39 +1000

    Move global utils to dedicated file in UITestsFoundation

--- WordPress/UITestsFoundation/BaseScreen.swift
+++ WordPress/UITestsFoundation/BaseScreen.swift
@@ -111,18 +111,3 @@ extension BaseScreen {
         return self
     }
 }
-
-// TODO: This should go into a UIDevice extension (eg: `UIDevice.current.isPad`)
-public var isIpad: Bool {
-    return UIDevice.current.userInterfaceIdiom == .pad
-}
-// TODO: This should go into a UIDevice extension (eg: `UIDevice.current.isPhone`)
-public var isIPhone: Bool {
-    return UIDevice.current.userInterfaceIdiom == .phone
-}
-
-// TODO: This should maybe go in an `XCUIApplication` extension? Also, should it be computed rather
-// than stored as a reference? 🤔
-public let navBackButton = XCUIApplication().navigationBars.element(boundBy: 0).buttons.element(boundBy: 0)
-
-
