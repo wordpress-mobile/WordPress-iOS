ef0556716b40274c2b40ec7f82cde66ee9f7efa7
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2021-06-16 16:29:01 -0500

    Add BloggingRemindersAnimator to improve the transition between screens in the blogging reminders setup flow

---
+++ WordPress/Classes/ViewRelated/Blog/Blogging Reminders/BloggingRemindersAnimator.swift
@@ -0,0 +1,37 @@
+/// A transition animator that moves in the pushed view controller horizontally.
+/// Does not handle the pop animation since the BloggingReminders setup flow does not allow to navigate back.
+class BloggingRemindersAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.2
+    }
+
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+
+        guard let sourceViewController =
+                transitionContext
+                .viewController(forKey: UITransitionContextViewControllerKey.from),
+              let destinationViewController =
+                transitionContext
+                .viewController(forKey: UITransitionContextViewControllerKey.to) else {
+
+            return
+        }
+        // final position of the destination view
+        let endFrame = transitionContext.finalFrame(for: destinationViewController)
+
+        // initial position of the destination view
+        let startFrame = endFrame.offsetBy(dx: endFrame.width, dy: .zero)
+        destinationViewController.view.frame = startFrame
+
+        sourceViewController.view.alpha = .zero
+        transitionContext.containerView.insertSubview(destinationViewController.view, aboveSubview: sourceViewController.view)
+
+        UIView.animate(withDuration: transitionDuration(using: transitionContext),
+                       animations: {
+                        destinationViewController.view.frame = endFrame
+                       }, completion: {_ in
+                        transitionContext.completeTransition(true)
+                       })
+    }
+}
