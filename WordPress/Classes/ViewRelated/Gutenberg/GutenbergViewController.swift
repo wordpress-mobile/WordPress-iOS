998e411936f10df080e335a5b3d7e9fd19f58b52
Siobhan <siobhan@automattic.com>
2021-06-21 23:06:05 +0100

    Refactor code for iPad-specific popoverController
    This commit refactors the code for iPad-specific popoverController. It's now more inline with similar code elsewhere in the GutenbergViewController.swift file, for example here:
    
    https://github.com/wordpress-mobile/WordPress-iOS/blob/develop/WordPress/Classes/ViewRelated/Gutenberg/GutenbergViewController.swift#L746

--- WordPress/Classes/ViewRelated/Gutenberg/GutenbergViewController.swift
+++ WordPress/Classes/ViewRelated/Gutenberg/GutenbergViewController.swift
@@ -698,13 +698,10 @@ extension GutenbergViewController: GutenbergBridgeDelegate {
 
         alertController.addAction(replaceAction)
         alertController.addCancelActionWithTitle(NSLocalizedString("Cancel", comment: "Button to cancel the replacement of a featured image."))
-
-        // iPad specific code
-        if let popoverController = alertController.popoverPresentationController {
-            popoverController.sourceView = self.view
-            popoverController.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0)
-            popoverController.permittedArrowDirections = []
-        }
+        
+        alertController.popoverPresentationController?.sourceView = view
+        alertController.popoverPresentationController?.sourceRect = view.frame
+        alertController.popoverPresentationController?.permittedArrowDirections = .any
 
         present(alertController, animated: true, completion: nil)
     }
