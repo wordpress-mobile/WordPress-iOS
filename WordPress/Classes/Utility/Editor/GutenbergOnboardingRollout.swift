986b184cf6707d0d5f7c844753c12524f5449c0f
Amanda Riu <amanda.riu@automattic.com>
2021-06-24 13:57:06 -0400

    Add userId hashing and increase to 50 percent rollout

--- WordPress/Classes/Utility/Editor/GutenbergOnboardingRollout.swift
+++ WordPress/Classes/Utility/Editor/GutenbergOnboardingRollout.swift
@@ -1,9 +1,19 @@
+import CryptoKit
+
 /// This structs helps encapsulate logic related to Gutenberg editor onboarding rollout phases.
 ///
 struct GutenbergOnboardingRollout {
-    private let phasePercentage = 0
+    private let phasePercentage = 50
 
     func isUserIdInPhaseRolloutPercentage(_ userId: Int) -> Bool {
-        return userId % 100 >= (100 - phasePercentage) || BuildConfiguration.current ~= [.localDeveloper, .a8cBranchTest]
+        return convertUserIdToRank(userId: String(userId)) >= (100 - phasePercentage) || BuildConfiguration.current ~= [.localDeveloper, .a8cBranchTest]
+    }
+
+    func convertUserIdToRank(userId: String) -> Int {
+        let inputString = userId + "can_view_editor_onboarding"
+        let inputData = Data(inputString.utf8)
+        let hashed = SHA256.hash(data: inputData)
+        let hashRank = abs(hashed.hashValue) % 100
+        return hashRank
     }
 }
