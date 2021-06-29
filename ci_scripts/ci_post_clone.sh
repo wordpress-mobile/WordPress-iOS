1d1c15a77d7a8d4df6ad27377ef7632f0b9bbe9e
Gio Lodi <giovanni.lodi42@gmail.com>
2021-06-29 17:06:52 +1000

    Add some print to learn if there's rvm/rbenv

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
+echo "~~~~ Ruby:"
 ruby -v
 
-sudo gem install bundler:2.2.19
+echo "~~~~ RVM?"
+which rvm
+
+echo "~~~~ rbenv?"
+which rbenv
+
+# Can't sudo
+# sudo gem install bundler:2.2.19
+
+# Running this just for fun, it won't get us to use Bundler 2.x
+bundle update --bundler
 
 bundle install
 bundle exec pod install
