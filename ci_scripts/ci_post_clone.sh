421fb0ad7a757260d0f46dc6926a32107baba9b8
Gio Lodi <giovanni.lodi42@gmail.com>
2021-06-29 16:55:07 +1000

    Try to get Bundler up to date
    Xcode Cloud log:
    
    Run command: 'source /Volumes/Task/ci_build.env && source /Volumes/Task/ci_plan.env && cd /Volumes/workspace/repository/ci_scripts && bash /Volumes/workspace/repository/ci_scripts/ci_post_clone.sh'
    
    ruby 2.6.3p62 (2019-04-16 revision 67580) [universal.x86_64-darwin20]
    
    /System/Library/Frameworks/Ruby.framework/Versions/2.6/usr/lib/ruby/2.6.0/rubygems.rb:283:in `find_spec_for_exe': Could not find 'bundler' (2.2.19) required by your /Volumes/workspace/repository/Gemfile.lock. (Gem::GemNotFoundException)
    
    To update to the latest version installed on your system, run `bundle update --bundler`.
    
    To install the missing version, run `gem install bundler:2.2.19`
    
    	from /System/Library/Frameworks/Ruby.framework/Versions/2.6/usr/lib/ruby/2.6.0/rubygems.rb:302:in `activate_bin_path'
    
    	from /usr/bin/bundle:23:in `<main>'
    
    /System/Library/Frameworks/Ruby.framework/Versions/2.6/usr/lib/ruby/2.6.0/rubygems.rb:283:in `find_spec_for_exe': Could not find 'bundler' (2.2.19) required by your /Volumes/workspace/repository/Gemfile.lock. (Gem::GemNotFoundException)
    
    To update to the latest version installed on your system, run `bundle update --bundler`.
    
    To install the missing version, run `gem install bundler:2.2.19`
    
    	from /System/Library/Frameworks/Ruby.framework/Versions/2.6/usr/lib/ruby/2.6.0/rubygems.rb:302:in `activate_bin_path'
    
    	from /usr/bin/bundle:23:in `<main>'

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,6 +2,12 @@
 
 ruby -v
 
-# gem install bundler # Is Bundler already available?
+# Get the latest version of Bundler.
+#
+# Xcode Cloud ships with system ruby (2.6.3p69) Hopefully this is enough to get
+#
+# Bundler 2.x installed, which is what the repo tooling needs.
+bundle update --bundler
+
 bundle install
 bundle exec pod install
