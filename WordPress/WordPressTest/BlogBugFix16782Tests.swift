7941ac48789b712c8672508dd33890099ef61cae
Gio Lodi <giovanni.lodi42@gmail.com>
2021-07-01 11:06:00 +1000

    Add tests for Blog.m fix in #16782

---
+++ WordPress/WordPressTest/BlogBugFix16782Tests.swift
@@ -0,0 +1,68 @@
+import Nimble
+@testable import WordPress
+import XCTest
+
+class BlogBugFix16782: XCTestCase {
+
+    private var contextManager: TestContextManager!
+
+    override func setUp() {
+        super.setUp()
+        contextManager = TestContextManager()
+    }
+
+    override func tearDown() {
+        contextManager = nil
+        super.tearDown()
+    }
+
+    func testDefaultAdminURLBehavior() {
+        let blog = BlogBuilder(contextManager.mainContext).build()
+
+        let adminURL = blog.adminUrl(withPath: "test")
+        debugPrint("~~~ adminURL: \(adminURL)")
+        // ~~~ adminURL: https://example.com/wp-admin/test
+
+        // Verify the URL is "valid"
+        expect(URL(string: adminURL)?.absoluteString).to(beginWith("http"))
+    }
+
+    func testAdminURLWithNullXMLRPC() {
+        let blog = BlogBuilder(contextManager.mainContext).build()
+
+        blog.xmlrpc = .none
+
+        let adminURL = blog.adminUrl(withPath: "test")
+        debugPrint("~~~ adminURL: \(adminURL)")
+        // ~~~ adminURL: "(null)test"
+
+        // Verify the URL is "valid"
+        XCTExpectFailure()
+        expect(URL(string: adminURL)?.absoluteString).to(beginWith("http"))
+    }
+
+    func testDefaultLoginURLBehavior() {
+        let blog = BlogBuilder(contextManager.mainContext).build()
+
+        let loginURL = blog.loginUrl()
+        debugPrint("~~~ loginURL: \(loginURL)")
+        // ~~~ loginURL: https://example.com/wp-login.php
+
+        // Verify the URL is "valid"
+        expect(URL(string: loginURL)?.absoluteString).to(beginWith("http"))
+    }
+
+    func testLoginURLWithNullXMLRPC() {
+        let blog = BlogBuilder(contextManager.mainContext).build()
+
+        blog.xmlrpc = .none
+
+        let loginURL = blog.loginUrl()
+        debugPrint("~~~ loginURL: \(loginURL)")
+        // ~~~ loginURL: [that's it. the value is an empty string]
+
+        // Verify the URL is "valid"
+        XCTExpectFailure()
+        expect(URL(string: loginURL)?.absoluteString).to(beginWith("http"))
+    }
+}
