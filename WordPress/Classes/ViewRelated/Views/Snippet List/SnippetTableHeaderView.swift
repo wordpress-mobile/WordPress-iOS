3c18ae7c5f3bb23068473319406ba9a99929c063
David Christiandy <1299411+dvdchr@users.noreply.github.com>
2021-07-08 16:03:46 +0700

    Polish SnippetTableHeaderView
    - Automatically uppercase titleLabel text through setter callbacks.
    - Configure styling for the titleLabel.
    - Hide UITableHeaderFooterView's textLabel due to interaction with NSFetchedResultsController.

--- WordPress/Classes/ViewRelated/Views/Snippet List/SnippetTableHeaderView.swift
+++ WordPress/Classes/ViewRelated/Views/Snippet List/SnippetTableHeaderView.swift
@@ -1,5 +1,3 @@
-import UIKit
-
 /// Renders a table header view with bottom separator, and meant to be used
 /// alongside `SnippetTableViewCell`.
 ///
@@ -10,14 +8,31 @@ class SnippetTableHeaderView: UITableViewHeaderFooterView, NibReusable {
     // MARK: IBOutlets
 
     @IBOutlet private weak var separatorsView: SeparatorsView!
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet private weak var titleLabel: UILabel!
+
+    // MARK: Properties
+
+    @objc var title: String? {
+        get {
+            titleLabel.text
+        }
+        set {
+            titleLabel.text = newValue?.localizedUppercase ?? String()
+        }
+    }
 
     // MARK: Initialization
 
     override func awakeFromNib() {
         super.awakeFromNib()
 
+        // Hide text label to prevent values being overwritten due to interaction with
+        // NSFetchedResultsController. By default, the results controller assigns the
+        // value of sectionNameKeyPath to UITableHeaderFooterView's textLabel.
+        textLabel?.isHidden = true
+
         // configure title label
+        titleLabel.font = WPStyleGuide.fontForTextStyle(.footnote, fontWeight: .semibold)
         titleLabel.textColor = .textSubtle
 
         // configure separators view
