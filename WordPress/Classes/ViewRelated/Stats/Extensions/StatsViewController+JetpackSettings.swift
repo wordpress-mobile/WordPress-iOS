566b28c786365a6cdde14188cd3571f21d927b2e
Momo Ozawa <momo.ozawa@automattic.com>
2021-07-08 15:03:58 +0100

    Add: activate the site stats module

---
+++ WordPress/Classes/ViewRelated/Stats/Extensions/StatsViewController+JetpackSettings.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+extension StatsViewController {
+
+    @objc func activateStatsModule(success: @escaping () -> Void, failure: @escaping (Error?) -> Void) {
+        guard let context = blog.settings?.managedObjectContext else {
+            return
+        }
+
+        let service = BlogJetpackSettingsService(managedObjectContext: context)
+
+        service.updateJetpackModuleActiveSettingForBlog(blog,
+                                                        module: Constants.statsModule,
+                                                        active: true,
+                                                        success: success,
+                                                        failure: failure)
+
+    }
+
+    private enum Constants {
+        static let statsModule = "stats"
+    }
+
+}
