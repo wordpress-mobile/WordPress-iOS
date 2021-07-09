3884e10e70189edabca3eb7007a03b2e9a88ffa5
David Christiandy <1299411+dvdchr@users.noreply.github.com>
2021-07-09 16:43:06 +0700

    Update view to match the design

--- WordPress/Classes/ViewRelated/Views/List/WPStyleGuide+List.swift
+++ WordPress/Classes/ViewRelated/Views/List/WPStyleGuide+List.swift
@@ -3,7 +3,7 @@
 extension WPStyleGuide {
     public enum List {
         // MARK: Section Headers
-        public static let sectionHeaderFont = WPStyleGuide.fontForTextStyle(.footnote, fontWeight: .semibold)
+        public static let sectionHeaderFont = WPStyleGuide.fontForTextStyle(.caption1, fontWeight: .medium)
         public static let sectionHeaderTitleColor = UIColor.textSubtle
         public static let sectionHeaderBackgroundColor = UIColor.basicBackground
 
