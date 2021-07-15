bdaf24ca5ea6c649eaa68b88729d97e122f51748
Gio Lodi <giovanni.lodi42@gmail.com>
2021-07-15 14:37:56 +1000

    Add check to ensure secrets are in input file list

--- Scripts/BuildPhases/ApplyConfiguration.sh
+++ Scripts/BuildPhases/ApplyConfiguration.sh
@@ -5,13 +5,60 @@ set -e
 # The Secrets File Sources
 SECRETS_ROOT="${HOME}/.configure/wordpress-ios/secrets"
 
+# To help the Xcode build system optimize the build, we want to ensure each of
+# the secrets we want to copy is defined as an input file for the run script
+# build phase.
+#
+# > The Xcode Build System will use [these files] to determine if your run
+# > scripts should actually run or not. So this should include any file that
+# > your run script phase, the script content, is actually going to read or
+# > look at during its process.
+#
+# > If you have no input files declared, the Xcode build system will need to
+# > run your run script phase on every single build.
+#
+# https://developer.apple.com/videos/play/wwdc2018/408/
+function ensure_is_in_input_files_list() {
+  # Loop through the file input lists looking for $1. If not found, fail the
+  # build.
+  if [ -z "$1" ]; then
+    echo "error: Input file list verification needs a path to verify!"
+    exit 1
+  fi
+
+  i=0
+  found=false
+  while [[ $i -lt $SCRIPT_INPUT_FILE_LIST_COUNT && "$found" = false ]]
+  do
+    # Need this two step process to access the input at index
+    file_list_resolved_var_name=SCRIPT_INPUT_FILE_LIST_${i}
+    # The following reads the processed xcfilelist line by line and
+    while read input_file; do
+      if [ "$1" == "$input_file" ]; then
+        found=true
+        break
+      fi
+    done <"${!file_list_resolved_var_name}"
+    let i=i+1
+  done
+  if [ "$found" = false ]; then
+    echo "error: Could not find $1 as an input to the build phase. Add $1 to the input files list using the .xcfilelist."
+    exit 1
+  fi
+}
+
 PRODUCTION_SECRETS_FILE="${SECRETS_ROOT}/WordPress-Secrets.swift"
+ensure_is_in_input_files_list $PRODUCTION_SECRETS_FILE
 INTERNAL_SECRETS_FILE="${SECRETS_ROOT}/WordPress-Secrets-Internal.swift"
+ensure_is_in_input_files_list $INTERNAL_SECRETS_FILE
 ALPHA_SECRETS_FILE="${SECRETS_ROOT}/WordPress-Secrets-Alpha.swift"
+ensure_is_in_input_files_list $ALPHA_SECRETS_FILE
 JETPACK_SECRETS_FILE="${SECRETS_ROOT}/Jetpack-Secrets.swift"
+ensure_is_in_input_files_list $JETPACK_SECRETS_FILE
 
 LOCAL_SECRETS_FILE="${SRCROOT}/Credentials/Secrets.swift"
 EXAMPLE_SECRETS_FILE="${SRCROOT}/Credentials/Secrets-example.swift"
+ensure_is_in_input_files_list $EXAMPLE_SECRETS_FILE
 
 # The Secrets file destination
 SECRETS_DESTINATION_FILE="${BUILD_DIR}/Secrets/Secrets.swift"
