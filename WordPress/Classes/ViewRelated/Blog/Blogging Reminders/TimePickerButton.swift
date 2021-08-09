6987ae736d8de35bcbc86df0b5e5b9553f656f02
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2021-08-09 17:16:15 -0500

    Add type TimePickerButton used in Blogging Reminders settings screen

--- WordPress/Classes/ViewRelated/Blog/Blogging Reminders/TimePickerButton.swift
+++ WordPress/Classes/ViewRelated/Blog/Blogging Reminders/TimePickerButton.swift
@@ -1,9 +1,74 @@
-//
-//  TimePickerButton.swift
-//  WordPress
-//
-//  Created by Giorgio Ruscigno on 8/9/21.
-//  Copyright © 2021 WordPress. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class TimePickerButton: UIButton {
+
+    override var isHighlighted: Bool {
+        didSet {
+            backgroundColor = isHighlighted ? .divider : .white
+            setNeedsDisplay()
+        }
+    }
+
+    let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.isUserInteractionEnabled = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.alignment = .center
+        stackView.axis = .horizontal
+        return stackView
+    }()
+
+    let pickerTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = true
+        label.adjustsFontSizeToFitWidth = true
+        label.text = Constants.title
+        return label
+    }()
+
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = true
+        label.adjustsFontSizeToFitWidth = true
+        label.text = "3:00 PM"
+        return label
+    }()
+
+    lazy var chevron: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage.gridicon(.chevronRight)
+        imageView.tintColor = .divider
+        return imageView
+    }()
+
+    lazy var chavronStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.addArrangedSubviews([UIView(), chevron, UIView()])
+        return stackView
+    }()
+
+    func configureStackView() {
+        stackView.addArrangedSubviews([pickerTitleLabel, UIView(), timeLabel, chavronStackView])
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureStackView()
+        addSubview(stackView)
+        pinSubviewToAllEdges(stackView, insets: Constants.insets)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    enum Constants {
+        static let title = NSLocalizedString("Notification time", comment: "Title for the time picker button in Blogging Reminders.")
+        static let insets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+    }
+}
