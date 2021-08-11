bd9a5f6d26f13da3eb1107dde74da2d421c912f8
David Christiandy <1299411+dvdchr@users.noreply.github.com>
2021-08-11 21:00:03 +0700

    Add constants for ImmuTable components

---
+++ WordPress/Classes/ViewRelated/Sharing/ShareAppContentPresenter+ImmuTable.swift
@@ -0,0 +1,8 @@
+/// Constants for share app content interoperability with ImmuTable components.
+///
+extension ShareAppContentPresenter {
+    struct RowConstants {
+        static let buttonTitle = NSLocalizedString("Share WordPress with a friend", comment: "Label for a button that recommends the app to others")
+        static let buttonIconImage = UIImage(systemName: "square.and.arrow.up")
+    }
+}
