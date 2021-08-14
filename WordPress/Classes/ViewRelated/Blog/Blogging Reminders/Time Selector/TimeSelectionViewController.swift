1d1b557ce873f904b61b7c0a4177d8c56e309549
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2021-08-14 02:13:29 -0500

    Refactor TimeSelectionViewController to pass the time picker date to a closure and use an injected date to configure the initial value of the time picker

--- WordPress/Classes/ViewRelated/Blog/Blogging Reminders/Time Selector/TimeSelectionViewController.swift
+++ WordPress/Classes/ViewRelated/Blog/Blogging Reminders/Time Selector/TimeSelectionViewController.swift
@@ -4,11 +4,28 @@ class TimeSelectionViewController: UIViewController {
 
     var preferredWidth: CGFloat?
 
+    private let scheduledTime: Date
+
+    private let tracker: BloggingRemindersTracker
+
+    private var onDismiss: ((Date) -> Void)?
+
     private lazy var timeSelectionView: TimeSelectionView = {
-        let view = TimeSelectionView(selectedTime: Date(timeIntervalSinceNow: 3600))
+        let view = TimeSelectionView(selectedTime: scheduledTime)
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
+
+    init(scheduledTime: Date, tracker: BloggingRemindersTracker, onDismiss: ((Date) -> Void)? = nil) {
+        self.scheduledTime = scheduledTime
+        self.tracker = tracker
+        self.onDismiss = onDismiss
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func loadView() {
         let mainView = timeSelectionView
@@ -39,7 +56,17 @@ class TimeSelectionViewController: UIViewController {
         super.viewWillDisappear(animated)
         navigationController?.setNavigationBarHidden(true, animated: false)
         if isMovingFromParent {
-            /// - TODO: Read the selected date here
+            onDismiss?(timeSelectionView.getDate())
+        }
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+
+        // If a parent VC is being dismissed, and this is the last view shown in its navigation controller, we'll assume
+        // the flow was interrupted.
+        if isBeingDismissedDirectlyOrByAncestor() && navigationController?.viewControllers.last == self {
+            tracker.flowDismissed(source: .timePicker)
         }
     }
 }
