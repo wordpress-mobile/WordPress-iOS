0442a83b0fb25ccdf71094bf120a8c5bd72a01a1
Gio Lodi <giovanni.lodi42@gmail.com>
2021-08-18 21:09:30 +1000

    Remove unused `tapStatusBarToScrollToTop` utility

--- WordPress/WordPressUITests/Utils/XCTest+Extensions.swift
+++ WordPress/WordPressUITests/Utils/XCTest+Extensions.swift
@@ -105,9 +105,4 @@ extension XCTestCase {
         guard element.exists && !element.frame.isEmpty && element.isHittable else { return false }
         return XCUIApplication().windows.element(boundBy: 0).frame.contains(element.frame)
     }
-
-    // A shortcut to scroll TableViews or CollectionViews to top
-    func tapStatusBarToScrollToTop() {
-        XCUIApplication().statusBars.firstMatch.tap()
-    }
 }
