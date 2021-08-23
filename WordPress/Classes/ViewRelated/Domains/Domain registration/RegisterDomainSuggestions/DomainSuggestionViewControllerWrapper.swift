7c430e00bb886039b18abab430880ecdf4502d93
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2021-08-23 14:59:48 -0500

    Add new types DomainSuggestionViewControllerWrapper and DomainsDashboardView to implement the new Domains Dashboard, accessible from My Site

---
+++ WordPress/Classes/ViewRelated/Domains/Domain registration/RegisterDomainSuggestions/DomainSuggestionViewControllerWrapper.swift
@@ -0,0 +1,9 @@
+//
+//  DomainSuggestionViewControllerWrapper.swift
+//  WordPress
+//
+//  Created by Giorgio Ruscigno on 8/20/21.
+//  Copyright © 2021 WordPress. All rights reserved.
+//
+
+import Foundation
---
+++ WordPress/Classes/ViewRelated/Domains/DomainsDashboardView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+import UIKit
+
+class DomainsDashboardViewController: UIHostingController<DomainsDashboardView> {
+
+}
+
+
+struct DomainsDashboardView: View {
+
+    var body: some View {
+        Text("Hello World")
+    }
+}
