f1b927576a97224a53c2e699f1881a60c51b3c55
David Christiandy <1299411+dvdchr@users.noreply.github.com>
2021-09-02 00:11:00 +0700

    Create the initial view for content cell

---
+++ WordPress/Classes/ViewRelated/Comments/CommentContentTableViewCell.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class CommentContentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
