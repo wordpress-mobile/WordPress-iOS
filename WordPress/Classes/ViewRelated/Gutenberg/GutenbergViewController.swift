58741295faeea6c5ec1bffe47819bcf81ace35ec
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2021-09-07 14:58:55 -0500

    Update replace Featured Image copy
    Leverage improved copy provided by designer.

--- WordPress/Classes/ViewRelated/Gutenberg/GutenbergViewController.swift
+++ WordPress/Classes/ViewRelated/Gutenberg/GutenbergViewController.swift
@@ -698,16 +698,16 @@ extension GutenbergViewController: GutenbergBridgeDelegate {
     }
 
     func showAlertForReplacingFeaturedImage(mediaID: Int32) {
-        let alertController = UIAlertController(title: NSLocalizedString("Featured Image Already Set", comment: "Title message on dialog that prompts user to confirm or cancel the replacement of a featured image."),
-                                                message: NSLocalizedString("You already have a featured image set. Do you want to replace it?", comment: "Main message on dialog that prompts user to confirm or cancel the replacement of a featured image."),
+        let alertController = UIAlertController(title: NSLocalizedString("Replace current featured image?", comment: "Title message on dialog that prompts user to confirm or cancel the replacement of a featured image."),
+                                                message: NSLocalizedString("You already have a featured image set. Do you want to replace it with the new image?", comment: "Main message on dialog that prompts user to confirm or cancel the replacement of a featured image."),
                                                 preferredStyle: .actionSheet)
 
-        let replaceAction = UIAlertAction(title: NSLocalizedString("Replace", comment: "Button to confirm the replacement of a featured image."), style: .default) { (action) in
+        let replaceAction = UIAlertAction(title: NSLocalizedString("Replace with new image", comment: "Button to confirm the replacement of a featured image."), style: .default) { (action) in
             self.featuredImageHelper.setFeaturedImage(mediaID: mediaID)
         }
 
         alertController.addAction(replaceAction)
-        alertController.addCancelActionWithTitle(NSLocalizedString("Cancel", comment: "Button to cancel the replacement of a featured image."))
+        alertController.addCancelActionWithTitle(NSLocalizedString("Keep current featured image", comment: "Button to cancel the replacement of a featured image."))
 
         alertController.popoverPresentationController?.sourceView = view
         alertController.popoverPresentationController?.sourceRect = view.bounds
