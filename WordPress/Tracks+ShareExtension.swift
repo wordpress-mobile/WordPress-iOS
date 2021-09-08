b8795d1fccee8e9bef1c96183b6fe97838d509fc
Sam Oakley <sam@automattic.blork.co.uk>
2021-09-08 08:53:21 +0100

    Remove unnecessary casts to optional

--- WordPress/Tracks+ShareExtension.swift
+++ WordPress/Tracks+ShareExtension.swift
@@ -8,17 +8,17 @@ extension Tracks {
 
     public func trackExtensionLaunched(_ wpcomAvailable: Bool) {
         let properties = ["is_configured_dotcom": wpcomAvailable]
-        trackExtensionEvent(.launched, properties: properties as [String: AnyObject]?)
+        trackExtensionEvent(.launched, properties: properties as [String: AnyObject])
     }
 
     public func trackExtensionPosted(_ status: String) {
         let properties = ["post_status": status]
-        trackExtensionEvent(.posted, properties: properties as [String: AnyObject]?)
+        trackExtensionEvent(.posted, properties: properties as [String: AnyObject])
     }
 
     public func trackExtensionError(_ error: NSError) {
         let properties = ["error_code": String(error.code), "error_domain": error.domain, "error_description": error.description]
-        trackExtensionEvent(.error, properties: properties as [String: AnyObject]?)
+        trackExtensionEvent(.error, properties: properties as [String: AnyObject])
     }
 
     public func trackExtensionCancelled() {
@@ -31,7 +31,7 @@ extension Tracks {
 
     public func trackExtensionTagsSelected(_ tags: String) {
         let properties = ["selected_tags": tags]
-        trackExtensionEvent(.tagsSelected, properties: properties as [String: AnyObject]?)
+        trackExtensionEvent(.tagsSelected, properties: properties as [String: AnyObject])
     }
 
     public func trackExtensionCategoriesOpened() {
@@ -40,7 +40,7 @@ extension Tracks {
 
     public func trackExtensionCategoriesSelected(_ categories: String) {
         let properties = ["categories_tags": categories]
-        trackExtensionEvent(.categoriesSelected, properties: properties as [String: AnyObject]?)
+        trackExtensionEvent(.categoriesSelected, properties: properties as [String: AnyObject])
     }
 
     public func trackExtensionPostTypeOpened() {
@@ -49,7 +49,7 @@ extension Tracks {
 
     public func trackExtensionPostTypeSelected(_ postType: String) {
         let properties = ["post_type": postType]
-        trackExtensionEvent(.postTypeSelected, properties: properties as [String: AnyObject]?)
+        trackExtensionEvent(.postTypeSelected, properties: properties as [String: AnyObject])
     }
 
     // MARK: - Private Helpers
--- WordPress/WordPressDraftActionExtension/Tracks+DraftAction.swift
+++ WordPress/WordPressDraftActionExtension/Tracks+DraftAction.swift
@@ -7,17 +7,17 @@ extension Tracks {
 
     public func trackExtensionLaunched(_ wpcomAvailable: Bool) {
         let properties = ["is_configured_dotcom": wpcomAvailable]
-        trackExtensionEvent(.launched, properties: properties as [String: AnyObject]?)
+        trackExtensionEvent(.launched, properties: properties as [String: AnyObject])
     }
 
     public func trackExtensionPosted(_ status: String) {
         let properties = ["post_status": status]
-        trackExtensionEvent(.posted, properties: properties as [String: AnyObject]?)
+        trackExtensionEvent(.posted, properties: properties as [String: AnyObject])
     }
 
     public func trackExtensionError(_ error: NSError) {
         let properties = ["error_code": String(error.code), "error_domain": error.domain, "error_description": error.description]
-        trackExtensionEvent(.error, properties: properties as [String: AnyObject]?)
+        trackExtensionEvent(.error, properties: properties as [String: AnyObject])
     }
 
     public func trackExtensionCancelled() {
@@ -30,7 +30,7 @@ extension Tracks {
 
     public func trackExtensionTagsSelected(_ tags: String) {
         let properties = ["selected_tags": tags]
-        trackExtensionEvent(.tagsSelected, properties: properties as [String: AnyObject]?)
+        trackExtensionEvent(.tagsSelected, properties: properties as [String: AnyObject])
     }
 
     public func trackExtensionCategoriesOpened() {
@@ -39,7 +39,7 @@ extension Tracks {
 
     public func trackExtensionCategoriesSelected(_ categories: String) {
         let properties = ["categories_tags": categories]
-        trackExtensionEvent(.categoriesSelected, properties: properties as [String: AnyObject]?)
+        trackExtensionEvent(.categoriesSelected, properties: properties as [String: AnyObject])
     }
 
     public func trackExtensionPostTypeOpened() {
@@ -47,8 +47,8 @@ extension Tracks {
     }
 
     public func trackExtensionPostTypeSelected(_ postType: String) {
-        trackExtensionEvent(.postTypeSelected, properties: properties as [String: AnyObject]?)
         let properties = ["post_type": postType]
+        trackExtensionEvent(.postTypeSelected, properties: properties as [String: AnyObject])
     }
 
     // MARK: - Private Helpers
