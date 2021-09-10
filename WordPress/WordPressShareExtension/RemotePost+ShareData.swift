436fbe52d400d323621e8a0e8ff728f9000270e3
Sam Oakley <sam@automattic.blork.co.uk>
2021-09-10 10:27:43 +0100

    Correct formatting and typo

--- WordPress/WordPressShareExtension/RemotePost+ShareData.swift
+++ WordPress/WordPressShareExtension/RemotePost+ShareData.swift
@@ -6,12 +6,12 @@ extension RemotePost {
     /// Create a new Remote Post from the data provided by the Share and Action extensions.
     /// - Parameters:
     ///   - shareData: The data from which to create the Remote Post
-    ///   - siteID:: Site ID the post will be uploaded to
+    ///   - siteID: Site ID the post will be uploaded to
     convenience init(shareData: ShareData, siteID: Int) {
         self.init(siteID: NSNumber(value: siteID),
-                   status: shareData.postStatus.rawValue,
-                   title: shareData.title,
-                   content: shareData.contentBody)
+                  status: shareData.postStatus.rawValue,
+                  title: shareData.title,
+                  content: shareData.contentBody)
 
         switch shareData.postType {
         case .post:
