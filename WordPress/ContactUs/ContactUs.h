e1d095b4f0febde645517e4a6ee8054501ad2c86
Gio Lodi <giovanni.lodi42@gmail.com>
2021-09-13 19:43:42 +1000

    Add new dedicated framework for the upcoming contact us self-service

---
+++ WordPress/ContactUs/ContactUs.h
@@ -0,0 +1,4 @@
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double ContactUsVersionNumber;
+FOUNDATION_EXPORT const unsigned char ContactUsVersionString[];
---
+++ WordPress/ContactUsTests/ContactUsTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import ContactUs
+
+class ContactUsTests: XCTestCase {
+
+    func testExample() throws {
+        XCTAssert(true)
+    }
+}
