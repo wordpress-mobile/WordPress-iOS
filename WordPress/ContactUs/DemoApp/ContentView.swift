30994ba8a13c0326a234a1ef68961fad34fc260b
Gio Lodi <giovanni.lodi42@gmail.com>
2021-09-14 16:45:06 +1000

    Add decision tree UI to contact us demo app – No URL loading yet

--- WordPress/ContactUs/DemoApp/ContentView.swift
+++ WordPress/ContactUs/DemoApp/ContentView.swift
@@ -1,10 +1,83 @@
+import ContactUs
 import SwiftUI
 
 struct ContentView: View {
+
+    @ObservedObject private var viewModel = ViewModel()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            if let questions = viewModel.questions {
+                QuestionList(questions: questions)
+            } else {
+                Text("Loading...")
+            }
+        }
     }
+}
+
+struct QuestionList: View {
+
+    let questions: [Question]
+
+    @State private var alertPresented = false
+
+    var body: some View {
+        List(questions) { item in
+            switch item.next {
+            case .page(let _questions):
+                NavigationLink(destination: QuestionList(questions: _questions)) {
+                    Text(item.message)
+                }
+            case .url(let url):
+                Button {
+                    self.alertPresented.toggle()
+                } label: {
+                    Text(item.message)
+                }
+                .alert(isPresented: $alertPresented, content: {
+                    Alert(
+                        title: Text("TODO"),
+                        message: Text("Load URL for \(url)"),
+                        dismissButton: .default(Text("Dismiss"))
+                    )
+                })
+
+            }
+        }
+    }
+}
+
+import Combine
+
+class ViewModel: ObservableObject {
+
+    let provider = ContactUsProvider()
+
+    @Published var questions: [Question]? = .none
+
+    private var cancellables = Set<AnyCancellable>()
+
+    init() {
+        provider
+            .loadDecisionTree()
+            .sink(
+                receiveCompletion: { completion in
+                    guard case .failure = completion else { return }
+                    // TODO: Implement better error handling
+                    self.questions = .none
+                },
+                receiveValue: { questions in
+                    self.questions = questions
+                }
+            )
+            .store(in: &cancellables)
+    }
+}
+
+extension Question: Identifiable {
+
+    public var id: String { message }
 }
 
 struct ContentView_Previews: PreviewProvider {
