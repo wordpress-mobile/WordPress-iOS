d115d9f7e016acc5c47afcfdf74f8e74d26c7a0c
sla8c <88816724+sla8c@users.noreply.github.com>
2021-09-28 18:52:45 +0200

    * Refactor + created TimeZoneFormatter to encapsulate TimeZone DateFormatter logic  * Unit tests for TimeZoneFormatter

--- WordPress/Classes/ViewRelated/Tools/Time Zone/TimeZoneSelectorViewController.swift
+++ WordPress/Classes/ViewRelated/Tools/Time Zone/TimeZoneSelectorViewController.swift
@@ -65,34 +65,15 @@ struct TimeZoneSelectorViewModel: Observable {
         })
     }
 
-    private let timeZoneOffsetFormatter = DateFormatter()
+    private var timeZoneFormatter: TimeZoneFormatter?
 
-    private let timeAtTimeZoneFormatter = DateFormatter()
-
-    private let currentDate = Date()
-
-    // DateFormatter is expensive https://sarunw.com/posts/how-expensive-is-dateformatter/
-    // initializer to ensure that:
-    // 1. Existing 3 property struct initialization of state, selectedValue, filter work normally
-    // 2. DateFormatters are configured whenever we create instance of TimeZoneSelectorViewModel
-    // 3. DateFormatters are private and their state not mutable from outside callers (thread-safety)
     init(state: State, selectedValue: String?, filter: String?) {
         self.state = state
         self.selectedValue = selectedValue
         self.filter = filter
 
         if FeatureFlag.timeZoneSuggester.enabled {
-            configureDateFormatter(formatter: timeZoneOffsetFormatter, timeStyle: .none, dateFormat: Constants.timeZoneOffsetFormat)
-            configureDateFormatter(formatter: timeAtTimeZoneFormatter, timeStyle: .short, dateFormat: nil)
-        }
-    }
-
-    private func configureDateFormatter(formatter: DateFormatter, timeStyle: DateFormatter.Style, dateFormat: String?) {
-        formatter.locale = Locale.autoupdatingCurrent
-        formatter.timeStyle = timeStyle
-
-        if let dateFormat = dateFormat {
-            formatter.dateFormat = dateFormat
+            timeZoneFormatter = TimeZoneFormatter(currentDate: Date())
         }
     }
 
@@ -103,25 +84,6 @@ struct TimeZoneSelectorViewModel: Observable {
             .first
     }
 
-    private func getZoneOffset(_ zone: WPTimeZone) -> String {
-        guard let namedTimeZone = zone as? NamedTimeZone, let timeZone = TimeZone(identifier: namedTimeZone.value), let timeZoneLocalized = timeZone.localizedName(for: .standard, locale: .current) else {
-            return ""
-        }
-        timeZoneOffsetFormatter.timeZone = timeZone
-
-        let offset = timeZoneOffsetFormatter.string(from: currentDate)
-        return "\(timeZoneLocalized) (\(offset))"
-    }
-
-    private func getTimeAtZone(_ zone: WPTimeZone) -> String {
-        guard let namedTimeZone = zone as? NamedTimeZone, let timeZone = TimeZone(identifier: namedTimeZone.value) else {
-            return ""
-        }
-        timeAtTimeZoneFormatter.timeZone = timeZone
-
-        return timeAtTimeZoneFormatter.string(from: currentDate)
-    }
-
     func tableViewModel(selectionHandler: @escaping (WPTimeZone) -> Void) -> ImmuTable {
         return ImmuTable(
             sections: filteredGroups.map({ (group) -> ImmuTableSection in
@@ -129,11 +91,17 @@ struct TimeZoneSelectorViewModel: Observable {
                     headerText: group.name,
                     rows: group.timezones.map({ (timezone) -> ImmuTableRow in
                         if FeatureFlag.timeZoneSuggester.enabled {
-                            return TimeZoneRow(title: timezone.label, leftSubtitle: getZoneOffset(timezone), rightSubtitle: getTimeAtZone(timezone), action: { _ in
+                            guard let timeZoneFormatter = timeZoneFormatter else {
+                                return CheckmarkRow(title: timezone.label, checked: timezone.value == selectedValue, action: { _ in
+                                    selectionHandler(timezone)
+                                })
+                            }
+                            return TimeZoneRow(title: timezone.label, leftSubtitle: timeZoneFormatter.getZoneOffset(timezone), rightSubtitle: timeZoneFormatter.getTimeAtZone(timezone), action: { _ in
                                 selectionHandler(timezone)
                             })
                         }
                         else {
+                            // below will be removed when feature flags no longer needed
                             return CheckmarkRow(title: timezone.label, checked: timezone.value == selectedValue, action: { _ in
                                 selectionHandler(timezone)
                             })
@@ -171,9 +139,6 @@ struct TimeZoneSelectorViewModel: Observable {
         static let noConnectionSubtitle = NSLocalizedString("An active internet connection is required", comment: "Error message when loading failed because there's no connection")
     }
 
-    private enum Constants {
-        static let timeZoneOffsetFormat = "ZZZZ"
-    }
 }
 
 class TimeZoneSelectorViewController: UITableViewController, UISearchResultsUpdating {
