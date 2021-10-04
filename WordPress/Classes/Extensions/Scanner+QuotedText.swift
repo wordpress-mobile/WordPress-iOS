08f7dedfe32bf150375141de5f6f7c7e57b2eb95
Evgeny Aleksandrov <evgeny@aleksandrov.ws>
2021-10-04 20:44:32 +0300

    Update logic and add comments for `scanQuotedText()`

--- WordPress/Classes/Extensions/Scanner+QuotedText.swift
+++ WordPress/Classes/Extensions/Scanner+QuotedText.swift
@@ -2,19 +2,20 @@ import Foundation
 
 extension Scanner {
     public func scanQuotedText() -> [String] {
-        var scanned = [String]()
-        var quote: String?
+        var allQuotedTextFound = [String]()
+        var textRead: String?
         let quoteString = "\""
         while self.isAtEnd == false {
-            _ = scanUpToString(quoteString)
-            _ = scanString(quoteString)
-            quote = scanUpToString(quoteString)
-            _ = scanString(quoteString)
+            _ = scanUpToString(quoteString) // scan up to quotation mark
+            _ = scanString(quoteString) // skip opening quotation mark
+            textRead = scanUpToString(quoteString) // read text up to next quotation mark
+            let closingMarkFound = scanString(quoteString) != nil // skip closing quotation mark
 
-            if let quoteString = quote, quoteString.isEmpty() == false {
-                scanned.append(quoteString as String)
+            if let quotedTextFound = textRead, quotedTextFound.isEmpty == false, closingMarkFound {
+                allQuotedTextFound.append(quotedTextFound as String)
             }
         }
-        return scanned
+
+        return allQuotedTextFound
     }
 }
