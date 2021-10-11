cdbd47a7b1194ec369873dcb6f0a224f9b7dcb53
Nikola Milicevic <nikola.milicevic@automattic.com>
2021-10-12 00:25:04 +0200

    Add add methods for comparing Publicize Services states

--- WordPress/Classes/ViewRelated/Blog/PublicizeServicesState.swift
+++ WordPress/Classes/ViewRelated/Blog/PublicizeServicesState.swift
@@ -1,13 +1,25 @@
 import Foundation
 
 @objc class PublicizeServicesState: NSObject {
-    private var connections = Set<PublicizeConnection>() {
-        didSet {
-            print(connections, connections.count)
-        }
+    private var connections = Set<PublicizeConnection>()
+
+    @objc func addInitialConnections(_ connections: [PublicizeConnection]) {
+        connections.forEach { self.connections.insert($0) }
     }
 
-    @objc func addConnections(_ connections: [PublicizeConnection]) {
-        connections.forEach { self.connections.insert($0) }
+    @objc func hasAddedNewConnectionTo(_ connections: [PublicizeConnection]) -> Bool {
+        guard connections.count > 0 else {
+            return false
+        }
+
+        if connections.count > self.connections.count {
+            return true
+        }
+
+        for connection in connections where !self.connections.contains(connection) {
+            return true
+        }
+
+        return false
     }
 }
