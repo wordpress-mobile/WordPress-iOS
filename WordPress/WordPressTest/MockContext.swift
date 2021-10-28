3ab1d62e748f215788636cfd80c3815a4b3690a8
Diego Rey Mendez <diego@reymendez.com>
2021-10-28 13:55:30 +0200

    Tries fixing the unit tests for Xcode 13.

--- WordPress/WordPressTest/MockContext.swift
+++ WordPress/WordPressTest/MockContext.swift
@@ -3,10 +3,23 @@ import CoreData
 
 @testable import WordPress
 
+class MockFetchResult: NSAsynchronousFetchResult<NSFetchRequestResult> {
+    let results: [NSFetchRequestResult]?
+
+    init(results: [NSFetchRequestResult]?) {
+        self.results = results
+    }
+
+    override open var finalResult: [NSFetchRequestResult]? {
+        results
+    }
+}
+
 /// Mock context that uses the existing Test Core Data Stack and overrides fetch for testing purposes
 class MockContext: NSManagedObjectContext {
     // set it to any array of objects you want ot return
-    var returnedObjects: [Any]?
+    var returnedObjects: [NSFetchRequestResult]?
+
     // set it to any error you want to return so simulate fetch error
     var fetchError: Error?
     // set to false to simulate fetch error
@@ -17,13 +30,15 @@ class MockContext: NSManagedObjectContext {
     var successExpectation: XCTestExpectation?
     var failureExpectation: XCTestExpectation?
 
-    override func fetch(_ request: NSFetchRequest<NSFetchRequestResult>) throws -> [Any] {
+    override
+    open func execute(_ request: NSPersistentStoreRequest) throws -> NSPersistentStoreResult {
         fetchExpectation?.fulfill()
         guard success else {
             failureExpectation?.fulfill()
             throw fetchError!
         }
         successExpectation?.fulfill()
-        return returnedObjects!
+
+        return MockFetchResult(results: returnedObjects)
     }
 }
