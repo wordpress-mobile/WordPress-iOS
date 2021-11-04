5c830f72b6be9139528d04be0eff6958f1976233
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2021-11-04 11:22:56 -0600

    Update .buildkite/commands/build-for-testing.sh
    Co-authored-by: Olivier Halligon <olivier.halligon@automattic.com>

--- .buildkite/commands/build-for-testing.sh
+++ .buildkite/commands/build-for-testing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
-# Temporary fix until we're on the Xcode 13.1 VM
+# FIXIT-13.1: Temporary fix until we're on the Xcode 13.1 VM
 echo "--- :rubygems: Fixing Ruby Setup"
 gem install bundler
 
