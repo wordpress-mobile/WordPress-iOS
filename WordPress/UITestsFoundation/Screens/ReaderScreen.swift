a80fdedc60787768a4f025d78834a736336d9ffb
Tiago Marques <tiago.marques@automattic.com>
2021-11-22 22:20:09 -0300

    Add reader ui critical tests

--- WordPress/UITestsFoundation/Screens/ReaderScreen.swift
+++ WordPress/UITestsFoundation/Screens/ReaderScreen.swift
@@ -20,6 +20,50 @@ public class ReaderScreen: ScreenObject {
         )
     }
 
+    public func openTheLastPostInApp() {
+        getTheLastPost().tap()
+    }
+
+    public func openTheLastPostInSafari() {
+        getTheLastPost().buttons["More"].tap()
+        app.buttons["Visit"].tap()
+    }
+
+    public func getTheLastPost() -> XCUIElement {
+        let postPosition = app.cells.count - 1
+        let post = app.cells.element(boundBy: postPosition)
+
+        scrollDownUntilElementIsHittable(element: post)
+        return post
+    }
+
+    private func scrollDownUntilElementIsHittable(element: XCUIElement) {
+        var loopCount = 0
+        while !element.waitForIsHittable(timeout: 3) && loopCount < 10 {
+            loopCount += 1
+            app.swipeUp(velocity: .fast)
+            print(loopCount)
+        }
+    }
+
+    public func postContentEquals(expected: String) -> Bool {
+        let equalsPostContent = NSPredicate(format: "label == %@", expected)
+        let isPostContentEqual = app.staticTexts.element(matching: equalsPostContent).waitForIsHittable(timeout: 3)
+
+        return isPostContentEqual
+    }
+
+    public func dismissPost() {
+        let backButton = app.buttons["Back"]
+        let dismissButton = app.buttons["Dismiss"]
+
+        if dismissButton.isHittable {
+            dismissButton.tap()
+        } else if backButton.isHittable {
+            backButton.tap()
+        }
+    }
+
     public static func isLoaded() -> Bool {
         (try? ReaderScreen().isLoaded) ?? false
     }
