b5be219f2148b9338be0bff0ac399fe2df4100d6
Olivier Halligon <olivier.halligon@automattic.com>
2021-11-25 23:46:23 +0100

    Better documentation for AppLocalizedString helper

--- WordPress/WordPressStatsWidgets/Views/Localization/AppLocalizedString.swift
+++ WordPress/WordPressStatsWidgets/Views/Localization/AppLocalizedString.swift
@@ -11,8 +11,16 @@ extension Bundle {
     }()
 }
 
-typealias LocalizedString = String // Useful only to express intent on your API; does not provide any compile-time guarantee
+/// This is mostly useful to express *intent* on your API. It does not provide any compile-time guarantee
+typealias LocalizedString = String
 
-func AppLocalizedString(_ key: String, value: String?, comment: String) -> LocalizedString {
+/// Be sure to use this function instead of `NSLocalizedString` in order to reference localized strings **from the app bundle**.
+/// As `NSLocalisedString` by default will look up strings in the current (main) bundle, which could be an App Extension for cases like a Widget
+/// but, in order to avoid duplicating our strings accross targets, it is better to make App Extensions / Widgets reference the strings
+/// from the `Localizable.strings` files that are hosted the app bundle instead of hosting their own `.strings` file.
+///
+/// Also, be sure to pass this function name as a custom routine when parsing the code to generate the main `.strings` file,
+/// using `genstrings -s AppLocalizedString` to that this helper method is recognized.
+func AppLocalizedString(_ key: String, tableName: String? = nil, value: String? = nil, comment: String) -> LocalizedString {
     Bundle.app.localizedString(forKey: key, value: value, table: nil)
 }
