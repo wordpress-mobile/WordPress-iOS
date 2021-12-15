8b4861b67d37197553cbfae213866b4ec59cc47b
Olivier Halligon <olivier.halligon@automattic.com>
2021-12-15 21:48:16 +0100

    🚨 TO REVERT — Introduce intentional violation to ensure that SwiftLint and Hound will bark at it.

--- WordPress/Classes/ViewRelated/Stats/Extensions/NoResultsViewController+StatsModule.swift
+++ WordPress/Classes/ViewRelated/Stats/Extensions/NoResultsViewController+StatsModule.swift
@@ -26,7 +26,7 @@ extension NoResultsViewController {
             static let title = AppLocalizedString("Looking for stats?", comment: "Title for the error view when the stats module is disabled.")
             static let subtitle = AppLocalizedString("Enable site stats to see detailed information about your traffic, likes, comments, and subscribers.", comment:
                                                       "Error message shown when trying to view Stats and the stats module is disabled.")
-            static let buttonTitle = AppLocalizedString("Enable Site Stats", comment: "Title for the button that will enable the site stats module.")
+            static let buttonTitle = NSLocalizedString("Enable Site Stats", comment: "Title for the button that will enable the site stats module.")
 
         }
 
