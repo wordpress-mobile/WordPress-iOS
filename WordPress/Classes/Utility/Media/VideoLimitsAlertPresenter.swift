8bc553891a7100481e1349a3b647c2ba0b032ba0
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2021-12-20 11:02:09 -0600

    Fixed typo in VideoLimitsAlertPresenter

--- WordPress/Classes/Utility/Media/VideoLimitsAlertPresenter.swift
+++ WordPress/Classes/Utility/Media/VideoLimitsAlertPresenter.swift
@@ -11,21 +11,21 @@ extension VideoLimitsAlertPresenter {
     func presentVideoLimitExceededFromPicker(on viewController: UIViewController) {
         let title = NSLocalizedString("Selection not allowed",
                                       comment: "Title of an alert informing users that the video they are trying to select is not allowed.")
-        persentVideoLimitsAlert(on: viewController, title: title)
+        presentVideoLimitsAlert(on: viewController, title: title)
     }
 
     /// Alerts users that the video they just recorded exceeds the allowed duration
     func presentVideoLimitExceededAfterCapture(on viewController: UIViewController) {
         let title = NSLocalizedString("Video not uploaded",
                                       comment: "Title of an alert informing users that the video they are trying to select is not allowed.")
-        persentVideoLimitsAlert(on: viewController, title: title)
+        presentVideoLimitsAlert(on: viewController, title: title)
     }
 
     /// Builds and presents an alert for users trying to upload a video that exceeds allowed limits
     /// - Parameters:
     ///   - viewController: presenting UIViewController
     ///   - title: title of the alert
-    private func persentVideoLimitsAlert(on viewController: UIViewController, title: String) {
+    private func presentVideoLimitsAlert(on viewController: UIViewController, title: String) {
         let message = NSLocalizedString("Uploading videos longer than 5 minutes requires a paid plan.",
                                         comment: "Message of an alert informing users that the video they are trying to select is not allowed.")
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
