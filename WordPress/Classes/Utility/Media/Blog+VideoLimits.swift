142843fc2f20fa3c3b1b0473688c7d21f3056451
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2021-12-20 18:18:59 -0600

    Update Blog+VideoLimits, fixed typo in comment

--- WordPress/Classes/Utility/Media/Blog+VideoLimits.swift
+++ WordPress/Classes/Utility/Media/Blog+VideoLimits.swift
@@ -1,6 +1,6 @@
 extension Blog {
 
-    /// returns true if the blog is allowed upload the given asset, true otherwise
+    /// returns true if the blog is allowed to upload the given asset, true otherwise
     func canUploadAsset(_ asset: WPMediaAsset) -> Bool {
         hasPaidPlan || !asset.exceedsFreeSitesAllowance()
     }
