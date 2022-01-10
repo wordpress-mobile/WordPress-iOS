1fb5728ea0a3c903fd06f898b60675b9d57e421c
Leandro Alonso <contato@leandroalonso.com>
2022-01-10 19:39:08 -0300

    add: a VC to display 3 posts

---
+++ WordPress/Classes/ViewRelated/Blog/Blog Details/PostsCardViewController.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+/// Render a small list of posts for a given blog and post status (drafts or scheduled)
+///
+/// This class handles showing posts from the database, syncing and interacting with them
+///
+@objc class PostsCardViewController: UIViewController {
+    @objc var blog: Blog?
+
+    private let postsTableView = IntrinsicTableView()
+
+    private var viewModel: PostsCardViewModel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+        viewModel = PostsCardViewModel(tableView: postsTableView, blog: blog!)
+        viewModel.viewDidLoad()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        postsTableView.dataSource = viewModel
+        viewModel.refresh()
+    }
+}
+
+private extension PostsCardViewController {
+    func configureView() {
+        view.addSubview(postsTableView)
+        postsTableView.translatesAutoresizingMaskIntoConstraints = false
+        postsTableView.pinSubviewToAllEdges(view)
+        let postCompactCellNib = UINib(nibName: "PostCompactCell", bundle: Bundle.main)
+        postsTableView.register(postCompactCellNib, forCellReuseIdentifier: "PostCompactCellIdentifier")
+    }
+}
