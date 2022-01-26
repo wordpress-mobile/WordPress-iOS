7fa0d678f89319e903f22db1f3b66ef742c394f0
Leandro Alonso <contato@leandroalonso.com>
2022-01-25 22:11:20 -0300

    add: quick actions cell

---
+++ WordPress/Classes/ViewRelated/Blog/Blog Details/BlogDetailsViewController+QuickActions.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+extension BlogDetailsViewController {
+    @objc func quickActionsSectionViewModel() -> BlogDetailsSection {
+        return BlogDetailsSection(title: nil,
+                                  rows: [BlogDetailsRow()],
+                                  footerTitle: nil,
+                                  category: .quickAction)
+    }
+}
+
+@objc class QuickActionsCell: UITableViewCell {
+    private var actionRow: ActionRow!
+
+    func configure(with items: [ActionRow.Item]) {
+        guard actionRow == nil else {
+            return
+        }
+
+        actionRow = ActionRow(items: items)
+        contentView.addSubview(actionRow)
+
+        setupConstraints()
+    }
+
+    private func setupConstraints() {
+        actionRow.translatesAutoresizingMaskIntoConstraints = false
+
+        let widthConstraint = actionRow.widthAnchor.constraint(equalToConstant: 350)
+        widthConstraint.priority = .defaultHigh
+
+        NSLayoutConstraint.activate([
+            actionRow.topAnchor.constraint(equalTo: contentView.topAnchor),
+            actionRow.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            actionRow.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            widthConstraint
+        ])
+    }
+}
