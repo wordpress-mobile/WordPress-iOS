23e8f53fe4667b1af09c0addabf33051d4552dd1
emilylaguna <emily.laguna@automattic.com>
2022-02-08 17:07:55 -0500

    Add AppConfig option to show or hide the add self hosted site button

--- WordPress/Jetpack/AppConfiguration.swift
+++ WordPress/Jetpack/AppConfiguration.swift
@@ -5,6 +5,7 @@ import Foundation
  */
 @objc class AppConfiguration: NSObject {
     @objc static let isJetpack: Bool = true
+    @objc static let showJetpackSitesOnly: Bool = false
     @objc static let allowsNewPostShortcut: Bool = false
     @objc static let allowsConnectSite: Bool = false
     @objc static let allowSiteCreation: Bool = false
