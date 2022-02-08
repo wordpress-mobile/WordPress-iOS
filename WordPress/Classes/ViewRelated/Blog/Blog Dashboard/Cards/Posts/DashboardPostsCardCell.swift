1b9a7c5385a87cb8bd124debdb1577d3681dd21d
Leandro Alonso <contato@leandroalonso.com>
2022-02-08 11:23:31 -0300

    add: configure draft and schedule for posts card

--- WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Posts/DashboardPostsCardCell.swift
+++ WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Posts/DashboardPostsCardCell.swift
@@ -2,11 +2,21 @@ import UIKit
 
 class DashboardPostsCardCell: UICollectionViewCell, Reusable, BlogDashboardCardConfigurable {
     func configure(blog: Blog, viewController: BlogDashboardViewController?, dataModel: NSDictionary?) {
-        guard let viewController = viewController else {
+        guard let viewController = viewController, let dataModel = dataModel else {
             return
         }
 
+        let hasDrafts = (dataModel["draft"] as? Array<Any>)?.count ?? 0 > 0
+        let hasScheduled = (dataModel["scheduled"] as? Array<Any>)?.count ?? 0 > 0
+
         let postsViewController = PostsCardViewController(blog: blog)
+
+        if hasDrafts {
+            postsViewController.status = .draft
+        } else if hasScheduled {
+            postsViewController.status = .scheduled
+        }
+
         viewController.addChild(postsViewController)
         contentView.addSubview(postsViewController.view)
         postsViewController.view.translatesAutoresizingMaskIntoConstraints = false
