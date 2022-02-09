b759a72394f98d0a09acba256870fb731cd04503
Leandro Alonso <contato@leandroalonso.com>
2022-02-09 13:38:29 -0300

    add: create one single instance of postsViewController and configure it

--- WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Posts/DashboardPostsCardCell.swift
+++ WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Posts/DashboardPostsCardCell.swift
@@ -1,26 +1,38 @@
 import UIKit
 
 class DashboardPostsCardCell: UICollectionViewCell, Reusable, BlogDashboardCardConfigurable {
+    private var postsViewController: PostsCardViewController?
+
     func configure(blog: Blog, viewController: BlogDashboardViewController?, dataModel: NSDictionary?) {
         guard let viewController = viewController, let dataModel = dataModel else {
             return
         }
 
+        /// Create the child VC in case it doesn't exist
+        if postsViewController == nil {
+            let postsViewController = PostsCardViewController(blog: blog)
+            self.postsViewController = postsViewController
+            showDraftsOrScheduled(dataModel)
+
+            // Embed in this cell and configure as a child VC
+            viewController.addChild(postsViewController)
+            contentView.addSubview(postsViewController.view)
+            postsViewController.view.translatesAutoresizingMaskIntoConstraints = false
+            contentView.pinSubviewToAllEdges(postsViewController.view)
+            postsViewController.didMove(toParent: viewController)
+        } else {
+            showDraftsOrScheduled(dataModel)
+        }
+    }
+
+    private func showDraftsOrScheduled(_ dataModel: NSDictionary) {
         let hasDrafts = dataModel["show_drafts"] as? Bool ?? false
         let hasScheduled = dataModel["show_scheduled"] as? Bool ?? false
 
-        let postsViewController = PostsCardViewController(blog: blog)
-
         if hasDrafts {
-            postsViewController.status = .draft
+            postsViewController?.status = .draft
         } else if hasScheduled {
-            postsViewController.status = .scheduled
+            postsViewController?.status = .scheduled
         }
-
-        viewController.addChild(postsViewController)
-        contentView.addSubview(postsViewController.view)
-        postsViewController.view.translatesAutoresizingMaskIntoConstraints = false
-        contentView.pinSubviewToAllEdges(postsViewController.view)
-        postsViewController.didMove(toParent: viewController)
     }
 }
