693c538694163e2e686780a3c52857d8bec5e192
James Frost <james@jamesfrost.co.uk>
2022-02-10 09:40:00 +0000

    Fix stats insights log message

--- WordPress/Classes/Stores/StatsInsightsStore.swift
+++ WordPress/Classes/Stores/StatsInsightsStore.swift
@@ -726,7 +726,7 @@ private extension StatsInsightsStore {
         let expired = interval > StatsInsightsStore.cacheTTL
 
         let intervalLogMessage = "(\(String(format: "%.2f", interval))s since last refresh)"
-        DDLogInfo("Stats: Insights cache for site \(siteID) \(expired ? "has " : "")expired \(intervalLogMessage)")
+        DDLogInfo("Stats: Insights cache for site \(siteID) has \(expired ? "" : "not ")expired \(intervalLogMessage)")
 
         return expired
     }
