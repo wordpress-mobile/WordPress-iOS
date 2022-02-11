cfb9ced370cf67619f873c2ad91d34965f97fd79
Momo Ozawa <momo.ozawa@automattic.com>
2022-02-11 12:23:03 +0000

    Refactor: extract Quick Actions extension to it's own file

--- WordPress/Classes/ViewRelated/Blog/Blog Dashboard/BlogDashboardViewController.swift
+++ WordPress/Classes/ViewRelated/Blog/Blog Dashboard/BlogDashboardViewController.swift
@@ -1,5 +1,4 @@
 import UIKit
-import WordPressShared
 
 typealias DashboardCollectionViewCell = UICollectionViewCell & Reusable & BlogDashboardCardConfigurable
 
@@ -76,56 +75,6 @@ final class BlogDashboardViewController: UIViewController {
 
     @objc private func updateCollectionViewHeight(notification: Notification) {
         collectionView.collectionViewLayout.invalidateLayout()
-    }
-}
-
-// MARK: - Quick Actions
-
-extension BlogDashboardViewController {
-
-    func showStats() {
-        trackQuickActionsEvent(.statsAccessed)
-
-        let controller = StatsViewController()
-        controller.blog = blog
-        controller.navigationItem.largeTitleDisplayMode = .never
-        showDetailViewController(controller, sender: self)
-
-        QuickStartTourGuide.shared.visited(.stats)
-    }
-
-    func showPostList() {
-        trackQuickActionsEvent(.openedPosts)
-
-        let controller = PostListViewController.controllerWithBlog(blog)
-        controller.navigationItem.largeTitleDisplayMode = .never
-        showDetailViewController(controller, sender: self)
-
-        QuickStartTourGuide.shared.visited(.blogDetailNavigation)
-    }
-
-    func showMediaLibrary() {
-        trackQuickActionsEvent(.openedMediaLibrary)
-
-        let controller = MediaLibraryViewController(blog: blog)
-        controller.navigationItem.largeTitleDisplayMode = .never
-        showDetailViewController(controller, sender: self)
-
-        QuickStartTourGuide.shared.visited(.blogDetailNavigation)
-    }
-
-    func showPageList() {
-        trackQuickActionsEvent(.openedPages)
-
-        let controller = PageListViewController.controllerWithBlog(blog)
-        controller.navigationItem.largeTitleDisplayMode = .never
-        showDetailViewController(controller, sender: self)
-
-        QuickStartTourGuide.shared.visited(.pages)
-    }
-
-    private func trackQuickActionsEvent(_ event: WPAnalyticsStat) {
-        WPAppAnalytics.track(event, withProperties: [WPAppAnalyticsKeyTapSource: "dashboard"], with: blog)
     }
 }
 
