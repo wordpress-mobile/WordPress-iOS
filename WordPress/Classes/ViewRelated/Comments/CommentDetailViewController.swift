4fe743cac499d78c74ce4e4864483284628ecaf5
Stephenie Harris <scoutharris@gmail.com>
2022-02-16 13:45:38 -0700

    When the header is tapped, show the comment thread with the Notification comment highlighted.

--- WordPress/Classes/ViewRelated/Comments/CommentDetailViewController.swift
+++ WordPress/Classes/ViewRelated/Comments/CommentDetailViewController.swift
@@ -570,6 +570,26 @@ private extension CommentDetailViewController {
 
     // MARK: Actions and navigations
 
+    // Shows the comment thread with the Notification comment highlighted.
+    func navigateToNotificationComment() {
+        guard let siteID = siteID,
+              let blog = comment.blog,
+              blog.supports(.wpComRESTAPI) else {
+                  openWebView(for: comment.commentURL())
+                  return
+              }
+
+        // Empty Back Button
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: String(), style: .plain, target: nil, action: nil)
+
+        try? contentCoordinator.displayCommentsWithPostId(NSNumber(value: comment.postID),
+                                                          siteID: siteID,
+                                                          commentID: NSNumber(value: comment.commentID),
+                                                          source: .commentNotification)
+    }
+
+
+
     // Shows the comment thread with the parent comment highlighted.
     func navigateToParentComment() {
         guard let parentComment = parentComment,
@@ -596,7 +616,7 @@ private extension CommentDetailViewController {
         try? contentCoordinator.displayCommentsWithPostId(NSNumber(value: comment.postID),
                                                           siteID: siteID,
                                                           commentID: NSNumber(value: replyID),
-                                                          source: .mySiteComment)
+                                                          source: isNotificationComment ? .commentNotification : .mySiteComment)
     }
 
     func navigateToPost() {
@@ -915,14 +935,15 @@ extension CommentDetailViewController: UITableViewDelegate, UITableViewDataSourc
 
         switch rows[indexPath.row] {
         case .header:
-            comment.hasParentComment() ? navigateToParentComment() : navigateToPost()
-
+            if isNotificationComment {
+                navigateToNotificationComment()
+            } else {
+                comment.hasParentComment() ? navigateToParentComment() : navigateToPost()
+            }
         case .replyIndicator:
             navigateToReplyComment()
-
         case .text(let title, _, _) where title == .webAddressLabelText:
             visitAuthorURL()
-
         default:
             break
         }
