9b261c1684aa07643b4b230496a97f7ab2a7bf1b
Ovi Trif <octrif@gmail.com>
2022-02-21 21:49:25 +0100

    Fix text alignment for RTL languages

--- WordPress/Classes/ViewRelated/Site Creation/Shared/TitleSubtitleTextfieldHeader.swift
+++ WordPress/Classes/ViewRelated/Site Creation/Shared/TitleSubtitleTextfieldHeader.swift
@@ -76,6 +76,10 @@ final class SearchTextField: UITextField {
         autocorrectionType = .no
         adjustsFontForContentSizeCategory = true
 
+        if effectiveUserInterfaceLayoutDirection == .rightToLeft {
+            textAlignment = .right
+        }
+
         setIconImage(view: searchIconImageView)
 
         NSLayoutConstraint.activate([
