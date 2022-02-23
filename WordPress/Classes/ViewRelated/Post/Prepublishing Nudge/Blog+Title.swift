14b99a58fc699e2a77c30325027fa05c5c1cb53f
sla8c <steve.lum@a8c.com>
2022-02-23 20:02:52 +0100

    small refactor to blog title

--- WordPress/Classes/ViewRelated/Post/Prepublishing Nudge/Blog+Title.swift
+++ WordPress/Classes/ViewRelated/Post/Prepublishing Nudge/Blog+Title.swift
@@ -4,9 +4,7 @@ extension Blog {
 
     /// The title of the blog
     var title: String? {
-        let blogName = settings?.name
-
-        guard let blogName = blogName, !blogName.isEmpty else {
+        guard let blogName = settings?.name, !blogName.isEmpty else {
             return displayURL as String?
         }
 
