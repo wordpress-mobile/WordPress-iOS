cb914895ea0527f382d910c38d226ba56301fdc2
Leandro Alonso <contato@leandroalonso.com>
2022-03-01 15:01:58 -0300

    add: ghost card cell complete view

--- WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Ghost/DashboardGhostCardCell.swift
+++ WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Ghost/DashboardGhostCardCell.swift
@@ -1,7 +1,29 @@
 import Foundation
+import UIKit
 
 class DashboardGhostCardCell: UICollectionViewCell, Reusable, BlogDashboardCardConfigurable {
-    func configure(blog: Blog, viewController: BlogDashboardViewController?, apiResponse: BlogDashboardRemoteEntity?) {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        let frameView = BlogDashboardCardFrameView()
 
+        let content = DashboardGhostCardContent.loadFromNib()
+        frameView.translatesAutoresizingMaskIntoConstraints = false
+        frameView.hideHeader()
+        frameView.add(subview: content)
+
+        contentView.addSubview(frameView)
+        contentView.pinSubviewToAllEdges(frameView)
+
+        isAccessibilityElement = false
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(blog: Blog, viewController: BlogDashboardViewController?, apiResponse: BlogDashboardRemoteEntity?) {
+        startGhostAnimation(style: GhostCellStyle.muriel)
     }
 }
+
+class DashboardGhostCardContent: UIView, NibLoadable { }
