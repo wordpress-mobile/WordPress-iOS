f9c6acbcabed69ff320ca4d12903149d50f25db4
Leandro Alonso <contato@leandroalonso.com>
2022-03-02 13:57:53 -0300

    add: rename properties and add comments

--- WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Helpers/BlogDashboardState.swift
+++ WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Helpers/BlogDashboardState.swift
@@ -3,8 +3,11 @@ import Foundation
 class BlogDashboardState {
     static let shared = BlogDashboardState()
 
+    /// If the dashboard is being loaded for the first time
     var firstTimeLoading = false
-    var initialLoadingFailed = false
+
+    /// If loading the cards in the dashboard failed
+    var loadingFailed = false
 
     private init() { }
 }
