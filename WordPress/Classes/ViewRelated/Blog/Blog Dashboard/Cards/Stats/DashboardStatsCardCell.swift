aa939e7b1a6ba092235aa14f16e07cedd17d82e5
Hassaan El-Garem <hassaanelgarem@gmail.com>
2022-03-03 02:29:05 +0200

    Add: Configure UI for dashboard stats card

--- WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Stats/DashboardStatsCardCell.swift
+++ WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Stats/DashboardStatsCardCell.swift
@@ -1,7 +1,96 @@
 import UIKit
 
-class DashboardStatsCardCell: UICollectionViewCell, Reusable, BlogDashboardCardConfigurable {
-    func configure(blog: Blog, viewController: BlogDashboardViewController?, apiResponse: BlogDashboardRemoteEntity?) {
+class DashboardStatsCardCell: UICollectionViewCell, Reusable {
 
+    // MARK: Private Variables
+
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = Constants.spacing
+        return stackView
+    }()
+
+    // MARK: Initializers
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(stackView)
+        contentView.pinSubviewToAllEdges(stackView)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
+
+// MARK: BlogDashboardCardConfigurable
+
+extension DashboardStatsCardCell: BlogDashboardCardConfigurable {
+    func configure(blog: Blog, viewController: BlogDashboardViewController?, apiResponse: BlogDashboardRemoteEntity?) {
+        guard let _ = viewController, let _ = apiResponse else {
+            return
+        }
+
+        // TODO: Use apiResponse to create a View Model and use it to populate the cell
+
+        clearFrames()
+        addTodayStatsCard()
+
+        // TODO: Add grow your audience card if needed
+    }
+
+    /// Remove any card frame, if present
+    private func clearFrames() {
+        stackView.removeAllSubviews()
+    }
+
+    private func addTodayStatsCard() {
+        let frameView = BlogDashboardCardFrameView()
+        frameView.title = Strings.statsTitle
+        frameView.icon = UIImage.gridicon(.statsAlt, size: Constants.iconSize)
+        stackView.addArrangedSubview(frameView)
+        let views = statsViews()
+        let statsStackview = createStatsStackView(arrangedSubviews: views)
+        frameView.add(subview: statsStackview)
+    }
+
+    private func createStatsStackView(arrangedSubviews: [UIView]) -> UIStackView {
+        let stackview = UIStackView(arrangedSubviews: arrangedSubviews)
+        stackview.axis = .horizontal
+        stackview.translatesAutoresizingMaskIntoConstraints = false
+        stackview.distribution = .fillEqually
+        stackview.isLayoutMarginsRelativeArrangement = true
+        stackview.directionalLayoutMargins = Constants.statsStackViewMargins
+        return stackview
+    }
+
+    // TODO: Data is now static. It should be brought in from the view model.
+    // View model should also return data for comments in the case of an iPad.
+    private func statsViews() -> [UIView] {
+        let viewsStatsView = DashboardSingleStatView(countString: "1,492", title: Strings.viewsTitle)
+        let visitorsStatsView = DashboardSingleStatView(countString: "885", title: Strings.visitorsTitle)
+        let likesStatsView = DashboardSingleStatView(countString: "112", title: Strings.likesTitle)
+        return [viewsStatsView, visitorsStatsView, likesStatsView]
+    }
+}
+
+// MARK: Constants
+
+private extension DashboardStatsCardCell {
+
+    enum Strings {
+        static let statsTitle = NSLocalizedString("Today's Stats", comment: "Title for the card displaying today's stats.")
+        static let viewsTitle = NSLocalizedString("Views", comment: "Today's Stats 'Views' label")
+        static let visitorsTitle = NSLocalizedString("Visitors", comment: "Today's Stats 'Visitors' label")
+        static let likesTitle = NSLocalizedString("Likes", comment: "Today's Stats 'Likes' label")
+        static let commentsTitle = NSLocalizedString("Comments", comment: "Today's Stats 'Comments' label")
+    }
+
+    enum Constants {
+        static let spacing: CGFloat = 20
+        static let iconSize = CGSize(width: 18, height: 18)
+        static let statsStackViewMargins = NSDirectionalEdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16)
     }
 }
