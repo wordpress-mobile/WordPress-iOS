fdb319aa2d0885fb5acbdad5d6b7f31bc4da575e
Olivier Halligon <olivier.halligon@automattic.com>
2022-03-14 21:34:22 +0100

    Adopt the new feature of merge/extract actions
    Using key prefixes for manually-maintained `.strings` files being merged within the generated `Localizable.strings`

--- fastlane/lanes/localization-lanes.rb
+++ fastlane/lanes/localization-lanes.rb
@@ -12,6 +12,8 @@ GLOTPRESS_WORDPRESS_METADATA_PROJECT_URL = 'https://translate.wordpress.org/proj
 GLOTPRESS_JETPACK_METADATA_PROJECT_URL = 'https://translate.wordpress.com/projects/jetpack/apps/ios/release-notes/',
 
 # List of locales used for the app strings
+# TODO: Replace with `LocaleHelper` once provided by release toolkit (https://github.com/wordpress-mobile/release-toolkit/pull/296)
+#
 GLOTPRESS_TO_LPROJ_APP_LOCALE_CODES = {
   'ar' => 'ar',         # Arabic
   'bg' => 'bg',         # Bulgarian
@@ -48,3 +50,14 @@ GLOTPRESS_TO_LPROJ_APP_LOCALE_CODES = {
   'zh-tw' => 'zh-Hant', # Chinese (Taiwan)
 }.freeze
 
+
+  # List of `.strings` files manually maintained by developers (as opposed to being automatically extracted from code and generated)
+  # which we will merge into the main `Localizable.strings` file imported by GlotPress, then extract back once we download the translations.
+  # Each `.strings` file to be merged/extracted is associated with a prefix to add the the keys being used to avoid conflicts and differentiate.
+  # See calls to `ios_merge_strings_files` and `ios_extract_keys_from_strings_files` for usage.
+  #
+  MANUALLY_MAINTAINED_STRINGS_FILES = {
+    File.join('WordPress', 'Resources', 'en.lproj', 'InfoPlist.strings') => 'infoplist.', # For now WordPress and Jetpack share the same InfoPlist.strings
+    File.join('WordPress', 'WordPressDraftActionExtension', 'en.lproj', 'InfoPlist.strings') => 'ios-sharesheet.', # CFBundleDisplayName for the "Save as Draft" share action
+    File.join('WordPress', 'WordPressIntents', 'en.lproj', 'Sites.strings') => 'ios-widget.' # Strings from the `.intentdefinition`, used for configuring the iOS Widget
+  }.freeze
