618704235d4ad30ef2c7a1c9f092a6e8969cbb24
Olivier Halligon <olivier.halligon@automattic.com>
2022-03-15 06:52:23 +0100

    Update fastlane/lanes/localization-lanes.rb
    Co-authored-by: Gio Lodi <giovanni.lodi42@gmail.com>

--- fastlane/lanes/localization-lanes.rb
+++ fastlane/lanes/localization-lanes.rb
@@ -51,13 +51,13 @@ GLOTPRESS_TO_LPROJ_APP_LOCALE_CODES = {
 }.freeze
 
 
-  # List of `.strings` files manually maintained by developers (as opposed to being automatically extracted from code and generated)
-  # which we will merge into the main `Localizable.strings` file imported by GlotPress, then extract back once we download the translations.
-  # Each `.strings` file to be merged/extracted is associated with a prefix to add the the keys being used to avoid conflicts and differentiate.
-  # See calls to `ios_merge_strings_files` and `ios_extract_keys_from_strings_files` for usage.
-  #
-  MANUALLY_MAINTAINED_STRINGS_FILES = {
-    File.join('WordPress', 'Resources', 'en.lproj', 'InfoPlist.strings') => 'infoplist.', # For now WordPress and Jetpack share the same InfoPlist.strings
-    File.join('WordPress', 'WordPressDraftActionExtension', 'en.lproj', 'InfoPlist.strings') => 'ios-sharesheet.', # CFBundleDisplayName for the "Save as Draft" share action
-    File.join('WordPress', 'WordPressIntents', 'en.lproj', 'Sites.strings') => 'ios-widget.' # Strings from the `.intentdefinition`, used for configuring the iOS Widget
-  }.freeze
+# List of `.strings` files manually maintained by developers (as opposed to being automatically extracted from code and generated)
+# which we will merge into the main `Localizable.strings` file imported by GlotPress, then extract back once we download the translations.
+# Each `.strings` file to be merged/extracted is associated with a prefix to add the the keys being used to avoid conflicts and differentiate.
+# See calls to `ios_merge_strings_files` and `ios_extract_keys_from_strings_files` for usage.
+#
+MANUALLY_MAINTAINED_STRINGS_FILES = {
+  File.join('WordPress', 'Resources', 'en.lproj', 'InfoPlist.strings') => 'infoplist.', # For now WordPress and Jetpack share the same InfoPlist.strings
+  File.join('WordPress', 'WordPressDraftActionExtension', 'en.lproj', 'InfoPlist.strings') => 'ios-sharesheet.', # CFBundleDisplayName for the "Save as Draft" share action
+  File.join('WordPress', 'WordPressIntents', 'en.lproj', 'Sites.strings') => 'ios-widget.' # Strings from the `.intentdefinition`, used for configuring the iOS Widget
+}.freeze
