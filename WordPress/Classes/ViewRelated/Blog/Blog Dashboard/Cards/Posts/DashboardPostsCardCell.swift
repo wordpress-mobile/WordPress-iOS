ebe920fc0076379641d4960e75e6cebe84c2fa2c
Leandro Alonso <contato@leandroalonso.com>
2022-03-18 15:15:09 -0300

    add: track opened posts from the card

--- WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Posts/DashboardPostsCardCell.swift
+++ WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Posts/DashboardPostsCardCell.swift
@@ -133,6 +133,7 @@ extension DashboardPostsCardCell: BlogDashboardCardConfigurable {
         }
 
         PostListViewController.showForBlog(blog, from: viewController, withPostStatus: status)
+        WPAppAnalytics.track(.openedPosts, withProperties: ["tab_source": "dashboard", "tap_source": "posts_card"], with: blog)
     }
 
     private enum Strings {
