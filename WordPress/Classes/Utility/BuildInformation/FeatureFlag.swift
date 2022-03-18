538820d52bdc30700794f32abde66b88f3637815
David Christiandy <1299411+dvdchr@users.noreply.github.com>
2022-03-18 23:18:54 +0700

    Remove commentThreadModerationMenu feature flag

--- WordPress/Classes/Utility/BuildInformation/FeatureFlag.swift
+++ WordPress/Classes/Utility/BuildInformation/FeatureFlag.swift
@@ -17,7 +17,6 @@ enum FeatureFlag: Int, CaseIterable, OverrideableFlag {
     case domains
     case timeZoneSuggester
     case aboutScreen
-    case commentThreadModerationMenu
     case mySiteDashboard
     case markAllNotificationsAsRead
     case mediaPickerPermissionsNotice
@@ -62,8 +61,6 @@ enum FeatureFlag: Int, CaseIterable, OverrideableFlag {
         case .timeZoneSuggester:
             return true
         case .aboutScreen:
-            return true
-        case .commentThreadModerationMenu:
             return true
         case .mySiteDashboard:
             return false
@@ -133,8 +130,6 @@ extension FeatureFlag {
             return "TimeZone Suggester"
         case .aboutScreen:
             return "New Unified About Screen"
-        case .commentThreadModerationMenu:
-            return "Comment Thread Moderation Menu"
         case .mySiteDashboard:
             return "My Site Dashboard"
         case .markAllNotificationsAsRead:
