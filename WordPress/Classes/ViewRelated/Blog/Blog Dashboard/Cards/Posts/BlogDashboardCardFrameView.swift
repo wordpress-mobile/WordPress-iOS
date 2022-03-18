6de6c6046f7ebfbaccc980b554d3a2aa2d49bcb2
Hassaan El-Garem <hassaanelgarem@gmail.com>
2022-03-18 02:44:40 +0200

    Add: Ellipsis Button to `BlogDashboardCardFrameView`

--- WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Posts/BlogDashboardCardFrameView.swift
+++ WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Posts/BlogDashboardCardFrameView.swift
@@ -17,7 +17,7 @@ class BlogDashboardCardFrameView: UIView {
     /// Header in which icon, title and chevron are added
     private lazy var headerStackView: UIStackView = {
         let topStackView = UIStackView()
-        topStackView.layoutMargins = Constants.headerPadding
+        topStackView.layoutMargins = Constants.headerPaddingWithEllipsisButtonHidden
         topStackView.isLayoutMarginsRelativeArrangement = true
         topStackView.spacing = Constants.headerHorizontalSpacing
         topStackView.alignment = .center
@@ -45,7 +45,7 @@ class BlogDashboardCardFrameView: UIView {
         return titleLabel
     }()
 
-    /// Chevron displayed in case there's any action associa
+    /// Chevron displayed in case there's any action associated
     private lazy var chevronImageView: UIImageView = {
         let chevronImageView = UIImageView(image: UIImage.gridicon(.chevronRight, size: Constants.iconSize).withRenderingMode(.alwaysTemplate))
         chevronImageView.frame = CGRect(x: 0, y: 0, width: Constants.iconSize.width, height: Constants.iconSize.height)
@@ -54,6 +54,20 @@ class BlogDashboardCardFrameView: UIView {
         chevronImageView.isAccessibilityElement = false
         chevronImageView.isHidden = true
         return chevronImageView
+    }()
+
+    /// Ellipsis Button displayed on the top right corner of the view.
+    /// Displayed only when an associated action is set
+    private lazy var ellipsisButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage.gridicon(.ellipsis).imageWithTintColor(.listIcon), for: .normal)
+        button.contentEdgeInsets = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)
+        button.isAccessibilityElement = false
+        button.isHidden = true
+        button.on(.touchUpInside) { [weak self] _ in
+            self?.onEllipsisButtonTap?()
+        }
+        return button
     }()
 
     weak var currentView: UIView?
@@ -90,6 +104,14 @@ class BlogDashboardCardFrameView: UIView {
             addHeaderTapGestureIfNeeded()
         }
 
+    }
+
+    /// Closure to be called when the ellipsis button is tapped..
+    /// If set, the ellipsis button image is displayed.
+    var onEllipsisButtonTap: (() -> Void)? {
+        didSet {
+            updateEllipsisButtonState()
+        }
     }
 
     override init(frame: CGRect) {
@@ -131,12 +153,30 @@ class BlogDashboardCardFrameView: UIView {
         headerStackView.addArrangedSubviews([
             iconImageView,
             titleLabel,
-            chevronImageView
+            chevronImageView,
+            ellipsisButton
         ])
     }
 
     private func updateChevronImageState() {
         chevronImageView.isHidden = onViewTap == nil && onHeaderTap == nil
+        assertOnTapRecognitionCorrectUsage()
+    }
+
+    private func updateEllipsisButtonState() {
+        ellipsisButton.isHidden = onEllipsisButtonTap == nil
+        let headerPadding = ellipsisButton.isHidden ?
+            Constants.headerPaddingWithEllipsisButtonHidden :
+            Constants.headerPaddingWithEllipsisButtonShown
+        headerStackView.layoutMargins = headerPadding
+        assertOnTapRecognitionCorrectUsage()
+    }
+
+    /// Only one of two types of action should be associated with the card.
+    /// Either ellipsis button tap, or view/header tap
+    private func assertOnTapRecognitionCorrectUsage() {
+        let bothTypesUsed = (onViewTap != nil || onHeaderTap != nil) && onEllipsisButtonTap != nil
+        assert(!bothTypesUsed, "Using onViewTap or onHeaderTap alongside onEllipsisButtonTap is not supported and will result in unexpected behavior.")
     }
 
     private func addHeaderTapGestureIfNeeded() {
@@ -172,14 +212,15 @@ class BlogDashboardCardFrameView: UIView {
         else {
             onViewTap?()
         }
-
     }
 
     private enum Constants {
         static let bottomPadding: CGFloat = 8
-        static let headerPadding = UIEdgeInsets(top: 12, left: 16, bottom: 8, right: 16)
+        static let headerPaddingWithEllipsisButtonHidden = UIEdgeInsets(top: 12, left: 16, bottom: 8, right: 16)
+        static let headerPaddingWithEllipsisButtonShown = UIEdgeInsets(top: 12, left: 16, bottom: 8, right: 8)
         static let headerHorizontalSpacing: CGFloat = 5
         static let iconSize = CGSize(width: 18, height: 18)
         static let cornerRadius: CGFloat = 10
+        static let ellipsisButtonPadding = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)
     }
 }
