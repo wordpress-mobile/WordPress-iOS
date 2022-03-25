3e80b64d25593c87d55b10d5a1b034c33579b119
Momo Ozawa <momo.ozawa@automattic.com>
2022-03-25 13:55:11 +0900

    Add: shouldDisplayNudge property

--- WordPress/Classes/ViewRelated/Blog/Blog Dashboard/ViewModel/DashboardStatsViewModel.swift
+++ WordPress/Classes/ViewRelated/Blog/Blog Dashboard/ViewModel/DashboardStatsViewModel.swift
@@ -25,4 +25,12 @@ class DashboardStatsViewModel {
     var todaysLikes: String {
         apiResponse.todaysStats?.likes?.abbreviatedString(forHeroNumber: true) ?? "0"
     }
+
+    var shouldDisplayNudge: Bool {
+        guard let todaysStats = apiResponse.todaysStats else {
+            return false
+        }
+
+        return todaysStats.views == 0 && todaysStats.visitors == 0 && todaysStats.likes == 0
+    }
 }
