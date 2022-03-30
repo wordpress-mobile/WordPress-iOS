115374336c128345289d6ca336d362e34be867d2
Hassaan El-Garem <hassaanelgarem@gmail.com>
2022-03-30 16:39:32 +0200

    Fix: typo in `growListTours` name

--- WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Quick Start/QuickStartTourStateView.swift
+++ WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Quick Start/QuickStartTourStateView.swift
@@ -47,7 +47,7 @@ final class QuickStarTourStateView: UIView {
         }
 
         growChecklistView.configure(
-            tours: QuickStartTourGuide.shared.createGrowListTours,
+            tours: QuickStartTourGuide.shared.growListTours,
             blog: blog,
             title: Strings.growTitle,
             hint: Strings.growHint
