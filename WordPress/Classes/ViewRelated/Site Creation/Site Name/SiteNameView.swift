08bb71f497ba4562fdd9e2e545ac437ffca21518
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2022-04-04 17:51:18 -0500

    Add new files SiteNameViewController and SiteNameView

---
+++ WordPress/Classes/ViewRelated/Site Creation/Site Name/SiteNameView.swift
@@ -0,0 +1,9 @@
+//
+//  SiteNameView.swift
+//  WordPress
+//
+//  Created by Giorgio Ruscigno on 4/4/22.
+//  Copyright © 2022 WordPress. All rights reserved.
+//
+
+import Foundation
---
+++ WordPress/Classes/ViewRelated/Site Creation/Site Name/SiteNameViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SiteNameViewController.swift
+//  WordPress
+//
+//  Created by Giorgio Ruscigno on 4/4/22.
+//  Copyright © 2022 WordPress. All rights reserved.
+//
+
+import Foundation
