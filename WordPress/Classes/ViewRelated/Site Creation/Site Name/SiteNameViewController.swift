5d1d3462aa7773d46dc8062b407710fff170c9d8
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2022-04-04 18:03:21 -0500

    Update SiteNameViewController: * Add SiteNameView in loadView() * Add method removeNavigationBarBorder() * Configure titles based on vertical size class in traitCollectionDidChange(_:)

--- WordPress/Classes/ViewRelated/Site Creation/Site Name/SiteNameViewController.swift
+++ WordPress/Classes/ViewRelated/Site Creation/Site Name/SiteNameViewController.swift
@@ -1,9 +1,47 @@
-//
-//  SiteNameViewController.swift
-//  WordPress
-//
-//  Created by Giorgio Ruscigno on 4/4/22.
-//  Copyright © 2022 WordPress. All rights reserved.
-//
 
-import Foundation
+import UIKit
+
+/// Site Name screen for the Site Creation flow
+class SiteNameViewController: UIViewController {
+
+    private let creator: SiteCreator
+
+    init(creator: SiteCreator) {
+        self.creator = creator
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func loadView() {
+        // TODO: SITENAME - This string is here only to test the UI until the implementation is complete.
+        view = SiteNameView(siteName: "Commuinity & Non-Profit")
+        removeNavigationBarBorder()
+    }
+
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        title = traitCollection.verticalSizeClass == .compact ? Self.titleForVerticalCompactSizeClass : ""
+    }
+}
+
+// MARK: Convenience methods and properties
+private extension SiteNameViewController {
+
+    /// Removes the separator line at the bottom of the navigation bar
+    func removeNavigationBarBorder() {
+
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.backgroundColor = .basicBackground
+        navBarAppearance.shadowColor = .clear
+        navBarAppearance.shadowImage = UIImage()
+        navigationItem.standardAppearance = navBarAppearance
+        navigationItem.scrollEdgeAppearance = navBarAppearance
+        navigationItem.compactAppearance = navBarAppearance
+        setNeedsStatusBarAppearanceUpdate()
+    }
+
+    static let titleForVerticalCompactSizeClass = NSLocalizedString("Give your website a name",
+                                                                    comment: "Title for Site Name screen in iPhone landscape.")
+}
