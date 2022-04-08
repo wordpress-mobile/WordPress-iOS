ba18b468fc9d3dcc184fcf845308b3b0965a447b
Leandro Alonso <contato@leandroalonso.com>
2022-04-08 14:51:17 -0300

    add: documentation to dashboard analytics

--- WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Helpers/BlogDashboardAnalytics.swift
+++ WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Helpers/BlogDashboardAnalytics.swift
@@ -7,10 +7,18 @@ class BlogDashboardAnalytics {
 
     private init() {}
 
+    /// Reset the history of fired events
     func reset() {
         fired = []
     }
 
+    /// This will track the given event and properties given they haven't been
+    /// triggered before.
+    ///
+    /// - Parameters:
+    ///   - event: a `String` that represents the event name
+    ///   - properties: a `Hash` that represents the properties
+    ///   - blog: a `Blog` asssociated with the event
     func track(_ event: WPAnalyticsEvent, properties: [AnyHashable: String] = [:], blog: Blog? = nil) {
         if !fired.contains(where: { $0 == (event, properties) }) {
             fired.append((event, properties))
