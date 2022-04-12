30b05900e31e8b7be4311ff82e115f6e8e92b981
Stephenie Harris <scoutharris@gmail.com>
2022-04-12 13:54:44 -0600

    Add gradient colors to the Blogging Prompts feature introduction header image.

--- WordPress/Classes/ViewRelated/Feature Introduction/Blogging Prompts/BloggingPromptsFeatureIntroduction.swift
+++ WordPress/Classes/ViewRelated/Feature Introduction/Blogging Prompts/BloggingPromptsFeatureIntroduction.swift
@@ -17,8 +17,8 @@ class BloggingPromptsFeatureIntroduction: FeatureIntroductionViewController {
             return featureDescriptionView
         }()
 
-        let headerImage = UIImage(named: Style.headerImageName)?
-            .withTintColor(Style.headerImageTintColor)
+        let headerImage = UIImage(named: HeaderStyle.imageName)?
+            .withTintColor(.clear)
 
         super.init(headerTitle: Strings.headerTitle,
                    headerSubtitle: Strings.headerSubtitle,
@@ -32,6 +32,13 @@ class BloggingPromptsFeatureIntroduction: FeatureIntroductionViewController {
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Add the gradient after the image has been added to the view so the gradient is the correct size.
+        addHeaderImageGradient()
     }
 
 }
@@ -48,8 +55,31 @@ extension BloggingPromptsFeatureIntroduction: FeatureIntroductionDelegate {
 
 }
 
-
 private extension BloggingPromptsFeatureIntroduction {
+
+    func addHeaderImageGradient() {
+        // Based on https://stackoverflow.com/a/54096829
+        let gradient = CAGradientLayer()
+
+        gradient.colors = [
+            HeaderStyle.startGradientColor.cgColor,
+            HeaderStyle.endGradientColor.cgColor
+        ]
+
+        // Create a gradient from top to bottom.
+        gradient.startPoint = CGPoint(x: 0.5, y: 0)
+        gradient.endPoint = CGPoint(x: 0.5, y: 1)
+        gradient.frame = headerImageView.bounds
+
+        // Add a mask to the gradient so the colors only apply to the image (and not the imageView).
+        let mask = CALayer()
+        mask.contents = headerImageView.image?.cgImage
+        mask.frame = gradient.bounds
+        gradient.mask = mask
+
+        // Add the gradient as a sublayer to the imageView's layer.
+        headerImageView.layer.addSublayer(gradient)
+    }
 
     enum Strings {
         static let headerTitle: String = NSLocalizedString("Introducing Prompts", comment: "Title displayed on the feature introduction view.")
@@ -58,9 +88,10 @@ private extension BloggingPromptsFeatureIntroduction {
         static let secondaryButtonTitle: String = NSLocalizedString("Remind me", comment: "Secondary button title on the feature introduction view.")
     }
 
-    enum Style {
-        static let headerImageName = "icon-lightbulb-outline"
-        static let headerImageTintColor: UIColor = .orange // TODO: use gradient colors
+    enum HeaderStyle {
+        static let imageName = "icon-lightbulb-outline"
+        static let startGradientColor: UIColor = .warning(.shade30)
+        static let endGradientColor: UIColor = .accent(.shade40)
     }
 
 }
