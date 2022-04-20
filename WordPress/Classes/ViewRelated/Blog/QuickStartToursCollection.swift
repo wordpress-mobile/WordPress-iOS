74e3d54a6bcb1370d32d49c6d03f6f9ab7744cf3
Hassaan El-Garem <hassaanelgarem@gmail.com>
2022-04-20 17:26:41 +0200

    Refactor: Make `QuickStartToursCollection` into a protocol And add `QuickStartFactory` to be responsible for returning collections

--- WordPress/Classes/ViewRelated/Blog/QuickStartToursCollection.swift
+++ WordPress/Classes/ViewRelated/Blog/QuickStartToursCollection.swift
@@ -1,25 +1,30 @@
 import Foundation
 
-enum QuickStartType {
-    case newUser
-    case existingUser
+protocol QuickStartToursCollection {
+    var title: String { get }
+    var hint: String { get }
+    var completedImageName: String { get }
+    var analyticsKey: String { get }
+    var tours: [QuickStartTour] { get }
+
+    init(blog: Blog)
 }
 
-struct QuickStartToursCollection {
+struct QuickStartCustomizeToursCollection: QuickStartToursCollection {
     let title: String
     let hint: String
     let completedImageName: String
     let analyticsKey: String
     let tours: [QuickStartTour]
 
-    private static func customizeToursCollection(blog: Blog) -> QuickStartToursCollection {
-        let title = NSLocalizedString("Customize Your Site",
+    init(blog: Blog) {
+        self.title = NSLocalizedString("Customize Your Site",
                                       comment: "Name of the Quick Start list that guides users through a few tasks to customize their new website.")
-        let hint = NSLocalizedString("A series of steps showing you how to add a theme, site icon and more.",
+        self.hint = NSLocalizedString("A series of steps showing you how to add a theme, site icon and more.",
                                      comment: "A VoiceOver hint to explain what the user gets when they select the 'Customize Your Site' button.")
-        let completedImageName = "wp-illustration-tasks-complete-site"
-        let analyticsKey = "customize"
-        let tours: [QuickStartTour] = [
+        self.completedImageName = "wp-illustration-tasks-complete-site"
+        self.analyticsKey = "customize"
+        self.tours = [
             QuickStartCreateTour(),
             QuickStartSiteTitleTour(blog: blog),
             QuickStartSiteIconTour(),
@@ -27,17 +32,24 @@ struct QuickStartToursCollection {
             QuickStartReviewPagesTour(),
             QuickStartViewTour(blog: blog)
         ]
-        return QuickStartToursCollection(title: title, hint: hint, completedImageName: completedImageName, analyticsKey: analyticsKey, tours: tours)
     }
+}
 
-    private static func growToursCollection() -> QuickStartToursCollection {
-        let title = NSLocalizedString("Grow Your Audience",
+struct QuickStartGrowToursCollection: QuickStartToursCollection {
+    let title: String
+    let hint: String
+    let completedImageName: String
+    let analyticsKey: String
+    let tours: [QuickStartTour]
+
+    init(blog: Blog) {
+        self.title = NSLocalizedString("Grow Your Audience",
                                       comment: "Name of the Quick Start list that guides users through a few tasks to customize their new website.")
-        let hint = NSLocalizedString("A series of steps to assist with growing your site's audience.",
+        self.hint = NSLocalizedString("A series of steps to assist with growing your site's audience.",
                                      comment: "A VoiceOver hint to explain what the user gets when they select the 'Grow Your Audience' button.")
-        let completedImageName = "wp-illustration-tasks-complete-audience"
-        let analyticsKey = "grow"
-        let tours: [QuickStartTour] = [
+        self.completedImageName = "wp-illustration-tasks-complete-audience"
+        self.analyticsKey = "grow"
+        self.tours = [
             QuickStartShareTour(),
             QuickStartPublishTour(),
             QuickStartFollowTour(),
@@ -45,11 +57,5 @@ struct QuickStartToursCollection {
     // Temporarily disabled
     //        QuickStartExplorePlansTour()
         ]
-        return QuickStartToursCollection(title: title, hint: hint, completedImageName: completedImageName, analyticsKey: analyticsKey, tours: tours)
-    }
-
-    static func collections(for blog: Blog) -> [QuickStartToursCollection] {
-        // TODO: Save QuickStartType in blog. Retrieve it here and return collections accordingly
-        return [customizeToursCollection(blog: blog), growToursCollection()]
     }
 }
