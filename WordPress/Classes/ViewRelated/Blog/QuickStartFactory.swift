72e7f6eacbe023db9544f24243dc521122b46e39
Hassaan El-Garem <hassaanelgarem@gmail.com>
2022-04-21 05:06:32 +0200

    Add: QuickStartFactory's logic and tests

--- WordPress/Classes/ViewRelated/Blog/QuickStartFactory.swift
+++ WordPress/Classes/ViewRelated/Blog/QuickStartFactory.swift
@@ -1,13 +1,27 @@
 import Foundation
 
-enum QuickStartType {
+enum QuickStartType: Int {
+    case undefined
     case newUser
     case existingUser
 }
 
 class QuickStartFactory {
     static func collections(for blog: Blog) -> [QuickStartToursCollection] {
-        // TODO: Save QuickStartType in blog. Retrieve it here and return collections accordingly
-        return [QuickStartCustomizeToursCollection(blog: blog), QuickStartGrowToursCollection(blog: blog)]
+        switch blog.quickStartType {
+        case .undefined:
+            guard let completedTours = blog.completedQuickStartTours, completedTours.count > 0 else {
+                return []
+            }
+            fallthrough
+        case .newUser:
+            return [QuickStartCustomizeToursCollection(blog: blog), QuickStartGrowToursCollection(blog: blog)]
+        case .existingUser:
+            return [QuickStartGetToKnowAppCollection(blog: blog)]
+        }
+    }
+
+    static func allTours(for blog: Blog) -> [QuickStartTour] {
+        collections(for: blog).flatMap({$0.tours} )
     }
 }
