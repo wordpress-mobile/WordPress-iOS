3cb77c30f595c62c2f8ad3a7e2cdd0956679ef9c
James Frost <james.frost@automattic.com>
2022-04-28 23:25:19 +0100

    Stats Insights:  Populate Latest Post Summary cell

--- WordPress/Classes/ViewRelated/Stats/Insights/StatsLatestPostSummaryInsightsCell.swift
+++ WordPress/Classes/ViewRelated/Stats/Insights/StatsLatestPostSummaryInsightsCell.swift
@@ -8,12 +8,13 @@ class StatsLatestPostSummaryInsightsCell: StatsBaseCell, LatestPostSummaryConfig
     private var lastPostDetails: StatsPostDetails?
     private var postTitle = StatSection.noPostTitle
 
-    private var postTitleLabel: UILabel!
-    private var postTimestampLabel: UILabel!
-    private var viewCountLabel: UILabel!
-    private var likeCountLabel: UILabel!
-    private var commentCountLabel: UILabel!
-    private var postImageView: CachedAnimatedImageView!
+    private var statsStackView: UIStackView!
+    private let postTitleLabel = UILabel()
+    private let postTimestampLabel = UILabel()
+    private let viewCountLabel = UILabel()
+    private let likeCountLabel = UILabel()
+    private let commentCountLabel = UILabel()
+    private let postImageView = CachedAnimatedImageView()
 
     lazy var imageLoader: ImageLoader = {
         return ImageLoader(imageView: postImageView, gifStrategy: .mediumGIFs)
@@ -38,7 +39,9 @@ class StatsLatestPostSummaryInsightsCell: StatsBaseCell, LatestPostSummaryConfig
         contentView.addSubview(stackView)
 
         let postStackView = makePostStackView()
+        statsStackView = makeStatsStackView()
         stackView.addArrangedSubview(postStackView)
+        stackView.addArrangedSubview(statsStackView)
 
         topConstraint = stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: StatsBaseCell.Metrics.padding)
 
@@ -71,18 +74,15 @@ class StatsLatestPostSummaryInsightsCell: StatsBaseCell, LatestPostSummaryConfig
         postInfoStackView.axis = .vertical
         postInfoStackView.spacing = Metrics.postStackViewVerticalSpacing
 
-        postTitleLabel = UILabel()
         postTitleLabel.textColor = .text
         postTitleLabel.numberOfLines = 2
         postTitleLabel.font = .preferredFont(forTextStyle: .headline)
 
-        postTimestampLabel = UILabel()
         postTimestampLabel.textColor = .textSubtle
-        postTimestampLabel.font = .preferredFont(forTextStyle: .body)
+        postTimestampLabel.font = .preferredFont(forTextStyle: .subheadline)
 
         postInfoStackView.addArrangedSubviews([postTitleLabel, postTimestampLabel])
 
-        postImageView = CachedAnimatedImageView()
         postImageView.contentMode = .scaleAspectFill
         postImageView.translatesAutoresizingMaskIntoConstraints = false
 
@@ -97,6 +97,69 @@ class StatsLatestPostSummaryInsightsCell: StatsBaseCell, LatestPostSummaryConfig
         stackView.addArrangedSubviews([postInfoStackView, postImageView])
 
         return stackView
+    }
+
+    private func makeStatsStackView() -> UIStackView {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.alignment = .top
+        stackView.spacing = Metrics.postStackViewHorizontalSpacing
+
+        let viewsStack = makeVerticalStatsStackView(with: viewCountLabel, title: TextContent.views)
+        let likesStack = makeVerticalStatsStackView(with: likeCountLabel, title: TextContent.likes)
+        let commentsStack = makeVerticalStatsStackView(with: commentCountLabel, title: TextContent.comments)
+
+        let divider1 = makeVerticalDivider()
+        let divider2 = makeVerticalDivider()
+
+        stackView.addArrangedSubviews([
+            viewsStack,
+            divider1,
+            likesStack,
+            divider2,
+            commentsStack
+        ])
+
+        NSLayoutConstraint.activate([
+            viewsStack.widthAnchor.constraint(equalTo: likesStack.widthAnchor),
+            likesStack.widthAnchor.constraint(equalTo: commentsStack.widthAnchor),
+            divider1.heightAnchor.constraint(equalTo: stackView.heightAnchor),
+            divider2.heightAnchor.constraint(equalTo: stackView.heightAnchor)
+        ])
+
+        return stackView
+    }
+
+    private func makeVerticalStatsStackView(with countLabel: UILabel, title: String) -> UIStackView {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = Metrics.statsStackViewVerticalSpacing
+
+        let topLabel = UILabel()
+        topLabel.font = UIFont.preferredFont(forTextStyle: .subheadline)
+        topLabel.textColor = .text
+        topLabel.text = title
+
+        countLabel.font = UIFont.preferredFont(forTextStyle: .title1).bold()
+        countLabel.textColor = .text
+        countLabel.adjustsFontSizeToFitWidth = true
+        countLabel.text = "0"
+
+        stackView.addArrangedSubviews([topLabel, countLabel])
+
+        return stackView
+    }
+
+    private func makeVerticalDivider() -> UIView {
+        let divider = UIView()
+        divider.translatesAutoresizingMaskIntoConstraints = false
+        divider.widthAnchor.constraint(equalToConstant: Metrics.dividerWidth).isActive = true
+
+        WPStyleGuide.Stats.configureViewAsVerticalSeparator(divider)
+
+        return divider
     }
 
     // MARK: - Public Configuration
@@ -117,6 +180,10 @@ class StatsLatestPostSummaryInsightsCell: StatsBaseCell, LatestPostSummaryConfig
         postTimestampLabel.text = String(format: TextContent.publishDate, date)
 
         configureFeaturedImage(url: lastPostInsight.featuredImageURL)
+
+        viewCountLabel.text = lastPostInsight.viewsCount.abbreviatedString()
+        likeCountLabel.text = lastPostInsight.likesCount.abbreviatedString()
+        commentCountLabel.text = lastPostInsight.commentsCount.abbreviatedString()
     }
 
     private func configureFeaturedImage(url: URL?) {
@@ -139,11 +206,16 @@ class StatsLatestPostSummaryInsightsCell: StatsBaseCell, LatestPostSummaryConfig
         static let outerStackViewSpacing: CGFloat = 16.0
         static let postStackViewHorizontalSpacing: CGFloat = 16.0
         static let postStackViewVerticalSpacing: CGFloat = 8.0
+        static let statsStackViewVerticalSpacing: CGFloat = 8.0
         static let thumbnailSize: CGFloat = 68.0
         static let thumbnailCornerRadius: CGFloat = 4.0
+        static let dividerWidth: CGFloat = 1.0
     }
 
     private enum TextContent {
         static let publishDate = NSLocalizedString("stats.insights.latestPostSummary.publishDate", value: "Published %@",  comment: "Publish date of a post displayed in Stats. Placeholder will be replaced with a localized relative time, e.g. 2 days ago")
+        static let views = NSLocalizedString("stats.insights.latestPostSummary.views", value: "Views", comment: "Title for Views count in Latest Post Summary stats card.")
+        static let likes = NSLocalizedString("stats.insights.latestPostSummary.likes", value: "Likes", comment: "Title for Likes count in Latest Post Summary stats card.")
+        static let comments = NSLocalizedString("stats.insights.latestPostSummary.comments", value: "Comments", comment: "Title for Comments count in Latest Post Summary stats card.")
     }
 }
