c879aee7a79f15fd32be90c49a7fb01984adb7c0
Olivier Halligon <olivier.halligon@automattic.com>
2022-05-04 03:03:53 +0200

    Enable optimizations when compiling the swift script

--- Scripts/BuildPhases/LintAppLocalizedStringsUsage.sh
+++ Scripts/BuildPhases/LintAppLocalizedStringsUsage.sh
@@ -8,7 +8,7 @@ LINTER_EXEC="${LINTER_BUILD_DIR}/$(basename "${SCRIPT_SRC}" .swift)"
 
 if [ ! -x "${LINTER_EXEC}" ] || ! (shasum -c "${LINTER_EXEC}.shasum" >/dev/null 2>/dev/null); then
   echo "Pre-compiling linter script to ${LINTER_EXEC}..."
-  swiftc -sdk "$(xcrun --sdk macosx --show-sdk-path)" "${SCRIPT_SRC}" -o "${LINTER_EXEC}"
+  swiftc -O -sdk "$(xcrun --sdk macosx --show-sdk-path)" "${SCRIPT_SRC}" -o "${LINTER_EXEC}"
   shasum "${SCRIPT_SRC}" >"${LINTER_EXEC}.shasum"
   chmod +x "${LINTER_EXEC}"
   echo "Pre-compiled linter script ready"
