1fa1b676150dc1294d0ba49c25b667d7c8259e95
Chris McGraw <2454408+wargcm@users.noreply.github.com>
2022-05-11 17:51:24 -0400

    Add blogging prompts attribution to prompts dashboard card
    See: #18558

--- WordPress/Classes/Services/BloggingPromptsService.swift
+++ WordPress/Classes/Services/BloggingPromptsService.swift
@@ -84,7 +84,7 @@ struct BloggingPrompt {
             answered: false,
             answerCount: 5,
             displayAvatarURLs: [],
-            attribution: ""
+            attribution: "dayone"
     )
 }
 
