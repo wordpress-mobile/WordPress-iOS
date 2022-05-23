24d07a1ff1dae374eda84b9fb1060bf2d847b4e3
Hassaan El-Garem <hassaanelgarem@gmail.com>
2022-05-24 01:54:03 +0200

    Remove: unneeded code from qs checklist manager

--- WordPress/Classes/ViewRelated/Blog/QuickStartChecklistManager.swift
+++ WordPress/Classes/ViewRelated/Blog/QuickStartChecklistManager.swift
@@ -71,70 +71,17 @@ extension QuickStartChecklistManager: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return Constants.headerHeight
     }
-
-    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
-        let tour = tour(at: indexPath)
-        return isCompleted(tour: tour) ? .none : .delete
-    }
-
-    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        let tour = tour(at: indexPath)
-        guard isCompleted(tour: tour) == false else {
-            return nil
-        }
-
-        let buttonTitle = NSLocalizedString("Skip", comment: "Button title that appears when you swipe to left the row. It indicates the possibility to skip a specific tour.")
-        let skip = UIContextualAction(style: .destructive, title: buttonTitle) { [weak self] (_, _, _) in
-            self?.tableView(tableView, completeTourAt: indexPath)
-        }
-        skip.backgroundColor = .error
-
-        return UISwipeActionsConfiguration(actions: [skip])
-    }
 }
 
 private extension QuickStartChecklistManager {
-    func isLastTour(at indexPath: IndexPath) -> Bool {
-        return (tours.count - 1) == indexPath.row
-    }
-
-    func hideTopSeparator(at indexPath: IndexPath) -> Bool {
-        return !(WPDeviceIdentification.isiPad() && indexPath.row == 0)
-    }
 
     func isCompleted(tour: QuickStartTour) -> Bool {
         return completedToursKeys.contains(tour.key)
-    }
-
-    func tableView(_ tableView: UITableView, completeTourAt indexPath: IndexPath) {
-        let tour = tours[indexPath.row]
-        completedToursKeys.insert(tour.key)
-        tableView.reloadRows(at: [indexPath], with: .fade)
-
-        WPAnalytics.trackQuickStartStat(.quickStartListItemSkipped,
-                                        properties: ["task_name": tour.analyticsKey],
-                                        blog: blog)
-        QuickStartTourGuide.shared.complete(tour: tour, for: self.blog, postNotification: true)
     }
 }
 
 private extension QuickStartChecklistManager {
     enum Constants {
         static let headerHeight: CGFloat = 204
-    }
-}
-
-private extension UITableView {
-    /// Allows multiple insert/delete/reload/move calls to be animated simultaneously.
-    ///
-    /// - Parameters:
-    ///   - update: The block that performs the relevant insert, delete, reload, or move operations.
-    ///   - completion: A completion handler block to execute when all of the operations are finished. The Boolean value indicating whether the animations completed successfully. The value of this parameter is false if the animations were interrupted for any reason. On iOS 10 the value is always true.
-    func perform(update: (UITableView) -> Void, _ completion: ((UITableView, Bool) -> Void)? = nil) {
-        performBatchUpdates({
-            update(self)
-        }) { success in
-            completion?(self, success)
-        }
     }
 }
