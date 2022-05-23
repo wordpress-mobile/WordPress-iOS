4aeb2c2c2bcfa9d0b50dbca8dc1fdcd12a5af7aa
Momo Ozawa <momo.ozawa@automattic.com>
2022-05-23 15:28:48 +0100

    Fix: prevent title from being truncated

--- WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Quick Start/QuickStartChecklistView.swift
+++ WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Quick Start/QuickStartChecklistView.swift
@@ -35,6 +35,9 @@ final class QuickStartChecklistView: UIView {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = WPStyleGuide.serifFontForTextStyle(.body, fontWeight: .semibold)
+        label.adjustsFontForContentSizeCategory = true
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = Metrics.labelMinimumScaleFactor
         label.textColor = .text
         return label
     }()
@@ -152,6 +155,7 @@ extension QuickStartChecklistView {
         static let mainStackViewSpacing = 16.0
         static let labelStackViewSpacing = 4.0
         static let progressIndicatorViewSize = 24.0
+        static let labelMinimumScaleFactor = 0.5
     }
 
     private enum Strings {
