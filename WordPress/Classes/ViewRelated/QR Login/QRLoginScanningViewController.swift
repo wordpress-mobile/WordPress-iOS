842722b8a4cc5e50e317474c9c51b7b6959ac83b
emilylaguna <emily.laguna@automattic.com>
2022-05-24 12:52:21 -0400

    Add a pulse animation to the scan focus ring

--- WordPress/Classes/ViewRelated/QR Login/QRLoginScanningViewController.swift
+++ WordPress/Classes/ViewRelated/QR Login/QRLoginScanningViewController.swift
@@ -18,5 +18,28 @@ extension QRLoginScanningViewController: QRLoginScanningView {
         view.layer.insertSublayer(previewLayer, below: overlayView.layer)
     }
 }
+// MARK: - Animations
+private extension QRLoginScanningViewController {
+    func startAnimations() {
+        pulsateFocusArea()
+    }
+
+    func stopAnimations() {
+        scanFocusImageView.layer.removeAllAnimations()
+    }
+
+    /// Creates a pulsing animation that scales the focus area corners up and down
+    func pulsateFocusArea() {
+        let layerAnimation = CABasicAnimation(keyPath: "transform.scale")
+        layerAnimation.fromValue = 1
+        layerAnimation.toValue = 1.05
+        layerAnimation.isAdditive = false
+        layerAnimation.duration = 1
+        layerAnimation.fillMode = .forwards
+        layerAnimation.isRemovedOnCompletion = true
+        layerAnimation.repeatCount = .infinity
+        layerAnimation.autoreverses = true
+
+        scanFocusImageView.layer.add(layerAnimation, forKey: "growingAnimation")
     }
 }
