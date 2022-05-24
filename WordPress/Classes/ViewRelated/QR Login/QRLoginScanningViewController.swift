e034b38d95bc63f58e7cecbc790e149a47ea24ac
emilylaguna <emily.laguna@automattic.com>
2022-05-24 12:50:34 -0400

    Show the camera preview on the view controller

--- WordPress/Classes/ViewRelated/QR Login/QRLoginScanningViewController.swift
+++ WordPress/Classes/ViewRelated/QR Login/QRLoginScanningViewController.swift
@@ -1,10 +1,22 @@
 import UIKit
+import AVFoundation
+
 class QRLoginScanningViewController: UIViewController {
+    @IBOutlet weak var overlayView: UIView!
+    @IBOutlet weak var scanFocusImageView: UIImageView!
+    @IBOutlet weak var errorLabel: UILabel!
+
     var coordinator: QRLoginScanningCoordinator?
 }
 extension QRLoginScanningViewController: QRLoginScanningView {
     func showError(_ message: String) {
     }
     func showCameraLayer(_ previewLayer: AVCaptureVideoPreviewLayer) {
+        previewLayer.frame = view.layer.bounds
+
+        // Insert the layer below our scan focus overlay image
+        view.layer.insertSublayer(previewLayer, below: overlayView.layer)
+    }
+}
     }
 }
