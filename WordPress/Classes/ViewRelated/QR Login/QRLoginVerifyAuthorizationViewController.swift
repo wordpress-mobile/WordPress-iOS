58bef43413b3a0bdd7a406145a32b139d562b3db
emilylaguna <emily.laguna@automattic.com>
2022-05-26 15:21:28 -0400

    Style the verify view xib

--- WordPress/Classes/ViewRelated/QR Login/QRLoginVerifyAuthorizationViewController.swift
+++ WordPress/Classes/ViewRelated/QR Login/QRLoginVerifyAuthorizationViewController.swift
@@ -1,6 +1,15 @@
 import UIKit
 
 class QRLoginVerifyAuthorizationViewController: UIViewController {
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subTitleLabel: UILabel!
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+
+
+    @IBOutlet weak var confirmButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
 
     var coordinator: QRLoginVerifyCoordinator?
     override func viewDidLoad() {
