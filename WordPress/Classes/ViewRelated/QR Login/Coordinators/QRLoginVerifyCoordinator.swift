85cd70e1dcb9790b68ae938ccb11b3b7cc04a3fe
emilylaguna <emily.laguna@automattic.com>
2022-05-26 15:18:53 -0400

    Add temporary verification flow with fake loading to the verify coordinator

--- WordPress/Classes/ViewRelated/QR Login/Coordinators/QRLoginVerifyCoordinator.swift
+++ WordPress/Classes/ViewRelated/QR Login/Coordinators/QRLoginVerifyCoordinator.swift
@@ -1,7 +1,16 @@
 import Foundation
 
 protocol QRLoginVerifyView {
+    // TODO: Pass view model to the render
+    func render()
+    func renderCompletion()
+
+    func showLoading()
+    func showAuthenticating()
+
+    func showNoConnectionError()
 }
+
 class QRLoginVerifyCoordinator {
     private let parentCoordinator: QRLoginCoordinator
     private let view: QRLoginVerifyView
@@ -12,5 +21,49 @@ class QRLoginVerifyCoordinator {
         self.loginCode = loginCode
         self.view = view
         self.parentCoordinator = parentCoordinator
+    }
+
+    enum ViewState {
+        case verifyingCode
+        case waitingForUserVerification
+        case authenticating
+        case done
+    }
+}
+
+// MARK: - View Interactions
+extension QRLoginVerifyCoordinator {
+    func start() {
+        state = .verifyingCode
+
+        //TODO: Make network request to validate the code and get the browser info
+        view.showLoading()
+
+        // Temporary loading -> render
+        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500)) {
+            self.state = .waitingForUserVerification
+            self.view.render()
+        }
+    }
+
+    func confirm() {
+        // If we're in the done state, dismiss the flow
+        if state == .done {
+            parentCoordinator.dismiss()
+            return
+        }
+
+        // TODO: Make network request to log the user in
+        view.showAuthenticating()
+        state = .authenticating
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500)) {
+            self.state = .done
+            self.view.renderCompletion()
+        }
+    }
+
+    func cancel() {
+        parentCoordinator.dismiss()
     }
 }
