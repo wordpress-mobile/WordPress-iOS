63d55fb935fe52566fe0630411a973fa4d79eeda
David Christiandy <1299411+dvdchr@users.noreply.github.com>
2022-05-30 13:07:13 +0700

    Finish implementation for scheduling methods, add some docs

--- WordPress/Classes/Utility/Blogging Prompts/ReminderScheduleCoordinator.swift
+++ WordPress/Classes/Utility/Blogging Prompts/ReminderScheduleCoordinator.swift
@@ -1,6 +1,10 @@
 import UserNotifications
 
-/// Abstraction layer between Blogging Reminders and Blogging Prompts.
+/// Bridges the logic between Blogging Reminders and Blogging Prompts.
+///
+/// Users can switch between receiving Blogging Reminders or Blogging Prompts based on the switch toggle in the reminder sheet. They are both delivered
+/// through local notifications, but the mechanism between the two is differentiated due to technical limitations. Blogging Prompts requires the content
+/// of each notification to be different, and this is not possible if we want to use a repeating `UNCalendarNotificationTrigger`.
 ///
 class ReminderScheduleCoordinator {
 
@@ -33,37 +37,82 @@ class ReminderScheduleCoordinator {
     }
 
 
+    /// Returns the user's reminder schedule for the given `blog`, based on the current reminder type.
+    ///
+    /// - Parameter blog: The blog associated with the reminders.
+    /// - Returns: The user's preferred reminder schedule.
     func schedule(for blog: Blog) -> BloggingRemindersScheduler.Schedule {
         switch reminderType(for: blog) {
         case .bloggingReminders:
             return bloggingRemindersScheduler.schedule(for: blog)
 
         case .bloggingPrompts:
-            return .none
+            guard let settings = promptReminderSettings(for: blog),
+                  let reminderDays = settings.reminderDays,
+                  !reminderDays.getActiveWeekdays().isEmpty else {
+                return .none
+            }
+
+            return .weekdays(reminderDays.getActiveWeekdays())
         }
     }
 
+    /// Returns the user's preferred time for the given `blog`, based on the current reminder type.
+    ///
+    /// - Parameter blog: The blog associated with the reminders.
+    /// - Returns: The user's preferred time returned in `Date`.
     func scheduledTime(for blog: Blog) -> Date {
         switch reminderType(for: blog) {
         case .bloggingReminders:
             return bloggingRemindersScheduler.scheduledTime(for: blog)
 
         case .bloggingPrompts:
-            return Date() // TODO.
+            guard let settings = promptReminderSettings(for: blog),
+                  let dateForTime = settings.reminderTimeDate() else {
+                return Constants.defaultTime
+            }
+
+            return dateForTime
         }
     }
 
+    /// Schedules a reminder notification for the given `blog` based on the current reminder type.
+    ///
+    /// - Note: Calling this method will trigger the push notification authorization flow.
+    ///
+    /// - Parameters:
+    ///   - schedule: The preferred notification schedule.
+    ///   - blog: The blog that will upload the user's post.
+    ///   - time: The user's preferred time to be notified.
+    ///   - completion: Closure called after the process completes.
     func schedule(_ schedule: BloggingRemindersScheduler.Schedule,
                   for blog: Blog,
                   time: Date? = nil,
                   completion: @escaping (Result<Void, Swift.Error>) -> ()) {
         switch reminderType(for: blog) {
         case .bloggingReminders:
-            bloggingRemindersScheduler.schedule(schedule, for: blog, time: time, completion: completion)
+            bloggingRemindersScheduler.schedule(schedule, for: blog, time: time) { [weak self] result in
+                // always unschedule prompt reminders in case the user toggled the switch.
+                self?.promptRemindersScheduler.unschedule(for: blog)
+                completion(result)
+            }
 
         case .bloggingPrompts:
-            promptRemindersScheduler.schedule(schedule, for: blog, time: time, completion: completion)
+            promptRemindersScheduler.schedule(schedule, for: blog, time: time) { [weak self] result in
+                // always unschedule blogging reminders in case the user toggled the switch.
+                self?.bloggingRemindersScheduler.unschedule(for: blog)
+                completion(result)
+            }
         }
+    }
+
+    /// Unschedules all future reminders from the given `blog`.
+    /// This applies to both Blogging Reminders and Blogging Prompts.
+    ///
+    /// - Parameter blog: The blog associated with the reminders.
+    func unschedule(for blog: Blog) {
+        bloggingRemindersScheduler.unschedule(for: blog)
+        promptRemindersScheduler.unschedule(for: blog)
     }
 
 }
@@ -77,9 +126,18 @@ private extension ReminderScheduleCoordinator {
         case bloggingPrompts
     }
 
+    enum Constants {
+        static let defaultHour = 10
+        static let defaultMinute = 0
+
+        static var defaultTime: Date {
+            let calendar = Calendar.current
+            return calendar.date(from: DateComponents(calendar: calendar, hour: defaultHour, minute: defaultMinute)) ?? Date()
+        }
+    }
+
     func promptReminderSettings(for blog: Blog) -> BloggingPromptSettings? {
-        guard Feature.enabled(.bloggingPrompts),
-              let service = bloggingPromptsServiceFactory.makeService(for: blog) else {
+        guard let service = bloggingPromptsServiceFactory.makeService(for: blog) else {
             return nil
         }
 
@@ -87,7 +145,8 @@ private extension ReminderScheduleCoordinator {
     }
 
     func reminderType(for blog: Blog) -> ReminderType {
-        if let settings = promptReminderSettings(for: blog),
+        if Feature.enabled(.bloggingPrompts),
+           let settings = promptReminderSettings(for: blog),
            settings.promptRemindersEnabled {
             return .bloggingPrompts
         }
