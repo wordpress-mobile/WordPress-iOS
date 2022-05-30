4ea4abf84da7c69c198c78a04007a183544c9bef
David Christiandy <1299411+dvdchr@users.noreply.github.com>
2022-05-31 02:28:04 +0700

    Throw BloggingReminders' error when push is rejected

--- WordPress/Classes/Utility/Blogging Prompts/PromptRemindersScheduler.swift
+++ WordPress/Classes/Utility/Blogging Prompts/PromptRemindersScheduler.swift
@@ -56,7 +56,7 @@ class PromptRemindersScheduler {
             }
 
             guard allowed else {
-                completion(.failure(Errors.needsPushAuthorization))
+                completion(.failure(BloggingRemindersScheduler.Error.needsPermissionForPushNotifications))
                 return
             }
 
