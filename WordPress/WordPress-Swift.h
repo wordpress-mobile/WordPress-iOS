51b4296105eccbc327c8ba14c697e206dcf8fa72
Hassaan El-Garem <hassaanelgarem@gmail.com>
2022-05-31 02:20:09 +0200

    Update: generated swift header file From Workaround to XcodeGenerated

--- WordPress/WordPress-Swift.h
+++ WordPress/WordPress-Swift.h
@@ -3,5 +3,5 @@
 /// As a workaround, we ignore all multiple method declarations warnings when importing WordPress-Swift.h
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wduplicate-method-match"
-#import "WordPress-Swift-Workaround.h"
+#import "WordPress-Swift-XcodeGenerated.h"
 #pragma clang diagnostic pop
