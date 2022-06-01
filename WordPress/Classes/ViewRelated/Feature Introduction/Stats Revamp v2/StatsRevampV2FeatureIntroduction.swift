41e440739214a16b4114410015f0aa7ab289c6eb
Paul Von Schrottky <paul.von.schrottky@automattic.com>
2022-05-31 20:16:07 -0400

    Add Stats Revamp announcement header img

--- WordPress/Classes/ViewRelated/Feature Introduction/Stats Revamp v2/StatsRevampV2FeatureIntroduction.swift
+++ WordPress/Classes/ViewRelated/Feature Introduction/Stats Revamp v2/StatsRevampV2FeatureIntroduction.swift
@@ -8,13 +8,22 @@ class StatsRevampV2FeatureIntroduction: FeatureIntroductionViewController {
         let featureDescriptionView = StatsRevampV2FeatureDescriptionView.loadFromNib()
         featureDescriptionView.translatesAutoresizingMaskIntoConstraints = false
 
-        super.init(headerTitle: HeaderStrings.title, headerSubtitle: "", headerImage: nil, featureDescriptionView: featureDescriptionView, primaryButtonTitle: ButtonStrings.showMe, secondaryButtonTitle: nil)
+        let headerImage = UIImage(named: HeaderStyle.imageName)?.withTintColor(.clear)
+
+        super.init(headerTitle: HeaderStrings.title, headerSubtitle: "", headerImage: headerImage, featureDescriptionView: featureDescriptionView, primaryButtonTitle: ButtonStrings.showMe, secondaryButtonTitle: nil)
 
         featureIntroductionDelegate = self
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Add the gradient after the image has been added to the view so the gradient is the correct size.
+        addHeaderImageGradient()
     }
 }
 
@@ -26,11 +35,41 @@ extension StatsRevampV2FeatureIntroduction: FeatureIntroductionDelegate {
 
 private extension StatsRevampV2FeatureIntroduction {
 
+    func addHeaderImageGradient() {
+        // Based on https://stackoverflow.com/a/54096829
+        let gradient = CAGradientLayer()
+
+        gradient.colors = [
+            HeaderStyle.startGradientColor.cgColor,
+            HeaderStyle.endGradientColor.cgColor
+        ]
+
+        // Create a gradient from top to bottom.
+        gradient.startPoint = CGPoint(x: 0.5, y: 0)
+        gradient.endPoint = CGPoint(x: 0.5, y: 1)
+        gradient.frame = headerImageView.bounds
+
+        // Add a mask to the gradient so the colors only apply to the image (and not the imageView).
+        let mask = CALayer()
+        mask.contents = headerImageView.image?.cgImage
+        mask.frame = gradient.bounds
+        gradient.mask = mask
+
+        // Add the gradient as a sublayer to the imageView's layer.
+        headerImageView.layer.addSublayer(gradient)
+    }
+
     enum ButtonStrings {
         static let showMe = NSLocalizedString("Try it now", comment: "Button title to take user to the new Stats Insights screen.")
     }
 
     enum HeaderStrings {
         static let title = NSLocalizedString("Insights update", comment: "Title displayed on the feature introduction view that announces the updated Stats Insight screen.")
+    }
+
+    enum HeaderStyle {
+        static let imageName = "icon-lightbulb-outline"
+        static let startGradientColor: UIColor = .warning(.shade30)
+        static let endGradientColor: UIColor = .accent(.shade40)
     }
 }
