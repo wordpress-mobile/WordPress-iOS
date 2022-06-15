27ee6ebc24a6a8079a85040df0677fc4ed27434f
emilylaguna <emily.laguna@automattic.com>
2022-06-15 11:36:18 -0400

    Use enum instead of struct for strings constant

--- WordPress/Classes/ViewRelated/QR Login/Coordinators/QRLoginScanningCoordinator.swift
+++ WordPress/Classes/ViewRelated/QR Login/Coordinators/QRLoginScanningCoordinator.swift
@@ -14,8 +14,8 @@ class QRLoginScanningCoordinator: NSObject {
         self.parentCoordinator = parentCoordinator
     }
     // MARK: - Strings
-    private struct Strings {
-        struct accessAlert {
+    private enum Strings {
+        enum accessAlert {
             static let title = NSLocalizedString("Camera access needed to scan login codes", comment: "Title of an alert informing the user the camera permission for the app is disabled and its needed to proceed")
             static let message = NSLocalizedString("This app needs permission to access the Camera to scan login codes, tap on the Open Settings button to enable it.", comment: "A description informing the user in order to proceed with this feature we will need camera permissions, and how to enable it.")
             static let openSettings = NSLocalizedString("Open Settings", comment: "Title of a button that opens the apps settings in the system Settings.app")
