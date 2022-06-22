b20080862ee707d94d7bbe2bbdb95d04b7ed7a35
emilylaguna <emily.laguna@automattic.com>
2022-06-22 16:19:32 -0400

    Move all the QRLogin protocols into a single file

---
+++ WordPress/Classes/ViewRelated/QR Login/QRLoginProtocols.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+/// Encapsulates the interactions between the child and parent coordinators
+protocol QRLoginParentCoordinator {
+    func track(_ event: WPAnalyticsEvent)
+    func track(_ event: WPAnalyticsEvent, properties: [AnyHashable: Any]?)
+
+    func scanAgain()
+    func didScanToken(_ token: QRLoginToken)
+    func dismiss()
+}
+
+/// Login camera scanning view
+protocol QRLoginScanningView {
+    func showError(_ message: String)
+    func showCameraLayer(_ layer: AVCaptureVideoPreviewLayer)
+}
+
+/// Login verify view and all its states
+protocol QRLoginVerifyView {
+    /* Completion States */
+    func render(response: QRLoginValidationResponse)
+    func renderCompletion()
+
+    /* Loading States */
+    func showLoading()
+    func showAuthenticating()
+
+    /* Error States */
+    func showNoConnectionError()
+    func showQRLoginError(error: QRLoginError?)
+    func showAuthenticationFailedError()
+}
