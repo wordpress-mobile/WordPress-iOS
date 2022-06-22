ea7633ba74e13c3be582d52f95ee875750fe8a6e
emilylaguna <emily.laguna@automattic.com>
2022-06-22 16:22:57 -0400

    Allow QRLoginServiceRemote to be injected into the QRLoginService

--- WordPress/Classes/Services/QRLoginService.swift
+++ WordPress/Classes/Services/QRLoginService.swift
@@ -2,12 +2,12 @@ import Foundation
 import WordPressKit
 
 class QRLoginService: LocalCoreDataService {
-    private lazy var service: QRLoginServiceRemote = {
-        let api = WordPressComRestApi.defaultApi(in: managedObjectContext,
-                                                 localeKey: WordPressComRestApi.LocaleKeyV2)
+    private let service: QRLoginServiceRemote
 
-        return QRLoginServiceRemote(wordPressComRestApi: api)
-    }()
+    init(managedObjectContext: NSManagedObjectContext, remoteService: QRLoginServiceRemote? = nil) {
+        self.service = remoteService ?? QRLoginServiceRemote(wordPressComRestApi: WordPressComRestApi.defaultApi(in: managedObjectContext, localeKey: WordPressComRestApi.LocaleKeyV2))
+        super.init()
+    }
 
     func validate(token: QRLoginToken, success: @escaping(QRLoginValidationResponse) -> Void, failure: @escaping(Error?, QRLoginError?) -> Void) {
         service.validate(token: token.token, data: token.data, success: success, failure: failure)
@@ -15,6 +15,5 @@ class QRLoginService: LocalCoreDataService {
 
     func authenticate(token: QRLoginToken, success: @escaping(Bool) -> Void, failure: @escaping(Error) -> Void) {
         service.authenticate(token: token.token, data: token.data, success: success, failure: failure)
-
     }
 }
