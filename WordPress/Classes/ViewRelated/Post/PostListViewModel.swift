958ab4a990ad227715217e107e774a69cd60e039
alpavanoglu <alp.avanoglu@automattic.com>
2022-06-30 13:56:25 +0200

    Add `PostListViewModelTests` along with edit tests

--- WordPress/Classes/ViewRelated/Post/PostListViewModel.swift
+++ WordPress/Classes/ViewRelated/Post/PostListViewModel.swift
@@ -1,11 +1,15 @@
 import Foundation
 
-protocol PostListViewModelProtocol {
-    
+protocol PostListViewModelOutputs {
+    var editingPostUploadFailed: (() -> Void)? { get set }
+    var editingPostUploadSuccess: (() -> Void)? { get set }
 }
 
+/// Convert to protocol if more inputs are needed.
+typealias PostListViewModelInputs = InteractivePostViewDelegate
+
 /// REFACTOR IN PROGRESS: Extracting VM logic from `PostListViewController`
-final class PostListViewModel: InteractivePostViewDelegate {
+final class PostListViewModel: PostListViewModelInputs, PostListViewModelOutputs {
     private enum Constants {
         enum AnalyticsProperty: String {
             case type
@@ -13,27 +17,39 @@ final class PostListViewModel: InteractivePostViewDelegate {
         }
     }
 
-    private let blog: Blog
+    // MARK: - Output Closures
+    var editingPostUploadFailed: (() -> Void)?
+    var editingPostUploadSuccess: (() -> Void)?
 
-    init(blog: Blog) {
-        self.blog = blog
-    }
-
+    // MARK: - Internal State
     lazy var filterSettings: PostListFilterSettings = {
         return PostListFilterSettings(blog: self.blog, postType: PostServiceType.post)
     }()
 
+    // MARK: - Private State
+    private let blog: Blog
+    private let postCoordinator: PostCoordinator
+
+    // MARK: - Lifecycle
+    init(blog: Blog, postCoordinator: PostCoordinator) {
+        self.blog = blog
+        self.postCoordinator = postCoordinator
+    }
+
+    // MARK: - Outputs
     func edit(_ post: AbstractPost) {
         guard let post = post as? Post else {
             return
         }
-//        guard !PostCoordinator.shared.isUploading(post: post) else {
+        guard !postCoordinator.isUploading(post: post) else {
 //            presentAlertForPostBeingUploaded()
-//            return
-//        }
+            editingPostUploadFailed?()
+            return
+        }
 
         WPAppAnalytics.track(.postListEditAction, withProperties: propertiesForAnalytics(), with: post)
 //        PostListEditorPresenter.handle(post: post, in: self, entryPoint: .postsList)
+        editingPostUploadSuccess?()
     }
 
     func view(_ post: AbstractPost) {
