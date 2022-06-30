363dbc9c4550c430b45a029a0899682d290aa12d
Povilas Staskus <povilas.staskus@gmail.com>
2022-06-30 12:50:26 +0300

    Remove StatsWidgetStore dependency from SiteAssemblyService
    To keep the StatsWidgetStore solution consistent, throw WPSiteCreated notification instead of calling StatsWidgetStore directly.
    
    We will be able to call this notification in other parts of application that creates a site or detects a new site being created.

--- WordPress/Classes/Stores/StatsWidgetsStore.swift
+++ WordPress/Classes/Stores/StatsWidgetsStore.swift
@@ -10,7 +10,7 @@ class StatsWidgetsStore {
         observeAccountChangesForWidgets()
         observeAccountSignInForWidgets()
         observeApplicationLaunched()
-        observeSiteDeletionForWidgets()
+        observeSiteUpdatesForWidgets()
     }
 
     /// Refreshes the site list used to configure the widgets when sites are added or deleted
@@ -313,12 +313,13 @@ private extension StatsWidgetsStore {
         initializeStatsWidgetsIfNeeded()
     }
 
-    func observeSiteDeletionForWidgets() {
-        NotificationCenter.default.addObserver(forName: .WPSiteDeleted,
-                                               object: nil,
-                                               queue: nil) { [weak self] _ in
-            self?.refreshStatsWidgetsSiteList()
+    func observeSiteUpdatesForWidgets() {
+        guard #available(iOS 14.0, *) else {
+            return
         }
+
+        NotificationCenter.default.addObserver(self, selector: #selector(refreshStatsWidgetsSiteList), name: .WPSiteCreated, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(refreshStatsWidgetsSiteList), name: .WPSiteDeleted, object: nil)
     }
 }
 
