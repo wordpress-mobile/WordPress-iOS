0d61cfb09475b17b6e0be107f6e9fd308f4229aa
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2022-07-01 11:54:57 -0500

    Update WPTabBarController, add extension WPTabBarController+JetpackBanner that adds or removes the Jetpack powered banner on top of the main tab bar

---
+++ WordPress/Classes/ViewRelated/Jetpack Branding/WPTabBarController+JetpackBanner.swift
@@ -0,0 +1,107 @@
+import UIKit
+
+extension WPTabBarController {
+
+    /// Adds a Jetpack powered banner on top of the main tab bar.
+    @objc func addJetpackBanner() {
+
+        guard AppConfiguration.isWordPress else {
+            return
+        }
+
+        bannerView = UIView()
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+
+
+        bannerView.backgroundColor = Appearance.jetpackBackgroundColor
+
+        view.insertSubview(bannerView, belowSubview: tabBar)
+
+        let jetpackButton = makeJetpackButton()
+        bannerView.addSubview(jetpackButton)
+
+        NSLayoutConstraint.activate([
+            bannerView.heightAnchor.constraint(equalToConstant: Appearance.jetpackBannerHeight),
+            bannerView.bottomAnchor.constraint(equalTo: tabBar.topAnchor),
+            bannerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            bannerView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        bannerView.pinSubviewToAllEdges(jetpackButton)
+        NotificationCenter.default.post(name: .jetpackBannerToggled, object: true)
+
+    }
+
+    private func makeJetpackButton() -> UIButton {
+        let jetpackButton = UIButton()
+        jetpackButton.translatesAutoresizingMaskIntoConstraints = false
+        jetpackButton.setTitle(Appearance.jetpackBannerTitle, for: .normal)
+        jetpackButton.tintColor = .muriel(color: .jetpackGreen, .shade40)
+        jetpackButton.setTitleColor(UIColor(light: .black, dark: .white), for: .normal)
+        jetpackButton.titleLabel?.font = Appearance.jetpackButtonFont
+        jetpackButton.setImage(.gridicon(.plans, size: Appearance.jetpackIconSize), for: .normal)
+        jetpackButton.imageEdgeInsets = Appearance.jetpackIconInsets
+
+        // sets the background of the jp logo to white
+        if let imageView = jetpackButton.imageView {
+            let view = UIView()
+            view.backgroundColor = .white
+            view.translatesAutoresizingMaskIntoConstraints = false
+            jetpackButton.insertSubview(view, belowSubview: imageView)
+            view.layer.cornerRadius = Appearance.jetpackIconBackgroundSize / 2
+            view.clipsToBounds = true
+            NSLayoutConstraint.activate([
+                view.centerXAnchor.constraint(equalTo: imageView.centerXAnchor),
+                view.centerYAnchor.constraint(equalTo: imageView.centerYAnchor),
+                view.heightAnchor.constraint(equalToConstant: Appearance.jetpackIconBackgroundSize),
+                view.widthAnchor.constraint(equalToConstant: Appearance.jetpackIconBackgroundSize)
+            ])
+        }
+
+        return jetpackButton
+    }
+
+    /// Removes the Jetpack powered banner from the top of the tab bar, if it exists.
+    @objc func removeJetpackBanner() {
+
+        guard AppConfiguration.isWordPress else {
+            return
+        }
+
+        bannerView?.removeFromSuperview()
+        bannerView = nil
+        NotificationCenter.default.post(name: .jetpackBannerToggled, object: false)
+    }
+
+    private enum Appearance {
+        static let jetpackBannerHeight: CGFloat = 44
+        static let jetpackBackgroundColor = UIColor(light: .muriel(color: .jetpackGreen, .shade0),
+                                                    dark: .muriel(color: .jetpackGreen, .shade90))
+        static let jetpackBannerTitle = NSLocalizedString("Jetpack powered",
+                                                          comment: "Title of the Jetpack powered banner.")
+        static let jetpackIconSize = CGSize(width: 28, height: 28)
+        static let jetpackIconInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 10)
+        static let jetpackButtonFont = UIFont.systemFont(ofSize: 16)
+        static let jetpackIconBackgroundSize: CGFloat = 22
+    }
+}
+
+extension NSNotification.Name {
+    static let jetpackBannerToggled = NSNotification.Name("PoweredByJetpackBannerToggled")
+}
+
+extension UIImage {
+  func withBackground(color: UIColor, opaque: Bool = true) -> UIImage {
+    UIGraphicsBeginImageContextWithOptions(size, opaque, scale)
+
+    guard let ctx = UIGraphicsGetCurrentContext(), let image = cgImage else { return self }
+    defer { UIGraphicsEndImageContext() }
+
+    let rect = CGRect(origin: .zero, size: size)
+    ctx.setFillColor(color.cgColor)
+    ctx.fill(rect)
+    ctx.concatenate(CGAffineTransform(a: 1, b: 0, c: 0, d: -1, tx: 0, ty: size.height))
+    ctx.draw(image, in: rect)
+
+    return UIGraphicsGetImageFromCurrentImageContext() ?? self
+  }
+}
