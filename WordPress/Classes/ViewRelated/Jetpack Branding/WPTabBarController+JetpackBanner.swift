b5e9d04687ff3de02cf54a7d2641a19d517e778d
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2022-07-01 16:46:10 -0500

    Remove unused UIImage extension

--- WordPress/Classes/ViewRelated/Jetpack Branding/WPTabBarController+JetpackBanner.swift
+++ WordPress/Classes/ViewRelated/Jetpack Branding/WPTabBarController+JetpackBanner.swift
@@ -92,20 +92,3 @@ extension WPTabBarController {
 extension NSNotification.Name {
     static let jetpackBannerToggled = NSNotification.Name("PoweredByJetpackBannerToggled")
 }
-
-extension UIImage {
-  func withBackground(color: UIColor, opaque: Bool = true) -> UIImage {
-    UIGraphicsBeginImageContextWithOptions(size, opaque, scale)
-
-    guard let ctx = UIGraphicsGetCurrentContext(), let image = cgImage else { return self }
-    defer { UIGraphicsEndImageContext() }
-
-    let rect = CGRect(origin: .zero, size: size)
-    ctx.setFillColor(color.cgColor)
-    ctx.fill(rect)
-    ctx.concatenate(CGAffineTransform(a: 1, b: 0, c: 0, d: -1, tx: 0, ty: size.height))
-    ctx.draw(image, in: rect)
-
-    return UIGraphicsGetImageFromCurrentImageContext() ?? self
-  }
-}
