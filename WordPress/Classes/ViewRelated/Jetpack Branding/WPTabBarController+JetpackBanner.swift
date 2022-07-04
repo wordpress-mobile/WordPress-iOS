8e54f7c7948c89f0cb1d9d23974ec5a62a81326e
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2022-07-04 12:06:56 -0500

    Update WPTabBarController+JetpackBanner, add optional title for container controller

--- WordPress/Classes/ViewRelated/Jetpack Branding/WPTabBarController+JetpackBanner.swift
+++ WordPress/Classes/ViewRelated/Jetpack Branding/WPTabBarController+JetpackBanner.swift
@@ -3,9 +3,11 @@ extension WPTabBarController {
 
     /// Encapsulates a `UIViewController` instance inside a `JetpackBannerContainerViewController`
     /// - Parameter contentController: the `UIViewController` instance to be encapsulated
+    /// - Parameter title: optional title to assign to the container controller
     /// - Returns: the newly created instance of `JetpackBannerContainerViewController`
-    @objc func makeJetpackContainerViewController(contentController: UIViewController) -> UIViewController {
+    @objc func makeJetpackContainerViewController(contentController: UIViewController, title: String? = nil) -> UIViewController {
         let controller = JetpackBannerContainerViewController(contentController: contentController)
+        controller.title = title
         return controller
     }
 }
