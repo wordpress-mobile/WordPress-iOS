30cbf990696805c3eecd0c157437ac64505a3a95
alpavanoglu <alp.avanoglu@automattic.com>
2022-07-04 16:17:44 +0200

    Move `NoResultsText` to `PostListStringStore`

--- WordPress/Classes/ViewRelated/Post/PostListStringStore.swift
+++ WordPress/Classes/ViewRelated/Post/PostListStringStore.swift
@@ -30,5 +30,73 @@ enum PostListStringStore {
         }
     }
 
-//    enum
+    enum NoResults {
+        static let noConnectionTitle = NSLocalizedString(
+            "Unable to load posts right now.",
+            comment: "Title for No results full page screen displayedfrom post list when there is no connection"
+        )
+
+        static let buttonTitle = NSLocalizedString("Create Post", comment: "Button title, encourages users to create post on their blog.")
+
+        static let noConnectionSubtitle = NSLocalizedString(
+            "Check your network connection and try again. Or draft a post.",
+            comment: "Subtitle for No results full page screen displayed from post list when there is no connection"
+        )
+
+        static let searchPosts = NSLocalizedString("Search posts", comment: "Text displayed when the search controller will be presented")
+
+        static func noResultsTitle(filterType: PostListFilter.Status, isSyncing: Bool, isSearching: Bool) -> String {
+            if isSyncing {
+                return NSLocalizedString(
+                    "Fetching posts...",
+                    comment: "A brief prompt shown when the reader is empty, letting the user know the app is currently fetching new posts."
+                )
+            }
+
+            if isSearching {
+                return NSLocalizedString(
+                    "No posts matching your search",
+                    comment: "Displayed when the user is searching the posts list and there are no matching posts"
+                )
+            }
+
+            return filteredTitle(
+                filterType: filterType
+            )
+        }
+
+        static func noResultsButtonTitle(filterType: PostListFilter.Status, isSyncing: Bool, isSearching: Bool) -> String? {
+            if isSyncing || isSearching {
+                return nil
+            }
+
+            let filterType = filterType
+            return filterType == .trashed ? nil : PostListStringStore.NoResults.buttonTitle
+        }
+
+        private static func filteredTitle(filterType: PostListFilter.Status) -> String {
+            switch filterType {
+            case .draft:
+                return NSLocalizedString(
+                    "You don't have any draft posts",
+                    comment: "Displayed when the user views drafts in the posts list and there are no posts"
+                )
+            case .scheduled:
+                return NSLocalizedString(
+                    "You don't have any scheduled posts",
+                    comment: "Displayed when the user views scheduled posts in the posts list and there are no posts"
+                )
+            case .trashed:
+                return NSLocalizedString(
+                    "You don't have any trashed posts",
+                    comment: "Displayed when the user views trashed in the posts list and there are no posts"
+                )
+            case .published:
+                return NSLocalizedString(
+                    "You haven't published any posts yet",
+                    comment: "Displayed when the user views published posts in the posts list and there are no posts"
+                )
+            }
+        }
+    }
 }
--- WordPress/Classes/ViewRelated/Post/PostListViewController.swift
+++ WordPress/Classes/ViewRelated/Post/PostListViewController.swift
@@ -817,76 +817,52 @@ private extension PostListViewController {
     func handleRefreshNoResultsViewController(_ noResultsViewController: NoResultsViewController) {
 
         guard connectionAvailable() else {
-            noResultsViewController.configure(title: "", noConnectionTitle: NoResultsText.noConnectionTitle, buttonTitle: NoResultsText.buttonTitle, subtitle: nil, noConnectionSubtitle: NoResultsText.noConnectionSubtitle, attributedSubtitle: nil, attributedSubtitleConfiguration: nil, image: nil, subtitleImage: nil, accessoryView: nil)
+            noResultsViewController.configure(
+                title: "",
+                noConnectionTitle: PostListStringStore.NoResults.noConnectionTitle,
+                buttonTitle: PostListStringStore.NoResults.buttonTitle,
+                subtitle: nil,
+                noConnectionSubtitle: PostListStringStore.NoResults.noConnectionSubtitle,
+                attributedSubtitle: nil,
+                attributedSubtitleConfiguration: nil,
+                image: nil,
+                subtitleImage: nil,
+                accessoryView: nil
+            )
             return
         }
 
+        let filterType = filterSettings.currentPostListFilter().filterType
+        let isSyncing = syncHelper.isSyncing == true
+        let noResultsTitle = PostListStringStore.NoResults.noResultsTitle(
+            filterType: filterType,
+            isSyncing: isSyncing,
+            isSearching: isSearching()
+        )
+
         if searchController.isActive {
             if currentSearchTerm()?.count == 0 {
-                noResultsViewController.configureForNoSearchResults(title: NoResultsText.searchPosts)
+                noResultsViewController.configureForNoSearchResults(title: PostListStringStore.NoResults.searchPosts)
             } else {
-                noResultsViewController.configureForNoSearchResults(title: noResultsTitle())
+                noResultsViewController.configureForNoSearchResults(title: noResultsTitle)
             }
         } else {
             let accessoryView = syncHelper.isSyncing ? NoResultsViewController.loadingAccessoryView() : nil
 
-            noResultsViewController.configure(title: noResultsTitle(),
-                                              buttonTitle: noResultsButtonTitle(),
-                                              image: noResultsImageName,
-                                              accessoryView: accessoryView)
+            noResultsViewController.configure(
+                title: noResultsTitle,
+                buttonTitle: PostListStringStore.NoResults.noResultsButtonTitle(
+                    filterType: filterType,
+                    isSyncing: isSyncing,
+                    isSearching: isSearching()
+                ),
+                image: noResultsImageName,
+                accessoryView: accessoryView)
         }
     }
 
     var noResultsImageName: String {
         return "posts-no-results"
-    }
-
-    func noResultsButtonTitle() -> String? {
-        if syncHelper.isSyncing == true || isSearching() {
-            return nil
-        }
-
-        let filterType = filterSettings.currentPostListFilter().filterType
-        return filterType == .trashed ? nil : NoResultsText.buttonTitle
-    }
-
-    func noResultsTitle() -> String {
-        if syncHelper.isSyncing == true {
-            return NoResultsText.fetchingTitle
-        }
-
-        if isSearching() {
-            return NoResultsText.noMatchesTitle
-        }
-
-        return noResultsFilteredTitle()
-    }
-
-    func noResultsFilteredTitle() -> String {
-        let filterType = filterSettings.currentPostListFilter().filterType
-        switch filterType {
-        case .draft:
-            return NoResultsText.noDraftsTitle
-        case .scheduled:
-            return NoResultsText.noScheduledTitle
-        case .trashed:
-            return NoResultsText.noTrashedTitle
-        case .published:
-            return NoResultsText.noPublishedTitle
-        }
-    }
-
-    struct NoResultsText {
-        static let buttonTitle = NSLocalizedString("Create Post", comment: "Button title, encourages users to create post on their blog.")
-        static let fetchingTitle = NSLocalizedString("Fetching posts...", comment: "A brief prompt shown when the reader is empty, letting the user know the app is currently fetching new posts.")
-        static let noMatchesTitle = NSLocalizedString("No posts matching your search", comment: "Displayed when the user is searching the posts list and there are no matching posts")
-        static let noDraftsTitle = NSLocalizedString("You don't have any draft posts", comment: "Displayed when the user views drafts in the posts list and there are no posts")
-        static let noScheduledTitle = NSLocalizedString("You don't have any scheduled posts", comment: "Displayed when the user views scheduled posts in the posts list and there are no posts")
-        static let noTrashedTitle = NSLocalizedString("You don't have any trashed posts", comment: "Displayed when the user views trashed in the posts list and there are no posts")
-        static let noPublishedTitle = NSLocalizedString("You haven't published any posts yet", comment: "Displayed when the user views published posts in the posts list and there are no posts")
-        static let noConnectionTitle: String = NSLocalizedString("Unable to load posts right now.", comment: "Title for No results full page screen displayedfrom post list when there is no connection")
-        static let noConnectionSubtitle: String = NSLocalizedString("Check your network connection and try again. Or draft a post.", comment: "Subtitle for No results full page screen displayed from post list when there is no connection")
-        static let searchPosts = NSLocalizedString("Search posts", comment: "Text displayed when the search controller will be presented")
     }
 }
 
