3847614474dde8ea211322f104b2f20ee3b89d3a
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2022-07-07 12:39:49 -0500

    Update JetpackBannerView, refactor to reuse the JetpackButton type

---
+++ WordPress/Classes/ViewRelated/Jetpack/Branding/Banner/JetpackBannerView.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+class JetpackBannerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    override var intrinsicContentSize: CGSize {
+        return isHidden ? CGSize.zero : super.intrinsicContentSize
+    }
+
+    func setup() {
+
+        backgroundColor = Self.jetpackBannerBackgroundColor
+
+        let jetpackButton = JetpackButton(style: .banner)
+        jetpackButton.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(jetpackButton)
+
+        pinSubviewToAllEdges(jetpackButton)
+    }
+
+    private static let jetpackBannerBackgroundColor = UIColor(light: .muriel(color: .jetpackGreen, .shade0),
+                                                              dark: .muriel(color: .jetpackGreen, .shade90))
+}
