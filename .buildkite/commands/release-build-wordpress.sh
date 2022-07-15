199480271f34a65f89050ea9df61ba50d1f8cf48
Gio Lodi <giovanni.lodi42@gmail.com>
2022-07-15 15:33:56 +1000

    DEBUG – Hack Buildkite pipeline and commands to test #19052

--- .buildkite/commands/release-build-wordpress.sh
+++ .buildkite/commands/release-build-wordpress.sh
@@ -3,25 +3,3 @@
 # FIXIT-13.1: Temporary fix until we're on the Xcode 13.1 VM
 echo "--- :rubygems: Fixing Ruby Setup"
 gem install bundler
-
-echo "--- :arrow_down: Installing Release Dependencies"
-brew update # Update homebrew to temporarily fix a bintray issue
-brew install imagemagick
-brew install ghostscript
-brew upgrade sentry-cli
-
-echo "--- :rubygems: Setting up Gems"
-install_gems
-
-echo "--- :cocoapods: Setting up Pods"
-install_cocoapods
-
-echo "--- :closed_lock_with_key: Installing Secrets"
-bundle exec fastlane run configure_apply
-
-echo "--- :hammer_and_wrench: Building"
-bundle exec fastlane build_and_upload_app_store_connect \
-  skip_confirm:true \
-  skip_prechecks:true \
-  create_release:true \
-  beta_release:${1:-true} # use first call param, default to true for safety
