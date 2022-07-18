2ced1bdd2e336e27afbb5646fa82e053de7b97c4
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2022-07-18 14:29:43 -0500

    Add JetpackBadgeCell and JetpackBadgeRow, a new type of ImmutableRow (using JetpackBadgeCell), containing the Jetpack powered banner

---
+++ WordPress/Classes/ViewRelated/Jetpack/Branding/Badge/JetpackBadgeCell.swift
@@ -0,0 +1,9 @@
+//
+//  JetpackBadgeCell.swift
+//  WordPress
+//
+//  Created by Giorgio Ruscigno on 7/18/22.
+//  Copyright © 2022 WordPress. All rights reserved.
+//
+
+import Foundation
---
+++ WordPress/Classes/ViewRelated/Jetpack/Branding/Badge/JetpackBadgeRow.swift
@@ -0,0 +1,9 @@
+//
+//  JetpackBadgeRow.swift
+//  WordPress
+//
+//  Created by Giorgio Ruscigno on 7/18/22.
+//  Copyright © 2022 WordPress. All rights reserved.
+//
+
+import Foundation
