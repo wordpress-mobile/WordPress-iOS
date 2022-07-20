c0e301b13c58bd45a1c9d6eb398965fcfc2b8fdf
Tiago Marques <tiago.marques@automattic.com>
2022-07-20 11:03:27 -0300

    Remove unused code

--- WordPress/WordPressUITests/Flows/LoginFlow.swift
+++ WordPress/WordPressUITests/Flows/LoginFlow.swift
@@ -49,49 +49,4 @@ class LoginFlow {
         }
         return try TabNavComponent()
     }
-
-    // Login with WP site via Site Address.
-    static func loginIfNeeded(siteUrl: String, email: String, password: String) throws -> TabNavComponent {
-        guard TabNavComponent.isLoaded() else {
-            return try login(siteUrl: siteUrl, email: email, password: password).tabBar
-        }
-        return try TabNavComponent()
-    }
-
-    static func logoutIfNeeded() throws {
-        try XCTContext.runActivity(named: "Log out of app if currently logged in") { (activity) in
-            if TabNavComponent.isLoaded() {
-                Logger.log(message: "Logging out...", event: .i)
-                let meScreen = try TabNavComponent().goToMeScreen()
-                if meScreen.isLoggedInToWpcom() {
-                    _ = try meScreen.logoutToPrologue()
-                } else {
-                    try meScreen.dismiss().removeSelfHostedSite()
-                }
-                return
-            }
-        }
-
-        try XCTContext.runActivity(named: "Return to app prologue screen if needed") { (activity) in
-            if !PrologueScreen.isLoaded() {
-                while PasswordScreen.isLoaded() || GetStartedScreen.isLoaded() || LinkOrPasswordScreen.isLoaded() || LoginSiteAddressScreen.isLoaded() || LoginUsernamePasswordScreen.isLoaded() || LoginCheckMagicLinkScreen.isLoaded() {
-                    if GetStartedScreen.isLoaded() && GetStartedScreen.isEmailEntered() {
-                        try GetStartedScreen().emailTextField.clearText()
-                    }
-                    navigateBack()
-                }
-            }
-
-            // TODO: remove when unifiedAuth is permanent.
-            // Leaving here for now in case unifiedAuth is disabled.
-//            if !WelcomeScreen.isLoaded() {
-//                while LoginPasswordScreen.isLoaded() || LoginEmailScreen.isLoaded() || LinkOrPasswordScreen.isLoaded() || LoginSiteAddressScreen.isLoaded() || LoginUsernamePasswordScreen.isLoaded() || LoginCheckMagicLinkScreen.isLoaded() {
-//                    if LoginEmailScreen.isLoaded() && LoginEmailScreen.isEmailEntered() {
-//                        LoginEmailScreen().emailTextField.clearTextIfNeeded()
-//                    }
-//                    navBackButton.tap()
-//                }
-//            }
-        }
-    }
 }
