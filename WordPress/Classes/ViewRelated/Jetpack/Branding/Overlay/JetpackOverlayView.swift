ef9361917d889aaa3a37239f34cf3e535b3bd95b
Giorgio Ruscigno <giorgio.ruscigno@automattic.com>
2022-07-28 17:46:02 -0500

    Add JetpacOverlayView, a view that contains the Jetpack powered overlay

---
+++ WordPress/Classes/ViewRelated/Jetpack/Branding/Overlay/JetpackOverlayView.swift
@@ -0,0 +1,121 @@
+import UIKit
+
+class JetpackOverlayView: UIView {
+
+    private var buttonAction: (() -> Void)?
+
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [imageView, titleLabel, descriptionLabel, getJetpackButton])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        return stackView
+    }()
+
+    // TODO: This will need to be replaced with the animated images
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: Images.jetpackLogo))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.adjustsFontForContentSizeCategory = true
+        label.adjustsFontSizeToFitWidth = true
+        label.font = WPStyleGuide.fontForTextStyle(.title1, fontWeight: .bold)
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        label.text = TextContent.title
+        return label
+    }()
+
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.adjustsFontForContentSizeCategory = true
+        label.adjustsFontSizeToFitWidth = true
+        label.font = .preferredFont(forTextStyle: .body)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.text = TextContent.description
+        return label
+    }()
+
+    private lazy var getJetpackButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .muriel(color: .jetpackGreen, .shade40)
+        button.setTitle(TextContent.buttonTitle, for: .normal)
+        button.titleLabel?.adjustsFontSizeToFitWidth = true
+        button.titleLabel?.adjustsFontForContentSizeCategory = true
+        button.layer.cornerRadius = Metrics.buttonCornerRadius
+        button.layer.cornerCurve = .continuous
+        return button
+    }()
+
+    @objc private func didTapButton() {
+
+    }
+
+    private func setup(buttonAction: (() -> Void)?) {
+        backgroundColor = UIColor(light: .muriel(color: .jetpackGreen, .shade0),
+                                  dark: .muriel(color: .jetpackGreen, .shade100))
+        addSubview(stackView)
+        stackView.setCustomSpacing(Metrics.largeSpacing, after: imageView)
+        stackView.setCustomSpacing(Metrics.largeSpacing, after: descriptionLabel)
+        getJetpackButton.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        configureConstraints()
+    }
+
+    init(buttonAction: (() -> Void)? = nil) {
+        super.init(frame: .zero)
+        setup(buttonAction: buttonAction)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configureConstraints() {
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Metrics.edgeMargins.left),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Metrics.edgeMargins.right),
+            stackView.topAnchor.constraint(equalTo: topAnchor, constant: Metrics.edgeMargins.top),
+            stackView.bottomAnchor.constraint(lessThanOrEqualTo: safeBottomAnchor, constant: -Metrics.edgeMargins.bottom),
+
+            getJetpackButton.heightAnchor.constraint(greaterThanOrEqualToConstant: Metrics.getJetpackButtonHeight),
+            getJetpackButton.widthAnchor.constraint(equalTo: stackView.widthAnchor),
+        ])
+    }
+}
+
+
+// MARK: Appearance
+private extension JetpackOverlayView {
+
+    enum Images {
+        static let jetpackLogo = "jetpack-install-logo"
+    }
+
+    enum Metrics {
+        static let stackSpacing: CGFloat = 10
+        static let largeSpacing: CGFloat = 46
+        static let edgeMargins = UIEdgeInsets(top: largeSpacing, left: 20, bottom: 20, right: 20)
+        static let getJetpackButtonHeight: CGFloat = 44
+        static let minimumScaleFactor: CGFloat = 0.6
+        static let buttonCornerRadius: CGFloat = 6
+    }
+
+    enum TextContent {
+        static let title = NSLocalizedString("jetpack.branding.overlay.title",
+                                             value: "Jetpack powered",
+                                             comment: "Title of the Jetpack powered overlay.")
+
+        static let description = NSLocalizedString("jetpack.branding.overlay.description",
+                                                   value: "Stats, Reader, Notifications, and other features are provided by Jetpack.",
+                                                   comment: "Description of the Jetpack powered overlay.")
+
+        static let buttonTitle = NSLocalizedString("jetpack.branding.overlay.button.title",
+                                                   value: "Get the new Jetpack app",
+                                                   comment: "Button title of the Jetpack powered overlay.")
+    }
+}
