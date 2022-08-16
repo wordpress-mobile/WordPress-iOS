51bb4340402806c7dfc95750c2174b9f2a8282b5
Gio Lodi <giovanni.lodi42@gmail.com>
2022-08-16 20:34:17 +1000

    Stop inject UI tests target name in CI; There's only one to use!

--- .buildkite/commands/run-ui-tests.sh
+++ .buildkite/commands/run-ui-tests.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -eu
 
-TEST_NAME=$1
-DEVICE=$2
-IOS_VERSION=$3
+DEVICE=$1
+IOS_VERSION=$2
 
-echo "Running $TEST_NAME on $DEVICE for iOS $IOS_VERSION"
+echo "Running UI tests $DEVICE for iOS $IOS_VERSION"
 
 # Run this at the start to fail early if value not available
 echo '--- :test-analytics: Configuring Test Analytics'
@@ -36,7 +35,7 @@ echo "--- 🔬 Testing"
 xcrun simctl list >> /dev/null
 rake mocks &
 set +e
-bundle exec fastlane test_without_building name:"$TEST_NAME" device:"$DEVICE" ios_version:"$IOS_VERSION"
+bundle exec fastlane test_without_building name:WordPressUITests device:"$DEVICE" ios_version:"$IOS_VERSION"
 TESTS_EXIT_STATUS=$?
 set -e
 
