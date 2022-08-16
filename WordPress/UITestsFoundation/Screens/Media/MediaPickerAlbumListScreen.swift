f5e87a8620a7efac8f062e3a78f671e0e4eb98dd
Tiago Marques <tiago.marques@automattic.com>
2022-08-16 17:47:32 -0300

    Add selectedAlbum element state logs

--- WordPress/UITestsFoundation/Screens/Media/MediaPickerAlbumListScreen.swift
+++ WordPress/UITestsFoundation/Screens/Media/MediaPickerAlbumListScreen.swift
@@ -18,6 +18,11 @@ public class MediaPickerAlbumListScreen: ScreenObject {
     public func selectAlbum(atIndex index: Int) throws -> MediaPickerAlbumScreen {
         Logger.log(message: app.debugDescription, event: .i)
         let selectedAlbum = albumListGetter(app).cells.element(boundBy: index)
+        Logger.log(message: "EXISTS: \(selectedAlbum.exists)", event: .i)
+        Logger.log(message: "IS HITTABLE: \(selectedAlbum.isHittable)", event: .i)
+        Logger.log(message: "IS ENABLED: \(selectedAlbum.isEnabled)", event: .i)
+        Logger.log(message: "IS SELECTED: \(selectedAlbum.isSelected)", event: .i)
+        Logger.log(message: "IS A. ELEMENT:\(selectedAlbum.isAccessibilityElement)", event: .i)
         XCTAssertTrue(selectedAlbum.waitForExistence(timeout: 5), "Selected album did not load")
         selectedAlbum.tap()
 
