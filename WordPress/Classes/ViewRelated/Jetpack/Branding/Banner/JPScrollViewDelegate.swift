9dc27140b2023ba046876917cc890d1b61eb6e6c
Tanner W. Stokes <tanner.stokes@automattic.com>
2022-08-16 23:16:47 -0400

    Add scroll visibility type and tests.

--- WordPress/Classes/ViewRelated/Jetpack/Branding/Banner/JPScrollViewDelegate.swift
+++ WordPress/Classes/ViewRelated/Jetpack/Branding/Banner/JPScrollViewDelegate.swift
@@ -16,28 +16,7 @@ extension JPScrollViewDelegate {
     }
 
     func processJetpackBannerVisibility(_ scrollView: UIScrollView) {
-        let shouldHideJetpackBanner = Self.shouldHideJetpackBanner(
-            contentHeight: scrollView.contentSize.height,
-            frameHeight: scrollView.frame.height,
-            verticalContentOffset: scrollView.contentOffset.y + scrollView.adjustedContentInset.top
-        )
-
-        scrollViewTranslationPublisher.send(shouldHideJetpackBanner)
-    }
-
-    static func shouldHideJetpackBanner(
-        contentHeight: CGFloat,
-        frameHeight: CGFloat,
-        verticalContentOffset: CGFloat
-    ) -> Bool {
-        /// The scrollable content isn't any larger than its frame, so don't hide the banner if the view is bounced.
-        if contentHeight <= frameHeight {
-            return false
-        /// Don't hide the banner until the view has scrolled down some. Currently the height of the banner itself.
-        } else if verticalContentOffset <= JetpackBannerView.minimumHeight {
-            return false
-        }
-
-        return true
+        let shouldHide = JetpackBannerScrollVisibility.shouldHide(scrollView)
+        scrollViewTranslationPublisher.send(shouldHide)
     }
 }
