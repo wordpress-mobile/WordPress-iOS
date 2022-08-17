7bd4d8c7321da9925f71c2ce77c701f7b97895d8
Tanner W. Stokes <tanner.stokes@automattic.com>
2022-08-16 22:33:57 -0400

    Send a bool to the banner view for visibility. Extend scroll delegate with visibility logic.

--- WordPress/Classes/ViewRelated/Jetpack/Branding/Banner/JPScrollViewDelegate.swift
+++ WordPress/Classes/ViewRelated/Jetpack/Branding/Banner/JPScrollViewDelegate.swift
@@ -1,10 +1,11 @@
 import Combine
 import CoreGraphics
+import UIKit
 
 /// Conform to this protocol to send scrollview translations to a `JetpackBannerView` instance
 protocol JPScrollViewDelegate: UIScrollViewDelegate {
 
-    var scrollViewTranslationPublisher: PassthroughSubject<CGFloat, Never> { get }
+    var scrollViewTranslationPublisher: PassthroughSubject<Bool, Never> { get }
     func addTranslationObserver(_ receiver: JetpackBannerView)
 }
 
@@ -12,5 +13,31 @@ extension JPScrollViewDelegate {
 
     func addTranslationObserver(_ receiver: JetpackBannerView) {
         scrollViewTranslationPublisher.subscribe(receiver)
+    }
+
+    func processJetpackBannerVisibility(_ scrollView: UIScrollView) {
+        let shouldHideJetpackBanner = Self.shouldHideJetpackBanner(
+            contentHeight: scrollView.contentSize.height,
+            frameHeight: scrollView.frame.height,
+            verticalContentOffset: scrollView.contentOffset.y + scrollView.adjustedContentInset.top
+        )
+
+        scrollViewTranslationPublisher.send(shouldHideJetpackBanner)
+    }
+
+    static func shouldHideJetpackBanner(
+        contentHeight: CGFloat,
+        frameHeight: CGFloat,
+        verticalContentOffset: CGFloat
+    ) -> Bool {
+        /// The scrollable content isn't any larger than its frame, so don't hide the banner if the view is bounced.
+        if contentHeight <= frameHeight {
+            return false
+        /// Don't hide the banner until the view has scrolled down some. Currently the height of the banner itself.
+        } else if verticalContentOffset <= JetpackBannerView.minimumHeight {
+            return false
+        }
+
+        return true
     }
 }
