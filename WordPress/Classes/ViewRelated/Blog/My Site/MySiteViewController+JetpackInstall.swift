36b29c455e69fe27dede3cb7471696d41400e57c
Povilas Staskus <povilas.staskus@automattic.com>
2022-08-30 13:37:07 +0300

    Simplify presentation of JetpackInstallPrompt
    The install coordination is now managed by JetpackInstallCoordinator and coming back to the prompt is not allowed, so we don't need to handle corner cases anymore

--- WordPress/Classes/ViewRelated/Blog/My Site/MySiteViewController+JetpackInstall.swift
+++ WordPress/Classes/ViewRelated/Blog/My Site/MySiteViewController+JetpackInstall.swift
@@ -13,56 +13,20 @@ extension MySiteViewController {
         }
 
         let installPromptViewController = JetpackInstallPromptViewController(blog: blog)
-        let installPromptNavigationController = UINavigationController(rootViewController: installPromptViewController)
-        installPromptNavigationController.modalPresentationStyle = .fullScreen
-
-        installPromptViewController.actionHandler = { [weak self] action in
-            switch action {
-            case .noThanks:
-                installPromptNavigationController.dismiss(animated: true)
-            case .install:
-                self?.presentJetpackInstall(on: installPromptNavigationController, blog: blog, jetpackInstallPromptSettings: promptSettings)
-            }
-        }
-
-        present(installPromptNavigationController, animated: true)
-    }
-
-    /// Presents the Jetpack plugin install flow on top of the prompt
-    private func presentJetpackInstall(
-        on controller: UINavigationController,
-        blog: Blog,
-        jetpackInstallPromptSettings: JetpackInstallPromptSettings
-    ) {
-        let jetpackLoginViewController = JetpackLoginViewController(blog: blog)
-        jetpackLoginViewController.promptType = .installPrompt
-        let navigationController = UINavigationController(rootViewController: jetpackLoginViewController)
+        let navigationController = UINavigationController(rootViewController: installPromptViewController)
         navigationController.modalPresentationStyle = .fullScreen
 
-        controller.present(navigationController, animated: true)
+        installPromptViewController.dismiss = { [weak self] dismissAction in
+            promptSettings.setPromptWasDismissed(true, for: blog)
 
-        jetpackLoginViewController.completionBlock = { [weak self] in
-            jetpackInstallPromptSettings.setPromptWasDismissed(true, for: blog)
-
-            /// If Jetpack install + WP.com login flow is completed, the blog will be synced and jetpack.isConnected will be true
-            /// Dismissing the prompt
-            if let jetpack = blog.jetpack, jetpack.isConnected {
+            switch dismissAction {
+            case .install:
                 self?.syncBlogs()
-                controller.dismiss(animated: true)
-
-            /// If Jetpack install + WP.com login flow was not fully completed, the blog will not be synced
-            /// Jetpack.isConnected can still be true if the flow is completed except for WP.com login
-            /// Sync the blog and dismiss the prompt if Jetpack.isConnected
-            } else {
-                let blogService = BlogService(managedObjectContext: ContextManager.shared.mainContext)
-                blogService.syncBlog(blog) {
-                    if let jetpack = blog.jetpack, jetpack.isConnected {
-                        controller.dismiss(animated: true)
-                    }
-                } failure: { _ in
-                    controller.dismiss(animated: true)
-                }
+            default:
+                break
             }
         }
+
+        present(navigationController, animated: true)
     }
 }
