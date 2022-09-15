e64e32a2af7a526c4c732db2d4fcddaf74071645
Tony Li <tony.li@automattic.com>
2022-09-15 14:50:32 +1200

    The bundler workaround is not needed when using xcode-14 CI image
    Resolves https://github.com/Automattic/bash-cache-buildkite-plugin/issues/16

--- .buildkite/commands/run-unit-tests.sh
+++ .buildkite/commands/run-unit-tests.sh
@@ -8,10 +8,6 @@ echo "--- 📦 Downloading Build Artifacts"
 download_artifact build-products.tar
 tar -xf build-products.tar
 
-# Temporary fix until we're on the Xcode 13.1 VM
-echo "--- :rubygems: Fixing Ruby Setup"
-gem install bundler
-
 echo "--- :rubygems: Setting up Gems"
 install_gems
 
