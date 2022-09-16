047f6708c90da38900a6e07976417e6326755e33
Tony Li <tony.li@automattic.com>
2022-09-16 18:17:34 +1200

    Move the function that counts local draft out of `PostService`

--- WordPress/Classes/Services/PostService.m
+++ WordPress/Classes/Services/PostService.m
@@ -777,14 +777,6 @@ typedef void (^AutosaveSuccessBlock)(RemotePost *post, NSString *previewURL);
     return [posts firstObject];
 }
 
-- (NSUInteger)countPostsWithoutRemote
-{
-    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:NSStringFromClass([AbstractPost class])];
-    request.predicate = [NSPredicate predicateWithFormat:@"postID = NULL OR postID <= 0"];
-
-    return [self.managedObjectContext countForFetchRequest:request error:nil];
-}
-
 - (NSDictionary *)remoteSyncParametersDictionaryForRemote:(nonnull id <PostServiceRemote>)remote
                                               withOptions:(nonnull PostServiceSyncOptions *)options
 {
