1389a8f6ebf0d658d8ed0417cd28b52e6af816d2
Povilas Staskus <povilas.staskus@automattic.com>
2022-09-21 12:28:17 +0300

    Update UI based on new requirements

--- WordPress/Classes/ViewRelated/Jetpack/JetpackInstallPromptViewController.swift
+++ WordPress/Classes/ViewRelated/Jetpack/JetpackInstallPromptViewController.swift
@@ -11,41 +11,21 @@ protocol JetpackInstallPromptDelegate: AnyObject {
 }
 
 class JetpackInstallPromptViewController: UIViewController {
-    var starFieldView: StarFieldView = {
-        let config = StarFieldViewConfig(particleImage: JetpackPromptStyles.Stars.particleImage,
-                                         starColors: JetpackPromptStyles.Stars.colors)
-        let view = StarFieldView(with: config)
-        view.layer.masksToBounds = true
-        return view
-    }()
-
-    var gradientLayer: CALayer = {
-        let gradientLayer = CAGradientLayer()
-
-        // Start color is the background color with no alpha because if we use clear it will fade to black
-        // instead of just disappearing
-        let startColor = JetpackPromptStyles.backgroundColor.withAlphaComponent(0)
-        let endColor = JetpackPromptStyles.backgroundColor
-
-        gradientLayer.colors = [startColor.cgColor, endColor.cgColor]
-        gradientLayer.locations = [0.0, 0.9]
-
-        return gradientLayer
-    }()
 
     // MARK: - IBOutlets
-    @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var textStackView: UIStackView!
 
     @IBOutlet weak var buttonsStackView: UIStackView!
     @IBOutlet weak var installButton: FancyButton!
     @IBOutlet weak var noThanksButton: FancyButton!
-    @IBOutlet weak var learnMoreButton: FancyButton!
 
-    @IBOutlet weak var textStackView: UIStackView!
-    @IBOutlet weak var lineItemStats: UILabel!
-    @IBOutlet weak var lineItemNotifications: UILabel!
-    @IBOutlet weak var lineItemAndMore: UILabel!
+    private lazy var learnMoreButton: UIBarButtonItem = {
+        let buttonItem = UIBarButtonItem(target: self, action: #selector(learnMoreButtonTapped))
+        navigationItem.rightBarButtonItem = buttonItem
+        return buttonItem
+    }()
 
     // MARK: - Properties
 
@@ -77,26 +57,14 @@ class JetpackInstallPromptViewController: UIViewController {
         applyStyles()
         localizeText()
         configureAccessibility()
-
-        backgroundView.addSubview(starFieldView)
-        backgroundView.layer.addSublayer(gradientLayer)
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
-        navigationController?.setNavigationBarHidden(true, animated: true)
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return [.portrait, .portraitUpsideDown]
-    }
-
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-
-        starFieldView.frame = view.bounds
-        gradientLayer.frame = view.bounds
     }
 
     // MARK: - Actions
@@ -135,69 +103,58 @@ class JetpackInstallPromptViewController: UIViewController {
     // MARK: - Private: Helpers
     private func localizeText() {
         titleLabel.text = Strings.title
-
-        lineItemStats.text = Strings.stats
-        lineItemNotifications.text = Strings.notifications
-        lineItemAndMore.text = Strings.andMore
+        navigationItem.title = Strings.navigationTitle
+        descriptionLabel.text = Strings.description
 
         installButton.setTitle(Strings.installButton, for: .normal)
-        learnMoreButton.setTitle(Strings.learnMoreButton, for: .normal)
         noThanksButton.setTitle(Strings.noThanksButton, for: .normal)
+        learnMoreButton.title = Strings.learnMoreButton
     }
 
     private func applyStyles() {
-        view.backgroundColor = JetpackPromptStyles.backgroundColor
-        backgroundView.backgroundColor = JetpackPromptStyles.backgroundColor
+        WPStyleGuide.configureColors(view: view, tableView: nil)
 
-        // Title
         titleLabel.font = JetpackPromptStyles.Title.font
         titleLabel.textColor = JetpackPromptStyles.Title.textColor
         titleLabel.adjustsFontForContentSizeCategory = true
 
-        // Line items
-        let lineItems = [lineItemStats, lineItemNotifications, lineItemAndMore]
-        for lineItem in lineItems {
-            lineItem?.font = JetpackPromptStyles.LineItem.font
-            lineItem?.textColor = JetpackPromptStyles.LineItem.textColor
-            lineItem?.adjustsFontForContentSizeCategory = true
-        }
+        descriptionLabel.font = JetpackPromptStyles.Description.font
+        descriptionLabel.textColor = JetpackPromptStyles.Description.textColor
+        descriptionLabel.adjustsFontForContentSizeCategory = true
 
-        // Buttons
-        installButton.isPrimary = true
-        configure(button: installButton, style: JetpackPromptStyles.installButtonStyle)
-        configure(button: noThanksButton, style: JetpackPromptStyles.noThanksButtonStyle)
-        configure(button: learnMoreButton, style: JetpackPromptStyles.learnMoreButtonStyle)
+        configureButtons()
     }
 
-    private func configure(button: FancyButton, style: NUXButtonStyle) {
-        guard button.isPrimary else {
-            button.secondaryNormalBackgroundColor = style.normal.backgroundColor
-            button.secondaryNormalBorderColor = style.normal.borderColor
-            button.secondaryTitleColor = style.normal.titleColor
-            button.secondaryHighlightBackgroundColor = style.highlighted.backgroundColor
-            button.secondaryHighlightBorderColor = style.highlighted.borderColor
-            return
-        }
+    private func configureButtons() {
+        installButton.isPrimary = true
 
-        button.primaryNormalBackgroundColor = style.normal.backgroundColor
-        button.primaryNormalBorderColor = style.normal.borderColor
-        button.primaryTitleColor = style.normal.titleColor
-        button.primaryHighlightBackgroundColor = style.highlighted.backgroundColor
-        button.primaryHighlightBorderColor = style.highlighted.borderColor
+        noThanksButton.secondaryNormalBorderColor = .clear
+        noThanksButton.secondaryHighlightBorderColor = .clear
+        noThanksButton.secondaryTitleColor = JetpackPromptStyles.Button.textColor
+        noThanksButton.secondaryHighlightBackgroundColor = .clear
+
+        learnMoreButton.setTitleTextAttributes([.foregroundColor: JetpackPromptStyles.Button.textColor], for: .normal)
+        learnMoreButton.setTitleTextAttributes([.foregroundColor: JetpackPromptStyles.Button.highlightedTextColor], for: .highlighted)
     }
 
     private func configureAccessibility() {
+        learnMoreButton.isAccessibilityElement = true
+        learnMoreButton.accessibilityLabel = Strings.learnMoreButton
         learnMoreButton.accessibilityHint = Strings.learnMoreButtonHint
         learnMoreButton.accessibilityTraits = .link
 
+        installButton.isAccessibilityElement = true
+        installButton.accessibilityLabel = Strings.installButton
         installButton.accessibilityHint = Strings.installButtonHint
         installButton.accessibilityTraits = .button
 
+        noThanksButton.isAccessibilityElement = true
+        noThanksButton.accessibilityLabel = Strings.noThanksButton
         noThanksButton.accessibilityHint = Strings.noThanksButtonHint
         noThanksButton.accessibilityTraits = .button
 
         textStackView.isAccessibilityElement = true
-        textStackView.accessibilityLabel = [Strings.title, Strings.stats, Strings.notifications, Strings.andMore].joined(separator: "\n")
+        textStackView.accessibilityLabel = [Strings.title, Strings.description].joined(separator: "\n")
     }
 }
 
@@ -208,77 +165,32 @@ extension NSNotification.Name {
 
 // MARK: - Localization
 private struct Strings {
-    static let title = NSLocalizedString("Would you like to install Jetpack?", comment: "Title for the prompt view asking the user if they'd like to install jetpack")
+    static let navigationTitle = NSLocalizedString("Jetpack", comment: "Navigation title for Jetpack install prompt")
+    static let title = NSLocalizedString("Make WordPress better by installing the Jetpack plugin.", comment: "Title for the prompt view explaining the user why they should install Jetpack plugin.")
+    static let description = NSLocalizedString("Get access to all your Jetpack features including Stats, Notifications, Backup, Security and much more by installing the Jetpack plugin for WordPress.", comment: "Label informing the user of the benefits of Jetpack plugin")
 
-    static let stats = NSLocalizedString("Clear, concise, and actionable site stats about visitors and traffic.", comment: "Label informing the user of the benefits of stats")
-    static let notifications = NSLocalizedString("Keep up with your site’s activity, even when you're away from your desk.", comment: "Label informing the user of the benefits of notifications")
-    static let andMore = NSLocalizedString("... and more!", comment: "Label, hint there are more features")
-
-    static let learnMoreButton = NSLocalizedString("Learn More", comment: "Button title, opens a webview with more features")
+    static let learnMoreButton = NSLocalizedString("Learn more", comment: "Navigation button title, opens a webview with more features")
     static let learnMoreButtonHint = NSLocalizedString("Opens Jetpack website for more information.", comment: "VoiceOver accessibility hint, informating the user that the button opens a website with more information")
-    static let installButton = NSLocalizedString("Install", comment: "Button title, accepting the install offer")
+    static let installButton = NSLocalizedString("Install Jetpack", comment: "Button title, accepting the install offer")
     static let installButtonHint = NSLocalizedString("Opens the Jetpack installation view.", comment: "VoiceOver accessibility hint, informating the user that the button opens a view that installs Jetpack")
-    static let noThanksButton = NSLocalizedString("No Thanks", comment: "Button title, declining the offer")
+    static let noThanksButton = NSLocalizedString("Continue without Jetpack", comment: "Button title, declining the offer")
     static let noThanksButtonHint = NSLocalizedString("Closes the installation prompt.", comment: "VoiceOver accessibility hint, informating the user that the button closes current view")
 }
 
 // MARK: - Styles
 private struct JetpackPromptStyles {
-    static let backgroundColor = UIColor(red: 0.00, green: 0.11, blue: 0.18, alpha: 1.00)
+    struct Button {
+        static let textColor = UIColor.primary
+        static let highlightedTextColor = UIColor.primaryDark
+    }
 
     struct Title {
-        static let font: UIFont = WPStyleGuide.fontForTextStyle(.title2, fontWeight: .semibold)
-        static let textColor: UIColor = .white
+        static let font: UIFont = WPStyleGuide.fontForTextStyle(.title1, fontWeight: .semibold)
+        static let textColor: UIColor = .black
     }
 
-    struct LineItem {
+    struct Description {
         static let font: UIFont = WPStyleGuide.fontForTextStyle(.body, fontWeight: .regular)
-        static let textColor: UIColor = .white
+        static let textColor: UIColor = .black
     }
-
-    struct Stars {
-        static let particleImage = UIImage(named: "jetpack-circle-particle")
-
-        static let colors = [
-            UIColor(red: 0.05, green: 0.27, blue: 0.44, alpha: 0.5),
-            UIColor(red: 0.64, green: 0.68, blue: 0.71, alpha: 0.5),
-            UIColor(red: 0.99, green: 0.99, blue: 0.99, alpha: 0.5)
-        ]
-    }
-    static let installButtonStyle = NUXButtonStyle(normal: .init(backgroundColor: .white,
-                                                                 borderColor: .white,
-                                                                 titleColor: Self.backgroundColor),
-
-                                                   highlighted: .init(backgroundColor: UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 0.90),
-                                                                      borderColor: UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 0.90),
-                                                                      titleColor: Self.backgroundColor),
-
-                                                   disabled: .init(backgroundColor: .white,
-                                                                   borderColor: .white,
-                                                                   titleColor: Self.backgroundColor))
-
-    static let noThanksButtonStyle = NUXButtonStyle(normal: .init(backgroundColor: Self.backgroundColor,
-                                                                   borderColor: UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 0.40),
-                                                                   titleColor: .white),
-
-                                                     highlighted: .init(backgroundColor: Self.backgroundColor,
-                                                                        borderColor: UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 0.20),
-                                                                        titleColor: UIColor.white.withAlphaComponent(0.7)),
-
-                                                     disabled: .init(backgroundColor: .white,
-                                                                     borderColor: .white,
-                                                                     titleColor: Self.backgroundColor))
-
-    static let learnMoreButtonStyle = NUXButtonStyle(normal: .init(backgroundColor: Self.backgroundColor,
-                                                                   borderColor: Self.backgroundColor,
-                                                                   titleColor: .white),
-
-                                                     highlighted: .init(backgroundColor: Self.backgroundColor,
-                                                                        borderColor: UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 0.20),
-                                                                        titleColor: UIColor.white.withAlphaComponent(0.7)),
-
-                                                     disabled: .init(backgroundColor: .white,
-                                                                     borderColor: .white,
-                                                                     titleColor: Self.backgroundColor))
-
 }
