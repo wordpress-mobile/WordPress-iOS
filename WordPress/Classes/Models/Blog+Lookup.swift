618c70ca1df1db9b637a187053cf1b982da7578f
Tony Li <tony.li@automattic.com>
2022-09-29 10:07:03 +1300

    Improve API doc comments

--- WordPress/Classes/Models/Blog+Lookup.swift
+++ WordPress/Classes/Models/Blog+Lookup.swift
@@ -62,7 +62,7 @@ public extension Blog {
     /// Find a cached comment with given ID.
     ///
     /// - Parameter id: The comment id
-    /// - Returns: The `Comment` object associated with the given id.
+    /// - Returns: The `Comment` object associated with the given id, or `nil` if none is found.
     @objc
     func comment(withID id: NSNumber) -> Comment? {
         comment(withID: id.int32Value)
@@ -71,7 +71,7 @@ public extension Blog {
     /// Find a cached comment with given ID.
     ///
     /// - Parameter id: The comment id
-    /// - Returns: The `Comment` object associated with the given id.
+    /// - Returns: The `Comment` object associated with the given id, or `nil` if none is found.
     func comment(withID id: Int32) -> Comment? {
         (comments as? Set<Comment>)?.first { $0.commentID == id }
     }
--- WordPress/Classes/Models/ReaderPost+Helper.swift
+++ WordPress/Classes/Models/ReaderPost+Helper.swift
@@ -5,7 +5,7 @@ extension ReaderPost {
     /// Find cached comment with given ID.
     ///
     /// - Parameter id: The comment id
-    /// - Returns: The `Comment` object associated with the given id.
+    /// - Returns: The `Comment` object associated with the given id, or `nil` if none is found.
     @objc
     func comment(withID id: NSNumber) -> Comment? {
         comment(withID: id.int32Value)
@@ -14,7 +14,7 @@ extension ReaderPost {
     /// Find cached comment with given ID.
     ///
     /// - Parameter id: The comment id
-    /// - Returns: The `Comment` object associated with the given id.
+    /// - Returns: The `Comment` object associated with the given id, or `nil` if none is found.
     func comment(withID id: Int32) -> Comment? {
         return (comments as? Set<Comment>)?.first { $0.commentID == id }
     }
