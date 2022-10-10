a2b4ac80feffb3decea4c0a6be9ac1e68ff3cf66
Tanner W. Stokes <tanner.stokes@automattic.com>
2022-10-10 19:49:10 -0400

    Simplify view rendering by doubling regardless of prompt count.

--- WordPress/Jetpack/Classes/NUX/New Landing Screen/Views/JetpackLandingScreenView.swift
+++ WordPress/Jetpack/Classes/NUX/New Landing Screen/Views/JetpackLandingScreenView.swift
@@ -6,19 +6,9 @@ struct JetpackLandingScreenView: View {
     private let evenColor = JetpackPromptsConfiguration.Constants.evenColor
     private let oddColor = JetpackPromptsConfiguration.Constants.oddColor
 
-    private var loopCount: Int {
-        if prompts.count % 2 == 0 {
-            return prompts.count
-        }
-
-        /// The number of prompts is odd so we loop twice
-        /// to prevent the same color from occurring when the view repeats.
-        return prompts.count * 2
-    }
-
     var body: some View {
         VStack(alignment: .leading) {
-            ForEach(0..<loopCount, id: \.hashValue) { index in
+            ForEach(0..<prompts.count * 2, id: \.hashValue) { index in
                 JetpackPromptView(
                     text: prompts[index % prompts.count],
                     color: index % 2 == 0 ? evenColor : oddColor
