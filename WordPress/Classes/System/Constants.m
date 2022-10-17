2065bf8093e9455f0a2513afca3e2cfb79d548ed
Povilas Staskus <povilas.staskus@automattic.com>
2022-10-17 17:35:08 +0300

    Split part of Constants into AppConstants to have different constants for different apps
    - Jetpack and WordPress apps share the same AppGroup
    - We need to differentiate between the keys of UserDefaults and file names to save different Widget preferences for Jetpack and WordPress apps

--- WordPress/Classes/System/Constants.m
+++ WordPress/Classes/System/Constants.m
@@ -50,29 +50,10 @@ NSString *const WPShareExtensionKeychainTokenKey                    = @"OAuth2To
 NSString *const WPShareExtensionKeychainServiceName                 = @"ShareExtension";
 NSString *const WPShareExtensionUserDefaultsPrimarySiteName         = @"WPShareUserDefaultsPrimarySiteName";
 NSString *const WPShareExtensionUserDefaultsPrimarySiteID           = @"WPShareUserDefaultsPrimarySiteID";
-NSString *const WPStatsHomeWidgetsUserDefaultsLoggedInKey           = @"WordPressHomeWidgetsLoggedIn";
 NSString *const WPShareExtensionUserDefaultsLastUsedSiteName        = @"WPShareUserDefaultsLastUsedSiteName";
 NSString *const WPShareExtensionUserDefaultsLastUsedSiteID          = @"WPShareUserDefaultsLastUsedSiteID";
 NSString *const WPShareExtensionMaximumMediaDimensionKey            = @"WPShareExtensionMaximumMediaDimensionKey";
 NSString *const WPShareExtensionRecentSitesKey                      = @"WPShareExtensionRecentSitesKey";
-
-/// Today Widget Constants
-///
-NSString *const WPStatsTodayWidgetKeychainTokenKey                  = @"OAuth2Token";
-NSString *const WPStatsTodayWidgetKeychainServiceName               = @"TodayWidget";
-NSString *const WPStatsTodayWidgetUserDefaultsSiteIdKey             = @"WordPressTodayWidgetSiteId";
-NSString *const WPStatsHomeWidgetsUserDefaultsSiteIdKey             = @"WordPressHomeWidgetsSiteId";
-NSString *const WPStatsTodayWidgetUserDefaultsSiteNameKey           = @"WordPressTodayWidgetSiteName";
-NSString *const WPStatsTodayWidgetUserDefaultsSiteUrlKey            = @"WordPressTodayWidgetSiteUrl";
-NSString *const WPStatsTodayWidgetUserDefaultsSiteTimeZoneKey       = @"WordPressTodayWidgetTimeZone";
-
-/// iOS 14 Widget Constants
-NSString *const WPHomeWidgetTodayKind                               = @"WordPressHomeWidgetToday";
-NSString *const WPHomeWidgetAllTimeKind                             = @"WordPressHomeWidgetAllTime";
-NSString *const WPHomeWidgetThisWeekKind                            = @"WordPressHomeWidgetThisWeek";
-NSString *const WPHomeWidgetTodayProperties                         = @"WordPressHomeWidgetTodayProperties";
-NSString *const WPHomeWidgetAllTimeProperties                       = @"WordPressHomeWidgetAllTimeProperties";
-NSString *const WPHomeWidgetThisWeekProperties                      = @"WordPressHomeWidgetThisWeekProperties";
 
 /// Apple ID Constants
 ///
