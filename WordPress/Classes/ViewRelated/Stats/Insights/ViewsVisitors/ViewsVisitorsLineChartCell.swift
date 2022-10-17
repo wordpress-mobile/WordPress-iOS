65bcdd4c02a5e3a3b17efc6405f00c29b377de72
Gio Lodi <gio@mokacoding.com>
2022-10-17 13:34:16 +1100

    Ensure `differenceLabel` is a localized strings literals
    `bundle exec fastlane complete_code_freeze` failed with the following
    error, thrown as part of the `generate_strings_file_for_glotpress` lane:
    
    ```
    [13:20:21]: genstrings: error: bad entry in file WordPress/Classes/ViewRelated/Stats/Insights/ViewsVisitors/ViewsVisitorsLineChartCell.swift (line = 58): Argument is not a literal string.
    [13:20:21]: fastlane finished with errors
    
    [!] genstrings: error: bad entry in file WordPress/Classes/ViewRelated/Stats/Insights/ViewsVisitors/ViewsVisitorsLineChartCell.swift (line = 58): Argument is not a literal string.
    ```

--- WordPress/Classes/ViewRelated/Stats/Insights/ViewsVisitors/ViewsVisitorsLineChartCell.swift
+++ WordPress/Classes/ViewRelated/Stats/Insights/ViewsVisitors/ViewsVisitorsLineChartCell.swift
@@ -54,14 +54,34 @@ struct StatsSegmentedControlData {
     }
 
     var differenceLabel: String {
-        let stringFormatValue = differencePercent != 0 ? "%@%@ (%@%%)" : "%@%@"
-        let stringFormat = NSLocalizedString(stringFormatValue, comment: "Difference label for Insights Overview stat, indicating change from previous period. Ex: +99.9K(5%)")
-        return String.localizedStringWithFormat(
-            stringFormat,
-            difference < 0 ? "" : "+",
-            difference.abbreviatedString(),
-            differencePercent.abbreviatedString()
-        )
+        // We want to show something like "+1.2K (5%)" if we have a percentage difference and "1.2K" if we don't.
+        // Because localized strings need to be strings literal, we cannot embed any conditional logic in the `localizedString...` call.
+        // We therefore need to generate different string literals base on the state.
+        let differenceSign = difference < 0 ? "" : "+"
+        if differencePercent != 0 {
+            let stringFormat = NSLocalizedString(
+                "insights.visitorsLineChartCell.differenceLabelWithPercentage",
+                value: "%@%@ (%@%%)",
+                comment: "Difference label for Insights Overview stat, indicating change from previous period, including percentage value. Example: +99.9K (5%)"
+            )
+            return String.localizedStringWithFormat(
+                stringFormat,
+                differenceSign,
+                difference.abbreviatedString(),
+                differencePercent.abbreviatedString()
+            )
+        } else {
+            let stringFormat = NSLocalizedString(
+                "insights.visitorsLineChartCell.differenceLabelWithoutPercentage",
+                value: "%@%@",
+                comment: "Difference label for Insights Overview stat, indicating change from previous period. Example: +99.9K"
+            )
+            return String.localizedStringWithFormat(
+                stringFormat,
+                differenceSign,
+                difference.abbreviatedString()
+            )
+        }
     }
 
     var differenceTextColor: UIColor {
