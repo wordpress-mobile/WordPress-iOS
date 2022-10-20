d1591dac79b2efc8a99dda1d71506f855bc7936e
Salim Braksa <salim.braksa@gmail.com>
2022-10-20 23:28:51 +0100

    Refactor JetpackLandingScreenView to UIKit and reduce prompts line spacing

--- WordPress/Jetpack/Classes/NUX/New Landing Screen/Views/JetpackLandingScreenView.swift
+++ WordPress/Jetpack/Classes/NUX/New Landing Screen/Views/JetpackLandingScreenView.swift
@@ -1,20 +1,62 @@
-import SwiftUI
+import UIKit
 
-struct JetpackLandingScreenView: View {
+final class JetpackLandingScreenView: UIView {
 
     private let prompts = JetpackPromptsConfiguration.Constants.basePrompts
     private let evenColor = JetpackPromptsConfiguration.Constants.evenColor
     private let oddColor = JetpackPromptsConfiguration.Constants.oddColor
+    private let lineSpacing: CGFloat = -8
+    private let interitemSpacing: CGFloat = 0
+    private let font: UIFont = UIFont.systemFont(ofSize: 40, weight: .bold)
+    private let insets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
 
-    var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            ForEach(0..<prompts.count * 2, id: \.hashValue) { index in
-                JetpackPromptView(
-                    text: prompts[index % prompts.count],
-                    color: index % 2 == 0 ? evenColor : oddColor
-                )
-            }
+    private lazy var body: UIView = {
+        let stackView = UIStackView()
+        stackView.alignment = .leading
+        stackView.axis = .vertical
+        stackView.spacing = self.interitemSpacing
+        for index in 0..<prompts.count * 2 {
+            let view = self.promptView(
+                text: prompts[index % prompts.count],
+                color: index % 2 == 0 ? evenColor : oddColor
+            )
+            stackView.addArrangedSubview(view)
         }
-        .frame(maxWidth: .infinity, alignment: .leading)
+        return stackView
+    }()
+
+    init() {
+        super.init(frame: .zero)
+        self.body.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(body)
+        self.pinSubviewToAllEdges(body, insets: insets)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func promptView(text: String, color: UIColor) -> UIView {
+        let textView = UITextView()
+        textView.isSelectable = false
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        textView.textContainer.maximumNumberOfLines = 0
+        textView.textContainer.lineFragmentPadding = 0
+        textView.textContainerInset = .zero
+        textView.backgroundColor = .clear
+
+        let attributedString = NSMutableAttributedString(string: text)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = lineSpacing
+        let attributes: [NSAttributedString.Key: Any] = [
+            .paragraphStyle: paragraphStyle,
+            .foregroundColor: color,
+            .font: font
+        ]
+        attributedString.addAttributes(attributes, range: NSMakeRange(0, text.count))
+        textView.attributedText = attributedString
+
+        return textView
     }
 }
