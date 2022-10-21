52643ee43bf3f4bbba95c9f0ca5110cc3b1f97d8
Salim Braksa <salim.braksa@gmail.com>
2022-10-21 17:10:33 +0100

    Refactor JetpackLandingScreenView private properties to static constants

--- WordPress/Jetpack/Classes/NUX/New Landing Screen/Views/JetpackLandingScreenView.swift
+++ WordPress/Jetpack/Classes/NUX/New Landing Screen/Views/JetpackLandingScreenView.swift
@@ -2,13 +2,7 @@ import UIKit
 
 final class JetpackLandingScreenView: UIView {
 
-    private let prompts = JetpackPromptsConfiguration.Constants.basePrompts
-    private let evenColor = JetpackPromptsConfiguration.Constants.evenColor
-    private let oddColor = JetpackPromptsConfiguration.Constants.oddColor
-    private let lineSpacing: CGFloat = -6
-    private let interitemSpacing: CGFloat = -2
-    private let font: UIFont = UIFont.systemFont(ofSize: 40, weight: .bold)
-    private let insets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+    // MARK: - Init
 
     init() {
         super.init(frame: .zero)
@@ -24,45 +18,61 @@ final class JetpackLandingScreenView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 
+    // MARK: - Setup
+
     private func setup() {
+        let prompts = Constants.prompts
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.alignment = .leading
         stackView.axis = .vertical
-        stackView.spacing = self.interitemSpacing
+        stackView.spacing = Constants.interitemSpacing
         for index in 0..<prompts.count * 2 {
-            let view = self.promptView(
+            let view = Self.promptView(
                 text: prompts[index % prompts.count],
-                color: index % 2 == 0 ? evenColor : oddColor
+                color: index % 2 == 0 ? Constants.evenColor : Constants.oddColor
             )
             stackView.addArrangedSubview(view)
         }
         self.addSubview(stackView)
-        self.pinSubviewToAllEdges(stackView, insets: insets)
+        self.pinSubviewToAllEdges(stackView, insets: Constants.insets)
     }
 
-    private func promptView(text: String, color: UIColor) -> UIView {
+    // MARK: - Subviews Factory
+
+    private static func promptView(text: String, color: UIColor) -> UIView {
         let textView = UITextView()
         textView.accessibilityElementsHidden = true
         textView.isSelectable = false
         textView.isEditable = false
         textView.isScrollEnabled = false
-        textView.textContainer.maximumNumberOfLines = 0
         textView.textContainer.lineFragmentPadding = 0
         textView.textContainerInset = .zero
         textView.backgroundColor = .clear
 
         let attributedString = NSMutableAttributedString(string: text)
         let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.lineSpacing = lineSpacing
+        paragraphStyle.lineSpacing = Constants.lineSpacing
         let attributes: [NSAttributedString.Key: Any] = [
             .paragraphStyle: paragraphStyle,
             .foregroundColor: color,
-            .font: font
+            .font: Constants.font
         ]
         attributedString.addAttributes(attributes, range: NSMakeRange(0, text.count))
         textView.attributedText = attributedString
 
         return textView
+    }
+
+    // MARK: - Constants
+
+    private struct Constants {
+        static let prompts = JetpackPromptsConfiguration.Constants.basePrompts
+        static let evenColor = JetpackPromptsConfiguration.Constants.evenColor
+        static let oddColor = JetpackPromptsConfiguration.Constants.oddColor
+        static let lineSpacing: CGFloat = -6
+        static let interitemSpacing: CGFloat = -2
+        static let font: UIFont = UIFont.systemFont(ofSize: 40, weight: .bold)
+        static let insets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
     }
 }
