3de65871b9ab391ad0a59c1cf764408de86a3d46
Hassaan El-Garem <hassaanelgarem@gmail.com>
2022-10-26 03:33:29 +0200

    Add: New Jetpack overlay view controller

---
+++ WordPress/Classes/ViewRelated/Jetpack/Branding/Overlay/JetpackFullScreenOverlayViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class JetpackFullScreenOverlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
