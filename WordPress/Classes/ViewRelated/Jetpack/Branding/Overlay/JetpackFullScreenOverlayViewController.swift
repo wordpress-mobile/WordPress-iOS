6f3bda4f058cfde67adffa86312b5d6864259360
Hassaan El-Garem <hassaanelgarem@gmail.com>
2022-10-26 04:57:21 +0200

    Add: overlay IB outlets

--- WordPress/Classes/ViewRelated/Jetpack/Branding/Overlay/JetpackFullScreenOverlayViewController.swift
+++ WordPress/Classes/ViewRelated/Jetpack/Branding/Overlay/JetpackFullScreenOverlayViewController.swift
@@ -2,6 +2,19 @@ import UIKit
 
 class JetpackFullScreenOverlayViewController: UIViewController {
 
+    // MARK: Outlets
+
+    @IBOutlet weak var contentStackView: UIStackView!
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var footnoteLabel: UILabel!
+    @IBOutlet weak var learnMoreButton: UIButton!
+    @IBOutlet weak var switchButton: UIButton!
+    @IBOutlet weak var continueButton: UIButton!
+
+    // MARK: View Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
