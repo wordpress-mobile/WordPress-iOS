405f0fbd6d2f1a65798483c14b658eef2abe1cf1
Hassaan El-Garem <hassaanelgarem@gmail.com>
2022-10-26 06:48:58 +0200

    Add: close button to overlay view

--- WordPress/Classes/ViewRelated/Jetpack/Branding/Overlay/JetpackFullscreenOverlayViewController.swift
+++ WordPress/Classes/ViewRelated/Jetpack/Branding/Overlay/JetpackFullscreenOverlayViewController.swift
@@ -6,6 +6,27 @@ class JetpackFullscreenOverlayViewController: UIViewController {
 
     private let config: JetpackFullscreenOverlayConfig
 
+    // MARK: Lazy Views
+
+    private lazy var closeButtonItem: UIBarButtonItem = {
+        let closeButton = UIButton()
+
+        let configuration = UIImage.SymbolConfiguration(pointSize: Constants.closeButtonSymbolSize, weight: .bold)
+        closeButton.setImage(UIImage(systemName: "xmark", withConfiguration: configuration), for: .normal)
+        closeButton.tintColor = .secondaryLabel
+        closeButton.backgroundColor = .quaternarySystemFill
+
+        NSLayoutConstraint.activate([
+            closeButton.widthAnchor.constraint(equalToConstant: Constants.closeButtonRadius),
+            closeButton.heightAnchor.constraint(equalTo: closeButton.widthAnchor)
+        ])
+        closeButton.layer.cornerRadius = Constants.closeButtonRadius * 0.5
+
+        closeButton.addTarget(self, action: #selector(closeButtonPressed), for: .touchUpInside)
+
+        return UIBarButtonItem(customView: closeButton)
+    }()
+
     // MARK: Outlets
 
     @IBOutlet weak var contentStackView: UIStackView!
@@ -33,11 +54,36 @@ class JetpackFullscreenOverlayViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        configureNavigationBar()
         applyStyles()
         setupContent()
+        setupColors()
+        setupFonts()
     }
 
     // MARK: Helpers
+
+    private func configureNavigationBar() {
+        addCloseButtonIfNeeded()
+
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .systemBackground
+        appearance.shadowColor = .clear
+        navigationItem.standardAppearance = appearance
+        navigationItem.compactAppearance = appearance
+        navigationItem.scrollEdgeAppearance = appearance
+        if #available(iOS 15.0, *) {
+            navigationItem.compactScrollEdgeAppearance = appearance
+        }
+    }
+
+    private func addCloseButtonIfNeeded() {
+        guard config.shouldShowCloseButton else {
+            return
+        }
+
+        navigationItem.rightBarButtonItem = closeButtonItem
+    }
 
     private func applyStyles() {
         iconImageView.clipsToBounds = false
@@ -53,5 +99,31 @@ class JetpackFullscreenOverlayViewController: UIViewController {
         footnoteLabel.isHidden = config.footnoteIsHidden
         learnMoreButton.isHidden = config.learnMoreButtonIsHidden
         continueButton.isHidden = config.continueButtonIsHidden
+    }
+
+    private func setupColors() {
+
+    }
+
+    private func setupFonts() {
+
+    }
+
+    // MARK: Actions
+
+    @objc private func closeButtonPressed(sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+// MARK: Constants
+
+private extension JetpackFullscreenOverlayViewController {
+    enum Strings {
+    }
+
+    enum Constants {
+        static let closeButtonRadius: CGFloat = 30
+        static let closeButtonSymbolSize: CGFloat = 16
     }
 }
