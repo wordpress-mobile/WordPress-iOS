66d881811ff6c84c6be149f6b5e28b6bea51cfe8
Gio Lodi <gio@mokacoding.com>
2022-11-01 20:52:43 +1100

    Add CI step to run `.strings` generation

---
+++ .buildkite/commands/lint-localized-strings-format.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -eu
+
+echo "--- :rubygems: Setting up Gems"
+install_gems
+
+echo "--- :cocoapods: Setting up Pods"
+install_cocoapods
+
+echo "--- :writing_hand: Copy Files"
+mkdir -pv ~/.configure/wordpress-ios/secrets
+cp -v fastlane/env/project.env-example ~/.configure/wordpress-ios/secrets/project.env
+
+echo "--- Lint Localized Strings Format"
+bundle exec fastlane generate_strings_file_for_glotpress skip_commit:true
