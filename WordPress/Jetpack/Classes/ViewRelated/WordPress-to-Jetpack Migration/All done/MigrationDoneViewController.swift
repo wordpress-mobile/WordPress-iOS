8e7786afb14acc25c6bb7b6347f114652a7f8565
Giorgio Ruscigno <g.ruscigno@gmail.com>
2022-11-06 16:11:01 -0600

    Add MigrationDoneViewController, view controller for the wp-to-jp migration-final step

---
+++ WordPress/Jetpack/Classes/ViewRelated/WordPress-to-Jetpack Migration/All done/MigrationDoneViewController.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class MigrationDoneViewController: UIViewController {
+
+    private let viewModel: MigrationDoneViewModel
+
+    init(viewModel: MigrationDoneViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func loadView() {
+        let centerView = UIView()
+        centerView.translatesAutoresizingMaskIntoConstraints = false
+        centerView.setContentHuggingPriority(UILayoutPriority.defaultLow, for: .vertical)
+
+        view = MigrationStepView(headerView: MigrationHeaderView(configuration: viewModel.configuration.headerConfiguration),
+                                 actionsView: MigrationActionsView(configuration: viewModel.configuration.actionsConfiguration),
+                                 centerView: centerView)
+    }
+}
