3b8216f7c81ec176e2d717f547f3f1299dfa43b4
Giorgio Ruscigno <g.ruscigno@gmail.com>
2022-11-06 16:09:07 -0600

    Add MigrationNotificationsViewController, view controller for the wp-to-jp migration-notifications permissions step

---
+++ WordPress/Jetpack/Classes/ViewRelated/WordPress-to-Jetpack Migration/Notifications Permission/MigrationNotificationsViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class MigrationNotificationsViewController: UIViewController {
+
+    private let viewModel: MigrationNotificationsViewModel
+
+    init(viewModel: MigrationNotificationsViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func loadView() {
+        let centerView = UIView()
+        centerView.translatesAutoresizingMaskIntoConstraints = false
+        centerView.setContentHuggingPriority(UILayoutPriority.defaultLow, for: .vertical)
+        view = MigrationStepView(headerView: MigrationHeaderView(configuration: viewModel.configuration.headerConfiguration),
+                                 actionsView: MigrationActionsView(configuration: viewModel.configuration.actionsConfiguration),
+                                 centerView: centerView)
+    }
+}
