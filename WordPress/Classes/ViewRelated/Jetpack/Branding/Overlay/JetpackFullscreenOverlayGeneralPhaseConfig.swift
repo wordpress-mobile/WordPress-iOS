7c42dce5e7213f60f68854826328fc1a9035f381
Hassaan El-Garem <hassaanelgarem@gmail.com>
2022-11-08 16:41:02 +0200

    Add: configure should show for general phase

--- WordPress/Classes/ViewRelated/Jetpack/Branding/Overlay/JetpackFullscreenOverlayGeneralPhaseConfig.swift
+++ WordPress/Classes/ViewRelated/Jetpack/Branding/Overlay/JetpackFullscreenOverlayGeneralPhaseConfig.swift
@@ -8,7 +8,41 @@ struct JetpackFullscreenOverlayGeneralConfig: JetpackFullscreenOverlayConfig {
     let source: JetpackFeaturesRemovalCoordinator.OverlaySource
 
     var shouldShowOverlay: Bool {
-        return true
+        switch (phase, source) {
+
+        // Phase One: Only show feature-specific overlays
+        case (.one, .stats):
+            fallthrough
+        case (.one, .notifications):
+            fallthrough
+        case (.one, .reader):
+            return true
+
+        // Phase Two: Only show feature-specific overlays
+        case (.two, .stats):
+            fallthrough
+        case (.two, .notifications):
+            fallthrough
+        case (.two, .reader):
+            return false // TODO: Change this to true when other phase 2 tasks are ready
+
+        // Phase Three: Show all overlays
+        case (.three, _):
+            return false // TODO: Change this to true when other phase 3 tasks are ready
+
+        // Phase Four: Show feature-collection overlays. Features are removed by this point so they are irrelevant.
+        case (.four, _):
+            return false // TODO: Change this to true when other phase 4 tasks are ready
+
+        // New Users Phase: Show feature-collection overlays. Do not show on app-open. Features are removed by this point so they are irrelevant.
+        case (.newUsers, .appOpen):
+            return false
+        case (.newUsers, _):
+            return false // TODO: Change this to true when other new users phase tasks are ready
+
+        default:
+            return false
+        }
     }
 
     var title: String {
