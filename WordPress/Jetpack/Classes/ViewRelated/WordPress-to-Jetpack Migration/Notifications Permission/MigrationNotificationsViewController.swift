06fc0c1cc1f814258f763a7b3b434c516947d8a2
Giorgio Ruscigno <g.ruscigno@gmail.com>
2022-11-08 22:02:06 -0600

    Update MigrationNotificationsViewController, use the new MigrationNotificationsCenterView

--- WordPress/Jetpack/Classes/ViewRelated/WordPress-to-Jetpack Migration/Notifications Permission/MigrationNotificationsViewController.swift
+++ WordPress/Jetpack/Classes/ViewRelated/WordPress-to-Jetpack Migration/Notifications Permission/MigrationNotificationsViewController.swift
@@ -14,13 +14,9 @@ class MigrationNotificationsViewController: UIViewController {
     }
 
     override func loadView() {
-        // TODO: replace this blank center view with the actual content
-        let centerView = UIView()
-        centerView.translatesAutoresizingMaskIntoConstraints = false
-        centerView.setContentHuggingPriority(UILayoutPriority.defaultLow, for: .vertical)
 
         view = MigrationStepView(headerView: MigrationHeaderView(configuration: viewModel.configuration.headerConfiguration),
                                  actionsView: MigrationActionsView(configuration: viewModel.configuration.actionsConfiguration),
-                                 centerView: centerView)
+                                 centerView: MigrationNotificationsCenterView())
     }
 }
