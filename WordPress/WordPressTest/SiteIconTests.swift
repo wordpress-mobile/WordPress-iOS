86e9953c10a9ed5a65f9af16934fc78f5144c8c2
Salim Braksa <salim.braksa@gmail.com>
2022-11-10 19:50:30 +0100

    Add few unit tests covering UIImageView.optimizedURL method

--- WordPress/WordPressTest/SiteIconTests.swift
+++ WordPress/WordPressTest/SiteIconTests.swift
@@ -11,10 +11,10 @@ final class SiteIconTests: XCTestCase {
 
     // MARK: - Test `optimizedURL(for:)`
 
-    /// Tests that a dotcom image URL is valid.
-    func testDotcomURL() {
+    /// Tests that a dotcom image URL with default image size is valid.
+    func testDotcomURLWithDefaultSize() {
         // Given
-        let path = "https://fake.files.wordpress.com/fake.png"
+        let path =  Constants.dotcomPath
 
         // When
         let optimizedURL = imageView.optimizedURL(for: path)
@@ -25,10 +25,10 @@ final class SiteIconTests: XCTestCase {
         XCTAssertEqual(optimizedURL, expectedURL)
     }
 
-    /// Tests that a gravatar image URL is valid.
-    func testBlavatarURL() {
+    /// Tests that a gravatar image URL with default image size is valid.
+    func testBlavatarURLWithDefaultSize() {
         // Given
-        let path = "https://secure.gravatar.com/blavatar/123"
+        let path = Constants.gravatarPath
 
         // When
         let optimizedURL = imageView.optimizedURL(for: path)
@@ -39,10 +39,10 @@ final class SiteIconTests: XCTestCase {
         XCTAssertEqual(optimizedURL, expectedURL)
     }
 
-    /// Tests that a photon image URL is valid.
-    func testPhotonURL() {
+    /// Tests that a photon image URL with default image size is valid.
+    func testPhotonURLWithDefaultSize() {
         // Given
-        let path = "https://fake.wp.com/fake.png"
+        let path = Constants.photonPath
 
         // When
         let optimizedURL = imageView.optimizedURL(for: path)
@@ -51,5 +51,59 @@ final class SiteIconTests: XCTestCase {
         let size = 40 * Int(UIScreen.main.scale)
         let expectedURL = URL(string: "\(path)?w=\(size)&h=\(size)")
         XCTAssertEqual(optimizedURL, expectedURL)
+    }
+
+    /// Tests that a dotcom image URL with custom image size is valid.
+    func testDotcomURLWithCustomSize() {
+        // Given
+        let sizeInPoints = Constants.customImageSize
+        let path = Constants.dotcomPath
+
+        // When
+        let optimizedURL = imageView.optimizedURL(for: path, imageSize: sizeInPoints)
+
+        // Then
+        let size = Int(sizeInPoints.width) * Int(UIScreen.main.scale)
+        let expectedURL = URL(string: "\(path)?w=\(size)&h=\(size)")
+        XCTAssertEqual(optimizedURL, expectedURL)
+    }
+
+    /// Tests that a gravatar image URL with custom image size is valid.
+    func testBlavatarURLWithCustomSize() {
+        // Given
+        let sizeInPoints = Constants.customImageSize
+        let path = Constants.gravatarPath
+
+        // When
+        let optimizedURL = imageView.optimizedURL(for: path, imageSize: sizeInPoints)
+
+        // Then
+        let size = Int(sizeInPoints.width) * Int(UIScreen.main.scale)
+        let expectedURL = URL(string: "\(path)?d=404&s=\(size)")
+        XCTAssertEqual(optimizedURL, expectedURL)
+    }
+
+    /// Tests that a photon image URL with custom image size is valid.
+    func testPhotonURLWithCustomSize() {
+        // Given
+        let sizeInPoints = Constants.customImageSize
+        let path = Constants.photonPath
+
+        // When
+        let optimizedURL = imageView.optimizedURL(for: path, imageSize: sizeInPoints)
+
+        // Then
+        let size = Int(sizeInPoints.width) * Int(UIScreen.main.scale)
+        let expectedURL = URL(string: "\(path)?w=\(size)&h=\(size)")
+        XCTAssertEqual(optimizedURL, expectedURL)
+    }
+
+    // MARK: - Constants
+
+    private struct Constants {
+        static let customImageSize = CGSize(width: 60, height: 60)
+        static let dotcomPath = "https://fake.files.wordpress.com/fake.png"
+        static let gravatarPath = "https://secure.gravatar.com/blavatar/123"
+        static let photonPath = "https://fake.wp.com/fake.png"
     }
 }
