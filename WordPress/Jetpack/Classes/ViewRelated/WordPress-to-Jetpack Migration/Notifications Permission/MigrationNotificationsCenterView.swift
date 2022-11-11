6c80cf33599ccf7575a650b90b78c69a41a8161a
Giorgio Ruscigno <g.ruscigno@gmail.com>
2022-11-11 01:12:34 -0600

    Update MigrationNotificationsCenterView, add fake alert image and fix spacings

--- WordPress/Jetpack/Classes/ViewRelated/WordPress-to-Jetpack Migration/Notifications Permission/MigrationNotificationsCenterView.swift
+++ WordPress/Jetpack/Classes/ViewRelated/WordPress-to-Jetpack Migration/Notifications Permission/MigrationNotificationsCenterView.swift
@@ -2,29 +2,37 @@ import UIKit
 
 class MigrationNotificationsCenterView: UIView {
 
-    private lazy var spacer: UIView = {
-        let spacer = UIView()
-        spacer.translatesAutoresizingMaskIntoConstraints = false
-        spacer.setContentHuggingPriority(UILayoutPriority.defaultLow, for: .vertical)
-        return spacer
-    }()
-
     private lazy var descriptionLabel: UILabel = {
         let label = UILabel()
-        label.font = WPStyleGuide.fontForTextStyle(.body, fontWeight: .regular)
+        label.font = WPStyleGuide.fontForTextStyle(.footnote, fontWeight: .regular)
         label.attributedText = Appearance.highlightString(Appearance.highlightedWord, inString: Appearance.description)
         label.textColor = .muriel(color: .gray, .shade50)
         label.numberOfLines = 0
         return label
     }()
 
+    private lazy var fakeAlertImageView: UIImageView = {
+        let imageView = UIImageView(image: Appearance.fakeAlertImage(for: traitCollection.layoutDirection))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
+        imageView.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        return imageView
+    }()
+
     private lazy var mainStackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [spacer, descriptionLabel])
+        let stackView = UIStackView(arrangedSubviews: [fakeAlertImageView, descriptionLabel, makeSpacer()])
         stackView.axis = .vertical
         stackView.alignment = .center
+        stackView.setCustomSpacing(Appearance.fakeAlertToDescriptionSpacing, after: fakeAlertImageView)
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
+
+    private func makeSpacer() -> UIView {
+        let spacer = UIView()
+        return spacer
+    }
 
     init() {
         super.init(frame: .zero)
@@ -36,7 +44,20 @@ class MigrationNotificationsCenterView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        guard traitCollection.layoutDirection != previousTraitCollection?.layoutDirection else {
+            return
+        }
+        // probably an edge case, but if users change language direction, then update the fake alert
+        fakeAlertImageView.image = Appearance.fakeAlertImage(for: traitCollection.layoutDirection)
+    }
+
     private enum Appearance {
+
+        static func fakeAlertImage(for textDirection: UITraitEnvironmentLayoutDirection) -> UIImage? {
+            let imageName = textDirection == .rightToLeft ? "wp-migration-fake-alert-rtl" : "wp-migration-fake-alert-ltr"
+            return UIImage(named: imageName)
+        }
 
         static let description = NSLocalizedString("migration.notifications.footer",
                                                    value: "When the alert apears tap Allow to continue receiving all your WordPress notifications.",
@@ -52,9 +73,11 @@ class MigrationNotificationsCenterView: UIView {
                 return attributedString
             }
 
-            attributedString.addAttributes([.font: WPStyleGuide.fontForTextStyle(.body, fontWeight: .bold)],
+            attributedString.addAttributes([.font: WPStyleGuide.fontForTextStyle(.footnote, fontWeight: .bold)],
                                            range: subStringRange)
             return attributedString
         }
+
+        static let fakeAlertToDescriptionSpacing: CGFloat = 20
     }
 }
