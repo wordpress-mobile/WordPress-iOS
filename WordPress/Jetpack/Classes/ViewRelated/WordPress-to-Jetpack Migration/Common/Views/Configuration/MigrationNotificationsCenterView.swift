9fb42b3ce1a48453c396f768883750229e491a91
Giorgio Ruscigno <g.ruscigno@gmail.com>
2022-11-11 14:39:13 -0600

    Update MigrationNotificationsCenterView, some refactor

--- WordPress/Jetpack/Classes/ViewRelated/WordPress-to-Jetpack Migration/Common/Views/Configuration/MigrationNotificationsCenterView.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MigrationNotificationsCenterView.swift
-//  Jetpack
-//
-//  Created by Giorgio Ruscigno on 11/11/22.
-//  Copyright © 2022 WordPress. All rights reserved.
-//
-
-import Foundation
