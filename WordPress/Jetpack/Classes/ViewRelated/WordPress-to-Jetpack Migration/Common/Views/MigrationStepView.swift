af6b9f9aeb51fb27903dacf74e494ad4034215c9
Giorgio Ruscigno <g.ruscigno@gmail.com>
2022-11-11 01:11:02 -0600

    Update MigrationStepView, refactor to add a scroll view

--- WordPress/Jetpack/Classes/ViewRelated/WordPress-to-Jetpack Migration/Common/Views/MigrationStepView.swift
+++ WordPress/Jetpack/Classes/ViewRelated/WordPress-to-Jetpack Migration/Common/Views/MigrationStepView.swift
@@ -7,10 +7,25 @@ class MigrationStepView: UIView {
     private let actionsView: MigrationActionsView
 
     private lazy var mainStackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [headerView, centerView, actionsView])
+        let stackView = UIStackView(arrangedSubviews: [headerView, centerView])
         stackView.axis = .vertical
+        stackView.spacing = Constants.stackViewSpacing
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
+    }()
+
+    private lazy var contentView: UIView = {
+        let contentView = UIView()
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(mainStackView)
+        return contentView
+    }()
+
+    private lazy var mainScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(contentView)
+        return scrollView
     }()
 
     init(headerView: MigrationHeaderView,
@@ -18,14 +33,27 @@ class MigrationStepView: UIView {
          centerView: UIView) {
 
         self.headerView = headerView
-        headerView.directionalLayoutMargins = Constants.contentMargins
         self.centerView = centerView
-        centerView.directionalLayoutMargins = Constants.contentMargins
         self.actionsView = actionsView
+        actionsView.translatesAutoresizingMaskIntoConstraints = false
         super.init(frame: .zero)
         backgroundColor = .systemBackground
-        addSubview(mainStackView)
-        pinSubviewToAllEdges(mainStackView)
+        addSubview(mainScrollView)
+        addSubview(actionsView)
+        activateConstraints()
+    }
+
+    private func activateConstraints() {
+        contentView.pinSubviewToAllEdges(mainStackView, insets: Constants.contentMargins)
+        pinSubviewToAllEdges(mainScrollView)
+        mainScrollView.pinSubviewToAllEdges(contentView)
+
+        NSLayoutConstraint.activate([
+            contentView.widthAnchor.constraint(equalTo: widthAnchor),
+            actionsView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            actionsView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            actionsView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
     }
 
     required init?(coder: NSCoder) {
@@ -33,6 +61,7 @@ class MigrationStepView: UIView {
     }
 
     private enum Constants {
-        static let contentMargins = NSDirectionalEdgeInsets(top: 0, leading: 30, bottom: 30, trailing: 30)
+        static let contentMargins = UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 30)
+        static let stackViewSpacing: CGFloat = 20
     }
 }
