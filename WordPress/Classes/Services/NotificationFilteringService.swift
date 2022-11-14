48633ce9f7882db5aa50789026eea7b09f277738
Povilas Staskus <povilas.staskus@automattic.com>
2022-11-14 12:04:54 +0000

    Jetpack Focus: Create foundations to Disable Notifications For WordPress app when Jetpack notifications enabled (#19531)
    * Added remote feature flag allowDisablingWPNotifications
    * Added enable notification switch to NotificationSettings under a feature flag
    * Created NotificationFilteringService to contain logic required to disable notifications

---
+++ WordPress/Classes/Services/NotificationFilteringService.swift
@@ -0,0 +1,71 @@
+import Foundation
+
+/// The service is created to support disabling WordPress notifications when Jetpack app is installed
+/// The service uses App Groups which allows Jetpack app to change the state of notifications flag and be later accessed by WordPress app
+/// This is a temporary solution to avoid duplicate notifications during the migration process from WordPress to Jetpack app
+/// This service and its usage can be deleted once the migration is done
+final class NotificationFilteringService {
+    private var notificationSettingsLoader: NotificationSettingsLoader
+    private var notificationsEnabled: Bool = false
+    private let allowDisablingWPNotifications: Bool
+    private let isWordPress: Bool
+    private let userDefaults = UserDefaults(suiteName: WPAppGroupName)
+
+    var wordPressNotificationsEnabled: Bool {
+        get {
+            guard let userDefaults = userDefaults,
+                  userDefaults.value(forKey: WPNotificationsEnabledKey) != nil else {
+                /// Treat this flag as enabled if it wasn't explicitly disabled
+                return true
+            }
+
+            return userDefaults.bool(forKey: WPNotificationsEnabledKey)
+        }
+
+        set {
+            userDefaults?.set(newValue, forKey: WPNotificationsEnabledKey)
+        }
+    }
+
+    init(notificationSettingsLoader: NotificationSettingsLoader = UNUserNotificationCenter.current(),
+         allowDisablingWPNotifications: Bool = FeatureFlag.allowDisablingWPNotifications.enabled,
+         isWordPress: Bool = AppConfiguration.isWordPress) {
+        self.notificationSettingsLoader = notificationSettingsLoader
+        self.allowDisablingWPNotifications = allowDisablingWPNotifications
+        self.isWordPress = isWordPress
+
+        notificationSettingsLoader.getNotificationAuthorizationStatus { [weak self] status in
+            self?.notificationsEnabled = status == .authorized
+        }
+    }
+
+    func shouldShowNotificationControl() -> Bool {
+        return allowDisablingWPNotifications && isWordPress && notificationsEnabled
+    }
+
+    func disableWordPressNotificationsIfNeeded() {
+        if allowDisablingWPNotifications, !isWordPress {
+            wordPressNotificationsEnabled = false
+        }
+    }
+
+    func shouldFilterWordPressNotifications() -> Bool {
+        return allowDisablingWPNotifications
+            && isWordPress
+            && !wordPressNotificationsEnabled
+    }
+}
+
+// MARK: - Helpers
+
+protocol NotificationSettingsLoader: AnyObject {
+    func getNotificationAuthorizationStatus(completionHandler: @escaping (UNAuthorizationStatus) -> Void)
+}
+
+extension UNUserNotificationCenter: NotificationSettingsLoader {
+    func getNotificationAuthorizationStatus(completionHandler: @escaping (UNAuthorizationStatus) -> Void) {
+        getNotificationSettings { settings in
+            completionHandler(settings.authorizationStatus)
+        }
+    }
+}
