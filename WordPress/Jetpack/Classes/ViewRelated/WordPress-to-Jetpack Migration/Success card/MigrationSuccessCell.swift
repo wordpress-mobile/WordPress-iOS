e97fbe804b10e2e85713a8fad2e150df295e83c7
Giorgio Ruscigno <g.ruscigno@gmail.com>
2022-11-22 17:31:18 -0600

    Add MigrationSuccessCell, a UITableViewCell that contains adds the migration success card in My Site Menu

---
+++ WordPress/Jetpack/Classes/ViewRelated/WordPress-to-Jetpack Migration/Success card/MigrationSuccessCell.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+@objc
+class MigrationSuccessCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setup() {
+        let view = MigrationSuccessCardView() {
+            // TODO: add card presentation logic here
+        }
+        view.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(view)
+        contentView.pinSubviewToAllEdges(view)
+    }
+}
+
+class MigrationSuccessRow: ImmuTableRow {
+    var action: ImmuTableAction?
+
+    func configureCell(_ cell: UITableViewCell) {
+
+    }
+
+    static let cell = ImmuTableCell.class(MigrationSuccessCell.self)
+}
+
+extension BlogDetailsViewController {
+
+    @objc func migrationSuccesstSectionViewModel() -> BlogDetailsSection {
+        let row = BlogDetailsRow()
+        row.callback = {}
+
+        let section = BlogDetailsSection(title: nil,
+                                         rows: [row],
+                                         footerTitle: nil,
+                                         category: .migrationSuccess)
+        return section
+    }
+}
