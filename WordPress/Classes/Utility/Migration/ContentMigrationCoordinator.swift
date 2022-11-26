83a10d520b3cbdf5fca9ffcac318f1b2a49c899d
David Christiandy <1299411+dvdchr@users.noreply.github.com>
2022-11-26 19:26:34 +0700

    Add ContentMigrationCoordinator

---
+++ WordPress/Classes/Utility/Migration/ContentMigrationCoordinator.swift
@@ -0,0 +1,98 @@
+/// Encapsulates logic related to content migration from WordPress to Jetpack.
+///
+class ContentMigrationCoordinator {
+
+    static let shared: ContentMigrationCoordinator = .init()
+
+    // MARK: Dependencies
+
+    private let dataMigrator: ContentDataMigrating
+    private let keyValueDatabase: KeyValueDatabase
+    private let eligibilityProvider: ContentMigrationEligibilityProvider
+
+    init(dataMigrator: ContentDataMigrating = DataMigrator(),
+         keyValueDatabase: KeyValueDatabase = UserDefaults.standard,
+         eligibilityProvider: ContentMigrationEligibilityProvider = AppConfiguration()) {
+        self.dataMigrator = dataMigrator
+        self.keyValueDatabase = keyValueDatabase
+        self.eligibilityProvider = eligibilityProvider
+    }
+
+    enum ContentMigrationCoordinatorError: Error {
+        case ineligible
+        case exportFailure
+    }
+
+    // MARK: Methods
+
+    /// Starts the content migration process of exporting app data to the shared location
+    /// that will be accessible by the Jetpack app.
+    ///
+    /// The completion block is intentionally called regardless of whether the export process
+    /// succeeds or fails. Since the export process consists of local file operations, we should
+    /// just let the user continue with the original intent in case of failure.
+    ///
+    /// - Parameter completion: Closure called after the export process completes.
+    func startAndDo(completion: ((Result<Void, ContentMigrationCoordinatorError>) -> Void)? = nil) {
+        guard eligibilityProvider.isEligibleForMigration else {
+            completion?(.failure(.ineligible))
+            return
+        }
+
+        // TODO: Sync local post drafts here.
+
+        dataMigrator.exportData { result in
+            if case let .failure(error) = result {
+                DDLogError("[Jetpack Migration] Error exporting data: \(error)")
+                completion?(.failure(.exportFailure))
+                return
+            }
+
+            completion?(.success(()))
+        }
+    }
+
+    /// Starts the content migration process once per installation.
+    ///
+    func startOnce(completion: (() -> Void)? = nil) {
+        if keyValueDatabase.bool(forKey: .oneOffMigrationKey) {
+            return
+        }
+
+        startAndDo { [weak self] result in
+            guard case .success = result else {
+                return
+            }
+
+            self?.keyValueDatabase.set(true, forKey: .oneOffMigrationKey)
+        }
+    }
+}
+
+// MARK: - Content Migrating
+
+
+protocol ContentDataMigrating {
+    func exportData(completion: ((Result<Void, DataMigrator.DataMigratorError>) -> Void)?)
+    func importData(completion: ((Result<Void, DataMigrator.DataMigratorError>) -> Void)?)
+}
+
+extension DataMigrator: ContentDataMigrating {}
+
+// MARK: - Content Migration Eligibility Provider
+
+protocol ContentMigrationEligibilityProvider {
+    var isEligibleForMigration: Bool { get }
+}
+
+extension AppConfiguration: ContentMigrationEligibilityProvider {
+    var isEligibleForMigration: Bool {
+        FeatureFlag.contentMigration.enabled && Self.isWordPress && AccountHelper.isLoggedIn && AccountHelper.hasBlogs
+    }
+}
+
+// MARK: - Constants
+
+private extension String {
+    static let oneOffMigrationKey = "wordpress_one_off_export"
+}
---
+++ WordPress/WordPressTest/ContentMigrationCoordinatorTests.swift
@@ -0,0 +1,155 @@
+import XCTest
+
+@testable import WordPress
+
+final class ContentMigrationCoordinatorTests: XCTestCase {
+
+    private let timeout: TimeInterval = 1
+
+    private var mockEligibilityProvider: MockEligibilityProvider!
+    private var mockDataMigrator: MockDataMigrator!
+    private var mockKeyValueDatabase: EphemeralKeyValueDatabase!
+    private var coordinator: ContentMigrationCoordinator!
+
+    override func setUp() {
+        super.setUp()
+
+        mockEligibilityProvider = MockEligibilityProvider()
+        mockDataMigrator = MockDataMigrator()
+        mockKeyValueDatabase = EphemeralKeyValueDatabase()
+        coordinator = makeCoordinator()
+    }
+
+    override func tearDown() {
+        mockEligibilityProvider = nil
+        mockDataMigrator = nil
+        mockKeyValueDatabase = nil
+        coordinator = nil
+
+        super.tearDown()
+    }
+
+    // MARK: `startAndDo` tests
+
+    func test_startAndDo_givenEligibleAccount_shouldMigrateContent() {
+        let expect = expectation(description: "Content migration should succeed")
+        coordinator.startAndDo { result in
+            guard case .success = result else {
+                XCTFail()
+                return
+            }
+
+            XCTAssertTrue(self.mockDataMigrator.exportCalled)
+            expect.fulfill()
+        }
+        wait(for: [expect], timeout: timeout)
+    }
+
+    func test_startAndDo_whenAccountNotEligible_shouldNotMigrateContent() {
+        mockEligibilityProvider.isEligibleForMigration = false
+
+        let expect = expectation(description: "Content migration should fail")
+        coordinator.startAndDo { result in
+            guard case .failure = result else {
+                XCTFail()
+                return
+            }
+
+            XCTAssertFalse(self.mockDataMigrator.exportCalled)
+            expect.fulfill()
+        }
+        wait(for: [expect], timeout: timeout)
+    }
+
+    func test_startAndDo_givenExportError_shouldInvokeClosureWithError() {
+        mockDataMigrator.exportErrorToReturn = .databaseCopyError
+
+        let expect = expectation(description: "Content migration should fail")
+        coordinator.startAndDo { _ in
+            XCTAssertTrue(self.mockDataMigrator.exportCalled)
+            expect.fulfill()
+        }
+        wait(for: [expect], timeout: timeout)
+    }
+
+    // MARK: `startOnce` tests
+
+    func test_startOnce_whenUserDefaultsDoesNotExist_shouldMigrate() {
+        let expect = expectation(description: "Content migration should succeed")
+        coordinator.startOnce { [unowned self] in
+            XCTAssertTrue(mockDataMigrator.exportCalled)
+            XCTAssertTrue(mockKeyValueDatabase.bool(forKey: userDefaultsKey))
+            expect.fulfill()
+        }
+        wait(for: [expect], timeout: timeout)
+    }
+
+    func test_startOnce_givenErrorResult_shouldNotSaveUserDefaults() {
+        mockDataMigrator.exportErrorToReturn = .localDraftsNotSynced
+
+        let expect = expectation(description: "Content migration should succeed")
+        coordinator.startOnce { [unowned self] in
+            XCTAssertTrue(mockDataMigrator.exportCalled)
+            XCTAssertFalse(mockKeyValueDatabase.bool(forKey: userDefaultsKey))
+            expect.fulfill()
+        }
+        wait(for: [expect], timeout: timeout)
+    }
+
+    func test_startOnce_whenUserDefaultsExists_shouldNotMigrate() {
+        mockKeyValueDatabase.set(true, forKey: userDefaultsKey)
+
+        let expect = expectation(description: "Content migration should not be called")
+        coordinator.startOnce { [unowned self] in
+            XCTAssertFalse(mockDataMigrator.exportCalled)
+            expect.fulfill()
+        }
+        wait(for: [expect], timeout: timeout)
+    }
+
+}
+
+// MARK: - Helpers
+
+private extension ContentMigrationCoordinatorTests {
+
+    var userDefaultsKey: String {
+        "wordpress_one_off_export"
+    }
+
+    final class MockEligibilityProvider: ContentMigrationEligibilityProvider {
+        var isEligibleForMigration: Bool = true
+    }
+
+    final class MockDataMigrator: ContentDataMigrating {
+        var exportErrorToReturn: DataMigrator.DataMigratorError? = nil
+        var exportCalled = false
+        var importErrorToReturn: DataMigrator.DataMigratorError? = nil
+        var importCalled = false
+
+        func exportData(completion: ((Result<Void, DataMigrator.DataMigratorError>) -> Void)? = nil) {
+            guard let exportErrorToReturn else {
+                exportCalled = true
+                completion?(.success(()))
+                return
+            }
+            completion?(.failure(exportErrorToReturn))
+        }
+
+        func importData(completion: ((Result<Void, DataMigrator.DataMigratorError>) -> Void)? = nil) {
+            guard let importErrorToReturn else {
+                importCalled = true
+                completion?(.success(()))
+                return
+            }
+            completion?(.failure(importErrorToReturn))
+        }
+    }
+
+    func makeCoordinator() -> ContentMigrationCoordinator {
+        return .init(dataMigrator: mockDataMigrator,
+                     keyValueDatabase: mockKeyValueDatabase,
+                     eligibilityProvider: mockEligibilityProvider)
+    }
+
+}
