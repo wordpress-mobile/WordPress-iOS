7d6ddd0f6c9a0cd9f55bec7a458d7056d4f8b7a3
alpavanoglu <alp.avanoglu@automattic.com>
2022-11-28 11:43:07 +0100

    Add import logic to `JetpackWindowManager`

--- WordPress/Jetpack/Classes/System/JetpackWindowManager.swift
+++ WordPress/Jetpack/Classes/System/JetpackWindowManager.swift
@@ -4,20 +4,32 @@ import Foundation
 class JetpackWindowManager: WindowManager {
     /// receives migration flow updates in order to dismiss it when needed.
     private var cancellable: AnyCancellable?
-
     override func showUI(for blog: Blog?) {
         // If the user is logged in and has blogs sync'd to their account
         if AccountHelper.isLoggedIn && AccountHelper.hasBlogs {
-            shouldShowMigrationUI ? showMigrationUI(blog) : showAppUI(for: blog)
+            showAppUI(for: blog)
             return
         }
 
-        // Show the sign in UI if the user isn't logged in
         guard AccountHelper.isLoggedIn else {
-            showSignInUI()
+            if shouldImportMigrationData {
+                DataMigrator().importData() { [weak self] result in
+                    guard let self else {
+                        return
+                    }
+
+                    switch result {
+                    case .success:
+                        UserPersistentStoreFactory.instance().isJPContentImportComplete = true
+                        NotificationCenter.default.post(name: .WPAccountDefaultWordPressComAccountChanged, object: nil)
+                        self.showMigrationUI(blog)
+                    case .failure:
+                        self.showSignInUI()
+                    }
+                }
+            }
             return
         }
-
         // If the user doesn't have any blogs, but they're still logged in, log them out
         // the `logOutDefaultWordPressComAccount` method will trigger the `showSignInUI` automatically
         AccountHelper.logOutDefaultWordPressComAccount()
@@ -43,6 +55,10 @@ class JetpackWindowManager: WindowManager {
 
     // TODO: Add logic in here to trigger migration UI if needed
     private var shouldShowMigrationUI: Bool {
-        return FeatureFlag.contentMigration.enabled
+        return FeatureFlag.contentMigration.enabled && AccountHelper.isLoggedIn
+    }
+
+    private var shouldImportMigrationData: Bool {
+        return !AccountHelper.isLoggedIn && !UserPersistentStoreFactory.instance().isJPContentImportComplete
     }
 }
