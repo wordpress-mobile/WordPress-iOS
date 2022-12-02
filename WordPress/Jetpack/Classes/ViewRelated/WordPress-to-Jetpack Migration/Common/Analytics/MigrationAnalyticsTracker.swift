1a5e47edfc1e4a6ab14456f6a8297314e919f0f8
Salim Braksa <salim.braksa@gmail.com>
2022-12-02 14:55:18 +0100

    Remove MigratableStateTracker and move its logic to MigrationAnalyticsTracker

--- WordPress/Jetpack/Classes/ViewRelated/WordPress-to-Jetpack Migration/Common/Analytics/MigrationAnalyticsTracker.swift
+++ WordPress/Jetpack/Classes/ViewRelated/WordPress-to-Jetpack Migration/Common/Analytics/MigrationAnalyticsTracker.swift
@@ -9,7 +9,26 @@ struct MigrationAnalyticsTracker {
         WPAnalytics.track(event)
     }
 
+    // MARK: - WordPress Migration Eligibility
+
+    /// Tracks an event representing the WordPress migratable state.
+    /// If WordPress is not installed, nothing is tracked.
+    func trackWordPressMigrationEligibility(compatible: Bool) {
+        let state = MigrationAppDetection.getWordPressInstallationState()
+        switch state {
+        case .wordPressInstalledAndMigratable:
+            let properties = ["compatible": "true"]
+            self.track(.wordPressDetected, properties: properties)
+        case .wordPressInstalledNotMigratable:
+            let properties = ["compatible": "false"]
+            self.track(.wordPressDetected, properties: properties)
+        default:
+            break
+        }
+    }
+
     // MARK: - Types
 
     typealias Properties = [String: String]
 }
+
