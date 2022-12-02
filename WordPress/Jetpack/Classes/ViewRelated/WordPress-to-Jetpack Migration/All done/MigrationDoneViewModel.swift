923af63e81a4ab528946987be4865d66ed2e4afe
Salim Braksa <salim.braksa@gmail.com>
2022-12-02 15:32:28 +0100

    Track thanks screen finish event

--- WordPress/Jetpack/Classes/ViewRelated/WordPress-to-Jetpack Migration/All done/MigrationDoneViewModel.swift
+++ WordPress/Jetpack/Classes/ViewRelated/WordPress-to-Jetpack Migration/All done/MigrationDoneViewModel.swift
@@ -2,13 +2,14 @@ class MigrationDoneViewModel {
 
     let configuration: MigrationStepConfiguration
 
-    init(coordinator: MigrationFlowCoordinator) {
+    init(coordinator: MigrationFlowCoordinator, tracker: MigrationAnalyticsTracker = .init()) {
 
         let headerConfiguration = MigrationHeaderConfiguration(step: .done)
 
         let centerViewConfigurartion = MigrationCenterViewConfiguration(step: .done)
 
         let actionsConfiguration = MigrationActionsViewConfiguration(step: .done, primaryHandler: { [weak coordinator] in
+            tracker.track(.thanksScreenFinishTapped)
             coordinator?.transitionToNextStep()
         })
         configuration = MigrationStepConfiguration(headerConfiguration: headerConfiguration,
