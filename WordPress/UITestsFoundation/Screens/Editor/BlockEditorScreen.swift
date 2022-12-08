a7fa7d6333ce8294dadee93957f193564fe081a0
hassaanelgarem <hassaanelgarem@gmail.com>
2022-12-08 16:06:49 +0200

    Update WordPress/UITestsFoundation/Screens/Editor/BlockEditorScreen.swift
    Co-authored-by: Jos <jostnes@users.noreply.github.com>

--- WordPress/UITestsFoundation/Screens/Editor/BlockEditorScreen.swift
+++ WordPress/UITestsFoundation/Screens/Editor/BlockEditorScreen.swift
@@ -39,9 +39,7 @@ public class BlockEditorScreen: ScreenObject {
      */
     public func enterTextInTitle(text: String) -> BlockEditorScreen {
         let titleView = app.otherElements["Post title. Empty"].firstMatch // Uses a localized string
-        guard titleView.waitForExistence(timeout: 3) else {
-            return self
-        }
+        XCTAssert(titleView.waitForExistence(timeout: 3), "Title View does not exist!")
 
         titleView.tap()
         titleView.typeText(text)
