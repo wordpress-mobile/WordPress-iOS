8bbc1e6cb2f16312bba808a41fd52a064d2b061d
Hassaan El-Garem <hassaanelgarem@gmail.com>
2022-12-16 03:09:29 +0200

    Add: `JetpackBrandingMenuCardPresenter` with basic implementation

--- WordPress/Classes/ViewRelated/Jetpack/Branding/Menu Card/JetpackBrandingMenuCardCoordinator.swift
+++ WordPress/Classes/ViewRelated/Jetpack/Branding/Menu Card/JetpackBrandingMenuCardCoordinator.swift
@@ -32,3 +32,68 @@ private extension JetpackBrandingMenuCardCoordinator {
                                                            comment: "Description inside a menu card communicating that features are moving to the Jetpack app.")
     }
 }
+
+class JetpackBrandingMenuCardPresenter {
+
+    // MARK: Private Variables
+
+    private let remoteConfigStore: RemoteConfigStore
+    private let persistenceStore: UserPersistentRepository
+    private let featureFlagStore: RemoteFeatureFlagStore
+
+    // MARK: Initializers
+
+    init(remoteConfigStore: RemoteConfigStore = RemoteConfigStore(),
+         featureFlagStore: RemoteFeatureFlagStore = RemoteFeatureFlagStore(),
+         persistenceStore: UserPersistentRepository = UserDefaults.standard) {
+        self.remoteConfigStore = remoteConfigStore
+        self.featureFlagStore = featureFlagStore
+        self.persistenceStore = persistenceStore
+    }
+
+    // MARK: Public Functions
+
+    func shouldShowCard() -> Bool {
+        return false
+    }
+
+    func remindLaterTapped() {
+        let now = Date()
+        let duration = Constants.remindLaterDurationInDays * Constants.secondInDay
+        let newDate = now.addingTimeInterval(TimeInterval(duration))
+        showCardOnDate = newDate
+    }
+
+    func hideThisTapped() {
+        shouldHideCard = true
+    }
+}
+
+private extension JetpackBrandingMenuCardPresenter {
+    enum Constants {
+        static let secondInDay = 86_400
+        static let remindLaterDurationInDays = 7
+        static let shouldHideCardKey = "JetpackBrandingShouldHideCardKey"
+        static let showCardOnDateKey = "JetpackBrandingShowCardOnDateKey"
+    }
+
+    var shouldHideCard: Bool {
+        get {
+            persistenceStore.bool(forKey: Constants.shouldHideCardKey)
+        }
+
+        set {
+            persistenceStore.set(newValue, forKey: Constants.shouldHideCardKey)
+        }
+    }
+
+    var showCardOnDate: Date? {
+        get {
+            persistenceStore.object(forKey: Constants.showCardOnDateKey) as? Date
+        }
+
+        set {
+            persistenceStore.set(newValue, forKey: Constants.showCardOnDateKey)
+        }
+    }
+}
