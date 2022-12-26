0fc3deff7e0d49c353accc05b8a5fc206a28da67
Hassaan El-Garem <hassaanelgarem@gmail.com>
2022-12-26 06:40:06 +0200

    Remove: Unneeded current UI type logic

--- WordPress/Classes/System/RootViewControllerCoordinator.swift
+++ WordPress/Classes/System/RootViewControllerCoordinator.swift
@@ -2,13 +2,6 @@ import Foundation
 
 class RootViewControllerCoordinator {
 
-    // MARK: Class Enum
-
-    enum AppUIType {
-        case normal
-        case simplified
-    }
-
     // MARK: Static shared variables
 
     static let shared = RootViewControllerCoordinator()
@@ -25,12 +18,10 @@ class RootViewControllerCoordinator {
 
     init() {
         if JetpackFeaturesRemovalCoordinator.shouldRemoveJetpackFeatures() {
-            self.currentAppUIType = .simplified
             let meScenePresenter = MeScenePresenter()
             self.rootViewPresenter = MySitesCoordinator(meScenePresenter: meScenePresenter, onBecomeActiveTab: {})
         }
         else {
-            self.currentAppUIType = .normal
             self.rootViewPresenter = WPTabBarController.sharedInstance() // TODO: Remove shared instance and create an instance here
         }
     }
