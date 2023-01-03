6640ab015597225f0968fed65a62d5afc6d56787
Povilas Staskus <povilas.staskus@automattic.com>
2023-01-03 13:16:10 +0200

    Fix TotalLikes details numbers by passing correct parameter to futureEndOfWeekDate

--- WordPress/Classes/ViewRelated/Stats/Shared Views/Stats Detail/SiteStatsInsightsDetailsViewModel.swift
+++ WordPress/Classes/ViewRelated/Stats/Shared Views/Stats Detail/SiteStatsInsightsDetailsViewModel.swift
@@ -498,8 +498,8 @@ class SiteStatsInsightsDetailsViewModel: Observable {
         return StatsTotalInsightsData.followersCount(insightsStore: insightsStore)
     }
 
-        func createLikesTotalInsightsRow(periodSummary: StatsSummaryTimeIntervalData?) -> StatsTotalInsightsData {
-        let weekEnd = futureEndOfWeekDate(for: periodStore.getSummary())
+    func createLikesTotalInsightsRow(periodSummary: StatsSummaryTimeIntervalData?) -> StatsTotalInsightsData {
+        let weekEnd = futureEndOfWeekDate(for: periodSummary)
         var data = StatsTotalInsightsData.createTotalInsightsData(periodSummary: periodSummary,
                                                                   insightsStore: insightsStore,
                                                                   statsSummaryType: .likes,
