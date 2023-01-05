fe04e1ef3d444c374711df595f40fb03a8ad4ba3
Hassaan El-Garem <hassaanelgarem@gmail.com>
2023-01-05 06:09:30 +0200

    Add: tests for the text in different phases Phases normal, one, two, four

--- WordPress/WordPressTest/JetpackBrandingTextProviderTests.swift
+++ WordPress/WordPressTest/JetpackBrandingTextProviderTests.swift
@@ -15,9 +15,45 @@ final class JetpackBrandingTextProviderTests: XCTestCase {
 
     // MARK: Tests
 
+    func testNormalPhaseText() {
+        // Given
+        let provider = JetpackBrandingTextProvider(featureFlagStore: remoteFeatureFlagsStore)
+
+        // When
+        let text = provider.brandingText()
+
+        // Then
+        XCTAssertEqual(text, "Jetpack powered")
+    }
+
+    func testPhaseOneText() {
+        // Given
+        let provider = JetpackBrandingTextProvider(featureFlagStore: remoteFeatureFlagsStore)
+        remoteFeatureFlagsStore.removalPhaseOne = true
+
+        // When
+        let text = provider.brandingText()
+
+        // Then
+        XCTAssertEqual(text, "Jetpack powered")
+    }
+
+    func testPhaseTwoText() {
+        // Given
+        let provider = JetpackBrandingTextProvider(featureFlagStore: remoteFeatureFlagsStore)
+        remoteFeatureFlagsStore.removalPhaseTwo = true
+
+        // When
+        let text = provider.brandingText()
+
+        // Then
+        XCTAssertEqual(text, "Get the Jetpack app")
+    }
+
     func testDefaultText() {
         // Given
         let provider = JetpackBrandingTextProvider(featureFlagStore: remoteFeatureFlagsStore)
+        remoteFeatureFlagsStore.removalPhaseFour = true
 
         // When
         let text = provider.brandingText()
