5bcd261a0a64c6383310774771d2b8864b9cfe88
Hassaan El-Garem <hassaanelgarem@gmail.com>
2023-01-05 07:17:19 +0200

    Update: Add ability to mock the removal deadline

--- WordPress/WordPressTest/RemoteConfigStoreMock.swift
+++ WordPress/WordPressTest/RemoteConfigStoreMock.swift
@@ -4,10 +4,14 @@ import Foundation
 class RemoteConfigStoreMock: RemoteConfigStore {
 
     var phaseThreeBlogPostUrl: String?
+    var removalDeadline: String?
 
     override func value(for key: String) -> Any? {
         if key == "phase_three_blog_post" {
             return phaseThreeBlogPostUrl
+        }
+        if key == "jp_deadline" {
+            return removalDeadline
         }
         return super.value(for: key)
     }
