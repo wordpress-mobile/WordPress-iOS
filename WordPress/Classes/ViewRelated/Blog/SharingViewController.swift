2a7761fe4ff84f38a3f8ecfdfee0789aa26257d9
Hassaan El-Garem <hassaanelgarem@gmail.com>
2023-01-05 21:52:57 +0200

    Update: Hook up text provider to the sharing badge

--- WordPress/Classes/ViewRelated/Blog/SharingViewController.swift
+++ WordPress/Classes/ViewRelated/Blog/SharingViewController.swift
@@ -1,9 +1,23 @@
 import Foundation
 
 extension SharingViewController {
+
+    static let jetpackBadgePadding: CGFloat = 30
+
     @objc
     static func jetpackBrandingVisibile() -> Bool {
         return JetpackBrandingVisibility.all.enabled
+    }
+
+    @objc
+    func makeJetpackBadge() -> UIView {
+        let textProvider = JetpackBrandingTextProvider(screen: JetpackBadgeScreen.sharing)
+        let badge = JetpackButton.makeBadgeView(title: textProvider.brandingText(),
+                                                topPadding: Self.jetpackBadgePadding,
+                                                bottomPadding: Self.jetpackBadgePadding,
+                                                target: self,
+                                                selector: #selector(presentJetpackOverlay))
+        return badge
     }
 
     @objc
