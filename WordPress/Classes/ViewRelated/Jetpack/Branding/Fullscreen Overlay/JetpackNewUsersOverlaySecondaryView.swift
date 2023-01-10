ae35071bf2528d3ba2c3d062ea791c463b9e34b1
Hassaan El-Garem <hassaanelgarem@gmail.com>
2023-01-10 04:53:25 +0200

    Add: Create `FeatureDetailsView` with static dummy data

--- WordPress/Classes/ViewRelated/Jetpack/Branding/Fullscreen Overlay/JetpackNewUsersOverlaySecondaryView.swift
+++ WordPress/Classes/ViewRelated/Jetpack/Branding/Fullscreen Overlay/JetpackNewUsersOverlaySecondaryView.swift
@@ -16,27 +16,24 @@ class JetpackNewUsersOverlaySecondaryView: UIView {
         return stackView
     }()
 
-    private lazy var featureRows: [UIView] = {
-        return [statsDetailsRow, readerDetailsRow, notificationsDetailsRow]
+    private lazy var featureRows: [FeatureDetailsView] = {
+        return [statsDetails, readerDetails, notificationsDetails]
     }()
 
-    private lazy var statsDetailsRow: UIView = {
-        let view = UIView()
-        view.backgroundColor = .red
+    private lazy var statsDetails: FeatureDetailsView = {
+        let view = FeatureDetailsView()
         view.heightAnchor.constraint(equalToConstant: Metrics.rowHeight).isActive = true
         return view
     }()
 
-    private lazy var readerDetailsRow: UIView = {
-        let view = UIView()
-        view.backgroundColor = .green
+    private lazy var readerDetails: FeatureDetailsView = {
+        let view = FeatureDetailsView()
         view.heightAnchor.constraint(equalToConstant: Metrics.rowHeight).isActive = true
         return view
     }()
 
-    private lazy var notificationsDetailsRow: UIView = {
-        let view = UIView()
-        view.backgroundColor = .blue
+    private lazy var notificationsDetails: FeatureDetailsView = {
+        let view = FeatureDetailsView()
         view.heightAnchor.constraint(equalToConstant: Metrics.rowHeight).isActive = true
         return view
     }()
@@ -65,9 +62,91 @@ private extension JetpackNewUsersOverlaySecondaryView {
         static let stackViewSpacing: CGFloat = 30
         static let stackViewLayoutMargins: NSDirectionalEdgeInsets = .init(top: 30, leading: 0, bottom: 0, trailing: 0)
         static let rowHeight: CGFloat = 60
+        static let iconImageViewSize: CGFloat = 30
+        static let labelsAndIconSpacing: CGFloat = 14
     }
 
     enum Strings {
 
+    }
+}
+
+private extension JetpackNewUsersOverlaySecondaryView {
+    class FeatureDetailsView: UIView {
+
+        // MARK: Lazy Loading View
+
+        private lazy var iconImageView: UIImageView = {
+            let imageView = UIImageView()
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            imageView.heightAnchor.constraint(equalToConstant: Metrics.iconImageViewSize).isActive = true
+            imageView.widthAnchor.constraint(equalToConstant: Metrics.iconImageViewSize).isActive = true
+            addSubview(imageView)
+            return imageView
+        }()
+
+        private lazy var titleLabel: UILabel = {
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.font = WPStyleGuide.fontForTextStyle(.subheadline, fontWeight: .bold)
+            label.textColor = .label
+            label.numberOfLines = 1
+            label.adjustsFontForContentSizeCategory = true
+            addSubview(label)
+            return label
+        }()
+
+        private lazy var subtitleLabel: UILabel = {
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.font = WPStyleGuide.fontForTextStyle(.subheadline, fontWeight: .regular)
+            label.textColor = .secondaryLabel
+            label.numberOfLines = 2
+            label.adjustsFontForContentSizeCategory = true
+            addSubview(label)
+            return label
+        }()
+
+        // MARK: Initializers
+
+        init() {
+            super.init(frame: .zero)
+            configureView()
+        }
+
+        required init?(coder: NSCoder) {
+            fatalError("Storyboard instantiation not supported.")
+        }
+
+        // MARK: Helpers
+
+        private func configureView() {
+            setupContent()
+            setupConstraints()
+        }
+
+        private func setupContent() {
+            iconImageView.image = UIImage(named: "icon-jetpack")
+            titleLabel.text = "Lorum Ipsum"
+            subtitleLabel.text = "Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum"
+        }
+
+        private func setupConstraints() {
+            // Icon Image View
+            iconImageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+            iconImageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+
+            // Title Label
+            titleLabel.topAnchor.constraint(equalTo: topAnchor).isActive = true
+            titleLabel.leadingAnchor.constraint(equalTo: iconImageView.trailingAnchor,
+                                                constant: Metrics.labelsAndIconSpacing).isActive = true
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+
+            // Subtitle Label
+            subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor).isActive = true
+            subtitleLabel.leadingAnchor.constraint(equalTo: iconImageView.trailingAnchor,
+                                                constant: Metrics.labelsAndIconSpacing).isActive = true
+            subtitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        }
     }
 }
