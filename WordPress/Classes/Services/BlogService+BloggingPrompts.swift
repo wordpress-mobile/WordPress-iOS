f7959e6f87d141cf5f98eb4f5e2923315752cc2d
Tony Li <tony.li@automattic.com>
2023-01-25 22:35:18 +1300

    Update WordPressKit

--- WordPress/Classes/Services/BlogService+BloggingPrompts.swift
+++ WordPress/Classes/Services/BlogService+BloggingPrompts.swift
@@ -4,8 +4,7 @@ extension BlogService {
 
     @objc func updatePromptSettings(for blog: RemoteBlog?, context: NSManagedObjectContext) {
         guard let blog = blog,
-              let siteID = blog.blogID,
-              let jsonSettings = blog.options?["blogging_prompts_settings"] as? [String: Any],
+              let jsonSettings = blog.options["blogging_prompts_settings"] as? [String: Any],
               let settingsValue = jsonSettings["value"],
               let data = try? JSONSerialization.data(withJSONObject: settingsValue),
               let remoteSettings = try? JSONDecoder().decode(RemoteBloggingPromptsSettings.self, from: data) else {
@@ -13,11 +12,11 @@ extension BlogService {
         }
 
         let fetchRequest = BloggingPromptSettings.fetchRequest()
-        fetchRequest.predicate = NSPredicate(format: "\(#keyPath(BloggingPromptSettings.siteID)) = %@", siteID)
+        fetchRequest.predicate = NSPredicate(format: "\(#keyPath(BloggingPromptSettings.siteID)) = %@", blog.blogID)
         fetchRequest.fetchLimit = 1
         let existingSettings = (try? context.fetch(fetchRequest))?.first
         let settings = existingSettings ?? BloggingPromptSettings(context: context)
-        settings.configure(with: remoteSettings, siteID: siteID.int32Value, context: context)
+        settings.configure(with: remoteSettings, siteID: blog.blogID.int32Value, context: context)
     }
 
 }
