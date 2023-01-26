8b2b3d9bbaac7a342cf4483da8074bb9bd14673a
Tony Li <tony.li@automattic.com>
2023-01-26 21:32:02 +1300

    Use `CoreDataStack` in `ReaderSearchSuggestionService`

--- WordPress/Classes/Services/ReaderSearchSuggestionService.swift
+++ WordPress/Classes/Services/ReaderSearchSuggestionService.swift
@@ -4,7 +4,14 @@ import CocoaLumberjack
 /// Provides functionality for fetching, saving, and deleting search phrases
 /// used to search for content in the reader.
 ///
-@objc class ReaderSearchSuggestionService: LocalCoreDataService {
+@objc class ReaderSearchSuggestionService: NSObject {
+
+    private let coreDataStack: CoreDataStack
+
+    @objc init(coreDataStack: CoreDataStack) {
+        self.coreDataStack = coreDataStack
+        super.init()
+    }
 
     /// Creates or updates an existing record for the specified search phrase.
     ///
@@ -12,14 +19,17 @@ import CocoaLumberjack
     ///     - phrase: The search phrase in question.
     ///
     @objc func createOrUpdateSuggestionForPhrase(_ phrase: String) {
-        var suggestion = findSuggestionForPhrase(phrase)
-        if suggestion == nil {
-            suggestion = NSEntityDescription.insertNewObject(forEntityName: ReaderSearchSuggestion.classNameWithoutNamespaces(),
-                                                                             into: managedObjectContext) as? ReaderSearchSuggestion
-            suggestion?.searchPhrase = phrase
+        self.coreDataStack.performAndSave { context in
+            var suggestion = self.findSuggestionForPhrase(phrase, in: context)
+            if suggestion == nil {
+                suggestion = NSEntityDescription.insertNewObject(
+                    forEntityName: ReaderSearchSuggestion.classNameWithoutNamespaces(),
+                    into: context
+                ) as? ReaderSearchSuggestion
+                suggestion?.searchPhrase = phrase
+            }
+            suggestion?.date = Date()
         }
-        suggestion?.date = Date()
-        ContextManager.sharedInstance().save(managedObjectContext)
     }
 
 
@@ -30,13 +40,13 @@ import CocoaLumberjack
     ///
     /// - Returns: A matching search phrase or nil.
     ///
-    @objc func findSuggestionForPhrase(_ phrase: String) -> ReaderSearchSuggestion? {
+    private func findSuggestionForPhrase(_ phrase: String, in context: NSManagedObjectContext) -> ReaderSearchSuggestion? {
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "ReaderSearchSuggestion")
         fetchRequest.predicate = NSPredicate(format: "searchPhrase MATCHES[cd] %@", phrase)
 
         var suggestions = [ReaderSearchSuggestion]()
         do {
-            suggestions = try managedObjectContext.fetch(fetchRequest) as! [ReaderSearchSuggestion]
+            suggestions = try context.fetch(fetchRequest) as! [ReaderSearchSuggestion]
         } catch let error as NSError {
             DDLogError("Error fetching search suggestion for phrase \(phrase) : \(error.localizedDescription)")
         }
@@ -44,57 +54,21 @@ import CocoaLumberjack
         return suggestions.first
     }
 
-
-    /// Finds and returns all ReaderSearchSuggestion starting with the specified search phrase.
-    ///
-    /// - Parameters:
-    ///     - phrase: The search phrase in question.
-    ///
-    /// - Returns: An array of matching `ReaderSearchSuggestion`s.
-    ///
-    @objc func fetchSuggestionsLikePhrase(_ phrase: String) -> [ReaderSearchSuggestion] {
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "ReaderSearchSuggestion")
-        fetchRequest.predicate = NSPredicate(format: "searchPhrase BEGINSWITH[cd] %@", phrase)
-
-        let sort = NSSortDescriptor(key: "date", ascending: false)
-        fetchRequest.sortDescriptors = [sort]
-
-        var suggestions = [ReaderSearchSuggestion]()
-        do {
-            suggestions = try managedObjectContext.fetch(fetchRequest) as! [ReaderSearchSuggestion]
-        } catch let error as NSError {
-            DDLogError("Error fetching search suggestions for phrase \(phrase) : \(error.localizedDescription)")
-        }
-
-        return suggestions
-    }
-
-
-    /// Deletes the specified search suggestion.
-    ///
-    /// - Parameters:
-    ///     - suggestion: The `ReaderSearchSuggestion` to delete.
-    ///
-    @objc func deleteSuggestion(_ suggestion: ReaderSearchSuggestion) {
-        managedObjectContext.delete(suggestion)
-        ContextManager.sharedInstance().saveContextAndWait(managedObjectContext)
-    }
-
-
     /// Deletes all saved search suggestions.
     ///
     @objc func deleteAllSuggestions() {
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "ReaderSearchSuggestion")
-        var suggestions = [ReaderSearchSuggestion]()
-        do {
-            suggestions = try managedObjectContext.fetch(fetchRequest) as! [ReaderSearchSuggestion]
-        } catch let error as NSError {
-            DDLogError("Error fetching search suggestion : \(error.localizedDescription)")
+        self.coreDataStack.performAndSave { context in
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "ReaderSearchSuggestion")
+            var suggestions = [ReaderSearchSuggestion]()
+            do {
+                suggestions = try context.fetch(fetchRequest) as! [ReaderSearchSuggestion]
+            } catch let error as NSError {
+                DDLogError("Error fetching search suggestion : \(error.localizedDescription)")
+            }
+            for suggestion in suggestions {
+                context.delete(suggestion)
+            }
         }
-        for suggestion in suggestions {
-            managedObjectContext.delete(suggestion)
-        }
-        ContextManager.sharedInstance().save(managedObjectContext)
     }
 
 }
