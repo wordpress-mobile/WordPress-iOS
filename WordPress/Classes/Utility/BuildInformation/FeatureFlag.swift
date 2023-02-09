3efae4608e8f731089766c867b76c8bb69fd1741
Povilas Staskus <povilas.staskus@automattic.com>
2023-02-09 12:53:59 +0200

    Make support forum feature flag identical across platforms

--- WordPress/Classes/Utility/BuildInformation/FeatureFlag.swift
+++ WordPress/Classes/Utility/BuildInformation/FeatureFlag.swift
@@ -171,7 +171,7 @@ enum FeatureFlag: Int, CaseIterable, OverrideableFlag {
         case .jetpackMigrationPreventDuplicateNotifications:
             return "prevent_duplicate_notifs_remote_field"
         case .wordPressSupportForum:
-            return "enable_wordpress_support_forum"
+            return "wordpress_support_forum_remote_field"
         case .blaze:
             return "blaze"
             default:
