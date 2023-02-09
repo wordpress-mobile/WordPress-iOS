006507e877563c4338de9ff7d18b955a5f43df51
Hassaan El-Garem <hassaanelgarem@gmail.com>
2023-02-09 23:58:43 +0200

    Add: Preliminary implementation of `BlazeWebViewController`

--- WordPress/Classes/ViewRelated/Blaze/BlazeWebViewController.swift
+++ WordPress/Classes/ViewRelated/Blaze/BlazeWebViewController.swift
@@ -3,4 +3,52 @@ import WebKit
 
 class BlazeWebViewController: UIViewController {
 
+    // MARK: Private Variables
+
+    let blog: Blog
+    let postID: NSNumber?
+    let webView: WKWebView
+    let progressView = WebProgressView()
+
+    // MARK: Initializers
+
+    init(blog: Blog, postID: NSNumber?) {
+        self.blog = blog
+        self.postID = postID
+        self.webView = WKWebView(frame: .zero)
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: View Lifecycles
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureSubviews()
+    }
+
+    // MARK: Private Helpers
+
+    private func configureSubviews() {
+        let subviews = [progressView, webView]
+        let stackView = UIStackView(arrangedSubviews: subviews)
+        stackView.axis = .vertical
+        view = stackView
+    }
+
+    private func configureWebView() {
+        // set delegate
+    }
+
+    private func configureNavBar() {
+        // set title
+        // set button
+    }
+
+    private func startBlazeFlow() {
+        // load url
+    }
 }
