566d83c9c9f3da23d8c04eae4f68ed37c4089454
Hassaan El-Garem <hassaanelgarem@gmail.com>
2023-02-09 23:28:10 +0200

    Add: present blaze flow function signature with empty implementation

--- WordPress/Classes/ViewRelated/Blaze/BlazeWebViewCoordinator.swift
+++ WordPress/Classes/ViewRelated/Blaze/BlazeWebViewCoordinator.swift
@@ -2,4 +2,9 @@ import Foundation
 
 class BlazeWebViewCoordinator {
 
+    static func presentBlazeFlow(in viewController: UIViewController,
+                                 blog: Blog,
+                                 postID: NSNumber?) {
+        // TODO: Implement this
+    }
 }
