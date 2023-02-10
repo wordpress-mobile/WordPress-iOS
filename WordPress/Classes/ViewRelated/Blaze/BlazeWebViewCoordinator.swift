a69a3540456895288db8361c398968fc84f9bfc9
Hassaan El-Garem <hassaanelgarem@gmail.com>
2023-02-10 05:10:54 +0200

    Add: documentation for `presentBlazeFlow` function

--- WordPress/Classes/ViewRelated/Blaze/BlazeWebViewCoordinator.swift
+++ WordPress/Classes/ViewRelated/Blaze/BlazeWebViewCoordinator.swift
@@ -8,6 +8,15 @@ class BlazeWebViewCoordinator {
         case postsList = "posts_list"
     }
 
+
+    /// Used to display the blaze web flow. Blazing a specific post
+    /// and displaying a list of posts to choose from are both supported by this function.
+    /// - Parameters:
+    ///   - viewController: The view controller where the web view should be presented in.
+    ///   - source: The source that triggers the display of the blaze web view.
+    ///   - blog: `Blog` object representing the site that is being blazed
+    ///   - postID: `NSNumber` representing the ID of the post being blazed. If `nil` is passed,
+    ///    the blaze site flow is triggered. If a valid value is passed, the blaze post flow is triggered.
     static func presentBlazeFlow(in viewController: UIViewController,
                                  source: Source,
                                  blog: Blog,
