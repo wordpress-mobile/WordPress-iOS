2c28ffa2ceb89f05daac786e97a432f9ce188d0a
Momo Ozawa <momo.ozawa@automattic.com>
2023-02-10 16:18:49 +0000

    Add: BlazeService

---
+++ WordPress/Classes/Services/BlazeService.swift
@@ -0,0 +1,47 @@
+import Foundation
+import WordPressKit
+
+final class BlazeService {
+
+    private let contextManager: CoreDataStack
+    private let remote: BlazeServiceRemote
+
+    // MARK: - Init
+
+    required init?(contextManager: CoreDataStack = ContextManager.shared,
+                   remote: BlazeServiceRemote? = nil) {
+        guard let account = try? WPAccount.lookupDefaultWordPressComAccount(in: contextManager.mainContext) else {
+            return nil
+        }
+
+        self.contextManager = contextManager
+        self.remote = remote ?? .init(wordPressComRestApi: account.wordPressComRestV2Api)
+    }
+
+    // MARK: - Methods
+
+    func getStatus(for blog: Blog,
+                   success: @escaping (Bool) -> Void,
+                   failure: @escaping (Error) -> Void) {
+        guard let siteId = blog.dotComID?.intValue else {
+            failure(BlazeServiceError.invalidSiteId)
+            return
+        }
+
+        remote.getStatus(forSiteId: siteId) { result in
+            switch result {
+            case .success(let approved):
+                success(approved)
+            case .failure(let error):
+                failure(error)
+            }
+        }
+    }
+}
+
+extension BlazeService {
+
+    enum BlazeServiceError: Error {
+        case invalidSiteId
+    }
+}
