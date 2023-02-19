955ce2275cfc1506c7dc374b73d4641686be03e9
David Christiandy <1299411+dvdchr@users.noreply.github.com>
2023-02-20 00:24:46 +0700

    Show support screen when secondary button is tapped

--- WordPress/Classes/ViewRelated/Jetpack/Branding/Coordinator/JetpackPluginOverlayCoordinator.swift
+++ WordPress/Classes/ViewRelated/Jetpack/Branding/Coordinator/JetpackPluginOverlayCoordinator.swift
@@ -1,3 +1,5 @@
+import WordPressAuthenticator
+
 final class JetpackPluginOverlayCoordinator: JetpackOverlayCoordinator {
     private unowned let viewController: UIViewController
 
@@ -14,12 +16,27 @@ final class JetpackPluginOverlayCoordinator: JetpackOverlayCoordinator {
     }
 
     func navigateToSecondaryRoute() {
-        // TODO: Contact support with support origin.
+        guard let navigationController = viewController.navigationController else {
+            return
+        }
+
+        let supportViewController = SupportTableViewController()
+        supportViewController.sourceTag = Constants.supportSourceTag
+        navigationController.pushViewController(supportViewController, animated: true)
     }
 
     func navigateToLinkRoute(url: URL, source: String) {
         let webViewController = WebViewControllerFactory.controller(url: url, source: source)
         let navigationController = UINavigationController(rootViewController: webViewController)
         presentingViewController.present(navigationController, animated: true)
+    }
+}
+
+private extension JetpackPluginOverlayCoordinator {
+    enum Constants {
+        static let supportSourceTag = WordPressSupportSourceTag(
+            name: "jetpackInstallFullPluginOverlay",
+            origin: "origin:jp-install-full-plugin-overlay"
+        )
     }
 }
