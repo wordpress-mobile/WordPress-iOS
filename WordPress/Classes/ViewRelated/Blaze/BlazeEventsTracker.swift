5e49a1f996886949a2b16d84e8c0ce043b71a24f
Hassaan El-Garem <hassaanelgarem@gmail.com>
2023-02-28 22:08:03 +0200

    Add: blaze flow tracking functions to `BlazeEventsTracker`

--- WordPress/Classes/ViewRelated/Blaze/BlazeEventsTracker.swift
+++ WordPress/Classes/ViewRelated/Blaze/BlazeEventsTracker.swift
@@ -2,6 +2,8 @@ import Foundation
 
 @objcMembers class BlazeEventsTracker: NSObject {
 
+    private static let currentStepPropertyKey = "current_step"
+
     static func trackBlazeFeatureDisplayed(for source: BlazeSource) {
         WPAnalytics.track(.blazeFeatureDisplayed, properties: analyticsProperties(for: source))
     }
@@ -18,7 +20,29 @@ import Foundation
         WPAnalytics.track(.blazeCardHidden, properties: analyticsProperties(for: source))
     }
 
+    static func trackBlazeFlowStarted(for source: BlazeSource) {
+        WPAnalytics.track(.blazeFlowStarted, properties: analyticsProperties(for: source))
+    }
+
+    static func trackBlazeFLowCompleted(for source: BlazeSource, currentStep: String) {
+        WPAnalytics.track(.blazeFlowCompleted, properties: analyticsProperties(for: source, currentStep: currentStep))
+    }
+
+    static func trackBlazeFlowCanceled(for source: BlazeSource, currentStep: String) {
+        WPAnalytics.track(.blazeFlowCanceled, properties: analyticsProperties(for: source, currentStep: currentStep))
+    }
+
+    static func trackBlazeFlowError(for source: BlazeSource, currentStep: String) {
+        WPAnalytics.track(.blazeFlowError, properties: analyticsProperties(for: source, currentStep: currentStep))
+    }
+
     private static func analyticsProperties(for source: BlazeSource) -> [String: String] {
         return [WPAppAnalyticsKeySource: source.description]
+    }
+
+    private static func analyticsProperties(for source: BlazeSource, currentStep: String) -> [String: String] {
+        return [
+            WPAppAnalyticsKeySource: source.description,
+            Self.currentStepPropertyKey: currentStep]
     }
 }
