454d3b7357fe6bcc6d417aa87b4582c338119e4f
Gio Lodi <gio@mokacoding.com>
2023-03-03 13:04:57 +1100

    Add note about the possibility of needing `force: true`

--- fastlane/lanes/codesign.rb
+++ fastlane/lanes/codesign.rb
@@ -191,6 +191,8 @@ def update_code_signing_app_store(readonly:, app_identifiers:)
 end
 
 def update_code_signing(type:, team_id:, readonly:, app_identifiers:, api_key_path:)
+  # NOTE: It might be neccessary to add `force: true` alongside `readonly: true` in order to regenerate some provisioning profiles.
+  # If this turns out to be a hard requirement, we should consider updating the method with logic to toggle the two setting based on whether we're fetching or renewing.
   match(
     storage_mode: 'google_cloud',
     google_cloud_bucket_name: 'a8c-fastlane-match',
