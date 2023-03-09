90e81c72a91d32594b57686fe0382c8b8b4f53dc
jos <17252150+jostnes@users.noreply.github.com>
2023-03-09 14:02:52 +0800

    temp comment out test analytics

--- fastlane/lanes/build.rb
+++ fastlane/lanes/build.rb
@@ -89,7 +89,7 @@ platform :ios do
 
     UI.user_error!("Unable to find .xctestrun file at #{build_products_path}. xctestrun path is #{xctestrun_path}") if xctestrun_path.nil? || !File.exist?((xctestrun_path))
 
-    inject_buildkite_analytics_environment(xctestrun_path: xctestrun_path) if buildkite_ci?
+    # inject_buildkite_analytics_environment(xctestrun_path: xctestrun_path) if buildkite_ci?
 
     run_tests(
       workspace: WORKSPACE_PATH,
