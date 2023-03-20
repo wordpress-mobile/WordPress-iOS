f6d5bbedde8f44d8f01457422fdb82c8180e055b
Beemo <tzef8220@gmail.com>
2023-03-20 11:02:30 +0800

    Add lock screen stats widget config and implementation to include required information

---
+++ WordPress/WordPressStatsWidgets/LockScreenWidgets/Configs/LockScreenTodayViewsStatWidgetConfig.swift
@@ -0,0 +1,45 @@
+import WidgetKit
+
+@available(iOS 16.0, *)
+struct LockScreenTodayViewsStatWidgetConfig: LockScreenStatsWidgetConfig {
+    typealias WidgetData = HomeWidgetTodayData
+    typealias ViewProvider = LockScreenSingleStatWidgetViewProvider
+
+    var supportFamilies: [WidgetFamily] {
+        [.accessoryRectangular]
+    }
+
+    var displayName: String {
+        LocalizableStrings.viewsInTodayTitle
+    }
+
+    var description: String {
+        LocalizableStrings.todayPreviewDescription
+    }
+
+    var kind: String {
+        AppConfiguration.Widget.Stats.lockScreenTodayViewsKind
+    }
+
+    var placeholderContent: HomeWidgetTodayData {
+        HomeWidgetTodayData(
+            siteID: 0,
+            siteName: "My WordPress Site",
+            url: "",
+            timeZone: TimeZone.current,
+            date: Date(),
+            stats: TodayWidgetStats(
+                views: 649,
+                visitors: 572,
+                likes: 16,
+                comments: 8
+            )
+        )
+    }
+
+    var viewProvider: ViewProvider {
+        LockScreenSingleStatWidgetViewProvider(
+            title: LocalizableStrings.viewsInTodayTitle
+        )
+    }
+}
