8f7e16a31b1093cb4d5aa3c7e79e4266a4dbc093
Hassaan El-Garem <hassaanelgarem@gmail.com>
2023-03-22 03:39:31 +0200

    Update: remove `remoteKey` requirement from `OverrideableFlag`

--- WordPress/Classes/Utility/BuildInformation/FeatureFlagOverrideStore.swift
+++ WordPress/Classes/Utility/BuildInformation/FeatureFlagOverrideStore.swift
@@ -6,9 +6,6 @@ import Foundation
 protocol OverrideableFlag: CustomStringConvertible {
     var enabled: Bool { get }
     var canOverride: Bool { get }
-
-    /// An optional key identifying this flag server-side. Not all flags will have this key – they may not use remote feature flagging
-    var remoteKey: String? { get }
 }
 
 /// Used to override values for feature flags at runtime in debug builds
