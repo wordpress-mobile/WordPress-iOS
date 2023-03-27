b4f12ce984e7e01185f9721b74bc8f67b9830d3d
Hassaan El-Garem <hassaanelgarem@gmail.com>
2023-03-27 08:49:54 +0200

    Tests: update `RemoteConfigParamTests` and add override test

--- WordPress/WordPressTest/RemoteConfigParameterTests.swift
+++ WordPress/WordPressTest/RemoteConfigParameterTests.swift
@@ -4,21 +4,23 @@ import XCTest
 final class RemoteConfigParameterTests: XCTestCase {
 
     private var remoteConfigStore: RemoteConfigStore!
+    private var overrideStore: RemoteConfigOverrideStore!
 
     override func setUp() {
         let mockUserDefaults = InMemoryUserDefaults()
         let config = ["param_key": "server_value"]
         let mock = MockRemoteConfigRemote(config: config)
         remoteConfigStore = RemoteConfigStore(remote: mock, persistenceStore: mockUserDefaults)
+        overrideStore = RemoteConfigOverrideStore(store: mockUserDefaults)
     }
 
     func testReturningServerValueIfPresent() {
         // Given
-        let param = RemoteConfigParameter(key: "param_key", defaultValue: "default_value", store: remoteConfigStore)
+        let param = MockRemoteParameter(key: "param_key", defaultValue: "default_value")
         remoteConfigStore.update()
 
         // When
-        let value = param.value
+        let value: String? = param.value(using: remoteConfigStore, overrideStore: overrideStore)
 
         // Then
         XCTAssertEqual(value, "server_value")
@@ -26,15 +28,34 @@ final class RemoteConfigParameterTests: XCTestCase {
 
     func testFallingBackToDefaultValue() {
         // Given
-        let param = RemoteConfigParameter(key: "param_key", defaultValue: "default_value", store: remoteConfigStore)
+        let param = MockRemoteParameter(key: "param_key", defaultValue: "default_value")
 
         // When
-        let value = param.value
+        let value: String? = param.value(using: remoteConfigStore, overrideStore: overrideStore)
 
         // Then
         XCTAssertEqual(value, "default_value")
     }
 
+    func testReturningOverriddenValueIfPresent() {
+        // Given
+        let param = MockRemoteParameter(key: "param_key", defaultValue: "default_value")
+        overrideStore.override(param, withValue: "overridden_value")
+        remoteConfigStore.update()
+
+        // When
+        let value: String? = param.value(using: remoteConfigStore, overrideStore: overrideStore)
+
+        // Then
+        XCTAssertEqual(value, "overridden_value")
+    }
+
+}
+
+struct MockRemoteParameter: RemoteParameter {
+    let key: String
+    let defaultValue: LosslessStringConvertible?
+    let description = "description"
 }
 
 class MockRemoteConfigRemote: RemoteConfigRemote {
