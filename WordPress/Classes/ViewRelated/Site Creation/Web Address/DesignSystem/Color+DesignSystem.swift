5197765441a794edb20c1c320092cf02992d4b6d
alpavanoglu <alp.avanoglu@automattic.com>
2023-04-14 18:25:44 +0200

    Populate `ColorGallery`

--- WordPress/Classes/ViewRelated/Site Creation/Web Address/DesignSystem/Color+DesignSystem.swift
+++ WordPress/Classes/ViewRelated/Site Creation/Web Address/DesignSystem/Color+DesignSystem.swift
@@ -6,12 +6,14 @@ extension Color {
             static let primary = Color("foregroundPrimary")
             static let secondary = Color("foregroundSecondary")
             static let tertiary = Color("foregroundTertiary")
+            static let quaternary = Color("backgroundQuaternary")
         }
 
         enum Background {
             static let primary = Color("backgroundPrimary")
             static let secondary = Color("backgroundSecondary")
             static let tertiary = Color("backgroundTertiary")
+            static let quaternary = Color("backgroundQuaternary")
         }
 
         enum Theme {
@@ -28,23 +30,39 @@ extension Color {
     }
 }
 
-// Replica of the `Color` structure
+/// Replica of the `Color` structure
+/// The reason for not using the `Color` intializer of `UIColor` is that
+/// it has dubious effects. Also the doc advises against it.
+/// Even though `UIColor(SwiftUI.Color)` keeps the adaptability for color theme,
+/// accessing to light or dark variants specifically via trait collection does not return the right values
+/// if the color is initialized as such. Probably one of the reasons why they advise against it.
+/// To make these values non-optional, we use `Color` versions as fallback.
 extension UIColor {
     enum DS {
         enum Foreground {
-            static let primary = UIColor(Color.DS.Foreground.primary)
-            static let secondary = UIColor(Color.DS.Foreground.secondary)
-            static let tertiary = UIColor(Color.DS.Foreground.tertiary)
+            static let primary = UIColor(named: "foregroundPrimary")
+            static let secondary = UIColor(named: "foregroundSecondary")
+            static let tertiary = UIColor(named: "foregroundTertiary")
+            static let quaternary = UIColor(named: "backgroundQuaternary")
         }
 
         enum Background {
-            static let primary = UIColor(Color.DS.Background.primary)
-            static let secondary = UIColor(Color.DS.Background.secondary)
-            static let tertiary = UIColor(Color.DS.Background.tertiary)
+            static let primary = UIColor(named: "backgroundPrimary")
+            static let secondary = UIColor(named: "backgroundSecondary")
+            static let tertiary = UIColor(named: "backgroundTertiary")
+            static let quaternary = UIColor(named: "backgroundQuaternary")
         }
 
         enum Theme {
-            static let primary = UIColor(Color.DS.Theme.primary)
+            static var primary: UIColor? {
+                if AppConfiguration.isJetpack {
+                    return jetpack
+                } else {
+                    return jetpack // FIXME: WordPress colors
+                }
+            }
+
+            private static let jetpack = UIColor(named: "themeJetpack")
         }
     }
 }
--- WordPress/Classes/ViewRelated/Site Creation/Web Address/DesignSystem/ColorGallery.swift
+++ WordPress/Classes/ViewRelated/Site Creation/Web Address/DesignSystem/ColorGallery.swift
@@ -1,19 +1,72 @@
 import SwiftUI
 
 struct ColorGallery: View {
+    @SwiftUI.Environment(\.colorScheme) var colorScheme
+
     var body: some View {
         List {
-            Section(header: Text("Foreground").font(Font.title3)) {
-                listItem(with: "Primary", color: Color.DS.Foreground.primary)
-                listItem(with: "Secondary", color: Color.DS.Foreground.secondary)
-                listItem(with: "Tertiary", color: Color.DS.Foreground.tertiary)
+            Section(header: sectionTitle("Theme")) {
+                listItem(
+                    with: "Primary",
+                    hexString: hexString(for: .DS.Theme.primary),
+                    color: Color.DS.Theme.primary
+                )
             }
-            Section(header: Text("Background").font(Font.title3)) {
-                listItem(with: "Primary", color: Color.DS.Background.primary)
-                listItem(with: "Secondary", color: Color.DS.Background.secondary)
-                listItem(with: "Tertiary", color: Color.DS.Background.tertiary)
+            Section(header: sectionTitle("Foreground")) {
+                listItem(
+                    with: "Primary",
+                    hexString: hexString(for: .DS.Foreground.primary),
+                    color: Color.DS.Foreground.primary
+                )
+                listItem(
+                    with: "Secondary",
+                    hexString: hexString(for: .DS.Foreground.secondary),
+                    color: Color.DS.Foreground.secondary
+                )
+                listItem(
+                    with: "Tertiary",
+                    hexString: hexString(for: .DS.Foreground.tertiary),
+                    color: Color.DS.Foreground.tertiary
+                )
+                listItem(
+                    with: "Quaternary",
+                    hexString: hexString(for: .DS.Foreground.quaternary),
+                    color: Color.DS.Foreground.quaternary
+                )
+            }
+            Section(header: sectionTitle("Background")) {
+                listItem(
+                    with: "Primary",
+                    hexString: hexString(for: .DS.Background.primary),
+                    color: Color.DS.Background.primary
+                )
+                listItem(
+                    with: "Secondary",
+                    hexString: hexString(for: .DS.Background.secondary),
+                    color: Color.DS.Background.secondary
+                )
+                listItem(
+                    with: "Tertiary",
+                    hexString: hexString(for: .DS.Background.tertiary),
+                    color: Color.DS.Background.tertiary
+                )
+                listItem(
+                    with: "Quaternary",
+                    hexString: hexString(for: .DS.Background.quaternary),
+                    color: Color.DS.Background.quaternary
+                )
             }
         }
+    }
+
+    private func hexString(for color: UIColor?) -> String? {
+        colorScheme == .light ? color?.lightVariant().hexString() : color?.darkVariant().hexString()
+    }
+
+    private func sectionTitle(_ title: String) -> some View {
+        Text(title)
+            .font(Font.headline)
+            .foregroundColor(.DS.Foreground.primary)
     }
 
     private func colorSquare(_ color: Color) -> some View {
@@ -22,10 +75,21 @@ struct ColorGallery: View {
             .frame(width: 44, height: 44)
     }
 
-    private func listItem(with name: String, color: Color) -> some View {
+    private func listItem(with name: String, hexString: String?, color: Color) -> some View {
         HStack(spacing: 16) {
             colorSquare(color)
-            Text(name)
+            VStack(spacing: 8) {
+                HStack {
+                    Text(name)
+                        .foregroundColor(.DS.Foreground.primary)
+                    Spacer()
+                }
+                HStack {
+                    Text("#\(hexString ?? "")")
+                        .foregroundColor(.DS.Foreground.secondary)
+                    Spacer()
+                }
+            }
         }
     }
 }
