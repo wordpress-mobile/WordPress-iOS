48fb4842b385519df0e1db7051cb41d27b99ecbd
alpavanoglu <alp.avanoglu@automattic.com>
2023-04-17 14:18:01 +0200

    Add border colors

--- WordPress/Classes/ViewRelated/Site Creation/Web Address/DesignSystem/Color+DesignSystem.swift
+++ WordPress/Classes/ViewRelated/Site Creation/Web Address/DesignSystem/Color+DesignSystem.swift
@@ -1,5 +1,7 @@
 import SwiftUI
 
+/// Design System Color extensions. Keep it in sync with its sibling file `UIColor+DesignSystem`
+/// to support borth API's equally.
 extension Color {
     enum DS {
         enum Foreground {
@@ -16,6 +18,12 @@ extension Color {
             static let quaternary = Color("backgroundQuaternary")
         }
 
+        enum Border {
+            static let primary = Color("borderPrimary")
+            static let secondary = Color("borderSecondary")
+            static let divider = Color("borderDivider")
+        }
+
         enum Theme {
             static var primary: Color {
                 if AppConfiguration.isJetpack {
@@ -26,43 +34,6 @@ extension Color {
             }
 
             private static let jetpack = Color("themeJetpack")
-        }
-    }
-}
-
-/// Replica of the `Color` structure
-/// The reason for not using the `Color` intializer of `UIColor` is that
-/// it has dubious effects. Also the doc advises against it.
-/// Even though `UIColor(SwiftUI.Color)` keeps the adaptability for color theme,
-/// accessing to light or dark variants specifically via trait collection does not return the right values
-/// if the color is initialized as such. Probably one of the reasons why they advise against it.
-/// To make these values non-optional, we use `Color` versions as fallback.
-extension UIColor {
-    enum DS {
-        enum Foreground {
-            static let primary = UIColor(named: "foregroundPrimary")
-            static let secondary = UIColor(named: "foregroundSecondary")
-            static let tertiary = UIColor(named: "foregroundTertiary")
-            static let quaternary = UIColor(named: "backgroundQuaternary")
-        }
-
-        enum Background {
-            static let primary = UIColor(named: "backgroundPrimary")
-            static let secondary = UIColor(named: "backgroundSecondary")
-            static let tertiary = UIColor(named: "backgroundTertiary")
-            static let quaternary = UIColor(named: "backgroundQuaternary")
-        }
-
-        enum Theme {
-            static var primary: UIColor? {
-                if AppConfiguration.isJetpack {
-                    return jetpack
-                } else {
-                    return jetpack // FIXME: WordPress colors
-                }
-            }
-
-            private static let jetpack = UIColor(named: "themeJetpack")
         }
     }
 }
