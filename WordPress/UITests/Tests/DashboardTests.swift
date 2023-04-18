60c68e596264e5120c034971d57295ab72c927f9
pachlava <pachlava@gmail.com>
2023-04-18 14:55:29 +0300

    First iteration of `testDomainsCard` test

---
+++ WordPress/UITests/Tests/DashboardTests.swift
@@ -0,0 +1,28 @@
+import UITestsFoundation
+import XCTest
+
+class DashboardTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        setUpTestSuite()
+
+        try LoginFlow.login(
+            siteUrl: WPUITestCredentials.testWPcomSiteAddress,
+            email: WPUITestCredentials.testWPcomUserEmail,
+            password: WPUITestCredentials.testWPcomPassword
+        )
+    }
+
+    override func tearDownWithError() throws {
+        takeScreenshotOfFailedTest()
+        removeApp()
+    }
+
+    func testDomainsCard() throws {
+        try MySiteScreen()
+            .verifyDomainsCard()
+            .tapDomainsCard()
+
+        XCTAssertTrue(DomainsScreen.isLoaded(), "\"Domains\" screen isn't loaded after \"Domains\" card tap.")
+    }
+}
---
+++ WordPress/UITestsFoundation/Screens/DomainsScreen.swift
@@ -0,0 +1,26 @@
+import ScreenObject
+import XCTest
+
+public class DomainsScreen: ScreenObject {
+    public let tabBar: TabNavComponent
+
+    let siteDomainsNavbarHeaderGetter: (XCUIApplication) -> XCUIElement = {
+        $0.staticTexts["Site Domains"]
+    }
+
+    var siteDomainsNavbarHeader: XCUIElement { siteDomainsNavbarHeaderGetter(app) }
+
+    public init(app: XCUIApplication = XCUIApplication()) throws {
+        tabBar = try TabNavComponent()
+
+        try super.init(
+            expectedElementGetters: [ siteDomainsNavbarHeaderGetter ],
+            app: app,
+            waitTimeout: 7
+        )
+    }
+
+    public static func isLoaded() -> Bool {
+        (try? DomainsScreen().isLoaded) ?? false
+    }
+}
