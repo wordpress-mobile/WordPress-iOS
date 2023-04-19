38f56b1e90b4fea517dc02f055e6941c74f3e3ab
Hassaan El-Garem <hassaanelgarem@gmail.com>
2023-04-19 02:30:32 +0200

    Update: add button and description label

--- WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Pages/DashboardPageCreationCell.swift
+++ WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Pages/DashboardPageCreationCell.swift
@@ -4,19 +4,64 @@ class DashboardPageCreationCell: UITableViewCell, Reusable {
 
     // MARK: Views
 
-    lazy var mainStackView: UIStackView = {
+    private lazy var mainStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .horizontal
-        stackView.addArrangedSubviews([descriptionLabel])
+        stackView.spacing = Metrics.mainStackViewSpacing
+        stackView.directionalLayoutMargins = Metrics.mainStackViewLayoutMargins
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.addArrangedSubviews([labelsStackView])
         return stackView
+    }()
+
+    private lazy var labelsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = Metrics.labelsStackViewSpacing
+        stackView.directionalLayoutMargins = Metrics.labelsStackViewLayoutMargins
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.addArrangedSubviews([createPageButton, descriptionLabel])
+        return stackView
+    }()
+
+    private lazy var createPageButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.contentHorizontalAlignment = .leading
+        button.titleLabel?.adjustsFontForContentSizeCategory = true
+        button.addTarget(self, action: #selector(createPageButtonTapped), for: .touchUpInside)
+        let font = WPStyleGuide.fontForTextStyle(.callout, fontWeight: .bold)
+
+        if #available(iOS 15.0, *) {
+            var buttonConfig: UIButton.Configuration = .plain()
+            buttonConfig.contentInsets = Metrics.createPageButtonContentInsets
+            buttonConfig.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer({ incoming in
+                var outgoing = incoming
+                outgoing.font = font
+                return outgoing
+            })
+            button.configuration = buttonConfig
+        } else {
+            button.titleLabel?.font = font
+            button.contentEdgeInsets = Metrics.createPageButtonContentEdgeInsets
+            button.flipInsetsForRightToLeftLayoutDirection()
+        }
+
+        // TODO: Remove this when configure() is implemented
+        button.setTitle(Strings.createPageButtonText, for: .normal)
+        return button
     }()
 
     private lazy var descriptionLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.adjustsFontForContentSizeCategory = true
-        label.font = WPStyleGuide.fontForTextStyle(.body, fontWeight: .bold)
+        label.numberOfLines = 0
+        label.font = WPStyleGuide.regularTextFont()
+        label.textColor = .secondaryLabel
+        label.text = Strings.descriptionLabelText
         return label
     }()
 
@@ -35,7 +80,7 @@ class DashboardPageCreationCell: UITableViewCell, Reusable {
     // MARK: Public Functions
 
     func configure(expanded: Bool, hasPages: Bool) {
-        descriptionLabel.text = "Hello World"
+        // TODO: Configure cell bases on passed values
     }
 
     // MARK: Helpers
@@ -53,12 +98,33 @@ class DashboardPageCreationCell: UITableViewCell, Reusable {
         contentView.addSubview(mainStackView)
         contentView.pinSubviewToAllEdges(mainStackView)
     }
+
+    // MARK: Actions
+
+    @objc func createPageButtonTapped() {
+        // TODO: To be implemented
+    }
 }
 
 private extension DashboardPageCreationCell {
     enum Metrics {
+        static let mainStackViewSpacing: CGFloat = 16
+        static let mainStackViewLayoutMargins: NSDirectionalEdgeInsets = .init(top: 0, leading: 16, bottom: 0, trailing: 16)
+        static let labelsStackViewSpacing: CGFloat = 2
+        static let labelsStackViewLayoutMargins: NSDirectionalEdgeInsets = .init(top: 16, leading: 0, bottom: 16, trailing: 0)
+        static let createPageButtonContentInsets = NSDirectionalEdgeInsets.zero
+        static let createPageButtonContentEdgeInsets = UIEdgeInsets.zero
     }
 
     enum Colors {
+    }
+
+    enum Strings {
+        static let descriptionLabelText = NSLocalizedString("dashboardCard.pages.create.description",
+                                                            value: "Start with bespoke, mobile friendly layouts.",
+                                                            comment: "Title of a label that encourages the user to create a new page.")
+        static let createPageButtonText = NSLocalizedString("dashboardCard.pages.create.button.title",
+                                                            value: "Create another page",
+                                                            comment: "Title of a button that starts the page creation flow.")
     }
 }
