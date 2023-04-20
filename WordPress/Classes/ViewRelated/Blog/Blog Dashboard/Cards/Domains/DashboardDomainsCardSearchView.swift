22f9a0e5e30172657aa07535473a4a11cd18005e
Povilas Staskus <povilas.staskus@automattic.com>
2023-04-20 14:40:54 +0300

    Create a decorative search view for DomainDashboardCard
    - A placeholder view for a search bar that suggests searching a domain
    - A view adapts to light and dark mode and fades out
    - Accessibility is turned off since it serves the same purpose as an image

---
+++ WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Domains/DashboardDomainsCardSearchView.swift
@@ -0,0 +1,71 @@
+import SwiftUI
+
+struct DashboardDomainsCardSearchVew: View {
+    @SwiftUI.Environment(\.colorScheme) var colorScheme: ColorScheme
+
+    var body: some View {
+        VStack(alignment: .center) {
+            HStack {
+                Image(systemName: Constants.iconName)
+                    .foregroundColor(Colors.icon)
+                    .font(.system(size: Metrics.iconSize))
+                Text(Constants.searchBarPlaceholder)
+                    .foregroundColor(Colors.text)
+                    .font(.system(size: Metrics.fontSize))
+                Spacer()
+            }
+            .padding(.horizontal, Metrics.padding)
+            .frame(height: Metrics.searchBarHeight)
+            .background(
+                RoundedRectangle(cornerRadius: Metrics.containerCornerRadius)
+                    .foregroundColor(Colors.containerBackground)
+            )
+            Spacer()
+            RoundedRectangle(cornerRadius: Metrics.containerCornerRadius)
+                .foregroundColor(Colors.containerBackground)
+        }
+        .padding([.leading, .trailing, .top], Metrics.padding)
+        .frame(height: Metrics.height)
+        .background(
+            ZStack {
+                LinearGradient(
+                    gradient: Gradient(
+                        colors: [
+                            colorScheme == .light ? Colors.gradientTopLight : Colors.gradientTopDark,
+                            Color.clear
+                        ]
+                    ),
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+            }
+        )
+        .cornerRadius(Metrics.cornerRadius)
+        .accessibilityHidden(true)
+    }
+}
+
+private extension DashboardDomainsCardSearchVew {
+    enum Metrics {
+        static let padding: CGFloat = 8
+        static let cornerRadius: CGFloat = 16
+        static let containerCornerRadius: CGFloat = 8
+        static let iconSize: CGFloat = 20
+        static let fontSize: CGFloat = 15 // fixed .footnote style size
+        static let height: CGFloat = 110
+        static let searchBarHeight: CGFloat = 40
+    }
+
+    enum Constants {
+        static let iconName = "globe"
+        static let searchBarPlaceholder = "domain.blog"
+    }
+
+    enum Colors {
+        static let gradientTopLight = Color(UIColor.secondarySystemBackground)
+        static let gradientTopDark = Color(UIColor.tertiarySystemBackground)
+        static let containerBackground = Color(UIColor.secondarySystemGroupedBackground)
+        static let icon = Color(UIColor.jetpackGreen)
+        static let text = Color(UIColor.textSubtle)
+    }
+}
