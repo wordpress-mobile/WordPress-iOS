edb61e1b703b47b42f103f3bf29a4c55d1a70ae9
Momo Ozawa <momo.ozawa@automattic.com>
2023-04-20 14:05:09 +0100

    Fix dashboard unit tests

--- WordPress/WordPressTest/Dashboard/BlogDashboardServiceTests.swift
+++ WordPress/WordPressTest/Dashboard/BlogDashboardServiceTests.swift
@@ -45,7 +45,7 @@ class BlogDashboardServiceTests: CoreDataTestCase {
 
         service.fetch(blog: blog) { _ in
             XCTAssertEqual(self.remoteServiceMock.didCallWithBlogID, self.wpComID)
-            XCTAssertEqual(self.remoteServiceMock.didRequestCards, ["todays_stats", "posts", "pages"])
+            XCTAssertEqual(self.remoteServiceMock.didRequestCards, ["todays_stats", "posts", "pages", "activity"])
             expect.fulfill()
         }
 
