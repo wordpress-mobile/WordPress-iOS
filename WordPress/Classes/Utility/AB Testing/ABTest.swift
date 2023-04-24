4466ad4d93d6872fd6ecf492fef433b264f608d7
Salim Braksa <salim.braksa@gmail.com>
2023-04-24 12:44:56 +0100

    Refactor ABTest enum to enable experiments for Jetpack only

--- WordPress/Classes/Utility/AB Testing/ABTest.swift
+++ WordPress/Classes/Utility/AB Testing/ABTest.swift
@@ -15,14 +15,15 @@ extension ABTest {
     /// Start the AB Testing platform if any experiment exists
     ///
     static func start() {
-        guard ABTest.allCases.count > 1, AccountHelper.isLoggedIn,
-              AppConfiguration.isWordPress else {
+        guard ABTest.allCases.count > 1,
+              AccountHelper.isLoggedIn,
+              AppConfiguration.isJetpack,
+              let exPlat = ExPlat.shared
+        else {
             return
         }
-
         let experimentNames = ABTest.allCases.filter { $0 != .unknown }.map { $0.rawValue }
-        ExPlat.shared?.register(experiments: experimentNames)
-
-        ExPlat.shared?.refresh()
+        exPlat.register(experiments: experimentNames)
+        exPlat.refresh()
     }
 }
