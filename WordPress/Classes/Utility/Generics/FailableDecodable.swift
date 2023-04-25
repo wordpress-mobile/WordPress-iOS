0172550a7ff9628fbdcd1341ac9b6ae174a8df0c
Hassaan El-Garem <hassaanelgarem@gmail.com>
2023-04-25 03:19:36 +0200

    Add: new struct `FailableDecodable` That acts as a wrapper for decodable types that we want to mark as failable.

---
+++ WordPress/Classes/Utility/Generics/FailableDecodable.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+
+/// Acts as a wrapper around decodable types, and marks them as failable.
+/// This allows the decoding process to succeed even if the decoder was unable to decode a failable item.
+struct FailableDecodable<T: Decodable & Hashable>: Decodable {
+    let result: Result<T, Error>
+
+    var value: T? {
+        return try? result.get()
+    }
+
+    init(from decoder: Decoder) throws {
+        result = Result(catching: { try T(from: decoder) })
+    }
+}
+
+extension FailableDecodable: Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(value)
+    }
+
+    static func == (lhs: FailableDecodable<T>, rhs: FailableDecodable<T>) -> Bool {
+        return lhs.value == rhs.value
+    }
+}
