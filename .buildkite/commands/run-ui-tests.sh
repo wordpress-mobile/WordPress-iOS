3ebfb17a5c6b755444cdceec4a9e9007a6caeba9
Tiago Marques <tiago.marques@automattic.com>
2023-05-09 16:44:24 -0300

    Create crashes directory

--- .buildkite/commands/run-ui-tests.sh
+++ .buildkite/commands/run-ui-tests.sh
@@ -40,8 +40,9 @@ echo "--- 📦 Zipping test results"
 cd build/results/ && zip -rq JetpackUITests.xcresult.zip JetpackUITests.xcresult && cd -
 
 echo "--- 💥 Collect Crash reports"
+mkdir -p build/results/crashes
 find ~/Library/Logs/DiagnosticReports -name '*.ips' -print0 | while read -d $'\0' -r file; do
-  cp  -p "$file" "build/results/crashes/$(basename "$file")"
+  cp "$file" "build/results/crashes/$(basename "$file")"
 done
 
 echo "--- 🚦 Report Tests Status"
