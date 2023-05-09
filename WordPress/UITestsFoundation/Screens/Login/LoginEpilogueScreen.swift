6762bd657fa2394809fec914de1a892840b67dd4
jos <17252150+jostnes@users.noreply.github.com>
2023-05-09 17:08:08 +0800

    increase to 90 seconds

--- WordPress/UITestsFoundation/Screens/Login/LoginEpilogueScreen.swift
+++ WordPress/UITestsFoundation/Screens/Login/LoginEpilogueScreen.swift
@@ -13,7 +13,7 @@ public class LoginEpilogueScreen: ScreenObject {
         try super.init(
             expectedElementGetters: [loginEpilogueTableGetter],
             app: app,
-            waitTimeout: 75
+            waitTimeout: 90
         )
     }
 
