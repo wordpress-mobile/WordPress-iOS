2459b34e4371d9d9bd3121586d8896b6e0e3ee11
Povilas Staskus <povilas.staskus@automattic.com>
2023-05-18 16:46:24 +0300

    Create FreeToPaidPlansCoordinator to contain free to paid plans domains flow

---
+++ WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Free to Paid Plans/FreeToPaidPlansCoordinator.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+@objc final class FreeToPaidPlansCoordinator: NSObject {
+    static func presentFreeDomainWithAnnualPlanFlow(
+        in dashboardViewController: BlogDashboardViewController,
+        source: String,
+        blog: Blog
+    ) {
+        let domainSuggestionsViewController = RegisterDomainSuggestionsViewController.instance(
+            site: blog,
+            domainType: .mapped,
+            includeSupportButton: false
+        )
+
+        domainSuggestionsViewController.domainAddedToCartCallback = {
+            // TODO: Present Plans Selection and Checkout Flow
+            // https://github.com/wordpress-mobile/WordPress-iOS/issues/20688
+        }
+
+        dashboardViewController.show(domainSuggestionsViewController, sender: nil)
+    }
+}
