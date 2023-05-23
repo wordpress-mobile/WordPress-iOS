817659389160570f8b65fe1f89f7293f07e80a55
Gio Lodi <gio@mokacoding.com>
2023-05-23 20:36:33 +1000

    Update `CopyGutenbergJS` build phase script to account for XCFramework

--- Scripts/BuildPhases/CopyGutenbergJS.sh
+++ Scripts/BuildPhases/CopyGutenbergJS.sh
@@ -1,7 +1,32 @@
-#!/bin/bash -eux
+#!/bin/bash -eu
 
 # Update the matching .outputs.xcfilelist when changing this
-DEST=$CONFIGURATION_BUILD_DIR/$UNLOCALIZED_RESOURCES_FOLDER_PATH
+DEST="$CONFIGURATION_BUILD_DIR/$UNLOCALIZED_RESOURCES_FOLDER_PATH"
+
+# CocoaPods fetches Gutenberg in two possible ways: XCFramework and source.
+#
+# We check first for the XCFramework setup, fallingback to source only if that is not there.
+#
+# Because the XCFramework location is in `PODS_XCFRAMEWORKS_BUILD_DIR`, it will only appear if CocoaPods use XCFrameworks.
+# This makes the setup robust against having both a copy of the XCFramework and of the Gutenberg source code in the Pods folder.
+
+# Update the matching .inputs.xcfilelist when changing these
+XCFRAMEWORK_BUNDLE_ROOT="$PODS_XCFRAMEWORKS_BUILD_DIR/Gutenberg/Gutenberg.framework"
+PODS_BUNDLE_ROOT="$PODS_ROOT/Gutenberg/bundle/ios"
+
+BUNDLE_FILE="$DEST/main.jsbundle"
+BUNDLE_ASSETS="$DEST/assets/"
+
+if [[ -d $XCFRAMEWORK_BUNDLE_ROOT ]]; then
+  cp "$XCFRAMEWORK_BUNDLE_ROOT/App.js" "$BUNDLE_FILE"
+  # It appears we don't need to copy the assets when working with the XCFramework
+elif [[ -d $PODS_BUNDLE_ROOT ]]; then
+  cp "$PODS_BUNDLE_ROOT/App.js" "$BUNDLE_FILE"
+  cp -r "$PODS_BUNDLE_ROOT/assets" "$BUNDLE_ASSETS"
+else
+  echo "error: Could not find Gutenberg bundle in either XCFramework or Pods."
+  exit 1
+fi
 
 if [[ "$CONFIGURATION" = *Debug* && ! "$PLATFORM_NAME" == *simulator ]]; then
   IP=$(ipconfig getifaddr en0)
@@ -11,12 +36,3 @@ if [[ "$CONFIGURATION" = *Debug* && ! "$PLATFORM_NAME" == *simulator ]]; then
 
   echo "$IP" > "$DEST/ip.txt"
 fi
-
-# Update the matching .inputs.xcfilelist when changing this
-PODS_BUNDLE_ROOT="$PODS_ROOT/Gutenberg/bundle/ios"
-
-BUNDLE_FILE="$DEST/main.jsbundle"
-cp "$PODS_BUNDLE_ROOT/App.js" "$BUNDLE_FILE"
-
-BUNDLE_ASSETS="$DEST/assets/"
-cp -r "$PODS_BUNDLE_ROOT/assets/" "$BUNDLE_ASSETS"
