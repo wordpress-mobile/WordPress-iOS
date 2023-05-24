092fdc8c270a298b74c523325acde88948c0da6e
Salim Braksa <salim.braksa@gmail.com>
2023-05-24 22:56:02 +0100

    Remove domain registration with paid plan duplicated logic

--- WordPress/Classes/ViewRelated/Domains/Domain credit/DomainCreditEligibilityChecker.swift
+++ WordPress/Classes/ViewRelated/Domains/Domain credit/DomainCreditEligibilityChecker.swift
@@ -1,6 +1,5 @@
 class DomainCreditEligibilityChecker: NSObject {
     @objc static func canRedeemDomainCredit(blog: Blog) -> Bool {
-        // TODO: This logic is a possible duplication of `blog.canRegisterDomainWithPaidPlan`
-        return (blog.isHostedAtWPcom || blog.isAtomic()) && blog.hasDomainCredit && JetpackFeaturesRemovalCoordinator.jetpackFeaturesEnabled()
+        return blog.canRegisterDomainWithPaidPlan && JetpackFeaturesRemovalCoordinator.jetpackFeaturesEnabled()
     }
 }
