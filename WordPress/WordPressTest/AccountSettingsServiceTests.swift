0d947e77815b47e54720031b44c1dc8e1853b6fe
Gio Lodi <gio@mokacoding.com>
2023-06-05 16:24:47 +1000

    Use a test double over HTTP stub in `testUpdateFailure`

--- WordPress/WordPressTest/AccountSettingsServiceTests.swift
+++ WordPress/WordPressTest/AccountSettingsServiceTests.swift
@@ -9,38 +9,15 @@ class AccountSettingsServiceTests: CoreDataTestCase {
 
     override func setUp() {
         let account = WPAccount.fixture(context: mainContext)
-
-        let settings = ManagedAccountSettings(context: mainContext)
-        settings.account = account
-        settings.username = "Username"
-        settings.displayName = "Display Name"
-        settings.primarySiteID = 1
-        settings.aboutMe = "<empty>"
-        settings.email = "test@email.com"
-        settings.firstName = "Test"
-        settings.lastName = "User"
-        settings.language = "en"
-        settings.webAddress = "https://test.wordpress.com"
-
+        _ = makeManagedAccountSettings(context: mainContext, account: account)
         contextManager.saveContextAndWait(mainContext)
+        service = makeService(contextManager: contextManager, account: account)
 
         service = AccountSettingsService(
             userID: account.userID.intValue,
             remote: AccountSettingsRemote(wordPressComRestApi: account.wordPressComRestApi),
             coreDataStack: contextManager
         )
-    }
-
-    private func managedAccountSettings() -> ManagedAccountSettings? {
-        contextManager.performQuery { context in
-            let request = NSFetchRequest<NSFetchRequestResult>(entityName: ManagedAccountSettings.entityName())
-            request.predicate = NSPredicate(format: "account.userID = %d", self.service.userID)
-            request.fetchLimit = 1
-            guard let results = (try? context.fetch(request)) as? [ManagedAccountSettings] else {
-                return nil
-            }
-            return results.first
-        }
     }
 
     func testUpdateSuccess() throws {
@@ -57,15 +34,21 @@ class AccountSettingsServiceTests: CoreDataTestCase {
     }
 
     func testUpdateFailure() throws {
-        stub(condition: isPath("/rest/v1.1/me/settings")) { _ in
-            HTTPStubsResponse(jsonObject: [String: Any](), statusCode: 500, headers: nil)
-        }
+        // We've seen some flakiness in CI on this test, and therefore are using a stub object rather than stubbing the HTTP requests.
+        // Since this approach bypasses the entire network stack, the hope is that it'll result in a more robust test.
+        let service = AccountSettingsService(
+            userID: 1,
+            remote: AccountSettingsRemoteInterfaceStub(updateSettingResult: .failure(TestError())),
+            coreDataStack: contextManager
+        )
+
         waitUntil { done in
-            self.service.saveChange(.firstName("Updated"), finished: { success in
+            service.saveChange(.firstName("Updated"), finished: { success in
                 expect(success).to(beFalse())
                 done()
             })
         }
+
         expect(self.managedAccountSettings()?.firstName).to(equal("Test"))
     }
 
@@ -94,5 +77,46 @@ class AccountSettingsServiceTests: CoreDataTestCase {
         let notCrash = expectation(description: "Not crash")
         notCrash.isInverted = true
         wait(for: [notCrash], timeout: 0.5)
+    }
+}
+
+extension AccountSettingsServiceTests {
+
+    private func makeManagedAccountSettings(
+        context: NSManagedObjectContext,
+        account: WPAccount
+    ) -> ManagedAccountSettings {
+        let settings = ManagedAccountSettings(context: context)
+        settings.account = account
+        settings.username = "Username"
+        settings.displayName = "Display Name"
+        settings.primarySiteID = 1
+        settings.aboutMe = "<empty>"
+        settings.email = "test@email.com"
+        settings.firstName = "Test"
+        settings.lastName = "User"
+        settings.language = "en"
+        settings.webAddress = "https://test.wordpress.com"
+        return settings
+    }
+
+    private func makeService(contextManager: ContextManager, account: WPAccount) -> AccountSettingsService {
+        AccountSettingsService(
+            userID: account.userID.intValue,
+            remote: AccountSettingsRemote(wordPressComRestApi: account.wordPressComRestApi),
+            coreDataStack: contextManager
+        )
+    }
+
+    private func managedAccountSettings() -> ManagedAccountSettings? {
+        contextManager.performQuery { context in
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: ManagedAccountSettings.entityName())
+            request.predicate = NSPredicate(format: "account.userID = %d", self.service.userID)
+            request.fetchLimit = 1
+            guard let results = (try? context.fetch(request)) as? [ManagedAccountSettings] else {
+                return nil
+            }
+            return results.first
+        }
     }
 }
