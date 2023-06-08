9f679e7b9d203b328f8f8865fd98d512577b3638
jos <17252150+jostnes@users.noreply.github.com>
2023-06-08 21:51:35 +0800

    use guard and exit early

--- WordPress/UITestsFoundation/Screens/Login/Unified/PasswordScreen.swift
+++ WordPress/UITestsFoundation/Screens/Login/Unified/PasswordScreen.swift
@@ -48,19 +48,27 @@ public class PasswordScreen: ScreenObject {
         let continueButton = app.buttons["Continue Button"]
         continueButton.tap()
 
-        if continueButton.waitForExistence(timeout: 1) {
-            let timeout: TimeInterval = 60
-            let startTime = Date()
-
-            while continueButton.exists && continueButton.isEnabled == false {
-                if Date().timeIntervalSince(startTime) > timeout {
-                    XCTFail("Continue button still disabled!")
-                }
-
-                RunLoop.current.run(until: Date(timeIntervalSinceNow: 0.1))
-            }
+        guard continueButton.waitForExistence(timeout: 1) else {
+            tapSaveButton()
+            return // Exit the function if the button does not exist
         }
 
+        let timeout: TimeInterval = 60
+        let startTime = Date()
+
+        while continueButton.exists && continueButton.isEnabled == false {
+            if Date().timeIntervalSince(startTime) > timeout {
+                XCTFail("Continue button still disabled!")
+                break // Exit the loop if the timeout is exceeded
+            }
+
+            RunLoop.current.run(until: Date(timeIntervalSinceNow: 0.1))
+        }
+
+        tapSaveButton()
+    }
+
+    private func tapSaveButton() {
         // The Simulator might ask to save the password which, of course, we don't want to do
         if app.buttons["Save Password"].waitForExistence(timeout: 3) {
             // There should be no need to wait for this button to exist since it's part of the same
