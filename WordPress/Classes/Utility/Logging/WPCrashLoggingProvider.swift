119b6d148cf3a2b9b7ed5cb4b8174bac4ed7ffab
Salim Braksa <salim.braksa@gmail.com>
2023-06-19 01:02:06 +0100

    Remove code that was added by accident

--- WordPress/Classes/Utility/Logging/WPCrashLoggingProvider.swift
+++ WordPress/Classes/Utility/Logging/WPCrashLoggingProvider.swift
@@ -62,12 +62,3 @@ struct WPCrashLoggingDataProvider: CrashLoggingDataProvider {
         }
     }
 }
-
-extension NSManagedObjectContext {
-
-    func proxy_save() throws {
-        // 1. Log this method call
-        // 2. Check for concurrency violation, otherwise send an error to Sentry
-        try self.save()
-    }
-}
