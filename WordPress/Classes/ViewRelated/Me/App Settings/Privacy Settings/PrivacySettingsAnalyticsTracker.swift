7087c1a6fdc7f8499e200b4fb6408ead30f84848
Salim Braksa <salim.braksa@gmail.com>
2023-06-22 16:30:26 +0100

    Implement a type to track privacy settings events

---
+++ WordPress/Classes/ViewRelated/Me/App Settings/Privacy Settings/PrivacySettingsAnalyticsTracker.swift
@@ -0,0 +1,26 @@
+import Foundation
+import AutomatticTracks
+
+final class PrivacySettingsAnalyticsTracker {
+
+    // MARK: - API
+
+    func trackPrivacySettingsReportCrashesToggled(enabled: Bool) {
+        let props: Properties = ["enabled": enabled.stringLiteral]
+        self.track(.privacyChoicesBannerSaveButtonTapped, properties: props)
+    }
+
+    func trackPrivacyChoicesBannerSaveButtonTapped(analyticsEnabled: Bool) {
+        let props: Properties = ["analytics_enabled": analyticsEnabled.stringLiteral]
+        self.track(.privacyChoicesBannerSaveButtonTapped, properties: props)
+    }
+
+    func track(_ event: PrivacySettingsAnalytics, properties: Properties = [:]) {
+        let event = AnalyticsEvent(name: event.rawValue, properties: properties)
+        WPAnalytics.track(event)
+    }
+
+    // MARK: - Types
+
+    typealias Properties = [String: String]
+}
