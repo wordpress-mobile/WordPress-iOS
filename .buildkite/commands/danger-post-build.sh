be09de5562a4e61df377902eb0ecec65b141aa6f
Ian Maia <ian.maia@automattic.com>
2023-06-23 21:58:16 +0200

    Reach .xcresult

--- .buildkite/commands/danger-post-build.sh
+++ .buildkite/commands/danger-post-build.sh
@@ -6,6 +6,7 @@ install_gems
 echo "--- 📦 Downloading Build Artifacts"
 download_artifact build-products-wordpress.tar
 tar -xf build-products-wordpress.tar
+ls
 
 echo "--- Running Danger: PR Check"
 bundle exec danger --fail-on-errors=true --dangerfile=.buildkite/danger/Dangerfile-post-build --danger_id=post-build
