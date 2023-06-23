e45a94d9fa15b65ff2cbb149479d95082689649d
Ian Maia <ian.maia@automattic.com>
2023-06-24 01:28:32 +0200

    Show more info on CI logs

--- .buildkite/commands/danger-post-build.sh
+++ .buildkite/commands/danger-post-build.sh
@@ -5,8 +5,8 @@ install_gems
 
 echo "--- 📦 Downloading Build Artifacts"
 download_artifact build-products-wordpress.tar
-tar -xf build-products-wordpress.tar
-ls
+tar -v -xf build-products-wordpress.tar
+ls -la
 
 echo "--- Running Danger: PR Check"
 bundle exec danger --fail-on-errors=true --dangerfile=.buildkite/danger/Dangerfile-post-build --danger_id=post-build
