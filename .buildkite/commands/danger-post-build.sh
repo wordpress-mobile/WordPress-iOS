35ec8dc92c9ef2e8bd1e2d34c2c3d6b27880d7e1
Ian Maia <ian.maia@automattic.com>
2023-06-23 20:12:13 +0200

    Setup for Danger post-build

---
+++ .buildkite/commands/danger-post-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -eu
+
+echo "--- :rubygems: Setting up Gems"
+install_gems
+
+echo "--- 📦 Downloading Build Artifacts"
+download_artifact build-products-wordpress.tar
+tar -xf build-products-wordpress.tar
+
+echo "--- Running Danger: PR Check"
+bundle exec danger --fail-on-errors=true --dangerfile=.buildkite/danger/Dangerfile-post-build --danger_id=post-build
