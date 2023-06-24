a69c568c7ea4e25ee31f888f49f1579633f05d1f
David Christiandy <1299411+dvdchr@users.noreply.github.com>
2023-06-24 21:17:55 +0700

    Add NSManagedObject subclass for PublicizeInfo

---
+++ WordPress/Classes/Models/PublicizeInfo+CoreDataClass.swift
@@ -0,0 +1,16 @@
+import Foundation
+import CoreData
+
+/// `PublicizeInfo` encapsulates the information related to Jetpack Social auto-sharing.
+///
+/// WP.com sites will not have a `PublicizeInfo`, and currently doesn't have auto-sharing limitations.
+/// Furthermore, sites eligible for unlimited sharing will still return a `PublicizeInfo` along with its sharing
+/// limitations, but the numbers should be ignored (at least for now).
+///
+public class PublicizeInfo: NSManagedObject {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<PublicizeInfo> {
+        return NSFetchRequest<PublicizeInfo>(entityName: "PublicizeInfo")
+    }
+
+}
---
+++ WordPress/Classes/Models/PublicizeInfo+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+import Foundation
+import CoreData
+
+extension PublicizeInfo {
+    /// The maximum number of Social shares for the associated `blog`.
+    @NSManaged public var sharingLimit: Int64
+
+    /// The number of Social sharing to be published in the future.
+    @NSManaged public var toBePublicizedCount: Int64
+
+    /// The number of posts that have been auto-shared.
+    @NSManaged public var sharedPostsCount: Int64
+
+    /// The remaining Social shares available for the associated `blog`.
+    @NSManaged public var sharesRemaining: Int64
+
+    /// The associated Blog instance.
+    @NSManaged public var blog: Blog?
+}
