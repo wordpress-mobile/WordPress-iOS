223580577dbcd9e70d876385b6b65bc97f42c7d2
Gio Lodi <gio@mokacoding.com>
2023-06-27 17:39:52 +1000

    Setup tests for `PrivacySettingsAnalyticsTracker`

---
+++ WordPress/Classes/Utility/Analytics/AnalyticsEventTracking.swift
@@ -0,0 +1,12 @@
+import WordPressShared
+
+// Temporary workaround to abstract the WPAnalytics implementation detail.
+//
+// Unfortunately, track(_ event: AnalyticsEvent) is defined as an extension on WPAnalytics which is a concrete class.
+// Until the method is moved to a protocol, i.e. at the WPAnalyticsTracker level, we'll have to wrap it like so.
+protocol AnalyticsEventTracking {
+
+    static func track(_ event: AnalyticsEvent)
+}
+
+extension WPAnalytics: AnalyticsEventTracking {}
