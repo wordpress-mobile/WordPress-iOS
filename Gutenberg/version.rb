b6669d5407d53a635fce7c25a3f6465bae7a5cef
Gio Lodi <gio@mokacoding.com>
2023-06-29 16:00:15 +1000

    Test an XCFramework build with RN 0.71 and the Hermes engine

--- Gutenberg/version.rb
+++ Gutenberg/version.rb
@@ -11,8 +11,12 @@
 #
 #   LOCAL_GUTENBERG=../my-gutenberg-fork bundle exec pod install
 GUTENBERG_CONFIG = {
-  # commit: ''
-  tag: 'v1.98.1'
+  # This is a version of Gutenberg built with RN 0.71
+  #
+  # See:
+  # - https://github.com/wordpress-mobile/gutenberg-mobile/pull/5924
+  commit: 'b275a9d4302aaddf1572cf1d22497a47c7076443'
+  # tag: 'v1.98.1'
 }
 
 GITHUB_ORG = 'wordpress-mobile'
