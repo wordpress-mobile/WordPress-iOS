1f0af8c2eed9ac0af2ef4b63055187af3837dfda
kean <grebenyuk.alexander@gmail.com>
2023-06-30 10:25:39 -0400

    Add negative scenario tests to BlazeCampaignsStreamTests

--- WordPress/WordPressTest/Dashboard/BlazeCampaignsStreamTests.swift
+++ WordPress/WordPressTest/Dashboard/BlazeCampaignsStreamTests.swift
@@ -27,7 +27,7 @@ final class BlazeCampaignsStreamTests: CoreDataTestCase {
         // When loading the first page
         try loadNextPage()
 
-        // Then
+        // Then the first page is loaded
         XCTAssertEqual(service.numberOfRequests, 1)
         XCTAssertEqual(delegate.appendedIndexPaths, [[IndexPath(row: 0, section: 0)]])
         guard delegate.states.count == 2 else {
@@ -80,6 +80,129 @@ final class BlazeCampaignsStreamTests: CoreDataTestCase {
         XCTAssertEqual(service.numberOfRequests, 2)
     }
 
+    func testFirstPageFailedToLoad() throws {
+        // Given
+        service.isFailing = true
+
+        // When loading the first page
+        do {
+            try loadNextPage()
+            XCTFail("Expected the request to fail")
+        } catch {
+            XCTAssertEqual((error as? URLError)?.code, .notConnectedToInternet)
+        }
+
+        // Then
+        XCTAssertEqual(service.numberOfRequests, 1)
+        XCTAssertEqual(delegate.appendedIndexPaths, [])
+        guard delegate.states.count == 2 else {
+            return XCTFail("Unexpected state updates recorded: \(delegate.states)")
+        }
+        do {
+            let state = delegate.states[0]
+            XCTAssertEqual(state.campaignIDs, [])
+            XCTAssertTrue(state.isLoading)
+            XCTAssertNil(state.error)
+        }
+        do {
+            let state = delegate.states[1]
+            XCTAssertEqual(state.campaignIDs, [])
+            XCTAssertFalse(state.isLoading)
+            let error = try XCTUnwrap(state.error)
+            XCTAssertEqual((error as? URLError)?.code, .notConnectedToInternet)
+        }
+
+        // When connection restored
+        service.isFailing = false
+
+        // When
+        delegate.reset()
+        try loadNextPage()
+
+        // Then the first page is loaded
+        XCTAssertEqual(service.numberOfRequests, 2)
+        XCTAssertEqual(delegate.appendedIndexPaths, [[IndexPath(row: 0, section: 0)]])
+        guard delegate.states.count == 2 else {
+            return XCTFail("Unexpected state updates recorded: \(delegate.states)")
+        }
+        do {
+            let state = delegate.states[0]
+            XCTAssertEqual(state.campaignIDs, [])
+            XCTAssertTrue(state.isLoading)
+            XCTAssertNil(state.error)
+        }
+        do {
+            let state = delegate.states[1]
+            XCTAssertEqual(state.campaignIDs, [1])
+            XCTAssertFalse(state.isLoading)
+            XCTAssertNil(state.error)
+        }
+    }
+
+    func testSecondPageFailedToLoad() throws {
+        // Given first page already loaded
+        try loadNextPage()
+
+        // When connection fails
+        service.isFailing = true
+
+        // When
+        delegate.reset()
+        do {
+            try loadNextPage()
+            XCTFail("Expected the request to fail")
+        } catch {
+            XCTAssertEqual((error as? URLError)?.code, .notConnectedToInternet)
+        }
+
+        // Then the second page fails to load
+        XCTAssertEqual(service.numberOfRequests, 2)
+        XCTAssertEqual(delegate.appendedIndexPaths, [])
+        guard delegate.states.count == 2 else {
+            return XCTFail("Unexpected state updates recorded: \(delegate.states)")
+        }
+        do {
+            let state = delegate.states[0]
+            XCTAssertEqual(state.campaignIDs, [1])
+            XCTAssertTrue(state.isLoading)
+            XCTAssertNil(state.error)
+        }
+        do {
+            let state = delegate.states[1]
+            XCTAssertEqual(state.campaignIDs, [1])
+            XCTAssertFalse(state.isLoading)
+            let error = try XCTUnwrap(state.error)
+            XCTAssertEqual((error as? URLError)?.code, .notConnectedToInternet)
+        }
+
+        // When connection is restored
+        service.isFailing = false
+
+        // When loading the second page
+        delegate.reset()
+        try loadNextPage()
+
+        // Then the second page is loaded
+        XCTAssertEqual(service.numberOfRequests, 3)
+        XCTAssertEqual(delegate.appendedIndexPaths, [[IndexPath(row: 1, section: 0)]])
+        guard delegate.states.count == 2 else {
+            return XCTFail("Unexpected state updates recorded: \(delegate.states)")
+        }
+        do {
+            let state = delegate.states[0]
+            XCTAssertEqual(state.campaignIDs, [1])
+            XCTAssertTrue(state.isLoading)
+            XCTAssertNil(state.error)
+        }
+        do {
+            let state = delegate.states[1]
+            // Then the duplicated campaign with ID "1" is skipped
+            XCTAssertEqual(state.campaignIDs, [1, 2])
+            XCTAssertFalse(state.isLoading)
+            XCTAssertNil(state.error)
+        }
+    }
+
     @discardableResult
     private func loadNextPage() throws -> BlazeCampaignsSearchResponse {
         let expectation = self.expectation(description: "didLoadNextPage")
@@ -120,12 +243,17 @@ private struct RecordedBlazeCampaignsStreamState {
 
 private final class MockBlazePaginatedService: BlazeServiceProtocol {
     var numberOfRequests = 0
+    var isFailing = false
 
     func getRecentCampaigns(for blog: Blog, page: Int, completion: @escaping (Result<BlazeCampaignsSearchResponse, Error>) -> Void) {
         XCTAssertEqual(blog.dotComID, 1)
         XCTAssertTrue([1, 2].contains(page))
 
         numberOfRequests += 1
+
+        guard !isFailing else {
+            return completion(.failure(URLError(.notConnectedToInternet)))
+        }
 
         do {
             let data = try Bundle.test.json(named: "blaze-search-page-\(page)")
