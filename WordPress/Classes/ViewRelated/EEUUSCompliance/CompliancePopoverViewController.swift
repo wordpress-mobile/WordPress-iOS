4b9cbb27c173f48b70841fd035a0d8bdf70c1aae
alpavanoglu <alp.avanoglu@automattic.com>
2023-06-30 17:49:27 +0200

    Add popover closure assignments to VC

--- WordPress/Classes/ViewRelated/EEUUSCompliance/CompliancePopoverViewController.swift
+++ WordPress/Classes/ViewRelated/EEUUSCompliance/CompliancePopoverViewController.swift
@@ -2,55 +2,11 @@ import UIKit
 import SwiftUI
 import WordPressUI
 
-//final class CompliancePopoverViewController: UIHostingController<CompliancePopover> {
-//
-//    private let viewModel: CompliancePopoverViewModelProtocol
-//
-//    /// Tracks the banner view intrinsic height.
-//    /// Needed to enable it's scrolling when it grows bigger than the screen.
-//    ///
-//    var bannerIntrinsicHeight: CGFloat = 0
-//
-//    init(viewModel: CompliancePopoverViewModelProtocol) {
-//        self.viewModel = viewModel
-//        super.init(rootView: CompliancePopover())
-//    }
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        rootView.goToSettingsAction = { [weak self] in
-//            self?.viewModel.didTapSettings()
-//        }
-//
-//        rootView.saveAction = { [weak self] in
-//            self?.viewModel.didTapSave()
-//        }
-//    }
-//
-//    /// Needed for protocol conformance.
-//    ///
-//    required dynamic init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//
-////    override func viewDidLayoutSubviews() {
-////        super.viewDidLayoutSubviews()
-////
-////        print("Kill me right now")
-////        // Make the banner scrollable when the banner height is bigger than the screen height.
-////        // Send it in the next run loop to avoid a recursive `viewDidLayoutSubviews`.
-////        bannerIntrinsicHeight = view.intrinsicContentSize.height
-////        DispatchQueue.main.async {
-////            self.rootView.shouldScroll = self.bannerIntrinsicHeight > self.view.frame.height
-////        }
-////    }
-//}
-
 final class CompliancePopoverViewController: UIViewController {
 
     // MARK: - Views
-    private let hostingController: UIViewController = {
-        let controller = UIHostingController(rootView: CompliancePopover())
+    private lazy var hostingController: UIHostingController = {
+        let controller = UIHostingController(rootView: CompliancePopover(viewModel: self.viewModel))
         controller.view.translatesAutoresizingMaskIntoConstraints = true
         return controller
     }()
@@ -59,9 +15,9 @@ final class CompliancePopoverViewController: UIViewController {
         return hostingController.view
     }
 
-    private let viewModel: CompliancePopoverViewModelProtocol
+    private let viewModel: CompliancePopoverViewModel
 
-    init(viewModel: CompliancePopoverViewModelProtocol) {
+    init(viewModel: CompliancePopoverViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
     }
@@ -74,6 +30,12 @@ final class CompliancePopoverViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.addContentView()
+        hostingController.rootView.goToSettingsAction = {
+            self.viewModel.didTapSettings()
+        }
+        hostingController.rootView.saveAction = {
+            self.viewModel.didTapSave()
+        }
     }
 
     override func viewDidLayoutSubviews() {
@@ -102,7 +64,6 @@ extension CompliancePopoverViewController: DrawerPresentable {
     }
 
     var allowsUserTransition: Bool {
-        print("89fas9(*")
         return false
     }
 
@@ -111,7 +72,6 @@ extension CompliancePopoverViewController: DrawerPresentable {
     }
 
     var allowsTapToDismiss: Bool {
-        print("AD*SJ*(")
         return false
     }
 }
