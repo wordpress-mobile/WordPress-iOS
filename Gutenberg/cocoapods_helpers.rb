d03be586024c9e5003a03a046951e7c948251adc
Gio Lodi <gio@mokacoding.com>
2023-07-04 09:31:13 +1000

    Add calls to RN hooks when using Gutenberg from local path

--- Gutenberg/cocoapods_helpers.rb
+++ Gutenberg/cocoapods_helpers.rb
@@ -43,6 +43,10 @@ def gutenberg_pod(config: GUTENBERG_CONFIG)
 
     puts "[Gutenberg] Installing pods using local Gutenberg version from #{local_gutenberg_path}"
 
+    react_native_path = require_react_native_helpers!(gutenberg_path: local_gutenberg_path)
+
+    use_react_native! path: react_native_path
+
     pod 'Gutenberg', options
     pod 'RNTAztecView', options
 
@@ -68,12 +72,30 @@ def gutenberg_dependencies(options:)
   podspec_prefix += '/third-party-podspecs'
   podspec_extension = 'podspec.json'
 
-  # FBReactNativeSpec needs special treatment because of react-native-codegen code generation
-  pod 'FBReactNativeSpec', podspec: "#{podspec_prefix}/FBReactNativeSpec/FBReactNativeSpec.#{podspec_extension}"
+  computed_dependencies = DEPENDENCIES.dup
 
-  DEPENDENCIES.each do |pod_name|
+  # Use a custom RNReanimated version while we coordinate a fix upstream
+  computed_dependencies.delete('RNReanimated')
+  pod 'RNReanimated', git: 'https://github.com/wordpress-mobile/react-native-reanimated', branch: 'mokagio/fix-custom-node_modules-bypass-multiple-versions-check-2.17.0'
+
+  computed_dependencies.each do |pod_name|
     pod pod_name, podspec: "#{podspec_prefix}/#{pod_name}.#{podspec_extension}"
   end
+end
+
+def gutenberg_pre_install
+  return unless should_use_local_gutenberg
+
+  raise "[Gutenberg] Could not find local Gutenberg at given path #{local_gutenberg_path}" unless File.exist?(local_gutenberg_path)
+
+  # This is required to workaround an issue with RNReanimated after upgrading to version 2.17.0
+  rn_node_modules = File.join(local_gutenberg_path, 'node_modules')
+
+  raise "Could not find node modules at given path #{rn_node_modules}" unless File.exist? rn_node_modules
+
+  ENV['REACT_NATIVE_NODE_MODULES_DIR'] = rn_node_modules
+
+  puts "[Gutenberg] Set REACT_NATIVE_NODE_MODULES_DIR env var for RNReanimated to #{rn_node_modules}"
 end
 
 def gutenberg_post_install(installer:)
@@ -81,13 +103,19 @@ def gutenberg_post_install(installer:)
 
   raise "[Gutenberg] Could not find local Gutenberg at given path #{local_gutenberg_path}" unless File.exist?(local_gutenberg_path)
 
-  react_native_path = File.join(local_gutenberg_path, 'gutenberg', 'node_modules', 'react-native')
+  react_native_path = require_react_native_helpers!(gutenberg_path: local_gutenberg_path)
 
-  raise "[Gutenberg] Could not find React Native at given path #{react_native_path}" unless File.exist?(react_native_path)
+  puts "[Gutenberg] Running Gunberg post install hook (RN path: #{react_native_path})"
 
-  require_relative File.join(react_native_path, 'scripts', 'react_native_pods')
-
-  puts '[Gutenberg] Running Gunberg post install hook'
+  # It seems like React Native prepends $PWD to the path internally in the post install hook.
+  # When using an absolute path, we get this error, notice the duplicated "/Users/gio/Developer/a8c/wpios":
+  #
+  #   [!] An error occurred while processing the post-install hook of the Podfile.
+  #
+  #   No such file or directory @ rb_sysopen - /Users/gio/Developer/a8c/wpios/Users/gio/Developer/a8c/wpios/Gutenberg/../../gutenberg-mobile/gutenberg/node_modules/react-native/package.json
+  #
+  # To workaround, we make sure the path is relative to Dir.pwd
+  react_native_path = Pathname.new(react_native_path).relative_path_from(Dir.pwd)
 
   react_native_post_install(installer, react_native_path)
 end
@@ -110,4 +138,20 @@ def local_gutenberg_path
   return local_gutenberg if File.exist?(local_gutenberg)
 
   DEFAULT_GUTENBERG_LOCATION
+end
+
+def require_react_native_helpers!(gutenberg_path:)
+  react_native_path = react_native_path!(gutenberg_path: gutenberg_path)
+
+  require_relative File.join(react_native_path, 'scripts', 'react_native_pods')
+
+  react_native_path
+end
+
+def react_native_path!(gutenberg_path:)
+  react_native_path = File.join(gutenberg_path, 'gutenberg', 'node_modules', 'react-native')
+
+  raise "[Gutenberg] Could not find React Native at given path #{react_native_path}" unless File.exist?(react_native_path)
+
+  react_native_path
 end
