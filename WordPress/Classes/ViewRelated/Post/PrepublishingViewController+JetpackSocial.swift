66be8921cff24041a791bbf66ca7fd376e149046
David Christiandy <1299411+dvdchr@users.noreply.github.com>
2023-07-09 23:41:32 +0700

    Add configuration methods for social cell in prepublishing VC

---
+++ WordPress/Classes/ViewRelated/Post/PrepublishingViewController+JetpackSocial.swift
@@ -0,0 +1,23 @@
+extension PrepublishingViewController {
+
+    /// Determines whether the account and the post's blog is eligible to see auto-sharing options.
+    var isEligibleForAutoSharing: Bool {
+        let postObjectID = post.objectID
+        let blogSupportsPublicize = coreDataStack.performQuery { context in
+            let post = (try? context.existingObject(with: postObjectID)) as? Post
+            return post?.blog.supportsPublicize() ?? false
+        }
+
+        return blogSupportsPublicize && FeatureFlag.jetpackSocial.enabled
+    }
+
+    func configureSocialCell(_ cell: UITableViewCell) {
+        // TODO:
+        // - Show the PrepublishingAutoSharingView.
+        // - Show the NoConnectionView if user has 0 connections.
+        // - Properly configure the view models.
+        let autoSharingView = UIView()
+        cell.contentView.addSubview(autoSharingView)
+        cell.pinSubviewToAllEdges(autoSharingView)
+    }
+}
