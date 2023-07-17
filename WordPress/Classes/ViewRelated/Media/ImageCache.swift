fcbc6a5ca91e8167a871ddfd8f267e822e29cc02
kean <grebenyuk.alexander@gmail.com>
2023-07-17 15:53:27 -0400

    Register shared ImageCache with WordPressUI extensions

--- WordPress/Classes/ViewRelated/Media/ImageCache.swift
+++ WordPress/Classes/ViewRelated/Media/ImageCache.swift
@@ -1,19 +1,36 @@
 import Foundation
 import AlamofireImage
+import WordPressUI
 
 final class ImageCache {
     /// A shared image cache used by the entire system.
-    ///
-    /// - warning: It's critical that there is only one cache instance to make
-    /// sure it can manage the RAM usage efficiently.
     static let shared = ImageCache()
 
-    private let cache = AutoPurgingImageCache(
+    fileprivate let cache = AutoPurgingImageCache(
         memoryCapacity: 128_000_000, // 128 MB
         preferredMemoryUsageAfterPurge: 64_000_000 // 64 MB
     )
 
     private init() {
 
+    }
+}
+
+extension ImageCache {
+    /// Registers the cache with all the image loading systems used by the app.
+    func register() {
+        WordPressUI.ImageCache.shared = WordpressUICacheAdapter(cache: .shared)
+    }
+}
+
+private struct WordpressUICacheAdapter: WordPressUI.ImageCaching {
+    let cache: ImageCache
+
+    func setImage(_ image: UIImage, forKey key: String) {
+        cache.cache.add(image, withIdentifier: key)
+    }
+
+    func getImage(forKey key: String) -> UIImage? {
+        cache.cache.image(withIdentifier: key)
     }
 }
