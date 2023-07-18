3cafbbb6659914ede13512c4ac889589488879ee
Gio Lodi <gio@mokacoding.com>
2023-07-18 20:48:53 +1000

    Use Hermes version via CocoaPods

--- Gutenberg/version.rb
+++ Gutenberg/version.rb
@@ -11,8 +11,9 @@
 #
 #   LOCAL_GUTENBERG=../my-gutenberg-fork bundle exec pod install
 GUTENBERG_CONFIG = {
-  # commit: ''
-  tag: 'v1.100.0-alpha1'
+  # See https://github.com/wordpress-mobile/gutenberg-mobile/pull/5973
+  commit: 'a5f1d5f78efb60a212eb30322623f7583d3cd5f8'
+  # tag: 'v1.100.0-alpha1'
 }
 
 GITHUB_ORG = 'wordpress-mobile'
