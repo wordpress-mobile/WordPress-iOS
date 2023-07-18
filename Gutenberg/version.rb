b05ef6ef1a2cbc977dae63878a2039adc498cd89
Gio Lodi <gio@mokacoding.com>
2023-07-18 21:28:18 +1000

    TEST – Switch to Gutenberg version with nested frameworks

--- Gutenberg/version.rb
+++ Gutenberg/version.rb
@@ -11,8 +11,8 @@
 #
 #   LOCAL_GUTENBERG=../my-gutenberg-fork bundle exec pod install
 GUTENBERG_CONFIG = {
-  # commit: ''
-  tag: 'v1.98.1'
+  commit: 'd04ce178518556f90634c329dd33a49a18e06236'
+  # tag: 'v1.98.1'
 }
 
 GITHUB_ORG = 'wordpress-mobile'
