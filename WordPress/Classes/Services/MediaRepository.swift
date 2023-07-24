2568eaad50dda0c534a75b72ee884668490d2e41
Tony Li <tony.li@automattic.com>
2023-07-24 18:08:34 +1200

    Re-implement getMedia in MediaRepository

---
+++ WordPress/Classes/Services/MediaRepository.swift
@@ -0,0 +1,62 @@
+import Foundation
+
+final class MediaRepository {
+
+    enum Error: Swift.Error {
+        case mediaNotFound
+        case remoteAPIUnavailable
+        case unknown
+    }
+
+    private let coreDataStack: CoreDataStackSwift
+    private let remoteFactory: MediaServiceRemoteFactory
+
+    init(coreDataStack: CoreDataStackSwift, remoteFactory: MediaServiceRemoteFactory = .init()) {
+        self.coreDataStack = coreDataStack
+        self.remoteFactory = remoteFactory
+    }
+
+    /// Get the Media object from the server using the blog and the mediaID as the identifier of the resource
+    func getMedia(withID mediaID: NSNumber, in blogID: CoreDataObjectIdentifier<Blog>) async throws -> CoreDataObjectIdentifier<Media> {
+        let remote = try await coreDataStack.performQuery { [remoteFactory] context in
+            let blog = try blogID.existingObject(in: context)
+            return remoteFactory.remote(for: blog)
+        }
+        guard let remote else {
+            throw MediaRepository.Error.remoteAPIUnavailable
+        }
+
+        let remoteMedia: RemoteMedia? = try await withCheckedThrowingContinuation { continuation in
+            remote.getMediaWithID(
+                mediaID, success: continuation.resume(returning:),
+                failure: { continuation.resume(throwing: $0 ?? MediaRepository.Error.unknown) })
+        }
+        guard let remoteMedia else {
+            throw MediaRepository.Error.mediaNotFound
+        }
+
+        return try await coreDataStack.performAndSave { context in
+            let blog = try blogID.existingObject(in: context)
+            let media = Media.existingMediaWith(mediaID: mediaID, inBlog: blog) ?? Media.makeMedia(blog: blog)
+            MediaHelper.update(media: media, with: remoteMedia)
+            return try .ofUnsaved(media)
+        }
+    }
+
+}
+
+@objc class MediaServiceRemoteFactory: NSObject {
+
+    @objc(remoteForBlog:)
+    func remote(for blog: Blog) -> MediaServiceRemote? {
+        if blog.supports(.wpComRESTAPI), let dotComID = blog.dotComID, let api = blog.wordPressComRestApi() {
+            return MediaServiceRemoteREST(wordPressComRestApi: api, siteID: dotComID)
+        }
+
+        if let username = blog.username, let password = blog.password, let api = blog.xmlrpcApi {
+            return MediaServiceRemoteXMLRPC(api: api, username: username, password: password)
+        }
+
+        return nil
+    }
+}
