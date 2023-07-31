a81d9c2933e85690b064e605fa6b23c0b7a09481
Derek Blank <derekpblank@gmail.com>
2023-07-31 12:36:55 +1000

    Update Gutenberg ref

--- Gutenberg/version.rb
+++ Gutenberg/version.rb
@@ -11,8 +11,8 @@
 #
 #   LOCAL_GUTENBERG=../my-gutenberg-fork bundle exec pod install
 GUTENBERG_CONFIG = {
-  # commit: ''
-  tag: 'v1.101.0-alpha1'
+  commit: '221042b70ec113be688e41f8159b3223fd690cd5'
+  # tag: 'v1.101.0-alpha1'
 }
 
 GITHUB_ORG = 'wordpress-mobile'
