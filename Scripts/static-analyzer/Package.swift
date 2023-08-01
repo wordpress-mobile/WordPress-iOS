feeb6bd049dc96da1b9c03a976a9bfcb16acff17
Tony Li <tony.li@automattic.com>
2023-08-01 14:15:04 +1200

    Dynamically lookup libIndexStore location

--- Scripts/static-analyzer/Package.swift
+++ Scripts/static-analyzer/Package.swift
@@ -14,8 +14,9 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
         .package(url: "https://github.com/apple/swift-syntax.git", exact: "508.0.0"),
-        .package(url: "https://github.com/apple/indexstore-db", branch: "release/5.8"),
+        .package(url: "https://github.com/apple/indexstore-db", revision: "swift-5.8-RELEASE"),
         .package(url: "https://github.com/jpsim/SourceKitten.git", .upToNextMinor(from: "0.34.1")),
+        .package(url: "https://github.com/apple/swift-tools-support-core.git", from: "0.5.2"),
     ],
     targets: [
         .executableTarget(
@@ -26,6 +27,7 @@ let package = Package(
                 .product(name: "SwiftSyntax", package: "swift-syntax"),
                 .product(name: "IndexStoreDB", package: "indexstore-db"),
                 .product(name: "SourceKittenFramework", package: "SourceKitten"),
+                .product(name: "TSCBasic", package: "swift-tools-support-core"),
             ],
             swiftSettings: [
                 .enableUpcomingFeature("BareSlashRegexLiterals")
