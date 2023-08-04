1cf0e1b4b86827553a7f6401df8e8b21803781fc
Carlos Garcia <fluiddot@gmail.com>
2023-08-04 15:06:17 +0200

    [TEST] Update Gutenberg Mobile ref

--- Gutenberg/version.rb
+++ Gutenberg/version.rb
@@ -11,7 +11,7 @@
 #
 #   LOCAL_GUTENBERG=../my-gutenberg-fork bundle exec pod install
 GUTENBERG_CONFIG = {
-  commit: '5300fc29c154328a9a0d57f5fd1a282de1784599'
+  commit: 'f09c83278f879ad6f28d7423b6e5891838f932d4'
   # tag: 'v1.101.0-alpha1'
 }
 
