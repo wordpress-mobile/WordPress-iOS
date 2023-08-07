f60fafa8f7d87f37e61b8bfd4f44b31b801b86be
Povilas Staskus <povilas.staskus@automattic.com>
2023-08-07 16:53:48 +0300

    Create boilerplate for loading support chat bot
    - Added example HTML and JS from DocsBot.ai
    - Loading files in a web view
    - Injecting chat id

---
+++ WordPress/Classes/ViewRelated/Support/SupportChatBot/SupportChatBotViewController.swift
@@ -0,0 +1,40 @@
+import WebKit
+
+final class SupportChatBotViewController: WebKitViewController {
+    private let viewModel: SupportChartBotViewModel
+
+    init(viewModel: SupportChartBotViewModel) {
+        self.viewModel = viewModel
+        let configuration = WebViewControllerConfiguration(url: viewModel.url)
+        configuration.secureInteraction = true
+        super.init(configuration: configuration)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        webView.evaluateJavaScript(createDocsBotInitCode(), completionHandler: { (result, error) in
+            if let error = error {
+                print("Failed to initialize docs bot code: \(error)")
+            }
+        })
+    }
+
+    /// Creating DocsBotAI JavaScript code so we could tweak configuration from within Swift code
+    /// https://docsbot.ai/docs/embeddable-chat-widget
+    private func createDocsBotInitCode() -> String {
+        """
+        (function() {
+            DocsBotAI.init({
+                id: '\(viewModel.id)',
+                options: {
+                    horizontalMargin: 40,
+                    verticalMargin: 60,
+                },
+            })
+        })();
+        """
+    }
+}
---
+++ WordPress/Classes/ViewRelated/Support/SupportChatBot/SupportChatBotViewModel.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+struct SupportChartBotViewModel {
+    let id = "" // TODO: Update ApiCredentials
+    let url = Bundle.main.url(forResource: "support_chat_widget", withExtension: "html")
+}
