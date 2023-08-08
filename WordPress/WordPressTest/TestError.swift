32821640abe2d73a71aeac1ea3cad78e8bea32a4
Gio Lodi <gio@mokacoding.com>
2023-08-08 12:13:13 +1000

    Add global free function `testError()` to simplify errors in tests

--- WordPress/WordPressTest/TestError.swift
+++ WordPress/WordPressTest/TestError.swift
@@ -47,3 +47,10 @@ extension NSError {
         )
     }
 }
+
+/// Global free function only available for the unit test target to make creating general purpose errors in the test DRY and self-documenting.
+///
+/// The Swift API guidelines reccomend against free functions, but the improved ergonomics and limited scope make the approach worth the trade off in this case.
+func testError(id: Int = 0, description: String = "A test error") -> Error {
+    NSError.testInstance(description: description, code: id)
+}
