c6d30b64d455c2f2afaee99c2e5740ba8ccaaa63
Povilas Staskus <povilas.staskus@automattic.com>
2023-08-09 17:11:37 +0300

    Present SupportChatBotViewController inside a plain WKWebView
    WebKitViewController carries functionality which we do not require and adds a lot of overrides that restricts development of SupportChatBotViewController

--- WordPress/Classes/ViewRelated/Support/SupportChatBot/SupportChatBotViewController.swift
+++ WordPress/Classes/ViewRelated/Support/SupportChatBot/SupportChatBotViewController.swift
@@ -1,25 +1,35 @@
 import WebKit
 
-final class SupportChatBotViewController: WebKitViewController {
+final class SupportChatBotViewController: UIViewController {
     private let viewModel: SupportChatBotViewModel
+    private lazy var webView = WKWebView()
 
     init(viewModel: SupportChatBotViewModel) {
         self.viewModel = viewModel
-        let configuration = WebViewControllerConfiguration(url: viewModel.url)
-        configuration.secureInteraction = true
-        super.init(configuration: configuration)
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    override func loadView() {
+        webView.navigationDelegate = self
+        self.view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = Strings.title
+        loadChatBot()
+    }
+
+    private func loadChatBot() {
+        if let url = viewModel.url {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        webView.evaluateJavaScript(createDocsBotInitCode(), completionHandler: { (result, error) in
-            if let error = error {
-                DDLogError("Failed to initialize docs bot code: \(error)")
-            }
-        })
     }
 
     /// Creating DocsBotAI JavaScript code so we could tweak configuration from within Swift code
@@ -34,7 +44,26 @@ final class SupportChatBotViewController: WebKitViewController {
                     verticalMargin: 60,
                 },
             })
+            setTimeout(() => {
+                DocsBotAI.open()
+            }, 200);
         })();
         """
+    }
+}
+
+extension SupportChatBotViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        webView.evaluateJavaScript(createDocsBotInitCode(), completionHandler: { (result, error) in
+            if let error = error {
+                DDLogError("Failed to initialize docs bot code: \(error)")
+            }
+        })
+    }
+}
+
+extension SupportChatBotViewController {
+    private enum Strings {
+        static let title = NSLocalizedString("support.chatBot.title", value: "Contact Support", comment: "Title of the view that shows support chat bot.")
     }
 }
