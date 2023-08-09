7b62c14537118de26c808299e71f51992369b21c
Povilas Staskus <povilas.staskus@automattic.com>
2023-08-09 16:41:41 +0300

    Run js code through prettier

--- WordPress/Classes/ViewRelated/Support/SupportChatBot/support_chat_widget.js
+++ WordPress/Classes/ViewRelated/Support/SupportChatBot/support_chat_widget.js
@@ -1,31 +1,35 @@
-;(window.DocsBotAI = window.DocsBotAI || {}),
-    (DocsBotAI.init = function (c) {
-      return new Promise(function (e, o) {
-        var t = document.createElement('script')
-        ;(t.type = 'text/javascript'), (t.async = !0), (t.src = 'https://widget.docsbot.ai/chat.js')
-        var n = document.getElementsByTagName('script')[0]
-        n.parentNode.insertBefore(t, n),
-          t.addEventListener('load', function () {
-            window.DocsBotAI.mount({
-              id: c.id,
-              supportCallback: c.supportCallback,
-              identify: c.identify,
-              options: c.options,
-            })
-            var t
-            ;(t = function (n) {
-              return new Promise(function (e) {
-                if (document.querySelector(n)) return e(document.querySelector(n))
-                var o = new MutationObserver(function (t) {
-                  document.querySelector(n) && (e(document.querySelector(n)), o.disconnect())
-                })
-                o.observe(document.body, { childList: !0, subtree: !0 })
-              })
-            }),
-              t && t('#docsbotai-root').then(e).catch(o)
-          }),
-          t.addEventListener('error', function (t) {
-            o(t.message)
-          })
-      })
-    })
+ (window.DocsBotAI = window.DocsBotAI || {}),
+   (DocsBotAI.init = function (c) {
+     return new Promise(function (e, o) {
+       var t = document.createElement("script");
+       (t.type = "text/javascript"),
+         (t.async = !0),
+         (t.src = "https://widget.docsbot.ai/chat.js");
+       var n = document.getElementsByTagName("script")[0];
+       n.parentNode.insertBefore(t, n),
+         t.addEventListener("load", function () {
+           window.DocsBotAI.mount({
+             id: c.id,
+             supportCallback: c.supportCallback,
+             identify: c.identify,
+             options: c.options,
+           });
+           var t;
+           (t = function (n) {
+             return new Promise(function (e) {
+               if (document.querySelector(n))
+                 return e(document.querySelector(n));
+               var o = new MutationObserver(function (t) {
+                 document.querySelector(n) &&
+                   (e(document.querySelector(n)), o.disconnect());
+               });
+               o.observe(document.body, { childList: !0, subtree: !0 });
+             });
+           }),
+             t && t("#docsbotai-root").then(e).catch(o);
+         }),
+         t.addEventListener("error", function (t) {
+           o(t.message);
+         });
+     });
+   });
