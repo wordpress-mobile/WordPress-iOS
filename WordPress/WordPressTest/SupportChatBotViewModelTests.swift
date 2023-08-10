68f43455924518416010248ac54088ddc60ff187
Paul Von Schrottky <paul.von.schrottky@automattic.com>
2023-08-10 16:53:27 -0400

    Fix compilation error in unit tests

--- WordPress/WordPressTest/SupportChatBotViewModelTests.swift
+++ WordPress/WordPressTest/SupportChatBotViewModelTests.swift
@@ -6,13 +6,13 @@ final class SupportChatBotViewModelTests: XCTestCase {
     private var zendeskUtils: ZendeskUtilsSpy!
 
     override func setUpWithError() throws {
-        super.setUpWithError()
+        try super.setUpWithError()
         zendeskUtils = ZendeskUtilsSpy()
         sut = SupportChatBotViewModel(zendeskUtils: zendeskUtils)
     }
 
     override func tearDownWithError() throws {
-        super.tearDownWithError()
+        try super.tearDownWithError()
         zendeskUtils = nil
         sut = nil
     }
