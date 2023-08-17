42c9b0b4e46883933af938b69c71540889fc272b
Povilas Staskus <povilas.staskus@automattic.com>
2023-08-17 14:41:18 +0300

    Create SupportCoordinator to reuse support views presentation
    - Allow SupportCoordinator to work within UINavigationController and UIViewController
    - Allow SupportChatBotViewController to be presented modally and pushed into navigation controller

---
+++ WordPress/Classes/ViewRelated/Support/SupportCoordinator.swift
@@ -0,0 +1,65 @@
+import Foundation
+import WordPressAuthenticator
+
+final class SupportCoordinator {
+    private weak var controllerToShowFrom: UIViewController?
+    private var tag: WordPressSupportSourceTag?
+
+    private var navigationController: UINavigationController? {
+        guard let navigationController = (controllerToShowFrom as? UINavigationController) ?? controllerToShowFrom?.navigationController else {
+            return nil
+        }
+
+        return navigationController
+    }
+
+    init(controllerToShowFrom: UIViewController?,
+         tag: WordPressSupportSourceTag? = nil) {
+        self.controllerToShowFrom = controllerToShowFrom
+        self.tag = tag
+    }
+
+    func showSupport(onIdentityUpdated: (() -> ())? = nil) {
+        guard let controllerToShowFrom = controllerToShowFrom else { return }
+
+        if RemoteFeatureFlag.contactSupport.enabled() {
+            let chatBotViewController = SupportChatBotViewController(viewModel: .init(), delegate: self)
+            if let navigationController = navigationController {
+                navigationController.pushViewController(chatBotViewController, animated: true)
+            } else {
+                let navigationController = UINavigationController(rootViewController: chatBotViewController)
+                controllerToShowFrom.present(navigationController, animated: true)
+            }
+        } else {
+            ZendeskUtils.sharedInstance.showNewRequestIfPossible(from: controllerToShowFrom, with: tag) { identityUpdated in
+                if identityUpdated {
+                    onIdentityUpdated?()
+                }
+            }
+        }
+    }
+
+    func showTicketView(onIdentityUpdated: (() -> ())? = nil) {
+        guard let controllerToShowFrom = controllerToShowFrom else { return }
+
+        ZendeskUtils.pushNotificationRead()
+        ZendeskUtils.sharedInstance.showTicketListIfPossible(from: controllerToShowFrom, with: tag) { identityUpdated in
+            if identityUpdated {
+                onIdentityUpdated?()
+            }
+        }
+    }
+}
+
+extension SupportCoordinator: SupportChatBotCreatedTicketDelegate {
+    func onTicketCreated() {
+        if let navigationController = navigationController {
+            navigationController.popViewController(animated: true)
+            showTicketView()
+        } else {
+            controllerToShowFrom?.dismiss(animated: true) { [weak self] in
+                self?.showTicketView()
+            }
+        }
+    }
+}
