965b9efc60b3bdac12428556e5666f681daa2f8f
kean <grebenyuk.alexander@gmail.com>
2023-08-23 16:19:33 -0400

    Move PHPickerResult.loadImage

--- WordPress/Classes/ViewRelated/Media/MediaPickerMenu.swift
+++ WordPress/Classes/ViewRelated/Media/MediaPickerMenu.swift
@@ -37,7 +37,7 @@ struct MediaPickerMenu {
 extension MediaPickerMenu {
     /// Returns an action for picking photos from the device's Photos library.
     ///
-    /// - note: Use `MediaPicker.loadImage(for:)` to retrieve an image from the result.
+    /// - note: Use `PHPickerResult.loadImage(for:)` to retrieve an image from the result.
     func makePhotosAction(delegate: PHPickerViewControllerDelegate) -> UIAction {
         UIAction(
             title: Strings.pickFromPhotosLibrary,
@@ -64,45 +64,6 @@ extension MediaPickerMenu {
         let picker = PHPickerViewController(configuration: configuration)
         picker.delegate = delegate
         presentingViewController?.present(picker, animated: true)
-    }
-
-    /// Retrieves an image for the given picker result.
-    ///
-    /// - parameter completion: The completion closure that gets called on the main thread.
-    static func loadImage(for result: PHPickerResult, _ completion: @escaping (UIImage?, Error?) -> Void) {
-        let provider = result.itemProvider
-        if provider.canLoadObject(ofClass: UIImage.self) {
-            provider.loadObject(ofClass: UIImage.self) { value, error in
-                DispatchQueue.main.async {
-                    if let image = value as? UIImage {
-                        completion(image, nil)
-                    } else {
-                        DDLogError("Failed to load image for provider with registered types \(provider.registeredTypeIdentifiers) with error \(String(describing: error))")
-
-                        completion(nil, error)
-                    }
-                }
-            }
-        } else if provider.hasItemConformingToTypeIdentifier(UTType.image.identifier) {
-            // This is required for certain image formats, such as WebP, for which
-            // NSItemProvider doesn't automatically provide the `UIImage` representation.
-            provider.loadDataRepresentation(forTypeIdentifier: UTType.image.identifier) { data, error in
-                let image = data.flatMap(UIImage.init)
-                DispatchQueue.main.async {
-                    if let image {
-                        completion(image, nil)
-                    } else {
-                        DDLogError("Failed to load image for provider with registered types \(provider.registeredTypeIdentifiers) with error \(String(describing: error))")
-                        completion(nil, error)
-                    }
-                }
-            }
-        } else {
-            DDLogError("No image representation available for provider with registered types: \(provider.registeredTypeIdentifiers)")
-            DispatchQueue.main.async {
-                completion(nil, nil)
-            }
-        }
     }
 }
 
