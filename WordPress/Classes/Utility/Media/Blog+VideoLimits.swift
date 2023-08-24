8c58788138a356e39a9a2f51d9e9cc37d2c08249
kean <grebenyuk.alexander@gmail.com>
2023-08-24 15:58:30 -0400

    Refactor Blog.canUploadAsset

--- WordPress/Classes/Utility/Media/Blog+VideoLimits.swift
+++ WordPress/Classes/Utility/Media/Blog+VideoLimits.swift
@@ -5,27 +5,32 @@ extension Blog {
     /// Maximum allowed duration for video uploads on free sites, in seconds (5 mins).
     static let maximumVideoDurationForFreeSites: TimeInterval = 300
 
+    /// Returns the video duration limit for the blog. If there is no limit, returns `nil`.
+    var videoDurationLimit: TimeInterval? {
+        if hasPaidPlan || !isHostedAtWPcom {
+            return nil
+        }
+        return Blog.maximumVideoDurationForFreeSites
+    }
+
     /// Returns `true` if the blog is allowed to upload the given asset.
     func canUploadAsset(_ asset: WPMediaAsset) -> Bool {
-        return canUploadAsset(asset.exceedsFreeSitesAllowance())
+        guard asset.assetType() == .video else {
+            return true
+        }
+        guard let limit = videoDurationLimit else {
+            return true
+        }
+        return asset.duration() <= limit
     }
 
     /// Returns `true` if the blog is allowed to upload the video at the given URL.
     func canUploadVideo(from videoURL: URL) -> Bool {
+        guard let limit = videoDurationLimit else {
+            return true
+        }
         let asset = AVAsset(url: videoURL)
         let duration = CMTimeGetSeconds(asset.duration)
-        let exceedsAllowance = duration > Blog.maximumVideoDurationForFreeSites
-        return canUploadAsset(exceedsAllowance)
-    }
-
-    public func canUploadAsset(_ assetExceedsFreeSitesAllowance: Bool) -> Bool {
-        return hasPaidPlan || !isHostedAtWPcom || !assetExceedsFreeSitesAllowance
-    }
-}
-
-private extension WPMediaAsset {
-    ///  Returns true if the asset is a video and its duration is longer than the maximum allowed duration on free sites.
-    func exceedsFreeSitesAllowance() -> Bool {
-        assetType() == .video && duration() > Blog.maximumVideoDurationForFreeSites
+        return duration <= limit
     }
 }
