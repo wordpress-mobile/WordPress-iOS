44089b434218c00477413ebdc170fc1e236e3af1
kean <grebenyuk.alexander@gmail.com>
2023-08-30 11:34:21 -0400

    Remove temp code

--- WordPress/Classes/ViewRelated/Blog/My Site/MySiteViewController.swift
+++ WordPress/Classes/ViewRelated/Blog/My Site/MySiteViewController.swift
@@ -170,13 +170,6 @@ class MySiteViewController: UIViewController, NoResultsViewHost, UIScrollViewDel
         startObservingQuickStart()
         startObservingOnboardingPrompt()
         subscribeToWillEnterForeground()
-
-        #warning("TEMP")
-        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
-            let vc = MediaViewController()
-            let nav = UINavigationController(rootViewController: vc)
-            self.present(nav, animated: true)
-        }
     }
 
     override func viewWillAppear(_ animated: Bool) {
