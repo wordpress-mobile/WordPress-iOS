e1781e0abb357f4db884e2b588728f00fa3cea8a
kean <grebenyuk.alexander@gmail.com>
2023-09-07 18:08:16 -0400

    Add comments menu item

--- WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Quick Actions/DashboardQuickActionsViewModel.swift
+++ WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Quick Actions/DashboardQuickActionsViewModel.swift
@@ -17,14 +17,12 @@ final class DashboardQuickActionsViewModel {
         let posts = DashboardQuickActionItemViewModel(
             image: UIImage(named: "site-menu-posts"),
             title: Strings.posts,
-            details: nil,
             action: .posts
         )
 
         let pages = DashboardQuickActionItemViewModel(
             image: UIImage(named: "site-menu-pages"),
             title: Strings.pages,
-            details: nil,
             tourElement: .pages,
             action: .pages
         )
@@ -32,15 +30,19 @@ final class DashboardQuickActionsViewModel {
         let media = DashboardQuickActionItemViewModel(
             image: UIImage(named: "site-menu-media"),
             title: Strings.media,
-            details: nil,
             tourElement: .mediaScreen,
             action: .media
+        )
+
+        let comments = DashboardQuickActionItemViewModel(
+            image: UIImage(named: "site-menu-comments"),
+            title: Strings.comments,
+            action: .comments
         )
 
         let stats = DashboardQuickActionItemViewModel(
             image: UIImage(named: "site-menu-stats"),
             title: Strings.stats,
-            details: nil,
             tourElement: .stats,
             action: .stats
         )
@@ -48,7 +50,6 @@ final class DashboardQuickActionsViewModel {
         let more = DashboardQuickActionItemViewModel(
             image: UIImage(named: "site-menu-more"),
             title: Strings.more,
-            details: nil,
             tourElement: .siteMenu,
             action: .more
         )
@@ -57,6 +58,7 @@ final class DashboardQuickActionsViewModel {
             posts,
             blog.supports(.pages) ? pages : nil,
             media,
+            comments,
             blog.supports(.stats) ? stats : nil,
             more
         ].compactMap { $0 }
@@ -87,6 +89,7 @@ enum DashboardQuickAction {
     case posts
     case pages
     case media
+    case comments
     case stats
     case more
 }
@@ -95,6 +98,7 @@ private enum Strings {
     static let stats = NSLocalizedString("dashboard.menu.stats", value: "Stats", comment: "Title for stats button on dashboard.")
     static let posts = NSLocalizedString("dashboard.menu.posts", value: "Posts", comment: "Title for posts button on dashboard.")
     static let media = NSLocalizedString("dashboard.menu.media", value: "Media", comment: "Title for media button on dashboard.")
+    static let comments = NSLocalizedString("dashboard.menu.comments", value: "Comments", comment: "Title for comments button on dashboard.")
     static let pages = NSLocalizedString("dashboard.menu.pages", value: "Pages", comment: "Title for pages button on dashboard.")
     static let more = NSLocalizedString("dashboard.menu.more", value: "More", comment: "Title for more button on dashboard.")
 }
