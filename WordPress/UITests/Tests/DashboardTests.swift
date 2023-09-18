520fa5a2efe3d66b68dcc256770a4109370dbdb8
Gio Lodi <gio@mokacoding.com>
2023-09-18 13:26:54 +1000

    Add test loading page in Safari

--- WordPress/UITests/Tests/DashboardTests.swift
+++ WordPress/UITests/Tests/DashboardTests.swift
@@ -59,3 +59,33 @@ class DashboardTests: XCTestCase {
             .verifyActivityLogScreen(hasActivityPartial: "This site is connected to")
     }
 }
+
+// Taken from a UI test in the Tumblr app
+class SafariTests: XCTestCase {
+
+    func testSafariLoadsURL() {
+        let safari = XCUIApplication(bundleIdentifier: "com.apple.mobilesafari")
+        safari.launch()
+
+        // [!] This test expect Safari to be in a clean state.
+        //     This is usually the case in CI where we use a fresh Simulator, but might not be on a dev machine.
+        //     Given this is only an experiment, I skipped handling "dirty" dev machine states.
+
+        let addressBar = safari.textFields["Address"]
+        XCTAssertTrue(addressBar.waitForExistence(timeout: 5))
+        addressBar.tap()
+        addressBar.typeText("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
+        addressBar.typeText("\n")
+
+        // We should now have a player, but it could be that the controls are not on screen.
+        // If so, tap to reveal them.
+        //
+        // There is not assertion for the other branch of the if, but that okay as the check passing is implicit validation of the expected behavior.
+        let playButton = safari.buttons["Play/Pause"]
+        if playButton.waitForExistence(timeout: 5) == false {
+            safari.tap()
+            XCTAssertTrue(playButton.waitForExistence(timeout: 5))
+        }
+        safari.terminate()
+    }
+}
