839cbae5740e6fce988b1d944610f31fa6a7182d
jos <17252150+jostnes@users.noreply.github.com>
2023-09-19 16:41:02 +0800

    go to published tab if not on that

--- WordPress/UITestsFoundation/Screens/PagesScreen.swift
+++ WordPress/UITestsFoundation/Screens/PagesScreen.swift
@@ -7,7 +7,12 @@ public class PagesScreen: ScreenObject {
         $0.tables["PagesTable"]
     }
 
+    private let publishedButtonGetter: (XCUIApplication) -> XCUIElement = {
+        $0.buttons["published"]
+    }
+
     var pagesTable: XCUIElement { pagesTableGetter(app) }
+    var publishedButton: XCUIElement { publishedButtonGetter(app) }
 
     init(app: XCUIApplication = XCUIApplication()) throws {
 
@@ -23,6 +28,11 @@ public class PagesScreen: ScreenObject {
 
     @discardableResult
     public func verifyPagesScreen(hasPage pageTitle: String) -> Self {
+        // If test is not on Published tab, tap to go to Published tab before asserting
+        if !publishedButton.isSelected {
+            publishedButton.tap()
+        }
+
         XCTAssertTrue(pagesTable.staticTexts[pageTitle].waitForIsHittable(), "Pages Screen: \"\(pageTitle)\" page not displayed.")
         return self
     }
