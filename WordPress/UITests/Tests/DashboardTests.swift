042e319264ef92ab650be744d2cf908b4426f2b0
Jos <jostnes@users.noreply.github.com>
2023-10-05 15:59:58 +0800

    fix hound errors

--- WordPress/UITests/Tests/DashboardTests.swift
+++ WordPress/UITests/Tests/DashboardTests.swift
@@ -6,6 +6,7 @@ class DashboardTests: XCTestCase {
 
     @MainActor
     override func setUpWithError() throws {
+        try super.setUpWithError()
         setUpTestSuite()
 
         try LoginFlow
@@ -14,6 +15,7 @@ class DashboardTests: XCTestCase {
 
     override func tearDownWithError() throws {
         takeScreenshotOfFailedTest()
+        try super.tearDownWithError()
     }
 
     func testFreeToPaidCardNavigation() throws {
