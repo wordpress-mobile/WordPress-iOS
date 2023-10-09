9f71d31c4541626a2a7aa114954cfe4183ad7ccd
Gio Lodi <gio@mokacoding.com>
2023-10-09 13:28:41 +1100

    Extract `TruthTable` in dedicated file

--- WordPress/WordPressTest/MySiteViewModelTests.swift
+++ WordPress/WordPressTest/MySiteViewModelTests.swift
@@ -110,25 +110,3 @@ class MySiteViewModelTests: CoreDataTestCase {
         return blog
     }
 }
-
-/// Convenience to iterate over all possible combinations of multiple boolean values.
-enum TruthTable {
-
-    static let twoValues: [(Bool, Bool)] = [
-        (true, true),
-        (true, false),
-        (false, true),
-        (false, false)
-    ]
-
-    static let threeValues: [(Bool, Bool, Bool)] = [
-        (true, true, true),
-        (true, true, false),
-        (true, false, true),
-        (true, false, false),
-        (false, true, true),
-        (false, true, false),
-        (false, false, true),
-        (false, false, false)
-    ]
-}
