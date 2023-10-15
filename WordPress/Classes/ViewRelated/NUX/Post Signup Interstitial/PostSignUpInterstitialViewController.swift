e5b010b298bba6530a35ee92ebb39acc664c2ef1
Hassaan El-Garem <hassaanelgarem@gmail.com>
2023-10-15 17:36:50 +0300

    Update: fix typo

--- WordPress/Classes/ViewRelated/NUX/Post Signup Interstitial/PostSignUpInterstitialViewController.swift
+++ WordPress/Classes/ViewRelated/NUX/Post Signup Interstitial/PostSignUpInterstitialViewController.swift
@@ -104,7 +104,7 @@ class PostSignUpInterstitialViewController: UIViewController {
     @IBAction func cancel(_ sender: Any) {
         dismiss?(.none)
 
-        RootViewCoordinator.shared.showPostSignUpTabForNotSites()
+        RootViewCoordinator.shared.showPostSignUpTabForNoSites()
 
         tracker.track(click: .dismiss, ifTrackingNotEnabled: {
             WPAnalytics.track(.welcomeNoSitesInterstitialDismissed)
