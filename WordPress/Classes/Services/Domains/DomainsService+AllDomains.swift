90e9bad3597b8aaa8e27b2730f593411c64a6d2d
Salim Braksa <salim.braksa@gmail.com>
2023-10-17 16:39:49 +0100

    Fix lint issues

--- WordPress/Classes/Services/Domains/DomainsService+AllDomains.swift
+++ WordPress/Classes/Services/Domains/DomainsService+AllDomains.swift
@@ -2,7 +2,7 @@ import Foundation
 import WordPressKit
 
 extension DomainsService {
-    
+
     func fetchAllDomains(params: AllDomainsEndpointParams = .init(), completion: @escaping (AllDomainsEndpointResult) -> Void) {
         remote.getAllDomains(params: params, completion: completion)
     }
