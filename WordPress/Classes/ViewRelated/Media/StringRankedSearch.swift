4fe5d25756e5c40cf2a146bb3ae2156983c39ac6
kean <grebenyuk.alexander@gmail.com>
2023-10-17 16:35:13 -0400

    Improve scoring algorithm

--- WordPress/Classes/ViewRelated/Media/StringRankedSearch.swift
+++ WordPress/Classes/ViewRelated/Media/StringRankedSearch.swift
@@ -4,18 +4,21 @@ struct StringRankedSearch {
     /// By default, `[.caseInsensitive, .diacriticInsensitive]`.
     var options: String.CompareOptions = [.caseInsensitive, .diacriticInsensitive]
 
+    private let term: String
     private let terms: [String]
 
     init(searchTerm: String) {
-        self.terms = searchTerm
-            .trimmingCharacters(in: .whitespaces)
-            .components(separatedBy: .whitespaces)
-            .filter { !$0.isEmpty }
+        self.term = searchTerm.trimmingCharacters(in: .whitespaces)
+        self.terms = term.components(separatedBy: .whitespaces).filter { !$0.isEmpty }
     }
 
     /// Returns a score in a `0.0...1.0` range where `1.0` is maximum confidence.
+    /// Anything above `0.5` suggests a good probability of a match.
     func score(for string: String?) -> Double {
-        let words = (string ?? "")
+        guard let string else {
+            return 0
+        }
+        let words = string
             .trimmingCharacters(in: .whitespaces)
             .components(separatedBy: .whitespaces)
             .filter { !$0.isEmpty }
@@ -23,49 +26,78 @@ struct StringRankedSearch {
             return 0
         }
         var score = 0.0
+        var matchIndices: Set<Int> = []
         for term in terms {
-            score += words.map { self.score(for: $0, term: term) }.max()!
+            // Get the maximum score for each word. There is no penalty for a
+            // position of the word in the input string.
+            let match = words.enumerated()
+                .map { (index, word) in (index: index, score: self.score(for: word, term: term)) }
+                .max { $0.score < $1.score }!
+            score += match.score
+            matchIndices.insert(match.index)
         }
-        return 0.5 * (score / Double(max(words.count, terms.count)) + score / Double(terms.count))
+        let bonusForDistanceBetweenMatches = self.bonusForDistanceBetweenMatches(matchIndices.sorted(), words: words)
+        let bonusForLengthMatch = score * (Double(min(string.count, term.count)) / Double(max(string.count, term.count)))
+        let bonusForCountMatch = score * (Double(min(terms.count, words.count)) / Double(max(terms.count, words.count)))
+        return (0.9 * (score / Double(terms.count))) +
+        (0.05 * bonusForDistanceBetweenMatches) +
+        (0.025 * bonusForLengthMatch) *
+        (0.025 * bonusForCountMatch)
     }
 
+
+    private func bonusForDistanceBetweenMatches(_ indices: [Int], words: [String]) -> Double {
+        let distance = zip(indices.dropLast(), indices.dropFirst())
+            .map { $1 - $0 }
+            .reduce(0, +)
+        return 1.0 - (Double(distance) / Double(words.count))
+    }
+
+    // Returns score in a `0.0...1.0` range.
     private func score(for input: String, term: String) -> Double {
         guard !input.isEmpty else {
             return 0
         }
-        let score = fuzzyScore(for: input[...], term: term)
-        return (0.8 * (score / Double(term.count))) + (0.2 * (score / Double(input.count)))
+        let score = fuzzyScore(for: input, term: term) / Double(term.count)
+        let bonusForLengthMatch = score * (Double(min(input.count, term.count)) / Double(max(input.count, term.count)))
+        let bonusForTermLength = term.count > 3 ? 0.1 : (term.count > 2 ? 0.05 : 0.0)
+        return (0.8 * score) + (0.1 * bonusForLengthMatch) + bonusForTermLength
     }
 
-    private func fuzzyScore(for input: Substring, term: String) -> Double {
+    // Returns score in a `0.0...1.0` range.
+    private func fuzzyScore(for input: String, term: String) -> Double {
         var score = 0.0
-        var misses = 0
-        var input = input
-        for character in term {
-            func findNextMatch() -> Range<String.Index>? {
-                if let range = input.range(of: term, options: options) {
-                    return range // Found these characters in a row
-                }
-                return input.range(of: String(character), options: options)
+        var inputIndex = input.startIndex
+        var termIndex = term.startIndex
+
+        func findNextMatch() -> Range<String.Index>? {
+            // Look for a perfect match first
+            if let range = input[inputIndex...].range(of: term[termIndex...], options: options) {
+                return range // Found these characters in a row
             }
-            if let index = findNextMatch()?.lowerBound {
-                if index == input.startIndex {
+            return input[inputIndex...].range(of: String(term[termIndex]), options: options)
+        }
+
+        while termIndex < term.endIndex, inputIndex < input.endIndex, let range = findNextMatch() {
+            var matchIndex = range.lowerBound
+            while matchIndex < range.upperBound {
+                if matchIndex == inputIndex {
                     score += 0.9 // Bonus: matches the position exactly
-                } else if character.isLetter && !input[input.index(before: index)].isLetter {
-                    score += 0.8 // Bonus: after non-letter, e.g. `'`
-                } else {
-                    score += 0.6
+                } else if term[termIndex].isLetter != input[input.index(before: matchIndex)].isLetter {
+                    score += 0.8 // Bonus: letter followed by non-letter or the other way around
                 }
-                if input[index] == character {
+                if input[matchIndex] == term[termIndex] {
                     score += 0.1 // Bonus: exact match, including case and diacritics
                 }
-                input = input.suffix(from: input.index(after: index))
-            } else {
-                misses += 1
-                if misses > 1 {
-                    return 0 // Allow up to one miss
-                }
+                termIndex = term.index(after: termIndex)
+                matchIndex = input.index(after: matchIndex)
+                inputIndex = matchIndex
             }
+            inputIndex = range.upperBound
+        }
+
+        guard term.distance(from: termIndex, to: term.endIndex) < 2 else {
+            return 0 // Too many misses
         }
         return score
     }
--- WordPress/WordPressTest/StringRankedSearchTests.swift
+++ WordPress/WordPressTest/StringRankedSearchTests.swift
@@ -2,29 +2,84 @@ import XCTest
 
 @testable import WordPress
 
-final class StringRankedSearchTests: CoreDataTestCase {
-    func testStringScore() throws {
-        func score(_ lhs: String, _ rhs: String) -> Double {
-            StringRankedSearch(searchTerm: rhs).score(for: lhs)
-        }
+final class StringRankedSearchTests: XCTestCase {
+    func testScoreInRange() {
+        // High confidence
+        XCTAssertInRange(0.8...1.0, score("Appleseed", "Appleseed"))
+        XCTAssertInRange(0.8...1.0, score("John Appleseed", "Appleseed"))
+        XCTAssertInRange(0.8...1.0, score("John Appleseed", "John"))
+        XCTAssertInRange(0.8...1.0, score("John Appleseed", "App"))
+        XCTAssertInRange(0.8...1.0, score("John O'Appleseed", "App"))
+        XCTAssertInRange(0.8...1.0, score("john-appleseed", "j-a"))
+        XCTAssertInRange(0.8...1.0, score("#john-appleseed", "john"))
+        XCTAssertInRange(0.8...1.0, score("John Appleseed", "Apseed"))
 
-        XCTAssertLessThan(score("John Appleseed", "x"), 0.3)
-        XCTAssertGreaterThan(score("John Appleseed", "o"), 0.3)
-        XCTAssertGreaterThan(score("John Appleseed", "Jo"), 0.3)
-        XCTAssertGreaterThan(score("John Appleseed", "jo"), 0.3)
-        XCTAssertGreaterThan(score("John Appleseed", "ohn"), 0.3)
-        XCTAssertGreaterThan(score("John Appleseed", "App"), 0.3)
-        XCTAssertGreaterThan(score("John Appleseed", "ppl"), 0.3)
+        // Medium confidence
+        XCTAssertInRange(0.5...0.8, score("John Appleseed", "A"))
+        XCTAssertInRange(0.5...0.8, score("John Appleseed", "Ap"))
+        XCTAssertInRange(0.5...0.8, score("John Appleseed", "ohn"))
+        XCTAssertInRange(0.5...0.8, score("#john-appleseed", "j-a"))
+        XCTAssertInRange(0.5...0.8, score("John Appleseed", "applex"))
 
-        XCTAssertLessThan(score("John Appleseed", "x"), score("John Appleseed", "o"))
-        XCTAssertLessThan(score("John Appleseed", "o"), score("John Appleseed", "j"))
-        XCTAssertLessThan(score("John Appleseed", "j"), score("John Appleseed", "J"))
-        XCTAssertLessThan(score("John Appleseed", "j"), score("John Appleseed", "jo"))
-        XCTAssertLessThan(score("John Appleseed", "jh"), score("John Appleseed", "jo"))
-        XCTAssertLessThan(score("John Appleseed", "j"), score("John Appleseed", "jo"))
-        XCTAssertLessThan(score("John Appleseed", "jh"), score("John Appleseed", "jhn"))
-        XCTAssertLessThan(score("John xAppleseed", "App"), score("John O'Appleseed", "app"))
-        XCTAssertLessThan(score("Andrew xAppleseed", "App"), score("Andrew O'Appleseed", "app"))
-        XCTAssertLessThan(score("project-thread-weekly", "project"), score("project-thread", "project"))
+        // Low confidence
+        XCTAssertInRange(0.2...0.5, score("John Appleseed", "Ae"))
+        XCTAssertInRange(0.2...0.5, score("John Appleseed", "Jn"))
+
+        // Very low confidence
+        XCTAssertInRange(0.0...0.2, score("John Appleseed", "o"))
+        XCTAssertInRange(0.0...0.2, score("John Appleseed", "X"))
+        XCTAssertInRange(0.0...0.2, score("John Appleseed", "x"))
+        XCTAssertInRange(0.0...0.2, score("John Appleseed", "applexx"))
     }
+
+    func testBonuses() {
+        // Bonus for the number of the matching words in the input.
+        XCTAssertLessThan(score("John Appleseed", "App"), score("Appleseed", "App"))
+
+        // Bonus for distance between matches
+        XCTAssertLessThan(score("John Xxxx Appleseed", "John Appleseed"), score("John Appleseed Xxxx", "John Appleseed"))
+
+        // Bonus for distance between matches
+        XCTAssertLessThan(score("John Xxxx Appleseed", "John Appleseed"), score("John Appleseed Xxxx", "John Appleseed"))
+
+        // Bonus for distance between matches
+        XCTAssertLessThan(score("John Xxxx Appleseed", "John Appleseed"), score("Xxxx John Appleseed", "John Appleseed"))
+
+        // Bonus for more characters in a row
+        XCTAssertLessThan(score("Apxplesee", "App"), score("Appleseed", "App"))
+
+        // Bonus for more characters in a row is higher than the penalty for a number of matches
+        XCTAssertLessThan(score("Apxplesee", "App"), score("John Appleseed", "App"))
+
+        // Bonus for more characters in a row is higher than the penalty for mismatches case.
+        XCTAssertLessThan(score("Apxplesee", "App"), score("appleseed", "App"))
+
+        // The diacritics are considered a match
+        XCTAssertLessThan(score("Kxhu", "Kahu"), score("Kāhu", "Kahu"))
+
+        // Bonus for exact match diacritics are present
+        XCTAssertLessThan(score("Kāhu", "Kahu"), score("Kahu", "Kahu"))
+
+        // Bonus for exact match diacritics are present
+        XCTAssertLessThan(score("Kāhu", "Kahu"), score("Kāhu", "Kāhu"))
+
+        // Bonus for number length match
+        XCTAssertLessThan(score("john-appleseed-xxxx", "project"), score("john-appleseed", "project"))
+    }
+
+    func xtestPerformance() throws {
+        measure {
+            for _ in 0..<10000 {
+                _ = score("John Appleseed", "John")
+            }
+        }
+    }
+}
+
+private func score(_ lhs: String, _ rhs: String) -> Double {
+    StringRankedSearch(searchTerm: rhs).score(for: lhs)
+}
+
+private func XCTAssertInRange<T: Comparable>(_ range: some RangeExpression<T>, _ value: T, file: StaticString = #filePath, line: UInt = #line) {
+    XCTAssert(range.contains(value), "(\"\(value)\") is not in (\"\(range)\")", file: file, line: line)
 }
