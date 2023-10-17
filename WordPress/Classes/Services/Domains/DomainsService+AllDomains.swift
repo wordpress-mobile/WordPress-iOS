902bfd33d259724a7b464558676f3341dae5c1bd
Salim Braksa <salim.braksa@gmail.com>
2023-10-17 17:11:10 +0100

    Minor updates due to changes in WordPressKit pod

--- WordPress/Classes/Services/Domains/DomainsService+AllDomains.swift
+++ WordPress/Classes/Services/Domains/DomainsService+AllDomains.swift
@@ -4,7 +4,7 @@ import WordPressKit
 extension DomainsService {
 
     func fetchAllDomains(params: AllDomainsEndpointParams = .init(), completion: @escaping (AllDomainsEndpointResult) -> Void) {
-        remote.getAllDomains(params: params, completion: completion)
+        remote.fetchAllDomains(params: params, completion: completion)
     }
 
     typealias AllDomainsEndpointResult = DomainsServiceRemote.AllDomainsEndpointResult
