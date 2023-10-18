94fc8392bedfbb2de6345de8664346fff9dd7c7e
Povilas Staskus <povilas.staskus@automattic.com>
2023-10-18 10:41:24 +0300

    Change Home Screen Small Widget design if it's displayed without background
    Small Home Screen Widget can be shown in iPad lock screen on horizontal orientation:
    - Change the order of title and value
    - Increase font size of value

--- WordPress/JetpackStatsWidgets/Views/SingleStatView.swift
+++ WordPress/JetpackStatsWidgets/Views/SingleStatView.swift
@@ -1,19 +1,47 @@
 import SwiftUI
+import WidgetKit
 
 struct SingleStatView: View {
 
-    let viewData: GroupedViewData
+    let title: String
+    let description: String
+    let valueTitle: String
+    let value: Int
+
+    init(viewData: GroupedViewData) {
+        self.title = viewData.widgetTitle
+        self.description = viewData.siteName
+        self.valueTitle = viewData.upperLeftTitle
+        self.value = viewData.upperLeftValue
+    }
+
+    init(title: String, description: String, valueTitle: String, value: Int) {
+        self.title = title
+        self.description = description
+        self.valueTitle = valueTitle
+        self.value = value
+    }
 
 
     var body: some View {
         HStack {
             VStack(alignment: .leading) {
-                FlexibleCard(axis: .vertical, title: viewData.widgetTitle, value: .description(viewData.siteName), lineLimit: 2)
+                FlexibleCard(axis: .vertical, title: title, value: .description(description), lineLimit: 2)
 
                 Spacer()
-                VerticalCard(title: viewData.upperLeftTitle, value: viewData.upperLeftValue, largeText: true)
+                VerticalCard(title: valueTitle, value: value, largeText: true)
             }
             Spacer()
         }
+    }
+}
+
+@available(iOS 16.0, *)
+struct SingleStatView_Previews: PreviewProvider {
+    static var previews: some View {
+        SingleStatView(title: "My WordPress Site", description: "Today", valueTitle: "Views", value: 124909)
+            .previewContext(
+                WidgetPreviewContext(family: .systemSmall)
+            )
     }
 }
