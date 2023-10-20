520006bf37f869a37103be3ca8b1221497f82682
Povilas Staskus <povilas.staskus@automattic.com>
2023-10-20 15:27:26 +0300

    Add icon to title view

--- WordPress/JetpackStatsWidgets/LockScreenWidgets/Views/LockScreenSiteTitleView.swift
+++ WordPress/JetpackStatsWidgets/LockScreenWidgets/Views/LockScreenSiteTitleView.swift
@@ -4,10 +4,16 @@ struct LockScreenSiteTitleView: View {
     let title: String
 
     var body: some View {
-        Text(title)
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .font(.system(size: 10))
-            .lineLimit(1)
-            .allowsTightening(true)
+        HStack(spacing: 2) {
+            Image(.iconJetpack)
+                .resizable()
+                .frame(width: 11, height: 11)
+            Text(title)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .font(.system(size: 11))
+                .lineLimit(1)
+                .allowsTightening(true)
+        }
+
     }
 }
