9bc14873a3a81fe597085ae7d7efafd74b127fdf
alpavanoglu <alp.avanoglu@automattic.com>
2023-10-24 09:28:18 +0200

    Remove the strings enum & WP target

--- WordPress/Classes/ViewRelated/Me/MyDomains/MyDomainsViewController.swift
+++ WordPress/Classes/ViewRelated/Me/MyDomains/MyDomainsViewController.swift
@@ -1,35 +1,6 @@
 import UIKit
 
 final class MyDomainsViewController: UIViewController {
-    enum Action: String {
-        case addToSite
-        case purchase
-        case transfer
-
-        var title: String {
-            switch self {
-            case .addToSite:
-                return NSLocalizedString(
-                    "domain.management.fab.add.domain.title",
-                    value: "Add domain to site",
-                    comment: "Domain Management FAB Add Domain title"
-                )
-            case .purchase:
-                return NSLocalizedString(
-                    "domain.management.fab.purchase.domain.title",
-                    value: "Purchase domain only",
-                    comment: "Domain Management FAB Purchase Domain title"
-                )
-            case .transfer:
-                return NSLocalizedString(
-                    "domain.management.fab.transfer.domain.title",
-                    value: "Transfer domain(s)",
-                    comment: "Domain Management FAB Transfer Domain title"
-                )
-            }
-        }
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
         title = Strings.title
