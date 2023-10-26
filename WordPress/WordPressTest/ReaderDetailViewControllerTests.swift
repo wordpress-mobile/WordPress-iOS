faf70b90327a2451a44076a06eb84bc4a8a309f8
Gio Lodi <gio@mokacoding.com>
2023-10-26 16:24:41 +1100

    Extract `ReaderPost` in dedicated file
    It's used across more than one test class, so it deserves to live in its
    own file.

--- WordPress/WordPressTest/ReaderDetailViewControllerTests.swift
+++ WordPress/WordPressTest/ReaderDetailViewControllerTests.swift
@@ -46,22 +46,6 @@ class ReaderDetailViewControllerTests: CoreDataTestCase {
 
 }
 
-/// Builds a ReaderPost
-///
-class ReaderPostBuilder {
-    private let post: ReaderPost
-
-    init(_ context: NSManagedObjectContext, blog: Blog? = nil) {
-        post = NSEntityDescription.insertNewObject(forEntityName: ReaderPost.entityName(), into: context) as! ReaderPost
-    }
-
-    func build() -> ReaderPost {
-        post.blogURL = "https://wordpress.com"
-        post.permaLink = "https://wordpress.com"
-        return post
-    }
-}
-
 private class ReaderDetailCoordinatorMock: ReaderDetailCoordinator {
     var didCallStart = false
 
