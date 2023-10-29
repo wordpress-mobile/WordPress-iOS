82d905cb27b7a03578290f80e38475509f5bc85a
Salim Braksa <salim.braksa@gmail.com>
2023-10-30 00:54:41 +0100

    Add All Domains empty state strings

---
+++ WordPress/Classes/ViewRelated/Me/All Domains/View Models/AllDomainsListViewModel+Strings.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+extension AllDomainsListViewModel {
+
+    enum Strings {
+        static let emptyStateTitle = NSLocalizedString(
+            "domain.management.default.empty.state.title",
+            value: "You don't have any domains",
+            comment: "The empty state title in All Domains screen when the user doesn't have any domains"
+        )
+        static let emptyStateDescription = NSLocalizedString(
+            "domain.management.default.empty.state.description",
+            value: "Tap the button below to add a new domain",
+            comment: "The empty state description in All Domains screen when the user doesn't have any domains"
+        )
+        static let emptyStateButtonTitle = NSLocalizedString(
+            "domain.management.default.empty.state.button.title",
+            value: "Add a domain",
+            comment: "The empty state button title in All Domains screen when the user doesn't have any domains"
+        )
+        static let offlineEmptyStateTitle = NSLocalizedString(
+            "domain.management.offline.empty.state.title",
+            value: "No Internet Connection",
+            comment: "The empty state title in All Domains screen when the user is offline"
+        )
+        static let offlineEmptyStateDescription = NSLocalizedString(
+            "domain.management.offline.empty.state.description",
+            value: "Please check your network connection and try again.",
+            comment: "The empty state description in All Domains screen when the user is offline"
+        )
+        static let errorEmptyStateTitle = NSLocalizedString(
+            "domain.management.error.empty.state.title",
+            value: "Something went wrong",
+            comment: "The empty state title in All Domains screen when an error occurs"
+        )
+        static let errorEmptyStateDescription = NSLocalizedString(
+            "domain.management.error.empty.state.description",
+            value: "Sorry, we encountered an error while loading your domains. Our team has been notified of the issue, and we're working to resolve it as soon as possible. Please try again later.",
+            comment: "The empty state description in All Domains screen when an error occurs"
+        )
+        static let errorStateButtonTitle = NSLocalizedString(
+            "domain.management.error.state.button.title",
+            value: "Try again",
+            comment: "The empty state button title in All Domains screen when an error occurs"
+        )
+    }
+}
