d77f52d8b62e37259d09b2829818d04f97497ca1
Hassaan El-Garem <hassaanelgarem@gmail.com>
2023-11-06 03:31:19 +0200

    Add: Basic implementation of `AllDomainsAddDomainCoordinator`

---
+++ WordPress/Classes/ViewRelated/Me/All Domains/Coordinators/AllDomainsAddDomainCoordinator.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+@objc final class AllDomainsAddDomainCoordinator: NSObject {
+    static func presentAddDomainFlow(in allDomainsViewController: AllDomainsListViewController,
+                                     source: String) {
+        let domainSuggestionsViewController = RegisterDomainSuggestionsViewController.instance(
+            site: nil,
+            domainSelectionType: .purchaseFromDomainManagement,
+            includeSupportButton: false,
+            title: Strings.searchTitle
+        )
+
+        let domainPurchasedCallback = { (domainViewController: RegisterDomainSuggestionsViewController, domainName: String) in
+            allDomainsViewController.reloadDomains()
+        }
+
+        domainSuggestionsViewController.domainPurchasedCallback = domainPurchasedCallback
+
+        let navigationController = UINavigationController(rootViewController: domainSuggestionsViewController)
+        navigationController.isModalInPresentation = true
+        allDomainsViewController.present(navigationController, animated: true)
+    }
+}
+
+extension AllDomainsAddDomainCoordinator {
+    private enum Strings {
+        static let searchTitle = NSLocalizedString("Search for a domain",
+                                                   comment: "Search domain - Title for the Suggested domains screen")
+    }
+}
