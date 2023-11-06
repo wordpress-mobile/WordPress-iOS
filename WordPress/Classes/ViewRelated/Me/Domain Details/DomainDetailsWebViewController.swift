afcc5043538673168d5a058bdc0598f47d6c7ed1
Salim Braksa <salim.braksa@gmail.com>
2023-11-06 14:20:57 +0100

    Go back to previous page or previous screen when an unsupported url is loaded

--- WordPress/Classes/ViewRelated/Me/Domain Details/DomainDetailsWebViewController.swift
+++ WordPress/Classes/ViewRelated/Me/Domain Details/DomainDetailsWebViewController.swift
@@ -9,7 +9,7 @@ final class DomainDetailsWebViewController: WebKitViewController {
     // MARK: - Init
 
     init(url: URL, analyticsSource: String? = nil) {
-        var configuration = WebViewControllerConfiguration(url: url)
+        let configuration = WebViewControllerConfiguration(url: url)
         configuration.analyticsSource = analyticsSource
         configuration.secureInteraction = true
         configuration.authenticateWithDefaultAccount()
@@ -35,16 +35,32 @@ final class DomainDetailsWebViewController: WebKitViewController {
                 return
             }
             if !self.shouldAllowNavigation(for: url) {
-                // Open URL in device browser then reset webview.
-                UIApplication.shared.open(url)
-                self.loadWebViewRequest()
+                // Open URL in device browser then go back to Domain Management page.
+                self.open(url)
+                self.goBack()
             }
         }
     }
 
+    // MARK: - Navigation
+
+    override func goBack() {
+        if webView.canGoBack {
+            webView.goBack()
+        } else if let navigationController {
+            navigationController.popViewController(animated: true)
+        } else {
+            dismiss(animated: true)
+        }
+    }
+
+    // MARK: - Helpers
+
     private func shouldAllowNavigation(for url: URL) -> Bool {
-        let isAuthenticationURL = authenticator?.isLogin(url: url) ?? false
-        let isDomainManagementURL = self.url?.absoluteString == url.absoluteString
-        return isAuthenticationURL || isDomainManagementURL
+        return self.url?.absoluteString == url.absoluteString
+    }
+
+    private func open(_ url: URL) {
+        UIApplication.shared.open(url)
     }
 }
