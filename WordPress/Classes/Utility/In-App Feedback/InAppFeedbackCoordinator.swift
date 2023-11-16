10f1830ad77027784db18372159737cb7534ffce
Salim Braksa <salim.braksa@gmail.com>
2023-11-16 11:55:06 +0100

    Implement groundwork for the in-app feedback feature

---
+++ WordPress/Classes/Utility/In-App Feedback/InAppFeedbackCoordinator.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+struct InAppFeedbackCoordinator {
+
+    private let userDefaults: UserDefaults
+
+    init(userDefaults: UserDefaults = .standard) {
+        self.userDefaults = userDefaults
+    }
+
+    func handlePositiveFeedback() {
+
+    }
+
+    func handleNegativeFeedback() {
+
+    }
+}
