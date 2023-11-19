4a2514ce79553a892279eaa800f692a597fc7ec9
Tony Li <tony.li@automattic.com>
2023-11-20 12:58:51 +1300

    Further improve PageTruee's performance (2x)

--- WordPress/WordPressTest/PagesListTests.swift
+++ WordPress/WordPressTest/PagesListTests.swift
@@ -164,7 +164,7 @@ class PagesListTests: CoreDataTestCase {
 final class PageTree {
 
     // A node in a tree, which of course is also a tree itself.
-    private final class TreeNode {
+    private class TreeNode {
         var page: Page
         var children = [TreeNode]()
         var parentNode: TreeNode?
@@ -185,6 +185,7 @@ final class PageTree {
             var pages = [Page]()
             _ = depthFirstSearch { level, node in
                 node.page.hierarchyIndex = level
+                node.page.hasVisibleParent = node.parentNode != nil
                 pages.append(node.page)
                 return false
             }
@@ -196,7 +197,7 @@ final class PageTree {
             depthFirstSearch(level: 0, using: closure)
         }
 
-        func depthFirstSearch(level: Int, using closure: (Int, TreeNode) -> Bool) -> Bool {
+        private func depthFirstSearch(level: Int, using closure: (Int, TreeNode) -> Bool) -> Bool {
             let shouldStop = closure(level, self)
             if shouldStop {
                 return true
@@ -225,23 +226,17 @@ final class PageTree {
             }
         }
 
-        func add(_ newNode: TreeNode) -> Bool {
-            assert(newNode.parentNode == nil)
+        func add(_ newNodes: [TreeNode], parentID: NSNumber) -> Bool {
+            assert(parentID != 0)
 
-            guard let parentID = newNode.page.parentID?.int64Value, parentID != 0 else {
-                return false
-            }
-
-            depthFirstSearch { _, node in
-                if node.page.postID?.int64Value == parentID {
-                    node.children.append(newNode)
-                    newNode.parentNode = node
+            return depthFirstSearch { _, node in
+                if node.page.postID == parentID {
+                    node.children.append(contentsOf: newNodes)
+                    newNodes.forEach { $0.parentNode = node }
                     return true
                 }
                 return false
             }
-
-            return newNode.parentNode != nil
         }
     }
 
@@ -250,31 +245,40 @@ final class PageTree {
     // - child nodes. They are top level pages.
     // - orphan nodes. They are pages that doesn't belong to the root level, but their parent pages haven't been loaded yet.
     private var nodes = [TreeNode]()
-    private var orphanNodes = IndexSet()
+    private var orphanNodes = [NSNumber: [Int]]()
 
     func add(_ newPages: [Page]) {
         let newNodes = newPages.map { TreeNode(page: $0) }
         relocateOrphans(to: newNodes)
 
         let batch = 100
-        for index in stride(from: 0, to: newPages.count, by: batch) {
+        for index in stride(from: 0, to: newNodes.count, by: batch) {
             let tree = PageTree()
-            tree.add(Array(newNodes[index..<min(index + batch, newPages.count)]))
-            self.add(tree.nodes)
+            tree.add(Array(newNodes[index..<min(index + batch, newNodes.count)]))
+            merge(tree)
         }
     }
 
     private func relocateOrphans(to newNodes: [TreeNode]) {
-        // If the orphan nodes can find their parents in `newNode`, move them under `newNode`.
-        let movedOrphanIndexes = orphanNodes.filter { index in
-            newNodes.contains { $0.add(nodes[index]) }
+        let relocated = orphanNodes.reduce(into: IndexSet()) { result, element in
+            let parentID = element.key
+            let indexes = element.value
+
+            let toBeRelocated = indexes.map { nodes[$0] }
+            let moved = newNodes.contains {
+                $0.add(toBeRelocated, parentID: parentID)
+            }
+            if moved {
+                result.formUnion(IndexSet(indexes))
+            }
         }
 
-        if !movedOrphanIndexes.isEmpty {
-            nodes.remove(atOffsets: IndexSet(movedOrphanIndexes))
-            orphanNodes = nodes.enumerated().reduce(into: IndexSet()) { indexes, node in
+        if !relocated.isEmpty {
+            nodes.remove(atOffsets: relocated)
+            orphanNodes = nodes.enumerated().reduce(into: [:]) { indexes, node in
                 if node.element.isOrphan {
-                    indexes.insert(node.offset)
+                    let parentID = node.element.page.parentID ?? 0
+                    indexes[parentID, default: []].append(node.offset)
                 }
             }
         }
@@ -282,15 +286,17 @@ final class PageTree {
 
     private func add(_ newNodes: [TreeNode]) {
         newNodes.forEach { newNode in
+            let parentID = newNode.page.parentID ?? 0
+
             // If the new node is at the root level, then simply add it as a child
-            if (newNode.page.parentID?.int64Value ?? 0) == 0 {
+            if parentID == 0 {
                 nodes.append(newNode)
                 return
             }
 
             // The new node is not at the root level, find its parent in the root level nodes.
             for child in nodes {
-                if child.add(newNode) {
+                if child.add([newNode], parentID: parentID) {
                     break
                 }
             }
@@ -298,9 +304,66 @@ final class PageTree {
             // Still not find their parent, add it to the root level nodes.
             if newNode.parentNode == nil {
                 nodes.append(newNode)
-                orphanNodes.insert(nodes.count - 1)
+                orphanNodes[parentID, default: []].append(nodes.count - 1)
             }
         }
+    }
+
+    private func merge(_ other: PageTree) {
+        var parentIDs = other.nodes.reduce(into: Set()) { $0.insert($1.page.parentID ?? 0) }
+        // No need to look for root level
+        parentIDs.remove(0)
+        let parentNodes = findNodes(postIDs: parentIDs)
+
+        other.nodes.forEach { newNode in
+            let parentID = newNode.page.parentID ?? 0
+
+            // If the new node is at the root level, then simply add it as a child
+            if parentID == 0 {
+                nodes.append(newNode)
+                return
+            }
+
+            // The new node is not at the root level, find its parent in the root level nodes.
+            if let parentNode = parentNodes[parentID] {
+                parentNode.children.append(newNode)
+                newNode.parentNode = parentNode
+            } else {
+                // New orphan
+                nodes.append(newNode)
+                orphanNodes[parentID, default: []].append(nodes.count - 1)
+            }
+        }
+    }
+
+    /// Find the node for the given page ids
+    private func findNodes(postIDs originalIDs: Set<NSNumber>) -> [NSNumber: TreeNode] {
+        guard !originalIDs.isEmpty else {
+            return [:]
+        }
+
+        var ids = originalIDs
+        var result = [NSNumber: TreeNode]()
+
+        // The new node is not at the root level, find its parent in the root level nodes.
+        for child in nodes {
+            if ids.isEmpty {
+                break
+            }
+
+            // Using BFS under the assumption that page tree in most sites is a shallow tree, where most pages are in top layers.
+            child.breadthFirstSearch { node in
+                let postID = node.page.postID ?? 0
+                let foundIndex = ids.firstIndex(of: postID)
+                if let foundIndex {
+                    ids.remove(at: foundIndex)
+                    result[postID] = node
+                }
+                return ids.isEmpty
+            }
+        }
+
+        return result
     }
 
     func hierarchyList() -> [Page] {
@@ -328,5 +391,13 @@ class UniquePool<Value: FixedWidthInteger> {
                 return value
             }
         } while true
+    }
+}
+
+extension Array where Element == Page {
+    func printHierarchyList() {
+        forEach { page in
+            print("\(String(repeating: " ", count: page.hierarchyIndex * 4)) id: \(page.postID!), parent: \(page.parentID ?? 0)")
+        }
     }
 }
