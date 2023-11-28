a7bc0a7d27f331bfbe90bc5625c4d8fe008ed366
kean <grebenyuk.alexander@gmail.com>
2023-11-28 11:30:27 -0500

    Fix an issue with pan gesture breaking Site Media picker in single selection mode

--- WordPress/Classes/ViewRelated/Media/SiteMedia/Controllers/SiteMediaCollectionViewController.swift
+++ WordPress/Classes/ViewRelated/Media/SiteMedia/Controllers/SiteMediaCollectionViewController.swift
@@ -240,7 +240,7 @@ final class SiteMediaCollectionViewController: UIViewController, NSFetchedResult
     }
 
     @objc private func didRecognizePanGesture(_ gesture: UIPanGestureRecognizer) {
-        guard isEditing else { return }
+        guard isEditing, allowsMultipleSelection else { return }
 
         switch gesture.state {
         case .began:
