ac1c997d3d069b645ff5c1c0ca0bf25aa7c03575
David Christiandy <1299411+dvdchr@users.noreply.github.com>
2023-12-01 17:29:56 +0700

    Enable remote feature flag

--- WordPress/Classes/Utility/BuildInformation/RemoteFeatureFlag.swift
+++ WordPress/Classes/Utility/BuildInformation/RemoteFeatureFlag.swift
@@ -67,7 +67,7 @@ enum RemoteFeatureFlag: Int, CaseIterable {
         case .readerImprovements:
             return true
         case .bloganuaryDashboardNudge:
-            return false
+            return AppConfiguration.isJetpack
         }
     }
 
