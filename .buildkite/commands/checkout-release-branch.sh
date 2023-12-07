605efa6d86e8a3f3530e9000dbcaf2446329adc1
Gio Lodi <gio@mokacoding.com>
2023-12-07 17:20:50 +1100

    Pass release version through instead of reading from env

--- .buildkite/commands/checkout-release-branch.sh
+++ .buildkite/commands/checkout-release-branch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -eu
 
-# RELEASE_VERSION is passed as an environment variable from fastlane to Buildkite
-#
-if [[ -z "${RELEASE_VERSION}" ]]; then
-    echo "RELEASE_VERSION is not set."
+RELEASE_NUMBER=$1
+
+if [[ -z "${RELEASE_NUMBER}" ]]; then
+    echo "Usage $0 <release number, e.g. 1.2.3>"
     exit 1
 fi
 
 # Buildkite, by default, checks out a specific commit. For many release actions, we need to be
 # on a release branch instead.
-BRANCH_NAME="test-release/${RELEASE_VERSION}"
+BRANCH_NAME="test-release/${RELEASE_NUMBER}"
 git fetch origin "$BRANCH_NAME"
 git checkout "$BRANCH_NAME"
--- .buildkite/commands/complete-code-freeze.sh
+++ .buildkite/commands/complete-code-freeze.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -eu
 
+RELEASE_NUMBER=$1
+
+if [[ -z "${RELEASE_NUMBER}" ]]; then
+    echo "Usage $0 <release number>"
+    exit 1
+fi
+
 echo '--- :git: Configure Git for release management'
 .buildkite/commands/configure-git-for-release-management.sh
-.buildkite/commands/checkout-release-branch.sh
+.buildkite/commands/checkout-release-branch.sh "$RELEASE_NUMBER"
 
 echo '--- :ruby: Setup Ruby tools'
 install_gems
