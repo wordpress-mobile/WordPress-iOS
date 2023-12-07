0217095fcc541a454a7f74aa09f7f65e16d2f847
Gio Lodi <gio@mokacoding.com>
2023-12-07 13:27:38 +1100

    Add automation to run code freeze completion in CI

--- fastlane/lanes/release_management_in_ci.rb
+++ fastlane/lanes/release_management_in_ci.rb
@@ -1,13 +1,31 @@
 # frozen_string_literal: true
 
+PIPELINES_ROOT = 'release-pipelines'
+
 platform :ios do
   lane :trigger_code_freeze_in_ci do
     buildkite_trigger_build(
       buildkite_organization: BUILDKITE_ORGANIZATION,
       buildkite_pipeline: BUILDKITE_PIPELINE,
       branch: DEFAULT_BRANCH,
-      pipeline_file: 'release-pipelines/code-freeze.yml',
+      pipeline_file: File.join(PIPELINES_ROOT, 'code-freeze.yml'),
       message: 'Code Freeze'
+    )
+  end
+
+  lane :trigger_complete_code_freeze_in_ci do |options|
+    release_version_key = :release_version
+    release_version = options[release_version_key]
+
+    UI.user_error!("Please specify a release version by calling this lane with a  #{release_version_key} parameter") unless release_version
+
+    buildkite_trigger_build(
+      buildkite_organization: BUILDKITE_ORGANIZATION,
+      buildkite_pipeline: BUILDKITE_PIPELINE,
+      branch: compute_release_branch_name(options:, version: release_version),
+      pipeline_file: File.join(PIPELINES_ROOT, 'complete-code-freeze.yml'),
+      message: 'Complete Code Freeze',
+      environment: { RELEASE_VERSION: release_version }
     )
   end
 end
