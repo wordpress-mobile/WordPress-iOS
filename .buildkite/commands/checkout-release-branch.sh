bc6be8b8b157564e1422f5e2d3a24f091b1910aa
Gio Lodi <gio@mokacoding.com>
2023-12-08 14:03:13 +1100

    Revert "Rewrite checkout release branch command to checkout any branch"
    This reverts commit bd2611d900f7a3d57d66c24a8fa6770070d080f5.

---
+++ .buildkite/commands/checkout-release-branch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eu
+
+RELEASE_NUMBER=$1
+
+if [[ -z "${RELEASE_NUMBER}" ]]; then
+    echo "Usage $0 <release number, e.g. 1.2.3>"
+    exit 1
+fi
+
+# Buildkite, by default, checks out a specific commit. For many release actions, we need to be
+# on a release branch instead.
+# FIXME: Don't forget to revert to "release/" only
+BRANCH_NAME="test-release/${RELEASE_NUMBER}"
+git fetch origin "$BRANCH_NAME"
+git checkout "$BRANCH_NAME"
