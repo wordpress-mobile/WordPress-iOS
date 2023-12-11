3a4368a2bb22b6faceaa399136d40c8f2e8d0190
Gio Lodi <gio@mokacoding.com>
2023-12-11 17:47:28 +1100

    Redefine JetpackStatsWidgetsCore as a `LibraryModule`

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -2,18 +2,63 @@
 
 import PackageDescription
 
-let jetpackStatsWidgetsCoreName = "JetpackStatsWidgetsCore"
+struct LibraryModule {
+
+    enum UnitTests {
+        case none
+        case some([Target.Dependency] = [])
+    }
+
+    /// The name of the library. Example: FeatureXBusinessLogic.
+    let name: String
+
+    /// The production code dependencies, if any.
+    let dependencies: [Target.Dependency]
+
+    /// The library unit tests configuration
+    let unitTests: UnitTests
+
+    var product: Product {
+        .library(name: name, targets: [name])
+    }
+
+    var target: Target {
+        .target(name: name, dependencies: dependencies)
+    }
+
+    var testTarget: Target? {
+        switch unitTests {
+        case .none:
+            return .none
+        case .some(let dependencies):
+            return .testTarget(
+                name: "\(name)Tests",
+                dependencies: [.target(name: target.name)] + dependencies
+            )
+        }
+    }
+
+    var targets: [Target]  {
+        [target, testTarget].compactMap { $0 }
+    }
+
+    init(
+        name: String,
+        dependencies: [Target.Dependency] = [],
+        unitTests: UnitTests = .some([])
+    ) {
+        self.name = name
+        self.dependencies = dependencies
+        self.unitTests = unitTests
+    }
+}
+
+let jetpackStatsWidgetsCore = LibraryModule(name: "JetpackStatsWidgetsCore")
 
 let package = Package(
     name: "Modules",
     products: [
-        .library(name: jetpackStatsWidgetsCoreName, targets: [jetpackStatsWidgetsCoreName]),
+        jetpackStatsWidgetsCore.product
     ],
-    targets: [
-        .target(name: jetpackStatsWidgetsCoreName),
-        .testTarget(
-            name: "\(jetpackStatsWidgetsCoreName)Tests",
-            dependencies: [.target(name: jetpackStatsWidgetsCoreName)]
-        )
-    ]
+    targets: jetpackStatsWidgetsCore.targets
 )
