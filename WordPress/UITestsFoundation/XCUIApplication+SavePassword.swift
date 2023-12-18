85f8c5d011c3922ff529832fc6218d0130845103
Jos <jostnes@users.noreply.github.com>
2023-12-18 11:55:05 +0800

    decrease timeout

--- WordPress/UITestsFoundation/XCUIApplication+SavePassword.swift
+++ WordPress/UITestsFoundation/XCUIApplication+SavePassword.swift
@@ -6,7 +6,7 @@ extension XCUIApplication {
     // This method encapsulates the logic to dimiss the prompt.
     func dismissSavePasswordPrompt() {
         XCTContext.runActivity(named: "Dismiss save password prompt if needed.") { _ in
-            guard buttons["Save Password"].waitForExistence(timeout: 30) else { return  }
+            guard buttons["Save Password"].waitForExistence(timeout: 20) else { return  }
 
             // There should be no need to wait for this button to exist since it's part of the same
             // alert where "Save Password" is.
