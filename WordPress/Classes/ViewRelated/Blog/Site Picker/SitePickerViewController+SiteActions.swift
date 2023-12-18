08d494b0071feddc2b3e9ee5a3a71adb4d6b2c7e
Momo Ozawa <momo.ozawa@automattic.com>
2023-12-18 15:40:27 +0000

    add an add site option

--- WordPress/Classes/ViewRelated/Blog/Site Picker/SitePickerViewController+SiteActions.swift
+++ WordPress/Classes/ViewRelated/Blog/Site Picker/SitePickerViewController+SiteActions.swift
@@ -22,6 +22,7 @@ extension SitePickerViewController {
     private func makePrimarySection() -> UIMenu {
         UIMenu(options: .displayInline, children: [
             MenuItem.visitSite(visitSiteTapped),
+            MenuItem.addSite(addSiteTapped),
             MenuItem.switchSite(siteSwitcherTapped)
         ].map { $0.toAction })
     }
@@ -39,6 +40,13 @@ extension SitePickerViewController {
         UIMenu(options: .displayInline, children: [
             MenuItem.personalizeHome(personalizeHomeTapped).toAction
         ])
+    }
+
+    private func addSiteTapped() {
+        guard let parent = parent as? MySiteViewController else {
+            return
+        }
+        parent.launchSiteCreation(source: "my_site")
     }
 
     private func personalizeHomeTapped() {
@@ -60,6 +68,7 @@ extension SitePickerViewController {
 
 private enum MenuItem {
     case visitSite(_ handler: () -> Void)
+    case addSite(_ handler: () -> Void)
     case switchSite(_ handler: () -> Void)
     case siteTitle(_ handler: () -> Void)
     case personalizeHome(_ handler: () -> Void)
@@ -67,6 +76,7 @@ private enum MenuItem {
     var title: String {
         switch self {
         case .visitSite: return Strings.visitSite
+        case .addSite: return Strings.addSite
         case .switchSite: return Strings.switchSite
         case .siteTitle: return Strings.siteTitle
         case .personalizeHome: return Strings.personalizeHome
@@ -76,6 +86,7 @@ private enum MenuItem {
     var icon: UIImage? {
         switch self {
         case .visitSite: return UIImage(systemName: "safari")
+        case .addSite: return UIImage(systemName: "plus")
         case .switchSite: return UIImage(systemName: "arrow.triangle.swap")
         case .siteTitle: return UIImage(systemName: "character")
         case .personalizeHome: return UIImage(systemName: "slider.horizontal.3")
@@ -85,6 +96,7 @@ private enum MenuItem {
     var toAction: UIAction {
         switch self {
         case .visitSite(let handler),
+             .addSite(let handler),
              .switchSite(let handler),
              .siteTitle(let handler),
              .personalizeHome(let handler):
@@ -95,6 +107,7 @@ private enum MenuItem {
 
 private enum Strings {
     static let visitSite = NSLocalizedString("mySite.siteActions.visitSite", value: "Visit site", comment: "Menu title for the visit site option")
+    static let addSite = NSLocalizedString("mySite.siteActions.addSite", value: "Add site", comment: "Menu title for the add site option")
     static let switchSite = NSLocalizedString("mySite.siteActions.switchSite", value: "Switch site", comment: "Menu title for the switch site option")
     static let siteTitle = NSLocalizedString("mySite.siteActions.siteTitle", value: "Change site title", comment: "Menu title for the change site title option")
     static let siteIcon = NSLocalizedString("mySite.siteActions.siteIcon", value: "Change site icon", comment: "Menu title for the change site icon option")
