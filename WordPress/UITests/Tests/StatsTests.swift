d8ec5d2f6b46771d6cc24c5fcd0a9bbc3608d94c
Jos <jostnes@users.noreply.github.com>
2023-12-18 15:34:10 +0800

    fix stats test by making year dynamic

--- WordPress/UITests/Tests/StatsTests.swift
+++ WordPress/UITests/Tests/StatsTests.swift
@@ -22,42 +22,43 @@ class StatsTests: XCTestCase {
         takeScreenshotOfFailedTest()
     }
 
-    let insightsStats: [String] = [
-        "Your views in the last 7-days are -9 (-82%) lower than the previous 7-days. ",
-        "Thursday",
-        "34% of views",
-        "Best Hour",
-        "4 AM",
-        "25% of views"
-    ]
-
-    let yearsStats: [String] = [
-        "9,148",
-        "+7,933 (653%)",
-        "United States, 60",
-        "Canada, 44",
-        "Germany, 15",
-        "France, 14",
-        "United Kingdom, 12",
-        "India, 121"
-    ]
-
-    let yearsChartBars: [String] = [
-        "Views,  2019: 9148",
-        "Visitors,  2019: 4216",
-        "Views,  2018: 1215",
-        "Visitors,  2018: 632",
-        "Views,  2017: 788",
-        "Visitors,  2017: 465"
-    ]
-
     func testInsightsStatsLoadProperly() throws {
+        let insightsStats: [String] = [
+            "Your views in the last 7-days are -9 (-82%) lower than the previous 7-days. ",
+            "Thursday",
+            "34% of views",
+            "Best Hour",
+            "4 AM",
+            "25% of views"
+        ]
+
         try StatsScreen()
             .switchTo(mode: "insights")
             .assertStatsAreLoaded(insightsStats)
     }
 
     func testYearsStatsLoadProperly() throws {
+        let yearsStats: [String] = [
+            "9,148",
+            "+7,933 (653%)",
+            "United States, 60",
+            "Canada, 44",
+            "Germany, 15",
+            "France, 14",
+            "United Kingdom, 12",
+            "India, 121"
+        ]
+
+        let currentYear = Calendar.current.component(.year, from: Date())
+        let yearsChartBars: [String] = [
+            "Views,  \(currentYear): 9148",
+            "Visitors,  \(currentYear): 4216",
+            "Views,  \(currentYear - 1): 1215",
+            "Visitors,  \(currentYear - 1): 632",
+            "Views,  \(currentYear - 2): 788",
+            "Visitors,  \(currentYear - 2): 465"
+        ]
+
         try StatsScreen()
             .switchTo(mode: "years")
             .assertStatsAreLoaded(yearsStats)
