c6c4e81ec033b5877ec634cadf828098111f4376
Povilas Staskus <povilas.staskus@automattic.com>
2023-12-20 14:10:54 +0200

    Update PresentationCard with a new UI
    Include domains search view and add spacing, and styles according to requirements

--- WordPress/Classes/ViewRelated/Domains/Views/PresentationCard.swift
+++ WordPress/Classes/ViewRelated/Domains/Views/PresentationCard.swift
@@ -8,25 +8,35 @@ struct PresentationCard: View {
     let destinationTitle: String
     @Binding var isShowingDestination: Bool
 
-    private let titleFontSize: CGFloat = 28
-
     var body: some View {
-        VStack {
-            Text(title)
-                .font(Font.system(size: titleFontSize, weight: .regular, design: .serif))
-                .padding()
-            Text(description)
-                .multilineTextAlignment(.center)
-                .fixedSize(horizontal: false, vertical: true)
-            DSButton(
-                title: title,
-                style: .init(
-                    emphasis: .primary,
-                    size: .large,
-                    isJetpack: AppConfiguration.isJetpack
-                )) {
-                    isShowingDestination = true
+        VStack(spacing: Length.Padding.medium) {
+            VStack(spacing: Length.Padding.single) {
+                VStack(spacing: -Length.Padding.large) {
+                    DashboardDomainsCardSearchView()
+                    Text(title)
+                        .style(.heading4)
+                        .foregroundColor(.DS.Foreground.primary)
                 }
+
+                Text(description)
+                    .style(.bodyLarge(.regular))
+                    .foregroundColor(.DS.Foreground.secondary)
+                    .multilineTextAlignment(.center)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .padding(.horizontal, Length.Padding.single)
+            }
+
+            VStack(spacing: Length.Padding.single) {
+                DSButton(
+                    title: title,
+                    style: .init(
+                        emphasis: .primary,
+                        size: .large,
+                        isJetpack: AppConfiguration.isJetpack
+                    )) {
+                        isShowingDestination = true
+                    }
+            }
         }
     }
 }
