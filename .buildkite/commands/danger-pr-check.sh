43c3a1a4b91c99591c70f18c8d1443cd04b0a08b
Ian Maia <ian.maia@automattic.com>
2023-12-22 18:40:48 +0100

    Remove Buildkite Danger step

--- .buildkite/commands/danger-pr-check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -eu
-
-echo "--- :rubygems: Setting up Gems"
-bundle install
-
-echo "--- Running Danger: PR Check"
-bundle exec danger --fail-on-errors=true --remove-previous-comments --danger_id=pr-check
