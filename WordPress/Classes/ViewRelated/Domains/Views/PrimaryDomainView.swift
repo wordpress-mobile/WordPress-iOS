ba863788e9b1b2e541fd4a7a516e070420c9daa1
Povilas Staskus <povilas.staskus@automattic.com>
2023-12-26 20:02:36 +0200

    Create PrimaryDomainView
    This view shows a primary domain badge

---
+++ WordPress/Classes/ViewRelated/Domains/Views/PrimaryDomainView.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+import DesignSystem
+
+struct PrimaryDomainView: View {
+    var body: some View {
+        Group {
+            HStack(spacing: Length.Padding.half) {
+                Image(systemName: "globe")
+                    .font(.callout)
+                    .foregroundStyle(Color.DS.Foreground.primary)
+                Text(Strings.primaryDomain)
+                    .font(.callout)
+                    .foregroundStyle(Color.DS.Foreground.primary)
+            }
+            .padding(.vertical, Length.Padding.half)
+            .padding(.horizontal, Length.Padding.single)
+        }
+        .background(Color.DS.Background.secondary)
+        .clipShape(RoundedRectangle(cornerRadius: Length.Radius.small))
+    }
+}
+
+private extension PrimaryDomainView {
+    enum Strings {
+        static let primaryDomain = NSLocalizedString("site.domains.primaryDomain.title",
+                                                     value: "Primary domain",
+                                                     comment: "Primary domain label, used in the site address section of the Domains Dashboard.")
+    }
+}
