d7e4da98e0f8bb8f37145a55c1e0273dc7988852
David Christiandy <1299411+dvdchr@users.noreply.github.com>
2024-01-05 01:50:58 +0700

    Show the filter sheet when tapped

--- WordPress/Classes/ViewRelated/Reader/Tab Navigation/ReaderTabViewController.swift
+++ WordPress/Classes/ViewRelated/Reader/Tab Navigation/ReaderTabViewController.swift
@@ -22,15 +22,15 @@ class ReaderTabViewController: UIViewController {
 
         ReaderCardService().clean()
 
-        viewModel.filterTapped = { [weak self] (fromView, completion) in
+        viewModel.filterTapped = { [weak self] (filter, fromView, completion) in
             guard let self = self else {
                 return
             }
 
-            self.viewModel.presentFilter(from: self, sourceView: fromView, completion: { [weak self] topic in
+            self.viewModel.presentFilter(filter: filter, from: self, sourceView: fromView) { [weak self] topic in
                 self?.dismiss(animated: true, completion: nil)
                 completion(topic)
-            })
+            }
         }
 
         NotificationCenter.default.addObserver(self, selector: #selector(defaultAccountDidChange(_:)), name: NSNotification.Name.WPAccountDefaultWordPressComAccountChanged, object: nil)
