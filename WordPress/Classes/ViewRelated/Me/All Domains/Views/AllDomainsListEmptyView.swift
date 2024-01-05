4b10eba8caf384679b496ba581914643d6bd35ff
Povilas Staskus <povilas.staskus@automattic.com>
2024-01-05 08:16:25 +0000

    Use DomainsStateView within AllDomainsView

--- WordPress/Classes/ViewRelated/Me/All Domains/Views/AllDomainsListEmptyView.swift
+++ WordPress/Classes/ViewRelated/Me/All Domains/Views/AllDomainsListEmptyView.swift
@@ -1,52 +1,19 @@
 import UIKit
 import WordPressUI
 import DesignSystem
+import SwiftUI
 
 final class AllDomainsListEmptyView: UIView {
 
     typealias ViewModel = DomainsStateViewModel
 
-    private enum Appearance {
-        static let labelsSpacing: CGFloat = Length.Padding.single
-        static let labelsButtonSpacing: CGFloat = Length.Padding.large
-        static let titleLabelFont: UIFont = WPStyleGuide.fontForTextStyle(.title2, fontWeight: .bold)
-        static let descriptionLabelFont: UIFont = WPStyleGuide.fontForTextStyle(.callout, fontWeight: .regular)
-        static let buttonLabelFont: UIFont = WPStyleGuide.fontForTextStyle(.body, fontWeight: .regular)
-        static let titleLabelColor: UIColor? = UIColor.DS.Foreground.secondary
-        static let descriptionLabelColor: UIColor? = UIColor.DS.Foreground.secondary
-    }
-
-    // MARK: - Actions
-
-    private var buttonAction: (() -> Void)?
-
     // MARK: - Views
 
-    private let titleLabel: UILabel = {
-        let label = UILabel()
-        label.textAlignment = .center
-        label.font = Appearance.titleLabelFont
-        label.textColor = Appearance.titleLabelColor
-        label.numberOfLines = 2
-        label.adjustsFontForContentSizeCategory = true
-        return label
-    }()
-
-    private let descriptionLabel: UILabel = {
-        let label = UILabel()
-        label.textAlignment = .center
-        label.font = Appearance.descriptionLabelFont
-        label.textColor = Appearance.descriptionLabelColor
-        label.numberOfLines = 0
-        label.adjustsFontForContentSizeCategory = true
-        return label
-    }()
-
-    private let button: UIButton = {
-        let button = FancyButton()
-        button.titleLabel?.font = Appearance.buttonLabelFont
-        button.isPrimary = true
-        return button
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.backgroundColor = .clear
+        return stackView
     }()
 
     // MARK: - Init
@@ -63,33 +30,18 @@ final class AllDomainsListEmptyView: UIView {
     // MARK: - Rendering
 
     private func render(with viewModel: ViewModel?) {
-        let stackView = UIStackView(arrangedSubviews: [titleLabel, descriptionLabel, button])
-        stackView.axis = .vertical
-        stackView.alignment = .fill
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.spacing = Appearance.labelsSpacing
-        stackView.setCustomSpacing(Appearance.labelsButtonSpacing, after: descriptionLabel)
         self.addSubview(stackView)
         self.pinSubviewToAllEdges(stackView)
-        self.button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
         self.update(with: viewModel)
     }
 
     func update(with viewModel: ViewModel?) {
-        self.titleLabel.text = viewModel?.title
-        self.descriptionLabel.text = viewModel?.description
-        if let button = viewModel?.button {
-            self.button.isHidden = false
-            self.button.setTitle(button.title, for: .normal)
-            self.buttonAction = button.action
-        } else {
-            self.button.isHidden = true
+        stackView.removeAllSubviews()
+
+        if let viewModel = viewModel,
+           let stateView = UIHostingController(rootView: DomainsStateView(viewModel: viewModel)).view {
+            stateView.backgroundColor = .clear
+            stackView.addArrangedSubview(stateView)
         }
-    }
-
-    // MARK: - User Interaction
-
-    @objc private func didTapButton() {
-        self.buttonAction?()
     }
 }
