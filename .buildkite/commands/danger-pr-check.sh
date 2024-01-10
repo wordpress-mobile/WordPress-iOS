6cca32edb14624c531c393e4825f8679a73f6e4f
Ian Maia <ian.maia@automattic.com>
2024-01-10 16:41:37 +0100

    Add github.com to known_hosts

--- .buildkite/commands/danger-pr-check.sh
+++ .buildkite/commands/danger-pr-check.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -eu
 
+HOST="github.com"
+echo "--- :github: Adding ${HOST} to '~/.ssh/known_hosts'..."
+for ip in $(dig @8.8.8.8 "${HOST}" +short); do ssh-keyscan "${HOST}","$ip"; ssh-keyscan "$ip"; done 2>/dev/null >> ~/.ssh/known_hosts || true
+
 echo "--- :rubygems: Setting up Gems"
 bundle install
 
