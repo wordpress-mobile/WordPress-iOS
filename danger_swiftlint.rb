41e69662bb3b7433e8225210d15d1901fa3da0c6
Gio Lodi <gio@mokacoding.com>
2024-01-18 09:16:37 +1100

    HACK - Remove other pipeline steps for faster feedback

---
+++ danger_swiftlint.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Dangerfile for the sole purpose of running SwiftLint and
+# annotating PRs with it.
+
+swiftlint.config_file = '.swiftlint.yml'
+swiftlint.binary_path = './Pods/SwiftLint/swiftlint'
+swiftlint.lint_files inline_mode: true
