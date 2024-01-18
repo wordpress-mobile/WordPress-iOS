35a048e14d74fe7f0ad61fed5ba63969b92df0a6
Salim Braksa <salim.braksa@gmail.com>
2024-01-18 14:53:33 +0100

    Convert InAppFeedbackCoordinator to class

--- WordPress/Classes/Utility/In-App Feedback/InAppFeedbackPromptCoordinator.swift
+++ WordPress/Classes/Utility/In-App Feedback/InAppFeedbackPromptCoordinator.swift
@@ -5,7 +5,7 @@ protocol InAppFeedbackPromptPresenting {
     func presentIfNeeded(in controller: UIViewController) -> Bool
 }
 
-struct InAppFeedbackPromptCoordinator: InAppFeedbackPromptPresenting {
+final class InAppFeedbackPromptCoordinator: InAppFeedbackPromptPresenting {
 
     // MARK: - Properties
 
@@ -40,11 +40,11 @@ struct InAppFeedbackPromptCoordinator: InAppFeedbackPromptPresenting {
             message: Strings.FeedbackAlert.message,
             preferredStyle: .alert
         )
-        let yes = UIAlertAction(title: Strings.FeedbackAlert.yes, style: .default) { _ in
-            self.handlePositiveFeedback(in: controller)
+        let yes = UIAlertAction(title: Strings.FeedbackAlert.yes, style: .default) { [weak self] _ in
+            self?.handlePositiveFeedback(in: controller)
         }
-        let no = UIAlertAction(title: Strings.FeedbackAlert.no, style: .default) { _ in
-            self.handleNegativeFeedback(in: controller)
+        let no = UIAlertAction(title: Strings.FeedbackAlert.no, style: .default) { [weak self] _ in
+            self?.handleNegativeFeedback(in: controller)
         }
         alert.addAction(no)
         alert.addAction(yes)
@@ -77,13 +77,16 @@ struct InAppFeedbackPromptCoordinator: InAppFeedbackPromptPresenting {
             message: Strings.NegativeFeedbackAlert.message,
             preferredStyle: .alert
         )
-        let yes = UIAlertAction(title: Strings.NegativeFeedbackAlert.yes, style: .default) { _ in
+        let yes = UIAlertAction(title: Strings.NegativeFeedbackAlert.yes, style: .default) { [weak self] _ in
+            guard let self else {
+                return
+            }
             let destination = UINavigationController(rootViewController: submitFeedbackViewController(in: controller))
             controller.present(destination, animated: true)
         }
-        let no = UIAlertAction(title: Strings.NegativeFeedbackAlert.no, style: .default) { _ in
+        let no = UIAlertAction(title: Strings.NegativeFeedbackAlert.no, style: .default) { [weak self] _ in
             WPAnalytics.track(.appReviewsDeclinedToRateApp)
-            self.appRatingUtility.declinedToRateCurrentVersion()
+            self?.appRatingUtility.declinedToRateCurrentVersion()
         }
         alert.addAction(no)
         alert.addAction(yes)
--- WordPress/Classes/ViewRelated/Blog/My Site/MySiteOverlaysCoordinator.swift
+++ WordPress/Classes/ViewRelated/Blog/My Site/MySiteOverlaysCoordinator.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-class MySiteOverlaysCoordinator {
+final class MySiteOverlaysCoordinator {
 
     // MARK: - Dependencies
 
@@ -17,7 +17,16 @@ class MySiteOverlaysCoordinator {
 
     // MARK: - API
 
-    /// Ensure only 1 overlay is presented at a time.
+    /// Presents an overlay on the specified view controller if conditions are met.
+    /// This method ensures that only one overlay is presented at a time by sequentially
+    /// checking and presenting overlays from various coordinators. Once an overlay is presented,
+    /// the respective coordinator is cleared to prevent multiple overlays from showing concurrently.
+    ///
+    /// The method checks for the following overlays in order of priority:
+    /// 1. Compliance Coordinator: Presents compliance-related content if required.
+    /// 2. In-App Feedback Coordinator: Presents in-app feedback if no compliance overlay is needed.
+    ///
+    /// - Parameter viewController: The UIViewController instance on which to potentially present an overlay.
     @MainActor func presentOverlayIfNeeded(in viewController: UIViewController) async {
         if let complianceCoordinator, await complianceCoordinator.presentIfNeeded() {
             self.complianceCoordinator = nil
