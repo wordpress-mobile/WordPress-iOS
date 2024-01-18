104b0c52a1168c8bc73da5fee16d942972ba7c48
Gio Lodi <gio@mokacoding.com>
2024-01-18 14:59:13 +1100

    Add `fail_on_error` to see if errors are reported correctly inline

--- danger_swiftlint.rb
+++ danger_swiftlint.rb
@@ -5,4 +5,4 @@
 
 swiftlint.config_file = '.swiftlint.yml'
 swiftlint.binary_path = './Pods/SwiftLint/swiftlint'
-swiftlint.lint_files inline_mode: true
+swiftlint.lint_files(inline_mode: true, fail_on_error: true)
