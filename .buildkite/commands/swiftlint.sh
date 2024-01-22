5a0cdd671dac6ef806fd82dc79ab13d53fa17120
Gio Lodi <gio@mokacoding.com>
2024-01-22 16:01:39 +1100

    Adopt SwiftLint CI improvements from @iangmaia Pocket Casts work
    See
    https://github.com/Automattic/pocket-casts-ios/commit/475e4e97936e4b7b79996d0687e45dc55fb4b8ec

--- .buildkite/commands/swiftlint.sh
+++ .buildkite/commands/swiftlint.sh
@@ -2,22 +2,22 @@
 
 echo "--- :swift: Running SwiftLint"
 
-# Using --strict to fail the build and increase the likelyhood devs will notice the violations
-swiftlint_output=$(swiftlint lint --quiet --strict --reporter csv)
-swiftlint_exit_status=$?
+set +e
+SWIFTLINT_OUTPUT=$(swiftlint lint --quiet "$@" --reporter relative-path)
+SWIFTLINT_EXIT_STATUS=$?
+set -e
 
-# TODO: It's likely that using --strict will result in all violations being errors
-warnings=$(echo "$swiftlint_output" | awk -F',' '$4=="Warning" {print "- `"$1":"$2"`: "$6}')
-errors=$(echo "$swiftlint_output" | awk -F',' '$4=="Error" {print "- `"$1":"$2"`: "$6}')
+WARNINGS=$(echo -e "$SWIFTLINT_OUTPUT" | awk -F': ' '/: warning:/ {printf "- `%s`: %s\n", $1, $4}')
+ERRORS=$(echo -e "$SWIFTLINT_OUTPUT" | awk -F': ' '/: error:/ {printf "- `%s`: %s\n", $1, $4}')
 
-if [ -n "$warnings" ]; then
-  echo "$warnings"
-  printf '**SwiftLint Warnings**\n%b' "$warnings" | buildkite-agent annotate --style 'warning'
+if [ -n "$WARNINGS" ]; then
+  echo "$WARNINGS"
+  printf "**SwiftLint Warnings**\n%b" "$WARNINGS" | buildkite-agent annotate --style 'warning'
 fi
 
-if [ -n "$errors" ]; then
-  echo "$errors"
-  printf '**SwiftLint Errors**\n%b' "$errors" | buildkite-agent annotate --style 'error'
+if [ -n "$ERRORS" ]; then
+  echo "$ERRORS"
+  printf "**SwiftLint Errors**\n%b" "$ERRORS" | buildkite-agent annotate --style 'error'
 fi
 
-exit $swiftlint_exit_status
+exit $SWIFTLINT_EXIT_STATUS
