73ce4d2edc3220aadcf51c812e03935ea6d131e0
Derek Blank <derekpblank@gmail.com>
2024-01-25 12:10:12 +1000

    Temporarily comment CocoaLumberjack.DDLogVerbose logs to unblock CI

--- WordPress/Classes/Utility/Logging/CocoaLumberjack.swift
+++ WordPress/Classes/Utility/Logging/CocoaLumberjack.swift
@@ -3,25 +3,25 @@ import CocoaLumberjack
 
 @inlinable
 public func DDLogVerbose(_ message: @autoclosure () -> Any, file: StaticString = #file, function: StaticString = #function, line: UInt = #line) {
-    CocoaLumberjack.DDLogVerbose(message(), file: file, function: function, line: line)
+//    CocoaLumberjack.DDLogVerbose(message(), file: file, function: function, line: line)
 }
 
 @inlinable
 public func DDLogDebug(_ message: @autoclosure () -> Any, file: StaticString = #file, function: StaticString = #function, line: UInt = #line) {
-    CocoaLumberjack.DDLogDebug(message(), file: file, function: function, line: line)
+//    CocoaLumberjack.DDLogDebug(message(), file: file, function: function, line: line)
 }
 
 @inlinable
 public func DDLogInfo(_ message: @autoclosure () -> Any, file: StaticString = #file, function: StaticString = #function, line: UInt = #line) {
-    CocoaLumberjack.DDLogInfo(message(), file: file, function: function, line: line)
+//    CocoaLumberjack.DDLogInfo(message(), file: file, function: function, line: line)
 }
 
 @inlinable
 public func DDLogWarn(_ message: @autoclosure () -> Any, file: StaticString = #file, function: StaticString = #function, line: UInt = #line) {
-    CocoaLumberjack.DDLogWarn(message(), file: file, function: function, line: line)
+//    CocoaLumberjack.DDLogWarn(message(), file: file, function: function, line: line)
 }
 
 @inlinable
 public func DDLogError(_ message: @autoclosure () -> Any, file: StaticString = #file, function: StaticString = #function, line: UInt = #line) {
-    CocoaLumberjack.DDLogError(message(), file: file, function: function, line: line)
+//    CocoaLumberjack.DDLogError(message(), file: file, function: function, line: line)
 }
