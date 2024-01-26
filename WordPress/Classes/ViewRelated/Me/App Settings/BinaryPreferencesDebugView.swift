f882eb693504ade21d693e028998f1d7113b2c89
Artyom Vlasov <mr.twago@gmail.com>
2024-01-26 12:31:13 -0700

    Make binary preferences screen editable
    Additionally, refactored the screen to use Dictionaries instead of custom class to simplify the code.

--- WordPress/Classes/ViewRelated/Me/App Settings/BinaryPreferencesDebugView.swift
+++ WordPress/Classes/ViewRelated/Me/App Settings/BinaryPreferencesDebugView.swift
@@ -1,28 +1,40 @@
 import SwiftUI
 
+typealias BinaryPreferenceSections = [String: BinaryPreferences]
+typealias BinaryPreferences = [String: Bool]
+
 struct BinaryPreferencesDebugView: View {
     @StateObject private var viewModel = BinaryPreferencesDebugViewModel()
 
     var body: some View {
         List {
-            ForEach($viewModel.preferenceSections) { section in
-                Section(header: Text(section.id)
-                    .font(.caption)
-                    .multilineTextAlignment(.leading)
-                    .lineLimit(nil)) {
-                        ForEach(section.settings) { setting in
-                            Toggle(setting.id, isOn: setting.value)
-                                .font(.caption)
-                        }
+            ForEach(viewModel.preferenceSections.keys.sorted(), id: \.self) { sectionKey in
+                let sectionPreferences = viewModel.preferenceSections[sectionKey] ?? [:]
+
+                Section(header: Text(sectionKey)
+                            .font(.caption)
+                            .multilineTextAlignment(.leading)
+                            .lineLimit(nil)) {
+                    ForEach(sectionPreferences.keys.sorted(), id: \.self) { preferenceKey in
+                        let isOn = Binding<Bool>(
+                            get: { sectionPreferences[preferenceKey] ?? false },
+                            set: { newValue in viewModel.updatePreference(newValue, forSection: sectionKey, forPreference: preferenceKey) }
+                        )
+                        Toggle(preferenceKey, isOn: isOn)
+                            .font(.caption)
                     }
+                }
             }
         }
         .navigationTitle(Strings.title)
+        .onAppear {
+            viewModel.load()
+        }
     }
 }
 
 private final class BinaryPreferencesDebugViewModel: ObservableObject {
-    @Published var preferenceSections: [BinaryPreferenceSection] = []
+    @Published var preferenceSections: BinaryPreferenceSections = [:]
     private var persistenceStore: UserPersistentRepository
 
     init() {
@@ -32,56 +44,35 @@ private final class BinaryPreferencesDebugViewModel: ObservableObject {
 
     func load() {
         let allPreferences = persistenceStore.dictionaryRepresentation()
-        var loadedSections: [BinaryPreferenceSection] = []
-        var ungroupedPreferences = BinaryPreferenceSection(key: "Other", settings: [])
+        var loadedPreferenceSections: BinaryPreferenceSections = [:]
 
         allPreferences.forEach { entryKey, entryValue in
-            switch entryValue {
-            case let groupedPreferences as [String: Bool]:
-                loadedSections.append(
-                    BinaryPreferenceSection(
-                        key: entryKey,
-                        settings: groupedPreferences.map {
-                            BinaryPreference(key: $0.key, value: $0.value) }))
-            case let binaryPreference as Bool where !isSystemPreference(entryKey):
-                ungroupedPreferences.settings.append(BinaryPreference(key: entryKey, value: binaryPreference))
-            default:
-                break
+            if let groupedPreferences = entryValue as? BinaryPreferences {
+                loadedPreferenceSections[entryKey] = groupedPreferences
+            } else if let binaryPreference = entryValue as? Bool, !isSystemPreference(entryKey) {
+                loadedPreferenceSections[Strings.otherPreferencesSectionID, default: [:]][entryKey] = binaryPreference
             }
         }
 
-        if !ungroupedPreferences.settings.isEmpty {
-            loadedSections.append(ungroupedPreferences)
+        preferenceSections = loadedPreferenceSections
+    }
+
+    func updatePreference(_ value: Bool, forSection sectionID: String, forPreference preferenceID: String) {
+        if sectionID == Strings.otherPreferencesSectionID {
+            persistenceStore.set(value, forKey: preferenceID)
+        } else if var section = preferenceSections[sectionID] {
+            section[preferenceID] = value
+            persistenceStore.set(section, forKey: sectionID)
         }
-
-        preferenceSections = loadedSections
+        load()
     }
 
-    func isSystemPreference(_ key: String) -> Bool {
+    private func isSystemPreference(_ key: String) -> Bool {
         key.starts(with: "com.wordpress.")
-    }
-}
-
-private struct BinaryPreferenceSection: Identifiable {
-    let id: String
-    var settings: [BinaryPreference]
-
-    init(key: String, settings: [BinaryPreference]) {
-        self.id = key
-        self.settings = settings
-    }
-}
-
-private struct BinaryPreference: Identifiable {
-    let id: String
-    var value: Bool
-
-    init(key: String, value: Bool) {
-        self.id = key
-        self.value = value
     }
 }
 
 private enum Strings {
     static let title = NSLocalizedString("debugMenu.binaryPreferences.title", value: "Binary Preferences", comment: "Binary Preferences Debug Menu screen title")
+    static let otherPreferencesSectionID = "Other"
 }
