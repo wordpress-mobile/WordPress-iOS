b30da69d85551001262c8557a86882ef0ffc53a7
Salim Braksa <salim.braksa@gmail.com>
2024-02-02 18:25:33 +0100

    Persist user default setting without needing to reload allUserDefaultsSections

--- WordPress/Classes/ViewRelated/Me/App Settings/Boolean User Defaults/BooleanUserDefaultsDebugView.swift
+++ WordPress/Classes/ViewRelated/Me/App Settings/Boolean User Defaults/BooleanUserDefaultsDebugView.swift
@@ -5,10 +5,10 @@ struct BooleanUserDefaultsDebugView: View {
 
     var body: some View {
         List {
-            ForEach(viewModel.userDefaultsSections, id: \.key) { section in
+            ForEach(viewModel.userDefaultsSections, id: \.id) { section in
                 Section(header: Text(section.key)
                     .font(.caption)) {
-                        ForEach(section.rows, id: \.key) { row in
+                        ForEach(section.rows, id: \.id) { row in
                             let isOn = Binding<Bool>(
                                 get: {
                                     row.value
@@ -16,8 +16,8 @@ struct BooleanUserDefaultsDebugView: View {
                                 set: { newValue in
                                     viewModel.updateUserDefault(
                                         newValue,
-                                        forSection: section.key,
-                                        forRow: row.key
+                                        section: section,
+                                        row: row
                                     )
                                 }
                             )
