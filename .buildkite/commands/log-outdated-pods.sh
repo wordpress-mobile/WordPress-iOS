d168e4bdf563c93da8073949b92bd17bc19fe81a
Gio Lodi <gio@mokacoding.com>
2024-02-20 15:45:46 +1100

    Add step to run `pod outdated` after code freeze completion

---
+++ .buildkite/commands/log-outdated-pods.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -eu
+
+RELEASE_NUMBER=$1
+
+if [[ -z "${RELEASE_NUMBER}" ]]; then
+    echo "Usage $0 <release number>"
+    exit 1
+fi
+
+echo '--- :git: Checkout release branch'
+.buildkite/commands/configure-git-for-release-management.sh
+.buildkite/commands/checkout-release-branch.sh "$RELEASE_NUMBER"
+
+echo '--- :ruby: Setup Ruby tools'
+install_gems
+
+echo '--- :closed_lock_with_key: Access secrets'
+bundle exec fastlane run configure_apply
+
+echo '+++ :cocoapods: Outdated Pods'
+bundle exec pod outdated
