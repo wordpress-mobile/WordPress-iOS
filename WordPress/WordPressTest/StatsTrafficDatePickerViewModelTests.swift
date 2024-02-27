6f641074177b752ef1a0e9de81ba91655db91a92
Paul Von Schrottky <paul.von.schrottky@automattic.com>
2024-02-26 22:05:51 -0300

    Updated tests

--- WordPress/WordPressTest/StatsTrafficDatePickerViewModelTests.swift
+++ WordPress/WordPressTest/StatsTrafficDatePickerViewModelTests.swift
@@ -3,74 +3,33 @@ import XCTest
 
 final class StatsTrafficDatePickerViewModelTests: XCTestCase {
     private var viewModel: StatsTrafficDatePickerViewModel!
-    let today = Date("2023-01-18")
-    let startOfWeek = Date("2023-01-15")
-    let endOfWeek = Date("2023-01-21")
-    let startOfMonth = Date("2023-01-01")
-    let endOfMonth = Date("2023-01-31")
-    let startOfYear = Date("2023-01-01")
-    let endOfYear = Date("2023-12-31")
-
-    let yesterday = Date("2023-01-17")
+    let today = Date("2024-01-18")
 
     override func setUpWithError() throws {
-        viewModel = StatsTrafficDatePickerViewModel(now: today)
+        viewModel = StatsTrafficDatePickerViewModel(period: .day, date: today)
     }
 
     override func tearDownWithError() throws {
         viewModel = nil
     }
 
-    func testDefaultPeriodIsDay() throws {
-        XCTAssertTrue(viewModel.selectedPeriod == .day)
-    }
+    func testNavigation() throws {
 
-    func testDefaultDateInterval() throws {
-        let dateInterval = viewModel.currentDateInterval
-        XCTAssertEqual(today, dateInterval.start)
-        XCTAssertEqual(today, dateInterval.end)
-    }
+        let yesterday = Date("2024-01-17")
+        let aWeekEarlier = Date("2024-01-10")
+        let aMonthEarlier = Date("2023-12-10")
 
-    func testWeekInterval() throws {
-        viewModel.selectedPeriod = .week
-        let dateInterval = viewModel.currentDateInterval
-        XCTAssertTrue(dateInterval.start.isSameDay(as: startOfWeek))
-        XCTAssertTrue(dateInterval.end.isSameDay(as: endOfWeek))
-    }
+        viewModel.goToPreviousPeriod()
+        XCTAssertEqual(yesterday, viewModel.date)
 
-    func testMonthInterval() throws {
-        viewModel.selectedPeriod = .month
-        let dateInterval = viewModel.currentDateInterval
-        XCTAssertTrue(dateInterval.start.isSameDay(as: startOfMonth))
-        XCTAssertTrue(dateInterval.end.isSameDay(as: endOfMonth))
-    }
+        viewModel.period = .week
+        viewModel.goToPreviousPeriod()
+        XCTAssertEqual(aWeekEarlier, viewModel.date)
 
-    func testYearInterval() throws {
-        viewModel.selectedPeriod = .year
-        let dateInterval = viewModel.currentDateInterval
-        XCTAssertTrue(dateInterval.start.isSameDay(as: startOfYear))
-        XCTAssertTrue(dateInterval.end.isSameDay(as: endOfYear))
+        viewModel.period = .month
+        viewModel.goToPreviousPeriod()
+        XCTAssertEqual(aMonthEarlier, viewModel.date)
     }
-
-    func testYesterdayInterval() throws {
-        viewModel.selectedPeriod = .day
-        viewModel.goToPreviousDateInterval()
-        XCTAssertEqual(yesterday, viewModel.currentDateInterval.start)
-        XCTAssertEqual(yesterday, viewModel.currentDateInterval.end)
-    }
-
-    func testTomorrowIntervalNotAvailable() throws {
-        viewModel.selectedPeriod = .day
-        XCTAssertFalse(viewModel.isNextDateIntervalAvailable)
-    }
-
-    func testStartOfWeekIsusedWhenSwitchingFromWeekToDay() throws {
-        viewModel.selectedPeriod = .week
-        viewModel.selectedPeriod = .day
-        XCTAssertEqual(startOfWeek, viewModel.currentDateInterval.start)
-        XCTAssertEqual(startOfWeek, viewModel.currentDateInterval.end)
-    }
-
 }
 
 private extension Date {
