69ba98aa7143c9464138d4de554d18d1b5291aac
Paul Von Schrottky <paul.von.schrottky@automattic.com>
2024-03-12 13:18:08 -0300

    Filter out only partner themes

--- WordPress/Classes/Services/ThemeService.m
+++ WordPress/Classes/Services/ThemeService.m
@@ -482,7 +482,7 @@ const NSInteger ThemeOrderTrailing = 9999;
     }
     theme.popularityRank = remoteTheme.popularityRank;
     theme.previewUrl = remoteTheme.previewUrl;
-    BOOL availableFree = remoteTheme.purchased.boolValue || [remoteTheme.tier isEqualToString:@"free"];
+    BOOL availableFree = remoteTheme.purchased.boolValue || ![remoteTheme.type isEqualToString:@"managed-external"];
     theme.premium = @(!availableFree);
     theme.price = remoteTheme.price;
     theme.purchased = remoteTheme.purchased;
