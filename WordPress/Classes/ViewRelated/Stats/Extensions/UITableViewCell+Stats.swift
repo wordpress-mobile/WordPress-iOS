98d5b7719b648da5a67260f80de33ac99d9235cd
Povilas Staskus <povilas.staskus@automattic.com>
2024-03-18 16:45:44 +0200

    Optimize TopTotalsCell to add rows only when the cell loads
    TopTotalsCell was calling addRows on every configuration of cell which in turn created and added a hierarchy of UIStackView-based views.
    
    Optimizing TopTotalsCell to only add rows once and then make manipulations on existing rows.

--- WordPress/Classes/ViewRelated/Stats/Extensions/UITableViewCell+Stats.swift
+++ WordPress/Classes/ViewRelated/Stats/Extensions/UITableViewCell+Stats.swift
@@ -71,3 +71,84 @@ extension UITableViewCell {
     }
 
 }
+
+ extension UITableViewCell {
+    struct StatsTotalRowConfiguration {
+        let limitRowsDisplayed: Bool
+        let rowDelegate: StatsTotalRowDelegate?
+        let referrerDelegate: StatsTotalRowReferrerDelegate?
+        let viewMoreDelegate: ViewMoreRowDelegate?
+    }
+
+    func addDefaultTotalRows(toStackView rowsStackView: UIStackView) {
+        for _ in 0..<StatsDataHelper.maxRowsToDisplay {
+            let row = StatsTotalRow.loadFromNib()
+            rowsStackView.addArrangedSubview(row)
+        }
+
+        let emptyRow = StatsNoDataRow.loadFromNib()
+        rowsStackView.addArrangedSubview(emptyRow)
+
+        let viewMoreRow = ViewMoreRow.loadFromNib()
+        rowsStackView.addArrangedSubview(viewMoreRow)
+    }
+
+     func configureTotalRows(_ dataRows: [StatsTotalRowData],
+                             inStackView rowsStackView: UIStackView,
+                             forType statType: StatType,
+                             configuration: StatsTotalRowConfiguration) {
+
+        guard !dataRows.isEmpty else {
+            configureForNoData(inStackView: rowsStackView, forType: statType)
+            return
+        }
+
+        let numberOfRowsToAdd = calculateNumberOfRowsToAdd(from: dataRows, withConfiguration: configuration)
+
+        rowsStackView.arrangedSubviews.enumerated().forEach { index, view in
+            configure(view: view, at: index, in: dataRows, numberOfRowsToAdd: numberOfRowsToAdd, configuration: configuration)
+        }
+    }
+
+    private func configureForNoData(inStackView rowsStackView: UIStackView, forType statType: StatType) {
+        rowsStackView.arrangedSubviews.forEach { view in
+            if let emptyRow = view as? StatsNoDataRow {
+                emptyRow.isHidden = false
+                emptyRow.configure(forType: statType)
+            } else {
+                view.isHidden = true
+            }
+        }
+    }
+
+     private func calculateNumberOfRowsToAdd(from dataRows: [StatsTotalRowData], withConfiguration configuration: StatsTotalRowConfiguration) -> Int {
+        if configuration.limitRowsDisplayed {
+            return min(dataRows.count, StatsDataHelper.maxRowsToDisplay)
+        }
+        return dataRows.count
+    }
+
+     private func configure(view: UIView, at index: Int, in dataRows: [StatsTotalRowData], numberOfRowsToAdd: Int, configuration: StatsTotalRowConfiguration) {
+        switch view {
+        case let view as StatsNoDataRow:
+            view.isHidden = true
+        case let view as ViewMoreRow:
+            configureViewMoreRow(view, at: index, in: dataRows, withConfiguration: configuration)
+        case let view as StatsTotalRow where index < dataRows.count:
+            view.isHidden = false
+            let dataRow = dataRows[index]
+            view.configure(rowData: dataRow, delegate: configuration.rowDelegate, referrerDelegate: configuration.referrerDelegate)
+            view.showSeparator = index != (numberOfRowsToAdd - 1)
+        default:
+            view.isHidden = true
+        }
+    }
+
+     private func configureViewMoreRow(_ viewMoreRow: ViewMoreRow, at index: Int, in dataRows: [StatsTotalRowData], withConfiguration configuration: StatsTotalRowConfiguration) {
+        let shouldShowViewMore = configuration.limitRowsDisplayed && dataRows.count > StatsDataHelper.maxRowsToDisplay
+        viewMoreRow.isHidden = !shouldShowViewMore
+        if shouldShowViewMore {
+            viewMoreRow.configure(statSection: dataRows.first?.statSection, delegate: configuration.viewMoreDelegate)
+        }
+    }
+}
