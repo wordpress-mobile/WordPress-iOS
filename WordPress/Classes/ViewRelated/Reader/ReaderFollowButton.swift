3f537682f64bce839ff2cd5f39e58d6ae60ecc6d
David Christiandy <1299411+dvdchr@users.noreply.github.com>
2024-03-29 03:05:32 +0700

    Update ReaderFollowButton to accept ReaderDisplaySetting
    The priority is still given to the `ButtonColor` object that's
    passed through the parameter. The `ReaderDisplaySetting` only takes
    precedence when there are no `ButtonColor` objects being passed.
    
    The `@State` wrapper is removed from the `color` property since
    it seems to be the cause of the button not updating colors after
    the display setting is updated.

--- WordPress/Classes/ViewRelated/Reader/ReaderFollowButton.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderFollowButton.swift
@@ -5,25 +5,37 @@ struct ReaderFollowButton: View {
     let isFollowing: Bool
     let isEnabled: Bool
     let size: ButtonSize
-    @State var color: ButtonColor = .init()
+    var color: ButtonColor = .init()
+    var displaySetting: ReaderDisplaySetting?
 
     let action: () -> Void
 
     struct ButtonColor {
         let followedText: Color
         let followedBackground: Color
+        let followedStroke: Color
 
         let unfollowedText: Color
         let unfollowedBackground: Color
 
         init(followedText: Color = .secondary,
              followedBackground: Color = .clear,
+             followedStroke: Color = Color(.separator),
              unfollowedText: Color = Color(UIColor.invertedLabel),
              unfollowedBackground: Color = Color(.label)) {
             self.followedText = followedText
             self.followedBackground = followedBackground
+            self.followedStroke = followedStroke
             self.unfollowedText = unfollowedText
             self.unfollowedBackground = unfollowedBackground
+        }
+
+        init(displaySetting: ReaderDisplaySetting) {
+            followedText = Color(displaySetting.color.secondaryForeground)
+            followedBackground = .clear
+            followedStroke = followedText
+            unfollowedText = Color(displaySetting.color.background)
+            unfollowedBackground = Color(displaySetting.color.foreground)
         }
     }
 
@@ -32,11 +44,29 @@ struct ReaderFollowButton: View {
         case regular
     }
 
+    init(isFollowing: Bool,
+         isEnabled: Bool,
+         size: ButtonSize,
+         color: ButtonColor? = nil,
+         displaySetting: ReaderDisplaySetting? = nil,
+         action: @escaping () -> Void) {
+        self.isFollowing = isFollowing
+        self.isEnabled = isEnabled
+        self.size = size
+        self.color = color ?? {
+            if let displaySetting {
+                return .init(displaySetting: displaySetting)
+            }
+            return .init()
+        }()
+        self.action = action
+    }
+
     var body: some View {
         if isFollowing {
             button.overlay(
                 RoundedRectangle(cornerRadius: 5)
-                    .stroke(Color(UIColor.separator), lineWidth: 1)
+                    .stroke(color.followedStroke, lineWidth: 1)
             )
         } else {
             button
@@ -52,12 +82,19 @@ struct ReaderFollowButton: View {
         } label: {
             Text(text)
                 .foregroundColor(textColor)
-                .font(.subheadline)
+                .font(buttonFont)
         }
         .disabled(!isEnabled)
         .padding(.horizontal, size == .compact ? 16.0 : 24.0)
         .padding(.vertical, 8.0)
         .background(backgroundColor)
         .cornerRadius(5.0)
+    }
+
+    var buttonFont: Font {
+        guard let displaySetting else {
+            return .subheadline
+        }
+        return Font(displaySetting.font(with: .subheadline))
     }
 }
