f318fba9ec1821494b1a0703016537d404680f68
David Christiandy <1299411+dvdchr@users.noreply.github.com>
2024-03-28 21:43:00 +0700

    Refactor display setting view

--- WordPress/Classes/ViewRelated/Reader/Theme/ReaderDisplaySettingViewController.swift
+++ WordPress/Classes/ViewRelated/Reader/Theme/ReaderDisplaySettingViewController.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import DesignSystem
 
 class ReaderDisplaySettingViewController: UIViewController {
 
@@ -19,148 +20,253 @@ class ReaderDisplaySettingViewController: UIViewController {
 
 // MARK: - SwiftUI
 
+// MARK: View Model
+
 class ReaderDisplaySettingSelectionViewModel: NSObject, ObservableObject {
+
+    let feedbackLinkString = String() // TODO: Update with actual link
 
     @Published var displaySetting: ReaderDisplaySetting
 
     init(displaySetting: ReaderDisplaySetting) {
         self.displaySetting = displaySetting
     }
+
+    // Convenience accessors
+
+    var foregroundColor: Color {
+        Color(displaySetting.color.foreground)
+    }
+
+    var backgroundColor: Color {
+        Color(displaySetting.color.background)
+    }
 }
+
+// MARK: Container View
 
 struct ReaderDisplaySettingSelectionView: View {
 
     @ObservedObject var viewModel: ReaderDisplaySettingSelectionViewModel
 
-    @State private var sliderValue: Double = 0
-
     var body: some View {
-        VStack(spacing: 24.0) {
-            previewView
-            colorSelectionView
-            fontSelectionView
-            sizeSelectionView
-
-            // TODO: Add a 'Done' button
-
-            Spacer()
+        ZStack {
+            PreviewView(viewModel: viewModel)
+            VStack {
+                Spacer() // stick the control view to the bottom.
+                ControlView(viewModel: viewModel)
+                    .overlay(alignment: .top, content: { // add 1pt top border.
+                        Rectangle()
+                            .frame(width: nil, height: 1.0, alignment: .top)
+                            .foregroundStyle(Color(.tertiaryLabel))
+                    })
+            }
         }
     }
 
-    var previewView: some View {
-        VStack(alignment: .leading, spacing: 16.0) {
-            Text("The quick brown fox jumps over the lazy dog")
-                .font(Font(viewModel.displaySetting.font(with: .title1)))
-                .foregroundStyle(foregroundColor)
+}
 
-            HStack(spacing: 8.0) {
-                ForEach(["dogs", "fox", "design", "writing"], id: \.self) { text in
+// MARK: - Preview View
+
+extension ReaderDisplaySettingSelectionView {
+
+    struct PreviewView: View {
+        @ObservedObject var viewModel: ReaderDisplaySettingSelectionViewModel
+
+        var body: some View {
+            ScrollView(.vertical) {
+                VStack(alignment: .leading, spacing: .DS.Padding.double) {
+                    Text(Strings.Preview.title)
+                        .font(Font(viewModel.displaySetting.font(with: .title1)))
+                        .foregroundStyle(Color(viewModel.displaySetting.color.foreground))
+
+                    tagsView
+
+                    // TODO: Add feature flag for feedback collection.
+                    // TODO: Apply link styles.
+                    if let feedbackURL = URL(string: viewModel.feedbackLinkString) {
+                        Link(Strings.Preview.feedbackLinkText, destination: feedbackURL)
+                    }
+
+                    Text(Strings.Preview.bodyDescription)
+                        .font(Font(viewModel.displaySetting.font(with: .callout)))
+                        .foregroundStyle(viewModel.foregroundColor)
+
+                    Text(Strings.Preview.bodyNotice)
+                        .font(Font(viewModel.displaySetting.font(with: .callout)))
+                        .foregroundStyle(viewModel.foregroundColor)
+
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .fixedSize(horizontal: false, vertical: true)
+            }
+            .padding(.DS.Padding.double)
+            .background(viewModel.backgroundColor)
+            .animation(.easeInOut, value: viewModel.displaySetting)
+        }
+
+        var tagsView: some View {
+            HStack(spacing: .DS.Padding.single) {
+                ForEach(Strings.Preview.tags, id: \.self) { text in
                     Text(text)
                         .font(Font(viewModel.displaySetting.font(with: .callout)))
-                        .foregroundStyle(foregroundColor)
-                        .padding(.horizontal, 16.0)
-                        .padding(.vertical, 8.0)
+                        .foregroundStyle(viewModel.foregroundColor)
+                        .padding(.horizontal, .DS.Padding.double)
+                        .padding(.vertical, .DS.Padding.single)
                         .overlay {
-                            RoundedRectangle(cornerRadius: 5.0)
-                                .strokeBorder(foregroundColor, lineWidth: 1.0)
+                            RoundedRectangle(cornerRadius: .DS.Radius.small)
+                                .strokeBorder(Color(viewModel.displaySetting.color.foreground.withAlphaComponent(0.3)), lineWidth: 1.0)
                         }
                 }
             }
-
-            Text("Once upon a time, in a quaint little village nestled between rolling hills and lush greenery, there lived a quick brown fox named Jasper.")
-                .font(Font(viewModel.displaySetting.font(with: .callout)))
-                .foregroundStyle(foregroundColor)
         }
-        .padding(.horizontal, 16)
-        .padding(.top, 16)
-        .padding(.bottom, 36)
-        .frame(maxWidth: .infinity)
-        .fixedSize(horizontal: false, vertical: true)
-        .background(backgroundColor)
-        .animation(.easeInOut, value: viewModel.displaySetting)
+
+        private struct Strings {
+            struct Preview {
+                static let title = NSLocalizedString(
+                    "reader.preferences.preview.title",
+                    value: "Choose your Reading Preferences",
+                    comment: "Title text for a preview"
+                )
+
+                static let tags = [
+                    NSLocalizedString("reader.preferences.preview.tags.1", value: "dogs", comment: "Example tag for preview"),
+                    NSLocalizedString("reader.preferences.preview.tags.2", value: "fox", comment: "Example tag for preview"),
+                    NSLocalizedString("reader.preferences.preview.tags.3", value: "design", comment: "Example tag for preview"),
+                    NSLocalizedString("reader.preferences.preview.tags.4", value: "writing", comment: "Example tag for preview"),
+                ]
+
+                static let feedbackLinkText = NSLocalizedString(
+                    "reader.preferences.preview.body.feedbackLink",
+                    value: "Send us a feedback on this feature",
+                    comment: "Text for a feedback link for the Reader Preferences feature"
+                )
+
+                static let bodyDescription = NSLocalizedString(
+                    "reader.preferences.preview.body.description",
+                    value: "Reading is personal, we want you to have control. Choose the styles that suit you.",
+                    comment: "Description text for the preview section of Reader Preferences"
+                )
+
+                static let bodyNotice = NSLocalizedString(
+                    "reader.customization.preview.body.notice",
+                    value: "This feature is still in development.",
+                    comment: "Footnote to be displayed in the preview section, noticing that the feature is in development."
+                )
+            }
+        }
     }
 
-    var colorSelectionView: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 4.0) {
-                ForEach(ReaderDisplaySetting.Color.allCases, id: \.rawValue) { color in
-                    Button {
-                        viewModel.displaySetting.color = color
-                    } label: {
-                        VStack(spacing: 8.0) {
-                            DualColorCircle(primaryColor: Color(color.foreground),
-                                            secondaryColor: Color(color.background))
-                            Text(color.label)
-                                .font(.footnote)
-                                .foregroundStyle(Color(.label))
-                        }
-                        .padding(.horizontal, 10.0)
-                        .padding(.vertical, 8.0)
-                        .clipShape(RoundedRectangle(cornerRadius: 5.0))
-                        .overlay {
-                            RoundedRectangle(cornerRadius: 5.0)
-                                .strokeBorder(color == viewModel.displaySetting.color ? .secondary : Color(.secondarySystemBackground), lineWidth: 1.0)
+}
+
+// MARK: - Control View
+
+extension ReaderDisplaySettingSelectionView {
+
+    struct ControlView: View {
+        @ObservedObject var viewModel: ReaderDisplaySettingSelectionViewModel
+
+        @State private var sliderValue: Double = 0
+
+        var body: some View {
+            VStack(spacing: .DS.Padding.large) {
+                colorSelectionView
+                fontSelectionView
+                sizeSelectionView
+                    .padding(.horizontal, .DS.Padding.double)
+                DSButton(title: Strings.doneButton, style: DSButtonStyle.init(emphasis: .primary, size: .large)) {
+                    // TODO: Impl
+                }
+                .padding(.horizontal, .DS.Padding.double)
+            }
+            .padding(.top, .DS.Padding.medium)
+            .background(Color(.systemBackground))
+        }
+
+        var colorSelectionView: some View {
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: .DS.Padding.half) {
+                    ForEach(ReaderDisplaySetting.Color.allCases, id: \.rawValue) { color in
+                        Button {
+                            viewModel.displaySetting.color = color
+                        } label: {
+                            VStack(spacing: .DS.Padding.single) {
+                                DualColorCircle(primaryColor: Color(color.foreground),
+                                                secondaryColor: Color(color.background))
+                                Text(color.label)
+                                    .font(.footnote)
+                                    .foregroundStyle(Color(.label))
+                            }
+                            .padding(.horizontal, .DS.Padding.split)
+                            .padding(.vertical, .DS.Padding.single)
+                            .overlay {
+                                RoundedRectangle(cornerRadius: .DS.Radius.small)
+                                    .strokeBorder(color == viewModel.displaySetting.color
+                                                  ? .primary
+                                                  : Color(UIColor.label.withAlphaComponent(0.1)), lineWidth: 1.0)
+                            }
                         }
                     }
                 }
+                .padding(.leading, .DS.Padding.double) // initial content offset
             }
-            .padding(.leading, 16.0) // initial content inset
         }
-    }
 
-    var fontSelectionView: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 4.0) {
-                ForEach(ReaderDisplaySetting.Font.allCases, id: \.rawValue) { font in
-                    Button {
-                        viewModel.displaySetting.font = font
-                    } label: {
-                        VStack(spacing: 4.0) {
-                            Text("Aa")
-                                .font(Font(ReaderDisplaySetting.font(with: font, textStyle: .largeTitle)).bold())
-                                .foregroundStyle(Color(.label))
-                            Text(font.rawValue.capitalized)
-                                .font(.footnote)
-                                .foregroundStyle(Color(.label))
-                        }
-                        .padding(.horizontal, 16.0)
-                        .padding(.vertical, 8.0)
-                        .clipShape(RoundedRectangle(cornerRadius: 5.0))
-                        .overlay {
-                            RoundedRectangle(cornerRadius: 5.0)
-                                .strokeBorder(font == viewModel.displaySetting.font ? .secondary : Color(.secondarySystemBackground), lineWidth: 1.0)
+        var fontSelectionView: some View {
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: .DS.Padding.half) {
+                    ForEach(ReaderDisplaySetting.Font.allCases, id: \.rawValue) { font in
+                        Button {
+                            viewModel.displaySetting.font = font
+                        } label: {
+                            VStack(spacing: .DS.Padding.half) {
+                                Text("Aa")
+                                    .font(Font(ReaderDisplaySetting.font(with: font, textStyle: .largeTitle)).bold())
+                                    .foregroundStyle(Color(.label))
+                                Text(font.rawValue.capitalized)
+                                    .font(.footnote)
+                                    .foregroundStyle(Color(.label))
+                            }
+                            .padding(.horizontal, .DS.Padding.double)
+                            .padding(.vertical, .DS.Padding.single)
+                            .overlay {
+                                RoundedRectangle(cornerRadius: .DS.Radius.small)
+                                    .strokeBorder(font == viewModel.displaySetting.font
+                                                  ? .primary
+                                                  : Color(UIColor.label.withAlphaComponent(0.1)), lineWidth: 1.0)
+                            }
                         }
                     }
                 }
+                .padding(.leading, .DS.Padding.double) // initial content offset
             }
-            .padding(.leading, 16.0) // initial content inset
+        }
+
+        var sizeSelectionView: some View {
+            Slider(value: $sliderValue,
+                   in: Double(ReaderDisplaySetting.Size.smaller.rawValue)...Double(ReaderDisplaySetting.Size.larger.rawValue),
+                   step: 1) {
+                Text("Size")
+            } minimumValueLabel: {
+                Text("A")
+                    .font(Font(ReaderDisplaySetting.font(with: .sans, size: .smaller, textStyle: .body)))
+            } maximumValueLabel: {
+                Text("A")
+                    .font(Font(ReaderDisplaySetting.font(with: .sans, size: .larger, textStyle: .body)))
+            } onEditingChanged: { _ in
+                viewModel.displaySetting.size = .init(rawValue: Int(sliderValue)) ?? .normal
+            }
+            .padding(.vertical, .DS.Padding.single)
         }
     }
 
-    var sizeSelectionView: some View {
-        Slider(value: $sliderValue,
-               in: Double(ReaderDisplaySetting.Size.smaller.rawValue)...Double(ReaderDisplaySetting.Size.larger.rawValue),
-               step: 1) {
-            Text("Size")
-        } minimumValueLabel: {
-            Text("A")
-                .font(Font(ReaderDisplaySetting.font(with: .sans, size: .smaller, textStyle: .body)))
-        } maximumValueLabel: {
-            Text("A")
-                .font(Font(ReaderDisplaySetting.font(with: .sans, size: .larger, textStyle: .body)))
-        } onEditingChanged: { _ in
-            viewModel.displaySetting.size = .init(rawValue: Int(sliderValue)) ?? .normal
-        }
-        .padding(.vertical, 8.0)
-        .padding(.horizontal, 16.0)
-    }
-
-    private var foregroundColor: Color {
-        Color(viewModel.displaySetting.color.foreground)
-    }
-
-    private var backgroundColor: Color {
-        Color(viewModel.displaySetting.color.background)
+    private struct Strings {
+        static let doneButton = NSLocalizedString(
+            "reader.customization.control.doneButton",
+            value: "Done",
+            comment: "Title for a button to save and apply the customized Reader Preferences settings when tapped."
+        )
     }
 }
 
