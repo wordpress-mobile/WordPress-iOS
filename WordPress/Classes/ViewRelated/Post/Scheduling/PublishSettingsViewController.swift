a542d8343dd0eda82d15dbec4609ddb70c46c1f4
Povilas Staskus <povilas.staskus@automattic.com>
2024-04-01 13:04:41 +0300

    Add close button to Post Schedule Date Picker on iPad

--- WordPress/Classes/ViewRelated/Post/Scheduling/PublishSettingsViewController.swift
+++ WordPress/Classes/ViewRelated/Post/Scheduling/PublishSettingsViewController.swift
@@ -209,6 +209,10 @@ private struct DateAndTimeRow: ImmuTableRow {
 
             if UIDevice.isPad() {
                 let navigation = UINavigationController(rootViewController: viewController)
+                let closeButton = UIBarButtonItem(systemItem: .close, primaryAction: .init(handler: { [weak navigation] _ in
+                    navigation?.dismiss(animated: true)
+                }))
+                viewController.navigationItem.leftBarButtonItem = closeButton
                 navigation.modalPresentationStyle = .popover
                 if let popoverController = navigation.popoverPresentationController {
                     popoverController.sourceView = self?.viewController?.tableView
