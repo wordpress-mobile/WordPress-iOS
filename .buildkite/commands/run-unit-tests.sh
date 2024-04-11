f0933dec7d5b057bdb3e5efc2be8f8071d81b552
Gio Lodi <gio@mokacoding.com>
2024-04-11 15:49:34 +1000

    Add CI header

--- .buildkite/commands/run-unit-tests.sh
+++ .buildkite/commands/run-unit-tests.sh
@@ -7,4 +7,8 @@ export BUILDKITE_ANALYTICS_TOKEN=$BUILDKITE_ANALYTICS_TOKEN_UNIT_TESTS
 echo "--- :rubygems: Setting up Gems"
 install_gems
 
+echo '--- :closed_lock_with_key: Access secrets'
+bundle exec fastlane run configure_apply
+
+echo '--- TEST FOR CI ANNOTATION ON PODS CHECK'
 bundle exec fastlane code_freeze
