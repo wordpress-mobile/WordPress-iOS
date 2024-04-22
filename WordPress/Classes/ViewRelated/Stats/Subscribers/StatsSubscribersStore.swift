9c8f92c6b437d1fccab6e2773fbb4c8f6feb2666
Povilas Staskus <povilas.staskus@automattic.com>
2024-04-22 12:42:34 +0300

    Create StatsSubscribersStore
    Creating StatsSubscribersStore to follow the pattern of StatsPeriodStore (Traffic tab), and StatsInsightStore (Insight tab).
    
    -Store is created without following previous WordPressFlux pattern, and instead using Combine.
    -Simplified usage by allowing to subscribe to any data type updates, and ask for any data type updates separately

---
+++ WordPress/Classes/ViewRelated/Stats/Subscribers/StatsSubscribersStore.swift
@@ -0,0 +1,37 @@
+import Foundation
+import Combine
+import WordPressKit
+
+struct StatsSubscribersStore {
+    enum State<Value: Equatable>: Equatable {
+        case idle
+        case loading
+        case success(Value)
+        case error
+    }
+
+    private let statsService: StatsServiceRemoteV2 = {
+        let siteID = SiteStatsInformation.sharedInstance.siteID?.intValue ?? 0
+        let timeZone = SiteStatsInformation.sharedInstance.siteTimeZone ?? .current
+        let wpApi = WordPressComRestApi.defaultApi(oAuthToken: SiteStatsInformation.sharedInstance.oauth2Token, userAgent: WPUserAgent.wordPress())
+        return StatsServiceRemoteV2(wordPressComRestApi: wpApi, siteID: siteID, siteTimezone: timeZone)
+    }()
+
+    var emailsSummary: CurrentValueSubject<State<StatsEmailsSummaryData>, Never> = .init(.idle)
+
+    func updateEmailsSummary() {
+        guard emailsSummary.value != .loading else { return }
+
+        emailsSummary.send(.loading)
+        statsService.getData(quantity: 6, sortField: .postId, sortOrder: .descending) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let data):
+                    self.emailsSummary.send(.success(data))
+                case .failure:
+                    self.emailsSummary.send(.error)
+                }
+            }
+        }
+    }
+}
