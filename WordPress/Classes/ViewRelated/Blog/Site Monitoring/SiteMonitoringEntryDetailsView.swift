613ffc769ce2d02b474f4ee891d9bc12e6fc1216
kean <grebenyuk.alexander@gmail.com>
2024-04-23 08:09:47 -0400

    Fix warnings in SiteMonitoringEntryDetailsView previews

--- WordPress/Classes/ViewRelated/Blog/Site Monitoring/SiteMonitoringEntryDetailsView.swift
+++ WordPress/Classes/ViewRelated/Blog/Site Monitoring/SiteMonitoringEntryDetailsView.swift
@@ -116,17 +116,13 @@ private enum Strings {
 
 #Preview("AtomicErrorLogEntry") {
     NavigationView {
-        if #available(iOS 16, *) {
-            SiteMonitoringEntryDetailsView(text: errorLogEntry.attributedDescription)
-        }
+        SiteMonitoringEntryDetailsView(text: errorLogEntry.attributedDescription)
     }
 }
 
 #Preview("AtomicWebServerLogEntry") {
     NavigationView {
-        if #available(iOS 16, *) {
-            SiteMonitoringEntryDetailsView(text: serverLogEntry.attributedDescription)
-        }
+        SiteMonitoringEntryDetailsView(text: serverLogEntry.attributedDescription)
     }
 }
 
