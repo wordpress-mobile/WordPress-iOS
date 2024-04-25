c4d9a5c08d37f1f7d5a2929869018940c2f4ba2e
kean <grebenyuk.alexander@gmail.com>
2024-04-25 11:06:07 -0400

    Add Feature.autoSaveDrafts

--- WordPress/Classes/Utility/BuildInformation/FeatureFlag.swift
+++ WordPress/Classes/Utility/BuildInformation/FeatureFlag.swift
@@ -12,6 +12,7 @@ enum FeatureFlag: Int, CaseIterable {
     case googleDomainsCard
     case newTabIcons
     case readerTagsFeed
+    case autoSaveDrafts
 
     /// Returns a boolean indicating if the feature is enabled
     var enabled: Bool {
@@ -40,6 +41,8 @@ enum FeatureFlag: Int, CaseIterable {
             return true
         case .readerTagsFeed:
             return false
+        case .autoSaveDrafts:
+            return true
         }
     }
 
@@ -82,6 +85,8 @@ extension FeatureFlag {
             return "New Tab Icons"
         case .readerTagsFeed:
             return "Reader Tags Feed"
+        case .autoSaveDrafts:
+            return "Autosave Drafts"
         }
     }
 }
