bc44929b2f3d11d461e16bb4355b5577e465d85b
Povilas Staskus <povilas.staskus@automattic.com>
2024-04-29 13:22:06 +0300

    Rename Total Followers to Total Subscribers

--- WordPress/Classes/ViewRelated/Stats/Helpers/StatSection.swift
+++ WordPress/Classes/ViewRelated/Stats/Helpers/StatSection.swift
@@ -93,7 +93,7 @@
         case .insightsCommentsTotals:
             return InsightsHeaders.commentsTotals
         case .insightsFollowerTotals:
-            return InsightsHeaders.followerTotals
+            return InsightsHeaders.subscribersTotal
         case .insightsMostPopularTime:
             return InsightsHeaders.mostPopularTime
         case .insightsTagsAndCategories:
@@ -401,7 +401,7 @@
         static let mostPopularTime = NSLocalizedString("stats.insights.mostPopularCard.title", value: "🔥 Most Popular Time", comment: "Insights 'Most Popular Time' header. Fire emoji should remain part of the string.")
         static let likesTotals = NSLocalizedString("Total Likes", comment: "Insights 'Total Likes' header")
         static let commentsTotals = NSLocalizedString("Total Comments", comment: "Insights 'Total Comments' header")
-        static let followerTotals = NSLocalizedString("Total Followers", comment: "Insights 'Total Followers' header")
+        static let subscribersTotal = NSLocalizedString("stats.insights.totalSubscribers.title", value: "Total Subscribers", comment: "Insights 'Total Subscribers' header")
         static let publicize = NSLocalizedString("Jetpack Social Connections", comment: "Insights 'Jetpack Social Connections' header")
         static let todaysStats = NSLocalizedString("Today", comment: "Insights 'Today' header")
         static let postingActivity = NSLocalizedString("Posting Activity", comment: "Insights 'Posting Activity' header")
