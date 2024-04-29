9ccbdcee0e9fc1630a63a2a1ed9dcd16da4af63f
alpavanoglu <alp.avanoglu@automattic.com>
2024-04-29 19:41:27 +0200

    Wrap HStack with Button

--- WordPress/Classes/ViewRelated/Notifications/Comment Moderation/CommentModerationOptionsView.swift
+++ WordPress/Classes/ViewRelated/Notifications/Comment Moderation/CommentModerationOptionsView.swift
@@ -2,34 +2,15 @@ import SwiftUI
 import DesignSystem
 
 struct CommentModerationOptionsView: View {
-    private struct Item {
-        let title: String
-        let iconName: IconName
-        let tintColor: Color
-    }
 
-    private let items = [
-        Item(
-            title: Strings.approveTitle,
-            iconName: .checkmarkCircle,
-            tintColor: .DS.Foreground.brand(isJetpack: true)
-        ),
-        Item(
-            title: Strings.pendingTitle,
-            iconName: .clock,
-            tintColor: .DS.Foreground.secondary
-        ),
-        Item(
-            title: Strings.trashTitle,
-            iconName: .trash,
-            tintColor: .DS.Foreground.warning
-        ),
-        Item(
-            title: Strings.spamTitle,
-            iconName: .exclamationCircle,
-            tintColor: .DS.Foreground.error
-        ),
+    private var options: [Option] = [
+        .approve,
+        .pending,
+        .trash,
+        .spam
     ]
+
+    var optionSelected: ((Option) -> Void)?
 
     var body: some View {
         VStack(alignment: .leading, spacing: .DS.Padding.medium) {
@@ -48,24 +29,28 @@ struct CommentModerationOptionsView: View {
 
     private var optionsVStack: some View {
         VStack(spacing: .DS.Padding.medium) {
-            ForEach(items, id: \.title) { item in
-                optionHStack(item: item)
+            ForEach(options, id: \.title) { option in
+                Button {
+                    optionSelected?(option)
+                } label: {
+                    optionHStack(option: option)
+                }
             }
         }
     }
 
-    private func optionHStack(item: Item) -> some View {
+    private func optionHStack(option: Option) -> some View {
         HStack(spacing: .DS.Padding.double) {
-            Image.DS.icon(named: item.iconName)
+            Image.DS.icon(named: option.iconName)
                 .resizable()
                 .renderingMode(.template)
-                .foregroundStyle(item.tintColor)
+                .foregroundStyle(option.tintColor)
                 .frame(
                     width: .DS.Padding.medium,
                     height: .DS.Padding.medium
                 )
 
-            Text(item.title)
+            Text(option.title)
                 .font(.DS.Body.large)
                 .foregroundStyle(
                     Color.DS.Foreground.primary
@@ -76,13 +61,66 @@ struct CommentModerationOptionsView: View {
     }
 }
 
-private extension CommentModerationOptionsView {
+extension CommentModerationOptionsView {
     enum Strings {
         static let title = NSLocalizedString(
             "comment.moderation.sheet.title",
             value: "Choose comment status",
             comment: "Title for the comment moderation sheet."
         )
+    }
+}
+
+extension CommentModerationOptionsView {
+    enum Option {
+        case approve
+        case pending
+        case trash
+        case spam
+
+        var title: String {
+            switch self {
+            case .approve:
+                return Strings.approveTitle
+            case .pending:
+                return Strings.pendingTitle
+            case .trash:
+                return Strings.trashTitle
+            case .spam:
+                return Strings.spamTitle
+            }
+        }
+
+        var iconName: IconName {
+            switch self {
+            case .approve:
+                return .checkmarkCircle
+            case .pending:
+                return .clock
+            case .trash:
+                return .trash
+            case .spam:
+                return .exclamationCircle
+            }
+        }
+
+        var tintColor: Color {
+            switch self {
+            case .approve:
+                return .DS.Foreground.brand(isJetpack: true)
+            case .pending:
+                return .DS.Foreground.secondary
+            case .trash:
+                return .DS.Foreground.warning
+            case .spam:
+                return .DS.Foreground.error
+            }
+        }
+    }
+}
+
+private extension CommentModerationOptionsView.Option {
+    enum Strings {
         static let approveTitle = NSLocalizedString(
             "comment.moderation.sheet.approve.title",
             value: "Approve",
