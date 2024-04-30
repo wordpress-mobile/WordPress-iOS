51d1876949bd8952377625d9bb17cd7995cc98f5
Paul Von Schrottky <paul.von.schrottky@automattic.com>
2024-04-30 18:26:25 -0400

    Split StatsChartMarker into separate files
    Split into a base class, a SubscribersChartMarker class, and a ViewsVisitorsChartMarker class

---
+++ WordPress/Classes/ViewRelated/Stats/Subscribers/SubscribersChartMarker.swift
@@ -0,0 +1,35 @@
+import DGCharts
+
+class SubscribersChartMarker: StatsChartMarker {
+    let date: Date
+
+    init(dotColor: UIColor, name: String, date: Date) {
+        self.date = date
+        super.init(dotColor: dotColor, name: name)
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func text(for entry: ChartDataEntry) -> (NSMutableAttributedString, CGSize)? {
+
+        let subscriberCountRowAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.preferredFont(forTextStyle: .headline),
+                                                                           .paragraphStyle: paragraphStyle,
+                                                                           .foregroundColor: UIColor.white]
+
+        let subscriberLabelRowAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.preferredFont(forTextStyle: .footnote),
+                                                                           .paragraphStyle: paragraphStyle,
+                                                                           .foregroundColor: UIColor.white]
+
+        let subscriberDateRowAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.preferredFont(forTextStyle: .footnote),
+                                                                          .paragraphStyle: paragraphStyle,
+                                                                          .foregroundColor: UIColor.white.withAlphaComponent(0.8)]
+
+        let attrString = NSMutableAttributedString()
+        attrString.append(NSAttributedString(string: entry.y.abbreviatedString() + "\n", attributes: subscriberCountRowAttributes))
+        attrString.append(NSAttributedString(string: "\(name)\n", attributes: subscriberLabelRowAttributes))
+        attrString.append(NSAttributedString(string: DateValueFormatter().dateFormatter.string(from: date), attributes: subscriberDateRowAttributes))
+        return (attrString, attrString.size())
+    }
+}
