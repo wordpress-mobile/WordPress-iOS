34e5ad746c8f68446528628a0ce9c7af17a72377
David Christiandy <1299411+dvdchr@users.noreply.github.com>
2024-05-10 02:19:37 +0700

    Simplify stream header configuration logic
    I've noticed that `configureStreamHeader()` had too many layout
    constraints juggling, so this simplifies it a bit.
    
    * `ReaderSiteHeaderView` now inherits from `UITableViewHeaderFooterView`,
      so that it has the same readableContentGuide of table views. This
      removes the need to put the SwiftUI view in a container view and
      align the horizontal anchors  manually to the table view.
    * Moved the border-adding logic to each own views.

--- WordPress/Classes/ViewRelated/Reader/ReaderStreamViewController.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderStreamViewController.swift
@@ -186,6 +186,7 @@ import AutomatticTracks
         }
     }
 
+    /// Whether the stream is being filtered by a site or tag.
     var isContentFiltered: Bool = false
 
     var contentType: ReaderContentType = .topic {
@@ -599,34 +600,17 @@ import AutomatticTracks
             return
         }
 
-        guard let header = headerForStream(topic, isLoggedIn: isLoggedIn, container: tableViewController) else {
+        guard let headerView = headerForStream(topic, isLoggedIn: isLoggedIn, container: tableViewController) else {
             tableView.tableHeaderView = nil
             return
         }
 
-        let isNewHeader = !isContentFiltered
-        let isNewSiteHeader = isNewHeader && ReaderHelpers.isTopicSite(topic)
-
-        let headerView = {
-            guard isNewSiteHeader else {
-                return header
-            }
-
-            // The container view is added so that the header respects the safe area boundaries and expands
-            // the header's background color to the screen's edges.
-            let containerView = UIView()
-            containerView.translatesAutoresizingMaskIntoConstraints = false
-            containerView.backgroundColor = header.backgroundColor
-            header.translatesAutoresizingMaskIntoConstraints = false
-            containerView.addSubview(header)
-            return containerView
-        }()
-
         if let tableHeaderView = tableView.tableHeaderView {
             headerView.isHidden = tableHeaderView.isHidden
         }
+
         tableView.tableHeaderView = headerView
-        streamHeader = header as? ReaderStreamHeader
+        streamHeader = headerView as? ReaderStreamHeader
 
         // This feels somewhat hacky, but it is the only way I found to insert a stack view into the header without breaking the autolayout constraints.
         let centerConstraint = headerView.centerXAnchor.constraint(equalTo: tableView.centerXAnchor)
@@ -635,37 +619,12 @@ import AutomatticTracks
         headerWidthConstraint.priority = UILayoutPriority(999)
         centerConstraint.priority = UILayoutPriority(999)
 
-        var constraints = [
+        NSLayoutConstraint.activate([
             centerConstraint,
             headerWidthConstraint,
             topConstraint
-        ]
+        ])
 
-        if isNewSiteHeader {
-            constraints.append(contentsOf: [
-                header.topAnchor.constraint(equalTo: headerView.topAnchor),
-                header.bottomAnchor.constraint(equalTo: headerView.bottomAnchor),
-                header.trailingAnchor.constraint(equalTo: tableView.readableContentGuide.trailingAnchor),
-                header.leadingAnchor.constraint(equalTo: tableView.readableContentGuide.leadingAnchor),
-            ])
-        }
-
-        // manually add a separator for the new header views.
-        if isNewHeader {
-            let borderView = UIView()
-            borderView.backgroundColor = .separator
-            borderView.translatesAutoresizingMaskIntoConstraints = false
-            headerView.addSubview(borderView)
-
-            constraints.append(contentsOf: [
-                borderView.bottomAnchor.constraint(equalTo: headerView.bottomAnchor),
-                borderView.heightAnchor.constraint(equalToConstant: 0.5),
-                borderView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-                borderView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-            ])
-        }
-
-        NSLayoutConstraint.activate(constraints)
         tableView.tableHeaderView?.layoutIfNeeded()
         tableView.tableHeaderView = tableView.tableHeaderView
     }
