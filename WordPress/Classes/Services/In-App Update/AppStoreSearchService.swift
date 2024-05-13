ca8c07d2f4b24b29d94bcb0cf0a2d71c0ea8d7d9
Momo Ozawa <momo.ozawa@automattic.com>
2024-05-13 11:00:20 +0100

    Add AppStoreSearchService

---
+++ WordPress/Classes/Services/In-App Update/AppStoreSearchService.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+struct AppStoreLookupResponse: Decodable {
+    let results: [AppStoreInfo]
+}
+
+struct AppStoreInfo: Decodable {
+    let trackName: String
+    let trackId: Int
+    let version: String
+    let fileSizeBytes: String
+    let releaseNotes: String
+    let minimumOsVersion: String
+}
+
+final class AppStoreSearchService {
+    func lookup(appID: String) async throws -> AppStoreLookupResponse {
+        guard let url = URL(string: "https://itunes.apple.com/lookup?id=\(appID)") else {
+            throw AppStoreSearchError.invalidURL
+        }
+        let (data, response) = try await URLSession.shared.data(from: url)
+        try validate(response: response)
+        return try JSONDecoder().decode(AppStoreLookupResponse.self, from: data)
+    }
+
+    private func validate(response: URLResponse) throws {
+        guard let response = response as? HTTPURLResponse else {
+            return
+        }
+        guard (200..<400).contains(response.statusCode) else {
+            throw AppStoreSearchError.unacceptableStatusCode(response.statusCode)
+        }
+    }
+}
+
+enum AppStoreSearchError: Error {
+    case invalidURL
+    case unacceptableStatusCode(_ statusCode: Int?)
+}
