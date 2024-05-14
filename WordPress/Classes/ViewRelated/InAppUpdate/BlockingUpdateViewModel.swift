595fd62aadd599d97c6d7fd6be76dbee05744c36
Momo Ozawa <momo.ozawa@automattic.com>
2024-05-14 09:49:56 +0100

    Remove fileSizeBytes since they were inaccurate

--- WordPress/Classes/ViewRelated/InAppUpdate/BlockingUpdateViewModel.swift
+++ WordPress/Classes/ViewRelated/InAppUpdate/BlockingUpdateViewModel.swift
@@ -2,20 +2,18 @@ import Foundation
 
 struct BlockingUpdateViewModel {
     let appName: String
-    let detailsString: String
+    let version: String
     let releaseNotes: [String.SubSequence]
     let onUpdateTapped: () -> Void
 
     init(appStoreInfo: AppStoreInfo, onUpdateTapped: @escaping () -> Void) {
         self.appName = appStoreInfo.trackName
-        self.detailsString = makeDetailsString(from: appStoreInfo)
+        self.version = String(format: Strings.versionFormat, appStoreInfo.version)
         self.releaseNotes = appStoreInfo.releaseNotes.split(whereSeparator: \.isNewline)
         self.onUpdateTapped = onUpdateTapped
     }
 }
 
-private func makeDetailsString(from appStoreInfo: AppStoreInfo) -> String {
-    let latestVersion = appStoreInfo.version
-    let fileSizeBytes = ByteCountFormatter.string(fromByteCount: Int64(appStoreInfo.fileSizeBytes) ?? 0, countStyle: .file)
-    return [latestVersion, fileSizeBytes].joined(separator: " · ")
+private enum Strings {
+    static let versionFormat = NSLocalizedString("inAppUpdate.appStoreInfo.version", value: "Version %@", comment: "Format for latest version available")
 }
