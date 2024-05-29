da95e195a0a17a1f927ecac2eaaec49e6c456c0b
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2024-05-28 19:11:05 -0600

    Use an SVG (and manually apply blur)
    Xcode’s SVG handling is interesting – it doesn’t properly handle filters, so the blur isn’t baked into the resulting image.
    
    This is actually a feature, not a bug – the old encoding approach was worst-case scenario for image size – a partially-transparent image with gradients (from the blur) and noise applied overtop; and because Xcode turns the SVG into a PNG for us it would get unexpected large very quickly.
    
    Doing the blur manually takes just as much CPU time as it would if we were processing the SVG within iOS, but results in a much smaller image inside the bundle.

---
+++ WordPress/Classes/Extensions/UIImage+Blur.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+extension UIImage {
+
+    // Adapted from https://gist.github.com/mxcl/76f40027b1ef515e4e6b41292b54fe92
+    func blur(radius: Float) -> UIImage? {
+        let ciContext = CIContext(options: nil)
+
+        guard
+            let cgImage = self.cgImage,
+            let ciFilter = CIFilter(name: "CIGaussianBlur")
+        else {
+            return self
+        }
+
+        let inputImage = CIImage(cgImage: cgImage)
+
+        ciFilter.setValue(inputImage, forKey: kCIInputImageKey)
+        ciFilter.setValue(radius, forKey: "inputRadius")
+
+        guard
+            let resultImage = ciFilter.value(forKey: kCIOutputImageKey) as? CIImage,
+            let outputImage = ciContext.createCGImage(resultImage, from: inputImage.extent)
+        else {
+            return self
+        }
+
+        return UIImage(cgImage: outputImage)
+    }
+}
