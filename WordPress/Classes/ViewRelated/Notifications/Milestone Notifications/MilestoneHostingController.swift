114db6234fdca45d46626140da1dc8293898e1a1
alpavanoglu <alp.avanoglu@automattic.com>
2024-06-06 12:35:44 +0200

    Add up/down arrows to MileStoneHostingController

--- WordPress/Classes/ViewRelated/Notifications/Milestone Notifications/MilestoneHostingController.swift
+++ WordPress/Classes/ViewRelated/Notifications/Milestone Notifications/MilestoneHostingController.swift
@@ -1,11 +1,51 @@
 import SwiftUI
 import UIKit
+import DesignSystem
 
 final class MilestoneHostingController<Content: View>: UIHostingController<Content> {
+    private lazy var nextButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(.gridicon(.arrowUp), for: .normal)
+//        button.addTarget(self, action: #selector(nextButtonTapped), for: .touchUpInside)
+        button.accessibilityLabel = NSLocalizedString("Next notification", comment: "Accessibility label for the next notification button")
+        return button
+    }()
+
+    private lazy var previousButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(.gridicon(.arrowDown), for: .normal)
+//        button.addTarget(self, action: #selector(previousButtonTapped), for: .touchUpInside)
+        button.accessibilityLabel = NSLocalizedString("Previous notification", comment: "Accessibility label for the previous notification button")
+        return button
+    }()
+
+    func configureNavBarButtons() {
+        var barButtonItems: [UIBarButtonItem] = []
+
+        if splitViewControllerIsHorizontallyCompact {
+            barButtonItems.append(makeNavigationButtons())
+        }
+
+        navigationItem.setRightBarButtonItems(barButtonItems, animated: false)
+    }
+
+    func makeNavigationButtons() -> UIBarButtonItem {
+        // Create custom view to match that in NotificationDetailsViewController.
+        let buttonStackView = UIStackView(arrangedSubviews: [nextButton, previousButton])
+        buttonStackView.axis = .horizontal
+        buttonStackView.spacing = .DS.Padding.split // Constants.arrowButtonSpacing
+
+        let width = CGFloat.DS.Padding.max + .DS.Padding.split // Constants.arrowButtonSpacing
+        buttonStackView.frame = CGRect(x: 0, y: 0, width: width, height: .DS.Padding.medium)
+
+        return UIBarButtonItem(customView: buttonStackView)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         makeNavigationBarTransparent()
         setupConstraints()
+        configureNavBarButtons()
     }
 
     private func makeNavigationBarTransparent() {
