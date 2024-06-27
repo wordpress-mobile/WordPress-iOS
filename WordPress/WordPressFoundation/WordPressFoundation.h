f5191d461a694312df5a9ffa330a5bb3a91d8cb5
kean <grebenyuk.alexander@gmail.com>
2024-06-27 19:02:29 -0400

    Add WordPressFoundation

---
+++ WordPress/WordPressFoundation/WordPressFoundation.h
@@ -0,0 +1,19 @@
+//
+//  WordPressFoundation.h
+//  WordPressFoundation
+//
+//  Created by Alex on 6/27/24.
+//  Copyright © 2024 WordPress. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WordPressFoundation.
+FOUNDATION_EXPORT double WordPressFoundationVersionNumber;
+
+//! Project version string for WordPressFoundation.
+FOUNDATION_EXPORT const unsigned char WordPressFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WordPressFoundation/PublicHeader.h>
+
+
