39186e5f6bea368c772860643d9aea537d99de2d
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2024-07-09 16:32:50 -0600

    Use pre-production wordpress.rs build

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -45,7 +45,7 @@ let package = Package(
         .package(url: "https://github.com/wordpress-mobile/NSURL-IDN", branch: "trunk"),
         .package(url: "https://github.com/wordpress-mobile/WordPressKit-iOS", branch: "wpios-edition"),
         .package(url: "https://github.com/zendesk/support_sdk_ios", from: "8.0.3"),
-        .package(url: "https://github.com/Automattic/wordpress-rs", revision: "5a92dd173fe4df0822a9f2c049cc632b58830bae"),
+        .package(url: "https://github.com/Automattic/wordpress-rs", from: "0.1.0"),
     ],
     targets: XcodeSupport.targets + [
         .target(name: "JetpackStatsWidgetsCore"),
