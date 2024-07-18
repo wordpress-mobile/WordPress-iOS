94783337be8d03f21acf536d897080aadef89eaa
Gio Lodi <gio@mokacoding.com>
2024-07-18 16:12:48 +1000

    Revert "Skip as experiment"
    This reverts commit e1be8fa67d69fafc55a2e09993d59a93ed248897.

--- .buildkite/commands/set-up-git-for-private-repos.sh
+++ .buildkite/commands/set-up-git-for-private-repos.sh
@@ -7,11 +7,10 @@ fi
 
 echo "--- :git: Change Git SSH key to fetch private dependencies"
 
-# PRIVATE_REPO_FETCH_KEY_NAME="private_repos_key"
-# # $PRIVATE_REPOS_BOT_KEY is declared in the `env` file of this pipeline in `mobile-secrets`
-# add_ssh_key_to_agent "$PRIVATE_REPOS_BOT_KEY" "$PRIVATE_REPO_FETCH_KEY_NAME"
-# PRIVATE_REPO_FETCH_KEY="$HOME/.ssh/$PRIVATE_REPO_FETCH_KEY_NAME"
+PRIVATE_REPO_FETCH_KEY_NAME="private_repos_key"
+# $PRIVATE_REPOS_BOT_KEY is declared in the `env` file of this pipeline in `mobile-secrets`
+add_ssh_key_to_agent "$PRIVATE_REPOS_BOT_KEY" "$PRIVATE_REPO_FETCH_KEY_NAME"
+PRIVATE_REPO_FETCH_KEY="$HOME/.ssh/$PRIVATE_REPO_FETCH_KEY_NAME"
 
-# export GIT_SSH_COMMAND="ssh -i $PRIVATE_REPO_FETCH_KEY -o IdentitiesOnly=yes"
-# echo "Git SSH command is now $GIT_SSH_COMMAND"
-echo "^^^ +++ SKIPPED AS AN EXPERIMENT"
+export GIT_SSH_COMMAND="ssh -i $PRIVATE_REPO_FETCH_KEY -o IdentitiesOnly=yes"
+echo "Git SSH command is now $GIT_SSH_COMMAND"
