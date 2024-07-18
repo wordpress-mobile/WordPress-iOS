b8fceb767363e9efcc09933b70af1b841a0019c2
Gio Lodi <gio@mokacoding.com>
2024-07-18 16:27:15 +1000

    Switch key used to fetch WordPress rs

--- .buildkite/commands/set-up-git-to-fetch-wordpress-rs.sh
+++ .buildkite/commands/set-up-git-to-fetch-wordpress-rs.sh
@@ -7,10 +7,10 @@ fi
 
 echo "--- :git: Change Git SSH key to fetch private dependencies"
 
-PRIVATE_REPO_FETCH_KEY_NAME="private_repos_key"
-# $PRIVATE_REPOS_BOT_KEY is declared in the `env` file of this pipeline in `mobile-secrets`
-add_ssh_key_to_agent "$PRIVATE_REPOS_BOT_KEY" "$PRIVATE_REPO_FETCH_KEY_NAME"
-PRIVATE_REPO_FETCH_KEY="$HOME/.ssh/$PRIVATE_REPO_FETCH_KEY_NAME"
+WORDPRESS_RS_FETCH_KEY_NAME="read_only_deploy_key_to_fetch_wordpress_rs"
+# $WORDPRESS_RS_KEY is declared in the `env` file of this pipeline in `mobile-secrets`
+add_ssh_key_to_agent "$WORDPRESS_RS_KEY" "$WORDPRESS_RS_FETCH_KEY_NAME"
+WORDPRESS_RS_FETCH_KEY="$HOME/.ssh/$WORDPRESS_RS_FETCH_KEY_NAME"
 
-export GIT_SSH_COMMAND="ssh -i $PRIVATE_REPO_FETCH_KEY -o IdentitiesOnly=yes"
+export GIT_SSH_COMMAND="ssh -i $WORDPRESS_RS_FETCH_KEY -o IdentitiesOnly=yes"
 echo "Git SSH command is now $GIT_SSH_COMMAND"
