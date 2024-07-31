28f3b3ad6bfc6a3071af5b591b103e67dbb250b7
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2024-07-31 16:12:37 -0600

    Remove BlogService+List

--- WordPress/Classes/Services/BlogService+List.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-extension BlogService {
-    func listBlogs(in context: NSManagedObjectContext) throws -> [Blog] {
-        try context.fetch(Blog.fetchRequest()).compactMap { $0 as? Blog }
-    }
-}
