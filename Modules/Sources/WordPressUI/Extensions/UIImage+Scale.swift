289e196404147b2f329ddc6e40a952c5d33efca0
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2024-08-07 11:32:27 -0600

    Add more tests

--- Modules/Sources/WordPressUI/Extensions/UIImage+Scale.swift
+++ Modules/Sources/WordPressUI/Extensions/UIImage+Scale.swift
@@ -9,27 +9,62 @@ public extension UIImage {
     }
 
     @objc
-    func resized (to size: CGSize, format: ScalingMode = .scaleAspectFit) -> UIImage {
-
-        let horizontalRatio = size.width / self.size.width
-        let verticalRatio = size.height / self.size.height
-
-        let ratio: CGFloat
-
-        switch format {
-        case .scaleAspectFill:
-            ratio = max(horizontalRatio, verticalRatio)
-        case .scaleAspectFit:
-            ratio = min(horizontalRatio, verticalRatio)
-        }
-
-        let newSize = CGSize(width: self.size.width * ratio, height: self.size.height * ratio)
-
+    func resized (to newSize: CGSize, format: ScalingMode = .scaleAspectFit) -> UIImage {
         let renderFormat = UIGraphicsImageRendererFormat.default()
         renderFormat.opaque = false
 
-        return UIGraphicsImageRenderer(size: newSize, format: renderFormat).image { _ in
-            self.draw(in: CGRect(origin: .zero, size: newSize))
+        return UIGraphicsImageRenderer(size: newSize, format: renderFormat).image { context in
+            context.cgContext.concatenate(transform(forSuggestedSize: newSize))
         }
+    }
+
+    func dimensions(forSuggestedSize newSize: CGSize, format: ScalingMode = .scaleAspectFit) -> CGSize {
+        let imageSize = self.size
+        let aspectWidth = newSize.width / imageSize.width
+        let aspectHeight = newSize.height / imageSize.height
+        var scaleFactor: CGFloat
+
+        switch format {
+        case .scaleAspectFit:
+            scaleFactor = min(aspectWidth, aspectHeight)
+        case .scaleAspectFill:
+            scaleFactor = max(aspectWidth, aspectHeight)
+        }
+
+        return CGSize(
+            width: (imageSize.width * scaleFactor).rounded(.toNearestOrEven),
+            height: (imageSize.height * scaleFactor).rounded(.toNearestOrEven)
+        )
+    }
+
+    func transform(forSuggestedSize newSize: CGSize) -> CGAffineTransform {
+        var transform = CGAffineTransformIdentity
+
+        switch self.imageOrientation {
+        case .down, .downMirrored:
+            transform = CGAffineTransformTranslate(transform, newSize.width, newSize.height)
+            transform = CGAffineTransformRotate(transform, .pi)
+        case .left, .leftMirrored:
+            transform = CGAffineTransformTranslate(transform, newSize.width, 0)
+            transform = CGAffineTransformRotate(transform, .pi / 2)
+        case .right, .rightMirrored:
+            transform = CGAffineTransformTranslate(transform, 0, newSize.height)
+            transform = CGAffineTransformRotate(transform, .pi / -2)
+        default:
+            break // Do nothing – the image is already using the right orientation
+        }
+
+        switch self.imageOrientation {
+        case .upMirrored, .downMirrored:
+            transform = CGAffineTransformTranslate(transform, newSize.width, 0)
+            transform = CGAffineTransformScale(transform, -1, 1)
+        case .leftMirrored, .rightMirrored:
+            transform = CGAffineTransformTranslate(transform, newSize.height, 0)
+            transform = CGAffineTransformScale(transform, -1, 1)
+        default:
+            break // Do nothing
+        }
+
+        return transform
     }
 }
---
+++ Modules/Tests/WordPressUITests/Extensions/UIImage+ScaleTests.swift
@@ -0,0 +1,49 @@
+import XCTest
+@testable import WordPressUI
+
+final class UIImage_ScaleTests: XCTestCase {
+
+    let originalImage = UIImage.from(color: .blue, havingSize: CGSize(width: 1024, height: 768))
+
+    func testAspectFitIntoSquare() {
+        let targetSize = CGSize(width: 1000, height: 1000)
+        let size = originalImage.dimensions(forSuggestedSize: targetSize, format: .scaleAspectFit)
+        XCTAssertEqual(size, CGSize(width: 1000, height: 750))
+    }
+
+    func testAspectFitIntoSmallerSize() {
+        let targetSize = CGSize(width: 101, height: 76)
+        let size = originalImage.dimensions(forSuggestedSize: targetSize, format: .scaleAspectFit)
+        XCTAssertEqual(size, targetSize)
+    }
+
+    func testAspectFitIntoLargerSize() {
+        let targetSize = CGSize(width: 2000, height: 1000)
+        let size = originalImage.dimensions(forSuggestedSize: targetSize, format: .scaleAspectFit)
+        XCTAssertEqual(size, CGSize(width: 1333, height: 1000))
+    }
+
+    func testAspectFillIntoSquare() {
+        let targetSize = CGSize(width: 100, height: 100)
+        let size = originalImage.dimensions(forSuggestedSize: targetSize, format: .scaleAspectFill)
+        XCTAssertEqual(size, CGSize(width: 133, height: 100))
+    }
+
+    func testAspectFillIntoSmallerSize() {
+        let targetSize = CGSize(width: 103, height: 77)
+        let size = originalImage.dimensions(forSuggestedSize: targetSize, format: .scaleAspectFill)
+        XCTAssertEqual(size, targetSize)
+    }
+
+    func testAspectFillIntoLargerSize() {
+        let targetSize = CGSize(width: 2000, height: 1000)
+        let size = originalImage.dimensions(forSuggestedSize: targetSize, format: .scaleAspectFill)
+        XCTAssertEqual(size, CGSize(width: 2000, height: 1500))
+    }
+
+    func testFoo() {
+        let targetSize = CGSize(width: 0, height: 0)
+        let originalImage = UIImage.from(color: .blue, havingSize: CGSize(width: 1024, height: 680))
+        let size = originalImage.dimensions(forSuggestedSize: targetSize, format: .scaleAspectFill)
+    }
+}
