5f3b399ca0fc8b5a1f15502c1a14b7d988079b12
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2024-08-08 00:51:20 -0600

    Document

--- Modules/Sources/WordPressUI/Extensions/UIImage+Color.swift
+++ Modules/Sources/WordPressUI/Extensions/UIImage+Color.swift
@@ -2,6 +2,9 @@ import UIKit
 
 public extension UIImage {
 
+    /// Create an image of the given `size` that's made of a single `color`.
+    ///
+    /// Size is in points.
     convenience init(color: UIColor, size: CGSize = CGSize(width: 1.0, height: 1.0)) {
         let uiImage = UIGraphicsImageRenderer(size: size).image { rendererContext in
             color.setFill()
