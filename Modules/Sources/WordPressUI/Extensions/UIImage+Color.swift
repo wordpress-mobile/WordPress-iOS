b60a712544c23058e1d33f980a921074323b259d
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2024-08-08 14:32:16 -0600

    Fix image

--- Modules/Sources/WordPressUI/Extensions/UIImage+Color.swift
+++ Modules/Sources/WordPressUI/Extensions/UIImage+Color.swift
@@ -6,11 +6,11 @@ public extension UIImage {
     ///
     /// Size is in points.
     convenience init(color: UIColor, size: CGSize = CGSize(width: 1.0, height: 1.0)) {
-        let uiImage = UIGraphicsImageRenderer(size: size).image { rendererContext in
+        let image = UIGraphicsImageRenderer(size: size).image { rendererContext in
             color.setFill()
             rendererContext.fill(CGRect(origin: .zero, size: size))
         }
 
-        self.init(ciImage: uiImage.ciImage!) // Force because there's no reason that the `ciImage` should be nil
+        self.init(cgImage: image.cgImage!) // Force because there's no reason that the `cgImage` should be nil
     }
 }
