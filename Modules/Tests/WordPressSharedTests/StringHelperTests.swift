c9a652af43140f7065575c50e80527829510404f
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2024-08-15 13:08:33 -0600

    Raise errors instead of crashing the test suite

--- Modules/Tests/WordPressSharedTests/StringHelperTests.swift
+++ Modules/Tests/WordPressSharedTests/StringHelperTests.swift
@@ -33,15 +33,15 @@ class StringHelperTests: XCTestCase {
         XCTAssertEqual(string, string.removingSuffix(".org/"))
     }
 
-    func testRemovePrefixPattern() {
+    func testRemovePrefixPattern() throws {
         let string = "X-Post: This is a test"
-        XCTAssertEqual("This is a test", try! string.removingPrefix(pattern: "X-.*?: +"))
-        XCTAssertEqual(string, try! string.removingPrefix(pattern: "Th.* "))
+        XCTAssertEqual("This is a test", try string.removingPrefix(pattern: "X-.*?: +"))
+        XCTAssertEqual(string, try string.removingPrefix(pattern: "Th.* "))
     }
 
-    func testRemoveSuffixPattern() {
+    func testRemoveSuffixPattern() throws {
         let string = "X-Post: This is a test"
-        XCTAssertEqual("X-Post: This is", try! string.removingSuffix(pattern: "( a)? +test"))
-        XCTAssertEqual(string, try! string.removingSuffix(pattern: "Th.* "))
+        XCTAssertEqual("X-Post: This is", try string.removingSuffix(pattern: "( a)? +test"))
+        XCTAssertEqual(string, try string.removingSuffix(pattern: "Th.* "))
     }
 }
