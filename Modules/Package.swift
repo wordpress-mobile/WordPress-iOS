717e43e76b6942249a80dd724b933cd616be4a98
Gerardo <gerardo.pacheco@automattic.com>
2024-09-04 16:16:03 +0200

    Update GutenbergKit ref

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -47,7 +47,7 @@ let package = Package(
         .package(url: "https://github.com/zendesk/support_sdk_ios", from: "8.0.3"),
         // We can't use wordpress-rs branches nor commits here. Only tags work.
         .package(url: "https://github.com/Automattic/wordpress-rs", revision: "alpha-swift-20240813"),
-        .package(url: "https://github.com/wordpress-mobile/GutenbergKit", revision: "715dd82e2c60445d463b5efe7368be03019f21b6"),
+        .package(url: "https://github.com/wordpress-mobile/GutenbergKit", revision: "cbf06853583406129e2511d44d435e5299fd3db0"),
     ],
     targets: XcodeSupport.targets + [
         .target(name: "JetpackStatsWidgetsCore"),
