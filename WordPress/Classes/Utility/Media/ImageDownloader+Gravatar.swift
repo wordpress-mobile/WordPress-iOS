cf61bbe67a811e417c6ba02f2657e95d0078eba4
Pinar Olguc <pinar.olguc@automattic.com>
2024-10-11 18:10:57 +0300

    Add forceRefresh option

--- WordPress/Classes/Utility/Media/ImageDownloader+Gravatar.swift
+++ WordPress/Classes/Utility/Media/ImageDownloader+Gravatar.swift
@@ -4,19 +4,26 @@ import Gravatar
 
 extension ImageDownloader {
 
-    nonisolated func downloadGravatarImage(with email: String, completion: @escaping (UIImage?) -> Void) {
+    nonisolated func downloadGravatarImage(with email: String, forceRefresh: Bool = false, completion: @escaping (UIImage?) -> Void) {
 
         guard let url = AvatarURL.url(for: email) else {
             completion(nil)
             return
         }
 
-        if let cachedImage = ImageCache.shared.getImage(forKey: url.absoluteString) {
+        if !forceRefresh, let cachedImage = ImageCache.shared.getImage(forKey: url.absoluteString) {
             completion(cachedImage)
             return
         }
-
-        downloadImage(at: url) { image, _ in
+        var urlToDownload = url
+        if forceRefresh {
+            var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)
+            // Gravatar doesn't support "Cache-Control: none" header. So we add a random query parameter to
+            // bypass the backend cache and get the latest image.
+            urlComponents?.queryItems?.append(.init(name: "_", value: "\(NSDate().timeIntervalSince1970)"))
+            urlToDownload = urlComponents?.url ?? url
+        }
+        downloadImage(at: urlToDownload) { image, _ in
             DispatchQueue.main.async {
 
                 guard let image else {
--- WordPress/Classes/ViewRelated/Gravatar/UIImageView+Gravatar.swift
+++ WordPress/Classes/ViewRelated/Gravatar/UIImageView+Gravatar.swift
@@ -37,13 +37,14 @@ extension UIImageView {
     public func downloadGravatar(
         for email: String,
         gravatarRating: Rating = .general,
-        placeholderImage: UIImage = .gravatarPlaceholderImage
+        placeholderImage: UIImage = .gravatarPlaceholderImage,
+        forceRefresh: Bool = false
     ) {
         let avatarURL = AvatarURL.url(for: email, preferredSize: .pixels(gravatarDefaultSize()), gravatarRating: gravatarRating)
-        downloadGravatar(fullURL: avatarURL, placeholder: placeholderImage, animate: false)
+        downloadGravatar(fullURL: avatarURL, placeholder: placeholderImage, animate: false, forceRefresh: forceRefresh)
     }
 
-    public func downloadGravatar(_ gravatar: AvatarURL?, placeholder: UIImage, animate: Bool) {
+    public func downloadGravatar(_ gravatar: AvatarURL?, placeholder: UIImage, animate: Bool, forceRefresh: Bool = false) {
         guard let gravatar = gravatar else {
             self.image = placeholder
             return
@@ -56,14 +57,27 @@ extension UIImageView {
         layoutIfNeeded()
 
         let size = Int(ceil(frame.width * min(2, UIScreen.main.scale)))
-        let url = gravatar.replacing(options: .init(preferredSize: .pixels(size)))?.url
-        downloadGravatar(fullURL: url, placeholder: placeholder, animate: animate)
+        guard let url = gravatar.replacing(options: .init(preferredSize: .pixels(size)))?.url else { return }
+        downloadGravatar(fullURL: url, placeholder: placeholder, animate: animate, forceRefresh: forceRefresh)
     }
 
-    private func downloadGravatar(fullURL: URL?, placeholder: UIImage, animate: Bool) {
+    private func downloadGravatar(fullURL: URL?, placeholder: UIImage, animate: Bool, forceRefresh: Bool = false) {
         wp.prepareForReuse()
         if let fullURL {
-            wp.setImage(with: fullURL)
+            var urlToDownload = fullURL
+            if forceRefresh {
+                var urlComponents = URLComponents(url: fullURL, resolvingAgainstBaseURL: false)
+                // Gravatar doesn't support "Cache-Control: none" header. So we add a random query parameter to
+                // bypass the backend cache and get the latest image.
+                urlComponents?.queryItems?.append(.init(name: "_", value: "\(NSDate().timeIntervalSince1970)"))
+                urlToDownload = urlComponents?.url ?? fullURL
+            }
+
+            wp.setImage(with: urlToDownload)
+            if forceRefresh {
+                // update the cache for the original url as well
+                ImageDownloader.shared.setCachedImage(image, for: fullURL)
+            }
             if image == nil { // If image wasn't found synchronously in memory cache
                 image = placeholder
             }
