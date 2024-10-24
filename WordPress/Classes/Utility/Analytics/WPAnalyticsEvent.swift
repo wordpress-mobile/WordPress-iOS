93d96641f5eb0d9ca42b50f7fde0d77ad699feeb
kean <grebenyuk.alexander@gmail.com>
2024-10-24 15:59:14 -0400

    Add .readerDiscoverChannelSelected

--- WordPress/Classes/Utility/Analytics/WPAnalyticsEvent.swift
+++ WordPress/Classes/Utility/Analytics/WPAnalyticsEvent.swift
@@ -320,6 +320,9 @@ import Foundation
     case postRepositoryConflictEncountered
     case postRepositoryPostsFetchFailed
 
+    // Reader: Discover
+    case readerDiscoverChannelSelected
+
     // Reader: Filter Sheet
     case readerFilterSheetDisplayed
     case readerFilterSheetDismissed
@@ -1197,6 +1200,10 @@ import Foundation
             return "post_repository_conflict_encountered"
         case .postRepositoryPostsFetchFailed:
             return "post_repository_posts_fetch_failed"
+
+        // Reader: Discover
+        case .readerDiscoverChannelSelected:
+            return "reader_discover_channel_selected"
 
         // Reader: Filter Sheet
         case .readerFilterSheetDisplayed:
