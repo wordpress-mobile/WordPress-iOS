1d8fd185469e9c4248024f5e222c1f16f584a17f
Ian Maia <ian.maia@automattic.com>
2024-10-25 13:04:07 +0200

    Add comment to configure-for-release.sh explaining the separate flags

--- .buildkite/commands/configure-for-release.sh
+++ .buildkite/commands/configure-for-release.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
   echo "This script must be 'source'd (instead of being called directly as an executable) to work properly"
   exit 1
 fi
 
+# Note: Set the flags separately (instead of via the shebang), because that script will be `source`'d and not run directly
 set -e
 set -u
 
