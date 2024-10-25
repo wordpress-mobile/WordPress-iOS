d71beac49fd19e7b2e791b238db3756b03fbb2b6
Pinar Olguc <pinar.olguc@automattic.com>
2024-10-25 12:35:13 +0300

    Adjust the update method

--- WordPress/Classes/Services/GravatarService.swift
+++ WordPress/Classes/Services/GravatarService.swift
@@ -8,7 +8,7 @@ import Gravatar
 
 public protocol GravatarImageUploader {
     @discardableResult
-    func upload(_ image: UIImage, email: Email, accessToken: String) async throws -> URLResponse
+    func upload(_ image: UIImage, selectionBehavior: AvatarSelection, accessToken: String) async throws -> AvatarType
 }
 
 extension AvatarService: GravatarImageUploader { }
@@ -68,7 +68,7 @@ public class GravatarService {
 
         Task {
             do {
-                try await imageUploader.upload(image, email: Email(email), accessToken: accountToken)
+                try await imageUploader.upload(image, selectionBehavior: .selectUploadedImage(for: Email(email)), accessToken: accountToken)
                 DDLogInfo("GravatarService.uploadImage Success!")
                 completion?(nil)
             } catch {
