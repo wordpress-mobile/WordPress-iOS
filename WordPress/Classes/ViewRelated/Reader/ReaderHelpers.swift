18ff7f5ab4a9f68950362d7f86e8276a1cb2d287
kean <grebenyuk.alexander@gmail.com>
2024-10-25 13:19:27 -0400

    Remove ReaderTagCardCellViewModel

--- WordPress/Classes/ViewRelated/Reader/ReaderHelpers.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderHelpers.swift
@@ -22,8 +22,6 @@ extension NSNotification.Name {
     static let ReaderUserBlockingWillBegin = NSNotification.Name(rawValue: "ReaderUserBlockingWillBegin")
     // Sent when the user blocking request is complete
     static let ReaderUserBlockingDidEnd = NSNotification.Name(rawValue: "ReaderUserBlockingDidEnd")
-    // Sent when the filter from a feed is updated
-    static let ReaderFilterUpdated = NSNotification.Name(rawValue: "ReaderFilterUpdated")
 }
 
 struct ReaderNotificationKeys {
