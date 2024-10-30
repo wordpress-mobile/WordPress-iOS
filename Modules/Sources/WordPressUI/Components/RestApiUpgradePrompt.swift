c3ac0e32c7c798029c75be1c984c0b63646c8a5f
Tony Li <tony.li@automattic.com>
2024-10-30 21:18:20 +1300

    Add a RestApiUpgradePrompt for migrating site to application passwords

---
+++ Modules/Sources/WordPressUI/Components/RestApiUpgradePrompt.swift
@@ -0,0 +1,63 @@
+import SwiftUI
+import DesignSystem
+
+public struct RestApiUpgradePrompt: View {
+
+    private let localizedFeatureName: String
+    private var didTapGetStarted: () -> Void
+
+    public init(localizedFeatureName: String, didTapGetStarted: @escaping () -> Void) {
+        self.localizedFeatureName = localizedFeatureName
+        self.didTapGetStarted = didTapGetStarted
+    }
+
+    public var body: some View {
+        VStack {
+            let scrollView = ScrollView {
+                VStack(alignment: .leading) {
+                    Text(Strings.title)
+                        .font(.largeTitle)
+                        .fontWeight(.semibold)
+                        .padding(.bottom)
+
+                    Text(Strings.description(localizedFeatureName: localizedFeatureName))
+                        .font(.body)
+                }.padding()
+            }
+
+            if #available(iOS 16.4, *) {
+                scrollView.scrollBounceBehavior(.basedOnSize, axes: [.vertical])
+            }
+
+            Spacer()
+            VStack {
+                Button(action: didTapGetStarted, label: {
+                    HStack {
+                        Spacer()
+                        Text("Get Started")
+                            .font(.headline)
+                            .padding(.DS.Padding.half)
+                        Spacer()
+                    }
+                }).buttonStyle(.borderedProminent)
+            }.padding()
+        }
+    }
+
+    private enum Strings {
+        static var title: String {
+            NSLocalizedString("applicationPasswordRequired.title", value: "Application Password Required", comment: "Title for the prompt to upgrade to Application Passwords")
+        }
+
+        static func description(localizedFeatureName: String) -> String {
+            let format = NSLocalizedString("applicationPasswordRequired.description", value: "Application passwords are a more secure way to connect to your self-hosted site, and enable support for features like %@.", comment: "Description for the prompt to upgrade to Application Passwords. The first argument is the name of the feature that requires Application Passwords.")
+            return String(format: format, localizedFeatureName)
+        }
+    }
+}
+
+#Preview {
+    RestApiUpgradePrompt(localizedFeatureName: "User Management") {
+        debugPrint("Tapped Get Started")
+    }
+}
