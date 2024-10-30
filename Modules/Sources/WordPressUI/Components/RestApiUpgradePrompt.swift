19cae00b472db09dd6acb173a14f09ff8273d3d2
Tony Li <tony.li@automattic.com>
2024-10-30 21:29:47 +1300

    Localize the "Get Started" button

--- Modules/Sources/WordPressUI/Components/RestApiUpgradePrompt.swift
+++ Modules/Sources/WordPressUI/Components/RestApiUpgradePrompt.swift
@@ -34,7 +34,7 @@ public struct RestApiUpgradePrompt: View {
                 Button(action: didTapGetStarted, label: {
                     HStack {
                         Spacer()
-                        Text("Get Started")
+                        Text(Strings.getStarted)
                             .font(.headline)
                             .padding(.DS.Padding.half)
                         Spacer()
@@ -52,6 +52,10 @@ public struct RestApiUpgradePrompt: View {
         static func description(localizedFeatureName: String) -> String {
             let format = NSLocalizedString("applicationPasswordRequired.description", value: "Application passwords are a more secure way to connect to your self-hosted site, and enable support for features like %@.", comment: "Description for the prompt to upgrade to Application Passwords. The first argument is the name of the feature that requires Application Passwords.")
             return String(format: format, localizedFeatureName)
+        }
+
+        static var getStarted: String {
+            NSLocalizedString("applicationPasswordRequired.getStartedButton", value: "Get Started", comment: "Title for the button to authenticate with Application Passwords")
         }
     }
 }
