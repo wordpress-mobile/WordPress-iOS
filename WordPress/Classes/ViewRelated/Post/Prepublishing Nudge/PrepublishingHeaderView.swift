9463f0967b92c556e6a8e7c491f52034c5bc5c19
kean <grebenyuk.alexander@gmail.com>
2024-11-01 17:34:30 -0400

    Remove downloadSiteIcon usage from PrepublishingHeaderView

--- WordPress/Classes/ViewRelated/Post/Prepublishing Nudge/PrepublishingHeaderView.swift
+++ WordPress/Classes/ViewRelated/Post/Prepublishing Nudge/PrepublishingHeaderView.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 final class PrepublishingHeaderView: UIView {
-    private let blogImageView = UIImageView()
+    private let blogImageView = SiteIconHostingView()
     private let publishingToLabel = UILabel()
     private let blogTitleLabel = UILabel()
 
@@ -34,9 +34,10 @@ final class PrepublishingHeaderView: UIView {
         WPStyleGuide.applyBorderStyle(separator)
         separator.alpha = 0
 
+        let imageSize = SiteIconViewModel.Size.regular.width
         NSLayoutConstraint.activate([
-            blogImageView.widthAnchor.constraint(equalToConstant: 44),
-            blogImageView.heightAnchor.constraint(equalToConstant: 44),
+            blogImageView.widthAnchor.constraint(equalToConstant: imageSize),
+            blogImageView.heightAnchor.constraint(equalToConstant: imageSize),
         ])
 
         let labelsStackView = UIStackView(arrangedSubviews: [publishingToLabel, blogTitleLabel])
@@ -72,7 +73,7 @@ final class PrepublishingHeaderView: UIView {
     }
 
     func configure(_ blog: Blog) {
-        blogImageView.downloadSiteIcon(for: blog)
+        blogImageView.setIcon(with: .init(blog: blog, size: .regular))
         blogTitleLabel.text = blog.title
     }
 }
