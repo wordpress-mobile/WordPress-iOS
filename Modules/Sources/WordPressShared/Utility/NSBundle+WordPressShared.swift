8720c7edef3d95d82441eb44a59475f825ad2a8e
Gio Lodi <gio@mokacoding.com>
2025-01-07 08:09:05 +0100

    Remove a couple of CocoaPods-specific code branches

--- Modules/Sources/WordPressShared/Utility/NSBundle+WordPressShared.swift
+++ Modules/Sources/WordPressShared/Utility/NSBundle+WordPressShared.swift
@@ -4,10 +4,6 @@ private class BundleFinder: NSObject {}
 
 extension Bundle {
 
-    /// Returns the WordPressShared Bundle
-    /// If installed via CocoaPods, this will be WordPressShared.bundle,
-    /// otherwise it will be the framework bundle.
-    ///
     @objc public class var wordPressSharedBundle: Bundle {
 #if DEBUG
         // Workaround for https://forums.swift.org/t/swift-5-3-swiftpm-resources-in-tests-uses-wrong-bundle-path/37051
