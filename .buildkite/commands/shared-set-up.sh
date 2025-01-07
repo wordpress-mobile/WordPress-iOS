68de61811b890d40d387a7d45ad546e9a9fb6b5a
Gio Lodi <gio@mokacoding.com>
2025-01-07 08:31:11 +0100

    Fetch XCFrameworks in CI

---
+++ .buildkite/commands/shared-set-up.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -eu
+
+echo "--- :rubygems: Setting up Gems"
+install_gems
+
+echo "--- :swift: Setting up Swift Packages"
+install_swiftpm_dependencies
+
+echo "--- :xcode: Fetch XCFrameworks"
+pushd WordPress
+make dependencies
+popd
