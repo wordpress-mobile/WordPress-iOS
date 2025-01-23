437f7aed306c35108c54a66b41d7289e013d13d0
Gio Lodi <gio@mokacoding.com>
2025-01-23 11:06:40 +1100

    Do not run build-time SwiftLint when building in CI

--- Scripts/BuildPhases/SwiftLint.sh
+++ Scripts/BuildPhases/SwiftLint.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -e
 
+# Do not run in CI environments.
+# Our CI has its own static linter.
+# As of 2025/01, this should save some 20-40s per build.
+if [ -n "${CI+x}" ]; then
+  echo 'CI environment detected. Skipping SwiftLint build phase in favor of dedicated CI process.'
+  exit 0
+fi
+
 rake lint
