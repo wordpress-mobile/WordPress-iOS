108380efba6925df6501339c891182b2ec142162
kean <grebenyuk.alexander@gmail.com>
2025-01-30 16:13:07 -0500

    Disable FF

--- WordPress/Classes/Utility/BuildInformation/FeatureFlag.swift
+++ WordPress/Classes/Utility/BuildInformation/FeatureFlag.swift
@@ -51,7 +51,7 @@ enum FeatureFlag: Int, CaseIterable {
         case .selfHostedSiteUserManagement:
             return false
         case .readerCommentsWebKit:
-            return true // BuildConfiguration.current != .appStore
+            return false
         }
     }
 
