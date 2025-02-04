e8a16a95318f4c48a455fe160450f241912933ef
Tony Li <tony.li@automattic.com>
2025-02-04 20:50:15 +1300

    Display plugin description in plain text instead of HTML

--- WordPress/Classes/Plugins/Views/PluginListItemView.swift
+++ WordPress/Classes/Plugins/Views/PluginListItemView.swift
@@ -3,11 +3,11 @@ import SwiftUI
 import AsyncImageKit
 import WordPressAPI
 import WordPressCore
+import WordPressShared
 import SafariServices
 
 struct PluginListItemView: View {
 
-    @ScaledMetric(relativeTo: .body) var descriptionFontSize: CGFloat = 14
     @State private var showingSafariView = false
 
     let plugin: InstalledPlugin
@@ -23,24 +23,15 @@ struct PluginListItemView: View {
             PluginIconView(slug: plugin.possibleWpOrgDirectorySlug, service: viewModel.service)
 
             VStack(alignment: .leading, spacing: 0) {
-                Text(plugin.name)
+                Text(plugin.name.makePlainText())
                     .lineLimit(1)
                     .font(.headline)
                     .foregroundStyle(.primary)
 
-                Group {
-                    if plugin.shortDescription.isEmpty {
-                        Text(Strings.noDescriptionAvailable)
-                            .font(.system(size: descriptionFontSize).italic())
-                    } else if let html = renderedDescription() {
-                        Text(html)
-                    } else {
-                        Text(plugin.shortDescription)
-                            .font(.system(size: descriptionFontSize))
-                    }
-                }
-                .lineLimit(2)
-                .padding(.vertical, 4)
+                Text(plugin.shortDescription.makePlainText())
+                    .lineLimit(2)
+                    .font(.body)
+                    .foregroundStyle(.primary)
 
                 Text(Strings.version(plugin.version))
                     .font(.caption)
@@ -94,36 +85,6 @@ struct PluginListItemView: View {
             if let url = wpOrgURL {
                 SafariView(url: url)
             }
-        }
-    }
-
-    // TODO: Use `WebCommentContentRenderer` instead.
-    // There are potential crash and performance issues in NSAttributedString's HTML support.
-    // http://www.openradar.me/20978452
-    func renderedDescription() -> AttributedString? {
-        guard var data = plugin.shortDescription.data(using: .utf8) else {
-            return nil
-        }
-
-        // We want to use the system font, instead of the default "Times New Roman" font in the rendered HTML.
-        // Using `.defaultAttributes: [.font: systemFont(...)]` in the `NSAttributedString` initialiser below doesn't
-        // work. Using a CSS style here as a workaround.
-        data.append(contentsOf: "<style> body { font-family: -apple-system; font-size: \(descriptionFontSize)px; } </style>".data(using: .utf8)!)
-
-        do {
-            let string = try NSAttributedString(
-                data: data,
-                options: [
-                    .documentType: NSAttributedString.DocumentType.html,
-                    .characterEncoding: String.Encoding.utf8.rawValue,
-                    .sourceTextScaling: NSTextScalingType.iOS,
-                ],
-                documentAttributes: nil
-            )
-            return try AttributedString(string, including: \.uiKit)
-        } catch {
-            DDLogError("Failed to parse HTML: \(error)")
-            return nil
         }
     }
 
