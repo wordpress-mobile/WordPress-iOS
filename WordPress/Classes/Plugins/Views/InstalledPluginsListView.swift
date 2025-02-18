3e164fe552fe503c2677492ef80601598284a763
Tony Li <tony.li@automattic.com>
2025-02-19 12:15:21 +1300

    Show disclosure indicator in the installed plugins list

--- WordPress/Classes/Plugins/Views/InstalledPluginsListView.swift
+++ WordPress/Classes/Plugins/Views/InstalledPluginsListView.swift
@@ -25,15 +25,16 @@ struct InstalledPluginsListView: View {
                     Section {
                         ForEach(viewModel.displayingPlugins, id: \.self) { plugin in
                             ZStack {
-                                PluginListItemView(plugin: plugin, updateAvailable: viewModel.updateAvailable.index(forKey: plugin.slug) != nil, service: viewModel.service)
-                                if let slug = plugin.possibleWpOrgDirectorySlug {
-                                    // Using `PluginListItemView` as `NavigationLink`'s content would show an disclosure
-                                    // indicator on the list cell, which looks a bit off with the ellipsis button on the
-                                    // list cell.
-                                    // Here we use an empty transparent `NavigationLink` as a workaround to hide the
-                                    // disclosure indicator.
-                                    NavigationLink { PluginDetailsView(slug: slug, plugin: plugin, service: viewModel.service) } label: { EmptyView() }
-                                        .opacity(0.0)
+                                NavigationLink {
+                                    if let slug = plugin.possibleWpOrgDirectorySlug {
+                                        PluginDetailsView(slug: slug, plugin: plugin, service: viewModel.service)
+                                    }
+                                } label: {
+                                    PluginListItemView(
+                                        plugin: plugin,
+                                        updateAvailable: viewModel.updateAvailable.index(forKey: plugin.slug) != nil,
+                                        service: viewModel.service
+                                    )
                                 }
                             }
                         }
