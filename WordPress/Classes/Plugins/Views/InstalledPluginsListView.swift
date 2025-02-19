0e345aa711efaf4ed25d2894c8d2784845ee88ba
Tony Li <tony.li@automattic.com>
2025-02-19 13:05:17 +1300

    Show active and inactive sections in the installed plugins list

--- WordPress/Classes/Plugins/Views/InstalledPluginsListView.swift
+++ WordPress/Classes/Plugins/Views/InstalledPluginsListView.swift
@@ -18,13 +18,13 @@ struct InstalledPluginsListView: View {
         ZStack {
             if let error = viewModel.error {
                 EmptyStateView(error, systemImage: "exclamationmark.triangle.fill")
-            } else if viewModel.isRefreshing && viewModel.displayingPlugins.isEmpty {
+            } else if viewModel.isRefreshing && viewModel.sections.isEmpty {
                 Label { Text(Strings.loading) } icon: { ProgressView() }
             } else {
                 List {
-                    Section {
-                        ForEach(viewModel.displayingPlugins, id: \.self) { plugin in
-                            ZStack {
+                    ForEach(viewModel.sections, id: \.self) { section in
+                        Section {
+                            ForEach(section.plugins, id: \.self) { plugin in
                                 NavigationLink {
                                     if let slug = plugin.possibleWpOrgDirectorySlug {
                                         PluginDetailsView(slug: slug, plugin: plugin, service: viewModel.service)
@@ -37,11 +37,17 @@ struct InstalledPluginsListView: View {
                                     )
                                 }
                             }
+                        } header: {
+                            Text(section.filter.title)
+                                .textCase(nil)
+                                .font(.headline)
+                                .foregroundStyle(.primary)
                         }
+                        .listSectionSeparator(.hidden, edges: .all)
                     }
-                    .listSectionSeparator(.hidden, edges: .top)
                 }
-                .listStyle(.plain)
+                .listStyle(.grouped)
+                .scrollContentBackground(.hidden)
                 .refreshable(action: viewModel.refreshItems)
             }
         }
@@ -57,9 +63,9 @@ struct InstalledPluginsListView: View {
             ToolbarItem(placement: .topBarTrailing) {
                 Menu {
                     Picker(Strings.filterTitle, selection: $viewModel.filter) {
-                        Text(Strings.filterOptionAll).tag(InstalledPluginsListViewModel.PluginFilter.all)
-                        Text(Strings.filterOptionActive).tag(InstalledPluginsListViewModel.PluginFilter.active)
-                        Text(Strings.filterOptionInactive).tag(InstalledPluginsListViewModel.PluginFilter.inactive)
+                        Text(Strings.filterOptionAll).tag(PluginFilter.all)
+                        Text(Strings.filterOptionActive).tag(PluginFilter.active)
+                        Text(Strings.filterOptionInactive).tag(PluginFilter.inactive)
                     }
                 } label: {
                     Image(systemName: "ellipsis.circle")
@@ -81,44 +87,63 @@ struct InstalledPluginsListView: View {
             await viewModel.performQuery()
         }
     }
+}
 
-    private enum Strings {
-        static let title: String = NSLocalizedString("site.plugins.title", value: "Plugins", comment: "Installed plugins list title")
-        static let loading: String = NSLocalizedString("site.plugins.loading", value: "Loading installed plugins…", comment: "Message displayed when fetching installed plugins from the site")
-        static let noPluginInstalled: String = NSLocalizedString("site.plugins.noInstalledPlugins", value: "You haven't installed any plugins yet", comment: "No installed plugins message")
-        static let filterTitle: String = NSLocalizedString("site.plugins.filter.title", value: "Filter", comment: "Title of the plugin filter picker")
-        static let filterOptionAll: String = NSLocalizedString("site.plugins.filter.option.all", value: "All", comment: "The plugin fillter option for displaying all plugins")
-        static let filterOptionActive: String = NSLocalizedString("site.plugins.filter.option.all", value: "Active", comment: "The plugin fillter option for displaying active plugins")
-        static let filterOptionInactive: String = NSLocalizedString("site.plugins.filter.option.all", value: "Inactive", comment: "The plugin fillter option for displaying inactive plugins")
+private enum Strings {
+    static let title: String = NSLocalizedString("site.plugins.title", value: "Plugins", comment: "Installed plugins list title")
+    static let loading: String = NSLocalizedString("site.plugins.loading", value: "Loading installed plugins…", comment: "Message displayed when fetching installed plugins from the site")
+    static let noPluginInstalled: String = NSLocalizedString("site.plugins.noInstalledPlugins", value: "You haven't installed any plugins yet", comment: "No installed plugins message")
+    static let filterTitle: String = NSLocalizedString("site.plugins.filter.title", value: "Filter", comment: "Title of the plugin filter picker")
+    static let filterOptionAll: String = NSLocalizedString("site.plugins.filter.option.all", value: "All", comment: "The plugin fillter option for displaying all plugins")
+    static let filterOptionActive: String = NSLocalizedString("site.plugins.filter.option.all", value: "Active", comment: "The plugin fillter option for displaying active plugins")
+    static let filterOptionInactive: String = NSLocalizedString("site.plugins.filter.option.all", value: "Inactive", comment: "The plugin fillter option for displaying inactive plugins")
+}
+
+private struct ListSection: Hashable, Identifiable {
+    var plugins: [InstalledPlugin]
+    var filter: PluginFilter
+
+    var id: PluginFilter { filter }
+}
+
+private enum PluginFilter: Int, Hashable {
+    // The order here matches the order of grouped plugins
+    case all
+    case active
+    case inactive
+
+    var query: PluginDataStoreQuery {
+        switch self {
+        case .all:
+            return .all
+        case .active:
+            return .active
+        case .inactive:
+            return .inactive
+        }
+    }
+
+    var title: String {
+        switch self {
+        case .all:
+            return Strings.filterOptionAll
+        case .active:
+            return Strings.filterOptionActive
+        case .inactive:
+            return Strings.filterOptionInactive
+        }
     }
 }
 
 @MainActor
-final class InstalledPluginsListViewModel: ObservableObject {
+private final class InstalledPluginsListViewModel: ObservableObject {
 
     let service: PluginServiceProtocol
     private var initialLoad = false
 
-    enum PluginFilter: Hashable {
-        case all
-        case active
-        case inactive
-
-        var query: PluginDataStoreQuery {
-            switch self {
-            case .all:
-                return .all
-            case .active:
-                return .active
-            case .inactive:
-                return .inactive
-            }
-        }
-    }
-
     @Published var isRefreshing: Bool = false
     @Published var filter: PluginFilter = .all
-    @Published var displayingPlugins: [InstalledPlugin] = []
+    @Published var sections = [ListSection]()
     @Published var updateAvailable: [PluginSlug: UpdateCheckPluginInfo] = [:]
     @Published var error: String? = nil
 
@@ -151,7 +176,15 @@ final class InstalledPluginsListViewModel: ObservableObject {
         for await update in await self.service.installedPluginsUpdates(query: filter.query) {
             switch update {
             case let .success(plugins):
-                self.displayingPlugins = plugins
+                self.sections = plugins
+                    .reduce(into: [PluginFilter: [InstalledPlugin]]()) { result, plugin in
+                        let filter: PluginFilter = plugin.isActive ? .active : .inactive
+                        result[filter, default: []].append(plugin)
+                    }
+                    .map { filter, plugins in
+                        ListSection(plugins: plugins, filter: filter)
+                    }
+                    .sorted(using: KeyPathComparator(\ListSection.filter.rawValue))
             case let .failure(error):
                 self.error = (error as? WpApiError)?.errorMessage ?? error.localizedDescription
             }
