1ce48c1dbc5a470981c6deb74c38cbce17d24a12
Tony Li <tony.li@automattic.com>
2025-02-20 14:35:08 +1300

    Change if statements to switch-case statements

--- WordPress/Classes/Plugins/Views/PluginDetailsView.swift
+++ WordPress/Classes/Plugins/Views/PluginDetailsView.swift
@@ -392,24 +392,24 @@ enum ActionButton {
     case activate(plugin: InstalledPlugin, action: () -> Void)
     case activated(plugin: InstalledPlugin)
 
+    @ViewBuilder
     var view: some View {
-        let button: AnyView
-        switch self {
-        case let .install(_, action):
-            button = AnyView(Button(Strings.installButton, action: action)
-                    .font(.callout.bold()))
-        case let .activate(_, action):
-            button = AnyView(Button(Strings.activateButton, action: action)
-                    .font(.callout.bold()))
-        case .activated:
-            button = AnyView(Button(Strings.activatedButton, action: { })
-                .font(.callout)
-                .disabled(true))
+        Group {
+            switch self {
+            case let .install(_, action):
+                Button(Strings.installButton, action: action)
+                    .font(.callout.bold())
+            case let .activate(_, action):
+                Button(Strings.activateButton, action: action)
+                    .font(.callout.bold())
+            case .activated:
+                Button(Strings.activatedButton, action: { })
+                    .font(.callout)
+                    .disabled(true)
+            }
         }
-
-        return button
-            .buttonStyle(.borderedProminent)
-            .buttonBorderShape(.capsule)
+        .buttonStyle(.borderedProminent)
+        .buttonBorderShape(.capsule)
     }
 }
 
