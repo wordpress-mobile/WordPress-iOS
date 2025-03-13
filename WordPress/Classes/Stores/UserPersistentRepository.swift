090bcb126b8faab1e5e28c72205e50311f45e608
Gio Lodi <gio@mokacoding.com>
2025-03-13 17:33:34 +1100

    Move `UserPersistentRepositoryReader` to WordPressShared

--- WordPress/Classes/Stores/UserPersistentRepository.swift
+++ WordPress/Classes/Stores/UserPersistentRepository.swift
@@ -1,17 +1,5 @@
 import WordPressShared
 
-protocol UserPersistentRepositoryReader {
-    func string(forKey key: String) -> String?
-    func bool(forKey key: String) -> Bool
-    func integer(forKey key: String) -> Int
-    func float(forKey key: String) -> Float
-    func double(forKey key: String) -> Double
-    func array(forKey key: String) -> [Any]?
-    func dictionary(forKey key: String) -> [String: Any]?
-    func url(forKey key: String) -> URL?
-    func dictionaryRepresentation() -> [String: Any]
-}
-
 protocol UserPersistentRepositoryWriter: KeyValueDatabase {
     func set(_ value: Any?, forKey key: String)
     func set(_ value: Int, forKey key: String)
