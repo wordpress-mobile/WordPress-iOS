04533bee598385aaa7355cf68ddf86b93cba7df0
Gio Lodi <gio@mokacoding.com>
2025-03-13 17:34:20 +1100

    Move `UserPersistentRepositoryWriter` to WordPressShared

--- WordPress/Classes/Stores/UserPersistentRepository.swift
+++ WordPress/Classes/Stores/UserPersistentRepository.swift
@@ -1,15 +1,5 @@
 import WordPressShared
 
-protocol UserPersistentRepositoryWriter: KeyValueDatabase {
-    func set(_ value: Any?, forKey key: String)
-    func set(_ value: Int, forKey key: String)
-    func set(_ value: Float, forKey key: String)
-    func set(_ value: Double, forKey key: String)
-    func set(_ value: Bool, forKey key: String)
-    func set(_ url: URL?, forKey key: String)
-    func removeObject(forKey key: String)
-}
-
 typealias UserPersistentRepository = UserPersistentRepositoryReader & UserPersistentRepositoryWriter & UserPersistentRepositoryUtility
 
 extension UserDefaults: UserPersistentRepository {}
