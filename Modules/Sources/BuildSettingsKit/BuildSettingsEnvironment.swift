46190c81698ebb50fff8f0919d71f3b0b5c46847
kean <grebenyuk.alexander@gmail.com>
2025-03-18 10:55:37 -0400

    Add support for swift-testing in BuildSettingsEnvironment

--- Modules/Sources/BuildSettingsKit/BuildSettingsEnvironment.swift
+++ Modules/Sources/BuildSettingsKit/BuildSettingsEnvironment.swift
@@ -6,14 +6,34 @@ enum BuildSettingsEnvironment {
 
     static let current: BuildSettingsEnvironment = {
 #if DEBUG
-        let environment = ProcessInfo.processInfo.environment
-        if environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1" {
+        let processInfo = ProcessInfo.processInfo
+        if processInfo.isXcodePreview {
             return .preview
         }
-        if NSClassFromString("XCTestCase") != nil {
+        if processInfo.isTesting {
             fatalError("BuildSettings are unavailable when running unit tests. Make sure to inject the values manually in system under test.")
         }
 #endif
         return .live
     }()
+}
+
+private extension ProcessInfo {
+    var isXcodePreview: Bool {
+        environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
+    }
+
+    var isTesting: Bool {
+        if environment.keys.contains("XCTestBundlePath") { return true }
+        if environment.keys.contains("XCTestConfigurationFilePath") { return true }
+        if environment.keys.contains("XCTestSessionIdentifier") { return true }
+
+        return arguments.contains { argument in
+            let path = URL(fileURLWithPath: argument)
+            return path.lastPathComponent == "swiftpm-testing-helper"
+            || argument == "--testing-library"
+            || path.lastPathComponent == "xctest"
+            || path.pathExtension == "xctest"
+        }
+    }
 }
