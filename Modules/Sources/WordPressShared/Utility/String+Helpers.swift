560f6051009c1611e2e1784a8b6b3cc61da3852b
kean <grebenyuk.alexander@gmail.com>
2025-03-19 20:07:14 -0400

    Add missing method

--- Modules/Sources/WordPressShared/Utility/String+Helpers.swift
+++ Modules/Sources/WordPressShared/Utility/String+Helpers.swift
@@ -224,4 +224,12 @@ extension String {
 
         return self.components(separatedBy: ",").map({ $0.trimmingCharacters(in: .whitespacesAndNewlines) })
     }
+
+    /// Returns a Boolean value indicating if this String begins with the provided prefix.
+    /// - Parameter prefix: The prefix to check for.
+    /// - Parameter options: The string comparison options to use when checking for the prefix.
+    public func hasPrefix(_ prefix: String, with options: CompareOptions) -> Bool {
+        let fullOptions = options.union([.anchored])
+        return range(of: prefix, options: fullOptions) != nil
+    }
 }
