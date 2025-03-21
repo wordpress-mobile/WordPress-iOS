09a6c69a96cf85823deb0a31f12743f66ac6a268
kean <grebenyuk.alexander@gmail.com>
2025-03-21 09:36:25 -0400

    Remove the Jetpack app variant of AppConfiguration (no conditional target membership)

--- WordPress/Classes/Utility/App Configuration/AppConfiguration.swift
+++ WordPress/Classes/Utility/App Configuration/AppConfiguration.swift
@@ -1,12 +1,12 @@
 import Foundation
+import BuildSettingsKit
 
-/**
- * WordPress Configuration
- * - Warning:
- * This configuration class has a **Jetpack** counterpart in the Jetpack bundle.
- * Make sure to keep them in sync to avoid build errors when building the Jetpack target.
- */
 @objc class AppConfiguration: NSObject {
-    @objc static let isJetpack: Bool = false
-    @objc static let isWordPress: Bool = true
+    @objc static var isJetpack: Bool {
+        BuildSettings.current.brand == .jetpack
+    }
+
+    @objc static var isWordPress: Bool {
+        BuildSettings.current.brand == .wordpress
+    }
 }
