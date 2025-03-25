4011f8068cbd4c75b16d70106ab9772adf38b0a7
kean <grebenyuk.alexander@gmail.com>
2025-03-25 16:07:55 -0400

    Remove unused WPStyleGuide files

--- WordPress/Classes/System/WordPress-Bridging-Header.h
+++ WordPress/Classes/System/WordPress-Bridging-Header.h
@@ -73,8 +73,6 @@
 #import "WPAuthTokenIssueSolver.h"
 #import "WPUploadStatusButton.h"
 #import "WPError.h"
-#import "WPStyleGuide+Pages.h"
-#import "WPStyleGuide+WebView.h"
 #import "WPTableViewHandler.h"
 #import "WPWebViewController.h"
 #import "WPTabBarController.h"
