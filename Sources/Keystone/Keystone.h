644b6697db3ed4e1659790ac490a525d3ab0f055
kean <grebenyuk.alexander@gmail.com>
2025-03-27 12:49:15 -0400

    Add empty Keystone target in Sources/Keystone

---
+++ Sources/Keystone/Keystone.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for Keystone.
+FOUNDATION_EXPORT double KeystoneVersionNumber;
+
+//! Project version string for Keystone.
+FOUNDATION_EXPORT const unsigned char KeystoneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Keystone/PublicHeader.h>
