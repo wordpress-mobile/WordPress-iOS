823386490e2f40ed767ad9f7e735431a4d353d6f
Gio Lodi <gio@mokacoding.com>
2025-03-28 11:22:46 +1100

    Extract `NotificationSyncMediatorProtocol` to dedicated file

--- WordPress/Classes/Services/NotificationSyncMediator.swift
+++ WordPress/Classes/Services/NotificationSyncMediator.swift
@@ -19,18 +19,6 @@ import WordPressShared
 //
 let NotificationSyncMediatorDidUpdateNotifications = "NotificationSyncMediatorDidUpdateNotifications"
 
-protocol NotificationSyncMediatorProtocol {
-    func updateLastSeen(_ timestamp: String, completion: ((Error?) -> Void)?)
-    func toggleLikeForPostNotification(isLike: Bool,
-                                       postID: UInt,
-                                       siteID: UInt,
-                                       completion: @escaping (Result<Bool, Swift.Error>) -> Void)
-    func toggleLikeForCommentNotification(isLike: Bool,
-                                          commentID: UInt,
-                                          siteID: UInt,
-                                          completion: @escaping (Result<Bool, Swift.Error>) -> Void)
-}
-
 // MARK: - NotificationSyncMediator
 //
 final class NotificationSyncMediator: NotificationSyncMediatorProtocol {
