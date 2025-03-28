4c1b9aedc9498d4219f90acbf2f1f8ec3de38c6c
Gio Lodi <gio@mokacoding.com>
2025-03-28 11:14:45 +1100

    Implement an explicit `init` for `GravatarProfile`
    This way, once it's in the WordPressData framework, it'll be accessible
    by consumers.

--- WordPress/Classes/Models/GravatarProfile.swift
+++ WordPress/Classes/Models/GravatarProfile.swift
@@ -1,14 +1,31 @@
-import Foundation
-
 public struct GravatarProfile {
 
-    var profileID = ""
-    var hash = ""
-    var requestHash = ""
-    var profileUrl = ""
-    var preferredUsername = ""
-    var thumbnailUrl = ""
-    var name = ""
-    var displayName = ""
+    public var profileID: String
+    public var hash: String
+    public var requestHash: String
+    public var profileUrl: String
+    public var preferredUsername: String
+    public var thumbnailUrl: String
+    public var name: String
+    public var displayName: String
 
+    public init(
+        profileID: String = "",
+        hash: String = "",
+        requestHash: String = "",
+        profileUrl: String = "",
+        preferredUsername: String = "",
+        thumbnailUrl: String = "",
+        name: String = "",
+        displayName: String = ""
+    ) {
+        self.profileID = profileID
+        self.hash = hash
+        self.requestHash = requestHash
+        self.profileUrl = profileUrl
+        self.preferredUsername = preferredUsername
+        self.thumbnailUrl = thumbnailUrl
+        self.name = name
+        self.displayName = displayName
+    }
 }
