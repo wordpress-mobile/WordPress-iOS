c32214567077d8ce47b57d82636e40522376b6a2
kean <grebenyuk.alexander@gmail.com>
2025-03-29 11:21:57 -0400

    Rename module back to Keystone

--- Sources/Keystone/Keystone.h
+++ Sources/Keystone/Keystone.h
@@ -1,10 +1,10 @@
 #import <Foundation/Foundation.h>
 
 //! Project version number for Keystone.
-FOUNDATION_EXPORT double WordPressVersionNumber;
+FOUNDATION_EXPORT double KeystoneVersionNumber;
 
 //! Project version string for Keystone.
-FOUNDATION_EXPORT const unsigned char WordPressVersionString[];
+FOUNDATION_EXPORT const unsigned char KeystoneVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Keystone/PublicHeader.h>
 
