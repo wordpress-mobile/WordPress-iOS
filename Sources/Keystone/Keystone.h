29945d027701b5d69467cb488c3c2842e09ef16a
kean <grebenyuk.alexander@gmail.com>
2025-03-29 11:50:40 -0400

    Remove -Wquoted-include-in-framework-header

--- Sources/Keystone/Keystone.h
+++ Sources/Keystone/Keystone.h
@@ -8,82 +8,77 @@ FOUNDATION_EXPORT const unsigned char KeystoneVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Keystone/PublicHeader.h>
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wquoted-include-in-framework-header"
+#import <Keystone/ActivityLogViewController.h>
+#import <Keystone/AccountService.h>
 
-#import "ActivityLogViewController.h"
-#import "AccountService.h"
+#import <Keystone/Blog.h>
+#import <Keystone/BlogService.h>
+#import <Keystone/BlogSyncFacade.h>
+#import <Keystone/BlogDetailsViewController.h>
 
-#import "Blog.h"
-#import "BlogService.h"
-#import "BlogSyncFacade.h"
-#import "BlogDetailsViewController.h"
+#import <Keystone/CommentService.h>
+#import <Keystone/CommentsViewController.h>
+#import <Keystone/Constants.h>
 
-#import "CommentService.h"
-#import "CommentsViewController.h"
-#import "Constants.h"
+#import <Keystone/LocalCoreDataService.h>
 
-#import "LocalCoreDataService.h"
+#import <Keystone/Media.h>
+#import <Keystone/MediaService.h>
+#import <Keystone/MenuItem.h>
+#import <Keystone/MenuItemsViewController.h>
+#import <Keystone/MenusService.h>
+#import <Keystone/MenusViewController.h>
+#import <Keystone/Media+Extensions.h>
 
-#import "Media.h"
-#import "MediaService.h"
-#import "MenuItem.h"
-#import "MenuItemsViewController.h"
-#import "MenusService.h"
-#import "MenusViewController.h"
-#import "Media+Extensions.h"
+#import <Keystone/NSObject+Helpers.h>
 
-#import "NSObject+Helpers.h"
+#import <Keystone/PageSettingsViewController.h>
+#import <Keystone/PostContentProvider.h>
+#import <Keystone/PostCategory.h>
+#import <Keystone/PostCategoryService.h>
+#import <Keystone/PostContentProvider.h>
+#import <Keystone/PostHelper.h>
+#import <Keystone/PostMetaButton.h>
+#import <Keystone/PostService.h>
+#import <Keystone/PostServiceOptions.h>
+#import <Keystone/PostSettingsViewController.h>
+#import <Keystone/PostTag.h>
+#import <Keystone/PostTagService.h>
 
-#import "PageSettingsViewController.h"
-#import "PostContentProvider.h"
-#import "PostCategory.h"
-#import "PostCategoryService.h"
-#import "PostContentProvider.h"
-#import "PostHelper.h"
-#import "PostMetaButton.h"
-#import "PostService.h"
-#import "PostServiceOptions.h"
-#import "PostSettingsViewController.h"
-#import "PostTag.h"
-#import "PostTagService.h"
+#import <Keystone/ReaderGapMarker.h>
+#import <Keystone/ReaderPost.h>
+#import <Keystone/ReaderPostService.h>
+#import <Keystone/ReaderSiteService.h>
+#import <Keystone/ReaderSiteService_Internal.h>
+#import <Keystone/ReaderTopicService.h>
 
-#import "ReaderGapMarker.h"
-#import "ReaderPost.h"
-#import "ReaderPostService.h"
-#import "ReaderSiteService.h"
-#import "ReaderSiteService_Internal.h"
-#import "ReaderTopicService.h"
+#import <Keystone/SettingsSelectionViewController.h>
+#import <Keystone/SettingsMultiTextViewController.h>
+#import <Keystone/SettingTableViewCell.h>
+#import <Keystone/SettingsTextViewController.h>
+#import <Keystone/SharingViewController.h>
+#import <Keystone/SiteSettingsViewController.h>
+#import <Keystone/SourcePostAttribution.h>
+#import <Keystone/StatsViewController.h>
+#import <Keystone/SuggestionsTableView.h>
+#import <Keystone/SuggestionsTableViewCell.h>
 
-#import "SettingsSelectionViewController.h"
-#import "SettingsMultiTextViewController.h"
-#import "SettingTableViewCell.h"
-#import "SettingsTextViewController.h"
-#import "SharingViewController.h"
-#import "SiteSettingsViewController.h"
-#import "SourcePostAttribution.h"
-#import "StatsViewController.h"
-#import "SuggestionsTableView.h"
-#import "SuggestionsTableViewCell.h"
+#import <Keystone/Theme.h>
+#import <Keystone/ThemeService.h>
 
-#import "Theme.h"
-#import "ThemeService.h"
+#import <Keystone/UIAlertControllerProxy.h>
+#import <Keystone/UIApplication+Helpers.h>
+#import <Keystone/UIView+Subviews.h>
 
-#import "UIAlertControllerProxy.h"
-#import "UIApplication+Helpers.h"
-#import "UIView+Subviews.h"
-
-#import "WPAccount.h"
-#import "WPActivityDefaults.h"
-#import "WPAppAnalytics.h"
-#import "WPAuthTokenIssueSolver.h"
-#import "WPUploadStatusButton.h"
-#import "WPError.h"
-#import "WPTableViewHandler.h"
-#import "WPWebViewController.h"
-#import "WPTabBarController.h"
-#import "WPLogger.h"
+#import <Keystone/WPAccount.h>
+#import <Keystone/WPActivityDefaults.h>
+#import <Keystone/WPAppAnalytics.h>
+#import <Keystone/WPAuthTokenIssueSolver.h>
+#import <Keystone/WPUploadStatusButton.h>
+#import <Keystone/WPError.h>
+#import <Keystone/WPTableViewHandler.h>
+#import <Keystone/WPWebViewController.h>
+#import <Keystone/WPTabBarController.h>
+#import <Keystone/WPLogger.h>
 
 FOUNDATION_EXTERN void SetCocoaLumberjackObjCLogLevel(NSUInteger ddLogLevelRawValue);
-
-#pragma clang diagnostic pop
