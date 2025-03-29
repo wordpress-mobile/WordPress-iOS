42f644a687a2563cfe6920f5a5e21221cf1746af
kean <grebenyuk.alexander@gmail.com>
2025-03-29 09:58:41 -0400

    Add headers to the umbrella header

--- Sources/Keystone/Keystone.h
+++ Sources/Keystone/Keystone.h
@@ -7,3 +7,78 @@ FOUNDATION_EXPORT double KeystoneVersionNumber;
 FOUNDATION_EXPORT const unsigned char KeystoneVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Keystone/PublicHeader.h>
+
+#import "ActivityLogViewController.h"
+#import "AccountService.h"
+
+#import "Blog.h"
+#import "BlogService.h"
+#import "BlogSyncFacade.h"
+#import "BlogDetailsViewController.h"
+
+#import "CommentService.h"
+#import "CommentsViewController.h"
+#import "Constants.h"
+
+#import "LocalCoreDataService.h"
+
+#import "Media.h"
+#import "MediaService.h"
+#import "MenuItem.h"
+#import "MenuItemsViewController.h"
+#import "MenusService.h"
+#import "MenusViewController.h"
+#import "Media+Extensions.h"
+
+#import "NSObject+Helpers.h"
+
+#import "PageSettingsViewController.h"
+#import "PostContentProvider.h"
+#import "PostCategory.h"
+#import "PostCategoryService.h"
+#import "PostContentProvider.h"
+#import "PostHelper.h"
+#import "PostMetaButton.h"
+#import "PostService.h"
+#import "PostServiceOptions.h"
+#import "PostSettingsViewController.h"
+#import "PostTag.h"
+#import "PostTagService.h"
+
+#import "ReaderGapMarker.h"
+#import "ReaderPost.h"
+#import "ReaderPostService.h"
+#import "ReaderSiteService.h"
+#import "ReaderSiteService_Internal.h"
+#import "ReaderTopicService.h"
+
+#import "SettingsSelectionViewController.h"
+#import "SettingsMultiTextViewController.h"
+#import "SettingTableViewCell.h"
+#import "SettingsTextViewController.h"
+#import "SharingViewController.h"
+#import "SiteSettingsViewController.h"
+#import "SourcePostAttribution.h"
+#import "StatsViewController.h"
+#import "SuggestionsTableView.h"
+#import "SuggestionsTableViewCell.h"
+
+#import "Theme.h"
+#import "ThemeService.h"
+
+#import "UIAlertControllerProxy.h"
+#import "UIApplication+Helpers.h"
+#import "UIView+Subviews.h"
+
+#import "WPAccount.h"
+#import "WPActivityDefaults.h"
+#import "WPAppAnalytics.h"
+#import "WPAuthTokenIssueSolver.h"
+#import "WPUploadStatusButton.h"
+#import "WPError.h"
+#import "WPTableViewHandler.h"
+#import "WPWebViewController.h"
+#import "WPTabBarController.h"
+#import "WPLogger.h"
+
+FOUNDATION_EXTERN void SetCocoaLumberjackObjCLogLevel(NSUInteger ddLogLevelRawValue);
