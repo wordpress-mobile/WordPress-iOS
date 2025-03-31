bd0f3391e7487d568dea188cf39a4ceda019cbfc
Gio Lodi <gio@mokacoding.com>
2025-04-01 10:51:53 +1100

    Add dummy type to verify setup

--- Tests/WordPressDataTests/WordPressDataTests.swift
+++ Tests/WordPressDataTests/WordPressDataTests.swift
@@ -1,9 +1,15 @@
+import Testing
 import XCTest
 @testable import WordPressData
 
 final class WordPressDataTests: XCTestCase {
 
     func testExample() throws {
-        XCTAssertTrue(false, "Fails on purpose to validate setup")
+        XCTAssertEqual(WordPressDataDummy.dummyText, "dummy")
     }
+}
+
+@Test("A dummy test just to check the setup")
+func dummy() {
+    #expect(WordPressDataDummy.dummyText == "dummy")
 }
