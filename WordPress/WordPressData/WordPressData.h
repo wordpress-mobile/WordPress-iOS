eac6ae9950de9e6dd563d35e9354100e1336bf89
Gio Lodi <gio@mokacoding.com>
2025-04-01 07:25:12 +1100

    Add empty WordPressData target + custom test plan

---
+++ WordPress/WordPressData/WordPressData.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for WordPressData.
+FOUNDATION_EXPORT double WordPressDataVersionNumber;
+
+//! Project version string for WordPressData.
+FOUNDATION_EXPORT const unsigned char WordPressDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WordPressData/PublicHeader.h>
---
+++ WordPress/WordPressDataTests/WordPressDataTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import WordPressData
+
+final class WordPressDataTests: XCTestCase {
+
+    func testExample() throws {
+        XCTAssertTrue(false, "Fails on purpose to validate setup")
+    }
+}
