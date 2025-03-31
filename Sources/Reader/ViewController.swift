3331ee95c6d559917e50570cca55e6b466313023
kean <grebenyuk.alexander@gmail.com>
2025-03-31 15:09:39 -0400

    Add empty Reader target

---
+++ Sources/Reader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reader
+//
+//  Created by Alex on 3/31/25.
+//  Copyright © 2025 WordPress. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
