e741a8473dfd89ac8abc49dddba42b81d8bba61e
Gio Lodi <gio@mokacoding.com>
2025-04-02 07:37:14 +1100

    Use `_private_wordPressComRestApi` in `Blog+WordPressComRestAPI.swift` to build WordPressData

--- WordPress/Classes/Models/Blog/Blog+WordPressComRestAPI.swift
+++ WordPress/Classes/Models/Blog/Blog+WordPressComRestAPI.swift
@@ -11,7 +11,8 @@ extension Blog {
     /// - Warning: this method doesn't know if a Jetpack blog has the JSON API disabled.
     @objc
     public var wordPressComRestApi: WordPressComRestApi? {
-        account?.wordPressComRestApi
+        // FIXME: We are banking on the fact that by the time a consumer reads this, WPAccount will already have initialized its API client instance.
+        account?._private_wordPressComRestApi
     }
 
     /// Whether the blog is hosted on WordPress.com or connected through Jetpack.
