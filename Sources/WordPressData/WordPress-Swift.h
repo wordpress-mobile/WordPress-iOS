fc469d92eac1ffc59f5e2098eb1d2516e28cea76
Gio Lodi <gio@mokacoding.com>
2025-04-01 19:39:29 +1100

    Add `WordPress-Swift.h` to WordPressData to work around build fail

--- Sources/WordPressData/WordPress-Swift.h
+++ Sources/WordPressData/WordPress-Swift.h
@@ -1,13 +1,4 @@
-//
-//  WordPress-Swift.h
-//  WordPress
-//
-//  Created by Gio Lodi on 1/4/2025.
-//  Copyright © 2025 WordPress. All rights reserved.
-//
-
 #ifndef WordPress_Swift_h
 #define WordPress_Swift_h
-
-
+// FIXME: Defined here to avoid compilation errors while files are migrated over...
 #endif /* WordPress_Swift_h */
