505544eebc2cec32870021ac2b16f718ef2d4788
Gio Lodi <gio@mokacoding.com>
2025-04-01 19:38:24 +1100

    Configure `Package.swift` with WordPressData framework deps

---
+++ Sources/WordPressData/WordPress-Swift.h
@@ -0,0 +1,13 @@
+//
+//  WordPress-Swift.h
+//  WordPress
+//
+//  Created by Gio Lodi on 1/4/2025.
+//  Copyright © 2025 WordPress. All rights reserved.
+//
+
+#ifndef WordPress_Swift_h
+#define WordPress_Swift_h
+
+
+#endif /* WordPress_Swift_h */
