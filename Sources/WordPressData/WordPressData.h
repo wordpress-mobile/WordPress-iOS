7345eb57dcd9a3da986bf2aa2c4b31f755ce0ccd
Gio Lodi <gio@mokacoding.com>
2025-04-01 19:41:36 +1100

    Add all required sources to WordPressData

--- Sources/WordPressData/WordPressData.h
+++ Sources/WordPressData/WordPressData.h
@@ -6,4 +6,28 @@ FOUNDATION_EXPORT double WordPressDataVersionNumber;
 //! Project version string for WordPressData.
 FOUNDATION_EXPORT const unsigned char WordPressDataVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <WordPressData/PublicHeader.h>
+// Note: Some of these might not need to be public, but it was simpler to extract to WordPressData by making everything public.
+// As we'll hopefully soon rewrite these in Swift, we can implement proper access level then.
+#import <WordPressData/AbstractPost.h>
+#import <WordPressData/BasePost.h>
+#import <WordPressData/Blog.h>
+#import <WordPressData/CoreDataStack.h>
+#import <WordPressData/LocalCoreDataService.h>
+#import <WordPressData/Media.h>
+#import <WordPressData/Menu.h>
+#import <WordPressData/MenuItem.h>
+#import <WordPressData/MenuLocation.h>
+#import <WordPressData/PostCategory.h>
+#import <WordPressData/PostContentProvider.h>
+#import <WordPressData/PostHelper.h>
+#import <WordPressData/PostService.h>
+#import <WordPressData/PostServiceOptions.h>
+#import <WordPressData/PostTag.h>
+#import <WordPressData/PostType.h>
+#import <WordPressData/ReaderGapMarker.h>
+#import <WordPressData/ReaderPost.h>
+#import <WordPressData/SourcePostAttribution.h>
+#import <WordPressData/Theme.h>
+#import <WordPressData/WPAccount.h>
+
+FOUNDATION_EXTERN void SetCocoaLumberjackObjCLogLevel(NSUInteger ddLogLevelRawValue);
