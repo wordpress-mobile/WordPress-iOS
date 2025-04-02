7236170a0c88f09e27b31c30a1e84cddd4892549
Gio Lodi <gio@mokacoding.com>
2025-04-02 13:20:33 +1100

    Duplicate test infra in WordPressData in order to run `BlogTests`

---
+++ Tests/WordPressDataTests/Helpers/WPAccount+Fixture.swift
@@ -0,0 +1,24 @@
+import WordPressData
+
+/// Centralized utility to generate preconfigured WPAccount instances
+///
+// FIXME: Currently duplicated from WordPressTests for ease of working. Eventually, should be extracted in a WordPressDataTestsHelper framework to share here and with the consumers.
+extension WPAccount {
+
+    static func fixture(
+        context: NSManagedObjectContext,
+        // Using a constant UUID by default to keep the tests deterministic.
+        // There's nothing special in the value itself. It's just a UUID() value copied over.
+        uuid: UUID = UUID(uuidString: "D0D0298F-D7EF-4F32-A1F8-DDDBB8ADB8DF")!,
+        userID: Int = 1,
+        username: String = "username",
+        authToken: String = "authToken"
+    ) -> WPAccount {
+        let account = WPAccount(context: context)
+        account.userID = NSNumber(value: userID)
+        account.username = username
+        account.authToken = authToken
+        account.uuid = uuid.uuidString
+        return account
+    }
+}
