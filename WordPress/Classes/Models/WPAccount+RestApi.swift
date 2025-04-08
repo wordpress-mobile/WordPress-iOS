97fd77fad4814fa8ccb132ec12b9dcd9b2d7426f
Tony Li <tony.li@automattic.com>
2025-04-08 13:41:26 +1200

    Move invalid token handler into a static function

--- WordPress/Classes/Models/WPAccount+RestApi.swift
+++ WordPress/Classes/Models/WPAccount+RestApi.swift
@@ -3,10 +3,6 @@ import WordPressShared
 
 extension WPAccount {
 
-    private var notificationCenter: NotificationCenter {
-        NotificationCenter.default
-    }
-
     /// A `WordPressRestComApi` object if the account is a WordPress.com account. Otherwise, `nil`.
     ///
     /// Warning: The getter has various side effects when there is no previous value set, including triggering the signup flow!
@@ -19,7 +15,7 @@ extension WPAccount {
         }
 
         guard let authToken, !authToken.isEmpty else {
-            notificationCenter.post(
+            NotificationCenter.default.post(
                 name: .wpAccountRequiresShowingSigninForWPComFixingAuthToken,
                 object: self
             )
@@ -38,28 +34,13 @@ extension WPAccount {
             localeKey: WordPressComRestApi.LocaleKeyDefault
         )
 
-        api.setInvalidTokenHandler { [weak self] in
-            guard let self else { return }
-
-            self.authToken = nil
-
-            notificationCenter.post(
-                name: .wpAccountRequiresShowingSigninForWPComFixingAuthToken,
-                object: self
-            )
-
-            if self.isDefaultWordPressComAccount {
-                // At the time of writing, there is an implicit assumption on what the object parameter value means.
-                // For example, the WordPressAppDelegate.handleDefaultAccountChangedNotification(_:) subscriber inspects the object parameter to decide whether the notification was sent as a result of a login.
-                // If the object is non-nil, then the method considers the source a login.
-                //
-                // The code path in which we are is that of an invalid token, and that's neither a login nor a logout, it's more appropriate to consider it a logout.
-                // That's because if the token is invalid the app will soon received errors from the API and it's therefore better to force the user to login again.
-                NotificationCenter.default.post(
-                    name: NSNotification.Name.WPAccountDefaultWordPressComAccountChanged,
-                    object: nil
-                )
-            }
+        let accountID = TaggedManagedObjectID(self)
+        let context = managedObjectContext
+        api.setInvalidTokenHandler {
+            // We use a static function here because it's not safe to access `self` in this closure.
+            // The `WPAccount` instance can be bound to any context object. There is no guarantee that the thread
+            // from which this closure is called is the same as the one in the context object.
+            WPAccount.handleInvalidToken(accountID: accountID, context: context)
         }
 
         return api
@@ -83,9 +64,33 @@ extension WPAccount {
         return account?.wordPressComRestApi
     }
 
+    static func handleInvalidToken(accountID: TaggedManagedObjectID<WPAccount>, context: NSManagedObjectContext?) {
+        let account = try? context?.existingObject(with: accountID)
+        account?.authToken = nil
+
+        NotificationCenter.default.post(
+            name: .wpAccountRequiresShowingSigninForWPComFixingAuthToken,
+            object: account
+        )
+
+        if account?.isDefaultWordPressComAccount == true {
+            // At the time of writing, there is an implicit assumption on what the object parameter value means.
+            // For example, the WordPressAppDelegate.handleDefaultAccountChangedNotification(_:) subscriber inspects the object parameter to decide whether the notification was sent as a result of a login.
+            // If the object is non-nil, then the method considers the source a login.
+            //
+            // The code path in which we are is that of an invalid token, and that's neither a login nor a logout, it's more appropriate to consider it a logout.
+            // That's because if the token is invalid the app will soon received errors from the API and it's therefore better to force the user to login again.
+            NotificationCenter.default.post(
+                name: NSNotification.Name.WPAccountDefaultWordPressComAccountChanged,
+                object: nil
+            )
+        }
+    }
 }
 
 extension Foundation.Notification.Name {
 
+    /// This notification is posted when a `WPAccount` instance's `authToken` is found to be invalid.
+    /// The object property of the posted notification is an `TaggedManagedObjectID<WPAccount>` instance.
     static let wpAccountRequiresShowingSigninForWPComFixingAuthToken = Foundation.Notification.Name("WPAccount.WPComAuthTokenNeedsFixing")
 }
