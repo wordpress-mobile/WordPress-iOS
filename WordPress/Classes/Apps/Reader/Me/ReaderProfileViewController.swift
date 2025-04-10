760c881a26443e5497af86dc0d30f55f4ba041ce
kean <grebenyuk.alexander@gmail.com>
2025-04-10 17:27:38 -0400

    Add missing profile VC

---
+++ WordPress/Classes/Apps/Reader/Me/ReaderProfileViewController.swift
@@ -0,0 +1,41 @@
+import UIKit
+import SwiftUI
+import WordPressUI
+
+final class ReaderProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let mySiteVC = MySiteViewController()
+        mySiteVC.isReaderModeEnabled = true
+        mySiteVC.willMove(toParent: self)
+        addChild(mySiteVC)
+        view.addSubview(mySiteVC.view)
+        mySiteVC.view.pinEdges()
+        mySiteVC.willMove(toParent: self)
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gearshape"), style: .plain, target: self, action: #selector(buttonSettingsTapped))
+
+        mySiteVC.view.backgroundColor = .systemBackground
+    }
+
+    @objc private func buttonSettingsTapped() {
+        let meVC = MeViewController()
+        let navigationVC = UINavigationController(rootViewController: meVC)
+        meVC.navigationItem.title = nil
+
+        meVC.navigationItem.rightBarButtonItem = {
+            let button = UIBarButtonItem(title: SharedStrings.Button.done, primaryAction: .init { [weak self] _ in
+                self?.dismiss(animated: true)
+            })
+            button.setTitleTextAttributes([.font: WPStyleGuide.fontForTextStyle(.body, fontWeight: .semibold)], for: .normal)
+            return button
+        }()
+
+        if traitCollection.horizontalSizeClass == .regular {
+            meVC.isSidebarModeEnabled = true
+            navigationVC.modalPresentationStyle = .formSheet
+        }
+        present(navigationVC, animated: true)
+    }
+}
