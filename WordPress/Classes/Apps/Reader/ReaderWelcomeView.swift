b47e0caf97287efd6c49d344c575cf91d285eb98
kean <grebenyuk.alexander@gmail.com>
2025-04-10 08:17:20 -0400

    Use SharedStrings.Reader.title

--- WordPress/Classes/Apps/Reader/ReaderWelcomeView.swift
+++ WordPress/Classes/Apps/Reader/ReaderWelcomeView.swift
@@ -11,7 +11,7 @@ struct ReaderWelcomeView: View {
             Spacer()
 
             VStack {
-                Text(Strings.title)
+                Text(SharedStrings.Reader.title)
                     .font(.make(.recoleta, size: 60, relativeTo: .largeTitle))
                     .padding(.top, 86)
                 Text(Strings.subtitle)
@@ -55,7 +55,6 @@ struct ReaderWelcomeView: View {
 }
 
 private enum Strings {
-    static let title = NSLocalizedString("reader.welcome.title", value: "Reader", comment: "Reader Welcome screen (brand name; may not need any translation!)")
     static let subtitle = NSLocalizedString("reader.welcome.subtitle", value: "Join the largest blogging community", comment: "Reader Welcome screen")
     static let continueText = NSLocalizedString("reader.welcome.continueText", value: "Continue with WordPress.com", comment: "Reader Welcome screen login button")
 }
