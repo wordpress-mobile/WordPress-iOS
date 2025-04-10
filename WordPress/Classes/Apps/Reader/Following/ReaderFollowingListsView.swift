87753c7457aaa6d90643ef4c501fb485be33edaf
kean <grebenyuk.alexander@gmail.com>
2025-04-10 11:26:30 -0400

    Add empty state for listss

--- WordPress/Classes/Apps/Reader/Following/ReaderFollowingListsView.swift
+++ WordPress/Classes/Apps/Reader/Following/ReaderFollowingListsView.swift
@@ -12,6 +12,16 @@ struct ReaderFollowingListsView: View {
     private var lists: FetchedResults<ReaderListTopic>
 
     var body: some View {
+        if lists.isEmpty {
+            EmptyStateView(Strings.emptyTitle, systemImage: "list.clipboard", description: Strings.emptyDetails)
+                .frame(height: 420)
+                .listRowSeparator(.hidden)
+        } else {
+            items
+        }
+    }
+
+    private var items: some View {
         ForEach(lists, id: \.self) { list in
             Button {
                 viewModel.navigate(to: .topic(list))
@@ -26,4 +36,9 @@ struct ReaderFollowingListsView: View {
             }
         }
     }
+}
+
+private enum Strings {
+    static let emptyTitle = NSLocalizedString("reader.following.lists.emptyTitle", value: "Lists Empty", comment: "Empty state view")
+    static let emptyDetails = NSLocalizedString("reader.following.lists.emptyTitle", value: "Create lists to following different topics in one convenient feed", comment: "Empty state view")
 }
