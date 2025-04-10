26d199ef1d222d3b85b00104d1c681328ceacc02
kean <grebenyuk.alexander@gmail.com>
2025-04-10 07:56:06 -0400

    Add create post button

--- WordPress/Classes/Apps/Reader/ReaderHomeViewController.swift
+++ WordPress/Classes/Apps/Reader/ReaderHomeViewController.swift
@@ -2,14 +2,22 @@ import UIKit
 import WordPressUI
 
 final class ReaderHomeViewController: ReaderStreamViewController {
+    private let mainContext = ContextManager.shared.mainContext
+
     override var isHomeModeEnabled: Bool { true }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupNavigationItems()
+    }
+
+    private func setupNavigationItems() {
         title = SharedStrings.Reader.home
         titleView.textLabel.text = SharedStrings.Reader.home
         navigationItem.titleView = titleView
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "wpl-add-card")?.resized(to: CGSize(width: 28, height: 28)), style: .plain, target: self, action: #selector(buttonCreatePostTapped))
     }
 
     override func headerForStream(_ topic: ReaderAbstractTopic?, container: UITableViewController) -> UIView? {
@@ -17,6 +25,29 @@ final class ReaderHomeViewController: ReaderStreamViewController {
         view.titleView.titleLabel.text = SharedStrings.Reader.home
         view.titleView.detailsTextView.text = Strings.homeDetails
         return view
+    }
+
+    @objc private func buttonCreatePostTapped() {
+        if let blog = Blog.lastUsedOrFirst(in: mainContext) {
+            showCreatePostScreen(blog: blog)
+        } else {
+            showCreateSiteFlow()
+        }
+    }
+
+    private func showCreateSiteFlow() {
+        let wizardLauncher = SiteCreationWizardLauncher()
+        guard let wizard = wizardLauncher.ui else {
+            return wpAssertionFailure("something went wrong")
+        }
+        present(wizard, animated: true)
+        SiteCreationAnalyticsHelper.trackSiteCreationAccessed(source: "home")
+    }
+
+    private func showCreatePostScreen(blog: Blog) {
+        let editorVC = EditPostViewController(blog: blog)
+        editorVC.entryPoint = .dashboard
+        present(editorVC, animated: true)
     }
 }
 
