6f335fe1cb42eef3bbe88bf61e8454ebb398411a
Gio Lodi <gio@mokacoding.com>
2025-04-16 13:11:11 +1000

    Make Reader use its own `ApiCredentials` value

--- Sources/Reader/main.swift
+++ Sources/Reader/main.swift
@@ -7,9 +7,7 @@ import WordPress
 // This is because `BuildSettings` are not propagated through the app via chain injection but accessed via a `static` `current` property for convenience.
 // Also for convenience, we assume the secrets not to be nil at runtime, to avoid unwrapping values that we know must be there.
 // Therefore, we need to make the secrets available to `BuildSettings` before the app starts.
-// FIXME: Sort ot Reader having its own ApiCredentials (soon to be BuildSecrets) instance
-//BuildSettings.configure(secrets: ApiCredentials.toSecrets())
-BuildSettings.configure(secrets: BuildSecrets.dummy)
+BuildSettings.configure(secrets: ApiCredentials.toSecrets())
 
 UIApplicationMain(
     CommandLine.argc,
