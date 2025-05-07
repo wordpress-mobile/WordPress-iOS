wordpress-mobile/WordPress-iOS

commit c337f10937ea33ffa4b963d7aed291b519d71277

Add missing NSURL package (#24519)

Modules/Package.swift
@@ -294,6 +294,7 @@ enum XcodeSupport {
             .product(name: "Lottie", package: "lottie-ios"),
             .product(name: "MediaEditor", package: "MediaEditor-iOS"),
             .product(name: "NSObject-SafeExpectations", package: "NSObject-SafeExpectations"),
+            .product(name: "NSURL+IDN", package: "NSURL-IDN"),
             .product(name: "Reachability", package: "Reachability"),
             .product(name: "Starscream", package: "Starscream"),
             .product(name: "SVProgressHUD", package: "SVProgressHUD"),

commit 33c25615309592cbd08aec81aeaa41010bffc200

Point NSURL-IDN package to version using "-" instead of "+"
This aims to address a validation error when the package is dynamically
linked, as introduced by
https://github.com/wordpress-mobile/WordPress-iOS/pull/24519

See TestFlight upload failure in
https://buildkite.com/automattic/wordpress-ios/builds/27601

> ERROR: [altool.600000D70180] Validation failed Invalid Info.plist value.
> The CFBundleExecutable Info.plist key contains the NSURL+IDN_-772325DC8649925D_PackageProduct value in the “Payload/Reader.app/Frameworks/NSURL+IDN_-772325DC8649925D_PackageProduct.framework” bundle.
> This key’s value can’t contain any of the following unsupported characters: \ [ ] { } ( ) + *

Modules/Package.swift
@@ -47,7 +47,7 @@ let package = Package(
         .package(url: "https://github.com/wordpress-mobile/FSInteractiveMap", from: "0.3.0"),
         .package(url: "https://github.com/wordpress-mobile/MediaEditor-iOS", branch: "task/spm-support"),
         .package(url: "https://github.com/wordpress-mobile/NSObject-SafeExpectations", from: "0.0.6"),
-        .package(url: "https://github.com/wordpress-mobile/NSURL-IDN", branch: "trunk"),
+        .package(url: "https://github.com/wordpress-mobile/NSURL-IDN", revision: "b34794c9a3f32312e1593d4a3d120572afa0d010"),
         .package(url: "https://github.com/wordpress-mobile/WordPressKit-iOS", branch: "wpios-edition"),
         .package(url: "https://github.com/zendesk/support_sdk_ios", from: "8.0.3"),
         // We can't use wordpress-rs branches nor commits here. Only tags work.
@@ -220,7 +220,7 @@ enum XcodeSupport {
             "WordPressShared",
             "WordPressUI",
             .product(name: "Gridicons", package: "Gridicons-iOS"),
-            .product(name: "NSURL+IDN", package: "NSURL-IDN"),
+            .product(name: "NSURL-IDN", package: "NSURL-IDN"),
             .product(name: "SVProgressHUD", package: "SVProgressHUD"),
             .product(name: "WordPressKit", package: "WordPressKit-iOS"),
             .product(name: "Gravatar", package: "Gravatar-SDK-iOS"),
@@ -294,7 +294,7 @@ enum XcodeSupport {
             .product(name: "Lottie", package: "lottie-ios"),
             .product(name: "MediaEditor", package: "MediaEditor-iOS"),
             .product(name: "NSObject-SafeExpectations", package: "NSObject-SafeExpectations"),
-            .product(name: "NSURL+IDN", package: "NSURL-IDN"),
+            .product(name: "NSURL-IDN", package: "NSURL-IDN"),
             .product(name: "Reachability", package: "Reachability"),
             .product(name: "Starscream", package: "Starscream"),
             .product(name: "SVProgressHUD", package: "SVProgressHUD"),
@@ -326,7 +326,7 @@ enum XcodeSupport {
                 .product(name: "CocoaLumberjack", package: "CocoaLumberjack"),
                 .product(name: "CocoaLumberjackSwift", package: "CocoaLumberjack"),
                 .product(name: "NSObject-SafeExpectations", package: "NSObject-SafeExpectations"),
-                .product(name: "NSURL+IDN", package: "NSURL-IDN"),
+                .product(name: "NSURL-IDN", package: "NSURL-IDN"),
                 .product(name: "WordPressAPI", package: "wordpress-rs"),
                 .product(name: "WordPressKit", package: "WordPressKit-iOS"),
             ]),
@@ -404,7 +404,7 @@ enum XcodeSupport {
                     .product(name: "CocoaLumberjackSwift", package: "CocoaLumberjack"),
                     .product(name: "Gravatar", package: "Gravatar-SDK-iOS"),
                     .product(name: "NSObject-SafeExpectations", package: "NSObject-SafeExpectations"),
-                    .product(name: "NSURL+IDN", package: "NSURL-IDN"),
+                    .product(name: "NSURL-IDN", package: "NSURL-IDN"),
                     .product(name: "WordPressAPI", package: "wordpress-rs"),
                     .product(name: "WordPressKit", package: "WordPressKit-iOS"),
                 ]

WordPress.xcworkspace/xcshareddata/swiftpm/Package.resolved
@@ -1,5 +1,5 @@
 {
-  "originHash" : "0ed52b944e605856a7ca56314dcbfee8c4c681a2a599fd3ef8d32d3c7ba95616",
+  "originHash" : "4d299ae36ff454e0e3ec59a9c04557210efa32d8b96b189963ad0f3ffebb3e70",
   "pins" : [
     {
       "identity" : "alamofire",
@@ -222,8 +222,8 @@
       "kind" : "remoteSourceControl",
       "location" : "https://github.com/wordpress-mobile/NSURL-IDN",
       "state" : {
-        "branch" : "trunk",
-        "revision" : "50a0ffd324bb27ef76a5ca685b990307528dd9ea"
+        "branch" : "cmm-322-remove-plus-from-name",
+        "revision" : "b34794c9a3f32312e1593d4a3d120572afa0d010"
       }
     },
     {