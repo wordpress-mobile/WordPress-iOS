8934b024400454d8c048e8fc815ab0f0a040ac8f
David Calhoun <github@davidcalhoun.me>
2025-05-08 12:36:00 -0400

    build: Update GutenbergKit version

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -52,7 +52,7 @@ let package = Package(
         .package(url: "https://github.com/zendesk/support_sdk_ios", from: "8.0.3"),
         // We can't use wordpress-rs branches nor commits here. Only tags work.
         .package(url: "https://github.com/Automattic/wordpress-rs", revision: "alpha-20250505"),
-        .package(url: "https://github.com/wordpress-mobile/GutenbergKit", revision: "9c0bdc3c64db767c5a550afd8418348761e1cf78"),
+        .package(url: "https://github.com/wordpress-mobile/GutenbergKit", revision: "0bb5fe6c07ecdbc57d3277182fa1722bef34d23d"),
         .package(url: "https://github.com/Automattic/color-studio", branch: "trunk"),
         .package(url: "https://github.com/wordpress-mobile/AztecEditor-iOS", from: "1.20.0"),
     ],
