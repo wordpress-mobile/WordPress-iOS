4e35a9103f1cf8a4d2163268a715a2f00509705b
Tony Li <tony.li@automattic.com>
2025-05-27 10:38:51 +1200

    Delete copied code

--- Sources/JetpackStatsWidgets/Sources/StatsWidgets.swift
+++ Sources/JetpackStatsWidgets/Sources/StatsWidgets.swift
@@ -20,27 +20,3 @@ struct JetpackStatsWidgets: WidgetBundle {
         LockScreenStatsWidget(config: LockScreenAllTimePostsBestViewsStatWidgetConfig())
     }
 }
-
-private extension ApiCredentials {
-
-    static func toSecrets() -> BuildSecrets {
-        BuildSecrets(
-            oauth: .init(client: client, secret: secret),
-            google: .init(
-                clientId: googleLoginClientId,
-                schemeId: googleLoginSchemeId,
-                serverClientId: googleLoginServerClientId
-            ),
-            zendesk: .init(
-                appId: zendeskAppId,
-                url: zendeskUrl,
-                clientId: zendeskClientId
-            ),
-            tenorApiKey: tenorApiKey,
-            sentryDSN: sentryDSN,
-            docsBotId: docsBotId,
-            encryptedLogsKey: encryptedLogKey,
-            debuggingKey: debuggingKey
-        )
-    }
-}
