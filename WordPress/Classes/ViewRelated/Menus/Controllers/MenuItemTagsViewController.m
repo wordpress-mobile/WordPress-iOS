4ab8b4c397a83d1dd4866b30ca9c8920ef363898
Gio Lodi <gio@mokacoding.com>
2025-06-26 12:07:16 +1000

    Replace all `@import WordPressData` with angle brackets

--- WordPress/Classes/ViewRelated/Menus/Controllers/MenuItemTagsViewController.m
+++ WordPress/Classes/ViewRelated/Menus/Controllers/MenuItemTagsViewController.m
@@ -1,6 +1,10 @@
 #import "MenuItemTagsViewController.h"
 #import "PostTagService.h"
-@import WordPressData;
+// For some reason, and only in some files, the modular import does not work.
+// Just to be on the safe side, _all_ imports use the angle brackets style.
+// We shall try to go back to the modular style on Keystone successfully builds for testing.
+// @import WordPressData;
+#import <WordPressData/WordPressData.h>
 
 static NSUInteger const MenuItemSourceTagSyncLimit = 100;
 
--- WordPress/Classes/ViewRelated/Menus/View Models/MenusSelectionItem.m
+++ WordPress/Classes/ViewRelated/Menus/View Models/MenusSelectionItem.m
@@ -1,5 +1,9 @@
 #import "MenusSelectionItem.h"
-@import WordPressData;
+// For some reason, and only in some files, the modular import does not work.
+// Just to be on the safe side, _all_ imports use the angle brackets style.
+// We shall try to go back to the modular style on Keystone successfully builds for testing.
+// @import WordPressData;
+#import <WordPressData/WordPressData.h>
 
 NSString * const MenusSelectionViewItemChangedSelectedNotification = @"MenusSelectionViewItemChangedSelectedNotification";
 NSString * const MenusSelectionViewItemUpdatedItemObjectNotification = @"MenusSelectionViewItemUpdatedItemObjectNotification";
