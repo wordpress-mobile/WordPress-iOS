d63157632323e3582814ba1ea478a5a38d791f17
Tony Li <tony.li@automattic.com>
2025-07-15 20:42:26 +1200

    Update wordpress-rs

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -53,7 +53,7 @@ let package = Package(
         ),
         .package(url: "https://github.com/zendesk/support_sdk_ios", from: "8.0.3"),
         // We can't use wordpress-rs branches nor commits here. Only tags work.
-        .package(url: "https://github.com/Automattic/wordpress-rs", revision: "alpha-20250707"),
+        .package(url: "https://github.com/Automattic/wordpress-rs", revision: "alpha-20250715"),
         .package(url: "https://github.com/wordpress-mobile/GutenbergKit", from: "0.4.1"),
         .package(
             url: "https://github.com/Automattic/color-studio",
