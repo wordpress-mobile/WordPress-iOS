b3f8eddc63828f480d8557db593c29291812d672
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2025-07-28 13:44:57 -0400

    Lighter bars in dark mode

--- Modules/Sources/JetpackStats/Views/TopList/TopListItemBarBackground.swift
+++ Modules/Sources/JetpackStats/Views/TopList/TopListItemBarBackground.swift
@@ -12,8 +12,8 @@ struct TopListItemBarBackground: View {
             HStack(spacing: 0) {
                 LinearGradient(
                     colors: [
-                        barColor.opacity(colorScheme == .light ? 0.06 : 0.25),
-                        barColor.opacity(colorScheme == .light ? 0.12 : 0.4),
+                        barColor.opacity(colorScheme == .light ? 0.06 : 0.22),
+                        barColor.opacity(colorScheme == .light ? 0.12 : 0.35),
                     ],
                     startPoint: .leading,
                     endPoint: .trailing
