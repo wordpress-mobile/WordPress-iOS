a9a546d2ccf6e1fa91e871ede55c15d32d4e54f9
Tony Li <tony.li@automattic.com>
2025-09-08 11:22:13 +1200

    Update iOS simulator version

--- .buildkite/commands/run-unit-tests-for-scheme.sh
+++ .buildkite/commands/run-unit-tests-for-scheme.sh
@@ -2,7 +2,7 @@
 
 SCHEME="${1:?Usage $0 SCHEME}"
 DEVICE="iPhone 16"
-OS_VERSION="18.4"
+OS_VERSION="26.0"
 
 if "$(dirname "${BASH_SOURCE[0]}")/should-skip-job.sh" --job-type validation; then
   exit 0
