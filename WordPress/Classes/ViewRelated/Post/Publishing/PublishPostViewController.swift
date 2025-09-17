3e6694764f5acecc39b565fca3e9f4f6e17b6ee8
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2025-09-17 10:08:01 -0400

    Show PostSettingsFormContentView

--- WordPress/Classes/ViewRelated/Post/Publishing/PublishPostViewController.swift
+++ WordPress/Classes/ViewRelated/Post/Publishing/PublishPostViewController.swift
@@ -8,8 +8,13 @@ import WordPressUI
 /// A screen shown just before publishing the post and allows you to change
 /// the post settings along with some publishing options like the publish date.
 final class PublishPostViewController: UIHostingController<NavigationView<PublishPostView>> {
+    private let settingsViewModel: PostSettingsViewModel
+
     init(post: AbstractPost) {
-        super.init(rootView: NavigationView { PublishPostView() })
+        // TODO: add isStandalone support
+        let settingsViewModel = PostSettingsViewModel(post: post, isStandalone: true)
+        self.settingsViewModel = settingsViewModel
+        super.init(rootView: NavigationView { PublishPostView(settingsViewModel: settingsViewModel) })
     }
 
     required dynamic init?(coder aDecoder: NSCoder) {
@@ -17,10 +22,13 @@ final class PublishPostViewController: UIHostingController<NavigationView<Publis
     }
 }
 
-// TODO: add isStandalone support
 // TODO: add `PrepublishingSheetResult`
 struct PublishPostView: View {
+    @ObservedObject var settingsViewModel: PostSettingsViewModel
+
     var body: some View {
-        Text("Here")
+        Form {
+            PostSettingsFormContentView(viewModel: settingsViewModel)
+        }
     }
 }
