fbbf2b16a48530703862be1e169e6745b8167cf2
David Calhoun <github@davidcalhoun.me>
2025-10-07 15:09:40 -0400

    feat: Disable header navigation when GutenbergKit opens dialogs (#24917)
    * feat: Disable header navigation when GutenbergKit opens dialogs
    
    Disabling the native header navigation respects GutenbergKit's web-based
    modal dialogs, where focus should remain on the dialog to mitigate
    confusion from surrounding UI elements.
    
    * build: Update GutenbergKit version
    
    * Revert "build: Update GutenbergKit version"
    
    This reverts commit 3f758ad0b11126911cf771313df927735ba323b0.
    
    * build: Update GutenbergKit version

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -53,7 +53,7 @@ let package = Package(
         .package(url: "https://github.com/zendesk/support_sdk_ios", from: "8.0.3"),
         // We can't use wordpress-rs branches nor commits here. Only tags work.
         .package(url: "https://github.com/Automattic/wordpress-rs", revision: "alpha-20250926"),
-        .package(url: "https://github.com/wordpress-mobile/GutenbergKit", from: "0.8.1-alpha.2"),
+        .package(url: "https://github.com/wordpress-mobile/GutenbergKit", from: "0.9.0"),
         .package(
             url: "https://github.com/Automattic/color-studio",
             revision: "bf141adc75e2769eb469a3e095bdc93dc30be8de"
