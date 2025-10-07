19fce9c935db1ff0db2a46d9fbac54f680235c89
David Calhoun <github@davidcalhoun.me>
2025-10-07 14:57:57 -0400

    fix: GutenbergKit allows empty editor settings
    Sites without GutenbergKit plugin do not have the editor settings REST
    API endpoint. For these sites or missing settings due to other reasons,
    we should allow the editor to open with empty editor settings.
    
    This approach aligns with the original implementation:
    
    https://github.com/wordpress-mobile/WordPress-iOS/commit/ac3ad4c5aea1bc54b0626b3532963f6fbf4498fe#diff-985e4813fea24c7bfb37425f15af08cda2f4ace0da805b3eecc249862fdca2d5L362-L367

--- WordPress/Classes/ViewRelated/NewGutenberg/NewGutenbergViewController.swift
+++ WordPress/Classes/ViewRelated/NewGutenberg/NewGutenbergViewController.swift
@@ -51,7 +51,7 @@ class NewGutenbergViewController: UIViewController, PostEditor, PublishingEditor
     }
 
     struct EditorDependencies {
-        let settings: String
+        let settings: String?
         let didLoadCookies: Bool
     }
 
@@ -383,13 +383,13 @@ class NewGutenbergViewController: UIViewController, PostEditor, PublishingEditor
     }
 
     @MainActor
-    func startEditor(settings: String) async throws {
+    func startEditor(settings: String?) async throws {
         guard case .dependenciesReady = self.editorState else {
             preconditionFailure("`startEditor` should only be called when the editor is in the `.dependenciesReady` state.")
         }
 
         let updatedConfiguration = self.editorViewController.configuration.toBuilder()
-            .setEditorSettings(settings)
+            .apply(settings) { $0.setEditorSettings($1) }
             .setTitle(post.postTitle ?? "")
             .setContent(post.content ?? "")
             .build()
@@ -469,7 +469,14 @@ class NewGutenbergViewController: UIViewController, PostEditor, PublishingEditor
 
     // MARK: - Editor Setup
     private func fetchEditorDependencies() async throws -> EditorDependencies {
-        let settings = try await blockEditorSettingsService.getSettingsString(allowingCachedResponse: true)
+        let settings: String?
+        do {
+            settings = try await blockEditorSettingsService.getSettingsString(allowingCachedResponse: true)
+        } catch {
+            DDLogError("Failed to fetch editor settings: \(error)")
+            settings = nil
+        }
+
         let loaded = await loadAuthenticationCookiesAsync()
 
         return EditorDependencies(settings: settings, didLoadCookies: loaded)
