68bff2e75af430ef0013932c5cf78f43902cf196
Tanner W. Stokes <tanner.stokes@automattic.com>
2025-11-15 16:25:12 -0500

    Update unit tests to use iOS 26.1.

--- .buildkite/commands/run-unit-tests-for-scheme.sh
+++ .buildkite/commands/run-unit-tests-for-scheme.sh
@@ -2,7 +2,7 @@
 
 SCHEME="${1:?Usage $0 SCHEME}"
 DEVICE="iPhone 17"
-OS_VERSION="26.0"
+OS_VERSION="26.1"
 
 if "$(dirname "${BASH_SOURCE[0]}")/should-skip-job.sh" --job-type validation; then
   exit 0
