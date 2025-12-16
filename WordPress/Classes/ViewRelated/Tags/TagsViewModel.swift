wordpress-mobile/WordPress-iOS

commit 9df3cf8c91216314245beab0f96c761b6833b2df

Show taxonomies and manage terms (#24955)
* Decouple `TagsService.getTags` from WordPressKit API

* Decouple from WordPressKit.RemotePostTag

* Add managing terms and taxonomies

* Update release notes

* Extract duplicated code

* Rephrase a user-facing message

WordPress/Classes/ViewRelated/Tags/TagsViewModel.swift
@@ -3,8 +3,10 @@ import WordPressKit
 import WordPressData
 import WordPressUI
 import WordPressShared
+import WordPressAPI
+import WordPressCore
 
-typealias TagsPaginatedResponse = DataViewPaginatedResponse<RemotePostTag, Int>
+typealias TagsPaginatedResponse = DataViewPaginatedResponse<AnyTermWithViewContext, Int>
 
 enum TagsViewMode {
     case selection(onSelectedTagsChanged: ((String) -> Void)?)
@@ -26,8 +28,10 @@ class TagsViewModel: ObservableObject {
     }
     private var selectedTagsSet: Set<String> = []
 
-    let tagsService: TagsService
+    let tagsService: TaxonomyServiceProtocol
     let mode: TagsViewMode
+    let labels: TaxonomyLocalizedLabels
+    let taxonomy: SiteTaxonomy?
 
     var isBrowseMode: Bool {
         if case .browse = mode {
@@ -37,9 +41,20 @@ class TagsViewModel: ObservableObject {
     }
 
     init(blog: Blog, selectedTags: String? = nil, mode: TagsViewMode) {
+        self.taxonomy = nil
         self.tagsService = TagsService(blog: blog)
         self.mode = mode
+        self.labels = TaxonomyLocalizedLabels.tag
         self.selectedTags = AbstractPost.makeTags(from: selectedTags ?? "")
+        self.selectedTagsSet = Set(self.selectedTags.map { $0.lowercased() })
+    }
+
+    init(blog: Blog, api: WordPressAPI, taxonomy: SiteTaxonomy, selectedTerms: String? = nil, mode: TagsViewMode) {
+        self.taxonomy = taxonomy
+        self.tagsService = AnyTermService(api: api, endpoint: taxonomy.endpoint)
+        self.mode = mode
+        self.labels = TaxonomyLocalizedLabels.from(taxonomy: taxonomy)
+        self.selectedTags = AbstractPost.makeTags(from: selectedTerms ?? "")
         self.selectedTagsSet = Set(self.selectedTags.map { $0.lowercased() })
     }
 
@@ -68,16 +83,14 @@ class TagsViewModel: ObservableObject {
                     throw TagsServiceError.noRemoteService
                 }
 
-                let offset = pageIndex ?? 0
+                let page = pageIndex ?? 0
                 let remoteTags = try await self.tagsService.getTags(
-                    number: 100,
-                    offset: offset,
-                    orderBy: self.isBrowseMode ? .byCount : .byName,
-                    order: self.isBrowseMode ? .orderDescending : .orderAscending
+                    page: page,
+                    recentlyUsed: self.isBrowseMode
                 )
 
                 let hasMore = remoteTags.count == 100
-                let nextPage = hasMore ? offset + 100 : nil
+                let nextPage = hasMore ? page + 1 : nil
 
                 return TagsPaginatedResponse.Page(
                     items: remoteTags,
@@ -119,8 +132,8 @@ class TagsViewModel: ObservableObject {
         }
     }
 
-    func toggleSelection(for tag: RemotePostTag) {
-        guard let tagName = tag.name else { return }
+    func toggleSelection(for term: AnyTermWithViewContext) {
+        let tagName = term.name
         let lowercasedTagName = tagName.lowercased()
         if selectedTagsSet.contains(lowercasedTagName) {
             selectedTagsSet.remove(lowercasedTagName)
@@ -142,20 +155,19 @@ class TagsViewModel: ObservableObject {
         // Create a new tag in the background, which is consistent with the web editor.
         Task {
             do {
-                _ = try await tagsService.createTag(named: name)
+                _ = try await tagsService.createTag(name: name, description: "")
             } catch {
                 removeSelectedTag(name)
             }
         }
     }
 
-    func isSelected(_ tag: RemotePostTag) -> Bool {
-        guard let tagName = tag.name else { return false }
-        return selectedTagsSet.contains(tagName.lowercased())
+    func isSelected(_ term: AnyTermWithViewContext) -> Bool {
+        return selectedTagsSet.contains(term.name.lowercased())
     }
 
-    func isNotSelected(_ tag: RemotePostTag) -> Bool {
-        !isSelected(tag)
+    func isNotSelected(_ term: AnyTermWithViewContext) -> Bool {
+        !isSelected(term)
     }
 
     func removeSelectedTag(_ tagName: String) {
@@ -177,4 +189,65 @@ extension Foundation.Notification.Name {
 struct TagNotificationUserInfoKeys {
     static let tagID = "tagID"
     static let tag = "tag"
+}
+
+struct TaxonomyLocalizedLabels {
+    var name: String
+    var empty: String
+    var emptyDescription: String
+    var searchPlaceholder: String
+
+    static func from(taxonomy: SiteTaxonomy) -> Self {
+        Self(
+            name: taxonomy.localizedName,
+            empty: (taxonomy.details.labels[.noTerms] ?? nil)
+                ?? String.localizedStringWithFormat(Strings.defaultNoTermsFormat, taxonomy.details.name),
+            emptyDescription: String.localizedStringWithFormat(Strings.defaultEmptyDescriptionFormat, taxonomy.details.name),
+            searchPlaceholder: (taxonomy.details.labels[.searchItems] ?? nil)
+                ?? String.localizedStringWithFormat(Strings.defaultSearchFormat, taxonomy.details.name)
+        )
+    }
+
+    static var tag: Self {
+        Self(
+            name: NSLocalizedString(
+                "tags.title",
+                value: "Tags",
+                comment: "Title for the tags screen"
+            ),
+            empty: NSLocalizedString(
+                "tags.empty.title",
+                value: "No Tags",
+                comment: "Title for empty state when there are no tags"
+            ),
+            emptyDescription: NSLocalizedString(
+                "tags.empty.description",
+                value: "Tags help organize your content and make it easier for readers to find related posts.",
+                comment: "Description for empty state when there are no tags"
+            ),
+            searchPlaceholder: NSLocalizedString(
+                "tags.search.placeholder",
+                value: "Search tags",
+                comment: "Placeholder text for the tag search field"
+            )
+        )
+    }
+}
+
+private enum Strings {
+    static let defaultNoTermsFormat = NSLocalizedString(
+        "localizedLabels.defaultNoTerms.format",
+        value: "No %1$@",
+        comment: "Default empty state message format when there are no terms. %1$@ is the taxonomy name."
+    )
+    static let defaultSearchFormat = NSLocalizedString(
+        "localizedLabels.defaultSearch.format",
+        value: "Search %1$@",
+        comment: "Default search placeholder format. %1$@ is the taxonomy name."
+    )
+    static let defaultEmptyDescriptionFormat = NSLocalizedString(
+        "tags.empty.description",
+        value: "%1$@ help organize your content and make it easier for readers to find related posts.",
+        comment: "Description for empty state when there are no tags. %1$@ is the taxonomy name."
+    )
 }

WordPress/Classes/ViewRelated/Blog/Site Settings/SiteSettingsViewController+Swift.swift
@@ -3,6 +3,9 @@ import SwiftUI
 import WordPressData
 import WordPressFlux
 import WordPressShared
+import WordPressAPI
+import WordPressAPIInternal
+import WordPressCore
 
 extension SiteSettingsViewController {
     // MARK: - General
@@ -50,6 +53,26 @@ extension SiteSettingsViewController {
     @objc public func showTagList() {
         let tagsVC = SiteTagsViewController(blog: blog)
         navigationController?.pushViewController(tagsVC, animated: true)
+    }
+
+    @objc public func showCustomTaxonomies() {
+        let viewController: UIViewController
+        if let client = try? WordPressClient(site: .init(blog: blog)) {
+            let rootView = SiteCustomTaxonomiesView(blog: self.blog, api: client.api)
+            viewController = UIHostingController(rootView: rootView)
+        } else {
+            let feature = NSLocalizedString(
+                "applicationPasswordRequired.feature.customTaxonomies",
+                value: "Taxonomies Management",
+                comment: "Feature name for managing terms and taxonomies in the app"
+            )
+            let rootView = ApplicationPasswordRequiredView(blog: self.blog, localizedFeatureName: feature, presentingViewController: self) { client in
+                SiteCustomTaxonomiesView(blog: self.blog, api: client.api)
+            }
+            viewController = UIHostingController(rootView: rootView)
+        }
+
+        self.navigationController?.pushViewController(viewController, animated: true)
     }
 
     // MARK: - Timezone

WordPress/Classes/ViewRelated/Blog/Site Settings/SiteSettingsViewController.m
@@ -33,6 +33,7 @@ NS_ENUM(NSInteger, SiteSettingsEditor) {
 NS_ENUM(NSInteger, SiteSettingsWriting) {
     SiteSettingsWritingDefaultCategory = 0,
     SiteSettingsWritingTags,
+    SiteSettingsWritingCustomTaxonomies,
     SiteSettingsWritingDefaultPostFormat,
     SiteSettingsWritingRelatedPosts,
     SiteSettingsWritingDateAndTimeFormat,
@@ -62,6 +63,7 @@ NS_ENUM(NSInteger, SiteSettingsJetpack) {
 @property (nonatomic, strong) SwitchTableViewCell  *editorSelectorCell;
 @property (nonatomic, strong) SettingTableViewCell *defaultCategoryCell;
 @property (nonatomic, strong) SettingTableViewCell *tagsCell;
+@property (nonatomic, strong) SettingTableViewCell *customTaxonomiesCell;
 @property (nonatomic, strong) SettingTableViewCell *defaultPostFormatCell;
 @property (nonatomic, strong) SettingTableViewCell *relatedPostsCell;
 @property (nonatomic, strong) SettingTableViewCell *dateAndTimeFormatCell;
@@ -204,6 +206,7 @@ NS_ENUM(NSInteger, SiteSettingsJetpack) {
     NSMutableArray *rows = [NSMutableArray arrayWithObjects:
                             @(SiteSettingsWritingDefaultCategory),
                             @(SiteSettingsWritingTags),
+                            @(SiteSettingsWritingCustomTaxonomies),
                             @(SiteSettingsWritingDefaultPostFormat), nil];
 
     BOOL jetpackFeaturesEnabled = [JetpackFeaturesRemovalCoordinator jetpackFeaturesEnabled];
@@ -364,6 +367,17 @@ NS_ENUM(NSInteger, SiteSettingsJetpack) {
                                                               editable: self.blog.isAdmin
                                                        reuseIdentifier:nil];
     return _tagsCell;
+}
+
+- (SettingTableViewCell *)customTaxonomiesCell
+{
+    if (_customTaxonomiesCell){
+        return _customTaxonomiesCell;
+    }
+    _customTaxonomiesCell = [[SettingTableViewCell alloc] initWithLabel:NSLocalizedString(@"Taxonomies", @"Label for viewing the custom taxonomies")
+                                                               editable:self.blog.isAdmin
+                                                        reuseIdentifier:nil];
+    return _customTaxonomiesCell;
 }
 
 - (SettingTableViewCell *)defaultPostFormatCell
@@ -532,6 +546,9 @@ NS_ENUM(NSInteger, SiteSettingsJetpack) {
 
         case (SiteSettingsWritingTags):
             return self.tagsCell;
+
+        case (SiteSettingsWritingCustomTaxonomies):
+            return self.customTaxonomiesCell;
 
         case (SiteSettingsWritingDefaultPostFormat):
             [self configureDefaultPostFormatCell];
@@ -856,6 +873,10 @@ NS_ENUM(NSInteger, SiteSettingsJetpack) {
 
         case SiteSettingsWritingTags:
             [self showTagList];
+            break;
+
+        case SiteSettingsWritingCustomTaxonomies:
+            [self showCustomTaxonomies];
             break;
 
         case SiteSettingsWritingDefaultPostFormat:

WordPress/Classes/ViewRelated/Tags/EditTagView.swift
@@ -2,21 +2,22 @@ import SwiftUI
 import WordPressUI
 import WordPressKit
 import WordPressData
+import WordPressAPI
 import SVProgressHUD
 
 struct EditTagView: View {
     @Environment(\.dismiss) private var dismiss
     @StateObject private var viewModel: EditTagViewModel
 
-    init(tag: RemotePostTag?, tagsService: TagsService) {
-        self._viewModel = StateObject(wrappedValue: EditTagViewModel(tag: tag, tagsService: tagsService))
+    init(term: AnyTermWithViewContext?, taxonomy: SiteTaxonomy?, tagsService: TaxonomyServiceProtocol) {
+        self._viewModel = StateObject(wrappedValue: EditTagViewModel(term: term, taxonomy: taxonomy, tagsService: tagsService))
     }
 
     var body: some View {
         Form {
-            Section(Strings.tagSectionHeader) {
+            Section {
                 HStack {
-                    TextField(Strings.tagNamePlaceholder, text: $viewModel.tagName)
+                    TextField(viewModel.localizedLabels.newPlaceholder, text: $viewModel.tagName)
                         .textFieldStyle(.plain)
                         .autocorrectionDisabled()
                         .textInputAutocapitalization(.never)
@@ -31,12 +32,24 @@ struct EditTagView: View {
                         }
                     }
                 }
+            } header: {
+                Text(Strings.tagSectionHeader)
+            } footer: {
+                if let text = viewModel.localizedLabels.nameFieldDescription {
+                    Text(verbatim: text)
+                }
             }
 
-            Section(Strings.descriptionSectionHeader) {
+            Section {
                 TextField(Strings.descriptionPlaceholder, text: $viewModel.tagDescription, axis: .vertical)
                     .textFieldStyle(.plain)
                     .lineLimit(5...15)
+            } header: {
+                Text(Strings.descriptionSectionHeader)
+            } footer: {
+                if let text = viewModel.localizedLabels.descriptionFieldDescription {
+                    Text(verbatim: text)
+                }
             }
 
             if viewModel.isExistingTag {
@@ -98,38 +111,39 @@ class EditTagViewModel: ObservableObject {
     @Published var showError = false
     @Published var errorMessage = ""
 
-    private let originalTag: RemotePostTag?
-    private let tagsService: TagsService
+    private let originalTerm: AnyTermWithViewContext?
+    private let tagsService: TaxonomyServiceProtocol
+    fileprivate let localizedLabels: LocalizedLabels
 
     var isExistingTag: Bool {
-        originalTag != nil
+        originalTerm != nil
     }
 
     var navigationTitle: String {
-        originalTag?.name ?? Strings.newTagTitle
+        originalTerm?.name ?? localizedLabels.newItemTitle
     }
 
-    init(tag: RemotePostTag?, tagsService: TagsService) {
-        self.originalTag = tag
+    init(term: AnyTermWithViewContext?, taxonomy: SiteTaxonomy?, tagsService: TaxonomyServiceProtocol) {
+        self.originalTerm = term
+        self.localizedLabels = taxonomy.flatMap(LocalizedLabels.from) ?? .tag
         self.tagsService = tagsService
-        self.tagName = tag?.name ?? ""
-        self.tagDescription = tag?.tagDescription ?? ""
+        self.tagName = term?.name ?? ""
+        self.tagDescription = term?.description ?? ""
     }
 
     func deleteTag() async -> Bool {
-        guard let tag = originalTag else { return false }
+        guard let term = originalTerm else { return false }
 
         SVProgressHUD.show()
         defer { SVProgressHUD.dismiss() }
 
         do {
-            try await tagsService.deleteTag(tag)
+            try await tagsService.deleteTag(term)
 
-            // Post notification to update the UI
             NotificationCenter.default.post(
                 name: .tagDeleted,
                 object: nil,
-                userInfo: [TagNotificationUserInfoKeys.tagID: tag.tagID ?? 0]
+                userInfo: [TagNotificationUserInfoKeys.tagID: NSNumber(value: term.id)]
             )
             return true
         } catch {
@@ -143,23 +157,21 @@ class EditTagViewModel: ObservableObject {
         SVProgressHUD.show()
         defer { SVProgressHUD.dismiss() }
 
-        let tagToSave: RemotePostTag
-        if let existingTag = originalTag {
-            tagToSave = existingTag
-        } else {
-            tagToSave = RemotePostTag()
-        }
-
-        tagToSave.name = tagName.trimmingCharacters(in: .whitespacesAndNewlines)
-        tagToSave.tagDescription = tagDescription.trimmingCharacters(in: .whitespacesAndNewlines)
-
         do {
-            let savedTag = try await tagsService.saveTag(tagToSave)
+            let savedTerm: AnyTermWithViewContext
+
+            let tagName = tagName.trimmingCharacters(in: .whitespacesAndNewlines)
+            let tagDescription = tagDescription.trimmingCharacters(in: .whitespacesAndNewlines)
+            if let existingTerm = originalTerm {
+                savedTerm = try await tagsService.updateTag(existingTerm, name: tagName, description: tagDescription)
+            } else {
+                savedTerm = try await tagsService.createTag(name: tagName, description: tagDescription)
+            }
 
             NotificationCenter.default.post(
-                name: originalTag == nil ? .tagCreated : .tagUpdated,
+                name: originalTerm == nil ? .tagCreated : .tagUpdated,
                 object: nil,
-                userInfo: [TagNotificationUserInfoKeys.tag: savedTag]
+                userInfo: [TagNotificationUserInfoKeys.tag: savedTerm]
             )
             return true
         } catch {
@@ -170,10 +182,43 @@ class EditTagViewModel: ObservableObject {
     }
 }
 
+private struct LocalizedLabels {
+    var newPlaceholder: String
+    var newItemTitle: String
+    var nameFieldDescription: String?
+    var descriptionFieldDescription: String?
+
+    static func from(taxonomy: SiteTaxonomy) -> Self {
+        Self(
+            newPlaceholder: (taxonomy.details.labels[.newItemName] ?? nil) ?? "",
+            newItemTitle: (taxonomy.details.labels[.addNewItem] ?? nil) ?? "",
+            nameFieldDescription: taxonomy.details.labels[.nameFieldDescription] ?? nil,
+            descriptionFieldDescription: taxonomy.details.labels[.descFieldDescription] ?? nil
+        )
+    }
+
+    static var tag: Self {
+        Self(
+            newPlaceholder: NSLocalizedString(
+                "edit.tag.name.placeholder",
+                value: "Tag name",
+                comment: "Placeholder text for tag name field"
+            ),
+             newItemTitle: NSLocalizedString(
+                "edit.tag.new.title",
+                value: "New Tag",
+                comment: "Navigation title for new tag creation"
+            ),
+            nameFieldDescription: nil,
+            descriptionFieldDescription: nil
+        )
+    }
+}
+
 private enum Strings {
     static let tagSectionHeader = NSLocalizedString(
         "edit.tag.section.tag",
-        value: "Tag",
+        value: "Name",
         comment: "Section header for tag name in edit tag view"
     )
 
@@ -183,33 +228,21 @@ private enum Strings {
         comment: "Section header for tag description in edit tag view"
     )
 
-    static let tagNamePlaceholder = NSLocalizedString(
-        "edit.tag.name.placeholder",
-        value: "Tag name",
-        comment: "Placeholder text for tag name field"
-    )
-
     static let descriptionPlaceholder = NSLocalizedString(
         "edit.tag.description.placeholder",
         value: "Add a description...",
         comment: "Placeholder text for tag description field"
     )
 
-    static let newTagTitle = NSLocalizedString(
-        "edit.tag.new.title",
-        value: "New Tag",
-        comment: "Navigation title for new tag creation"
-    )
-
     static let deleteConfirmationTitle = NSLocalizedString(
         "edit.tag.delete.confirmation.title",
-        value: "Delete Tag",
-        comment: "Title for delete tag confirmation dialog"
+        value: "Delete",
+        comment: "Title for delete a term confirmation dialog"
     )
 
     static let deleteConfirmationMessage = NSLocalizedString(
         "edit.tag.delete.confirmation.message",
-        value: "Are you sure you want to delete this tag?",
+        value: "Are you sure you want to delete this?",
         comment: "Message for delete tag confirmation dialog"
     )
 }

WordPress/Classes/ViewRelated/Tags/RemotePostTag+Identifiable.swift
@@ -1,8 +1,0 @@
-import Foundation
-import WordPressKit
-
-extension RemotePostTag: @retroactive Identifiable {
-    public var id: Int {
-        return tagID?.intValue ?? 0
-    }
-}

WordPress/Classes/ViewRelated/Tags/SiteCustomTaxonomiesView.swift
@@ -1,0 +1,100 @@
+import Foundation
+import SwiftUI
+import WordPressCore
+import WordPressAPI
+import WordPressAPIInternal
+import WordPressShared
+import WordPressUI
+
+struct SiteCustomTaxonomiesView: View {
+    let blog: Blog
+    let api: WordPressAPI
+
+    @State private var isLoading: Bool = false
+    @State private var taxonomies: [SiteTaxonomy]? = nil
+    @State private var error: Error?
+
+    init(blog: Blog, api: WordPressAPI) {
+        self.blog = blog
+        self.api = api
+    }
+
+    var body: some View {
+        List {
+            ForEach(taxonomies ?? [], id: \.slug) { taxonomy in
+                NavigationLink {
+                    SiteTagsView(viewModel: .init(blog: blog, api: api, taxonomy: taxonomy, mode: .browse))
+                } label: {
+                    Text(taxonomy.localizedName)
+                }
+            }
+        }
+        .overlay {
+            if isLoading {
+                ProgressView()
+            } else if let taxonomies, taxonomies.isEmpty {
+                EmptyStateView(
+                    Strings.emptyTitle,
+                    systemImage: "archivebox",
+                    description: Strings.emptyDescription
+                )
+            } else if let error {
+                EmptyStateView.failure(error: error)
+            }
+        }
+        .navigationTitle(Strings.navigationTitle)
+        .task {
+            await loadTaxonomies()
+        }
+    }
+
+    private func loadTaxonomies() async {
+        guard taxonomies == nil else { return }
+
+        isLoading = true
+        defer { isLoading = false }
+
+        self.error = nil
+        do {
+            let result = try await api.taxonomies.listWithEditContext(params: .init()).data
+            // The "Categories" and "Tags" are displayed in its own views. This view only displays custom taxonomies.
+            let customTaxonomies: [TaxonomyTypeDetailsWithEditContext] = result.taxonomyTypes.compactMap { (type, taxonomy) in
+                switch type {
+                case .category, .navMenu, .postTag, .wpPatternCategory:
+                    nil
+                case .custom:
+                    taxonomy
+                }
+            }
+            self.taxonomies = customTaxonomies.map(SiteTaxonomy.init)
+        } catch {
+            self.error = error
+        }
+    }
+}
+
+private enum Strings {
+    static let navigationTitle = NSLocalizedString(
+        "siteCustomTaxonomies.navigationTitle",
+        value: "Taxonomies",
+        comment: "Navigation title for the custom taxonomies screen"
+    )
+
+    static let defaultAddNew = NSLocalizedString(
+        "siteCustomTaxonomies.defaultAddNew",
+        value: "Add",
+        comment: "Default text for adding a new item when no label is provided"
+    )
+
+    static let emptyTitle = NSLocalizedString(
+        "siteCustomTaxonomies.empty.title",
+        value: "No custom taxonomies",
+        comment: "Title for empty state when there are no custom taxonomies"
+    )
+
+    static let emptyDescription = NSLocalizedString(
+        "siteCustomTaxonomies.empty.description",
+        value: "Taxonomies help you organize content beyond standard categories and tags.",
+        comment: "Description for empty state when there are no custom taxonomies"
+    )
+}

commit def95186830d376328f9f2e30407181fe7f1d520

Invert a boolean check of most recently used tags first
The issue was introduced in #24955. I missed a `!` in the refactor.

WordPress/Classes/ViewRelated/Tags/TagsViewModel.swift
@@ -90,7 +90,7 @@ class TagsViewModel: ObservableObject {
                 let page = pageIndex ?? 0
                 let remoteTags = try await self.tagsService.getTags(
                     page: page,
-                    recentlyUsed: self.isBrowseMode
+                    recentlyUsed: !self.isBrowseMode
                 )
 
                 let hasMore = remoteTags.count == 100