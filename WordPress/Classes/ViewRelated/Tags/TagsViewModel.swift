618227cfece06f634caab9a8c6a4baa79d8aa874
Tony Li <tony.li@automattic.com>
2025-12-17 14:26:11 +1300

    Move finding existing tag code (#25068)
    Moving the code to the view model, so that it can cover tags and custom
    taxonomy terms, which are two seperate `TagsServiceProtocol`
    implementations.

--- WordPress/Classes/ViewRelated/Tags/TagsViewModel.swift
+++ WordPress/Classes/ViewRelated/Tags/TagsViewModel.swift
@@ -159,7 +159,14 @@ class TagsViewModel: ObservableObject {
         // Create a new tag in the background, which is consistent with the web editor.
         Task {
             do {
-                let newTag = try await tagsService.createTag(name: name, description: "")
+                let newTag: AnyTermWithViewContext
+                if let existing = try await tagsService.searchTags(with: name)
+                    .first(where: { $0.name.compare(name, options: .caseInsensitive) == .orderedSame }) {
+                    newTag = existing
+                } else {
+                    newTag = try await tagsService.createTag(name: name, description: "")
+                }
+
                 // The original input `name` was used as a temporary tag before sending the API request.
                 // Replace it with the actual tag returned by the API.
                 if newTag.name != name, let index = selectedTags.firstIndex(of: name) {
