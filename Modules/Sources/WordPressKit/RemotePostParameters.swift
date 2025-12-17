wordpress-mobile/WordPress-iOS

commit a252e96a3051182d322e7e7b86e4647d4222fbe3

Post Settings: Support custom taxonomies (#24964)
* Post Settings: Support custom taxonomies

* Implement comparing terms function in Swift

* Fix a swiftlint issue

Modules/Sources/WordPressKit/RemotePostParameters.swift
@@ -21,6 +21,7 @@ public struct RemotePostCreateParameters: Equatable {
     public var format: String?
     public var isSticky = false
     public var tags: [String] = []
+    public var otherTerms: [String: [String]] = [:]
     public var categoryIDs: [Int] = []
     public var metadata: Set<RemotePostMetadataItem> = []
     public var discussion: RemotePostDiscussionSettings = .default
@@ -52,6 +53,7 @@ public struct RemotePostUpdateParameters: Equatable {
     public var format: String??
     public var isSticky: Bool?
     public var tags: [String]?
+    public var otherTerms: [String: [String]]?
     public var categoryIDs: [Int]?
     public var metadata: Set<RemotePostMetadataItem>?
     public var discussion: RemotePostDiscussionSettings?
@@ -138,6 +140,9 @@ extension RemotePostCreateParameters {
         if previous.tags != tags {
             changes.tags = tags
         }
+        if !RemotePost.compare(otherTerms: previous.otherTerms, withAnother: otherTerms) {
+            changes.otherTerms = otherTerms
+        }
         if Set(previous.categoryIDs) != Set(categoryIDs) {
             changes.categoryIDs = categoryIDs
         }
@@ -190,6 +195,9 @@ extension RemotePostCreateParameters {
         }
         if let tags = changes.tags {
             self.tags = tags
+        }
+        if let otherTerms = changes.otherTerms {
+            self.otherTerms = otherTerms
         }
         if let categoryIDs = changes.categoryIDs {
             self.categoryIDs = categoryIDs
@@ -253,9 +261,19 @@ struct RemotePostCreateParametersWordPressComEncoder: Encodable {
 
         // Posts
         try container.encodeIfPresent(parameters.format, forKey: .format)
+
+        // Terms (including tags)
+        var terms = [String: [String]]()
         if !parameters.tags.isEmpty {
-            try container.encode([RemotePostWordPressComCodingKeys.postTags: parameters.tags], forKey: .terms)
+            terms[RemotePostWordPressComCodingKeys.postTags] = parameters.tags
         }
+        if !parameters.otherTerms.isEmpty {
+            terms.merge(parameters.otherTerms) { old, _ in old }
+        }
+        if !terms.isEmpty {
+            try container.encode(terms, forKey: .terms)
+        }
+
         if !parameters.categoryIDs.isEmpty {
             try container.encodeIfPresent(parameters.categoryIDs, forKey: .categoryIDs)
         }
@@ -330,6 +348,18 @@ struct RemotePostUpdateParametersWordPressComEncoder: Encodable {
             try container.encode(metadata, forKey: .metadata)
         }
 
+        // Terms (including tags)
+        var terms = [String: [String]]()
+        if let tags = parameters.tags, !tags.isEmpty {
+            terms[RemotePostWordPressComCodingKeys.postTags] = tags
+        }
+        if let otherTerms = parameters.otherTerms, !otherTerms.isEmpty {
+            terms.merge(otherTerms) { old, _ in old }
+        }
+        if !terms.isEmpty {
+            try container.encode(terms, forKey: .terms)
+        }
+
         // Pages
         if let parentPageID = parameters.parentPageID {
             try container.encodeNullableID(parentPageID, forKey: .parentPageID)
@@ -337,9 +367,6 @@ struct RemotePostUpdateParametersWordPressComEncoder: Encodable {
 
         // Posts
         try container.encodeIfPresent(parameters.format, forKey: .format)
-        if let tags = parameters.tags {
-            try container.encode([RemotePostWordPressComCodingKeys.postTags: tags], forKey: .terms)
-        }
         try container.encodeIfPresent(parameters.categoryIDs, forKey: .categoryIDs)
         try container.encodeIfPresent(parameters.isSticky, forKey: .isSticky)
         if let discussion = parameters.discussion {
@@ -395,14 +422,23 @@ struct RemotePostCreateParametersXMLRPCEncoder: Encodable {
             try container.encode(metadata, forKey: .metadata)
         }
 
+        // Terms (including tags)
+        var terms = [String: [String]]()
+        if !parameters.tags.isEmpty {
+            terms[RemotePostXMLRPCCodingKeys.taxonomyTag] = parameters.tags
+        }
+        if !parameters.otherTerms.isEmpty {
+            terms.merge(parameters.otherTerms) { old, _ in old }
+        }
+        if !terms.isEmpty {
+            try container.encode(terms, forKey: .termNames)
+        }
+
         // Pages
         try container.encodeIfPresent(parameters.parentPageID, forKey: .parentPageID)
 
         // Posts
         try container.encodeIfPresent(parameters.format, forKey: .format)
-        if !parameters.tags.isEmpty {
-            try container.encode([RemotePostXMLRPCCodingKeys.taxonomyTag: parameters.tags], forKey: .termNames)
-        }
         if !parameters.categoryIDs.isEmpty {
             try container.encode([RemotePostXMLRPCCodingKeys.taxonomyCategory: parameters.categoryIDs], forKey: .terms)
         }
@@ -438,6 +474,18 @@ struct RemotePostUpdateParametersXMLRPCEncoder: Encodable {
             try container.encode(metadata, forKey: .metadata)
         }
 
+        // Terms (including tags)
+        var terms = [String: [String]]()
+        if let tags = parameters.tags, !tags.isEmpty {
+            terms[RemotePostXMLRPCCodingKeys.taxonomyTag] = tags
+        }
+        if let otherTerms = parameters.otherTerms, !otherTerms.isEmpty {
+            terms.merge(otherTerms) { old, _ in old }
+        }
+        if !terms.isEmpty {
+            try container.encode(terms, forKey: .termNames)
+        }
+
         // Pages
         if let parentPageID = parameters.parentPageID {
             try container.encodeNullableID(parentPageID, forKey: .parentPageID)
@@ -445,9 +493,6 @@ struct RemotePostUpdateParametersXMLRPCEncoder: Encodable {
 
         // Posts
         try container.encodeStringIfPresent(parameters.format, forKey: .format)
-        if let tags = parameters.tags {
-            try container.encode([RemotePostXMLRPCCodingKeys.taxonomyTag: tags], forKey: .termNames)
-        }
         if let categoryIDs = parameters.categoryIDs {
             try container.encode([RemotePostXMLRPCCodingKeys.taxonomyCategory: categoryIDs], forKey: .terms)
         }

Modules/Sources/WordPressKit/RemotePost.swift
@@ -1,0 +1,10 @@
+import Foundation
+import WordPressKitObjC
+
+extension RemotePost {
+    public static func compare(otherTerms lhs: [String: [String]], withAnother rhs: [String: [String]]) -> Bool {
+        guard lhs.count == rhs.count else { return false }
+
+        return lhs.mapValues { Set($0) } == rhs.mapValues { Set($0) }
+    }
+}

Modules/Sources/WordPressKitObjC/PostServiceRemoteREST.m
@@ -1,6 +1,7 @@
 #import "PostServiceRemoteREST.h"
 #import "RemotePost.h"
 #import "RemotePostCategory.h"
+#import "RemotePostTerm.h"
 #import "FilePart.h"
 #import "WPMapFilterReduce.h"
 #import "DisplayableImageHelper.h"
@@ -453,6 +454,7 @@ static NSString * const RemoteOptionValueOrderByPostID = @"ID";
         post.categories = [self remoteCategoriesFromJSONArray:[categories allValues]];
     }
     post.tags = [self tagNamesFromJSONDictionary:jsonPost[@"tags"]];
+    post.otherTerms = [RemotePostTerm simpleMappingRepresentation:[self otherTermsFromJSONDictionary:jsonPost[@"terms"]]];
 
     post.revisions = [jsonPost arrayForKey:@"revisions"];
 
@@ -522,11 +524,19 @@ static NSString * const RemoteOptionValueOrderByPostID = @"ID";
         parameters[@"categories_by_id"] = [post.categories valueForKey:@"categoryID"];
     }
 
-    if (post.tags) {
-        NSArray *tags = post.tags;
-        NSDictionary *postTags = @{@"post_tag":tags};
-        parameters[@"terms"] = postTags;
+    {
+        NSMutableDictionary *terms = [NSMutableDictionary dictionary];
+        if (post.tags) {
+            terms[@"post_tag"] = post.tags;
+        }
+        [post.otherTerms enumerateKeysAndObjectsUsingBlock:^(NSString *taxonomy, NSArray<NSString *> *theTerms, BOOL *stop) {
+            terms[taxonomy] = theTerms;
+        }];
+        if (terms.count > 0) {
+            parameters[@"terms"] = terms;
+        }
     }
+
     if (post.format) {
         parameters[@"format"] = post.format;
     }
@@ -594,6 +604,26 @@ static NSString * const RemoteOptionValueOrderByPostID = @"ID";
 
 + (NSArray *)tagNamesFromJSONDictionary:(NSDictionary *)jsonTags {
     return [jsonTags allKeys];
+}
+
++ (NSArray<RemotePostTerm *> *)otherTermsFromJSONDictionary:(NSDictionary *)terms {
+    NSMutableArray<RemotePostTerm *> *otherTerms = [NSMutableArray array];
+
+    for (NSString *taxonomySlug in terms) {
+        if ([taxonomySlug isEqualToString:@"category"] ||
+            [taxonomySlug isEqualToString:@"post_tag"] ||
+            [taxonomySlug isEqualToString:@"post_format"]) {
+            continue;
+        }
+
+        NSDictionary *taxonomyTerms = terms[taxonomySlug];
+        for (NSString *termKey in taxonomyTerms) {
+            NSDictionary *termData = taxonomyTerms[termKey];
+            [otherTerms addObject:[[RemotePostTerm alloc] initWithRESTAPIResponse:termData taxonomySlug:taxonomySlug]];
+        }
+    }
+
+    return [otherTerms copy];
 }
 
 /**

Modules/Sources/WordPressKitObjC/PostServiceRemoteXMLRPC.m
@@ -1,6 +1,7 @@
 #import "PostServiceRemoteXMLRPC.h"
 #import "RemotePost.h"
 #import "RemotePostCategory.h"
+#import "RemotePostTerm.h"
 #import "NSMutableDictionary+Helpers.h"
 #import "NSString+Helpers.h"
 #import "WPMapFilterReduce.h"
@@ -333,6 +334,7 @@ static NSString * const RemoteOptionValueOrderByPostID = @"ID";
     NSArray *terms = [xmlrpcDictionary arrayForKey:@"terms"];
     post.tags = [self tagsFromXMLRPCTermsArray:terms];
     post.categories = [self remoteCategoriesFromXMLRPCTermsArray:terms];
+    post.otherTerms = [RemotePostTerm simpleMappingRepresentation:[self otherTermsFromXMLRPCTermsArray:terms]];
 
     post.commentsStatus = [xmlrpcDictionary stringForKey:@"comment_status"];
     post.pingsStatus = [xmlrpcDictionary stringForKey:@"ping_status"];
@@ -370,6 +372,15 @@ static NSString * const RemoteOptionValueOrderByPostID = @"ID";
         return [[category stringForKey:@"taxonomy"] isEqualToString:@"category"];
     }] wpkit_map:^id(NSDictionary *category) {
         return [self remoteCategoryFromXMLRPCDictionary:category];
+    }];
+}
+
++ (NSArray<RemotePostTerm *> *)otherTermsFromXMLRPCTermsArray:(NSArray *)terms {
+    return [[terms wpkit_filter:^BOOL(NSDictionary *category) {
+        return ![[category stringForKey:@"taxonomy"] isEqualToString:@"category"]
+            && ![[category stringForKey:@"taxonomy"] isEqualToString:@"post_tag"];
+    }] wpkit_map:^id(NSDictionary *term) {
+        return [[RemotePostTerm alloc] initWithXMLRPCResponse:term];
     }];
 }
 
@@ -423,6 +434,10 @@ static NSString * const RemoteOptionValueOrderByPostID = @"ID";
         }];
 
         postParams[@"categories"] = categoryNames;
+    }
+
+    if (post.otherTerms.count > 0) {
+        postParams[@"terms_names"] = post.otherTerms;
     }
 
     if ([post.metadata count] > 0) {

Modules/Sources/WordPressKitObjC/RemotePostTerm.m
@@ -1,0 +1,74 @@
+#import "RemotePostTerm.h"
+
+@import NSObject_SafeExpectations;
+
+@implementation RemotePostTerm
+
+- (instancetype)initWithXMLRPCResponse:(NSDictionary *)response
+{
+    self = [super init];
+    if (self) {
+        self.termID = [response numberForKey:@"term_id"];
+        self.name = [response stringForKey:@"name"];
+        self.slug = [response stringForKey:@"slug"];
+        self.taxonomySlug = [response stringForKey:@"taxonomy"];
+        self.termDescription = [response stringForKey:@"description"];
+        self.count = [response numberForKey:@"count"];
+    }
+    return self;
+}
+
+- (instancetype)initWithRESTAPIResponse:(NSDictionary *)response taxonomySlug:(NSString *)taxonomySlug
+{
+    self = [super init];
+    if (self) {
+        self.termID = [response numberForKey:@"ID"];
+        self.name = [response stringForKey:@"name"];
+        self.slug = [response stringForKey:@"slug"];
+        self.taxonomySlug = taxonomySlug;
+        self.termDescription = [response stringForKey:@"description"];
+        self.count = [response numberForKey:@"post_count"];
+    }
+    return self;
+}
+
+- (NSDictionary *)RESTAPIRepresentation
+{
+    return @{
+        @"ID": self.termID,
+        @"name": self.name,
+        @"slug": self.slug,
+        @"description": self.termDescription ?: @"",
+        @"post_count": self.count
+    };
+}
+
++ (NSDictionary<NSString *, NSArray<NSString *> *> *)simpleMappingRepresentation:(NSArray<RemotePostTerm *> *)terms
+{
+    NSMutableDictionary<NSString *, NSMutableArray<NSString *> *> *dict = [NSMutableDictionary dictionary];
+    for (RemotePostTerm *term in terms) {
+        NSMutableArray *termNames = dict[term.taxonomySlug];
+        if (termNames == nil) {
+            termNames = [NSMutableArray array];
+            dict[term.taxonomySlug] = termNames;
+        }
+        [termNames addObject:term.name];
+    }
+    return [dict copy];
+}
+
+- (NSString *)debugDescription
+{
+    NSDictionary *properties = @{
+                                 @"ID": self.termID,
+                                 @"name": self.name
+                                 };
+    return [NSString stringWithFormat:@"<%@: %p> (%@)", NSStringFromClass([self class]), self, properties];
+}
+
+- (NSString *)description
+{
+    return [NSString stringWithFormat:@"<%@: %p> %@[%@]", NSStringFromClass([self class]), self, self.name, self.termID];
+}
+
+@end

Modules/Sources/WordPressKitObjC/include/RemotePost.h
@@ -1,5 +1,7 @@
 #import <Foundation/Foundation.h>
+
 @class RemotePostAutosave;
+@class RemotePostTerm;
 
 extern NSString * const PostStatusDraft;
 extern NSString * const PostStatusPending;
@@ -54,6 +56,7 @@ extern NSString * const PostStatusDeleted;
 @property (nonatomic, strong) NSArray *categories;
 @property (nonatomic, strong) NSArray *revisions;
 @property (nonatomic, strong) NSArray *tags;
+@property (nonatomic, strong) NSDictionary<NSString *, NSArray<NSString *> *> *otherTerms;
 @property (nonatomic, strong) NSString *pathForDisplayImage;
 @property (nonatomic, assign) NSNumber *isStickyPost;
 @property (nonatomic, assign) BOOL isFeaturedImageChanged;

Modules/Sources/WordPressKitObjC/include/RemotePostTerm.h
@@ -1,0 +1,23 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RemotePostTerm : NSObject
+
+@property (nonatomic, strong) NSNumber *termID;
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *slug;
+@property (nonatomic, strong) NSString *taxonomySlug;
+@property (nonatomic, strong) NSString *termDescription;
+@property (nonatomic, strong) NSNumber *count;
+
+- (instancetype)initWithXMLRPCResponse:(NSDictionary *)response;
+- (instancetype)initWithRESTAPIResponse:(NSDictionary *)response taxonomySlug:(NSString *)taxonomySlug;
+
+- (NSDictionary *)RESTAPIRepresentation;
+
++ (NSDictionary<NSString *, NSArray<NSString *> *> *)simpleMappingRepresentation:(NSArray<RemotePostTerm *> *)terms;
+
+@end
+
+NS_ASSUME_NONNULL_END

Sources/WordPressData/Blog+Extensions.swift
@@ -1,6 +1,7 @@
 import Foundation
 import CoreData
 import WordPressShared
+import WordPressAPI
 
 extension Blog {
 
@@ -79,5 +80,15 @@ extension Blog {
         return roles.sorted { lhs, rhs in
             (lhs.order?.intValue ?? 0) < (rhs.order?.intValue ?? 0)
         }
+    }
+
+    public var taxonomies: [SiteTaxonomy] {
+        get throws {
+            try rawTaxonomies.flatMap { try JSONDecoder().decode([SiteTaxonomy].self, from: $0) } ?? []
+        }
+    }
+
+    public func setTaxonomies(_ taxonomies: [SiteTaxonomy]) throws {
+        self.rawTaxonomies = try JSONEncoder().encode(taxonomies)
     }
 }

commit c81ff3966246c868980f4b9e8d3ae46fd6b7f0a6

Fix an issue where the app fails to remove tags from posts
This is a regression introduced in #24964. We should not check if values
are an empty list in the "update" parameter types.

Modules/Sources/WordPressKit/RemotePostParameters.swift
@@ -350,10 +350,10 @@ struct RemotePostUpdateParametersWordPressComEncoder: Encodable {
 
         // Terms (including tags)
         var terms = [String: [String]]()
-        if let tags = parameters.tags, !tags.isEmpty {
+        if let tags = parameters.tags {
             terms[RemotePostWordPressComCodingKeys.postTags] = tags
         }
-        if let otherTerms = parameters.otherTerms, !otherTerms.isEmpty {
+        if let otherTerms = parameters.otherTerms {
             terms.merge(otherTerms) { old, _ in old }
         }
         if !terms.isEmpty {
@@ -476,10 +476,10 @@ struct RemotePostUpdateParametersXMLRPCEncoder: Encodable {
 
         // Terms (including tags)
         var terms = [String: [String]]()
-        if let tags = parameters.tags, !tags.isEmpty {
+        if let tags = parameters.tags {
             terms[RemotePostXMLRPCCodingKeys.taxonomyTag] = tags
         }
-        if let otherTerms = parameters.otherTerms, !otherTerms.isEmpty {
+        if let otherTerms = parameters.otherTerms {
             terms.merge(otherTerms) { old, _ in old }
         }
         if !terms.isEmpty {