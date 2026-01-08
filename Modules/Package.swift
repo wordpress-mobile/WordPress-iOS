49755f9a05119b13ea461bce8b7f3d194436a89e
Gio Lodi <gio@mokacoding.com>
2026-01-08 17:26:25 +1100

    Switch GutenbergKit to branch using HTML resources XCFramework

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -55,7 +55,11 @@ let package = Package(
         .package(url: "https://github.com/wordpress-mobile/wpxmlrpc", from: "0.9.0"),
         .package(url: "https://github.com/wordpress-mobile/NSURL-IDN", revision: "b34794c9a3f32312e1593d4a3d120572afa0d010"),
         .package(url: "https://github.com/zendesk/support_sdk_ios", from: "8.0.3"),
-        .package(url: "https://github.com/wordpress-mobile/GutenbergKit", from: "0.10.1"),
+        // To test https://github.com/wordpress-mobile/GutenbergKit/pull/271
+        .package(
+            url: "https://github.com/wordpress-mobile/GutenbergKit",
+            revision: "6cbc42aa40441375dbc071f7f390a5e7f1f9403e"
+        ),
         // We can't use wordpress-rs branches nor commits here. Only tags work.
         .package(url: "https://github.com/Automattic/wordpress-rs", revision: "alpha-20251101"),
         .package(
