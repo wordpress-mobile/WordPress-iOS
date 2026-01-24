7e2cea64b317d2a596f1f16e4a8bbbc676190875
Tony Li <tony.li@automattic.com>
2026-01-24 22:57:33 +1300

    Move extension functions out to its own file

--- WordPress/Classes/ViewRelated/NewGutenberg/NewGutenbergViewController.swift
+++ WordPress/Classes/ViewRelated/NewGutenberg/NewGutenbergViewController.swift
@@ -341,33 +341,6 @@ extension NewGutenbergViewController: GutenbergKit.EditorViewControllerDelegate 
     }
 }
 
-extension GutenbergKit.EditorViewControllerDelegate {
-    func editor(_ viewController: GutenbergKit.EditorViewController, didLogNetworkRequest request: GutenbergKit.RecordedNetworkRequest) {
-        guard ExtensiveLogging.enabled, let url = URL(string: request.url) else {
-            return
-        }
-
-        var urlRequest = URLRequest(url: url)
-        urlRequest.httpMethod = request.method
-        urlRequest.allHTTPHeaderFields = request.requestHeaders
-        urlRequest.httpBody = request.requestBody?.data(using: .utf8)
-
-        let httpResponse = HTTPURLResponse(
-            url: url,
-            statusCode: request.status,
-            httpVersion: nil,
-            headerFields: request.responseHeaders
-        )
-
-        LoggerStore.shared.storeRequest(
-            urlRequest,
-            response: httpResponse,
-            error: nil,
-            data: request.responseBody?.data(using: .utf8)
-        )
-    }
-}
-
 // MARK: - GutenbergBridgeDelegate
 
 extension NewGutenbergViewController {
@@ -626,20 +599,3 @@ private extension NewGutenbergViewController {
 // Extend Gutenberg JavaScript exception struct to conform the protocol defined in the Crash Logging service
 extension GutenbergJSException.StacktraceLine: @retroactive AutomatticTracks.JSStacktraceLine {}
 extension GutenbergJSException: @retroactive AutomatticTracks.JSException {}
-
-private func getLocalizedString(for value: GutenbergKit.EditorLocalizableString) -> String {
-    switch value {
-    case .showMore: NSLocalizedString("editor.blockInserter.showMore", value: "Show More", comment: "Button title to expand and show more blocks")
-    case .showLess: NSLocalizedString("editor.blockInserter.showLess", value: "Show Less", comment: "Button title to collapse and show fewer blocks")
-    case .search: NSLocalizedString("editor.blockInserter.search", value: "Search", comment: "Placeholder text for block search field")
-    case .insertBlock: NSLocalizedString("editor.blockInserter.insertBlock", value: "Insert Block", comment: "Context menu action to insert a block")
-    case .failedToInsertMedia: NSLocalizedString("editor.media.failedToInsert", value: "Failed to insert media", comment: "Error message when media insertion fails")
-    case .patterns: NSLocalizedString("editor.patterns.title", value: "Patterns", comment: "Navigation title for patterns view")
-    case .noPatternsFound: NSLocalizedString("editor.patterns.noPatternsFound", value: "No Patterns Found", comment: "Title shown when no patterns match the search")
-    case .insertPattern: NSLocalizedString("editor.patterns.insertPattern", value: "Insert Pattern", comment: "Context menu action to insert a pattern")
-    case .patternsCategoryUncategorized: NSLocalizedString("editor.patterns.uncategorized", value: "Uncategorized", comment: "Category name for patterns without a category")
-    case .patternsCategoryAll: NSLocalizedString("editor.patterns.all", value: "All", comment: "Category name for section showing all patterns")
-    case .loadingEditor: NSLocalizedString("editor.loading.title", value: "Loading Editor", comment: "Text shown while the editor is loading")
-    case .editorError: NSLocalizedString("editor.error.title", value: "Editor Error", comment: "Title shown when the editor encounters an error")
-    }
-}
