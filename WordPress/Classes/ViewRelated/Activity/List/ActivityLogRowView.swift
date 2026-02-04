0b0c921b757e28b88d81715bc32e3be8363508f0
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-02-04 14:31:47 -0500

    Update Activity Log design

--- WordPress/Classes/ViewRelated/Activity/List/ActivityLogRowView.swift
+++ WordPress/Classes/ViewRelated/Activity/List/ActivityLogRowView.swift
@@ -6,44 +6,53 @@ struct ActivityLogRowView: View {
     let viewModel: ActivityLogRowViewModel
 
     var body: some View {
-        HStack(alignment: .center, spacing: 12) {
+        HStack(alignment: .top, spacing: 12) {
             icon
+                .offset(y: -2)
 
             VStack(alignment: .leading, spacing: 4) {
-                HStack {
-                    Text(viewModel.subtitle)
-                        .font(.caption)
+                HStack(alignment: .firstTextBaseline, spacing: 0) {
+                    Text(viewModel.title)
+                        .font(.callout)
                         .fontWeight(.medium)
-                        .foregroundStyle(.secondary)
-                    Spacer()
+                        .foregroundStyle(.primary)
+                    Spacer(minLength: 9)
                     Text(viewModel.time)
-                        .font(.caption2)
+                        .font(.caption)
                         .foregroundColor(.secondary)
                 }
 
-                Text(viewModel.title.isEmpty ? "—" : viewModel.title)
-                    .font(.subheadline)
-                    .lineLimit(2)
-                    .foregroundColor(viewModel.title.isEmpty ? .secondary : .primary)
+                // Details text
+                if !viewModel.subtitle.isEmpty {
+                    Text(viewModel.subtitle)
+                        .font(.subheadline)
+                        .lineLimit(2)
+                        .foregroundColor(.secondary)
+                }
 
-                if let actor = viewModel.activity.actor {
-                    HStack(spacing: 6) {
-                        ActivityActorAvatarView(actor: actor, diameter: 16)
-                        HStack(spacing: 4) {
-                            Text(actor.displayName.isEmpty ? Activity.Strings.unknownUser : actor.displayName)
-                                .font(.footnote)
-                                .foregroundColor(.secondary)
-                            if let subtitle = viewModel.actorSubtitle {
-                                Text("·")
-                                    .font(.footnote)
-                                    .foregroundColor(.secondary)
-                                Text(subtitle)
-                                    .font(.footnote)
-                                    .foregroundColor(.secondary)
-                            }
-                        }
-                    }
+                metadata
                     .padding(.top, 4)
+            }
+        }
+    }
+
+    @ViewBuilder
+    private var metadata: some View {
+        if let actor = viewModel.activity.actor {
+            HStack(alignment: .center, spacing: 0) {
+                HStack(spacing: 6) {
+                    ActivityActorAvatarView(actor: actor, diameter: 16)
+                    Text(actor.displayName.isEmpty ? Activity.Strings.unknownUser : actor.displayName)
+                        .font(.footnote)
+                        .foregroundColor(.secondary)
+                    if let subtitle = viewModel.actorSubtitle {
+                        Text("·")
+                            .font(.footnote)
+                            .foregroundColor(.secondary)
+                        Text(subtitle)
+                            .font(.footnote)
+                            .foregroundColor(.secondary)
+                    }
                 }
             }
         }
@@ -53,14 +62,14 @@ struct ActivityLogRowView: View {
         ZStack {
             RoundedRectangle(cornerRadius: 10)
                 .fill(viewModel.tintColor.opacity(0.15))
-                .frame(width: 36, height: 36)
+                .frame(width: 32, height: 32)
 
             if let iconImage = viewModel.icon {
                 Image(uiImage: iconImage)
                     .renderingMode(.template)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: 20, height: 20)
+                    .frame(width: 18, height: 18)
                     .foregroundColor(viewModel.tintColor)
             }
         }
