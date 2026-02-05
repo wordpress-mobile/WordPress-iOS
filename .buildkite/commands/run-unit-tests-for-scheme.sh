78efb86adff60e1d6599f9153e180266be7fb914
Tony Li <tony.li@automattic.com>
2026-02-05 20:31:27 +1300

    Remove the os version argument

--- .buildkite/commands/run-unit-tests-for-scheme.sh
+++ .buildkite/commands/run-unit-tests-for-scheme.sh
@@ -2,7 +2,6 @@
 
 SCHEME="${1:?Usage $0 SCHEME}"
 DEVICE="iPhone 17"
-OS_VERSION="26.0"
 
 if "$(dirname "${BASH_SOURCE[0]}")/should-skip-job.sh" --job-type validation; then
   exit 0
@@ -12,6 +11,6 @@ $(dirname "${BASH_SOURCE[0]}")/shared-set-up.sh
 
 xcodebuild \
   -scheme "${SCHEME}" \
-  -destination "platform=iOS Simulator,OS=${OS_VERSION},name=${DEVICE}" \
+  -destination "platform=iOS Simulator,name=${DEVICE}" \
   test \
   | xcbeautify
