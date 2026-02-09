8a0c9a4205853ca1682ef8b102f781487a9bac34
Tony Li <tony.li@automattic.com>
2026-02-10 10:16:03 +1300

    Show pinned post types on My Site

--- WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Quick Actions/DashboardQuickActionCell.swift
+++ WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Cards/Quick Actions/DashboardQuickActionCell.swift
@@ -28,6 +28,8 @@ final class DashboardQuickActionCell: UITableViewCell {
         titleLabel.adjustsFontForContentSizeCategory = true
 
         iconView.tintColor = .label
+        iconView.contentMode = .center
+        iconView.widthAnchor.constraint(equalToConstant: 24).isActive = true
 
         let spacer = UIView()
         spacer.translatesAutoresizingMaskIntoConstraints = false
--- WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Service/BlogDashboardPersonalizationService.swift
+++ WordPress/Classes/ViewRelated/Blog/Blog Dashboard/Service/BlogDashboardPersonalizationService.swift
@@ -136,7 +136,7 @@ struct BlogDashboardPersonalizationService {
 }
 
 private func makeKey(for action: DashboardQuickAction) -> String {
-    "quick-action-\(action.rawValue)-hidden"
+    "quick-action-\(action.settingsKey)-hidden"
 }
 
 extension NSNotification.Name {
