d0d04f31b9ee08d1a76c1045e1be0ad01b04f122
Tony Li <tony.li@automattic.com>
2026-02-17 13:25:58 +1300

    Update GutenbergKit

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -56,7 +56,7 @@ let package = Package(
         .package(url: "https://github.com/wordpress-mobile/wpxmlrpc", from: "0.9.0"),
         .package(url: "https://github.com/wordpress-mobile/NSURL-IDN", revision: "b34794c9a3f32312e1593d4a3d120572afa0d010"),
         .package(url: "https://github.com/zendesk/support_sdk_ios", from: "8.0.3"),
-        .package(url: "https://github.com/wordpress-mobile/GutenbergKit", revision: "c60333ebf4bd37eafc8981cfc8387b71bdfa5dcc"),
+        .package(url: "https://github.com/wordpress-mobile/GutenbergKit", revision: "v0.14.0-alpha.0"),
         // We can't use wordpress-rs branches nor commits here. Only tags work.
         .package(url: "https://github.com/Automattic/wordpress-rs", revision: "alpha-20260210"),
         .package(
