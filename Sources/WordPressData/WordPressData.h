f2860915328b451d5509f6708fdcf2e37db6ea99
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-03-03 14:06:58 -0500

    Remove CocoaLumberJackConfiguration.m

--- Sources/WordPressData/WordPressData.h
+++ Sources/WordPressData/WordPressData.h
@@ -9,5 +9,3 @@ FOUNDATION_EXPORT const unsigned char WordPressDataVersionString[];
 // Note: Some of these might not need to be public, but it was simpler to extract to WordPressData by making everything public.
 // As we'll hopefully soon rewrite these in Swift, we can implement proper access level then.
 #import <WordPressData/Blog.h>
-
-FOUNDATION_EXTERN void SetCocoaLumberjackObjCLogLevel(NSUInteger ddLogLevelRawValue);
