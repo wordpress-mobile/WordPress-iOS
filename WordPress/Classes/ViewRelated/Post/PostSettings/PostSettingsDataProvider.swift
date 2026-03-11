ae151df1a7e330371c87d13bbfd594c46f4cdcfb
Tony Li <tony.li@automattic.com>
2026-03-11 22:42:24 +1300

    Define PostSettingsDataProvider protocol

---
+++ WordPress/Classes/ViewRelated/Post/PostSettings/PostSettingsDataProvider.swift
@@ -0,0 +1,52 @@
+import Foundation
+import WordPressData
+
+@MainActor
+protocol PostSettingsDataProvider: AnyObject {
+    var blog: Blog { get }
+    var capabilities: PostSettingsCapabilities { get }
+
+    // Post identity and state
+    var postContent: String { get }
+    var isScheduled: Bool { get }
+    var isDraftOrPending: Bool { get }
+    var isPost: Bool { get }
+    var postID: Int? { get }
+    var hasRemote: Bool { get }
+    var isDeleted: Bool { get }
+
+    // Display strings
+    var navigationTitle: String { get }
+    var authorFallbackDisplayName: String { get }
+    var suggestedSlug: String? { get }
+    var permalinkTemplate: String? { get }
+    var lastEditedText: String? { get }
+
+    // Settings management
+    func makeSettings() -> PostSettings
+    func makeFeaturedImageViewModel() -> PostSettingsFeaturedImageViewModel?
+    func resolveDisplayedCategories(for settings: PostSettings) -> [String]
+    func customTaxonomies() -> [SiteTaxonomy]
+    func resolveTerms(in settings: inout PostSettings) async
+
+    // Persistence
+    func applyLocally(settings: PostSettings)
+    func save(settings: PostSettings) async throws
+    func publish(settings: PostSettings) async throws
+
+    // Optional features
+    var isEligibleForSocialSharing: Bool { get }
+    func parentPageText(for pageID: Int?) -> String?
+    func suggestedTags() async throws -> [String]
+    var supportsJetpackMetadata: Bool { get }
+}
+
+extension PostSettingsDataProvider {
+    var isEligibleForSocialSharing: Bool { false }
+
+    func parentPageText(for pageID: Int?) -> String? { nil }
+
+    func suggestedTags() async throws -> [String] { [] }
+
+    var supportsJetpackMetadata: Bool { false }
+}
