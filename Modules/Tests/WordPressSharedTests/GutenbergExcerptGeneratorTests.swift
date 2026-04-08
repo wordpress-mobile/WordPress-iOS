8bc54fc53b3fcd2083089980629e13592e2139d9
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-04-08 12:52:23 -0400

    Improve <br> handling

--- Modules/Tests/WordPressSharedTests/GutenbergExcerptGeneratorTests.swift
+++ Modules/Tests/WordPressSharedTests/GutenbergExcerptGeneratorTests.swift
@@ -14,14 +14,14 @@ struct GutenbergPostExcerptGeneratorTests {
         let content = "<!-- wp:gallery {\"ids\":[2315,2309,2308]} --><figure class=\"wp-block-gallery columns-3 is-cropped\"><ul class=\"blocks-gallery-grid\"><li class=\"blocks-gallery-item\"><figure><img src=\"https://diegotest4.files.wordpress.com/2020/01/img_0005-1-1.jpg\" data-id=\"2315\" class=\"wp-image-2315\"/><figcaption class=\"blocks-gallery-item__caption\">Asdasdasd</figcaption></figure></li><li class=\"blocks-gallery-item\"><figure><img src=\"https://diegotest4.files.wordpress.com/2020/01/img_0111-1-1.jpg\" data-id=\"2309\" class=\"wp-image-2309\"/><figcaption class=\"blocks-gallery-item__caption\">Asdasdasd</figcaption></figure></li><li class=\"blocks-gallery-item\"><figure><img src=\"https://diegotest4.files.wordpress.com/2020/01/img_0004-1.jpg\" data-id=\"2308\" class=\"wp-image-2308\"/><figcaption class=\"blocks-gallery-item__caption\">Adsasdasdasd</figcaption></figure></li></ul></figure><!-- /wp:gallery --><p>Some Content</p>"
 
         let summary = GutenbergExcerptGenerator.firstParagraph(from: content, maxLength: 150)
-        #expect(summary == "Some Content…")
+        #expect(summary == "Some Content")
     }
 
     @Test func summaryForContentWithGallery2() {
         let content = "<p>Before</p>\n<!-- wp:gallery {\"ids\":[2315,2309,2308]} --><figure class=\"wp-block-gallery columns-3 is-cropped\"><ul class=\"blocks-gallery-grid\"><li class=\"blocks-gallery-item\"><figure><img src=\"https://diegotest4.files.wordpress.com/2020/01/img_0005-1-1.jpg\" data-id=\"2315\" class=\"wp-image-2315\"/><figcaption class=\"blocks-gallery-item__caption\">Asdasdasd</figcaption></figure></li><li class=\"blocks-gallery-item\"><figure><img src=\"https://diegotest4.files.wordpress.com/2020/01/img_0111-1-1.jpg\" data-id=\"2309\" class=\"wp-image-2309\"/><figcaption class=\"blocks-gallery-item__caption\">Asdasdasd</figcaption></figure></li><li class=\"blocks-gallery-item\"><figure><img src=\"https://diegotest4.files.wordpress.com/2020/01/img_0004-1.jpg\" data-id=\"2308\" class=\"wp-image-2308\"/><figcaption class=\"blocks-gallery-item__caption\">Adsasdasdasd</figcaption></figure></li></ul></figure><!-- /wp:gallery --><p>After</p>"
 
         let summary = GutenbergExcerptGenerator.firstParagraph(from: content, maxLength: 150)
-        #expect(summary == "Before…")
+        #expect(summary == "Before")
     }
 
     @Test
@@ -29,14 +29,13 @@ struct GutenbergPostExcerptGeneratorTests {
         let content = "<p>Before</p>\n<!-- wp:videopress/video {\"title\":\"demo\",\"description\":\"\",\"id\":5297,\"guid\":\"AbCDe\",\"videoRatio\":56.333333333333336,\"privacySetting\":2,\"allowDownload\":false,\"rating\":\"G\",\"isPrivate\":true,\"duration\":1673} -->\n<figure class=\"wp-block-videopress-video wp-block-jetpack-videopress jetpack-videopress-player\"><div class=\"jetpack-videopress-player__wrapper\">\nhttps://videopress.com/v/AbCDe?resizeToParent=true&amp;cover=true&amp;preloadContent=metadata&amp;useAverageColor=true\n</div></figure>\n<!-- /wp:videopress/video -->\n<p>After</p>"
 
         let summary = GutenbergExcerptGenerator.firstParagraph(from: content, maxLength: 150)
-        #expect(summary == "Before…")
+        #expect(summary == "Before")
     }
 
     @Test func testPostWithBRTags() {
-        let content = #"<p class="wp-block-paragraph">Yes,<br>look behind<br>in remembrance and with gratitude.</p><p class="wp-block-paragraph">Then,<br>stay present,<br>or miss memories in the making.</p>"#
+        let content = #"<p class="wp-block-paragraph">Yes,<br>look behind<br><br>in remembrance and with gratitude.</p><p class="wp-block-paragraph">Then,<br>stay present,<BR />or miss memories in the making.</p>"#
 
         let summary = GutenbergExcerptGenerator.firstParagraph(from: content, maxLength: 150)
-        print(summary)
         #expect(summary == "Yes, look behind in remembrance and with gratitude.")
     }
 }
