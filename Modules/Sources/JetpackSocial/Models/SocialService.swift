aec0e8699c22503ca5049d44db5d100fd9e06b4f
Tony Li <tony.li@automattic.com>
2026-05-08 13:45:55 +1200

    Add code comment about nil-connectURL

--- Modules/Sources/JetpackSocial/Models/SocialService.swift
+++ Modules/Sources/JetpackSocial/Models/SocialService.swift
@@ -11,6 +11,13 @@ public struct SocialService: Identifiable, Hashable, Sendable {
     /// `AccountConfirmationView` uses this to hide the primary row from the picker.
     public let additionalUsersOnly: Bool
     public let isActive: Bool
+    /// URL to load in the WKWebView to connect a social media account in
+    /// this service to Jetpack Social.
+    ///
+    /// Optional only as a parser safety net. The `wpcom/v2/publicize/services`
+    /// schema declares `url` as a non-null string and wordpress-rs deserializes
+    /// it as `String`, so this is nil only when `URL(string:)` rejects a
+    /// malformed wire value, i.e. a server-side anomaly.
     public let connectURL: URL?
 
     public init(
