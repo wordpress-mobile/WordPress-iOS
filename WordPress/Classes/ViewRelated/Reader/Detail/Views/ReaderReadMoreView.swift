6d56cc43361a4308e65db45638089b678878eade
Tony Li <tony.li@automattic.com>
2026-06-26 20:49:33 +1200

    Format ReaderReadMoreView before migration

--- WordPress/Classes/ViewRelated/Reader/Detail/Views/ReaderReadMoreView.swift
+++ WordPress/Classes/ViewRelated/Reader/Detail/Views/ReaderReadMoreView.swift
@@ -3,7 +3,9 @@ import SafariServices
 import WordPressData
 
 // […]
-final class ReaderReadMoreView: UIView, UIAdaptivePresentationControllerDelegate, UIPopoverPresentationControllerDelegate {
+final class ReaderReadMoreView: UIView, UIAdaptivePresentationControllerDelegate,
+    UIPopoverPresentationControllerDelegate
+{ // swiftlint:disable:this opening_brace
     private let textView = UITextView.makeLabel()
     private let infoIconView = UIImageView(image: UIImage(systemName: "info.circle.fill"))
     private var postURL: URL?
@@ -21,9 +23,12 @@ final class ReaderReadMoreView: UIView, UIAdaptivePresentationControllerDelegate
         textView.adjustsFontForContentSizeCategory = true
         textView.isUserInteractionEnabled = false // Ignore taps on a link and disable selection
 
-        let string = NSMutableAttributedString(string: Strings.viewFullPost, attributes: [
-            .font: UIFont.preferredFont(forTextStyle: .body)
-        ])
+        let string = NSMutableAttributedString(
+            string: Strings.viewFullPost,
+            attributes: [
+                .font: UIFont.preferredFont(forTextStyle: .body)
+            ]
+        )
         if let postURL = post.permaLink.flatMap(URL.init) {
             string.addAttribute(.link, value: postURL, range: NSRange(location: 0, length: string.length))
             self.postURL = postURL
@@ -72,7 +77,9 @@ final class ReaderReadMoreView: UIView, UIAdaptivePresentationControllerDelegate
         popoverVC.view.backgroundColor = .systemBackground
         popoverVC.view.addSubview(detailsLabel)
         detailsLabel.pinEdges(insets: UIEdgeInsets(.all, 16))
-        popoverVC.preferredContentSize = popoverVC.view.systemLayoutSizeFitting(CGSize(width: preferredWidth, height: 1200))
+        popoverVC.preferredContentSize = popoverVC.view.systemLayoutSizeFitting(
+            CGSize(width: preferredWidth, height: 1200)
+        )
 
         popoverVC.modalPresentationStyle = .popover
         popoverVC.popoverPresentationController?.delegate = self
@@ -88,7 +95,20 @@ final class ReaderReadMoreView: UIView, UIAdaptivePresentationControllerDelegate
 }
 
 private enum Strings {
-    static let viewFullPost = NSLocalizedString("reader.postDetails.viewFullPost", value: "View full post", comment: "Button title")
-    static let blog = NSLocalizedString("reader.postDetails.blog", value: "blog", comment: "Reader post details view placeholder when blog name not avail")
-    static let details = NSLocalizedString("reader.postDetails.viewModeDescription", value: "The owner of this site only allows us to show a brief summary of their content. To view the full post, you'll have to visit their site.", comment: "Reader post details view")
+    static let viewFullPost = NSLocalizedString(
+        "reader.postDetails.viewFullPost",
+        value: "View full post",
+        comment: "Button title"
+    )
+    static let blog = NSLocalizedString(
+        "reader.postDetails.blog",
+        value: "blog",
+        comment: "Reader post details view placeholder when blog name not avail"
+    )
+    static let details = NSLocalizedString(
+        "reader.postDetails.viewModeDescription",
+        value:
+            "The owner of this site only allows us to show a brief summary of their content. To view the full post, you'll have to visit their site.",
+        comment: "Reader post details view"
+    )
 }
