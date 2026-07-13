f19bd49a7ef9bb17765f90c276c2587a82db5bc4
Tony Li <tony.li@automattic.com>
2026-07-14 11:18:03 +1200

    Update GutenbergKit to the latest version 0.17.2 (#25776)
    * Format GutenbergKit extensions
    
    * Update GutenbergKit dependency
    
    * Add Lockdown Mode release note

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -60,7 +60,7 @@ let package = Package(
             revision: "b34794c9a3f32312e1593d4a3d120572afa0d010"
         ),
         .package(url: "https://github.com/zendesk/support_sdk_ios", from: "8.0.3"),
-        .package(url: "https://github.com/wordpress-mobile/GutenbergKit", from: "0.15.0"),
+        .package(url: "https://github.com/wordpress-mobile/GutenbergKit", from: "0.17.2"),
         .package(
             url: "https://github.com/automattic/wordpress-rs",
             exact: "0.4.0"
