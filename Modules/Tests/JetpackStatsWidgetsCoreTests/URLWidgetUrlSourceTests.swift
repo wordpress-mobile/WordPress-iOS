88a2558b34cefbd9bf8338ae56865e858707b188
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2026-07-22 17:50:52 -0600

    Convert JetpackStatsWidgetsCoreTests to Swift Testing

--- Modules/Tests/JetpackStatsWidgetsCoreTests/URLWidgetUrlSourceTests.swift
+++ Modules/Tests/JetpackStatsWidgetsCoreTests/URLWidgetUrlSourceTests.swift
@@ -1,19 +1,20 @@
 // Notice the import is not @testable.
 // Let's test the behavior of the public interface and interact with the library the same a consumer would.
+import Foundation
 import JetpackStatsWidgetsCore
-import XCTest
+import Testing
 
-final class WidgetUrlSourceTests: XCTestCase {
+struct WidgetUrlSourceTests {
 
-    func testHomeScreenWidgetSource() throws {
-        let url = try XCTUnwrap(URL(string: "https://test"))
+    @Test func testHomeScreenWidgetSource() throws {
+        let url = try #require(URL(string: "https://test"))
         let widgetUrl = url.appendingSource(.homeScreenWidget)
-        XCTAssertEqual(widgetUrl.absoluteString, "https://test?source=widget")
+        #expect(widgetUrl.absoluteString == "https://test?source=widget")
     }
 
-    func testLockScreenWidgetSource() throws {
-        let url = try XCTUnwrap(URL(string: "https://test"))
+    @Test func testLockScreenWidgetSource() throws {
+        let url = try #require(URL(string: "https://test"))
         let widgetUrl = url.appendingSource(.lockScreenWidget)
-        XCTAssertEqual(widgetUrl.absoluteString, "https://test?source=lockscreen_widget")
+        #expect(widgetUrl.absoluteString == "https://test?source=lockscreen_widget")
     }
 }
