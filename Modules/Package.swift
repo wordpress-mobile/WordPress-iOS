131127826e98474cf1265a7380040e7811f478c6
David Calhoun <github@davidcalhoun.me>
2026-07-29 15:17:58 -0400

    build: Update GutenbergKit version

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -64,7 +64,7 @@ let package = Package(
         .package(url: "https://github.com/zendesk/support_sdk_ios", from: "8.0.3"),
         // TODO: Restore a version pin once GutenbergKit#569 ships a release.
         .package(url: "https://github.com/wordpress-mobile/GutenbergKit",
-        revision: "40f35ecb2c8a88ba8759f911f28cf79571e969a5"),
+        revision: "b681e106123c9b11b9bdd7923ea18f227dc5a85f"),
         .package(
             url: "https://github.com/automattic/wordpress-rs",
             exact: "0.6.0"
