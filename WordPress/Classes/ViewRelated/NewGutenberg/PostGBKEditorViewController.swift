2419535d4e54dab68951e5d765b7630b359656dd
Tony Li <tony.li@automattic.com>
2026-07-31 09:48:47 +1200

    Share Gallery media library handling between GutenbergKit editors (#25855)
    * Allow Gutenberg media picker to use a blog
    
    * Share Gallery media library handling between GutenbergKit editors

--- WordPress/Classes/ViewRelated/NewGutenberg/PostGBKEditorViewController.swift
+++ WordPress/Classes/ViewRelated/NewGutenberg/PostGBKEditorViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 import WebKit
 import SafariServices
 import GutenbergKit
+import WordPressData
 import WordPressShared
 import WordPressUI
 
@@ -12,6 +13,8 @@ class PostGBKEditorViewController: UIViewController, GutenbergKit.EditorViewCont
 
     /* private */ let editorViewController: GutenbergKit.EditorViewController
     private let status: String // TODO: Can be deleted?
+
+    private lazy var mediaPickerHelper = GutenbergMediaPickerHelper(context: self, blog: blog)
 
     private var keyboardShowObserver: Any?
     private var keyboardHideObserver: Any?
@@ -200,7 +203,47 @@ class PostGBKEditorViewController: UIViewController, GutenbergKit.EditorViewCont
         _ viewController: GutenbergKit.EditorViewController,
         didRequestMediaFromSiteMediaLibrary config: OpenMediaLibraryAction
     ) {
-        // Do nothing
+        let flags = mediaFilterFlags(using: config.allowedTypes ?? [])
+
+        let initialSelectionArray: [Int]
+        switch config.value {
+        case .single(let id):
+            initialSelectionArray = [id]
+        case .multiple(let ids):
+            initialSelectionArray = ids
+        case .none:
+            initialSelectionArray = []
+        }
+
+        mediaPickerHelper.presentSiteMediaPicker(
+            filter: flags,
+            allowMultipleSelection: config.multiple,
+            initialSelection: initialSelectionArray
+        ) { [weak self] assets in
+            guard let self, let media = assets as? [Media], !media.isEmpty else {
+                return
+            }
+            let mediaInfos = media.map { item in
+                var metadata: [String: String] = [:]
+                if let videopressGUID = item.videopressGUID {
+                    metadata["videopressGUID"] = videopressGUID
+                }
+                return MediaInfo(
+                    id: item.mediaID?.int32Value,
+                    url: item.remoteURL,
+                    type: item.mediaTypeString,
+                    caption: item.caption,
+                    title: item.filename,
+                    alt: item.alt,
+                    metadata: [:]
+                )
+            }
+            if let jsonString = convertMediaInfoArrayToJSONString(mediaInfos) {
+                // Escape the string for JavaScript
+                let escapedJsonString = jsonString.replacingOccurrences(of: "'", with: "\\'")
+                editorViewController.setMediaUploadAttachment(escapedJsonString)
+            }
+        }
     }
 
     func editor(_ viewController: GutenbergKit.EditorViewController, didTriggerAutocompleter type: String) {
@@ -243,6 +286,40 @@ class PostGBKEditorViewController: UIViewController, GutenbergKit.EditorViewCont
     ) -> (title: String, content: String)? {
         // Do nothing
         nil
+    }
+
+    private func convertMediaInfoArrayToJSONString(_ mediaInfoArray: [MediaInfo]) -> String? {
+        do {
+            let jsonData = try JSONEncoder().encode(mediaInfoArray)
+            if let jsonString = String(data: jsonData, encoding: .utf8) {
+                return jsonString
+            }
+        } catch {
+            DDLogError("Error encoding MediaInfo array: \(error)")
+        }
+        return nil
+    }
+
+    private func mediaFilterFlags(using filterArray: [OpenMediaLibraryAction.MediaType]) -> GutenbergMediaType {
+        var mediaType: Int = 0
+        for filter in filterArray {
+            switch filter {
+            case .image:
+                mediaType = mediaType | GutenbergMediaType.image.rawValue
+            case .video:
+                mediaType = mediaType | GutenbergMediaType.video.rawValue
+            case .audio:
+                mediaType = mediaType | GutenbergMediaType.audio.rawValue
+            case .other:
+                mediaType = mediaType | GutenbergMediaType.other.rawValue
+            case .any:
+                mediaType = mediaType | GutenbergMediaType.all.rawValue
+            @unknown default:
+                fatalError()
+            }
+        }
+
+        return GutenbergMediaType(rawValue: mediaType)
     }
 }
 
