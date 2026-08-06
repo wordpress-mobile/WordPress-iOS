b4090e1d1a2d5eb4bff50822384b7db70ae0db5c
David Calhoun <github@davidcalhoun.me>
2026-08-06 17:14:21 -0400

    build: Update to GutenbergKit 0.19.0 (#25874)
    Address CI failures caused by GutenbergKit pre-release semver
    conventions. The `-` character included in the semver spec is
    incompatible with Apple's required naming. We'll need to address the
    naming incompatibility of semver pre-releases in a different manner
    later.

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -62,7 +62,7 @@ let package = Package(
             revision: "b34794c9a3f32312e1593d4a3d120572afa0d010"
         ),
         .package(url: "https://github.com/zendesk/support_sdk_ios", from: "8.0.3"),
-        .package(url: "https://github.com/wordpress-mobile/GutenbergKit", from: "0.19.0-alpha.0"),
+        .package(url: "https://github.com/wordpress-mobile/GutenbergKit", from: "0.19.0"),
         .package(
             url: "https://github.com/automattic/wordpress-rs",
             exact: "0.6.0"
