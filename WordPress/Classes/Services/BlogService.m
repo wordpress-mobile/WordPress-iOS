a28cfa75327dbc3956b5cd4969c1403baa31d8e8
Tony Li <tony.li@automattic.com>
2026-08-12 10:33:30 +1200

    Evict WordPress clients when sync removes sites

--- WordPress/Classes/Services/BlogService.m
+++ WordPress/Classes/Services/BlogService.m
@@ -375,6 +375,7 @@ NSString *const WPBlogSettingsUpdatedNotification = @"WPBlogSettingsUpdatedNotif
         for (Blog *blog in account.blogs) {
             if ([toDelete containsObject:blog.dotComID]) {
                 [self unscheduleBloggingRemindersFor:blog];
+                [self evictWordPressClientForBlog:blog];
                 // Consider switching this to a call to removeBlog in the future
                 // to consolidate behaviour @frosty
                 [context deleteObject:blog];
